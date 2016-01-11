6a46333d67e15492e69be3c6839304f3c3eb7b4a
Hedy <hedywoo@gmail.com>
2016-01-11 16:42:39 -0700

    hola

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,18 +1,27 @@
-require './data/merchants.csv'
+require 'pry'
+require 'csv'
 
 class MerchantRepository
 
   def initialize
-    m = Merchant.new
+
   end
 
 
   def all
-      Merchant.new
   end
 
-  def load_data
+  def load_data(filename)
+    contents = CSV.open filename, headers: true, header_converters: :symbol
+    contents.each do |row|
+      name = row[:name]
+      puts name
+    end
   end
 
 
 end
+
+mr = MerchantRepository.new
+merchant_list = mr.load_data('../data/merchants.csv')
+puts merchant_list
