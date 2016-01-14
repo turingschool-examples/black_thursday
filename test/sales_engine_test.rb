4cae0776a5a166da823ad09862aa65c6165ecabb
Matt Pindell <pindell.m@gmail.com>
2016-01-13 18:32:21 -0700

    test: updated sales_engine tests

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,60 +3,50 @@ require_relative '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
 
-  def test_to_create_a_sales_engine_object_we_use_the_factory
-    se = SalesEngine.from_csv({
+  def setup
+    @se = SalesEngine.from_csv({
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
       })
-    assert_kind_of SalesEngine, se
+  end
+
+  def test_to_create_a_sales_engine_object_we_use_the_factory
+    assert_kind_of SalesEngine, @se
   end
 
   def test_sales_engine_can_create_CSV_objects
+    skip
+    binding.pry
+    submitted =
+    expected =
+
+    assert_equal
+  end
+
+  def test_sales_engine_can_receive_one_csv_file
+
+  end
+
+  def test_sales_engine_can_receive_two_csv_files
 
   end
 
   def test_sales_engine_can_create_Merchant_Repos
-    # self.merchants
+    mr = @se.merchants
+    expected = "#<Merchant:0"
+    submitted = mr.find_by_name("CJsDecor").to_s
+
+    assert submitted.include?(expected)
+    assert_kind_of MerchantRepository, mr
   end
 
   def test_sales_engine_can_create_Items_Repos
-    # self.items
+    ir = @se.items
+    expected = "#<Items:0"
+    submitted = ir.find_by_name("510+ RealPush Icon Set").to_s
+
+    assert submitted.include?(expected)
+    assert_kind_of ItemRepository, ir
   end
 
-  # def test_if_two_files_are_entered_returns_files
-  #   skip
-  #   file_one = "./data/merchants.csv"
-  #   file_two = "./data/items.csv"
-  #   file_array = [file_one, file_two]
-  #   @se = SalesEngine.new(file_one, file_two)
-  #   submitted = @se.file
-  #
-  #   assert_equal file_array, submitted
-  # end
-  #
-  # def test_if_three_files_are_entered_returns_files
-  #   skip
-  #   file_one = "./data/merchants.csv"
-  #   file_two = "./data/items.csv"
-  #   file_three = "./data/invoices.csv"
-  #   file_array = [file_one, file_two, file_three]
-  #   @se = SalesEngine.new(file_one, file_two, file_three)
-  #   submitted = @se.file
-  #
-  #   assert_equal file_array, submitted
-  # end
-  #
-  # def test_receive_command_one_file
-  #   skip
-  #   file = "./data/merchants.csv"
-  #   command = "all"
-  #   @se = SalesEngine.new(file)
-  #   submitted = @se.command
-  #
-  #   assert_equal command, submitted
-  # end
-  #
-  # def test_receive_command_two_files
-  #   skip
-  # end
 end
