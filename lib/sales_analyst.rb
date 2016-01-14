a94c6872e290984f31699baa25d0853512a511f9
Alex <alexxnavarretee@gmail.com>
2016-01-13 23:10:42 -0700

    created the sales analyst class and added tests to the methods that need to exist within it

---
+++ lib/sales_analyst.rb
@@ -0,0 +1,27 @@
+class SalesAnalyst
+
+
+  def average_items_per_merchant
+
+  end
+
+  def average_items_per_merchant_standard_deviation
+
+  end
+
+  def merchants_with_low_item_count
+
+  end
+
+  def average_item_price_for_merchant
+
+  end
+
+  def average_price_per_merchant
+
+  end
+
+  def golden_items
+
+  end
+end
---
+++ test/sales_analyst_test.rb
@@ -0,0 +1,38 @@
+require_relative './../lib/sales_analyst'
+require_relative './test_helper'
+require          'pry'
+require          'minitest/autorun'
+require          'minitest/pride'
+
+
+class SalesAnalystTest < Minitest::Test
+
+  def test_that_class_exist
+    assert SalesAnalyst
+  end
+
+  def test_that_average_items_per_merchant_method_exist
+      assert SalesAnalyst.method_defined? :average_items_per_merchant
+  end
+
+  def test_that_average_items_per_merchant_standard_deviation_method_exist
+      assert SalesAnalyst.method_defined? :average_items_per_merchant_standard_deviation
+  end
+
+  def test_that_merchants_with_low_item_count_method_exist
+      assert SalesAnalyst.method_defined? :merchants_with_low_item_count
+  end
+
+  def test_that_average_item_price_for_merchant_method_exist
+      assert SalesAnalyst.method_defined? :average_item_price_for_merchant
+  end
+
+  def test_that_average_price_per_merchant_method_exist
+      assert SalesAnalyst.method_defined? :average_price_per_merchant
+  end
+
+  def test_that_golden_items_method_exist
+      assert SalesAnalyst.method_defined? :golden_items
+  end
+
+end
