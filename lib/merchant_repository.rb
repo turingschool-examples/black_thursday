a9872022411b1315612a7b141363fb3802ac29a1
Alex <alexxnavarretee@gmail.com>
2016-01-15 06:06:02 -0700

    refactored and redid every fucking test

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -17,29 +17,20 @@ class MerchantRepository
     merchant_instances
   end
 
-  def stdrd(data_to_be_standardized)
+  def standard(data_to_be_standardized)
     data_to_be_standardized.to_s.downcase.gsub(" ", "")
   end
 
-  def find_by_id(merchant_id_inputed)
-    stdrd_merchant_id = stdrd(merchant_id_inputed)
-    found_item_instances =
-      merchant_instances.find {|item| stdrd(item.id) == stdrd_merchant_id}
-    found_item_instances.nil? ? nil : found_item_instances
+  def find_by_id(merchant_id)
+    merchant_instances.find { |item| item.id == merchant_id.to_i}
   end
 
-  def find_by_name(merchant_name_inputed)
-    stdrd_merchant_name = stdrd(merchant_name_inputed)
-    found_item_instances =
-      merchant_instances.find {|item| stdrd(item.name) == stdrd_merchant_name}
-    found_item_instances.nil? ? nil : found_item_instances
+  def find_by_name(merchant_name)
+    merchant_instances.find {|merchant| standard(merchant.name) == standard(merchant_name)}
   end
 
-  def find_all_by_name(merchant_name_inputed)
-    stdrd_merchant_name = stdrd(merchant_name_inputed)
-    found_item_instances =
-      merchant_instances.find_all {|item| item.name.downcase.include?(standard_merchant_name)}
-    found_item_instances.nil? ? [] : found_item_instances
+  def find_all_by_name(merchant_name)
+    merchant_instances.find_all { |merchant| merchant.name.downcase.include? standard(merchant_name) }
   end
 
   def inspect
