adea63897b2a409fb548619928527772cf4eb0e1
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-01-16 21:12:33 -0700

    pulled new spec, working before changes

--- lib/item.rb
+++ lib/item.rb
@@ -10,7 +10,7 @@ class Item
     @name          = item_info[:name]
     @id            = item_info[:id].to_i
     @description   = item_info[:description]
-    @unit_price    = BigDecimal.new((item_info[:unit_price].to_f), 4)
+    @unit_price    = BigDecimal.new(item_info[:unit_price])
     @created_at    = Time.parse(item_info[:created_at])
     @updated_at    = Time.parse(item_info[:updated_at])
     @merchant_id   = item_info[:merchant_id].to_i
