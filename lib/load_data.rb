1206ea52e63a093349fefe777b47120f9e674cc2
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-01-17 15:03:25 -0700

    trying to fix merge issues

--- lib/load_data.rb
+++
@@ -1,8 +0,0 @@
-module LoadData
-  extend self
-  def load_data(data)
-    contents = CSV.open data, headers: true, header_converters: :symbol
-    contents.to_a.map {|row| row.to_hash}
-  end
-
-end
