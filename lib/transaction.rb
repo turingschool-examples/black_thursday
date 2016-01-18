ecb27efda4c5631f67cee213f0557676e053c2f3
Alex <alexxnavarretee@gmail.com>
2016-01-18 01:53:34 -0700

    got all of the things needed for transaction to work for now

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -7,23 +7,13 @@ class Transaction
               :created_at, :updated_at
 
   def initialize(trans_info)
-    @id                          = trans_info[:id]
-    @invoice_id                  = trans_info[:invoice_id]
-    @credit_card_number          = trans_info[:credit_card_number]
-    @credit_card_expiration_date = trans_info[:credit_card_expiration_date]
+    @id                          = trans_info[:id].to_i
+    @invoice_id                  = trans_info[:invoice_id].to_i
+    @credit_card_number          = trans_info[:credit_card_number].to_i
+    @credit_card_expiration_date = trans_info[:credit_card_expiration_date].to_i
     @result                      = trans_info[:result]
-    @created_at                  = trans_info[:created_at]
-    @updated_at                  = trans_info[:updated_at]
+    @created_at                  = Time.parse(trans_info[:created_at])
+    @updated_at                  = Time.parse(trans_info[:updated_at])
   end
-
+  
 end
-
-# id - returns the integer id
-# invoice_id - returns the invoice id
-# credit_card_number - returns the credit card number
-# credit_card_expiration_date - returns the credit card expiration date
-# result - the transaction result
-# created_at - returns a Time instance for the date the transaction was first created
-# updated_at - returns a Time instance for the date the transaction was last modified
-# We create an instance like this:
-#
