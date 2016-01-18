f57d7d9fe46780e06be6d190dd7dd56aacd511b9
Alex <alexxnavarretee@gmail.com>
2016-01-17 23:48:59 -0700

    got the basic invoice item methods running

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -4,8 +4,23 @@ require 'time'
 
 
 class InvoiceItem
+  attr_reader :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+
+  def initialize(invoice_item_info)
+    @id           = invoice_item_info[:id].to_i
+    @item_id      = invoice_item_info[:item_id].to_i
+    @invoice_id   = invoice_item_info[:invoice_id].to_i
+    @quantity     = invoice_item_info[:quantity].to_i
+    @unit_price   = BigDecimal.new(invoice_item_info[:unit_price])
+    @created_at   = Time.parse(invoice_item_info[:created_at])
+    @updated_at   = Time.parse(invoice_item_info[:updated_at])
+  end
 
 
+
+  def unit_price_to_dollars
+    (unit_price.to_f / 100).to_f
+  end
 
 
 
