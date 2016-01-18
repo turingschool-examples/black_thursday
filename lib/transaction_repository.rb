dd89946f406ee6b5e7d521b03a426f0bfd76d7fb
Alex <alexxnavarretee@gmail.com>
2016-01-18 02:43:40 -0700

    sleepy but grinding

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -14,10 +14,13 @@ class TransactionRepository
   end
 
   def inspect
-  "#<#{self.class} #{@merchants.size} rows>"
-end
+    "#<#{self.class} #{@merchants.size} rows>"
+  end
 
-
-
+#   all - returns an array of all known Transaction instances
+# find_by_id - returns either nil or an instance of InvoiceItem with a matching ID
+# find_all_by_invoice_id - returns either [] or one or more matches which have a matching invoice ID
+# find_all_by_credit_card_number - returns either [] or one or more matches which have a matching credit card number
+# find_all_by_result - returns either [] or one or more matches which have a matching status
 
 end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -13,6 +13,26 @@ class TransactionRepositoryTest < Minitest::Test
     assert TransactionRepository
   end
 
+  def setup
+    @repo = TransactionRepository.new([{:id=>"1", :invoice_id=>"1", :credit_card_number=>"4.06863E+15", :credit_card_expiration_date=>"217", :result=>"success", :created_at=>"2012-02-26 20:56:56 UTC", :updated_at=>"2012-02-26 20:56:56 UTC"},
+                 {:id=>"2", :invoice_id=>"2", :credit_card_number=>"4.17782E+15", :credit_card_expiration_date=>"813", :result=>"success", :created_at=>"2012-02-26 20:56:56 UTC", :updated_at=>"2012-02-26 20:56:56 UTC"},
+                 {:id=>"3", :invoice_id=>"3", :credit_card_number=>"4.27181E+15", :credit_card_expiration_date=>"1220", :result=>"success", :created_at=>"2012-02-26 20:56:56 UTC", :updated_at=>"2012-02-26 20:56:56 UTC"},
+                 {:id=>"4", :invoice_id=>"4", :credit_card_number=>"4.04803E+15", :credit_card_expiration_date=>"313", :result=>"success", :created_at=>"2012-02-26 20:56:56 UTC", :updated_at=>"2012-02-26 20:56:56 UTC"},
+                 {:id=>"5", :invoice_id=>"5", :credit_card_number=>"4.29722E+15", :credit_card_expiration_date=>"1215", :result=>"success", :created_at=>"2012-02-26 20:56:56 UTC", :updated_at=>"2012-02-26 20:56:56 UTC"},
+                 {:id=>"6", :invoice_id=>"6", :credit_card_number=>"4.55837E+15", :credit_card_expiration_date=>"417", :result=>"success", :created_at=>"2012-02-26 20:56:57 UTC", :updated_at=>"2012-02-26 20:56:57 UTC"},
+                 {:id=>"7", :invoice_id=>"7", :credit_card_number=>"4.61325E+15", :credit_card_expiration_date=>"314", :result=>"success", :created_at=>"2012-02-26 20:56:57 UTC", :updated_at=>"2012-02-26 20:56:57 UTC"},
+                 {:id=>"8", :invoice_id=>"8", :credit_card_number=>"4.83951E+15", :credit_card_expiration_date=>"213", :result=>"success", :created_at=>"2012-02-26 20:56:57 UTC", :updated_at=>"2012-02-26 20:56:57 UTC"},
+                 {:id=>"9", :invoice_id=>"9", :credit_card_number=>"4.46353E+15", :credit_card_expiration_date=>"618", :result=>"failed", :created_at=>"2012-02-26 20:56:57 UTC", :updated_at=>"2012-02-26 20:56:57 UTC"},
+                 {:id=>"10", :invoice_id=>"10", :credit_card_number=>"4.14965E+15", :credit_card_expiration_date=>"420", :result=>"success", :created_at=>"2012-02-26 20:56:57 UTC", :updated_at=>"2012-02-26 20:56:57 UTC"}])
+  end
+
+  def test_that_all_is_an_array_with_transaction_instances
+    assert_equal Array, repo.all.class
+    assert_equal Transaction, repo.all[0].class
+  end
+
+
+
 
 
 end
