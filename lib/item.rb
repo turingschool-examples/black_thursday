4d69300da44a7e0097eb266c6092c0a993f51b89
Claudia Kiesenhofer <c.kiesenhofer@gmail.com>
2016-03-01 07:30:24 -0700

    set up item and repository class

---
+++ lib/item.rb
@@ -0,0 +1,16 @@
+class Item
+  # attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
+   def initialize(hash)
+    #  (id, name, description, unit_price, created_at, updated_at, merchant_id)
+     @id
+     @name
+     @description
+     @unit_price
+     @created_at
+     @updated_at
+     @merchant_id
+   end
+
+   def unit_price_per_dollars
+   end
+end
