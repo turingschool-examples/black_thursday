5cf8d5ecae27a7afd9436e4bf1b14c76ac2e0a98
lingtran <lxiutran@gmail.com>
2016-03-02 00:45:41 -0700

    added SalesAnalyst#merchants_with_high_item_count; need to resolve error for nil:NilClass error in test

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -8,23 +8,36 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-    (@se.items.repository.count.to_f/@se.merchants.repository.count.to_f).round(2)
+    @average_items_per_merchant = (@se.items.repository.count.to_f/@se.merchants.repository.count.to_f).round(2)
   end
 
-  def items_per_merchant_hash
+  def items_per_merchant_hash #need test?
     @se.items.repository.group_by { |item| item.merchant_id }
   end
 
-  def item_count_per_merchant_hash
-    items_per_merchant_hash.map {|merchant_id, items| [merchant_id, items.count] }.to_h
+  def item_count_per_merchant_hash # need test?
+    items_per_merchant_hash.map { |merchant_id, items| [merchant_id, items.count] }.to_h
   end
 
   def average_items_per_merchant_standard_deviation
     n = item_count_per_merchant_hash.values.map do |value|
-      (value - average_items_per_merchant)**2
-    end.reduce(:+)/(@se.merchants.repository.count.to_f - 1)
-    
-    Math.sqrt(n).round(2)
+          (value - average_items_per_merchant)**2
+        end.reduce(:+)/(@se.merchants.repository.count.to_f - 1)
+
+    @std_dev = Math.sqrt(n).round(2)
+  end
+
+  def merchants_with_high_item_count # >= 1 std_dev or > 1 std_dev?
+    # display as an array merchants who have the most items for sale
+    # eligibility determined by std_dev > 1 above the average number of products offered; so want merchants with items numbering >= 9.40 = 3.26 (2 std_dev)+ 2.88(avg. items per merchant)
+    # more than 1 std_dev meaning > 6.14
+    one_std_dev = @std_dev + @average_items_per_merchant
+
+    merchants_with_high_item_count = item_count_per_merchant_hash.find_all do |merchant_id, item_count|
+                                        merchant_id if item_count > one_std_dev
+                                      end.map do |element|
+                                        @se.merchants.find_by_id(element[0])
+                                      end
   end
 
 end
--- test/sales_analyst_integration_test.rb
+++ test/sales_analyst_integration_test.rb
@@ -15,11 +15,10 @@ class SalesAnalystTest < Minitest::Test
       :merchants => "../data/merchants.csv",
     })
     sa = SalesAnalyst.new(se)
-
     assert sa
   end
 
-  def test_sales_analyst_can_calculate_average_items_per_merchant
+  def test_can_calculate_average_items_per_merchant
     se = SalesEngine.from_csv({
       :items     => "../data/items.csv",
       :merchants => "../data/merchants.csv",
@@ -28,12 +27,21 @@ class SalesAnalystTest < Minitest::Test
     assert_equal 2.88, sa.average_items_per_merchant
   end
 
-  def test_sales_analyst_can_calculate_average_items_per_merchant_standard_deviation
+  def test_can_calculate_average_items_per_merchant_standard_deviation
     se = SalesEngine.from_csv({
       :items     => "../data/items.csv",
       :merchants => "../data/merchants.csv",
     })
     sa = SalesAnalyst.new(se)
     assert_equal 3.26, sa.average_items_per_merchant_standard_deviation
+  end
+
+  def test_can_calculate_merchants_more_than_one_std_dev_from_avg_number_of_products_offered
+    se = SalesEngine.from_csv({
+      :items     => "../data/items.csv",
+      :merchants => "../data/merchants.csv",
+    })
+    sa = SalesAnalyst.new(se)
+    assert_equal 52, sa.merchants_with_high_item_count.count
   end
 end
