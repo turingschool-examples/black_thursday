e78c1fff14022747c92dd64fb77b3af469742d84
Lucy Fox <lucyreneefox@gmail.com>
2016-04-18 17:14:46 -0600

    tests for item_test added

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,23 @@
 class Item
+  attr_reader :id, :name, :description, :merchant_id
+
+  def initialize(item_hash = {})
+    @name = item_hash.fetch(:name)
+    @description = item_hash.fetch(:description)
+    @id = item_hash.fetch(:id)
+    @merchant_id = item_hash.fetch(:merchant_id)
+    @created_at = item_hash.fetch(:created_at)
+    @updated_at = item_hash.fetch(:updated_at)
+    # @unit_price = item_hash.fetch(:unit_price)
+  end
 end
+
+# The Item instance offers the following methods:
+# unit_price - returns the price of the item formatted as a BigDecimal
+# created_at - returns a Time instance for the date the item was first created
+# updated_at - returns a Time instance for the date the item was last modified
+# merchant_id - returns the integer merchant id of the item
+# It also offers the following method:
+#
+# unit_price_to_dollars - returns the price of the item in dollars formatted as a Float
+# We create an instance like this:
--- test/item_test.rb
+++ test/item_test.rb
@@ -2,8 +2,46 @@ require './test/test_helper'
 require './lib/item'
 
 class ItemTest < Minitest::Test
-  def test_it_created_instance_of_item_class
-    i = Item.new
-    assert_equal Item, i.class
+
+  def setup
+    @i = Item.new({:id => 12345, :name => "Pencil",
+      :description => "You can use it to write things",
+      # :unit_price => BigDecimal.new(10.99,4),
+      :merchant_id => 98765,
+      :created_at => Time.now, :updated_at => Time.now})
   end
+
+  def test_it_created_instance_of_item_class
+    assert_equal Item, @i.class
+  end
+
+  def test_it_returns_the_integer_id_of_the_item
+    assert_equal 12345, @i.id
+  end
+
+  def test_it_returns_the_name_of_the_item
+    assert_equal "Pencil", @i.name
+  end
+
+  def test_it_returns_the_description_of_the_item
+    assert @i.description.include?("write")
+  end
+
+  def test_it_returns_price_of_item_formatted_as_big_decimal
+    skip
+    #figure out how big decimal works!
+  end
+
+  def test_it_returns_updated_time_later_than_time_created_at
+  end
+
+  def test_it_returns_merchant_id
+    assert_equal 98765, @i.merchant_id
+  end
+
+  def test_it_can_return_unit_price_in_dollars
+    #again figure out what this data means
+  end
+
+
 end
