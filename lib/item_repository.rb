b5a371d5304246e29c0039f6a5d26bb0ee0d8ec4
Robbie Jaeger <jaeger.rob@gmail.com>
2016-04-18 17:01:00 -0600

    Add methods to item repository class

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,3 +1,24 @@
 class ItemRepository
-  
+
+  def all
+  end
+
+  def find_by_id
+  end
+
+  def find_by_name
+  end
+
+  def find_all_with_description
+  end
+
+  def find_all_by_price
+  end
+
+  def find_all_by_price_in_range
+  end
+
+  def find_all_by_merchant_id
+  end
+
 end
