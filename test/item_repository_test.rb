17b2bb37bd7946d1f598815c7987e854093cfa40
Colin Osborn <colin.osborn@gmail.com>
2016-04-19 16:05:13 -0600

    item repo

---
+++ test/item_repository_test.rb
@@ -0,0 +1,67 @@
+require './test/test_helper'
+require './lib/item_repository'
+require './lib/sales_engine'
+
+
+class ItemRepositoryTest < Minitest::Test
+
+  attr_reader :item_repo, :se
+  def setup
+    @se = SalesEngine.from_csv({
+      :items     => "./data/small_items.csv",
+      :merchants => "./data/small_merchants.csv",})
+    @se.items
+    @item_repo = @se.item_repository
+  end
+
+  def test_all_returns_array
+    assert_equal Array, item_repo.all.class
+  end
+
+  def test_returns_all_items_within_array
+    assert_equal 19, item_repo.all.length
+  end
+
+  def test_all_returns_array_of_item_objects
+    assert_equal Item, item_repo.all[7].class
+  end
+
+  def test_find_by_id_returns_nil_for_a_unmatched_ID
+    assert_equal nil, item_repo.find_by_id(6)
+  end
+
+  def test_find_by_id_returns_a_item_object_upon_match
+    assert_equal "Free standing Woden letters", item_repo.find_by_id(263396013).name
+  end
+
+  def test_find_by_name_returns_nil_when_no_match
+    assert_equal nil, item_repo.find_by_name("fake")
+  end
+
+  def test_find_by_name_returns_merch_object_upon_match
+    assert_equal 263396013, item_repo.find_by_name("Free standing Woden letters").id
+    assert_equal 263396013, item_repo.find_by_name("Free standing WODen letters").id
+  end
+
+  def test_find_all_by_name_returns_empty_array_for_no_matches
+    assert_equal [], item_repo.find_all_with_description("fake")
+  end
+
+  def test_find_all_by_name_single_match
+    assert_equal "Glitter scrabble frames", item_repo.find_all_with_description("Glitter scrabble")[0].name
+  end
+
+  def test_find_all_with_description_multi_match
+    assert_equal true, item_repo.find_all_with_description("Acrylique")[0].name.include?("Cache cache")
+    assert_equal true, item_repo.find_all_with_description("Acrylique")[1].name.include?("Course")
+  end
+
+  def test_find_by_id_returns_nil_for_a_unmatched_merchant_ID
+    assert_equal nil, item_repo.find_by_merchant_id(6)
+  end
+
+  def test_find_by_merchant_id_returns_a_item_object_upon_match
+    assert_equal "Glitter scrabble frames", item_repo.find_by_merchant_id(12334185).name
+  end
+
+end
