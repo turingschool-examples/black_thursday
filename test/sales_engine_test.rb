43e18305d6210b9d8f48ef0de0e73ced7a475bd1
Lucy Fox <lucyreneefox@gmail.com>
2016-04-19 11:54:19 -0600

    refactored sales engine

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -4,10 +4,14 @@ require './lib/sales_engine'
 class SalesEngineTest < Minitest::Test
 
   def setup
-    @s = SalesEngine.from_csv({:items => "./data/items.csv", :merchants => "./data/merchants.csv"})
+    @s = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"})
   end
 
   def test_it_created_instance_of_sales_engine_class
-    assert_equal SalesEngine, @s.class
+    assert_equal false, @s.empty?
   end
+
+
 end
