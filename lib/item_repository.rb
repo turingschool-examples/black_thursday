cbbfdde8eed2d8e5032b59061c058631266798c1
Jennifer <jennifersoden@hotmail.com>
2016-04-19 09:08:47 -0600

    started item repo

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,3 +1,5 @@
 class ItemRepository
 
+  
+
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -4,19 +4,11 @@ require './lib/item_repository'
 
 class ItemRepositoryTest < Minitest::Test
 
-  attr_reader :i
+  attr_reader :
 
   def setup
-    @i = Item.new({
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
-      :created_at  => Time.now,
-      :updated_at  => Time.now,
-    })
+
   end
 
-  def test_it_can_give_a_name
-    assert_equal "", i.name
-  end
+
 end
