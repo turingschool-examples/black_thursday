5ba8ea4f329abb24b246cfbd053dafe8d7ed4bf9
Robbie Jaeger <jaeger.rob@gmail.com>
2016-04-18 21:31:24 -0600

    Can add items to array from csv

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,16 +1,15 @@
 require 'csv'
+require './lib/item'
 
 class ItemRepository
 
-  attr_reader :items
-
   def initialize(csv_filepath)
-    @csv_filepath = csv_filepath
-    create_items(@csv_filepath)
     @items = []
+    create_items(csv_filepath)
   end
 
   def all
+    @items
   end
 
   def find_by_id
@@ -31,7 +30,7 @@ class ItemRepository
   def find_all_by_merchant_id
   end
 
-# private
+private
 
   def create_items(csv_filepath)
     parse_csv_data(csv_filepath)
@@ -47,7 +46,26 @@ class ItemRepository
       merchant_id = row[:merchant_id]
       created_at = row[:created_at]
       updated_at = row[:updated_at]
+
+      create_item_hash(id, name, description, unit_price, merchant_id, created_at, updated_at)
     end
+  end
+
+  def create_item_hash(id, name, description, unit_price, merchant_id, created_at, updated_at)
+    item_creation_hash = {}
+    item_creation_hash[:id] = id
+    item_creation_hash[:name] = name
+    item_creation_hash[:description] = description
+    item_creation_hash[:unit_price] = unit_price
+    item_creation_hash[:merchant_id] = merchant_id
+    item_creation_hash[:created_at] = created_at
+    item_creation_hash[:updated_at] = updated_at
+    item_creation_hash
+    add_item(item_creation_hash)
+  end
+
+  def add_item(item_creation_hash)
+    @items << Item.new(item_creation_hash)
   end
 
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -15,7 +15,14 @@ class ItemRepositoryTest < Minitest::Test
     csv_filepath = "./data/items_small.csv"
     ir = ItemRepository.new(csv_filepath)
 
-    
+    assert ir.all.kind_of?(Array)
+  end
+
+  def test_it_can_return_array_of_item_objects
+    csv_filepath = "./data/items_small.csv"
+    ir = ItemRepository.new(csv_filepath)
+
+    assert ir.all[0].kind_of?(Item)
   end
 
 end
