5b8fdd5e8d42e469a446385c3a4a2fdd65b9a68c
Jennifer <jennifersoden@hotmail.com>
2016-04-19 14:30:10 -0600

    added setup in merchant_test

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,16 +1,25 @@
+require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/merchant.rb'
 
 class MerchantTest < MiniTest::Test
 
+  attr_reader :m
+
+  def setup
+    @m = Merchant.new({:id => 5, :name => "Turing School"})
+  end
+
+  def test_it_is_a_merchant_object
+    assert_equal Merchant, m.class
+  end
+
   def test_it_initializes_with_correct_id
-    m = Merchant.new({:id => 5, :name => "Turing School"})
     assert_equal 5, m.id
   end
 
   def test_it_initializes_with_correct_name
-    m = Merchant.new({:id => 5, :name => "Turing School"})
     assert_equal "Turing School", m.name
   end
 
