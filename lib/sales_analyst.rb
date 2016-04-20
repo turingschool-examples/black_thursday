832194668552da51771e4cf258e23f95e2ed68ca
Colin Osborn <colin.osborn@gmail.com>
2016-04-20 15:04:50 -0600

    adding module

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,3 +1,13 @@
 class SalesAnalyst
+  attr_reader :sales_engine
+
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+  end
+
+  def average_items_per_merchant
+    (sales_engine.items.items.length.to_f/sales_engine.merchants.merchants.length).round(2)
+  end
+
 
 end
