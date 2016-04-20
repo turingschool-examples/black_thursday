9ea04d71aa7f7477da0464b2c24396058403df2e
Colin Osborn <colin.osborn@gmail.com>
2016-04-20 15:32:50 -0600

    adding modules

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -4,7 +4,19 @@ require_relative '../lib/sales_engine'
 
 class SalesAnalystTest < Minitest::Test
 
-  def test_setup
-    assert SalesAnalyst.new.class
+  attr_reader :merch_repo, :se, :sa
+
+  def setup
+    @se = SalesEngine.from_csv({
+      :items     => "./data/small_items.csv",
+      :merchants => "./data/small_merchants.csv",})
+    @sa = SalesAnalyst.new(se)
+    @se.merchants
+    @se.items
+    @merch_repo = @se.merchant_repo
+  end
+
+  def test_average_items_per_merchant_gives_correct_average
+    assert_equal 1.90, sa.average_items_per_merchant
   end
 end
