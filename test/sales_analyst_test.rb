f1e1e9e1c188c525ed46bbf07894ef056e9d505a
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-21 08:37:16 -0600

    sales analyst in progress

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -19,5 +19,21 @@ class SalesAnalystTest < MiniTest::Test
     assert sa
   end
 
+  def test_it_finds_number_of_items_for_each_merchant
+    assert_equal 475, sa.items_per_merchant.length
+  end
+
+  def test_it_finds_average_items_per_merchant
+    assert_equal 2.88, sa.average_items_per_merchant
+  end
+
+  def test_it_finds_standard_deviation
+    assert_equal 3.26, sa.average_items_per_merchant_standard_deviation
+  end
+
+  # def test_it_finds_merchants_with_high_item_count
+  #   assert_equal [], sa.merchants_with_high_item_count
+  # end
+
 
 end
