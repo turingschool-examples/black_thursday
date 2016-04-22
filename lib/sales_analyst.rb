473d62dc3d4db16f3be8aa34a22e4daec657e6ac
Kerry <kerry.sheldon@gmail.com>
2016-04-21 20:28:40 -0600

    checkin before trying threshold again

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -42,10 +42,50 @@ class SalesAnalyst
   end
 
   def golden_items
-    threshold = item_price_average + (2 * item_price_standard_deviation)
+    threshold = item_price_average +
+                (2 * item_price_standard_deviation)
     sales_engine.items.all.find_all do |item|
       item.unit_price_to_dollars >= threshold
     end
+  end
+
+  def average_invoices_per_merchant
+    num_invoices = sales_engine.invoices.all.length.to_f
+    num_merchants = sales_engine.merchants.all.length
+    (num_invoices/num_merchants).round(2)
+  end
+
+  def average_invoices_per_merchant_standard_deviation
+    standard_deviation(invoice_count_by_merchant)
+  end
+
+  def top_merchants_by_invoice_count
+    threshold = average_invoices_per_merchant +
+            2 * average_invoices_per_merchant_standard_deviation
+    sales_engine.merchants.all.find_all do |merchant|
+      merchant.invoices.length >= threshold
+    end
+  end
+
+  def bottom_merchants_by_invoice_count
+    threshold = average_invoices_per_merchant -
+            2 * average_invoices_per_merchant_standard_deviation
+    sales_engine.merchants.all.find_all do |merchant|
+      merchant.invoices.length <= threshold
+    end
+  end
+
+  def top_days_by_invoice_count
+    threshold = average_invoices_per_day +
+          standard_deviation(group_invoices_by_day_count.values)
+    group_invoices_by_day_count.delete_if do | key, value |
+      value <= threshold
+    end.keys
+  end
+
+  def invoice_status(status)
+    (group_invoices_status[status].length.to_f /
+    sales_engine.invoices.all.length * 100).round(2)
   end
 
   def item_price_array
@@ -72,53 +112,10 @@ class SalesAnalyst
     standard_deviation(item_count_by_merchant)
   end
 
-  def sum(array)
-    array.reduce(0) { |sum, item| sum + item }
-  end
-
-  def average(array)
-    sum(array)/array.length.to_f
-  end
-
-  def standard_deviation(array)
-    average = average(array)
-    sum_squares = array.reduce(0) do |sum, item|
-      sum + (item - average)**2
-    end
-    result = sum_squares/(array.length - 1).to_f
-    Math.sqrt(result).round(2)
-  end
-
-  def average_invoices_per_merchant
-    num_invoices = sales_engine.invoices.all.length.to_f
-    num_merchants = sales_engine.merchants.all.length
-    (num_invoices/num_merchants).round(2)
-  end
-
   def invoice_count_by_merchant
     sales_engine.merchants.all.map do |merchant|
       merchant.invoices.length
     end.sort
-  end
-
-  def average_invoices_per_merchant_standard_deviation
-    standard_deviation(invoice_count_by_merchant)
-  end
-
-  def top_merchants_by_invoice_count
-    threshold = average_invoices_per_merchant +
-            2 * average_invoices_per_merchant_standard_deviation
-    sales_engine.merchants.all.find_all do |merchant|
-      merchant.invoices.length >= threshold
-    end
-  end
-
-  def bottom_merchants_by_invoice_count
-    threshold = average_invoices_per_merchant -
-            2 * average_invoices_per_merchant_standard_deviation
-    sales_engine.merchants.all.find_all do |merchant|
-      merchant.invoices.length <= threshold
-    end
   end
 
   def find_day_of_week(date)
@@ -142,22 +139,29 @@ class SalesAnalyst
     average(group_invoices_by_day_count.values).round(2)
   end
 
-  def top_days_by_invoice_count
-    threshold = average_invoices_per_day + standard_deviation(group_invoices_by_day_count.values)
-    group_invoices_by_day_count.delete_if do | key, value |
-      value <= threshold
-    end.keys
-  end
-
   def group_invoices_status
     sales_engine.invoices.all.group_by do |invoice|
       invoice.status
     end
   end
 
-  def invoice_status(status)
-    (group_invoices_status[status].length.to_f /
-    sales_engine.invoices.all.length * 100).round(2)
+  def sum(array)
+    array.reduce(0) { |sum, item| sum + item }
+  end
+
+  def average(array)
+    sum(array)/array.length.to_f
+  end
+
+  def standard_deviation(array)
+    result = array.reduce(0) do |sum, item|
+      sum + (item - average(array))**2
+    end/(array.length - 1).to_f
+    Math.sqrt(result).round(2)
+  end
+
+  def threshold(array, num_std_devs)
+    average(array) + num_std_devs * standard_deviation(array)
   end
 
 end
