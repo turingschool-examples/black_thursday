004594f0051af533c0c7d2cc5d8394d2cc6507f0
Kerry <kerry.sheldon@gmail.com>
2016-04-21 20:55:07 -0600

    small refactors

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,6 +1,9 @@
 require 'bigdecimal'
+require_relative 'standard_deviation'
 
 class SalesAnalyst
+  include StandardDeviation
+
   attr_reader :sales_engine
 
   def initialize(sales_engine)
@@ -138,25 +141,6 @@ class SalesAnalyst
     sales_engine.invoices.all.group_by do |invoice|
       invoice.status
     end
-  end
-
-  def sum(array)
-    array.reduce(0) { |sum, item| sum + item }
-  end
-
-  def average(array)
-    sum(array)/array.length.to_f
-  end
-
-  def standard_deviation(array)
-    result = array.reduce(0) do |sum, item|
-      sum + (item - average(array))**2
-    end/(array.length - 1).to_f
-    Math.sqrt(result).round(2)
-  end
-
-  def threshold(array, num_std_devs)
-    (average(array) + num_std_devs * standard_deviation(array)).round(2)
   end
 
 end
--- test/standard_deviation_test.rb
+++ test/standard_deviation_test.rb
@@ -2,11 +2,6 @@ require_relative "test_helper"
 require_relative "../lib/standard_deviation"
 
 class StandardDeviationTest < Minitest::Test
-  include StandardDeviation
 
-  def test_sum
-    skip
-    assert_equal 6, [1,2,3].sum
-  end
 
 end
