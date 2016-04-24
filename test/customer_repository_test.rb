5b0f3c74c80550bfc7a5703d130bc4eff1177c8d
Jennifer <jennifersoden@hotmail.com>
2016-04-23 18:39:59 -0600

    error in parsing time

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -4,4 +4,16 @@ require 'minitest/pride'
 require_relative '../lib/customer_repository'
 
 class CustomerRepositoryTest < MiniTest::Test
+
+  attr_reader :cr
+
+  def setup
+    test_helper = TestHelper.new
+    @cr = CustomerRepository.new(test_helper.customers)
+  end
+
+  def test_it_can_return_an_array_of_all_customers
+    assert_equal [], cr.all
+  end
+
 end
