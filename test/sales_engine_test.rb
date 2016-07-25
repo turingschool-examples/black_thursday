e4aa62404083d992d6fcb304925d2da59812aaeb
David Davydov <kctrlv@gmail.com>
2016-07-25 17:47:24 -0600

    added master test for sales_engine

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,4 +1,13 @@
 require './test/test_helper'
 
 class SalesEngineTest < Minitest::Test
+  def test_master_test
+    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+    assert_equal MerchantRepository, se.merchants.class
+    assert_equal ItemRepository, se.items.class
+  end
 end
