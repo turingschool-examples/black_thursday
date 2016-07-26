6af94abe256aaf46cf156a560ae6cdf241790db3
David Davydov <kctrlv@gmail.com>
2016-07-25 21:24:31 -0600

    item class in progress

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,15 @@
 class Item
+  attr_reader :name, :description, :unit_price, :created_at, :updated_at, :id,
+              :merchant_id
+
+  def initialize(properties = Hash.new)
+    @id =           properties[:id]
+    @name =         properties[:name]
+    @description =  properties[:description]
+    @unit_price =   properties[:unit_price]
+    @created_at =   properties[:created_at]
+    @updated_at =   properties[:updated_at]
+    @merchant_id =  properties[:merchant_id]
+  end
+
 end
