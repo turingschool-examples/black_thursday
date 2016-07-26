991956d870d84cbbd1ceb0ee7cebe6ec2176d054
Ben Pepper <bfpepper87@gmail.com>
2016-07-26 17:27:56 -0600

    Part of iteration 1

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,12 +1,21 @@
+require "./lib/merchant_repo"
+
 class Merchant
 
   attr_reader :id, :name, :created_at, :updated_at
 
-  def initialize(row)
+  def initialize(row, parent = nil)
+    @parent = parent
     @id = row[:id].to_i
     @name = row[:name]
     @created_at = Time.strptime(row[:created_at], "%Y-%m-%d")
     @updated_at = Time.strptime(row[:updated_at], "%Y-%m-%d")
   end
+
+  def items
+    @parent.find_items_by_merchant_id(self.id)
+  end
+
+
 
 end
