ffdfd88339dac44d9c72ab49972f0d62c6631736
David Davydov <kctrlv@gmail.com>
2016-07-25 19:58:44 -0600

    got a test to pass

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,2 +1,11 @@
 class SalesEngine
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def self.from_csv(hash)
+    SalesEngine.new(hash)
+  end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,13 +1,20 @@
 require './test/test_helper'
+require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
+  def test_from_cvs_method_can_return_hash_argument
+    hash = {:a => "value_a", :b => "value_b"}
+    se = SalesEngine.from_csv(hash)
+    assert_equal hash, se.data
+  end
+
   def test_master_test
     skip
     se = SalesEngine.from_csv({
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
     })
-    assert_equal MerchantRepository, se.merchants.class
     assert_equal ItemRepository, se.items.class
+    assert_equal MerchantRepository, se.merchants.class
   end
 end
