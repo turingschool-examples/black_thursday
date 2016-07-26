77cc94b60cda57957f7c32d56529a4e59dce9432
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-07-26 17:10:03 -0600

    Added link from merchant to item

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -4,10 +4,14 @@ class Merchant
               :name,
               :parent_repo
 
-  def initialize(datum, parent_repo)
+  def initialize(datum, parent_repo = nil)
     @id = datum[:id]
     @name = datum[:name]
     @parent_repo = parent_repo
+  end
+
+  def items
+    parent_repo.parent_engine.items.find_all_by_merchant_id(@id)
   end
 
 end
