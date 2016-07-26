c42524cf0844772ead18ea38e353def167f24fc7
Brian Heim <bheim67@gmail.com>
2016-07-26 08:55:01 -0600

    added merchant repository test, merchant class, and methods to merchant_repository

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,6 +1,22 @@
+require './lib/merchant'
 class MerchantRepository
+  attr_reader :list_of_merchants
 
   def initialize(merchants_data)
+    @list_of_merchants = []
+    populate_merchants(merchants_data)
   end
+
+  def populate_merchants(merchants_data)
+    merchants_data.each do |datum|
+      @list_of_merchants << Merchant.new(datum)
+    end
+  end
+
+  def all
+    list_of_merchants
+
+  end
+
 
 end
