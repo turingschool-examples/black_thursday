89a34f105dfd4118b0323198ae8391a6e3c73765
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-07-27 08:54:29 -0600

    Added more robust testing to sales analyst

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -9,10 +9,25 @@ class SalesAnalystTest < Minitest::Test
     assert sa.sales_engine.is_a?(SalesEngine)
   end
 
+  def test_it_can_find_one_merchant
+    se = SalesEngine.from_csv({ items: "./data/item_sample.csv", merchants: "./data/merchants_sample.csv" })
+    sa = SalesAnalyst.new(se)
+    assert_equal "Shopin1901", sa.find_merchant("12334105").name
+  end
+
+
   def test_it_can_find_all_merchants
     se = SalesEngine.from_csv({ items: "./data/item_sample.csv", merchants: "./data/merchants_sample.csv" })
     sa = SalesAnalyst.new(se)
-    assert_equal 100, sa.sales_engine.merchants.all.length
+    assert_equal 100, sa.find_all_merchants.length
+  end
+
+  def test_it_can_find_total_item_price_for_a_merchant
+    se = SalesEngine.from_csv({ items: "./data/item_sample.csv", merchants: "./data/merchants_sample.csv" })
+    sa = SalesAnalyst.new(se)
+    merchant = sa.find_merchant("12334185")
+
+    assert_equal BigDecimal(33.50, 4), sa.total_item_price_for_merchant(merchant)
   end
 
   def test_it_can_find_average_item_price_for_a_merchant
@@ -34,6 +49,23 @@ class SalesAnalystTest < Minitest::Test
     sa = SalesAnalyst.new(se)
 
     assert_equal nil, sa.average_item_price_for_merchant("99999999")
+  end
+
+  def test_it_can_find_active_merchants
+    se = SalesEngine.from_csv({ items: "./data/item_sample.csv", merchants: "./data/merchants_sample.csv" })
+    sa = SalesAnalyst.new(se)
+    all_merchants = sa.find_all_merchants
+
+    assert_equal 10, sa.find_active_merchants(all_merchants).length
+  end
+
+  def test_it_can_find_total_of_all_averages
+    se = SalesEngine.from_csv({ items: "./data/item_sample.csv", merchants: "./data/merchants_sample.csv" })
+    sa = SalesAnalyst.new(se)
+    all_merchants = sa.find_all_merchants
+    active_merchants = sa.find_active_merchants(all_merchants)
+
+    assert_equal BigDecimal(710.38, 5), sa.total_item_price_for_active_merchants(active_merchants)
   end
 
   def test_it_can_find_average_average_item_price_for_all_merchants
