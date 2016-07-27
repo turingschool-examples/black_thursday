bc6823bd2bafed62ee633c1b487eab3e7749137c
Chase Dunagan <cdunagan@utexas.edu>
2016-07-26 21:22:54 -0600

    changed merchant repo tests

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -7,26 +7,23 @@ require "./lib/merchant"
 class MerchantTest < MiniTest::Test
 
   def test_holds_a_name
-    @merchant = Merchant.new(["12334105", "Shopin1901", "2010-12-10", "2011-12-04"])
-
+    @merchant = Merchant.new({ :name => "mc'ds", :id => 666})
     assert_equal "mc'ds", @merchant.name
   end
-  def test_it_sets_default_name_to_empty_string
-    @merchant = Merchant.new( 666, "string")
 
+#I don't think this test needs to exist anymore.
+  def test_it_sets_default_name_to_empty_string
+    skip
+    @merchant = Merchant.new( { :name => "", :id => 666})
     assert_equal "", @merchant.name
   end
-  def test_it_holds_an_id
-    @merchant = Merchant.new("mc'ds", 666, "string")
 
+  def test_it_holds_an_id
+    @merchant = Merchant.new({ :name => "mc'ds", :id => 666})
     assert_equal 666, @merchant.id
   end
-  def test_it_will_recieve_a_merchant_repo
-    @merchant = Merchant.new("mc'ds", 666, "string")
 
-    assert_equal "string", @merchant.repo
-  end
-
+#need to impliment repo fully before testing this???
   def test_it_can_commincate_with_the_repo
     skip
   end
