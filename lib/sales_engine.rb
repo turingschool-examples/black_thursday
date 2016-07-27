5d24b39f34f14ae8ce662c09946e313b4145ecd7
Jean Joeris <joeris.peter@gmail.com>
2016-07-27 16:26:47 -0600

    cleaned up sales engine for merging

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,46 +4,47 @@ require 'CSV'
 require 'pry'
 
 class SalesEngine
-  attr_reader :merchant_repo,
-              :item_repo
+  require_relative './item_repo'
+  require_relative './merchant_repo'
+  require 'CSV'
+  require 'pry'
 
-  def initialize
-    @merchant_repo = MerchantRepo.new(self)
-    @item_repo     = ItemRepo.new(self)
-  end
+  class SalesEngine
+    attr_reader :merchant_repo,
+                :item_repo
 
-  def from_csv(csv_path_info)
-    if csv_path_info[:merchants]
-      merchant_data = CSV.read(csv_path_info[:merchants], headers:true)
-      add_merchants_to_repo(merchant_data)
+    def initialize(csv_path_info)
+      @merchant_repo = MerchantRepo.new(self)
+      @item_repo     = ItemRepo.new(self)
+      if csv_path_info.class == Hash
+        add_data_to_repos(csv_path_info)
+      end
     end
-    if csv_path_info[:items]
-      item_data = CSV.read(csv_path_info[:items], headers:true)
-      add_items_to_repo(item_data)
+
+    def add_data_to_repos(csv_path_info)
+      if csv_path_info[:merchants]
+        add_merchants(csv_path_info[:merchants])
+      end
+      if csv_path_info[:items]
+        add_items(csv_path_info[:items])
+      end
     end
-  end
 
-  def add_merchants_to_repo(merchant_data)
-    headers = merchant_data.headers
-    merchant_data.each do |row|
-      merchant_details = create_initializing_details(row, headers)
-      @merchant_repo.add_merchant(merchant_details)
+    def self.from_csv(csv_path_info)
+      self.new(csv_path_info)
     end
-  end
 
-  def add_items_to_repo(item_data)
-    headers = item_data.headers
-    item_data.each do |row|
-      item_details = create_initializing_details(row, headers)
-      @item_repo.add_item(item_details)
+    def add_merchants(path_info)
+      CSV.foreach(path_info, headers:true, header_converters: :symbol) do |row|
+        @merchant_repo.add_merchant(row)
+      end
     end
-  end
 
-  def create_initializing_details(entry, headers)
-    header_syms = headers.map { |h| h.to_sym }
-    header_syms.zip(entry.fields).to_h
-  end
-
+    def add_items(path_info)
+      CSV.foreach(path_info, headers:true, header_converters: :symbol) do |row|
+         @item_repo.add_item(row)
+      end
+    end
 
   # these methods are for passing child queries
   # into other repos
