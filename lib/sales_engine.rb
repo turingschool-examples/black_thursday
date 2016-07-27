a195d6964299338b5b304c52cbfed22026d97420
Jean Joeris <joeris.peter@gmail.com>
2016-07-27 14:12:50 -0600

    use csv rows as our hashes, added from_csv as a class method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -7,34 +7,36 @@ class SalesEngine
   attr_reader :merchant_repo,
               :item_repo
 
-  def initialize
+  def initialize(csv_path_info)
     @merchant_repo = MerchantRepo.new(self)
     @item_repo     = ItemRepo.new(self)
+    if csv_path_info.class == Hash
+      add_data_to_repos(csv_path_info)
+    end
   end
 
-  def from_csv(csv_path_info)
+  def add_data_to_repos(csv_path_info)
     if csv_path_info[:merchants]
-      merchant_data = CSV.read(csv_path_info[:merchants], headers:true, header_converters: :symbol)
-      add_merchants_to_repo(merchant_data)
+      add_merchants(csv_path_info[:merchants])
     end
     if csv_path_info[:items]
-      item_data = CSV.read(csv_path_info[:items], headers:true, header_converters: :symbol)
-      add_items_to_repo(item_data)
+      add_items(csv_path_info[:items])
     end
   end
 
-  def add_merchants_to_repo(merchant_data)
-    headers = merchant_data.headers
-    merchant_data.each do |row|
-      @merchant_repo.add_merchant(row.to_hash)
+  def self.from_csv(csv_path_info)
+    self.new(csv_path_info)
+  end
+
+  def add_merchants(path_info)
+    CSV.foreach(path_info, headers:true, header_converters: :symbol) do |row|
+      @merchant_repo.add_merchant(row)
     end
   end
 
-  def add_items_to_repo(item_data)
-    headers = item_data.headers
-    item_data.each do |row|
-      @item_repo.add_item(row.to_hash)
+  def add_items(path_info)
+    CSV.foreach(path_info, headers:true, header_converters: :symbol) do |row|
+       @item_repo.add_item(row)
     end
   end
-
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,7 +3,7 @@ require_relative '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
   def test_it_has_item_and_merchant_repository
-    se = SalesEngine.new
+    se = SalesEngine.new("empty")
     mr = se.merchant_repo
     ir = se.item_repo
     assert_equal MerchantRepo, mr.class
@@ -11,8 +11,8 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_single_line_csv_merchant
-    se = SalesEngine.new
-    se.from_csv({
+
+    se = SalesEngine.from_csv({
       :merchants => './test/support/single_merchant.csv'
       })
     assert_equal 1, se.merchant_repo.all.count
@@ -20,8 +20,8 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_add_small_merchant_csv
-    se = SalesEngine.new
-    se.from_csv({
+    # se = SalesEngine.new
+    se = SalesEngine.from_csv({
       :merchants => './test/support/small_merchant_list.csv'
       })
 
@@ -29,8 +29,8 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_single_line_csv_item
-    se = SalesEngine.new
-    se.from_csv({
+    # se = SalesEngine.new
+    se = SalesEngine.from_csv({
       :items => './test/support/single_item.csv'
       })
     assert_equal 1, se.item_repo.all.count
@@ -38,21 +38,21 @@ class SalesEngineTest < Minitest::Test
   end
 
   def test_small_item_csv
-    se = SalesEngine.new
-    se.from_csv({
+    # se = SalesEngine.new
+    se = SalesEngine.from_csv({
       :items => './test/support/small_item_list.csv'
       })
     assert_equal 10, se.item_repo.all.count
   end
 
   def test_item_repo_can_access_sales_engine
-    se = SalesEngine.new
+    se = SalesEngine.new("empty")
     item_repo = se.item_repo
     assert_equal se, item_repo.sales_engine
   end
 
   def test_merchant_repo_can_access_sales_engine
-    se = SalesEngine.new
+    se = SalesEngine.new("empty")
     merchant_repo = se.merchant_repo
     assert_equal se, merchant_repo.sales_engine
   end
