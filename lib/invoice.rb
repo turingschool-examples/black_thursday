1ff04ad0c37c3f8cc17ea48b4ceba1d6db199fbd
Chase Dunagan <cdunagan@utexas.edu>
2016-07-31 14:49:37 -0600

    added invoice and invoice repo

---
+++ lib/invoice.rb
@@ -0,0 +1,24 @@
+require "pry"
+require "time"
+
+class Invoice
+
+  attr_reader :id,
+              :customer_id,
+              :merchant_id,
+              :status,
+              :created_at,
+              :updated_at,
+              :parent
+
+  def initialize(invoice_data, parent=nil)
+              @id          = invoice_data[:id].to_i
+              @customer_id = invoice_data[:customer_id].to_i
+              @merchant_id = invoice_data[:merchant_id].to_i
+              @status      = invoice_data[:status].to_s
+              @created_at  = Time.parse(invoice_data[:created_at])
+              @updated_at  = Time.parse(invoice_data[:updated_at])
+              @parent      = parent
+  end
+
+end
