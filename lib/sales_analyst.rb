096ab0ef3361098f41d4a759c089ad7cfd17acdb
Chase Dunagan <cdunagan@utexas.edu>
2016-07-31 10:40:34 -0600

    checking to see if spec harness is failing. iteration 1

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -35,8 +35,9 @@ class SalesAnalyst
   end
 
   def merchants_with_high_item_count
+    standard_deviation = average_items_per_merchant_standard_deviation
     all_merchants.find_all do |merchant|
-      merchant_items_count(merchant.id) > (average_items_per_merchant_standard_deviation*2)
+      merchant_items_count(merchant.id) > (standard_deviation*2)
     end
   end
 
