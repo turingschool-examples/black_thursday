3b03674cbc8b64a77915ec6d5eee67e9bb33d5ac
David Davydov <kctrlv@gmail.com>
2016-08-01 15:41:56 -0600

    made customer and tests

---
+++ lib/customer.rb
@@ -0,0 +1,26 @@
+class Customer
+  attr_reader :id,
+              :first_name,
+              :last_name,
+              :created_at,
+              :updated_at,
+              :parent
+
+  def initialize(data, parent = nil)
+    @id =   data[:id].to_i
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+    @created_at = prep_time(data[:created_at])
+    @updated_at = prep_time(data[:updated_at])
+    @parent = parent
+  end
+
+  def prep_time(time)
+    return nil if !time
+    Time.parse(time)
+  end
+
+  def merchants
+    merchants = @parent.find_all_merchants_by_id(@id)
+  end
+end
