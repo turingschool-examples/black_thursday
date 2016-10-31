5fa5469a795a6779abeeee067eae168cf8ae20fc
Drew Palazzari <drewpalazzari@hotmail.com>
2016-10-31 16:11:16 -0600

    I've done some things

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -2,8 +2,21 @@ require 'minitest/autorun'
 require 'minitest/nyan_cat'
 require './lib/merchant.rb'
 
+
 class MerchantTest < Minitest::Test
 
 
+  attr_reader :merchant
+  def setup
+    @merchant = Merchant.new
+  end
+
+  def test_it_exists
+    assert merchant
+  end
+
+  def test_has_some_merchants
+    assert merchant.merchants
+  end
 
 end
