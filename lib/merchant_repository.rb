cdda25db51546f4135b10e8679f968cfc77ba831
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-10-31 17:33:21 -0600

    Added merchant repo and merchant repo tests. All passing.

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -2,8 +2,24 @@ require './lib/merchant'
 
 class MerchantRepository
 
-  def initialize(merchants)
+  attr_reader :all
 
+  def initialize(merchants)
+    @all = merchants
+  end
+
+  def find_by_id(id)
+    all.find {|merchant| merchant.id == id.to_s}
+  end
+
+  def find_by_name(name)
+    all.find {|merchant| merchant.name.upcase == name.upcase}
+  end
+
+  def find_all_by_name(name_fragment)
+    all.find_all do |merchant|
+      merchant.name.upcase.include?(name_fragment.upcase)
+    end
   end
 
 end
--- test/merchant_respository_test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/merchant_repository'
-
-class MerchantRepositoryTest < Minitest::Test
-
-
-end
