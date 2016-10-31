1188f90064f9c4e558759ad6704c537a14d80dc8
Ilana Corson <ilana.corson@gmail.com>
2016-10-31 13:14:42 -0600

    add colon to minitest directive

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/merchant'
 
-class MerchantTest < Minitest:Test
+class MerchantTest < Minitest::Test
 
   def test_merchant_has_a_name_and_id
     m = Merchant.new({:id => 5, :name => "Turing School"})
