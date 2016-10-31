4d610ee1895cd200431329085254e35ef3cd24ac
Daniel Rodriguez <drod2793@gmail.com>
2016-10-31 16:47:59 -0600

    Repositories: commented method requirements

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,3 +1,31 @@
 class ItemRepository
 
+  #returns an array of all known Item instances
+  def all
+  end
+
+  #returns either nil or an instance of Item with a matching ID
+  def find_by_id
+  end
+
+  #returns either nil or an instance of Item having done a case insensitive search
+  def find_by_name
+  end
+
+  #returns either [] or instances of Item where the supplied string appears in the item description (case insensitive)
+  def find_all_with_description
+  end
+
+  #returns either [] or instances of Item where the supplied price exactly matches
+  def find_all_by_price
+  end
+
+  #returns either [] or instances of Item where the supplied price is in the supplied range (a single Ruby range instance is passed in)
+  def find_all_by_price_in_range
+  end
+
+  #returns either [] or instances of Item where the supplied merchant ID matches that supplied
+  def find_all_by_merchant_id
+  end
+
 end
--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,2 +1,19 @@
 class MerchantRepository
+
+  #returns an array of all known Merchant instances
+  def all
+  end
+
+  #returns either nil or an instance of Merchant with a matching ID
+  def find_by_id
+  end
+
+  #returns either nil or an instance of Merchant having done a case insensitive search
+  def find_by_name
+  end
+
+  #returns either [] or one or more matches which contain the supplied name fragment, case insensitive
+  def find_all_by_name
+  end
+
 end
