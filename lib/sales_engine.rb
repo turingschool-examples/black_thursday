f67adaaa277d1348b4423c81fb6f6a7a6ac7374e
Mike Schutte <tmikeschutte@gmail.com>
2016-10-31 11:37:28 -0600

    initial commit

---
+++ lib/sales_engine.rb
@@ -0,0 +1,7 @@
+class SalesEngine
+
+  def from_csv(hash)
+    
+  end
+
+end
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,8 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require "minitest/nyan_cat"
+require './lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+
+end
