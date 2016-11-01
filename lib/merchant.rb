6ee117d3b1029b190389a8e25c86dd35003f7e0a
Ethan Bennett <ebennett19@gmail.com>
2016-10-31 23:18:12 -0600

    Working merchant/proto-MerchantRepo classes

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -2,26 +2,34 @@ require 'pry'
 require 'csv'
 
 class FakeRepository
-  attr_accessor  :merchants_repo
+  attr_reader  :all
 
   def initialize
-    @handle =  CSV.open './data/merchants_test.csv', headers: true, header_converters: :symbol
-    @merchants_repo = []
+    @handle = CSV.open './data/merchants.csv', headers: true, header_converters: :symbol
+    @all    = @handle.map { |line| Merchant.new(line).merchant }
   end
 
-  def merchant_list
-    @handle.each do |line|
-      @merchants_repo << Merchant.new(line).merchant
+  def find_by_id(id)
+    @all.find { |merchant| merchant["id"].eql?(id) }
+  end 
+
+  def find_by_name(name)
+    @all.find { |merchant| merchant["name"].eql?(name) }
+  end
+
+  def find_all_by_name(fragment)
+    results = @all.find_all do |merchant|
+      merchant["name"].downcase.include?(fragment.downcase)
     end
-  puts @merchants_repo
+  results
   end
 end
 
 class Merchant < FakeRepository
-  attr_accessor  :merchant
+  attr_reader  :merchant
 
   def initialize(line)
-    @line = line
+    @line     = line
     @merchant = { "id" => id, "name" => name }
   end
 
@@ -35,4 +43,4 @@ class Merchant < FakeRepository
 
 end
 
-FakeRepository.new.merchant_list
+puts FakeRepository.new.find_all_by_name("Jai")
