13ad40f2fe56b29ac20c5149bb2fbefc35bdfba8
Jesse Shipley <jesse.radack@gmail.com>
2016-11-01 10:46:40 -0600

    adding to master

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,4 +1,21 @@
-class Merchant
+require './lib/merchant_repository'
 
+class Merchant
+  attr_reader  :merchant
+
+  def initialize(line)
+    @line     = line
+    @merchant = { "id" => id, "name" => name }
+  end
+
+  def id
+    @line[:id]
+  end
+
+  def name
+    @line[:name]
+  end
 
 end
+
+puts MerchantRepository.new.find_all_by_name("ime")
