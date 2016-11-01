38c9fe9cf0a32da86e6be49cae7431063a80744c
Molly Brown <mollbrown@gmail.com>
2016-11-01 14:09:49 -0600

    Finish item repository and added passing tests

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -22,4 +22,19 @@ class ItemRepository
     end
   end
 
+  def find_all_by_price(unit_price)
+    all.find_all {|item| item.unit_price == unit_price}
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    all.find_all {|item| item.merchant_id == merchant_id}
+  end
+
+  def find_all_by_price_in_range(price_range)
+    all.find_all {|item| price_range.include?(item.unit_price)}
+  end
+
+
+
+
 end
