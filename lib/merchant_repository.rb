19bce1daf8a3b6c1117b17b9a835bccc8d928e12
Daniel Rodriguez <drod2793@gmail.com>
2016-11-01 17:09:23 -0600

    mr fixed

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,33 +1,49 @@
-require 'csv'
 require_relative 'merchant'
+require 'csv'
+require 'pry'
 
 class MerchantRepository
 
-  def initialize(item_hash)
-    @id = item_hash[:id]
-    @name = item_hash[:name]
-    @created_at = item_hash[:created_at]
-    @updated_at = item_hash[:updated_at]
-  end
-  #returns an array of all known Merchant instances
-  # def all
-  #   ObjectSpace.each_object(MerchantRepository) {|merchant| << merchant}
-  # end
+  attr_reader   :contents,
+                :merchants
 
-  #returns either nil or an instance of Merchant with a matching ID
+  def initialize(path)
+    @contents = CSV.open path, headers: true, header_converters: :symbol
+    @merchants = contents.map do |line|
+      Merchant.new(line)
+    end
+  end
+
+  def all
+    @merchants
+  end
+
   def id(id_number)
-    csv_text = File.read('merchants.csv')
-    csv = CSV.parse(csv_text, :headers => true)
-      csv.each do |row|
-        puts csv.find {|row| row[id_number]}
+    value = nil
+    merchants.each do |merchant|
+      if merchant.id == id_number
+        value = merchant
       end
-  end
-  #returns either nil or an instance of Merchant having done a case insensitive search
-  def find_by_name
+    end
+    value
   end
 
-  #returns either [] or one or more matches which contain the supplied name fragment, case insensitive
-  def find_all_by_name
+  def find_by_name(name)
+    value = nil
+    merchants.each do |merchant|
+      if merchant.name == name
+        value = merchant
+      end
+    value
+  end
+
+  def find_all_by_name(partial_search)
+    by_name = []
+    merchants.each do |merchant|
+      if merchant.name.include? partial_search
+        by_name << merchant
+      end
+      by_name
   end
 
 end
