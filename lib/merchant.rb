ba38559c1ef7d7c7aa29666f740cee297dde37ef
Jesse Shipley <jesse.radack@gmail.com>
2016-11-01 10:44:02 -0600

    fixing merge error, deleted merchant innards

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,17 +1,4 @@
 class Merchant
 
-  attr_reader :id,
-              :name,
-              :created,
-              :updated
-              
-  def initialize(details)
-    @id = details[:id].to_i
-    @name = details[:name]
-    @created = details[:created_at]
-    @updated = details[:updated_at]
-  end
-
-  
 
 end
