c2eb109d697e57a8817b53e330093e7583d5992e
Mike Schutte <tmikeschutte@gmail.com>
2016-11-01 15:19:19 -0600

    cleans methods with @all

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -7,12 +7,12 @@ class ItemRepository
   include FindFunctions
 
   attr_reader :file_contents, 
-              :item_objects
+              :all
 
   def initialize(file_name = nil)
     return unless file_name
     @file_contents = load(file_name)
-    @item_objects = create_item_objects
+    @all = create_item_objects
   end
 
   def load(file_name)
@@ -20,35 +20,31 @@ class ItemRepository
   end
 
   def create_item_objects
-    @file_contents.map {|row| Item.new(row)}
-  end
-
-  def all
-    @item_objects
+    @all.map {|row| Item.new(row)}
   end
 
   def find_by_id(id)
-    find_by(@item_objects, :id, id)
+    find_by(:id, id)
   end
 
   def find_by_name(name)
-    find_by(@item_objects, :name, name)
+    find_by(:name, name)
   end
 
   def find_all_with_description(description)
-    find_all(@item_objects, :description, description)
+    find_all(:description, description)
   end
 
   def find_all_by_price(price)
-    find_all(@item_objects, :unit_price, price)
+    find_all(:unit_price, price)
   end
 
   def find_all_by_price_in_range(price_range)
-    @item_objects.find_all {|item| price_range.include?(item.unit_price)}
+    @all.find_all {|item| price_range.include?(item.unit_price)}
   end
 
   def find_all_by_merchant_id(merchant_id)
-    find_all(@item_objects, :merchant_id, merchant_id)
+    find_all(:merchant_id, merchant_id)
   end
 
 end
