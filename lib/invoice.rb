f93d1194b535ee38bd5be39acb6c647cb6dfd262
njgheorghita <njgheorghita@email.wm.edu>
2016-11-01 17:52:40 -0600

    invoice structure created, with tests, everything is working, don't break it

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,17 +1,18 @@
-class Inventory
+class Invoice
 
   attr_reader :id,
               :customer_id,
               :merchant_id,
               :status,
-              :
+              :created_at,
+              :updated_at
 
   def initialize(data)
-    @id = data[:id]
+    @id           = data[:id].to_i
     @customer_id  = data[:customer_id]
     @merchant_id  = data[:merchant_id]
-    @status       = data[:status]
-    @created_at   = data[:created_at] 
+    @status       = data[:status].to_sym
+    @created_at   = data[:created_at]
     @updated_at   = data[:updated_at]
   end
 end
