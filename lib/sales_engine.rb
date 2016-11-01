b2d435a9de2ebb71cea9e7c6257261a4124a45d5
Ann Wolff <annwolffe@gmail.com>
2016-11-01 08:53:50 -0600

    builds items in SalesEngine and tests

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,8 +1,13 @@
 require 'csv'
+require 'htmlentities'
 require_relative '../lib/merchant_repository'
-# require_relative '../lib/item_repository'
+require_relative '../lib/merchant'
+require_relative '../lib/item_repository'
+require_relative '../lib/item'
 
 class SalesEngine
+
+  attr_reader :items, :merchants
 
   def from_file(data)
     insert_item_repository(data[:items])
@@ -10,16 +15,37 @@ class SalesEngine
   end
 
   def insert_merchant_repository(file_path)
-    merchants_data = CSV.read(file_path, headers:true)
-    merchants_collection = []  
-    merchants_data.foreach do |row|
-      merchants_collection << Merchant.new({:id => row[:id], :name => row[:name]})
-    end
-    @merchants = MerchantRepository.new(merchants_collection)
+    merchants_data = parse_merchants(file_path)
+    @merchants = MerchantRepository.new(merchants_data)
   end
 
   def insert_item_repository(file_path)
-    true
+    items_data = parse_items(file_path)
+    @items = ItemRepository.new(items_data)
+  end
+
+  def parse_merchants(file_path)
+    merchants_data = []
+    CSV.foreach(file_path, headers:true) do |row|
+      merchants_data << Merchant.new({:id => row['id'].to_i, 
+                                      :name => row['name']})
+    end
+    merchants_data
+  end
+
+  def parse_items(file_path)
+    items_data = []
+    CSV.foreach(file_path, headers:true) do |row|
+      items_data << Item.new({:id => row['id'].to_i, 
+                              :name => row['name'],
+                              :description => HTMLEntities.new.decode(row['description']),
+                              :unit_price => BigDecimal.new(row['unit_price']),
+                              :merchant_id => row['merchant_id'],
+                              :created_at => Time.parse(row['updated_at']),
+                              :updated_at => Time.parse(row['created_at'])
+                            })
+    end
+    items_data
   end
 
 end
