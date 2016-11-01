e21f45797e02480ceef4947b059caf3596d12491
Jesse Shipley <jesse.radack@gmail.com>
2016-10-31 22:23:48 -0600

    restructured merch class, with tests for funct

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,7 +1,17 @@
 class Merchant
 
-  def id
-    12334105
+  attr_reader :id,
+              :name,
+              :created,
+              :updated
+              
+  def initialize(details)
+    @id = details[:id].to_i
+    @name = details[:name]
+    @created = details[:created_at]
+    @updated = details[:updated_at]
   end
+
+  
 
 end
