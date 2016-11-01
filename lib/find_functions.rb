4d9e2644b23dc653a815ed1590f725fe5a5dd660
Mike Schutte <tmikeschutte@gmail.com>
2016-11-01 14:28:03 -0600

    finalizes find functions module and test

--- lib/find_functions.rb
+++ lib/find_functions.rb
@@ -5,17 +5,22 @@ module FindFunctions
   end
 
   def find_all(collection, method, input)
-    return find_all_numbers(collection, method, input) if input.is_a? Fixnum
+    return find_all_prices(collection, input)    if method == :unit_price
+    return find_all_merch_ids(collection, input) if method == :merchant_id
     find_all_strings(collection, method, input)
   end
 
-  def find_all_numbers(collection, method, input)
-    collection.find_all {|row| row.send(method) == input }
+  def find_all_prices(collection, input)
+    collection.find_all {|row| row.unit_price.to_f == input.to_f }
+  end
+
+  def find_all_merch_ids(collection, input)
+    collection.find_all {|row| row.merchant_id == input }
   end
 
   def find_all_strings(collection, method, input)
-    collection.find_all do |row| 
-      row = row.send(method).to_s.downcase
+    collection.find_all do |row|
+      row = row.send(method).downcase
       row.include?(input.downcase)
     end
   end
--- test/find_functions_test.rb
+++ test/find_functions_test.rb
@@ -1,13 +1,16 @@
 require_relative 'test_helper'
 require './lib/find_functions'
 require './lib/item_repository'
+require './lib/merchant_repository'
 
 class FindFunctionsTest < Minitest::Test
   include FindFunctions
-  attr_reader :collection
+  attr_reader :collection, 
+              :collection2
   
   def setup
    @collection = ItemRepository.new("./data/test_items.csv").item_objects
+   @collection2 = MerchantRepository.new("./data/test_merchants.csv").merchant_objects
   end
 
   def test_find_functions_exists_and_is_a_module
@@ -24,15 +27,94 @@ class FindFunctionsTest < Minitest::Test
     assert_equal nil, find_by(collection, :name, input)
   end
 
-  def test_it_finds_all_instances_of_an_element
-    input = "510+ RealPush Icon Set"
-    assert_equal 1, find_all(collection, :name, input).count
+  def test_find_all_prices_returns_item_object_for_unique_price
+    assert find_all_prices(collection, 119.00).one?{|item| item.class == Item}
   end
 
-  def test_it_returns_an_empty_array_if_none_are_found
-    input = "BetterCallSaul"
-    assert_equal [], find_all(collection, :name, input)
+  def test_find_all_prices_returns_one_object_for_a_unique_price
+    assert find_all_prices(collection, 119.00).count == 1
   end
 
-  # def test_it_checks_if_attribute_is
+  def test_find_all_prices_returns_item_objects_for_common_price
+    assert find_all_prices(collection, 12.00).all?{|item| item.class == Item}
+  end
+
+  def test_find_all_prices_returns_more_than_one_object_for_common_price
+    assert find_all_prices(collection, 12.00).count > 1
+  end
+
+  def test_find_all_prices_returns_an_empty_array_if_no_prices_found
+    assert_equal [], find_all_prices(collection, 18.75)
+  end
+
+  def test_find_all_merch_ids_returns_item_objects
+    assert find_all_merch_ids(collection, 12334185).all?{|item| item.class == Item}
+  end
+
+  def test_find_all_merch_ids_returns_more_than_one_object_for_common_merch_id
+    assert find_all_merch_ids(collection, 12334185).count > 1
+  end
+  
+  def test_find_all_merch_ids_returns_item_object_for_unique_price
+    assert find_all_merch_ids(collection, 12334141).one?{|item| item.class == Item}
+  end
+
+  def test_find_all_merch_ids_returns_one_object_for_a_unique_price
+    assert_equal 1, find_all_merch_ids(collection, 12334141).count
+  end
+
+  def test_find_all_merch_ids_returns_an_empty_array_if_no_merch_ids_found
+    assert_equal [], find_all_merch_ids(collection, 1)
+  end
+  
+  def test_finds_all_strings_returns_matching_item_object_for_description
+    input = "handwoven pillow"
+    assert_equal 1, find_all_strings(collection, :description, input).count
+  end
+  
+  def test_finds_all_strings_returns_matching_merchant_object_for_name
+    input = "MiniatureBikez"
+    assert_equal 1, find_all_strings(collection2, :name, input).count
+  end
+  
+  def test_finds_all_strings_returns_matching_merchant_object_for_name_fragment
+    input = "mad"
+    assert_equal 5, find_all_strings(collection2, :name, input).count
+  end
+
+  def test_find_all_strings_returns_an_empty_array_if_none_are_found
+    input = "schutte"
+    assert_equal [], find_all_strings(collection, :description, input)
+  end
+  
+  def test_finds_all_strings_returns_matching_merchant_object_for_name
+    input = "MiniatureBikez"
+    assert_equal 1, find_all_strings(collection2, :name, input).count
+  end
+  
+  def test_finds_all_strings_returns_matching_merchant_object_for_name_fragment
+    input = "mad"
+    assert_equal 5, find_all_strings(collection2, :name, input).count
+  end
+
+  def test_find_all_returns_item_objects_for_common_price_if_method_unit_price
+    prices = find_all(collection, :unit_price, 12.00)
+    assert prices.all?{|item| item.class == Item}
+  end
+
+  def test_find_all_returns_item_objects_for_common_merch_id_if_method_merchant_id
+    merchant_ids = find_all(collection, :merchant_id, 12334141)
+    assert merchant_ids.all?{|item| item.class == Item}
+  end
+
+  def test_find_all_returns_merchant_objects_for_common_name_if_method_name
+    names = find_all(collection2, :name, "MiniatureBikez")
+    assert names.all?{|item| item.class == Merchant}
+  end
+
+  def test_find_all_returns_item_objects_for_common_name_if_method_name
+    descriptions = find_all(collection, :description, "mad")
+    assert descriptions.all?{|item| item.class == Item}
+  end
+  
 end
