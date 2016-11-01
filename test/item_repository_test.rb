569a0ae9dcbf052fd81534d5cdae78bdb5db4197
Ann Wolff <annwolffe@gmail.com>
2016-10-31 19:17:26 -0600

    implements item_repo_tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -12,17 +12,18 @@ class ItemRepositoryTest < Minitest::test_helper
                                           :merchant_id => 12334141, 
                                           :created_at => Time.utc(2016, 1, 11, 9, 34, 06), 
                                           :updated_at => Time.utc(2007, 6, 4, 21, 35, 10)
-                                          })
-                                Item.new({:id => 263395721, 
-                                          :name => "Disney scrabble frames", 
+                                          }),
+                                Item.new({:id => 263395617, 
+                                          :name => "Glitter scrabble frames", 
                                           :description => HTMLEntities.new.decode("Disney glitter frames Any colour glitter available and can do any characters you require Different colour scrabble tiles Blue Black Pink Wooden"), 
                                           :unit_price => BigDecimal.new(1350), 
                                           :merchant_id => 12334185, 
                                           :created_at => Time.utc(2016, 01, 11, 11, 51, 37), 
-                                          :updated_at => Time.utc(2008, 04, 02, 13, 48, 57)})
-                                Item.new({:id => 263395721, 
+                                          :updated_at => Time.utc(2008, 04, 02, 13, 48, 57)
+                                          }),
+                                Item.new({:id => 263396013, 
                                           :name => "Free standing Woden letters", 
-                                          :description => HTMLEntities.new.decode("Disney glitteFree standing wooden letters 15cm Any colours"), 
+                                          :description => HTMLEntities.new.decode("Disney glitter Free standing wooden letters 15cm Any colours"), 
                                           :unit_price => BigDecimal.new(700), 
                                           :merchant_id => 12334185, 
                                           :created_at => Time.utc(2016, 0, 11, 11, 51, 36), 
@@ -31,8 +32,53 @@ class ItemRepositoryTest < Minitest::test_helper
                                 ])
   end
 
+  def test_it_finds_by_id
+    assert_equal "Free standing Woden letters", repo.find_by_id(263395721).name
+  end
 
-  def test_it
+  def test_it_returns_nil_when_id_not_present
+    assert_equal nil, repo.find_by_id(45)
+  end
+
+  def test_it_finds_by_name_when_present
+    assert_equal 263395721, repo.find_by_name("Free standing Woden letters").id
+  end
+
+  def test_it_returns_nil_when_name_not_present
+    assert_equal nil, repo.find_by_name("Hello")
+  end
+
+  def test_it_returns_empty_array_when_description_not_present
+    assert_equal [], repo.find_all_by_description("Boogers")
+  end
+
+  def test_it_returns_array_when_description
+    assert_equal "Free standing Woden letters", repo.find_all_by_description("Disney glitter")[0].name
+  end
+
+  def test_it_finds_item_by_exact_price
+    assert_equal "Free standing Woden letters", repo.find_all_by_price(13.50)
+  end
+
+  def test_it_returns_empty_array_when_no_prices_match
+    assert_equal [], repo.find_all_by_price(20.00)
+  end
+
+  def test_it_returns_array_of_price_in_range
+    assert_equal "Free standing Woden letters", repo.find_all_by_price(10.00..14.00)[1].name
+  end
+
+  def test_it_returns_empty_array_if_no_price_in_range
+    assert_equal [], repo.find_all_by_price(14.00..15.00)
+  end
+
+  def test_it_returns_array_of_items_that_have_matching_merchant_id
+    assert_equal "Disney scrabble frames", repo.find_all_by_merchant_id(12334185)[0].name
+  end
+
+  def test_it_returns_empty_array_if_items_have_merchant_id
+    assert_equal [], repo.find_all_by_merchant_id(45)
+  end
 
 end
 
