39115df3729533028e175a1418569d9c090f494a
Kyle Heppenstall <[kyle.heppenstall@gmail.com]>
2016-11-02 16:47:44 -0600

    Adds invoice repo and tests.

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -8,6 +8,22 @@ class InvoiceRepository
     @all = invoices
   end
 
+  def find_by_id(id)
+    all.find {|invoice| invoice.id == id}
+  end
+
+  def find_all_by_customer_id(customer_id)
+    all.find_all {|invoice| invoice.customer_id == customer_id}
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    all.find_all {|invoice| invoice.merchant_id == merchant_id}
+  end
+
+  def find_all_by_status(status)
+    all.find_all {|invoice| invoice.status == status}
+  end
+
   def inspect
     "#<#{self.class} #{@all.size} rows>"
   end
