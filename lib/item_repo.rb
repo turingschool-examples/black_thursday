79ca74c680a5e5c56f3d84da9c5f5c43c06207e5
Amara Lovato <amaracuenca@gmail.com>
2016-11-01 23:22:08 -0600

    fixes item_repo_test.rb

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -3,7 +3,8 @@ require 'csv'
 require 'pry'
 
 class ItemRepo
-  def initialize 
+  attr_reader :all
+  def initialize
     # @parent = SalesEngine.new
     @all = []
   end
@@ -11,15 +12,16 @@ class ItemRepo
   def set_up(file)
     file = CSV.read "./data/items.csv", headers: true, header_converters: :symbol
     parse_file(file)
-  end 
+  end
 
   def all
+    @all
   end
-  
+
   def parse_file(file)
     CSV.foreach("./data/items.csv") do |row|
       next if row[0] == "id"
-      instantiate_item(row)  
+      instantiate_item(row)
     end
   end
 
@@ -35,43 +37,52 @@ class ItemRepo
       @all << Item.new(item, self)
   end
 
-
-  # def find_by_id(id_num)
-  #   right_item = []
-  #   binding.pry
-  #   @file.find do |item|
-  #     right_item << item[:id] == id_num
-  #     binding.pry
-  #     right_item
-  #   end
-  # end
-
-  def find_by_name
-    #search for a name
-    #return item with that name, case insensitive
+  def find_by_id(desired_id)
+    @all.find do |item|
+      item.id == desired_id
+    end
   end
 
-  def find_all_with_description
+  def find_by_name(desired_name)
+    @all.find do |item|
+      item.name.downcase == desired_name
+      return item.id
+    end
+  end
+
+  def find_all_with_description(desired_description)
+    @all.find_all do |item|
+      item.description.downcase == desired_description
+    end
     #returns instances of item where the supplied string occurs in the description
     #case insensitive
   end
 
-  def find_all_by_price
-    #returns objects that match with price provided
+  def find_all_by_price(desired_price)
+  #returns objects that match with price provided
+    @all.find_all do |item|
+      item.unit_price == desired_price
+      return item.name
+    end
+
   end
 
-  def find_all_by_price_in_range
-      #returns objects that match with range price provided
+  def find_all_by_price_in_range(price1, price2)
+    #returns objects that match with range price provided
+      @all.find_all do |item|
+        item.unit_price >= price1 &&
+        item.unit_price <= price2
+        return item.name
+      end
   end
 
-  def find_all_by_merchant_id
+  def find_all_by_merchant_id(merchant_id)
     #search merchant with that id
     #returns all items of that merchant
+    @all.find_all do |item|
+      item.merchant_id == merchant_id
+      return item.name
+    end
+
   end
 end
-
-
-trial = ItemRepo.new
-file = trial.set_up("./data/items.csv")
-trial.parse_file(file)
-
