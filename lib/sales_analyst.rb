6330ebd1ed32aeaeecb769cadc9a949a6c40f4ab
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2016-11-04 08:51:32 -0600

    final method for iteration 1

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -18,10 +18,6 @@ class SalesAnalyst
   Math.sqrt(mean.reduce(:+) / sales_engine.merchants.all.count).round(2)
   end
 
-  def item_count_per_merchant
-    sales_engine.merchants.all.map { |merchant| merchant.items.count }
-  end
-
   def merchants_with_high_item_count
     threshold = (average_items_per_merchant_standard_deviation + average_items_per_merchant)
     sales_engine.merchants.all.reduce([]) do |result, merchant|
@@ -33,12 +29,23 @@ class SalesAnalyst
   end
 
   def average_item_price_for_merchant(merchant_id)
-    #find items by merchant id
-    #find the prices associated with those items for that merchant
-    #average those prices
+    merchant_items = sales_engine.items.find_all_by_merchant_id(merchant_id)
+    total = merchant_items.reduce(0) do |result, item|
+      result += item.unit_price
+      result
+    end
+    (total / merchant_items.count).to_d.round(2)
   end
 
   def average_average_price_per_merchant
-    
+    total_average_price = sales_engine.merchants.all.reduce(0) do |result, merchant|
+     result += average_item_price_for_merchant(merchant.id)
+     result
+   end
+    (total_average_price / sales_engine.merchants.all.count).floor(2)
+  end
+
+  def golden_items
+    threshold = (average_average_price_per_merchant + average_items_per_merchant_standard_deviation)
   end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -25,7 +25,18 @@ class SalesAnalystTest < Minitest::Test
 
   def test_sales_analyst_can_find_average_item_price_for_merchant_by_id
     se = SalesEngine.from_csv(file_path)
-    assert_equal 1, SalesAnalyst.new(se).average_item_price_for_merchant(6)
+    assert_equal 33.5, SalesAnalyst.new(se).average_item_price_for_merchant(12334871)
+    assert_equal 813.33, SalesAnalyst.new(se).average_item_price_for_merchant(12334371).round(2)
+  end
+
+  def test_sales_analyst_can_find_the_average_average_price_per_merchant
+    se = SalesEngine.from_csv(file_path)
+    assert_equal 350.29, SalesAnalyst.new(se).average_average_price_per_merchant
+  end
+
+  def test_sales_analyst_can_find_the_golden_items
+    se = SalesEngine.from_csv(file_path)
+    assert_equal 1, SalesAnalyst.new(se).golden_items.count 
   end
 
   def file_path
