bf0f983bdc15867c7fd3578c9174a49ee368ca16
Ethan Bennett <ebennett19@gmail.com>
2016-11-05 14:56:00 -0600

    100% compliant with spec harness except remaining st dev methods

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,5 +1,7 @@
 require_relative 'sales_engine'
 require_relative 'standard_deviation'
+require 'bigdecimal'
+require 'pry'
 
 class SalesAnalyst
   include StandardDeviation
@@ -15,26 +17,26 @@ class SalesAnalyst
       merchants_and_items = sales_engine.merchants.all.map do |merchant|
         sample_set << sales_engine.find_all_items_by_merchant_id(merchant.id).count
       end
-    standard_deviation(sample_set).round(2)
+    standard_deviation(sample_set).round(2).to_f
   end
 
   def average_items_per_merchant
-    @item_counts = sales_engine.merchants.all.map { |merchant| merchant.items.size }
-    average(@item_counts)
+    @item_counts = sales_engine.merchants.all.map { |merchant| BigDecimal.new(merchant.items.size) }
+    average(@item_counts).round(2).to_f
   end
 
   def average_item_price_for_merchant(id)
     all_prices = sales_engine.merchants.find_all_items_by_merchant(id).map do |item|
-      item.unit_price.to_f
+      item.unit_price
     end
-    average(all_prices)
+    average(all_prices).round(2)
   end
 
   def average_average_price_per_merchant
     averages = sales_engine.merchants.all.map do |merchant|
-      average_item_price_for_merchant(merchant.id)
+      BigDecimal.new(average_item_price_for_merchant(merchant.id))
     end
-    average(averages)
+    average(averages).round(2)
   end
 
   def average(collection)
@@ -42,9 +44,9 @@ class SalesAnalyst
   end
 
   def merchants_with_high_item_count
-    names = sales_engine.merchants.all.map { |merchant| merchant.name }
-    @item_counts.sort!.reverse!
-    @item_counts.zip(names)[0..2]
+    # names = sales_engine.merchants.all.map { |merchant| merchant.name }
+    # @item_counts.sort!.reverse!
+    # @item_counts.zip(names)[0..2]
   end
 
   def golden_items
