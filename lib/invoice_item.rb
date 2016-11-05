12d15bee25ed16b82f4146320e37419eb8bccf3b
Daniel Rodriguez <drod2793@gmail.com>
2016-11-04 18:10:34 -0600

    II: Done

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -2,28 +2,28 @@ require 'time'
 require 'bigdecimal'
 
 class InvoiceItem
-  # attr_reader   :id,
-  #               :item_id,
-  #               :invoice_id,
-  #               :quantity,
-  #               :unit_price,
-  #               :created_at,
-  #               :updated_at,
-  #               :parent
+  attr_reader   :id,
+                :item_id,
+                :invoice_id,
+                :quantity,
+                :unit_price,
+                :created_at,
+                :updated_at,
+                :parent
 
-  # def initialize(invoice_item_hash, parent=nil)
-  #   @id = invoice_item_hash[:id].to_i
-  #   @item_id = invoice_item_hash[:item_id].to_i
-  #   @invoice_id = invoice_item_hash[:invoice_id].to_i
-  #   @quantity = invoice_item_hash[:quantity]
-  #   @unit_price = BigDecimal(format_unit_price(invoice_item_hash[:unit_price]))
-  #   @created_at = Time.parse(invoice_item_hash[:created_at])
-  #   @updated_at = Time.parse(invoice_item_hash[:updated_at])
-  #   @parent = parent
-  # end
+  def initialize(invoice_item_hash, parent=nil)
+    @id = invoice_item_hash[:id].to_i
+    @item_id = invoice_item_hash[:item_id].to_i
+    @invoice_id = invoice_item_hash[:invoice_id].to_i
+    @quantity = invoice_item_hash[:quantity].to_i
+    @unit_price = BigDecimal(format_unit_price(invoice_item_hash[:unit_price]))
+    @created_at = Time.parse(invoice_item_hash[:created_at])
+    @updated_at = Time.parse(invoice_item_hash[:updated_at])
+    @parent = parent
+  end
 
-  # def format_unit_price(price)
-  #   price.chars.insert(-3, ".").join
-  # end
+  def format_unit_price(price)
+    price.chars.insert(-3, ".").join
+  end
 
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -5,16 +5,14 @@ require 'minitest/pride'
 require './lib/invoice_item'
 #require './lib/invoice_item_repository'
 require './lib/sales_engine'
-require 'pry'
 
 class InvoiceItemTest < Minitest::Test
-    attr_reader   :invoice_item,
-                  :invoice_item_2,
-                  #:repository
+  attr_reader   :invoice_item,
+                :invoice_item_2
+                #:repository
 
   def setup
     #@repository = InvoiceItemRepository.new('./fixture/invoice_items.csv')
-    binding.pry
     @invoice_item = InvoiceItem.new({
       :id => "1",
       :item_id => "1",
@@ -25,7 +23,7 @@ class InvoiceItemTest < Minitest::Test
       :updated_at => "2015-10-10 11:11:37 UTC",
     })
 
-    @invoice_item = InvoiceItem.new({
+    @invoice_item_2 = InvoiceItem.new({
       :id => "2",
       :item_id => "2",
       :invoice_id => "102",
@@ -37,37 +35,31 @@ class InvoiceItemTest < Minitest::Test
   end
 
   def test_it_can_create_an_invoice_item
-    binding.pry
     assert invoice_item
     assert invoice_item_2
   end
 
   def test_it_can_return_id
-    skip
     assert_equal 1, invoice_item.id
     assert_equal 2, invoice_item_2.id
   end
 
   def test_it_can_return_item_id
-    skip
     assert_equal 1, invoice_item.item_id
     assert_equal 2, invoice_item_2.item_id
   end
 
   def test_it_can_return_invoice_id
-    skip
     assert_equal 101, invoice_item.invoice_id
     assert_equal 102, invoice_item_2.invoice_id
   end
 
   def test_it_can_return_quantity
-    skip
-    assert_equal 101, invoice_item.quantity
-    assert_equal 102, invoice_item_2.quantity
+    assert_equal 2, invoice_item.quantity
+    assert_equal 2, invoice_item_2.quantity
   end
 
   def test_it_can_return_unit_price
-    skip
     assert_equal 10.99, invoice_item.unit_price
     assert_equal 20.00, invoice_item_2.unit_price
     assert_instance_of BigDecimal, invoice_item.unit_price
@@ -75,13 +67,11 @@ class InvoiceItemTest < Minitest::Test
   end
 
   def test_it_can_return_created_at_as_time
-    skip
     assert_instance_of Time, invoice_item.created_at
     assert_instance_of Time, invoice_item_2.created_at
   end
 
   def test_it_can_return_updated_at_as_time
-    skip
     assert_instance_of Time, invoice_item.updated_at
     assert_instance_of Time, invoice_item_2.updated_at
   end
