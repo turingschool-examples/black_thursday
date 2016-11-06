3ecf4e1377748aac44c333be7365c3f5190c6228
Ethan Bennett <ebennett19@gmail.com>
2016-11-05 20:27:51 -0600

    I2 status method done

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -18,22 +18,6 @@ class SalesAnalyst
   def average_invoices_per_merchant_standard_deviation
     BigDecimal.new(standard_deviation(invoice_counts).to_s).round(2).to_f
   end
-
-  # def high_invoice_qualifier
-  #   average_invoices_per_merchant + standard_deviation(invoice_counts) + standard_deviation(invoice_counts)
-  # end
-
-  # def top_merchants_by_invoice_count
-  #   invoice_counts.find_all do |invoice_count|
-  #     invoice_count > high_invoice_qualifier
-  #   end
-  # end
-
-  # def merchants_with_high_item_count
-  #   item_counts.find_all do |item_count|
-  #   item_count > average_items_per_merchant + standard_deviation(item_counts)
-  #   end
-  # end
   
   def average_items_per_merchant
     BigDecimal.new(average(item_counts).to_s).round(2).to_f
@@ -41,6 +25,32 @@ class SalesAnalyst
 
   def average_invoices_per_merchant
     BigDecimal.new(average(invoice_counts).to_s).round(2).to_f
+  end
+
+  def average_item_price_for_merchant(id)
+    BigDecimal.new(average(price_counts(id)).to_s).round(2)
+  end
+
+  def average_average_price_per_merchant
+    averages = sales_engine.merchants.all.map do |merchant|
+      average_item_price_for_merchant(merchant.id)
+    end
+    BigDecimal.new(average(averages).to_s).round(2)
+  end
+
+  def invoice_status(status)
+    matches = sales_engine.invoices.all.find_all do |invoice| 
+      invoice.id if invoice.status.eql?(status)
+    end
+    BigDecimal.new(status_average_operator(matches)).round(2).to_f
+  end
+
+  def status_average_operator(matches)
+    ((matches.size.to_f / sales_engine.invoices.all.size.to_f) * 100).to_s
+  end
+
+  def average(collection)
+    collection.reduce(&:+).to_f / collection.size.to_f
   end
 
   def invoice_counts
@@ -51,22 +61,10 @@ class SalesAnalyst
     sales_engine.merchants.all.map { |merchant| merchant.items.size }
   end
 
-  def average_item_price_for_merchant(id)
-    all_prices = sales_engine.merchants.find_all_items_by_merchant(id).map do |item|
+  def price_counts(id)
+    sales_engine.merchants.find_all_items_by_merchant(id).map do |item|
       item.unit_price
     end
-    BigDecimal.new(average(all_prices).to_s).round(2)
-  end
-
-  def average_average_price_per_merchant
-    averages = sales_engine.merchants.all.map do |merchant|
-      average_item_price_for_merchant(merchant.id)
-    end
-    BigDecimal.new(average(averages).to_s).round(2)
-  end
-
-  def average(collection)
-    collection.reduce(&:+).to_f / collection.size.to_f
   end
 
   # def golden_items
