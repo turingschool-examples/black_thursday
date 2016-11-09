27e43dc362afaba8fc80722c4eb88e3f93d43d9d
Ann Wolff <annwolffe@gmail.com>
2016-11-08 18:27:48 -0700

    re-arranges sales analyst to prettify

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -10,7 +10,7 @@ class SalesAnalyst
   include Statistics
   extend Forwardable
 
-  attr_reader :engine, :items_count_analyst, :items_price_analyst, :invoice_count_analyst, :merchants_revenue_analyst
+  attr_reader :engine
 
   def initialize(engine)
     @engine = engine
@@ -20,27 +20,33 @@ class SalesAnalyst
     @merchants_revenue_analyst = MerchantsRevenueAnalyst.new(engine)
   end
 
-  def_delegator :items_count_analyst, :average_items_per_merchant
-  def_delegator :items_count_analyst, :average_items_per_merchant_standard_deviation
-  def_delegator :items_count_analyst, :merchants_with_high_item_count
-  def_delegator :items_count_analyst, :merchants_with_only_one_item
-  def_delegator :items_count_analyst, :merchants_with_only_one_item_registered_in_month
-  def_delegator :items_price_analyst, :average_item_price_for_merchant
-  def_delegator :items_price_analyst, :average_average_price_per_merchant
-  def_delegator :items_price_analyst, :golden_items
-  def_delegator :invoice_count_analyst, :merchant_invoice_count
-  def_delegator :invoice_count_analyst, :average_invoices_per_merchant
-  def_delegator :invoice_count_analyst, :average_invoices_per_merchant_standard_deviation
-  def_delegator :invoice_count_analyst, :top_merchants_by_invoice_count
-  def_delegator :invoice_count_analyst, :bottom_merchants_by_invoice_count
-  def_delegator :invoice_count_analyst, :top_days_by_invoice_count
-  def_delegator :invoice_count_analyst, :invoice_status
-  def_delegator :invoice_count_analyst, :merchants_with_pending_invoices
-  def_delegator :merchants_revenue_analyst, :total_revenue_by_date
-  def_delegator :merchants_revenue_analyst, :top_revenue_earners
-  def_delegator :merchants_revenue_analyst, :merchants_ranked_by_revenue
-  def_delegator :merchants_revenue_analyst, :revenue_by_merchant
-  def_delegator :merchants_revenue_analyst, :best_item_for_merchant
-  def_delegator :merchants_revenue_analyst, :most_sold_item_for_merchant
+  def_delegators :@items_count_analyst,
+                    :average_items_per_merchant,
+                    :average_items_per_merchant_standard_deviation,
+                    :merchants_with_high_item_count,
+                    :merchants_with_only_one_item,
+                    :merchants_with_only_one_item_registered_in_month
 
+  def_delegators :@items_price_analyst,
+                    :average_item_price_for_merchant,
+                    :average_average_price_per_merchant,
+                    :golden_items
+
+  def_delegators :@invoice_count_analyst,
+                    :merchant_invoice_count,
+                    :average_invoices_per_merchant,
+                    :average_invoices_per_merchant_standard_deviation,
+                    :top_merchants_by_invoice_count,
+                    :bottom_merchants_by_invoice_count,
+                    :top_days_by_invoice_count,
+                    :invoice_status,
+                    :merchants_with_pending_invoices
+
+  def_delegators :@merchants_revenue_analyst,
+                    :total_revenue_by_date,
+                    :top_revenue_earners,
+                    :merchants_ranked_by_revenue,
+                    :revenue_by_merchant,
+                    :best_item_for_merchant,
+                    :most_sold_item_for_merchant
 end
