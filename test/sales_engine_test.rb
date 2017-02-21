0bcaea8d6c8243552ae7029a4db7b35825f250a4
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-21 16:43:09 -0700

    sales engine test

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,17 +3,19 @@ require 'minitest/autorun'
 require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-
   def test_sales_engine_exists
-    engine = SalesEngine.new
-    assert engine
+    se = SalesEngine.new("data/merchants.csv")
+    assert_instance_of SalesEngine, se
   end
 
   def test_sales_engine_can_read_file
-    skip
-    se = SalesEngine.new
-
-    assert_equal ""
+    se = SalesEngine.new("data/merchants.csv")
+    merchants = se.merchant_data
+    assert_instance_of CSV, merchants
   end
 
+  def test_se_merchant_method
+    se = SalesEngine.new("data/merchants.csv")
+    assert_instance_of CSV, se.merchants
+  end
 end
