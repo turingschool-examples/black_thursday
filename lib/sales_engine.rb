a3be5fcd59810e9ec18885447f50ae85c3c2455f
Jack Pincus <jwpincus@gmail.com>
2017-02-22 12:42:21 -0700

    lunch break progress

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,14 +1,15 @@
 require 'csv'
 require_relative 'merchant_repository'
 class SalesEngine
+  attr_reader :item_csv
   def initialize
     @item_csv = ''
     @merchant_csv = ''
   end
 
   def from_csv(csv_hash)
-    @item_csv = CSV.open csv_hash[:items]
-    @merchant_csv = CSV.open csv_hash[:merchants]
+    @item_csv = CSV.read csv_hash[:items]
+    @merchant_csv = CSV.read csv_hash[:merchants]
   end
 
   def items
@@ -20,3 +21,9 @@ class SalesEngine
   end
   
 end
+
+sales =  SalesEngine.new
+
+sales.from_csv({
+  :items     => "./data/items.csv",
+  :merchants => "./data/merchants.csv"})
