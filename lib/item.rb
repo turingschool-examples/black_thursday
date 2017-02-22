aed5c08ba4cf49ca5426661163b0c2e4b012e9d1
lukyans <sukyans@gmail.com>
2017-02-22 10:32:37 -0700

    changes

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,10 @@
 class Item
-  end
+end
+
+
+
+# i = Item.new({:name=> "Pencil", :description => "You can use it to write things",
+#   :unit_price  => BigDecimal.new(10.99,4),
+#   :created_at  => Time.now,
+#   :updated_at  => Time.now,
+# })
