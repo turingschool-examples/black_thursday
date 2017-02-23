c166bb2972b725bf3d7109ad471a5a1abbaddaab
frustratedswede <andrew.mossefin@gmail.com>
2017-02-22 22:08:14 -0700

    Working with new files

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -2,21 +2,22 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'simplecov'
 require 'pry'
-require '../lib/merchant.rb'
+require 'csv'
+require './lib/merchant.rb'
 
 class MerchantTest < Minitest::Test
 
   def test_class_exists
-    assert_instance_of Merchant, Merchant.new
+    assert_instance_of Merchant, Merchant.new({id:"12334105",  name:"Shopin1901", created_at:"2010-12-10", updated_at:"2011-12-04"})
   end
 
   def test_id
-    m = Merchant.new({:id => 12334105, :name => "Shopin1901"})
+    m = Merchant.new({id:"12334105",  name:"Shopin1901", created_at:"2010-12-10", updated_at:"2011-12-04"})
     assert_equal 12334105, m.id 
   end
 
   def test_name
-    m = Merchant.new({:id => 12334105, :name => "Shopin1901"})
+    m = Merchant.new({id:"12334105",  name:"Shopin1901", created_at:"2010-12-10", updated_at:"2011-12-04"})
     assert_equal "Shopin1901", m.name
   end
 
