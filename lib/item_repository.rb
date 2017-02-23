39769350fa54c400acb714659e61e654a69dfdab
Sam Landfried <samlandfried@gmail.com>
2017-02-23 13:21:55 -0700

    Separate repository methods into module

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,51 +1,56 @@
 require 'csv'
 require_relative './item'
+require_relative './repository_methods'
 require 'pry'
 
 class ItemRepository
-  attr_accessor :items
+  attr_accessor :collection, :child
 
   def initialize(path)
-    @items = Hash.new
+    @child = Item
+    @collection = Hash.new
     populate_repository(path)
+    require "pry"; binding.pry
+    ""
   end
 
-  def populate_repository(path)
-    data = CSV.read(path, headers: true, header_converters: :symbol)
-      data.each do |item|
-        items[item[:id].to_sym] = Item.new(item)
-      end
-  end
+  include RepositoryMethods
+  # def populate_repository(path)
+  #   data = CSV.read(path, headers: true, header_converters: :symbol)
+  #     data.each do |item|
+  #       items[item[:id].to_sym] = Item.new(item)
+  #     end
+  # end
 
   def inspect
     "#<#{self.class} #{@items.size} rows>"
   end
 
-  def all
-    all_items = []
-    items.each do |id, item|
-      all_items << item
-    end
-    all_items
-    # returns an array of all known Item instances
-  end
+  # def all
+  #   all_items = []
+  #   items.each do |id, item|
+  #     all_items << item
+  #   end
+  #   all_items
+  #   # returns an array of all known Item instances
+  # end
 
-  def find_by_id(id)
-    items[id.to_s.to_sym]
-  # returns either nil or an instance of Item with a matching ID
-  end
+  # def find_by_id(id)
+  #   items[id.to_s.to_sym]
+  # # returns either nil or an instance of Item with a matching ID
+  # end
 
-  def find_by_name(name_to_search_for)
-    items.each do |id, item|
-      return item if item.name.downcase == name_to_search_for.downcase
-    end
-    nil
-    # returns either nil or an instance of Item having done a case insensitive search
-  end
+  # def find_by_name(name_to_search_for)
+  #   items.each do |id, item|
+  #     return item if item.name.downcase == name_to_search_for.downcase
+  #   end
+  #   nil
+  #   # returns either nil or an instance of Item having done a case insensitive search
+  # end
 
   def find_all_with_description(description_fragment)
     items_matching_description = []
-    items.each do |id, item|
+    collection.each do |id, item|
       items_matching_description << item if item.description.downcase.include?(description_fragment.downcase)
     end
     items_matching_description
@@ -54,7 +59,7 @@ class ItemRepository
 
   def find_all_by_price(price)
     items_matching_price = []
-    items.each do |id, item|
+    collection.each do |id, item|
       items_matching_price << item if item.unit_price == price
     end
     items_matching_price
@@ -66,7 +71,7 @@ class ItemRepository
     range_minimum = range.first
     range_maximum = range.last
     items_matching_supplied_range = []
-    items.each do |id, item|
+    collection.each do |id, item|
       items_matching_supplied_range << item if item.unit_price >= range_minimum && item.unit_price <= range_maximum
 
     end
@@ -74,14 +79,7 @@ class ItemRepository
     #returns either [] or instances of Item where the supplied price is in the supplied range (a single Ruby range instance is passed in)
   end
 
-  def find_all_by_merchant_id(merchant_id)
-    items_matching_merchant_id = []
-    items.each do |id, item|
-      items_matching_merchant_id << item if item.merchant_id == merchant_id
-    end
-    items_matching_merchant_id
-    #returns either [] or instances of Item where the supplied merchant ID matches that supplied
-  end
+
 
 
 end
