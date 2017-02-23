457edfa7a681970d556da6326435037a39d7eda5
Jack Pincus <jwpincus@gmail.com>
2017-02-23 16:14:11 -0700

    tremendous strides in making the dumb spec harness work

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,8 +2,9 @@ require_relative "item"
 class ItemRepository
   attr_reader :repository
   def initialize(item_csv, parent)
-    @item_csv = item_csv
+    @item_csv = CSV.open item_csv, headers: true, header_converters: :symbol
     @parent = parent
+    make_repository
   end
 
   def inspect
@@ -19,8 +20,28 @@ class ItemRepository
   end
 
   def all
-    @repository.map do |item|
-      item
+    @repository.map do |key, value|
+      value
+    end
+  end
+
+  def find_by_id(id)
+    if @repository[id.to_s]
+      @repository[id.to_s]
+    else
+      nil
+    end
+  end
+
+  def find_by_name(name_search)
+    all.find do |value|
+      value.name.downcase == name_search.downcase
+    end
+  end
+
+  def find_all_with_description(description)
+    all.find_all do |item|
+      item.description.downcase.include?(description.downcase)
     end
   end
 
