2f8d722b7b84e9d1a7ae22989b06d7bae42e0b1c
Beth Knight <bethknight1234@gmail.com>
2017-02-23 13:42:44 -0700

    finished merchant

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -5,7 +5,6 @@ require_relative '../lib/merchant'
 
 class MerchantTest < Minitest::Test
 
-
   def test_it_creates_an_instance_of_merchant
     m = Merchant.new({:id => 12334105, :name => "Shopin1901"})
     assert m
@@ -20,7 +19,7 @@ class MerchantTest < Minitest::Test
 
 
   def test_it_can_find_a_name
-    skip
+
     m = Merchant.new({:id => 12334105, :name => "Shopin1901"})
 
     assert_equal "Shopin1901", m.name
