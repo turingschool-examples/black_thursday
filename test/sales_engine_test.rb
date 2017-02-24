43491bb37c6d7a74e808b5e5ff8721f701d2cbe8
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-23 21:44:39 -0700

    add setup method to sales_engine_test

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,20 +2,23 @@ require './test/test_helper'
 require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
+
+  def setup
+    @se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",})
+  end
+
   def test_sales_engine_exists
-    se = SalesEngine.from_csv({:merchants => "./data/merchants.csv"})
-    assert_instance_of SalesEngine, se
+    assert_instance_of SalesEngine, @se
   end
 
   def test_se_merchant_method_reads_merchant_file
-    se = SalesEngine.from_csv({:merchants => "./data/merchants.csv"})
-    assert_instance_of MerchantRepository, se.merchants
+    assert_instance_of MerchantRepository, @se.merchants
   end
 
   def test_se_merchant_method_reads_items_file
-    skip
-    se = SalesEngine.from_csv({:items => "./data/items.csv"})
-    assert_instance_of ItemRepository, se.items
+    assert_instance_of ItemRepository, @se.items
   end
 
 end
