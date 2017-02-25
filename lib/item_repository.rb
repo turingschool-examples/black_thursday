00209aad83313e41da1f2ff5512d26cee3fdb181
Sam Landfried <samlandfried@gmail.com>
2017-02-24 18:25:56 -0700

    Pull methods into RepositoryMethods module

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -13,38 +13,11 @@ class ItemRepository
   end
 
   include RepositoryMethods
-  # def populate_repository(path)
-  #   data = CSV.read(path, headers: true, header_converters: :symbol)
-  #     data.each do |item|
-  #       items[item[:id].to_sym] = Item.new(item)
-  #     end
-  # end
+
 
   def inspect
     "#<#{self.class} #{@items.size} rows>"
   end
-
-  # def all
-  #   all_items = []
-  #   items.each do |id, item|
-  #     all_items << item
-  #   end
-  #   all_items
-  #   # returns an array of all known Item instances
-  # end
-
-  # def find_by_id(id)
-  #   items[id.to_s.to_sym]
-  # # returns either nil or an instance of Item with a matching ID
-  # end
-
-  # def find_by_name(name_to_search_for)
-  #   items.each do |id, item|
-  #     return item if item.name.downcase == name_to_search_for.downcase
-  #   end
-  #   nil
-  #   # returns either nil or an instance of Item having done a case insensitive search
-  # end
 
   def find_all_with_description(description_fragment)
     items_matching_description = []
@@ -52,7 +25,6 @@ class ItemRepository
       items_matching_description << item if item.description.downcase.include?(description_fragment.downcase)
     end
     items_matching_description
-    #returns either [] or instances of Item where the supplied string appears in the item description (case insensitive)
   end
 
   def find_all_by_price(price)
@@ -61,7 +33,6 @@ class ItemRepository
       items_matching_price << item if item.unit_price == price
     end
     items_matching_price
-    # returns either [] or instances of Item where the supplied price exactly matches
 
   end
 
@@ -71,13 +42,8 @@ class ItemRepository
     items_matching_supplied_range = []
     collection.each do |id, item|
       items_matching_supplied_range << item if item.unit_price >= range_minimum && item.unit_price <= range_maximum
-
     end
     items_matching_supplied_range
-    #returns either [] or instances of Item where the supplied price is in the supplied range (a single Ruby range instance is passed in)
   end
-
-
-
 
 end
--- lib/merchant.rb
+++ lib/merchant.rb
@@ -2,8 +2,8 @@ class Merchant
 
   attr_reader :name, :id
 
-  def initialize(name, id)
-    @name = name
-    @id = id.to_i
+  def initialize(params)
+    @name = params[:name]
+    @id = params[:id].to_i
   end
 end
