e2fb029c5b21a85f151f67ba4f3eeb335e3a1d20
jk1dd <portajonk@gmail.com>
2017-02-25 10:27:42 -0700

    customer repository and tests

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,14 +4,17 @@ require_relative '../lib/merchant_repository'
 require_relative '../lib/item_repository'
 require_relative '../lib/merchant'
 require_relative '../lib/item'
+require_relative '../lib/customer_repository'
+require_relative '../lib/customer'
 
 
 class SalesEngine
-  attr_reader :merchants, :items
+  attr_reader :merchants, :items, :customers
 
   def initialize(files_to_be_loaded)
     @merchants ||= MerchantRepository.new(files_to_be_loaded[:merchants])
     @items ||= ItemRepository.new(files_to_be_loaded[:items])
+    @customers ||= CustomerRepository.new(files_to_be_loaded[:customers])
   end
 
   def self.from_csv(files_to_be_loaded)
