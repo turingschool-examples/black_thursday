33267247c79f915fd23d44480800f4abab1d68d3
ryt11 <rileyt_2000@hotmail.com>
2017-02-24 22:07:12 -0600

    add items/merchants refactor in SalesEngine and average_items/std_dev in SalesAnalyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,4 +1,31 @@
+require 'pry'
+
 class SalesAnalyst
+  attr_reader :sales_engine_instance
 
+  def initialize(sales_engine_instance)
+    @sales_engine_instance = sales_engine_instance
+  end
 
+  def average_items_per_merchant
+	(sales_engine_instance.items.all.count.to_f / sales_engine_instance.merchants.all.count.to_f).round(2)
+  end
+
+  def difference_between_mean_and_item_count_squared_summed
+    mean = average_items_per_merchant
+    get_merchant_items_set.map do |item_count|
+      (item_count - mean) ** 2
+    end.reduce(:+)
+  end
+
+  def get_merchant_items_set
+    sales_engine_instance.merchants.all.map do |merchant|
+      merchant.items.count
+    end
+  end
+  
+  def average_items_per_merchant_standard_deviation
+    set_length = (get_merchant_items_set.length - 1)
+    Math.sqrt(difference_between_mean_and_item_count_squared_summed/set_length).round(2)
+  end
 end
