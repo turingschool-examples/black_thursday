4b2401662d437ad2bfe742f33f6be98ec7083ab6
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-25 22:41:55 -0700

    add average_items_per_merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -13,4 +13,42 @@ class SalesAnalyst
     end
   end
 
+  def all_merchants
+    sales_engine.merchants.all.map do |merchant|
+      merchant.id
+    end
+  end
+
+  def build_hash
+    merchant_prices = Hash.new{ |hash, key| hash[key] = [] }
+    sales_engine.merchants.all.map do |merchant|
+      merchant_items = sales_engine.items.find_all_by_merchant_id(merchant.id)
+      merchant_prices[merchant.id] = merchant_items.map {|item| item.unit_price_to_dollars}
+    end
+    merchant_prices
+  end
+
+  def get_prices_of_merchant(merchant_id)
+    items = sales_engine.items.find_all_by_merchant_id(merchant_id)
+    prices = items.map do |item|
+      item.unit_price_to_dollars
+    end
+    {merchant_id => prices}.to_a
+  end
+
+  def average_items_per_merchant
+    data = build_hash
+    merchant_count = data.keys.count
+    item_count = data.values.reduce(0) do |sum, x|
+      if x.count == 0
+        sum
+      else
+        sum + x.count
+      end
+    end
+    average = item_count.to_f/merchant_count.to_f
+    average.round(2)
+
+  end
+
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -18,4 +18,26 @@ class SalesAnalystTest < Minitest::Test
     assert_equal 116268.99, @sa.all_prices
   end
 
+  def test_can_return_array_of_all_merchant_ids
+    assert_equal 14, @sa.all_merchants.count
+    assert_equal 12334105, @sa.all_merchants.first
+  end
+
+  def test_can_build_hash_of_merchants_and_prices
+    merchant_hash = @sa.build_hash
+    assert_equal 14, merchant_hash.count
+    assert_equal [[12334105, [29.99]], [12334112, [15.0]], [12334113, [150.0]]], merchant_hash.take(3)
+
+  end
+
+  def test_can_return_all_prices_of_single_merchant
+    results = @sa.get_prices_of_merchant(12334145)
+    assert_equal [[12334145, [30.0, 25.0, 25.0, 20.0]]], results
+  end
+
+  def test_average_items_per_merchant
+    results = @sa.average_items_per_merchant
+    assert_equal 1.79, results
+  end
+
 end
