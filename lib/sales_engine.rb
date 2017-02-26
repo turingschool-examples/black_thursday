f318ffbf6341aad774ab85b75aa359a4c51f1d5a
ryt11 <rileyt_2000@hotmail.com>
2017-02-26 15:29:11 -0700

    add start of item repo and changes to sales_engine for new classes

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,15 +1,22 @@
 require_relative "merchant_repository"
 require_relative "item_repository"
 require_relative "invoice_repository"
+require_relative "invoice_item_repository"
+require_relative "transaction_repository"
+require_relative "customer_repository"
+
+
 
 class SalesEngine
-    attr_accessor :paths, :items, :merchants,:invoices
+    attr_accessor :paths, :items, :merchants, :invoices, :invoice_items, :transactions, :customers
   def initialize (hash)
     @paths = hash
-    @items = ItemRepository.new(@paths[:items], self)
-    @merchants = MerchantRepository.new(@paths[:merchants], self)
-    @invoices = InvoiceRepository.new(@paths[:invoices], self)
-    # @invoice_items = InvoiceItemsRepository.new(@paths[:invoice])
+    @items = ItemRepository.new(@paths[:items], self) unless @paths[:items] == nil
+    @merchants = MerchantRepository.new(@paths[:merchants], self) unless @paths[:merchants] == nil
+    @invoices = InvoiceRepository.new(@paths[:invoices], self) unless @paths[:invoices] == nil
+    @invoice_items = InvoiceItemRepository.new(@paths[:invoice_items], self) unless @paths[:invoice_items] == nil
+    @transactions = TransactionRepository.new(@paths[:transactions], self) unless @paths[:transactions] == nil
+    @customers = CustomerRepository.new(@paths[:customers], self) unless @paths[:customers] == nil
   end
 
   def self.from_csv(hash)
