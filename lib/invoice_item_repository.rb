cac2dff81a37ae94e8837c693947354681b901b3
Beth Knight <bethknight1234@gmail.com>
2017-02-26 10:53:44 -0700

    finds all by invoice id

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -15,15 +15,16 @@ class InvoiceItemRepository < Repository
   end
 
 
-  def find_by_id
+  def find_by_id(id)
     data.select { |invoice_item| invoice_item.id == id }.first
   end
 
-  def find_all_by_item_id
-
+  def find_all_by_item_id(item_id)
+    data.select { |invoice_item| invoice_item.item_id == item_id }
   end
 
-  def find_all_by_invoice_id
+  def find_all_by_invoice_id(invoice_id)
+    data.select { |invoice_item| invoice_item.invoice_id == invoice_id }
   end
 
 end
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -5,6 +5,7 @@ require_relative '../lib/repository'
 require_relative '../lib/sales_engine'
 require_relative '../lib/invoice_item_repository'
 require_relative '../lib/item_repository'
+require_relative '../lib/invoice_item'
 require_relative '../lib/merchant'
 require_relative '../lib/item'
 
@@ -22,8 +23,27 @@ class InvoiceItemRepositoryTest < Minitest::Test
     }
     @path = 'test/fixtures/invoice_items_small.csv'
     @se = SalesEngine.from_csv(file_hash)
-    @repo = Repository.new(se, path, Invoice)
-    @invoice__item_repository = InvoiceItemRepository.new(se, path)
+    @repo = Repository.new(se, path, InvoiceItem)
+    @invoice_item_repository = InvoiceItemRepository.new(se, path)
+  end
+
+  def test_finds_all_invoice_items
+    assert_equal Array, invoice_item_repository.all.class
+    refute_empty invoice_item_repository.all
+  end
+
+  def test_find_by_id
+    assert_equal InvoiceItem, invoice_item_repository.find_by_id(1).class
+  end
+
+  def test_it_finds_all_by_item_id
+    assert_equal Array, invoice_item_repository.find_all_by_item_id(263519844).class
+    refute_empty invoice_item_repository.find_all_by_item_id(263519844)
+  end
+
+  def test_it_finds_all_by_invoice_id
+    assert_equal Array, invoice_item_repository.find_all_by_invoice_id(1).class
+    refute_empty invoice_item_repository.find_all_by_invoice_id(1)
   end
 
 end
