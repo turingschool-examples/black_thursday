9f3ef5a11a7a3faea3d0490c6e4237c168dd376e
Diana Whalen <whalendianaj@gmail.com>
2017-02-27 14:38:21 -0700

    transactions and repo and tests

---
+++ lib/transaction.rb
@@ -0,0 +1,18 @@
+require 'time'
+
+class Transaction
+    attr_reader :id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at
+
+  def initialize(row, parent=nil)
+    @id = row[:id].to_i
+    @invoice_id = row[:invoice_id].to_i
+    @credit_card_number = row[:credit_card_number].to_t
+    @credit_card_expiration_date = row[:credit_card_expiration_date].to_t
+    @result = row[:result]
+    @created_at = Time.parse(row[:created_at])
+    @updated_at = Time.parse(row[:updated_at])
+    @parent = parent
+  end
+end
+
+# CHECK FORMAT OF RETURNED CREDIT CARD NUMBER WITHOUT TAB OR SPACE
---
+++ lib/transaction_repository.rb
@@ -0,0 +1,42 @@
+class TransactionRepository
+  attr_reader :path, :engine
+
+  def initialize(path, engine)
+    @path = path
+    @engine = engine
+  end
+
+  def csv
+    @csv ||= CSV.open(path, headers:true, header_converters: :symbol)
+  end
+
+  def all
+    @all ||= csv.map do |row|
+      Transaction.new(row, self)
+    end
+  end
+
+  def find_by_id(id)
+    all.find do |transaction|
+      transaction.id.to_i == id.to_i
+    end
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    all.find_all do |transaction|
+      transaction.invoice_id == invoice_id.to_i
+    end
+  end
+
+  def find_all_by_credit_card_number(credit_card_number)
+    all.find_all do |transaction|
+      transaction.credit_card_number == credit_card_number.to_t
+    end
+  end
+
+  def find_all_by_result(result)
+    all.find_all do |transaction|
+      transaction.result == result
+    end
+  end
+end
