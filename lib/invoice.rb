35726db68eaadc47c39321b13693a876c7d8b01f
glassjoseph <glassjoseph@gmail.com>
2017-02-28 14:38:08 -0700

    made business intelligence methods

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -19,9 +19,12 @@ class Invoice
     parent.parent.merchants.find_by_id(merchant_id)
   end
 
+  def invoice_items
+    parent.parent.invoice_items.find_all_by_invoice_id(id)
+  end
+
   def items
-    all_invoices = parent.parent.invoice_items.find_all_by_invoice_id(id)
-    item_ids = all_invoices.map { |invoice_item| invoice_item.item_id }
+    item_ids = invoice_items.map { |invoice_item| invoice_item.item_id }
     item_ids.map {|item_id| self.parent.parent.items.find_by_id(item_id) }
   end
 
@@ -33,5 +36,17 @@ class Invoice
     parent.parent.customers.find_by_id(customer_id)
   end
 
+  def is_paid_in_full?
+    invoice_transactions = parent.parent.transactions.find_all_by_invoice_id(id)
+    return false if transactions.empty? 
+    invoice_transactions.all? {|transaction| transaction.result == "success"}
+  end
+
+  def total
+    #returns total $ amount of instance
+      # binding.pry
+    return 0.0 unless is_paid_in_full?
+    invoice_items.inject(0){ |sum, invoice_item| sum + invoice_item.unit_price * invoice_item.quantity } 
+  end
 
 end
