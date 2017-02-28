1bf3e29a5f08561d1fb7eace20b78c62a09b71ad
frustratedswede <andrew.mossefin@gmail.com>
2017-02-27 17:50:33 -0700

    Iteration 3 added Invoice methods

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,4 +1,5 @@
 require 'time'
+require_relative 'invoice item'
 
 class Invoice
 
@@ -17,12 +18,24 @@ class Invoice
     @merchant_id = row[:merchant_id].to_i
     @status      = row[:status].to_sym
     @created_at  = Time.parse(row[:created_at])
-    @updated_at   = Time.parse(row[:updated_at])
+    @updated_at  = Time.parse(row[:updated_at])
     @repository  = repository
   end
 
   def merchant
     repository.sales_engine.merchants.find_by_id(self.merchant_id)
+  end
+
+  def items
+    repository.sales_engine.invoice_items.find_all_by_invoice_id(self.id)
+  end
+
+  def transactions
+    repository.sales_engine.transactions.find_all_by_invoice_id(self.id)
+  end
+
+  def customer
+    repository.sales_engine.customers.find_by_id(self.customer_id)
   end
 end
     
