13205e86b2168dda4652e5f4ff44329e3fd25593
Katie Shermer <katieshermer@gmail.com>
2017-05-30 14:14:20 -0600

    Created item_test file

---
+++ test/item_test.rb
@@ -0,0 +1,13 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class ItemTest < Minitest::Test
+
+  def test_item_exists
+  end
+
+end
