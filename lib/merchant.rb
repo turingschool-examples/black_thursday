d7551d6b73560761da90cbb200755ea477434cf8
JF-Lalonde <lalondej88@gmail.com>
2017-05-30 17:34:05 -0600

    created Merchant class and test

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,5 +1,10 @@
+require 'csv'
+
 class Merchant
   def initialize(merchant_data)
-
+    complete_merchant_data = CSV.open "./data/merchants.csv",
+    headers: true, header_converters: :symbol
   end
+
+
 end
---
+++ test/merchant_test.rb
@@ -0,0 +1,12 @@
+require_relative 'test_helper'
+require './lib/merchant'
+
+class MerchantTest < Minitest::Test
+
+  def test_it_opens_a_file
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_instance_of Merchant, merchant
+  end
+
+end
