b5e62f559df5b41e61fd59ad224eb132de479ef5
MikelSage <michael.sagapolutele@yahoo.com>
2017-05-30 17:35:14 -0600

    add more robust test

--- test/item_test.rb
+++ test/item_test.rb
@@ -45,4 +45,22 @@ class ItemTest < Minitest::Test
   def test_it_can_convert_to_dollars
     assert_equal 10.99, item.unit_price_to_dollars
   end
+
+  def test_it_can_have_other_values
+    item2 = Item.new({
+      name: "Eraser",
+      description: "You can use it to erase things.",
+      unit_price: 300,
+      created_at: Time.now,
+      updated_at: Time.now,
+      merchant_id: 12341234
+      })
+
+    assert_equal "Eraser", item2.name
+    assert_equal "You can use it to erase things.", item2.description
+    assert_equal 300, item2.unit_price
+    assert_instance_of Time, item2.created_at
+    assert_instance_of Time, item2.updated_at
+    assert_equal 3.00, item2.unit_price_to_dollars
+  end
 end
