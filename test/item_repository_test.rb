e872bab5415ee14b6ef4c5893db2591dd0b60a55
Katie Shermer <katieshermer@gmail.com>
2017-05-30 14:41:31 -0600

    Created item_repository_test file

---
+++ test/item_repository_test.rb
@@ -0,0 +1,14 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item.rb'
+require 'pry'
+
+class ItemRepositoryTest < Minitest::Test
+
+  def test_item_repository_exists
+  end
+
+end
