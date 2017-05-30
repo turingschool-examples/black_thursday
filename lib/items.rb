e81de51c1aef670da1e9df4cd16dfba1cfea66e7
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-05-30 13:49:30 -0600

    creates Items class and initialize method

---
+++ lib/items.rb
@@ -0,0 +1,15 @@
+require 'csv'
+
+class Items
+
+  def initialize(id, name, description, unit_price, merchant_id, created_at, updated_at)
+    @id = id
+    @name = name
+    @description = description
+    @unit_price = unit_price
+    @merchant_id = merchant_id
+    @created_at = created_at
+    @updated_at = updated_at
+  end
+
+end
