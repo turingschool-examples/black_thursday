09b28cbcf4f9af7a990a0d9b32c65a6543482fb5
BFunk <bret.e.funk@gmail.com>
2017-05-30 21:23:26 -0600

    add tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -2,8 +2,18 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/item_repository'
+#require './data/items.csv'
 
 class ItemRepoTest < Minitest::Test
 
+  def setup
+    skip
+    test = ItemRepository.new('items.csv')
+  end
+
+  def test_class_exits
+    skip
+    assert_instance_of ItemRepository, test
+  end
 
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -5,5 +5,27 @@ require './lib/item'
 
 class ItemTest < Minitest::Test
 
+  def test_item_class_exists
+    result = Item.new(["toothbrush", "brush teeth", 3])
+    assert_instance_of Item, result
+  end
+
+  def test_item_name
+    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    result = new_instance.name
+    assert_equal "toothbrush", result
+  end
+
+  def test_item_description
+    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    result = new_instance.description
+    assert_equal "brush teeth", result
+  end
+
+  def test_item_unit_price
+    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    result = new_instance.unit_price
+    assert_equal 3, result
+  end
 
 end
