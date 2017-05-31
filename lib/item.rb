fdf11813c6c47a43938a4b5f10831e39c7ac6ab0
jtruong2 <jtruong2@comcast.net>
2017-05-30 20:50:41 -0600

    add id,name,description,unit_price,merchant_id,created_at,updated_at method to Item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,43 @@
 require 'pry'
+require 'bigdecimal'
 class Item
-  
+  attr_reader :information
+
+  def initialize(information)
+    @information = information
+
+  end
+
+  def id
+    information["id"].to_i
+  end
+
+  def name
+    information["name"]
+  end
+
+  def description
+    information["description"]
+  end
+
+  def unit_price
+    info = information["unit_price"]
+    price = BigDecimal.new(info)
+  end
+
+  def merchant_id
+    information["merchant_id"].to_i
+  end
+
+  def created_at
+    start = information["created_at"]
+    list = start.split(/[- :]/)
+    time = Time.utc(list[0].to_i, list[1].to_i, list[2].to_i, list[3].to_i, list[4].to_i, list[5].to_i)
+  end
+
+  def updated_at
+    update = information["updated_at"]
+    list = update.split(/[- :]/)
+    time = Time.utc(list[0].to_i, list[1].to_i, list[2].to_i, list[3].to_i, list[4].to_i, list[5].to_i)
+  end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -1,11 +1,64 @@
 require 'minitest/autorun'
 require 'minitest/emoji'
 require './lib/item'
+require 'csv'
+require 'bigdecimal'
 class ItemTest < Minitest::Test
-  def test_it_exists
-    it = Item.new
+  def file_setup
+    header = CSV.read("/Users/jimmytruong/turing/1module/projects/black_thursday/test/itemtestfile.csv")[0]
+    content = CSV.read("/Users/jimmytruong/turing/1module/projects/black_thursday/test/itemtestfile.csv")[1 .. -1].flatten
+    line = header.zip(content).flatten.compact
+    item_info = Hash[*line]
+  end
 
-    assert_instance_of Item, it
+  def test_it_exists
+    i = Item.new(file_setup)
+
+    assert_instance_of Item, i
+  end
+
+  def test_has_ID
+    i = Item.new(file_setup)
+
+    assert_equal 263395721, i.id
+  end
+
+  def test_has_name
+    i = Item.new(file_setup)
+
+    assert_equal "Disney scrabble frames", i.name
+  end
+
+  def test_has_description
+    i = Item.new(file_setup)
+
+    assert_equal "Disney glitter frames", i.description
+  end
+
+  def test_has_unit_price
+    i = Item.new(file_setup)
+    price = BigDecimal.new("1350")
+
+    assert_equal price, i.unit_price
+  end
+
+  def test_has_merchant_id
+    i = Item.new(file_setup)
+
+    assert_equal 12334185, i.merchant_id
+  end
+
+  def test_has_time_of_creation
+    i = Item.new(file_setup)
+    time = Time.utc(2016,01,11,11,51,37)
+    assert_equal time, i.created_at
+  end
+
+  def test_has_time_that_it_was_last_updated
+    i = Item.new(file_setup)
+    time = Time.utc(2008,04,02,13,48,57)
+
+    assert_equal time, i.updated_at
   end
 
 
