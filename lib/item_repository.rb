57784676dad85afd038b66ef72e7729257e74a6b
Samantha Gongora <samantha.gongora@gmail.com>
2017-05-30 21:52:39 -0600

    creates in item_repository.rb all method for all instances and test and method for find_by_id

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,3 +1,5 @@
+require_relative './item'
+
 class ItemRepository
 
   attr_reader :all
@@ -6,5 +8,13 @@ class ItemRepository
     @all = []
   end
 
+  def find_by_id(id)
+    @all.find do |item|
+      if item.id == id
+        return item
+      end
+      nil
+    end
+  end
 
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -22,25 +22,47 @@ class ItemRepositoryTest < MiniTest::Test
     assert_equal [], ir.all
   end
 
-  # def test_if_all_works
-  #   ir = ItemRepository.new
-  #   i1 = Item.new({:name        => "Pencil",
-  #                 :description => "You can use it to write things",
-  #                 :unit_price  => BigDecimal.new(10.99,4),
-  #                 :created_at  => Time.now,
-  #                 :updated_at  => Time.now
-  #                }, 1)
-  #
-  #   i2 = Item.new({:name        => "Pen",
-  #                 :description => "You can use it to write things",
-  #                 :unit_price  => BigDecimal.new(11.99,4),
-  #                 :created_at  => Time.now,
-  #                 :updated_at  => Time.now
-  #                }, 2)
-  #
-  #
-  # end
+  def test_if_all_returns_array_of_instances
+    ir = ItemRepository.new
+    i1 = Item.new({:name        => "Pencil",
+                  :description => "You can use it to write things",
+                  :unit_price  => BigDecimal.new(10.99,4),
+                  :created_at  => Time.now,
+                  :updated_at  => Time.now
+                 }, 1)
 
+    i2 = Item.new({:name        => "Pen",
+                  :description => "You can use it to write things",
+                  :unit_price  => BigDecimal.new(11.99,4),
+                  :created_at  => Time.now,
+                  :updated_at  => Time.now
+                 }, 2)
+    ir.all << i1
+    ir.all << i2
 
+    assert_equal 2, ir.all.length
+  end
+
+  def test_if_find_by_id_returns_correct_value_for_method
+    ir = ItemRepository.new
+    i1 = Item.new({:name        => "Pencil",
+                  :description => "You can use it to write things",
+                  :unit_price  => BigDecimal.new(10.99,4),
+                  :created_at  => Time.now,
+                  :updated_at  => Time.now
+                 }, 1)
+
+    i2 = Item.new({:name        => "Pen",
+                  :description => "You can use it to write things",
+                  :unit_price  => BigDecimal.new(11.99,4),
+                  :created_at  => Time.now,
+                  :updated_at  => Time.now
+                 }, 2)
+    ir.all << i1
+    ir.all << i2
+
+    assert_equal i2, ir.find_by_id(2)
+    assert_nil ir.find_by_id(3)
+  end
 
 end
