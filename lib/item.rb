327ade48b666e8c6b2cd5f2f5ae89a7dcd6cbe92
BFunk <bret.e.funk@gmail.com>
2017-05-30 22:02:15 -0600

    modifed tests and item.rb to include id

--- lib/item.rb
+++ lib/item.rb
@@ -1,12 +1,13 @@
 class Item
-  attr_reader :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
+  attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
   def initialize(params)
-  @name = params[0]
-  @description = params[1]
-  @unit_price = params[2]
-  @created_at = Time.now
-  @updated_at = Time.now
-  @merchant_id = 0
+    @id = params[0]
+    @name = params[1]
+    @description = params[2]
+    @unit_price = params[3]
+    @created_at = Time.now
+    @updated_at = Time.now
+    @merchant_id = 0
   end
 
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -6,43 +6,49 @@ require './lib/item'
 class ItemTest < Minitest::Test
 
   def test_item_class_exists
-    result = Item.new(["toothbrush", "brush teeth", 3])
+    result = Item.new([0, "toothbrush", "brush teeth", 3])
     assert_instance_of Item, result
   end
 
+  def test_item_id
+    new_instance = Item.new([0, "toothbrush", "brush teeth", 3])
+    result = new_instance.name
+    assert_equal 0, result
+  end
+
   def test_item_name
-    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    new_instance = Item.new([0, "toothbrush", "brush teeth", 3])
     result = new_instance.name
     assert_equal "toothbrush", result
   end
 
   def test_item_description
-    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    new_instance = Item.new([0, "toothbrush", "brush teeth", 3])
     result = new_instance.description
     assert_equal "brush teeth", result
   end
 
   def test_item_unit_price
-    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    new_instance = Item.new([0, "toothbrush", "brush teeth", 3])
     result = new_instance.unit_price
     assert_equal 3, result
   end
 
   def test_created_at
-    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    new_instance = Item.new([0, "toothbrush", "brush teeth", 3])
     result = new_instance.created_at
     refute_equal Time.now, result
   end
 
   def test_updated_at
-    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    new_instance = Item.new([0, "toothbrush", "brush teeth", 3])
     result = new_instance.updated_at
     refute_equal Time.now, result #-0600
   end
 
   def test_merchant_id
     skip
-    new_instance = Item.new(["toothbrush", "brush teeth", 3])
+    new_instance = Item.new([0, "toothbrush", "brush teeth", 3])
     result = new_instance.merchant_id
     assert_equal 0, result
   end
