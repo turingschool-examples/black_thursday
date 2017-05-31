f57b6a5eef9a49f1d7c4e37cd592b6c56ac3de27
jtruong2 <jtruong2@comcast.net>
2017-05-31 08:54:49 -0600

    add name and id test for itemrepository_test.rb

--- test/itemrepository_test.rb
+++ test/itemrepository_test.rb
@@ -6,6 +6,18 @@ class ItemRepositoryTest < Minitest::Test
   def sample_path
     "./test/itemreposample.csv"
   end
+
+  def instance
+    {"id"=>"263395721",
+   "name"=>"Disney scrabble frames",
+   "description"=>
+    "Disney glitter frames\n\nAny colour glitter available and can do any characters you require\n\nDifferent colour scrabble tiles\n\nBlue\nBlack\nPink\nWooden",
+   "unit_price"=>"1350",
+   "merchant_id"=>"12334185",
+   "created_at"=>"2016-01-11 11:51:37 UTC",
+   "updated_at"=>"2008-04-02 13:48:57 UTC"}
+  end
+
   def test_it_exists
     ir = ItemRepository.new(sample_path)
 
@@ -24,10 +36,33 @@ class ItemRepositoryTest < Minitest::Test
     assert_nil ir.content
   end
 
+  def test_find_all_instance
+  end
+
   def test_return_instance_or_nil_with_id
     ir = ItemRepository.new(sample_path)
     ir.organize
-    instance = 
+
+    assert_nil ir.find_by_id(0)
+    assert_equal instance, ir.find_by_id(263395721).information
+  end
+
+  def test_return_instance_or_nil_with_name
+    ir = ItemRepository.new(sample_path)
+    ir.organize
+
+    assert_nil ir.find_by_name("hello")
+    assert_equal instance, ir.find_by_name("Disney scrabble frames").information
+  end
+
+  def test_return_empty_array_or_all_with_description
+    ir = ItemRepository.new(sample_path)
+    ir.organize
+
+    assert_equal [], ir.find_all_with_description("hello")
+    result = ir.find_all_with_description("Disney")
+    assert_equal [instance], result
+  end
 
 
 end
