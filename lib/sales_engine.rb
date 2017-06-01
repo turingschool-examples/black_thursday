52427ef68dc6efb10e9c72d4324980845d1938f4
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-01 17:27:54 -0600

    refactor and retest SalesEngine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,32 +4,15 @@ require_relative 'item_repository'
 
 class SalesEngine
   attr_reader :files, :merchants, :items
+
   def initialize(files)
     @files = files
     @merchants = MerchantRepository.new(files[:merchants])
-    @items = ItemRepository.new(files[:items])
-    
+    @items = ItemRepository.new(files[:items])    
   end
 
   def self.from_csv(files)
     new(files)
   end
 
-  # def merchants
-  #   i = 0
-  #   CSV.foreach('./data/merchants_test.csv', row_sep: :auto) do |line|
-  #     i += 1
-  #     next if i == 1
-  #     @merchants.populate_merchants(line)
-  #   end
-  # end
-
-  # def items
-  #   i = 0
-  #   CSV.foreach('./data/items_test.csv', row_sep: :auto) do |line|
-  #     i += 1
-  #     next if i == 1
-  #     ir.populate_items(line)
-  #   end
-  # end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -4,42 +4,29 @@ require_relative '../lib/sales_engine'
 
 class SalesEngineTest < MiniTest::Test
 
-  def test_from_csv_creates_an_instance_of_sales_engine
-    se = SalesEngine.from_csv({:items => './data/items_test.csv',
-                               :merchants => './data/merchants_test.csv'})
-
-    assert_instance_of SalesEngine, se
+  def setup
+    @files = {:items => './test/data/items_test.csv',
+              :merchants => './test/data/merchants_test.csv'}
   end
 
-  # def test_from_csv_opens_csv_files_without_headers
-  #   se = SalesEngine.from_csv({:items => './data/items_test.csv',
-  #                              :merchants => './data/merchants_test.csv'})
-  #   merchant_library
-  #   items_library
-  #
-  #
-  # end
-
   def test_from_csv_creates_an_instance_of_sales_engine
-    se = SalesEngine.from_csv({:items => './data/items_test.csv',
-                               :merchants => './data/merchants_test.csv'})
+    se = SalesEngine.from_csv(@files)
 
     assert_instance_of SalesEngine, se
   end
 
   def test_merchant_repo_creates
-    se = SalesEngine.from_csv({:items => './data/items_test.csv',
-                               :merchants => './data/merchants_test.csv'})
+    se = SalesEngine.from_csv(@files)
     mr = se.merchants
 
     assert_instance_of MerchantRepository, mr
   end
 
   def test_items_repo_creates
-    se = SalesEngine.from_csv({:items => './data/items_test.csv',
-                               :merchants => './data/merchants_test.csv'})
+    se = SalesEngine.from_csv(@files)
     ir = se.items
 
     assert_instance_of ItemRepository, ir
   end
+
 end
