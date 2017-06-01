4e828e70eecf6dad6cf097b815bf61537d40d0fc
MikelSage <michael.sagapolutele@yahoo.com>
2017-06-01 14:03:40 -0600

    add tests for all merchant instances and all item instances

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,6 +2,10 @@ require_relative "item_repository"
 require_relative "merchant_repository"
 
 class SalesEngine
+
+  attr_reader :items,
+              :merchants
+
   def initialize(item_rows, merchant_rows)
     @items     = ItemRepository.new(item_rows)
     @merchants = MerchantRepository.new(merchant_rows)
@@ -16,16 +20,4 @@ class SalesEngine
 
     SalesEngine.new(item_data, merch_data)
   end
-  #
-  # def self.from_csv(attrs)
-  #   item_path = attrs[:items]
-  #   merchants_path = attrs[:merchants]
-  #
-  #   item_data = CSV.open item_path, headers: true, header_converters: :symbol
-  #   merchants_data = CSV.open merchants_path, headers: true, header_converters: :symbol
-  #
-  #   se = SalesEngine.new(item_data, merchants_data)
-  # end
-
-
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -21,4 +21,46 @@ class SalesEngineTest < Minitest::Test
 
     assert_instance_of SalesEngine, se
   end
+
+  def test_sales_engine_items_returns_item_repo_instance
+    small_csv_paths = {
+                        :items     => "./data/small_item_set.csv",
+                        :merchants => "./data/merchant_sample.csv",
+                      }
+    se = SalesEngine.from_csv(small_csv_paths)
+
+    assert_instance_of ItemRepository, se.items
+  end
+
+  def test_sales_engine_merchants_returns_merchant_repo_instance
+    small_csv_paths = {
+                        :items     => "./data/small_item_set.csv",
+                        :merchants => "./data/merchant_sample.csv",
+                      }
+    se = SalesEngine.from_csv(small_csv_paths)
+
+    assert_instance_of MerchantRepository, se.merchants
+  end
+
+  def test_sales_engine_items_returns_all_item_instances
+    small_csv_paths = {
+                        :items     => "./data/small_item_set.csv",
+                        :merchants => "./data/merchant_sample.csv",
+                      }
+    se = SalesEngine.from_csv(small_csv_paths)
+
+    assert_equal 6, se.items.all.length
+    assert_instance_of Item, se.items.all.sample
+  end
+
+  def test_sales_engine_merchants_returns_all_merchant_instances
+    small_csv_paths = {
+                        :items     => "./data/small_item_set.csv",
+                        :merchants => "./data/merchant_sample.csv",
+                      }
+    se = SalesEngine.from_csv(small_csv_paths)
+
+    assert_equal 5, se.merchants.all.length
+    assert_instance_of Merchant, se.merchants.all.sample
+  end
 end
