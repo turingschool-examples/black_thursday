c601004e344965fd11d9427f25b253ff05eb1368
JF-Lalonde <lalondej88@gmail.com>
2017-05-31 18:23:46 -0600

    building item repository and tests

---
+++ lib/item_repository.rb
@@ -0,0 +1,32 @@
+require 'csv'
+require './lib/item'
+
+class ItemRepository
+
+  attr_reader :all_item_data
+
+  def initialize
+    @all_item_data = CSV.open("./data/items.csv",
+    headers: true, header_converters: :symbol).map do |row|
+      Item.new(row)
+    end
+  end
+  #
+  # def all
+  #   @all_item_data
+  # end
+  #
+  # def find_by_id(id)
+  #   @all_item_data.find{|item| item.id == id}
+  # end
+  #
+  # def find_by_name(name)
+  #   @all_item_data.find{|item| item.name == name}
+  # end
+  #
+  # def find_all_with_description(description)
+  #   @all_item_data.find_all{|item| /#{description}/i =~ item.description}
+  # end
+  # end
+
+end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,5 +1,47 @@
 require_relative 'test_helper'
 
-require './lib/sales_engine'
+require './lib/item_repository'
 
-class SalesEngineTest < Minitest::Test
+class ItemRepositoryTest < Minitest::Test
+  def test_it_opens_csv_into_array
+    item     = ItemRepository.new
+    actual   = item.all_item_data.class
+    expected = Array
+
+    assert_equal expected, actual
+  end
+  #
+  # def test_it_returns_item_instances
+  #   skip #No visible difference...wtf?
+  #   item = itemRepository.new
+  #   actual   = item.all[0]
+  #   expected = "#<item:0xXXXXXX @id=\"12334105\", @name=\"Shopin1901\">"
+  #
+  #   assert_equal expected, actual
+  # end
+  #
+  # def test_it_can_return_ids
+  #   item = itemRepository.new
+  #   actual   = item.find_by_id("12334105")
+  #   expected = item.all[0]
+  #
+  #   assert_equal expected, actual
+  # end
+  #
+  # def test_it_can_return_names
+  #   item = itemRepository.new
+  #   actual   = item.find_by_name("Shopin1901")
+  #   expected = item.all[0]
+  #
+  #   assert_equal expected, actual
+  # end
+  #
+  # def test_it_can_find_all_by_name
+  #   item = itemRepository.new
+  #   actual   = item.find_all_by_name("shopin")
+  #   expected = [item.all[0]]
+  #
+  #   assert_equal expected, actual
+  # end
+
+end
