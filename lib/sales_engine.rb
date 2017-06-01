62e4d4cccfd892cb8c25d27c09309d942baafbd3
Samantha Gongora <samantha.gongora@gmail.com>
2017-05-31 18:12:30 -0600

    Test & Pass for mr creation and ir creation

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,4 +1,24 @@
+require_relative 'merchant_repository'
+require_relative 'item_repository'
+
 class SalesEngine
 
+  def initialize(params = {})
+    @items = params.fetch(:items, "")
+    @merchants = params.fetch(:merchants, "")
+  end
+
+  def self.from_csv(files)
+    SalesEngine.new(files)
+  end
+
+  def merchants
+    mr = MerchantRepository.new
+    # mr.create_merchants(merchants)
+  end
+
+  def items
+    ir = ItemRepository.new
+  end 
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
@@ -6,12 +7,26 @@ require_relative '../lib/sales_engine'
 
 class SalesEngineTest < MiniTest::Test
 
-  def test_if_create_class
-    se = SalesEngine.new
+  def test_from_csv_creates_an_instance_of_sales_engine
+    se = SalesEngine.from_csv({:items => './data/items_test.csv',
+                               :merchants => './data/merchants_test.csv'})
 
     assert_instance_of SalesEngine, se
   end
 
+  def test_merchant_repo_creates
+    se = SalesEngine.from_csv({:items => './data/items_test.csv',
+                               :merchants => './data/merchants_test.csv'})
+    mr = se.merchants
 
+    assert_instance_of MerchantRepository, mr
+  end
 
+  def test_items_repo_creates
+    se = SalesEngine.from_csv({:items => './data/items_test.csv',
+                               :merchants => './data/merchants_test.csv'})
+    ir = se.items
+
+    assert_instance_of ItemRepository, ir
+  end
 end
