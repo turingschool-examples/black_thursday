81b3869a46b8744da6fe00b97ba406fe644e6963
Katie Shermer <katieshermer@gmail.com>
2017-06-01 09:54:55 -0600

    Fixed MerchantRepo tests to account for new array structure

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -15,34 +15,27 @@ class MerchantRepoTest < Minitest::Test
   def test_it_initiates_with_hash
     merchant = MerchantRepository.new("./data/merchants.csv")
     result = merchant.merchants
-    assert_instance_of Hash, result
+    assert_instance_of Array, result
   end
 
   def test_it_can_populate_array_with_all_475_merchants
     merchant = MerchantRepository.new("./data/merchants.csv")
     result = merchant.merchants
-    assert_instance_of Hash, result
+    assert_instance_of Array, result
     result2 = merchant.merchants
     assert_equal 475, result2.length
-  end
-
-  def test_merchants_attr_reader_has_fixnums_as_keys
-    merchant = MerchantRepository.new("./data/merchants.csv")
-    merchant_list = merchant.merchants
-    merchant_1 = merchant_list.keys
-    assert_instance_of Fixnum, merchant_1[0]
   end
 
   def test_find_by_name
     merchant = MerchantRepository.new("./data/merchants.csv")
     result = merchant.find_by_name("shopin1901")
-    assert_equal "shopin1901", result.name
+    assert_equal "shopin1901", result.first.name
   end
 
   def test_merchants_carry_id
     merchant = MerchantRepository.new("./data/merchants.csv")
     result = merchant.find_by_id("12334105")
-    assert_instance_of Merchant, result
+    assert_instance_of Merchant, result.first
   end
 
   def test_find_bys_return_nil_if_no_match
@@ -56,7 +49,7 @@ class MerchantRepoTest < Minitest::Test
   def test_it_can_match_with_improper_case
     merchant = MerchantRepository.new("./data/merchants.csv")
     result = merchant.find_by_name("Shopin1901")
-    assert_equal "shopin1901", result.name
+    assert_equal "shopin1901", result.first.name
   end
 
   def test_it_can_return_multiple_name_matches
