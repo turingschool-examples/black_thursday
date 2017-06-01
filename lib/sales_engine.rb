4e3ae392ad3107073a405c409e322780035e5aa6
MikelSage <michael.sagapolutele@yahoo.com>
2017-05-31 21:03:25 -0600

    initial commit

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,20 +1,22 @@
 class SalesEngine
-  def initialize(item_rows, merchant_rows)
-    @items     = ItemRepository.new
-    @merchants = MerchantRepository.new
+  # def initialize(item_rows, merchant_rows)
+  #   @items     = ItemRepository.new
+  #   @merchants = MerchantRepository.new
+  #
+  #   item_rows.each  do |row|
+  #     @items.add(row_data)
+  #   end
+  # end
+  #
+  # def self.from_csv(attrs)
+  #   item_path = attrs[:items]
+  #   merchants_path = attrs[:merchants]
+  #
+  #   item_data = CSV.open item_path, headers: true, header_converters: :symbol
+  #   merchants_data = CSV.open merchants_path, headers: true, header_converters: :symbol
+  #
+  #   se = SalesEngine.new(item_data, merchants_data)
+  # end
 
-    item_rows.each  do |row|
-      @items.add(row_data)
-    end
-  end
-
-  def self.from_csv(attrs)
-    item_path = attrs[:items]
-    merchants_path = attrs[:merchants]
-
-    item_data = CSV.open item_path, headers: true, header_converters: :symbol
-    merchants_data = CSV.open merchants_path, headers: true, header_converters: :symbol
-
-    se = SalesEngine.new(item_data, merchants_data)
-  end
+  
 end
