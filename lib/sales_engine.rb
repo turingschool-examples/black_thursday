ea8a79cc29d5dbc4aac405b65a301ec91c451735
MikelSage <michael.sagapolutele@yahoo.com>
2017-06-01 13:50:16 -0600

    add from_csv method to salesengine class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -6,6 +6,16 @@ class SalesEngine
     @items     = ItemRepository.new(item_rows)
     @merchants = MerchantRepository.new(merchant_rows)
   end
+
+  def self.from_csv(paths)
+    item_path  = paths[:items]
+    merch_path = paths[:merchants]
+
+    item_data  = CSV.open item_path, headers: true, header_converters: :symbol
+    merch_data = CSV.open merch_path, headers: true, header_converters: :symbol
+
+    SalesEngine.new(item_data, merch_data)
+  end
   #
   # def self.from_csv(attrs)
   #   item_path = attrs[:items]
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,7 +5,19 @@ require "pry"
 
 class SalesEngineTest < Minitest::Test
   def test_it_exists
-    se = SalesEngine.new('dummy_variable', 'dummer_variable')
+    item_dummy = CSV.open './data/small_item_set.csv', headers: true, header_converters: :symbol
+    merch_dummy = CSV.open './data/merchant_sample.csv', headers: true, header_converters: :symbol
+    se = SalesEngine.new(item_dummy, merch_dummy)
+
+    assert_instance_of SalesEngine, se
+  end
+
+  def test_from_csv_returns_instance_of_sales_engine
+    small_csv_paths = {
+                        :items     => "./data/small_item_set.csv",
+                        :merchants => "./data/merchant_sample.csv",
+                      }
+    se = SalesEngine.from_csv(small_csv_paths)
 
     assert_instance_of SalesEngine, se
   end
