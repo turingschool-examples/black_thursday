1e9611d6b62d0b7cd2713617dceb76dd6a30542a
MikelSage <michael.sagapolutele@yahoo.com>
2017-06-01 12:27:58 -0600

    formats unit_price, created_at, updated_at, and merchant_id to correct datatypes

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,4 @@
+require "bigdecimal"
 require "pry"
 
 class Item
@@ -11,10 +12,10 @@ class Item
   def initialize(data)
     @name = data[:name]
     @description = data[:description]
-    @unit_price = data[:unit_price]
-    @created_at = data[:created_at]
-    @updated_at = data[:updated_at]
-    @merchant_id = data[:merchant_id]
+    @unit_price = BigDecimal.new(data[:unit_price], 4)
+    @created_at = Time.new(data[:created_at])
+    @updated_at = Time.new(data[:updated_at])
+    @merchant_id = data[:merchant_id].to_i
   end
 
   def unit_price_to_dollars
