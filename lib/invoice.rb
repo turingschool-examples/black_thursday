114d10dab7e7f12e8ad993cc626e1dd3d336ae2b
erose357 <erose357@gmail.com>
2017-06-02 16:31:09 -0600

    Clean up invoice

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -21,33 +21,5 @@ class Invoice
     date = from_file.split("-")
     time = Time.new(date[0], date[1], date[2])
   end
-  #
-  # def id
-  #   @information[:id].to_i
-  # end
-  #
-  # def customer_id
-  #   @information[:customer_id].to_i
-  # end
-  #
-  # def merchant_id
-  #   @information[:merchant_id].to_i
-  # end
-  #
-  # def status
-  #   @information[:status]
-  # end
-  #
-  # def created_at
-  #   date = @information[:created_at].split("-")
-  #   time =Time.new(date[0],date[1],date[2])
-  #   time
-  # end
-  #
-  # def updated_at
-  #   date = @information[:updated_at].split("-")
-  #   time = Time.new(date[0],date[1],date[2])
-  #   time
-  # end
 
 end
