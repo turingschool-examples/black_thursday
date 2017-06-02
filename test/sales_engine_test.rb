d680906194de7c3910d660f7cbd7d1b6a9351220
Michael Centrelli <centm860@newschool.edu>
2017-06-01 19:08:11 -0600

    Sales engine tests

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -24,6 +24,26 @@ class SalesEngineTest < MiniTest::Test
     refute_equal 2, items.find_all_by_price(300).length
   end
 
+  def test_items_searched_by_name
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv"})
+    items = se.items
+
+    assert_instance_of Item, items.find_by_name("Glitter scrabble frames")
+  end
+
+  def test_items_searched_by_merchant_id
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv"})
+    items = se.items
+
+    assert_equal 1, items.find_all_by_merchant_id(12334141).length
+    refute_equal 2, items.find_all_by_merchant_id(12334141).length
+  end
+
+
 
 
 
