da4806e9269773ce68e18ff16924239c0b8b855c
erose357 <erose357@gmail.com>
2017-06-01 18:39:06 -0600

    add date convert method to merchant

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -10,8 +10,12 @@ class Merchant
   def initialize(data, parent)
       @name = data[:name]
       @id = data[:id].to_i
-      @created_at = data[:created_at]
-      @updated_at = data[:updated_at]
+      @created_at = date_convert(data[:created_at])
+      @updated_at = date_convert(data[:updated_at])
   end
 
+  def date_convert(from_file)
+    date = from_file.split("-")
+    time = Time.new(date[0], date[1], date[2])
+  end
 end
