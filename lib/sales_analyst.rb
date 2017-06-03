d801deae138237b94d2edb04c959c07c3cac3e78
JF-Lalonde <lalondej88@gmail.com>
2017-06-03 14:26:48 -0600

    adding strd dev to sales analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -12,17 +12,20 @@ class SalesAnalyst
   def average_items_per_merchant
     @avg_items = @sales_engine.merchants.all_merchant_data.map do |merch|
     merch.items.count
-  end
+    end
     (avg_items.reduce{|sum, num| sum + num}.to_f / avg_items.count).round(2)
   end
 
   def average_items_per_merchant_standard_deviation
-
+    mean = self.average_items_per_merchant
+    sum = @avg_items.reduce(0){|sum, num| sum + (num - mean)**2}
+    actual = Math.sqrt(sum/(@avg_items.count - 1)).round(2)
   end
+
 end
-se = SalesEngine.from_csv({
-  :items     => "./data/items.csv",
-  :merchants => "./data/merchants.csv",
-})
-sa = SalesAnalyst.new(se)
-puts sa.average_items_per_merchant
+# se = SalesEngine.from_csv({
+#   :items     => "./data/items.csv",
+#   :merchants => "./data/merchants.csv",
+# })
+# sa = SalesAnalyst.new(se)
+# puts sa.average_items_per_merchant_standard_deviation
