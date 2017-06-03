c903b974b0a653e92d453f744a58d072668fcbd4
Michael Centrelli <centm860@newschool.edu>
2017-06-03 01:01:36 -0600

    Ir tests done

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -40,24 +40,17 @@ class ItemRepositoryTest < MiniTest::Test
 
     assert_equal 1, ir.find_all_by_price(12).length
   end
-  #
-  # def test_if_can_find_all_by_price_in_range
-  #   ir = ItemRepository.new
-  #   ir.add_items(item_1)
-  #   ir.add_items(item_2)
-  #   results = ir.find_all_by_price_in_range(5..15)
-  #
-  #   assert results.include?(item_1)
-  #   assert results.include?(item_2)
-  # end
-  #
-  # def test_if_can_find_all_by_merchant_id
-  #   ir = ItemRepository.new
-  #   ir.add_items(item_1)
-  #   ir.add_items(item_2)
-  #   results = ir.find_all_by_merchant_id(123)
-  #
-  #   assert results.include?(item_1)
-  # end
+
+  def test_find_all_by_price_in_range
+    ir  = ItemRepository.new("./test/data/items_fixture.csv", sales_engine = nil)
+
+    assert_equal 1, ir.find_all_by_price_in_range(5...13).length
+  end
+
+  def test_if_can_find_all_by_merchant_id
+    ir  = ItemRepository.new("./test/data/items_fixture.csv", sales_engine = nil)
+
+    assert_equal 1, ir.find_all_by_merchant_id(12334141).length
+  end
 
 end
