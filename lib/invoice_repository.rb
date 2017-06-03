5d9b7b4137f6efd4bcc3f3578c9a66160a41072e
Michael Centrelli <centm860@newschool.edu>
2017-06-03 14:01:45 -0600

    First method Iv-repo

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,10 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative '../lib/invoice'
 require 'pry'
 
 class InvoiceRepository
 
+  def initialize(file, sales_engine)
+    @sales_engine = sales_engine
+    @all = []
+  end
 
-
+  def populate_invoice_repo(file)
+    invoice_lines = CSV.open(file, headers: true, header_converters: :symbol)
+    invoice_lines.each do |row|
+      invoice = Invoice.new(row, self)
+      all << invoice
+    end
+    invoice_lines.close
+  end
 
 end
