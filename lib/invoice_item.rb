73bb36ab851b465a3ae343d410afb4c0106cd7d5
erose357 <erose357@gmail.com>
2017-06-03 11:42:05 -0600

    Add tests for invoice item

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+
 class InvoiceItem
 
   attr_reader :id,
@@ -13,7 +15,7 @@ class InvoiceItem
     @item_id    = data[:item_id].to_i
     @invoice_id = data[:invoice_id].to_i
     @quantity   = data[:quantity].to_i
-    @unit_price = data[:unit_price]
+    @unit_price = BigDecimal.new(data[:unit_price].to_i, 4)
     @created_at = date_convert(data[:created_at])
     @updated_at = date_convert(data[:updated_at])
   end
@@ -22,6 +24,8 @@ class InvoiceItem
     date = from_file.split(/[-," ",:]/)
     time = Time.utc(date[0], date[1], date[2], date[3], date[4], date[5], date[6], date[7])
   end
+
+  
 
 
 
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -8,7 +8,25 @@ class InvoiceItemTest < Minitest::Test
 
   def test_new_instance
     iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
-binding.pry
+
     assert_instance_of InvoiceItem, iir.contents[1]
+  end
+
+  def test_return_id_integer
+    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
+
+    assert_equal 13, iir.contents[13].id
+  end
+
+  def test_return_invoice_id
+    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
+
+    assert_equal 5, iir.contents[24].invoice_id
+  end
+
+  def test_return_item_id
+    iir = InvoiceItemRepository.new("./test/data/invoice_items_fixture.csv", self)
+
+    assert_equal 263537372, iir.contents[41].item_id
   end
 end
