aa55bda1a527ca1ff49dbf42c3333b433a12a8cf
MikelSage <michael.sagapolutele@yahoo.com>
2017-06-03 11:05:19 -0600

    modifies unit_price to return rounded bigdecimal

--- lib/item.rb
+++ lib/item.rb
@@ -16,7 +16,7 @@ class Item
     @id = data[:id].to_i
     @name = data[:name]
     @description = data[:description]
-    @unit_price = BigDecimal.new(((data[:unit_price].to_i)/100.0), 4)
+    @unit_price = (BigDecimal.new(((data[:unit_price].to_i)/100.0), 0)).round(2)
     @created_at = Time.parse(data[:created_at])
     @updated_at = Time.parse(data[:updated_at])
     @merchant_id = data[:merchant_id].to_i
