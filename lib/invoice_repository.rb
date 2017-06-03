527158f89d28d51fbf15da1312f2690778e7988d
Samantha Gongora <samantha.gongora@gmail.com>
2017-06-03 15:55:25 -0600

    commit that shit

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,11 +1,27 @@
 require 'pry'
 require 'csv'
-require_relative './lib/invoice'
+require_relative 'invoice'
 
 class InvoiceRepository
+
+  attr_reader :all
+
   def initialize(file_path, parent = nil)
     @parent = parent
     @all = []
-    populate_items(file_path)
+    populate_invoices(file_path)
   end
+
+  def populate_invoices(file_path)
+    CSV.foreach(file_path, row_sep: :auto, headers: true) do |line|
+      self.all << Invoice.new(line, self)
+    end
+  end
+
+  def find_by_id(id)
+    @all.find do |invoice|
+      invoice.id == id
+    end
+  end
+
 end
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,9 +1,29 @@
 require 'pry'
+require 'csv'
 require_relative 'test_helper'
 require_relative '../lib/invoice_repository'
 
 class InvoiceRepositoryTest < Minitest::Test
   def setup
-    
+    @files = './test/data/invoices_test.csv'
+  end
+
+  def test_if_create_class
+    invr = InvoiceRepository.new(@files)
+
+    assert_instance_of InvoiceRepository, invr
+  end
+
+  def test_initialize_and_population_of_items
+    invr = InvoiceRepository.new(@files)
+
+    assert_equal 10, invr.all.length
+  end
+
+  def test_if_find_by_id_returns_correct_value_for_method
+    invr = InvoiceRepository.new(@files)
+
+    assert_equal invr.all[0], invr.find_by_id(1)
+    assert_nil invr.find_by_id(13)
   end
 end
