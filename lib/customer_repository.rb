dfe24ac123a361c7c0d57fb62f192a2d185b6bd9
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-04 15:57:26 -0600

    customer repo populates all test passes

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,4 +1,20 @@
+require 'csv'
+require_relative 'customer'
+
 class CustomerRepository
 
+  attr_reader :all
+
+  def initialize(file_path, parent = nil)
+    @parent = parent
+    @all = []
+    populate_customers(file_path)
+  end
+
+  def populate_customers(file_path)
+    CSV.foreach(file_path, row_sep: :auto, headers: true) do |line|
+      self.all << Customer.new(line, self)
+    end
+  end
 
 end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -4,10 +4,20 @@ require_relative '../lib/customer_repository'
 
 class CustomerRepositoryTest < MiniTest::Test
 
-  def test_if_class_creates
-    cr = CustomerRepository.new
+  def setup
+    @file_path = './test/data/customers_test.csv'
+  end
 
-    assert_instance_of CustomerRepository, cr    
+  def test_if_class_creates
+    cr = CustomerRepository.new(@file_path)
+
+    assert_instance_of CustomerRepository, cr
+  end
+
+  def test_if_populates_repo_with_all
+    cr = CustomerRepository.new(@file_path)
+
+    assert_equal 10, cr.all.length
   end
 
 
