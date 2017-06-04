92afd95499e036b08e94698041a223d3b0a84416
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-04 12:10:54 -0600

    invoice_items default attributes passes

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,5 +1,26 @@
+require 'bigdecimal'
+require 'time'
+
 class InvoiceItem
 
+  attr_reader   :id,
+                :item_id,
+                :invoice_id,
+                :quantity,
+                :unit_price,
+                :created_at,
+                :updated_at
+
+  def initialize(params = {}, parent = nil)
+    @parent     = parent
+    @id         = params["id"].to_i
+    @item_id    = params["item_id"].to_i
+    @invoice_id = params["invoice_id"].to_i
+    @quantity   = params["quantity"].to_i
+    @unit_price = BigDecimal.new(params["unit_price"])/100
+    @created_at = Time.parse(params["created_at"])
+    @updated_at = Time.parse(params["updated_at"])
+  end
 
 
 end
