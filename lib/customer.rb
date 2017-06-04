8535a3a22964be4a4dbe2e18b796add78f726dd7
erose357 <erose357@gmail.com>
2017-06-04 17:08:00 -0600

    Add all methods and tests for customer repo

--- lib/customer.rb
+++ lib/customer.rb
@@ -8,7 +8,7 @@ class Customer
               :updated_at
 
   def initialize(data, parent)
-    @id         = data[:id]
+    @id         = data[:id].to_i
     @first_name = data[:first_name]
     @last_name  = data[:last_name]
     @created_at = date_convert(data[:created_at])
