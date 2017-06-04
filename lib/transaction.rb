eb7370568a934fd70ab100ba77a03b5ec46e13e5
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-04 13:36:49 -0600

    test default_attributes_and_format passes for transaction_test

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,4 +1,25 @@
+require 'time'
+
 class Transaction
+
+  attr_reader :id,
+              :invoice_id,
+              :credit_card_number,
+              :credit_card_expiration_date,
+              :result,
+              :created_at,
+              :updated_at
+
+  def initialize(params = {}, parent = nil)
+    @parent                      = parent
+    @id                          = params["id"].to_i
+    @invoice_id                  = params["invoice_id"].to_i
+    @result                      = params["result"]
+    @credit_card_number          = params["credit_card_number"]
+    @credit_card_expiration_date = params["credit_card_expiration_date"]
+    @created_at                  = Time.parse(params["created_at"])
+    @updated_at                  = Time.parse(params["updated_at"])
+  end
 
 
 
