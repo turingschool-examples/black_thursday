b7c745c37a355eef17753f87fcddc62a4dc58f82
Matt DeVoe <matthew.devoe@gmail.com>
2017-06-04 14:45:58 -0600

    test TR population of items success

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,7 +1,21 @@
 require_relative 'transaction'
+require 'csv'
 
 class TransactionRepository
 
+  attr_reader :all
+
+  def initialize(file_path, parent = nil)
+    @parent = parent
+    @all = []
+    populate_items(file_path)
+  end
+
+  def populate_items(file_path)
+    CSV.foreach(file_path, row_sep: :auto, headers: true) do |line|
+      self.all << Transaction.new(line, self)
+    end
+  end
 
 
 end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -1,4 +1,23 @@
+require 'csv'
 require_relative 'test_helper'
 require_relative '../lib/transaction_repository'
 
 class TransactionRepositoryTest < MiniTest::Test
+
+  def setup
+    @files = './test/data/transactions_test.csv'
+  end
+
+  def test_if_create_class
+    tr = TransactionRepository.new(@files)
+
+    assert_instance_of TransactionRepository, tr
+  end
+
+  def test_initialize_and_population_of_items
+    tr = TransactionRepository.new(@files)
+
+    assert_equal 10, tr.all.length
+  end
+
+end
