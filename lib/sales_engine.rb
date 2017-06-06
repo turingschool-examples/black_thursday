f7c50d49d62281de87d2610cf2a4f4849529fb14
JF-Lalonde <lalondej88@gmail.com>
2017-06-06 13:47:46 -0600

    testing

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,26 +1,26 @@
 require 'csv'
 require_relative '../lib/merchant_repository'
 require_relative '../lib/item_repository'
-
 require_relative '../lib/invoice_item_repository'
 require_relative '../lib/transaction_repository'
 require_relative '../lib/customer_repository'
+require_relative '../lib/invoice_repository'
 
 class SalesEngine
   attr_reader :items,
               :merchants,
-
               :invoice_items,
               :transactions,
-              :customers
+              :customers,
+              :invoices
 
   def initialize(data_files)
     @items = ItemRepository.new(data_files, self)
     @merchants = MerchantRepository.new(data_files, self)
-
     @invoice_items = InvoiceItemRepository.new(data_files, self)
     @transactions = TransactionRepository.new(data_files, self)
     @customers = CustomerRepository.new(data_files, self)
+    @invoices = InvoiceRepository.new(data_files, self)
   end
 
   def self.from_csv(data_files)
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -1,5 +1,31 @@
 require_relative 'test_helper'
 require_relative '../lib/invoice'
 
-class InvoiceTest
+class InvoiceTest < Minitest::Test
+
+  attr_reader :invoice
+
+  def setup
+    @invoice = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      }, self)
+  end
+
+  def test_it_exists
+    @invoice
+
+    assert_instance_of Invoice, actual
+  end
+
+  def test_it_contains_all_invoice_data
+    actual = @invoice.all
+    expected = @all_invoice_data
+
+    assert_equal expected, actual
+  end
 end
