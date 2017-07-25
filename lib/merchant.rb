cbad05531d56f2b206e7ded8dd3418df99f8e47c
snayrouz <samuel.nayrouz@gmail.com>
2017-07-25 15:30:56 -0600

    Edit transaction repo class and test

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,8 +1,8 @@
 require 'time'
 
 class Merchant
-  attr_reader :id, :name, :created_at, :updated_at, :repo
-
+  attr_reader :id, :name, :created_at, :updated_at
+  # , :repo
   def initialize(merch_hash)
     @id         = merch_hash[:id].to_i
     @name       = merch_hash[:name].to_s
@@ -11,6 +11,7 @@ class Merchant
   end
   #
   # @merch_repo = merch_repo
+
   # def items
   #   merch_repo.find_items_by_merchant_id(id)
   # end
--- lib/transaction_repo.rb
+++ lib/transaction_repo.rb
@@ -1,28 +1,34 @@
 class TransactionRepository
-    
+  attr_reader :transactions
+
+  def initialize(csv_data)
+    @transactions = csv_data
+  end
 
   # The TransactionRepository is responsible for holding and searching our Transaction instances.
   def all
+    transactions
     # all - returns an array of all known Transaction instances
   end
 
-  def find_by_id
+  def find_by_id(id)
+    transactions.detect { |transaction| transaction.id == id }
     # find_by_id - returns either nil or an instance of Transaction with a matching ID
   end
 
-  def find_all_by_invoice_id
+  def find_all_by_invoice_id(invoice_id)
+    transactions.select { |transaction| transaction.invoice_id == invoice_id }
     # find_all_by_invoice_id - returns either [] or one or more matches which have a matching invoice ID
   end
 
-  def find_all_by_credit_card_number
+  def find_all_by_credit_card_number(credit_card_number)
+    transactions.select { |transaction| transaction.credit_card_number == credit_card_number }
     # find_all_by_credit_card_number - returns either [] or one or more matches which have a matching credit card number
   end
 
-  def find_all_by_result
+  def find_all_by_result(status)
+    transactions.select { |transaction| transaction.status == status }
     # find_all_by_result - returns either [] or one or more matches which have a matching status
   end
-
-
-
 
 end
