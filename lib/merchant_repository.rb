45b809526ff1ff4baa34f5f13413df4d7d126e15
mimilettd <mimilettd@gmail.com>
2017-07-24 18:34:40 -0600

    load csv file

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,4 +1,19 @@
 require_relative 'merchant'
+require 'csv'
+require 'pry'
 
 class MerchantRepository
+  def initialize(data)
+    load_csv_file(data)
+  end
+
+  def load_csv_file(data)
+    contents = CSV.open data, headers: true, header_converters: :symbol
+    contents.each do |row|
+      id = row[:id]
+      name = row[:name]
+      created_at = row[:created_at]
+      updated_at = row[:updated_at]
+    end
+  end
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,11 +1,16 @@
 require_relative 'test_helper'
 require './lib/merchant_repository'
+require './lib/merchant'
+require 'pry'
 
 class MerchantRepositoryTest < Minitest::Test
   def test_that_merchant_repo_class_exist
-    mr = MerchantRepository.new
+    mr = MerchantRepository.new("./data/merchants.csv")
     assert_instance_of MerchantRepository, mr
   end
 
-  
+  def test_it_can_load_csv_file
+    mr = MerchantRepository.new("./data/merchants.csv")
+    assert mr
+  end
 end
