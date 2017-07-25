a5b2e6688abb1097e23c85568bb52054a7562091
Mike Heft <mikeheft@gmail.com>
2017-07-25 11:04:36 -0600

    cosmetic change in se init

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -10,10 +10,10 @@ class SalesEngine
 
   def initialize(data)
     @invoices = InvoiceRepo.new(data[:invoices], self)
-    # @merchants = MerchantRepo.new(data[:merchants], self)
-    # @items     = ItemRepo.new(data[:items], self)
-    # @customers = CustomerRepo.new(data[:customers], self)
-    # @transactions = TransactionRepo.new(data[:transactions], self)
+    # @merchants     = MerchantRepo.new(data[:merchants], self)
+    # @items         = ItemRepo.new(data[:items], self)
+    # @customers     = CustomerRepo.new(data[:customers], self)
+    # @transactions  = TransactionRepo.new(data[:transactions], self)
     # @invoice_items = InvoiceItemsRepo.new(data[:invoice_items], self)
   end
 
