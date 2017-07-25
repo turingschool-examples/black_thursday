e104e5f03c9e489d34f71b28b7661ac06b2bedd9
samuelssnider <sam_redins@yahoo.com>
2017-07-25 17:32:53 -0600

    average working

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,9 +1,29 @@
-require './sales_engine'
+require './lib/sales_engine'
+require 'pry'
 
 
 class SalesAnalyst
   def initialize(sales_engine)
+    @sales_engine = sales_engine
+  end
 
+  def average_items_per_merchant
+    the_merchants = @sales_engine.merchants.all
+    average_array = []
+    the_merchants.each do |merchant|
+      average_array << total_matches(merchant.id)
+    end
+    sum = 0.0
+    average_array.each do |average|
+      sum += average
+    end
+    average = sum/average_array.count.to_f
+    average
+  end
+
+  def total_matches(id)
+    count = @sales_engine.items.find_all_by_merchant_id(id).count
+    count
   end
 
 end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -37,5 +37,5 @@ class SalesEngine
     end
     created
   end
-  
+
 end
