f0572c8ce069c43e6afc297a42648a2a20e7c3cf
RickyAmparo <rickyamparo@gmail.com>
2017-07-25 16:08:47 -0600

    Take load_merchants functionality out of all, all now returns array of merchant objects

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -15,7 +15,8 @@ class MerchantRepositoryTest < Minitest::Test
   def test_can_return_array_of_all_known_merchant_instances
     merchant_repo = MerchantRepository.new("./data/merchants.csv", "salesengine")
 
-    assert_instance_of Hash, merchant_repo.all
+    assert_instance_of Array, merchant_repo.all
+    assert_instance_of Merchant, merchant_repo.all[0]
   end
 
   def test_can_find_by_id
