cdaff049ed086f50d23bfe4b3c17cb95e6d53d36
Mike Heft <mikeheft@gmail.com>
2017-07-25 10:57:28 -0600

    change parent names

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,6 +1,9 @@
 require_relative '../lib/invoice_repo'
 # require_relative '../lib/merchant_repo'
 # require_relative '../lib/item_repo'
+# require_relative '../lib/customer_repo'
+# require_relative '../lib/transaction_repo'
+# require_relative '../lib/invoice_item_repo'
 
 class SalesEngine
   attr_reader :invoices
@@ -9,5 +12,13 @@ class SalesEngine
     @invoices = InvoiceRepo.new(data[:invoices], self)
     # @merchants = MerchantRepo.new(data[:merchants], self)
     # @items     = ItemRepo.new(data[:items], self)
+    # @customers = CustomerRepo.new(data[:customers], self)
+    # @transactions = TransactionRepo.new(data[:transactions], self)
+    # @invoice_items = InvoiceItemsRepo.new(data[:invoice_items], self)
   end
+
+  def self.from_csv(data)
+    SalesEngine.new(data)
+  end
+
 end
