0332b2d1b35c24c5716f45c49c2eedb93e66917f
marvin.bonds <marvinbonds@yahoo.com>
2017-07-25 16:45:03 -0600

    Tests added

--- lib/merchant_repo.rb
+++ lib/merchant_repo.rb
@@ -1,14 +1,32 @@
-
+require_relative 'merchant'
+require 'pry'
 class MerchantRepo
 
-  def initialize
+  attr_reader :merchants
 
+  def initialize(file,se = nil)
+    @merchants = {}
+    open_file(file)
 
+  end
+
+  def open_file(file)
+    CSV.foreach file,  headers: true, header_converters: :symbol do |row|
+      data = row.to_h
+      merchants[data[:id].to_i] = Merchant.new(data, self)
+    end
   end
 
   def all
-
+    merchants.values
   end
+
+  def find_by_id(id)
+    merchants[id]
+  end
+
+  def 
+
 
 
 end
--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -9,10 +9,19 @@ class MerchantRepoTest < Minitest::Test
   end
 
   def test_find_all_merchants
-
     assert_equal 100, mr.all.count
-
   end
+
+  def test_it_returns_nil_when_no_id_present
+    assert_nil mr.find_by_id(5)
+  end
+
+  def test_it_finds_instance_of_merchant_by_id
+    assert_equal Merchant, mr.find_by_id(12334135).class
+  end
+
+  def test_it_finds_name_of_merchant
+    
 
 
 end
