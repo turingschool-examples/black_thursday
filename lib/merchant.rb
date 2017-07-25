4ab75ce12867e731320b4c702bf0555c9bbab164
snayrouz <samuel.nayrouz@gmail.com>
2017-07-24 19:09:49 -0600

    Add to item class and test

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -4,8 +4,8 @@ class Merchant
   attr_reader :id, :name
 
   def initialize(merch_hash)
-    @id = merch_hash[:id]
-    @name = merch_hash[:name]
+    @id = merch_hash[:id].to_i
+    @name = merch_hash[:name].to_s
   end
 
 
--- test/item_test.rb
+++ test/item_test.rb
@@ -7,7 +7,7 @@ require 'bigdecimal'
 class ItemTest < Minitest::Test
 
   def test_it_exist
-    item = Item.new({
+    item = Item.new({:id         => 263395237,
                     :name        => "Pencil",
                     :description => "You can use it to write things",
                     :unit_price  => BigDecimal.new(10.99,4),
@@ -17,8 +17,19 @@ class ItemTest < Minitest::Test
     assert_instance_of Item, item
   end
 
+  def test_it_has_an_id_that_returns_an_integer
+    item = Item.new({:id         => 263395237,
+                    :name        => "Pencil",
+                    :description => "You can use it to write things",
+                    :unit_price  => BigDecimal.new(10.99,4),
+                    :created_at  => Time.now,
+                    :updated_at  => Time.now, })
+
+    assert_equal 263395237, item.id
+  end
+
   def test_it_has_a_name
-    item = Item.new({
+    item = Item.new({:id         => 263395237,
                     :name        => "Pencil",
                     :description => "You can use it to write things",
                     :unit_price  => BigDecimal.new(10.99,4),
@@ -29,45 +40,47 @@ class ItemTest < Minitest::Test
   end
 
   def test_it_has_a_description
-    item = Item.new({
+    item = Item.new({:id         => 263395237,
                     :name        => "Pencil",
                     :description => "You can use it to write things",
                     :unit_price  => BigDecimal.new(10.99,4),
                     :created_at  => Time.now,
                     :updated_at  => Time.now, })
+
     assert_equal "You can use it to write things", item.description
   end
 
   def test_it_has_a_unit_price
-    item = Item.new({
+    item = Item.new({:id         => 263395237,
                     :name        => "Pencil",
                     :description => "You can use it to write things",
                     :unit_price  => BigDecimal.new(10.99,4),
                     :created_at  => Time.now,
                     :updated_at  => Time.now, })
+
     assert_instance_of BigDecimal, item.unit_price
   end
 
   def test_it_has_a_created_time
-    item = Item.new({
+    item = Item.new({:id         => 263395237,
                     :name        => "Pencil",
                     :description => "You can use it to write things",
                     :unit_price  => BigDecimal.new(10.99,4),
                     :created_at  => Time.now,
                     :updated_at  => Time.now, })
+
     assert_instance_of Time, item.created_at
   end
 
   def test_it_has_an_updated_time
-    item = Item.new({
+    item = Item.new({:id         => 263395237,
                     :name        => "Pencil",
                     :description => "You can use it to write things",
                     :unit_price  => BigDecimal.new(10.99,4),
                     :created_at  => Time.now,
                     :updated_at  => Time.now, })
+
     assert_instance_of Time, item.updated_at
   end
-
-
 
 end
