5d151f7516857bc19ff4f2346fcd0f95ca8729e9
snayrouz <samuel.nayrouz@gmail.com>
2017-07-25 15:02:47 -0600

    Methods for invoice repo

--- lib/invoice_repo.rb
+++ lib/invoice_repo.rb
@@ -1,15 +1,35 @@
 class InvoiceRepository
+  attr_reader :invoices
 
+  def initialize(csv_data)
+    @invoices = csv_data
+  end
   # sales engine will be passed through each repo
-  # The InvoiceRepository is responsible for holding and searching our Invoice instances. It offers the following methods:
-  #
-  # all - returns an array of all known Invoice instances
-  # find_by_id - returns either nil or an instance of Invoice with a matching ID
-  # find_all_by_customer_id - returns either [] or one or more matches which have a matching customer ID
-  # find_all_by_merchant_id - returns either [] or one or more matches which have a matching merchant ID
-  # find_all_by_status - returns either [] or one or more matches which have a matching status
-  # The data can be found in data/invoices.csv so the instance is created and used like this:
 
+  def all
+    invoices
+  # returns an array of all known Invoice instances
+  end
+
+  def find_by_id
+    invoices.detect { |invoice| invoice.id == id }
+  # returns either nil or an instance of Invoice with a matching ID
+  end
+
+  def find_all_by_customer_id
+    invoices.select { |invoice| invoice.customer_id == customer_id }
+  # returns either [] or one or more matches which have a matching customer ID
+  end
+
+  def find_all_by_merchant_id
+    invoices.select { |invoice| invoice.merchant_id == merchant_id }
+  # find_all_by_merchant_id - returns either [] or one or more matches which have a matching merchant ID
+  end
+
+  def find_all_by_status
+    invoices.select { |invoice| invoice.status == status }
+  # find_all_by_status - returns either [] or one or more matches which have a matching status
+  end
 
 
 end
