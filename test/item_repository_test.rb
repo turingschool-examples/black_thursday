46091f6a6fa7148bd01fc366a7430e1e63fc028c
sidewinder2020 <sdwinder1@gmail.com>
2017-07-25 16:21:16 -0600

    add test

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -7,18 +7,21 @@ require 'pry'
 class ItemRepositoryTest < Minitest::Test
 
   def test_it_exists
-    ir = ItemRepository.new("./data/items.csv")
+    ir = ItemRepository.new("./test/test_data/item_test.csv")
 
     assert_instance_of ItemRepository, ir
   end
 
   def test_all_returns_all_items_in_the_correct_format
-      ir = ItemRepository.new("./data/items.csv")
+      ir = ItemRepository.new("./test/test_data/item_test.csv")
 
       assert_instance_of Item, ir.items[0]
       assert_equal 'Shopin1901', ir.items[0].name
       assert_equal 12334105, ir.items[0].id
       assert_equal 475, ir.items.count
+  end
+
+  def test_it_return_all
   end
 
 end
