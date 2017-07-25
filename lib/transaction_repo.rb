5f6b5442070fc9107e9aa8c202d53e772fe80e14
snayrouz <samuel.nayrouz@gmail.com>
2017-07-25 07:46:13 -0600

    Build out customer and transaction class

--- lib/transaction_repo.rb
+++ lib/transaction_repo.rb
@@ -1,12 +1,25 @@
 class TransactionRepository
 
-  # The TransactionRepository is responsible for holding and searching our Transaction instances. It offers the following methods:
-  #
-  # all - returns an array of all known Transaction instances
-  # find_by_id - returns either nil or an instance of Transaction with a matching ID
-  # find_all_by_invoice_id - returns either [] or one or more matches which have a matching invoice ID
-  # find_all_by_credit_card_number - returns either [] or one or more matches which have a matching credit card number
-  # find_all_by_result - returns either [] or one or more matches which have a matching status
+  # The TransactionRepository is responsible for holding and searching our Transaction instances. 
+  def all
+    # all - returns an array of all known Transaction instances
+  end
+
+  def find_by_id
+    # find_by_id - returns either nil or an instance of Transaction with a matching ID
+  end
+
+  def find_all_by_invoice_id
+    # find_all_by_invoice_id - returns either [] or one or more matches which have a matching invoice ID
+  end
+
+  def find_all_by_credit_card_number
+    # find_all_by_credit_card_number - returns either [] or one or more matches which have a matching credit card number
+  end
+
+  def find_all_by_result
+    # find_all_by_result - returns either [] or one or more matches which have a matching status
+  end
 
 
 
