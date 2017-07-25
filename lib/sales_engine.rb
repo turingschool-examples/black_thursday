e56267218e3152da910952860f2e14dfafd42de1
samuelssnider <sam_redins@yahoo.com>
2017-07-25 13:00:08 -0600

    Add to class method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,31 +5,35 @@ require './lib/merchant_repository'
 
 class SalesEngine
 
-  attr_reader :mr
+  attr_reader :merchants,
+              :items
 
   def initialize
     @array = []
-    @it = ItemRepository.new
-    @mr = MerchantRepository.new
   end
 
-  def from_csv(input)
+  def self.init
+    @items = ItemRepository.new
+    @merchants = MerchantRepository.new
+  end
+
+  def self.from_csv(input)
+    init
     input.each_pair do |key, value|
-      load_csv(value, key)
+      row = CSV.open value, headers: true, header_converters: :symbol
+      if key == :items
+        row.each do |data|
+          @items.add_data(data.to_hash)
+        end
+      elsif key == :merchants
+        row.each do |data|
+          @merchants.add_data(data.to_hash)
+        end
+      end
     end
   end
 
-  def open_csv(path)
-    row = CSV.open path, headers: true, header_converters: :symbol
-    row.each do |data|
-    # # @merchants.add_data(data)
-    #   # binding.pry
-    #   array << data.to_h
-      @items.add_data(data.to_hash)
-      # puts data.inspect
-      # puts "\n\n END DATA \n\n"
-    end
-  end
+
 
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -7,7 +7,18 @@ require 'pry'
 
 class SalesEngineTest < Minitest::Test
   def test_it_can_parse
+    skip
     se = SalesEngine.new
-    se.open_csv("./data/merchants_short.csv")
+    se.load_csv("./data/merchants_short.csv", :merchants)
+    assert_equal 5, se.merchants.merchants.count
+  end
+
+  def test_class_method_working
+    se = SalesEngine.from_csv({
+                              :items     => "./data/items.csv",
+                              :merchants => "./data/merchants.csv",
+                                                                  })
+    assert_instance_of ItemRepository, se.items
+    assert_instance_of MerchantRepository, se.merchants
   end
 end
