cf18c4d3e36a9281bc7d3299d39a68aa26db49b6
snayrouz <samuel.nayrouz@gmail.com>
2017-07-25 11:27:30 -0600

    Add to salesengine

--- lib/salesengine.rb
+++ lib/salesengine.rb
@@ -1,22 +1,25 @@
 require './lib/merchant_repo'
 require './lib/item_repo'
+require './lib/invoice_repo'
 require 'csv'
 
 class SalesEngine
-  attr_reader :items, :merchants, :invoices
+  attr_reader :items, :merchants, :invoices, :invoice_items
 
   def initialize(se_hash)
     @items = ItemRepository.new(se_hash[:item])
     @merchants = MerchantRepository.new(se_hash[:merchant])
     @invoices = InvoiceRepository.new(se_hash(:invoice))
+    @invoice_items = InvoiceItemRepository.new(:invoice_items)
   end
 
   def self.from_csv(se_hash)
     item_data = Item.load_data(se_hash[:items])
     merchant_data = Merchant.load_data(se_hash[:merchants])
     invoice_data = Invoice.load_data(se_hash[:invoices])
+    invoice_item_data = InvoiceItem.load_data(se_hash[:invoice_items])
 
-    Salesengine.new(item_data, merchant_data, invoice_data)
+    Salesengine.new(item_data, merchant_data, invoice_data, invoice_item_data)
   end
 
   def self.load_data(se_hash)
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -1,12 +1,15 @@
 require 'simplecov'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/invoice_repo'
+require './lib/invoice'
 
+class InvoiceTest < Minitest::Test
 
-class InvoiceRepositoryTest < Minitest::Test
+  def test_it_exist
+    invoice = Invoice.new
 
-
+    assert_instance_of Invoice, invoice
+  end
 
 
 
