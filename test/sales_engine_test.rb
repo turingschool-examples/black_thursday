012e40620eff25026949de917cf01944ddee535e
mimilettd <mimilettd@gmail.com>
2017-07-25 22:53:12 -0600

    refactor from_csv method and test it can find merchant by name

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -15,4 +15,24 @@ class SalesEngineTest < Minitest::Test
       })
     assert se
   end
+
+  def test_it_can_create_a_new_instance_of_merchant_repo_class
+    sales_engine = SalesEngine.new
+    se = sales_engine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+    assert_instance_of MerchantRepository, se
+  end
+
+  def test_it_can_find_merchant_by_name
+    se = SalesEngine.new
+    se.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+    mr = se.merchants
+    merchant = mr.find_by_name("CJsDecor")
+    assert merchant
+  end
 end
