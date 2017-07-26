09c914afeadfc63ec320564049a88af0d6e97d4c
mimilettd <mimilettd@gmail.com>
2017-07-25 21:40:37 -0600

    test find_all_by_price_in_range returns empty array if no price in range

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -71,6 +71,13 @@ class ItemRepositoryTest < Minitest::Test
     assert ir.find_all_by_price_in_range(1..1200)
   end
 
+  def test_it_returns_an_empty_arrah_if_no_price_in_range
+    ir = ItemRepository.new("./data/mini_items.csv")
+    expected = []
+
+    assert_equal expected, ir.find_all_by_price_in_range(1400..1600)
+  end
+
   def test_it_can_find_all_by_merchant_id
     ir = ItemRepository.new("./data/mini_items.csv")
     assert ir.find_all_by_merchant_id(12334185)
