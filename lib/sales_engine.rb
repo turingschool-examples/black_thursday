defc44ae821f4d0697e3d1de53d4974ec6606bd5
amhursh <amhursh@gmail.com>
2017-07-27 01:37:09 -0600

    Add invoices to sales repo

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,15 +4,17 @@ require_relative 'item_repository'
 class SalesEngine
 
   attr_reader :items,
-              :merchants
+              :merchants,
+              :invoices
 
   def initialize(item_file, merchant_file)
     @items = ItemRepository.new(item_file, self)
     @merchants = MerchantRepository.new(merchant_file, self)
+    @invoices = InvoiceRepository.new(invoice_file, self)
   end
 
   def self.from_csv(file = {})
-    SalesEngine.new(file[:items], file[:merchants])
+    SalesEngine.new(file[:items], file[:merchants], file[:invoices])
   end
 
 end
