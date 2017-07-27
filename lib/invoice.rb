fd5e0768b2beb1f2d21332cd1eacb6dfcfd1c521
samuelssnider <sam_redins@yahoo.com>
2017-07-27 11:29:08 -0600

    commiting before branch

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -14,4 +14,6 @@ class Invoice
     @created_at  = (data[:created_at])
     @updated_at  = (data[:update_at])
   end
+
+  
 end
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -9,4 +9,23 @@ class InvoiceRepositoryTest < Minitest::Test
     assert i
     assert_instance_of InvoiceRepository, i
   end
+
+  def test_it_can_be_added_to
+    ir = InvoiceRepository.new
+    hash_one = {status: "shipped",
+                id: 1, merchant_id: 3}
+    hash_two = {status: "pending",
+                id: 2, merchant_id: 4}
+
+    ir.add_data(hash_one)
+    ir.add_data(hash_two)
+
+    assert_equal 1        , ir.invoices.first.id
+    assert_equal "shipped", ir.invoices.first.status
+    assert_equal 3        , ir.invoices.first.merchant_id
+    assert_equal 4        , ir.invoices.last.merchant_id
+    assert_equal "pending", ir.invoices.last.status
+    assert_equal 2        , ir.invoices.last.id
+
+  end
 end
