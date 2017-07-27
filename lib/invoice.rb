603483b69434f1ee0a0a8427443c806561b32d04
samuelssnider <sam_redins@yahoo.com>
2017-07-27 09:24:54 -0600

    more work on invoice tests

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,10 +1,10 @@
 class Invoice
   def initialize(data)
-    @id - returns the integer id
+    @id=
     @customer_id - returns the customer id
     @merchant_id - returns the merchant id
     @status - returns the status
-    @created_at Time.parse(data)
+    @created_at Time.parse(data[:created_at])
     @updated_at - returns a Time instance for the date the item was last modified
   end
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -5,8 +5,8 @@ require './lib/invoice'
 
 class InvoiceTest < Minitest::Test
   def setup
-    hash = {id: 1, customer_id: 3, merchant_id: 4
-            status: "Closed", created_at: Time.now
+    hash = {id: 1, customer_id: 3, merchant_id: 4,
+            status: "Closed", created_at: Time.now,
             update_at: Time.now                   }
     @in_v = Invoice.new(hash)
   end
@@ -21,15 +21,27 @@ class InvoiceTest < Minitest::Test
   def test_own_id_can_be_got
     assert_equal 1, @in_v.id
   end
+
   def test_customer_id_can_be_got
     assert_equal 3, @in_v.customer_id
   end
+
   def test_merchant_id_can_be_got
     assert_equal 4, @in_v.merchant_id
   end
+
   def test_status_can_be_got
-    @in_v.status
+    assert_equal "Closed", @in_v.status
   end
+
+  def test_created_at_can_be_got
+    assert_equal Time.now, @in_v.created_at
+  end
+
+  def test_updated_at_can_be_got
+    assert_equal Time.now, @in_v.updated_at
+  end
+
 
 
 
