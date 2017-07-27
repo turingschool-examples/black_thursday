47d516e5e2f189958db7d85a0b99d3728060ec54
amhursh <amhursh@gmail.com>
2017-07-27 02:12:32 -0600

    Build and pass tests for invoice instance vars

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -10,9 +10,9 @@ class Invoice
               :updated_at
 
   def initialize(invoice_info, invoice_repo)
-    @id =             invoice_info[:name]
-    @customer_id =    invoice_info[:customer_id]
-    @merchant_id =    invoice_info[:merchant_id]
+    @id =             invoice_info[:id].to_i
+    @customer_id =    invoice_info[:customer_id].to_i
+    @merchant_id =    invoice_info[:merchant_id].to_i
     @status =         invoice_info[:status]
     @created_at =     Time.parse(invoice_info[:created_at])
     @updated_at =     Time.parse(invoice_info[:updated_at])
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -13,4 +13,58 @@ class InvoiceTest < Minitest::Test
     assert_instance_of Invoice, invoice
   end
 
+  def test_invoice_has_id
+    invoice = Invoice.new({:id => 17, :customer_id => 5, :merchant_id => 12334912,
+                           :status => "pending", :created_at => "2005-06-03",
+                           :updated_at => "2015-07-01"}, self)
+    id = invoice.id
+
+    assert_equal 17, invoice.id
+  end
+
+  def test_invoice_has_customer_id
+    invoice = Invoice.new({:id => 17, :customer_id => 5, :merchant_id => 12334912,
+                           :status => "pending", :created_at => "2005-06-03",
+                           :updated_at => "2015-07-01"}, self)
+    customer_id = invoice.customer_id
+
+    assert_equal 5, customer_id
+  end
+
+  def test_invoice_has_merchant_id
+    invoice = Invoice.new({:id => 17, :customer_id => 5, :merchant_id => 12334912,
+                           :status => "pending", :created_at => "2005-06-03",
+                           :updated_at => "2015-07-01"}, self)
+    merchant_id = invoice.merchant_id
+
+    assert_equal 12334912, merchant_id
+  end
+
+  def test_invoice_has_status
+    invoice = Invoice.new({:id => 17, :customer_id => 5, :merchant_id => 12334912,
+                           :status => "pending", :created_at => "2005-06-03",
+                           :updated_at => "2015-07-01"}, self)
+    status = invoice.status
+
+    assert_equal "pending", status
+  end
+
+  def test_invoice_has_created_at
+    invoice = Invoice.new({:id => 17, :customer_id => 5, :merchant_id => 12334912,
+                           :status => "pending", :created_at => "2005-06-03",
+                           :updated_at => "2015-07-01"}, self)
+    created_at = invoice.created_at
+
+    assert_instance_of Time, created_at
+  end
+
+  def test_invoice_has_updated_at
+    invoice = Invoice.new({:id => 17, :customer_id => 5, :merchant_id => 12334912,
+                           :status => "pending", :created_at => "2005-06-03",
+                           :updated_at => "2015-07-01"}, self)
+    updated_at = invoice.updated_at
+
+    assert_instance_of Time, updated_at
+  end
+
 end
