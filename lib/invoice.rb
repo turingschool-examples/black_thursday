ae15a235187fda80d44a7b6c2d3ef4b9df23f43f
samuelssnider <sam_redins@yahoo.com>
2017-07-27 10:16:06 -0600

    ivtest more

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,10 +1,11 @@
+
 class Invoice
   def initialize(data)
-    @id=
-    @customer_id - returns the customer id
-    @merchant_id - returns the merchant id
-    @status - returns the status
-    @created_at Time.parse(data[:created_at])
-    @updated_at - returns a Time instance for the date the item was last modified
+    @id          = data[:id]
+    @customer_id = data[:customer_id]
+    @merchant_id = data[:merchant_id]
+    @status      = data[:status]
+    @created_at  = Time.parse(data[:created_at])
+    @updated_at  = Time.parse(data[:update_at])
   end
 end
