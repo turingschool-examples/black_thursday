3f7896909fc1d991334f1fa582d57c5630a1c99c
samuelssnider <sam_redins@yahoo.com>
2017-07-27 12:28:04 -0600

    invoice repo test suite work

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -8,12 +8,27 @@ class InvoiceRepository
   end
 
   def all # - returns an array of all known Invoice instances
+    @invoices
   end
 
-  def find_by_id #- returns either nil or an instance of Invoice with a matching ID
+  def find_by_id(id) #- returns either nil or an instance of Invoice with a matching ID
+    result = nil
+    @invoices.each do |invoice|
+      if invoice.id == id
+        result = invoice
+      end
+    end
+    result
   end
 
-  def find_all_by_customer_id #- returns either [] or one or more matches which have a matching customer ID
+  def find_all_by_customer_id(cust_id)
+    results = []
+    @invoices.each do |invoice|
+      if invoice.customer_id == cust_id
+        results << invoice
+      end
+    end
+    results
   end
 
   def find_all_by_merchant_id #- returns either [] or one or more matches which have a matching merchant ID
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,6 +1,6 @@
 require 'minitest'
 require 'minitest/autorun'
-# require 'minitest/emoji'
+require 'minitest/pride'
 require_relative '../lib/invoice_repository'
 
 class InvoiceRepositoryTest < Minitest::Test
@@ -46,5 +46,60 @@ class InvoiceRepositoryTest < Minitest::Test
   end
 
   def test_all_is_working
-    
+    assert_equal 4        , @invoice_r.all.count
+    assert_equal 123      , @invoice_r.all.first.id
+    assert_equal "shipped", @invoice_r.all.first.status
+    assert_equal "pending", @invoice_r.all[1].status
+    assert_equal 125      , @invoice_r.all.last.id
+  end
+
+  def test_find_by_id_returning_nil_for_non_existing_ids
+    assert_nil @invoice_r.find_by_id(14331)
+    assert_nil @invoice_r.find_by_id(54322)
+  end
+
+  def test_find_by_id_working
+    save_one = @invoice_r.find_by_id(123)
+    save_two = @invoice_r.find_by_id(125)
+
+    assert_equal @invoice_r.invoices[1], save_one
+    assert_equal @invoice_r.invoices.last, save_two
+  end
+
+  def test_find_all_by_customer_id_returns_blank_if_none_match
+    assert_equal [], @invoice_r.find_all_by_customer_id(120)
+    assert_equal [], @invoice_r.find_all_by_customer_id(12032)
+  end
+
+  def test_find_all_by_customer_id_works_for_one_entry
+    list_one = @invoice_r.find_all_by_customer_id(3)
+    list_two = @invoice_r.find_all_by_customer_id(2)
+
+    assert_equal 1        , list_one.count
+    assert_equal "shipped", list_one.first.status
+    assert_equal 125      , list_one.first.id
+
+    assert_equal 1        , list_two.count
+    assert_equal "pending", list_two.first.status
+    assert_equal 123      , list_two.first.id
+  end
+
+  def test_find_all_by_customer_id_works_for_multiple_entries
+    list = @invoice_r.find_all_by_customer_id(1)
+
+    assert_equal 2        , list.count
+    assert_equal "shipped", list.first.status
+    assert_equal 7        , list.first.merchant_id
+    assert_equal "shipped", list.last.status
+    assert_equal 5        , list.last.merchant_id
+  end
+
+  def test_find_all_by_merchant_id_returns_blank_if_none_match
+    assert_equal [], @invoice_r.find_all_by_merchant_id(120)
+    assert_equal [], @invoice_r.find_all_by_merchant_id(12032)
+  end
+
+  def test_find_all_by_merchant_id_works_for_one_entry
+  end
+
 end
