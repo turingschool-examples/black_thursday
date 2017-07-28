eda25e492cb06ea4e520283ab1507f831b3e4dce
samuelssnider <sam_redins@yahoo.com>
2017-07-28 16:34:52 -0600

    invoice item done (for non crossover)

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,6 +1,20 @@
+require 'time'
+
 class InvoiceItem
-  attr_reader :id
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :updated_at,
+              :created_at
   def initialize(data, sales_engine)
-    @id = data[:id].to_i
+    @id         = data[:id]
+    @item_id    = data[:item_id]
+    @invoice_id = data[:invoice_id]
+    @quantity   = data[:quantity]
+    @unit_price = data[:unit_price]
+    @created_at   = Time.parse(data[:created_at]) if data[:created_at]
+    @updated_at   = Time.parse(data[:updated_at]) if data[:updated_at]
   end
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -13,7 +13,7 @@ class InvoiceItemTest < Minitest::Test
 
   def setup
     hash = {id: 1, item_id: 3, invoice_id: 4,
-            quantity: :Closed, unit_price: 1200,
+            quantity: 41, unit_price: 1200,
             created_at: "2012-11-23", updated_at: "2013-04-14"}
     @in_item = InvoiceItem.new(hash, 1)
   end
@@ -27,18 +27,23 @@ class InvoiceItemTest < Minitest::Test
   end
 
   def test_invoice_id_can_be_got
+    assert_equal 4, @in_item.invoice_id
   end
 
   def test_quantitity_can_be_got
+    assert_equal 41, @in_item.quantity
   end
 
   def test_unit_price_can_be_got
+    assert_equal 1200, @in_item.unit_price
   end
 
   def test_created_at_can_be_got
+    assert_equal Time.parse("2012-11-23"), @in_item.created_at
   end
 
   def test_updated_at_can_be_got
+    assert_equal Time.parse("2013-04-14"), @in_item.updated_at
   end
 
 end
