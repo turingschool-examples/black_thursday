23610d68724ab6f566ec13dc4a442a53057bcb43
snayrouz <samuel.nayrouz@gmail.com>
2017-07-27 18:35:50 -0600

    Pseudocode sales analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -5,11 +5,21 @@ class SalesAnalyst
   end
 
    def average_items_per_merchant
-     # How many products do merchants sell?
-     # Do most of our merchants offer just a few items or do they represent a warehouse?
+  #     it "#average_items_per_merchant returns average items per merchant" do
+  #     expected = sales_analyst.average_items_per_merchant
+  #
+  #     expect(expected).to eq 2.88
+  #     expect(expected.class).to eq Float
+  #     end
    end
 
    def average_items_per_merchant_standard_deviation
+     #  it "#average_items_per_merchant_standard_deviation returns the standard deviation" do
+     #   expected = sales_analyst.average_items_per_merchant_standard_deviation
+     #
+     #   expect(expected).to eq 3.26
+     #   expect(expected.class).to eq Float
+     # end
      # Note on Standard Deviations
      #
      # There are two ways for calculating standard deviations – for a population and for a sample.
@@ -34,22 +44,47 @@ class SalesAnalyst
     # Which merchants sell the most items?
     # Maybe we could set a good example for our lower sellers by displaying the merchants who have the most items for sale.
     # Which merchants are more than one standard deviation above the average number of products offered?
+    # it "#merchants_with_high_item_count returns merchants more than one standard deviation above the average number of products offered" do
+    #   expected = sales_analyst.merchants_with_high_item_count
+    #
+    #   expect(expected.length).to eq 52
+    #   expect(expected.first.class).to eq Merchant
+    # end
   end
 
   def average_item_price_for_merchant(merchant_id)
     # What are prices like on our platform?
     # Are these merchants selling commodity or luxury goods?
     # Let’s find the average price of a merchant’s items (by supplying the merchant ID):
+    # it "#average_item_price_for_merchant returns the average item price for the given merchant" do
+    #   merchant_id = 12334105
+    #   expected = sales_analyst.average_item_price_for_merchant(merchant_id)
+    #
+    #   expect(expected).to eq 16.66
+    #   expect(expected.class).to eq BigDecimal
+    # end
   end
 
   def average_average_price_per_merchant
     # Then we can sum all of the averages and find the average price across all merchants
-    #(this implies that each merchant’s average has equal weight in the calculation):
+    #(this implies that each merchant’s average has equal weight in the calculation)
+    # it "#average_average_price_per_merchant returns the average price for all merchants" do
+    #   expected = sales_analyst.average_average_price_per_merchant
+    #
+    #   expect(expected).to eq 350.29
+    #   expect(expected.class).to eq BigDecimal
+    # end
   end
 
   def golden_items
     # Given that our platform is going to charge merchants based on their sales, expensive items are extra exciting to us. Which are our “Golden Items”,
     # those two standard-deviations above the average item price? Return the item objects of these “Golden Items”.
+    # it "#golden_items returns items that are two standard deviations above the average price" do
+    #   expected = sales_analyst.golden_items
+    #
+    #   expect(expected.length).to eq 5
+    #   expect(expected.first.class).to eq Item
+    # end
   end
 
 end
