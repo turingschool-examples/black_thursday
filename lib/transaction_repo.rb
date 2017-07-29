98628ab06c42c9fae2fe1023e51b31155c40c2a9
snayrouz <samuel.nayrouz@gmail.com>
2017-07-29 17:58:01 -0600

    Pass transaction test

--- lib/transaction_repo.rb
+++ lib/transaction_repo.rb
@@ -1,12 +1,21 @@
 require_relative 'transaction'
-
+require 'csv'
+require 'pry'
 
 class TransactionRepository
   attr_reader :transactions, :engine
 
-  def initialize(csv_data, engine)
+  def initialize(csvfile, engine)
     @engine       = engine
-    @transactions = csv_data
+    @transactions = create_hash_of_transactions(csvfile)
+  end
+
+  def create_hash_of_transactions(csvfile)
+    all_transactions = {}
+    csvfile.each do |row|
+      all_transactions[row[:id]] = Transaction.new(row, self)
+    end
+    all_transactions
   end
 
   def inspect
@@ -14,23 +23,38 @@ class TransactionRepository
   end
 
   def all
-    transactions
+    @transactions.values
   end
 
   def find_by_id(id)
-    transactions.detect { |transaction| transaction.id == id }
+    @transactions[id.to_s]
   end
 
   def find_all_by_invoice_id(invoice_id)
-    transactions.select { |transaction| transaction.invoice_id == invoice_id } || []
+    array_of_matching_transactions = []
+    all.find_all do |transaction|
+      if transaction.invoice_id == invoice_id
+        array_of_matching_transactions << transaction
+      end
+    end
   end
 
   def find_all_by_credit_card_number(credit_card_number)
-    transactions.select { |transaction| transaction.credit_card_number == credit_card_number } || []
+    array_of_matching_ccs = []
+    all.find_all do |transaction|
+        if transaction.credit_card_number = credit_card_number
+          array_of_matching_ccs << transaction
+        end
+    end
   end
 
-  def find_all_by_result(status)
-    transactions.select { |transaction| transaction.status == status } || []
+  def find_all_by_result(result)
+    array_of_matching_results = []
+    all.find_all do |transaction|
+      if transaction.result == result.to_sym
+        array_of_matching_results << transaction
+      end
+    end
   end
 
 end
