24655bd140fc31a84e32f56cc8649fff43916af2
mimilettd <mimilettd@gmail.com>
2017-07-29 15:42:11 -0600

    test for empty arrays

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -23,14 +23,31 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_instance_of InvoiceItem, ir.find_by_id(1)
   end
 
+  def test_it_returns_nil_for_no_matching_id
+    ir = InvoiceItemRepository.new("./data/mini_invoice_items.csv")
+    assert_nil ir.find_by_id(5)
+  end
+
   def test_it_can_find_invoice_items_by_matching_item_id
     ir = InvoiceItemRepository.new("./data/mini_invoice_items.csv")
     assert_instance_of Array, ir.find_all_by_item_id(263454779)
   end
 
+  def test_it_returns_empty_array_for_no_matching_item_id
+    ir = InvoiceItemRepository.new("./data/mini_invoice_items.csv")
+    expected = []
+    assert_equal expected, ir.find_all_by_item_id(263454780)
+  end
+
   def test_it_can_find_invoice_items_by_matching_invoice_id
     ir = InvoiceItemRepository.new("./data/mini_invoice_items.csv")
     assert_instance_of Array, ir.find_all_by_invoice_id(1)
+  end
+
+  def test_it_returns_empty_array_for_no_matching_invoice_id
+    ir = InvoiceItemRepository.new("./data/mini_invoice_items.csv")
+    expected = []
+    assert_equal expected, ir.find_all_by_invoice_id(10)
   end
 
 end
