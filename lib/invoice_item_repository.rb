662b66e0e8acf961ce7560fdc1bc036fc22a718c
mimilettd <mimilettd@gmail.com>
2017-07-29 14:55:15 -0600

    create method to find all by item id

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -18,4 +18,14 @@ class InvoiceItemRepository
     repository.values
   end
 
+  def find_by_id(id)
+    repository[id]
+  end
+
+  def find_all_by_item_id(item_id)
+    all.find_all do |invoice_item|
+      invoice_item.item_id == item_id
+    end
+  end
+
 end
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -18,4 +18,14 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 2, ir.all.count
   end
 
+  def test_it_can_find_by_id
+    ir = InvoiceItemRepository.new("./data/mini_invoice_items.csv")
+    assert_instance_of InvoiceItem, ir.find_by_id(1)
+  end
+
+  def test_it_can_find_invoice_items_by_matching_item_id
+    ir = InvoiceItemRepository.new("./data/mini_invoice_items.csv")
+    assert_instance_of Array, ir.find_all_by_item_id(263454779)
+  end
+
 end
