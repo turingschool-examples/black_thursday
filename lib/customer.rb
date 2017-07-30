fee5bdd0676d2c94dc51df7c70c0e7e46c7c3c63
Mike Heft <mikeheft@gmail.com>
2017-07-30 16:03:29 -0600

    work on customer repo test up to first name find

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,9 @@
 class Customer
   attr_reader :id, :first_name, :last_name,
               :created_at, :updated_at
-              
+
   def initialize(params, repo=nil)
-    @id         = params[:id]
+    @id         = params[:id].to_i
     @first_name = params[:first_name]
     @last_name  = params[:last_name]
     @created_at = Time.parse(params[:created_at].to_s)
---
+++ lib/customer_repo.rb
@@ -0,0 +1,30 @@
+require 'csv'
+require_relative 'customer'
+
+class CustomerRepo
+  attr_reader :customers, :parent
+
+  def initialize(filename, se=nil)
+    @customers = {}
+    open_file(filename)
+    @parent    = se
+  end
+
+  def open_file(filename)
+    CSV.foreach filename, headers: true, header_converters: :symbol do |row|
+      customers[row[:id].to_i] = Customer.new(row, self)
+    end
+  end
+
+  def all
+    customers.values
+  end
+
+  def find_by_id(id)
+    customers[id]
+  end
+
+  def find_all_by_first_name(f_name)
+    all.find_all {|cust| cust.first_name.downcase == f_name.downcase}
+  end
+end
