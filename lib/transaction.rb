3ebf87fb3c6994c4ca829d840bc35c3bb9a23ab1
amhursh <amhursh@gmail.com>
2017-07-30 16:12:50 -0600

    Build id for transaction class and pass test

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,9 @@
 class Transaction
 
+  attr_reader :id
+
   def initialize(transaction_info, transaction_repo)
-    
+    @id = transaction_info[:id].to_i
   end
 
 end
--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -14,4 +14,16 @@ class TransactionTest < Minitest::Test
     assert_instance_of Transaction, transaction
   end
 
+  def test_transaction_has_id
+    transaction = Transaction.new({
+      :id => 23, :invoice_id => 2639, :credit_card_number => 4566763237167619,
+      :credit_card_expiration_date => "0220", :result => "success",
+      :created_at => "2012-02-26 20:56:57 UTC",
+      :updated_at => "2012-02-26 20:56:57 UTC"
+      }, self)
+    id = transaction.id
+
+    assert_equal 23, id 
+  end
+
 end
