378c3c75ec42e25c505c1b572261a16ac3573d1c
amhursh <amhursh@gmail.com>
2017-07-30 01:42:51 -0600

    Pass check if invoice_item has id test

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -14,4 +14,16 @@ class InvoiceItemTest < Minitest::Test
     assert_instance_of InvoiceItem, invoice_item
   end
 
+  def test_invoice_item_has_id
+    invoice_item = InvoiceItem.new({
+      :id => 15, :item_id => 263533242, :invoice_id => 3,
+      :quantity => 5, :unit_price => 72018,
+      :created_at => "2012-03-27 14:54:09 UTC",
+      :updated_at => "2012-03-27 14:54:09 UTC"}, self
+    )
+    id = invoice_item.id
+
+    assert_equal 15, invoice_item.id
+  end
+
 end
