1771333700cf92a6821e4480e8a8907636451ca3
amhursh <amhursh@gmail.com>
2017-07-30 23:42:45 -0600

    Refactor customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -9,9 +9,9 @@ class Customer
               :updated_at
 
   def initialize(customer_info, customer_repo)
-    @id = customer_info[:id].to_i
+    @id         = customer_info[:id].to_i
     @first_name = customer_info[:first_name]
-    @last_name = customer_info[:last_name]
+    @last_name  = customer_info[:last_name]
     @created_at = Time.parse(customer_info[:created_at])
     @updated_at = Time.parse(customer_info[:updated_at])
   end
