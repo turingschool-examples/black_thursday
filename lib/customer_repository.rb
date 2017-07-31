88c366a74db0efc8fa085e48fec03deeedc2d679
amhursh <amhursh@gmail.com>
2017-07-30 23:12:30 -0600

    Build out non-passing load_id_repo method for customer repo class. waiting on customer build to pass tests

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,11 +1,21 @@
 class CustomerRepository
 
   attr_reader :sales_engine,
-              :file_path
+              :file_path,
+              :id_repo
 
   def initialize(file_path, sales_engine)
-    @file_path = file_path
+    @file_path    = file_path
     @sales_engine = sales_engine
+    @id_repo      = {}
+  end
+
+  def load_repo
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      customer_identification = row[:id]
+      customer = Customer.new(row, self)
+      @id_repo[customer_identification.to_i] = customer
+    end
   end
 
 end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -19,10 +19,18 @@ class CustomerRepositoryTest < Minitest::Test
     assert_instance_of SalesEngine, cr.sales_engine
   end
 
-  def test_custoerm_repo_has_file_path
+  def test_customer_repo_has_file_path
     cr = CustomerRepository.new('./data/customers.csv', self)
 
     assert_equal './data/customers.csv', cr.file_path
+  end
+
+  def test_customer_repo_can_load_id_repo
+    cr = CustomerRepository.new('./data/customers.csv', self)
+
+    assert_instance_of Hash, cr.id_repo
+    refute cr.id_repo.empty?
+    assert_equal 1000, cr.id_repo.count
   end
 
 end
