cef118092bb4433dcd3517ae608f4cce56bc2b99
liambarstad <liambarstad@gmail.com>
2017-07-31 16:49:42 -0600

    customer analytics out of the way

--- lib/customer_analytics.rb
+++ lib/customer_analytics.rb
@@ -1,14 +1,14 @@
 module CustomerAnalytics
 
-  def top_buyers(num=20)
-    duplicated_merchant_repo = @merchants.id_repo.clone
-    top = []
-    number.times do
-      max = duplicated_merchant_repo.max_by {|merchant| revenue_by_merchant(merchant[0])}
-      duplicated_merchant_repo.delete(max[0])
-      top << @merchants.find_by_id(max[0])
-    end
-    top
+  # def top_buyers(num=20)
+  #   duplicated_merchant_repo = @merchants.id_repo.clone
+  #   top = []
+  #   number.times do
+  #     max = duplicated_merchant_repo.max_by {|merchant| revenue_by_merchant(merchant[0])}
+  #     duplicated_merchant_repo.delete(max[0])
+  #     top << @merchants.find_by_id(max[0])
+  #   end
+  #   top
 
 
 
