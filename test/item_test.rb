46232ef76b4fa8fe8fdda62d23606cccf83e45b5
Josi <junipertmcclellan@gmail.com>
2017-09-11 13:16:34 -0600

    use setup for item test

--- test/item_test.rb
+++ test/item_test.rb
@@ -3,124 +3,54 @@ require './test/test_helper'
 
 class ItemTest < Minitest::Test
 
+  attr_reader :time, :item
+  def setup
+    @time = Time.now
+    @item = Item.new({
+      :id          => "1",
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => time,
+      :updated_at  => time,
+      :merchant_id => "33"
+    })
+  end
+
   def test_instance_of_Item_exists
-    i = Item.new
-
-    assert_instance_of Item, i
+    assert_instance_of Item, item
   end
 
   def test_id_returns_the_integer_id_of_the_item
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal "1" , i.id
+    assert_equal "1" , item.id
   end
 
   def test_id_returns_the_integer_id_of_the_item
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal "1" , i.id
+    assert_equal "1" , item.id
   end
 
   def test_name_returns_the_name_of_the_item
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal "Pencil" , i.name
+    assert_equal "Pencil" , item.name
   end
 
   def test_decription_returns_the_description_of_the_item
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal "You can use it to write things" , i.description
+    assert_equal "You can use it to write things" , item.description
   end
 
-
   def test_unit_price_returns_the_price_of_the_item
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal  BigDecimal.new(10.99,4), i.unit_price
+    assert_equal BigDecimal.new(10.99,4), item.unit_price
   end
 
   def test_created_at_returns_a_Time_instance_for_the_date
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal Time.now, i.created_at
+    assert_equal time, item.created_at
   end
 
   def test_updated_at_returns_a_Time_instance_for_the_date_last_modified
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal Time.now, i.updated_at
+    assert_equal time, item.updated_at
   end
 
   def test_merchant_id_returns_the_integer_merchant
-    i = Item.new({
-    :id          => "1",
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal.new(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => "33"
-  })
-
-  assert_equal "33", i.merchant_id
+    assert_equal "33", item.merchant_id
   end
-
 
 end
