107ff4fc0fc4219aedb31800960282b49e27703a
NicholasMbogo <nicholasmbogo@yahoo.com>
2017-09-11 13:42:27 -0600

    updated repository

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -9,4 +9,9 @@ class ItemRepositoryTest < Minitest::Test
     csv_file_name = './data/items.csv'
     assert_instance_of ItemRepository, ItemRepository.new(csv_file_name)
   end
+
+  def test_it_creates_item_objects_for_each_row
+    csv_file_name = './data/items.csv'
+    repository = ItemRepository.new(csv_file_name)
+
 end
