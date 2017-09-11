8a1085af0358d8f16f2df7ac47b610be1eace5fb
Adrian Lara <adrianblara@gmail.com>
2017-09-11 17:45:47 -0600

    merchant repo is WIP, test created for how merchant list will be saved

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -4,11 +4,25 @@ require './lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
 
-  def test_it_exists
-    mr = MerchantRepository.new
+  attr_reader :mr
 
-    assert_instance_of MerchantRepository, mr
+  def setup
+    @mr = MerchantRepository.new
   end
 
+  def test_it_exists
+    assert_instance_of MerchantRepository, @mr
+  end
+
+  def test_merchant_list_returns_a_hash_of_ids_as_keys_and_names_as_values
+    actual = @mr.merchant_list
+    expected = { 12334105: "Shopin1901", 12334112: "Candisart" }
+
+    assert_equal expected, actual
+  end
+
+  def test_all_returns_array_of_all_known_merchant_instances
+    skip
+  end
 
 end
