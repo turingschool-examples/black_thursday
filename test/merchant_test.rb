4636c17f8de8f07df4406a218fc5cf2daadf2743
Josi <junipertmcclellan@gmail.com>
2017-09-11 13:03:09 -0600

    test merchant class

---
+++ test/merchant_test.rb
@@ -0,0 +1,24 @@
+require './test/test_helper'
+
+class MerchantTest < Minitest::Test
+
+  def test_initialize_takes_a_hash_of_fields
+    assert_instance_of Merchant, turing_school
+  end
+
+  def test_it_has_an_id
+    assert_equal 5, turing_school.id
+  end
+
+  def test_it_has_a_name
+    assert_equal "Turing School", turing_school.name
+  end
+
+  def turing_school
+    Merchant.new({
+      id: 5,
+      name: "Turing School"
+    })
+  end
+
+end
