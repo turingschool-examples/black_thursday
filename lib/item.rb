b388fb778763bc3742e124032bf513e9fd983536
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-11 11:46:05 -0600

    initialize and create methods for hash passed into Item

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,17 @@
 class Item
 
+  attr_reader :name,
+              :description,
+              :unit_price,
+              :created_at,
+              :updated_at
+
+  def initialize(item_hash)
+    @name = item_hash[:name]
+    @description = item_hash[:description]
+    @unit_price = item_hash[:unit_price]
+    @created_at = item_hash[:created_at]
+    @updated_at = item_hash[:updated_at]
+  end
+
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -6,11 +6,38 @@ class ItemTest < Minitest::Test
   attr_reader :item
 
   def setup
-    @item = Item.new
+    item_hash = {
+                    :name        => "Pencil",
+                    :description => "You can use it to write things",
+                    :unit_price  => 10.99,
+                    :created_at  => 11,
+                    :updated_at  => 12,
+                  }
+    @item = Item.new(item_hash)
   end
 
   def test_it_exists
     assert_instance_of Item, item
+  end
+
+  def test_item_has_a_name
+    assert_equal "Pencil", item.name
+  end
+
+  def test_item_has_a_description
+    assert_equal "You can use it to write things", item.description
+  end
+
+  def test_item_has_a_unit_price
+    assert_equal 10.99, item.unit_price
+  end
+
+  def test_item_has_created_at_time
+    assert_equal 11, item.created_at
+  end
+
+  def test_item_has_updated_at_time
+    assert_equal 12, item.updated_at
   end
 
 end
