f778dbb7b82958753ed374b26b8f1f5d9e5cca38
Hugh Morrison <hmorri32@gmail.com>
2017-09-12 12:35:36 -0600

    sales engine setup

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,4 +1,17 @@
+require_relative './merchant_repository'
+require_relative './item_repository'
+
 class SalesEngine 
 
-end
+  def self.from_csv(file_path)
 
+  end
+
+  def self.merchants 
+    
+  end
+
+  def self.items 
+
+  end
+end
