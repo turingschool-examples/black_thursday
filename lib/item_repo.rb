290182859b2dfec9da03cbdad0adbca87387903c
Erika Smith <erikaannesmith@gmail.com>
2017-09-12 15:49:58 -0600

    Build additional methods to add to search capabilities

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -27,6 +27,30 @@ class ItemRepo
   end
 
   def find_by_name(name)
+    items.find { |item| item.name == name }
+  end
 
+  def find_all_with_description(description)
+    item_match_by_description = []
+    item_match_by_description << items.find_all { |item| item.description.include?(/#{decription}/i) }
+    item_match_by_description
+  end
+
+  def find_all_by_price(price)
+    item_match_by_price = []
+    item_match_by_price << items.find_all { |item| item.price == price }
+    item_match_by_price
+  end
+
+  def find_all_by_price_in_range(range)
+    item_match_by_price_in_range = []
+    item_match_by_price_in_range << items.find_all { |item| range.include?(item.price) }
+    item_match_by_price_in_range
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    item_match_by_merchant_id = []
+    item_match_by_merchant_id << items.find_all { |item| item.merchant.id == merchant_id }
+    item_match_by_merchant_id
   end
 end
