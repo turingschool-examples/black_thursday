8850f63a65f151f149a97aedbf8f13ffe9aa7438
Erika Smith <erikaannesmith@gmail.com>
2017-09-12 17:01:08 -0600

    Add tests for searching methods

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -11,10 +11,42 @@ class MerchantRepoTest < MiniTest::Test
     assert_instance_of MerchantRepo, mr
   end
 
-  def test_it_does_smtg
-    mr = MerchantRepo.new("./data/merchants.csv")
+  def test_all_returns_all_merchants
+    merchants = MerchantRepo.new("./data/merchants.csv")
 
-    binding.pry
+    assert_equal 475, merchants.all.count
   end
 
+  def test_find_by_id_returns_matching_id
+    merchants = MerchantRepo.new("./data/merchants.csv")
+
+    assert_equal "ElisabettaComotto", merchants.find_by_id("12334228").name
+    assert_nil merchants.find_by_id("777777")
+  end
+
+  def test_find_by_name_returns_matching_name
+    merchants = MerchantRepo.new("./data/merchants.csv")
+
+    assert_equal "12334228", merchants.find_by_name("ElisabettaComotto").id
+    assert_equal "12334228", merchants.find_by_name("ELISABETTACOMOTTO").id
+  end
+
+  def test_find_by_returns_nil_if_no_match
+    merchants = MerchantRepo.new("./data/merchants.csv")
+
+    assert_nil merchants.find_by_name("xxxxxxxxxx")
+  end
+
+  def test_find_all_by_name_returns_all_with_match
+    merchants = MerchantRepo.new("./data/merchants.csv")
+
+    assert_equal 26, merchants.find_all_by_name("shop").count
+    assert_equal 26, merchants.find_all_by_name("SHOP").count
+  end
+
+  def test_find_all_by_name_returns_empty_array_if_no_match
+    merchants = MerchantRepo.new("./data/merchants.csv")
+
+    assert_equal [], merchants.find_all_by_name("xxxxxxxxxx")
+  end
 end
