13ed7441aa4c383dfe5575fe0d1c871c9d1726f0
marvin.bonds <marvinbonds@yahoo.com>
2017-09-11 23:01:33 -0600

    Zero iteration, need lots more tests. error line 38

---
+++ lib/sales_engine.rb
@@ -0,0 +1,24 @@
+require './lib/items_repo'
+require './lib/merchant_repo'
+
+
+class SalesEngine
+  attr_reader :merchants, :items
+
+  def initialize(data)
+    @merchants     = MerchantRepo.new(data[:merchants], self)
+    @items         = ItemRepo.new(data[:items], self)
+  end
+
+  def self.from_csv(data)
+    se = SalesEngine.new(data)
+  end
+
+  def items_of_merchant(id)
+    items.find_all_by_merchant_id(id)
+  end
+
+  def merchant_item(id)
+    merchants.find_by_id(id)
+  end
+end
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+  attr_reader :se
+
+  def setup
+    @se = SalesEngine.from_csv({items: "./data/items.csv",
+                            merchants: "./data/merchants.csv"})
+  end
+
+  def test_it_exists
+    assert_instance_of SalesEngine, se
+  end
+
+  def test_engine_can_find_merchant_by_name
+    mr = se.merchants
+    merchant = mr.find_by_name("MiniatureBikez")
+    assert_instance_of Merchant, merchant
+    assert_equal "MiniatureBikez", merchant.name
+  end
+
+  def test_engine_can_find_item_by_name
+    ir   = se.items
+    item = ir.find_by_name("Glitter scrabble frames")
+    assert_instance_of Item, item
+    assert_equal "Glitter scrabble frames", item.name
+  end
+
+  def test_it_can_find_all_items_of_particular_merchant
+    merchant = se.merchants.find_by_id(12334185)
+    assert_equal 6, merchant.items.count
+  end
+
+  def test_it_can_find_merchant_by_item_id
+    item = se.items.find_by_id(263395237)
+    assert_instance_of Merchant, item.merchant
+  end
+end
