71c3e14d564c75c999a75e7147435ef1c38fe928
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-12 10:48:15 -0600

    change make_items to class method load_csv

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,16 +2,16 @@ require 'csv'
 
 class ItemRepository
 
-  def initialize(item_csv)
-    @item_csv = item_csv
-  end
-
-  def make_items
+  def self.load_csv(item_csv)
     item_array = []
     CSV.foreach(@item_csv, headers: true, header_converters: :symbol) do |line|
       item_array << Item.new(line)
     end
     item_array
+  end
+
+  def initialize(item_csv)
+    @item_csv = item_csv
   end
 
 end
