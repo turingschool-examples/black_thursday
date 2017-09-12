5fa75c9c59274bb807aeefddbb83b9d8b655e6c6
Erika Smith <erikaannesmith@gmail.com>
2017-09-12 17:00:46 -0600

    Modify test to reflect item repo intiialization

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,23 +1,26 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/sales_engine'
-
+require 'pry'
 class SalesEngineTest < MiniTest::Test
 
   def test_it_exists
-    se = SalesEngine.new(merchants:"./data/merchants.csv")
+    se = SalesEngine.new({merchants:"./data/merchants.csv",
+    items:"./data/items.csv"})
 
     assert_instance_of SalesEngine, se
   end
 
   def test_merchants_method
-    se = SalesEngine.new(merchants:"./data/merchants.csv")
+    se = SalesEngine.new({merchants:"./data/merchants.csv",
+    items:"./data/items.csv"})
 
     assert_instance_of MerchantRepo, se.merchants
   end
 
   def test_items_method
-    se = SalesEngine.new(merchants:"./data/merchants.csv")
+    se = SalesEngine.new({merchants:"./data/merchants.csv",
+    items:"./data/items.csv"})
 
     assert_instance_of ItemRepo, se.items
   end
