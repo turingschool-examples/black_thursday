02ab5b6148f2e7984554d2a3ca322c090f103e29
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-12 08:27:27 -0600

    add merchant_id and id to item class

--- lib/item.rb
+++ lib/item.rb
@@ -2,18 +2,22 @@ require 'bigdecimal'
 
 class Item
 
-  attr_reader :name,
+  attr_reader :id,
+              :name,
               :description,
               :unit_price,
               :created_at,
-              :updated_at
+              :updated_at,
+              :merchant_id
 
   def initialize(item_hash)
+    @id = item_hash[:id]
     @name = item_hash[:name]
     @description = item_hash[:description]
     @unit_price = item_hash[:unit_price]
     @created_at = item_hash[:created_at]
     @updated_at = item_hash[:updated_at]
+    @merchant_id = item_hash[:merchant_id]
   end
 
   def unit_price_to_dollars(unit_price)
--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,7 +1,13 @@
+require 'csv'
+
 class ItemRepository
 
   def initialize(item_csv)
     @item_csv = item_csv
+  end
+
+  def from_csv
+    
   end
 
 end
