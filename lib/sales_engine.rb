02135c0841cb6dc16335b9bbcd8c5bed2aa451a4
Hugh Morrison <hmorri32@gmail.com>
2017-09-12 12:52:02 -0600

    init sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,14 +4,18 @@ require_relative './item_repository'
 class SalesEngine 
 
   def self.from_csv(file_path)
-
+    @items     = file_path[:items]
+    @merchants = file_path[:merchants]
+    self.merchants
+    self.items
+    self
   end
 
   def self.merchants 
-    
+    MerchantRepository.new(@merchants, self)
   end
 
-  def self.items 
-
+  def self.items
+    ItemRepository.new(@items, self)
   end
 end
