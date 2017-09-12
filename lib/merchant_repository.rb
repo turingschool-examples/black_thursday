c950291dcb62020216f348fca871802e6f3fff15
Marshall Houston <marshall.houston@gmail.com>
2017-09-11 20:55:51 -0600

    add merch_repo tests and merch_repo methods

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -11,4 +11,22 @@ attr_accessor :merchants
     merchant_parse_data(file_name)
   end
 
+  def all
+    @merchants
+  end
+
+  def find_by_id(id)
+    @merchants.find {|merchant| merchant.id == id }
+  end
+
+  def find_by_name(name)
+    @merchants.find { |merchant| merchant.name.downcase == name.downcase }
+  end
+
+  def find_all_by_name(name)
+    @merchants.select do |merchant|
+      merchant.name.downcase.include?(name.downcase)
+    end
+  end
+
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -28,4 +28,51 @@ class MerchantRepositoryTest < Minitest::Test
 
     assert_equal "Shopin1901", mr.merchants[0].name
   end
+
+  #all - returns an array of all known Merchant instances
+  def test_all_returns_all_the_merchants
+    mr = MerchantRepository.new('./data/merchants_fixture.csv')
+
+    assert_equal 19, mr.all.count
+  end
+
+  # find_by_id - returns either nil or an instance of Merchant with a matching ID
+  def test_it_can_find_a_merchant_using_id
+    mr = MerchantRepository.new('./data/merchants_fixture.csv')
+    id_1 = "12334174"
+    id_2 = "1"
+    id_3 = 1
+
+    assert mr.find_by_id(id_1)
+    assert_nil mr.find_by_id(id_2)
+    assert_nil mr.find_by_id(id_3)
+    #feel like these tests need to be more robust.... but I'm not sure ho
+  end
+
+  # find_by_name - returns either nil or an instance of Merchant having done a case insensitive search
+  def test_it_can_find_a_merchant_using_name
+    mr = MerchantRepository.new('./data/merchants_fixture.csv')
+    name_1 = "GoldenRayPress"
+    name_2 = "goldenRaypress"
+    name_3 = ""
+    name_4 = "hi"
+
+    assert mr.find_by_name(name_1)
+    assert mr.find_by_name(name_2)
+    assert_nil mr.find_by_name(name_3)
+    assert_nil mr.find_by_name(name_4)
+  end
+
+  # find_all_by_name - returns either [] or one or more matches which contain the supplied name fragment, case insensitive
+  def test_it_can_find_all_merchants_with_a_given_name
+    mr = MerchantRepository.new('./data/merchants_fixture.csv')
+    name_1 = "in"
+    name_2 = "Turing School of Software"
+    name_3 = "Mo"
+
+    assert_equal 4, mr.find_all_by_name(name_1).length
+    assert_equal [], mr.find_all_by_name(name_2)
+    assert_equal 3, mr.find_all_by_name(name_3).length
+  end
+
 end
