4a73f41b26c62b62a25befdee83b8d0a8b332f7e
BLaurenB <blbillington1@gmail.com>
2017-09-12 12:48:27 -0600

    merchant done, MR started

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,30 +1,15 @@
-#Lauren doing this one!
-
-=begin
-Merchant
-
-The merchant is one of the critical concepts in our data hierarchy.
-
-id - returns the integer id of the merchant
-name - returns the name of the merchant
-We create an instance like this:
-
-m = Merchant.new({:id => 5, :name => "Turing School"})
-=end
-
 class Merchant
+
+  def self.load_from_csv(csv)
+    id = csv[:id]
+    name = csv[:name]
+  end
 
   attr_reader :id, :name
 
-  def initialize(hash)
-    @merchant_hash = hash
-    @id = hash[:id]
-    @name = hash[:name]
-  end
-
-
-  def id
-    @id.to_i
+  def initialize(id, name)
+      @id = id
+    @name = name
   end
 
 end
--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -5,22 +5,29 @@ The MerchantRepository is responsible for holding and searching our Merchant ins
 
 all - returns an array of all known Merchant instances
 find_by_id - returns either nil or an instance of Merchant with a matching ID
+  -- for each instance, check the id. use .find?
 find_by_name - returns either nil or an instance of Merchant having done a case insensitive search
 find_all_by_name - returns either [] or one or more matches which contain the supplied name fragment, case insensitive
 The data can be found in data/merchants.csv so the instance is created and used like this:
 =end
+require "csv"
+
 
 class MerchantRepository
 
-  def initialize(merchant_repository)
-    all = []
+  attr_reader :all
+
+  def initialize(FILENAME)
+    @all = []
+
+    CSV.foreach('./FILENAME.csv', headers: true, header_converters: :symbol) do |row|
+      @all << Merchant.load_from_csv(row)
+    end
+
   end
 
-
-  def all
-    [] << Merchant
+  def find_by_id
   end
-
 
 
 
