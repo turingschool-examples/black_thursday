615776023203688a2e80d400fe44fc8d1543b71e
Aaron Schreck <schreck.aaron@gmail.com>
2017-09-12 12:10:11 -0600

    Add sales analyst class and empty methods

---
+++ lib/sales_analyst.rb
@@ -0,0 +1,32 @@
+class SalesAnalyst
+
+    def initialize (se)
+        @se = se
+    end 
+
+    def average_items_per_merchant
+
+    end 
+
+    def average_items_per_merchant_standard_deviation
+    end 
+
+    def merchants_with_high_item_count
+
+    end 
+
+    def merchants_with_high_item_count
+    end 
+
+    def average_item_price_for_merchant(id)
+
+    end 
+
+    def average_price_per_merchant
+
+    end 
+
+    def golden_items
+
+    end 
+end 
