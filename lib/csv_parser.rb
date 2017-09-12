3f24c32e3937933e8f80fb081d7390689ebc551a
Marshall Houston <marshall.houston@gmail.com>
2017-09-12 06:40:52 -0600

    move item instantiate to item_repo, refactors csv_parser

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -6,17 +6,4 @@ module CsvParser
     CSV.open file_name, headers: true, header_converters: :symbol
   end
 
-  def item_parse_data(file_name)
-    contents = CSV.open file_name, headers: true, header_converters: :symbol
-
-    contents.each do |row|
-      @items << Item.new(
-        {name: row[:name],
-        description: row[:description],
-        unit_price: row[:unit_price],
-        created_at: row[:created_at],
-        updated_at: row[:updated_at]})
-    end
-  end
-
 end
--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -8,33 +8,41 @@ attr_accessor :items
 
     def initialize(file_name)
         @items = []
-        item_parse_data(file_name)
+        item_contents = parse_data(file_name)
+        item_contents.each do |row|
+          @items << Item.new(
+            {name: row[:name],
+            description: row[:description],
+            unit_price: row[:unit_price],
+            created_at: row[:created_at],
+            updated_at: row[:updated_at]})
+        end
     end
 
     def all
         @items
-    end 
+    end
 
     def find_by_id
 
-    end 
+    end
 
     def find_by_name(name)
         @items.select {|item| item.name == name}
-    end 
+    end
 
     def find_all_with_description(description)
         @items.select {|item| item.description == description}
-    end 
+    end
 
     def find_all_by_price
          @items.select {|item| item.unit_price == unit_price}
-    end 
+    end
 
     def find_all_by_price_in_range(lower, higher)
         @items.select {|item| lower.to_i < item.unit_price.to_i && item.unit_price.to_i < higher.to_i }
-    end 
+    end
 
     def find_all_by_merchant_id
-    end 
+    end
 end
