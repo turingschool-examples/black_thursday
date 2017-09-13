a0017bf94c4dfb7d3ed8f7c7e711baa9c04b3301
Hugh Morrison <hmorri32@gmail.com>
2017-09-13 14:12:53 -0600

    to_sym on @status

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -11,7 +11,7 @@ class Invoice
     @id          = invoice[:id].to_i
     @customer_id = invoice[:customer_id].to_i
     @merchant_id = invoice[:merchant_id].to_i
-    @status      = invoice[:status]
+    @status      = invoice[:status].to_sym
     @created_at  = Time.parse(invoice[:created_at].to_s)
     @updated_at  = Time.parse(invoice[:updated_at].to_s)
     @parent      = nil
