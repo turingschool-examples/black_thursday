d8ed30dc6f54fe5ff9caf0c58c29df99681c875a
Erika Smith <erikaannesmith@gmail.com>
2017-09-12 18:20:41 -0600

    Modify initalize method to account for created at and updated at methods

--- lib/item.rb
+++ lib/item.rb
@@ -1,19 +1,30 @@
 require 'bigdecimal'
+require 'time'
+require 'pry'
 
 class Item
-  attr_reader :id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :merchant_id,
+              :created_at,
+              :updated_at,
+              :price
 
   def initialize(item)
     @id = item[:id]
     @name = item[:name]
     @description = item[:description]
-    @unit_price = item[:unit_price]
+    @price = BigDecimal.new(item[:unit_price])
+    @unit_price = unit_price_to_dollars(@price)
     @merchant_id = item[:merchant_id]
-    @created_at = item[:created_at]
+    @created_at = Time.parse(item[:created_at].to_s)
+    @updated_at = Time.parse(item[:updated_at].to_s)
   end
 
   def unit_price_to_dollars(unit_price)
-    @unit_price/100
+    unit_price / 100
   end
 
 end
