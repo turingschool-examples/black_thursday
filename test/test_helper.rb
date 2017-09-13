84a40728b8409f23f8513d007bf64ea3c7c8a71e
Josi <junipertmcclellan@gmail.com>
2017-09-13 16:09:05 -0600

    move fixute methods to own file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 # require 'simplecov'
-# SimpleCov.add_filter(/_test.rb$/)
+# SimpleCov.add_filter(/\/test\//)
 # SimpleCov.start
 
 require 'minitest/autorun'
 require 'minitest/emoji'
+
+require_relative 'fixture'
