b9085c9173aeab01e0e08d76278afd106d5c56fc
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-13 12:51:16 -0600

    continue adjusting to comply with spec harness

--- lib/item.rb
+++ lib/item.rb
@@ -3,10 +3,10 @@ require 'bigdecimal'
 class Item
 
   def self.load_csv(items)
-    id = items[:id].to_s.to_i
+    id = items[:id].to_i
     name = items[:name]
     description = items[:description]
-    unit_price = items[:unit_price].to_s.to_i
+    unit_price = items[:unit_price]
     created_at = items[:created_at]
     updated_at = items[:updated_at]
     merchant_id = items[:merchant_id]
@@ -37,14 +37,15 @@ class Item
     @id = id
     @name = name
     @description = description
-    @unit_price = unit_price
+    @unit_price = unit_price_to_dollars(unit_price)
     @created_at = created_at
     @updated_at = updated_at
     @merchant_id = merchant_id
   end
 
   def unit_price_to_dollars(unit_price)
-    unit_price.to_f
+    dollars = unit_price.to_f / 100
+    BigDecimal.new(dollars, 4)
   end
 
 end
