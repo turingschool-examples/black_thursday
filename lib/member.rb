10f67849a2b3bb65061de657635035a6d03b7b25
Josi <junipertmcclellan@gmail.com>
2017-09-12 21:38:01 -0600

    have member apply defaults

--- lib/member.rb
+++ lib/member.rb
@@ -1,8 +1,15 @@
 class Member
 
   attr_reader :id, :repo
-  def initialize(repo)
+  def initialize(repo, fields)
+    apply_defaults(fields)
     @repo = repo
+  end
+
+  def apply_defaults(fields)
+    fields[:id]         ||= repo.unused_id
+    fields[:created_at] ||= Time.now
+    fields[:updated_at] ||= Time.now
   end
 
   def ==(other)
