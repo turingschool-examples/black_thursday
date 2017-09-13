106961279b9ff2357b0ebd843026755e528966b8
Erika Smith <erikaannesmith@gmail.com>
2017-09-12 18:27:42 -0600

    Add tests to check for price conversion and time for updated at and created at methods

--- test/item_test.rb
+++ test/item_test.rb
@@ -10,7 +10,7 @@ class ItemTest < MiniTest::Test
     item = Item.new({
       :name        => "Pencil",
       :description => "You can use it to write things",
-      :unit_price  => "1000",
+      :unit_price  => 1000,
       :created_at  => Time.parse("2016-12-30 10:12:12 -0600"),
       :updated_at  => Time.parse("2016-12-30 10:12:12 -0600"),
       })
@@ -22,10 +22,36 @@ class ItemTest < MiniTest::Test
     item = Item.new({
       :name        => "Pencil",
       :description => "You can use it to write things",
-      :unit_price  => 10,
-      :created_at  => 10,
-      :updated_at  => 10,
+      :unit_price  => 1000,
+      :created_at  => Time.parse("2016-12-30 10:12:12 -0600"),
+      :updated_at  => Time.parse("2016-12-30 10:12:12 -0600"),
       })
     assert_equal "Pencil", item.name
+  end
+
+  def test_unit_price_to_dollars
+    item = Item.new({
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => 1000,
+      :created_at  => Time.parse("2016-12-30 10:12:12 -0600"),
+      :updated_at  => Time.parse("2016-12-30 10:12:12 -0600"),
+      })
+
+      assert_equal 10.00, item.unit_price_to_dollars(1000)
+  end
+
+  def test_time_parse_methods_work
+    item = Item.new({
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => 1000,
+      :created_at  => Time.parse("2016-12-30 10:12:12 -0600"),
+      :updated_at  => Time.parse("2016-12-30 10:12:12 -0600"),
+      })
+      expected = Time.parse("2016-12-30 10:12:12 -0600")
+
+      assert_equal expected, item.created_at
+      assert_equal expected, item.updated_at
   end
 end
