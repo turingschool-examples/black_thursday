526c2b50bc8dafb0df443141410572853c6dcb1a
katyjane8 <katy.jane8@gmail.com>
2017-09-13 16:54:13 -0600

    merchant repo test all working

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -1,20 +1,24 @@
-require_relative 'test/test_helper'
-require_relative 'lib/merchant_repo'
-require_relative 'lib/merchant'
+require_relative 'test_helper'
+require_relative '../lib/merchant_repo'
+require_relative '../lib/merchant'
 
 class MerchantRepositoryTest < Minitest::Test
   def test_item_exists
-    merch = MerchantRepository.new
+    merchant_list = []
+    merch = MerchantRepository.new(merchant_list)
 
     assert_instance_of MerchantRepository, merch
   end
 
   def test_all_merchants
-    merch = MerchantRepository.new([{:id=>"12334105", :name=>"Shopin1901"},
- {:id=>"12334112", :name=>"Candisart"},
- {:id=>"12334113", :name=>"MiniatureBikez"},
- {:id=>"12334115", :name=>"LolaMarleys"},
- {:id=>"12334123", :name=>"Keckenbauer"}])
+    merchant_list = []
+    merchant_list << merch1 = Merchant.new({:id=>"12334105", :name=>"Shopin1901"})
+    merchant_list << merch2 = Merchant.new({:id=>"12334112", :name=>"Candisart"})
+    merchant_list << merch3 = Merchant.new({:id=>"12334113", :name=>"MiniatureBikez"})
+    merchant_list << merch4 = Merchant.new({:id=>"12334115", :name=>"LolaMarleys"})
+    merchant_list << merch5 = Merchant.new({:id=>"12334123", :name=>"Keckenbauer"})
+
+    merch = MerchantRepository.new(merchant_list)
 
     assert_equal 5, merch.all.count
   end
