91bb5bf841080d24e5ff53a6c4f8c600b0e5b7fd
Erika Smith <erikaannesmith@gmail.com>
2017-09-13 16:21:07 -0600

    Add merchant items and item merchant methods to access data points between each

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,23 +1,33 @@
 require_relative 'merchant_repo'
 require_relative 'item_repo'
 require 'pry'
+
 class SalesEngine
+  attr_reader :merchant_repo, :item_repo
 
   def self.from_csv(data_hash)
     new(data_hash)
   end
 
   def initialize(data_hash)
-    @merchant_repo = MerchantRepo.new(data_hash[:merchants])
-    @item_repo = ItemRepo.new(data_hash[:items])
+    @merchant_repo = MerchantRepo.new(data_hash[:merchants],self)
+    @item_repo = ItemRepo.new(data_hash[:items],self)
   end
 
   def merchants
-    @merchant_repo
+    merchant_repo
   end
 
   def items
-    @item_repo
+    item_repo
+  end
+
+  def merchant_items(id)
+    item_repo.find_all_by_merchant_id(id)
+  end
+
+  def item_merchant(id)
+    merchant_repo.find_by_id(id)
   end
 
 end
