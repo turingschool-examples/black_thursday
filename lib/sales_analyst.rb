8beb066b2c299d23387884e03d686b06109d9274
Aurora Ziobrowski <aziobrow@gmail.com>
2017-09-13 21:32:58 -0600

    started sales analyst

---
+++ lib/sales_analyst.rb
@@ -0,0 +1,35 @@
+require_relative 'sales_engine'
+
+class SalesAnalyst
+
+  attr_reader :engine
+
+  def initialize(engine)
+    @engine = engine
+  end
+
+  def total_item_count_per_merchant(merchant_repo)
+    merchants = merchant_repo.merchants
+    merchants.reduce(0) do |item_count, merchant|
+      item_count + merchant.items.count
+    end
+  end
+
+  def average_items_per_merchant
+    #is this just items to merchants?
+    #or is it only items specifically associated with merchants?
+    #is it 0 or nil if merchant has no items?
+    merchant_repo = @engine.merchants
+    merchant_count = merchant_repo.merchants.count
+    total_items = total_item_count_per_merchant(merchant_repo)
+    total_items.to_f / merchant_count.to_f
+  end
+  #
+  # def average_items_per_merchant_standard_deviation
+  #   mean = average_items_per_merchant
+  #   merchant_repo = @engine.merchants
+  #   merchant_item_count = merchant_repo.merchants.
+  # end
+
+
+end
---
+++ test/sales_analyst_test.rb
@@ -0,0 +1,32 @@
+require_relative 'test_helper'
+require './lib/sales_analyst'
+
+class SalesAnalystTest < Minitest::Test
+
+  def setup
+    item_file_path = './test/fixtures/items_truncated.csv'
+    merchant_file_path = './test/fixtures/merchants_truncated.csv'
+    @engine = SalesEngine.new(item_file_path, merchant_file_path)
+    @analyst = SalesAnalyst.new(@engine)
+  end
+
+  def test_it_exists
+    assert_instance_of SalesAnalyst, @analyst
+    assert_instance_of SalesEngine, @engine
+  end
+
+  def test_item_count_returns_total_number_of_items
+    repo = @engine.merchants
+
+    assert_equal 3, @analyst.total_item_count_per_merchant(repo)
+  end
+
+  def test_average_items_per_merchant_returns_decimal_mean
+    assert_equal 0.75, @analyst.average_items_per_merchant
+  end
+
+  # def test_average_items_per_merchant_standard_deviation_returns_deviation_from_mean
+  # end
+
+
+end
