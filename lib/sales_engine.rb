c42698e759bdb60dcc663427f0b22d69dbc6311e
seoulstice <jungyoungjoon@gmail.com>
2017-09-13 20:01:37 -0600

    item method is a fail

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,4 +1,5 @@
 require './lib/item_repository'
+require './lib/merchant'
 require './lib/merchant_repository'
 
 class SalesEngine
@@ -7,12 +8,16 @@ class SalesEngine
     SalesEngine.new(source_files)
   end
 
-  attr_reader :items,
-              :merchants
+  attr_reader :item_repository,
+              :merchant_repository
 
   def initialize(source_files)
-    @items = ItemRepository.new(source_files[:items])
-    @merchants = MerchantRepository.new(source_files[:merchants])
+    @item_repository = ItemRepository.new(source_files[:items])
+    @merchant_repository = MerchantRepository.new(source_files[:merchants])
+  end
+
+  def items
+    item_repository.find_all_by_merchant_id(self.id)
   end
 
 end
