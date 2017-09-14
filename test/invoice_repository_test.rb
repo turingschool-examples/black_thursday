fcdb3f75acb103bd0a7964f9e91d180e38a3ce18
Aaron Schreck <schreck.aaron@gmail.com>
2017-09-14 14:55:11 -0600

    add invoice repository functionality

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -3,6 +3,25 @@ require_relative  '../lib/invoice_repository'
 
 class InvoiceRepositoryTest < Minitest::Test
 
-  def 
+  def setup
+    se = SalesEngine.from_csv({
+      :items     => "./test/fixtures/items_fixture.csv",
+      :merchants => "./test/fixtures/merchants_fixture.csv",
+      :invoices => "./test/fixtures/invoice_fixture.csv"
+    })
+    vr = se.invoices
+  end
+
+  def test_invoice_repo_returns_array_of_all_invoices
+    vr = setup
+
+    assert_equal ,vr.all
+  end 
+
+  def test_it_finds_invoice_by_id
+    vr = setup
+
+    assert_equal ,vr.find_by_id()
+  end 
 
 end 
