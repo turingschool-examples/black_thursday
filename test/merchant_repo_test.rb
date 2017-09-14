9566fa0d7de0b125dccabf0c1641860ab9fddbee
katyjane8 <katy.jane8@gmail.com>
2017-09-13 18:37:34 -0600

    merchant repo test finds all

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -49,9 +49,71 @@ class MerchantRepositoryTest < Minitest::Test
     assert_instance_of Merchant, merch.find_by_id(12334115)
   end
 
-  def test_find_by_name
+  def test_find_by_name_nil
+    merchant_list = []
+    merchant_list << merch1 = Merchant.new({:id=>"12334105", :name=>"Shopin1901"})
+    merchant_list << merch2 = Merchant.new({:id=>"12334112", :name=>"Candisart"})
+    merchant_list << merch3 = Merchant.new({:id=>"12334113", :name=>"MiniatureBikez"})
+    merchant_list << merch4 = Merchant.new({:id=>"12334115", :name=>"LolaMarleys"})
+    merchant_list << merch5 = Merchant.new({:id=>"12334123", :name=>"Keckenbauer"})
+
+    merch = MerchantRepository.new(merchant_list)
+
+    assert_nil merch.find_by_name("Gerald")
+  end
+
+  def test_find_specific_name
+    merchant_list = []
+    merchant_list << merch1 = Merchant.new({:id=>"12334105", :name=>"Shopin1901"})
+    merchant_list << merch2 = Merchant.new({:id=>"12334112", :name=>"Candisart"})
+    merchant_list << merch3 = Merchant.new({:id=>"12334113", :name=>"MiniatureBikez"})
+    merchant_list << merch4 = Merchant.new({:id=>"12334115", :name=>"LolaMarleys"})
+    merchant_list << merch5 = Merchant.new({:id=>"12334123", :name=>"Keckenbauer"})
+
+    merch = MerchantRepository.new(merchant_list)
+    name = "MiniatureBikez".upcase
+    assert_instance_of Merchant, merch.find_by_name(name)
   end
 
   def test_find_all_by_name
+    merchant_list = []
+    merchant_list << merch1 = Merchant.new({:id=>"12334105", :name=>"Shopin1901"})
+    merchant_list << merch2 = Merchant.new({:id=>"12334112", :name=>"Candisart"})
+    merchant_list << merch3 = Merchant.new({:id=>"12334113", :name=>"MiniatureBikez"})
+    merchant_list << merch4 = Merchant.new({:id=>"12334115", :name=>"LolaMarleys"})
+    merchant_list << merch5 = Merchant.new({:id=>"12334123", :name=>"Keckenbauer"})
+
+    merch = MerchantRepository.new(merchant_list)
+    fragment = "Goboababab"
+
+    assert_equal [], merch.find_all_by_name(fragment)
+  end
+
+  def test_find_all_by_name_specific_name
+    merchant_list = []
+    merchant_list << merch1 = Merchant.new({:id=>"12334105", :name=>"Shopin1901"})
+    merchant_list << merch2 = Merchant.new({:id=>"12334112", :name=>"Candisart"})
+    merchant_list << merch3 = Merchant.new({:id=>"12334113", :name=>"MiniatureBikez"})
+    merchant_list << merch4 = Merchant.new({:id=>"12334115", :name=>"LolaMarleys"})
+    merchant_list << merch5 = Merchant.new({:id=>"12334123", :name=>"Keckenbauer"})
+
+    merch = MerchantRepository.new(merchant_list)
+    fragment = "Shop"
+
+    assert_equal 1, merch.find_all_by_name(fragment).count
+  end
+
+  def test_find_all_by_name_specific_name_again
+    merchant_list = []
+    merchant_list << merch1 = Merchant.new({:id=>"12334105", :name=>"Shopin1901"})
+    merchant_list << merch2 = Merchant.new({:id=>"12334112", :name=>"Shop30943"})
+    merchant_list << merch3 = Merchant.new({:id=>"12334113", :name=>"MiniatureBikez"})
+    merchant_list << merch4 = Merchant.new({:id=>"12334115", :name=>"LolaMarleys"})
+    merchant_list << merch5 = Merchant.new({:id=>"12334123", :name=>"Keckenbauer"})
+
+    merch = MerchantRepository.new(merchant_list)
+    fragment = "Shop"
+
+    assert_equal 2, merch.find_all_by_name(fragment).count
   end
 end
