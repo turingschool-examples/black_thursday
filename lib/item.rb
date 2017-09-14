19bf03603969ad786a41edd2170bc12addc5f9e7
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-14 08:43:17 -0600

    change item initialize to take item repo as a argument

--- lib/item.rb
+++ lib/item.rb
@@ -1,24 +1,8 @@
+require 'item_repository'
 require 'bigdecimal'
 require 'time'
 
 class Item
-
-  def self.load_csv(items)
-    id = items[:id].to_i
-    name = items[:name]
-    description = items[:description]
-    unit_price = items[:unit_price]
-    created_at = items[:created_at]
-    updated_at = items[:updated_at]
-    merchant_id = items[:merchant_id]
-    Item.new(id,
-             name,
-             description,
-             unit_price,
-             created_at,
-             updated_at,
-             merchant_id)
-  end
 
   attr_reader :id,
               :name,
@@ -28,29 +12,20 @@ class Item
               :updated_at,
               :merchant_id
 
-  def initialize(id,
-                 name,
-                 description,
-                 unit_price,
-                 created_at,
-                 updated_at,
-                 merchant_id)
-    @id = id
-    @name = name
-    @description = description
-    @unit_price = unit_price_to_dollars(unit_price)
-    @created_at = Time.parse(created_at.to_s)
-    @updated_at = Time.parse(updated_at.to_s)
-    @merchant_id = merchant_id
+  def initialize(ir, csv_info)
+    @id = csv_info[:id]
+    @name = csv_info[:name]
+    @description = csv_info[:description]
+    @unit_price = unit_price_to_dollars(csv_info[:unit_price])
+    @created_at = Time.parse(csv_info[:created_at].to_s)
+    @updated_at = Time.parse(csv_info[:updated_at].to_s)
+    @merchant_id = csv_info[:merchant_id]
+    @item_repository = ir
   end
 
   def unit_price_to_dollars(unit_price)
     dollars = unit_price.to_f / 100
     BigDecimal.new(dollars, 4)
-  end
-
-  def merchant
-    item_repository.sales_engine.merchants.find_by_id(id)
   end
 
 end
