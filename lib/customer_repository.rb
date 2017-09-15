c269ed675ea14a86b72f8bf6df152dcdd609b1b1
Hugh Morrison <hmorri32@gmail.com>
2017-09-15 17:23:33 -0600

    customer_repository init

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,5 +1,30 @@
- class CustomerRepository 
-  def initialize(file_path)
+require 'csv'
 
+class CustomerRepository 
+  attr_accessor :all, :parent
+
+  def initialize(file_path, parent=nil)
+    @all    = create_customers(file_path)
+    @parent = parent
   end
- end
+
+  def csv_parse(file_path)
+    CSV.open file_path, headers: true, header_converters: :symbol
+  end
+
+  def create_customers(file_path)
+    csv_parse(file_path).map {|row| Customer.new(row, self)}
+  end
+
+  def find_by_id (id)
+    all.find {|customer| customer.id == id}
+  end
+
+  def find_all_by_first_name(prefix)
+    all.select {|customer| customer.first_name.downcase.include?(prefix.downcase)}
+  end
+
+  def find_all_by_last_name(prefix)
+    all.select {|customer| customer.last_name.downcase.include?(prefix.downcase)}
+  end
+end
