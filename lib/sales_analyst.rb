86334a402ff447faf4954870ae4f33011d1db0d6
Hugh Morrison <hmorri32@gmail.com>
2017-09-14 19:31:24 -0600

    ultra refactor sesh underway

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -9,84 +9,88 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-    (engine.items.all.length.to_f / engine.merchants.all.length.to_f).round(2)
+    average(merchants.map {|merchant| merchant.items.length})
+    # (items.length.to_f / merchants.length.to_f).round(2)
   end
 
   def average_items_per_merchant_standard_deviation
-    mean = engine.merchants.all.map do |merchant|
-      (merchant.items.length - average_items_per_merchant)**2
-    end
-    Math.sqrt(mean.sum / (engine.merchants.all.count-1)).round(2)
+    standard_deviation(merchants.map {|merchant| merchant.items.length})
+    # mean = merchants.map do |merchant|
+    #   (merchant.items.length - average_items_per_merchant)**2
+    # end
+    # Math.sqrt(mean.sum / (merchants.count-1)).round(2)
   end
 
   def merchants_with_high_item_count
     count = average_items_per_merchant + average_items_per_merchant_standard_deviation
-    engine.merchants.all.select do |merchant|
-      merchant.items.count > count
-    end
+    merchants.select {|merchant| merchant.items.count > count}
   end
 
   def average_item_price_for_merchant(merchant_id)
-    items       = engine.find_all_by_merchant_id(merchant_id)
-    item_prices = items.map { |item| item.unit_price }
-    (item_prices.sum / items.length).to_d.round(2)
+    items = engine.find_all_by_merchant_id(merchant_id)
+    average(items.map(&:unit_price))
+    # average(items.map { |item| item.unit_price })
+    # item_prices = items.map { |item| item.unit_price }
+    # (item_prices.sum / items.length).to_d.round(2)
   end
 
   def average_average_price_per_merchant
-    averages = engine.merchants.all.map do |merchant|
+    averages = merchants.map do |merchant|
       average_item_price_for_merchant(merchant.id)
     end
-    (averages.sum / engine.merchants.all.length).floor(2)
+    (averages.sum / averages.length).floor(2)
   end
 
   def golden_items
     count = (avg_item_price + avg_item_price_std_deviation * 2)
-    engine.items.all.select {|item| item.unit_price > count}
+    items.select {|item| item.unit_price > count}
   end
 
   def avg_item_price
-    prices = engine.items.all.map{|item| item.unit_price}
-    prices.sum / prices.count
+    average(items.map{|item| item.unit_price})
+    # prices = items.map{|item| item.unit_price}
+    # prices.sum / prices.count
   end
 
   def avg_item_price_std_deviation
-    mean = engine.items.all.map do |item|
+    mean = items.map do |item|
       (item.unit_price - avg_item_price)**2
     end
-    Math.sqrt(mean.sum / engine.items.all.count).round(2)
+    Math.sqrt(mean.sum / items.count).round(2)
   end
 
   def average_invoices_per_merchant
-    (engine.invoices.all.count.to_f / engine.merchants.all.count.to_f).round(2)
+    (invoices.count.to_f / merchants.count.to_f).round(2)
   end
 
   def average_invoices_per_merchant_standard_deviation
-    mean = engine.merchants.all.map do |merchant|
+    mean = merchants.map do |merchant|
       (merchant.invoices.length - average_invoices_per_merchant)**2
     end
-    Math.sqrt(mean.sum / (engine.merchants.all.count)).round(2)
+    Math.sqrt(mean.sum / (merchants.count)).round(2)
   end
 
   def avg_invoice_standard_deviation
-    mean = engine.merchants.all.map do |merchant|
+    mean = merchants.map do |merchant|
       (merchant.invoices.length - avg_invoice_count)**2
     end
-    Math.sqrt(mean.sum / engine.merchants.all.length).round(2)
+    Math.sqrt(mean.sum / merchants.length).round(2)
   end
 
   def avg_invoice_count
-    invoices = engine.merchants.all.map {|merchant| merchant.invoices.length}
-    invoices.sum / invoices.length
+    average(merchants.map {|merchant| merchant.invoices.length})
+    # invoices = merchants.map {|merchant| merchant.invoices.length}
+    # invoices.sum / invoices.length
   end
 
   def top_merchants_by_invoice_count
     count = (average_invoices_per_merchant + avg_invoice_standard_deviation * 2)
-    engine.merchants.all.select {|merchant| merchant.invoices.length > count}
+    merchants.select {|merchant| merchant.invoices.length > count}
   end
 
   def bottom_merchants_by_invoice_count
     count = (average_invoices_per_merchant - avg_invoice_standard_deviation * 2)
-    engine.merchants.all.select {|merchant| merchant.invoices.length < count}
+    merchants.select {|merchant| merchant.invoices.length < count}
   end
 
   def top_days_by_invoice_count
@@ -112,16 +116,44 @@ class SalesAnalyst
   end
 
   def grouped_invoices
-    engine.invoices.all.group_by {|invoice|invoice.created_at.strftime("%A")}
+    invoices.group_by {|invoice|invoice.created_at.strftime("%A")}
   end
 
   def avg_inv_per_day
-    engine.invoices.all.length / 7
+    invoices.length / 7
   end
 
   def invoice_status(status)
-    total = engine.invoices.all.length
-    selected  = engine.invoices.all.select {|invoice| invoice.status == status}
+    total = invoices.length
+    selected  = invoices.select {|invoice| invoice.status == status}
     ((selected.length.to_f / total.to_f) * 100.0).round(2)
+  end
+
+  def items 
+    engine.items.all
+  end
+  
+  def invoices 
+    engine.invoices.all
+  end
+
+  def merchants 
+    engine.merchants.all
+  end
+
+  def average(collection)
+    (collection.sum / collection.length.to_f).round(2)
+  end
+
+  def standard_deviation(collection)
+    Math.sqrt(s_d_numerator(collection) / collection.length).round(2)
+  end
+  
+  def squared_diff(number, average)
+    (number - average)**2
+  end
+  
+  def s_d_numerator(collection)
+    collection.map {|number| squared_diff(number, average(collection))}.sum
   end
 end
