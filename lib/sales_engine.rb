0ee328f227a89060335e61b4cb1f31fbaa1e7d61
Hugh Morrison <hmorri32@gmail.com>
2017-09-16 10:34:27 -0600

    init invoice_items and transactions

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,20 +2,26 @@ require_relative './merchant_repository'
 require_relative './item_repository'
 require_relative './invoice_repository'
 require_relative './customer_repository'
+require_relative './invoice_item_repository'
+require_relative './transaction_repository'
 
 class SalesEngine
   attr_reader :items,
               :merchants,
               :invoices,
-              :customers
+              :customers,
+              :invoice_items,
+              :transactions
 
   def initialize(file_path)
-    @merchants = MerchantRepository.new(file_path[:merchants], self)
-    @items     = ItemRepository.new(file_path[:items], self)
-    @invoices  = InvoiceRepository.new(file_path[:invoices], self)
-    @customers = CustomerRepository.new(file_path[:customers], self)
+    @merchants     = MerchantRepository.new(file_path[:merchants], self)
+    @items         = ItemRepository.new(file_path[:items], self)
+    @invoices      = InvoiceRepository.new(file_path[:invoices], self)
+    @customers     = CustomerRepository.new(file_path[:customers], self)
+    @invoice_items = InvoiceItemRepository.new(file_path[:invoice_items], self)
+    @transactions  = TransactionRepository.new(file_path[:transactions], self)
   end
-
+  
   def self.from_csv(file_path)
    SalesEngine.new(file_path)
   end
