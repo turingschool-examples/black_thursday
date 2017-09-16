6b2ba873cb85c4a7e97a442762fedc05d71e7c5a
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-16 14:18:31 -0600

    break things

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,7 +4,7 @@ require_relative "merchant_repository"
 require_relative "sales_analyst"
 
 class SalesEngine
-  attr_reader :items, :merchants
+  attr_reader :items, :merchants, :invoices
 
   def self.from_csv(file_names)
     @item_file = file_names[:items]
@@ -12,8 +12,9 @@ class SalesEngine
     SalesEngine.new(@item_file, @merchant_file)
   end
 
-  def initialize(item_csv, merchant_csv)
+  def initialize(item_csv, merchant_csv, invoice_csv)
     @items = ItemRepository.new(self, item_csv)
     @merchants = MerchantRepository.new(self, merchant_csv)
+    @invoices = InvoiceRepository.new(self, invoice_csv)
   end
 end
