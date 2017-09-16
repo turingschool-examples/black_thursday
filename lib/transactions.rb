5e179c82891b87e8e2174f19f5951500a72e7c91
Hugh Morrison <hmorri32@gmail.com>
2017-09-16 13:32:38 -0600

    spacing in init

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -9,14 +9,14 @@ class Transaction
               :parent
 
   def initialize(info, parent = nil)
-    @id = info[:id].to_i
-    @invoice_id = info[:invoice_id].to_i
-    @credit_card_number = info[:credit_card_number].to_i
+    @id                          = info[:id].to_i
+    @invoice_id                  = info[:invoice_id].to_i
+    @credit_card_number          = info[:credit_card_number].to_i
     @credit_card_expiration_date = info[:credit_card_expiration_date]
-    @result = info[:result]
-    @created_at = Time.parse(info[:created_at])
-    @updated_at = Time.parse(info[:updated_at])
-    @parent = parent
+    @result                      = info[:result]
+    @created_at                  = Time.parse(info[:created_at])
+    @updated_at                  = Time.parse(info[:updated_at])
+    @parent                      = parent
   end
 
   def invoice
