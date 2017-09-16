3249e6a87d7598915d2d7b515a11ee0a586cd495
BLaurenB <blbillington1@gmail.com>
2017-09-16 15:25:33 -0600

    updating invoice.rb

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -26,6 +26,19 @@ class Invoice
     invoice_repository.sales_engine.merchants.find_by_id(merchant_id)
   end
 
+  def merchant
+     #merchant_repository.sales_engine.items.find_all_by_merchant_id(id)
+     #item_repository.se.merchants.find_by_id(id)
+
+     id = merchant_id.to_i
+
+     return 0 if id.nil?
+     se = item_repository.se
+     merchants = se.merchants
+     #binding.pry
+     merchants.find_by_id(id)
+  end
+
 =begin
   Then connect our invoices to our merchants:
 
