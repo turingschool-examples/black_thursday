a91f416b3d3e56026c8c2f97ab3609cda8e46e46
BLaurenB <blbillington1@gmail.com>
2017-09-16 19:14:18 -0600

    sales engine and sales analyst minitests working again

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -7,13 +7,16 @@ require "./lib/item"
 
 class SalesEngineTest < Minitest::Test
 
-  attr_reader :new_engine
+  attr_reader :new_engine, :csv_hash
 
   def setup
-    csv_hash = {
+    @csv_hash = {
       :items     => "./test/test_fixtures/items_medium.csv",
       :merchants => "./test/test_fixtures/merchants_medium.csv",
-      :invoices => "./test/test_fixtures/invoices_medium.csv"
+      :invoices => "./test/test_fixtures/invoices_medium.csv",
+      :invoice_items => "./test/test_fixtures/invoice_items_medium.csv",
+      :transactions => "./test/test_fixtures/transactions_medium.csv",
+      :customers => "./test/test_fixtures/customers_medium.csv"
     }
     @new_engine = SalesEngine.from_csv(csv_hash)
   end
@@ -21,28 +24,23 @@ class SalesEngineTest < Minitest::Test
   def test_its_exists
     assert_instance_of SalesEngine, new_engine
   end
-  
+
   def test_from_csv_creates_new_sales_engine
-    csv_hash = {
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-    }
+
     new_engine = SalesEngine.from_csv(csv_hash)
-
     assert_instance_of SalesEngine, new_engine
-
   end
-  #
-  # def test_items
-  #   assert_instance_of ItemRepository, new_engine.items
-  #   assert_instance_of Array, new_engine.items.all
-  #   assert_instance_of Item, new_engine.items.all[0]
-  # end
-  #
-  # def test_merchants
-  #   assert_instance_of MerchantRepository, new_engine.merchants
-  #   assert_instance_of Array, new_engine.merchants.all
-  #   assert_instance_of Merchant, new_engine.merchants.all[0]
-  # end
-  #
+
+  def test_items
+    assert_instance_of ItemRepository, new_engine.items
+    assert_instance_of Array, new_engine.items.all
+    assert_instance_of Item, new_engine.items.all[0]
+  end
+
+  def test_merchants
+    assert_instance_of MerchantRepository, new_engine.merchants
+    assert_instance_of Array, new_engine.merchants.all
+    assert_instance_of Merchant, new_engine.merchants.all[0]
+  end
+
 end
