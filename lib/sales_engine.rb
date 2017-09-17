3de216cc4b4d95a9cf87be146b206fe9d8063542
katyjane8 <katy.jane8@gmail.com>
2017-09-17 12:46:41 -0600

    adding invoice fixture file, references to the invoices in SE and tests

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,20 +1,23 @@
 require_relative 'merchant_repo'
 require_relative 'item_repo'
+require_relative 'invoice_repo'
 require 'csv'
 
 class SalesEngine
   def self.from_csv(files)
     items = files[:items]
     merchants = files[:merchants]
-    sales_engine = SalesEngine.new(items, merchants)
+    invoices = files[:invoices]
+    sales_engine = SalesEngine.new(items, merchants, invoices)
     # SalesAnalyst.new(sales_engine)
   end
 
-  attr_reader :merchants, :items
+  attr_reader :merchants, :items, :invoices
 
-  def initialize(items, merchants)
+  def initialize(items, merchants, invoices)
     @items = ItemRepository.new(items, self)
     @merchants = MerchantRepository.new(merchants, self)
+    @invoices = InvoiceRepository.new(invoices, self)
   end
 
   def find_merchant_items(merchant_id)
--- test/invoice_repo_test.rb
+++ test/invoice_repo_test.rb
@@ -3,13 +3,16 @@ require_relative '../lib/invoice_repo'
 require_relative '../lib/invoice'
 require_relative '../lib/sales_engine'
 
-class MerchantRepositoryTest < Minitest::Test
-  attr_reader :merchant_repo
+class InvoiceRepositoryTest < Minitest::Test
+  attr_reader :invoice_repo
 
   def set_up
     files = ({:invoices => "./test/fixtures/invoice_fixture.csv"})
     SalesEngine.from_csv(files).invoices
   end
 
+  def test_invoices_exists
+    assert_instance_of Invoices, set_up
+  end
 
 end
