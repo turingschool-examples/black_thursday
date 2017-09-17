100be7b235662ac6b7e54fe0a05c05fe9ebde2f2
Aaron Schreck <schreck.aaron@gmail.com>
2017-09-16 21:46:10 -0600

    Add functional Transaction Repo and Transaction

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -8,36 +8,36 @@ class InvoiceRepository
     attr_accessor :invoices
 
     def initialize(file_name, sales_engine)
-        
-        @invoices = []
-        invoice_contents = parse_data(file_name)
-        invoice_contents.each do |row|
-            @invoices << Invoice.new(row, self)
-        end 
-        @sales_engine = sales_engine
-    end 
+
+      @invoices = []
+      contents = parse_data(file_name)
+      contents.each do |row|
+          @invoices << Invoice.new(row, self)
+      end
+      @sales_engine = sales_engine
+    end
 
     def all
-        @invoices
-    end 
-    
+      @invoices
+    end
+
     def find_by_id(id)
-        @invoices.find {|invoice| invoice.id == id }
+      @invoices.find {|invoice| invoice.id == id }
     end
 
     def find_all_by_customer_id(id)
-        @invoices.select {|invoice| invoice.customer_id == id }
+      @invoices.select {|invoice| invoice.customer_id == id }
     end
 
     def find_all_by_merchant_id(id)
-        @invoices.select {|invoice| invoice.merchant_id == id }
-    end 
+      @invoices.select {|invoice| invoice.merchant_id == id }
+    end
 
     def find_all_by_status(status)
-        @invoices.select {|invoice| invoice.status == status}
-    end 
+      @invoices.select {|invoice| invoice.status == status}
+    end
 
     def inspect
     "#<#{self.class} #{@merchants.size} rows>"
     end
-end 
+end
