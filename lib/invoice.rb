d2460e02ab278d05c7ce3098ac7b97d272f8cf1c
Emily Wise <wise.m.emily@gmail.com>
2017-09-16 22:16:11 -0600

    WIP Invoice and invoice Repo Class

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,6 +1,21 @@
-class Invoice
+require_relative 'repository/record'
+require 'time'
 
-  def initialize(*args)
+class Invoice < Repository::Record
+
+  attr_reader :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
+  def initialize(repo, fields)
+    super(repo, fields)
+    @id =           fields[:id].to_i
+    @customer_id =  fields[:customer_id]
+    @merchant_id =  fields[:merchant_id]
+    @status =   fields[:status]
+    @created_at = Time.parse([:created_at])
+    @updated_at = Time.parse([:updated_at])
+  end
+
+  def merchant
+    repo.parent(:merchants, merchant_id)
   end
 
 end
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -2,7 +2,20 @@ require_relative 'repository'
 
 class InvoiceRepository < Repository
 
-  def initialize(*args)
+  def record_class
+    Invoice 
+  end
+
+  def find_all_by_customer_id(customer_id)
+    find_all {|invoice| invoice.customer_id == customer_id}
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    find_all {|invoice| invoice.merchant_id == merchant_id}
+  end
+
+  def find_all_by_status(status)
+    find_all {|invoice| invoice.status == status}
   end
 
 end
