d47a3e6a42c26a098547bca3f35c519d25206f1a
Marshall Houston <marshall.houston@gmail.com>
2017-09-17 10:23:45 -0600

    add attr_reader for three transaction attributes

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,14 +4,17 @@ class Transaction
   attr_reader :id,
               :invoice_id,
               :credit_card_number,
-              :result
+              :result,
+              :credit_card_expiration_date,
+              :created_at,
+              :updated_at
 
   def initialize(info, transaction_repo)
     @id = info[:id].to_i
     @invoice_id = info[:invoice_id].to_i
     @credit_card_number = info[:credit_card_number].to_i
     @credit_card_expiration_date = info[:credit_card_expiration_date]
-    @result = info[:result].to_sym
+    @result = info[:result]
     @created_at = Time.parse(info[:created_at])
     @updated_at = Time.parse(info[:updated_at])
     @transaction_repo = transaction_repo
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -1,5 +1,6 @@
 require_relative 'test_helper'
 require_relative '../lib/transaction_repository'
+require_relative '../lib/sales_engine'
 
 
 class TransactionRepositoryTest  < Minitest::Test
