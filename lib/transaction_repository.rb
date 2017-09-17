76c55b805cd4aac839145af161693611dd6c41b5
Hugh Morrison <hmorri32@gmail.com>
2017-09-17 15:44:40 -0600

    fix reek

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -6,8 +6,12 @@ class TransactionRepository
               :parent
 
   def initialize(file_path, parent = nil)
-    @all = from_csv(file_path).map { |transaction| Transaction.new(transaction, self) }
+    @all    = create_transactions(file_path)
     @parent = parent
+  end
+
+  def create_transactions(file_path)
+    from_csv(file_path).map {|transaction| Transaction.new(transaction, self)}
   end
 
   def from_csv(file_path)
@@ -15,7 +19,7 @@ class TransactionRepository
   end
 
   def find_by_id(id)
-    all.find { |transaction| transaction.id == id.to_i}
+    all.find {|transaction| transaction.id == id.to_i}
   end
 
   def find_all_by_invoice_id(invoice_id)
@@ -23,7 +27,7 @@ class TransactionRepository
   end
 
   def find_all_by_credit_card_number(credit_card_number)
-    all.select { |transaction| transaction.credit_card_number == credit_card_number}
+    all.select { |t| t.credit_card_number == credit_card_number}
   end
 
   def find_all_by_result(result)
