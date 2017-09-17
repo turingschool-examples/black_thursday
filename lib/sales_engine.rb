c2ed9c1b9dfe12e1e6ba9d27f257d003b08a90e7
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-16 19:02:06 -0600

    update sales engine to include new repos

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,6 +2,9 @@
 require_relative "item_repository"
 require_relative "merchant_repository"
 require_relative "invoice_repository"
+require_relative "invoice_item_repository"
+require_relative "transaction_repository"
+require_relative "customer_repository"
 require_relative "sales_analyst"
 
 class SalesEngine
@@ -11,15 +14,23 @@ class SalesEngine
     @item_file = file_names[:items]
     @merchant_file = file_names[:merchants]
     @invoice_file = file_names[:invoices]
-    SalesEngine.new(@item_file, @merchant_file, @invoice_file)
+    @invoice_item_file = file_names[:invoice_items]
+    @transaction_file = file_names[:transactions]
+    @customer_file = file_names[:customers]
+    SalesEngine.new(@item_file,
+                    @merchant_file,
+                    @invoice_file,
+                    @invoice_item_file,
+                    @transaction_file,
+                    @customer_file)
   end
 
-  def initialize(item_csv, merchant_csv, invoice_csv)
+  def initialize(item_csv, merchant_csv, invoice_csv, invoice_items_csv, transaction_csv, customer_csv)
     @items = ItemRepository.new(self, item_csv)
-    puts items.all[0].item_repository
     @merchants = MerchantRepository.new(self, merchant_csv)
-    puts merchants.all[0].merchant_repository
     @invoices = InvoiceRepository.new(self, invoice_csv)
-    puts invoices.all[0].invoice_repository
+    @invoice_items = InvoiceItemsRepository.new(self, invoice_items_csv)
+    @transactions = TransactionRepository.new(self, transaction_csv)
+    @customers = CustomerRepository.new(self, customer_csv)
   end
 end
