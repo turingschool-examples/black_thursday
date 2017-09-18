d6e22ce4970d0ec0245be2df5414f902d4ba4563
Emily Wise <wise.m.emily@gmail.com>
2017-09-17 22:28:59 -0600

    WIP Customer Relationship

--- lib/customer.rb
+++ lib/customer.rb
@@ -10,8 +10,11 @@ class Customer < Repository::Record
     @last_name =   fields[:last_name]
   end
 
+
+
   def merchants
-    repo.children(:merchants, id)
-  end
+    matching_invoices = repo.children(:invoices, id)
+    matching_invoices.find_all do |record|
+      record.foreign_id(record_class) == repo.children(:merchants, id)
 
 end
