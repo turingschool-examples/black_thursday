f56122161fa1b35dfa8f6441b42b6aa68e6dea57
Marshall Houston <marshall.houston@gmail.com>
2017-09-18 15:50:54 -0600

    update test in item_repo because of addition in test fixture

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -15,36 +15,36 @@ class InvoiceRepositoryTest < Minitest::Test
 
   def test_invoice_repo_returns_array_of_all_invoices
     vr = setup
-    
-    assert_equal 45, vr.all.length
-  end 
+
+    assert_equal 46, vr.all.length
+  end
 
   def test_it_finds_invoice_by_id
     vr = setup
-    
+
     assert_instance_of Invoice, vr.find_by_id(74)
-  end 
+  end
 
   def test_it_finds_all_by_customer_id
     vr = setup
-    
+
     assert_equal 1, vr.find_all_by_customer_id(14).count
     assert_equal 2, vr.find_all_by_customer_id(500).count
-  end 
+  end
 
   def test_it_finds_all_by_merchant_id
     vr = setup
-    
+
     assert_equal 3, vr.find_all_by_merchant_id(12334105).count
     assert_equal 6, vr.find_all_by_merchant_id(12334123).length
-  end 
+  end
 
   def test_it_finds_all_by_status
     vr =setup
 
     assert_equal 9, vr.find_all_by_status(:returned).count
-    assert_equal 19, vr.find_all_by_status(:shipped).count
+    assert_equal 20, vr.find_all_by_status(:shipped).count
     assert_equal 17, vr.find_all_by_status(:pending).count
-  end 
+  end
 
-end 
+end
