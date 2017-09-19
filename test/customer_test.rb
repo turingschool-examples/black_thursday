18b9184e1cea4997995ef4930dfdb86066a93c52
Marshall Houston <marshall.houston@gmail.com>
2017-09-19 16:14:32 -0600

    update customer test (previous commit was customer repo test)

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -18,34 +18,30 @@ class CustomerTest < Minitest::Test
     se.invoice_items
     se.invoices
     se.merchants
-    se.customers
+    @cr = se.customers
   end
 
   def test_customer_exists
-    cr = setup
-    assert_instance_of Customer, cr.customers[0]
+    assert_instance_of Customer, @cr.customers[0]
   end
 
   def test_it_returns_customer_attributes
-    cr = setup
-    assert_equal 1, cr.all.first.id
-    assert_equal 'Joey', cr.all.first.first_name
-    assert_equal 'Ondricka', cr.all.first.last_name
-    assert_instance_of Time, cr.all.first.created_at
-    assert_instance_of Time, cr.all.first.updated_at
+    assert_equal 1, @cr.all.first.id
+    assert_equal 'Joey', @cr.all.first.first_name
+    assert_equal 'Ondricka', @cr.all.first.last_name
+    assert_instance_of Time, @cr.all.first.created_at
+    assert_instance_of Time, @cr.all.first.updated_at
   end
 
   def test_it_can_connect_with_invoices
-    cr = setup
 
-    customer = cr.find_by_id(1)
+    customer = @cr.find_by_id(1)
     assert_instance_of Invoice, customer.invoices[0]
   end
 
   def test_it_can_connect_with_merchants
-    cr = setup
 
-    customer = cr.find_by_id(1)
+    customer = @cr.find_by_id(1)
     merchants = customer.merchants
     assert_instance_of Merchant, merchants[0]
   end
