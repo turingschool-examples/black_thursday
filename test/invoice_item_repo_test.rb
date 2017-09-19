39d856e003e9c9e8ef6ba02165cac81ea530f483
katyjane8 <katy.jane8@gmail.com>
2017-09-19 15:10:01 -0600

    invoice item repo test added

--- test/invoice_item_repo_test.rb
+++ test/invoice_item_repo_test.rb
@@ -5,8 +5,14 @@ require_relative '../lib/invoice_item_repo'
 class InvoiceItemRepositoryTest < Minitest::Test
 
   def set_up
-    item_invoice = ({:invoices => "./test/fixtures/invoice_fixture.csv", :items => "./test/fixtures/item_fixture.csv", :merchants => "./test/fixtures/merchant_fixture.csv", :invoice_items => "./test/fixtures/invoice_items_fixture.csv", :transactions => "./test/fixtures/transactions_fixture.csv", :customers => "./test/fixtures/customers_fixture.csv"})
-    
+    files = ({:invoices => "./test/fixtures/invoice_fixture.csv", :items => "./test/fixtures/item_fixture.csv", :merchants => "./test/fixtures/merchant_fixture.csv", :invoice_items => "./test/fixtures/invoice_items_fixture.csv", :transactions => "./test/fixtures/transactions_fixture.csv", :customers => "./test/fixtures/customers_fixture.csv"})
+    SalesEngine.from_csv(files).invoice_items
   end
+
+  def test_item_exists
+    assert_instance_of InvoiceItem, set_up
+  end
+
+  
 
 end
