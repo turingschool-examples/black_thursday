74bf0f179ded319a15c9e059ab46c61a726b72b3
Dione Wilson <dionew1@gmail.com>
2017-09-19 15:05:50 -0600

    wip item_invoice_repo

--- test/invoice_item_repo_test.rb
+++ test/invoice_item_repo_test.rb
@@ -5,7 +5,8 @@ require_relative '../lib/invoice_item_repo'
 class InvoiceItemRepositoryTest < Minitest::Test
 
   def set_up
-    ({:invoices => "./test/fixtures/invoice_fixture.csv", :items => "./test/fixtures/item_fixture.csv", :merchants => "./test/fixtures/merchant_fixture.csv", :invoice_items => "./test/fixtures/invoice_items_fixture.csv", :transactions => "./test/fixtures/transactions_fixture.csv", :customers => "./test/fixtures/customers_fixture.csv"})
+    item_invoice = ({:invoices => "./test/fixtures/invoice_fixture.csv", :items => "./test/fixtures/item_fixture.csv", :merchants => "./test/fixtures/merchant_fixture.csv", :invoice_items => "./test/fixtures/invoice_items_fixture.csv", :transactions => "./test/fixtures/transactions_fixture.csv", :customers => "./test/fixtures/customers_fixture.csv"})
+    
   end
 
 end
