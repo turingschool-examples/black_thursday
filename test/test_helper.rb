2c22f4ef88fbbc2787d67fd6ecee0791ce841f2d
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-20 20:28:11 -0600

    add filters to simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,20 @@
-require "simplecov"
-SimpleCov.start
+require 'simplecov'
+SimpleCov.start do
+  add_filter "../merchant_math_test.rb"
+  add_filter "../merchant_test.rb"
+  add_filter "../invoice_repository_test.rb"
+  add_filter "../merchant_repository_test.rb"
+  add_filter "../item_repository_test.rb"
+  add_filter "../customer_test.rb"
+  add_filter "../invoice_item_repository_test.rb"
+  add_filter "../invoice_item_test.rb"
+  add_filter "../invoice_test.rb"
+  add_filter "../item_test.rb"
+  add_filter "../sales_analyst_test.rb"
+  add_filter "../sales_engine_test.rb"
+  add_filter "../transaction_repository_test.rb"
+  add_filter "../transaction_test.rb"
+end
 
 require "minitest/autorun"
 require "minitest/pride"
