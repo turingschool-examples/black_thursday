0dd3ddae8d50a1f6f8cf9708252b79de1a4571cc
abarnes26 <abarnes26@yahoo.com>
2017-10-30 13:31:29 -0600

    building merchant repo

---
+++ lib/merchant_repo.rb
@@ -0,0 +1,29 @@
+require './lib/merchant'
+
+class MerchantRepository
+
+  def initialize
+    @merchant_csv = CSV.open './data/merchants.csv',
+                    headers: true, header converter: :symbol
+    @merchant_queue = []
+    @merchant_csv.map do |row|
+      @merchant_queue << Merchant.new(row)
+      end
+  end
+
+  def all
+    @merchant_queue
+    #returns an array of all known Merchant instances
+  end
+
+  def find_by_id(id)
+    #returns either nil or an instance of Merchant with a matching ID
+  end
+
+  def find_by_name(name)
+    #returns either nil or an instance of Merchant having done a case insensitive search
+  end
+
+  def find_all_by_name(name)
+    #returns either [] or one or more matches which contain the supplied name fragment, case insensitive
+  end
