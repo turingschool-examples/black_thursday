fc589d48327776e01dd2a9037a4e1a8553fead82
maxscores <maxstackhouse@gmail.com>
2017-10-30 17:48:02 -0600

    Add Sales Engine, w/o Merchant Repo

---
+++ lib/sales_engine.rb
@@ -0,0 +1,25 @@
+require 'csv'
+require './lib/item_repository'
+
+class SalesEngine
+  attr_reader :item_repository, :merchant_repository
+
+  def initialize
+    @item_repository = nil
+    @merchant_repository = nil
+  end
+
+  def from_csv(files)
+    @item_repository = ItemRepository.new(load(files[:items]))
+    # @merchant_repository = MerchantRepository.new(load(files[merchants]))
+  end
+
+  #module candidate w/ cleanup
+  def load(file_name)
+    data = CSV.open(file_name, headers: true, header_converters: :symbol)
+    data.map do |row|
+      row
+    end
+  end
+
+end
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,25 @@
+require_relative './test_helper'
+
+class TestSalesEngine < Minitest::Test
+    def test_it_exists
+      se = SalesEngine.new()
+
+      assert_instance_of SalesEngine, se
+      assert_nil se.item_repository
+      assert_nil se.merchant_repository
+    end
+
+    def test_item_repository_populated_after_load
+      se = SalesEngine.new()
+      se.from_csv({
+        :items => "./test/item_fixture.csv",
+        :merchants => './test/merchants_fixture.csv'
+        })
+
+      assert_instance_of ItemRepository, se.item_repository
+      assert_equal 4, se.item_repository.items.count
+      # assert_instance_of MerchantRepository, se.merchant_repository
+      # assert_equal 5, se.merchant_repository.items.count
+    end
+
+end
