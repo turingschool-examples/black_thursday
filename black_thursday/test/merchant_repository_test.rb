7b5cd7dece2d6a4e4e398f823a806b1e7cab92c9
Ecksi <jimmy.dechant@gmail.com>
2017-10-30 17:33:14 -0600

    update find_merchant_by_id test

--- black_thursday/test/merchant_repository_test.rb
+++ black_thursday/test/merchant_repository_test.rb
@@ -22,7 +22,6 @@ class MerchantRepositoryTest < Minitest::Test
   def test_it_can_find_merchant_by_id
     assert_equal Merchant, repository.find_by_id(12334113).class
     assert_equal "MiniatureBikez", repository.find_by_id(12334113).name
+    assert_equal "2010-03-30", repository.find_by_id(12334113).created_at
   end
-
-
 end
