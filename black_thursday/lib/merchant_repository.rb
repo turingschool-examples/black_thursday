e92eb2bc4083b1ece4bcf221f13c9fc221ad9e41
Ecksi <jimmy.dechant@gmail.com>
2017-10-30 17:10:18 -0600

    asd

--- black_thursday/lib/merchant_repository.rb
+++ black_thursday/lib/merchant_repository.rb
@@ -4,11 +4,23 @@ class MerchantRepository
               :parent
 
   def initialize(merchants, parent = "")
-    @merchants = merchants
+    @merchants = load_csv(merchants).map { |row| Merchant.new(row, self) }
     @parent    = parent
   end
 
   def load_csv(filename)
-    CSV.open(filename)
+    CSV.open filename, headers: true, header_converters: :symbol
+  end
+
+  # def rows_from(csv_filename)
+  #   File.open csv_filename, "rb" do |f|
+  #     CSV f, headers: true, header_converters: :symbol do |csv|
+  #       csv.to_a
+  #     end
+  #   end
+  # end
+
+  def all
+    @merchants
   end
 end
--- black_thursday/test/merchant_repository_test.rb
+++ black_thursday/test/merchant_repository_test.rb
@@ -1,4 +1,5 @@
-require_relative "test_helper"
+require_relative 'test_helper'
+require 'csv'
 require './lib/merchant'
 require './lib/merchant_repository'
 
@@ -6,18 +7,18 @@ class MerchantRepositoryTest < Minitest::Test
   attr_reader :merchants,
               :repository
 
-
   def setup
-    @merchants = load_csv("./date/merchants.csv").map do |row|
-      Merchant.new(row)
-      @repository = MerchantRepository.new(@merchants)
-    end
+    @repository = MerchantRepository.new("./data/merchants.csv")
+  end
+
+  def test_it_exists
+    assert_instance_of MerchantRepository, @repository    
   end
 
   def test_it_can_return_all_merchants
-
-    assert_equal @merchants, @repository.all
+    assert_equal repository.merchants, @repository.all
   end
+
 
 
 end
