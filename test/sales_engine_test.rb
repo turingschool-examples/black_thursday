b188605a2a17296532ca62970ad9f7824b362896
kelleyjenkins <ckjenkins3@gmail.com>
2017-10-30 11:22:29 -0600

    Setup file structure

---
+++ test/sales_engine_test.rb
@@ -0,0 +1,12 @@
+require_relative 'test_helper'
+require_relative '../lib/sales_engine'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+
+class SalesEngineTest < Minitest::Test
+  def test_it_exists
+    
+  end
+
+end
