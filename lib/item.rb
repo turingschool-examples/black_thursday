5e4bd0d32b3e371a39c8783f124bbdfe7b0ec656
leepuppychow <leepuppychow@gmail.com>
2017-10-31 12:40:18 -0600

    Item class built

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,25 @@
 require_relative "../test/test_helper"
 
 class Item
+  attr_reader   :id,
+                :name,
+                :description,
+                :unit_price,
+                :created_at,
+                :updated_at,
+                :merchant_id,
+                :unit_price_to_dollars
+
+  def initialize(attributes = {}, parent = nil)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @description = attributes[:description]
+    @unit_price = attributes[:unit_price]
+    @unit_price_to_dollars = attributes[:unit_price_to_dollars]
+    @created_at = attributes[:created_at]
+    @updated_at = attributes[:updated_at]
+    @merchant_id = attributes[:merchant_id]
+  end
 
 
 end
