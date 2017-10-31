f4a1d1e3b897895713637d5a865de7cbd24377c4
maxscores <maxstackhouse@gmail.com>
2017-10-31 17:21:15 -0600

    Add Invoice and Invoice Repo

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,22 +2,30 @@ require_relative './test_helper'
 
 class TestSalesEngine < Minitest::Test
     def test_it_exists
-      se = SalesEngine.new('item_repository', 'merchant_repository')
+      se = SalesEngine.new(
+        'item_repository',
+        'merchant_repository',
+        'invoice_repository'
+      )
 
       assert_instance_of SalesEngine, se
       assert_equal 'item_repository', se.item_repository
       assert_equal 'merchant_repository', se.merchant_repository
+      assert_equal 'invoice_repository', se.invoice_repository
     end
 
     def test_item_repository_populated_after_load
       se = SalesEngine.from_csv({
         :items => "./test/item_fixture.csv",
-        :merchants => './test/merchants_fixture.csv'
+        :merchants => './test/merchants_fixture.csv',
+        :invoices => './test/invoice_fixture.csv'
         })
 
       assert_instance_of ItemRepository, se.item_repository
       assert_equal 4, se.item_repository.items.count
       assert_instance_of MerchantRepository, se.merchant_repository
       assert_equal 5, se.merchant_repository.merchants.count
+      assert_instance_of InvoiceRepository, se.invoice_repository
+      assert_equal 5, se.invoice_repository.invoices.count
     end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ require 'minitest/pride'
 
 require './lib/merchant'
 require './lib/item'
+require './lib/invoice'
 require './lib/merchant_repository'
 require './lib/item_repository'
+require './lib/invoice_repository'
 require './lib/sales_engine'
