6b4bb11f9ab1f5432077a62aa676754d95bb7d33
Ecksi <jimmy.dechant@gmail.com>
2017-11-01 17:03:20 -0600

    add test for find merchant/item by id

--- black_thursday/test/sales_engine_test.rb
+++ black_thursday/test/sales_engine_test.rb
@@ -21,4 +21,21 @@ class SalesEngineTest < Minitest::Test
   def test_it_loads_an_item_repository
     assert_equal 21, @engine.items.all.count
   end
+
+  def test_it_can_find_merchant_by_id
+    merchant = @engine.merchants.find_by_id(12334185)
+
+    assert_equal "Madewithgitterxx", merchant.name
+    assert_equal 6, merchant.items.length
+    assert_equal 263499920, merchant.items.first.id
+  end
+
+  def test_it_can_find_item_by_id
+    item = @engine.items.find_by_id(263499920)
+
+
+    assert_equal "Madewithgitterxx", item.merchant.name
+    assert_equal Merchant, item.merchant.class
+    assert_equal 12334185 , item.merchant.id
+  end
 end
