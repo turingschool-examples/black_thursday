8b75b83e5d5b22b634e501cb7c8ab6d1dc8f5be5
heybluguy <heybluguy@yahoo.com>
2017-11-01 17:05:08 -0600

    formatting

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,6 @@
 require 'bigdecimal'
+require 'time'
+
 
 class Item
   attr_reader   :id,
@@ -16,15 +18,14 @@ class Item
     @name         = attributes[:name]
     @description  = attributes[:description]
     @unit_price   = BigDecimal.new(attributes[:unit_price])/100
-    @created_at   = attributes[:created_at]
-    @updated_at   = attributes[:updated_at]
+    @created_at   = Time.parse(attributes[:created_at])
+    @updated_at   = Time.parse(attributes[:updated_at])
     @merchant_id  = attributes[:merchant_id]
     @parent = parent
   end
 
   def unit_price_to_dollars
     unit_price / 100
-    # BigDecimal.new(@unit_price).to_f
   end
 
 
