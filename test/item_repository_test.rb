2d1235d4d2f6f2cae724201e420393f499287d73
maxscores <maxstackhouse@gmail.com>
2017-11-02 11:06:57 -0600

    Add parent/child tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -2,8 +2,10 @@ require_relative './test_helper'
 require './lib/item_repository'
 
 class TestItemRepository < Minitest::Test
+  attr_reader :items
+
   def setup
-    items = [{
+    new_items = [{
       :id => '12345',
       :name => "Key Pad",
       :description => "Numberpad used for a fast lock",
@@ -20,59 +22,51 @@ class TestItemRepository < Minitest::Test
       :created_at => '2016-01-11 09:34:06 UTC',
       :updated_at => '2007-06-04 21:35:10 UTC'
     }]
-    ItemRepository.new(items, self)
+    parent = mock('parent')
+    @items = ItemRepository.new(new_items, parent)
   end
 
   def test_setup_exists
-    ir = setup
-    assert_instance_of ItemRepository, ir
+    assert_instance_of ItemRepository, items
   end
 
   def test_item_has_all_method
-    ir = setup
-
-    assert_equal 2, ir.all.count
+    assert_equal 2, items.all.count
   end
 
   def test_can_find_item_by_id
-    ir = setup
-
-    assert_equal '12345', ir.find_by_id('12345').id
-    assert_equal '23456', ir.find_by_id('23456').id
+    assert_equal '12345', items.find_by_id('12345').id
+    assert_equal '23456', items.find_by_id('23456').id
   end
 
   def test_can_find_item_by_name
-    ir = setup
-
-    assert_equal 'Key Pad', ir.find_by_name('key pad').name
-    assert_equal 'Nike Super Fast', ir.find_by_name('Nike Super Fast').name
+    assert_equal 'Key Pad', items.find_by_name('key pad').name
+    assert_equal 'Nike Super Fast', items.find_by_name('Nike Super Fast').name
   end
 
   def test_can_find_all_by_description
-    ir = setup
-
-    assert_equal 2, ir.find_all_with_description('fast').count
+    assert_equal 2, items.find_all_with_description('fast').count
   end
 
   def test_can_find_all_by_price
-    ir = setup
-
-    assert_equal 'Key Pad', ir.find_all_by_price(5)[0].name
-    assert_equal 'Nike Super Fast', ir.find_all_by_price(10000)[0].name
-    assert_equal [], ir.find_all_by_price(250)
+    assert_equal 'Key Pad', items.find_all_by_price(5)[0].name
+    assert_equal 'Nike Super Fast', items.find_all_by_price(10000)[0].name
+    assert_equal [], items.find_all_by_price(250)
   end
 
   def test_can_find_all_by_price_in_range
-    ir = setup
-
-    assert_equal 2, ir.find_all_by_price_in_range(10001, 4).count
-    assert_equal 0, ir.find_all_by_price_in_range(500, 100).count
+    assert_equal 2, items.find_all_by_price_in_range(10001, 4).count
+    assert_equal 0, items.find_all_by_price_in_range(500, 100).count
   end
 
   def test_can_find_by_merchant_id
-    ir = setup
+    assert_equal 1, items.find_all_by_merchant_id(65432).count
+    assert_equal [], items.find_all_by_merchant_id(12393)
+  end
 
-    assert_equal 1, ir.find_all_by_merchant_id(65432).count
-    assert_equal [], ir.find_all_by_merchant_id(12393)
+  def test_can_use_find_merchant_by_id
+    items.parent.stubs(:find_merchant_by_id).with("54321").returns(true)
+
+    assert items.find_merchant_by_id('54321')
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -2,7 +2,10 @@ require './test/test_helper'
 require './lib/item'
 
 class TestItem < Minitest::Test
-  def test_it_initializes_with_attributes
+  attr_reader :item, :item_2
+
+  def setup
+    parent = mock('parent')
     key_pad = {
       :id => '12345',
       :name => "Key Pad",
@@ -12,8 +15,21 @@ class TestItem < Minitest::Test
       :created_at => Time.now.to_s,
       :updated_at => Time.now.to_s
     }
-    item = Item.new(key_pad, 'parent')
+    @item = Item.new(key_pad, parent)
+    # require "pry"; binding.pry
+    shoe = {
+      :id => '23456',
+      :name => 'Nike Super Fast',
+      :description => 'Really fast shoes',
+      :unit_price => '10000',
+      :merchant_id => '65432',
+      :created_at => '2016-01-11 09:34:06 UTC',
+      :updated_at => '2007-06-04 21:35:10 UTC'
+    }
+    @item_2 = Item.new(shoe, parent)
+  end
 
+  def test_it_initializes_with_attributes
     assert_instance_of Item, item
     assert_equal '12345', item.id
     assert_equal "Key Pad", item.name
@@ -25,50 +41,24 @@ class TestItem < Minitest::Test
   end
 
   def test_it_initializes_with_other_attributes
-    shoe = {
-      :id => '23456',
-      :name => 'Nike Super Fast',
-      :description => 'Really fast shoes',
-      :unit_price => '10000',
-      :merchant_id => '65432',
-      :created_at => '2016-01-11 09:34:06 UTC',
-      :updated_at => '2007-06-04 21:35:10 UTC'
-    }
-    item = Item.new(shoe, 'parent')
-
-    assert_instance_of Item, item
-    assert_equal '23456', item.id
-    assert_equal "Nike Super Fast", item.name
-    assert_equal "Really fast shoes", item.description
-    assert_equal 1e4, item.unit_price
-    assert_equal '65432', item.merchant_id
-    assert_equal '2016-01-11 09:34:06 UTC', item.created_at
-    assert_equal '2007-06-04 21:35:10 UTC', item.updated_at
+    assert_instance_of Item, item_2
+    assert_equal '23456', item_2.id
+    assert_equal "Nike Super Fast", item_2.name
+    assert_equal "Really fast shoes", item_2.description
+    assert_equal 1e4, item_2.unit_price
+    assert_equal '65432', item_2.merchant_id
+    assert_equal '2016-01-11 09:34:06 UTC', item_2.created_at
+    assert_equal '2007-06-04 21:35:10 UTC', item_2.updated_at
   end
 
   def test_unit_price_to_dollars
-    key_pad = {
-      :id => '12345',
-      :name => "Key Pad",
-      :description => "Numberpad used for a lock",
-      :unit_price => '5',
-      :merchant_id => '54321',
-      :created_at => Time.now.to_s,
-      :updated_at => Time.now.to_s
-    }
-    item_key_pad = Item.new(key_pad, 'parent')
-    shoe = {
-      :id => '23456',
-      :name => 'Nike Super Fast',
-      :description => 'Really fast shoes',
-      :unit_price => '10000',
-      :merchant_id => '65432',
-      :created_at => '2016-01-11 09:34:06 UTC',
-      :updated_at => '2007-06-04 21:35:10 UTC'
-    }
-    item_shoe = Item.new(shoe, 'parent')
+    assert_equal 0.05, item.unit_price_to_dollars
+    assert_equal 100.00, item_2.unit_price_to_dollars
+  end
 
-    assert_equal 100.00, item_shoe.unit_price_to_dollars
-    assert_equal 0.05, item_key_pad.unit_price_to_dollars
+  def test_can_use_merchant
+    item.parent.stubs(:find_merchant_by_id).with("54321").returns(true)
+
+    assert item.merchant
   end
 end
