60660e8043e4fb9df6824980c34e09bf3d7199c2
leepuppychow <leepuppychow@gmail.com>
2017-11-02 17:10:35 -0600

    Populate invoice_repository working

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,3 +1,18 @@
+require_relative 'invoice'
+
 class InvoiceRepository
 
+  attr_reader     :all
+
+  def initialize
+    @all = []
+  end
+
+  def populate(filename)
+    CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+      @all << Invoice.new(row, self)
+    end
+  end
+
+  
 end
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,7 +1,17 @@
 require_relative 'test_helper'
-require_relative './lib/invoice_repository'
+require_relative '../lib/invoice_repository'
 
 class InvoiceRepositoryTest < MiniTest::Test
+
+  def test_all_contains_all_invoice_objects
+    invoice_repo = InvoiceRepository.new
+
+    invoice_repo.populate('./test/fixtures/invoices_fixture.csv')
+
+    assert_instance_of Array, invoice_repo.all
+    assert_instance_of Invoice, invoice_repo.all.first
+    assert_equal 4, invoice_repo.all.count
+  end
 
 
 end
