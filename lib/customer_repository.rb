82521ab21c6588103d6d03a9f72d3b42ecd4d405
maxscores <maxstackhouse@gmail.com>
2017-11-04 12:24:40 -0600

    Finish Customer Repository

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -7,4 +7,25 @@ class CustomerRepository
     @customers = customers.map {|customer| Customer.new(customer, self)}
   end
 
+  def all
+    customers
+  end
+
+  def find_by_id(id)
+    all.find do |customer|
+      customer.id == id.to_i
+    end
+  end
+
+  def find_all_by_first_name(name)
+    all.find_all do |customer|
+      customer.first_name.downcase == name.downcase
+    end
+  end
+
+  def find_all_by_last_name(name)
+    all.find_all do |customer|
+      customer.last_name.downcase == name.downcase
+    end
+  end
 end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -11,8 +11,14 @@ class CustomerRepositoryTest < Minitest::Test
       :first_name => "Max",
       :last_name => "Stackhouse",
       :created_at => '1990-07-18',
-      :updated_at => '2017-11-03'},
-      {
+      :updated_at => '2017-11-03'
+      },{
+      :id => "789987",
+      :first_name => "Max",
+      :last_name => "Payne",
+      :created_at => '1963-10-13',
+      :updated_at => '2017-11-11'
+      },{
       :id => "223344",
       :first_name => "Nicholas",
       :last_name => "Mbogo",
@@ -24,10 +30,32 @@ class CustomerRepositoryTest < Minitest::Test
 
   def test_it_initializes_with_attributes
     assert_instance_of CustomerRepository, customers
-    assert_equal 2, customers.customers.count
-    assert_instance_of Customer, customers.customers.first
-    assert_instance_of Customer, customers.customers.last
-    assert_instance_of CustomerRepository, customers.customers.first.parent
+    assert_equal 3, customers.all.count
+    assert_instance_of Customer, customers.all.first
+    assert_instance_of Customer, customers.all.last
+    assert_instance_of CustomerRepository, customers.all.first.parent
   end
 
+  def test_find_by_id
+    assert_instance_of Customer, customers.find_by_id(112233)
+    assert_equal "Max", customers.find_by_id(112233).first_name
+    assert_nil customers.find_by_id(123456)
+  end
+
+  def test_find_all_by_first_name
+    all_max = customers.find_all_by_first_name("Max")
+
+    assert_equal 2, all_max.count
+    assert_instance_of Customer, all_max.first
+    assert_equal "Max", all_max.first_name.first.first_name
+    assert_equal "Max", all_max.first_name.last.first_name
+  end
+
+  def test_find_all_by_last_name
+    all_payne = customers.find_all_by_last_name("Payne")
+
+    assert_equal 1, all_payne.count
+    assert_instance_of Customer, all_payne.first
+    assert_equal "Payne", all_payne.first.first_name
+  end
 end
