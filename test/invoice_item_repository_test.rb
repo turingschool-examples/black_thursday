ffb2461d37e5e4fbce84a7b0172abff39659a33e
NicholasMbogo <nicholasmbogo@yahoo.com>
2017-11-04 15:05:00 -0600

    updated invoice_item_repository

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -99,8 +99,26 @@ class InvoiceRepositoryTest < Minitest::Test
   end
 
   def test_it_can_find_invoice_items_by_invoice_id
-
-    assert_equal 7, ir.find_by_id("7").id
-    assert_equal 2, ir.find_by_id("2").id
+    parent = mock('parent')
+    invoice_items = [{
+      :id => "3",
+      :item_id => "223454178",
+      :invoice_id => "7",
+      :quantity => "9",
+      :unit_price => "400",
+      :created_at => "2012-03-27",
+      :updated_at => "2012-04-27"
+    },{
+      :id => "4",
+      :item_id => "22454278",
+      :invoice_id => "2",
+      :quantity => "4",
+      :unit_price => "10001",
+      :created_at => '2016-01-11 09:34:06 UTC',
+      :updated_at => '2007-06-04 21:35:10 UTC'
+    }]
+    ir = InvoiceItemRepository.new(invoice_items, parent)
+    assert_equal 7, ir.find_by_invoice_id("7").invoice_id
+    assert_equal 2, ir.find_by_invoice_id("2").invoice_id
   end
 end
