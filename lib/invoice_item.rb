1e798df24a607783c9b77f057eb9056229dde7c1
heybluguy <heybluguy@yahoo.com>
2017-11-04 15:04:10 -0600

    iteration 3

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -16,7 +16,7 @@ class InvoiceItem
     @item_id             = attributes[:item_id].to_i
     @invoice_id          = attributes[:invoice_id].to_i
     @quantity            = attributes[:quantity].to_i
-    @unit_price          = BigDecimal.new(attributes[:unit_price].to_i/100.0, 4)
+    @unit_price          = BigDecimal.new(attributes[:unit_price].to_i/100.0, 5)
     @created_at          = Time.parse(attributes[:created_at])
     @updated_at          = Time.parse(attributes[:updated_at])
     @invoice_item_repo   = parent
--- lib/transaction.rb
+++ lib/transaction.rb
@@ -12,8 +12,8 @@ class Transaction
 
   def initialize(attributes = {}, parent = nil)
     @id                           = attributes[:id].to_i
-    @invoice_id                   = attributes[:invoice_id]
-    @credit_card_number           = attributes[:credit_card_number]
+    @invoice_id                   = attributes[:invoice_id].to_i
+    @credit_card_number           = attributes[:credit_card_number].to_i
     @credit_card_expiration_date  = attributes[:credit_card_expiration_date]
     @result                       = attributes[:result]
     @created_at                   = Time.parse(attributes[:created_at])
