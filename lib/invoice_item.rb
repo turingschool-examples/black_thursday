281ab23eddd1c2cf3f320a8e31115c90c1edbcf7
NicholasMbogo <nicholasmbogo@yahoo.com>
2017-11-04 12:33:16 -0600

    updated ivoice item

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,5 +1,7 @@
+require 'bigdecimal'
 require 'time'
-class InvoiceItemRepository
+
+class InvoiceItem
   attr_reader :id,
               :item_id,
               :invoice_id,
@@ -13,8 +15,13 @@ class InvoiceItemRepository
     @item_id      = attributes[:item_id].to_i
     @invoice_id   = attributes[:invoice_id].to_i
     @quantity     = attributes[:quantity].to_i
+    @unit_price   = BigDecimal.new(attributes[:unit_price]) / 100
     @created_at   = Time.parse(attributes[:created_at])
     @updated_at   = Time.parse(attributes[:updated_at])
     @parent       = parent
+  end
+
+  def unit_price_to_dollars
+    @unit_price
   end
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -1,5 +1,5 @@
 require_relative 'test_helper'
-require './lib/invoice_item_repository'
+require './lib/invoice_item'
 
 class InvoiceItemRepositoryTest < Minitest::Test
 
@@ -7,21 +7,21 @@ class InvoiceItemRepositoryTest < Minitest::Test
 
   def setup
     parent = mock("parent")
-    @ir_1 = InvoiceItemRepository.new({
+    @ir_1 = InvoiceItem.new({
       :id => "1",
       :item_id => "223454178",
       :invoice_id => "1",
       :quantity => "7",
-      :unit_price => "33424",
+      :unit_price => "400",
       :created_at => "2012-03-27",
       :updated_at => "2012-04-27"
       }, parent)
-    @ir_2 = InvoiceItemRepository.new({
+    @ir_2 = InvoiceItem.new({
       :id => "3",
       :item_id => "22454278",
       :invoice_id => "2",
       :quantity => "4",
-      :unit_price => "23234",
+      :unit_price => "10000",
       :created_at => '2016-01-11 09:34:06 UTC',
       :updated_at => '2007-06-04 21:35:10 UTC'
     }, parent)
@@ -32,9 +32,10 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 223454178, ir_1.item_id
     assert_equal 1, ir_1.invoice_id
     assert_equal 7, ir_1.quantity
+    assert_equal 4, ir_1.unit_price
     assert_equal Time.parse("2012-03-27"), ir_1.created_at
     assert_equal Time.parse("2012-04-27"), ir_1.updated_at
-    assert_instance_of InvoiceItemRepository, ir_1
+    assert_instance_of InvoiceItem, ir_1
   end
 
   def test_can_be_initialized_with_other_attributes
@@ -42,24 +43,14 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 22454278, ir_2.item_id
     assert_equal 2, ir_2.invoice_id
     assert_equal 4, ir_2.quantity
+    assert_equal 100, ir_2.unit_price
     assert_equal Time.parse("2016-01-11 09:34:06 UTC"), ir_2.created_at
     assert_equal Time.parse("2007-06-04 21:35:10 UTC"), ir_2.updated_at
-    assert_instance_of InvoiceItemRepository, ir_2
+    assert_instance_of InvoiceItem, ir_2
   end
 
-  #
-  # def test_it_can_use_customers
-  #   parent = mock('parent')
-  #   customer = Customer.new({
-  #     :id => "1",
-  #     :item_id => "223454178",
-  #     :invoice_id => "2",
-  #     :quantity => "7",
-  #     :created_at => '1990-07-18',
-  #     :updated_at => '2017-11-04'
-  #     }, parent)
-  #   parent.stubs(:find_by_id).with(1).returns(true)
-  #
-  #   assert ir.customer
-  # end
+  def test_unit_price_to_dollars
+    assert_equal 4.00, ir_1.unit_price_to_dollars
+    assert_equal 100.00, ir_2.unit_price_to_dollars
+  end
 end
