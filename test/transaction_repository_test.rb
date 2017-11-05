572dc6c2ab81fbdabefc75483eeca940fad2dd82
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-11-05 12:14:16 -0700

    starting building transaction/trans_repo and tests for each

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -1,5 +1,22 @@
 require_relative 'test_helper'
+require_relative '../lib/sales_engine'
+require_relative '../lib/transaction_repository'
+require 'pry'
 
 class TransactionRepositoryTest < Minitest::Test
+
+  def setup
+    @sales_engine = SalesEngine.from_csv({
+      :items => './test/fixtures/items_fixture.csv',
+      :merchants => './test/fixtures/merchants.csv',
+      :invoices => './test/fixtures/invoices.csv',
+      :invoice_items => './test/fixtures/invoice_items.csv',
+      :transactions => './test/fixtures/transactions.csv'})
+    @transactions = @sales_engine.transactions
+  end
+
+  def test_that_it_finds_all_transactions
+    assert_equal @transactions.transactions, @transactions.all
+  end
 
 end
