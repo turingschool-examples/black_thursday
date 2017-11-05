2897b2a744b6a31693e554723e82c118559eb2ab
Tim Tyrrell <timomitchel23@gmail.com>
2017-11-05 12:24:05 -0700

    iteration 1 complete with all methods tested in sales analyst

--- black_thursday/test/sales_analyst_test.rb
+++ black_thursday/test/sales_analyst_test.rb
@@ -33,12 +33,42 @@ class SalesAnalystTest < Minitest::Test
   def test_can_find_average_average_price_per_merchant
     result = analyst.average_average_price_per_merchant
 
-    assert_equal 0.35029e3, result
+    assert_equal 0.1206e2, result
+  end
+
+  def test_count_merchants
+    assert_equal 60.0, analyst.count_merchants
+  end
+
+  def test_count_items
+    assert_equal 21.0, analyst.count_items
+  end
+
+  def test_can_find_merchant_items_by_id
+    assert_equal 6, analyst.merchant_items_by_id(12334185).length
+  end
+
+  def test_sum_prices
+    assert_equal 0.647e2, analyst.sum_prices(12334185)
+  end
+
+  def test_avg
+    expected = BigDecimal.new(0.10783333333333333333333333333e2, 4).round(2)
+    actual = BigDecimal.new(analyst.avg(12334185), 4).round(2)
+    assert_equal expected, actual
+  end
+
+  def test_standard_deviation_of_item_price
+    assert_equal 166.73, analyst.standard_deviation_of_item_price
   end
 
   def test_can_find_average_items_per_merchant_standard_deviation
     result = analyst.average_items_per_merchant_standard_deviation
     assert_equal 0.85, result
+  end
+
+  def test_one_standard_deviation_of_items_per_merchant
+    assert_equal 1.2, analyst.one_standard_deviation_of_merchant_items
   end
 
   def test_can_find_merchants_with_high_item_count
@@ -53,9 +83,21 @@ class SalesAnalystTest < Minitest::Test
     assert_equal 6, analyst.merchants_with_one_or_more_items.length
   end
 
+  def test_names_of_merchants_with_no_items
+    assert_equal 54, analyst.names_of_merchants_without_items.length
+  end
+
   def test_names_of_merchants_with_at_least_one_item
     expected = ["Candisart", "Keckenbauer", "thepurplepenshop", "Madewithgitterxx", "ElisabettaComotto", "MomsTshirts"]
 
     assert_equal expected, analyst.names_of_merchants_with_at_least_one_item
+  end
+
+  def test_two_standard_deviations_above_price
+    assert_equal 0.34552e3, analyst.two_standard_deviations_above_price
+  end
+
+  def test_golden_items
+    assert_equal 2, analyst.golden_items.length
   end
 end
