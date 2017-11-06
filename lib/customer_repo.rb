613210dbe50e2315752b6a20bc40def38fab855a
abarnes26 <abarnes26@yahoo.com>
2017-11-05 23:39:46 -0700

    created customer repo class

---
+++ lib/customer_repo.rb
@@ -0,0 +1,43 @@
+require_relative "customer"
+require_relative "sales_engine"
+require 'csv'
+require 'pry'
+
+class CustomerRepository
+  attr_reader :customers,
+              :sales_engine
+
+  def initialize(parent, filename)
+    @customers           = []
+    @sales_engine        = parent
+    @load                = load_file(filename)
+  end
+
+  def load_file(filename)
+    customers_csv = CSV.open filename,
+                             headers: true,
+                             header_converters: :symbol
+    customers_csv.each do |customer| @customers << Customer.new(customer, self) end
+  end
+
+  def all
+    customers
+  end
+
+  def find_by_id(id)
+    customers.find { |customer| customer.id == id.to_i }
+  end
+
+  def find_all_by_first_name(first_name)
+    customers.find_all { |customer| customer.first_name == first_name }
+  end
+
+  def find_all_by_last_name(last_name)
+    customers.find_all { |customer| customer.last_name == last_name }
+  end
+
+  def inspect
+      "#<#{self.class} #{@customers.size} rows>"
+  end
+
+end
