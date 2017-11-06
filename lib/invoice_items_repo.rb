55cfbcf9a82ccec8f51eadb3e3ff300815a3f632
abarnes26 <abarnes26@yahoo.com>
2017-11-05 23:03:11 -0700

    created_invoice_items_repo

---
+++ lib/invoice_items_repo.rb
@@ -0,0 +1,47 @@
+require_relative "invoice_items"
+require_relative "sales_engine"
+require 'csv'
+require 'pry'
+
+class InvoiceRepository
+  attr_reader :invoice_items,
+              :sales_engine
+
+  def initialize(parent, filename)
+    @invoice_items       = []
+    @sales_engine        = parent
+    @load                = load_file(filename)
+  end
+
+  def load_file(filename)
+    invoice_item_csv = CSV.open filename,
+                             headers: true,
+                             header_converters: :symbol
+    invoice_item_csv.each do |invoice_item| @invoice_items << InvoiceItems.new(invoice_item, self) end
+  end
+
+  def all
+    invoice_items
+  end
+
+  def find_by_id(id)
+    invoice_items.find { |invoice_item| invoice_item.id == id.to_i }
+  end
+
+  def find_all_by_item_id(item_id)
+    invoice_items.find_all { |invoice_item| invoice_item.item_id == item_id }
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    invoice_items.find_all { |invoice_item| invoice_item.invoice_id == invoice_id.to_i }
+  end
+
+  # def find_merchant(id)
+  #   @sales_engine.find_merchant(id)
+  # end
+
+  def inspect
+      "#<#{self.class} #{@invoice_items.size} rows>"
+  end
+
+end
