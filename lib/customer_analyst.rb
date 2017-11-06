86f93d3fc8e6d072fb8c10bcdfd6ffc47beb32cc
maxscores <maxstackhouse@gmail.com>
2017-11-05 21:50:45 -0700

    Benchmark top_buyers

--- lib/customer_analyst.rb
+++ lib/customer_analyst.rb
@@ -1,26 +1,41 @@
 module CustomerAnalyst
   def top_buyers(x = 20)
-    customer_invoices = se.invoices.all.reduce({}) do |result, invoice|
-      result[invoice.customer_id] = [] if !result.include?(invoice.customer_id)
-      result[invoice.customer_id] << invoice.id
-      result
-    end
-    customer_spend = customer_invoices.reduce({}) do |result, (customer_id, invoice_ids)|
-      result[customer_id] = 0
-      invoice_ids.map do |invoice_id|
-        invoice = se.find_invoice_by_invoice_id(invoice_id)
-        result[customer_id] += invoice.total
-      end
-      result
-    end
-    sorted = customer_spend.sort_by do |(customer_id, spend)|
-      (-1) * spend
-    end
-    top_x_customers = sorted.take(x)
-    top_x_customers.map do |(customer_id, spend)|
+    # customer_invoices
+    customer_spend = customer_spend(customer_invoices)
+    sorted = sorted_customer_spend(customer_spend)
+    sorted.take(x).map do |(customer_id, spend)|
       se.find_customer_by_id(customer_id)
     end
   end
 
+  def customer_invoices
+    se.invoices.all.reduce({}) do |result, invoice|
+      result[invoice.customer_id] = [] if !result.include?(invoice.customer_id)
+      result[invoice.customer_id] << invoice.id
+      result
+    end
+  end
 
+  def customer_spend(customer_invoices, index = 0)
+    return {} if index == se.customers.all.length
+    result = {}
+    result = customer_spend(customer_invoices, (index + 1))
+    if !customer_invoices[index].nil?
+      result[index] = customer_total(customer_invoices[index])
+    end
+    return result
+  end
+
+  def customer_total(invoice_ids)
+    invoice_ids.map do |invoice_id|
+      invoice = se.find_invoice_by_invoice_id(invoice_id)
+      invoice.total
+    end.sum
+  end
+
+  def sorted_customer_spend(customer_spend)
+    customer_spend.sort_by do |(customer_id, spend)|
+      (-1) * spend
+    end
+  end
 end
