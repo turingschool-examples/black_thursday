52eb7589a1f4b4a7770ba9eb4e4681b112169864
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-02 15:13:28 -0700

    pulling master

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,15 +3,15 @@ require './lib/item.rb'
 
 class SalesEngine
 
-  def initialize(data = {})
+  def self.from_csv(data = {})
     @items = data[:items],
     @merchants = data[:merchants]
   end
 
 end
 
-se = SalesEngine.new
-a = se.csv_reader('./data/items.csv')
-s = a.each do |row|
-  Item.new(row)
-end
+# se = SalesEngine.new
+# a = se.csv_reader('./data/items.csv')
+# s = a.each do |row|
+#   Item.new(row)
+# end
