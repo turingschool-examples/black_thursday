c990064bf056b4720ceffcb8b9bc6fc4a1769480
Joe Jobes <jmrjobes@gmail.com>
2018-01-02 14:24:49 -0700

    Updating sales engine test

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,11 +1,4 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter "/test/"
-end
-require "minitest/autorun"
-require "minitest/pride"
-require "./lib/sales_engine"
-require "pry"
+ 
 
 
 class SalesEngineTest < Minitest::Test
