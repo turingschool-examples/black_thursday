36cc7de092fa315e94833fedd486aafc14b16fa8
Luke Chambers <lukenatchambers@gmail.com>
2018-01-02 15:28:01 -0700

    Fix sanitation issues

--- lib/item.rb
+++ lib/item.rb
@@ -10,11 +10,11 @@ class Item
               :created_at,
               :updated_at
 
-  def initialize(id, name, description, unit_price, merchant_id, created_at, updated_at)
+  def initialize(id, name, desc, price, merchant_id, created_at, updated_at)
     @id          = id
     @name        = name
-    @description = description
-    @unit_price  = unit_price
+    @description = desc
+    @unit_price  = price
     @merchant_id = merchant_id
     @created_at  = created_at
     @updated_at  = updated_at
