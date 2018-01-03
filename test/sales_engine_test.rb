02c91c09706b09d739bcb3b5cc54c4238b0d66bd
Anna Royer <anna.royer@colorado.edu>
2018-01-02 19:00:25 -0700

    fix formatting

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,9 +3,10 @@ require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
   def setup
-    @se = SalesEngine.new({ items:     './test/fixtures/items_truncated.csv',
-                            merchants: './test/fixtures/merchants_truncated.csv'
-                          })
+    @se = SalesEngine.new({
+      items:     './test/fixtures/items_truncated.csv',
+      merchants: './test/fixtures/merchants_truncated.csv'
+    })
   end
 
   def test_creates_instance_of_item_repository
