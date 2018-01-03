0bb9d09e1188eea727b9b29e7905010e107569d2
Anna Royer <anna.royer@colorado.edu>
2018-01-02 20:40:19 -0700

    add vertical traversal to find a merchant's items

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,21 +1,21 @@
-require './test/test_helper'
-require './lib/merchant'
+require_relative '../test/test_helper'
+require_relative '../lib/merchant'
 
 class MerchantTest < Minitest::Test
   def test_it_has_id
-    m = Merchant.new({:id => 5, :name => "Turing School"}, mock('SalesEngine'))
+    m = Merchant.new({:id => 5, :name => "Turing School"}, mock('MerchantRepository'))
 
     assert_equal 5, m.id
   end
 
   def test_it_has_name
-    m = Merchant.new({:id => 5, :name => "Turing School"}, mock('SalesEngine'))
+    m = Merchant.new({:id => 5, :name => "Turing School"}, mock('MerchantRepository'))
 
     assert_equal "Turing School", m.name
   end
 
   def test_items_returns_an_array_of_all_merchants_items
-    m = Merchant.new({:id => 5, :name => "Turing School"}, mock('SalesEngine'))
+    m = Merchant.new({:id => 5, :name => "Turing School"}, mock('MerchantRepository'))
 
     result = m.items
 
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,5 +1,5 @@
-require './test/test_helper'
-require './lib/sales_engine'
+require '../test/test_helper'
+require '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
   def setup
@@ -15,5 +15,10 @@ class SalesEngineTest < Minitest::Test
 
   def test_creates_instance_of_merchant_repository
     assert_instance_of MerchantRepository, @se.merchants
+  end
+
+  def test_it_finds_items_by_merchant_id
+    merchant_id = ####
+    result = @se.find_items_by_merchant_id(merchant_id)
   end
 end
