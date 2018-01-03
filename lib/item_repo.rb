48ff98883f7e143b8e2e64f2ee61382de5b53828
Luke Chambers <lukenatchambers@gmail.com>
2018-01-03 12:48:07 -0700

    Fix gitignore

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -12,7 +12,7 @@ class ItemRepo
   def initialize(data, parent)
     @items = create_elements(data).reduce({}) do |item_collection, item|
       item_collection[item[:id].to_i] = Item.new(item)
-      item_collection
+      # item_collection
     end
     @parent = parent
   end
@@ -21,28 +21,56 @@ class ItemRepo
     items.values
   end
 
-  def find_by_id
-
+  def find_by_id(id)
+    @items.map do |item|
+      return item if item.id == id
+    end
   end
 
-  def find_by_name
-
+  def find_by_name(name)
+    @items.map do |item|
+      return item if item.name == name
+    end
   end
 
-  def find_all_with_description
-
+  def find_all_with_description(description)
+    @items.reduce([]) do |result, item|
+      if item.description == description
+        result << merchant
+      else
+        result
+      end
+    end
   end
 
-  def find_all_by_price
-
+  def find_all_by_price(price)
+    @items.reduce([]) do |result, item|
+      if item.price == price
+        result << merchant
+      else
+        result
+      end
+    end
   end
 
-  def find_all_by_price_in_range
-
+  def find_all_by_price_in_range(high, low)
+    @items.reduce([]) do |result, item|
+      if (low..high).include? item.price
+        result << merchant
+      else
+        result
+      end
+    end
   end
 
-  def find_all_by_merchant_id
-
+  def find_all_by_merchant_id(merchant_id)
+    @items.reduce([]) do |result, item|
+      if item.merchant_id == merchant_id
+        result << merchant
+      else
+        result
+      end
+    end
   end
 
 end
