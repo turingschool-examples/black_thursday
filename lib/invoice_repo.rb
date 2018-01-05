6d65320086dcffcd9a5d5d3b7e50a51ac5baf951
Luis Garcia <garcia@yahoo.com>
2018-01-05 11:06:14 -0700

    Create IR methods nd class

--- lib/invoice_repo.rb
+++ lib/invoice_repo.rb
@@ -1,4 +1,52 @@
-class InvoiceRepo
+require_relative 'invoice'
+require_relative 'sales_engine'
+require 'csv'
 
+class InvoiceRepo
+  attr_reader :invoices, 
+              :sales_engine
+
+  def initialize(sales_engine, filename)
+    @invoices     = []
+    @sales_engine = sales_engine
+    @load_invoices(filename)
+  end
+
+  def load_invoices(filename)
+    invoice_csv = CSV.open filename,
+                           headers: true,
+                           header_converters: :symbols,
+                           converters: :numeric
+    invoice_csv.each do |row| @invoices << Invoice.new(row, self) 
+    end   
+  end
+
+  def all
+    invoices
+  end
+
+  def find_by_id(id)
+    invoices.find { |invoice| invoice.id == id }
+  end
+
+  def find_all_by_customer_id(customer_id)
+    invoices.find_all { |invoice| invoice.customer_id == customer_id }
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    invoices.find_all { |invoice| invoice.merchant_id == merchant_id }
+  end
+
+  def find_all_by_status(status)
+    invoices.find_all { |invoice| invoice.status.to_sym == status }
+  end
+
+  def find_all_by_created_date(date)
+    invoices.find_all { |invoice| invoice.created_at == date }
+  end
+
+  def inspect
+    "#<#{self.class} #{invoice.size} rows>"
+  end
 
 end
