c7a53c9cbb4afee190ffe2a505adb8c4b434eecf
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-06 16:26:01 -0700

    Adds invoice items and tests

---
+++ lib/invoice_item.rb
@@ -0,0 +1,27 @@
+require 'bigdecimal'
+require 'time'
+
+class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
+
+  def initialize(info, invoice_item_repository = "")
+    @id         = info[:id].to_i
+    @item_id    = info[:item_id].to_i
+    @invoice_id = info[:invoice_id].to_i
+    @quantity   = info[:quantity].to_i
+    @unit_price = BigDecimal.new((info[:unit_price].to_i)/100.0, 4)
+    @created_at = Time.strptime(info[:created_at],"%Y-%m-%d %H:%M:%S %Z")
+    @updated_at = Time.strptime(info[:updated_at],"%Y-%m-%d %H:%M:%S %Z")
+    @parent     = invoice_item_repository
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
+  end
+end
