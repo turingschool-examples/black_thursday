c06b147bd42a7c4fb18215018ee228efb6644335
mgmilton <matt.milton@gmail.com>
2018-01-07 10:14:00 -0700

    adds transaction_test

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -12,7 +12,7 @@ class Transaction
     @id = info[:id].to_i
     @invoice_id = info[:invoice_id].to_i
     @credit_card_number = BigDecimal.new(info[:credit_card_number], 16).to_i.to_s
-    @credit_card_experiation_date = info[:credit_card_expiration_date].to_s.rjust(4,"0")
+    @credit_card_expiration_date = info[:credit_card_expiration_date].to_s.rjust(4,"0")
     @result = info[:result].to_s
     @created_at = Time.strptime(info[:created_at],"%Y-%m-%d %H:%M:%S %Z")
     @updated_at = Time.strptime(info[:updated_at],"%Y-%m-%d %H:%M:%S %Z")
