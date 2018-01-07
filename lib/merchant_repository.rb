a855a6f8561ac46c7c6d33193b38e39682249391
Joe Jobes <jmrjobes@gmail.com>
2018-01-07 08:25:53 -0700

    Getting rid of comments

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -23,29 +23,29 @@ class MerchantRepository
     @merchants
   end
 
-  def find_by_id(id)  # Returns Merchant by Id
+  def find_by_id(id)
     @merchants.find do |merchant|
       merchant if merchant.id == id
     end
   end
 
-  def find_by_name(name) # Returns if merchant name is == to name
+  def find_by_name(name)
     @merchants.find do |merchant|
       merchant if merchant.name.downcase == name.downcase
     end
   end
 
-  def find_all_by_name(name) # Returns if name is included with Merchant Name
+  def find_all_by_name(name)
     @merchants.find_all do |merchant|
       merchant if merchant.name.downcase.include?(name.downcase)
     end
   end
 
-  def find_item(id) # Returns Item by Merchant Id
+  def find_item(id)
     se.find_item_by_merchant_id(id)
   end
 
-  def grab_array_of_items # NEEDS TESTS || Returns array of items per merchant
+  def grab_array_of_items
     merchants.map { |merchant| merchant.items.count }
   end
 
