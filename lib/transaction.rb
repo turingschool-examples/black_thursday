fe1bcddf396540bb36a8d5129037b542be175b70
Luis Garcia <garcia@yahoo.com>
2018-01-07 21:48:34 -0700

    Add Transaction and TransactionRepo test

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -15,10 +15,10 @@ class Transaction
     @id                          = row[:id].to_i
     @invoice_id                  = row[:invoice_id].to_i
     @credit_card_number          = row[:credit_card_number].to_i
-    @credit_card_expiration_date = row[:credit_card_expiration_date].to_s
+    @credit_card_expiration_date = row[:credit_card_expiration_date].to_i
     @result                      = row[:result]
-    @created_at                  = Time.parse(row[:created_at])
-    @updated_at                  = Time.parse(row[:updated_at])
+    @created_at                  = Time.parse(row[:created_at]).to_s
+    @updated_at                  = Time.parse(row[:updated_at]).to_s
     @transaction_repo            = transaction_repo
   end
 end
