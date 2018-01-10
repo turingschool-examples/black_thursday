d3f7f4e8717d357c4e3d05a7e193fb25af35f609
Joe Jobes <jmrjobes@gmail.com>
2018-01-10 15:53:41 -0700

    Finishing most_sold_item_for_merchant(merchant_id)

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -191,16 +191,40 @@ class SalesAnalyst
     end
   end
 
-  def most_sold_item_for_merchant(merchant_id)
-    # binding.pry
-  end
-
-  def group_merchants_items_to_invoice_items(merchant_id) #TESTS
+  def grab_invoices_from_merchants(merchant_id) #TEST
     merchant = se.merchants.find_by_id(merchant_id)
     invoice_items = merchant.invoices.map do |invoice|
       invoice.invoice_items if invoice.is_paid_in_full?
-    end.delete_if(&:nil?)
-    invoice_items.flatten(1).group_by(&:item_id)
+    end.compact.flatten(1)
+  end
+
+  def group_items_to_invoice_attributes(merchant_id)
+    invoice_items = grab_invoices_from_merchants(merchant_id)
+    invoice_items.reduce(Hash.new(0)) do |result, inv_item|
+      result.merge!({inv_item.item_id => [inv_item.quantity, inv_item.unit_price]})
+    end
+  end
+
+  def sort_by_quantity(merchant_id) #TEST
+    item_attributes = group_items_to_invoice_attributes(merchant_id)
+    item_attributes.sort_by { |item, attributes| attributes[0] }.reverse
+  end
+
+  def grab_most_sold_items(merchant_id) # TEST
+    highest_quantity = sort_by_quantity(merchant_id)[0][1][0]
+    sort_by_quantity(merchant_id).map do |item|
+      item[0] if item[1][0] == highest_quantity
+    end.compact
+  end
+
+  def most_sold_item_for_merchant(merchant_id) # TEST
+    grab_most_sold_items(merchant_id).map do |item_id|
+      se.items.find_by_id(item_id)
+    end
+  end
+
+  def group_merchants_items_to_invoice_items(merchant_id) #TESTS
+    grab_invoices_from_merchants(merchant_id).group_by(&:item_id)
   end
 
   def group_items_to_revenue(merchant_id) #TESTS
