c455b57d7cb4be97494211ecec57c5e112f8218b
VadLusk <AdLusk@gmail.com>
2018-02-17 19:46:59 -0700

    Add merchant attributes id and name.

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -16,7 +16,6 @@ class MerchantTest < Minitest::Test
   end
 
   def test_other_attributes
-    skip
     merchant = Merchant.new({:id => 1, :name => "Haliburton"})
 
     assert_equal 1, merchant.id
