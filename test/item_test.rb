56d6ae65ca1d6cc04ecbd7e20580481141417c9f
VadLusk <AdLusk@gmail.com>
2018-02-17 21:01:59 -0700

    Add attributes and one method to Item. Unsure of assertion for current time.

--- test/item_test.rb
+++ test/item_test.rb
@@ -13,38 +13,34 @@ class ItemTest < Minitest::Test
   end
 
   def test_item_attributes
-    skip
     item = Item.new({:name => "Pencil",
     :description => "You can use it to write things",
     :unit_price  => BigDecimal.new(10.99,4),
     :created_at  => Time.now,
     :updated_at  => Time.now,})
-    expected = "You can use it to write things"
+    expected  = "You can use it to write things"
+    # expected2 = Date.today.strftime('%Y-%m-%d %H:%M:%S')
 
     assert_equal "Pencil", item.name
     assert_equal expected, item.description
-    assert_equal "Pencil", item.unit_price
-    assert_equal "Pencil", item.created_at
-    assert_equal "Pencil", item.updated_at
+    assert_equal 0.1099e2, item.unit_price
+    # assert_equal expected2, item.created_at
+    # assert_equal expected2, item.updated_at
   end
 
   def test_item_can_have_different_attributes
-    skip
     item = Item.new({:name => "Wine",
     :description => "It gets you drunk",
-    :unit_price  => BigDecimal.new(10.99,4),
+    :unit_price  => BigDecimal.new(79.59,4),
     :created_at  => Time.now,
     :updated_at  => Time.now,})
 
     assert_equal "Wine", item.name
     assert_equal "It gets you drunk", item.description
-    assert_equal "Pencil", item.unit_price
-    assert_equal "Pencil", item.created_at
-    assert_equal "Pencil", item.updated_at
+    assert_equal 0.7959e2, item.unit_price
   end
 
   def test_unit_price_to_dollars
-    skip
     item = Item.new({:name => "Pencil",
     :description => "You can use it to write things",
     :unit_price  => BigDecimal.new(10.99,4),
