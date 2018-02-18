4b1d78f0d63b4e9c66acbe90307ec44c38338eb1
VadLusk <AdLusk@gmail.com>
2018-02-17 19:36:38 -0700

    Initial commit. Add test_helper, merchant_test. Create file for Merchant.

---
+++ test/merchant_test.rb
@@ -0,0 +1,24 @@
+require './test/test_helper'
+require './lib/merchant'
+
+class MerchantTest < Minitest::Test
+  def test_merchant_class_exists
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_instance_of Merchant, merchant
+  end
+
+  def test_id_and_name
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_equal 5, merchant.id
+    assert_equal "Turing School", merchant.name
+  end
+
+  def test_other_attributes
+    merchant = Merchant.new({:id => 1, :name => "Haliburton"})
+
+    assert_equal 1, merchant.id
+    assert_equal "Haliburton", merchant.name
+  end
+end
