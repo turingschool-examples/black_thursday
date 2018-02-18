14354d333a31514c4189fb6c1f1fa32b0c55fdbe
hmesander <haleymesander@gmail.com>
2018-02-18 15:55:48 -0700

    Adds merchant repo tests and merchant repo class through the all method.  Adds merchant test and merchant class. Creates truncated merchant csv file for merchant repo testing.

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,2 +1,18 @@
-def initialize(csv_path, parent)
+require 'csv'
+
+class MerchantRepository
+  attr_reader :merchant_csv_path, :parent
+  def initialize(merchant_csv_path, parent)
+    @merchant_csv_path = merchant_csv_path
+    @parent            = parent
+  end
+
+  def all
+    merchants = []
+    contents = CSV.open @merchant_csv_path, headers: true
+    contents.each do |row|
+      merchants << Merchant.new(id: row[0], name: row[1])
+    end
+    merchants
+  end
 end
---
+++ test/merchant_repo_test.rb
@@ -0,0 +1,30 @@
+require_relative 'test_helper'
+require_relative '../lib/merchant_repository'
+require_relative '../lib/sales_engine'
+# require_relative './fixtures/merchants_list_truncated.csv'
+
+class MerchantRepositoryTest < Minitest::Test
+  def test_it_exists
+    merchant_repository = MerchantRepository.new('test_path', 'sales_engine')
+
+    assert_instance_of MerchantRepository, merchant_repository
+  end
+
+  def test_for_initialized_arguments
+    merchant_repository = MerchantRepository.new('test_path', 'sales_engine')
+
+    assert_equal 'test_path', merchant_repository.merchant_csv_path
+    assert_equal 'sales_engine', merchant_repository.parent
+  end
+
+  def test_for_all_method
+    se = SalesEngine.new
+    se.from_csv({:items => './data/items.csv',
+                 :merchants => './fixtures/merchants_list_truncated.csv'})
+    mr = se.merchants
+
+    assert_instance_of Array, mr.all
+    assert mr[0].is_a?(Merchant)
+    assert_equal 6, mr.all.count
+  end
+end
