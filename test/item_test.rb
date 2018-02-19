2333107097a43d1d4e078a86f532c6ad44ebfb7f
Lihkin <shniks@gmail.com>
2018-02-19 13:42:05 -0700

    Adding test for unit_price_to_dollars method and creating code.

--- test/item_test.rb
+++ test/item_test.rb
@@ -9,16 +9,29 @@ class ItemTest < Minitest::Test
       :id          => "137519844",
       :name        => "Pencil",
       :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
+      :unit_price  => BigDecimal.new(10.994, 4),
       :created_at  => "2018-19-02 14:37:20 -0700",
       :updated_at  => "2018-19-02 14:37:20 -0700",
-      :merchant_id => "12334105"
-        }
+      :merchant_id => "12334105"}
     @item = Item.new(data)
   end
 
   def test_if_it_exists
     assert_instance_of Item, @item
+  end
+
+  def test_if_it_has_attributes
+    assert_equal 137519844, @item.id
+    assert_equal "Pencil", @item.name
+    assert_equal "You can use it to write things", @item.description
+    assert_equal 0.1099e2, @item.unit_price
+    assert_equal "2018-19-02 14:37:20 -0700", @item.created_at
+    assert_equal "2018-19-02 14:37:20 -0700", @item.updated_at
+    assert_equal 12334105, @item.merchant_id
+  end
+
+  def test_if_it_can_return_unit_price_in_dollars
+    assert_equal "$10.99", @item.unit_price_to_dollars
   end
 
 end
