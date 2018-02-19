476181cd9cc2dec90621acd021cf5c33873a7dd8
lilwillifo <margaretwilliford4@gmail.com>
2018-02-19 08:57:41 -0700

    refactoring find_items method

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -12,16 +12,8 @@ class ItemRepository
   end
 
   def find_items(filepath)
-    item_repo = CSV.read(filepath, headers: true)
-    id          = item_repo['id']
-    name        = item_repo['name']
-    description = item_repo['description']
-    unit_price  = item_repo['unit_price']
-    merchant_id = item_repo['merchant_id']
-    created_at  = item_repo['created_at']
-    updated_at  = item_repo['updated_at']
-    
-    [id, name, description, unit_price, merchant_id,
-     created_at, updated_at].transpose
+    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
+      @items << Item.new(data)
+    end
   end
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,6 +1,7 @@
 require_relative 'test_helper'
 require_relative '../lib/item_repository'
 require 'bigdecimal'
+require 'pry'
 
 class ItemRepositoryTest < Minitest::Test
   def test_it_exists
@@ -12,7 +13,7 @@ class ItemRepositoryTest < Minitest::Test
   def test_item_repo_has_items
     item_repository = ItemRepository.new('./test/fixtures/items.csv')
 
-    assert_equal 4, item_repository.all.count
+    assert_equal 9, item_repository.all.count
     assert_instance_of Array, item_repository.all
     assert item_repository.all.all? { |item| item.is_a?(Item) }
   end
