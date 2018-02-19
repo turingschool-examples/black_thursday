00868ec317960bad2a003e7cdd2a63f3a3d6f4c5
Megan Arellano <ssciolist@gmail.com>
2018-02-18 18:59:25 -0700

    update item and add itemrepo tests

--- lib/item.rb
+++ lib/item.rb
@@ -1,14 +1,17 @@
 class Item
-  attr_reader :name,
+  attr_reader :id,
+              :name,
               :description,
               :unit_price,
               :created_at,
               :updated_at
 
   def initialize(data)
+    @id = data[:id]
     @name = data[:name]
     @description = data[:description]
     @unit_price  = data[:unit_price].to_i
+    @merchant_id = data[:merchant_id]
     @created_at  = data[:created_at]
     @updated_at  = data[:updated_at]
   end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -33,4 +33,57 @@ class ItemRepositoryTest < Minitest::Test
     assert_equal "510+ RealPush Icon Set", result.name
   end
 
+  def test_it_can_find_item_by_id
+    item_repository = ItemRepository.new("./test/fixtures/items.csv")
+
+    result = item_repository.find_by_id(263395617)
+
+    assert_instance_of Item, result
+    assert_equal "Glitter scrabble frames", result.name
+    assert_equal 263395617, result.id
+  end
+
+  def test_it_can_find_all_with_description
+    skip
+    item_repository = ItemRepository.new("./test/fixtures/items.csv")
+
+    result = item_repository.find_all_with_description("Disney glitter frames")
+
+    assert_instance_of Item, result
+    assert_equal 1350, result.unit_price
+    assert_equal "Disney scrabble frames", result.name
+  end
+
+  def test_it_can_find_all_by_price
+    skip
+    item_repository = ItemRepository.new("./test/fixtures/items.csv")
+
+    result = item_repository.find_all_by_price(700)
+
+    assert_instance_of Item, result
+    assert_equal 263396013, result.id
+    assert_equal 700, result.unit_price
+  end
+
+  def test_it_can_find_all_by_price_range
+    skip
+    item_repository = ItemRepository.new("./test/fixtures/items.csv")
+
+    result = item_repository.find_all_by_price((0..900))
+
+    assert_instance_of Item, result
+    assert_equal 700, result.unit_price
+    assert_equal "Free standing Woden letters", result.name
+  end
+
+  def test_it_can_find_all_by_merchant_id
+    skip
+    item_repository = ItemRepository.new("./test/fixtures/items.csv")
+
+    result = item_repository.find_by_merchant_id(12334105)
+
+    assert_instance_of Item, result
+    assert_equal 1200, result.unit_price
+    assert_equal 12334105, result.merchant_id
+  end
 end
