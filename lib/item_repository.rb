297c836b292c9c939b1c0f0aac5fa58cfe85848f
adam-conway <adam.n.conway@gmail.com>
2018-02-18 19:10:09 -0700

    Working on adding find methods in item repo

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -15,4 +15,29 @@ class ItemRepository
       @items << Item.new(row)
     end
   end
+
+  def all
+    @items
+  end
+
+  def find_by_id(id)
+    @items.find { |item| item.id == id.to_i }
+  end
+
+  def find_by_name(name)
+    # @items.find { |item| name.casecmp(item.name) } #can't get this to work
+    @items.find { |item| name.downcase == item.name.downcase }
+  end
+
+  def find_all_with_description(string)
+    @items.find_all { |item| item.description.include?(string) }
+  end
+
+#   all - returns an array of all known Item instances
+# find_by_id - returns either nil or an instance of Item with a matching ID
+# find_by_name - returns either nil or an instance of Item having done a case insensitive search
+# find_all_with_description - returns either [] or instances of Item where the supplied string appears in the item description (case insensitive)
+# find_all_by_price - returns either [] or instances of Item where the supplied price exactly matches
+# find_all_by_price_in_range - returns either [] or instances of Item where the supplied price is in the supplied range (a single Ruby range instance is passed in)
+# find_all_by_merchant_id - returns either [] or instances of Item where the supplied merchant ID matches that supplied
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -15,6 +15,27 @@ class ItemRepositoryTest < Minitest::Test
 
   def test_item_csv_parsed
     assert_equal 4, @ir.items.length
-    assert_equal 263395237, @ir.items.first.id
+    assert_equal 263_395_237, @ir.items.first.id
+  end
+
+  def test_all_items
+    assert_equal 4, @ir.all.length
+    assert @ir.all[3].is_a?(Item)
+    assert @ir.all[0].description.include?('googlepicasa')
+  end
+
+  def test_find_by_id
+    assert_nil @ir.find_by_id(111)
+    assert_equal 263_395_237, @ir.find_by_id(263_395_237).id
+  end
+
+  def test_find_by_name
+    assert_nil @ir.find_by_name('help meadsfadsf')
+    assert_equal '510+ RealPush Icon Set', @ir.find_by_name('510+ RealPush Icon Set').name
+  end
+
+  def test_find_all_with_description
+    assert_equal [], @ir.find_all_with_description('help me')
+    assert_equal 1, @ir.find_all_with_description('Free standing woo').length
   end
 end
