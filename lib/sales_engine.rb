3e36dc2f037a1d5ad44cc49d4695e959e1b7e62e
Evan Wheeler <anon0mys87@gmail.com>
2018-02-19 13:17:13 -0700

    Revised sales engine init and from_csv methods.

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -6,15 +6,12 @@ require 'pry'
 class SalesEngine
   attr_accessor :items, :merchants
 
-  def initialize
-    @items = nil
-    @merchants = nil
+  def initialize(repositories)
+    @items = ItemRepository.new(repositories[:items])
+    @merchants = MerchantRepository.new(repositories[:merchants])
   end
 
   def self.from_csv(repositories)
-    engine = new
-    engine.items = ItemRepository.new(repositories[:items])
-    engine.merchants = MerchantRepository.new(repositories[:merchants])
-    engine
+    new(repositories)
   end
 end
