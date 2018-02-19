0a1c6d9e0cbbffcf6caf9961daef91573e25a1bc
lilwillifo <margaretwilliford4@gmail.com>
2018-02-18 21:21:15 -0700

    work in progress, working towards reading in items repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,4 +1,19 @@
+require 'CSV'
+require_relative '../lib/item'
+require 'pry'
 class ItemRepository
-  def initialize(file)
+  def initialize(filepath)
+    @items = []
+    load_items(filepath)
+  end
+
+  def all
+    @items
+  end
+
+  def load_items(filepath)
+    CSV.foreach(filepath, headers:true, header_converters: :symbol) do |data|
+      @items << Item.new(data)
+    end
   end
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -4,14 +4,16 @@ require 'bigdecimal'
 
 class ItemRepositoryTest < Minitest::Test
   def test_it_exists
-    item_repository = ItemRepository.new('./data/items.rb')
+    item_repository = ItemRepository.new('./test/fixtures/items.csv')
 
     assert_instance_of ItemRepository, item_repository
   end
 
   def test_item_repo_has_items
-    item_repository = ItemRepository.new('./data/items.rb')
+    item_repository = ItemRepository.new('./test/fixtures/items.csv')
 
-    
+    assert_equal 4, item_repository.all.count
+    assert_instance_of Array, item_repository.all
+    assert item_repository.all.all? { |item| item.is_a?(Item) }
   end
 end
