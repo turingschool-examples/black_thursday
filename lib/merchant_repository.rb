a67bf69a31022d9708326ff6b304a371f87dce1c
brickstar <brickstar@gmail.com>
2018-02-19 15:15:12 -0700

    testing find by id

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,5 +1,7 @@
 require 'CSV'
 require 'pry'
+require './lib/merchant'
+
 class MerchantRepository
   def initialize(filepath)
     @merchants = []
@@ -11,9 +13,13 @@ class MerchantRepository
   end
 
   def load_merchants(filepath)
-    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
-    puts row
+    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
+    @merchants << Merchant.new(data)
+    end
   end
+
+  def find_by_id(id)
+    @merchants.find { |merchant| merchant.id == id}
   end
 
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -10,12 +10,28 @@ class MerchantRepositoryTest < Minitest::Test
     assert_instance_of MerchantRepository, merchant_repository
   end
 
-  def test_a_merchant_repo_has_merchants
+  def test_merchant_repo_has_merchants
     merchant_repository = MerchantRepository.new("./test/fixtures/merchants.csv")
 
     assert_equal 4, merchant_repository.all.count
     assert_instance_of Array, merchant_repository.all
-    assert merchant_repository.all? { |merchant| merchant.is_a?(Merchant)}
+    assert merchant_repository.all { |merchant| merchant.is_a?(Merchant)}
     assert_equal "Shopin1901", merchant_repository.all.first.name
+  end
+
+  def test_it_can_find_merchant_by_id
+    merchant_repository = MerchantRepository.new("./test/fixtures/merchants.csv")
+
+    result = merchant_repository.find_by_id(1)
+
+    assert_instance_of Merchant, result
+    assert_equal "Shopin1901", result.name
+    assert_equal 1, result.id
+  end
+
+  def test_it_returns_nil_when_no_find_match
+    merchant_repository = MerchantRepository.new("./test/fixtures/merchants.csv")
+
+    result = merchant_repository.find_by_id
   end
 end
