9297efcaf80bbb6e176750cee1131518ed13f0c7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-20 13:49:17 -0700

    Semantics

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -6,26 +6,26 @@ require_relative '../lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
   def test_it_exists
-    merchant_repository = MerchantRepository.new('./test/fixtures/merchants.csv')
+    mr = MerchantRepository.new './test/fixtures/merchants.csv'
 
-    assert_instance_of MerchantRepository, merchant_repository
+    assert_instance_of MerchantRepository, mr
   end
 
   def test_a_merchant_repo_has_merchants
-    merchant_repo = MerchantRepository.new('./test/fixtures/merchants.csv')
+    merchant_repo = MerchantRepository.new './test/fixtures/merchants.csv'
 
     assert_equal 9, merchant_repo.all.count
     assert_instance_of Array, merchant_repo.all
     assert merchant_repo.all.all? do |merchant|
-      merchant.is_a?(Merchant)
+      merchant.is_a? Merchant
     end
     assert_equal 'Shopin1901', merchant_repo.all.first.name
   end
 
   def test_it_can_find_merchant_by_id
-    merchant_repo = MerchantRepository.new('./test/fixtures/merchants.csv')
+    merchant_repo = MerchantRepository.new './test/fixtures/merchants.csv'
 
-    result = merchant_repo.find_by_id(1)
+    result = merchant_repo.find_by_id 1
 
     assert_instance_of Merchant, result
     assert_equal 'Shopin1901', result.name
@@ -33,17 +33,17 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_it_returns_nil_when_no_find_match
-    merchant_repo = MerchantRepository.new('./test/fixtures/merchants.csv')
+    merchant_repo = MerchantRepository.new './test/fixtures/merchants.csv'
 
-    result = merchant_repo.find_by_id(876)
+    result = merchant_repo.find_by_id 876
 
     assert_nil result
   end
 
   def test_it_can_find_merchant_by_name
-    merchant_repo = MerchantRepository.new('./test/fixtures/merchants.csv')
+    merchant_repo = MerchantRepository.new './test/fixtures/merchants.csv'
 
-    result = merchant_repo.find_by_name('Candisart')
+    result = merchant_repo.find_by_name 'Candisart'
 
     assert_instance_of Merchant, result
     assert_equal 'Candisart', result.name
@@ -51,9 +51,9 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_it_can_find_merchant_by_name_case_insensitive
-    merchant_repo = MerchantRepository.new('./test/fixtures/merchants.csv')
+    merchant_repo = MerchantRepository.new './test/fixtures/merchants.csv'
 
-    result = merchant_repo.find_by_name('lolamarleys')
+    result = merchant_repo.find_by_name 'lolamarleys'
 
     assert_instance_of Merchant, result
     assert_equal 'LolaMarleys', result.name
@@ -61,9 +61,9 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_it_can_find_an_array_of_matches_by_name_fragment
-    merchant_repo = MerchantRepository.new('./test/fixtures/merchants.csv')
+    merchant_repo = MerchantRepository.new './test/fixtures/merchants.csv'
 
-    result = merchant_repo.find_all_by_name('mini')
+    result = merchant_repo.find_all_by_name 'mini'
 
     assert_instance_of Array, result
     assert_instance_of Merchant, result[0]
