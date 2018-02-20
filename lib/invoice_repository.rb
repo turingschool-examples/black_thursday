53f8323e30a92bb3a779b2aa8817c2f8416e706c
Eliot Swank <eliotswank@gmail.com>
2018-02-20 12:31:00 -0700

    work on iteration 2--fixed issue with find_by_id taking integer instead of string

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -2,8 +2,10 @@ require_relative 'searching'
 # require_relative 'invoice'
 
 class InvoiceRepository
-  include Searching
-	attr_reader :all
+
+	def initialize(file_name) 
+		
+	end
 
 
 end
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,14 +1,21 @@
 require './test/test_helper'
 require './lib/invoice_repository'
+require './lib/sales_engine'
 
 class InvoiceRepositoryTest < Minitest::Test
   def setup
-		@invoice_eng = InvoiceEngine.from_csv({:invoices => "./data/invoices.csv"})
-		invoice = se.invoices.find_by_id(6)
+    file_name   = "./data/invoices.csv"
+    @invoice_repo = InvoiceRepository.new(file_name)
   end
 
 	def test_it_exists
-		assert_instance_of InvoiceRepository, @invoice_eng 
+	assert_instance_of InvoiceRepository, @invoice_repo 
 	end
+
+	def	test_it_finds_invoice_id
+    assert_nil @invoice_repo.find_by_id(6)
+    assert_instance_of Merchant, @merch_repo.find_by_id('12334105')
+    assert_equal 'Shopin1901', @merch_repo.find_by_id('12334105').name
+  end
 
 end
