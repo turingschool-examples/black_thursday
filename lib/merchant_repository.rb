545b71238904feabafed5fb739799d205e6a6cc8
JNelson <nelson.jimmy@gmail.com>
2018-02-20 13:34:35 -0700

    fixing missing merchant_repo file

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,40 @@
+require 'CSV'
+require 'pry'
+require_relative 'merchant'
+
+# Merchant Repository gets data from CSV
+class MerchantRepository
+  def initialize(filepath)
+    @merchants = []
+    load_merchants(filepath)
+  end
+
+  def all
+    @merchants
+  end
+
+  def load_merchants(filepath)
+    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
+      @merchants << Merchant.new(data)
+    end
+  end
+
+  def find_by_id(id)
+    @merchants.find do |merchant|
+      merchant.id == id
+    end
+  end
+
+  def find_by_name(name)
+    @merchants.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
+  def find_all_by_name(segment)
+    segment = segment.downcase
+    @merchants.select do |merchant|
+      merchant.name.downcase.include?(segment)
+    end
+  end
+end
