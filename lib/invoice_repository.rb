d4e3dc1a0e7b2d5c45f419619c7727e54ddece8f
Eliot Swank <eliotswank@gmail.com>
2018-02-20 11:26:00 -0700

    work on iteration 2--invoice repo

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,3 +1,9 @@
+require_relative 'searching'
+# require_relative 'invoice'
+
 class InvoiceRepository
-    
+  include Searching
+	attr_reader :all
+
+
 end
--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -2,11 +2,13 @@ require './test/test_helper'
 require './lib/invoice_repository'
 
 class InvoiceRepositoryTest < Minitest::Test
+  def setup
+		@invoice_eng = InvoiceEngine.from_csv({:invoices => "./data/invoices.csv"})
+		invoice = se.invoices.find_by_id(6)
+  end
 
 	def test_it_exists
-		invoice = InvoiceRepository.new
-
-		assert_instance_of invoice, InvoiceRepository
+		assert_instance_of InvoiceRepository, @invoice_eng 
 	end
 
 end
