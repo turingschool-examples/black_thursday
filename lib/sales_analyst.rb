da67f26d62c330768cda1366b677dcc1dae3f3d0
Megan Arellano <ssciolist@gmail.com>
2018-02-20 14:19:14 -0700

    minor aver_items_per_merchant refactor

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,4 +1,4 @@
-require 'ruby_native_statistics'
+
 require 'bigdecimal'
 require_relative 'sales_engine.rb'
 
@@ -9,25 +9,33 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-    all_merchants = merchant_collector
-    total_items = all_merchants.map do |merchant|
-      merchant.items.length
-    end.sum.to_f
-    (total_items / all_merchants.length).round(2)
+    all_merchants = merchant_collector.length
+    total_items = item_collector.length
+    (total_items.to_f / all_merchants).round(2)
   end
 
-  def average_items_per_merchant_standard_deviation
-    merchant_collector.map do |merchant|
-      merchant.items.length
-    end.stdev.round(2)
-  end
+  # def average_items_per_merchant
+  #   all_merchants = merchant_collector
+  #   total_items = all_merchants.map do |merchant|
+  #     merchant.items.length
+  #   end.sum.to_f
+  #   (total_items / all_merchants.length).round(2)
+  # end
 
-  def merchants_with_high_item_count
-    avg_item_std_dev = average_items_per_merchant_standard_deviation
-    merchant_collector.map do |merchant|
-      merchant if (merchant.items.length - avg_item_std_dev) > avg_item_std_dev
-    end.compact
-  end
+
+
+  # def average_items_per_merchant_standard_deviation
+  #   merchant_collector.map do |merchant|
+  #     merchant.items.length
+  #   end.stdev.round(2)
+  # end
+
+  # def merchants_with_high_item_count
+  #   avg_item_std_dev = average_items_per_merchant_standard_deviation
+  #   merchant_collector.map do |merchant|
+  #     merchant if (merchant.items.length - avg_item_std_dev) > avg_item_std_dev
+  #   end.compact
+  # end
 
   def average_item_price_for_merchant(merch_id)
     merch_items = engine.merchants.find_by_id(merch_id).items
@@ -55,13 +63,14 @@ class SalesAnalyst
     engine.merchants.all
   end
 
+
   def item_collector
     engine.items.all
   end
 
-  def price_standard_deviation
-    item_collector.map do |item|
-      item.unit_price.truncate
-    end.stdev.round(2)
-  end
+  # def price_standard_deviation
+  #   item_collector.map do |item|
+  #     item.unit_price.truncate
+  #   end.stdev.round(2)
+  # end
 end
