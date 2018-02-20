24aa22d9e1832e60aedbee4e5adce7fd56bf051f
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-20 10:36:19 -0700

    Added item info tests

--- test/item_test.rb
+++ test/item_test.rb
@@ -15,4 +15,21 @@ class ItemTest < Minitest::Test
 
     assert_instance_of Item, item
   end
+
+  def test_does_store_item_info
+    item = Item.new(
+      name: 'Pencil',
+      description: 'You can use it to write things',
+      unit_price: BigDecimal.new(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now
+    )
+
+    assert_equal 0, item.id
+    assert_equal 'Pencil', item.name
+    assert_equal 'You can use it to write things', item.description
+    assert_equal 0.1099e2, item.unit_price
+    assert_equal Time.now, item.created_at
+    assert_equal Time.now, item.updated_at
+  end
 end
