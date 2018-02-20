dc6383eeaaec389f479bdd0639f04b3b0149f9e5
VadLusk <AdLusk@gmail.com>
2018-02-19 18:12:19 -0700

    Psuedocode merch repo test.

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -2,9 +2,36 @@ require './test/test_helper'
 require './lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
-  def test_merchant_repository_class_exists
-    merch_repo = MerchantRepository.new("./data/sample_data/merchants.csv")
+  def setup
+    file_name   = "./data/sample_data/merchants.csv"
+    @merch_repo = MerchantRepository.new(file_name)
+  end
 
-    assert_instance_of MerchantRepository, merch_repo
+  def test_merchant_repository_class_exists
+    assert_instance_of MerchantRepository, @merch_repo
+  end
+
+  def test_all_method
+#   all - returns an array of all known Merchant instances
+    skip
+  end
+
+  def test_find_by_id_method
+#   find_by_id - returns either nil
+#   or an instance of Merchant with a matching ID
+    skip
+  end
+
+  def test_find_by_name
+#   find_by_name - returns either nil
+#   or an instance of Merchant having done
+#   a case insensitive search
+    skip
+  end
+
+  def test_find_all_by_name
+#   find_all_by_name - returns either [] or one or more matches
+#   which contain the supplied name fragment, case insensitive
+    skip
   end
 end
