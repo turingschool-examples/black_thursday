6b5e36fa8882306e4e79e3360c5a827ffc6d6b08
hmesander <haleymesander@gmail.com>
2018-02-20 09:59:22 -0700

    Completes tests for sales analyst - iteration 1

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,5 +1,7 @@
 require 'bigdecimal'
+require 'ruby_native_statistics'
 require 'descriptive_statistics'
+require 'pry'
 
 class SalesAnalyst
   attr_reader :sales_engine
@@ -18,19 +20,8 @@ class SalesAnalyst
     items_per_merchant.mean
   end
 
-  def differences_squared
-    items_per_merchant.map do |num|
-      (num - average_items_per_merchant)**2
-    end
-  end
-
-  def sum_of_differences_squared
-    differences_squared.inject(0) { |sum, num| sum + num }
-  end
-
   def average_items_per_merchant_standard_deviation
-    divisor = items_per_merchant.count - 1
-    Math.sqrt(sum_of_differences_squared / divisor).round(2)
+    items_per_merchant.stdev.round(2)
   end
 
   def merchants_with_high_item_count
@@ -44,16 +35,41 @@ class SalesAnalyst
   def average_item_price_for_merchant(merchant_id)
     merchant = @sales_engine.merchants.find_by_id(merchant_id)
     merchant_items = merchant.items
-    item_average_price = merchant_items.map(&:unit_price).mean
-    BigDecimal.new item_average_price.to_i
+    item_prices = merchant_items.map(&:unit_price)
+    item_average_price = item_prices.mean
+    BigDecimal.new(item_average_price, 6)
   end
 
   def average_average_price_per_merchant
     merchants = @sales_engine.merchants.all
     merchant_average_price = merchants.map do |merchant|
+      if merchant.items.empty?
+        0
+      else
       average_item_price_for_merchant(merchant.id)
+      end
     end.mean
+    BigDecimal.new(merchant_average_price, 6)
+  end
 
-    BigDecimal.new(merchant_average_price.to_s, 0)
+  def item_unit_prices
+    items = @sales_engine.items.all
+    items.map(&:unit_price)
+  end
+
+  def average_item_price
+    item_unit_prices.mean.round(2)
+  end
+
+  def item_price_standard_deviation
+    item_unit_prices.stdev.round(2)
+  end
+
+  def golden_items
+    zipped = item_unit_prices.zip(@sales_engine.items.all)
+    average = average_item_price
+    stdev = item_price_standard_deviation
+    found = zipped.find_all { |item| item[0] > (average + (stdev * 2)) }
+    found.map { |item| item[1] }
   end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -42,29 +42,43 @@ class SalesAnalystTest < Minitest::Test
     actual = @sales_analyst.average_item_price_for_merchant(12334185)
 
     assert actual.is_a?(BigDecimal)
-    assert_equal 0.1116e4, actual
+    assert_equal 0.111667e4, actual
   end
 
   def test_for_average_average_price_per_merchant
     actual = @sales_analyst.average_average_price_per_merchant
 
     assert actual.is_a?(BigDecimal)
-    assert_equal 0.34075e3, actual
+    assert_equal 0.340784e3, actual
+  end
+
+  def test_for_item_unit_prices
+    expected = [0.12e4, 0.13e4, 0.135e4, 0.7e3, 0.15e4, 0.2999e4,
+                0.149e5, 0.149e4, 0.69e3, 0.4e5, 0.13e5, 0.399e3,
+                0.8e4, 0.6e5, 0.65e5, 0.4e4, 0.239e4, 0.5e5, 0.239e4,
+                0.5e5]
+    actual = @sales_analyst.item_unit_prices
+
+    assert_equal expected, actual
   end
 
   def test_for_average_item_price
-    skip
-    actual = @sales_analyst.average_item_price_standard_deviation
+    actual = @sales_analyst.average_item_price
 
-    assert_equal 5, actual
+    assert_equal 16065.40, actual
+  end
+
+  def test_for_item_price_standard_deviation
+    actual = @sales_analyst.item_price_standard_deviation
+
+    assert_equal 22670.45, actual
   end
 
   def test_for_golden_items
-    skip
     actual = @sales_analyst.golden_items
 
     assert actual.is_a?(Array)
     assert actual[0].is_a?(Item)
-    assert_equal '', actual[0].name
+    assert_equal 'La prière', actual[0].name
   end
 end
