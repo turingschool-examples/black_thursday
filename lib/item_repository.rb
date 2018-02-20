9de22c24974520352d6745f812292e4aa788fb9b
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-20 11:45:16 -0700

    ItemRepository loading/#all tests

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
+require 'csv'
+require_relative 'item'
+
 # Defines ItemRepository, holding a list of Items
 class ItemRepository
   def initialize(filename)
-    
+    load_csv filename
+  end
+
+  def load_csv(filename)
+    @items = []
+    CSV.foreach(
+      filename,
+      headers: true,
+      header_converters: :symbol
+    ) do |item_info|
+      item = Item.new item_info
+      @items.push item
+    end
   end
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -3,14 +3,25 @@
 require_relative 'test_helper'
 
 require 'bigdecimal'
-require_relative '../lib/item_repository.rb'
+require_relative '../lib/item_repository'
+require_relative '../lib/item'
 
 class ItemReposityTest < Minitest::Test
   def setup
-    @ir = ItemRepository.new './data/items.csv'
+    @ir = ItemRepository.new './test/fixtures/items.csv'
   end
 
   def test_does_create_repository
     assert_instance_of ItemRepository, @ir
+  end
+
+  def test_did_load_items
+    items = @ir.all
+    assert_instance_of Array, items
+    items.each do |item|
+      assert_instance_of Item, item
+    end
+
+    assert_equal 'Desc A', items[0].description
   end
 end
