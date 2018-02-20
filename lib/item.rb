0a28075ac761edebaa036f9c47fea9b974efc5e7
brickstar <brickstar@gmail.com>
2018-02-20 11:46:32 -0700

    spec harness errors resolved

--- lib/item.rb
+++ lib/item.rb
@@ -16,8 +16,8 @@ class Item
     @description = data[:description]
     @unit_price = BigDecimal.new(data[:unit_price]) / 100
     @merchant_id = data[:merchant_id]
-    @created_at = Time.new(data[:created_at])
-    @updated_at = Time.new(data[:updated_at])
+    @created_at = Time.parse(data[:created_at])
+    @updated_at = Time.parse(data[:updated_at])
   end
 
   def unit_price_to_dollars
