2e4d8f3e78b4b5bddfea8ca8235beee0a948a5bd
VadLusk <AdLusk@gmail.com>
2018-02-20 00:09:15 -0700

    Flesh out tests. Assertions in progress.

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -15,37 +15,49 @@ class ItemRepositoryTest < Minitest::Test
     skip
     assert_instance_of Array, @item_repo.all
     assert_instance_of Item, @item_repo.all.first
-    assert_equal '12334105', @item_repo.all.first.id
-    assert_equal 'LolaMarleys', @item_repo.all.last.name
+    assert_equal '1', @item_repo.all.first.id
+    assert_equal 'Eraser', @item_repo.all.last.name
   end
 
   def test_find_by_id_method
     skip
-    assert_nil @merch_repo.find_by_id('8')
-    assert_instance_of Merchant, @merch_repo.find_by_id('12334105')
-    assert_equal 'Shopin1901', @merch_repo.find_by_id('12334105').name
+    assert_nil @item_repo.find_by_id('8')
+    assert_instance_of Item, @item_repo.find_by_id('2')
+    assert_equal 'Pen', @item_repo.find_by_id('3').name
   end
 
   def test_find_by_name
-    assert_nil @merch_repo.find_by_name('Satisfaction')
-    assert_instance_of Merchant, @merch_repo.find_by_name('LolaMarleys')
-    assert_instance_of Merchant, @merch_repo.find_by_name('lolAmarLeYs')
-    assert_equal '12334115', @merch_repo.find_by_name('lolAmarLeYs').id
+    skip
+    assert_nil @item_repo.find_by_name('Satisfaction')
+    assert_instance_of Item, @item_repo.find_by_name('Pencil')
+    assert_instance_of Item, @item_repo.find_by_name('pEnCiL')
+    assert_equal '1', @item_repo.find_by_name('pEnCiL').id
   end
 
-
-# find_all_with_description - returns either []
+  def test_find_all_with_description
+    skip
+    assert_equal [], @item_repo.find_all_with_description("qwijybo")
 # or instances of Item where the supplied string appears
 # in the item description (case insensitive)
+  end
 
-# find_all_by_price - returns either []
+  def test_find_all_by_price
+    skip
+    assert_equal [], @item_repo.find_all_by_price("0012")
 # or instances of Item where the supplied price exactly matches
+  end
 
-# find_all_by_price_in_range - returns either []
+  def test_find_all_by_price_in_range
+    skip
+    assert_equal [], @item_repo.find_all_by_price_in_range("0012")
 # or instances of Item where the supplied price is
 # in the supplied range (a single Ruby range instance is passed in)
+  end
 
-# find_all_by_merchant_id - returns either []
+  def test_find_all_by_merchant_id
+    skip
+    assert_equal [], @item_repo.find_all_by_merchant_id("2")
 # or instances of Item where the supplied merchant ID
 # matches that supplied
+  end
 end
