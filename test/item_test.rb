d515c63ac59809833522df9d967926a142bbc4fd
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-20 10:32:09 -0700

    Fixed tests

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,10 +1,17 @@
 require_relative 'test_helper'
 
+require 'bigdecimal'
 require_relative '../lib/item.rb'
 
 class ItemTest < Minitest::Test
   def test_does_create_item
-    item = Item.new
+    item = Item.new(
+      name: 'Pencil',
+      description: 'You can use it to write things',
+      unit_price: BigDecimal.new(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now
+    )
 
     assert_instance_of Item, item
   end
