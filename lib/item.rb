40f8ddb516c9c7c29f37ea99b943f3e34ca732f3
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 13:05:26 -0700

    More hound fixes

--- lib/item.rb
+++ lib/item.rb
@@ -17,10 +17,14 @@ class Item
     @id = data[:id].to_i
     @name = data[:name]
     @description = data[:description]
-    @unit_price = BigDecimal.new(data[:unit_price].to_i) / 100.0
+    @unit_price = price_to_big_decimal data[:unit_price]
     @created_at = Time.parse data[:created_at]
     @updated_at = Time.parse data[:updated_at]
     @merchant_id = data[:merchant_id].to_i
     @item_repository = item_repository
+  end
+
+  def price_to_big_decimal(price)
+    BigDecimal.new(price.to_i) / 100.0
   end
 end
