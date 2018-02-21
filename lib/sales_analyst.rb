097cecaff918433dc3a590203bc318943e85d716
VadLusk <AdLusk@gmail.com>
2018-02-20 21:54:58 -0700

    Add avg items per merch method.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,7 +1,10 @@
+#Business metadata calculator
 class SalesAnalyst
   def initialize(sales_eng)
+    @se = sales_eng
   end
 
   def average_items_per_merchant
+    (@se.items.all.length.to_f / @se.merchants.all.length).round(2)
   end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,11 +1,13 @@
 require './test/test_helper'
-require './test/fixtures/mock_sales_engine'
+require './lib/sales_engine'
 require './lib/sales_analyst'
 
 class SalesAnalystTest < Minitest::Test
   def setup
-    sales_eng = MockSalesEngine.new
-    @sa       = SalesAnalyst.new(sales_eng)
+    repositories = {items: './data/items.csv',
+                    merchants: './data/merchants.csv'}
+    sales_eng    = SalesEngine.new(repositories)
+    @sa          = SalesAnalyst.new(sales_eng)
   end
 
   def test_sales_analyst_class_exists
@@ -13,36 +15,6 @@ class SalesAnalystTest < Minitest::Test
   end
 
   def test_average_items_per_merchant
-    skip
     assert_equal 2.88, @sa.average_items_per_merchant
-  end
-
-  def test_average_items_per_merchant_standard_deviation
-    skip
-    actual = @sa.average_items_per_merchant_standard_deviation
-    assert_equal 3.26, actual
-  end
-
-  def test_merchants_with_high_item_count
-    skip
-    actual = @sa.merchants_with_high_item_count
-  # [merchant, merchant, merchant]
-  end
-
-  def test_average_item_price_for_merchant
-    skip
-    actual = @sa.average_item_price_for_merchant(12334159)
-  #  # => BigDecimal
-  end
-
-  def test_average_average_price_per_merchant
-    skip
-    actual = @sa.average_average_price_per_merchant
-  #  # => BigDecimal
-  end
-
-  def test_golden_items
-    skip
-  # sa.golden_items # => [<item>, <item>, <item>, <item>]
   end
 end
