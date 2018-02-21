6b726c66c2634c8e5a23cf8026db66af13cef797
brickstar <brickstar@gmail.com>
2018-02-21 08:15:54 -0700

    invoice repsitory and invoice class initial commit

---
+++ test/invoice_repository_test.rb
@@ -0,0 +1,25 @@
+require_relative 'test_helper'
+require_relative '../lib/invoice_repository'
+require_relative '../lib/sales_engine'
+require 'bigdecimal'
+require 'pry'
+
+# test for item class
+class InvoiceRepositoryTest < Minitest::Test
+  def setup
+    invoice_repo = InvoiceRepository.new('./test/fixtures/truncated_invoices.csv',
+                                          'parent')
+  end
+
+  def test_it_exits
+    assert_instance_of InvoiceRepository, invoice_repo
+  end
+
+  def test_it_has_invoices
+    assert_instance_of Array, invoice_repo.all
+    assert_equal 9, invoice_repo.length
+    assert_instance_of Invoice, invoice_repo[0]
+  end
+
+  
+end
