98980b7736820a08cbdcb7b3b020535fe627321c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 13:03:53 -0700

    Hound fix

--- test/mocks/test_engine.rb
+++ test/mocks/test_engine.rb
@@ -4,7 +4,9 @@ require_relative '../../lib/sales_engine'
 require_relative '../../lib/item_repository'
 require_relative '../../lib/merchant_repository'
 
-MOCK_MERCHANT_REPOSITORY = MerchantRepository.new './test/fixtures/merchants.csv'
+MOCK_MERCHANT_REPOSITORY = MerchantRepository.new(
+  './test/fixtures/merchants.csv'
+)
 MOCK_ITEM_REPOSITORY = ItemRepository.new './test/fixtures/items.csv'
 
 MOCK_SALES_ENGINE = SalesEngine.new(
