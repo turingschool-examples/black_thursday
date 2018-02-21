f33646a9e3e00a4f87b063b80feb082fcb9d2f22
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-21 13:37:06 -0700

    Sales analyst class tests

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,8 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/sales_analyst'
-
+require_relative '../lib/sales_engine'
+require_relative 'mocks/test_engine'
 
 class SalesAnalystTest < Minitest::Test
+  def setup
+    @sa = SalesAnalyst.new MOCK_SALES_ENGINE
+  end
 
+  def test_does_create_sales_analyst
+    assert_instance_of SalesAnalyst, @sa
+  end
+
+  def test_does_have_sales_engine
+    assert_instance_of SalesEngine, @sa.sales_engine
+  end
 end
