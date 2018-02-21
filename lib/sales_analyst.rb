9c684be093d15833a8d70eb36267216579ca471c
VadLusk <AdLusk@gmail.com>
2018-02-21 00:56:41 -0700

    Add std deviation method.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -8,8 +8,24 @@ class SalesAnalyst
     (@se.items.all.length.to_f / @se.merchants.all.length).round(2)
   end
 
-  def average_items_per_merchant_standard_deviation
+  def items_for_each_merchant
+    @se.merchants.all.map do |obj|
+      @se.find_merchant_items(obj.id).length
+    end
+  end
 
+  def square_differences
+    items_for_each_merchant.map do |num|
+      (num - average_items_per_merchant) ** 2
+    end
+  end
+
+  def divided_sum
+    square_differences.reduce(:+) / (@se.merchants.all.length - 1)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    Math.sqrt(divided_sum).round(2)
   end
 
   def merchants_with_high_item_count
@@ -30,6 +46,6 @@ class SalesAnalyst
     (total_avg_price.reduce(:+) / @se.merchants.all.length).round(2)
   end
 
-  # def golden_items
-  # end
+  def golden_items
+  end
 end
