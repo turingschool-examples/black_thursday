6a39946b9cda6590114914ef5f601bac8c661c2c
lilwillifo <margaretwilliford4@gmail.com>
2018-02-20 21:29:38 -0700

    refactor sales analyst for readability, still needs unit testing

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -8,34 +8,39 @@ class SalesAnalyst
     @sales_engine = se
   end
 
-  def all_merchants
+  def merchants
     @sales_engine.merchants.all
   end
 
+  def items
+    @sales_engine.items.all
+  end
+
+  # I think we can break out average and std deviation into a module
+  def average(numerator, denominator)
+    (BigDecimal(numerator) / BigDecimal(denominator)).round(2)
+  end
+
   def average_items_per_merchant
-    @total = 0
-    all_merchants.map do |merchant|
-      @total += merchant.items.length
-    end
-    ((@total.to_f / all_merchants.length.to_f) * 100).round / 100.0
+    average(items.length, merchants.length)
   end
 
   def average_items_per_merchant_standard_deviation
     squares = number_of_items_for_every_merchant.map do |num|
       (num - average_items_per_merchant)**2
     end
-    chaos = squares.sum / (all_merchants.length - 1)
+    chaos = squares.sum / (merchants.length - 1)
     Math.sqrt(chaos)
   end
 
   def number_of_items_for_every_merchant
-    all_merchants.map do |merchant|
+    merchants.map do |merchant|
       merchant.items.length
     end
   end
 
   def merchants_with_high_item_count
-    all_merchants.collect do |merchant|
+    merchants.collect do |merchant|
       difference = (merchant.items.length - average_items_per_merchant)
       merchant if difference > average_items_per_merchant_standard_deviation
     end.compact
@@ -43,10 +48,9 @@ class SalesAnalyst
 
   def average_item_price_for_merchant(id)
     return 0 if find_items_with_merchant_id(id).length.zero?
-    result = @sales_engine.pass_id_to_item_repo(id).map do |item|
-      item.unit_price
-    end.sum
-    BigDecimal.new((result / find_items_with_merchant_id(id).length).to_s).round(2)
+    items = @sales_engine.pass_id_to_item_repo(id)
+    prices = items.map(&:unit_price)
+    average(prices.reduce(:+), items.length)
   end
 
   def find_items_with_merchant_id(id)
@@ -54,10 +58,10 @@ class SalesAnalyst
   end
 
   def average_average_price_per_merchant
-    result = all_merchants.map do |merchant|
-      average_item_price_for_merchant(merchant.id)
+    result = merchants.reduce(0) do |sum, merchant|
+      sum + average_item_price_for_merchant(merchant.id)
     end
-    (result.sum / result.length).round(2)
+    average(result, merchants.length)
   end
 
   def golden_items
