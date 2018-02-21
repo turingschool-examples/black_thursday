e2840f347d5f0c53fb613cfb56be816adef52d48
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-21 14:43:52 -0700

    Update master_hash.rb

--- test/master_hash.rb
+++ test/master_hash.rb
@@ -1,6 +1,6 @@
 class TestEngine
   def god_hash
-    @god_hash = {
+    {
     items: './test/fixtures/items.csv',
     merchants: './test/fixtures/merchants_fix.csv',
     invoices: './test/fixtures/invoices.csv'
