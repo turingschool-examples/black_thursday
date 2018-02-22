a56cd1c74829761912d37852d7737c51ea91042b
JNelson <nelson.jimmy@gmail.com>
2018-02-22 10:49:33 -0700

    working on invoice repository

---
+++ lib/invoice_repository.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'csv'
+require_relative 'invoice'
+
+# Defines Invoice Repository
+class InvoiceRepository
+  def initialize(filename)
+    @invoices = []
+    load_invoices(filename)
+  end
+
+  def load_invoices(filename)
+    CSV.foreach(filename, headers: true, header_converters: :symbol) do |data|
+      @invoices << Invoice.new(data)
+    end
+  end
+
+end
---
+++ test/invoice_repository_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+require_relative '../lib/invoice_repository'
+
+class InvoiceRepositoryTest < Minitest::Test
+  def setup
+    @invoice_repo = InvoiceRepository.new './test/fixtures/invoices.csv'
+  end
+
+  def test_does_create_repository
+    assert_instance_of InvoiceRepository, @invoice_repo
+  end
+
+  def test_loads_invoices
+    asssert_equal 10, @invoice_repo.all.count
+    assert_instance_of Array, @invoice_repo.all
+    @invoice_repo.all.each do |invoice|
+      asssert_instance_of Invoice, invoice
+    end
+    assert_equal 1, @invoice_repo.all.first.customer_id
+    assert_equal 3, @invoice_repo.all.first.merchant_id
+  end
+
+end
