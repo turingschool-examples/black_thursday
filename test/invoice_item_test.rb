b29f969e352e150f31e94c6bee1bb5410e9e7242
brickstar <brickstar@gmail.com>
2018-02-21 23:04:58 -0700

    new hash syntax fix invoice item test

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -8,13 +8,13 @@ require 'pry'
 
 class InvoiceItemTest < Minitest::Test
   def setup
-    @ii = InvoiceItem.new({ :id => 6,
-                            :item_id => 7,
-                            :invoice_id => 8,
-                            :quantity => 1,
-                            :unit_price => BigDecimal.new(10.99, 4),
-                            :created_at => "2012-03-27 14:54:09 UTC",
-                            :updated_at => "2012-03-27 14:54:09 UTC"},
+    @ii = InvoiceItem.new({ id: 6,
+                            item_id: 7,
+                            invoice_id: 8,
+                            quantity: 1,
+                            unit_price: BigDecimal.new(10.99, 4),
+                            created_at: "2012-03-27 14:54:09 UTC",
+                            updated_at: "2012-03-27 14:54:09 UTC"},
                           'parent' )
   end
 
