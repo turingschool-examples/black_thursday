6f1112bb47949b9b319387997345ad537ce64eb9
jrambold <jrambold@gmail.com>
2018-02-22 10:08:05 -0700

    sales analyst with std dev added

---
+++ lib/sales_analyst.rb
@@ -0,0 +1,35 @@
+class SalesAnalyst
+  def initialize(sales_engine)
+    @se = sales_engine
+  end
+
+  def average_items_per_merchant
+    mean_items_per_merchant(items_count_per_merchant)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    num_items = items_count_per_merchant
+    mean = mean_items_per_merchant(num_items)
+    std_dev = 0.0
+    num_items.each do |count|
+      std_dev += (count - mean)**2
+    end
+    std_dev /= num_items.length - 1
+    Math.sqrt(std_dev)
+  end
+
+  def mean_items_per_merchant(item_array)
+    mean = 0.0
+    item_array.each do |count|
+      mean += count
+    end
+    mean / item_array.length
+  end
+
+  def items_count_per_merchant
+    @se.merchants.all.map do |merchant|
+      @se.find_items_by_merchant_id(merchant.id).length
+    end
+  end
+
+end
