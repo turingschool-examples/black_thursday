fffae0e59700e7f557d18d396ed5c539a7dcd5f2
Evan Wheeler <anon0mys87@gmail.com>
2018-02-22 09:35:17 -0700

    Hound and style edits

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -5,18 +5,18 @@ require './lib/searching'
 
 class InvoiceRepositoryTest < Minitest::Test
   def setup
-    file_name   = './data/sample_data/invoices.csv'
-		@invoice_repo = InvoiceRepository.new(file_name)
+    file_name = './data/sample_data/invoices.csv'
+    @invoice_repo = InvoiceRepository.new(file_name)
   end
 
-	def test_it_exists
-		assert_instance_of InvoiceRepository, @invoice_repo
-	end
+  def test_it_exists
+    assert_instance_of InvoiceRepository, @invoice_repo
+  end
 
-	def	test_it_finds_invoice_id
-		assert_instance_of Array, @invoice_repo.all
-		assert_nil @invoice_repo.find_by_id(10)
-		assert_instance_of Invoice, @invoice_repo.find_by_id(4)
+  def	test_it_finds_invoice_id
+    assert_instance_of Array, @invoice_repo.all
+    assert_nil @invoice_repo.find_by_id(10)
+    assert_instance_of Invoice, @invoice_repo.find_by_id(4)
   end
 
   def test_it_can_find_all_by_customer_id
@@ -31,5 +31,10 @@ class InvoiceRepositoryTest < Minitest::Test
     assert_instance_of Array, @invoice_repo.find_all_by_status('shipped')
     assert_equal 2, @invoice_repo.find_all_by_status('pending').length
     assert_equal 2, @invoice_repo.find_all_by_status('shipped').length
+  end
+
+  def test_inspect
+    assert_equal @invoice_repo, @invoice_repo.inspect
+    refute @invoice_repo.inspect.to_s.length > 50
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -11,8 +11,8 @@ class ItemTest < Minitest::Test
                        description: 'You can use it to write things',
                        unit_price: BigDecimal.new(10.99, 4),
                        merchant_id: 6,
-                       created_at: Time.now.to_s,
-                       updated_at: Time.now.to_s }, mock_repo)
+                       created_at: '2016-01-11 09:34:06 UTC',
+                       updated_at: '2016-01-11 09:34:06 UTC' }, mock_repo)
   end
 
   def test_item_class_exists
@@ -21,22 +21,22 @@ class ItemTest < Minitest::Test
 
   def test_item_attributes
     expected = 'You can use it to write things'
-    # expected2 = Date.today.strftime('%Y-%m-%d %H:%M:%S')
+    expected2 = Time.parse('2016-01-11 09:34:06 UTC')
 
     assert_equal 5, @item.id
     assert_equal 'Pencil', @item.name
     assert_equal expected, @item.description
     assert_equal 0.1099e0, @item.unit_price
     assert_equal 6, @item.merchant_id
-    # assert_equal expected2, @item.created_at
-    # assert_equal expected2, @item.updated_at
+    assert_equal expected2, @item.created_at
+    assert_equal expected2, @item.updated_at
   end
 
   def test_item_can_have_different_attributes
     item = Item.new({ id: 1,
                       name: 'Wine',
                       description: 'It gets you drunk',
-                      unit_price: BigDecimal.new(79.59, 4),
+                      unit_price: '7959',
                       merchant_id: 343_414,
                       created_at: Time.now.to_s,
                       updated_at: Time.now.to_s }, 'parent')
@@ -45,7 +45,7 @@ class ItemTest < Minitest::Test
     assert_equal 'Wine', item.name
     assert_equal 'It gets you drunk', item.description
     assert_equal 343_414, item.merchant_id
-    # assert_equal 0.7959e0, item.unit_price
+    assert_equal BigDecimal.new(79.59, 4), item.unit_price
   end
 
   def test_unit_price_to_dollars
