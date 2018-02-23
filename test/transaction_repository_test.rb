5f42200d6d2a8deefeefdb5fa11d253b8d7a289c
lilwillifo <margaretwilliford4@gmail.com>
2018-02-22 20:53:41 -0700

    transaction passes iteration 3 spec harness

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -25,8 +25,8 @@ class TransactionRepositoryTest < Minitest::Test
   end
 
   def test_can_find_all_by_credit_card_number
-    assert_equal [], @transaction_repo.find_all_by_credit_card_number("493828384882123423")
-    assert_equal 2179, @transaction_repo.find_all_by_credit_card_number("4068631943231473").first.invoice_id
+    assert_equal [], @transaction_repo.find_all_by_credit_card_number(493828384882123423)
+    assert_equal 2179, @transaction_repo.find_all_by_credit_card_number(4068631943231473).first.invoice_id
   end
 
   def test_can_find_all_by_result
--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -20,7 +20,7 @@ class TransactionTest < Minitest::Test
   def test_class_has_attributes_from_data
     assert_equal 6, @transaction.id
     assert_equal 8, @transaction.invoice_id
-    assert_equal '4242424242424242', @transaction.credit_card_number
+    assert_equal 4242424242424242, @transaction.credit_card_number
     assert_equal '0220', @transaction.credit_card_expiration_date
     assert_equal 'success', @transaction.result
     assert_instance_of Time, @transaction.created_at
