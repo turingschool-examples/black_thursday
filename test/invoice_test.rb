fe2a3d4b6dc626fb983b325b721d75b46c12a9a7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-02-22 20:26:31 -0700

    Invoice take instance of InvoiceRepository

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -4,18 +4,20 @@ require 'time'
 require_relative 'test_helper.rb'
 
 require './lib/invoice.rb'
+require './lib/invoice_repository'
+require './test/mocks/test_engine'
 
 class InvoiceTest < Minitest::Test
   TIME = Time.parse '2016-01-11 17:42:32 UTC'
   def setup
-    @invoice = Invoice.new(
+    @invoice = Invoice.new({
       id: 6,
       customer_id: 7,
       merchant_id: 8,
       status: 'pending',
       created_at: '2016-01-11 17:42:32 UTC',
       updated_at: '2016-01-11 17:42:32 UTC'
-    )
+    }, MOCK_INVOICE_REPOSITORY)
   end
 
   def test_does_create_invoice
@@ -29,5 +31,9 @@ class InvoiceTest < Minitest::Test
     assert_equal :pending, @invoice.status
     assert_equal TIME, @invoice.created_at
     assert_equal TIME, @invoice.updated_at
+  end
+
+  def test_can_get_merchant
+    assert_equal 8, @invoice.merchant.id
   end
 end
