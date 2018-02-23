b13752d5508c8ea0d598bf6072195a36f849cde9
Eliot Swank <eliotswank@gmail.com>
2018-02-23 11:17:57 -0700

    continue work on invoice_item. Almost done. Missing unit_price_to_dollars method.

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,8 +1,20 @@
 class InvoiceItem
-  attr_reader :id
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
 
   def initialize(data)
-    @id = data[:id].to_i
-  end 
+    @id         = data[:id].to_i
+    @item_id    = data[:item_id]
+    @invoice_id = data[:invoice_id]
+    @quantity   = data[:quantity]
+    @unit_price = data[:unit_price]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
+  end
 
 end
