980854139eeeb5eab6ddea69f58222e38042f58f
jrambold <jrambold@gmail.com>
2018-02-23 12:54:07 -0700

    high item count added

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -33,6 +33,14 @@ class SalesAnalyst
     end
   end
 
+  def merchants_with_high_item_count
+    mean = average_items_per_merchant
+    std_dev = average_items_per_merchant_standard_deviation
+    @se.merchants.all.find_all do |merchant|
+      @se.find_items_by_merchant_id(merchant.id).length > (std_dev + mean)
+    end
+  end
+
   def average_item_price_for_merchant(merchant_id)
     items = @se.find_items_by_merchant_id(merchant_id)
     avg = 0
