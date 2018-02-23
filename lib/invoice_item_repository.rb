2907b7043dc9739e8077c3cc540c70376c6b75a1
Eliot Swank <eliotswank@gmail.com>
2018-02-23 12:23:02 -0700

    work on invoice_item_repo find_all_by_item_id method

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,10 +1,15 @@
 require_relative 'searching'
 
 class InvoiceItemRepository
+  include Searching
   attr_reader :all
 
   def initialize(file_path)
     @all = from_csv(file_path)
+  end
+
+  def add_elements(data)
+    data.map { |row| InvoiceItem.new(row) }
   end
 
   def find_all_by_item_id(id)
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -11,9 +11,9 @@ class InvoiceItemRepositoryTest < Minitest::Test
       assert_instance_of InvoiceItemRepository, @invoice_item_repo
   end
 
-  # def test_it_finds_by_item_id
-  #   assert_instance_of Array, @invoice_item_repo.find_all_by_item_id(10)
-  #   assert_equal 0, @invoice_item_repo.find_all_by_item_id(10).length
-  #   assert_equal 1, @invoice_item_repo.find_all_by_item_id(263454779).length
-  # end
+  def test_it_finds_by_item_id
+    assert_instance_of Array, @invoice_item_repo.find_all_by_item_id(10)
+    assert_equal 0, @invoice_item_repo.find_all_by_item_id(10).length
+    assert_equal 1, @invoice_item_repo.find_all_by_item_id(263454779).length
+  end
 end
