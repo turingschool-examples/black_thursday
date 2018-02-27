0da47632e91082e1a65eb1fbd117addecc3e5c4e
lilwillifo <margaretwilliford4@gmail.com>
2018-02-26 19:40:45 -0700

    unit testing for customer repo

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -12,9 +12,19 @@ class CustomerRepositoryTest < Minitest::Test
     assert_instance_of CustomerRepository, @customer_repo
   end
 
+  def test_it_can_have_a_parent
+    customer_repo = CustomerRepository.new('./test/fixtures/customers.csv', 'parent')
+    assert_equal 'parent', customer_repo.parent
+  end
+
+  def test_inspect_method
+    assert_equal '#<CustomerRepository 5 rows>', @customer_repo.inspect
+  end
+
   def test_class_can_load_customers_and_display_them
     assert_equal 5,    @customer_repo.all.count
     assert_instance_of Customer, @customer_repo.all.first
+    assert_nil         @customer_repo.load_customers('./test/fixtures/customers.csv')
   end
 
   def test_class_can_find_by_id
@@ -37,5 +47,12 @@ class CustomerRepositoryTest < Minitest::Test
     parent.stubs(:pass_customer_id_to_invoices).returns('invoice')
     customer_repo = CustomerRepository.new('./test/fixtures/customers.csv', parent)
     assert_equal customer_repo.pass_customer_id_to_se(2), parent.pass_customer_id_to_invoices(2)
+  end
+
+  def test_it_can_pass_merchant_id
+    parent = mock
+    parent.stubs(:pass_merchant_id_to_merchant_repo).returns('merchant')
+    customer_repo = CustomerRepository.new('./test/fixtures/customers.csv', parent)
+    assert_equal customer_repo.pass_merchant_id_to_se(2), parent.pass_merchant_id_to_merchant_repo(2)
   end
 end
