a616556b9da4711be9ad6ded06b8f567cac680c5
Judson Stevens <Judson.Stevens2gmail.com>
2018-04-06 20:50:07 -0600

    Introduction of change module and working on item repo, WIP

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -5,11 +5,29 @@
 # row, denoted here by the item variable, we insantiate a new item object that
 # includes a reference to it's parent. We store this list in the item_list
 # isntance variable, allowing us to reference the list outside of this class.
+# The list is stored as an array.
 class ItemRepository
   attr_reader :item_list,
               :parent
   def initialize(items, parent)
-    @item_list = items.each { |item| Item.new(item, self) }
+    @item_list = items.map { |item| Item.new(item, self) }
     @parent = parent
   end
+
+  def find_all_by_description
+  end
+
+  def find_all_by_price(price)
+  end
+
+  def find_all_by_price_in_range(range)
+
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+  end
+
+  def update(id, attribute)
+  end
+
 end
