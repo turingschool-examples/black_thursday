8cf47a641ecc40194e40d5a7e38513ffe9c7f967
JamisonOrdway <jamisonordway@gmail.com>
2018-04-07 11:54:19 -0600

    Adds tests

--- test/item_test.rb
+++ test/item_test.rb
@@ -16,4 +16,19 @@ class ItemTest  < Minitest::Test
       assert_instance_of Item, item
   end
 
+  def test_it_has_attributes
+    item = Item.new({
+        :name => 'Pencil',
+        :description => 'Can be used to write things',
+        :unit_price => BigDecimal.new(10.99, 4),
+        :created_at => Time.now,
+        :updated_at => Time.now
+      })
+
+      assert_equal "Pencil", item.name
+      assert_equal "You can use it to write things", item.description
+      assert_equal 10.99, item.unit_price
+      assert_equal Time.now, item.created_at
+      assert_equal Time.now, item.updated_at
+  end
 end
