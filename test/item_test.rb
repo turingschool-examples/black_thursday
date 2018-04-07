b757e020ff8f9b8f100e23efa221dc7d2862f10d
zdcthomas <zdcthomas@yahoo.com>
2018-04-07 10:30:14 -0600

    refactors item test for time variable

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,21 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'bigdecimal'
 require_relative '../lib/item'
 
 class ItemTest < Minitest::Test
+  def setup
+    @time = Time.now
+  end 
   def test_it_exists
     item = Item.new({
       :name        => "Pencil",
       :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
-      :created_at  => Time.now,
-      :updated_at  => Time.now,
+      :unit_price  => BigDecimal.new(10.99, 4),
+      :created_at  => @time,
+      :updated_at  => @time,
     })
 
     assert_equal "Pencil", item.name
-    assert_equal "You can use it to write things". item.description
+    assert_equal "You can use it to write things", item.description
     assert_equal 10.99, item.unit_price
-    assert_equal Time.now, item.created_at
-    assert_equal Time.now, item.updated_at
+    assert_equal @time, item.created_at
+    assert_equal @time, item.updated_at
   end 
 end 
