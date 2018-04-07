47174c5aa4039faf578a636b8074aebc45fa8271
zdcthomas <zdcthomas@yahoo.com>
2018-04-07 10:41:57 -0600

    TESTS merchant class WIP

---
+++ test/merchant_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+require './lib/merchant'
+SimpleCov.start
+class MerchantTest < Minitest::Test
+  def test_it_exists
+    merchant = Merchant.new({:id => 5,:name => "Turing School"})
+    assert_equal 5, merchant.id
+    assert_equal "Turing School", merchant.name
+  end 
+end 
+    
+
