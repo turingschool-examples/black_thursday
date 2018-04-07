bd165b982cd6be7b76c2555ba4e24d3b0228b92f
zdcthomas <zdcthomas@yahoo.com>
2018-04-07 10:31:25 -0600

    Implements item class WIP

---
+++ lib/item.rb
@@ -0,0 +1,13 @@
+class Item
+  attr_reader :name, :description, :unit_price, :created_at, :updated_at
+
+  def initialize(data)
+    @created_at = data[:created_at]
+    @description = data[:description]
+    @id = data[:id]
+    @name = data[:name]
+    @unit_price = data[:unit_price]
+    @updated_at = data[:updated_at]
+  end 
+
+end 
