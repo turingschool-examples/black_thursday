ebbdc8452a2bdc4683b485872bf1bbddbf5cd9de
brickstar <brickstar@gmail.com>
2018-04-07 13:25:13 -0600

    need to switch to proper branch

---
+++ test/item_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+class ItemTest < Minitest::Test
+  def test_it_exists
+    def setup
+      @data = { id: 1,
+                name: 'Pencil',
+                description: 'You can use it to write things',
+                unit_price: 1200,
+                merchant_id: 2,
+                created_at: '2016-01-11 11:51:37 UTC',
+                updated_at: '2016-01-11 11:51:37 UTC' }
+    end
+  end
+end
