34c7504e476207bd118fd5b3fe475d87cd9400cc
zdcthomas <zdcthomas@yahoo.com>
2018-04-07 11:19:18 -0600

    cleans up sytling

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,4 @@
+# class item takes a hash and has attribute readers for :name, :description, :unit_price, :created_at, :updated_at
 class Item
   attr_reader :name, :description, :unit_price, :created_at, :updated_at
 
@@ -8,6 +9,5 @@ class Item
     @name = data[:name]
     @unit_price = data[:unit_price]
     @updated_at = data[:updated_at]
-  end 
-
-end 
+  end
+end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require './test/test_helper'
 require './lib/merchant'
 SimpleCov.start
 class MerchantTest < Minitest::Test
   def test_it_exists
-    merchant = Merchant.new({:id => 5,:name => "Turing School"})
+    merchant = Merchant.new(id: 5, name: 'Turing School')
     assert_equal 5, merchant.id
-    assert_equal "Turing School", merchant.name
+    assert_equal 'Turing School', merchant.name
   end
 end
