b4aab1e620cc43b81c5d290210a888788dcd419a
Manoj Panta <manojpanta95@gmail.com>
2018-04-06 19:45:32 -0600

    testing merchant repository class

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,8 +1,18 @@
+require 'csv'
+require_relative 'merchant'
 class MerchantRepository
-
   def initialize(path)
-
+    @merchants = []
+    load_path(path)
   end
 
+  def all
+    @merchants
+  end
 
+  def load_path(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |data|
+      @merchants << Merchant.new(data)
+    end
+  end
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -4,8 +4,14 @@ require 'minitest/pride'
 
 class MerchantRepositoryTest< MiniTest::Test
   def test_it_exists
-    merchant_repo = MerchantRepository.new()
+    merchant_repo = MerchantRepository.new("./data/merchants.csv")
     assert_instance_of MerchantRepository, merchant_repo
+  end
+
+  def test_it_can_load_all_data
+    merchant_repo = MerchantRepository.new("./data/merchants.csv")
+    assert_equal 475, merchant_repo.all.count
+    assert_equal "Shopin1901", merchant_repo.all.first.name
   end
 
 end
