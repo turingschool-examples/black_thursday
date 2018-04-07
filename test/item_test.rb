31bc17adf3e469003e2506b8a4dbb968e0ad4c70
JamisonOrdway <jamisonordway@gmail.com>
2018-04-07 11:36:21 -0600

    Updates test file requries

---
+++ test/item_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/item'
+
+class ItemTest  < Minitest::Test
+
+end
---
+++ test/merchant_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/item'
+
+class MerchantTest < Minitest::Test
+
+end
