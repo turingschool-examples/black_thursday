dd89b0a70af686a19bb69e414e1d35b5e4928b86
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-06 22:22:28 -0600

    Adds in tests for the data_file attribute of ItemRepository and MerchantRepository

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -7,10 +7,14 @@ require_relative '../lib/item_repository'
 # Test for Item Repository class
 class ItemRepositoryTest < Minitest::Test
   def setup
-    @item_repository = ItemRepository.new
+    @item_repository = ItemRepository.new('../data/items.csv')
   end
 
   def test_item_repository_exists
     assert_instance_of ItemRepository, @item_repository
+  end
+
+  def test_item_respository_has_data_file_attribute
+    assert_equal '../data/items.csv', @item_repository.data_file
   end
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -6,10 +6,14 @@ require_relative '../lib/'
 
 class MerchantRepositoryTest < Minitest::Test
   def setup
-    @merchant_repository = MerchantRepository.new
+    @merchant_repository = MerchantRepository.new('../data/merchants.csv')
   end
 
   def test_merchant_repository_exists
     assert_instance_of MerchantRepository, @merchant_repository
+  end
+
+  def test_merchant_repository_can_have_data_file_attribute
+    assert_equal '../data/merchatns.csv', @merchant_repository.data_file
   end
 end
