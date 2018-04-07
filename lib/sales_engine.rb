20036f6b9f12944a000a1a0e5b871627a4127a7d
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-06 22:28:55 -0600

    Adds ItemRepository and MerchantRepository assignments into from_csv method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,13 +5,14 @@ class SalesEngine
   attr_reader :items,
               :merchants
 
-  def initialize(items_and_merchants = nil)
-    @items = items_and_merchants[:items]
-    @merchants = items_and_merchants[:merchants]
+  def initialize(item_repo, merchant_repo)
+    @items = item_repo
+    @merchants = merchant_repo
   end
 
   def self.from_csv(items_and_merchants)
-    items_repo = ItemRepository.new()
-    new(items_and_merchants)
+    items_repo = ItemRepository.new(items_and_merchants[:items])
+    merchants_repo = MerchantRepository.new(items_and_merchants[:merchants])
+    new(items_repo, merchants_repo)
   end
 end
