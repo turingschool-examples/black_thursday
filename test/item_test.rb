196107783b01dd796cb4f82b5585f8298c216184
Tyler Westlie <tyler.westlie@gmail.com>
2018-04-07 15:05:56 -0600

    testing new branch

---
+++ test/item_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/item'
+
+class TestItem < Minitest::Test
+    attr_reader :item
+  def setup
+    @item = Item.new({
+      :name => "Pencil",
+      :description => "Writes stuff",
+      :unit_price => 10.99,
+      :created_at => Time.now,
+      :updated_at => Time.now
+      })
+  end
+
+  def test_exists
+      assert_instance_of Item, @item
+  end
+
+  def test_attributes
+    time = Time.now
+    
+    assert_equal "Pencil", item.name
+    assert_equal "Writes stuff", item.description
+    assert_equal 10.99, item.unit_price
+    assert_equal time, item.created_at
+    assert_equal time, item.updated_at
+  end
+end
