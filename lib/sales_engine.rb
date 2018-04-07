8e72ba1266a8771ed92f24136efa368c841a63d5
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-06 22:08:38 -0600

    Adds in source code for SalesEngine, ItemRepository, and MerchantRepository classes

---
+++ lib/sales_engine.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Sales Engine class for managing data
+class SalesEngine
+  attr_reader :items,
+              :merchants
+
+  def initialize(items_and_merchants = nil)
+    @items = items_and_merchants[:items]
+    @merchants = items_and_merchants[:merchants]
+  end
+
+  def self.from_csv(items_and_merchants)
+    new(items_and_merchants)
+  end
+end
