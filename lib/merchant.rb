21046689528f576099646b8898b0135a43dad6cc
Manoj Panta <manojpanta95@gmail.com>
2018-04-06 18:55:16 -0600

    initial commit

---
+++ lib/merchant.rb
@@ -0,0 +1,10 @@
+# this is merchant class
+class Merchant
+  attr_reader :id,
+              :name
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:name]
+  end
+
+end
---
+++ test/merchant_test.rb
@@ -0,0 +1,26 @@
+require './lib/merchant'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class MerchantTest< MiniTest::Test
+  def test_it_exists
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_instance_of Merchant, merchant
+  end
+
+  def test_merchant_can_have_name_and_id
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+    assert_equal "Turing School", merchant.name
+    assert_equal 5, merchant.id
+  end
+
+  def test_merchant_can_have_name_and_id_for_another_merchant
+    merchant = Merchant.new({:id => 3, :name => "Walmart"})
+
+    assert_equal "Walmart", merchant.name
+    assert_equal 3, merchant.id
+  end
+
+end
