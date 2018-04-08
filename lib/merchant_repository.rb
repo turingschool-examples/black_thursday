ac6b023c9411ab16c32b07e35941d7d0cc24b1ff
Chris Powell <chris.ian.powell@gmail.com>
2018-04-08 10:36:31 -0600

    more methods for merch repo

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require './lib/merchant'
 
 class MerchantRepository
   attr_reader :path,
@@ -7,5 +8,29 @@ class MerchantRepository
   def initialize(path)
     @path = path
     @merchants = []
+    pack_merchants(path)
   end
+
+  def pack_merchants(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |merchant|
+    merchants << Merchant.new(merchant)
+    end
+  end
+
+  def all
+    merchants
+  end
+
+  def find_by_id(id)
+    merchants.find do |merchant|
+      merchant.id == id
+    end
+  end
+
+  def find_by_name(name)
+    merchants.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
 end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,14 +1,15 @@
 require './test/test_helper'
 require './lib/merchant'
-
+require 'pry'
 class MerchantTest < Minitest::Test
   def test_merchant_exists
-    m = Merchant.new({:id => 5, :name => "Turing School"})
+    m = Merchant.new(:id => 5, :name => 'Turing School')
     assert_instance_of Merchant, m
   end
 
   def test_mechant_collects_id_and_name
-    m = Merchant.new({:id => 5, :name => "Turing School"})
+    m = Merchant.new(:id => 5, :name => 'Turing School')
     assert_equal 5, m.id
+    assert_equal 'Turing School', m.name
   end
 end
