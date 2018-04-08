cf2e3d828074655ce4327a9ae47272432e4cbdf7
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-08 17:42:02 -0600

    add methods to have item tests passing

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,46 @@
 class Item
-  def initialize(data)
+  attr_reader :data
+  def initialize(item)
+    @data = {
+      id:          item[:id],
+      name:        item[:name],
+      description: item[:description],
+      unit_price:  BigDecimal.new(item[:unit_price], 4),
+      created_at:  item[:created_at],
+      updated_at:  item[:updated_at],
+      merchant_id: item[:merchant_id]
+    }
+  end
+
+  def id
+    data[:id].to_i
+  end
+
+  def name
+    data[:name]
+  end
+
+  def description
+    data[:description]
+  end
+
+  def unit_price
+    data[:unit_price]
+  end
+
+  def created_at
+    data[:created_at]
+  end
+
+  def updated_at
+    data[:updated_at]
+  end
+
+  def merchant_id
+    data[:merchant_id].to_i
+  end
+
+  def unit_price_to_dollars
+    unit_price.to_f
   end
 end
