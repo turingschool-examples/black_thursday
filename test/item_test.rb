500596c762b494a81b978a3ed24ef78801303595
JerrelMitchell <jerrelmitchell@ymail.com>
2018-04-08 12:16:10 -0600

    update item test for rubocop specifications. split up attributes test

--- test/item_test.rb
+++ test/item_test.rb
@@ -2,28 +2,28 @@ require './test/test_helper'
 require_relative '../lib/item'
 
 class ItemTest < Minitest::Test
-  def test_it_exists
-    item = Item.new(
+  attr_reader :item
+  def setup
+    @item = Item.new(
       name: 'Pencil',
       description: 'Can be used to write things',
       unit_price: BigDecimal.new(10.99, 4),
       created_at: Time.now,
       updated_at: Time.now
     )
+  end
+
+  def test_it_exists
     assert_instance_of Item, item
   end
 
   def test_it_has_attributes
-    item = Item.new(
-      name: 'Pencil',
-      description: 'Can be used to write things',
-      unit_price: BigDecimal.new(10.99, 4),
-      created_at: Time.now,
-      updated_at: Time.now
-    )
     assert_equal 'Pencil', item.name
     assert_equal 'Can be used to write things', item.description
     assert_equal 10.99, item.unit_price
+  end
+
+  def test_it_can_show_times_when_created_and_updated
     assert_equal Time.now, item.created_at
     assert_equal Time.now, item.updated_at
   end
