5d82c2f01ae223e936aea78abbc6ec8a27c99fbd
brickstar <brickstar@gmail.com>
2018-04-07 20:53:29 -0600

    adds tests and implementation to item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,6 @@
+require 'time'
+require 'bigdecimal'
+
 class Item
   attr_reader :id,
               :name,
@@ -5,14 +8,18 @@ class Item
               :unit_price,
               :merchant_id,
               :created_at,
-              :updated_at,
+              :updated_at
   def initialize(data)
-    @id          = data[:id]
+    @id          = data[:id].to_i
     @name        = data[:name]
     @description = data[:description]
-    @unit_price  = data[:unit_price]
+    @unit_price  = BigDecimal.new(data[:unit_price]) / 100.0
     @merchant_id = data[:merchant_id]
     @created_at  = data[:created_at]
     @updated_at  = data[:updated_at]
+  end
+
+  def unit_price_to_dollars
+    @unit_price
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'bigdecimal'
 require './lib/item'
 class ItemTest < Minitest::Test
   def setup
@@ -25,26 +26,44 @@ class ItemTest < Minitest::Test
   end
 
   def test_it_has_a_name
+    item = Item.new(@data)
 
+    assert_equal 'Pencil', item.name
   end
 
   def test_has_a_description
+    item = Item.new(@data)
 
+    assert_equal 'You can use it to write things', item.description
   end
 
   def test_it_has_a_unit_price
+    item = Item.new(@data)
 
+    assert_equal 12.00, item.unit_price
   end
 
   def test_it_has_a_merchant_id
+    item = Item.new(@data)
 
+    assert_equal 2, item.merchant_id
   end
 
   def test_it_has_created_at
+    item = Item.new(@data)
 
+    assert_equal "2016-01-11 11:51:37 UTC", item.created_at
   end
 
   def test_it_has_updated_at
+    item = Item.new(@data)
 
+    assert_equal "2016-01-11 11:51:37 UTC", item.updated_at
+  end
+
+  def test_unit_price_to_dollars
+    item = Item.new(@data)
+
+    assert_equal 12, item.unit_price
   end
 end
