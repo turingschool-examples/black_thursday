72ec2f9d2a3bef3ce664805498d95df564ba6712
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-08 10:39:44 -0600

    Adds tests for index creaton to merchant repoistory class

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,7 +1,34 @@
+# frozen_string_literal:true
+
+require 'csv'
+require './lib/merchant'
+
+# A class for containing all Merchant objects
 class MerchantRepository
-  attr_reader :data_file
+  attr_reader :data_file,
+              :by_id
 
   def initialize(data_file)
-    @data_file = data_file
+    @by_id = generate_merchants_data(data_file)
+    # @by_name = {}
+  end
+
+  def generate_merchants_data(data_file)
+    csv_string = File.read(data_file)
+    data = CSV.parse(csv_string)
+    create_index(data)
+  end
+  
+  def create_index(data)
+    merchant_information = data.shift
+    merchant_data = {}
+    data.each do |merchant|
+      # require 'pry';binding.pry
+      merchant_data[merchant[0]] = Merchant.new(merchant[0], merchant[1])
+    end
+    merchant_data
+  end
+
+  def all
   end
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -6,14 +6,33 @@ require_relative '../lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
   def setup
-    @merchant_repository = MerchantRepository.new('../data/merchants.csv')
+    @merchant_repository = MerchantRepository.new('./data/test_merchants.csv')
   end
 
   def test_merchant_repository_exists
     assert_instance_of MerchantRepository, @merchant_repository
   end
 
-  def test_merchant_repository_can_have_data_file_attribute
-    assert_equal '../data/merchants.csv', @merchant_repository.data_file
+  def test_generating_merchant_data_array
+    expected = [%w[id name created_at updated_at],
+                %w[12334105 Shopin1901 2010-12-10 2011-12-04],
+                %w[12334112 Candisart 2009-05-30 2010-08-29],
+                %w[12334113 MiniatureBikez 2010-03-30 2013-01-21],
+                %w[12334115 LolaMarleys 2008-06-09 2015-04-16],
+                %w[12334123 Keckenbauer 2010-07-15 2012-07-25],
+                %w[12334132 perlesemoi 2009-03-21 2014-05-19],
+                %w[12334135 GoldenRayPress 2011-12-13 2012-04-16]]
+    assert_equal expected, @merchant_repository.generate_merchants_data('./data/test_merchants.csv')
+  end
+
+  def test_creating_an_index_of_merchants_from_data
+    assert_instance_of Hash, @merchant_repository.by_id
+    assert_instance_of Merchant, @merchant_repository.by_id[12334105]
+    assert_instance_of Merchant, @merchant_repository.by_id[12334112]
+    assert_instance_of Merchant, @merchant_repository.by_id[12334113]
+    assert_instance_of Merchant, @merchant_repository.by_id[12334115]
+    assert_instance_of Merchant, @merchant_repository.by_id[12334123]
+    assert_instance_of Merchant, @merchant_repository.by_id[12334132]
+    assert_instance_of Merchant, @merchant_repository.by_id[12334135]
   end
 end
