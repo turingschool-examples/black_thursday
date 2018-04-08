ebbbdbcf573f595bd0f9db11c47e8deb857aaddf
Chris Powell <chris.ian.powell@gmail.com>
2018-04-07 19:59:16 -0600

    making small steps on merchant repo

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,3 +1,11 @@
-class MerchantRepository
+require 'csv'
 
+class MerchantRepository
+  attr_reader :path,
+              :merchants
+
+  def initialize(path)
+    @path = path
+    @merchants = []
+  end
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -3,7 +3,12 @@ require './lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
   def test_merchant_repository_exists
-    mr = MerchantRepository.new
+    mr = MerchantRepository.new('./test/fixtures/merchants_truncated')
     assert_instance_of MerchantRepository, mr
+  end
+
+  def test_returns_all_instances
+    mr = MerchantRepository.new('./test/fixtures/merchants_truncated')
+    assert mr.merchants.empty?, mr.merchants
   end
 end
