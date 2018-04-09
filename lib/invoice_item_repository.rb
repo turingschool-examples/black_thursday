753412010b06c9072df68f3ce88f14e406066d55
justinetroyke <jjtroyke@gmail.com>
2018-04-09 15:53:57 -0600

    started invoice item repo and testing

---
+++ lib/invoice_item_repository.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'invoice_item.rb'
+
+# This class holds how invoices are connected to items
+class InvoiceItemRepository
+end
---
+++ test/invoice_item_repository_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
+require './lib/invoice_item_repository.rb'
+require './lib/sales_engine.rb'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/emoji'
+require 'pry'
+
+# Tests the functionality of the invoice item repository
+class InvoiceItemRepositorytest < Minitest::Test
+  attr_reader :invoice_items
+
+  def setup
+    se = SalesEngine.from_csv({:items => './fixtures/invoice_item_repo_test.csv'})
+    @invoice_items = se.invoices
+  end
+
+  def test_invoice_item_repo_exists
+    assert_instance_of InvoiceItemRepository, invoice_items
+  end
+end
