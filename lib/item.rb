102efbbc96650308b41ea68c94eef7301ac14d7b
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-09 15:33:55 -0600

    Updates unit_price conversions BigDecimal and Float

--- lib/item.rb
+++ lib/item.rb
@@ -14,15 +14,13 @@ class Item
     @id          = item_hash[:id].to_i
     @name        = item_hash[:name]
     @description = item_hash[:description]
-    @unit_price  = BigDecimal(item_hash[:unit_price], sigdigits)
+    @unit_price  = BigDecimal(item_hash[:unit_price], sigdigits) / 100
     @merchant_id = item_hash[:merchant_id].to_i
     @created_at  = item_hash[:created_at]
     @updated_at  = item_hash[:updated_at]
-
-    set_unit_price_to_dollars
   end
 
-  def set_unit_price_to_dollars
-    @unit_price = (@unit_price / 100).round(2)
+  def unit_price_to_dollars
+    @unit_price = (@unit_price.to_f).round(2)
   end
 end
