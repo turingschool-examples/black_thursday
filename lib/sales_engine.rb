2a2855945cc4e5dfe4dbdbb43192e1cedf48c1d0
rcm32000 <rcm32000@yahoo.com>
2018-04-08 18:00:14 -0600

    Sales Engine added

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,2 +1,16 @@
+require_relative 'item_repository'
+require_relative 'merchant_repository'
+# Ties together DAL
 class SalesEngine
+  attr_reader :items, :merchants
+  def initialize(attributes)
+    @items = ItemRepository.new
+    @items.from_csv(attributes[:items])
+    @merchants = MerchantRepository.new
+    @merchants.from_csv(attributes[:merchants])
+  end
+
+  def self.from_csv(attributes)
+    SalesEngine.new(attributes)
+  end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,15 +2,14 @@ require_relative 'test_helper'
 require_relative '../lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-  def setup
-    @se = SalesEngine.new
-  end
 
-  def test_it_exists
-    assert_instance_of SalesEngine, @se
-  end
-
-  def test_
-
+  def test_it_returns_repositories
+    se = SalesEngine.from_csv({
+                              :items      => './data/items.csv',
+                              :merchants  => './data/merchants.csv'
+                              })
+    assert_instance_of ItemRepository, se.items
+    assert_instance_of MerchantRepository, se.merchants
+    assert_instance_of SalesEngine, se
   end
 end
