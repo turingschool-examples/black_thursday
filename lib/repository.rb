67137a76c83d8dd7ba40d881f753d6fa2579eab7
Emmie Hayes <emmiehayes2@gmail.com>
2018-04-09 17:55:07 -0600

    for the win

--- lib/repository.rb
+++ lib/repository.rb
@@ -1,15 +1,4 @@
-require './lib/loader'
 # Repositoyr module
 module Repository
-  def all
-    repository
-  end
-
-  def find_by_id(id)
-    repository.find { |child| child.id == id }
-  end
-
-  def find_by_name(name)
-    repository.find { |child| child.cascmp == name.casecmp }
-  end
+#enter all common methods from item repo and merchant repo once completed.
 end
