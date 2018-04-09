42b96f7ac82a6bd01f153f1b8b76fcda89e51cec
justinetroyke <jjtroyke@gmail.com>
2018-04-09 16:56:40 -0600

    added intialize to invoice items

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -6,16 +6,19 @@ require 'time'
 
 # Processes invoices for individual items
 class InvoiceItem
+  attr_reader :invoice_item_specs,
+              :parent
 
   def initialize(invoice_items, parent)
     @invoice_items_specs = {
-      id: invoice_items[:id].to_i,
-      item_id: ',
-      invoice_id:,
-      quantity: ,
-      unit_price: ,
-      created_at: ,
-      updated_at:
+      id:                     invoice_items[:id].to_i,
+      item_id:                invoice_items[:item_id].to_i,
+      invoice_id:             invoice_items[:invoice_id].to_i,
+      quantity:               invoice_items[:quantity].to_i,
+      unit_price:             BigDecimal(invoice_items[:unit_price]) / 100,
+      created_at:             invoice_items[:created_at],
+      updated_at:             invoice_items[:updated_at]
     }
+    @parent = parent
   end
 end
