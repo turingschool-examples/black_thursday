67cfe9e47c52d88900984477fb9b821cad92960b
brickstar <brickstar@gmail.com>
2018-04-09 23:41:54 -0600

    adds memoization to @merchants

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -7,7 +7,7 @@ class SalesEngine
   attr_reader :items, :merchants
   def initialize(data)
      @items ||= ItemRepository.new(data[:items])
-     @merchants = MerchantRepository.new(data[:merchants])
+     @merchants ||= MerchantRepository.new(data[:merchants])
   end
 
   def self.from_csv(data)
