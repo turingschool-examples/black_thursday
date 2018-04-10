67440372e46cbe949ac5fb244d577b2c4ace144a
Cole Hart <cole@colehart.com>
2018-04-10 17:34:05 -0600

    created test for creating new id and new invoice

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,3 +1,4 @@
+require 'time'
 require_relative 'test_helper'
 require_relative '../lib/fileio'
 require_relative '../lib/invoice'
@@ -6,8 +7,16 @@ require_relative '../lib/invoice_repository'
 # Test for the InvoiceRepository class
 class InvoiceRepositoryTest < Minitest::Test
   def setup
+    time = Time.now
     file_path = FileIO.load('./test/fixtures/test_invoices.csv')
     @inv_repo = InvoiceRepository.new(file_path)
+    @new_invoice = @inv_repo.create(
+      customer_id: '7',
+      merchant_id: '12334105',
+      status: 'pending',
+      created_at: '2009-12-09 12:08:04 UTC',
+      updated_at: '2010-12-09 12:08:04 UTC'
+    )
   end
 
   def test_it_exists
@@ -28,14 +37,14 @@ class InvoiceRepositoryTest < Minitest::Test
   def test_all_returns_correct_ids
     all_invoices = @inv_repo.all
     actual_all_ids = all_invoices.map(&:id)
-    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 37]
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 37, 38]
     assert_equal expected, actual_all_ids
   end
 
   def test_all_returns_correct_customer_ids
     all_invoices = @inv_repo.all
     actual_all_cust_ids = all_invoices.map(&:customer_id)
-    expected = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 6, 9]
+    expected = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 6, 9, 7]
     assert_equal expected, actual_all_cust_ids
   end
 
@@ -76,10 +85,43 @@ class InvoiceRepositoryTest < Minitest::Test
     assert_instance_of Invoice, actual_shipped[0]
     assert_instance_of Invoice, actual_returned[0]
     pending_ids = actual_pending.map(&:id)
-    assert_equal [1, 4, 5, 6, 7, 10], pending_ids
+    assert_equal [1, 4, 5, 6, 7, 10, 38], pending_ids
     shipped_ids = actual_shipped.map(&:id)
     assert_equal [2, 3, 8, 9], shipped_ids
     returned_ids = actual_returned.map(&:id)
     assert_equal [25, 37], returned_ids
+  end
+
+  def test_it_can_generate_next_invoice_id
+    expected = 39
+    actual = @inv_repo.create_new_id
+    assert_equal expected, actual
+  end
+
+  def test_can_create_new_invoice
+    assert_instance_of Invoice, @new_invoice
+    assert_equal 13, @inv_repo.invoices.count
+    assert_equal 7, @inv_repo.invoices[38].customer_id
+    assert_equal 12334105, @inv_repo.invoices[38].merchant_id
+    assert_equal '2009-12-09 12:08:04 UTC', @inv_repo.invoices[38].created_at
+    assert_equal '2010-12-09 12:08:04 UTC', @inv_repo.invoices[38].updated_at
+  end
+
+  def test_invoice_can_be_updated
+    skip
+    @inv_repo.update(263567475, name: 'Roly Poly Coley',
+                              description: 'Best toy ever.',
+                              unit_price: 15.00,
+                              merchant_id: 12334135,
+                              created_at: '2009-12-09 12:08:04 UTC',
+                              updated_at: @time)
+    assert_equal 'Roly Poly Coley', @inv_repo.invoices[263567475].name
+  end
+
+  def test_invoice_can_be_deleted
+    skip
+    @inv_repo.delete(263567475)
+    assert_equal 3, @inv_repo.invoices.count
+    assert_nil @inv_repo.invoices[263567475]
   end
 end
