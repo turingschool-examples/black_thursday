fc1a5b37013f06607eae91fc4da3a70cef3ce1f7
John Livingston <jclivingston316@gmail.com>
2018-04-09 20:10:19 -0600

    item_repo methods WIP

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -14,24 +14,43 @@ class ItemRepo
     end
 
     def all
-        @repository
+        repository
     end
 
     def find_by_id(id)
-        repository.find do |child|
-            child.id == id
+        repository.find do |item
+            item.id == id
         end
     end
 
-    def find_by_name(name)
+    def find_by_name(item)
         repository.find do |child|
-            child.name.downcase == name.downcase
+            item.name.downcase == name.downcase
         end
     end
     
     def find_all_with_description(description)
+        repository.find_all do |item|
+            item.description.downcase.include?(description.downcase)
+        end
     end
 
+    def find_all_by_price(price)
+        repository.find_all do |item|
+            item.price == BigDecimal.new(price)
+        end
+    end
 
+   def find_all_by_price_in_range(price_range)
+    repository.find_all do |item|
+      price_range.include?(item.unit_price)
+    end
+  end
+
+  def find_all_by_merchant_id(id)
+    repository.find_all do |item| 
+        item.merchant_id == id
+    end
+  end
 
 end
