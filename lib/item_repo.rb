b9e78e9e26b93ef3734895d22ef38eb0d25d9114
John Livingston <jclivingston316@gmail.com>
2018-04-09 19:33:11 -0600

    item_repo WIP

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -1,2 +1,17 @@
+require_relative '../lib/item'
+require_relative '../lib/repo'
+
 class ItemRepo
+  attr_reader :items,
+              :contents,
+              :parent,
+              :repository
+
+def initialize(data, parent)
+    @repository = data.map do |row| 
+      Item.new(row, self)
+    end
+    @parent = parent
+  end
+
 end
