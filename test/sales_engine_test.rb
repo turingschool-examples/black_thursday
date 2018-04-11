8187043ac22fc04e42076f963b8638865bc2b759
zdcthomas <zdcthomas@yahoo.com>
2018-04-11 13:33:44 -0600

    first pass at sales engine, fixes test typos

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,13 +1,13 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/item_repository'
-require './lib/merchant_repository'
+require_relative '../lib/item_repository'
+require_relative '../lib/merchant_repository'
 require 'simplecov'
 SimpleCov.start
-require './lib/sales_engine'
+require_relative '../lib/sales_engine'
 class SalesEngineTest < Minitest::Test
-  
+
   def setup
     @se = SalesEngine.from_csv({
       items: "./data/items.csv",
