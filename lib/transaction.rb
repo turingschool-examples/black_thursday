415e674758b3d08fbdb84cc3358cf3ce5cd7f669
Cole Hart <cole@colehart.com>
2018-04-11 15:53:11 -0600

    got transaction.rb and transaction_test.rb working

---
+++ lib/transaction.rb
@@ -0,0 +1,21 @@
+# transaction class
+class Transaction
+  attr_reader :id,
+              :invoice_id,
+              :credit_card_number,
+              :credit_card_expiration_date,
+              :created_at
+
+  attr_accessor :result,
+                :updated_at
+
+  def initialize(transact_hash = Hash.new(0))
+    @id = transact_hash[:id].to_i
+    @invoice_id = transact_hash[:invoice_id].to_i
+    @credit_card_number = transact_hash[:credit_card_number].to_i
+    @credit_card_expiration_date = transact_hash[:credit_card_expiration_date]
+    @result      = transact_hash[:result]
+    @created_at  = transact_hash[:created_at]
+    @updated_at  = transact_hash[:updated_at]
+  end
+end
