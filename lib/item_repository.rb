c45268ba5e69d005cd11327b5917528eb7b71b36
Sabrina Robinson <sabrinarob@gmail.com>
2018-04-10 19:04:02 -0600

    Added method to Item Repo class with tests.

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -5,14 +5,39 @@ class ItemRepository
               :parent
 
   def initialize(path, parent)
-    @contents = []
+    @contents = {}
     @parent = parent
     load_path(path)
   end
 
   def load_path(path)
     CSV.foreach(path, headers: true, header_converters: :symbol) do |row|
-      @contents << Item.new(row, self)
+      @contents[row[:id]] = Item.new(row, self)
     end
+  end
+
+  def all
+    @contents.values
+  end
+
+  def find_by_id(id)
+    @contents[id]
+  end
+
+  def find_by_name(name)
+    @contents.each do |_, item|
+      return item if name.downcase == item.name.downcase
+    end
+    nil
+  end
+
+  def find_all_with_description(description)
+    items = []
+    @contents.each do |_, item|
+      if item.description == description
+        items << item
+      end
+    end
+    items
   end
 end
