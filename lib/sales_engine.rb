ca3fd1315f8a94a160a1571c030703ef2bbc5245
rcm32000 <rcm32000@yahoo.com>
2018-04-11 15:51:14 -0600

    TransactionRepository complete

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,11 +2,15 @@ require_relative 'item_repository'
 require_relative 'merchant_repository'
 require_relative 'invoice_repository'
 require_relative 'sales_analyst'
+require_relative 'invoice_item_repository'
+require_relative 'transaction_repository'
 # Ties together DAL
 class SalesEngine
   attr_reader :items,
               :merchants,
-              :invoices
+              :invoices,
+              :invoice_items,
+              :transactions
 
   def initialize(attributes)
     @items = ItemRepository.new(self)
@@ -15,6 +19,10 @@ class SalesEngine
     @merchants.from_csv(attributes[:merchants]) if attributes[:merchants]
     @invoices = InvoiceRepository.new(self)
     @invoices.from_csv(attributes[:invoices]) if attributes[:invoices]
+    @invoice_items = InvoiceItemRepository.new(self)
+    @invoice_items.from_csv(attributes[:invoice_items]) if attributes[:invoice_items]
+    @transactions = TransactionRepository.new(self)
+    @transactions.from_csv(attributes[:transactions]) if attributes[:transactions]
   end
 
   def self.from_csv(attributes)
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -8,6 +8,8 @@ class SalesEngineTest < Minitest::Test
                               items:      './data/items.csv',
                               merchants:  './test/fixtures/merchants_fixtures.csv',
                               invoices:   './test/fixtures/invoices_fixtures.csv'
+                              invoice_items:   './test/fixtures/invoice_items_fixtures.csv'
+                              transactions:   './test/fixtures/transactions_fixtures.csv'
                               )
   end
 
@@ -15,6 +17,8 @@ class SalesEngineTest < Minitest::Test
     assert_instance_of ItemRepository, @se.items
     assert_instance_of MerchantRepository, @se.merchants
     assert_instance_of InvoiceRepository, @se.invoices
+    assert_instance_of InvoiceItemRepository, @se.invoice_items
+    assert_instance_of TransactionRepository, @se.transactions
     assert_instance_of SalesEngine, @se
   end
 
