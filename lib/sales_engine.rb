f931435d8268fbc4c814847df537c7c29e61104f
John Livingston <jclivingston316@gmail.com>
2018-04-11 14:09:09 -0600

    sales engine and sales_relationships

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,16 +1,24 @@
 require_relative 'merchant_repo'
 require_relative 'item_repo'
+require_relative 'class_relationships'
 
 class SalesEngine
+  include sales_relationships
   attr_accessor :items, :merchants
 
-  def self.from_csv(hash)
-    new(hash)
+  def initialize(path)
+    @path = path
   end
 
-  def initialize(hash)
-    # @items = ItemRepo.new
-    @merchants = MerchantRepo.new(hash[:merchants])
+   def self.from_csv(path)
+    SalesEngine.new(path)
+  end
 
+  def items
+    @items ||= ItemRepository.new(File.load(path[:items]), self)
+  end
+
+  def merchants
+    @merchants ||= MerchantRepository.new(File.load(path[:merchants]), self)
   end
 end
