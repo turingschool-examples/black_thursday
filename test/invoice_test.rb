50c76913dc5de99ad4735deba8664c2f269664e7
justinetroyke <jjtroyke@gmail.com>
2018-04-10 19:39:26 -0600

    invoice testing

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -18,14 +18,14 @@ class MockInvoiceRepository
   end
 end
 
-# 
+#
 class InvoiceTest < Minitest::Test
   INVOICE_BODY = {
-    id: '2'
-    customer_id: '1'
-    merchant_id: '12334753'
-    status: 'shipped'
-    created_at: '2012-11-23'
+    id: '2',
+    customer_id: '1',
+    merchant_id: '12334753',
+    status: 'shipped',
+    created_at: '2012-11-23',
     updated_at: '2013-04-14'
   }.freeze
 
@@ -33,5 +33,13 @@ class InvoiceTest < Minitest::Test
 
   def setup
     @invoice = Invoice.new(INVOICE_BODY, MockInvoiceRepository.new)
+  end
+
+  def test_invoice_exists
+    assert_instance_of Invoice, invoice
+  end
+
+  def test_it_has_an_integer_id
+    assert_equal 2, invoice.id
   end
 end
