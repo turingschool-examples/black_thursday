73ea25750da91ab9c8869f29a979882f8ef052af
brickstar <brickstar@gmail.com>
2018-04-12 16:01:59 -0600

    refactor

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -41,20 +41,28 @@ class SalesAnalyst
     end.compact
   end
 
+  # def average_item_price_for_merchant(id)
+  #   merchants_items = items.map do |item|
+  #     item if item.merchant_id == (id)
+  #   end.compact
+  #   averages = merchants_items.map do |item|
+  #     item.unit_price
+  #   end.reduce(:+) / merchants_items.length
+  #   averages.round(2)
+  # end
+
   def average_item_price_for_merchant(id)
-    merchants_items = items.map do |item|
-      item if item.merchant_id == (id)
-    end.compact
-    averages = merchants_items.map do |item|
+    found = @engine.items.find_all_by_merchant_id(id)
+    to_average = found.map do |item|
       item.unit_price
-    end.reduce(:+) / merchants_items.length
-    averages.round(2)
+    end.reduce(:+) / found.length
+    to_average.round(2)
   end
 
   private
 
   def total_number_of_items_per_merchant
-    @total ||= merchants.map do |merchant|
+    @_total ||= merchants.map do |merchant|
       @engine.items.find_all_by_merchant_id(merchant.id).count
     end
   end
