d8d8e91dda5e03678ee8f02dd674887b9649ee63
zdcthomas <zdcthomas@yahoo.com>
2018-04-12 12:12:13 -0600

    Tests sales analyst existence

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require './test_helper'
+require './test/test_helper'
 require './lib/sales_analyst'
 # tests sales analyst
-class SalesAnalyst < Minitest::Test
+class SalesAnalystTest < Minitest::Test
   def test_it_exists
     se = SalesAnalyst.new
     assert_instance_of SalesAnalyst, se
