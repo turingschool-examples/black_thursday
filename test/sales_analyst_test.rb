72c8dfcbcc8809aa5aa64fe5d90cfd671e0fb9df
zdcthomas <zdcthomas@yahoo.com>
2018-04-12 12:34:46 -0600

    TESTS average items per merchant WIP

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
+require './lib/sales_engine'
 require './test/test_helper'
 require './lib/sales_analyst'
 # tests sales analyst
 class SalesAnalystTest < Minitest::Test
+  def setup
+    @sales_engine_full = SalesEngine.new({ items:     './data/items.csv',
+                                           merchants: './data/merchants.csv'})
+  end
+
   def test_it_exists
-    se = SalesAnalyst.new
+    sa = SalesAnalyst.new(@sales_engine_full)
     assert_instance_of SalesAnalyst, se
+  end
+
+  def test_average_items_per_merchant
+    sa = SalesAnalyst.new(@sales_engine_full)
+    assert_equal 2.88, sa.average_items_per_merchant
   end
 end
