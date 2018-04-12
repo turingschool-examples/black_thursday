79c0e85c301ce4df641aed7c58753dc9bb20a5db
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-12 13:48:32 -0600

    tests - average items per merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class SalesAnalyst
+
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+  end
+
+  def all_items_per_merchant
+    @sales_engine.items.all.group_by{|item| item.merchant_id}
+  end
+
+  def average_items_per_merchant
+    item_count = all_items_per_merchant.values.map{|merch_items| merch_items.count}
+    item_sum = item_count.inject(0){|sum, number| sum + number}
+    (item_sum.to_f / all_items_per_merchant.values.count).round(2)
+  end
+
+
+
+
+
+
+
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -12,7 +12,13 @@ class SalesAnalystTest < Minitest::Test
 
   def test_it_exists
     sa = SalesAnalyst.new(@sales_engine_full)
-    assert_instance_of SalesAnalyst, se
+    assert_instance_of SalesAnalyst, sa
+  end
+
+  def test_all_items_per_by_merchant
+    sa = SalesAnalyst.new(@sales_engine_full)
+    assert_equal 475, sa.all_items_per_merchant.keys.count
+    assert_equal 1367, sa.all_items_per_merchant.values.flatten.count
   end
 
   def test_average_items_per_merchant
