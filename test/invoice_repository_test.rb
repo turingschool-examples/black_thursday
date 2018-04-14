f3d099cf7934b1a1cdfa6aa0716ecb7ac0e7236e
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-14 14:42:08 -0600

    TESTS invoice exists and has attributes. Initial invoice tests made and passed with implementation code.

---
+++ test/invoice_repository_test.rb
@@ -0,0 +1,15 @@
+require './lib/invoice_repository'
+require 'pry'
+
+class InvoiceRepositoryTest < Minitest::Test
+
+  def test_it_exists
+    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
+    invoice = se.invoices.find_by_id(6)
+  end
+
+  def test_
+
+
+
+end
