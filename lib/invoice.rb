f0d45b00edf252a7a232fcc9e737e0e1c58eb12f
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-15 14:57:17 -0600

    Changes initialize parameter for invoices from invoice_hash to attrs

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -12,12 +12,12 @@ class Invoice
   attr_accessor :status,
                 :updated_at
 
-  def initialize(invoice_hash = Hash.new(0))
-    @id          = invoice_hash[:id].to_i
-    @customer_id = invoice_hash[:customer_id].to_i
-    @merchant_id = invoice_hash[:merchant_id].to_i
-    @status      = invoice_hash[:status].to_sym
-    @created_at  = format_time(invoice_hash[:created_at])
-    @updated_at  = format_time(invoice_hash[:updated_at])
+  def initialize(attrs)
+    @id          = attrs[:id].to_i
+    @customer_id = attrs[:customer_id].to_i
+    @merchant_id = attrs[:merchant_id].to_i
+    @status      = attrs[:status].to_sym
+    @created_at  = format_time(attrs[:created_at])
+    @updated_at  = format_time(attrs[:updated_at])
   end
 end
