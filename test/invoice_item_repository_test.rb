0eb4228feb08535fe4f520a64f80e59736b4009f
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-16 12:25:42 -0600

    Files for invoice_item repo tests and code created.

---
+++ test/invoice_item_repository_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/invoice_item_repository'
+
+
+class InvoiceItemRepositoryTest < Minitest::Test
+
+  def test_it_exists
+  assert_instance_of InvoiceItemRepository,class_variable
+  end
+  def test_it
+  end
+end
