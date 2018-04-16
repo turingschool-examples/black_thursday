e7907e778f38b90e175781f8bc92c007f686acb1
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-16 12:15:48 -0600

    tests for invoice_item as well as implementation code complete

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -1,19 +1,21 @@
 require 'minitest/autorun'
+require 'bigdecimal'
+require 'time'
 require 'minitest/pride'
 require './lib/invoice_item'
 
 class InvoiceItemTest < Minitest::Test
 
   def setup
-    @now
+    @time = "2016-01-11 09:34:06 UTC"
     @ii = InvoiceItem.new({
       :id => "6",
       :item_id => "7",
       :invoice_id => "8",
       :quantity => "1",
       :unit_price => "1099",
-      :created_at => @now.to_s,
-      :updated_at => @now.to_s
+      :created_at => @time,
+      :updated_at => @time
     })
   end
 
@@ -26,9 +28,9 @@ class InvoiceItemTest < Minitest::Test
     assert_equal 7, @ii.item_id
     assert_equal 8, @ii.invoice_id
     assert_equal 1, @ii.quantity
-    assert_equal BigDecimal.new(10.99, 4), @ii.unit_price
-    assert_equal @now, @ii.created_at
-    assert_equal @now, @ii.updated_at
+    assert_instance_of BigDecimal, @ii.unit_price
+    assert_instance_of Time, @ii.created_at
+    assert_instance_of Time, @ii.updated_at
   end
 
 
