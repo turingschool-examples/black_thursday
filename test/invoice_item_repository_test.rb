ea9507999ba014c48c437110d55962e7e26d1707
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-16 13:46:55 -0600

    Created test and code for find_all_by_item_id

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -80,6 +80,17 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 5, actual[0].invoice_id
   end
 
+  def test_it_can_find_all_by_item_id
+      actual = @iir.find_all_by_item_id(263519844)
+      assert_equal 1, actual[0].invoice_id
+
+      actual = @iir.find_all_by_item_id(263529264)
+      assert_equal 9, actual[0].id
+
+      actual = @iir.find_all_by_item_id(263543136)
+      assert_equal 9, actual[0].quantity
+  end
+
   def test_it_can_find_all_by_invoice_id
       actual = @iir.find_all_by_invoice_id(1)
       assert_equal 263519844, actual[0].item_id
