5a2dce81a13a249fd245e8b0275af7ec0848f398
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-16 12:03:58 -0600

    TESTS attributes of invoice_item, WIP.

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -7,18 +7,28 @@ class InvoiceItemTest < Minitest::Test
   def setup
     @now
     @ii = InvoiceItem.new({
-      :id => 6,
-      :item_id => 7,
-      :invoice_id => 8,
-      :quantity => 1,
-      :unit_price => BigDecimal.new(10.99, 4),
-      :created_at => @now,
-      :updated_at => @now
+      :id => "6",
+      :item_id => "7",
+      :invoice_id => "8",
+      :quantity => "1",
+      :unit_price => "1099",
+      :created_at => @now.to_s,
+      :updated_at => @now.to_s
     })
   end
 
   def test_it_exists
     assert_instance_of InvoiceItem, @ii
+  end
+
+  def test_it_has_attributes
+    assert_equal 6, @ii.id
+    assert_equal 7, @ii.item_id
+    assert_equal 8, @ii.invoice_id
+    assert_equal 1, @ii.quantity
+    assert_equal BigDecimal.new(10.99, 4), @ii.unit_price
+    assert_equal @now, @ii.created_at
+    assert_equal @now, @ii.updated_at
   end
 
 
