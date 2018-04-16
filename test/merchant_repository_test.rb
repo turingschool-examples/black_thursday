73eab15d960581afab053ca624bf97a1a2df0237
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-16 10:02:59 -0600

    Updates merchant_repository test to remove redundant tests, updates to use actual object comparison

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,3 +1,4 @@
+require 'time'
 require_relative 'test_helper'
 require_relative '../lib/merchant_repository'
 require_relative '../lib/fileio'
@@ -5,8 +6,25 @@ require_relative '../lib/fileio'
 # merchant repository class
 class MerchantRepositoryTest < Minitest::Test
   def setup
-    file_path = FileIO.load('./test/fixtures/test_merchants0.csv')
-    @m_repo = MerchantRepository.new(file_path)
+    merchants_data = %(id,name,created_at,updated_at
+                       12334105,Shopin1901,2010-12-10,2011-12-04
+                       12334112,Candisart,2009-05-30,2010-08-29
+                       12334113,MiniatureBikez,2010-03-30,2013-01-21
+                       12334115,LolaMarleys,2008-06-09,2015-04-16
+                       12334123,Keckenbauer,2010-07-15,2012-07-25
+                       12334132,perlesemoi,2009-03-21,2014-05-19
+                       12334135,GoldenRayPress,2011-12-13,2012-04-16)
+    # file_path = FileIO.load('./test/fixtures/test_merchants0.csv')
+    csv = CSV.parse(merchants_data, headers: :true, header_converters: :symbol)
+    @time = Time.now
+    @m_repo = MerchantRepository.new(csv)
+    @merchant12334105 = @m_repo.merchants[12334105]
+    @merchant12334112 = @m_repo.merchants[12334112]
+    @merchant12334113 = @m_repo.merchants[12334113]
+    @merchant12334115 = @m_repo.merchants[12334115]
+    @merchant12334123 = @m_repo.merchants[12334123]
+    @merchant12334132 = @m_repo.merchants[12334132]
+    @merchant12334135 = @m_repo.merchants[12334135]
   end
 
   def test_merchant_repository_exists
@@ -14,45 +32,58 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_creating_an_index_of_merchants_from_data
-    assert_instance_of Hash, @m_repo.merchants
-    assert_instance_of Merchant, @m_repo.merchants[12334105]
-    assert_instance_of Merchant, @m_repo.merchants[12334112]
-    assert_instance_of Merchant, @m_repo.merchants[12334113]
+    # assert_instance_of Hash, @m_repo.merchants
+    # assert_instance_of Merchant, @m_repo.merchants[12334105]
+    # assert_instance_of Merchant, @m_repo.merchants[12334112]
+    # assert_instance_of Merchant, @m_repo.merchants[12334113]
+    expected = { 12334105 => @merchant12334105, 12334112 => @merchant12334112,
+                 12334113 => @merchant12334113, 12334115 => @merchant12334115,
+                 12334123 => @merchant12334123, 12334132 => @merchant12334132,
+                 12334135 => @merchant12334135 }
+    assert_equal expected, @m_repo.merchants
   end
 
   def test_all_returns_an_array_of_all_merchant_instances
-    assert_instance_of Array, @m_repo.all
+    # assert_instance_of Array, @m_repo.all
+    assert_equal [@merchant12334105, @merchant12334112,
+                  @merchant12334113, @merchant12334115,
+                  @merchant12334123, @merchant12334132,
+                  @merchant12334135], @m_repo.all
   end
 
-  def test_all_returns_correct_names
-    all_merchants = @m_repo.all
-    actual_all_names = all_merchants.map(&:name)
-    expected = %w[Shopin1901
-                  Candisart
-                  MiniatureBikez
-                  LolaMarleys
-                  Keckenbauer
-                  perlesemoi
-                  GoldenRayPress]
-    assert_equal expected, actual_all_names
-  end
+  # def test_all_returns_correct_names
+  #   all_merchants = @m_repo.all
+  #   actual_all_names = all_merchants.map(&:name)
+  #   expected = %w[Shopin1901
+  #                 Candisart
+  #                 MiniatureBikez
+  #                 LolaMarleys
+  #                 Keckenbauer
+  #                 perlesemoi
+  #                 GoldenRayPress]
+  #   assert_equal expected, actual_all_names
+  # end
 
   def test_can_find_by_id
-    actual_shopin = @m_repo.find_by_id(12334105)
-    actual_candis = @m_repo.find_by_id(12334112)
-    assert_instance_of Merchant, actual_shopin
-    assert_instance_of Merchant, actual_candis
-    assert_equal 'Shopin1901', actual_shopin.name
-    assert_equal 'Candisart', actual_candis.name
+    # actual_shopin = @m_repo.find_by_id(12334105)
+    # actual_candis = @m_repo.find_by_id(12334112)
+    # assert_instance_of Merchant, actual_shopin
+    # assert_instance_of Merchant, actual_candis
+    # assert_equal 'Shopin1901', actual_shopin.name
+    # assert_equal 'Candisart', actual_candis.name
+    assert_equal @merchant12334105, @m_repo.find_by_id(12334105)
+    assert_equal @merchant12334112, @m_repo.find_by_id(12334112)
   end
 
   def test_can_find_by_name
-    actual_shopin = @m_repo.find_by_name('Shopin1901')
-    actual_candis = @m_repo.find_by_name('Candisart')
-    assert_instance_of Merchant, actual_shopin
-    assert_instance_of Merchant, actual_candis
-    assert_equal 12334105, actual_shopin.id
-    assert_equal 12334112, actual_candis.id
+    # actual_shopin = @m_repo.find_by_name('Shopin1901')
+    # actual_candis = @m_repo.find_by_name('Candisart')
+    # assert_instance_of Merchant, actual_shopin
+    # assert_instance_of Merchant, actual_candis
+    # assert_equal 12334105, actual_shopin.id
+    # assert_equal 12334112, actual_candis.id
+    assert_equal @merchant12334105, @m_repo.find_by_name('Shopin1901')
+    assert_equal @merchant12334112, @m_repo.find_by_name('Candisart')
   end
 
   def test_returns_nil_if_no_name_found
@@ -62,18 +93,19 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_can_find_name_by_fragment
     actual = @m_repo.find_all_by_name('re')
-    result = actual.all? do |merchant|
-      merchant.class == Merchant
-    end
-    assert result
-    names = actual.map(&:name)
-    assert_equal %w[MiniatureBikez GoldenRayPress], names
+    # result = actual.all? do |merchant|
+    #   merchant.class == Merchant
+    # end
+    # assert result
+    # names = actual.map(&:name)
+    # assert_equal %w[MiniatureBikez GoldenRayPress], names
+    assert_equal [@merchant12334113, @merchant12334135], actual
   end
 
   def test_it_can_generate_next_merchant_id
-    expected = 12334136
-    actual = @m_repo.create_new_id
-    assert_equal expected, actual
+    # expected = 12334136
+    # actual = @m_repo.create_new_id
+    assert_equal 12334136, @m_repo.create_new_id
   end
 
   def test_can_create_new_merchant
@@ -116,7 +148,7 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_magic_spec_harness_method_works
-    expected = "#<MerchantRepository #{@m_repo.merchants.size} rows>"
+    expected = "#<MerchantRepository 7 rows>"
     assert_equal expected, @m_repo.inspect
   end
 end
