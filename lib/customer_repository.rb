15ce7f64f01925c90d9a2284128f2778a0dbd472
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-16 17:38:41 -0600

    Customer repository files created with initial tests.

---
+++ lib/customer_repository.rb
@@ -0,0 +1,14 @@
+require_relative './repository'
+require_relative './customer'
+require 'time'
+
+class CustomerRepository
+  include Repository
+
+  attr_reader   :repository
+
+
+
+
+  
+end
---
+++ test/customer_repository_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'time'
+require 'minitest/pride'
+require './lib/customer_repository'
+require 'pry'
+
+class CustomerRepositoryTest < Minitest::Test
+
+  def setup
+    @customer1 = [
+      [:id, '4'],
+      [:first_name, 'Leanne'],
+      [:last_name, 'Braun'],
+      [:created_at, '2012-03-27 14:54:10 UTC'],
+      [:updated_at, '2012-03-27 14:54:10 UTC']
+      ]
+    @customer2 = [
+      [:id, '35'],
+      [:first_name, 'Pamela'],
+      [:last_name, 'Gulgowski'],
+      [:created_at, '2012-03-27 14:54:18 UTC'],
+      [:updated_at, '2012-03-27 14:54:18 UTC']
+      ]
+    @customer3 = [
+      [:id, '140'],
+      [:first_name, 'Vladimir'],
+      [:last_name, 'Crist'],
+      [:created_at, '2012-03-27 14:54:43 UTC'],
+      [:updated_at, '2012-03-27 14:54:43 UTC']
+      ]
+    @customers = [@customer1, @customer2, @customer3]
+    @cr = CustomerRepository.new(@customers)
+  end
+
+  def test_it_exists
+      assert_instance_of CustomerRepository, @cr
+  end
+
+
+end
