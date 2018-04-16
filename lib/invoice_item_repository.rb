15e40dfdb5d6a01d79767066672c8ddab37fa246
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-16 13:44:16 -0600

    Finished tests and code for find_all_by_invoice_id

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -27,8 +27,11 @@ class InvoiceItemRepository
     invoice_item_hash
   end
 
-  def find_all_by_invoice_id
-    
+  def find_all_by_invoice_id(ii)
+    @repository.values.find_all do |invoice_item|
+      invoice_item.invoice_id == ii
+    end
+  end
 
 
 
