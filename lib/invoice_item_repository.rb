e9e8e69391676797b11609fcb9a6efbd64e88bd9
zdcthomas <zdcthomas@yahoo.com>
2018-04-16 19:07:45 -0600

    refactors invoiceitem repo by relocating initialize block into repository module

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -8,23 +8,7 @@ class InvoiceItemRepository
   attr_reader   :repository
 
   def initialize(invoice_items)
-    invoice_item_array = []
-    @repository = {}
-    invoice_items.each { |invoice_item| invoice_item_array << InvoiceItem.new(to_invoice_item(invoice_item))}
-    invoice_item_array.each do |invoice_item|
-      if invoice_item.nil?
-      else
-        @repository[invoice_item.id] = invoice_item
-      end
-    end
-  end
-
-  def to_invoice_item(invoice_item)
-    invoice_item_hash = {}
-    invoice_item.each do |line|
-      invoice_item_hash[line[0]] = line[1]
-    end
-    invoice_item_hash
+    create_repository(invoice_items, InvoiceItem)
   end
 
   def find_all_by_item_id(ii)
