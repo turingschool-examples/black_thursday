0667339519735b16ebfcad10f3363946e1e3f6ef
zdcthomas <zdcthomas@yahoo.com>
2018-04-16 19:43:44 -0600

    relocates item repo create to repo module and removes uneccesary methods

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -15,22 +15,6 @@ class ItemRepository
   end
 
   def create(attributes)
-    attributes[:id] = (find_highest_id + 1)
-    if attributes[:created_at].nil?
-      attributes[:created_at] = Time.now.to_s
-    else
-      attributes[:created_at] = attributes[:created_at].to_s
-    end
-    attributes[:updated_at] = attributes[:updated_at].to_s
-    item = Item.new(attributes)
-    @repository[item.id] = item
-  end
-
-  def sterilize_attributes(attributes, item)
-    temp_attr = attributes.dup
-    temp_attr[:id] = item.attributes[:id]
-    temp_attr[:merchant_id] = item.attributes[:merchant_id]
-    temp_attr[:created_at] = item.attributes[:created_at]
-    temp_attr
+    general_create(attributes, Item)
   end
 end
--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative './repository'
 require_relative './merchant'
 # holds, and provides methods for finding, merchants
@@ -7,7 +8,7 @@ class MerchantRepository
   def initialize(merchants)
     create_repository(merchants, Merchant)
   end
-  
+
   def create(attributes)
     general_create(attributes, Merchant)
   end
