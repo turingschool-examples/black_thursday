676b1bfb21b9ed52e03e19251ca43c6d4d4c9125
zdcthomas <zdcthomas@yahoo.com>
2018-04-16 21:31:47 -0600

    cleans up customer repo

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -5,23 +5,11 @@ require_relative './customer'
 # holds and retrieves customer data
 class CustomerRepository
   include Repository
-  attr_reader   :repository
+  attr_reader :repository
 
   def initialize(customers)
     create_repository(customers, Customer)
   end
-
-  # def find_all_by_first_name(name)
-  #   @repository.values.find_all do |customer|
-  #     customer.first_name.downcase.include?(name.downcase)
-  #   end
-  # end
-
-  # def find_all_by_last_name(name)
-  #   @repository.values.find_all do |customer|
-  #     customer.last_name.downcase.include?(name.downcase)
-  #   end
-  # end
 
   def create(attributes)
     general_create(attributes, Customer)
