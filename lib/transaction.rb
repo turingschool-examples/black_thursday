db400b09cb8df05aa184defd3ec54d6d262d94c6
brickstar <brickstar@gmail.com>
2018-04-18 09:11:08 -0600

    transaction naming cleanup

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -12,17 +12,17 @@ class Transaction
               :result,
               :created_at,
               :updated_at,
-              :parent
+              :transaction_repository
 
   def initialize(data, parent)
-    @id                           = data[:id].to_i
-    @invoice_id                   = data[:invoice_id].to_i
-    @credit_card_number           = data[:credit_card_number]
-    @credit_card_expiration_date  = data[:credit_card_expiration_date]
-    @result                       = data[:result].to_sym
-    @created_at                   = Time.parse(data[:created_at])
-    @updated_at                   = Time.parse(data[:updated_at])
-    @parent                       = parent
+    @id                          = data[:id].to_i
+    @invoice_id                  = data[:invoice_id].to_i
+    @credit_card_number          = data[:credit_card_number]
+    @credit_card_expiration_date = data[:credit_card_expiration_date]
+    @result                      = data[:result].to_sym
+    @created_at                  = Time.parse(data[:created_at])
+    @updated_at                  = Time.parse(data[:updated_at])
+    @transaction_repository      = parent
   end
 
   def change_updated_at
