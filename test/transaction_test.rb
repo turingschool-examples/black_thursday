e11b774535d241d33f30f906c195300e7953cac2
brickstar <brickstar@gmail.com>
2018-04-18 09:51:37 -0600

    cleanup

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -37,4 +37,18 @@ class TransactionTest < Minitest::Test
     assert_equal Time.now.to_s, @t.created_at.to_s
     assert_equal Time.now.to_s, @t.updated_at.to_s
   end
+
+  def test_change_attributes
+    assert_equal '4242424242424242', @t.credit_card_number
+    assert_equal '0220', @t.credit_card_expiration_date
+    assert_equal :success, @t.result
+
+    @t.change_credit_card_number('111111111111111')
+    @t.change_expiration_date('1111')
+    @t.change_result('failed')
+
+    assert_equal '111111111111111', @t.credit_card_number
+    assert_equal '1111', @t.credit_card_expiration_date
+    assert_equal :failed, @t.result
+  end
 end
