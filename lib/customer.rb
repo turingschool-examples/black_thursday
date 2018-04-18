3e855a8c4c257420df97002d194f59fc1cbfe315
brickstar <brickstar@gmail.com>
2018-04-18 09:17:11 -0600

    naming cleanup

--- lib/customer.rb
+++ lib/customer.rb
@@ -7,15 +7,15 @@ class Customer
               :last_name,
               :created_at,
               :updated_at,
-              :parent
+              :customer_repository
 
   def initialize(data, parent)
-    @id         = data[:id].to_i
-    @first_name = data[:first_name]
-    @last_name  = data[:last_name]
-    @created_at = Time.parse(data[:created_at])
-    @updated_at = Time.parse(data[:updated_at])
-    @parent     = parent
+    @id                  = data[:id].to_i
+    @first_name          = data[:first_name]
+    @last_name           = data[:last_name]
+    @created_at          = Time.parse(data[:created_at])
+    @updated_at          = Time.parse(data[:updated_at])
+    @customer_repository = parent
   end
 
   def change_first_name(name)
