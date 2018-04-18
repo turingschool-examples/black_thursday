496a38becfffc92b63b55f8ecf7a59ae75f16dcd
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-18 08:39:59 -0600

    Pulls sales angine and sales analyst into frontend dashboard

--- frontend/black_thursday_app.rb
+++ frontend/black_thursday_app.rb
@@ -1,10 +1,19 @@
 require 'sinatra'
-require_relative '../lib/sales_analyst'
+require_relative '../lib/sales_engine'
 
 get '/' do
   "Black Thursday!"
 end
 
 get '/dashboard' do
-  haml :dashboard
+  engine = SalesEngine.from_csv(
+    customers: './data/customers.csv',
+    invoices: './data/invoices.csv',
+    invoice_items: './data/invoice_items.csv',
+    items: './data/items.csv',
+    merchants: './data/merchants.csv',
+    transactions: './data/transactions.csv'
+  )
+  analyst = engine.analyst
+  haml :dashboard, :locals
 end
