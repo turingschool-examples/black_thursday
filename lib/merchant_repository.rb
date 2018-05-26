ec443a6b72a7b862bca5adae1213c39c6bd66b8d
Seth Schwartz <sschwartz214@gmail.com>
2018-05-26 16:44:13 -0600

    adds instance variables

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,7 +1,12 @@
 class MerchantRepository
 
+  def initialzie(merchant_data, parent)
+    @merchant_repo = merchant_data.map {|merchant| Merchant.new(merchant, self)}
+    @parent = parent
+  end
+
   def all
-    
+
   end
 
 end
