ffab4b5877f0bc93e74a23a05a998cf85ee01191
amcolling <amcolling@gmail.com>
2018-05-26 15:56:57 -0600

    created instance of salesengine

---
+++ lib/salesengine.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class SalesEngine
+  attr_reader :items,
+              :merchants
+
+  def initialize
+    @items
+    @merchants
+  end
+
+  def from_csv(sales_data)
+    @items = CSV.read(sales_data[:items])
+    @merchants = CSV.read(sales_data[:merchants])
+    binding.pry
+  end
+
+end
