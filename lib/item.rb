bc59c9daf06cbcae117bc682e067b4775390ecc0
Angela Duran <duranangela19@gmail.com>
2018-05-26 22:14:42 -0600

    Made item attributes retrievable

--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,18 @@
 class Item
-  attr_reader :item
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :created_at,
+              :updated_at
 
   def initialize(item)
-    @item = item
+      @id   =        item[:id].to_i,
+      @name    =    item[:name],
+      @description = item[:description],
+      @unit_price =   item[:unit_price],
+      @created_at =  item[:created_at].to_s,
+      @updated_at =  item[:updated_at].to_s
   end
 
 end
