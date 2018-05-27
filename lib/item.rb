4087ecf585342aca72d60c65d677edcf832d4cfa
Stephen Malek <malek.stephen44@gmail.com>
2018-05-27 17:49:28 -0600

    adds method for creating new instance of item

--- lib/item.rb
+++ lib/item.rb
@@ -15,10 +15,10 @@ class Item
     @id = data[:id].to_i
     @name = data[:name]
     @description = data[:description]
-    @unit_price = BigDecimal.new(data[:unit_price]) / 100
     @merchant_id = data[:merchant_id].to_i
-    @created_at = Time.parse(data[:created_at])
-    @updated_at = Time.parse(data[:updated_at])
+    @created_at = Time.parse(data[:created_at].to_s)
+    @updated_at = Time.parse(data[:updated_at].to_s)
     @parent = parent
+    @unit_price = BigDecimal.new(data[:unit_price]) / 100
   end
 end
--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -28,4 +28,36 @@ class ItemRepository
   def find_all_by_price(price)
     item_repo.find_all {|item| item.unit_price.to_f == price.to_f}
   end
+
+  def find_all_by_price_in_range(range)
+    item_repo.find_all {|item| range.include?(item.unit_price.to_f)}
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    item_repo.find_all {|item| item.merchant_id == merchant_id}
+  end
+
+  def create(attributes)
+    highest = all.max_by {|item| item.id.to_i }
+    item = {name: attributes[:name],
+            description: attributes[:description],
+            unit_price: convert_price(attributes),
+            id: (highest.id + 1),
+            created_at: attributes[:created_at],
+            updated_at: attributes[:updated_at],
+            merchant_id: attributes[:merchant_id]}
+    @item_repo.push(Item.new(item, self))
+  end
+
+  def convert_price(attributes)
+    if attributes[:unit_price].class == BigDecimal
+      (attributes[:unit_price] * 100).to_i
+    else
+      attributes[:unit_price]
+    end
+  end
+
+  def inspect
+   "#{self.class} #{@item_repo.size} rows"
+  end
 end
