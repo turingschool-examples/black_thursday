fc0833efb8a9a417c146399ec49c6d8cdec40e2e
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-05-28 16:08:50 -0600

    Add tests to sales_engine.

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,8 +1,14 @@
 require 'csv'
+require './lib/repository'
+require './lib/item_repository'
+require './lib/merchant_repository'
 
 class SalesEngine
+  attr_reader   :items,
+                :merchants
 
   def csv_to_hash(path)
+    binding.pry
     raw_input = CSV.read(path)
     raw_input.map do | pre_hash |
       pre_hash.to_h
@@ -10,19 +16,21 @@ class SalesEngine
   end
 
   def hashes_to_repos(hashes, blank_repo)
+    binding.pry
     hashes.each do | hash |
       blank_repo.create(hash)
     end
   end
+
   def from_csv(paths)
     item_hashes = csv_to_hash(paths[:items])
     @items = ItemRepository.new
+    binding.pry
     hashes_to_repos(item_hashes, @items)
+    binding.pry
 
     merchant_hashes = csv_to_hash(paths[:merchants])
     @merchants = MerchantRepository.new
     hashes_to_repos(merchant_hashes, @merchants)
   end
-
-
 end
