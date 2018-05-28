c350d92e9c0c0134e224616320ff9e1e844103e4
Chris Powell <chris.ian.powell@gmail.com>
2018-05-27 18:22:50 -0600

    items repo class is loading item instances from csv

--- lib/items_repository.rb
+++ lib/items_repository.rb
@@ -1,5 +1,18 @@
+require_relative 'item'
+
 class ItemsRepository
-  def initialize(items)
-    @items = items
-  end 
+  attr_reader :items_csv,
+              :items
+
+  def initialize(items_csv)
+    @items_csv = items_csv
+    @items = []
+  end
+
+  def load_items(items_csv)
+    items_csv.each do |item|
+      @items << Item.new(item)
+    end
+  end
+  
 end
--- test/items_repository_test.rb
+++ test/items_repository_test.rb
@@ -13,4 +13,15 @@ class ItemsRepositoryTest < Minitest::Test
   def test_items_repo_exists
     assert_instance_of ItemsRepository, @ir
   end
+
+  def test_it_initializes_empty
+    assert_equal [], @ir.items
+  end
+
+  def test_it_can_load_items
+    csv = @ir.items_csv
+    @ir.load_items(csv)
+
+    assert_equal 5, @ir.items.length
+  end
 end
