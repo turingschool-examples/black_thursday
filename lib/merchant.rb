be0c4348c905378b73491bcf6953c40eb32a49db
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-05-28 12:13:32 -0600

    Remove merchant attribute from merchant class

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,9 +1,8 @@
 class Merchant
-  attr_reader :merchant, :id, :created_at
+  attr_reader :id, :created_at
   attr_accessor :name, :updated_at
 
   def initialize(merchant)
-    @merchant = merchant
     @id = merchant[:id].to_i
     @name = merchant[:name]
     @created_at = merchant[:created_at]
