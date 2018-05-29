a8299647df233b1748aea9e60adac3948e91e6b9
Connor <connorosburn@gmail.com>
2018-05-29 16:22:35 -0600

    All iteration 0 passing harness

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,5 @@
 require 'bigdecimal'
+require 'time'
 
 class Item
   attr_accessor     :id,
@@ -16,9 +17,18 @@ class Item
     @merchant_id = item_attributes[:merchant_id].to_i
     @name = item_attributes[:name]
     @description = item_attributes[:description]
-    @unit_price = BigDecimal.new(item_attributes[:unit_price] / 100)
-    @created_at = Time.new
-    @updated_at = Time.new
+    @unit_price = BigDecimal.new(item_attributes[:unit_price].to_i) / 100
+
+    @created_at = time_conversion(item_attributes[:created_at])
+    @updated_at = time_conversion(item_attributes[:updated_at])
+  end
+
+  def time_conversion(time)
+    if time.class == String
+      time = Time.parse(time)
+    end
+
+    time
   end
 
   def unit_price_to_dollars
