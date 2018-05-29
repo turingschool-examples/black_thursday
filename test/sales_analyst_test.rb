c1dce039ec12217be2e09c86d00fbe4bbd61bdc6
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-05-29 17:15:34 -0600

    Add more tests to solidify certainty of data analysis.

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,31 +1,56 @@
-# require './test/test_helper'
-# require './lib/sales_engine'
-# require './lib/sales_analyst'
-#
-# class SalesAnalyst < Minitest::Test
-#   def test_sales_analyst_exists
-#     skip
-#     @se = SalesEngine.new
-#     @sa = @se.analyst
-#   end
-#
-#   def test_can_sum_items_per_merchant
-#     skip
-#     assert_equal 23, @sa.find_sum(merchant)
-#   end
-#
-#   def test_analyst_finds_average_items_per_merchant
-#   end
-#
-#   def test_analyst_finds_standard_deviation_of_items_per_merchant
-#   end
-#
-#   def test_analyst_finds_which_merchants_sell_the_most_items
-#   end
-#
-#   def test_analyst_finds_average_price_per_merchant
-#   end
-#
-#   def test_analyst_finds_golden_items
-#   end
-# end
+require './test/test_helper'
+require './lib/sales_engine'
+require './lib/sales_analyst'
+
+class SalesAnalyst < Minitest::Test
+  def test_sales_analyst_exists
+    se = SalesEngine.from_csv({
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+    })
+    @sa = se.analyst
+  end
+
+  def test_sales_analyst_finds_average_items_per_merchant
+    expected = @sa.average_items_per_merchant
+
+    assert_equal 2.88, expected
+    assert_equal Float, expected.class
+  end
+
+  def test_sales_analyst_finds_standard_deviation_of_items_per_merchant
+    expected = @sa.average_items_per_merchant_standard_deviation
+
+    assert_equal 3.26, expected
+    assert_equal Float, expected.class
+  end
+
+  def test_sales_analyst_finds_which_merchants_have_high_item_counts
+    expected = @sa.merchants_with_high_item_count
+
+    assert_equal 52, expected
+    assert_equal Merchant, expected.first.class
+  end
+
+  def test_sales_analyst_finds_average_item_price_per_merchant
+    merchant_id = 12334105
+    expected = @sa.average_item_price_for_merchant(merchant_id)
+
+    assert_equal 16.66, expected
+    assert_equal BigDecimal, expected.class
+  end
+
+  def test_sales_analyst_finds_average_average_price_per_merchant
+    expected = @sa.average_average_price_per_merchant
+
+    assert_equal 350.29,
+    assert_equal BigDecimal, expected.class
+  end
+
+  def test_sales_analyst_finds_golden_items
+    expected = @sa.golden_items
+
+    assert_equal 5, expected.length
+    assert_equal Item, expected.first.class
+  end
+end
