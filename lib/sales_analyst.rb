5941e4cf41e3e0ee87b2145cd5b852e05a314287
JohnRoemer <jt.r@live.com>
2018-05-29 19:49:41 -0600

    analyst can get average items per merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -5,4 +5,13 @@ class SalesAnalyst
     @sales_engine = sales_engine
   end
 
+  def average_items_per_merchant
+    amount_of_items = @sales_engine.items.collection.length
+    amount_of_merchants = @sales_engine.merchants.collection.length
+    average(amount_of_items, amount_of_merchants)
+  end
+
+  def average(first_list, seond_list)
+    (first_list.to_f / seond_list.to_f).round(2)
+  end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -10,12 +10,23 @@ class SalesAnalystTest < MiniTest::Test
 
   def test_it_exists
     sales_engine = SalesEngine.from_csv({
-      :items => "./data/item_sample.csv",
-      :merchants => "./data/merchant_sample.csv"
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
     })
 
     sales_analyst = sales_engine.analyst
 
     assert_instance_of SalesAnalyst, sales_analyst
+  end
+
+  def test_it_can_get_average_items_per_merchant
+    sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    sales_analyst = sales_engine.analyst
+
+    assert_equal 2.88, sales_analyst.average_items_per_merchant
   end
 end
