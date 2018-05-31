69aaeb33982a5b4a943a8e3c8a1f0ca9be0baeac
Andrew Mockett <califormula@gmail.com>
2018-05-31 13:24:06 -0600

    transaction class finished

---
+++ lib/transaction.rb
@@ -0,0 +1,21 @@
+class Transaction
+
+  attr_reader :id,
+              :invoice_id,
+              :credit_card_number,
+              :credit_card_expiration_date,
+              :result,
+              :created_at,
+              :updated_at
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @invoice_id = attributes[:invoice_id]
+    @credit_card_number = attributes[:credit_card_number]
+    @credit_card_expiration_date = attributes[:credit_card_expiration_date]
+    @result = attributes[:result].to_sym
+    @created_at = attributes[:created_at]
+    @updated_at = attributes[:updated_at]
+  end
+
+end
