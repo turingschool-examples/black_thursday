743fa3fa959bf7e26fefa139a706437bd00801b7
JohnRoemer <jt.r@live.com>
2018-05-31 09:56:17 -0600

    starts invoice and invoice repo

--- test/math_methods_test.rb
+++ test/math_methods_test.rb
@@ -1,14 +1,14 @@
-# require './test_helper'
-# require './test/mock.rb'
-#
-#
-# class MathMethodsTest < MiniTest::Test
-#
-#   def setup
-#     @tester = Mock.new
-#   end
-#
-#   def test_it_can_get_mean
-#     assert_equal 4, @tester.mean([3,4,5])
-#   end
-# end
+require './test_helper'
+
+
+
+class MathMethodsTest < MiniTest::Test
+
+  def setup
+    @tester = Mock.new
+  end
+
+  def test_it_can_get_mean
+    assert_equal 4, @tester.mean([3,4,5])
+  end
+end
