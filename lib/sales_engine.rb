81859201f1c230377174f3f31c8cb568c6e99e62
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-05-31 09:52:19 -0600

    merge conflicts

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,19 +1,11 @@
 require_relative 'merchant_repository'
 require_relative 'item_repository'
+require_relative 'sales_analyst'
 require_relative 'file_reader'
 require 'csv'
 
 class SalesEngine
   include FileReader
-  # builds access to items and merchants
-  # attr_reader :merchants, :items
-
-  # def self.from_csv(file_path)
-  #   @items = ItemRepository.new(FileReader.load(file_path[:items]))
-  #   @merchants = MerchantRepository.new(FileReader.load(file_path[:merchants]))
-  # end
-
-# end
 
   def initialize(file_path)
     @file_path = file_path
@@ -29,5 +21,9 @@ class SalesEngine
 
   def items
     ItemRepository.new(FileReader.load(@file_path[:items]))
+  end
+
+  def analyst
+    SalesAnalyst.new(self)
   end
 end
