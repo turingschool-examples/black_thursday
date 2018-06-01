fdadf96a0e7314bd7d63f59d3ab3aeee2bec35c8
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-06-01 08:07:16 -0600

    Make additional refactors to sales_analyst class.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,7 +1,11 @@
 require_relative 'mathematics_module'
+require_relative './analyst_assistants/item_assistant'
+require_relative './analyst_assistants/invoice_assistant'
 
 class SalesAnalyst
   include Mathematics
+  include ItemAssistant
+  include InvoiceAssistant
 
   attr_reader     :items,
                   :merchants,
@@ -19,128 +23,18 @@ class SalesAnalyst
     @customers = customers
   end
 
-  def items_per_merchant
-    items_per_merchant = Hash.new(0)
-    @items.members.each do | item |
-      items_per_merchant[item.merchant_id] += 1
-    end
-    items_per_merchant.values
-  end
-
   def average_items_per_merchant
-    calculate_average(items_per_merchant)
+    calculate_average(items_by_merchant_id.values)
   end
 
   def average_items_per_merchant_standard_deviation
-    calculate_standard_deviation(items_per_merchant)
-  end
-
-  def invoices_per_merchant
-    invoices_per_merchant = Hash.new(0)
-    @invoices.members.each do | invoice |
-      invoices_per_merchant[invoice.merchant_id] += 1
-    end
-    invoices_per_merchant.values
-  end
-
-  def average_invoices_per_merchant
-    calculate_average(invoices_per_merchant)
-  end
-
-  def average_invoices_per_merchant_standard_deviation
-    calculate_standard_deviation(invoices_per_merchant)
-  end
-
-  def items_by_merchant_id
-    items_per_merchant = Hash.new(0)
-    @items.members.each do | item |
-      items_per_merchant[item.merchant_id] += 1
-    end
-    items_per_merchant
-  end
-
-  def merchant_ids_with_more_items_than_one_standard_deviation
-    higher_than = average_items_per_merchant + average_items_per_merchant_standard_deviation
-    ids = items_by_merchant_id.keys.map do | merchant |
-      if items_by_merchant_id[merchant] > higher_than
-        merchant
-      end
-    end.compact
+    calculate_standard_deviation(items_by_merchant_id.values)
   end
 
   def merchants_with_high_item_count
     merchant_ids_with_more_items_than_one_standard_deviation.map do | merchant |
       @merchants.find_by_id(merchant)
     end
-  end
-
-  def invoices_by_merchant_id
-    invoices_per_merchant = Hash.new(0)
-    @invoices.members.each do | invoice |
-      invoices_per_merchant[invoice.merchant_id] += 1
-    end
-    invoices_per_merchant
-  end
-
-  def merchant_ids_with_more_invoices_than_two_standard_deviations_above_average
-    higher_than = average_invoices_per_merchant + (average_invoices_per_merchant_standard_deviation * 2)
-    ids = invoices_by_merchant_id.keys.map do | merchant |
-      if invoices_by_merchant_id[merchant] > higher_than
-        merchant
-      end
-    end.compact
-  end
-
-  def top_merchants_by_invoice_count
-    merchant_ids_with_more_invoices_than_two_standard_deviations_above_average.map do | merchant |
-      @merchants.find_by_id(merchant)
-    end
-  end
-
-  def merchant_ids_with_less_invoices_than_two_standard_deviations_below_average
-    lower_than = average_invoices_per_merchant - (average_invoices_per_merchant_standard_deviation * 2)
-    ids = invoices_by_merchant_id.keys.map do | merchant |
-      if invoices_by_merchant_id[merchant] < lower_than
-        merchant
-      end
-    end.compact
-  end
-
-  def bottom_merchants_by_invoice_count
-    merchant_ids_with_less_invoices_than_two_standard_deviations_below_average.map do | merchant |
-      @merchants.find_by_id(merchant)
-    end
-  end
-
-  def top_days_by_invoice_count
-    by_day = @invoices.members.group_by do |invoice|
-      invoice.created_at.strftime('%A')
-    end
-
-    number_by_day = by_day.values.map do |day|
-      day.count
-    end
-
-    std_dev = calculate_standard_deviation(number_by_day)
-    mean = calculate_average(number_by_day)
-    highest_days = []
-    by_day.each_pair do |day, invoices|
-      if invoices.count > (mean + std_dev)
-        highest_days << day
-      end
-    end
-    highest_days
-  end
-
-  def invoice_status(state)
-    divided = 0
-    total = @invoices.members.count
-    @invoices.members.map do |invoice|
-      if invoice.status == state
-        divided += 1
-      end
-    end
-    ((divided.to_f / total.to_f) * 100).round(2)
   end
 
   def average_item_price_for_merchant(merchant_id)
@@ -189,6 +83,58 @@ class SalesAnalyst
       end
     end.compact
   end
+
+  def average_invoices_per_merchant
+    calculate_average(invoices_by_merchant_id.values)
+  end
+
+  def average_invoices_per_merchant_standard_deviation
+    calculate_standard_deviation(invoices_by_merchant_id.values)
+  end
+
+  def top_merchants_by_invoice_count
+    merchant_ids_with_more_invoices_than_two_standard_deviations_above_average.map do | merchant |
+      @merchants.find_by_id(merchant)
+    end
+  end
+
+  def bottom_merchants_by_invoice_count
+    merchant_ids_with_less_invoices_than_two_standard_deviations_below_average.map do | merchant |
+      @merchants.find_by_id(merchant)
+    end
+  end
+
+  def top_days_by_invoice_count
+    by_day = @invoices.members.group_by do |invoice|
+      invoice.created_at.strftime('%A')
+    end
+
+    number_by_day = by_day.values.map do |day|
+      day.count
+    end
+
+    std_dev = calculate_standard_deviation(number_by_day)
+    mean = calculate_average(number_by_day)
+    highest_days = []
+    by_day.each_pair do |day, invoices|
+      if invoices.count > (mean + std_dev)
+        highest_days << day
+      end
+    end
+    highest_days
+  end
+
+  def invoice_status(state)
+    divided = 0
+    total = @invoices.members.count
+    @invoices.members.map do |invoice|
+      if invoice.status == state
+        divided += 1
+      end
+    end
+    ((divided.to_f / total.to_f) * 100).round(2)
+  end
+
 
   def invoice_paid_in_full?(inv_id)
     success_array = []
