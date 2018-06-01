eed49c13b849cf89bf2dd57e89e149ca207d5ac8
Andrew Mockett <califormula@gmail.com>
2018-05-31 20:47:14 -0600

    customer repo done

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,11 +6,16 @@ class Customer
               :updated_at
 
   def initialize(attributes)
-    @id = attributes[:id]
+    @id = attributes[:id].to_i
     @first_name = attributes[:first_name]
     @last_name = attributes[:last_name]
     @created_at = attributes[:created_at]
     @updated_at = attributes[:updated_at]
   end
 
+  def update(attributes)
+    @first_name = attributes[:first_name] || @first_name
+    @last_name = attributes[:last_name] || @last_name
+    @updated_at = Time.new
+  end
 end
