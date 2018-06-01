0285a10c313487d46d66110b4e342b54d2b403ed
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-01 10:22:56 -0600

    transaction class and test are 100% styled. test has 100% coverage

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: false
+
 require_relative 'test_helper'
 require './lib/transaction'
 
 class TransactionTest < Minitest::Test
-
   def setup
     @args = {
-      :id                           => '6',
-      :invoice_id                   => '8',
-      :credit_card_number           => '4242424242424242',
-      :credit_card_expiration_date  => '0220',
-      :result                       => 'success',
-      :created_at                   => '2016-01-11 09:34:06 UTC',
-      :updated_at                   => '2007-06-04 21:35:10 UTC'
-      }
+      id:                          '6',
+      invoice_id:                  '8',
+      credit_card_number:         '4242424242424242',
+      credit_card_expiration_date: '0220',
+      result:                      'success',
+      created_at:                  '2016-01-11 09:34:06 UTC',
+      updated_at:                  '2007-06-04 21:35:10 UTC'
+    }
     @transaction = Transaction.new(@args)
   end
 
@@ -35,7 +36,7 @@ class TransactionTest < Minitest::Test
 
   def test_time_attributes_for_updated
     assert_instance_of Time, @transaction.updated_at
-    assert_equal 06, @transaction.updated_at.mon
+    assert_equal 0o6, @transaction.updated_at.mon
     assert_equal 21, @transaction.updated_at.hour
   end
 end
