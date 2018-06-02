520886bb222253b569bd0ad0b9b14d027fc9a37f
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-06-02 12:44:38 -0600

    Refactor top revenue earners into 4 separate methods

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -16,6 +16,7 @@ class SalesAnalyst
     @price_standard_deviation = item_price_standard_deviation
     @average_invoices = average_invoices_per_merchant
     @invoice_standard_deviation = average_invoices_per_merchant_standard_deviation
+    @revenue_for_merchants = total_revenue_for_each_merchant 
   end
 
   def average_items_per_merchant
@@ -261,7 +262,7 @@ class SalesAnalyst
     end
   end
 
-  def top_revenue_earners(num = 20)
+  def total_revenue_for_each_merchant
     earners = {}
     invoices_grouped_by_merchant.each do |merchant_id, invoices|
       earners[merchant_id] = invoices.map do |invoice|
@@ -270,19 +271,31 @@ class SalesAnalyst
         end
       end.compact.inject(:+)
     end
-    earners.keep_if do |merchant_id, earned|
+    earners
+  end
+
+  def merchants_with_a_sale
+    @revenue_for_merchants.keep_if do |merchant_id, earned|
       earned != nil
     end
-    earners.keep_if do |merchant_id, earned|
-      earners.values.sort[-num..-1].include?(earned)
-    end
-    top_values = earners.values.sort
-    top_merchants = []
-    top_values.each do |value|
-      earners.each do |merchant_id, earned|
-        top_merchants << merchant_id if earned == value
+  end
+
+  def hash_to_array_ordered_by_value(hash)
+    sorted_values = hash.values.sort
+    array = []
+    sorted_values.each do |value|
+      hash.each do |key, pair_value|
+        array << key if pair_value == value
       end
     end
+  end
+
+  def top_revenue_earners(num = 20)
+    merchants_with_a_sale.keep_if do |merchant_id, earned|
+      sorted_merchants = merchants_with_a_sale.values.sort
+      sorted_merchants[-num..-1].include?(earned)
+    end
+    top_merchants = hash_to_array_ordered_by_value(merchants_with_a_sale)
     top_merchants.map do |merchant_id|
       @merchants.find_by_id(merchant_id)
     end.reverse
