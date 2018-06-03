4ff36356068170dce680754e6a75ce8a2895a406
Chris Powell <chris.ian.powell@gmail.com>
2018-06-03 15:46:38 -0600

    push for update

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -7,72 +7,89 @@ class SalesAnalystTest < Minitest::Test
     se = SalesEngine.from_csv({
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
-      :invoices  => "./data/invoices.csv"
+      :invoices  => "./data/invoices.csv",
+      :invoice_items => "./data/invoice_items.csv",
+      :transactions => "./data/transactions.csv",
+      :customers => "./data/customers.csv"
     })
 
     @sa = se.analyst
   end
 
   def test_it_exists
+    skip
     assert_instance_of SalesAnalyst, @sa
   end
 
   def test_it_has_repos
+    skip
     assert_instance_of ItemsRepository, @sa.items
     assert_instance_of MerchantRepo, @sa.merchants
   end
 
   def test_average_items_per_merchant
+    skip
     assert_equal 2.88, @sa.average_items_per_merchant
   end
 
   def test_average_item_price_for_merchant
+    skip
     merchant_id = 12334159
     avg_price = @sa.average_item_price_for_merchant(merchant_id)
     assert_instance_of BigDecimal, avg_price
   end
 
   def test_average_average_price_per_merchant
+    skip
     avg_avg = @sa.average_average_price_per_merchant
     assert_instance_of BigDecimal, avg_avg
   end
 
   def test_average_items_per_merchant_standard_deviation
+    skip
     assert_equal 3.26, @sa.average_items_per_merchant_standard_deviation
   end
 
   def test_merchants_with_high_item_count
+    skip
     assert_equal 52, @sa.merchants_with_high_item_count.length
   end
 
   def test_golden_items
+    skip
     assert_equal 5, @sa.golden_items.length
   end
 
   def test_average_invoices_per_merchant
+    skip
     assert_equal 10.49, @sa.average_invoices_per_merchant
   end
 
   def test_average_invoices_per_merchant_standard_deviation
+    skip
     assert_equal 3.29, @sa.average_invoices_per_merchant_standard_deviation
   end
 
   def test_top_merchants_by_invoice_count
+    skip
     assert_instance_of Merchant, @sa.top_merchants_by_invoice_count.first
     assert_equal 12, @sa.top_merchants_by_invoice_count.length
   end
 
   def test_bottom_merchants_by_invoice_count
+    skip
     assert_instance_of Merchant, @sa.bottom_merchants_by_invoice_count.first
     assert_equal 4, @sa.bottom_merchants_by_invoice_count.length
   end
 
   def test_top_days_by_invoice_count
+    skip
     assert_equal 1, @sa.top_days_by_invoice_count.length
     assert_equal "Wednesday", @sa.top_days_by_invoice_count.first
   end
 
   def test_invoice_status
+    skip
     assert_equal 29.55, @sa.invoice_status(:pending)
 
 
