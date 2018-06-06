5b96a250779a1ca329b7129b016b7ee64f918239
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-05 22:48:40 -0600

    customer and transaction repo styled 100%

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -29,7 +29,7 @@ class TransactionRepository
   def update(id, attributes)
     transaction = find_by_id(id)
     unless transaction.nil?
-      transaction.result      = attributes[:result] if attributes[:result]
+      transaction.result = attributes[:result] if attributes[:result]
       exp_date = attributes[:credit_card_expiration_date]
       transaction.credit_card_expiration_date = exp_date if exp_date
       cc_number = attributes[:credit_card_number]
