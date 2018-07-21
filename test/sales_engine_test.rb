3a3150c7daf5a6a5b93f2700715ac6aab8249aab
Tara Craig <tara.craig7@gmail.com>
2018-07-21 16:33:28 -0600

    Update test it exists

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,23 +3,24 @@ require_relative "../lib/sales_engine"
 
 class SalesEngineTest < Minitest::Test
   def setup
-    @content = {
+    @csv_hash = {
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
     }
-    @sales_engine = SalesEngine.from_csv(@content)
+    @sales_engine = SalesEngine.from_csv(@csv_hash)
   end
 
   def test_it_exists
     assert_instance_of SalesEngine, @sales_engine
   end
 
-  def test_it_has_attributes
-    assert_equal @content, @sales_engine.content
-  end
+  # def test_it_has_attributes
+  #   skip
+  #   assert_equal , @sales_engine.
+  # end
 
-  def test_can_get_data_from_csv
-    assert_equal @content, @sales_engine.from_csv(@content)
+  def test_it_can_get_data_from_csv
+    assert_equal @csv_hash, @sales_engine
   end
 
 
