e3a371f9c786e98398494731ad8e6e7db7035f16
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-20 19:12:52 -0600

    adds test for attributes

--- lib/item.rb
+++ lib/item.rb
@@ -4,7 +4,10 @@ class Item
     @id = information[:id]
     @name = information[:name]
     @description = information[:description]
-    @unit_price = information[]
+    @unit_price = information[:unit_price]
+    @merchant_id = information[:merchant_id]
+    @created_at = information[:created_at]
+    @updated_at = information[:updated_at]
   end
 
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -1,15 +1,35 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'bigdecimal'
 require_relative '../lib/item.rb'
 
 class ItemTest < Minitest::Test
   def test_it_exists
-    item = Item.new
+    item = Item.new({
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+})
 
     assert_instance_of Item, item
   end
 
-  
+  def test_it_has_attributes
+
+    item = Item.new({
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+})
+
+    assert_equal "Pencil", item.name
+    assert_equal "You can use it to write things", item.description
+  end
+
 
 
 end
