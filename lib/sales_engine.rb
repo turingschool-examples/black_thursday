29d6361525660f2057831b387c836f0d2bcdeba6
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-21 15:21:43 -0600

    add tests for SalesEngine & make tests pass

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,8 +2,12 @@ require_relative "./file_loader"
 
 class SalesEngine
   include FileLoader
+  attr_reader :content
   def initialize(content)
     @content = content
   end
-  
+
+  def self.from_csv(content)
+    
+  end
 end
