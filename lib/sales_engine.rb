bb0528f25db6e322df8face06742797171a101d3
Tara Craig <tara.craig7@gmail.com>
2018-07-21 16:49:00 -0600

    Clean up

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,17 +1,18 @@
+require_relative './file_loader'
 require_relative './merchant_repository'
 
 class SalesEngine
 
   attr_reader     :location
 
+  def self.from_csv(location)
+    SalesEngine.new(location)
+  end
 
   def initialize(location)
     @file_loader = FileLoader.new(location)
     @location = location
   end
 
-  def self.from_csv(location)
-    SalesEngine.new(location)
-  end
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,26 +2,14 @@ require_relative "test_helper.rb"
 require_relative "../lib/sales_engine"
 
 class SalesEngineTest < Minitest::Test
-  def setup
-    @csv_hash = {
+
+  def test_it_exists
+    csv_hash = {
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
     }
-    @sales_engine = SalesEngine.from_csv(@csv_hash)
+    se = SalesEngine.from_csv(csv_hash)
+
+    assert_instance_of SalesEngine, se
   end
-
-  def test_it_exists
-    assert_instance_of SalesEngine, @sales_engine
-  end
-
-  # def test_it_has_attributes
-  #   skip
-  #   assert_equal , @sales_engine.
-  # end
-
-  def test_it_can_get_data_from_csv
-    assert_equal @csv_hash, @sales_engine
-  end
-
-
 end
