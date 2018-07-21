601c01e384b6e80923e66b419aeb82ee6e68f144
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-21 12:44:29 -0600

    adds create method

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,6 +1,15 @@
+require './lib/merchant'
+
 class MerchantRepository
 
-def
+  def initialize
+    @merchants = []
+  end
 
+  def create(params)
+    Merchant.new(params).tap do |merchant|
+      @merchants << merchant
+    end
+  end
 
 end
