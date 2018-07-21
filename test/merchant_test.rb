f4ed83f86f62dfccbef53254a4aae1a19b956014
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-21 17:56:42 -0600

    finishes tests for merchant, merchant_repo, sales_engine up to this point

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,19 +4,18 @@ require_relative '../lib/merchant.rb'
 
 class MerchantTest < Minitest::Test
 
+  def setup
+    @merchant = Merchant.new({:id => 5, :name => "Turing School"})
+  end
   def test_it_exists
-    merchant = Merchant.new({:id => 5, :name => "Turing School"})
-    assert_instance_of Merchant, merchant
+    assert_instance_of Merchant, @merchant
   end
 
   def test_it_has_a_name
-    merchant = Merchant.new({:id => 5, :name => "Turing School"})
-    assert_equal "Turing School", merchant.name
+    assert_equal "Turing School", @merchant.name
   end
 
   def test_it_has_a_id
-    merchant = Merchant.new({:id => 5, :name => "Turing School"})
-    assert_equal 5, merchant.id
+    assert_equal 5, @merchant.id
   end
-
 end
