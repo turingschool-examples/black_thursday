c74c6798d5cea2b6c1e8323da508d770beded965
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-20 19:46:21 -0600

    updates tests for items

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,13 @@
+require 'bigdecimal'
+
 class Item
+    attr_reader :id,
+                :name,
+                :description,
+                :unit_price,
+                :merchant_id,
+                :created_at,
+                :updated_at
 
   def initialize(information)
     @id = information[:id]
