9638ec4faba6e78e4ddd5a664bb3818abe24c752
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-21 14:48:56 -0600

    add item repo functionality, refactor merchant

--- lib/item.rb
+++ lib/item.rb
@@ -1,19 +1,22 @@
 require 'bigdecimal'
 
 class Item
-  attr_reader :name,
+  attr_reader :id,
+              :name,
               :description,
               :unit_price,
               :created_at,
-              :updated_at
+              :updated_at,
+              :merchant_id
 
   def initialize(params)
-    #will need id and merchant id
+    @id          = params[:id]
     @name        = params[:name]
     @description = params[:description]
     @unit_price  = params[:unit_price]
     @created_at  = params[:created_at]
     @updated_at  = params[:updated_at]
+    @merchant_id = params[:merchant_id]
   end
 
   def unit_price_to_dollars
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -8,7 +8,12 @@ require './lib/merchant'
 class MerchantTest < Minitest::Test
 
   def setup
-    @merchant = Merchant.new({:id => 5, :name => "Turing School"})
+    @merchant = Merchant.new({
+      :id         => 5,
+      :name       => "Turing School",
+      :created_at => "2009-05-30",
+      :updated_at => "2010-08-29"
+      })
   end
 
   def test_it_exists
@@ -18,6 +23,8 @@ class MerchantTest < Minitest::Test
   def test_it_has_attributes
     assert_equal 5, @merchant.id
     assert_equal "Turing School", @merchant.name
+    assert_equal "2009-05-30", @merchant.created_at
+    assert_equal "2010-08-29", @merchant.updated_at
   end
 
 end
