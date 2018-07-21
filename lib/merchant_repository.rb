05bb1c6ac790f1c24e77005e0d927238c15b3fe0
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-21 13:05:21 -0600

    adds test for find all by name

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -12,4 +12,20 @@ class MerchantRepository
     end
   end
 
+  def all
+    @merchants
+  end
+
+  def find_by_id(id)
+    @merchants.find do |merchant|
+      merchant.id == id
+    end
+  end
+
+  def find_by_name(name)
+    @merchants.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -17,7 +17,30 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_it_creates_new_merchant_instance
-    assert_instance_of Merchant, mr.create({:id => 5, :name => "Turing School"})
+    assert_instance_of Merchant, @mr.create({:id => 5, :name => "Turing School"})
   end
 
+  def test_it_returns_array_of_all_merchants
+    created = @mr.create({:id => 5, :name => "Turing School"})
+    assert_equal [created], @mr.all
+  end
+
+  def test_it_can_find_by_id
+    created = @mr.create({:id => 5, :name => "Turing School"})
+    assert_equal created, @mr.find_by_id(5)
+  end
+
+  def test_it_can_find_by_name
+    created = @mr.create({:id => 5, :name => "Turing School"})
+    assert_equal created, @mr.find_by_name("Turing School")
+  end
+
+  def test_it_can_find_all_by_name
+    created1 = @mr.create({:id => 5, :name => "Turing School"})
+    created2 = @mr.create({:id => 12, :name => "turing store"})
+    created3 = @mr.create({:id => 888, :name => "BoutiqueTuring"})
+    created4 = @mr.create({:id => 1115, :name => "Kmart"})
+    expected = [created1, created2, created3]
+    assert_equal expected, @mr.find_all_by_name("Turing")
+  end
 end
