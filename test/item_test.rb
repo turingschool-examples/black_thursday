a850df9b8b865fcd04b25fe5a3922f422a1ed73a
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-21 19:12:28 -0600

    finishes adding attributes to item class

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,11 +4,13 @@ require_relative '../lib/item'
 class ItemTest < MiniTest::Test
   def setup
     @item = Item.new({
+                      :id          => 1,
                       :name        => "Pencil",
                       :description => "You can use it to write things"
-                      # :unit_price  => BigDecimal.new(10.99,4),
-                      # :created_at  => Time.now,
-                      # :updated_at  => Time.now
+                      :unit_price  => BigDecimal.new(10.99,4),
+                      :created_at  => Time.now,
+                      :updated_at  => Time.now,
+                      :merchant_id => 2
                     })
   end
 
@@ -23,4 +25,5 @@ class ItemTest < MiniTest::Test
   def test_it_has_a_description
     assert_equal "You can use it to write things", @item.description
   end
+
 end
