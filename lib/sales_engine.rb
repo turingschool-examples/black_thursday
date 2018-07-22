99a890d753a1631a0643b157c5b554075450d58b
andrewetobin <andrewetobin@gmail.com>
2018-07-22 16:03:52 -0600

    adds tests to sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,24 +1,30 @@
-require_relative "merchant_repository"
-require_relative "item_repository"
-require_relative "file_loader"
-require_relative "repo_helper"
-require_relative "sales_analyst"
+require_relative 'file_loader'
+require_relative 'merchant_repository'
+require_relative 'item_repository'
+require_relative 'sales_analyst'
 
 class SalesEngine
-  def initialize(hash)
-    @hash = hash
+  include FileLoader
+  attr_reader :info
+
+  def initialize(info)
+    @info = info
   end
 
-  def self.from_csv(hash)
-    new(hash)
+  def self.from_csv(info)
+    new(info)
   end
 
   def items
-    @items ||= ItemRepository.new(load_file(hash[:items]))
+    @items ||= ItemRepository.new(load_file(info[:items]))
   end
 
   def merchants
-    @merchants ||= MerchantRepository.new(load_file(hash[:merchants]))
+    @merchants ||= MerchantRepository.new(load_file(info[:merchants]))
+  end
+
+  def analyst
+    @analyst = SalesAnalyst.new(self)
   end
 
 
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,42 @@
+require './test/test_helper.rb'
+require './lib/sales_engine.rb'
+
+class SalesEngineTest < Minitest::Test
+  def test_it_exists
+    sales_engine = SalesEngine.from_csv('content')
+    assert_instance_of SalesEngine, sales_engine
+  end
+
+  def test_it_has_from_csv
+    sales_engine = SalesEngine.from_csv(
+      items: './data/items.csv',
+      merchants: './data/merchants.csv'
+    )
+    assert_equal ({
+      items: './data/items.csv',
+      merchants: './data/merchants.csv'
+    }), sales_engine.info
+  end
+
+  def test_merchants_creates_array_of_merchants
+    sales_engine = SalesEngine.from_csv(
+      items: './data/items.csv',
+      merchants: './data/merchants.csv'
+    )
+    assert_equal 475, sales_engine.merchants.repo.count
+  end
+
+  def test_items_creates_repository_of_items
+    sales_engine = SalesEngine.from_csv(
+      items: './data/items.csv',
+      merchants: './data/merchants.csv'
+    )
+    assert_instance_of ItemRepository, sales_engine.items
+  end
+
+
+
+
+
+
+end
