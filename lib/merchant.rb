35e0d0203a8f18577f14d57126441b20c0416c24
andrewetobin <andrewetobin@gmail.com>
2018-07-21 19:36:31 -0600

    adds created_at and updated_at per spec harness review

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,10 +1,12 @@
 
 class Merchant
-  attr_reader :id
-  attr_accessor :name
+  attr_reader :id, :created_at
+  attr_accessor :name, :updated_at
 
   def initialize(hash)
     @id = hash[:id].to_i
     @name = hash[:name]
+    @created_at = Time.parse(hash[:created_at].to_s)
+    @updated_at = Time.parse(hash[:updated_at].to_s)
   end
 end
