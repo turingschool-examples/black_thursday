dda2930a15ec64fbb07a72911feccfaa124d59e2
Tim Fielder <fiel1139@gmail.com>
2018-07-22 09:04:28 -0600

    adds some notes to the sales_engine class.

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -10,4 +10,14 @@ class SalesEngine
     @merchants = CSV.open(hash[:merchants], headers: true, header_converters: :symbol)
   end
 
+  def items
+    #get the CSV and create item instances
+    #serve up those item instances
+  end
+
+  def merchants
+    #get the CSV and create the merchant instances
+    #serve up those merchant instances
+  end
+
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,21 +1,21 @@
 require_relative "../test/test_helper"
 require_relative "../lib/sales_engine"
-
+require "pry"
 
 class SalesEngineTest < Minitest::Test
 
   def test_it_exists
     sales_engine = SalesEngine.new
-    
+
     assert_instance_of SalesEngine, sales_engine
-  end  
-  
+  end
+
   def test_from_csv
     sales_engine = SalesEngine.from_csv({
                               :items     => "./data/items.csv",
                               :merchants => "./data/merchants.csv",})
-  
+    binding.pry
     assert_instance_of CSV, sales_engine
   end
-  
+
 end
