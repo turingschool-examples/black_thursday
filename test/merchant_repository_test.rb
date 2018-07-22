f4b9aa44dcc2dd427632bce02f9b6f77950999e7
Harper Bellows <harper.bellows@gmail.com>
2018-07-22 08:14:24 -0600

    add tests for search methods id, name, all by name fragment

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -8,7 +8,7 @@ class MerchantRepositoryTest < Minitest::Test
   include FileLoader
 
   def setup
-    @mr = MerchantRepository.new(load_file('./data/merchants_test_data.csv'))
+    @mr = MerchantRepository.new(load_file('./data/merchants.csv'))
   end
 
   def test_it_exists
@@ -17,23 +17,47 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_it_can_return_all_merchants
     # skip
-    assert_equal 5, @mr.all.count
-    assert_kind_of Merchant, @mr.all[0]
+    assert_equal 475, @mr.all.count
+    assert_instance_of Merchant, @mr.all[0]
+    assert_instance_of Merchant, @mr.all[15]
   end
 
   def test_it_can_find_merchant_by_id
     # skip
-    assert_kind_of Merchant, @mr.find_by_id(12334105)
+
+    assert_instance_of Merchant , @mr.find_by_id(12334112)
   end
 
   def test_it_can_find_merchant_by_name
     # skip
-    assert_instance_of Merchant, @mr.find_by_name("CadeauJudaica")
+    search_1 = @mr.find_by_name('JEJUM')
+    assert_equal true, search_1.name.include?('jejum')
+  end
+
+  def test_it_can_find_a_different_merchant_by_name
+    skip
+    search = @mr.find_by_name('Shopin1901')
+    assert_equal true, search.name.include?('shopin1901')
   end
 
   def test_it_can_find_all_merchants_with_matching_name_fragment
-    
+    # skip
+    search_1 = @mr.find_all_by_name("M")
+    assert_equal 164, search_1.count
+    #201 instances of the letter "M", but only 164 objects
 
+    search_2 = @mr.find_all_by_name("mi")
+    assert_equal 28, search_2.count
+
+    search_3 = @mr.find_all_by_name("MINI")
+    assert_equal 1, search_3.count
   end
 
+  def test_it_can_create_a_new_merchant
+    
+  end
+
+  def test_newly_created_merchant_has_highest_id_number
+
+  end
 end
