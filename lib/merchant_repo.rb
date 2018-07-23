a8af06d4a6c9f89fd7e8a2a352ba3c027bf996c8
Tim Fielder <fiel1139@gmail.com>
2018-07-23 15:11:02 -0600

    changes Merchant attr_reader for :name to accessor.  Adds test for merchant it creates attributes and method create to merchantrepo class.

--- lib/merchant_repo.rb
+++ lib/merchant_repo.rb
@@ -13,41 +13,33 @@ class MerchantRepo
     end
     @merchants
   end
-    
+
   def add_merchant(merchant)
     @merchants << Merchant.new(merchant)
   end
-<<<<<<< HEAD
+
+  def all
+    @merchants
+  end
 
   def find_by_id(id)
     @merchants.find do |merchant|
       merchant.id.to_i == id
     end
   end
-=======
-  
-  def all 
-    @merchants
-  end 
-  
-  def find_by_id(id)
-    @merchants.find do |merchant| 
-      merchant.id.to_i == id
-    end 
-  end
-  
+
   def find_by_name(name)
     @merchants.find do |merchant|
-      merchant.name == name 
+      merchant.name == name
     end
   end
-  
+
   def find_all_by_name(name)
     @merchants.find_all do |merchant|
       merchant.name == name
     end
   end
-  
+
   def create(attributes)
     merchant_new = Merchant.new(attributes)
     max_merchant = @merchants.max_by do |merchant|
@@ -57,21 +49,20 @@ class MerchantRepo
     merchant_new.id = max_num
     @merchants << merchant_new
   end
-  
-  # def sort_them 
+
+  # def sort_them
   #   sorted_merchants = @merchants.sort_by do |merchant|
-  #     merchant.id 
+  #     merchant.id
   #   end
   #   require "pry"; binding.pry
   # end
-  # 
-  # def update(id, attributes)
-  # 
-  # end
-  # 
+  #
+  def update(id, attributes)
+    merchant_to_change = find_by_id(id)
+    merchant_to_change.name = attributes  #how are attributes passed? In what form
+  end
+  #
   # def delete(id)
-  # 
+  #
   # end
->>>>>>> 95f261eca16c70a64e038458ee9f730f143c583c
 end
-
