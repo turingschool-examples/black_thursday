a77e5c62b3f2eba7431ce7c1f8963d45a9049969
Tim Fielder <fiel1139@gmail.com>
2018-07-22 20:18:45 -0600

    all of it.

--- test/merchant_repo_test.rb
+++ test/merchant_repo_test.rb
@@ -1,6 +1,7 @@
 require_relative "../test/test_helper"
 require_relative "../lib/merchant"
 require_relative "../lib/merchant_repo"
+require "pry"
 
 class MerchantRepoTest < Minitest::Test
 
@@ -12,5 +13,12 @@ class MerchantRepoTest < Minitest::Test
   def test_it_loads_file
     mer_repo = MerchantRepo.new
     assert_equal mer_repo.merchants, mer_repo.load_file("./data/merchants.csv")
+  end
+
+  def test_it_finds_by_id
+    mer_repo = MerchantRepo.new
+    mer_repo.load_file("./data/merchants.csv")
+    #binding.pry
+    assert_instance_of Merchant, mer_repo.find_by_id(12334112)
   end
 end
