2c779eb0048675ab9882055dbe00f8a14a910cdd
Harper Bellows <harper.bellows@gmail.com>
2018-07-22 19:37:20 -0600

    adjust file open method from open to foreach

--- lib/file_loader.rb
+++ lib/file_loader.rb
@@ -2,6 +2,6 @@ require 'csv'
 
 module FileLoader
   def load_file(file_name)
-    CSV.open file_name, headers: true, header_converters: :symbol
+    CSV.foreach file_name, headers: true, header_converters: :symbol
   end
 end
