9bd747ad0aa4a9e5bb9e019d41c2050f514688f9
Tara Craig <tara.craig7@gmail.com>
2018-07-24 11:31:14 -0600

    Add test to see if sales_analyst.sales_engine returns an instance of SalesEngine.

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,5 +1,6 @@
 require_relative './test_helper'
 require './lib/sales_analyst'
+require './sales_engine'
 
 class SalesAnalystTest < Minitest::Test
 
@@ -7,5 +8,11 @@ class SalesAnalystTest < Minitest::Test
     sa = SalesAnalyst.new
 
     assert_instance_of SalesAnalyst, sa
+  end
+
+  def test_it_can_create_instance_of_sales_engine
+    sa = SalesAnalyst.new
+
+    assert_equal SalesEngine, sa.sales_engine
   end
 end
