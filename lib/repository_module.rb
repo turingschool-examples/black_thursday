3181a8d4958c38d9d54e2fa7bc10124bfcaac1f1
Harper Bellows <harper.bellows@gmail.com>
2018-07-24 08:04:41 -0600

    update module methods and add find_by_name method

--- lib/repository_module.rb
+++ lib/repository_module.rb
@@ -1,19 +1,25 @@
 module RepositoryModule
 
   def all
-    @repository.find_all do |element|
-      element
+    @repository.find_all do |repo_object|
+      repo_object
+    end
+  end
+
+  def find_by_name(name)
+    @repository.find do |repo_object|
+      repo_object.name.downcase == name.downcase
+    end
   end
 
   def find_by_id(id)
-    @repository.find do |element|
-      element.id == id
+    @repository.find do |repo_object|
+      repo_object.id == id
     end
   end
 
   def delete(id)
-    @repository.delete_if {|element| element.id == id}
-    end
+    found_id = find_by_id(id)
+    @repository.delete(found_id)
   end
-
 end
