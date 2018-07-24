0cb70872d5b373c6203ac6523783292d710bbbbe
Tara Craig <tara.craig7@gmail.com>
2018-07-24 15:53:01 -0600

    Add updated at attribute to attr accessor

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -2,12 +2,13 @@ class Merchant
 
   attr_reader :id
 
-  attr_accessor :name
+  attr_accessor :name,
+                :updated_at
 
   def initialize(info)
     @id = info[:id].to_i
     @name = info[:name]
-    @created_at = info[:created_at]
-    @updated_at = info[:updated_at]
+    @created_at = Time.parse(info[:created_at].to_s)
+    @updated_at = Time.parse(info[:updated_at].to_s)
   end
 end
