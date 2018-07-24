425e6e7cd89d47f97d973b773bf717d94a331cb9
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-24 16:09:20 -0600

    adds id method to class file, with test

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -3,10 +3,15 @@ require 'bigdecimal/util'
 require 'time'
 
 class Invoice
+  attr_reader :id
 
-  def initialize(invoice_information)
-
+  def initialize(information)
+    @id = information[:id].to_i
+    @customer_id = information[:customer_id].to_i
+    @merchant_id = information[:merchant_id].to_i
+    @status = information[:status]
+    @created_at = Time.parse(information[:created_at].to_s)
+    @updated_at = Time.parse(information[:updated_at].to_s)
   end
-
 
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -3,8 +3,8 @@ require_relative '../lib/invoice.rb'
 
 class InvoiceTest < Minitest::Test
 
-  def test_it_exists
-    invoice = Invoice.new({
+  def setup
+    @invoice = Invoice.new({
       :id          => 6,
       :customer_id => 7,
       :merchant_id => 8,
@@ -12,9 +12,14 @@ class InvoiceTest < Minitest::Test
       :created_at  => Time.now,
       :updated_at  => Time.now,
     })
-
-    assert_instance_of Invoice, invoice
   end
 
+  def test_it_exists
+    assert_instance_of Invoice, @invoice
+  end
+
+  def test_it_has_an_id
+    assert_equal 6, @invoice.id
+  end
 
 end
