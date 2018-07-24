a0501c7b8a529d705a48b3ae4438d96dd6681194
Patrick Shobe <patrick@patrickshobe.com>
2018-07-24 08:52:39 -0600

    Refactored

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,6 +1,8 @@
-require 'bigdecimal'
-require 'pry'
+# frozen_string_literals: true
 
+require 'bigdecimal'
+
+# ./lib/item_repository
 class ItemRepository
   attr_reader :items
 
@@ -16,69 +18,53 @@ class ItemRepository
     @items
   end
 
-  def find_by_id(id)
-    @items.find do |item|
-      item.id == id
-    end
+  def find_by_id(search_id)
+    @items.find { |item| item.id == search_id }
   end
 
-  def find_by_name(name)
-    @items.find do |item|
-      item.name == name
-    end
+  def find_by_name(search_name)
+    @items.find { |item| item.name == search_name }
   end
 
-  def find_all_with_description(description)
+  def find_all_with_description(partial_description)
     @items.find_all do |item|
-      item.description.downcase.include?(description.downcase)
+      item_description = item.description.downcase
+      item_description.include?(partial_description.downcase)
     end
   end
 
-  def find_all_by_price(price)
-     @items.find_all do |item|
-      item.unit_price == price
-    end
+  def find_all_by_price(search_price)
+    @items.find_all { |item| item.unit_price == search_price }
   end
 
-  def find_all_by_price_in_range(range)
+  def find_all_by_price_in_range(search_price_range)
     @items.find_all do |item|
-      range.cover?(item.unit_price_to_dollars)
+      search_price_range.cover?(item.unit_price_to_dollars)
     end
   end
 
-  def find_all_by_merchant_id(id)
-    @items.find_all do |item|
-      item.merchant_id == id
-    end
+  def find_all_by_merchant_id(search_id)
+    @items.find_all { |item| item.merchant_id == search_id }
   end
-
 
   def create_with_id(attributes)
-    item = Item.new(attributes)
-    @items << item
-    item
+    @items << Item.new(attributes)
   end
 
   def create(attributes)
-    item = Item.create(attributes)
-    @items << item
-    item
+    @items << Item.create(attributes)
   end
 
   def update(id, attributes)
     item = find_by_id(id)
-    unless item.nil?
-      item.name = attributes[:name] unless attributes[:name].nil?
-      item.description = attributes[:description] unless attributes[:description].nil?
-      item.unit_price = attributes[:unit_price] unless attributes[:unit_price].nil?
-      item.updated_at = Time.now
-    end
+    return nil if item.nil?
+    item.name = attributes[:name] unless attributes[:name].nil?
+    item.description = attributes[:description] unless attributes[:description].nil?
+    item.unit_price = attributes[:unit_price] unless attributes[:unit_price].nil?
+    item.updated_at = Time.now
   end
 
   def delete(id)
     @items.delete(find_by_id(id))
   end
-
-
-
 end
