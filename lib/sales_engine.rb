48057f91033d58aa3960344120a74444f406ee24
Mike McKee <mikecm@gmail.com>
2018-07-24 15:14:32 -0600

    Removing attr_reader for items

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,8 +5,6 @@ require_relative './merchant_repository'
 require_relative './item_repository'
 
 class SalesEngine
-  attr_reader :items
-
   def self.from_csv(filepaths) # Hash values are file paths
     new(filepaths)
   end
