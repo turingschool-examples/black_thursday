5b31e0a5efb6e773ac217f5b77b0b284a29270af
Tara Craig <tara.craig7@gmail.com>
2018-07-24 16:26:40 -0600

    Change tests to include created at and updated at in the merchant tests data.

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,13 +4,27 @@ require './lib/merchant'
 class MerchantTest < Minitest::Test
 
   def test_it_exists
-    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+    merchant = Merchant.new(
+      {
+        :id => 5,
+        :name => "Turing School",
+        :created_at => "2010-12-10",
+        :updated_at => "2011-12-04"
+        }
+      )
 
     assert_instance_of Merchant, merchant
   end
 
   def test_it_has_attributes
-    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+    merchant = Merchant.new(
+      {
+        :id => 5,
+        :name => "Turing School",
+        :created_at => "2010-12-10",
+        :updated_at => "2011-12-04"
+        }
+      )
 
     assert_equal 5, merchant.id
     assert_equal "Turing School", merchant.name
