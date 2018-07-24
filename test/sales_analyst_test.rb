55cae65da519d699a65e7d55739517ab7a1280dc
Tara Craig <tara.craig7@gmail.com>
2018-07-24 11:51:08 -0600

    Get tests in sales analyst test file to pass.

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,18 +1,23 @@
 require_relative './test_helper'
 require './lib/sales_analyst'
-require './sales_engine'
+require './lib/sales_engine'
 
 class SalesAnalystTest < Minitest::Test
 
-  def test_it_exists
-    sa = SalesAnalyst.new
+  def setup
+    @se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
 
-    assert_instance_of SalesAnalyst, sa
+    @sa = SalesAnalyst.new(@se)
+  end
+
+  def test_it_exists
+    assert_instance_of SalesAnalyst, @sa
   end
 
   def test_it_can_create_instance_of_sales_engine
-    sa = SalesAnalyst.new
-
-    assert_equal SalesEngine, sa.sales_engine
+    assert_instance_of SalesEngine, @se
   end
 end
