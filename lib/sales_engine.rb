a1c998587bf135110287559fb6c5fd9b8d71b7e2
Mike McKee <mikecm@gmail.com>
2018-07-24 11:31:01 -0600

    Starting sales engine class and tests

---
+++ lib/sales_engine.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class SalesEngine
+  attr_reader :items,
+              :merchants
+  def self.from_csv(params)
+
+  end
+
+  def initialize
+    @items     = items
+    @merchants = merchants
+  end
+end
---
+++ test/sales_engine_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/emoji'
+require './lib/sales_engine'
+
+# Sales engine test
+class SalesEngineTest < Minitest::Test
+  def setup
+    @se = SalesEngine.new
+  end
+
+  def test_it_exists
+    assert_instance_of SalesEngine, @se
+  end
+
+  def test_it_has_attributes
+    assert_nil @se.items
+    assert_nil @se.merchants
+  end
+end
