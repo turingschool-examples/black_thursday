dfac99739f82076110806f2472d56b084c1e4ed0
Tara Craig <tara.craig7@gmail.com>
2018-07-24 19:54:27 -0600

    Add method to group items by merchant, find the number of merchants, find the number of items, and average the number of items per merchant.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,7 +1,29 @@
+require_relative './sales_engine'
+
 class SalesAnalyst
 
   def initialize(sales_engine)
     @sales_engine = sales_engine
+  end
+
+  def average_items_per_merchant
+    (find_number_of_items.to_f / find_number_of_merchants).round(2)
+  end
+
+  def group_items_by_merchant
+    @sales_engine.items.all.group_by(&:merchant_id)
+  end
+
+  def find_number_of_merchants
+    group_items_by_merchant.inject(0) do |count, (id, items)|
+      count + 1
+    end
+  end
+
+  def find_number_of_items
+    group_items_by_merchant.inject(0) do |count, (id, items)|
+      count + items.count
+    end
   end
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -21,8 +21,20 @@ class SalesAnalystTest < Minitest::Test
     assert_instance_of SalesEngine, @se
   end
 
-  # def test_it_can_find_average_items_per_merchant
-  #   assert_equal , sa.average_items_per_merchant
-  #
-  # end
+  def test_it_can_group_items_by_merchant_id
+    assert_equal 1, @sa.group_items_by_merchant[12334141].count
+    assert_equal 6, @sa.group_items_by_merchant[12334185].count
+  end
+
+  def test_it_can_find_number_of_merchants
+    assert_equal 475, @sa.find_number_of_merchants
+  end
+
+  def test_it_can_find_number_items
+    assert_equal 1367, @sa.find_number_of_items
+  end
+
+  def test_it_can_find_average_items_per_merchant
+    assert_equal 2.88, @sa.average_items_per_merchant
+  end
 end
