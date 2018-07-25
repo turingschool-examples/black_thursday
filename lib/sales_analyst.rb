b78604063ce21c210514fb4c8a54eb404ab628f3
Tara Craig <tara.craig7@gmail.com>
2018-07-25 10:46:13 -0600

    Add methods to find the items_per_merchant array, the variance of the array, the square root of the variance, and find the average items per merchant standard deviation, and corresponding tests.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -7,23 +7,50 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-    (find_number_of_items.to_f / find_number_of_merchants).round(2)
+    grouped_items = group_items_by_merchant
+    merchants_total = find_number_of_merchants(grouped_items)
+    items_total = find_total_number_of_items(grouped_items)
+    (items_total.to_f / merchants_total).round(2)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    average = average_items_per_merchant
+    grouped_items = group_items_by_merchant
+    ipm = items_per_merchant(grouped_items)
+    v = variance(average, ipm)
+    square_root_of_variance(v, ipm)
   end
 
   def group_items_by_merchant
     @sales_engine.items.all.group_by(&:merchant_id)
   end
 
-  def find_number_of_merchants
-    group_items_by_merchant.inject(0) do |count, (id, items)|
+  def find_number_of_merchants(grouped_items)
+    grouped_items.inject(0) do |count, (id, items)|
       count + 1
     end
   end
 
-  def find_number_of_items
-    group_items_by_merchant.inject(0) do |count, (id, items)|
+  def find_total_number_of_items(grouped_items)
+    grouped_items.inject(0) do |count, (id, items)|
       count + items.count
     end
+  end
+
+  def items_per_merchant(grouped_items)
+    grouped_items.map do |id, items|
+      items.count
+    end
+  end
+
+  def variance(average, ipm)
+    variance = ipm.inject(0) do |count, items|
+      count += (items - average) ** 2
+    end
+  end
+
+  def square_root_of_variance(v, ipm)
+    (Math.sqrt(v/(ipm.size-1))).round(2)
   end
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -27,14 +27,49 @@ class SalesAnalystTest < Minitest::Test
   end
 
   def test_it_can_find_number_of_merchants
-    assert_equal 475, @sa.find_number_of_merchants
+    grouped_items = {
+                    12334141 => ["item_1", "item_2"],
+                    12334185 => ["item_1", "item_2", "item_3"]
+    }
+    assert_equal 2, @sa.find_number_of_merchants(grouped_items)
   end
 
   def test_it_can_find_number_items
-    assert_equal 1367, @sa.find_number_of_items
+    grouped_items = {
+                    12334141 => ["item_1", "item_2"],
+                    12334185 => ["item_1", "item_2", "item_3"]
+    }
+    assert_equal 5, @sa.find_total_number_of_items(grouped_items)
   end
 
   def test_it_can_find_average_items_per_merchant
     assert_equal 2.88, @sa.average_items_per_merchant
+    assert_equal Float, @sa.average_items_per_merchant.class
+  end
+
+  def test_it_can_find_average_items_per_merchant_standard_deviation
+    assert_equal 3.26, @sa.average_items_per_merchant_standard_deviation
+    assert_equal Float, @sa.average_items_per_merchant_standard_deviation.class
+  end
+
+  def test_it_can_find_number_of_items_for_each_merchant
+    grouped_items = {
+                    12334141 => ["item_1", "item_2"],
+                    12334185 => ["item_1", "item_2", "item_3"],
+                    12345678 => ["item_1", "item_2", "item_3", "item_4"]
+                  }
+    assert_equal [2, 3, 4], @sa.items_per_merchant(grouped_items)
+  end
+
+  def test_it_can_find_variance
+    item_count_array = [2, 3, 4, 3, 5]
+    average = 3.4
+    assert_equal 5.2, @sa.variance(average, item_count_array)
+  end
+
+  def test_it_can_find_square_root_of_variance
+    v = 5.2
+    ipm = [2, 3, 4, 3, 5]
+    assert_equal 1.14, @sa.square_root_of_variance(v, ipm)
   end
 end
