dfd8176966f8fceb07801814382184e76b86885c
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-25 17:47:22 -0600

    add require for invoice repository

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,6 +1,7 @@
 require_relative 'file_loader'
 require_relative 'merchant_repository'
 require_relative 'item_repository'
+require_relative 'invoice_repository'
 require_relative 'sales_analyst'
 
 class SalesEngine
