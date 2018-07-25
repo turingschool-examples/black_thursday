3d13e2d772771a39ccd7c4f24556a7386b55f4ed
Harper Bellows <harper.bellows@gmail.com>
2018-07-25 11:35:13 -0600

    add initial tests for average item per merchant functionality

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -13,5 +13,15 @@ class SalesAnalystTest < Minitest::Test
     @sa = SalesAnalyst.new(@se)
   end
 
+  def test_it_can_find_total_number_of_merchants
+    assert_equal 475, @se.total_merchants
+  end
 
+  def test_it_can_find_total_number_of_items
+    assert_equal 1603, @se.total_items
+  end
+
+  def test_it_can_find_the_average_number_of_items_per_merchant
+    assert_equal 2.88, @sa.average_items_per_merchant
+  end
 end
