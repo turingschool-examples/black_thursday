27f5b856b4401d4607b30fb303f7f5aacceb835b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-07-25 16:01:59 -0600

    Adds tests for merchant repository and attr accessor to merchant class.

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,73 +1,74 @@
 require 'csv'
 require 'pry'
 require './lib/merchant.rb'
-class MerchantRepository 
-  
+class MerchantRepository
+
   attr_reader     :filepath,
-                  :all 
+                  :all
   def initialize(filepath)
     @filepath = filepath
     @all = []
-  end 
-  
+  end
+
   def create_all_from_csv(filepath)
     CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
       @all << Merchant.new(row)
-    end 
+    end
   end
 
   def find_by_id(id)
     @all.find do |object|
-      object.id == id 
-    end 
+      object.id == id
+    end
   end
-  
+
   def find_by_name(name)
     @all.find do |object|
-      object.name.downcase == name.downcase 
-    end 
-  end 
-  
+      object.name.downcase == name.downcase
+    end
+  end
+
   def find_all_by_name(name)
-    @all.find_all do |object| 
+    @all.find_all do |object|
       object.name.downcase.include?(name.downcase)
-    end 
-  end 
-  
+    end
+  end
+
   def find_highest_id
     @all.max_by do |object|
-      object.id 
-    end 
-  end 
-  
+      object.id
+    end
+  end
+
   def create(attributes)
     attributes[:id] = find_highest_id.id.to_i + 1
     mr = Merchant.new(attributes)
     @all << mr
-    return mr 
-  end 
-  
+    return mr
+  end
+
   def update(id, attributes)
     merchant = find_by_id(id.to_s)
     merchant.attributes_hash[:name] = attributes[:name]
-  end 
-  
+    #binding.pry
+    merchant.name = attributes[:name]
+  end
+
   def delete(id)
     @all = @all.reject do |object|
-      object.id == id 
+      object.id == id
     end
-  end  
-end 
+  end
+end
 
-mr = MerchantRepository.new("./data/dummy_merchants.csv")
-mr.create_all_from_csv("./data/dummy_merchants.csv")
+#mr = MerchantRepository.new("./data/dummy_merchants.csv")
+#mr.create_all_from_csv("./data/dummy_merchants.csv")
 # p mr.find_by_id("12334112")
 # p mr.find_all_by_name("Candi")
 # p mr.find_highest_id
-puts mr.all.count
+#puts mr.all.count
 # p mr.all[0].id
 # p mr.create({:name => "Donald's"})
-# puts mr.all.count 
-mr.update(12334105, {:id => 512334105, :name => "Jerry's"}) 
-p mr.all[0]
-
+# puts mr.all.count
+#mr.update(12334105, {:id => 12334105, :name => "Jerry's"})
+#p mr.all[0]
