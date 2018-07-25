077b1279742331e4a243461cc56fed233f1122dc
Harper Bellows <harper.bellows@gmail.com>
2018-07-25 07:30:20 -0600

    remove update functiionality and add to repo module

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -43,15 +43,6 @@ class ItemRepository
      @repository << Item.new(attributes)
   end
 
-  def update(id, attributes)
-    item = find_by_id(id)
-    return if item.nil?
-    item.name = attributes[:name] unless attributes[:name].nil?
-    item.description = attributes[:description] unless attributes[:description].nil?
-    item.unit_price = attributes[:unit_price] unless attributes[:unit_price].nil?
-    item.updated_at = Time.now
-  end
-
   def inspect
   "#<#{self.class} #{@items.size} rows>"
   end
