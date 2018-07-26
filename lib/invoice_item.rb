a906d79eeec210b1b05d2bb83af6a48043fe6598
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-25 20:46:26 -0600

    Adds test and method for id

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -4,9 +4,26 @@ require 'time'
 require_relative 'repository'
 
 class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
 
   def initialize(information)
-
-  end 
+    @id = information[:id].to_i
+    @item_id = information[:item_id].to_i
+    @invoice_id = information[:invoice_id].to_i
+    @quantity = information[:quantity].to_i
+    @unit_price = if information[:unit_price].class != BigDecimal
+        BigDecimal.new((information[:unit_price].to_f / 100), 5)
+      else
+        information[:unit_price]
+      end
+    @created_at = Time.parse(information[:created_at].to_s)
+    @updated_at = Time.parse(information[:updated_at].to_s)
+  end
 
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -3,8 +3,8 @@ require_relative '../lib/invoice_item'
 
 class InvoiceItemTest < Minitest::Test
 
-  def test_it_exists
-    invoice_item = InvoiceItem.new({
+  def setup
+    @invoice_item = InvoiceItem.new({
                 :id => 6,
                 :item_id => 7,
                 :invoice_id => 8,
@@ -13,8 +13,15 @@ class InvoiceItemTest < Minitest::Test
                 :created_at => Time.now,
                 :updated_at => Time.now
               })
-
-    assert_instance_of InvoiceItem, invoice_item
   end
+
+  def test_it_exists
+    assert_instance_of InvoiceItem, @invoice_item
+  end
+
+  def test_it_has_id
+    assert_equal 6, @invoice_item.id 
+  end
+
 
 end
