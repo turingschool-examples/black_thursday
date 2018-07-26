017d0ccd0e9b5ca4fbe7ed5bdd1322a7c5ed9103
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-26 17:16:23 -0600

    Adds test for find by invoice id

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -7,7 +7,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
   def setup
     @invoice_item_1 = InvoiceItem.new({:id => 6, :item_id => 7, :invoice_id => 88, :quantity => 1, :unit_price => BigDecimal.new(100.99, 4), :created_at => Time.now, :updated_at => Time.now})
     @invoice_item_2 = InvoiceItem.new({:id => 7, :item_id => 33, :invoice_id => 99, :quantity => 1, :unit_price => BigDecimal.new(5.99, 4), :created_at => Time.now, :updated_at => Time.now})
-    @invoice_item_3 = InvoiceItem.new({:id => 8, :item_id => 7, :invoice_id => 77, :quantity => 1, :unit_price => BigDecimal.new(12.36, 4), :created_at => Time.now, :updated_at => Time.now})
+    @invoice_item_3 = InvoiceItem.new({:id => 8, :item_id => 7, :invoice_id => 99, :quantity => 1, :unit_price => BigDecimal.new(12.36, 4), :created_at => Time.now, :updated_at => Time.now})
     @invoice_item_4 = InvoiceItem.new({:id => 9, :item_id => 987, :invoice_id => 66, :quantity => 1, :unit_price => BigDecimal.new(9.79, 4), :created_at => Time.now, :updated_at => Time.now})
     @invoice_items = [@invoice_item_1, @invoice_item_2, @invoice_item_3, @invoice_item_4]
     @invoice_item_repository = InvoiceItemRepository.new(@invoice_items)
@@ -30,5 +30,12 @@ class InvoiceItemRepositoryTest < Minitest::Test
     expected = [@invoice_item_1, @invoice_item_3]
     assert_equal expected, @invoice_item_repository.find_all_by_item_id(7)
     assert_equal [], @invoice_item_repository.find_all_by_item_id(9432)
+  end
+
+  def test_it_finds_all_by_invoice_id
+    expected = [@invoice_item_2, @invoice_item_3]
+    actual = @invoice_item_repository.find_all_by_invoice_id(99)
+    assert_equal expected, actual
+    assert_equal [], @invoice_item_repository.find_all_by_invoice_id(1001)
   end
 end
