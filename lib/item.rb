bf20485bf0f5535840018f1e6066c8900c3e3aa2
Tim Fielder <fiel1139@gmail.com>
2018-07-26 11:24:29 -0600

    makes sure items are data converted when created.

--- lib/item.rb
+++ lib/item.rb
@@ -9,13 +9,13 @@ class Item
                 :merchant_id
 
   def initialize(hash)
-    @id = hash[:id]
-    @name = hash[:name]
-    @description = hash[:description]
+    @id = hash[:id].to_i
+    @name = hash[:name].to_s
+    @description = hash[:description].to_s
     @unit_price = hash[:unit_price]
     @created_at = hash[:created_at]
     @updated_at = hash[:updated_at]
-    @merchant_id = hash[:merchant_id]
+    @merchant_id = hash[:merchant_id].to_i
   end
 
 end
