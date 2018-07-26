27d274ba384b48f10923e61ef63648848c6b3b7f
abdyqew <aqudrat630@gmail,com>
2018-07-26 11:08:04 -0600

    creates invoice item class and test

---
+++ lib/invoice_item.rb
@@ -0,0 +1,26 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
+require 'time'
+
+class InvoiceItem
+  attr_reader   :id,
+                :item_id,
+                :invoice_id,
+                :unit_price,
+                :quantity,
+                :created_at
+
+
+  attr_accessor :updated_at
+
+  def initialize(invoice_item_hash)
+    @id = invoice_item_hash[:id].to_i
+    @item_id = invoice_item_hash[:item_id].to_i
+    @invoice_id = invoice_item_hash[:invoice_id].to_i
+    @quantity = invoice_item_hash[:quantity]
+    @unit_price = BigDecimal(item_hash[:unit_price]) / 100
+    @created_at = Time.parse(invoice_item_hash[:created_at])
+    @updated_at = Time.parse(invoice_item_hash[:updated_at])
+  end
+
+end
