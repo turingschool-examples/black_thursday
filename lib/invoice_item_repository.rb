acd1749ed37ca44d805b3bec39da6628a67b2edf
Harper Bellows <harper.bellows@gmail.com>
2018-07-26 08:09:33 -0600

    add final functionality of invoice_item_repo class

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,10 +1,31 @@
+require_relative 'invoice_item'
 require_relative 'repository_assistant'
 
-class InvoiceItemsRepository
+class InvoiceItemRepository
   include RepositoryAssistant
 
-end
+  def initialize(data_file)
+    @repository = data_file.map {|item| InvoiceItem.new(item)}
+  end
 
-def inspect
-  "#<#{self.class} #{@invoice_items.size} rows>"
+  def find_all_by_item_id(id)
+    @repository.find_all do |invoice_item|
+      invoice_item.item_id == id
+    end
+  end
+
+  def find_all_by_invoice_id(id)
+    @repository.find_all do |invoice_item|
+      invoice_item.invoice_id == id
+    end
+  end
+
+  def create(attributes)
+    attributes[:id] = create_new_id_number
+    @repository << InvoiceItem.new(attributes)
+  end
+
+  def inspect
+    "#<#{self.class} #{@invoice_items.size} rows>"
+  end
 end
