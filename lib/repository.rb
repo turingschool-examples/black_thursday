5eb4f074a3c5862cd5ed12dba86ce69b4ad2b657
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-26 17:16:33 -0600

    Adds method to find by invoice id

--- lib/repository.rb
+++ lib/repository.rb
@@ -28,6 +28,12 @@ module Repository
     end
   end
 
+  def find_all_by_invoice_id(invoice_id)
+    @list.find_all do |list_item|
+      list_item.invoice_id == invoice_id
+    end
+  end
+
   def find_highest_id
     highest_list_id = @list.max_by do |list_item|
       list_item.id
