4f9b759a186c2fdd36f13a785e3b316d523e6d65
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-26 17:23:50 -0600

    Adds create new method

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -8,4 +8,10 @@ class InvoiceItemRepository
   def initialize(invoice_items)
     @list = invoice_items
   end
+
+  def create(attributes)
+    highest_invoice_item_id = find_highest_id
+    attributes[:id] = highest_invoice_item_id.id + 1
+    @list << InvoiceItem.new(attributes)
+  end
 end
