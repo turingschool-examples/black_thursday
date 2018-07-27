8f2982c06891f395f689574e184efecf83c5676f
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-07-26 21:43:35 -0600

    WIP - instantiate SalesAnalyst with @sales_engine, @item_repository, and @merchant_repository attributes; tested same

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,4 +1,14 @@
 require_relative './sales_engine'
 
 class SalesAnalyst 
+  attr_reader       :sales_engine, :item_repository, :merchant_repository
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+    @item_repository = @sales_engine.items
+    @merchant_repository = @sales_engine.merchants
+  end
+  
+  def average_items_per_merchant 
+    
+  end 
 end 
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,23 +1,57 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
-require_relative './sales_engine'
+require_relative '../lib/sales_engine'
+require_relative '../lib/sales_analyst'
 
 class SalesAnalystTest < Minitest::Test 
-  def test_it_exists
-    sales_analyst = SalesAnalyst.new  
-    
-    assert_instance_of SalesAnalyst, sales_analyst
-  end 
-  
-  def test_it_exists
-    sales_engine = SalesEngine.new({
+
+  def setup 
+    @sales_engine = SalesEngine.new({
     :items     => "./data/items.csv",
     :merchants => "./data/merchants.csv",
      })
-    sales_analyst = sales_engine.analyst 
-    
-    assert_instance_of SalesAnalyst, sales_analyst
+    @sales_analyst = @sales_engine.analyst
+    # binding.pry 
+  end 
+  
+  def test_it_exists
+    assert_instance_of SalesAnalyst, @sales_analyst
   end
   
+  def test_it_has_attributes
+    assert_equal SalesEngine, @sales_analyst.sales_engine.class
+    assert_equal ItemRepository, @sales_analyst.item_repository.class
+    assert_equal 1367, @sales_analyst.item_repository.items.count
+    assert_equal MerchantRepository, @sales_analyst.merchant_repository.class
+    assert_equal 475, @sales_analyst.merchant_repository.all.count
+  end  
+    
+  
+  # def test_average_items_per_merchant 
+  #   sales_engine = SalesEngine.new({
+  #   :items     => "./data/items.csv",
+  #   :merchants => "./data/merchants.csv",
+  #    })
+  #   sales_analyst = sales_engine.analyst 
+  # 
+  #   assert_equal 2.88, sales_engine.analyst.average_items_per_merchant 
+  #   assert_equal Float, sales_engine.average_items_per_merchant.class 
+  # end
+  
+  # let(:sales_analyst) { engine.analyst }
+  # 
+  # it "#average_items_per_merchant returns average items per merchant" do
+  #   expected = sales_analyst.average_items_per_merchant
+  # 
+  #   expect(expected).to eq 2.88
+  #   expect(expected.class).to eq Float
+  # end
+  # 
+  # it "#average_items_per_merchant_standard_deviation returns the standard deviation" do
+  #   expected = sales_analyst.average_items_per_merchant_standard_deviation
+  # 
+  #   expect(expected).to eq 3.26
+  #   expect(expected.class).to eq Float
+  # end
 end 
