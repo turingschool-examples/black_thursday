b663063c7788db3bbb50af1cd513a1d769b91cbb
Tara Craig <tara.craig7@gmail.com>
2018-07-27 17:00:15 -0600

    Rename method so it is not the same as the other find number of merchants.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -125,62 +125,60 @@ class SalesAnalyst
 
 #---------------ITERATION-2-STUFF------------------------#
 # sales_analyst.average_invoices_per_merchant # => 10.49
-  # def average_invoices_per_merchant
-  #   grouped_invoices = group_invoices_by_merchant
-  #   merchants_total = find_number_of_merchants(grouped_invoices)
-  #   invoices_total = find_total_number_of_invoices(grouped_invoices)
-  #   (invoices_total.to_f / merchants_total).round(2)
-  # end
+  def average_invoices_per_merchant
+    merchants_total = find_number_of_merchants_for_invoices
+    invoices_total = find_total_number_of_invoices
+    (invoices_total.to_f / merchants_total).round(2)
+  end
 # sales_analyst.average_invoices_per_merchant_standard_deviation # => 3.29
-  # def average_invoices_per_merchant_standard_deviation
-  #   average = average_invoices_per_merchant
-  #   grouped_invoices = group_invoices_by_merchant
-  #   ipm = invoices_per_merchant(grouped_invoices)
-  #   v = variance(average, ipm)
-  #   square_root_of_variance(v, ipm)
-  # end
-  #
-  # def group_invoices_by_merchant
-  #   @sales_engine.invoices.all.group_by(&:merchant_id)
-  # end
-  #
-  # def find_number_of_merchants(grouped_invoices)
-  #   grouped_invoices.inject(0) do |count, (id, invoices)|
-  #     count + 1
-  #   end
-  # end
-  #
-  # def find_total_number_of_invoices(grouped_invoices)
-  #   grouped_invoices.inject(0) do |count, (id, invoices)|
-  #     count + invoices.count
-  #   end
-  # end
-  #
-  # def invoices_per_merchant(grouped_invoices)
-  #   grouped_invoices.map do |id, invoices|
-  #     invoices.count
-  #   end
-  # end
+  def average_invoices_per_merchant_standard_deviation
+    average = average_invoices_per_merchant
+    ipm = invoices_per_merchant
+    v = variance(average, ipm)
+    square_root_of_variance(v, ipm)
+  end
+
+  def group_invoices_by_merchant
+    @sales_engine.invoices.all.group_by(&:merchant_id)
+  end
+
+  def find_number_of_merchants_for_invoices
+    grouped_invoices.inject(0) do |count, (id, invoices)|
+      count + 1
+    end
+  end
+
+  def find_total_number_of_invoices
+    grouped_invoices.inject(0) do |count, (id, invoices)|
+      count + invoices.count
+    end
+  end
+
+  def invoices_per_merchant(grouped_invoices)
+    grouped_invoices.map do |id, invoices|
+      invoices.count
+    end
+  end
 
 # Which merchants are more than two standard deviations above the mean?
 # sales_analyst.top_merchants_by_invoice_count # => [merchant, merchant, merchant]
-  # def two_standard_deviations_above
-  #   average_invoices_per_merchant + average_invoices_per_merchant_standard_deviation*2
-  # end
-  #
-  # def top_merchants_by_invoice_count
-  #   group_invoices_by_merchant.find_all do |id, invoices|
-  #     invoices.count > two_standard_deviations_above
-  #   end
-  # end
+  def invoices_two_standard_deviations_above
+    average_invoices_per_merchant + average_invoices_per_merchant_standard_deviation*2
+  end
+
+  def top_merchants_by_invoice_count
+    group_invoices_by_merchant.find_all do |id, invoices|
+      invoices.count >= invoices_two_standard_deviations_above
+    end
+  end
 # sales_analyst.bottom_merchants_by_invoice_count # => [merchant, merchant, merchant]
-#   def two_standard_deviations_below
-#     average_invoices_per_merchant - average_invoices_per_merchant_standard_deviation*2
-#   end
-#
-#   def bottom_merchants_by_invoice_count
-#     group_invoices_by_merchant.find_all do |id, invoices|
-#       invoices.count < two_standard_deviations_below
-#     end
-#   end
+  def invoices_two_standard_deviations_below
+    average_invoices_per_merchant - average_invoices_per_merchant_standard_deviation*2
+  end
+
+  def bottom_merchants_by_invoice_count
+    group_invoices_by_merchant.find_all do |id, invoices|
+      invoices.count <= invoices_two_standard_deviations_below
+    end
+  end
 end
