23467573d0b684ef329ae6a70adab55e99e9c9d4
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-07-27 10:51:20 -0600

    Creates a repository helper that passes iteration 0, WIP.

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,86 +1,35 @@
 require 'csv'
 require_relative '../lib/item'
+require_relative '../lib/repository_helper'
+require 'bigdecimal'
 
 class ItemRepository
-  attr_reader :items
+  include RepositoryHelper
+  attr_reader :items,
+              :all
+
   def initialize(filepath)
-    @filepath = filepath #"./data/items.csv"
-    @items = [ ]
+    @filepath = filepath
+     @items = [ ]
+    @all= []
   end
 
   def create_items
     CSV.foreach(@filepath, headers: true, header_converters: :symbol) do |row|
-      @items << Item.new(row)
+      @all << Item.new(row)
     end
-  end
-
-  def all
-    @items
-  end
-
-  def find_by_id(id)
-  @items.find do |item|
-    item.id == id
-  end
-end
-
-  def find_by_name(name)
-    @items.find do |item|
-    item.name.downcase == name.downcase
-    end
-  end
-
-  def find_all_with_description(description)
-    @items.find_all do |item|
-      item.description.downcase.include?(description.downcase)
-    end
-  end
-
-  def find_all_by_price(price)
-    kittens = @items.find_all do |item|
-
-    item.unit_price == price
-  end#.flatten
-  end
-
-  def find_all_by_price_in_range(range)
-    @items.find_all do |item|
-      #binding.pry
-    range.include?(item.unit_price.to_f)
-    end
-  end
-
-  def find_all_by_merchant_id(merchant_id)
-    @items.find_all do |item|
-      item.merchant_id == merchant_id
-    end
-  end
-
-  def find_highest_id
-    @items.max_by do |item|
-      item.id
-    end
-  end
-
-  def create_id
-    find_highest_id.id.to_i + 1
   end
 
   def create(attributes)
     attributes[:id] = create_id
     item = Item.new(attributes)
-    @items << item
+    @all << item
   end
 
   def update(id, attributes)
-      if find_by_id(id) != nil
-        find_by_id(id).update_attributes(attributes)
-      end
+    if find_by_id(id) != nil
+      find_by_id(id).update_attributes(attributes)
     end
-
-  def delete(id)
-    item = find_by_id(id)
-    @items.delete(item)
   end
 
   def inspect
