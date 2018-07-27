0b9b226aa7df99210472ce9e57a77e5257030d70
Tim Fielder <fiel1139@gmail.com>
2018-07-27 11:44:17 -0600

    adds test for calculating average items per merchant and implementation.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,3 +1,10 @@
+require_relative 'merchant_repo'
+require_relative 'item_repo'
+require 'time'
+require 'bigdecimal'
+require 'pry'
+
+
 class SalesAnalyst
 
   attr_reader :merchants, :items
@@ -7,5 +14,9 @@ class SalesAnalyst
     @items = item_repo
   end
 
-
+  def average_items_per_merchant
+    total_items = @items.all.count.to_f
+    total_merchants = @merchants.all.count.to_f
+    (total_items / total_merchants)
+  end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -33,4 +33,47 @@ class SalesAnalystTest < Minitest::Test
     assert_instance_of ItemRepo, @sales_analyst.items
   end
 
+  # How many products do merchants sell?
+  # Do most of our merchants offer just a few items or do they represent a warehouse?
+  def test_it_calculates_average_items_per_merchant
+
+    assert_equal 1.2, @sales_analyst.average_items_per_merchant
+  end
+  # sales_analyst.average_items_per_merchant # => 2.88
+
+  # And what’s the standard deviation?
+  #
+  # sales_analyst.average_items_per_merchant_standard_deviation # => 3.26
+  # Note on Standard Deviations
+  # There are two ways for calculating standard deviations – for a population and for a sample.
+  #
+  # For this project, use the sample standard deviation.
+  #
+  # As an example, given the set 3,4,5. We would calculate the deviation using the following steps:
+  #
+  # Take the difference between each number and the mean and square it
+  # Sum these square differences together
+  # Divide the sum by the number of elements minus 1
+  # Take the square root of this result
+  # Or, in pseudocode:
+  #
+  # set = [3,4,5]
+  #
+  # std_dev = sqrt( ( (3-4)^2+(4-4)^2+(5-4)^2 ) / 2 )
+  # Which merchants sell the most items?
+  # Maybe we could set a good example for our lower sellers by displaying the merchants who have the most items for sale. Which merchants are more than one standard deviation above the average number of products offered?
+  #
+  # sales_analyst.merchants_with_high_item_count # => [merchant, merchant, merchant]
+  # What are prices like on our platform?
+  # Are these merchants selling commodity or luxury goods? Let’s find the average price of a merchant’s items (by supplying the merchant ID):
+  #
+  # sales_analyst.average_item_price_for_merchant(12334159) # => BigDecimal
+  # Then we can sum all of the averages and find the average price across all merchants (this implies that each merchant’s average has equal weight in the calculation):
+  #
+  # sales_analyst.average_average_price_per_merchant # => BigDecimal
+  # Which are our Golden Items?
+  # Given that our platform is going to charge merchants based on their sales, expensive items are extra exciting to us. Which are our “Golden Items”, those two standard-deviations above the average item price? Return the item objects of these “Golden Items”.
+  #
+  # sales_analyst.golden_items # => [<item>, <item>, <item>, <item>]
+
 end
