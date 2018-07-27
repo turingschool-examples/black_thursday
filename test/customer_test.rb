b5baec198ee1d65008cc3ae2485db97d4f494c22
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-26 18:30:45 -0600

    Adds tests for attributes

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -3,15 +3,25 @@ require_relative '../lib/customer'
 
 class CustomerTest < Minitest::Test
 
-  def test_it_exists
-    customer = Customer.new({
+  def setup
+    @customer = Customer.new({
       :id => 6,
       :first_name => "Joan",
       :last_name => "Clarke",
       :created_at => Time.now,
       :updated_at => Time.now
       })
-
-    assert_instance_of Customer, customer
   end
-end 
+
+  def test_it_exists
+    assert_instance_of Customer, @customer
+  end
+
+  def test_it_has_attributes
+    assert_equal 6, @customer.id
+    assert_equal "Joan", @customer.first_name
+    assert_equal "Clarke", @customer.last_name
+    assert_instance_of Time, @customer.created_at
+    assert_instance_of Time, @customer.updated_at
+  end
+end
