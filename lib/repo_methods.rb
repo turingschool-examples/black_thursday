7f75b5ddfceefa0c11ac40c043851b64f8f76e6f
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-28 11:06:41 -0600

    add methods to repo methods module that prior to now were duplicated across multiple repository classes

--- lib/repo_methods.rb
+++ lib/repo_methods.rb
@@ -1,4 +1,46 @@
 module RepoMethods
 
+  def all
+    @repo
+  end
 
+  def find_by_id(id)
+    @repo.find do |object|
+      object.id == id
+    end
+  end
+
+  def find_by_name(name)
+    @repo.find do |object|
+      object.name.casecmp(name).zero? # if case-insensitive returns 0, = the same name
+    end
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    @repo.find_all do |object|
+      object.merchant_id == merchant_id
+    end
+  end
+
+  def create_id
+    find_highest_id.id + 1
+  end
+
+  def find_highest_id
+    @repo.max_by do |object|
+      object.id
+    end
+  end
+
+  def delete(id)
+    object = @repo.find_index do |object|
+      object.id == id
+    end
+    return if object.nil?
+    @repo.delete_at(object)
+  end
+
+  def inspect
+    "#<#{self.class} #{@repo.size} rows>"
+  end
 end
