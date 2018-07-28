f5e0df60602a096c006b5020079eba0f8aecac69
Tara Craig <tara.craig7@gmail.com>
2018-07-28 10:27:17 -0600

    Refactor instance variable @item to @repo so we can start building a module to help with methods that are similar across multiple repos.

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class ItemRepository
   def initialize(item_data)
     @item_rows ||= build_item(item_data)
-    @items = @item_rows
+    @repo = @item_rows
   end
 
   def build_item(item_data)
@@ -13,47 +13,47 @@ class ItemRepository
   end
 
   def all
-    @items
+    @repo
   end
 
   def find_by_id(id)
-    @items.find do |item|
+    @repo.find do |item|
       item.id == id
     end
   end
 
   def find_by_name(name)
-    @items.find do |item|
+    @repo.find do |item|
       item.name.casecmp(name).zero?
     end
   end
 
   def find_all_with_description(description)
-    @items.find_all do |item|
+    @repo.find_all do |item|
       item.description.downcase.include?(description.downcase)
     end
   end
 
   def find_all_by_price(price)
-    @items.find_all do |item|
+    @repo.find_all do |item|
       item.unit_price == price
     end
   end
 
   def find_all_by_price_in_range(range)
-    @items.find_all do |item|
+    @repo.find_all do |item|
       range.include?(item.unit_price)
     end
   end
 
   def find_all_by_merchant_id(merchant_id)
-    @items.find_all do |item|
+    @repo.find_all do |item|
       item.merchant_id == merchant_id
     end
   end
 
   def find_highest_id
-    @items.max_by do |item|
+    @repo.max_by do |item|
       item.id
     end
   end
@@ -66,7 +66,7 @@ class ItemRepository
     id = create_id
     attributes[:id] = id
     item = Item.new(attributes)
-    @items << item
+    @repo << item
   end
 
   def update(id, attributes)
@@ -79,14 +79,14 @@ class ItemRepository
   end
 
   def delete(id)
-    item = @items.find_index do |item|
+    item = @repo.find_index do |item|
       item.id == id
     end
     return if item.nil?
-    @items.delete_at(item)
+    @repo.delete_at(item)
   end
 
   def inspect
-    "#<#{self.class} #{@items.size} rows>"
+    "#<#{self.class} #{@repo.size} rows>"
   end
 end
