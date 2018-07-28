08250432bd2d30dcca2812b2a1549fb0ce5ae652
Tara Craig <tara.craig7@gmail.com>
2018-07-28 14:46:16 -0600

    Add files paths for invoices, invoice items, and transactions.

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -7,6 +7,9 @@ class SalesEngineTest < Minitest::Test
     csv_hash = {
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv",
+      :invoice_items => "./data/invoice_items.csv",
+      :transactions => "./data/transactions.csv"
     }
     se = SalesEngine.from_csv(csv_hash)
 
