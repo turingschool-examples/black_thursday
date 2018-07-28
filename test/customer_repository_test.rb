5b5890d250eb4179a1f78db61938fd519a39ff56
Mike McKee <mikecm@gmail.com>
2018-07-28 13:56:44 -0600

    Update customer_repository_test.rb

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -13,6 +13,87 @@ class CustomerRepositoryTest < Minitest::Test
   def setup
     @cr = CustomerRepository.new
   end
+#frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './lib/customer_repository'
+
+# Customer repository test class
+class CustomerRepositoryTest < Minitest::Test
+  def setup
+    @cr = CustomerRepository.new
+  end
+
+  def test_it_exists
+    assert_instance_of CustomerRepository, @cr
+  end
+
+  def test_it_can_create_a_customer_instance
+    actual = @cr.create(id: 7, first_name: 'Wes', last_name: 'Anderson')
+
+    assert_instance_of Customer, actual
+  end
+
+  def test_it_returns_array_of_all_customers
+    customer_list = @cr.create(id: 7, first_name: 'Wes', last_name: 'Anderson', updated_at: '2009-05-30'),
+                    @cr.create(id: 8, first_name: 'Quentin', last_name: 'Tarantino', updated_at: '2011-08-20')
+
+    assert_equal [customer_list].flatten, @cr.all
+  end
+
+  def test_it_can_find_by_id
+    expected = @cr.create(id: 7, first_name: 'Wes', last_name: 'Anderson')
+
+    assert_equal expected, @cr.find_by_id(7)
+  end
+
+  def test_it_can_find_by_all_first_name
+    created1 = @cr.create(id: 7, first_name: 'Wes', last_name: 'Anderson')
+    created2 = @cr.create(id: 8, first_name: 'Wes', last_name: 'Mcfly')
+    created3 = @cr.create(id: 9, first_name: 'Napoleon', last_name: 'Winter')
+
+    expected = [created1, created2]
+
+    assert_equal expected, @cr.find_all_by_first_name('Wes')
+  end
+
+  def test_it_can_find_all_by_last_name
+    created1 = @cr.create(id: 7, first_name: "Wes", last_name: "Anderson")
+    created2 = @cr.create(id: 8, first_name: "Neo", last_name: "Anderson")
+    created3 = @cr.create(id: 9, first_name: "Napoleon", last_name: "Winter")
+
+    expected = [created1, created2]
+
+    assert_equal expected, @cr.find_all_by_last_name("Anderson")
+  end
+
+  def test_it_can_update_a_customer_first_name
+
+    @cr.create(id: 7, first_name: "Wes", last_name: "Anderson")
+
+    expected = @cr.update(7, first_name: "Alexandre", last_name: "Anderson")
+    assert_equal "Alexandre", @cr.find_by_id(7).first_name
+  end
+
+  def test_it_can_update_a_customer_last_name
+    @cr.create(id: 7, first_name: "Wes", last_name: "Anderson")
+    expected = @cr.update(7, first_name: "Wes", last_name: "Snipes")
+
+    assert_equal "Snipes", @cr.find_by_id(7).last_name
+  end
+
+  def test_it_can_delete_customer_by_id
+    created = @cr.create(id: 7, first_name: "Wes", last_name: "Anderson")
+
+    assert_equal created, @cr.delete(7)
+    assert_equal [], @cr.all
+  end
+end
 
   def test_it_exists
     assert_instance_of CustomerRepository, @cr
