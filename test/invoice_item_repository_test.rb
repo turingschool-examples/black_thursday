02404f0445a70a3a8599ba94840f005892d10950
Tara Craig <tara.craig7@gmail.com>
2018-07-28 11:58:18 -0600

    Add empty files for future use for invoice item repo and test.

---
+++ test/invoice_item_repository_test.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/invoice_item_repository'
+require_relative '../lib/invoice_item'
+require_relative './test_helper'
+
+class InvoiceItemRepositoryTest < Minitest::Test
+  def test_it_exists
+
+  end
+
+end
