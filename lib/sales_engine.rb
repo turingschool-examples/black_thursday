fffb616bb8e1cf83353c07c155338383e8614f9c
Tara Craig <tara.craig7@gmail.com>
2018-07-28 17:05:54 -0600

    Add require relative transaction repo so we can run spec harness.

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,6 +4,7 @@ require_relative 'merchant_repository'
 require_relative 'item_repository'
 require_relative 'invoice_repository'
 require_relative 'invoice_item_repository'
+require_relative 'transaction_repository'
 require_relative 'repo_methods'
 
 class SalesEngine
