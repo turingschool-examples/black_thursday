9302fce6c753137377f88ea6df1b937b60820474
Mike McKee <mikecm@gmail.com>
2018-07-28 11:43:04 -0600

    Completing sales anaylst refactor

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -29,7 +29,6 @@ class SalesAnalyst
 
   def merchants_with_high_item_count
     threshold = average_items_per_merchant + average_items_per_merchant_standard_deviation
-    all_merchants = @engine.merchants.all
 
     all_merchants.find_all do |merchant|
       @engine.items.find_all_by_merchant_id(merchant.id).size > threshold
@@ -47,8 +46,7 @@ class SalesAnalyst
   end
 
   def average_average_price_per_merchant
-    merchants = @engine.merchants.all
-    average_price_array = merchants.map do |merchant|
+    average_price_array = all_merchants.map do |merchant|
       average_item_price_for_merchant(merchant.id)
     end
     average_price_sum = average_price_array.inject(0) do |sum, price|
@@ -69,28 +67,22 @@ class SalesAnalyst
   end
 
   def average_invoices_per_merchant
-    total_invoices = @engine.invoices.all.size.to_f
-    total_merchants = @engine.merchants.all.size.to_f
+    total_invoices = all_invoices.size.to_f
+    total_merchants = all_merchants.size.to_f
     (total_invoices / total_merchants).round(2)
   end
 
   def average_invoices_per_merchant_standard_deviation
     mean = average_invoices_per_merchant
-    all_merchants = @engine.merchants.all
-    # invoices_per_merchant = []
     invoices_per_merchant = all_merchants.map do |merchant|
       @engine.invoices.find_all_by_merchant_id(merchant.id).size
     end
-    equation = invoices_per_merchant.inject(0) do |sum, number_invoices|
-      sum + (number_invoices - mean)**2
-    end
-    ((Math.sqrt(equation / (invoices_per_merchant.size - 1)).round(2))).to_f
+    standard_deviation(invoices_per_merchant, mean)
   end
 
   def top_merchants_by_invoice_count
     mean = average_invoices_per_merchant
     two_std = average_invoices_per_merchant_standard_deviation * 2
-    all_merchants = @engine.merchants.all
 
     all_merchants.find_all do |merchant|
       @engine.invoices.find_all_by_merchant_id(merchant.id).size > (two_std + mean)
@@ -100,7 +92,6 @@ class SalesAnalyst
   def bottom_merchants_by_invoice_count
     mean = average_invoices_per_merchant
     two_std = average_invoices_per_merchant_standard_deviation * 2
-    all_merchants = @engine.merchants.all
 
     all_merchants.find_all do |merchant|
       @engine.invoices.find_all_by_merchant_id(merchant.id).size < (mean - two_std)
@@ -108,30 +99,14 @@ class SalesAnalyst
   end
 
   def top_days_by_invoice_count
-    all_invoices = @engine.invoices.all
-    invoices_per_day = all_invoices.group_by do |invoice|
-      invoice.created_at.strftime("%A")
-    end
-    count_per_day = invoices_per_day.map do |day, invoice|
-      invoice.count
-    end
-    total_invoices = count_per_day.inject(0) do |sum, count|
-      sum += count
-    end
-    mean = total_invoices.to_f / count_per_day.size
+    mean = all_invoices.size.to_f / count_invoices_per_day.size
+    threshold = mean + standard_deviation(count_invoices_per_day, mean)
 
-    std_sum = count_per_day.inject(0) do |sum, count|
-      sum + (count - mean)**2
-    end
-    std = Math.sqrt(std_sum.to_f / 6)
-    threshold = mean + std
-
-    top_days = invoices_per_day.find_all do |day, object_array|
+    top_days = invoices_by_day.find_all do |_, object_array|
       object_array.size > threshold
     end
-    top_days.flatten.delete_if do |element|
-      element.is_a?(Invoice)
-    end
+
+    remove_keys(top_days, Invoice)
   end
 
   def invoice_status(status)
@@ -155,6 +130,14 @@ class SalesAnalyst
     @engine.items.all
   end
 
+  def all_merchants
+    @engine.merchants.all
+  end
+
+  def all_invoices
+    @engine.invoices.all
+  end
+
   def all_prices
     all_items.map(&:unit_price)
   end
@@ -162,6 +145,24 @@ class SalesAnalyst
   def all_prices_sum
     all_items.inject(0) do |sum, item|
       sum + item.unit_price
+    end
+  end
+
+  def invoices_by_day
+    all_invoices.group_by do |invoice|
+      invoice.created_at.strftime('%A')
+    end
+  end
+
+  def remove_keys(data, type)
+    data.flatten.delete_if do |element|
+      element.is_a?(type)
+    end
+  end
+
+  def count_invoices_per_day
+    invoices_by_day.map do |_, invoice|
+      invoice.count
     end
   end
 end
