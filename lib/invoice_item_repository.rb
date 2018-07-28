aea7e4b05d48cb1444a84074499f1f0f7278c652
Tara Craig <tara.craig7@gmail.com>
2018-07-28 13:10:52 -0600

    Add find all by invoice id method.

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -16,8 +16,14 @@ class InvoiceItemRepository
   end
 
   def find_all_by_item_id(item_id)
-    @repo.find_all do |item|
-      item.item_id == item_id
+    @repo.find_all do |invoice_item|
+      invoice_item.item_id == item_id
+    end
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    @repo.find_all do |invoice_item|
+      invoice_item.invoice_id == invoice_id
     end
   end
 end
