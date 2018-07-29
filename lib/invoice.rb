ec83f4a37fea78037a26e9ef1386815251cce1fa
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-07-28 18:31:20 -0600

    invoice class

---
+++ lib/invoice.rb
@@ -0,0 +1,31 @@
+require 'time'
+
+class Invoice 
+  attr_accessor :id,
+                :customer_id,
+                :merchant_id,
+                :status,
+                :created_at,
+                :updated_at
+                
+  def initialize(hash)
+    @id = hash[:id].to_i
+    @customer_id = hash[:customer_id].to_i
+    @merchant_id = hash[:merchant_id].to_i
+    @status = status
+    @created_at = if hash[:created_at].class == String
+                  Time.parse(hash[:created_at])
+                    else
+                      Time.now
+                  end
+    @updated_at = if hash[:updated_at].class == String
+                    Time.parse(hash[:updated_at])
+                    else
+                      Time.now
+                  end
+  end
+  
+  
+end
+
+
---
+++ lib/invoice_repo.rb
@@ -0,0 +1,20 @@
+# require_relative 'repo_module'
+# require_relative 'invoice'
+# 
+# class InvoiceRepo 
+# 
+#   include Repository
+# 
+#   attr_accessor :
+# 
+#   def initialize(invoices)
+#     @invoices = invoices 
+#   end
+# 
+#   def all 
+#     @invoices
+#   end
+# 
+#   def find_all
+  
+  
