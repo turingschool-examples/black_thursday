cd51fa28c37c5e3e8adf7b84c2770ba2151b65d5
Tara Craig <tara.craig7@gmail.com>
2018-07-29 11:41:25 -0600

    Create test file for customer repo, add setup method.

---
+++ test/customer_repository_test.rb
@@ -0,0 +1,27 @@
+require_relative '../lib/customer_repository'
+require_relative '../lib/customer'
+require_relative './test_helper'
+
+class CustomerRepositoryTest < Minitest::Test
+  def setup
+    @customers =
+      [{id: 1,
+      first_name: 'Jane',
+      last_name: 'Doe',
+      created_at: '2010-12-10',
+      updated_at: '2011-12-04'},
+      {id: 2,
+      first_name: 'John',
+      last_name: 'Doe',
+      created_at: '2009-05-30',
+      updated_at: '2010-08-29'},
+      {id: 3,
+      first_name: 'Jerry',
+      last_name: 'Doe',
+      created_at: '2010-03-30',
+      updated_at: '2013-01-21'}]
+
+    @customer_repository = CustomerRepository.new(@customers)
+  end
+
+end
