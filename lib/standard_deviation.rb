704658da0cbc2df144e108280c6a56065b51090a
Harper Bellows <harper.bellows@gmail.com>
2018-07-28 20:00:59 -0600

    final standard deviation module methods

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,8 +1,17 @@
 module StandardDeviation
 
   def average(info)
-    
+    (info.inject(:+) / info.count.to_f).round(2)
+  end
 
+  def variance(info)
+    current_average = average(info)
+    info.map {|number| (current_average - number) ** 2}
+  end
+
+  def standard_deviation(info)
+    v = variance(info)
+    Math.sqrt(average(v)).round(2)
   end
 
 end
