bf7235d85758bda55483c1e95501240a162ec062
Mike McKee <mikecm@gmail.com>
2018-07-30 09:11:06 -0600

    Merchant repo more refactoring out methods

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -8,19 +8,19 @@ class MerchantRepository
   include RepositoryHelper
 
   def initialize
-    @merchants = {}
+    @repository = {}
   end
 
   def create(params)
-    params[:id] = @merchants.max[0] + 1 if params[:id].nil?
+    params[:id] = @repository.max[0] + 1 if params[:id].nil?
 
     Merchant.new(params).tap do |merchant|
-      @merchants[params[:id].to_i] = merchant
+      @repository[params[:id].to_i] = merchant
     end
   end
 
   def update(id, params)
-    return nil unless @merchants.key?(id)
+    return nil unless @repository.key?(id)
     new_name = params[:name]
     merchant = find_by_id(id)
     merchant.name = new_name
@@ -28,13 +28,8 @@ class MerchantRepository
   end
 
   def all
-    merchant_pairs = @merchants.to_a.flatten
+    merchant_pairs = @repository.to_a.flatten
     remove_keys(merchant_pairs, Merchant)
-  end
-
-  def find_by_id(id)
-    return nil unless @merchants.key?(id)
-    @merchants.fetch(id)
   end
 
   def find_by_name(name)
@@ -44,17 +39,8 @@ class MerchantRepository
   end
 
   def find_all_by_name(name)
-    merchant_pairs = @merchants.find_all do |_, merchant|
+    all.find_all do |merchant|
       merchant.name.downcase.include?(name.downcase)
-    end.flatten
-    remove_keys(merchant_pairs, Merchant)
-  end
-
-  def delete(id)
-    @merchants.delete(id)
-  end
-
-  def inspect
-    "#<#{self.class} #{@merchants.size} rows>"
+    end
   end
 end
--- lib/repository_helper.rb
+++ lib/repository_helper.rb
@@ -8,9 +8,22 @@ module RepositoryHelper
     end
   end
 
+  def find_by_id(id)
+    return nil unless @repository.key?(id)
+    @repository.fetch(id)
+  end
+
+  def delete(id)
+    @repository.delete(id)
+  end
+
   def remove_keys(data, type)
     data.keep_if do |element|
       element.is_a?(type)
     end
+  end
+
+  def inspect
+    "#<#{self.class} #{@repository.size} rows>"
   end
 end
