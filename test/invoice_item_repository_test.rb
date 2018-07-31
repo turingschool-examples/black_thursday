f0d989f3e3ac1c71e6b541a812117127fbc0df18
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-07-30 18:27:05 -0600

    Finishes mini test for invoice item

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -24,13 +24,13 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 9, @iir.all.count
   end
 
-  def test_it_can_find_an_invoice_item_by_a_valid_id
+  def test_it_can_find_by_id
     item_invoice = @iir.find_by_id(5)
     assert_instance_of InvoiceItem, item_invoice
     assert_equal 5, item_invoice.id
   end
 
-  def test_it_returns_nil_if_invoice_id_is_invalid
+  def test_it_cannot_find_by_invalid_id
     invoice = @iir.find_by_id(25)
     assert_nil invoice
   end
@@ -38,54 +38,50 @@ class InvoiceItemRepositoryTest < Minitest::Test
   def test_it_can_find_all_by_item_id
     assert_equal 3, @iir.find_all_by_item_id(263519844).count
   end
-  #
-  def test_it_can_find_all_invoice_items_by_invoice_id
+
+  def test_it_can_find_all_by_invoice_id
     assert_equal 8, @iir.find_all_by_invoice_id(1).count
   end
+
+  def test_it_can_create_new_id
+    new_invoice_item = @iir.create_id
+    assert_equal 10, new_invoice_item
+  end
   #
-  # def test_it_can_create_new_id
-  #   invoice_item = @iir.create_id
-  #   assert_equal 4, invoice_item
-  # end
+  def test_it_can_create_new_invoice_item
+    attributes = {  item_id: 50,
+                    invoice_id: 6666,
+                    quantity: 2,
+                    unit_price: 2250,
+                    created_at: '2018-07-30',
+                    updated_at: '2018-07-30'
+                  }
+    invoice_item = @iir.create(attributes)
+    assert_equal 10, invoice_item.id
+    assert_equal 50, invoice_item.item_id
+    assert_equal 6666, invoice_item.invoice_id
+    assert_equal 2, invoice_item.quantity
+    assert_equal BigDecimal, invoice_item.unit_price.class
+    assert_instance_of Time, invoice_item.created_at
+    assert_instance_of Time, invoice_item.updated_at
+  end
   #
-  # def test_it_can_create_new_invoice_item
-  #   attributes = {  item_id: 27,
-  #                   invoice_id: 2772,
-  #                   quantity: 4,
-  #                   unit_price: 2134,
-  #                   created_at: '2018-07-28',
-  #                   updated_at: '2018-07-28'
-  #                 }
-  #   invoice_item = @iir.create(attributes)
-  #   assert_equal 4, invoice_item.id
-  #   assert_equal 27, invoice_item.item_id
-  #   assert_equal 2772, invoice_item.invoice_id
-  #   assert_equal 4, invoice_item.quantity
-  #   assert_equal BigDecimal, invoice_item.unit_price.class
-  #   assert_instance_of Time, invoice_item.created_at
-  #   assert_instance_of Time, invoice_item.updated_at
-  # end
-  #
-  # def test_it_can_update_invoice_item
-  #   attributes = {
-  #     quantity: 4,
-  #     unit_price: 15635
-  #   }
-  #   id = 1
-  #   invoice_item = @iir.update(id, attributes)
-  #   expected = @iir.find_by_id(id)
-  #   assert_instance_of Time, invoice_item.updated_at # maybe refactor later
-  #   assert_equal 1, expected.invoice_id
-  #   assert_equal 4, expected.quantity
-  #   assert_equal 15635, expected.unit_price
-  # end
-  #
-  # def test_it_can_delete_invoice
-  #   id = 2
-  #
-  #   invoice = @iir.delete(id)
-  #   expected = @iir.find_by_id(2)
-  #
-  #   assert_nil expected
-  # end
+  def test_it_can_update_invoice_item
+    attributes = {
+      quantity: 4,
+      unit_price: 1776
+    }
+    id = 1
+    invoice_item = @iir.update(id, attributes)
+    expected = @iir.find_by_id(id)
+    assert_instance_of Time, invoice_item.updated_at
+    assert_equal 1, expected.invoice_id
+    assert_equal 4, expected.quantity
+    assert_equal 1776, expected.unit_price
+  end
+
+  def test_it_can_delete
+    @iir.delete(2)
+    assert_nil @iir.find_by_id(2)
+  end
 end
