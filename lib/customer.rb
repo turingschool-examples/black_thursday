88525c6b38f3f87aeb5c54e5b34d397f0dd029ef
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-07-31 13:35:14 -0600

    Creates and finishes tdd for customer class.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,11 @@
 require 'time'
 class Customer
+  attr_reader :id,
+              :name,
+              :first_name,
+              :last_name,
+              :created_at,
+              :updated_at
   def initialize(attributes)
     @id = attributes[:id].to_i
     @first_name = attributes[:first_name]
--- test/customer_test.rb
+++ test/customer_test.rb
@@ -12,9 +12,16 @@ class CustomerTest < Minitest::Test
       :updated_at => Time.now
       } )
   end
+
   def test_it_exists
     assert_instance_of Customer, @c
   end
 
-
+  def test_it_has_attributes
+    assert_equal 6, @c.id
+    assert_equal "Joan", @c.first_name
+    assert_equal "Clarke", @c.last_name
+    assert_instance_of Time, @c.created_at
+    assert_instance_of Time, @c.updated_at
+  end
 end
