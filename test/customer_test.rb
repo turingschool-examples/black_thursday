a1695780ed14f651842dcefc095d8b9aa28430f5
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-01 10:13:44 -0600

    resolve rubocop indentation errors. all tests pass

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -3,15 +3,11 @@ require './lib/customer'
 
 class CustomerTest < Minitest::Test
   def test_it_exists
-    customer = Customer.new(
-      {
-        :id => 5,
-        :first_name => "Jane",
-        :last_name => "Doe",
-        :created_at => "2010-12-10",
-        :updated_at => "2011-12-04"
-        }
-      )
+    customer = Customer.new(id: 5,
+                            first_name: 'Jane',
+                            last_name: 'Doe',
+                            created_at: '2010-12-10',
+                            updated_at: '2011-12-04')
 
     assert_instance_of Customer, customer
   end
@@ -20,16 +16,16 @@ class CustomerTest < Minitest::Test
     customer = Customer.new(
       {
         :id => 5,
-        :first_name => "Jane",
-        :last_name => "Doe",
-        :created_at => "2010-12-10",
-        :updated_at => "2011-12-04"
+        :first_name => 'Jane',
+        :last_name => 'Doe',
+        :created_at => '2010-12-10',
+        :updated_at => '2011-12-04'
         }
       )
 
     assert_equal 5, customer.id
-    assert_equal "Jane", customer.first_name
-    assert_equal "Doe", customer.last_name
+    assert_equal 'Jane', customer.first_name
+    assert_equal 'Doe', customer.last_name
     assert_equal Time, customer.created_at.class
     assert_equal Time, customer.updated_at.class
   end
