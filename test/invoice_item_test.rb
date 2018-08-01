067fe82afa1edf109d7e71664a9e16760abd39e0
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-01 10:48:40 -0600

    resolve rubocop errors; all tests pass

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -1,20 +1,15 @@
-require_relative '../lib/invoice_item'
 require_relative './test_helper'
+require './lib/invoice_item'
 
 class InvoiceItemTest < Minitest::Test
-
   def setup
-    @invoice_item = InvoiceItem.new(
-      {
-        id: 1,
-        item_id: 263519844,
-        invoice_id: 1,
-        quantity: 5,
-        unit_price: 13635,
-        created_at: '2012-03-27 14:54:09 UTC',
-        updated_at: '2012-03-27 14:54:09 UTC'
-      }
-    )
+    @invoice_item = InvoiceItem.new(id: 1,
+                                    item_id: 263519844,
+                                    invoice_id: 1,
+                                    quantity: 5,
+                                    unit_price: 13635,
+                                    created_at: '2012-03-27 14:54:09 UTC',
+                                    updated_at: '2012-03-27 14:54:09 UTC')
   end
 
   def test_it_exists
@@ -24,5 +19,4 @@ class InvoiceItemTest < Minitest::Test
   def test_it_can_find_unit_price_to_dollars
     assert_equal 136.35, @invoice_item.unit_price_to_dollars
   end
-
 end
