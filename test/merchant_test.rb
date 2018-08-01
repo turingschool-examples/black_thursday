33d923cba1237a8f34a3a9d1ebcf725d4c6987d0
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-01 11:25:57 -0600

    resolve rubocop errors; all tests pass

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -2,32 +2,22 @@ require_relative './test_helper'
 require './lib/merchant'
 
 class MerchantTest < Minitest::Test
-
   def test_it_exists
-    merchant = Merchant.new(
-      {
-        :id => 5,
-        :name => "Turing School",
-        :created_at => "2010-12-10",
-        :updated_at => "2011-12-04"
-        }
-      )
+    merchant = Merchant.new(id: 5,
+                            name: 'Turing School',
+                            created_at: '2010-12-10',
+                            updated_at: '2011-12-04')
 
     assert_instance_of Merchant, merchant
   end
 
   def test_it_has_attributes
-    merchant = Merchant.new(
-      {
-        :id => 5,
-        :name => "Turing School",
-        :created_at => "2010-12-10",
-        :updated_at => "2011-12-04"
-        }
-      )
+    merchant = Merchant.new(id: 5,
+                            name: 'Turing School',
+                            created_at: '2010-12-10',
+                            updated_at: '2011-12-04')
 
     assert_equal 5, merchant.id
-    assert_equal "Turing School", merchant.name
+    assert_equal 'Turing School', merchant.name
   end
-
 end
