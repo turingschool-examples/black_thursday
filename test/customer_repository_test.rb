862881e0eeb287e4bfc4d6a39cc7da2f9f01387f
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-01 09:00:27 -0600

    WIP robo errors on indentation

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -5,27 +5,34 @@ require_relative './test_helper'
 class CustomerRepositoryTest < Minitest::Test
   def setup
     @customers =
-      [{id: 1,
+    [{
+      id: 1,
       first_name: 'Jane',
       last_name: 'Doe',
       created_at: '2010-12-10',
-      updated_at: '2011-12-04'},
-      {id: 2,
-      first_name: 'John',
-      last_name: 'Doe',
-      created_at: '2009-05-30',
-      updated_at: '2010-08-29'},
-      {id: 3,
-      first_name: 'Jerry',
-      last_name: 'Doe',
-      created_at: '2010-03-30',
-      updated_at: '2013-01-21'},
-      {id: 4,
-      first_name: 'Jerry',
-      last_name: 'Don',
-      created_at: '2010-03-30',
-      updated_at: '2013-01-21'}
-    ]
+      updated_at: '2011-12-04'
+    },
+     {
+       id: 2,
+       first_name: 'John',
+       last_name: 'Doe',
+       created_at: '2009-05-30',
+       updated_at: '2010-08-29'
+    },
+     {
+       id: 3,
+       first_name: 'Jerry',
+       last_name: 'Doe',
+       created_at: '2010-03-30',
+       updated_at: '2013-01-21'
+    },
+     {
+        id: 4,
+        first_name: 'Jerry',
+        last_name: 'Don',
+        created_at: '2010-03-30',
+        updated_at: '2013-01-21'
+      }]
 
     @customer_repository = CustomerRepository.new(@customers)
   end
