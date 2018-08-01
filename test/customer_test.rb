17846dd52dcf59be1853896fbe1aa37486f69116
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-01 10:16:28 -0600

    resolve remaining rubocop errors; all tests pass

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -13,15 +13,11 @@ class CustomerTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    customer = Customer.new(
-      {
-        :id => 5,
-        :first_name => 'Jane',
-        :last_name => 'Doe',
-        :created_at => '2010-12-10',
-        :updated_at => '2011-12-04'
-        }
-      )
+    customer = Customer.new(id: 5,
+                            first_name: 'Jane',
+                            last_name: 'Doe',
+                            created_at: '2010-12-10',
+                            updated_at: '2011-12-04')
 
     assert_equal 5, customer.id
     assert_equal 'Jane', customer.first_name
@@ -29,5 +25,4 @@ class CustomerTest < Minitest::Test
     assert_equal Time, customer.created_at.class
     assert_equal Time, customer.updated_at.class
   end
-
 end
