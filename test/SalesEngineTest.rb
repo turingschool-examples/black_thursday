f23b4e630016bc0feda1b5b644147f69154a3a50
Mary Bork <maryebork@outlook.com>
2018-09-08 15:33:10 -0600

    Adds SalesEngine Lib/Test Files

---
+++ test/SalesEngineTest.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/SalesEngine'
+require 'pry'
+
+class SalesEngineTest < Minitest::Test
+  def test_it_exists
+    se = SalesEngine.new
+
+    assert_instance_of SalesEngine, se
+  end
+end
