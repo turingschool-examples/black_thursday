d6880e7b52bd0ed9ad4d48d8c8e81189f44cc52d
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-08 17:19:07 -0600

    add attributes and tests

--- lib/item.rb
+++ lib/item.rb
@@ -2,11 +2,23 @@ require 'pry'
 
 class Item
   attr_reader :id, 
-              :name
+              :created_at,
+              :merchant_id
+
+  attr_accessor :name,
+                :description,
+                :unit_price,
+                :updated_at
 
   def initialize(hash)
     @id = hash[:id]
+    @created_at = hash[:created_at]
+    @merchant_id = hash[:merchant_id]
+    
     @name = hash[:name]
+    @description = hash[:description]
+    @unit_price = hash[:unit_price]
+    @updated_at = hash[:updated_at]
   end
 
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -7,15 +7,21 @@ require 'bigdecimal'
 class ItemTest < Minitest::Test
 
   def setup
+    
+    @decimal = BigDecimal.new(10.99, 4)
+    @created = Time.new(2018, 9, 7)
+    @updated = Time.new(2018, 9, 8)
+
     input = {
       :id => 1, 
       :name => "Pencil",
       :description => "You can use it to write things",
-      :unitt_price => BigDecimal.new(10.99, 4),
-      :created_at => Time.now,
-      :updated_at => Time.now,
+      :unit_price => @decimal,
+      :created_at => @created,
+      :updated_at => @updated,
       :merchant_id => 2
     }
+    
     @m = Item.new(input)
   end
 
@@ -25,7 +31,13 @@ class ItemTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal 1, @m.id
+    assert_equal @created, @m.created_at
+    assert_equal 2, @m.merchant_id
+
     assert_equal "Pencil", @m.name
+    assert_equal "You can use it to write things", @m.description
+    assert_equal @decimal, @m.unit_price
+    assert_equal @updated, @m.updated_at
   end
 
 
