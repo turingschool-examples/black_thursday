f7f4f0f9b13b0b81a48a50ae86c5be9d897193dd
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-08 16:42:20 -0600

    create merchant class and test

---
+++ test/merchant_test.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/merchant'
+
+
+class MerchantTest < Minitest::Test
+
+  def setup
+    input = {:id => 5, :name => "Turing School"}
+    @m = Merchant.new(input)
+  end
+
+  def test_it_exists
+    assert_instance_of Merchant, @m
+  end
+
+  def test_it_has_attributes
+    assert_equal 5, @m.id
+    assert_equal "Turing School", @m.name
+  end
+
+
+end
