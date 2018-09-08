5aefc0b57d1330e8967100fe570f12debf31d0a1
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-09-08 13:22:40 -0600

    undos a module and comments it out for later use

--- lib/black_thursday_helper.rb
+++ lib/black_thursday_helper.rb
@@ -2,10 +2,10 @@ require 'CSV'
 require 'pry'
 module Black_Thursday_Helper
 
-  def populate(filepath)
-    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
-      @merchants << Merchant.new(data)
-    end
-  end
+  # def populate(filepath)
+  #   CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
+  #     @merchants << Merchant.new(data)
+  #   end
+  # end
 
 end
--- lib/merchant_repo.rb
+++ lib/merchant_repo.rb
@@ -1,7 +1,11 @@
-require_relative 'black_thursday_helper'
+# require_relative 'black_thursday_helper'
+require 'CSV'
+require 'pry'
+require './lib/merchant'
+
 class MerchantRepo
   #how do we indent this?
-  include Black_Thursday_Helper
+  # include Black_Thursday_Helper
 
   def initialize(file_path)
     @merchants = []
@@ -10,6 +14,12 @@ class MerchantRepo
 
   def all
     @merchants
+  end
+
+  def populate(filepath)
+    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
+      @merchants << Merchant.new(data)
+    end
   end
 
 
