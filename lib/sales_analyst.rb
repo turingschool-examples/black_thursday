8a8a96cd2c9294416326a3e12b8095717afcdd75
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-09 15:55:22 -0600

    Write average items per merchant and average items stand deviation methods

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,8 +2,42 @@ require_relative '../lib/sales_analyst'
 
 class SalesAnalyst
 
-  def average_items_per_merchant
+  def initialize(item_repo, merchant_repo)
+    @item_repo = item_repo
+    @merchant_repo = merchant_repo
+  end
 
+  def average_items_per_merchant
+    (@item_repo.all.count.to_f / @merchant_repo.all.count).round(2)
+  end
+
+  def items_per_merchant_array
+    @merchant_repo.all.map do |merchant|
+      @item_repo.all.find_all do |item|
+        merchant.id == item.merchant_id
+      end.count
+    end
+  end
+
+  def sum(array)
+    array.inject(0) do |total, item|
+      total += item
+    end
+  end
+
+  def subtract_square_sum_array
+    set = items_per_merchant_array
+    average = average_items_per_merchant
+    new_set = set.map do |element|
+      (average - element)**2
+    end
+    sum(new_set)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    step_one = subtract_square_sum_array
+    step_two = step_one/(@merchant_repo.all.count - 1)
+    Math.sqrt(step_two).round(2)
   end
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -13,4 +13,40 @@ class SalesAnalystTest < Minitest::Test
     sa = se.analyst
     assert_equal 2.88, sa.average_items_per_merchant
   end
+
+  def test_it_can_get_array_of_items_per_merchant
+    se = SalesEngine.from_csv({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+      })
+    sa = se.analyst
+    assert_equal 475, sa.items_per_merchant_array.length
+  end
+
+  def test_it_can_take_difference_between_a_set_and_mean_and_square_it
+    se = SalesEngine.from_csv({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+      })
+    sa = se.analyst
+    assert_equal 5034.919999999962, sa.subtract_square_sum_array
+  end
+
+  def test_it_can_sum_array
+    se = SalesEngine.from_csv({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+      })
+    sa = se.analyst
+    assert_equal 15, sa.sum([1, 2, 3, 4, 5])
+  end
+
+  def test_it_can_calculate_average_items_per_merchange_st_dev
+    se = SalesEngine.from_csv({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+      })
+    sa = se.analyst
+    assert_equal 3.26, sa.average_items_per_merchant_standard_deviation
+  end
 end
