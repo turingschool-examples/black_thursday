b3eebe6c9d7ecb71bc64cd300b75313013be7b95
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-08 18:05:35 -0600

    create finder mnodule outline

---
+++ lib/finder.rb
@@ -0,0 +1,33 @@
+
+#find_all uses find_by
+
+module Finder
+
+  def find_by(class, attribute)
+    # case class
+    # when "merchant"
+    #   => {:id = 12345, :name => "Turing School"}
+    # 
+    # when "item"
+    #   => {:id = 1, 
+    #       :name => "Pencil", 
+    #       :description => "You can use it to write things",
+    #       ...}
+    #
+
+  end
+
+
+  def find_all_by(class, attribute)
+    # case class
+    # 
+    # calls find_by to return all chosen
+    #
+    #
+    #
+    #
+    #
+    #    
+
+  end
+end
