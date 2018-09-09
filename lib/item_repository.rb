f9e498fdd98b703976a5348b7fdf1ed0335af294
Aizar <aizar.cabrera@icloud.com>
2018-09-08 19:31:32 -0600

    add test for all and make_items, both passed

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -4,5 +4,18 @@ require './lib/item'
 
 class ItemRepository
   def initialize(item_path)
+    @items = []
+    make_items(item_path)
   end
+
+  def all
+    @items
+  end
+
+  def make_items(item_path)
+    CSV.foreach(item_path, headers: true, header_converters: :symbol) do |row|
+      @items << Item.new(row)
+    end
+  end
+
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -8,11 +8,11 @@ class MerchantTest<Minitest::Test
 
     assert_instance_of ItemRepository, ir
   end
-  #
-  # def test_merchant_repo_has_merchants
-  #   mr = MerchantRepository.new("./data/merchants.csv")
-  #
-  #   assert_equal 475 , mr.all.count
-  #   assert_instance_of Array, mr.all
-  # end
+
+  def test_item_repo_has_items
+    ir = ItemRepository.new("./data/items.csv")
+
+    assert_equal 1367 , ir.all.count
+    assert_instance_of Array, ir.all
+  end
 end
