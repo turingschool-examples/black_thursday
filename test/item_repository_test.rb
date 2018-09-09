2a32aab7d9f183cd9950f11a525b488049745fa1
Aizar <aizar.cabrera@icloud.com>
2018-09-09 16:42:03 -0600

    add method setup for item and item_tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -4,16 +4,16 @@ require 'CSV'
 
 class MerchantTest<Minitest::Test
 
-  def test_it_exists
-    ir = ItemRepository.new('./test/fixtures/items_fixtures.csv')
+  def setup
+    @ir = ItemRepository.new('./test/fixtures/items_fixtures.csv')
+  end
 
-    assert_instance_of ItemRepository, ir
+  def test_it_exists
+    assert_instance_of ItemRepository, @ir
   end
 
   def test_item_repo_has_items
-    ir = ItemRepository.new('./test/fixtures/items_fixtures.csv')
-
-    assert_equal 10 , ir.all.count
-    assert_instance_of Array, ir.all
+    assert_equal 10 , @ir.all.count
+    assert_instance_of Array, @ir.all
   end
 end
