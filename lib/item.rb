cb4502add5ebdb6fc38005c7fe5736df2935e680
Aizar <aizar.cabrera@icloud.com>
2018-09-08 18:09:24 -0600

    add item class and test with attributes

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,21 @@
 require 'pry'
 
 class Item
-  def initialize(item_data)
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :merchant_id,
+              :created_at,
+              :updated_at
+
+  def initialize(item_hash)
+    @id          = item_hash[:id]
+    @name        = item_hash[:name]
+    @description = item_hash[:description]
+    @unit_price  = item_hash[:unit_price]
+    @merchant_id = item_hash[:merchant_id]
+    @created_at  = item_hash[:created_at]
+    @updated_at  = item_hash[:updated_at]
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -18,17 +18,25 @@ class ItemTest<Minitest::Test
 
       assert_instance_of Item, i
   end
-  #
-  # def test_it_has_attributes
-  #   item = Item.new(ITEM_DATA)
-  #
-  #   assert_equal 263_538_760, item.id
-  #   assert_equal 'Pencil', item.name
-  #   assert_equal 'You can use it to write things', item.description
-  #   assert_instance_of BigDecimal, item.unit_price
-  #   assert_equal 12_334_185, item.merchant_id
-  #   assert_instance_of Time, item.created_at
-  #   assert_instance_of Time, item.updated_at
-  # end
+
+  def test_it_has_attributes
+    i = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+      })
+
+    assert_equal 1, i.id
+    assert_equal 'Pencil', i.name
+    assert_equal 'You can use it to write things', i.description
+    assert_instance_of BigDecimal, i.unit_price
+    assert_instance_of Time, i.created_at
+    assert_instance_of Time, i.updated_at
+    assert_equal 2, i.merchant_id
+  end
 
 end
