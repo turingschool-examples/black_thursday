90a9f4a333f4c007625b3e35413a180ebf7eacbe
Meghan Stang <mstang15@gmail.com>
2018-09-10 08:50:10 -0600

    adds start of sales_engine test

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,12 +3,21 @@ require_relative 'item_repository'
 
 
 class SalesEngine
+  attr_reader :data
   def initialize(data)
     @data = data
   end
 
   def self.from_csv(data)
     new(data)
+  end
+
+  def items
+    @items ||= ItemRepository.new(load_file(data[:items]))
+  end
+
+  def merchants
+    @merchants ||= MerchantRepository.new(load_file(data[:merchants]))
   end
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -10,4 +10,12 @@ class SalesEngineTest <Minitest::Test
 
   assert_instance_of SalesEngine, se
   end
+
+  def test_it_has_from_csv
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+    })
+    assert_equal ({items: './data/items.csv', merchants: './data/merchants.csv'}), se.data
+  end
 end
