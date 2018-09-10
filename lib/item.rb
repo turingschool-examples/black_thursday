fa5fc122e8d72e14780a92e10b53edb82100f539
Quinn Krug <quinn.krug@gmail.com>
2018-09-10 16:40:44 -0600

    item repo built and tested

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,8 @@
 class Item
 
-  attr_reader :id, :name,:description, :unit_price,
-              :created_at, :updated_at, :merchant_id
+  attr_reader :id, :created_at, :merchant_id
+
+  attr_accessor :name, :description, :unit_price, :updated_at
 
   def initialize(item_hash)
     @id = item_hash[:id]
