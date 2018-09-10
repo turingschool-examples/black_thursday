09c3a4f51f3d85038fcbea52a848e6a8fed2a319
Quinn Krug <quinn.krug@gmail.com>
2018-09-09 20:44:04 -0600

    find all tested and working

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -19,5 +19,13 @@ class MerchantRepository
     merchants.find {|merchant| merchant.id == id}
   end
 
+  def find_by_name(name)
+    merchants.find {|merchant| merchant.name.downcase == name}
+  end
+
+  def find_all_by_name(name)
+    merchants.find_all {|merchant| merchant.name.downcase == name}
+  end
+
 
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -52,11 +52,44 @@ class MerchantRepositoryTest < Minitest::Test
 
     mr.add_merchant(m)
     mr.add_merchant(m2)
-  
+
     actual = mr.find_by_id(6).name
 
     assert_equal "Basement", actual
 
   end
+
+  def test_we_can_find_merchant_by_name
+    mr = MerchantRepository.new
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+    m2 = Merchant.new({:id => 6, :name => "Basement"})
+
+    mr.add_merchant(m)
+    mr.add_merchant(m2)
+
+    actual = mr.find_by_name("basement").name
+
+    assert_equal "Basement", actual
+
+  end
+
+  def test_we_can_find_all_merchants_by_name
+    mr = MerchantRepository.new
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+    m2 = Merchant.new({:id => 6, :name => "Basement"})
+    m3 = Merchant.new({:id => 7, :name => "BaseMent"})
+    m4 = Merchant.new({:id => 8, :name => "bAsEmEnT"})
+
+    mr.add_merchant(m)
+    mr.add_merchant(m2)
+    mr.add_merchant(m3)
+    mr.add_merchant(m4)
+
+    actual = mr.find_all_by_name("basement")
+
+    assert_equal [m2,m3,m4], actual
+
+  end
+
 
 end
