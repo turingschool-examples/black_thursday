1f39fe377e7826401293df6875438ca9bf02aa7d
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-09-10 14:42:07 -0600

    making sure everything is working

--- lib/black_thursday_helper.rb
+++ lib/black_thursday_helper.rb
@@ -2,10 +2,18 @@ require 'CSV'
 require 'pry'
 module Black_Thursday_Helper
 
-  # def populate(filepath)
-  #   CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
-  #     @merchants << Merchant.new(data)
-  #   end
-  # end
+  def populate(filepath)
+    CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
+      @merchants << Merchant.new(data)
+    end
+  end
+
+  def populate(filepath)
+      csv_objects = CSV.open(filepath, headers: true, header_converters: :symbol)
+      csv_objects.map do |object|
+        object[:id] = object[:id].to_i
+      collection << object.to_h
+    end
+  end
 
 end
