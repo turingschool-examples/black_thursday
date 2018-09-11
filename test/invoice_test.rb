a42d2c60ba628cb590ee79c2dd1ecfa8969fb1b3
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-11 11:48:13 -0600

    create invoice class and attribute tests

---
+++ test/invoice_test.rb
@@ -0,0 +1,41 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/invoice'
+
+
+class InvoiceTest < Minitest::Test
+
+  def setup
+    @hash = {
+      :id => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status => "pending",
+      :created_at => Time.now,
+      :updated_at => Time.now
+    }
+
+    @invoice = Invoice.new(@hash)
+  end
+
+
+  def test_it_exists
+    assert_instance_of Invoice, @invoice
+  end
+
+  def test_it_gets_attributes
+    # -- Read Only --
+    assert_equal @hash[:id], @invoice.id
+    assert_equal @hash[:customer_id], @invoice.customer_id
+    assert_equal @hash[:merchant_id], @invoice.merchant_id
+    # TO DO - Assert we cannot write to these values https://docs.ruby-lang.org/en/2.1.0/MiniTest/Assertions.html
+    # -- Accessible --
+    assert_equal @hash[:created_at], @invoice.created_at
+    assert_equal @hash[:updated_at], @invoice.updated_at
+    # TO DO - Assert we can write to these values 
+  end
+
+  
+
+end
