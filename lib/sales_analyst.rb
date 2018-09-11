6e2c93b4cb02c7ac0af2f628f39670f42c79179b
Aizar <aizar.cabrera@icloud.com>
2018-09-11 17:54:17 -0600

    add method for average items per merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,6 +1,17 @@
-class SalesAnalyst
+require 'pry'
 
-  def initiliaze
+class SalesAnalyst
+  attr_reader :se,
+              :mr,
+              :ir
+
+  def initialize(se)
+    @se = se
+    @mr = @se.merchants
+    @ir = @se.items
   end
 
+  def average_items_per_merchant
+    (@ir.all.count.to_f/@mr.all.count).round(2)
+  end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -9,32 +9,19 @@ require 'pry'
      se = SalesEngine.from_csv({
      :merchants     => './test/fixtures/merchants_fixtures.csv',
      :items         => './test/fixtures/items_fixtures.csv'
-                               })
+                             })
      @sa = se.analyst
     end
 
-   def test_it_exists
-   assert_instance_of SalesAnalyst, @sa
-   end
+    def test_it_exists
+     assert_instance_of SalesAnalyst, @sa
+    end
 
+    def test_it_returns_average_items_per_merchant
+      result = @sa.average_items_per_merchant
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      assert_equal 0.77, result # <= We're using fixtures here...!!!
+      assert_equal Float, result.class
+    end
 
  end
