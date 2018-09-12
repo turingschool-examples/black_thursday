2c2b15baf895012f132624b0498a29fb6dd3f5f4
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-09-12 08:59:01 -0600

    yay a different error

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,6 +1,5 @@
 require_relative "../lib/items_repo"
 require_relative "../lib/merchant_repo"
-require_relative "../lib/black_thursday_helper"
 
 class SalesEngine
   attr_reader :merchants,
@@ -8,9 +7,9 @@ class SalesEngine
 
   def self.from_csv(params)
     merchants = MerchantRepo.new(params[:merchants])
-    populate_merchants(params[:merchant])
+    self.populate_merchants(params[:merchant])
     items = ItemsRepo.new(params[:items])
-    populate_items(params[:items])
+    self.populate_items(params[:items])
 
     SalesEngine.new(merchants, items)
   end
@@ -20,13 +19,13 @@ class SalesEngine
     @items = items
   end
 
-  def populate_merchants(filepath)
+  def self.populate_merchants(filepath)
     CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
       @collections << Merchant.new(data)
     end
   end
 
-  def populate_items(filepath)
+  def self.populate_items(filepath)
     CSV.foreach(filepath, headers: true, header_converters: :symbol) do |data|
       @collections << Item.new(data)
     end
