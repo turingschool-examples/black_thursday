857fc89d595b3302ecee00d9b1eee3071dd274d2
Jordan Whitten <jordanwa1947@gmail.com>
2018-09-12 17:32:19 -0600

    finished golden items and average merchants price as well as passed the spec harness

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -52,7 +52,41 @@ class SalesAnalystTest < Minitest::Test
     sales_analyst = se.analyst
 
     assert_equal 52, sales_analyst.merchants_with_high_item_count.count
-
   end
 
+  def test_sales_analyst_calculates_merchant_average_item_price
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+    })
+    sales_analyst = se.analyst
+
+    actual = sales_analyst.average_item_price_for_merchant(12334105)
+    assert_instance_of BigDecimal, actual
+    assert_equal 16.66, (actual.to_f).round(2)
+  end
+
+  def test_that_sales_analyst_average_average_price_per_merchant
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+    })
+    sales_analyst = se.analyst
+
+    actual = sales_analyst.average_average_price_per_merchant
+    assert_instance_of BigDecimal, actual
+    assert_equal 350.29, (actual.to_f).round(2)
+  end
+
+  def test_that_golden_items_returns_most_expensive_items
+    se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+    })
+    sales_analyst = se.analyst
+
+    actual = (sales_analyst.golden_items).count
+    assert_equal 5, actual
+    assert_instance_of Item, sales_analyst.golden_items[0]
+  end
 end
