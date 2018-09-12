25184138e4b284f386c1ca3e19b42dd4f042f432
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-09-12 16:55:07 -0600

    add invoice methods, add Item tests for Time

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -3,4 +3,56 @@ require_relative '../lib/invoice'
 require 'bigdecimal'
 
 class InvoiceTest < Minitest::Test
+
+    def test_it_exists
+      i = Invoice.new({
+        :id          => 6,
+        :customer_id => 7,
+        :merchant_id => 8,
+        :status      => "pending",
+        :created_at  => Time.now,
+        :updated_at  => Time.now
+      })
+      assert_instance_of Invoice, i
+    end
+
+    def test_it_has_attributes
+      i = Invoice.new({
+        :id          => 6,
+        :customer_id => 7,
+        :merchant_id => 8,
+        :status      => "pending",
+        :created_at  => Time.now,
+        :updated_at  => Time.now
+      })
+      assert_equal 6, i.id
+      assert_equal 7, i.customer_id
+      assert_equal 8, i.merchant_id
+      assert_equal "pending", i.status
+    end
+
+    def test_it_returns_created_at
+      i = Invoice.new({
+        :id          => 6,
+        :customer_id => 7,
+        :merchant_id => 8,
+        :status      => "pending",
+        :created_at  => Time.now,
+        :updated_at  => Time.now
+      })
+      assert_instance_of Time, i.created_at
+    end
+
+    def test_it_returns_updated_at
+      i = Invoice.new({
+        :id          => 6,
+        :customer_id => 7,
+        :merchant_id => 8,
+        :status      => "pending",
+        :created_at  => Time.now,
+        :updated_at  => Time.now
+      })
+      assert_instance_of Time, i.updated_at
+    end
+
 end
