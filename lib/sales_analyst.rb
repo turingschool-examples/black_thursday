ae226713e8eba7583936effac8463aee5060ef64
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-09-13 12:42:04 -0600

    working std_dev

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,3 +1,30 @@
-class SalesAnalyst
+require 'bigdecimal'
+require_relative '../lib/sales_engine'
+require_relative '../lib/black_thursday_helper'
+require_relative '../lib/sales_analyst_deviation_helper'
+require 'pry'
 
-end 
+class SalesAnalyst
+  include BlackThursdayHelper
+  include SalesAnalystHelper
+
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+  end
+
+  def average_items_per_merchant
+    total_merchants = @sales_engine.merchants.all.count
+    total_items = @sales_engine.items.all.count
+    BigDecimal((total_merchants.to_f/total_items),3)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    mean = mean_method
+    items_per_merchant = all_merchants.map do |merchant|
+      @sales_engine.items.find_all_by_merchant_id(merchant.id).size
+    end
+
+    standard_dev(items_per_merchant, mean)
+  end
+
+end
