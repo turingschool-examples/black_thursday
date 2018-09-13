8e7d56b82cdff0add267450ac734d68f5b5fcf7a
Mary Bork <maryebork@outlook.com>
2018-09-13 17:56:54 -0600

    Adds Invoice Class and Invoice Test

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,8 +1,22 @@
 require 'pry'
 
 class Invoice
+  attr_reader :id,
+              :customer_id,
+              :merchant_id,
+              :status,
+              :created_at,
+              :updated_at
+
   def initialize(sale_stats)
     @sale_stats = {}
+    @id = sale_stats[:id]
+    @customer_id = sale_stats[:customer_id]
+    @merchant_id = sale_stats[:merchant_id]
+    @status = sale_stats[:status]
+    @created_at = sale_stats[:created_at]
+    @updated_at = sale_stats[:updated_at]
   end
+
 
 end
