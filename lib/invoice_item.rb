049b8c0298204cad6ef4c11a8bb977e1a261f74a
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-09-12 21:28:12 -0600

    invoice item class finished

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,3 +1,36 @@
 class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :created_at
 
+  attr_accessor :quantity,
+                :unit_price,
+                :updated_at
+
+  def initialize(data)
+    @id = data[:id].to_i
+    @item_id = data[:item_id].to_i
+    @invoice_id = data[:invoice_id].to_i
+    @quantity = data[:quantity].to_i
+    @unit_price = data[:unit_price]
+    @created_at = data[:created_at].to_s
+    @updated_at = data[:updated_at].to_s
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
+  end
+
+  def created_at
+    if @created_at != nil
+      Time.parse(@created_at)
+    end
+  end
+
+  def updated_at
+    if @updated_at != nil
+      Time.parse(@updated_at)
+    end
+  end
 end
