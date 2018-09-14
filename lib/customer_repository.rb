748a404237a9dd52669310ae3ac26a29932bd769
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-09-14 13:18:45 -0500

    need to push up to master

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,6 +1,17 @@
 require_relative './customer'
 require_relative './repository'
 
-class CustomerRepository < Repository 
+class CustomerRepository < Repository
+
+  def initialize(filepath)
+    super()
+    load_items(filepath)
+  end
+
+  def load_items(filepath)
+    CSV.foreach(filepath, headers: true, header_converters: :symbol ) do |datum|
+      @data << Customer.new(datum)
+    end
+  end
 
 end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -4,4 +4,32 @@ require './lib/customer_repository'
 
 class CustomerRepositoryTest < Minitest::Test
 
+  def test_it_exists
+    cr = CustomerRepository.new("./data/customers.csv")
+    assert_instance_of CustomerRepository, cr
+  end
+
+  def test_it_has_items
+    cr = CustomerRepository.new("./data/customers.csv")
+    assert_equal 1000, cr.all.count
+
+    assert_equal 1, cr.all.first.id
+  end
+
+  def test_it_can_find_item_by_id
+    cr = CustomerRepository.new("./data/customers.csv")
+
+    actual = cr.find_by_id(1)
+
+    assert_instance_of Customer, actual
+    assert_equal 1, actual.id
+  end
+
+  def test_returns_nil_when_no_match_is_found
+    cr = CustomerRepository.new("./data/customers.csv")
+
+    actual = cr.find_by_id(99999)
+
+    assert_nil actual
+  end
 end
