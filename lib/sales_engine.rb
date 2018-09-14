4cc7fcbf5513c9cf2a4af5b1da302ade129a1da3
normanrs <normanrs@live.com>
2018-09-14 16:35:05 -0600

    advancement

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,7 +4,6 @@ require_relative 'item'
 require_relative 'item_repository'
 require_relative 'csv_adapter'
 require_relative 'crud'
-
 require 'csv'
 
 class SalesEngine
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -2,13 +2,16 @@ require_relative 'test_helper'
 require_relative '../lib/sales_analyst.rb'
 
 class SalesAnalystTest < Minitest::Test
+	attr_reader :sales_analyst
 
   def setup
-    @sales_engine = SalesEngine.from_csv({:merchants => "./data/merchants_tiny.csv"})
-    @sales_analyst = sales_engine.analyst
-    require "pry"; binding.pry
+    sales_engine = SalesEngine.from_csv({:merchants => "./data/merchants_tiny.csv"})
+    sales_analyst = sales_engine.analyst
+    @sales_analyst = sales_analyst
   end
 
-
+  def test_it_exists
+    assert_equal SalesAnalyst, sales_analyst
+  end
 
 end
