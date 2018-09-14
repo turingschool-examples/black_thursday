68ccbeef499090e6397ff360d5921a97520c3ccb
normanrs <normanrs@live.com>
2018-09-13 19:29:23 -0600

    working maths incl standard dev

--- lib/maths.rb
+++ lib/maths.rb
@@ -4,47 +4,44 @@ module Maths
     number ** 2
   end
 
-  def double(number)
-    number * 2
-  end
-
   def sum(number_array)
-    number_array.reduce(0.0) { |total, number| total += number}
+    summed = 0
+    number_array.each do |number|
+      summed += number
+    end
+    summed
   end
 
   def mean(number_array)
-    BigDecimal.new(((sum(number_array) / number_array.length)).round(2), 10)
-  end
-
-  def deviation(number, mean)
-    number - mean
+    BigDecimal.new((sum(number_array)) / number_array.length)
   end
 
   def percent(number, total)
-    ((number / total.to_f) * 100).round(2)
+    ((number / total) * 100)
   end
 
-  def square_deviation(number, mean)
-    square(deviation(number, mean))
-  end
-
-  def variance_numerator(number_array)
-    sum(number_array.map { |number| square(deviation(number, mean(number_array))) })
-  end
-
-  def variance(number_array)
-    (variance_numerator(number_array) / (number_array.length - 1))
-  end
-
-  def standard_deviation(number_array)
-    Math.sqrt(variance(number_array)).round(2)
-  end
-
-  def outlier?(number, mean, standard_deviation, deviations)
-    if deviations < 0
-      number < sum([mean, standard_deviation * deviations])
-    else
-      number > sum([mean, standard_deviation * deviations])
+  def subtract_mean(array)
+    avg = mean(array)
+    array.map do |number|
+      number - avg
     end
   end
+
+  def squares(number_array)
+    number_array.map do |number|
+      number ** 2
+    end
+  end
+
+  def sample(array_in)
+    sum(array_in) / (array_in.length - 1)
+  end
+
+  def standard_deviation(array_in)
+    mean_subtracted = subtract_mean(array_in)
+    meansubs_squared = squares(mean_subtracted)
+    sampled = sample(meansubs_squared)
+    Math.sqrt(sampled).round(2)
+  end
+
 end
