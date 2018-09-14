5692aaebbcd4619e6d36fdb525c13d5d3d83a196
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-09-14 10:59:53 -0600

    added transaction

---
+++ lib/transaction.rb
@@ -0,0 +1,26 @@
+require 'pry'
+require 'time'
+require_relative '../lib/black_thursday_helper'
+
+
+class Transaction
+  include BlackThursdayHelper
+
+  attr_reader :id,
+              :invoice_id,
+              :credit_card_number,
+              :credit_card_experation_date,
+              :result,
+              :created_at,
+              :updated_at
+
+  def initialize(params)
+    @id = params[:id].to_i
+    @invoice_id = params[:invoice_id]
+    @credit_card_number = params[:credit_card_number]
+    @credit_card_experation_date = params[:credit_card_experation_date]
+    @result = params[:result]
+    @created_at = Time.parse(params[:created_at].to_s)
+    @updated_at = Time.parse(params[:updated_at].to_s)
+  end
+end
