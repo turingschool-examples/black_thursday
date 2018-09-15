08f87b5810db622fd3715259b94793484fd4fe58
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-14 19:34:40 -0600

    Updates to Transaction repo & tests for SimpleCov and datatypes -- 100% coverage

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -1,11 +1,7 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-
-require 'pry'
+require_relative 'test_helper'
 
 require './lib/transaction_repository'
 require './lib/transaction'
-require './lib/finder'
 
 
 class TransactionRepositoryTest < Minitest::Test
@@ -14,27 +10,41 @@ class TransactionRepositoryTest < Minitest::Test
     # these tests might break with specharness / require relative
     @path = './data/transactions.csv'
     @repo = TransactionRepository.new(@path)
+    # CSV:
+    # id,invoice_id,credit_card_number,credit_card_expiration_date,result,created_at,updated_at
+    # 1,2179,4068631943231473,0217,success,2012-02-26 20:56:56 UTC,2012-02-26 20:56:56 UTC
+    # @key = :"1"
+    # @values = { :invoice_id                  => "2179"
+    #             :credit_card_number          => "4068631943231473"
+    #             :credit_card_expiration_date => "0217"
+    #             :result                      => "success"
+    #             :created_at                  => "2012-02-26 20:56:56 UTC"
+    #             :updated_at                  => "2012-02-26 20:56:56 UTC"
+    # }
+
   end
 
   def test_it_exists
     assert_instance_of TransactionRepository, @repo
-  end 
+  end
 
   def test_it_can_make_transactions
     trans = @repo.all.first(100)
+    assert_instance_of Array, @repo.all
+    assert_instance_of Transaction, @repo.all.last
     assert_equal 100, trans.count
     assert_equal 100, trans.uniq.count
     assert_equal 1, trans[0].id
-    assert_equal "2179", trans[0].invoice_id
+    assert_equal 2179, trans[0].invoice_id
     assert_equal "4068631943231473", trans[0].credit_card_number
     assert_equal "0217", trans[0].credit_card_expiration_date
     assert_equal "success", trans[0].result
     assert_equal "2012-02-26 20:56:56 UTC", trans[0].created_at
     assert_equal "2012-02-26 20:56:56 UTC", trans[0].updated_at
-  end 
+  end
 
   def test_it_can_find_all
-    assert_equal 9970, @repo.all.count
+    assert_equal 4985, @repo.all.count
   end
 
 end
