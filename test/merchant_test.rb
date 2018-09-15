637fa85653d3a07e4ff2c0d74e915315b314f7f7
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-14 18:12:22 -0600

    Updates to Merchant and Tests for SimpleCov & Datatypes -- 100% coverage

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -6,17 +6,18 @@ require './lib/merchant'
 class MerchantTest < Minitest::Test
 
   def setup
-    input = {:id => 5, :name => "Turing School"}
-    @m = Merchant.new(input)
+    # CSV: 12334105,Shopin1901,2010-12-10,2011-12-04
+    input = {:id => "12334105", :name => "Shopin1901"}
+    @merchant = Merchant.new(input)
   end
 
   def test_it_exists
-    assert_instance_of Merchant, @m
+    assert_instance_of Merchant, @merchant
   end
 
   def test_it_has_attributes
-    assert_equal 5, @m.id
-    assert_equal "Turing School", @m.name
+    assert_equal 12334105, @merchant.id
+    assert_equal "Shopin1901", @merchant.name
   end
 
 
