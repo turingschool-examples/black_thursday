9ac8b0a830378579924f2ea1bdfd013377f512a2
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-14 18:25:52 -0600

    Update Customer Repo & tests with Simplecov & datatypes -- 100% coverage

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -1,31 +1,29 @@
+require_relative 'test_helper'
 
-require 'minitest/autorun'
-require 'minitest/pride'
-require 'pry'
-
-require './lib/customer'
 require './lib/customer_repository'
+require './lib/customer'
 
 class CustomerRepositoryTest < Minitest::Test
 
   def setup
-    # these tests might break with specharness / require relative
     @path = './data/customers.csv'
     @repo = CustomerRepository.new(@path)
   end
 
   def test_it_exists
     assert_instance_of CustomerRepository, @repo
-  end 
+  end
 
   def test_it_gets_invoice_items
     invoice_items = @repo.all.first(100)
     assert_instance_of Array, @repo.all
+    assert_instance_of Customer, @repo.all.first
+    assert_instance_of Customer, @repo.all.last
     assert_equal 100, invoice_items.count
     assert_equal 100, invoice_items.uniq.count
   end
-  
-  
+
+
   def test_it_can_find_all
     assert_equal 1000, @repo.all.count
   end
