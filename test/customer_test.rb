a2a8a071407c8e49039eb7d8a55d7fe68adc9be1
Rene Casco <rene.casco@icloud.com>
2018-09-15 11:41:54 -0600

    added tests to customer and transaction

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -7,14 +7,30 @@ require './lib/customer'
 class CustomerTest < Minitest::Test
 
   def test_it_exits
-  c = Customer.new({
-  :id => 6,
-  :first_name => "Joan",
-  :last_name => "Clarke",
-  :created_at => Time.now,
-  :updated_at => Time.now
-  })
+    c = Customer.new({
+    :id => 6,
+    :first_name => "Joan",
+    :last_name => "Clarke",
+    :created_at => Time.now,
+    :updated_at => Time.now
+    })
 
   assert_instance_of Customer, c
+  end
+
+  def test_it_has_attributes
+    c = Customer.new({
+    :id => 6,
+    :first_name => "Joan",
+    :last_name => "Clarke",
+    :created_at => Time.now,
+    :updated_at => Time.now
+    })
+
+    assert_equal 6, c.id
+    assert_equal "Joan", c.first_name
+    assert_equal "Clarke", c.last_name
+    assert_instance_of Time, c.created_at
+    assert_instance_of Time, c.updated_at
   end
 end
