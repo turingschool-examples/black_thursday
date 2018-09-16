b59677447e4ba50f7f5a25f3f2cc501eae08a362
Mary Bork <maryebork@outlook.com>
2018-09-15 20:16:48 -0600

    Adds Transaction/Repo Class and Test

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,6 +4,8 @@ require_relative './item_repository'
 require_relative './item'
 require_relative './invoice_repository'
 require_relative './invoice'
+require_relative './transaction_repository'
+require_relative './transaction'
 require 'csv'
 require 'bigdecimal'
 require 'time'
@@ -11,23 +13,26 @@ require 'time'
 class SalesEngine
   attr_reader :merchants,
               :items,
-              :invoices
+              :invoices,
+              :transactions
 
-  def initialize(merchants_filepath, items_filepath, invoices_filepath)
+  def initialize(merchants_filepath, items_filepath, invoices_filepath, transactions_filepath)
     @merchants = MerchantRepository.new(merchants_filepath)
     @items = ItemRepository.new(items_filepath)
     @invoices = InvoiceRepository.new(invoices_filepath)
+    @transactions = TransactionRepository.new(transactions_filepath)
   end
 
   def self.from_csv(filepath_hash)
     merchants_filepath = filepath_hash[:merchants]
     items_filepath = filepath_hash[:items]
     invoices_filepath = filepath_hash[:invoices]
-    SalesEngine.new(merchants_filepath, items_filepath, invoices_filepath)
+    transactions_filepath = filepath_hash[:transactions]
+    SalesEngine.new(merchants_filepath, items_filepath, invoices_filepath, transactions_filepath)
   end
 
   def analyst
-    SalesAnalyst.new(@merchants, @items, @invoices)
+    SalesAnalyst.new(@merchants, @items, @invoices, @transactions)
   end
 
 end
---
+++ lib/transaction_repository.rb
@@ -0,0 +1,104 @@
+require 'csv'
+require 'bigdecimal'
+require 'time'
+require_relative './repository_module'
+
+class TransactionRepository
+  include RepoMethods
+
+  def initialize(filepath = nil)
+    @filepath = filepath
+    @all = []
+    split(filepath) if filepath != nil
+  end
+
+  def split(filepath)
+    objects = CSV.open(filepath, headers: true, header_converters: :symbol)
+
+    attributes_array = []
+    objects.map do |object|
+      object[:id] = object[:id].to_i
+
+      object[:invoice_id] = object[:invoice_id].to_i
+
+      object[:credit_card_number] = object[:credit_card_number].to_s
+
+      object[:result] = object[:result].to_sym
+
+      object[:created_at] = Time.parse(object[:created_at])
+
+      object[:updated_at] = Time.parse(object[:updated_at])
+      attributes_array << object.to_h
+    end
+
+    attributes_array.each do |hash|
+      create(hash)
+    end
+  end
+
+  def all
+    @all
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+      @all.find_all do |object|
+        object.invoice_id == invoice_id
+      end
+  end
+
+  def find_all_by_credit_card_number(credit_card_number)
+    @all.find_all do |object|
+      object.credit_card_number == credit_card_number
+    end
+  end
+
+  def find_all_by_result(result)
+    @all.find_all do |object|
+      object.result == result
+    end
+  end
+
+  def create(attributes)
+    is_included = @all.any? do |transaction|
+      attributes[:id] == transaction.id
+    end
+    has_id = attributes[:id] != nil
+    is_included = false if @all == []
+    if has_id && !is_included
+      @all << Transaction.new(attributes)
+    elsif @all == []
+      new_id = 1
+      attributes[:id] = new_id
+      @all << Transaction.new(attributes)
+    else
+      highest_id = @all.max_by do |transaction|
+        transaction.id
+      end.id
+      new_id = highest_id + 1
+      attributes[:id] = new_id
+      @all << Transaction.new(attributes)
+    end
+  end
+
+  def update(id, attributes)
+    return nil if attributes == {}
+    object = find_by_id(id)
+    if attributes[:credit_card_number] != nil
+      object.credit_card_number = attributes[:credit_card_number]
+    end
+    if attributes[:credit_card_expiration_date] != nil
+      object.credit_card_expiration_date = attributes[:credit_card_expiration_date]
+    end
+    if attributes[:result] != nil
+      object.result = attributes[:result]
+    end
+    if object.updated_at != nil
+      object.updated_at = Time.now
+    end
+  end
+
+  def inspect
+   "#<#{self.class} #{@transactions.size} rows>"
+  end
+
+end
