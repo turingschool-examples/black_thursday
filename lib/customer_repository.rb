a9bc8aadc1e7f3a4657617f4206bdff465d55076
Katelyn <katelyn.rimkunas@gmail.com>
2018-09-16 01:02:53 -0600

    Customer Repo - updates for Finder Methods

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,18 +1,19 @@
 require 'pry'
 
 require_relative 'csv_parse'
+require_relative 'finderclass'
+
 require_relative 'customer'
 
 class CustomerRepository
 
-  attr_reader :all,
-              :customers
+  attr_reader :all
 
   def initialize(path)
     @csv = CSVParse.create_repo(path)
     @customers = []
     make_customers
-    @all = customers # retains permissions
+    @all = @customers
   end
 
   def make_customers
@@ -29,7 +30,19 @@ class CustomerRepository
     return hash
   end
 
-  
 
+  # --- Find By ---
+
+  def find_by_id(id)
+    FinderClass.find_by(all, :id, id)
+  end
+
+  def find_all_by_first_name(name)
+    FinderClass.find_by_fragment(all, :first_name, name)
+  end
+
+  def find_all_by_last_name(name)
+    FinderClass.find_by_fragment(all, :last_name, name)
+  end
 
 end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -1,30 +1,75 @@
 require_relative 'test_helper'
 
-require './lib/customer_repository'
-require './lib/customer'
+require_relative '../lib/customer_repository'
+require_relative '../lib/customer'
 
 class CustomerRepositoryTest < Minitest::Test
 
   def setup
-    @path = './data/customers.csv'
-    @repo = CustomerRepository.new(@path)
+    path = './data/customers.csv'
+    @repo = CustomerRepository.new(path)
+    # 1,Joey,Ondricka,2012-03-27 14:54:09 UTC,2012-03-27 14:54:09 UTC
+    customer_1_hash = { :"1" => { first_name: "Joey",
+                                   last_name:  "Ondricka",
+                                   created_at: "2012-03-27 14:54:09 UTC",
+                                   updated_at: "2012-03-27 14:54:09 UTC"
+                                  } }
+    @key = customer_1_hash.keys.first
+    @values = customer_1_hash.values.first
   end
 
   def test_it_exists
     assert_instance_of CustomerRepository, @repo
   end
 
-  def test_it_gets_invoice_items
-    invoice_items = @repo.all.first(100)
+  def test_it_gets_attributes
+    # --- Read Only ---
     assert_instance_of Array, @repo.all
-    assert_instance_of Customer, @repo.all.first
-    assert_instance_of Customer, @repo.all.last
-    assert_equal 100, invoice_items.count
-    assert_equal 100, invoice_items.uniq.count
-  end
-
-
-  def test_it_can_find_all
+    assert_instance_of Customer, @repo.all[0]
+    assert_equal @repo.all.count, @repo.all.uniq.count
     assert_equal 1000, @repo.all.count
   end
+
+  def test_it_makes_customers
+    # This test is skipped because it will affect other tests.
+    skip
+    @repo.make_customers
+    assert_equal 1000 * 2, @repo.all.count
+  end
+
+  def test_it_makes_a_formatted_hash
+    # -- The new hash needs an additional column --
+    new_column = {:id => 1}
+    new_hash = new_column.merge(@values.dup)
+    assert_equal new_hash, @repo.make_hash(@key, @values)
+  end
+
+
+  # --- Find By ---
+
+  def test_it_can_find_a_customer_by_id
+    assert_nil @repo.find_by_id(000)
+    found = @repo.find_by_id(1)
+    assert_instance_of Customer, found
+    assert_equal 1, found.id
+  end
+
+  def test_it_can_find_all_customers_by_first_name_fragment
+    assert_equal [], @repo.find_all_by_first_name("zzz")
+    found = @repo.find_all_by_first_name("Joey")
+    assert_instance_of Array, found
+    assert_instance_of Customer, found.first
+    assert_equal "Joey", found.first.first_name
+  end
+
+  def test_it_can_find_all_customers_by_last_name_fragment
+    assert_equal [], @repo.find_all_by_last_name("zzz")
+    found = @repo.find_all_by_last_name("Ondricka")
+    assert_instance_of Array, found
+    assert_instance_of Customer, found.first
+    assert_equal "Ondricka", found.first.last_name
+  end
+
+
+
 end
