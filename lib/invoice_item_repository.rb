5b3f491ddfa6edc68c8db27f7f5270aaed9ce4ab
Aizar <aizar.cabrera@icloud.com>
2018-09-18 16:09:20 -0600

    starts iteration 3

---
+++ lib/invoice_item_repository.rb
@@ -0,0 +1,16 @@
+require './test/helper'
+
+class InvoiceItemRepository
+attr_reader :invoice_items
+
+  def initialize(invoice_items_path)
+    @invoice_items = []
+    make_invoice_items(invoice_items_path)
+  end
+
+  def make_invoice_items(invoice_items_path)
+    CSV.foreach(invoice_items_path, headers: true, header_converters: :symbol) do |row|
+      @invoice_items << InvoiceItem.new(row)
+    end
+  end
+end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,12 +3,14 @@ require'./test/helper'
 class SalesEngine
   attr_reader :merchants,
               :items,
-              :invoices
+              :invoices,
+              :invoice_items
 
   def initialize(csv_hash)
-    @merchants = MerchantRepository.new(csv_hash[:merchants])
-    @items     = ItemRepository.new(csv_hash[:items])
-    @invoices  = InvoiceRepository.new(csv_hash[:invoices])
+    @merchants     = MerchantRepository.new(csv_hash[:merchants])
+    @items         = ItemRepository.new(csv_hash[:items])
+    @invoices      = InvoiceRepository.new(csv_hash[:invoices])
+    @invoice_items = InvoiceItemRepository.new(csv_hash[:invoice_items])
   end
 
   def self.from_csv(csv_hash)
