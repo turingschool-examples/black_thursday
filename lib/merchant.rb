a688a4c014eb24423ce9855d77eeddbc29c70e3c
Meghan Stang <mstang15@gmail.com>
2018-09-18 17:32:00 -0600

    finally got total revenue working

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,9 +1,11 @@
 class Merchant
-  attr_reader :id
+  attr_reader :id,
+              :created_at
   attr_accessor :name
 
   def initialize(hash)
     @id = hash[:id].to_i
     @name = hash[:name]
+    @created_at = Time.parse(hash[:created_at].to_s)
   end
 end
