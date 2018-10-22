77b26943a866d4c148149f7d7ac53ff4eee1eca2
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-22 11:40:26 -0600

    Add: Test_helper, .gitignore

---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'simplecov'
+SimpleCov.start
+# Include future files that are required underneath SimpleCov, so it has started and they are tabulated in the coverage.
