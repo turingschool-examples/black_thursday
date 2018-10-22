be318d05f2250e9316b22c897fc4d5dacccce227
jpclark6 <jpclark6@gmail.com>
2018-10-22 14:58:22 -0600

    Writes sales engine test

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -6,4 +6,13 @@ class SalesEngineTest < Minitest::Test
     se = SalesEngine.new
     assert_instance_of SalesEngine, se
   end
+
+  def test_it_can_load_csv_files_items_and_merchants
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+    assert_instance_of MerchantRepository, se.merchants
+    assert_instance_of ItemRepository, se.items
+  end
 end
