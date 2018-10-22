fa1adacbe91e74a4453b851b2435b7bae5b0076e
Anten Skrabec <cebarks@gmail.com>
2018-10-22 16:29:16 -0600

    remove trailing whitespace

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,5 +1,4 @@
 require './test/test_helper'
-
 require './lib/item'
 
 class ItemTest < Minitest::Test
@@ -15,39 +14,39 @@ class ItemTest < Minitest::Test
       :updated_at  => @current_time,
       :merchant_id => 2})
   end
-  
+
   def test_it_exists
     assert_instance_of Item, @item
   end
-  
+
   def test_it_has_an_id
     assert_equal 1, @item.id
   end
-  
+
   def test_it_has_a_name
     assert_equal "Pencil", @item.name
   end
-  
+
   def test_it_has_a_description
     assert_equal "You can use it to write things", @item.description
   end
-  
+
   def test_it_has_a_unit_price
     assert_equal BigDecimal.new(10.99,4), @item.unit_price
   end
-  
+
   def test_it_has_a_created_at_time
     assert_equal @current_time, @item.created_at
   end
-  
+
   def test_it_has_a_updated_at_time
     assert_equal @current_time, @item.updated_at
   end
-  
+
   def test_it_has_a_merchant_id
     assert_equal 2, @item.merchant_id
   end
-  
+
   def test_item_can_convert_units_to_dollars
     assert_equal "$3.00", Item.unit_price_to_dollars(3)
   end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,20 +1,19 @@
 require './test/test_helper'
-
 require './lib/merchant'
 
 class MerchantTest < Minitest::Test
   def setup
     @merchant = Merchant.new({:id => 5, :name => "Turing School"})
   end
-  
+
   def test_it_exists
     assert_instance_of Merchant, @merchant
   end
-  
+
   def test_it_has_an_id
     assert_equal 5, @merchant.id
   end
-  
+
   def test_it_has_a_name
     assert_equal "Turing School", @merchant.name
   end
