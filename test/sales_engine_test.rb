d025e11485a344501ce301a12d07c2568db62975
vjp888 <rellik888@gmail.com>
2018-10-22 15:25:10 -0600

    Creates more tests for sales engine.

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -8,9 +8,16 @@ class SalesEngineTest < Minitest::Test
   def test_it_exists
     sales = SalesEngine.new
 
-    assert_instance_of SalesEngine, sales    
+    assert_instance_of SalesEngine, sales
+  end
+
+  def test_sales_can_make_merchant_repo_instance
 
   end
 
+  def test_sales_can_make_item_repo_instance
 
+  end
+
+  
 end
