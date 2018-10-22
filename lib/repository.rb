a3e856308c3950f64b4bc3c514b279c5bd3629ac
Ben Lee <bendelonlee@gmail.com>
2018-10-22 16:20:09 -0600

    removes extra and incorrect lines from Repository class

--- lib/repository.rb
+++ lib/repository.rb
@@ -1,5 +1,5 @@
 class Repository
-  attr_reader
+
   def initialize
     @instances = []
   end
@@ -8,23 +8,8 @@ class Repository
     @instances
   end
 
-
-  class MerchantRepository
-    attr_reader
-    def initialize :all
-      @instances = []
-      @merchants = []
-    end
-
-    def all
-      @instances
-    end
-
-    def find_by_id(id)
-      @instances.find{|instance| instance.id == id }
-    end
+  def find_by_id(id)
+    @instances.find{|instance| instance.id == id }
   end
-
-
 
 end
