2891f530252b6b55ea9e59d2316f31f12181ee72
Anten Skrabec <cebarks@gmail.com>
2018-10-22 16:25:51 -0600

    fix typos

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,19 +5,19 @@ require './lib/merchant'
 class SalesEngineTest
   def setup
     @se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv"})
+      items: './data/items.csv',
+      merchants: './data/merchants.csv'})
   end
-  
+
   def test_it_exists
     assert_instance_of SalesEngine, @se
   end
-  
+
   def test_it_holds_an_item_repository
     assert_instance_of ItemRepository, @se.items
   end
-  
-  def test_it_holds_an_item_repository
+
+  def test_it_holds_an_merchant_repository
     assert_instance_of MerchantRepository, @se.merchants
   end
 end
