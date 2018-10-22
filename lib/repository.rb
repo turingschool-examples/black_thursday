5f6029596c988202fe22205b310161dc9fbf8d82
Ben Lee <bendelonlee@gmail.com>
2018-10-22 17:25:32 -0600

    Adds repository methods

--- lib/repository.rb
+++ lib/repository.rb
@@ -12,4 +12,16 @@ class Repository
     @instances.find{|instance| instance.id == id }
   end
 
+  def find_by_name(name)
+    @instances.find{|instance| instance.name == name }
+  end
+
+  def find_all_by_name(name)
+    @instances.find_all{|instance| instance.name == name }
+  end
+
+  def create(object)
+    @instances << object
+  end
+
 end
