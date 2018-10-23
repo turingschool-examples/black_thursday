c62a837a4cb298cf30bc78a7c01e2b0bcf818ef5
jpclark6 <jpclark6@gmail.com>
2018-10-23 15:45:55 -0600

    Updates tests to have setup defs

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -3,33 +3,27 @@ require './lib/merchant_repository'
 require './lib/merchant'
 
 class MerchantRepositoryTest < Minitest::Test
+  def setup
+    @mr = MerchantRepository.new
+    @merchant_1 = Merchant.new({:id => 12334123, :name => "Keckenbauer"})
+    @merchant_2 = Merchant.new({:id => 12334145, :name => "BowlsByChris"})
+    @merchant_3 = Merchant.new({:id => 12334159, :name => "SassyStrangeArt"})
+    @mr.add_merchant(@merchant_1)
+    @mr.add_merchant(@merchant_2)
+    @mr.add_merchant(@merchant_3)
+  end
+
   def test_it_exists
-    mr = MerchantRepository.new
-    assert_instance_of MerchantRepository, mr
+    assert_instance_of MerchantRepository, @mr
   end
 
   def test_it_can_store_merchants
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new({:id => 12334123, :name => "Keckenbauer"})
-    merchant_2 = Merchant.new({:id => 12334145, :name => "BowlsByChris"})
-    merchant_3 = Merchant.new({:id => 12334159, :name => "SassyStrangeArt"})
-    mr.add_merchant(merchant_1)
-    mr.add_merchant(merchant_2)
-    mr.add_merchant(merchant_3)
-    expected = [merchant_1, merchant_2, merchant_3]
-    assert_equal expected, mr.all
+    expected = [@merchant_1, @merchant_2, @merchant_3]
+    assert_equal expected, @mr.all
   end
 
   def test_it_can_find_merchant_by_id
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new({:id => 12334123, :name => "Keckenbauer"})
-    merchant_2 = Merchant.new({:id => 12334145, :name => "BowlsByChris"})
-    merchant_3 = Merchant.new({:id => 12334159, :name => "SassyStrangeArt"})
-    mr.add_merchant(merchant_1)
-    mr.add_merchant(merchant_2)
-    mr.add_merchant(merchant_3)
-    assert_equal merchant_2, mr.find_by_id(12334145)
+    assert_equal @merchant_2, @mr.find_by_id(12334145)
   end
-
 
 end
