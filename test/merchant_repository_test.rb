acec95930885ecafb22689c28e65640400237bd5
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-23 17:25:32 -0600

    add first test for MerchantRepo

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -3,6 +3,7 @@ require_relative '../lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
   def test_it_exists
-    
+    mr = MerchantRepository.new('./data/merchant_mini.csv')
+    assert_instance_of MerchantRepository, mr
   end
 end
