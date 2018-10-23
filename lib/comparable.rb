ec1ef866c3cbd5363cc5409266707eaf335ce87b
Ben Lee <bendelonlee@gmail.com>
2018-10-23 13:45:57 -0600

    Adds Comparable module. Passes tests

--- lib/comparable.rb
+++ lib/comparable.rb
@@ -1,1 +1,11 @@
-Module
+require 'pry'
+module Comparable
+  def ==(other)
+    return false unless self.class == other.class
+    self.instance_variables.each do |var|
+      var = var.to_s[1..-1]
+      return false unless self.send(var) == other.send(var)
+    end
+  end
+  true
+end
