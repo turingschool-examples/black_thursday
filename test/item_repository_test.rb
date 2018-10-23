b5533463ef24f6ddb44b509e92687d5f6471c5b1
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-22 18:34:50 -0600

    Adds testing for creating item instances

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,11 +1,15 @@
 require './lib/item_repository'
-require './lib/merchant'
+require './lib/item'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class ItemRepositoryTest < Minitest::Test
   def setup
     @ir = ItemRepository.new
+
+    @merchant_1 = Item.new(id: 1, name: "Turing School")
+    @merchant_2 = Item.new(id: 2, name: "Coding Dojo")
+    @merchant_3 = Item.new(id: 3, name: "General Assembly")
   end
 
   def test_it_exists
@@ -15,10 +19,12 @@ class ItemRepositoryTest < Minitest::Test
   def test_it_creates_a_merchant_instance
     assert_empty @ir.instances
 
-    
-    # Merchant.new(id: 1, name: "Turing School")
-    # @merchant_2 = Merchant.new(id: 2, name: "Coding Dojo")
-    # @merchant_3 = Merchant.new(id: 3, name: "General Assembly")
+    @ir.create(id: 1, name: "Turing School")
+    @ir.create(id: 2, name: "Coding Dojo")
+    @ir.create(id: 3, name: "General Assembly")
+
+    assert_equal 3, @ir.instances.count
+    assert diff [@merchant_1, @merchant_2, @merchant_3], @ir.instances
   end
 
   def test_find_all_with_description_returns_empty_array_if_none_found
