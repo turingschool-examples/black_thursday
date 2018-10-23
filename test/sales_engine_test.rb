20b7f764b3870898aed11b14028422238c700869
jpclark6 <jpclark6@gmail.com>
2018-10-23 13:31:30 -0600

    Adds test for loading merchants correctly

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,6 +2,7 @@ require './test/test_helper'
 require './lib/sales_engine'
 require './lib/item_repository'
 require './lib/merchant_repository'
+require './lib/merchant'
 
 class SalesEngineTest < Minitest::Test
   def test_it_exists
@@ -20,5 +21,18 @@ class SalesEngineTest < Minitest::Test
     assert_instance_of ItemRepository, se.items
   end
 
-  
+  def test_it_can_load_merchants_correctly
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+    merchant_1 = Merchant.new({id: 12334105, name: "Shopin1901"})
+    merchant_2 = Merchant.new({id: 12334112, name: "Candisart"})
+    assert_equal merchant_1.id, se.merchants[0].id
+    assert_equal merchant_1.name, se.merchants[0].name
+    assert_equal merchant_2.id, se.merchants[1].id
+    assert_equal merchant_2.name, se.merchants[1].name
+  end
+
+
 end
