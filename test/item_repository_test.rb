4c174774110c60050ae98b76a53eabc65860d35d
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-22 18:42:52 -0600

    Adds refactor for setup and testing create

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -2,14 +2,41 @@ require './lib/item_repository'
 require './lib/item'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'bigdecimal'
 
 class ItemRepositoryTest < Minitest::Test
   def setup
     @ir = ItemRepository.new
 
-    @merchant_1 = Item.new(id: 1, name: "Turing School")
-    @merchant_2 = Item.new(id: 2, name: "Coding Dojo")
-    @merchant_3 = Item.new(id: 3, name: "General Assembly")
+    @item_1 = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
+
+    @item_2 = Item.new({
+      :id          => 2,
+      :name        => "Penguin",
+      :description => "You can use it to sled",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
+
+    @item_3 = Item.new({
+      :id          => 3,
+      :name        => "Pajamas",
+      :description => "You can use it to wear around the house",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
   end
 
   def test_it_exists
@@ -19,12 +46,38 @@ class ItemRepositoryTest < Minitest::Test
   def test_it_creates_a_merchant_instance
     assert_empty @ir.instances
 
-    @ir.create(id: 1, name: "Turing School")
-    @ir.create(id: 2, name: "Coding Dojo")
-    @ir.create(id: 3, name: "General Assembly")
+    @ir.create({
+      :id          => 3,
+      :name        => "Pajamas",
+      :description => "You can use it to wear around the house",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
+
+    @ir.create({
+      :id          => 2,
+      :name        => "Penguin",
+      :description => "You can use it to sled",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
+
+    @ir.create({
+      :id          => 3,
+      :name        => "Pajamas",
+      :description => "You can use it to wear around the house",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
 
     assert_equal 3, @ir.instances.count
-    assert diff [@merchant_1, @merchant_2, @merchant_3], @ir.instances
+    assert diff [@item_1, @item_2, @item_3], @ir.instances
   end
 
   def test_find_all_with_description_returns_empty_array_if_none_found
