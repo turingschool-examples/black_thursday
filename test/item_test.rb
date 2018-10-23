2032c0009dafc7b43bc0219f491f8595e92e4d8c
William Petty <william@snakeweed.com>
2018-10-23 13:47:29 -0600

    Class Item: tests for created_at, updated_at, merchant_id

--- test/item_test.rb
+++ test/item_test.rb
@@ -3,13 +3,14 @@ require './test/test_helper'
 class ItemTest < Minitest::Test
 
   def setup
+    @t = Time.now
     @i = Item.new({
 	  :id          => 1,
 	  :name        => "Pencil",
 	  :description => "You can use it to write things",
 	  :unit_price  => BigDecimal.new(10.99,4),
-	  :created_at  => Time.now,
-	  :updated_at  => Time.now,
+	  :created_at  => @t,
+	  :updated_at  => @t,
 	  :merchant_id => 2
 	})
   end
@@ -23,18 +24,34 @@ class ItemTest < Minitest::Test
   end
 
   def test_it_has_name
+    skip
     assert_equal "Pencil", @i.name
   end
 
   def test_it_has_description
+    skip
     assert_equal "You can use it to write things", @i.description
   end
 
   def test_it_has_unit_price
+    skip
     assert_equal BigDecimal.new(10.99,4), @i.unit_price
   end
 
+  def test_it_has_created_at
+    skip
+    p "Verify current time of #{@t}"
+    assert_equal @t, @i.created_at
+  end
 
+  def test_it_has_updated_at
+    skip
+    assert_equal @t, @i.updated_at
+  end
 
+  def test_it_has_merchant_id
+    skip
+    assert_equal 2, @i.merchant_id
+  end
 
 end
