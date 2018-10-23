c5c71d2dbc8dfd3cb37b665f89acca0ee3f8f46c
Ben Lee <bendelonlee@gmail.com>
2018-10-22 20:09:57 -0600

    Adds test for update method in repository

--- test/repository_test.rb
+++ test/repository_test.rb
@@ -86,15 +86,12 @@ class RepositoryTest < Minitest::Test
     assert_equal expected, @rep.all
   end
 
-  
+  def test_update
+    @rep.create(@ob3)
+    @rep.create(@ob4)
+    @rep.create(@ob5)
+    @rep.update(5, {name: "Alf"})
+    assert_equal "Alf", @rep.find_by_id(5).name
+  end
 
 end
-
-
-# - all - returns an array of all known Merchant instances
-# - find_by_id(id) - returns either nil or an instance of Merchant with a matching ID
-# - find_by_name(name) - returns either nil or an instance of Merchant having done a case insensitive search
-#
-# - find_all_by_name(name) - returns either [] or one or more matches which contain the supplied name fragment, case insensitive
-# - create(attributes) - create a new Merchant instance with the provided attributes. The new Merchant’s id should be the current highest Merchant id plus 1.
-# - delete(id) - delete the Merchant instance with the corresponding id
