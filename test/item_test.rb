1fe4b490c4f1c7ebd0e4ad8da91c3730dd906582
Leah Miller <lkmiller.87@gmail.com>
2018-10-23 15:42:41 -0600

    Add setup value in ItemTest

--- test/item_test.rb
+++ test/item_test.rb
@@ -3,17 +3,21 @@ require_relative '../lib/item'
 
 class ItemTest < Minitest::Test
 
-def test_item_exists
-  i = Item.new({
-  :id          => 1,
-  :name        => "Pencil",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 2
-})
-  assert_instance_of Item, i 
-end
+  def setup
+    @i = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+      })
+  end
+
+  def test_item_exists
+    assert_instance_of Item, @i
+  end
+
 
 end
