3b916669c07c605d5db6ab81b8560ae4ebbbc956
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-23 10:10:15 -0600

    Adds find_all_by_price_range

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -13,4 +13,10 @@ class ItemRepository < Repository
   def find_all_by_price(price)
     @instances.find_all {|item| item.unit_price == price}
   end
+
+  def find_all_by_price_range(start_number, end_number)
+    @instances.find_all do |item|
+      item.unit_price >= start_number || item.unit_price <= end_number
+    end
+  end
 end
