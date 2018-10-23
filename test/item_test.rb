54a499b250145e444557c7b1016a0cd6974a8324
William Petty <william@snakeweed.com>
2018-10-23 12:48:56 -0600

    Initial commit for item_test

---
+++ test/item_test.rb
@@ -0,0 +1,19 @@
+require "./test/test_helper"
+
+class ItemTest < Minitest::Test
+
+  def setup
+    i = Item.new({
+	  :id          => 1,
+	  :name        => "Pencil",
+	  :description => "You can use it to write things",
+	  :unit_price  => BigDecimal.new(10.99,4),
+	  :created_at  => Time.now,
+	  :updated_at  => Time.now,
+	  :merchant_id => 2
+	})
+  end
+
+  def test_it_exists
+    assert_instance_of Item, i
+  end
