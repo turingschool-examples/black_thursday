619b28670798432f50b43eebd6233d31f5204bac
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-22 18:55:58 -0600

    Adds find_all_with_description

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -5,4 +5,8 @@ class ItemRepository < Repository
   def create(args)
     super(Item.new(args))
   end
+
+  def find_all_with_description(description)
+    @instances.find_all {|item| item.description == description}
+  end
 end
