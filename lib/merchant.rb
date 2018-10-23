a17a83d778ccf5202643e3e1759a3db7247edfc6
Justin Mauldin <justin.mauldin7@gmail.com>
2018-10-23 14:55:21 -0600

    Gets merchant class built & tests passing.

---
+++ lib/merchant.rb
@@ -0,0 +1,12 @@
+class Merchant
+  attr_reader :id, :name, :created_at, :updated_at
+
+  def initialize(merchant_hash)
+    @id = merchant_hash[:id]
+    @name = merchant_hash[:name]
+    @created_at = merchant_hash[:created_at]
+    @updated_at = merchant_hash[:updated_at]
+  end
+
+
+end
