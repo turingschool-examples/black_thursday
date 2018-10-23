2bfee7f4a2cfb6054a00d190cae81b740b4fd636
vjp888 <rellik888@gmail.com>
2018-10-23 15:25:42 -0600

    Creates tests for merchant class

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -6,9 +6,19 @@ require './lib/merchant'
 class MerchantTest < MiniTest::Test
 
   def test_it_exists
+    mer = Merchant.new({:id => 5, :name => "Turing School"})
 
+    assert_instance_of Merchant, mer
   end
 
+  def test_it_has_id
+    mer = Merchant.new({:id => 5, :name => "Turing School"})
 
+    assert_equal 5, mer.id
+  end
 
+  def test_it_has_a_name
+    mer = Merchant.new({:id => 5, :name => "Turing School"})
+    assert_equal "Turing School", mer.name
+  end
 end
