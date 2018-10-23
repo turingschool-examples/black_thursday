7d89c5a2052746283e2041a64712635a5f6514c2
William Petty <william@snakeweed.com>
2018-10-23 08:58:29 -0600

    Final commits from group session

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,8 +1,9 @@
 class Merchant
-  attr_reader :id
+  attr_reader :id, :name
 
-def initialize(merchant_data)
-  @id = merchant_data[:id]
-end
+  def initialize(merchant_data)
+    @id = merchant_data[:id]
+    @name = merchant_data[:name]
+  end
 
 end
