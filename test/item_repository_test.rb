3950094e1082102de431e452f07c98aaa34bbe51
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-22 18:52:29 -0600

    Adds testing for find_all_with_description

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -37,6 +37,16 @@ class ItemRepositoryTest < Minitest::Test
       :updated_at  => Time.now,
       :merchant_id => 2
     }
+
+    @item_4 = {
+      :id          => 4,
+      :name        => "Red Pajamas",
+      :description => "You can use it to wear around the house",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    }
   end
 
   def test_it_exists
@@ -55,10 +65,22 @@ class ItemRepositoryTest < Minitest::Test
   end
 
   def test_find_all_with_description_returns_empty_array_if_none_found
-    skip
+    @ir.create(@item_1)
+    @ir.create(@item_2)
+    @ir.create(@item_3)
+    @ir.create(@item_4)
+
+    assert_empty
+    @ir.find_all_with_description("You can use it to eliminate rodents")
   end
 
   def test_find_all_with_description_returns_matching_merchants
-    skip
+    @ir.create(@item_1)
+    @ir.create(@item_2)
+    @ir.create(@item_3)
+    @ir.create(@item_4)
+
+    assert diff [Item.new(@item_3), Item.new(@item_4)],
+    @ir.find_all_with_description("You can use it to wear around the house")
   end
 end
