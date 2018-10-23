ea1e1aae456db1b86eed87ff5e159b5b6256296f
Brad Niedt <bradleyniedt@gmail.com>
2018-10-23 15:21:24 -0600

    adds name test and passing implementation code

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -9,6 +9,12 @@ class MerchantTest < Minitest::Test
 
   def test_it_has_id
     m = Merchant.new({:id => 5, :name => "Turing School"})
-    assert_equal 5, m.id 
+    assert_equal 5, m.id
   end
+
+  def test_it_has_name
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+    assert_equal "Turing School", m.name  
+  end
+
 end
