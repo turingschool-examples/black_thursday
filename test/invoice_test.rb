3edc7d4bd274e4a765bf69fda5a54ddefb1a606c
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-24 15:04:47 -0600

    Adds testing for instance variables

---
+++ test/invoice_test.rb
@@ -0,0 +1,46 @@
+require './lib/invoice'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'bigdecimal'
+
+class InvoiceTest < Minitest::Test
+  def setup
+    @now = Time.now
+    @invoice = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => @now,
+      :updated_at  => @now,
+    })
+  end
+
+  def test_it_exists
+    assert_instance_of Invoice, @invoice
+  end
+
+  def test_it_has_an_id
+    assert_equal 6, @invoice.id
+  end
+
+  def test_it_has_a_customer_id
+    assert_equal 7, @invoice.customer_id
+  end
+
+  def test_it_has_a_merchant_id
+    assert_equal 8, @invoice.merchant_id
+  end
+
+  def test_it_has_a_status
+    assert_equal "pending", @invoice.status
+  end
+
+  def test_it_has_a_created_at_time
+    assert_equal @now, @invoice.created_at
+  end
+
+  def test_it_has_an_updated_at_time
+    assert_equal @now, @invoice.updated_at
+  end
+end
