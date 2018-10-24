8c4959da494660f5ee070b27cdb3b426b64efbf5
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-24 16:30:21 -0600

    Adds test for finding by status

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -8,7 +8,7 @@ class InvoiceRepositoryTest < Minitest::Test
     @now = Time.now
     @ir = InvoiceRepository.new
 
-    @item_1 = {
+    @invoice_1 = {
       :id          => 1,
       :customer_id => 3,
       :merchant_id => 8,
@@ -17,7 +17,7 @@ class InvoiceRepositoryTest < Minitest::Test
       :updated_at  => @now,
     }
 
-    @item_2 = {
+    @invoice_2 = {
       :id          => 2,
       :customer_id => 6,
       :merchant_id => 8,
@@ -26,16 +26,16 @@ class InvoiceRepositoryTest < Minitest::Test
       :updated_at  => @now,
     }
 
-    @item_3 = {
+    @invoice_3 = {
       :id          => 3,
       :customer_id => 7,
       :merchant_id => 8,
-      :status      => "returned",
+      :status      => "shipped",
       :created_at  => @now,
       :updated_at  => @now,
     }
 
-    @item_4 = {
+    @invoice_4 = {
       :id          => 4,
       :customer_id => 7,
       :merchant_id => 8,
@@ -46,10 +46,10 @@ class InvoiceRepositoryTest < Minitest::Test
   end
 
   def create_invoices
-    @ir.create(@item_1)
-    @ir.create(@item_2)
-    @ir.create(@item_3)
-    @ir.create(@item_4)
+    @ir.create(@invoice_1)
+    @ir.create(@invoice_2)
+    @ir.create(@invoice_3)
+    @ir.create(@invoice_4)
   end
 
   def test_it_exists
@@ -61,7 +61,7 @@ class InvoiceRepositoryTest < Minitest::Test
 
     create_invoices
 
-    expected = [Invoice.new(@item_1), Invoice.new(@item_2), Invoice.new(@item_3), Invoice.new(@item_4)]
+    expected = [Invoice.new(@invoice_1), Invoice.new(@invoice_2), Invoice.new(@item_3), Invoice.new(@item_4)]
     result = @ir.instances
 
     result.each_with_index {|invoice, index| assert_equal invoice, expected[index]}
@@ -77,8 +77,23 @@ class InvoiceRepositoryTest < Minitest::Test
   def test_find_all_by_customer_id_returns_matches
     create_invoices
 
-    expected = [Invoice.new(@item_3), Invoice.new(@item_4)]
+    expected = [Invoice.new(@invoice_3), Invoice.new(@invoice_4)]
     result = @ir.find_all_by_customer_id(9)
+
+    result.each_with_index {|invoice, index| assert_equal invoice, expected[index]}
+  end
+
+  def test_find_all_by_status_returns_empty_array_if_no_matches
+    create_invoices
+
+    assert_equal [], @ir.find_all_by_status(:returned)
+  end
+
+  def test_find_all_by_status_returns_matches
+    create_invoices
+
+    expected = [Invoice.new(@invoice_1), Invoice.new(@invoice_2)]
+    result = @ir.find_all_by_status(:shipped)
 
     result.each_with_index {|invoice, index| assert_equal invoice, expected[index]}
   end
