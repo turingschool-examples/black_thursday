75c4b1723fd265802dafba7b88bc928420536b81
William Petty <william@snakeweed.com>
2018-10-24 11:59:14 -0600

    Unification of sales engine with item repo

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,14 +1,14 @@
 class SalesEngine
-  attr_reader :merchants
+  attr_reader :merchants, :items
 
   def initialize(merch_repo, item_repo)
     @merchants = merch_repo
-    #@items = item_repo
+    @items = item_repo
   end
 
     def self.from_csv(csv_data_paths)
       merch_repo = MerchantRepository.new(csv_data_paths[:merchants])
-      item_repo = "ItemRepo"#ItemRepository.new
+      item_repo = ItemRepository.new(csv_data_paths[:items])
       self.new(merch_repo, item_repo)
     end
 
