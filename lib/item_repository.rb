8e9d72377fa1e22f27794de121c36725a3e91090
vjp888 <rellik888@gmail.com>
2018-10-23 18:46:30 -0600

    adds functionality to item repo class and made small test adjustments to better cover possible issues

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -8,4 +8,34 @@
      @items
    end
 
+   def find_by_id(id)
+    all.find do |item|
+      item.id == id
+    end
+   end
+
+   def find_by_name(name)
+     @items.find do |item|
+       item.name.upcase == name.upcase
+     end
+   end
+
+   def find_all_with_description(desc)
+     @items.find_all do |item|
+       item.description.upcase.include?(desc.upcase)
+     end
+   end
+
+   def find_all_by_price(price)
+     price = BigDecimal.new(price,4)
+     all.find_all do |item|
+       item.unit_price == price
+     end
+   end
+
+   def find_all_by_price_in_range(range)
+     all.find_all do |item|
+       range.member?(item.unit_price)
+     end
+   end
  end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -36,36 +36,32 @@ class ItemRepositoryTest < Minitest::Test
   end
 
   def test_it_can_find_by_id
-    skip
     assert_equal @item_1, @ir.find_by_id(1)
     assert_nil @ir.find_by_id(10)
   end
 
   def test_it_can_find_by_name
-    skip
     assert_equal @item_2, @ir.find_by_name("PeN")
     assert_equal @item_2, @ir.find_by_name("pen")
     assert_nil  @ir.find_by_name("steve")
   end
 
   def test_it_can_find_all_with_a_description
-    skip
     assert_equal [], @ir.find_all_with_description("find me")
     assert_equal [@item_2], @ir.find_all_with_description("ink")
     assert_equal [@item_1, @item_2], @ir.find_all_with_description("wrIte thIngS")
   end
 
   def test_can_find_all_items_with_same_price
-    skip
     assert_equal [], @ir.find_all_by_price(50)
     assert_equal [@item_1], @ir.find_all_by_price(10.99)
   end
 
   def test_it_can_find_items_within_range_of_price
-    skip
     assert_equal [], @ir.find_all_by_price_in_range((100..102))
     assert_equal [@item_1, @item_2], @ir.find_all_by_price_in_range((0..40))
     assert_equal [@item_1], @ir.find_all_by_price_in_range((0..13))
+    assert_equal [@item_1], @ir.find_all_by_price_in_range((0.0..13.1))
   end
 
   def test_it_can_find_all_items_with_merchant_id
