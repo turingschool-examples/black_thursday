3a4a11adb9c673e8a8b53bbc701f639fca6726e5
Brad Niedt <bradleyniedt@gmail.com>
2018-10-23 19:30:24 -0600

    adds test to confirm creation of items and adds pry to helper

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -4,5 +4,18 @@ class ItemRepository
     @collection = []
   end
 
-  
+  def items
+    @item_data.each do |row|
+      @collection << Item.new({id: "#{row[:id]}",
+                              name: "#{row[:name]}",
+                              description: "#{row[:description]}",
+                              unit_price: "#{row[:unit_price]}",
+                              merchant_id: "#{row[:merchant_id]}",
+                              created_at: "#{row[:created_at]}",
+                              updated_at: "#{row[:updated_at]}"})
+    end
+    @collection
+  end
+
+
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,16 +1,14 @@
 require_relative './helper'
 require_relative '../lib/item_repository'
 require_relative '../lib/item'
-
 class ItemRepositoryTest < Minitest::Test
   def test_it_exists
-    ir = ItemRepository.new('./data/items_mini.csv')
+    ir = ItemRepository.new('./data/items.csv')
     assert_instance_of ItemRepository, ir
   end
 
   def test_it_can_create_items
-    ir = ItemRepository.new('./data/items_mini.csv')
-    assert_equal 3, ir.items.count
+    ir = ItemRepository.new('./data/items.csv')
+    assert_equal 1367, ir.items.count
   end
-
 end
