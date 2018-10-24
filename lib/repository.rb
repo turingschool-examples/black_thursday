e8c06e717607b15d7d994fd8da8e2b0685c564e1
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-10-24 14:47:10 -0600

    adds new hashes in place of arrays for merchant and item repositories

--- lib/repository.rb
+++ lib/repository.rb
@@ -1,23 +1,21 @@
 class Repository
 
   def all
-    @collection
+    @collection.values
   end
 
   def find_by_id(id)
-    @collection.find do |collection|
-      collection.id == id
-    end
+    @collection[id]
   end
 
   def find_by_name(name)
-    @collection.find do |collection|
-      collection.name == name
+    @collection.values.find do |collection|
+      collection.name.downcase == name.downcase
     end
   end
 
   def find_new_id
-    max_id_object = @collection.max_by do |collection|
+    max_id_object = @collection.values.max_by do |collection|
       collection.id
     end
     max_id_object.id + 1
