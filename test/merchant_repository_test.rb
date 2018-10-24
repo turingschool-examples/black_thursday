6cc2a443dfa9b950d52da8bdf748814ae7edd650
Lance Taylor <lance.taylor98@hotmail.com>
2018-10-24 11:38:19 -0600

    refactor to all method and associated tests

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -13,7 +13,7 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_merchant_reposity_has_merchants
     mr = MerchantRepository.new("./data/merchants.csv")
-    assert_instance_of Merchant, mr.merchants_array[0]
+    assert_instance_of Merchant, mr.all[0]
   end
 
   def test_it_can_find_all_merchants
@@ -23,17 +23,17 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_it_can_find_merchant_by_id
     mr = MerchantRepository.new("./data/merchants.csv")
-    assert_equal mr.merchants_array[4], mr.find_by_id(12334123)
+    assert_equal mr.all[4], mr.find_by_id(12334123)
   end
 
   def test_it_can_find_merchants_by_name
     mr = MerchantRepository.new("./data/merchants.csv")
-    assert_equal mr.merchants_array[6], mr.find_by_name("GoldenRayPress")
+    assert_equal mr.all[6], mr.find_by_name("GoldenRayPress")
   end
 
   def test_it_can_find_all_merchants_by_name
     mr = MerchantRepository.new("./data/merchants.csv")
-    assert_equal [mr.merchants_array[6], mr.merchants_array[143]], mr.find_all_by_name("Ray")
+    assert_equal [mr.all[6], mr.all[143]], mr.find_all_by_name("Ray")
   end
 
   def test_it_can_create_a_new_merchant_in_the_array
@@ -49,7 +49,7 @@ class MerchantRepositoryTest < Minitest::Test
   def test_it_can_update_an_existing_merchant
     mr = MerchantRepository.new("./data/merchants.csv")
     mr.update(12334105, {name: "Shopin2018"})
-    updated_merchant = mr.merchants_array[0]
+    updated_merchant = mr.all[0]
     actual = updated_merchant.name
     assert_equal "Shopin2018" , actual
   end
