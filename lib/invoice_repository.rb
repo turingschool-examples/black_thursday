5f58ecf8ba93e69216f85061539c3b3bc44c054d
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-24 16:24:35 -0600

    Adds find_all_by_customer_id

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,9 +1,14 @@
 require_relative 'invoice'
+require_relative 'repository'
 
-class InvoiceRepository
+class InvoiceRepository < Repository
   def initialize
     @type = Invoice
     @attr_whitelist = [:status]
     super
+  end
+
+  def find_all_by_customer_id(id)
+    @instances.find_all {|invoice| invoice.customer_id == id}
   end
 end
