95e9ff0994b31aeb821848494f7620ee5a355651
Maddie Jones <maddyg91@gmail.com>
2018-10-24 16:05:01 -0600

    assigned work for next iteration in comments

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,3 +1,9 @@
 class SalesAnalyst
 
 end
+# sales_analyst.average_items_per_merchant # => 2.88 ~Maddie
+# sales_analyst.average_items_per_merchant_standard_deviation # => 3.26 ~JC
+# sales_analyst.merchants_with_high_item_count # => [merchant, merchant, merchant] ~Jennica
+# sales_analyst.average_item_price_for_merchant(12334159) # => BigDecimal ~Maddie
+# sales_analyst.average_average_price_per_merchant # => BigDecimal ~Jennica
+# sales_analyst.golden_items # => [<item>, <item>, <item>, <item>]  ~JC
