5871d0d086911ef57bebf155a73e39e79e713cc2
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-23 18:27:50 -0600

    Test: creates_merchant_array (partial test)

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -7,11 +7,19 @@ class MerchantRepositoryTest < Minitest::Test
     mr = MerchantRepository.new(CSV.read("./data/merchants.csv", headers: true, header_converters: :symbol))
     assert_instance_of MerchantRepository, mr
   end
-  
+
   def test_it_has_merchants
     mr = MerchantRepository.new(CSV.read("./data/merchants.csv", headers: true, header_converters: :symbol))
     expected = CSV.read("./data/merchants.csv", headers: true, header_converters: :symbol)
     assert_equal expected, mr.merchants
+  end
+
+  def test_it_creates_merchant_array
+    # make an array of merchants
+    
+    # use test file
+
+    assert_equal
   end
 
 end
