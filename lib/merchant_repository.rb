dd25ecfa20bba7c61b68c3838bdca51b83ea6532
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-23 18:21:47 -0600

    create merchants test and merchants method update

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,6 +1,15 @@
 class MerchantRepository
+  attr_reader
 
   def initialize(data)
-    
+    @merchant_data  = CSV.open(data, headers: true, header_converters: :symbol)
+    @merchant_objects = []
+  end
+
+  def merchants
+    @merchant_data.each do |row|
+      @merchant_objects << Merchant.new({id: "#{row[:id]}", name: "#{row[:name]}"})
+    end
+    @merchant_objects
   end
 end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,9 +1,15 @@
 require_relative './helper'
+require_relative '../lib/merchant'
 require_relative '../lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
   def test_it_exists
     mr = MerchantRepository.new('./data/merchant_mini.csv')
     assert_instance_of MerchantRepository, mr
+  end
+
+  def test_it_can_create_merchants
+    mr = MerchantRepository.new('./data/merchant_mini.csv')
+    assert_equal 3, mr.merchants.count
   end
 end
