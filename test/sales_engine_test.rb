5eb3e1e1f9ad9a9e5967cde4a360b1c66dc35fad
William Petty <william@snakeweed.com>
2018-10-23 19:25:29 -0600

    SalesEngineTest Initial commit

---
+++ test/sales_engine_test.rb
@@ -0,0 +1,24 @@
+require './test/test_helper'
+require './lib/sales_engine'
+
+class SalesEngineTest < Minitest::Test
+
+  def setup
+    @se = SalesEngine.from_csv({
+    	  :items     => "./data/items.csv",
+    	  :merchants => "./data/merchants.csv",
+    	})
+  end
+
+  def test_it_exists
+  end
+
+  def test_it_returns_items
+    skip
+  end
+
+  def test_it_returns_merchants
+    skip
+  end
+
+end
