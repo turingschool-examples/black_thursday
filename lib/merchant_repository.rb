a8fc5d86719218df91fc9e22b9f189d7e38f2da3
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-23 18:29:19 -0600

    update to initialize method to collection

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -3,13 +3,13 @@ class MerchantRepository
 
   def initialize(data)
     @merchant_data  = CSV.open(data, headers: true, header_converters: :symbol)
-    @merchant_objects = []
+    @collection = []
   end
 
   def merchants
     @merchant_data.each do |row|
-      @merchant_objects << Merchant.new({id: "#{row[:id]}", name: "#{row[:name]}"})
+      @collection << Merchant.new({id: "#{row[:id]}", name: "#{row[:name]}"})
     end
-    @merchant_objects
+    @collection
   end
 end
