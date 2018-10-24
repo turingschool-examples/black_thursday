1e7c9c789a309043d79a4699e6e1f5a86514444e
Brad Niedt <bradleyniedt@gmail.com>
2018-10-23 19:43:01 -0600

    adds houndci changes

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -6,16 +6,16 @@ class ItemRepository
 
   def items
     @item_data.each do |row|
-      @collection << Item.new({id: "#{row[:id]}",
-                              name: "#{row[:name]}",
-                              description: "#{row[:description]}",
-                              unit_price: "#{row[:unit_price]}",
-                              merchant_id: "#{row[:merchant_id]}",
-                              created_at: "#{row[:created_at]}",
-                              updated_at: "#{row[:updated_at]}"})
+      @collection << Item.new({
+                               id: row[:id].to_s,
+                               name: row[:name].to_s,
+                               description: row[:description].to_s,
+                               unit_price: row[:unit_price].to_s,
+                               merchant_id: row[:merchant_id].to_s,
+                               created_at: row[:created_at].to_s,
+                               updated_at: row[:updated_at].to_s
+                             })
     end
     @collection
   end
-
-
 end
