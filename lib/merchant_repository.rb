eba75ed03bfb7f11862918045de1427b4ceedcef
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-23 18:36:08 -0600

    removing white spaces

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,5 +1,4 @@
 class MerchantRepository
-  attr_reader
 
   def initialize(data)
     @merchant_data  = CSV.open(data, headers: true, header_converters: :symbol)
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,7 +1,6 @@
 require_relative './helper'
 require_relative '../lib/merchant'
 require_relative '../lib/merchant_repository'
-
 class MerchantRepositoryTest < Minitest::Test
   def test_it_exists
     mr = MerchantRepository.new('./data/merchant_mini.csv')
