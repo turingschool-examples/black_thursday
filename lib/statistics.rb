773077aecffef49bb51b2f6639b3b883f183ba5f
Ben Lee <bendelonlee@gmail.com>
2018-10-24 14:33:34 -0600

    Standard deviation test passes

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -1,8 +1,10 @@
 module Statistics
-  def stand_deviation(*nums)
-    nums
+  def standard_deviation(*nums)
+    av = average(*nums)
+    diffs = nums.map{|n| n - av}
+    Math.sqrt(diffs.map{|n|n**2}.inject(&:+).to_f / 2.to_f)
   end
   def average(*nums)
-    nums.inject(&:+) / nums.size
+    nums.inject(&:+) / nums.size.to_f
   end
 end
--- test/statistics_test.rb
+++ test/statistics_test.rb
@@ -11,9 +11,10 @@ class StatisticsTest < Minitest::Test
     @rep = Repository.new
   end
   def test_most_basic_standard_deviation
-    skip
-    assert_equal 1, @rep.stand_deviation(3,4,5)
-
+    assert_equal 1, @rep.standard_deviation(3,4,5)
+  end
+  def test_standard_deviation
+    assert_equal 1.5275252316519465, @rep.standard_deviation(3,4,6)
   end
   def test_average
     assert_equal 4, @rep.average(3,4,5)
