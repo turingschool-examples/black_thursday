3a088d6655d41345351cda6c4f573fffe916cdd0
Maddie Jones <maddyg91@gmail.com>
2018-10-24 15:49:38 -0600

    test for sales analyst assert instance passing

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,6 +3,7 @@ require_relative './merchant_repository'
 require_relative './item_repository'
 require_relative './merchant'
 require_relative './item'
+require_relative './sales_analyst'
 
 class SalesEngine
   attr_reader :merchants, :items
