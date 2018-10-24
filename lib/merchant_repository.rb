71606c191170747504bfce39af5a6c7db1a54b77
Nico <nicovigil1@gmail.com>
2018-10-24 11:42:06 -0600

    adds rspec prereqs

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -2,29 +2,29 @@ class MerchantRepository
     def initialize(merchants)
       @merchants = merchants
     end
-    
+
     def all
       @merchants
     end
-    
+
     def find_by_id(id)
       @merchants.find do |merchant|
         merchant.id == id
       end
     end
-    
+
     def find_by_name(name)
       @merchants.find do |merchant|
         merchant.name.upcase == name.upcase
       end
     end
-    
+
     def find_all_by_name(name)
       @merchants.find_all do |merchant|
         merchant.name.upcase.include?(name.upcase)
       end
     end
-    
+
     def create(attributes)
       max_id = @merchants.max_by do |merchant|
         merchant.id
@@ -35,17 +35,21 @@ class MerchantRepository
       @merchants << new_merchant
       new_merchant
     end
-    
+
     def update(id, attributes)
       merchant = find_by_id(id)
       #update merchant with given attribute
       new_name = attributes[:name]
       merchant.name = new_name
     end
-    
+
     def delete(id)
       merchant = find_by_id(id)
       @merchants.delete(merchant)
     end
-    
+
+    def inspect
+      "#<#{self.class} #{@merchants.size} rows>"
+    end
+
 end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,3 +1,5 @@
+require_relative "../lib/merchant_repository"
+require_relative "../lib/item_repository"
 class SalesEngine
   attr_reader :items, :merchants
 
