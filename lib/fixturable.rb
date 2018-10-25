fef9b6507215220f4f11b21096ecf382eb4f7359
Ben Lee <bendelonlee@gmail.com>
2018-10-25 14:40:34 -0600

    Sales_analyst tests now works with fixture data

--- lib/fixturable.rb
+++ lib/fixturable.rb
@@ -1,19 +1,23 @@
-module Fixtureable
+module Fixturable
   def sample(merch_ids = nil)
     merch_ids = [12334105, 12334141, 12334185, 12334195, 12334207, 12334213, 12334235] unless merch_ids
     instance_variables.each do |var|
-      next if var == :@merchants
-      related_array = instance_variable_get(var).all.select{|instance| merch_ids.include?(instance.merchant_id)}
-      file_name = "test_" + var.to_s.delete('@')
+      related_array =
+      if var == :@merchants
+        instance_variable_get(var).all.select{|instance| merch_ids.include?(instance.id)}
+      else
+        instance_variable_get(var).all.select{|instance| merch_ids.include?(instance.merchant_id)}
+      end
+      file_name = "test_#{var.to_s.delete('@')}.csv"
       CSV.open(file_name, "wb") do |csv|
-        csv << to_a_of_hs(related_array)[0].keys # adds the attributes name on the first line
-        to_a_of_hs(related_array).each do |hash|
+        csv << to_arr_of_hashes(related_array)[0].keys # adds the attributes name on the first line
+        to_arr_of_hashes(related_array).each do |hash|
           csv << hash.values
         end
       end
     end
   end
-  def to_a_of_hs(arr_of_objects)
+  def to_arr_of_hashes(arr_of_objects)
     arr_of_objects.reduce([]) do |arr, ob|
       arr << to_h(ob)
     end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -5,33 +5,31 @@ require './lib/sales_engine'
 class SalesAnalystTest < Minitest::Test
   def setup
     se = SalesEngine.from_csv({
-      items: '.test/data/test_items.csv',
-      merchants: '.test/data/test_merchants.csv',
-      invoices: '.test/data/invoices.csv'
+      items: './test/data/test_items.csv',
+      merchants: './test/data/test_merchants.csv',
+      invoices: './test/data/test_invoices.csv'
     })
     @sa = se.analyst
   end
 
   def test_average_items_per_merchant
-    assert_equal 0.03, @sa.average_items_per_merchant
+    assert_equal 4.86, @sa.average_items_per_merchant
   end
 
   def test_average_items_per_merchant_standard_deviation
-    assert_equal 0.95, @sa.average_items_per_merchant_standard_deviation
+    assert_equal 6.91, @sa.average_items_per_merchant_standard_deviation
   end
 
   def test_merchants_with_high_item_count
-    assert_equal 7, @sa.merchants_with_high_item_count.size
+    assert_equal 1, @sa.merchants_with_high_item_count.size
   end
 
   def test_average_item_price_for_merchant
-
-    assert_equal 14.0, @sa.average_item_price_for_merchant(12334135)
+    assert_equal 0.11, @sa.average_item_price_for_merchant(12334185)
   end
 
   def test_average_average_price_per_merchant
-    skip
-    assert_equal 350.29, @sa.average_average_price_per_merchant
+    assert_equal 42857.98, @sa.average_average_price_per_merchant
   end
 
   def test_golden_items
@@ -39,33 +37,30 @@ class SalesAnalystTest < Minitest::Test
   end
 
   def test_it_finds_percentage_of_invoices_status_returned
-    assert_equal 7.69, @sa.invoice_status(:returned)
+    assert_equal 19.18, @sa.invoice_status(:returned)
   end
 
   def test_it_finds_percentage_of_invoices_status_pending
-    assert_equal 23.08, @sa.invoice_status(:pending)
+    assert_equal 39.73, @sa.invoice_status(:pending)
   end
 
   def test_it_finds_percentage_of_invoices_status_shipped
-    assert_equal 69.23, @sa.invoice_status(:shipped)
+    assert_equal 41.1, @sa.invoice_status(:shipped)
   end
 
   def test_average_invoices_per_merchant
-    assert_equal 0.03, @sa.average_invoices_per_merchant
+    assert_equal 10.43, @sa.average_invoices_per_merchant
   end
   def test_average_invoices_per_merchant_standard_deviation
-    skip
-    assert_equal 3.29, @sa.average_invoices_per_merchant_standard_deviation
+    assert_equal 4.79, @sa.average_invoices_per_merchant_standard_deviation
   end
   def test_top_merchants_by_invoice_count
-    skip
-    assert_equal 12, @sa.top_merchants_by_invoice_count.size
+    assert_equal 0, @sa.top_merchants_by_invoice_count.size
   end
   def test_bottom_merchants_by_invoice_count
-    skip
-    assert_equal 4, @sa.bottom_merchants_by_invoice_count.size
+    assert_equal 0, @sa.bottom_merchants_by_invoice_count.size
   end
   def test_top_days_by_invoice_count
-    assert_equal ["Monday", "Wednesday"], @sa.top_days_by_invoice_count
+    assert_equal [], @sa.top_days_by_invoice_count
   end
 end
