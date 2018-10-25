edcdecbd3774376d25caed4fe684599cbeac385c
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-25 14:03:25 -0600

    Adds attr methods, initialize, and find_all_by_credit_card

---
+++ lib/transaction_repository.rb
@@ -0,0 +1,15 @@
+require_relative 'transaction'
+require_relative 'repository'
+
+class TransactionRepository < Repository
+  attr_reader :type, :attr_whitelist
+  def initialize
+    @type = Transaction
+    @attr_whitelist = [:credit_card_number, :credit_card_expiration_date, :result]
+    super
+  end
+
+  def test_find_all_by_credit_card_number(card_number)
+    @instances.find_all {|transaction| transaction.credit_card_number == card_number}
+  end
+end
