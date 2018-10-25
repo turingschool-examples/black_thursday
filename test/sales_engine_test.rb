5a06751f3884f50e673ebce51bb13a109d871152
Anten Skrabec <cebarks@gmail.com>
2018-10-24 19:18:55 -0600

    update SalesEngine to parse invoices.csv and populate InvoiceRepository

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -6,7 +6,8 @@ class SalesEngineTest < Minitest::Test
   def setup
     @se = SalesEngine.from_csv({
       items: './data/items.csv',
-      merchants: './data/merchants.csv'})
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv'})
   end
 
   def test_it_exists
@@ -17,7 +18,23 @@ class SalesEngineTest < Minitest::Test
     assert_instance_of ItemRepository, @se.items
   end
 
-  def test_it_holds_an_merchant_repository
+  def test_it_parses_items_correctly
+    assert_equal 1367, @se.items.all.length
+  end
+
+  def test_it_parses_invoices_correctly
+    assert_equal 4985, @se.invoices.all.length
+  end
+
+  def test_it_parses_merchants_correctly
+    assert_equal 475, @se.merchants.all.length
+  end
+
+  def test_it_holds_a_merchant_repository
     assert_instance_of MerchantRepository, @se.merchants
+  end
+
+  def test_it_holds_an_invoice_repository
+    assert_instance_of InvoiceRepository, @se.invoices
   end
 end
