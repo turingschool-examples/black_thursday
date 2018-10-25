93aba1efdee0430e280ceea72bce382989fadd4e
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-25 16:49:39 -0600

    changed intialize to downcase names

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,8 +5,8 @@ class Customer < BusinessData
   attr_accessor :first_name, :last_name, :updated_at
   def initialize(input_hash)
     @id = input_hash[:id]
-    @first_name = input_hash[:first_name]
-    @last_name = input_hash[:last_name]
+    @first_name = input_hash[:first_name].downcase
+    @last_name = input_hash[:last_name].downcase
     @created_at = input_hash[:created_at]
     @updated_at = input_hash[:updated_at]
   end
