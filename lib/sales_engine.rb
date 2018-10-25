51bb24cd68ff2c3afa04e1311c2fc63701759b55
vjp888 <rellik888@gmail.com>
2018-10-25 12:57:59 -0600

    Adds average price method. Needs to be linked to Sales engine.

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,19 +1,19 @@
 require_relative "../lib/merchant_repository"
 require_relative "../lib/item_repository"
+require_relative './sales_analyst'
+
 class SalesEngine
-  attr_reader :items, :merchants
+  attr_reader :items, :merchants, :analyst
 
   def initialize(items, merchants)
     @items = ItemRepository.new(items)
     @merchants = MerchantRepository.new(merchants)
+    @analyst = SalesAnalyst.new
   end
 
   def self.from_csv(info)
     self.new(info[:items], info[:merchants])
   end
 
-  def analyst
-    SalesAnalyst.new
-  end
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,22 +1,33 @@
 require_relative './test_helper'
 
 class SalesAnalystTest < Minitest::Test
-  
+
   def setup
     @sales_engine = SalesEngine.from_csv({
     :items     => "./data/items.csv",
     :merchants => "./data/merchants.csv",
     })
   end
-  
+
   def test_it_exists
     sales_analyst = SalesAnalyst.new
     assert_instance_of SalesAnalyst, sales_analyst
   end
-  
+
   def test_it_can_be_created_by_sales_engine
       sales_analyst = @sales_engine.analyst
       assert_instance_of SalesAnalyst, sales_analyst
   end
-  
+
+  def test_analyst_can_find_who_sells_the_most
+    skip
+
+  end
+
+  def test_it_can_average_price_of_items_by_merchant
+    sa = @sales_engine.analyst
+    average = sa.average_item_price_for_merchant(12334185)
+
+    assert_equal 0, average
+  end
 end
