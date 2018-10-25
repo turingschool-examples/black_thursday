01da46e7f1db66d792399cd8363f0a4d0471436c
Anten Skrabec <cebarks@gmail.com>
2018-10-25 16:47:33 -0600

    Fixes Transaction related rspec failures

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -129,7 +129,7 @@ class SalesEngine
       invoice_id: row[:invoice_id].to_i,
       credit_card_number: row[:credit_card_number],
       credit_card_expiration_date: row[:credit_card_expiration_date],
-      result: row[:result],
+      result: row[:result].to_sym,
       created_at: Time.parse(row[:created_at]),
       updated_at: Time.parse(row[:updated_at])
     }
