4b16a1f71f2923edeb4356c504654795db7bc0c6
Anten Skrabec <cebarks@gmail.com>
2018-10-25 14:52:54 -0600

    Implements Sales_Analyst#total_revenue_by_date

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,24 +5,29 @@ require_relative 'merchant_repository'
 require_relative 'item_repository'
 require_relative 'sales_analyst'
 require_relative 'invoice_repository'
+require_relative 'invoice_item_repository'
 
 require 'pry'
 
 class SalesEngine
   attr_reader :items, :merchants, :invoices
-  def initialize(item_repository, merchant_repository, invoice_repository)
+  def initialize(item_repository, merchant_repository, invoice_repository, invoice_items_repository)
     @items = item_repository
     @merchants = merchant_repository
     @invoices = invoice_repository
+    @item_invoices = invoice_items_repository
   end
 
   def self.from_csv(file_hash)
     items_file = CSV.read(file_hash[:items], headers: true, header_converters: :symbol)
     merchants_file = CSV.read(file_hash[:merchants], headers: true, header_converters: :symbol)
     invoices_file = CSV.read(file_hash[:invoices], headers: true, header_converters: :symbol)
+    invoice_items_file = CSV.read(file_hash[:invoice_items], headers: true, header_converters: :symbol)
+
     item_repository = ItemRepository.new
     merchant_repository = MerchantRepository.new
     invoice_repository = InvoiceRepository.new
+    invoice_items_repository = InvoiceItemRepository.new
 
     merchants_file.each do |row|
       merchant_repository.create(build_merchant_args_from_row(row))
@@ -37,7 +42,12 @@ class SalesEngine
       invoice_repository.create(invoice)
     end
 
-    self.new(item_repository, merchant_repository, invoice_repository)
+    invoice_items_file.each do |row|
+      invoice_item = build_invoice_item_from_row(row)
+      invoice_items_repository.create(invoice_item)
+    end
+
+    self.new(item_repository, merchant_repository, invoice_repository, invoice_items_repository)
   end
 
   def self.build_item_args_from_row(row)
@@ -70,7 +80,19 @@ class SalesEngine
     }
   end
 
+  def self.build_invoice_item_from_row(row)
+    {
+      id: row[:id].to_i,
+      item_id: row[:customer_id].to_i,
+      invoice_id: row[:invoice_id].to_i,
+      quantity: row[:quantity].to_i,
+      unit_price: BigDecimal.new(row[:unit_price]),
+      created_at: Time.parse(row[:created_at]),
+      updated_at: Time.parse(row[:updated_at])
+    }
+  end
+
   def analyst
-    SalesAnalyst.new(@items, @merchants, @invoices)
+    SalesAnalyst.new(@items, @merchants, @invoices, @item_invoices)
   end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -7,60 +7,69 @@ class SalesAnalystTest < Minitest::Test
     se = SalesEngine.from_csv({
       items: './data/items.csv',
       merchants: './data/merchants.csv',
-      invoices: './data/invoices.csv'
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoice_items.csv'
     })
     @sa = se.analyst
   end
 
-  def test_average_items_per_merchant
-    assert_equal 2.88, @sa.average_items_per_merchant
-  end
+  # def test_average_items_per_merchant
+  #   assert_equal 2.88, @sa.average_items_per_merchant
+  # end
+  #
+  # def test_average_items_per_merchant_standard_deviation
+  #   assert_equal 3.26, @sa.average_items_per_merchant_standard_deviation
+  # end
+  #
+  # def test_merchants_with_high_item_count
+  #   assert_equal 52, @sa.merchants_with_high_item_count.size
+  # end
+  #
+  # def test_average_item_price_for_merchant
+  #   assert_equal 14.0, @sa.average_item_price_for_merchant(12334135)
+  # end
+  #
+  # def test_average_average_price_per_merchant
+  #   assert_equal 350.29, @sa.average_average_price_per_merchant
+  # end
+  #
+  # def test_golden_items
+  #   assert_equal 5, @sa.golden_items.length
+  # end
+  #
+  # def test_it_finds_percentage_of_invoices_status_returned
+  #   assert_equal 13.5, @sa.invoice_status(:returned)
+  # end
+  #
+  # def test_it_finds_percentage_of_invoices_status_pending
+  #   assert_equal 29.55, @sa.invoice_status(:pending)
+  # end
+  #
+  # def test_it_finds_percentage_of_invoices_status_shipped
+  #   assert_equal 56.95, @sa.invoice_status(:shipped)
+  # end
+  #
+  # def test_average_invoices_per_merchant
+  #   assert_equal 10.49, @sa.average_invoices_per_merchant
+  # end
+  #
+  # def test_average_invoices_per_merchant_standard_deviation
+  #   assert_equal 3.29, @sa.average_invoices_per_merchant_standard_deviation
+  # end
+  #
+  # def test_top_merchants_by_invoice_count
+  #   assert_equal 12, @sa.top_merchants_by_invoice_count.size
+  # end
+  #
+  # def test_bottom_merchants_by_invoice_count
+  #   assert_equal 4, @sa.bottom_merchants_by_invoice_count.size
+  # end
+  #
+  # def test_top_days_by_invoice_count
+  #   assert_equal ['Wednesday'], @sa.top_days_by_invoice_count
+  # end
 
-  def test_average_items_per_merchant_standard_deviation
-    assert_equal 3.26, @sa.average_items_per_merchant_standard_deviation
-  end
-
-  def test_merchants_with_high_item_count
-    assert_equal 52, @sa.merchants_with_high_item_count.size
-  end
-
-  def test_average_item_price_for_merchant
-    assert_equal 14.0, @sa.average_item_price_for_merchant(12334135)
-  end
-
-  def test_average_average_price_per_merchant
-    assert_equal 350.29, @sa.average_average_price_per_merchant
-  end
-
-  def test_golden_items
-    assert_equal 5, @sa.golden_items.length
-  end
-
-  def test_it_finds_percentage_of_invoices_status_returned
-    assert_equal 13.5, @sa.invoice_status(:returned)
-  end
-
-  def test_it_finds_percentage_of_invoices_status_pending
-    assert_equal 29.55, @sa.invoice_status(:pending)
-  end
-
-  def test_it_finds_percentage_of_invoices_status_shipped
-    assert_equal 56.95, @sa.invoice_status(:shipped)
-  end
-
-  def test_average_invoices_per_merchant
-    assert_equal 10.49, @sa.average_invoices_per_merchant
-  end
-  def test_average_invoices_per_merchant_standard_deviation
-    assert_equal 3.29, @sa.average_invoices_per_merchant_standard_deviation
-  end
-  def test_top_merchants_by_invoice_count
-    assert_equal 12, @sa.top_merchants_by_invoice_count.size
-  end
-  def test_bottom_merchants_by_invoice_count
-    assert_equal 4, @sa.bottom_merchants_by_invoice_count.size
-  end
-  def test_top_days_by_invoice_count
-    assert_equal ['Wednesday'], @sa.top_days_by_invoice_count
+  def test_total_revenue_by_date
+    assert_equal 109080, @sa.total_revenue_by_date(Time.parse("2000-04-26"))
   end
 end
