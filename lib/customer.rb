5466511f5e18f5151173d927dea7d299661c95e1
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-25 16:56:17 -0600

    removed downcasing in initialize

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,8 +5,8 @@ class Customer < BusinessData
   attr_accessor :first_name, :last_name, :updated_at
   def initialize(input_hash)
     @id = input_hash[:id]
-    @first_name = input_hash[:first_name].downcase
-    @last_name = input_hash[:last_name].downcase
+    @first_name = input_hash[:first_name]
+    @last_name = input_hash[:last_name]
     @created_at = input_hash[:created_at]
     @updated_at = input_hash[:updated_at]
   end
