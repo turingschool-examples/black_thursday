24113d35153aee4727d7bf7c6bc7bd3e60a66dd2
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-25 16:50:40 -0600

    refactor to find_all using downcased names

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -10,10 +10,10 @@ class CustomerRepository < Repository
   end
 
   def find_all_by_first_name(first_name)
-    @instances.find_all {|customer| customer.first_name.include?(first_name)}
+    @instances.find_all {|customer| customer.first_name.include?(first_name.downcase)}
   end
 
   def find_all_by_last_name(last_name)
-    @instances.find_all {|customer| customer.last_name.include?(last_name)}
+    @instances.find_all {|customer| customer.last_name.include?(last_name.downcase)}
   end
 end
