69790284c947d94284185e98693505694d098398
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-25 16:47:17 -0600

    Adds refactor for find_all_by_name to include fragments

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -10,10 +10,10 @@ class CustomerRepository < Repository
   end
 
   def find_all_by_first_name(first_name)
-    @instances.find_all {|customer| customer.first_name == first_name}
+    @instances.find_all {|customer| customer.first_name.include?(first_name)}
   end
 
   def find_all_by_last_name(last_name)
-    @instances.find_all {|customer| customer.last_name == last_name}
+    @instances.find_all {|customer| customer.last_name.include?(last_name)}
   end
 end
