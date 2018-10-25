7532ec8092c2209638b86d924fdf291d05338441
Ben Lee <bendelonlee@gmail.com>
2018-10-25 16:46:41 -0600

    needed to divide by 100 when creating from csv

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -107,7 +107,7 @@ class SalesEngine
       item_id: row[:item_id].to_i,
       invoice_id: row[:invoice_id].to_i,
       quantity: row[:quantity].to_i,
-      unit_price: BigDecimal.new(row[:unit_price]),
+      unit_price: BigDecimal.new(row[:unit_price]) / 100,
       created_at: Time.parse(row[:created_at]),
       updated_at: Time.parse(row[:updated_at])
     }
