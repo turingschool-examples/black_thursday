0dc35eb3e66dbbc2b84624a2e174c0baf9ef4fe3
vjp888 <rellik888@gmail.com>
2018-10-26 13:05:30 -0600

    Creates invoicerepo and invoice lib and test file with initial test

---
+++ test/invoice_repository_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/invoice_repository'
+require './lib/invoice'
+
+class InvoiceRepositoryTest < MiniTest::Test
+
+  def test_invoice_repo_exists
+
+  end
+end
---
+++ test/invoice_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/invoice'
+
+class InvoiceTest < MiniTest::Test
+
+  def test_invoice_exists
+
+  end
+
+end
