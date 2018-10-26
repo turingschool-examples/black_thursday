35362cc96cbc11e49bfd9e3b31d100caf9d443a1
Maddie Jones <maddyg91@gmail.com>
2018-10-26 12:54:11 -0600

    added test for add_invoice in invoice repository

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,1 +1,13 @@
 require_relative './repository'
+
+class InvoiceRepository < Repository
+
+  def initialize
+    @collection = {}
+  end
+
+  def add_invoice(invoice)
+
+  end
+
+end
---
+++ test/invoice_repository_test.rb
@@ -0,0 +1,41 @@
+require './test/test_helper'
+require './lib/invoice_repository'
+require './lib/invoice'
+
+class InvoiceRepositoryTest < Minitest::Test
+  def setup
+    @invoice_1 = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 10,
+      :status      => "pending",
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      })
+    @invoice_2 = Invoice.new({
+      :id          => 7,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      })
+    @invoice_3 = Invoice.new({
+      :id          => 8,
+      :customer_id => 9,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      })
+    @inr = InvoiceRepository.new
+  end
+
+  def test_it_exists
+    assert_instance_of InvoiceRepository, @inr
+  end
+  def test_it_can_add_invoice
+    @inr.add_invoice(@invoice_1)
+    assert_equal [@invoice_1], @inr.all
+  end
+end
