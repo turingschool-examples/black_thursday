08ee5dd6f609ddb7996165e794d1518b4e00f90f
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-26 17:48:18 -0600

    start of standard deviation breakdown

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,11 +1,41 @@
 require_relative '../lib/sales_engine'
 class SalesAnalyst
+  attr_reader     :items, :merchants
   def initialize(items, merchants)
     @items      = items
     @merchants  = merchants
   end
 
-  def average_items_per_merchant
-    (@items.all.count.to_f / @merchants.all.count).round(2)
+  def count_all_items
+    @items.all.count.round(2)
   end
+
+  def count_all_merchants
+    @merchants.all.count.round(2)
+  end
+
+  def average_items_per_merchant
+    (count_all_items / count_all_merchants).round(2)
+  end
+
+  def all_merchant_item_count
+    @merchants.all.map do |merchant|
+      x = merchant.id
+      y = items.find_all_by_merchant_id(x)
+      y.count
+    end
+  end
+
+  def merchant_count_minus_average
+    avg = average_items_per_merchant
+    all_merchant_item_count.map do |num|
+      num - avg
+    end
+  end
+
+  # def average_items_per_merchant_standard_deviation
+  #
+  #   (array_numbers.inject(:+)) / (@items.all.count - 1.00)
+  # end
+
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -17,7 +17,24 @@ class SalesAnalystTest < Minitest::Test
     assert_equal 2.88, @sa.average_items_per_merchant
   end
 
-  def test_it_can_calculate_standard_deviation
-    assert_equal 3.26, @sa.average_items_per_merchant_standard_deviation
+  def test_it_can_count_total_number_of_items
+    assert_equal 1367.00, @sa.count_all_items
+    assert_equal 1366.00, @sa.count_all_items - 1
   end
+
+  def test_it_can_count_all_merchants
+    assert_equal 475.00, @sa.count_all_merchants
+  end
+
+  def test_merchant_item_count
+    assert_equal 475, @sa.all_merchant_item_count.count
+  end
+
+  def test_merchant_count_minus_average
+    assert_equal 475, @sa.merchant_count_minus_average.count
+  end
+  
+  # def test_it_can_calculate_standard_deviation
+  #   assert_equal 3.26, @sa.average_items_per_merchant_standard_deviation
+  # end
 end
