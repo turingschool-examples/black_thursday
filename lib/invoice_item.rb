369de2a05b6dafe6383e08228a4b10c0d625175a
Aizar <aizar.cabrera@icloud.com>
2018-10-26 17:03:45 -0600

    adds test_it_has_quantity for InvoiceItem passing

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,10 +1,11 @@
 class InvoiceItem
 
-  attr_reader :id, :item_id, :invoice_id
+  attr_reader :id, :item_id, :invoice_id, :quantity
 
   def initialize(invoice_item_data)
     @id = invoice_item_data[:id].to_i
     @item_id = invoice_item_data[:item_id].to_i
     @invoice_id = invoice_item_data[:invoice_id].to_i
+    @quantity = invoice_item_data[:quantity].to_i
   end
 end
