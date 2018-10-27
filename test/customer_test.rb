9393a69cdead3a4e258cd88fb919cd5b0e3cfd4f
Aizar <aizar.cabrera@icloud.com>
2018-10-27 14:07:07 -0600

    adds test_it_exists for Customer

---
+++ test/customer_test.rb
@@ -0,0 +1,19 @@
+require './test/test_helper'
+
+class CustomerTest < Minitest::Test
+
+  def setup
+    @c = Customer.new({
+    :id => 6,
+    :first_name => "Joan",
+    :last_name => "Clarke",
+    :created_at => Time.now,
+    :updated_at => Time.now
+    })
+  end
+
+  def test_it_exists
+    assert_instance_of Customer, @c
+  end
+
+end
