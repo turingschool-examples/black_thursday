e98d63ea00c9872c7bf4dc5ce390d0c2ad6bdb2a
Aizar <aizar.cabrera@icloud.com>
2018-10-27 14:22:20 -0600

    adds test_it_is_updated_at for Customer now really passing

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,12 +2,14 @@ class Customer
 
   attr_reader :first_name,
               :last_name,
-              :created_at
+              :created_at,
+              :updated_at
 
   def initialize(customer_data)
     @first_name = customer_data[:first_name]
     @last_name  = customer_data[:last_name]
     @created_at = customer_data[:created_at]
+    @updated_at = customer_data[:updated_at]
   end
 
 end
