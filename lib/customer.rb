73e8b2588af1e0bfa7e2a7afbfb6ceae320d54ee
Aizar <aizar.cabrera@icloud.com>
2018-10-27 14:19:34 -0600

    adds created_at for Customer passing

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,13 @@
 class Customer
 
   attr_reader :first_name,
-              :last_name
+              :last_name,
+              :created_at
 
   def initialize(customer_data)
     @first_name = customer_data[:first_name]
-    @last_name = customer_data[:last_name]
+    @last_name  = customer_data[:last_name]
+    @created_at = customer_data[:created_at]
   end
 
 end
