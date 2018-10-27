0ea6fe36031b846690e202358c952c729daa4486
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-27 17:21:19 -0600

    psuedo code methods

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -76,13 +76,14 @@ class SalesAnalyst
     golden_array
   end
 
-  # def average_invoices_per_merchant
-  ## number of invoices / number of merchants
-  #all method
-  #@invoices.id.count /
-  # end
+  def average_invoices_per_merchant
+  # number of invoices / number of merchants
+  @invoices.count / @merchants.count
+  # use #all method?
+  end
 
   # def average_invoices_per_merchant_standard_deviation
+  #   average_invoices_per_merchant
   # end
 
   # def top_merchants_by_invoice_count
@@ -99,9 +100,12 @@ class SalesAnalyst
   # On which days are invoices created at more than one standard deviation above the mean?
   # end
 
-  # def invoice_status(argument)
-  # What percentage of invoices are shipped vs pending vs returned? (takes symbol as argument)
-  # end
+  def invoice_status(status)
+  # What percentage of invoices are shipped vs pending vs returned?
+  # (total invoices for that status / total invoices) * 100
+  (@invoices[status].count / @invoices.count) * 100
+  # the above line might not be quite right
+  end
 
 
 end
