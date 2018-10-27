b27c77227102c4c6774502d39217248273b80c15
Aizar <aizar.cabrera@icloud.com>
2018-10-26 19:04:33 -0600

    adds test_it_has_invoice_id for Transaction passing

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,10 @@
 class Transaction
 
-  attr_reader :id
+  attr_reader :id,
+              :invoice_id
 
   def initialize(transaction_data)
     @id = transaction_data[:id].to_i
+    @invoice_id = transaction_data[:invoice_id].to_i
   end
 end
