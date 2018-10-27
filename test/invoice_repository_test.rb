1eb6246a1012be6d99e3e1b74585976a1be887ce
Leah Miller <lkmiller.87@gmail.com>
2018-10-27 16:38:35 -0600

    Add new test and lib files for invoice repo

---
+++ test/invoice_repository_test.rb
@@ -0,0 +1,14 @@
+require_relative './helper'
+require_relative '../lib/invoice_repository'
+require_relative '../lib/invoice_repository'
+require_relative '../lib/sales_engine'
+class InvoiceRepositoryTest < Minitest::Test
+  def setup
+    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
+    @invoices = se.invoices
+  end
+
+  def test_it_exists
+    assert_instance_of InvoiceRepository, @invoices
+  end 
+end
