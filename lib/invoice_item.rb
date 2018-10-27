eb71d6a39445bab609d8556bd8dcdcca9ae1af01
Nico <nicovigil1@gmail.com>
2018-10-27 14:27:22 -0600

    adds invoice item class with passing test

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,7 +1,12 @@
 class InvoiceItem
-  attr_reader :id, :item_id
+  attr_reader :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
   def initialize(stats)
     @id = stats[:id]
     @item_id = stats[:item_id]
+    @invoice_id = stats[:invoice_id]
+    @quantity = stats[:quantity]
+    @unit_price = stats[:unit_price]
+    @created_at = stats[:created_at]
+    @updated_at = stats[:updated_at]
   end
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -3,11 +3,16 @@ require_relative "./test_helper"
 class InvoiceItemTest<Minitest::Test
 
   def setup
+    @time = Time.now
     stats = {
       id:5,
-      item_id:10
+      item_id:10,
+      invoice_id:15,
+      quantity:20,
+      unit_price:25,
+      created_at:@time,
+      updated_at:@time
     }
-    @time = Time.now
     @invoice_item = InvoiceItem.new(stats)
   end
 
@@ -22,34 +27,24 @@ class InvoiceItemTest<Minitest::Test
   def test_it_has_item_id
     assert_equal 10, @invoice_item.item_id
   end
-# invoice_id - returns the invoice id
 
   def test_it_has_invoice_id
-    skip
     assert_equal 15, @invoice_item.invoice_id
   end
-# quantity - returns the quantity
 
   def test_it_has_a_quantity
-    skip
     assert_equal 20, @invoice_item.quantity
   end
-# unit_price - returns the unit_price
 
   def test_it_has_unit_price
-    skip
     assert_equal 25, @invoice_item.unit_price
   end
-# created_at - returns a Time instance for the date the invoice item was first created
 
   def test_it_has_a_created_at_time
-    skip
     assert_equal @time, @invoice_item.created_at
   end
-# updated_at - returns a Time instance for the date the invoice item was last modified
 
   def test_it_has_an_updated_at_time
-    skip
     assert_equal @time, @invoice_item.updated_at
   end
 
