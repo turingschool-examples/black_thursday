13ed9863619400a26534d8da4b030f150dc49895
Aizar <aizar.cabrera@icloud.com>
2018-10-26 19:08:32 -0600

    adds test_it_returns_credit_card_number to Transaction passing

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,12 @@
 class Transaction
 
   attr_reader :id,
-              :invoice_id
+              :invoice_id,
+              :credit_card_number
 
   def initialize(transaction_data)
     @id = transaction_data[:id].to_i
     @invoice_id = transaction_data[:invoice_id].to_i
+    @credit_card_number = transaction_data[:credit_card_number]
   end
 end
