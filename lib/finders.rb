cda40e519c7c86bb9546a86ea4b333ebc05129ad
Ben Lee <bendelonlee@gmail.com>
2018-10-28 10:02:06 -0600

    minor adjustments passes basic test

--- lib/finders.rb
+++ lib/finders.rb
@@ -4,15 +4,14 @@ module Finders
     class_string = business_data.class.to_s
     case class_string
     when 'Merchant', 'Customer'
-      binding.pry
       method_name = "find_all_by_#{class_string.downcase}_id"
       @invoices.public_send(method_name, business_data.id)
     when 'InvoiceItem', 'Transaction'
-      @invoices.find_all_by_invoice_id(business_data.invoice_id)
+      [@invoices.find_by_id(business_data.invoice_id)]
     when 'Item'
       # find_invoices_from()
     when 'Invoice'
-      self
+      [business_data]
     end
   end
 
