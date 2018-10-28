84995bea524ca28591549aeca263307d7ab973a3
vjp888 <rellik888@gmail.com>
2018-10-28 17:15:43 -0600

    Itegrates customer repo into sales engine passes minitest and customer repo and customer are passing rspec test

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,6 +3,7 @@ require_relative '../lib/merchant_repository'
 require_relative '../lib/item_repository'
 require_relative '../lib/invoice_repository'
 require_relative '../lib/transaction_repository'
+require_relative '../lib/customer_repository'
 require_relative './sales_analyst'
 
 
@@ -11,14 +12,16 @@ class SalesEngine
               :merchants,
               :invoices,
               :transactions,
+              :customers,
               :analyst
 
 
-  def initialize(items, merchants, invoices = nil, transactions)
+  def initialize(items, merchants, invoices = nil, transactions, customers)
     @items = ItemRepository.new(populate_items(items))
     @merchants = MerchantRepository.new(populate_merchants(merchants))
     @invoices = InvoiceRepository.new(populate_invoices(invoices))
     @transactions = TransactionRepository.new(populate_transactions(transactions))
+    @customers = CustomerRepository.new(populate_customers(customers))
     @analyst = SalesAnalyst.new(@items, @merchants, @invoices, @transactions)
   end
 
@@ -27,7 +30,8 @@ class SalesEngine
     info[:merchants] ? merchants = info[:merchants] : merchants = nil
     info[:invoices] ? invoices = info[:invoices] : invoices = nil
     info[:transactions] ? transactions = info[:transactions] : transactions = nil
-    self.new(items, merchants, invoices, transactions)
+    info[:customers] ? customers = info[:customers] : customers = nil
+    self.new(items, merchants, invoices, transactions, customers)
   end
 
   def populate_invoices(file_path)
@@ -72,5 +76,14 @@ class SalesEngine
   #
   # end
 
-  
+  def populate_customers(file_path)
+    if file_path
+      file = CSV.read(file_path, headers: true, header_converters: :symbol )
+      file.map do |row|
+        Customer.new(row)
+      end
+    end
+  end
+
+
 end
