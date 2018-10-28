f44599e83de9d3cf7b491d9eccd8aedc7737e8a6
jpclark6 <jpclark6@gmail.com>
2018-10-28 16:33:12 -0600

    Finishes customer repo

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,4 +1,5 @@
 require_relative './repository'
+require_relative './customer'
 
 class CustomerRepository < Repository
   attr_reader :collection
@@ -11,5 +12,23 @@ class CustomerRepository < Repository
     @collection[customer.id] = customer
   end
 
+  def find_all_by_first_name(first_name)
+    all.find_all { |customer| customer.first_name == first_name }
+  end
 
+  def find_all_by_last_name(last_name)
+    all.find_all { |customer| customer.last_name == last_name }
+  end
+
+  def create(info)
+    info[:id] = find_new_id
+    customer = Customer.new(info)
+    add_customer(customer)
+  end
+
+  def update(id, attributes)
+    customer = find_by_id(id)
+    customer.first_name = attributes[:first_name] if attributes.has_key?(:first_name)
+    customer.last_name = attributes[:last_name] if attributes.has_key?(:last_name)
+  end
 end
