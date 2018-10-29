7a7dbf7f3f4e3d6835e63f6dd07e48dc05dd7fe0
Rene Casco <rene.casco@icloud.com>
2018-10-29 17:45:54 -0600

    invoice_item completed

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,7 +1,26 @@
 class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
+
 
   def initialize(invoice_items_hash)
-
+      @id = invoice_items_hash[:id]
+      @item_id = invoice_items_hash[:item_id]
+      @invoice_id = invoice_items_hash[:invoice_id]
+      @quantity = invoice_items_hash[:quantity]
+      @unit_price = BigDecimal(invoice_items_hash[:unit_price].to_i)/100
+      @created_at = invoice_items_hash[:created_at]
+      @updated_at = invoice_items_hash[:updated_at]
   end
+
+  def unit_price_to_dollars
+    @unit_price.to_f.round(2)
+  end
+
 
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -27,16 +27,29 @@ class InvoiceItemTest < Minitest::Test
     :invoice_id => 8,
     :quantity => 1,
     :unit_price => BigDecimal.new(10.99, 4),
-    :created_at => Time.now.to_s,
-    :updated_at => Time.now.to_s
+    :created_at => Time.now,
+    :updated_at => Time.now
     })
     assert_equal 6, ii.id
     assert_equal 7, ii.item_id
     assert_equal 8, ii.invoice_id
     assert_equal 1, ii.quantity
-    assert_instance_of BigDecimal, ii.unit_price
+    assert_equal BigDecimal.new(10.99, 4), ii.unit_price
     assert_instance_of Time, ii.created_at
     assert_instance_of Time, ii.updated_at
+  end
+
+  def test_it_has_attirbutes
+    ii = InvoiceItem.new({
+    :id => 6,
+    :item_id => 7,
+    :invoice_id => 8,
+    :quantity => 1,
+    :unit_price => BigDecimal.new(1099.111,4),
+    :created_at => Time.now,
+    :updated_at => Time.now
+    })
+    assert_equal 10.99, ii.unit_price_to_dollars
   end
 
 end
