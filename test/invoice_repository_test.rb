bc8fe98c08ad6de22196ca007e84e6eb3c314fd7
Aizar <aizar.cabrera@icloud.com>
2018-10-29 15:43:22 -0600

    adds test_it_can_find_all_by_updated_at passing

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -68,8 +68,8 @@ class InvoiceRepositoryTest < Minitest::Test
     assert_equal nil, @ir.find_by_id(13)
   end
 
-  def test_it_can_find_by_date
-    date = Time.parse("2009-02-07")
-    assert_equal 8, @ir.find_by_updated_at(date)
+  def test_it_can_find_all_by_updated_at
+    date = Time.parse("2014-03-15")
+    assert_instance_of Invoice, @ir.find_all_by_updated_at(date).first
   end
 end
