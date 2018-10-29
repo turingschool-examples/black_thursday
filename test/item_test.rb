9c1d3a56f3ffb8fd8b43bf6f9896260a4e86b2e8
Asa Holt <holt.ace@icloud.com>
2018-10-29 11:39:20 -0600

    test item name

--- test/item_test.rb
+++ test/item_test.rb
@@ -22,4 +22,17 @@ class ItemTest < Minitest::Test
     assert_equal 1, i.id
   end
 
+  def test_it_has_an_name
+    i = Item.new({
+  :id          => 1,
+  :name        => "Pencil",
+  :description => "You can use it to write things",
+  :unit_price  => BigDecimal.new(10.99,4),
+  :created_at  => Time.now,
+  :updated_at  => Time.now,
+  :merchant_id => 2
+})
+    assert_equal "Pencil", i.name
+  end
+
 end
