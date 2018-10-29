2a9a1b844e6fc4e0631db41b34146388304b8ec5
Aizar <aizar.cabrera@icloud.com>
2018-10-29 12:32:15 -0600

    adds fixed test for create, customer_repository

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -16,7 +16,7 @@ class CustomerRepositoryTest < Minitest::Test
 
   def test_it_can_find_customer_by_id
     cr = CustomerRepository.new('./test/data/customer_sample.csv')
-    assert_instance_of Customer, cr.find_by_id("6")
+    assert_instance_of Customer, cr.find_by_id(6)
   end
 
   def test_it_can_find_all_by_first_name
@@ -29,6 +29,13 @@ class CustomerRepositoryTest < Minitest::Test
     cr = CustomerRepository.new('./test/data/customer_sample.csv')
     assert_equal "Ondricka", cr.find_all_by_last_name("Ondricka").first.last_name
     assert_instance_of Customer, cr.find_all_by_last_name("Ondricka").first
+  end
+
+  def test_merchant_repo_can_create_a_merchant
+    cr = CustomerRepository.new('./test/data/customer_sample.csv')
+    new_customer = cr.create({:first_name => 'Duck'})
+    assert_equal 'Duck', new_customer.first_name
+    assert_equal 11, new_customer.id
   end
 
 end
