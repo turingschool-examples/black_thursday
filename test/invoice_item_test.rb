fec39e8c4dc675a3f3f54e136f0c120dfe27284e
Rene Casco <rene.casco@icloud.com>
2018-10-29 17:05:35 -0600

    invoice_item and repo WIP

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -20,4 +20,23 @@ class InvoiceItemTest < Minitest::Test
   assert_instance_of InvoiceItem, ii
   end
 
+  def test_it_has_attirbutes
+    ii = InvoiceItem.new({
+    :id => 6,
+    :item_id => 7,
+    :invoice_id => 8,
+    :quantity => 1,
+    :unit_price => BigDecimal.new(10.99, 4),
+    :created_at => Time.now.to_s,
+    :updated_at => Time.now.to_s
+    })
+    assert_equal 6, ii.id
+    assert_equal 7, ii.item_id
+    assert_equal 8, ii.invoice_id
+    assert_equal 1, ii.quantity
+    assert_instance_of BigDecimal, ii.unit_price
+    assert_instance_of Time, ii.created_at
+    assert_instance_of Time, ii.updated_at
+  end
+
 end
