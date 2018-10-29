48c68f422a9874ff90df7b532c56f5c56e3393a2
Asa Holt <holt.ace@icloud.com>
2018-10-29 12:51:10 -0600

    passing description test

--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,9 @@
 class Item
-  attr_reader :id, :name
+  attr_reader :id, :name, :description
   def initialize(info)
     @id = info[:id]
     @name = info[:name]
+    @description = info[:description]
   end
 
 end
