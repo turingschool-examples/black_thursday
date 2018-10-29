4493fd3777ccb9230ea8359914924fe53d154899
Asa Holt <holt.ace@icloud.com>
2018-10-29 15:24:44 -0600

    test merchant id

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -8,6 +8,12 @@ class MerchantTest < Minitest::Test
     assert_instance_of Merchant, m
   end
 
+  def test_it_has_an_id
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+    assert_equal 5, m.id
+  end
+    
+
 
 
 end
