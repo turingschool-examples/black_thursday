50bf5aad9c97fde4a1da4eac0ad9cc0fa2372da8
Asa Holt <holt.ace@icloud.com>
2018-10-29 15:27:04 -0600

    test merchant has a name

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -12,7 +12,12 @@ class MerchantTest < Minitest::Test
     m = Merchant.new({:id => 5, :name => "Turing School"})
     assert_equal 5, m.id
   end
-    
+
+  def test_it_has_an_name
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+    assert_equal "Turing School", m.name
+  end
+
 
 
 
