2bddc9fe1be8a81c822300bad83c43aefd39e859
Asa Holt <holt.ace@icloud.com>
2018-10-29 12:56:54 -0600

    passing unit price test

--- lib/item.rb
+++ lib/item.rb
@@ -1,9 +1,10 @@
 class Item
-  attr_reader :id, :name, :description
+  attr_reader :id, :name, :description, :unit_price
   def initialize(info)
     @id = info[:id]
     @name = info[:name]
     @description = info[:description]
+    @unit_price = info[:unit_price]
   end
 
 end
