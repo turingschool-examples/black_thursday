19369252acefc8a3ac97fa8e2e2cbae7b72e8584
Asa Holt <holt.ace@icloud.com>
2018-10-29 15:00:58 -0600

    passing updated at test

--- lib/item.rb
+++ lib/item.rb
@@ -3,13 +3,15 @@ class Item
               :name,
               :description,
               :unit_price,
-              :created_at
+              :created_at,
+              :updated_at
   def initialize(info)
     @id = info[:id]
     @name = info[:name]
     @description = info[:description]
     @unit_price = info[:unit_price]
     @created_at = info[:created_at]
+    @updated_at = info[:updated_at]
   end
 
 end
