f5f6f29aba3dfcd9992438d5eff78351dce9bcec
Asa Holt <holt.ace@icloud.com>
2018-10-30 14:50:11 -0600

    modify merchant repo exists test

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/merchant_repository'
 
-class MerchantRepositoryTest
+class MerchantRepositoryTest < Minitest::Test
   def test_it_exists
     mr = MerchantRepository.new
 
