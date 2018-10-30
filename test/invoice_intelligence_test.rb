ca5f470f942da9b6ca1dd9f81fb76e044b9cd456
Ben Lee <bendelonlee@gmail.com>
2018-10-30 17:05:22 -0600

    Adds test for invoice_paid_in_full in true case

--- test/invoice_intelligence_test.rb
+++ test/invoice_intelligence_test.rb
@@ -13,5 +13,12 @@ class InvoiceIntelligenceTest < Minitest::Test
     assert @sa.invoice_has_no_transactions?(invoice[0].id)
   end
 
+  def test_invoice_paid_in_full_can_return_true
+    setup_empty_sales_engine
+    invoice = @se.invoices.create(id: 1, customer_id: 1, merchant_id: 4, status: :shipped)
+    @se.transactions.create(id:1, invoice_id: 1, credit_card_number: 2, result: :success, credit_card_expiration_date: Time.now)
+    assert @sa.invoice_paid_in_full?(invoice[0].id)
+  end
+
 
 end
