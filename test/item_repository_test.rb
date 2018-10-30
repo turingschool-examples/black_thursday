1fc794ebabcefaabc33d8581b87911b1aab4cb35
Asa Holt <holt.ace@icloud.com>
2018-10-30 12:48:43 -0600

    test all returns array of instances

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -8,4 +8,10 @@ class ItemRepositoryTest < Minitest::Test
 
     assert_instance_of ItemRepository, ir
   end
+
+  def test_all_returns_array_of_instances
+    ir = ItemRepository.new
+
+     assert_equal 1367, ir.all
+  end
 end
