83a12e01ab42d648e9ff656b8f82cbf0657ab8ea
Ben Lee <bendelonlee@gmail.com>
2018-10-30 16:59:36 -0600

    adds test_invoice_has_no_transactions

--- test/invoice_intelligence_test.rb
+++ test/invoice_intelligence_test.rb
@@ -7,5 +7,11 @@ require './test/test_data'
 class InvoiceIntelligenceTest < Minitest::Test
   include TestData, TestSetup
 
+  def test_invoice_has_no_transactions
+    setup_empty_sales_engine
+    invoice = @se.invoices.create(id: 1, customer_id: 1, merchant_id: 4, status: :shipped)
+    assert @sa.invoice_has_no_transactions?(invoice[0].id)
+  end
+
 
 end
