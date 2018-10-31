7da74eb20feeb90d90b1bddda0349d564c722ae6
Anten Skrabec <cebarks@gmail.com>
2018-10-31 17:02:56 -0600

    Adds RepositoryTest#test_sum

--- test/statistics_test.rb
+++ test/statistics_test.rb
@@ -10,13 +10,20 @@ class StatisticsTest < Minitest::Test
   def setup
     @rep = ItemRepository.new
   end
+
   def test_most_basic_standard_deviation
     assert_equal 1, @rep.standard_deviation(3,4,5)
   end
+
   def test_standard_deviation
     assert_equal 1.5275252316519465, @rep.standard_deviation(3,4,6)
   end
+
   def test_average
     assert_equal 4, @rep.average(3,4,5)
+  end
+
+  def test_sum
+    assert_equal 10, @rep.sum(*(1..4))
   end
 end
