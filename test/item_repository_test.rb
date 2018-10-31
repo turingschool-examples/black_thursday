3e7eefe5cdd6bc65e8b3e20127b1ceea2e10ecc0
Asa Holt <holt.ace@icloud.com>
2018-10-31 15:50:38 -0600

    fixes item repo tests and refactors

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -3,107 +3,60 @@ require './lib/item_repository'
 require './lib/item'
 
 class ItemRepositoryTest < Minitest::Test
+  def setup
+    @ir = ItemRepository.new
+    @ir.create({
+    :id          => 1,
+    :name        => "Pencil",
+    :description => "You can use it to write things",
+    :unit_price  => BigDecimal.new(10.99,4),
+    :created_at  => Time.now,
+    :updated_at  => Time.now,
+    :merchant_id => 2
+    })
+  end
   def test_it_exists
-    ir = ItemRepository.new
 
-    assert_instance_of ItemRepository, ir
+    assert_instance_of ItemRepository, @ir
   end
 
   def test_all_returns_array_of_instances
-    ir = ItemRepository.new
 
-     assert_equal 1, ir.all.count
+     assert_instance_of Array, @ir.all
   end
 
   def test_find_by_ID
-    ir = ItemRepository.new
-    i = Item.new({
-  :id          => 1,
-  :name        => "Pencil",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 2
-})
 
-    assert_equal i, ir.find_by_ID(1)
+    assert_equal "Pencil", @ir.find_by_ID(1).name
   end
 
   def test_find_by_name
-    ir = ItemRepository.new
-    i = Item.new({
-  :id          => 1,
-  :name        => "Pencil",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 2
-})
 
-    assert_equal i, ir.find_by_name("Pencil")
+    assert_equal 1, @ir.find_by_name("Pencil").id
   end
 
   def test_find_all_with_description
-    ir = ItemRepository.new
-    i = Item.new({
-  :id          => 1,
-  :name        => "Pencil",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 2
-})
-    assert_equal i, ir.find_all_with_description("You can use it to write things")
+
+    assert_equal 1, @ir.find_all_with_description("You can use it to write things").first.id
   end
 
   def find_all_by_price
-    ir = ItemRepository.new
-    i = Item.new({
-  :id          => 1,
-  :name        => "Pencil",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 2
-})
-    assert_equal i, ir.find_all_by_price(BigDecimal.new(10.99,4))
+
+    assert_equal 1, @ir.find_all_by_price(BigDecimal.new(10.99,4)).first.id
   end
 
   def find_all_by_price_in_range
-    ir = ItemRepository.new
-    i = Item.new({
-  :id          => 1,
-  :name        => "Pencil",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 2
-})
-    assert_equal i, ir.find_all_by_price_in_range(1..11)
+
+    assert_equal 1, @ir.find_all_by_price_in_range(1..11).first.id
   end
 
   def find_all_by_merchant_id
-    ir = ItemRepository.new
-    i = Item.new({
-  :id          => 1,
-  :name        => "Pencil",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 2
-})
-    assert_equal i, ir.find_all_by_merchant_id(2)
+
+    assert_equal 1, @ir.find_all_by_merchant_id(2).first.id
   end
 
   def test_create
-    ir = ItemRepository.new
-    ir.create({
+    @ir.create({
   :id          => 100,
   :name        => "Asa",
   :description => "My name",
@@ -112,22 +65,12 @@ class ItemRepositoryTest < Minitest::Test
   :updated_at  => Time.now,
   :merchant_id => 19
 })
-    assert ir.all.map(&:id).include?(100)
+    assert @ir.all.map(&:id).include?(100)
   end
 
   def test_update
-    ir = ItemRepository.new
-    i = Item.new({
-  :id          => 1,
-  :name        => "Pen",
-  :description => "You can use it to write things",
-  :unit_price  => BigDecimal.new(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
-  :merchant_id => 11
-})
 
-    assert_equal i, ir.update(1, {
+    assert_equal @i, @ir.update(1, {
   :name        => "Pen",
   :description => "You can use it to write things",
   :unit_price  => BigDecimal.new(10.99,4),
@@ -138,9 +81,8 @@ class ItemRepositoryTest < Minitest::Test
   end
 
   def test_delete
-    ir = ItemRepository.new
-     ir.delete(5)
+     @ir.delete(5)
 
-    refute ir.all.map(&:id).include?(5)
+    refute @ir.all.map(&:id).include?(5)
   end
 end
