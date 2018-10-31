12c238eb72ed562c8b6a27493a80f02de759ba27
Asa Holt <holt.ace@icloud.com>
2018-10-31 15:57:15 -0600

    fixes merchant repo tests and refactors

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -3,58 +3,50 @@ require './lib/merchant_repository'
 require './lib/merchant'
 
 class MerchantRepositoryTest < Minitest::Test
-  def test_it_exists
-    mr = MerchantRepository.new
 
-    assert_instance_of MerchantRepository, mr
+  def setup
+    @mr = MerchantRepository.new
+    @mr.create({:id => 5, :name => "Turing School"})
+  end
+  def test_it_exists
+
+    assert_instance_of MerchantRepository, @mr
   end
 
   def test_all_returns_array_of_known_merchants
-    mr = MerchantRepository.new
 
-    assert_equal 1, mr.all.count
+    assert_equal 1, @mr.all.count
   end
 
   def test_find_by_id
-    mr = MerchantRepository.new
-    m = Merchant.new({:id => 5, :name => "Turing School"})
 
-    assert_equal m, mr.find_by_id(5)
+    assert_equal "Turing School", @mr.find_by_id(5).name
   end
 
   def test_find_by_name
-    mr = MerchantRepository.new
-    m = Merchant.new({:id => 5, :name => "Turing School"})
 
-    assert_equal m, mr.find_by_name("Turing School")
+    assert_equal 5, @mr.find_by_name("Turing School").id
   end
 
   def test_find_all_by_name
-    mr = MerchantRepository.new
-    m = Merchant.new({:id => 5, :name => "Turing School"})
-    m2 = Merchant.new({:id => 6, :name => "Turing School"})
 
-    assert_equal [m, m2], mr.find_all_by_name("Turing School")
+    assert_equal 5, @mr.find_all_by_name("Turing School").first.id
   end
 
   def test_create
-    mr = MerchantRepository.new
-    mr.create({:id => 10, :name => "Asa"})
+    @mr.create({:id => 10, :name => "Asa"})
 
-    assert mr.all.map(&:id).include?(10)
+    assert @mr.all.map(&:id).include?(10)
   end
 
   def test_update
-    mr = MerchantRepository.new
-    m = Merchant.new({:id => 5, :name => "Programming"})
 
-    assert_equal m, mr.update(5, {:name => "Programming"})
+    assert_equal @mr.all.map(&:name).iclude?("Programming"), @mr.update(5, {:name => "Programming"})
   end
 
   def test_delete
-    mr = MerchantRepository.new
-     mr.delete(5)
+     @mr.delete(5)
 
-    refute mr.all.map(&:id).include?(5)
+    refute @mr.all.map(&:id).include?(5)
   end
 end
