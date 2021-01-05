8c29fcc3de759779a789ea84962cca81f24b4033
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-05 09:48:55 -0700

    Add parsing method and all indexing

--- lib/items_repo.rb
+++ lib/items_repo.rb
@@ -1,1 +1,31 @@
+require 'CSV'
 require './lib/items'
+require 'time'
+require 'bigdecimal'
+require 'bigdecimal/util'
+class ItemsRepo
+  attr_reader :items
+  def initialize(data)
+    @data = data
+    @items = populate_items
+  end
+
+  def populate_items
+    items = []
+    CSV.foreach(@data, headers: true, header_converters: :symbol) do |row|
+      items << Item.new({:id => row[:id],
+                          :name => row[:name],
+                          :description => row[:description],
+                          :unit_price => row[:unit_price],
+                          :merchant_id => row[:merchant_id],
+                          :created_at => row[:created_at],
+                          :updated_at => row[:updated_at]
+                         })
+    end
+    items
+  end
+
+  def all
+    @items
+  end
+end
