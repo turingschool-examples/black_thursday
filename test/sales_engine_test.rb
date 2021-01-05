c9ff28414ba0e8021334eb0bdb282d9c7183be9e
DougWelchons <doug.welchons@gmail.com>
2021-01-04 18:31:47 -0700

    Add readable attrabutes test for sales engine class

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -12,4 +12,12 @@ class SalesEngineTest < Minitest::Test
     assert_instance_of SalesEngine, se
   end
 
+  def test_it_hasa_readable_attrabutes
+    se = SalesEngine.from_csv({
+                              :items     => "./data/items.csv",
+                              :merchants => "./data/merchants.csv",
+                              })
+
+    assert_equal CSV::Table, se.items.class
+  end
 end
