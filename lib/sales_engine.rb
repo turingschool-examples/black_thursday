485540b67adfd1b89ea49222b9da79466cb0fb33
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-05 13:51:44 -0500

    Initialize merchant_repo and file

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,20 @@
+require './lib/merchant_repo'
+
+
 class SalesEngine
-  attr_reader :items,
-              :merchants
+  attr_reader :items
 
   def initialize(data)
-    @items = data[:item]
-    @merchants = data[:merchants]
+    @items = data[:items]
+    @file = data[:merchants]
+    @merchant_repo = MerchantRepository.new(@file)
   end
 
   def self.from_csv(data)
     SalesEngine.new(data)
+  end
+
+  def merchants
+    @merchant_repo.build_merchants
   end
 end
