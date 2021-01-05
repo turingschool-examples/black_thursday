7ce130097f199618bc8a3e72ea49d74a10f1a082
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-05 11:23:22 -0700

    Add item price indexing and rake/simplecov

--- test/items_repo_test.rb
+++ test/items_repo_test.rb
@@ -1,9 +1,5 @@
 require 'CSV'
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/items'
-require './lib/items_repo'
-require 'pry'
+require './test/test_helper'
 
 class ItemsRepoTest < Minitest::Test
 
@@ -28,5 +24,12 @@ class ItemsRepoTest < Minitest::Test
   def test_it_can_find_by_id
     assert_instance_of Item, @repo.find_by_id("263395237")
     assert_nil @repo.find_by_id("111111")
+  end
+
+  def test_it_can_find_by_price
+    assert_instance_of Array, @repo.find_by_price(14900)
+    assert_instance_of Item,  @repo.find_by_price(14900)[0]
+    assert_instance_of BigDecimal, @repo.find_by_price(14900)[0].unit_price
+    assert_instance_of Item,  @repo.find_by_price(14900)[-1]
   end
 end
