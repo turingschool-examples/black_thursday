7a882dd708eb154c863d5a14d426dbf962f353be
kylejschulz <kylschulz@gmail.com>
2021-01-05 17:04:40 -0500

    Add test for existence and attributes for merchant class

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -3,14 +3,17 @@ require 'minitest/pride'
 require './lib/merchant'
 class MerchantTest < Minitest::Test
   def setup
-    @merchant = Merchant.new ()
+    args = {id: 1, name: "Kasey's Pizza"}
+    @merchant = Merchant.new(args[:id], args[:name])
   end
 
   def test_it_exists
-  assert_instance_of Merchant, merchant
+  assert_instance_of Merchant, @merchant
   end
 
   def test_it_has_attributes
+    assert_equal 1, @merchant.id
+    assert_equal "Kasey's Pizza", @merchant.name
 
   end
   def test_it_can_have_different_attributes
