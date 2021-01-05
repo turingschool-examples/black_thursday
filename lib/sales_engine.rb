b710620cb18100f719559fe19e8dc795747adba5
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-05 16:36:03 -0700

    Build path for item/merchant repo to come

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,6 +1,29 @@
+require "csv"
+require "pry"
+
 class SalesEngine
+  attr_reader :items_path,
+              :merchants_path
+
   def initialize(sales_data)
-    @items = sales_data[:items]
-    @merchants = sales_data[:merchants]
+    @items_path = sales_data[:items]
+    @merchants_path = sales_data[:merchants]
   end
+
+  def self.from_csv(sales_data)
+    SalesEngine.new(sales_data)
+  end 
+
+  def items
+    ItemRepository.new(@items_path)    
+    binding.pry
+  end
+
+  def merchants
+    MerchantRepository.new(@merchants_path)
+  end
+
+
 end
+
+
