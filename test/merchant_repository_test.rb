43cd66d5c041010f1eb9444199037a58e41f15ff
kylejschulz <kylschulz@gmail.com>
2021-01-05 16:45:48 -0500

    Add item class and test

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,22 +1,14 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/merchant_repository'
+require_relative './test_helper'
 require './lib/sales_engine'
 class MerchantRepositoryTest < Minitest::Test
   def setup
-    item_path = "./data/items.csv"
     merchant_path = "./data/merchants.csv"
-    arguments = {:merchants => merchant_path,
-                 :items     => item_path,
-                  }
+    arguments = {:merchants => merchant_path}
     @se = SalesEngine.new(arguments)
   end
   def test_it_exists
-
     assert_instance_of MerchantRepository, @se.merchant_repository
-    assert_instance_of ItemRepository, @se.item_repository
     assert_equal  475, @se.merchant_repository.merchants.length
-    assert_equal 1367, @se.item_repository.items.length
   end
 
   def test_it_has_attributes
