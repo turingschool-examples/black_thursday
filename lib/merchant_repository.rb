687f97ce5ff1446ebfe53ad3a40540e039ed3a57
kylejschulz <kylschulz@gmail.com>
2021-01-05 18:01:29 -0500

    Add find method tests and methods for merchant repo class

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -22,24 +22,32 @@ class MerchantRepository
   end
 
   def find_by_id(id)
-    merchants.find_all do |merchant|
-      merchant[:id] == id
+     id_found = merchants.find_all do |merchant|
+      merchant.id == id
+    end
+    if id_found.empty?
+      return nil
+    else
+      id_found
     end
   end
 
   def find_by_name(name)
-    merchants.find do |merchant|
-      merchant[:name].downcase == name.downcase
+    name_found = merchants.find do |merchant|
+      merchant.name.downcase == name.downcase
     end
   end
 
   def find_all_by_name(name)
-    found_merchants = merchants.find do |merchant|
-      merchant[:name].downcase == name.downcase
+    found_merchants = []
+    merchants.find do |merchant|
+      found_merchants << merchant if merchant.name.downcase == name.downcase
     end
+    found_merchants
   end
 
-  def create(attributes)
+  def create(name)
+    @merchants << Merchant.new(highest_merchant_id_plus_one, name)
   end
 
   def update(id, attributes)
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,8 +1,17 @@
 require_relative './test_helper'
 require './lib/sales_engine'
+require './lib/merchant'
 require './lib/merchant_repository'
 class MerchantRepositoryTest < Minitest::Test
   def setup
+    args_1 = {id: 12334105, name: "Shopin1901"}
+    args_2 = {id: 12334112, name: "Candisart"}
+    args_3 = {id: 12334113, name: "MiniatureBikez"}
+    args_4 = {id: 12334115, name: "LolaMarleys"}
+    @merchant_1 = Merchant.new(args_1[:id], args_1[:name])
+    @merchant_2 = Merchant.new(args_2[:id], args_2[:name])
+    @merchant_3 = Merchant.new(args_3[:id], args_3[:name])
+    @merchant_4 = Merchant.new(args_4[:id], args_4[:name])
     merchant_path = "./data/merchant_repo_test.csv"
     arguments = merchant_path
     @mr = MerchantRepository.new(arguments)
@@ -10,7 +19,7 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
     assert_instance_of MerchantRepository, @mr
-    assert_equal  4, @mr.merchants.length
+    assert_equal 4, @mr.merchants.length
   end
 
   def test_it_has_attributes
@@ -22,5 +31,20 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_it_can_return_all_merchants
     assert_equal 4, @mr.all.length
+  end
+
+  def test_it_can_find_merchants_by_id
+    assert_equal [@merchant_1], @mr.find_by_id(12334105)
+    assert_nil @mr.find_by_id(123341059)
+  end
+
+  def test_it_can_find_merchants_by_name
+    assert_equal @merchant_1, @mr.find_by_name("Shopin1901")
+    assert_nil @mr.find_by_name("Shopin1901fg")
+  end
+
+  def test_it_can_find__all_merchants_by_name
+    assert_equal [@merchant_2], @mr.find_all_by_name("Candisart")
+    assert_equal [], @mr.find_all_by_name("Candisart_2")
   end
 end
