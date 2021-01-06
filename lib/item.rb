430ef7b84add3549e89ccb4546f976082c76d6c1
DougWelchons <doug.welchons@gmail.com>
2021-01-05 21:42:57 -0700

    Add Item refactors

--- lib/item.rb
+++ lib/item.rb
@@ -15,13 +15,13 @@ class Item
     @id           = data[:id].to_i
     @name         = data[:name]
     @description  = data[:description]
-    @unit_price   = BigDecimal.new(data[:unit_price])
+    @unit_price   = BigDecimal.new((data[:unit_price].to_i)) / 100
     @merchant_id  = data[:merchant_id].to_i
-    @created_at   = data[:created_at]
-    @updated_at   = data[:updated_at]
+    @created_at   = Time.parse(data[:created_at])
+    @updated_at   = Time.parse(data[:updated_at])
   end
 
   def unit_price_to_dollars
-    @unit_price.to_f / 100
+    @unit_price.to_f
   end
 end
