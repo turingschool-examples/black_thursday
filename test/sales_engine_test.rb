60b98cc3869084118d3f73703dabdec261bda9db
Andrew Johnston <av.johnst@gmail.com>
2021-01-06 13:47:22 -0700

    Finish sales_engine_test file

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,23 +1,39 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-  def test_it_exists
-    se = SalesEngine.from_csv({
+  def setup
+    @file_items = CSV.readlines("./data/items.csv", headers: true, header_converters: :symbol)
+    @file_merchants = CSV.readlines("./data/merchants.csv", headers: true, header_converters: :symbol)
+    @se = SalesEngine.from_csv({
                               :items     => "./data/items.csv",
                               :merchants => "./data/merchants.csv",
                               })
-
-    assert_instance_of SalesEngine, se
   end
 
-  def test_it_hasa_readable_attrabutes
-    se = SalesEngine.from_csv({
+  def test_it_exists
+    assert_instance_of SalesEngine, @se
+  end
+
+  def test_it_can_from_csv
+    sales_engine = SalesEngine.from_csv({
                               :items     => "./data/items.csv",
                               :merchants => "./data/merchants.csv",
                               })
+    assert_instance_of SalesEngine, sales_engine
+  end
 
-    assert_equal CSV::Table, se.items.class
+  def test_it_has_readable_attributes
+    assert_equal ItemRepository, @se.items.class
+    assert_equal MerchantRepository, @se.merchants.class
+  end
+
+  def test_it_can_create_arrays_of_items_and_merchants
+    assert_instance_of Array, @se.item_repository(@file_items).items
+    assert_instance_of Array, @se.merchant_repository(@file_merchants).merchants
   end
 end
