c09cc99c692bd1ce1bc26b9ff406ecc56cf6e772
Dominic Padula <domopadula2192@gmail.com>
2021-01-06 16:02:53 -0700

    Passing tests through delete for merchant repo

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -6,6 +6,12 @@ require './lib/sales_engine'
 require 'csv'
 
 class MerchantRepositoryTest < Minitest::Test
+
+  def setup
+    mr = MerchantRepository.new("./data/merchants.csv")
+    @merchants = []
+
+  end
   def test_it_exists_and_has_attributes
     mr = MerchantRepository.new("./data/merchants.csv")
 
@@ -46,5 +52,24 @@ class MerchantRepositoryTest < Minitest::Test
 
     assert_equal [m].count, mr.find_all_by_name("andisar").count
     assert_equal [], mr.find_all_by_name("abc")
+  end
+
+  def test_create_attributes_adds_merchant_objects
+    mr = MerchantRepository.new("./data/merchants.csv")
+    mr.create({:name => "Ruby"})
+    assert_equal 12337412, mr.merchants.last.id
+  end
+
+  def test_update_changes_merchant_object_names
+    mr = MerchantRepository.new("./data/merchants.csv")
+    mr.update(12334112, "Harry's Appliances")
+    assert_equal "Harry's Appliances", mr.find_by_id(12334112).name
+
+  end
+
+  def test_delete_removes_merchant_object
+    mr = MerchantRepository.new("./data/merchants.csv")
+    mr.delete(12334112)
+    assert_nil mr.find_by_id(12334112)
   end
 end
