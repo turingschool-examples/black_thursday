acc762eaefe7512be77b516e0539d3120e304afc
Gvieve <gnuebel84@yahoo.com>
2021-01-06 16:02:23 -0700

    Update time_store method to account for nil value

--- lib/time_store_module.rb
+++ lib/time_store_module.rb
@@ -2,7 +2,9 @@ require "time"
 
 module TimeStoreable
   def time_store(time_data)
-    if time_data.class == String
+    if time_data.nil?
+      Time.now
+    elsif time_data.class == String
       Time.parse(time_data)
     else
       time_data
