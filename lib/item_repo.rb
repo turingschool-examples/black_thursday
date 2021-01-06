e436d506665d2860865d388daf245fc37ab5668a
Wil McCauley <wmccauley724@gmail.com>
2021-01-05 17:31:06 -0700

    Add cleaner to initialize, add methods: read_contents, all

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -1,4 +1,5 @@
 require 'CSV'
+require './lib/cleaner'
 
 class ItemRepository
 
@@ -6,16 +7,18 @@ class ItemRepository
 
   def initialize(file='./data/items.csv')
     @file = file
+    @cleaner = Cleaner.new
     @items = []
-    @data = CSV.open @file, headers: true, header_converters: :symbol
+    # @data = CSV.open @file, headers: true, header_converters: :symbol
+    @contents = @cleaner.open_csv(@file)
+  end
+
+  def read_contents
+    File.read(@file)
   end
 
   def all
-    
+    read_contents
   end
-
-
-
-
 
 end
