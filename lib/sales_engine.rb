61c6b2591346b236e09473719e62635baf091061
Trevor Robinson <trevor.robi1254@gmail.com>
2021-01-05 17:17:45 -0700

    Create functional sales engine first steps

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,15 +1,38 @@
 require 'csv'
+require 'pry'
 class SalesEngine
-  attr_reader:item_path,
-             :merchant_path
+  attr_reader :merchants,
+              :items
+
   def initialize(locations)
-  @item_path = locations[:items]
-  @merchant_path = locations[:merchants]
+    @merchants = MerchantRepository.new(locations[:merchants])
+    @items = ItemRepository.new(locations[:items])
   end
 
   def self.from_csv(locations)
-    first_row = CSV.read("./data/merchants.csv") do |line|
-      p line
-    end
+    SalesEngine.new(locations)
   end
+
+  # def read_merchant(locations)
+  #   CSV.foreach(locations[:merchants], headers: :true , header_converters: :symbol) do |row|
+  #      # ;binding.pry
+  #
+  #     # Merchant.new(row)
+  #
+  #     @merchants << Merchant.new(row)
+  #
+  #
+  #   # first_row = CSV.read(locations) do |line|
+  #   # rows.map do |row|
+  #
+  #   end
+  #   ;binding.pry
+  #   return @merchants
+  #
+  # end
+
+  def all
+    puts @merchants
+  end
+
 end
