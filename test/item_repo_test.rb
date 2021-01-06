45002ced444ad903fa0186b0217a0f317b0583ec
Joseph Budina <joebud3@gmail.com>
2021-01-05 20:52:45 -0700

    Add all items test

--- test/item_repo_test.rb
+++ test/item_repo_test.rb
@@ -5,15 +5,20 @@ require './lib/item_repo'
 
 class ItemRepositoryTest < Minitest::Test
 
-  def test_it_exists
-    ir = ItemRepository.new
-    assert_instance_of ItemRepository, ir
+  def setup
+    @item_repository = ItemRepository.new
+  end
 
+  def test_it_exists
+    assert_instance_of ItemRepository, @item_repository
   end
 
   def test_it_returns_all_items
-    skip
+    assert_equal 1367, @item_repository.all
   end
 
+  def test_it_can_find_by_id
+    id = 263538760
+  end
 
 end
