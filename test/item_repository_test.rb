97a1bd471e709ac138882efaf01606a5f89e7a1a
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-05 17:30:55 -0700

    Create find_by_name test

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -17,22 +17,24 @@ class ItemRepositoryTest < Minitest::Test
   end
 
   def test_find_by_id_finds_an_item_by_id
+    result_1 = @ir.find_by_id(263538760)
 
-    result = @ir.find_by_id(263538760)
-    require "pry"; binding.pry
+    assert_equal 263538760, result_1.id.to_i
+    assert_equal "Puppy blankie", result_1.name
 
-    assert_equal 263538760, result.id
-          #
-          #
-          # expect(expected.id).to eq id
-          # expect(expected.name).to eq "Puppy blankie"
-          #
-          #
-          #
-          #
-          # id = 1
-          # expected = engine.items.find_by_id(id)
-          #
-          # expect(expected).to eq nil
+    result_2 = @ir.find_by_id(1)
+
+    assert_nil nil, result_2
+  end
+
+  def test_find_by_name_finds_an_item_by_name
+    result_1 = @ir.find_by_name("Puppy blankie")
+
+    assert_equal "Puppy blankie", result_1.name
+    assert_equal 263538760, result_1.id.to_i
+
+    result_2 = @ir.find_by_name("Sales Engine")
+
+    assert_nil nil, result_2
   end
 end
