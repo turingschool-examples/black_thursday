b71ce0716b28b9052906df7d38590ef11aae9bd1
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-05 21:32:11 -0700

    Refactor methods

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,5 +1,6 @@
 class MerchantRepository
-  attr_reader :data
+  attr_reader :data,
+              :merchant_info
 
 
   def initialize(data)
@@ -18,18 +19,36 @@ class MerchantRepository
   end
 
   def all
-    @merchant_info
+    @merchant_info.values
   end
 
   def find_by_id(id)
-    all.find do |merchant|
+    @merchant_info.find do |merchant|
       merchant[0] == id.to_s
     end
   end
 
+  # def find_by_id(id)
+  #   all.find do |merchant|
+  #     merchant[0] == id.to_s
+  #   end
+  # end
+
   def find_by_name(name)
-    all.find do |merchant|
+    @merchant_info.find do |merchant|
       merchant[1].name.upcase == name.upcase
+    end
+  end
+
+  # def find_by_name(name)
+  #   all.find do |merchant|
+  #     merchant[1].name.upcase == name.upcase
+  #   end
+  # end
+
+  def find_all_by_name(search_string)
+    @merchant_info.find_all do |key, value|
+      value.name.upcase.include?(search_string.upcase)
     end
   end
 
@@ -39,28 +58,40 @@ class MerchantRepository
   #   end
   # end
 
+  def find_all_by_name(search_string)
+    @merchant_info.find_all do |key, value|
+      value.name.upcase.include?(search_string.upcase)
+    end
+  end
+
   # def max_id
   #   all.max_by do |key, record|
   #     record.id
   #   end
   # end
-  #
-  # def new_id
-  #   max_id[0].to_i + 1
-  # end
 
-  # def create(new_name)
-  #   @merchant_info[:new_id.to_s] = Merchant.new({:id => new_id.to_s, :name => new_name})
-  # end
+  def max_id
+    @merchant_info.max_by do |key, record|
+      record.id
+    end
+  end
 
-  # def update(id, new_name)
-  #   @merchant_info[id] = Merchant.new({:id => id, :name => new_name})
-  # end
+  def new_id
+    max_id[0].to_i + 1
+  end
 
-  # def delete(id)
-  #   @merchant_info.delete_if do |key, value|
-  #     key == id
-  #   end
-  # end
+  def create(new_name)
+    @merchant_info[:new_id.to_s] = Merchant.new({:id => new_id.to_s, :name => new_name})
+  end
+
+  def update(id, new_name)
+    @merchant_info[id] = Merchant.new({:id => id, :name => new_name})
+  end
+
+  def delete(id)
+    @merchant_info.delete_if do |key, value|
+      key == id
+    end
+  end
 
 end
