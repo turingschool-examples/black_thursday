5e9d29bebdef9689da46afc80e3758a46a8886a9
Trevor Suter <Trevorsuter@icloud.com>
2021-01-06 15:37:26 -0700

    added tests for item objects, added to find item by id

--- test/item_repo_test.rb
+++ test/item_repo_test.rb
@@ -1,12 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-
 require './lib/item_repo'
+require './lib/cleaner'
 
 class ItemRepositoryTest < Minitest::Test
 
   def setup
     @item_repository = ItemRepository.new
+    @cleaner = Cleaner.new
   end
 
   def test_it_exists
@@ -14,11 +15,20 @@ class ItemRepositoryTest < Minitest::Test
   end
 
   def test_it_returns_all_items
-    assert_equal 1367, @item_repository.all
+    assert_equal 1367, @item_repository.all.length
   end
 
   def test_it_can_find_by_id
-    id = 263538760
+    # require 'pry'; binding.pry
+      assert_equal 263538760, @item_repository.find_item_by_id(263538760).id
+      assert_equal "Puppy blankie", @item_repository.find_item_by_id(263538760).name
+      assert_equal nil, @item_repository.find_item_by_id(1)
   end
 
+  def test_item_objects
+    csv = (@cleaner.open_csv('./data/items.csv'))
+    assert_equal 1367, @item_repository.item_objects(csv).length
+    # require 'pry'; binding.pry
+    assert_equal "Free standing Woden letters", @item_repository.item_objects(csv)[3].name
+  end
 end
