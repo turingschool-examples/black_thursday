12678148bf95781ff6f2252faa78700e944b7e1c
Wil McCauley <wmccauley724@gmail.com>
2021-01-05 17:05:41 -0700

    Add item repo tests - passing it_exists

---
+++ test/item_repo_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './lib/item_repo'
+
+class ItemRepositoryTest < Minitest::Test
+
+  def test_it_exists
+    ir = ItemRepository.new
+
+    assert_instance_of ItemRepository, ir
+  end
+
+  def test_it_returns_all_items
+    skip
+  end
+
+
+end
