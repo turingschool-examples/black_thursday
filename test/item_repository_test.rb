cfbe4e545d96670189c90ac1e12b68d3f94b263e
Adam Bowers <adam.j.bowers@gmail.com>
2021-01-05 17:18:05 -0700

    Create all and find_by_id tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,19 +1,38 @@
 require_relative './test_helper'
 require './lib/sales_engine'
+
 class ItemRepositoryTest < Minitest::Test
   def setup
-    item_path = "./data/items.csv"
-    merchant_path = "./data/merchants.csv"
-    arguments = {:items => item_path}
-    @se = SalesEngine.new(arguments)
-  end
-  def test_it_exists
-    assert_instance_of ItemRepository, @se.item_repository
-    assert_equal 1367, @se.item_repository.items.length
+    # item_path = "./data/items.csv"
+    # arguments = {:items => item_path}
+    @ir = ItemRepository.new("./data/items.csv")
   end
 
-  def test_it_has_attributes
+  def test_it_exists
+    assert_instance_of ItemRepository, @ir
   end
-  def test_it_can_have_different_attributes
+
+  def test_all_displays_all_items
+    assert_equal 1367, @ir.all
+  end
+
+  def test_find_by_id_finds_an_item_by_id
+
+    result = @ir.find_by_id(263538760)
+    require "pry"; binding.pry
+
+    assert_equal 263538760, result.id
+          #
+          #
+          # expect(expected.id).to eq id
+          # expect(expected.name).to eq "Puppy blankie"
+          #
+          #
+          #
+          #
+          # id = 1
+          # expected = engine.items.find_by_id(id)
+          #
+          # expect(expected).to eq nil
   end
 end
