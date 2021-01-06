cd62dc03e2e887fae43b163dab85b11dfe94ea21
Wil McCauley <wmccauley724@gmail.com>
2021-01-05 17:04:44 -0700

    Add initialize with argument: file

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -1,7 +1,21 @@
+require 'CSV'
+
 class ItemRepository
 
+  attr_reader :data
+
   def initialize(file='./data/items.csv')
+    @file = file
+    @items = []
+    @data = CSV.open @file, headers: true, header_converters: :symbol
+  end
+
+  def all
     
   end
+
+
+
+
 
 end
