2c2b37c80d0137629ad0f2d7fb8e2715ddf55a60
Trevor Suter <Trevorsuter@icloud.com>
2021-01-06 15:36:18 -0700

    added item objects, fixed find item by id

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -1,32 +1,42 @@
 require 'CSV'
 require './lib/cleaner'
+require './lib/item'
 
 class ItemRepository
   attr_reader 
 
   def initialize(file = './data/items.csv')
     @file = file
-    @cleaner = Cleaner.new
     @items = []
-    # @data = CSV.open @file, headers: true, header_converters: :symbol
-    @contents = @cleaner.open_csv(@file)
+    @cleaner = Cleaner.new
+    @items_csv = @cleaner.open_csv(@file)
   end
 
-  def read_contents
-    File.read(@file)
+  def item_objects(items)
+    items.each do |item|
+     @items << Item.new({:id => item[:id].to_i,
+                :name        => item[:name],
+                :description => item[:description],
+                :unit_price  => item[:unit_price],
+                :created_at  => item[:created_at],
+                :updated_at  => item[:updated_at],
+                :merchant_id => item[:merchant_id]})
+    end
+    @items
   end
 
   def all
-    @contents.each do |row|
-      @items << row
+    @items_csv.map do |row|
+      row
     end
-    @items.length
   end
-
-  # def all
-  #   read_contents
-  # end
 
   def find_item_by_id(id)
+    name = []
+    item_objects(@items_csv).each do |item|
+      name << item if item.id == id
+    end
+    name[0]
   end
+
 end
