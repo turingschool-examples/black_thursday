ae2ea08d420fb1314322d87195f681e745cd50b7
Gvieve <gnuebel84@yahoo.com>
2021-01-05 19:20:11 -0700

    Create time_store_module for create/update dates

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,19 +1,18 @@
 require "csv"
 require "pry"
+require_relative "./time_store_module"
 
-class Merchant 
+class Merchant
+  include TimeStoreable
+
   attr_reader :id
 
   attr_accessor :name
 
   def initialize(data)
      @id = data[:id].to_i
-     @name = data[:name].to_s
-    
+     @name = data[:name]
+     @created_at = time_store(data[:created_at])
+     @updated_at = time_store(data[:updated_at])
   end
-
-  def created_at
-    #make hash here
-  end
-
 end
