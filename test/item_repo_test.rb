ce1a70475e4f6efa55ee239cf2dc602e3f96c751
Wil McCauley <wmccauley724@gmail.com>
2021-01-06 16:46:13 -0700

    Add tests for find all by description method

--- test/item_repo_test.rb
+++ test/item_repo_test.rb
@@ -37,4 +37,17 @@ class ItemRepositoryTest < Minitest::Test
     assert_equal 263538760, @item_repository.find_by_name("Puppy blankie").id
     assert_equal nil, @item_repository.find_by_name("SalesEngine")
   end
+
+  def test_it_finds_all_with_description
+      expected = "A large Yeti of sorts, casually devours a cow as the others watch numbly."
+      assert_equal expected, @item_repository.find_all_with_description(expected)[0].description
+      assert_equal 263550472, @item_repository.find_all_with_description(expected).first.id
+
+      bad_case = "A LARGE yeti of SOrtS, casually devoURS a COw as the OTHERS WaTch NUmbly."
+      assert_equal expected, @item_repository.find_all_with_description(bad_case)[0].description
+      assert_equal 263550472, @item_repository.find_all_with_description(expected).first.id
+
+      description = "Sales Engine is a relational database"
+      assert_equal 0, @item_repository.find_all_with_description(description).length
+  end
 end
