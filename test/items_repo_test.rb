964f680910bbbf56089f81ad4dd3fbc184710ee8
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-01-05 21:09:55 -0700

    Change @repo to @dummy_repo

--- test/items_repo_test.rb
+++ test/items_repo_test.rb
@@ -9,42 +9,42 @@ class ItemsRepoTest < Minitest::Test
   end
 
   def test_it_is
-    assert_instance_of ItemsRepo, @repo
+    assert_instance_of ItemsRepo, @dummy_repo
   end
 
   def test_it_has_attributes
-    assert_instance_of Hash , @repo.items
+    assert_instance_of Hash , @dummy_repo.items
   end
 
   def test_it_can_populate
-  	assert_instance_of Hash, @repo.populate_items
+  	assert_instance_of Hash, @dummy_repo.populate_items
   end
 
   def test_it_can_gather_all_items
-    assert_instance_of Item, @repo.all.values[0]
-    assert_instance_of Item, @repo.all.values[-1]
-    assert_equal 1367, @repo.all.values.length
+    assert_instance_of Item, @dummy_repo.all.values[0]
+    assert_instance_of Item, @dummy_repo.all.values[-1]
+    assert_equal 4, @dummy_repo.all.values.length
   end
 
   def test_it_can_find_by_id
-    assert_instance_of Item, @repo.find_by_id("263395237")
-    assert_nil @repo.find_by_id("111111")
+    assert_instance_of Item, @dummy_repo.find_by_id("123")
+    assert_nil @dummy_repo.find_by_id("111111")
   end
 
   def test_it_can_find_by_price
-    assert_instance_of Array, @repo.find_by_price(14900)
-    assert_instance_of Item,  @repo.find_by_price(14900)[0]
-    assert_instance_of BigDecimal, @repo.find_by_price(14900)[0].unit_price
-    assert_instance_of Item,  @repo.find_by_price(14900)[-1]
+    assert_instance_of Array, @dummy_repo.find_by_price(14900)
+    assert_instance_of Item,  @dummy_repo.find_by_price(200)[0]
+    assert_instance_of BigDecimal, @dummy_repo.find_by_price(200)[0].unit_price
+    assert_instance_of Item,  @dummy_repo.find_by_price(46000)[-1]
   end
 
 
   def test_it_can_find_by_name
-  	assert_equal "263399475", @repo.find_by_name("Ironstone Pitcher (Small)").id
+  	assert_equal "345", @dummy_repo.find_by_name("etsy").id
   end
 
   def test_it_can_find_by_description
-  	data_set = @repo.find_all_with_description("Size 3m")
+  	data_set = @dummy_repo.find_all_with_description("Size 3m")
 
   	sub_str = ["Size","3m"]
   	data_set.each do |item|
