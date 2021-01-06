9126aeddadf750dab93e99754e8d640644287fd0
Dominic Padula <domopadula2192@gmail.com>
2021-01-06 16:48:33 -0700

    Passing code for item object

---
+++ lib/item.rb
@@ -0,0 +1,23 @@
+class Item
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :created_at,
+              :updated_at,
+              :merchant_id
+
+  def initialize(info)
+    @id = info[:id]
+    @name = info[:name]
+    @description = info[:description]
+    @unit_price = info[:unit_price]
+    @created_at = info[:created_at]
+    @updated_at = info[:updated_at]
+    @merchant_id = info[:merchant_id]
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
+  end
+end
