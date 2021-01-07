d7c6cf499482e1885aad78cebc80f3621c7d2ffd
Gvieve <gnuebel84@yahoo.com>
2021-01-06 18:07:37 -0700

    Refactor, add method for highest_id, bring down SalesEngine
    -Create method for new_highest_id when using create method
    -Refactor code to follow formatting convention
    -Bring down SalesEngine into initialize

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,12 +1,14 @@
-require "pry"
-require "csv"
-require_relative "./merchant"
+require 'pry'
+require 'csv'
+require_relative './merchant'
 
 class MerchantRepository
-  attr_reader :all
+  attr_reader :all,
+              :engine
 
-  def initialize(merchants_path)
+  def initialize(merchants_path, engine)
     @all = []
+    @engine = engine
 
     CSV.foreach(merchants_path, headers: true, header_converters: :symbol) do |row|
       @all << convert_to_merchant(row)
@@ -14,7 +16,11 @@ class MerchantRepository
   end
 
   def convert_to_merchant(row)
-    row = Merchant.new({id: row[:id], name: row[:name], created_at: row[:created_at], updated_at: row[:updated_at]})
+    row = Merchant.new({id: row[:id],
+                        name: row[:name],
+                        created_at: row[:created_at],
+                        updated_at: row[:updated_at]},
+                        self)
   end
 
   def find_by_id(id)
