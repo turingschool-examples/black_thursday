c312c0efb03413172a2435649e8ab0151faf3938
Wil McCauley <wmccauley724@gmail.com>
2021-01-07 15:51:55 -0700

    Add additonal state test to test it can delete from the middle of the data

--- test/item_repo_test.rb
+++ test/item_repo_test.rb
@@ -95,7 +95,7 @@ class ItemRepositoryTest < Minitest::Test
     assert_equal 263395721, @item_repository.sort_by_id[2].id
   end
 
-  def test_it_deletes_item
+  def test_it_deletes_items
     attributes = {
       name: "Capita Defenders of Awesome 2018",
       description: "This board both rips and shreds",
@@ -108,8 +108,13 @@ class ItemRepositoryTest < Minitest::Test
     @item_repository.create(attributes)
     assert_equal "Capita Defenders of Awesome 2018", @item_repository.find_item_by_id(263567475)[0].name
     assert_equal 1368, @item_repository.items.length
+
     @item_repository.delete(263567475)
     assert_equal nil, @item_repository.find_item_by_id(263567475)
     assert_equal 1367, @item_repository.items.length
+
+    @item_repository.delete(263538760)
+    assert_equal nil, @item_repository.find_item_by_id(263538760)
+    assert_equal 1366, @item_repository.items.length
   end
 end
