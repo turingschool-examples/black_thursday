9a98c149f9f0df7eb4fbebb2637b3d4c0fb098c5
Olivia Charles <ocharles92@gmail.com>
2021-01-06 21:05:25 -0500

    Commented out merchant repo info

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,15 +2,15 @@ require_relative './test_helper'
 require_relative './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-  def setup
-  end
-
-  def test_it_exists
-
-  assert_instance_of
-  end
-  def test_it_has_attributes
-  end
-  def test_it_can_have_different_attributes
-  end
+  # def setup
+  # end
+  #
+  # def test_it_exists
+  #
+  # assert_instance_of
+  # end
+  # def test_it_has_attributes
+  # end
+  # def test_it_can_have_different_attributes
+  # end
 end
