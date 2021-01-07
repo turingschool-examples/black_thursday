7a2be42f2d835549e5681e74b3a0363c2b4f5326
Olivia Charles <ocharles92@gmail.com>
2021-01-06 20:16:21 -0500

    Created method update for item repository

--- lib/item.rb
+++ lib/item.rb
@@ -8,14 +8,22 @@ class Item
               :merchant_id
 
   def initialize(args)
-    @args = args
-    @id = args[:id].to_s 
-    @name = args[:name].to_s
-    @description  = args[:description].to_s
-    @unit_price = args[:unit_price].to_s
-    @created_at = args[:created_at].to_s
-    @updated_at = args[:updated_at].to_s
+    @args        = args
+    @id          = args[:id].to_s
+    @name        = args[:name].to_s
+    @description = args[:description].to_s
+    @unit_price  = args[:unit_price].to_s
+    @created_at  = args[:created_at].to_s
+    @updated_at  = args[:updated_at].to_s
     @merchant_id = args[:merchant_id].to_s
+  end
+
+  def update(args)
+
+    @unit_price  = args[:unit_price].to_f.to_s if !args[:unit_price].nil?
+    @description = args[:description].to_s if !args[:description].nil?
+    @name        = args[:name].to_s  if !args[:name].nil?
+    @updated_at  = Time.new.to_s
   end
 
 
