bc2a2b260e8a7a7777f76facb1aa392b8ab1edd8
Olivia Charles <ocharles92@gmail.com>
2021-01-06 21:04:57 -0500

    Commented out merchant repo info

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,18 +1,18 @@
 require_relative './test_helper'
 require './lib/sales_engine'
 class MerchantRepositoryTest < Minitest::Test
-  def setup
-    merchant_path = "./data/merchants.csv"
-    arguments = {:merchants => merchant_path}
-    @se = SalesEngine.new(arguments)
-  end
-  def test_it_exists
-    assert_instance_of MerchantRepository, @se.merchant_repository
-    assert_equal  475, @se.merchant_repository.merchants.length
-  end
-
-  def test_it_has_attributes
-  end
-  def test_it_can_have_different_attributes
-  end
+  # def setup
+  #   merchant_path = "./data/merchants.csv"
+  #   arguments = {:merchants => merchant_path}
+  #   @se = SalesEngine.new(arguments)
+  # end
+  # def test_it_exists
+  #   assert_instance_of MerchantRepository, @se.merchant_repository
+  #   assert_equal  475, @se.merchant_repository.merchants.length
+  # end
+  #
+  # def test_it_has_attributes
+  # end
+  # def test_it_can_have_different_attributes
+  # end
 end
