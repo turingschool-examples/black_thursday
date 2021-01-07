802843beef6614f63a4d8dee4dbd656c1536f7c2
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-07 18:50:08 -0500

    Create update merchant method and helper method

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -12,4 +12,20 @@ class Merchant
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
   end
+
+  def update(symbol, new_value)
+    instance_var_to_string
+    if @id_1 == symbol.to_s
+      @id = new_value
+    elsif @name_1 == symbol.to_s
+      @name = new_value
+    end
+  end
+
+  def instance_var_to_string
+    @repo_1 = "repo"
+    @id_1 = "id"
+    @name_1 = "name"
+  end
+
 end
