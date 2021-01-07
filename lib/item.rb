042967cacdbbf3db7fbc9e748b1a110ec2ad5103
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-06 21:49:29 -0700

    Write some methods/tests for items

--- lib/item.rb
+++ lib/item.rb
@@ -2,23 +2,25 @@ require_relative './time_store_module'
 
 class Item
   include TimeStoreable
+
+  attr_accessor :name,
+               :descripition,
+               :unit_price
+
   attr_reader :id,
               :repository,
-              :name,
-              :description,
-              :unit_price,
               :created_at,
               :updated_at,
               :merchant_id
 
   def initialize(data, repository)
     @repository   = repository
-    @id           = data[:id]
+    @id           = data[:id].to_i
     @name         = data[:name]
     @description  = data[:description]
-    @unit_price   = data[:unit_price]
+    @unit_price   = data[:unit_price].to_f
     @created_at   = time_store(data[:created_at])
     @updated_at   = time_store(data[:updated_at])
-    @merchant_id  = data[:merchant_id]
+    @merchant_id  = data[:merchant_id].to_i
   end
 end
