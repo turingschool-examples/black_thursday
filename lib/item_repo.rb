932dfbce87adbbaa0e214c1fabfdfdbc34f7f698
Regina Casias <reginaannvigil@gmail.com>
2021-01-06 18:31:04 -0800

    Added tests and methods to item repo

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -19,4 +19,44 @@ class ItemRepo
   def all
     item_list
   end
+
+  def find_by_id(id)
+    if id.nil?
+      nil
+    else
+       @item_list.find do |item|
+        item.id == id
+      end
+    end
+  end
+
+  def find_by_name(name)
+    if name.nil?
+      nil
+    else
+       @item_list.find do |item|
+        item.name == name
+      end
+    end
+  end
+
+  def find_all_by_description(description)
+    description = description.downcase
+
+    return [] if description.nil?
+
+    @item_list.find_all do |item|
+      item.description.downcase.include?(description)
+    end
+  end
+
+  def find_all_by_price(price)
+    return [] if price.nil?
+
+    @item_list.find_all do |item|
+      item.unit_price_to_dollars == price
+    end
+  end
+
+
 end
--- test/item_repo_test.rb
+++ test/item_repo_test.rb
@@ -5,6 +5,7 @@ require './lib/merchant_repo'
 require './lib/item_repo'
 require './lib/item'
 require './lib/merchant'
+require 'mocha/minitest'
 
 class ItemRepoTest < Minitest::Test
 
@@ -24,5 +25,47 @@ class ItemRepoTest < Minitest::Test
                               })
     ir   = se.items
     assert_equal ir.item_list, ir.all
+  end
+
+  def test_find_by_name
+    se = SalesEngine.from_csv({
+                              :items     => "./data/items.csv",
+                              :merchants => "./data/merchants.csv"
+                              })
+    ir   = se.items
+    item = ir.find_by_name("Antique Rocking Horse")
+    assert_equal "Antique Rocking Horse", item.name
+  end
+
+  def test_find_by_name_nil
+    se = SalesEngine.from_csv({
+                              :items     => "./data/items.csv",
+                              :merchants => "./data/merchants.csv"
+                              })
+    ir   = se.items
+    item = ir.find_by_name(" ")
+    assert_nil item
+  end
+
+  def test_find_all_with_description
+    se = SalesEngine.from_csv({
+                              :items     => "./data/items_sample.csv",
+                              :merchants => "./data/merchants.csv"
+                              })
+    ir   = se.items
+
+    expected = ir.find_all_by_description("Gorgeous hand knitted baby bootees")
+    assert_equal "263399735", expected[0].id
+  end
+
+  def test_find_all_with_price
+    se = SalesEngine.from_csv({
+                              :items     => "./data/items_sample.csv",
+                              :merchants => "./data/merchants.csv"
+                              })
+    ir   = se.items
+
+    expected = ir.find_all_by_price(50000.0)
+    assert_equal "263397919", expected[0].id
   end
 end
