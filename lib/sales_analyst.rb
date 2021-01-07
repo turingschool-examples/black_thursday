da0eef9829ee5cd83a55183d8ffcb71ed1e83806
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-07 16:31:45 -0600

    Add average_items_per_merchant method and helper methods to SalesAnalyst class, all test pass.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -6,6 +6,14 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
+    (all_items_count / all_merchants_count.to_f).round(2)
+  end
 
+  def all_items_count
+    @parent.items.all #TODO add length
+  end
+
+  def all_merchants_count
+    @parent.merchants.all.length
   end
 end
