26d549e8936e263e55c61f6aefbc949404bec033
Trevor Suter <Trevorsuter@icloud.com>
2021-01-07 20:00:07 -0700

    modified sales engine for rspec

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,20 +1,22 @@
-require './lib/merchant_repo'
+require_relative './merchant_repo'
+require_relative './item_repo'
 
 
 class SalesEngine
-  attr_reader :items
+  attr_reader :items,
+              :merchants
 
   def initialize(data)
     @items = data[:items]
     @file = data[:merchants]
-    @merchant_repo = MerchantRepository.new(@file, self)
+    @merchants = MerchantRepository.new(@file, self)
   end
 
   def self.from_csv(data)
     new(data)
   end
 
-  def merchants
-    @merchant_repo.build_merchants
-  end
+  # def merchants
+  #   @merchant_repo.build_merchants
+  # end
 end
