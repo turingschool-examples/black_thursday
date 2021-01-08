b9a345be0351a832fa5a86840cd941f0645b8a05
DougWelchons <doug.welchons@gmail.com>
2021-01-07 22:16:52 -0700

    Add refactors to incorperate sample CSV files into item repo tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -47,86 +47,91 @@ class ItemRepositoryTest < Minitest::Test
                         updated_at: '2003-12-21 19:00:37 UTC',
                         merchant_id: 85
                         })
-    @sample_data = [@item1, @item2, @item3, @item4]
+    @sample_data = "./test/fixtures/sample_items.csv"
+    # @file_path =
   end
 
   def test_it_exists
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
     assert_instance_of ItemRepository, test
   end
 
   def test_it_has_readable_attributes
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
-    assert_equal [@item1, @item2, @item3, @item4], test.items
+    assert_equal test.items[0..3], test.items
   end
 
   def test_it_can_inspect_an_item
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
     assert_equal '#<ItemRepository 4 rows>', test.inspect
   end
 
   def test_it_can_give_all_items
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
-    assert_equal @sample_data, test.all
+    assert_equal test.items, test.all
   end
 
   def test_it_can_find_by_id
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
     assert_nil test.find_by_id(5)
-    assert_equal @item1, test.find_by_id(1)
+    assert_equal test.items[0], test.find_by_id(1)
   end
 
   def test_it_can_find_by_name
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
     assert_nil test.find_by_name('Hello World')
-    assert_equal @item3, test.find_by_name('Something to do Things')
+    assert_equal test.items[2], test.find_by_name('Something to do Things')
 
   end
 
   def test_it_can_find_all_with_description
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
     assert_equal [], test.find_all_with_description('fboagbrprgruoan')
-    assert_equal [@item3, @item4], test.find_all_with_description('you')
+    assert_equal test.items[2, 3], test.find_all_with_description('you')
   end
 
   def test_it_can_find_all_by_price
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
     price = BigDecimal(3)
 
     assert_equal [], test.find_all_by_price(100)
-    assert_equal [@item3, @item4], test.find_all_by_price(price)
+    assert_equal test.items[2, 3], test.find_all_by_price(price)
   end
 
   def test_it_can_find_all_by_price_in_range
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
     price_range = (3.00..4.00)
 
+    expected = [test.items[0], test.items[2], test.items[3]]
+
     assert_equal [], test.find_all_by_price_in_range(100..101)
-    assert_equal [@item1, @item3, @item4], test.find_all_by_price_in_range(price_range)
+    assert_equal expected, test.find_all_by_price_in_range(price_range)
   end
 
   def test_it_can_find_all_by_merchant_id
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
+
+    expected = [test.items[0], test.items[2]]
 
     assert_equal [], test.find_all_by_merchant_id(7)
-    assert_equal [@item1, @item3], test.find_all_by_merchant_id(25)
+    assert_equal expected, test.find_all_by_merchant_id(25)
   end
 
   def test_max_item_id
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
     assert_equal 4, test.max_item_id
   end
 
   def test_it_can_create
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
     attributes = {
                   name: 'Yet another Thing!',
                   description: 'Get it now! you must have it!',
@@ -136,15 +141,14 @@ class ItemRepositoryTest < Minitest::Test
                   merchant_id: 25
                   }
 
-        test.create(attributes)
+    test.create(attributes)
 
-        assert_equal 'Yet another Thing!', test.find_by_id(5).name
-        assert_equal 5.99, test.find_by_id(5).unit_price
-
+    assert_equal 'Yet another Thing!', test.find_by_id(5).name
+    assert_equal 5.99, test.find_by_id(5).unit_price
   end
 
   def test_it_can_update
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
     update = {
               name: 'Computer',
               description: 'This is a computer.',
@@ -153,13 +157,13 @@ class ItemRepositoryTest < Minitest::Test
 
     test.update(1, update)
 
-    assert_equal 'Computer', @item1.name
-    assert_equal 'This is a computer.', @item1.description
-    assert_equal 125, @item1.unit_price
+    assert_equal 'Computer', test.items[0].name
+    assert_equal 'This is a computer.', test.items[0].description
+    assert_equal 125, test.items[0].unit_price
   end
 
   def test_it_can_delete
-    test = ItemRepository.new(@sample_data)
+    test = ItemRepository.new(@sample_data, 'engine')
 
     test.delete(1)
 
