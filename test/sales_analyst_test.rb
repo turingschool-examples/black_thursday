1ad1716a3a930fec685afd5ac3127b2bc1e7f9f5
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-08 14:47:59 -0700

    Get sales analyst tdd started

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,7 +1,13 @@
 require_relative './test_helper'
+require './lib/sales_analyst'
 
 class SalesAnalystTest < Minitest::Test
-  def test_it_exists_and_has_attributes
 
-    assert_instance_of SalesAnalyst, code
+  def setup
+    @sales_analyst = SalesAnalyst.new
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of SalesAnalyst, @sales_analyst
+  end
 end
