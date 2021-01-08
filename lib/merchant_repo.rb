31a3bafca2c5af786bb47e889e96f71323f84867
Katy La Tour <klatour324@gmail.com>
2021-01-07 18:19:51 -0600

    Add max merchant id and create(attribute) methods to Merchant Repo class

--- lib/merchant_repo.rb
+++ lib/merchant_repo.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'time'
 
 class MerchantRepo
   attr_reader :merchant_list
@@ -46,5 +47,20 @@ class MerchantRepo
       @merchant_list.find_all do |merchant|
       merchant.name.downcase.include?(name)
     end
+  end
+
+  def max_merchant_id
+    @merchant_list.max_by do |merchant|
+      merchant.id
+    end.id
+  end
+
+  def create(attributes)
+    @merchant_list.push(Merchant.new({
+                                      id: max_merchant_id.to_i + 1,
+                                      name: attributes[:name],
+                                      created_at: DateTime.now,
+                                      updated_at: DateTime.now
+                                    }))
   end
 end
