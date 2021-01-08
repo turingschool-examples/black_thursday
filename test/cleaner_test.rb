863b72d600ae25e73aa6b6cd0fccf5af66a28847
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-08 08:33:30 -0500

    Clear white space and comments

--- test/cleaner_test.rb
+++ test/cleaner_test.rb
@@ -5,22 +5,13 @@ require './lib/cleaner'
 class CleanerTest < MiniTest::Test
 
   def setup
-    # @merchant_csv = "./data/merchants.csv"
-    # @items_csv = "./data/items.csv"
     @cleaner = Cleaner.new
-    # @merchant_data = CSV.open(@merchant_csv, headers: true, header_converters: :symbol)
-    # @items_data = CSV.open(@items_csv, headers: true, header_converters: :symbol)
-    # @merchant_rows = @merchant_data.map { |row| row }
-    # @item_rows = @items_data.map { |row| row }
   end
 
   def test_it_exists
+    
     assert_instance_of Cleaner, @cleaner
   end
-
-  # def test_it_can_open_data
-  #   assert_equal @merchant_data.class, @cleaner.open_csv(@merchant_csv).class
-  # end
 
   def test_it_id_is_an_integer
     expected = 12334105
