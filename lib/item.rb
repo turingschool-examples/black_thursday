ac7da5ca86e7c2104f153a74f42c6b6bba8038a2
Olivia Charles <ocharles92@gmail.com>
2021-01-08 14:44:19 -0500

    Fixed created_at and updated_at in initialize

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,9 @@
+###Ask about created_at returns the Time the item was created and updated_at returns the Time the item was last updated!!#####
+
+require 'bigdecimal'
+require 'bigdecimal/util'
+require 'time'
+
 class Item
   attr_reader :id,
               :name,
@@ -9,24 +15,25 @@ class Item
 
   def initialize(args)
     @args        = args
-    @id          = args[:id].to_s
+    @id          = args[:id].to_i
     @name        = args[:name].to_s
     @description = args[:description].to_s
-    @unit_price  = args[:unit_price].to_s
-    @created_at  = args[:created_at].to_s
-    @updated_at  = args[:updated_at].to_s
+    @unit_price  = (args[:unit_price].to_d)/ 100
+    @created_at  = args[:created_at]
+    @updated_at  = Time.now
     @merchant_id = args[:merchant_id].to_s
   end
 
   def update(args)
 
-    @unit_price  = args[:unit_price].to_f.to_s if !args[:unit_price].nil?
+    @unit_price  = args[:unit_price].to_f if !args[:unit_price].nil?
     @description = args[:description].to_s if !args[:description].nil?
     @name        = args[:name].to_s  if !args[:name].nil?
-    @updated_at  = Time.new.to_s
+    @updated_at  = Time.now
   end
 
-
-
+  def unit_price_to_dollars
+    @unit_price.to_f
+  end
 
 end
