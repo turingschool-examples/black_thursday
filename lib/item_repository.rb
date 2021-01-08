b2db35769e952cfc7a8c61d8c5f8fb721c946418
Olivia Charles <ocharles92@gmail.com>
2021-01-08 14:46:19 -0500

    Refactored unit_price methods and updated_at, created_at.

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,5 +1,9 @@
-require_relative 'item'
-require 'csv'
+require_relative "item"
+require 'bigdecimal'
+require 'bigdecimal/util'
+require 'time'
+require "csv"
+
 
 class ItemRepository
   attr_reader :filename,
@@ -8,7 +12,12 @@ class ItemRepository
   def initialize(filename)
     @filename = filename
     @items = Array.new
+
     generate_items(filename)
+  end
+
+  def inspect
+    "#<#{self.class} #{@items.size} rows>"
   end
 
   def generate_items(filename)
@@ -19,12 +28,12 @@ class ItemRepository
   end
 
   def all
-    @items.length
+    @items
   end
 
   def find_by_id(id)
     @items.find do |item|
-      item.id == id.to_s
+      item.id.to_i == id
     end
   end
 
@@ -41,19 +50,14 @@ class ItemRepository
   end
 
   def find_all_by_price(price)
-    price_fix = "#{price}00"
     @items.find_all do |item|
-      item.unit_price == price_fix
+      item.unit_price == price
     end
-  end
-
-  def inspect
-    "#<#{self.class} #{@items.size} rows>"
   end
 
   def find_all_by_price_in_range(range)
     @items.find_all do |item|
-      range.include?(item.unit_price.to_f / 100)
+      range.include?(item.unit_price)
     end
   end
 
@@ -65,11 +69,11 @@ class ItemRepository
 
   def create(attributes)
     id = @items[-1].id.to_i
+
     id += 1
     id = id.to_s
     attributes[:id] = id
-    string_unit_price = attributes[:unit_price].to_f
-    attributes[:unit_price] = string_unit_price
+    attributes[:unit_price]
     item = Item.new(attributes)
     @items.push(item)
   end
@@ -80,7 +84,6 @@ class ItemRepository
     update_item.update(attributes) if !attributes[:name].nil?
     update_item.update(attributes) if !attributes[:description].nil?
     update_item.update(attributes) if !attributes[:unit_price].nil?
-    update_item.update(attributes) if !attributes[:updated_at].nil?
 
     update_item
   end
@@ -88,6 +91,5 @@ class ItemRepository
   def delete(id)
     delete = find_by_id(id)
     @items.delete(delete)
-  
   end
 end
