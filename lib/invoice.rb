1246c701f1903f8c3878f94706ecff27d9e7a7a5
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-01-08 07:33:16 -0700

    Add attributes and setup method in test

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,6 +1,11 @@
 class Invoice
-  def initialize()
-    @argument = argument
+  def initialize(data, repo)
+    @id = data[:id]
+    @customer_id = data[:customer_id]
+    @merchant_id = data[:merchant_id]
+    @status = data[:status]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
   end
 
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -5,9 +5,21 @@ require './lib/invoice'
 
 class InvoiceTest < MiniTest::Test
 
-  def test_it_exists
-    invoice = Invoice.new
-
-    assert_instance_of Invoice, invoice
+  def setup
+    @repo = "repo"
+    @i = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      }, @repo)
   end
+
+  def test_it_exists
+
+    assert_instance_of Invoice, @i
+  end
+
 end
