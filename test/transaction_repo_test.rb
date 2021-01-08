823bc99e742561acd0f106125e2bd298bb1aa318
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-01-08 09:24:26 -0700

    Finished transaction tests

--- test/transaction_repo_test.rb
+++ test/transaction_repo_test.rb
@@ -26,49 +26,58 @@ class TransactionRepoTest < Minitest::Test
     assert_equal "4068631943231473", actual[0].credit_card_number
   end
 
-  # def test_it_can_create_new_item
-  #   data ={
-  #     :id => "910",
-  #     :item_id => "11",
-  #     :invoice_id => "6",
-  #     :quantity => "4",
-  #     :unit_price => "9999",
-  #     :created_at => "2125-09-22 09:34:06 UTC",
-  #     :updated_at => "2034-09-04 21:35:10 UTC"
-  #     }
-  #   @dummy_repo.create(data)
-  #   actual = @dummy_repo.find_by_id("910")
-  #   assert_equal "910", actual.id
-  # end
-  #
-  # def test_update
-  #   actual = @dummy_repo.find_all_by_item_id("263539664")
-  #   assert_equal "5", actual[0].quantity
-  #   actual = @dummy_repo.find_by_id("6")
-  #   assert_equal "263539664", actual.item_id
-  #
-  #   @dummy_repo.update({id: "6", quantity: "355", unit_price: 1000})
-  #
-  #   actual = @dummy_repo.find_by_id("6")
-  #   assert_equal "355", actual.quantity
-  # end
-  #
-  # def test_delete
-  #   data ={
-  #     :id => "910",
-  #     :item_id => "11",
-  #     :invoice_id => "6",
-  #     :quantity => "4",
-  #     :unit_price => "9999",
-  #     :created_at => "2125-09-22 09:34:06 UTC",
-  #     :updated_at => "2034-09-04 21:35:10 UTC"
-  #     }
-  #   @dummy_repo.create(data)
-  #   actual = @dummy_repo.find_by_id("910")
-  #   assert_equal "11", actual.item_id
-  #   @dummy_repo.delete("910")
-  #
-  #   assert_nil nil, @dummy_repo.find_by_id("910")
-  # end
+  def test_find_all_by_credit_card_number
+    actual = @dummy_repo.test_find_all_by_credit_card_number("4558368405929183")
+    assert_equal "4966", actual[0].invoice_id
+  end
+
+  def test_find_all_by_result
+    actual = @dummy_repo.find_all_by_result("failed")
+    assert_equal "4463525332822998", actual[0].credit_card_number
+  end
+
+  def test_it_can_create_new_item
+    data = {
+      :id => "20",
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at => Time.now,
+      :updated_at => Time.now  }
+    @dummy_repo.create(data)
+    actual = @dummy_repo.find_by_id("10")
+    assert_equal "success", actual.result
+  end
+
+  def test_update
+    actual = @dummy_repo.find_by_id("9")
+    assert_equal "failed", actual.result
+    actual = @dummy_repo.find_all_by_invoice_id("1752")
+    assert_equal "4463525332822998", actual[0].credit_card_number
+
+    @dummy_repo.update({id: "9",
+                        credit_card_number: "1234567891011", credit_card_expiration_date: "0221", status: "success"})
+
+    actual = @dummy_repo.find_by_id("9")
+    assert_equal "0221", actual.credit_card_expiration_date
+  end
+
+  def test_delete
+    data = {
+      :id => "20",
+      :invoice_id => "8",
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at => Time.now,
+      :updated_at => Time.now  }
+    @dummy_repo.create(data)
+    actual = @dummy_repo.find_all_by_invoice_id("8")
+    assert_equal "10", actual[0].id
+    @dummy_repo.delete("10")
+
+    assert_nil nil, @dummy_repo.find_by_id("10")
+  end
 
 end
