2dfd1685782eea73e9ec777d2c28f042b5344733
Trevor Suter <Trevorsuter@icloud.com>
2021-01-08 18:16:39 -0700

    test all

--- test/customer_repo_test.rb
+++ test/customer_repo_test.rb
@@ -21,4 +21,9 @@ class CustomerRepositoryTest < MiniTest::Test
     assert_equal "Lempi", @cr.customers[369].first_name
     refute_equal "Trevor", @cr.customers[2].last_name
   end
+
+  def test_all
+    assert_equal 1000, @cr.all.length
+    assert_equal Customer, @cr.all.first.class
+  end
 end
