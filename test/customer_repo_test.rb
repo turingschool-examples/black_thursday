a84369d2b73d6e71bd4def67531f82fcd62a3ab4
Trevor Suter <Trevorsuter@icloud.com>
2021-01-08 17:59:00 -0700

    build merchant test

--- test/customer_repo_test.rb
+++ test/customer_repo_test.rb
@@ -7,10 +7,18 @@ require './lib/customer_repo'
 class CustomerRepositoryTest < MiniTest::Test
 
   def setup
-    @cr = CustomerRepository.new
+    @engine = "engine"
+    @cr = CustomerRepository.new (@engine)
   end
 
-  def test_it_exists
+  def test_it_exists_with_attributes
     assert_instance_of CustomerRepository, @cr
+    assert_equal @engine, @cr.engine
+  end
+
+  def test_building_a_customer
+    assert_equal 1, @cr.customers[1].id
+    assert_equal "Lempi", @cr.customers[369].first_name
+    refute_equal "Trevor", @cr.customers[2].last_name
   end
 end
