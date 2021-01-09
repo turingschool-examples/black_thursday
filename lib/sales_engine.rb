ff83d5f1e16caf094289ea9de3b63996d2173843
Joseph Budina <joebud3@gmail.com>
2021-01-09 08:55:03 -0700

    Add analyst method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,6 +1,7 @@
 require_relative './merchant_repo'
 require_relative './item_repo'
 require_relative './invoice_repo'
+require_relative './sales_analyst'
 
 
 class SalesEngine
@@ -19,12 +20,16 @@ class SalesEngine
   def process_data(data)
     data.each do |key, value|
       if key == :merchants
-        @merchants = MerchantRepository.new(data[:merchants], self)
+        @merchants = MerchantRepository.new(data[:merchants])
       elsif key == :items
-        @items = ItemRepository.new(data[:items], self)
+        @items = ItemRepository.new(data[:items])
       elsif key == :invoices
-        @invoices = InvoiceRepository.new(data[:invoices], self)
+        @invoices = InvoiceRepository.new(data[:invoices])
       end
     end
+  end
+
+  def analyst
+    SalesAnalyst.new
   end
 end
