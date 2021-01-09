438098d66ec7148ac98812e131b07625ea78b37c
Joseph Budina <joebud3@gmail.com>
2021-01-09 09:04:56 -0700

    Add average items per merchant test

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -11,4 +11,9 @@ class TestSalesAnalyst < MiniTest::Test
   def test_it_exists
     assert_instance_of SalesAnalyst, @sales_analyst
   end
+
+  def test_it_finds_average_items_per_merchant
+    assert_equal 2.88, @sales_analyst.average_items_per_merchant
+    assert_instance_of Float, @sales_analyst.average_items_per_merchant
+  end
 end
