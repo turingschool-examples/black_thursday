e731ce1f1b98c73e184940e6dbddc7bbd6e08a0c
Jacob Volpe <volpe.jacob@gmail.com>
2021-01-09 13:16:12 -0700

    Fix issues in invoice, finalize invoice_repo methods

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -4,19 +4,19 @@ require 'bigdecimal'
 class Invoice
   include TimeStoreable
   
-  attr_reader :status,
-              :updated_at
+  attr_accessor :status,
+                :updated_at
 
-  attr_accessor :id,
+  attr_reader   :id,
                 :customer_id,
                 :merchant_id,
                 :created_at
                
   def initialize(data, repository)
-    @id           = data[:id]
+    @id           = data[:id].to_i
     @customer_id  = data[:customer_id].to_i
     @merchant_id  = data[:merchant_id].to_i
-    @status       = data[:status]
+    @status       = :"#{data[:status]}"
     @created_at   = time_store(data[:created_at])
     @updated_at   = time_store(data[:updated_at])
     @repository   = repository
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,6 +1,7 @@
 require 'time'
 require './lib/invoice'
 require 'csv'
+require 'pry'
 
 class InvoiceRepository
   include TimeStoreable
@@ -26,4 +27,62 @@ class InvoiceRepository
                     updated_at: row[:updated_at]
                    }, self)
   end
+
+  def find_by_id(id)
+    @all.find do |invoice|
+     invoice.id == id
+    end
+  end
+
+  def find_all_by_customer_id(id)
+    @all.find_all do |invoice|
+     invoice.customer_id == id
+    end
+  end
+
+  def find_all_by_merchant_id(id)
+    @all.find_all do |invoice|
+     invoice.merchant_id == id
+    end
+  end
+
+  def find_all_by_status(status)
+    @all.find_all do |invoice|
+     invoice.status.downcase == status
+    end
+  end
+
+  def new_highest_id
+    all.max_by do |instance|
+      instance.id
+    end.id + 1
+  end
+
+  def create(attributes)
+    attributes[:id] = new_highest_id
+    @all << Invoice.new(attributes, self)
+  end
+
+  def update(id, attributes)
+    if find_by_id(id) != nil && check_status_is_valid(attributes)
+      new_status = attributes[:status]
+      update_invoice = all.find { |invoice| invoice.id == id }
+      update_invoice.status = new_status
+      update_invoice.updated_at = Time.now
+    end
+  end
+
+  def check_status_is_valid(attributes)
+   if attributes.class == Hash
+      statuses = [:pending, :returned, :shipped, :sold, :success]
+      status = attributes[:status]
+      statuses.include?(status)
+    end
+  end
+
+  def delete(id)
+    remove = find_by_id(id)
+    @all.delete(remove)
+  end
+
 end
