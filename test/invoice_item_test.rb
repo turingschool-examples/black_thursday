3de3ab4b72af9ca83128a89c870adfab696ba9e4
Wil McCauley <wmccauley724@gmail.com>
2021-01-09 16:49:11 -0700

    Add test for quantity, add test for price to dollars method, rework setup to reflect harness spec

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -9,7 +9,7 @@ class InvoiceItemTest < Minitest::Test
       :item_id => 7,
       :invoice_id => 8,
       :quantity => 1,
-      :unit_price => BigDecimal.new(10.99, 4),
+      :unit_price => BigDecimal(84787 * 0.01, 5),
       :created_at => 10,
       :updated_at => 12
     })
@@ -23,9 +23,14 @@ class InvoiceItemTest < Minitest::Test
     assert_equal Fixnum, @ii.item_id.class
     assert_equal 8, @ii.invoice_id
     assert_equal Fixnum, @ii.invoice_id.class
-    assert_equal 0.1099e2, @ii.unit_price
+    assert_equal 1, @ii.quantity
+    assert_equal 847.87, @ii.unit_price
     assert_equal BigDecimal, @ii.unit_price.class
     assert_equal 10, @ii.created_at
     assert_equal 12, @ii.updated_at
+  end
+
+  def test_it_returns_unit_price_to_dollar_float
+    assert_equal 847.87, @ii.unit_price_to_dollars
   end
 end
