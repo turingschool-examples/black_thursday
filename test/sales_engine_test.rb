30d54461101795671ede473c7b9ab0f260383533
DougWelchons <doug.welchons@gmail.com>
2021-01-08 21:22:14 -0700

    Add sales engine test refactors

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -6,35 +6,71 @@ require 'minitest/pride'
 require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-  def setup
-    @file_items = CSV.readlines("./data/items.csv", headers: true, header_converters: :symbol)
-    @file_merchants = CSV.readlines("./data/merchants.csv", headers: true, header_converters: :symbol)
-    @se = SalesEngine.from_csv({
-                              :items     => "./data/items.csv",
-                              :merchants => "./data/merchants.csv",
-                              })
-  end
-
-  def test_it_exists
-    assert_instance_of SalesEngine, @se
-  end
-
-  def test_it_can_from_csv
+  def test_from_csv_creates_sales_engine_objects
     sales_engine = SalesEngine.from_csv({
-                              :items     => "./data/items.csv",
-                              :merchants => "./data/merchants.csv",
-                              })
+                                          :items     => "./data/items.csv",
+                                          :merchants => "./data/merchants.csv",
+                                          :invoices => "./data/invoices.csv",
+                                          :customers  => "./data/customers.csv",
+                                        })
 
     assert_instance_of SalesEngine, sales_engine
   end
 
-  def test_it_has_readable_attributes
-    assert_equal ItemRepository, @se.items.class
-    assert_equal MerchantRepository, @se.merchants.class
+  def test_it_exists
+    sales_engine = SalesEngine.from_csv({
+                                          :items     => "./data/items.csv",
+                                          :merchants => "./data/merchants.csv",
+                                          :invoices => "./data/invoices.csv",
+                                          :customers => "./data/customers.csv",
+                                        })
+    assert_instance_of SalesEngine, sales_engine
   end
 
-  def test_it_can_create_arrays_of_items_and_merchants
-    assert_instance_of Array, @se.item_repository(@file_items).items
-    assert_instance_of Array, @se.merchant_repository(@file_merchants).merchants
+  def test_it_creates_instances_of_objects
+    sales_engine = SalesEngine.from_csv({
+                                          :items     => "./data/items.csv",
+                                          :merchants => "./data/merchants.csv",
+                                          :invoices => "./data/invoices.csv",
+                                          :customers => "./data/customers.csv",
+                                        })
+
+    assert_instance_of ItemRepository, sales_engine.items
+    assert_instance_of MerchantRepository, sales_engine.merchants
+    assert_instance_of InvoiceRepository, sales_engine.invoices
+    assert_instance_of SalesAnalyst, sales_engine.analyst
   end
+
+  def test_it_can_find_all_merchants
+    sales_engine = SalesEngine.from_csv({
+                                          :items     => "./data/items.csv",
+                                          :merchants => "./data/merchants.csv",
+                                          :invoices => "./data/invoices.csv",
+                                          :customers => "./data/customers.csv",
+                                        })
+
+      assert_equal 475, sales_engine.all_merchants.count
+  end
+
+    def test_it_can_find_all_items_by_merchant_id
+      sales_engine = SalesEngine.from_csv({
+                                            :items     => "./data/items.csv",
+                                            :merchants => "./data/merchants.csv",
+                                            :invoices => "./data/invoices.csv",
+                                            :customers => "./data/customers.csv",
+                                          })
+
+        assert_equal 1, sales_engine.find_all_items_by_merchant_id(12334112).count
+    end
+
+    def test_it_can_find_all_items
+      sales_engine = SalesEngine.from_csv({
+                                            :items     => "./data/items.csv",
+                                            :merchants => "./data/merchants.csv",
+                                            :invoices => "./data/invoices.csv",
+                                            :customers => "./data/customers.csv",
+                                          })
+
+        assert_equal 1367, sales_engine.all_items.count
+    end
 end
