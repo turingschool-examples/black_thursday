8a9ef0eda98484a010b24fc3ba8aea288c5d1dc4
Wil McCauley <wmccauley724@gmail.com>
2021-01-08 17:33:26 -0700

    Add test for update method, begin refactoring to function with update method in item

--- test/item_test.rb
+++ test/item_test.rb
@@ -2,6 +2,10 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/item'
 require './lib/cleaner'
+# refactor to mock/stubs to avoid redundancy?
+# But in the update test we do need to check on a real item i think?
+require './lib/item_repo'
+
 
 class ItemTest < Minitest::Test
   def setup
@@ -14,7 +18,7 @@ class ItemTest < Minitest::Test
       :updated_at  => 12,
       :merchant_id => 2
       })
-      @cleaner = Cleaner.new
+      @cleaner = Cleaner.new # refactor out?
   end
 
   def test_it_exists_with_attributes
@@ -27,10 +31,36 @@ class ItemTest < Minitest::Test
     assert_equal 12, @item.updated_at
     assert_equal 2, @item.merchant_id
   end
-  
+
   def test_unit_price_to_dollars
     assert_equal 0.11, @item.unit_price_to_dollars
   end
 
-  
+  def test_update
+    ir = ItemRepository.new
+
+    attributes = {
+      name: "Capita Defenders of Awesome 2018",
+      description: "This board both rips and shreds",
+      unit_price: BigDecimal.new(399.99, 5),
+      created_at: Time.now,
+      updated_at: Time.now,
+      merchant_id: 25
+    }
+    updated_attribute_hash = {
+      :name => "Bleeps",
+      :description => "doop doop doop",
+      :unit_price => BigDecimal.new(599.99, 5)
+    }
+    # updated_time = mock()
+    # updated_time.stubs(:updated_at).returns(2021-01-07 18:03:23 -0700)
+    # allow(Time).to receive(:now).and_return(@time_now)
+    # Time.stubs(:now).returns(Time.mktime(1970,1,1))
+    ir.create(attributes)
+    target_item = ir.find_item_by_id(263567475)[0]
+    require "pry"; binding.pry
+    target_item.update(263567475, updated_attribute_hash)
+    assert_equal 263567475, ir.find_by_name("Bleeps").id
+  end
+
 end
