a0f639452f38981114ca75ad493a9f2da02e0c3b
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-09 16:26:36 -0600

    Add test for #average_average_price_per_merchant of SalesAnalyst class.

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -49,4 +49,8 @@ class SalesAnalystTest < Minitest::Test
   def test_it_can_returns_the_average_item_price_for_the_given_merchant
     assert_equal 16.66, @sales_analyst.average_item_price_for_merchant(12334105)
   end
+
+  def test_it_returns_the_average_price_for_all_merchants
+    assert_equal 350.29, @sales_analyst.average_average_price_per_merchant
+  end
 end
