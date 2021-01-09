4a855b33ae1c40019d119e019b3ea6745331a6cf
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-08 19:10:37 -0600

    Add #merchants_with_high_item_count to SalesAnalyst class, all test pass.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -26,6 +26,7 @@ class SalesAnalyst
 
     items_per_merchant = merchant_ids.map do |id|
       @parent.items.find_all_by_merchant_id(id).count
+      # if count > mean then push merchant.find_by_id
     end
 
     all_items_minus_one = (items_per_merchant.length) - 1
@@ -37,6 +38,22 @@ class SalesAnalyst
 
     standard_deviaton = Math.sqrt(total / all_items_minus_one)
     standard_deviaton.round(2)
+  end
+
+  def merchants_with_high_item_count
+    high_seller_count = average_items_per_merchant + average_items_per_merchant_standard_deviation
+
+    merchant_ids = @parent.merchants.merchants.map do |merchant|
+      merchant.id
+    end
+    merchants = []
+    merchant_ids.each do |id|
+      item_count = @parent.items.find_all_by_merchant_id(id).count
+      if item_count > high_seller_count
+        merchants << @parent.merchants.find_by_id(id)
+      end
+    end
+    merchants
   end
 
 end
