9f298a8a1fdd1ec133c8e56fc367ff8cb9a48106
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-08 22:20:08 -0500

    Remove whitespace

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -14,17 +14,6 @@ class Merchant
     @updated_at = data[:updated_at]
   end
 
-  # def update(attribute)
-  #   if attribute[:id] == nil
-  #     @updated_at = Time.now
-  #     if attribute[:name] != nil
-  #       @name = attribute[:name]
-  #     elsif attribute[:repo] != nil
-  #       @repo == attribute[:repo]
-  #     end
-  #   end
-  # end
-
   def update(attributes)
     @name        = attributes[:name] if attributes[:name]
     @repo        = attributes[:repo] if attributes[:repo]
