0c2f64f9b88affeb4b1ad3682f9bd36249a9688a
Trevor-Robinson <trevor.robi1254@gmail.com>
2021-01-09 11:17:33 -0700

    Create sales analyst creation test

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/sales_engine'
+require './lib/sales_analyst'
 require 'csv'
 
 class SalesEngineTest < Minitest::Test
@@ -12,5 +13,15 @@ class SalesEngineTest < Minitest::Test
 
     se = SalesEngine.from_csv(locations)
     assert_instance_of SalesEngine, se
+  end
+
+  def test_it_creates_sales_analyst
+    merchant_path = './data/merchants.csv'
+    item_path = './data/items.csv'
+    locations = { items: item_path,
+                  merchants: merchant_path }
+
+    sales_engine = SalesEngine.from_csv(locations)
+    assert_instance_of SalesAnalyst, sales_engine.analyst
   end
 end
