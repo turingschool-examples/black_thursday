8f939076672186def028911da2f8b3ee6c3e58fe
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-08 18:38:12 -0600

    Add #average_items_per_merchant_standard_deviation to SalesAnalyst class, all test pass.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -17,7 +17,26 @@ class SalesAnalyst
     @parent.merchants.all.length
   end
 
-  def average_item_price_for_merchant(id)
-    @parent.find_merchant_by_id(id)
+  def average_items_per_merchant_standard_deviation
+    mean = average_items_per_merchant
+
+    merchant_ids = @parent.merchants.merchants.map do |merchant|
+      merchant.id
+    end
+
+    items_per_merchant = merchant_ids.map do |id|
+      @parent.items.find_all_by_merchant_id(id).count
+    end
+
+    all_items_minus_one = (items_per_merchant.length) - 1
+
+    total = 0
+    items_per_merchant.each do |item_number|
+      total += ((item_number - mean) ** 2)
+    end
+
+    standard_deviaton = Math.sqrt(total / all_items_minus_one)
+    standard_deviaton.round(2)
   end
+
 end
