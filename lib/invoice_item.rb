d55afda9e059dbeda8ac3e5aa1309a7a54a226e5
Wil McCauley <wmccauley724@gmail.com>
2021-01-09 16:50:16 -0700

    Rework price to dollars method to reflect new passed value to match spec harness

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -4,16 +4,22 @@ class InvoiceItem
   attr_reader :id,
               :item_id,
               :invoice_id,
+              :quantity,
               :unit_price,
               :created_at,
               :updated_at
 
   def initialize(attributes)
-    @id = attributes[:id]
-    @item_id = attributes[:item_id]
+    @id         = attributes[:id]
+    @item_id    = attributes[:item_id]
     @invoice_id = attributes[:invoice_id]
+    @quantity   = attributes[:quantity]
     @unit_price = attributes[:unit_price]
     @created_at = attributes[:created_at]
     @updated_at = attributes[:updated_at]
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
   end
 end
