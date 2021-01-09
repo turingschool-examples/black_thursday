aa673e71f66993471b23111749b2b727c63a1d55
Olivia Charles <ocharles92@gmail.com>
2021-01-09 16:56:40 -0500

    Created Invoice Tests, It Exitsts, id returns id and customer id returns the invoice customer id.

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -10,3 +10,16 @@ class InvoiceTest < MiniTest::Test
     @engine = SalesEngine.from_csv(arguments)
     @invoice = @engine.invoices.find_by_id(3452)
   end
+
+  def test_it_exists
+   assert_instance_of Invoice, @engine.invoices.all.first.class
+ end
+
+ def test_id_returns_invoice_id
+   assert_equal Fixnum, @invoice.id.class
+ end
+ 
+ def test_customer_id_returns_the_invoice_customer_id
+   assert_equal 679, @invoice.customer_id
+   assert_equal Fixnum, @invoice.customer_id.class
+ end
