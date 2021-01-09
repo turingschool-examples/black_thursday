6408779e0317ea8f3af01e469afed2e0b20ff193
Wil McCauley <wmccauley724@gmail.com>
2021-01-09 14:03:46 -0700

    Add attributes and attr_read to them to pass attribute tests

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,6 +1,19 @@
 require 'bigdecimal'
 
 class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :unit_price,
+              :created_at,
+              :updated_at
+
   def initialize(attributes)
+    @id = attributes[:id]
+    @item_id = attributes[:item_id]
+    @invoice_id = attributes[:invoice_id]
+    @unit_price = attributes[:unit_price]
+    @created_at = attributes[:created_at]
+    @updated_at = attributes[:updated_at]
   end
 end
