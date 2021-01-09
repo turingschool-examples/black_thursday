a69b3ab9d0402e350598a9efddf8fdcc09d04db6
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-08 19:15:20 -0700

    Adding working test and code for standard deviation

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,6 +1,32 @@
 require 'csv'
 require 'pry'
+require 'descriptive_statistics'
+require_relative './sales_engine'
 
 class SalesAnalyst
-  
+
+  attr_reader :engine
+  def initialize(engine)
+    @engine = engine
+  end
+
+  def average_items_per_merchant
+    (engine.items.all.count.to_f / engine.merchants.all.count).round(2)
+  end
+
+  def average_items_per_merchant_standard_deviation
+    shops_items = engine.merchants.all.reduce({}) do |memo, merchant|
+      memo[merchant.name] = []
+      engine.items.all.each do |item|
+        if item.merchant_id == merchant.id
+          memo[merchant.name] << item.name
+        end
+      end
+      memo
+    end
+    number_of_items = shops_items.map do |merchant, item|
+      item.count
+    end
+    number_of_items.standard_deviation.round(2)
+  end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -4,10 +4,24 @@ require './lib/sales_analyst'
 class SalesAnalystTest < Minitest::Test
 
   def setup
-    @sales_analyst = SalesAnalyst.new
+    @se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+
+    @sales_analyst = SalesAnalyst.new(@se)
   end
 
   def test_it_exists_and_has_attributes
     assert_instance_of SalesAnalyst, @sales_analyst
+  end
+
+  def test_average_items_per_merchant
+    assert_equal 2.88, @sales_analyst.average_items_per_merchant
+  end
+
+  def test_average_items_per_merchant_standard_devitation
+    assert_equal 3.26, @sales_analyst.average_items_per_merchant_standard_deviation
+
   end
 end
