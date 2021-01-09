2e16ffa7dd528d9250c211a700353fc920602002
Olivia Charles <ocharles92@gmail.com>
2021-01-09 17:02:15 -0500

    Created test for it exists

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -8,3 +8,15 @@ class InvoiceRepositoryTest < MiniTest::Test
     #parent = mock("parent")
     @engine = SalesEngine.from_csv(arguments)
   end
+
+  def test_it_exists
+    invoice_id = 3452
+    expected = @engine.invoices.find_by_id(invoice_id)
+    assert_equal invoice_id, @expected.id
+    assert_equal 12335690, @expected.merchant_id
+    assert_equal 679, @expected.customer_id
+    assert_equal :pending, @expected.status
+    invoice_id = 5000
+    expected = @engine.invoices.find_by_id(invoice_id)
+    assert_nil expected
+  end
