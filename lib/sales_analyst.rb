bf5f97b1678c6fad75c6c910159fc026e6fe0328
Trevor-Robinson <trevor.robi1254@gmail.com>
2021-01-09 13:34:21 -0700

    Create merchant methods and associated tests

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -10,6 +10,34 @@ class SalesAnalyst
     (total_items.to_f / total_merchants).round(2)
   end
 
+  def average_items_per_merchant_standard_deviation
+    average = average_items_per_merchant
+    total = 0
+    merchant_item_count.each do |key, value|
+      total += (average - value)**2
+    end
+    Math.sqrt(total/(total_merchants - 1)).round(2)
+  end
+
+  def merchants_with_high_item_count
+    greater_than_sd = average_items_per_merchant + average_items_per_merchant_standard_deviation
+    merchants = []
+    merchant_item_count.find_all do |merchant, count|
+      if count >= greater_than_sd
+        merchants << @sales_engine.merchants.find_by_id(merchant)
+      end
+    end
+    merchants
+  end
+
+  def merchant_item_count
+    sales_engine.items.all.reduce({}) do |acc, item|
+      merchant_item_count = sales_engine.items.find_all_by_merchant_id(item.merchant_id).length
+      acc[item.merchant_id] = merchant_item_count
+      acc
+    end
+  end
+
   def total_items
     sales_engine.items.all.length
   end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -23,12 +23,25 @@ class SalesAnalystTest < Minitest::Test
     assert_equal 2.88, @sales_analyst.average_items_per_merchant
   end
 
+  def test_it_can_return_average_items_per_merchant_standard_deviation
+    assert_equal 3.26, @sales_analyst.average_items_per_merchant_standard_deviation
+  end
+
+  def test_it_can_return_merchants_with_high_items_count
+    assert_equal 52, @sales_analyst.merchants_with_high_item_count.length
+    assert_instance_of Merchant, @sales_analyst.merchants_with_high_item_count.last
+  end
+
   def test_it_can_return_total_items
     assert_equal 1367, @sales_analyst.total_items
   end
 
   def test_it_can_return_total_merchants
     assert_equal 475, @sales_analyst.total_merchants
+  end
+
+  def test_merchant_items_count
+    assert_equal 475, @sales_analyst.merchant_item_count.length
   end
 
 end
