17c7e68d9ba150a0f2fea9c705589311a67e95e0
Joseph Budina <joebud3@gmail.com>
2021-01-09 10:19:25 -0700

    Add all merchant_item_count and average_items per merchant methods

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,7 +1,19 @@
-class SalesAnalyst
+require_relative './sales_engine'
 
-  def initialize
-    
+class SalesAnalyst
+  attr_reader :sales_engine
+
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
   end
 
+  def average_items_per_merchant
+    (all_merchant_item_count.sum.to_f / all_merchant_item_count.size).round(2)
+  end
+
+  def all_merchant_item_count
+    @sales_engine.merchants.all.map do |merchant|
+      @sales_engine.merchant_items(merchant.id).length
+    end
+  end
 end
