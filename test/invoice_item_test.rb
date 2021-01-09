39e7efd8f19d4fafe72e5655d43f8c5d4af31301
Wil McCauley <wmccauley724@gmail.com>
2021-01-09 14:02:48 -0700

    Add tests for all attributes

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -10,12 +10,22 @@ class InvoiceItemTest < Minitest::Test
       :invoice_id => 8,
       :quantity => 1,
       :unit_price => BigDecimal.new(10.99, 4),
-      :created_at => Time.now,
-      :updated_at => Time.now
+      :created_at => 10,
+      :updated_at => 12
     })
   end
 
   def test_it_exists_with_attributes
     assert_instance_of InvoiceItem, @ii
+    assert_equal 6, @ii.id
+    assert_equal Fixnum, @ii.id.class
+    assert_equal 7, @ii.item_id
+    assert_equal Fixnum, @ii.item_id.class
+    assert_equal 8, @ii.invoice_id
+    assert_equal Fixnum, @ii.invoice_id.class
+    assert_equal 0.1099e2, @ii.unit_price
+    assert_equal BigDecimal, @ii.unit_price.class
+    assert_equal 10, @ii.created_at
+    assert_equal 12, @ii.updated_at
   end
 end
