554f1cd336febe61da45bf4321a775aab16226a5
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-09 06:39:47 -0500

    Create data_check method in se so 1 or many data types can be entered at a time

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,17 +1,33 @@
 require_relative './merchant_repo'
 require_relative './item_repo'
+require_relative './invoice_repo'
 
 
 class SalesEngine
   attr_reader :items,
-              :merchants
+              :merchants,
+              :invoices
 
   def initialize(data)
-    @merchants = MerchantRepository.new(data[:merchants], self)
-    @items = ItemRepository.new(data[:items], self)
+    # @merchants = MerchantRepository.new(data[:merchants], self)
+    # @items = ItemRepository.new(data[:items], self)
+    # @invoices = InvoiceRepository.new(data[:invoices], self)
+    data_check(data)
   end
 
   def self.from_csv(data)
     new(data)
+  end
+
+  def data_check(data)
+    data.each do |key, value|
+      if key == :merchants
+        @merchants = MerchantRepository.new(data[:merchants], self)
+      elsif key == :items
+        @items = ItemRepository.new(data[:items], self)
+      elsif key == :invoices
+        @invoices = InvoiceRepository.new(data[:invoices], self)
+      end
+    end
   end
 end
