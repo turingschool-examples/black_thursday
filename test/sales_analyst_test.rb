f58a1e9bec1168fd5ce6358d5dff0b54d44985d3
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-01-09 18:28:28 -0700

    Change fixture file and test assertions

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -6,7 +6,7 @@ class SalesAnalystTest < Minitest::Test
   def setup
     @se = SalesEngine.from_csv({
       :items     => "./fixture_data/items_fixtures_file.csv",
-      :merchants => "./fixture_data/merchants.csv",
+      :merchants => "./fixture_data/merchants_sample.csv",
       })
   end
 
@@ -15,30 +15,30 @@ class SalesAnalystTest < Minitest::Test
   end
 
   def test_average_items_per_merchant
-    assert_equal 2.88, @se.analyst.average_items_per_merchant
+    assert_equal 2.42, @se.analyst.average_items_per_merchant
   end
 
   def test_average_items_per_merchant_standard_deviation
-    assert_equal 0.58, @se.analyst.average_item_standard_deviation
+    assert_equal 2.84, @se.analyst.average_item_standard_deviation
   end
-  
+
   def test_merchants_with_high_item_count
-    assert_equal 52, @se.analyst.merchant_names_with_high_item_count.count
+    assert_equal 1, @se.analyst.merchant_names_with_high_item_count.count
     assert_equal Merchant, @se.analyst.merchants_with_high_item_count[0].class
   end
 
   def test_average_item_price_for_merchant
-    assert_equal 16.66, @se.analyst.average_item_price_for_merchant(12334105)
+    assert_equal 29.99, @se.analyst.average_item_price_for_merchant(12334105)
   end
 
   def test_average_average_price_per_merchant
-    assert_equal 350.29, @se.analyst.average_average_price_per_merchant
+    assert_equal 67.40, @se.analyst.average_average_price_per_merchant
   end
 
   def test_golden_items
     assert_equal 29, @se.analyst.unit_price_array.count
     assert_equal Array, @se.analyst.golden_items.class
     assert_equal Item, @se.analyst.golden_items[1].class
-    assert_equal 7, @se.analyst.golden_items.count
+    assert_equal 5, @se.analyst.golden_items.count
   end
 end
