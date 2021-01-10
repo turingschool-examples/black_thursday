03ba317d22e784c34ebcb547795e6215df6a5973
Olivia Charles <ocharles92@gmail.com>
2021-01-09 18:21:52 -0700

    Completed invoice rb runs with minitest

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,3 +1,7 @@
+# require 'bigdecimal'
+# require 'bigdecimal/util'
+
+
 class Invoice
   attr_reader :id,
               :customer_id,
@@ -8,12 +12,12 @@ class Invoice
               :parent
 
   def initialize(args, parent)
-    @id = args[:id].to_i
+    @id          = args[:id].to_i
     @customer_id = args[:customer_id].to_i
     @merchant_id = args[:merchant_id].to_i
-    @status = args[:status].to_sym
-    @created_at = Time.parse(args[:created_at].to_s)
-    @updated_at = Time.now
-    @parent = parent
+    @status      = args[:status].to_sym
+    @created_at  = Time.parse(args[:created_at].to_s)
+    @updated_at  = Time.parse(args[:updated_at].to_s)
+    @parent      = parent
   end
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -16,20 +16,20 @@ class InvoiceTest < MiniTest::Test
   end
 
   def test_id_returns_invoice_id
-    skip
-    assert_equal Fixnum, @invoice.id.class
+
+    assert_equal Integer, @invoice.id.class
   end
 
   def test_customer_id_returns_the_invoice_customer_id
     assert_equal 679, @invoice.customer_id
 
-    # assert_equal Fixnum, @invoice.customer_id.class
+    assert_equal Integer, @invoice.customer_id.class
   end
 
   def test_merchant_id_returns_the_invoice_merchant_id
     assert_equal 12335690, @invoice.merchant_id
 
-    # assert_equal Fixnum, @invoice.merchant_id.class
+    assert_equal Integer, @invoice.merchant_id.class
   end
 
   def test_status_returns_the_invoice_status
@@ -38,7 +38,7 @@ class InvoiceTest < MiniTest::Test
   end
 
   def test_created_at_returns_the_time_instance_for_the_date_the_invoice_wasa_created
-    # assert_equal Time.parse("2015-07-10 00:00:00 -0600"), @invoice.created_at
+    assert_equal Time.parse("2015-07-10 00:00:00 -0600"), @invoice.created_at
      assert_equal Time, @invoice.created_at.class
   end
 
