901e799a38ad29db7f3873a64b8fa677094bd635
Andrew Smith <andrewpatrick138@gmail.com>
2021-01-10 13:59:42 -0800

    golden item and standard deviation maths

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -11,11 +11,11 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-    @sales_engine.find_average
+    find_average
   end
 
   def average_items_per_merchant_standard_deviation
-    @sales_engine.standard_deviation
+    standard_deviation
   end
 
   def merchants_with_high_item_count
@@ -41,8 +41,56 @@ class SalesAnalyst
     result = @sales_engine.merchants.merchant_list.map do |merchant|
       average_item_price_for_merchant(merchant.id)
     end
-    expected = result.sum(0.00) / result.size
+    expected = result.sum(0.0) / result.size
     total_averages = BigDecimal(expected, 5).to_s("F")
     total_averages.to_f.floor(2)
+  end
+
+  def find_average
+    (@sales_engine.items.item_list.count.to_f / @sales_engine.merchants.merchant_list.count.to_f).round(2)
+  end
+
+
+  # def standard_deviation
+  #   sum = 0
+  #   avg = average_average_price_per_merchant
+  #   @sales_engine.items.all.each do |item|
+  #     sum += (item.unit_price.to_f - avg)**2
+  #   end
+  #   variance = sum / (@sales_engine.items.all.length - 1)
+  #   Math.sqrt(variance)
+  # end
+
+  def standard_deviation
+    merchants = @sales_engine.merchants.all.map do |merchant|
+      @sales_engine.items.find_all_by_merchant_id(merchant.id).count
+    end
+    average = find_average
+    differences = 0
+    merchants.map do |num|
+      differences += (num - average)**2
+    end
+    Math.sqrt(differences / (merchants.count - 1).to_f).round(2)
+  end
+
+
+  # def golden_items
+  #   two_above = (standard_deviation * 2)
+  #   golden_item_price = (two_above + average_average_price_per_merchant)
+  #   golden_item_list = []
+  #   @sales_engine.items.item_list.each do |item|
+  #     if item.unit_price_to_dollars >= golden_item_price
+  #       golden_item_list << item.unit_price_to_dollars
+  #     end
+  #   end
+  #   golden_item_list
+  # end
+
+  def golden_items
+    require 'pry'; binding.pry
+    golden_price = find_average + standard_deviation * 2
+    @sales_engine.items.all.find_all do |item|
+      item.unit_price.to_f > golden_price
+    end
   end
 end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -16,24 +16,7 @@ class SalesEngine
   def find_items_by_id(id)
     items.find_all_by_merchant_id(id)
   end
-
-  def find_average
-    (items.item_list.count.to_f / merchants.merchant_list.count.to_f).round(2)
-  end
-
-  def standard_deviation
-    total_count = @merchants.merchant_list.reduce([]) do |acc, merchant|
-      acc << merchant.item_name.count
-      acc
-    end
-
-    sum = total_count.sum do |value|
-      ((value - find_average)**2)
-    end
-    result = (sum / 475)
-
-    Math.sqrt(result).round(2)
-  end
+  
 
   def find_merchants_with_most_items
     total_count = @merchants.merchant_list.reduce({}) do |acc, merchant|
@@ -45,7 +28,6 @@ class SalesEngine
       value >= 7
     end
   end
-
 
   def analyst
     SalesAnalyst.new(self)
@@ -63,31 +45,3 @@ class SalesEngine
     @items = ItemRepo.new(csv_data[:items], self)
   end
 end
-
-
-
-
-
-
-
-
-  # most_common_merchant_id_number_array = []
-  # item_list = []
-  # @sales_engine.items.item_list.each do |item|
-  #   item_list << item.merchant_id
-  # end
-  # until most_common_merchant_id_number_array.length == 3
-  #   this_is_the_most_common_merchant =  item_list.max_by {|merchant_id|item_list.count(merchant_id)}
-  #   most_common_merchant_id_number_array << this_is_the_most_common_merchant
-  #   item_list.delete(this_is_the_most_common_merchant)
-  # end
-  # answer = []
-  # @sales_engine.items.item_list.each do |item|
-  #     most_common_merchant_id_number_array.each do |most_common|
-  #     if most_common == item
-  #       answer << item
-  #     end
-  #   end
-  # end
-  # answer.flatten
-  # end
