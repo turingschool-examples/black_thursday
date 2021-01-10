f2e17d96cbfadf4ce09f50afc64a71bfea8e23ea
Trevor Suter <Trevorsuter@icloud.com>
2021-01-10 13:35:48 -0700

    initialize transaction

---
+++ test/transaction_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require 'pry'
+require './lib/transaction'
+
+class TestTransaction < Minitest::Test
+  def setup
+    @transaction = Transaction.new({
+      :id => 6,
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at => Time.now,
+      :updated_at => Time.now
+    })
+  end
+  
+  def test_it_exists
+    assert_instance_of Transaction, @transaction
+  end
+end
