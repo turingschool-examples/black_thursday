f8fce5383645b8f7baeaeca9d2fe671deeeb96cd
Trevor Suter <Trevorsuter@icloud.com>
2021-01-10 10:47:26 -0700

    integrated repos for sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,13 +2,19 @@ require_relative './merchant_repo'
 require_relative './item_repo'
 require_relative './invoice_repo'
 require_relative './sales_analyst'
+require_relative './invoice_item_repo'
+require_relative './customer_repo'
+require_relative './transaction_repo'
 
 
 class SalesEngine
   attr_reader :items,
               :merchants,
               :invoices,
-              :sales_analyst
+              :sales_analyst,
+              :invoice_items,
+              :customers,
+              :transactions
 
   def initialize(data)
     process_data(data)
@@ -26,6 +32,12 @@ class SalesEngine
         @items = ItemRepository.new(data[:items], self)
       elsif key == :invoices
         @invoices = InvoiceRepository.new(data[:invoices], self)
+      elsif key == :invoice_items
+        @invoice_items = InvoiceItemRepository.new(data[:invoice_items], self)
+      elsif key == :customers
+        @customers = CustomerRepository.new(data[:customers], self)
+      elsif key == :transactions
+        @transactions = TransactionRepository.new(data[:transactions], self)
       end
     end
   end
