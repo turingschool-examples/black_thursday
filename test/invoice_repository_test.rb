4e035bd5a26b74814d287a5c63c8e7ecd9996285
Olivia Charles <ocharles92@gmail.com>
2021-01-09 19:27:25 -0500

    Completed invoice tests, started running invoice_repository tests

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -18,7 +18,7 @@ class InvoiceRepositoryTest < MiniTest::Test
   def test_it_exists
     invoice_id = 3452
     expected = @engine.invoices.find_by_id(invoice_id)
-    
+
     assert_equal invoice_id, expected.id
     assert_equal 12335690, expected.merchant_id
     assert_equal 679, expected.customer_id
@@ -29,14 +29,15 @@ class InvoiceRepositoryTest < MiniTest::Test
     assert_nil expected
   end
 
-  # def test_find_all_by_customer_id
-  #   customer_id = 300
-  #   expected = @engine.invoices.find_all_by_customer_id(customer_id)
-  #   assert_equal 10, expected.length
-  #   customer_id = 1000
-  #   expected = @engine.invoices.find_all_by_customer_id(customer_id)
-  #   assert_equal [], @expected
-  # end
+  def test_find_all_by_customer_id
+    customer_id = 300
+    expected = @engine.invoices.find_all_by_customer_id(customer_id)
+    assert_equal 10, expected.length
+
+    customer_id = 1000
+    expected = @engine.invoices.find_all_by_customer_id(customer_id)
+    assert_equal [], @expected
+  end
   #
   # def test_find_all_by_merchant_id
   #   merchant_id = 12335080
