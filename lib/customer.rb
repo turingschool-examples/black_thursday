0a4404b964b751d897b0c31e2dfd043cc6b5d206
Olivia Charles <ocharles92@gmail.com>
2021-01-10 15:14:58 -0700

    Created Customer class and initialized

---
+++ lib/customer.rb
@@ -0,0 +1,18 @@
+class Customer
+  attr_reader :id,
+              :first_name,
+              :last_name,
+              :created_at,
+              :updated_at,
+              :parent
+
+  def initialize(args, parent)
+    @args        = args
+    @id          = args[:id]
+    @first_name  = args[:first_name]
+    @last_name   = args[:last_name]
+    @created_at  = Time.parse(args[:created_at].to_s)
+    @updated_at  = Time.parse(args[:updated_at].to_s)
+    @parent      = parent
+  end
+end
