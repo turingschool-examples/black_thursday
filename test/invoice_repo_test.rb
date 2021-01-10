f9febb3c6421fb2d971ce1a313c50959181570f8
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-01-10 14:19:58 -0700

    Add returns_all_invoices for test; passing.

--- test/invoice_repo_test.rb
+++ test/invoice_repo_test.rb
@@ -1,17 +1,26 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/invoice_repo'
-
+require './lib/cleaner'
 
 class InvoiceRepositoryTest < MiniTest::Test
 
+  def setup
+    @invoice_repo = InvoiceRepository.new
+    @cleaner = Cleaner.new
+  end
+
   def test_it_exists
-    invoice_repo = InvoiceRepository.new
-    assert_instance_of InvoiceRepository, invoice_repo
+    assert_instance_of InvoiceRepository, @invoice_repo
   end
 
   def test_it_has_attributes
-    invoice_repo = InvoiceRepository.new
-    assert_equal 4985, invoice_repo.invoices.count
+    skip
+    assert_equal [], @invoice_repo.invoices
   end
+
+  def test_it_returns_all_invoices
+    assert_equal 4985, @invoice_repo.all.length
+  end
+
 end
