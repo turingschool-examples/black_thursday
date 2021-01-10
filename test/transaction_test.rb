9394e836de438e8f2f15d1b1f62eb3dea6d16ce2
Olivia Charles <ocharles92@gmail.com>
2021-01-10 15:49:54 -0700

    Created test for update at returns a time instance for the date the invoice it was lasted changed

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -24,38 +24,38 @@ class TransactionTest < Minitest::Test
     #let(:transaction) { engine.transactions.find_by_id(1) }
   end
 
-    def test_id_returns_the_transaction_id
-      assert_equal 1, @transaction.id
-      assert_equal Fixnum, @transaction.id.clas
-    end
+  def test_id_returns_the_transaction_id
+    assert_equal 1, @transaction.id
+    assert_equal Fixnum, @transaction.id.clas
+  end
 
-    def test_invoice_id_returns_the_invoice_id
-      assert_equal 2179, @transaction.invoice_id
-      assert_equal Fixnum, @transaction.invoice_id.class
-    end
+  def test_invoice_id_returns_the_invoice_id
+    assert_equal 2179, @transaction.invoice_id
+    assert_equal Fixnum, @transaction.invoice_id.class
+  end
 
-    def test_credit_card_number_returns_the_credit_card_number
-      assert_equal "4068631943231473", @transaction.credit_card_number
-      assert_equal String, @transaction.credit_card_number.class
-    end
+  def test_credit_card_number_returns_the_credit_card_number
+    assert_equal "4068631943231473", @transaction.credit_card_number
+    assert_equal String, @transaction.credit_card_number.class
+  end
 
-    def test_credit_card_expiration_date_returns_the_credit_card_expiration
-      assert_equal "0217", @transaction.credit_card_expiration_date
-      assert_equal String, @transaction.credit_card_expiration_date.class
-    end
+  def test_credit_card_expiration_date_returns_the_credit_card_expiration
+    assert_equal "0217", @transaction.credit_card_expiration_date
+    assert_equal String, @transaction.credit_card_expiration_date.class
+  end
 
-    def test_result_returns_the_result
-      assert_equal :success, @transaction.result
-      assert_equal Symbol, @transaction.result.class
-    end
+  def test_result_returns_the_result
+    assert_equal :success, @transaction.result
+    assert_equal Symbol, @transaction.result.class
+  end
 
-    def test_created_at_returns_a_Time_instance_for_the_date_the_invoice_item_was_created
-      assert_equal  Time.parse("2012-02-26 20:56:56 UTC"), @transaction.created_at
-      assert_equal Time, @transaction.created_at.class
-    end
+  def test_created_at_returns_a_Time_instance_for_the_date_the_invoice_item_was_created
+    assert_equal  Time.parse("2012-02-26 20:56:56 UTC"), @transaction.created_at
+    assert_equal Time, @transaction.created_at.class
+  end
 
-  #     it "#updated_at returns a Time instance for the date the invoice item was last updated" do
-  #       expect(transaction.updated_at).to eq Time.parse("2012-02-26 20:56:56 UTC")
-  #       expect(transaction.updated_at.class).to eq Time
-  #     end
-  #   end
+  def test_updated_at_returns_a_time_instance_for_the_date_the_invoice_item_was_last
+    assert_equal Time.parse("2012-02-26 20:56:56 UTC"), @transaction.updated_at
+    assert_equal Time, @transaction.updated_at.class
+  end
+end
