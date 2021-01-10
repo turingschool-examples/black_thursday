86f173b04afdcde1478cb395979a70f33d62e305
Wil McCauley <wmccauley724@gmail.com>
2021-01-10 14:12:25 -0700

    Add test for max_id method, clean up test for create method

--- test/invoice_item_repo_test.rb
+++ test/invoice_item_repo_test.rb
@@ -36,4 +36,25 @@ class InvoiceItemRepo < Minitest::Test
     assert_equal 4, @ii_repo.find_all_by_invoice_id(519).length
     assert_equal [], @ii_repo.find_all_by_invoice_id(22)
   end
+
+  def test_it_returns_max_id
+    assert_equal 2353, @ii_repo.max_id
+  end
+
+  def test_it_can_create_a_new_item
+    ii_attr = {
+      :item_id => 7,
+      :invoice_id => 8,
+      :quantity => 1,
+      :unit_price => BigDecimal(10.99, 4),
+      :created_at => 10,
+      :updated_at => 12
+    }
+    assert_equal 2353, @ii_repo.max_id
+    @ii_repo.create(ii_attr)
+    ii = @ii_repo.find_by_id(2354)
+    assert_equal 2354, @ii_repo.max_id
+    assert_equal 8, ii.invoice_id
+    assert_equal 10.99, ii.unit_price
+  end
 end
