edb537c199f59444f6573feb609c7f75713f5d34
Wil McCauley <wmccauley724@gmail.com>
2021-01-09 18:06:41 -0700

    Add code to refactor object builder method and all method. Pass tests

--- lib/invoice_item_repo.rb
+++ lib/invoice_item_repo.rb
@@ -3,29 +3,34 @@ require_relative './invoice_item'
 require_relative './cleaner'
 
 class InvoiceItemRepository
+  attr_reader :invoice_items
+
   def initialize(file = './data/invoice_items.csv')
     @file = file
     @ii_csv = CSV.open(@file, headers: true, header_converters: :symbol)
-    @ii_array = []
+    @invoice_items = []
+    @cleaner = Cleaner.new
     ii_objects(@ii_csv)
   end
 
   def ii_objects(ii_rows)
     ii_rows.each do |row|
-      precision = row[:unit_price].to_s.length
-      @ii_array << InvoiceItem.new({:id       => row[:id].to_i,
+      precision      = row[:unit_price].length
+      value_adjusted = row[:unit_price].to_i * 0.01
+      # A module could hold assignment hashes for all classes
+      @invoice_items << InvoiceItem.new({:id       => row[:id].to_i,
                           :item_id        => row[:name],
-                          :description => row[:description],
-                          :unit_price  => BigDecimal.new(row[:unit_price]),
+                          :invoice_id => row[:invoice_id],
+                          :quantity    => row[:quantity],
+                          :unit_price  => BigDecimal.new(value_adjusted, precision),
                           :created_at  => @cleaner.clean_date(row[:created_at]),
                           :updated_at  => @cleaner.clean_date(row[:updated_at]),
-                          :merchant_id => row[:merchant_id].to_i
                         })
     end
-    @ii_array
+    @invoice_items
   end
 
   def all
-    @ii_array
+    @invoice_items
   end
 end
