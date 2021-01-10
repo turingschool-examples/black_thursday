4ad092b8e7cf63b6bad685246050ae4b0ba037a4
Olivia Charles <ocharles92@gmail.com>
2021-01-09 18:52:02 -0700

    Adjusted spacing in tests

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -4,16 +4,20 @@ class ItemRepositoryTest < Minitest::Test
     @parent = mock('parent')
     @ir = ItemRepository.new("./data/items.csv", @parent)
   end
+
   def test_it_exists
     assert_instance_of ItemRepository, @ir
   end
+
   def test_it_has_a_parent
     assert_equal @parent, @ir.parent
   end
+
   def test_all_displays_all_items
     x = @ir.all
     assert_equal 1367, x.length
   end
+
   def test_find_by_id_finds_an_item_by_id
     result_1 = @ir.find_by_id(263538760)
     assert_equal "263538760", result_1.id.to_s
@@ -21,6 +25,7 @@ class ItemRepositoryTest < Minitest::Test
     result_2 = @ir.find_by_id(1)
     assert_nil nil, result_2
   end
+
   def test_find_by_name_finds_an_item_by_name
     result_1 = @ir.find_by_name("Puppy blankie")
     assert_equal "Puppy blankie", result_1.name
@@ -28,6 +33,7 @@ class ItemRepositoryTest < Minitest::Test
     result_2 = @ir.find_by_name("Sales Engine")
     assert_nil nil, result_2
   end
+
   def test_find_all_with_description_finds_all_items_matching_given_description
     expected_1 = "A large Yeti of sorts, casually devours a cow as the others watch numbly."
     result_1 = @ir.find_all_with_description(expected_1)
@@ -40,6 +46,7 @@ class ItemRepositoryTest < Minitest::Test
     result_3 = @ir.find_all_with_description(expected_3)
     assert_equal 0, result_3.length
   end
+
   def test_find_all_by_price_finds_all_items_matching_given_price
     result_1 = @ir.find_all_by_price(25)
     assert_equal 79, result_1.length
@@ -51,6 +58,7 @@ class ItemRepositoryTest < Minitest::Test
     result_3 = @ir.find_all_by_price(20000)
     assert_equal 0, result_3.length
   end
+
   def test_find_all_by_price_in_range_finds_all_items_matching_given_price_range
     result_1 = @ir.find_all_by_price_in_range(1000.00..1500.00)
     assert_equal 19, result_1.length
@@ -61,12 +69,14 @@ class ItemRepositoryTest < Minitest::Test
     result_4 = @ir.find_all_by_price_in_range(0.00..10.00)
     assert_equal 302, result_4.length
   end
+
   def test_find_all_by_merchant_id_returns_array_of_items_associated_with_given_merchant_id
     result_1 = @ir.find_all_by_merchant_id(12334326)
     assert_equal 6, result_1.length
     result_2 = @ir.find_all_by_merchant_id(12336020)
     assert_equal 2, result_2.length
   end
+
   def test_create_creates_a_new_item_instance
     attributes = {
       name: "Capita Defenders of Awesome 2018",
@@ -80,16 +90,20 @@ class ItemRepositoryTest < Minitest::Test
     assert_equal 263567475, result_1[-1].id.to_i
     assert_equal "Capita Defenders of Awesome 2018", result_1[-1].name
   end
+
   def test_update_updates_an_item
     attributes_1 = {unit_price: BigDecimal.new(379.99, 5)}
     result_1 = @ir.update(263538760, attributes_1)
     assert_equal  379.99 , result_1.unit_price.to_f
+
     attributes_2 = {description: "Expensive dog blanket"}
     result_2 = @ir.update(263538760, attributes_2)
+    
     assert_equal "Expensive dog blanket", result_2.description
     time = Time.now
-     assert  time
+    assert  time
   end
+
   def test_update_cannot_update_id_update_at_update_merchant_id
     attributes = {
       id: 270000000,
@@ -98,13 +112,15 @@ class ItemRepositoryTest < Minitest::Test
     }
      x =  @ir.update(263538760, attributes)
      assert_nil  @ir.find_by_id(270000000)
-      # assert_nil  x.updated_at
+     # assert_nil  x.updated_at
      assert_equal [], @ir.find_all_by_merchant_id(1)
   end
+
   def test_delete_deletes_the_specified_item
     @ir.delete(263538760)
     assert_nil @ir.find_by_id(263538760)
   end
+
   def test_delete_unknown_item_does_nothing
     @ir.delete(270000000)
   end
