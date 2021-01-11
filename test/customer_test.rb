09cf3b72bb7ac2212ef51980d5760bd2004205f1
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-11 13:35:58 -0700

    Write attributes test

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -6,17 +6,25 @@ require 'mocha/minitest'
 
 class CustomerTest < Minitest::Test
   def setup
-    @customer = Customer.new
+    @customer = Customer.new({
+                        :id => 6,
+                        :first_name => "Joan",
+                        :last_name => 'Clarke',
+                        :created_at => Time.now,
+                        :updated_at => Time.now
+      })
   end
 
   def test_it_exists
     assert_instance_of Customer, @customer
   end
-  #
-  # def test_it_has_attributes
-  #   assert_equal "filler", @customer.filler
-  # end
-  #
-  # def test_it_can_have_different_attributes
-  # end
+
+  def test_it_has_attributes
+    assert_equal 6, @customer.id
+    assert_equal "Joan", @customer.first_name
+    assert_equal "Clarke", @customer.last_name
+    assert_equal Time.now.strftime("%d/%m/%Y"), @customer.created_at
+    assert_equal Time.now.strftime("%d/%m/%Y").updated_at
+  end
+
 end
