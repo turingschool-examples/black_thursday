a64cdddabec6cfe48234364f0aec239d85b43694
Regina Casias <reginaannvigil@gmail.com>
2021-01-11 13:47:35 -0800

    Fixed asserts for transaction

--- test/transaction_repo_test.rb
+++ test/transaction_repo_test.rb
@@ -21,7 +21,7 @@ class TransactionRepoTest < MiniTest::Test
   end
 
   def test_it_exists
-    assert_instance_of TransactionRepo, @se.transactions
+    assert_instance_of TransactionRepo, @transaction
   end
 
   def test_transaction_repo_can_find_all
@@ -30,8 +30,8 @@ class TransactionRepoTest < MiniTest::Test
   end
 
   def test_transaction_repo_can_find_by_id
-    expected = @se.transactions.find_by_id(2)
-    expect_nil = @se.transactions.find_by_id(000)
+    expected = @transaction.find_by_id(2)
+    expect_nil = @transaction.find_by_id(000)
 
     assert_equal 2, expected.id
     assert_equal Transaction, expected.class
@@ -47,14 +47,14 @@ class TransactionRepoTest < MiniTest::Test
   end
 
   def test_find_all_by_credit_card_number
-  expected = @se.transactions.find_all_by_credit_card_number("4848466917766329")
+  expected = @transaction.find_all_by_credit_card_number("4848466917766329")
 
   assert_equal 1, expected.length
   assert_equal Transaction, expected.first.class
   end
 
   def test_find_find_all_by_result
-  expected = @se.transactions.find_all_by_result(:success)
+  expected = @transaction.find_all_by_result(:success)
 
   assert_equal 4158, expected.length
   assert_equal Transaction, expected.first.class
@@ -70,7 +70,7 @@ class TransactionRepoTest < MiniTest::Test
                 :updated_at => Time.now
               }
   @se.transactions.create(attributes)
-  expected = @se.transactions.find_by_id(4986)
+  expected = @transaction.find_by_id(4986)
   assert_equal 8, expected.invoice_id
   end
 
@@ -80,14 +80,14 @@ class TransactionRepoTest < MiniTest::Test
   attributes = {
                 result: :failed
                 }
-  @se.transactions.update(4158, attributes)
-  expected = @se.transactions.find_by_id(4158)
+  @transaction.update(4158, attributes)
+  expected = @transaction.find_by_id(4158)
   assert_equal :failed, expected.result
   end
 
   def test_delete_a_tansaction
-  @se.transactions.delete(4986)
-  expected = @se.transactions.find_by_id(4986)
+  @transaction.delete(4986)
+  expected = @transaction.find_by_id(4986)
   assert_nil expected
   end
 end
