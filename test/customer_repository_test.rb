3ff5665e5eb28f064593b9821f2554482714c554
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-11 14:32:44 -0700

    Write assertations for id find test

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -16,12 +16,17 @@ class CustomerRepositoryTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal 1000, @repository.all.length
-    assert_equal "1", @repository.all[0].id
+    assert_equal 1, @repository.all[0].id
     assert_equal "Joey", @repository.all[0].first_name
     assert_equal "Ondricka", @repository.all[0].last_name
     assert_equal "27/03/2012", @repository.all[0].updated_at
     assert_equal "27/03/2012", @repository.all[0].created_at
   end
 
+
+  def test_it_finds_by_id
+    assert_equal nil, @repository.find_by_id(1001)
+    assert_equal "Logan", @repository.find_by_id(11).first_name
+  end
 
 end
