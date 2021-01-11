f6193d894bcfe1cc50d07586a756f6d6291a87c5
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-11 13:58:18 -0700

    Write tests for CustomerRepository attributes

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,4 +1,7 @@
 require 'csv'
+require 'bigdecimal'
+require 'time'
+require_relative './customer'
 
 class CustomerRepository
   attr_reader :all,
@@ -9,8 +12,12 @@ class CustomerRepository
     @engine = engine
 
     CSV.foreach(items_path, headers: true, header_converters: :symbol) do |row|
-      @all << row
+      @all << convert_to_customer(row)
     end
+  end
+
+  def convert_to_customer(row)
+
   end
 
 
