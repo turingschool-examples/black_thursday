b8d1030922a7ab132a5a69177fca8cbf6f02983d
Gvieve <gnuebel84@yahoo.com>
2021-01-11 16:15:13 -0700

    Create remaining tests/methods for transaction_respository

---
+++ lib/transaction_repository.rb
@@ -0,0 +1,93 @@
+require 'csv'
+require 'bigdecimal'
+require 'time'
+require_relative './transaction'
+
+class TransactionRepository
+  attr_reader :all,
+              :engine
+
+  def initialize(transaction_path, engine)
+    @all = []
+    @engine = engine
+
+    CSV.foreach(transaction_path, headers: true, header_converters: :symbol) do |row|
+      @all << convert_to_item(row)
+    end
+  end
+
+  def convert_to_item(row)
+    row = Transaction.new({
+                id: row[:id],
+                invoice_id: row[:invoice_id],
+                credit_card_number: row[:credit_card_number],
+                credit_card_expiration_date: row[:credit_card_expiration_date],
+                result: row[:result],
+                created_at: row[:created_at],
+                updated_at: row[:updated_at]
+                   }, self)
+  end
+
+  def find_by_id(id)
+    @all.find do |transaction|
+      transaction.id == id
+    end
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    @all.find_all do |transaction|
+      transaction.invoice_id.include?(invoice_id.to_s)
+    end
+  end
+
+  def find_all_by_credit_card_number(cc_number)
+    @all.find_all do |transaction|
+      transaction.credit_card_number.include?(cc_number)
+    end
+  end
+
+  def find_all_by_result(result)
+    @all.find_all do |transaction|
+      transaction.result == result
+    end
+  end
+
+  def new_highest_id
+    all.max_by do |instance|
+      instance.id
+    end.id + 1
+  end
+
+  def create(attributes)
+    attributes[:id] = new_highest_id
+    transaction = Transaction.new(attributes, self)
+    @all << transaction
+    transaction
+  end
+
+
+  def check_result_is_valid(attributes)
+   if attributes.class == Hash
+      results = [:success, :failed]
+      result = :"#{attributes[:result]}"
+      results.include?(result)
+    end
+  end
+
+  def update(id, attributes)
+    # require "pry"; binding.pry
+    if !find_by_id(id).nil? && check_result_is_valid(attributes)
+      new_result = :"#{attributes[:result]}"
+      update_transaction = all.find { |transaction| transaction.id == id }
+      update_transaction.credit_card_number = attributes[:credit_card_number] if !attributes[:credit_card_number].nil?
+      update_transaction.credit_card_expiration_date = attributes[:credit_card_expiration_date] if !attributes[:credit_card_expiration_date].nil?
+      update_transaction.result = new_result if !attributes[:result].nil?
+      update_transaction.updated_at = Time.now
+    end
+  end
+
+  def delete(id)
+    remove = find_by_id(id)
+    @all.delete(remove)
+  end
+end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -2,16 +2,91 @@ require_relative './test_helper'
 require 'time'
 require './lib/transaction'
 require './lib/transaction_repository'
-require 'csv'
+require 'mocha/minitest'
 
-class InvoiceRepositoryTest < Minitest::Test
+class TransactionRepositoryTest < Minitest::Test
 
   def setup
-    @tr = TransactionRepository.new("./fixture_data/transactions_sample.csv", "engine")
+    @engine = mock
+    @tr = TransactionRepository.new("./fixture_data/transactions_sample.csv", @engine)
   end
 
   def test_it_exists_and_has_attributes
     assert_instance_of TransactionRepository, @tr
+  end
+
+  def test_it_can_create_transactions_from_csv
     assert_equal 128, @tr.all.count
+  end
+
+  def test_it_finds_by_id
+    transaction = @tr.all.first
+
+    assert_equal transaction, @tr.find_by_id(6)
+    assert_nil @tr.find_by_id(1)
+  end
+
+  def test_find_all_by_invoice_id
+    assert_equal 4, @tr.find_all_by_invoice_id(45).count
+    assert_equal [], @tr.find_all_by_invoice_id(14105848451)
+  end
+
+  def test_find_all_by_credit_card_number
+    credit_card_number_1 = "4709998508762802"
+    credit_card_number_2 = "2844"
+
+    assert_equal 2, @tr.find_all_by_credit_card_number(credit_card_number_1).count
+    assert_equal 1, @tr.find_all_by_credit_card_number(credit_card_number_2).count
+    assert_equal [], @tr.find_all_by_credit_card_number("4709998508762711")
+  end
+
+  def test_find_all_by_result
+    result = :success
+
+    assert_equal 108, @tr.find_all_by_result(result).count
+    assert_equal [], @tr.find_all_by_result(:testing)
+  end
+
+
+  def test_new_highest_id
+    assert_equal 4945, @tr.new_highest_id
+  end
+
+  def test_it_can_create_new_transaction
+    expected = @tr.new_highest_id
+    attributes =  {
+                  :id => 6,
+                  :invoice_id => 8,
+                  :credit_card_number => "4242424242424242",
+                  :credit_card_expiration_date => "0220",
+                  :result => "success",
+                  :created_at => Time.now
+                  }
+
+    @tr.create(attributes)
+
+    assert_equal expected, attributes[:id]
+  end
+
+  def test_it_can_update_transaction
+    original_time = @tr.find_by_id(14).updated_at
+    attributes = {credit_card_number: "4558368405929183",
+                  credit_card_expiration_date: "0417",
+                  result: "success",
+                  id: 9999}
+
+    @tr.update(14, attributes)
+
+    assert_equal "4558368405929183", @tr.find_by_id(14).credit_card_number
+    assert_equal "0417", @tr.find_by_id(14).credit_card_expiration_date
+    assert_equal :success, @tr.find_by_id(14).result
+    assert_equal 14, @tr.find_by_id(14).id
+    assert_equal true, @tr.find_by_id(14).updated_at > original_time
+  end
+
+  def test_it_can_delete_transction
+    @tr.delete(14)
+
+    assert_nil @tr.find_by_id(14)
   end
 end
