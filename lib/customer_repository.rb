2966e5c9f13b1e19a7d893304271577a560f6ee7
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-11 14:33:06 -0700

    Write code to pass id test

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -17,7 +17,7 @@ class CustomerRepository
   end
 
   def convert_to_customer(row)
-    row = Customer.new({id: row[:id],
+    row = Customer.new({id: row[:id].to_i,
                     first_name: row[:first_name],
                     last_name: row[:last_name],
                     created_at: row[:created_at],
@@ -25,8 +25,11 @@ class CustomerRepository
                    }, self)
   end
 
-  def turd
-    require "pry"; binding.pry
+
+  def find_by_id(id)
+    @all.find do |customer|
+      customer.id == id
+    end
   end
 
 end
