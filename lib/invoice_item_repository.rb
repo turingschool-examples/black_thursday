2f2351ae0446ac21d88626902eae43b548f6deae
Gvieve <gnuebel84@yahoo.com>
2021-01-11 19:43:26 -0700

    Update InvoiceItem & Repo to pass spec harness

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -34,14 +34,14 @@ class InvoiceItemRepository
     end
   end
 
-  def find_by_item_id(item_id)
-    @all.find do |invoice_item|
+  def find_all_by_item_id(item_id)
+    @all.find_all do |invoice_item|
       invoice_item.item_id == item_id
     end
   end
 
   def find_all_by_invoice_id(invoice_id)
-    @all.find do |invoice_item|
+    @all.find_all do |invoice_item|
       invoice_item.invoice_id == invoice_id
     end
   end
