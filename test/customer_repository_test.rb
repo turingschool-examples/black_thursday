3c40f24487f12212b4390cf487c87285f71f7785
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-11 18:00:20 -0700

    Add asssertion for deleting customer

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -29,11 +29,6 @@ class CustomerRepositoryTest < Minitest::Test
     assert_equal "Logan", @repository.find_by_id(11).first_name
   end
 
-  # def test_it_finds_by_name
-  #   assert_equal nil, @repository.find(1001)
-  #   assert_equal "Logan", @repository.find(11).first_name
-  # end
-
   def test_find_all_by_name
     assert_equal 5, @repository.find_all_with_name("first name", "joe").length
     assert_equal [], @repository.find_all_with_name("first name", "little lord fauntleroy")
@@ -41,4 +36,28 @@ class CustomerRepositoryTest < Minitest::Test
     assert_equal Customer, @repository.find_all_with_name("last name", "toy")[0].class
   end
 
+  def test_new_highest_id
+    assert_equal 1001, @repository.new_highest_id
+  end
+
+  def test_it_can_create
+    @repository.create(:first_name => "Jaqob",
+                       :last_name => "Hilario",
+                       :created_at => Time.now,
+                       :updated_at => Time.now)
+    assert_equal "Jaqob", @repository.all[-1].first_name
+  end
+  #
+  def test_it_can_update_customer
+    attributes = {first_name: "Charles",
+                  last_name: "Vera"}
+
+    @repository.update(3, attributes)
+
+  end
+
+  def test_it_can_delete_customer
+    @repository.delete(1000)
+    assert_nil @repository.find_by_id(1000)
+  end
 end
