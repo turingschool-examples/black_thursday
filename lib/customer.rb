bce0d22b5aeafab6e5e17184d9274b51371b82c6
Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>
2021-01-11 19:24:52 -0700

    Update customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,8 +16,8 @@ class Customer
     @id = data[:id]
     @first_name = data[:first_name]
     @last_name = data[:last_name]
-    @created_at = time_store(data[:created_at]).strftime("%d/%m/%Y")
-    @updated_at = time_store(data[:updated_at]).strftime("%d/%m/%Y")
+    @created_at = time_store(data[:created_at])
+    @updated_at = time_store(data[:updated_at])
     @repository = repository
   end
 
