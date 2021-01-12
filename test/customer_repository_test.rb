1c6e3fd723eb0bdb9b83c5cd3efadcf8037254e6
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-11 17:14:10 -0700

    Write assertions for find_by_name test

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -21,12 +21,24 @@ class CustomerRepositoryTest < Minitest::Test
     assert_equal "Ondricka", @repository.all[0].last_name
     assert_equal "27/03/2012", @repository.all[0].updated_at
     assert_equal "27/03/2012", @repository.all[0].created_at
+    assert_equal @engine, @repository.engine
   end
-
 
   def test_it_finds_by_id
     assert_equal nil, @repository.find_by_id(1001)
     assert_equal "Logan", @repository.find_by_id(11).first_name
+  end
+
+  # def test_it_finds_by_name
+  #   assert_equal nil, @repository.find(1001)
+  #   assert_equal "Logan", @repository.find(11).first_name
+  # end
+
+  def test_find_all_by_name
+    assert_equal 5, @repository.find_all_with_name("first name", "joe").length
+    assert_equal [], @repository.find_all_with_name("first name", "little lord fauntleroy")
+    assert_equal 2, @repository.find_all_with_name("last name", "toy").length
+    assert_equal Customer, @repository.find_all_with_name("last name", "toy")[0].class
   end
 
 end
