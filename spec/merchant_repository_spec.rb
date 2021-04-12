4743eb0f015050e0581dfa8a6c2f02fb07b789c8
Richard <rdesilvey@gmail.com>
2021-04-12 16:52:48 -0600

    Test for empty repo

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -7,5 +7,9 @@ describe MerchantRepository do
       m_repo = MerchantRepository.new
       expect(m_repo).to be_instance_of MerchantRepository
     end
+    it 'has by default an empty repo' do
+      m_repo = MerchantRepository.new
+      expect(m_repo.all).to eq []
+    end
   end
 end
