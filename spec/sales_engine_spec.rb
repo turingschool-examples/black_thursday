4f3eecd3f7d083f6b2226669ef0cfd7d5e798482
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-04-12 15:57:46 -0600

    Update sales_engine_spec.rb
    More hound style requests.

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -4,10 +4,10 @@ require './lib/sales_engine'
 RSpec.describe 'SalesEngine' do
   describe 'initialize' do
     it 'stores the hash for csv location' do
-      se = SalesEngine.from_csv({
+      se = SalesEngine.from_csv(
         :items => './data/items.csv',
         :merchants => './data/merchants.csv',
-        })
+                               )
 
       expect(se.class).to eq(SalesEngine)
     end
