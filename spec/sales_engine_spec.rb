9b88b8f3d5aa5b40f3c173027c0db7eb982dbd43
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2021-04-12 15:56:49 -0600

    Update sales_engine_spec.rb
    Style changes per hound request.

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -1,14 +1,13 @@
-require "csv"
-require "./lib/sales_engine"
+require 'csv'
+require './lib/sales_engine'
 
-RSpec.describe "SalesEngine" do
-  describe "initialize" do
-    it "stores the hash for csv location" do
+RSpec.describe 'SalesEngine' do
+  describe 'initialize' do
+    it 'stores the hash for csv location' do
       se = SalesEngine.from_csv({
-        :items     => "./data/items.csv",
-        :merchants => "./data/merchants.csv",
+        :items => './data/items.csv',
+        :merchants => './data/merchants.csv',
         })
-        require "pry"; binding.pry
 
       expect(se.class).to eq(SalesEngine)
     end
