252c7129039f2b90c4176a7af37c2b9ba73a3dfc
NoahZinter <noah.zinter@gmail.com>
2021-04-12 16:59:44 -0600

    Complete attr_reader tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -65,4 +65,66 @@ describe Item do
     expect(item.description).to eq "You can use it to write things"
     expect(item.description).is_a? String
   end
+
+  it 'has a unit price' do
+    details = {
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    }
+    item = Item.new(details)
+
+    expect(item.unit_price).to eq BigDecimal.new(10.99,4)
+    expect(item.unit_price).is_a? BigDecimal
+  end
+
+  it 'has a created_at Time' do
+    details = {
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    }
+    item = Item.new(details)
+
+    expect(item.created_at).is_a? Time
+  end
+
+  it 'has a updated_at Time' do
+    details = {
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    }
+    item = Item.new(details)
+
+    expect(item.updated_at).is_a? Time
+  end
+
+  it 'has a merchant_id' do 
+    details = {
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal.new(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    }
+    item = Item.new(details)
+
+    expect(item.merchant_id).to eq 2
+    expect(item.merchant_id).is_a? Integer
+  end
 end
