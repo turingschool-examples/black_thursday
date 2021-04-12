854058bffc104a0a67ed4d733fdb8f1c88320672
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-12 15:48:20 -0600

    Create and test SalesEngine

---
+++ lib/sales_engine.rb
@@ -0,0 +1,21 @@
+require "csv"
+require "./lib/merchant_repository"
+
+class SalesEngine
+
+  def initialize(file_hash)
+    @location_hash = file_hash
+  end
+
+  def self.from_csv(file_hash)
+    SalesEngine.new(file_hash)
+  end
+
+  def items
+    ItemRepository.new(CSV.parse(File.read(@location_hash[:items]), headers: true))
+  end
+
+  def merchants
+    MerchantRepository.new(CSV.parse(File.read(@location_hash[:merchants]), headers: true))
+  end
+end
---
+++ spec/sales_engine_spec.rb
@@ -0,0 +1,16 @@
+require "csv"
+require "./lib/sales_engine"
+
+RSpec.describe "SalesEngine" do
+  describe "initialize" do
+    it "stores the hash for csv location" do
+      se = SalesEngine.from_csv({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+        })
+        require "pry"; binding.pry
+
+      expect(se.class).to eq(SalesEngine)
+    end
+  end
+end
