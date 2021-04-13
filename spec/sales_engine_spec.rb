287427e4d46b1facb6f2430fd1271c79af3f45b4
Richard <rdesilvey@gmail.com>
2021-04-13 08:48:24 -0600

    Fix test

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -3,8 +3,9 @@ require './lib/sales_engine'
 
 describe SalesEngine do
   describe '#from_csv' do
-    it 'creates a new instance of SalesEngine with no files' do
-      sales_engine = SalesEngine.from_csv(items: '', merchants: '')
+    it 'creates a new instance of SalesEngine' do
+      sales_engine = SalesEngine.from_csv(items: './data/items.csv', 
+                                          merchants: './data/merchants.csv')
       expect(sales_engine).to be_instance_of SalesEngine
     end
   end
