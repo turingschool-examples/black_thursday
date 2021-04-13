526e6738ac57461aa9fccbf1e11a14fe1c16e621
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-04-13 12:15:53 -0600

    Create from_csv method

--- lib/salesengine.rb
+++ lib/salesengine.rb
@@ -1,2 +1,19 @@
+require 'CSV'
+
 class SalesEngine
+  attr_reader :item,
+              :merchant
+  def initialize(sales_info)
+    @item = sales_info[:items]
+    @merchant = sales_info[:merchants]
+  end
+
+  def self.from_csv(sales_info)
+    items = sales_info[:items]
+    merchants = sales_info[:merchants]
+    CSV.foreach(items, headers: true, header_converters: :symbol) do |row|
+      require "pry"; binding.pry
+
+    end
+  end
 end
--- spec/salesengine_spec.rb
+++ spec/salesengine_spec.rb
@@ -1,18 +1,21 @@
 require "CSV"
-require "rspec"
-require "./data/items.csv"
-require "./data/merchants.csv"
+require "RSpec"
 require "./lib/salesengine"
-require "./lib/merchantrepository"
-require "./lib/itemrepository"
 
 RSpec.describe SalesEngine do
   describe 'instantiation' do
     it '::new' do
-      se = SalesEngine.from_csv({:items => "./data/items.csv",
-                                 :merchants => "./data/merchants.csv",})
+
+      se = SalesEngine.new({:items => "./data/items.csv",
+                            :merchants => "./data/merchants.csv",})
 
       expect(se).to be_an_instance_of(SalesEngine)
+    end
+
+    it 'has csv' do
+      se = SalesEngine.from_csv({:items => "./data/items.csv",
+                                 :merchants => "./data/merchants.csv",})
+      expect(se.from_csv).to eq([])
     end
   end
 end
