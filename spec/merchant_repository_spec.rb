ceb43ce7bdb495bc85097f016cae76511e56cb9c
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-12 19:38:39 -0600

    Create tests for an instance and all method

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -1,20 +1,38 @@
 require 'SimpleCOV'
 require 'csv'
+require './lib/sales_engine'
 require './lib/merchant_repository'
 
 RSpec.describe MerchantRepository do
-  it 'exists' do
-    mr = MerchantRepository.new
+  describe 'Instance' do
+    it 'exists' do
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv')
 
-    expect().to be_an_instance_og(MerchantRepository)
+      mr = se.merchants
+
+      expect(mr).to be_an_instance_og(MerchantRepository)
+    end
   end
 
-  it 'returns an array of all merchant instances' do
-    mr = MerchantRepository.new
+  describe '#all' do
+    it 'returns an array of all merchant instances' do
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv')
 
-    expected =
+      mr = se.merchants
 
-    expect(mr.all).to eq(expected)
+      merchant_1 = Merchant.new({
+                    id: '12334105',
+                    name: 'Shopin1901',
+                    created_at:	'2010-12-10',
+                    updated_at:	'2011-12-04'
+        })
+
+      expect(mr.all[0]).to eq(merchant_1)
+    end
   end
 
   it 'finds merchant by id' do
