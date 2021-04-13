a2810c9991aab0325a996e0d5fdf598d67167091
Richard <rdesilvey@gmail.com>
2021-04-13 12:22:44 -0600

    Implement helper methods
    for load_items and load_merchants

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,11 +1,28 @@
-class SalesEngine
-  def self.from_csv(files)
-    SalesEngine.new(files)
-  end
-  private
+require 'CSV'
+require './lib/item'
+require './lib/merchant'
 
-  def initialize(files)
-    load_items(files[:items])
-    load_merchants(files[:merchants])
+class SalesEngine
+  attr_reader :items, :merchants
+
+  def self.from_csv(files)
+    instance = SalesEngine.new
+    instance.load_items(files[:items])
+    instance.load_merchants(files[:merchants])
+    return instance
+  end
+
+  def load_items(file_name)
+    @items = []
+    CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
+      @items << Item.new(row)
+    end
+  end
+
+  def load_merchants(file_name)
+    @merchants = []
+    CSV.foreach(file_name, headers: true, header_converters: :symbol) do |row|
+      @merchants << Merchant.new(row)
+    end
   end
 end
