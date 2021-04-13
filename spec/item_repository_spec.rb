2b4c44cfb7590e9c74fa9b4742981085f923a6d3
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-12 20:10:46 -0600

    Create ItemRepository exits test

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -3,11 +3,14 @@ require './lib/sales_engine'
 RSpec.describe 'ItemRepository' do
   describe '#initialize' do
     it 'creates an instance of ItemRepository' do
-      se = SalesEngine.from_csv({
+      se = SalesEngine.from_csv(
         :items     => "./data/items.csv",
-        :merchants => "./data/merchants.csv",
-        })
-      require "pry"; binding.pry
+        :merchants => "./data/merchants.csv"
+      )
+
+      ir = se.items
+
+      expect(ir.class).to eq(ItemRepository)
     end
   end
 end
