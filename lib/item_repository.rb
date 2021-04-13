6ea31a95aff452773821465bee853d173ce58859
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-13 16:14:36 -0600

    Create find_by_id method

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -4,4 +4,14 @@ class ItemRepository
   def initialize(items = [])
     @items = items
   end
+
+  def all
+    @items
+  end
+
+  def find_by_id(id)
+    @items.find {|item| item.id == id}
+  end
+
+
 end
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,4 +1,6 @@
 require 'rspec'
+require 'bigdecimal'
+require './lib/item'
 require './lib/item_repository'
 
 describe ItemRepository do
@@ -18,5 +20,48 @@ describe ItemRepository do
     end
   end
 
+  describe '#all' do
+    it 'returns a list of all items' do
+      item1 = instance_double("Item")
+      item2 = instance_double("Item")
+      items = [item1, item2]
+      item_repository = ItemRepository.new(items)
 
+      expect(item_repository.all).to eq [item1, item2]
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'returns nil if no item has the specified id' do
+      details = {
+        id: 1,
+        name: 'Pencil',
+        description: 'You can use it to write things',
+        unit_price: BigDecimal(10.99,4),
+        created_at: Time.now,
+        updated_at: Time.now,
+        merchant_id: 2
+      }
+      item = Item.new(details)
+      item_repository = ItemRepository.new([item])
+
+      expect(item_repository.find_by_id(2)).to eq nil
+    end
+
+    it 'returns nil if no item has the specified id' do
+      details = {
+        id: 1,
+        name: 'Pencil',
+        description: 'You can use it to write things',
+        unit_price: BigDecimal(10.99,4),
+        created_at: Time.now,
+        updated_at: Time.now,
+        merchant_id: 2
+      }
+      item = Item.new(details)
+      item_repository = ItemRepository.new([item])
+
+      expect(item_repository.find_by_id(1)).to eq item 
+    end
+  end
 end
