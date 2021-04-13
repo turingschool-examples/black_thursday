0faf96fc5fd51aaf9f0ba9a0da0a70b47c7c21da
Caleb W. (He/Him) <78196294+Caleb1991@users.noreply.github.com>
2021-04-13 16:30:40 -0600

    Update merchant_repository.rb

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -12,25 +12,25 @@ class MerchantRepository
 
   def find_by_id(id)
     @csv_array.find do |merchant|
-    merchant.id == id
+      merchant.id == id
     end
   end
 
   def find_by_name(name)
     @csv_array.find do |merchant|
-    merchant.name.casecmp == name.case.cmp
+      merchant.name.casecmp == name.case.cmp
     end
   end
 
   def find_all_by_name(name)
     @csv_array.find_all do |merchant|
-    merchant.name.downcase.include?(name.downcase)
+      merchant.name.downcase.include?(name.downcase)
     end
   end
 
   def max_id_plus_one
     max = @csv_array.max_by do |merchant|
-    merchant.id
+      merchant.id
     end
     new = max.id.to_i + 1
     new.to_s
@@ -43,7 +43,7 @@ class MerchantRepository
 
   def update(id, name)
     new = @csv_array.find do |merchant|
-    merchant.id == id
+      merchant.id == id
     end
 
     new.name = name
