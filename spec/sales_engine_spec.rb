feb4e729b6e35a24d72e76c926e0f93c9d273c55
Richard <rdesilvey@gmail.com>
2021-04-13 08:56:16 -0600

    Test creating an instance loads data

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -4,9 +4,17 @@ require './lib/sales_engine'
 describe SalesEngine do
   describe '#from_csv' do
     it 'creates a new instance of SalesEngine' do
-      sales_engine = SalesEngine.from_csv(items: './data/items.csv', 
+      sales_engine = SalesEngine.from_csv(items: './data/items.csv',
                                           merchants: './data/merchants.csv')
       expect(sales_engine).to be_instance_of SalesEngine
+    end
+    it 'loads files' do
+      sales_engine = SalesEngine.from_csv(items: './data/items.csv',
+                                          merchants: './data/merchants.csv')
+      items = sales_engine.items
+      merchants = sales_engine.merchants
+      expect(items).not_to eq []
+      expect(merchants).not_to eq []
     end
   end
 end
