385d5ddfb6c8dcf32b995d474b589c6332970101
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-13 15:33:32 -0600

    Refactor given hound suggestions

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -12,38 +12,38 @@ class MerchantRepository
 
   def find_by_id(id)
     @csv_array.find do |merchant|
-      merchant.id == id
+    merchant.id == id
     end
   end
 
   def find_by_name(name)
     @csv_array.find do |merchant|
-      merchant.name.downcase == name.downcase
+    merchant.name.downcase == name.downcase
     end
   end
 
   def find_all_by_name(name)
     @csv_array.find_all do |merchant|
-      merchant.name.downcase.include?(name.downcase)
+    merchant.name.downcase.include?(name.downcase)
     end
   end
 
   def max_id_plus_one
-      max = @csv_array.max_by do |merchant|
-          merchant.id
-      end
+    max = @csv_array.max_by do |merchant|
+    merchant.id
+    end
     new = max.id.to_i + 1
     new.to_s
   end
 
   def create(name)
     Merchant.new(id: max_id_plus_one,
-                  name: name)
+                 name: name)
   end
 
   def update(id, name)
     new = @csv_array.find do |merchant|
-      merchant.id == id
+    merchant.id == id
     end
 
     new.name = name
