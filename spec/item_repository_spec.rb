ae62ec325ff73091c2d20184d859ab9457481712
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-12 20:24:54 -0600

    Add nil test to find_by_id

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'ItemRepository' do
   describe '#initialize' do
     it 'creates an instance of ItemRepository' do
       se = SalesEngine.from_csv(
-        :items     => "./data/items.csv",
-        :merchants => "./data/merchants.csv"
+        :items     => './data/items.csv',
+        :merchants => './data/merchants.csv'
       )
 
       ir = se.items
@@ -14,8 +14,8 @@ RSpec.describe 'ItemRepository' do
     end
     it 'is created with an array of items' do
       se = SalesEngine.from_csv(
-        :items     => "./data/items.csv",
-        :merchants => "./data/merchants.csv"
+        :items     => './data/items.csv',
+        :merchants => './data/merchants.csv'
       )
 
       ir = se.items
@@ -27,8 +27,8 @@ RSpec.describe 'ItemRepository' do
    describe '#all' do
      it 'returns an array of all items created so far' do
        se = SalesEngine.from_csv(
-         :items     => "./data/items.csv",
-         :merchants => "./data/merchants.csv"
+         :items     => './data/items.csv',
+         :merchants => './data/merchants.csv'
        )
 
        ir = se.items
@@ -37,15 +37,25 @@ RSpec.describe 'ItemRepository' do
      end
    end
    describe '#find_by_id' do
-     it "returns an instance of an item" do
+     it 'returns an instance of an item' do
        se = SalesEngine.from_csv(
-         :items     => "./data/items.csv",
-         :merchants => "./data/merchants.csv"
+         :items     => './data/items.csv',
+         :merchants => './data/merchants.csv'
        )
 
        ir = se.items
 
        expect(ir.find_by_id('263397919').name).to eq('Le câlin')
+     end
+     it 'returns nil if no item has matching id' do
+       se = SalesEngine.from_csv(
+         :items     => './data/items.csv',
+         :merchants => './data/merchants.csv'
+       )
+
+       ir = se.items
+
+       expect(ir.find_by_id('123')).to eq(nil)
      end
    end
 end
