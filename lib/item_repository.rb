5fdad713298028bb6097c0d6e8b8ec6fe0f886fc
Zach T <ztrokey@gmail.com>
2021-04-13 20:18:09 -0600

    Add method and test for item_repo find_all_by_price

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -16,4 +16,37 @@ class ItemRepository
     end
   end
 
+  def all
+    @items
+  end
+
+  def find_by_id(id)
+    @items.find do |item|
+      item.id == id
+    end
+  end
+
+  def find_by_name(name)
+    @items.find do |item|
+      item.name.downcase == name.downcase
+    end
+  end
+
+  def find_all_with_description(description)
+    @items.find_all do |item|
+      item.description.downcase.include?(description.downcase)
+    end
+  end
+
+  def find_all_by_price(price)
+    @items.find_all do |item|
+      item.unit_price == price
+    end
+  end
+
+  def find_all_by_price_in_range(range)
+    @items.find_all do |item|
+      range.include?(item.unit_price)
+    end
+  end
 end
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -2,27 +2,125 @@ require 'simplecov'
 SimpleCov.start
 require './lib/sales_engine'
 require './lib/item_repository'
+require 'bigdecimal'
 
 RSpec.describe ItemRepository do
-  before do
-    @se = SalesEngine.new
-    @ir = @se.items
-  end
-
   describe '#initialize' do
     it 'exists' do
-      expect(@ir).to be_instance_of(ItemRepository)
+      ir = ItemRepository.new('./data/items.csv')
+
+      expect(ir).to be_instance_of(ItemRepository)
     end
 
     it 'has items' do
-      # binding.pry
-      expect(@ir.items.count).to eq(1367)
+      ir = ItemRepository.new('./data/items.csv')
+
+      expect(ir.items.count).to eq(1367)
     end
   end
 
-  describe '#make items' do
-    it 'makes items' do
-      expect(@ir.make_items(@se.make_hash('./data/items.csv')).first).to be_instance_of(Item)
+  describe '#make_items' do
+    it 'makes_items' do
+      ir = ItemRepository.new('./data/items.csv')
+
+      expect(ir.items.first).to be_instance_of(Item)
+    end
+  end
+
+  describe '#all' do
+    it 'contains all the items' do
+      ir = ItemRepository.new('./data/items.csv')
+
+      expect(ir.all.count).to eq(1367)
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'finds items by id' do
+      ir = ItemRepository.new('./data/items.csv')
+      test_item = Item.new(id: '1',
+      name: 'Cool Stuff',
+      description: 'supaaa cool',
+      unit_price: '1300',
+      merchant_id: '12334185',
+      created_at: '2016-01-11 11:51:37 UTC',
+      updated_at: '1993-09-29 11:56:40 UTC')
+      ir.items << test_item
+
+      expect(ir.find_by_id(1)).to eq(test_item)
+      expect(ir.find_by_id(7)).to eq(nil)
+    end
+  end
+
+  describe '#find_by_name' do
+    it 'finds items by name' do
+      ir = ItemRepository.new('./data/items.csv')
+      test_item = Item.new(id: '1',
+      name: 'Cool Stuff',
+      description: 'supaaa cool',
+      unit_price: '1300',
+      merchant_id: '12334185',
+      created_at: '2016-01-11 11:51:37 UTC',
+      updated_at: '1993-09-29 11:56:40 UTC')
+      ir.items << test_item
+
+      expect(ir.find_by_name('CoOL StUfF')).to eq(test_item)
+      expect(ir.find_by_name('neato')).to eq(nil)
+    end
+  end
+
+  describe '#find_all_with_description' do
+    it 'finds items by description' do
+      ir = ItemRepository.new('./data/items.csv')
+      test_item = Item.new(id: '1',
+      name: 'Cool Stuff',
+      description: 'supaaa cool',
+      unit_price: '1300',
+      merchant_id: '12334185',
+      created_at: '2016-01-11 11:51:37 UTC',
+      updated_at: '1993-09-29 11:56:40 UTC')
+      ir.items << test_item
+
+      expect(ir.find_all_with_description('sUPaAa')).to eq([test_item])
+      expect(ir.find_all_with_description('neato burrito')).to eq([])
+    end
+  end
+
+  describe '#find_all_by_price' do
+    it 'finds items by price' do
+      ir = ItemRepository.new('./data/items.csv')
+      test_item = Item.new(id: '1',
+      name: 'Cool Stuff',
+      description: 'supaaa cool',
+      unit_price: '1357',
+      merchant_id: '12334185',
+      created_at: '2016-01-11 11:51:37 UTC',
+      updated_at: '1993-09-29 11:56:40 UTC')
+      ir.items << test_item
+      price_1 = BigDecimal(1357)
+      price_2 = BigDecimal(7541234541)
+
+      expect(ir.find_all_by_price(price_1)).to eq([test_item])
+      expect(ir.find_all_by_price(7541234541)).to eq([])
+    end
+  end
+
+  describe '#find_all_by_price_in_range' do
+    it 'finds items by price range' do
+      ir = ItemRepository.new('./data/items.csv')
+      test_item = Item.new(id: '1',
+      name: 'Cool Stuff',
+      description: 'supaaa cool',
+      unit_price: '1357',
+      merchant_id: '12334185',
+      created_at: '2016-01-11 11:51:37 UTC',
+      updated_at: '1993-09-29 11:56:40 UTC')
+      ir.items << test_item
+      range_1 = (1000.00..1500.00)
+      range_2 = (1..2)
+
+      expect(ir.find_all_by_price_in_range(range_1).count).to eq(206)
+      expect(ir.find_all_by_price_in_range(range_2)).to eq([])
     end
   end
 end
