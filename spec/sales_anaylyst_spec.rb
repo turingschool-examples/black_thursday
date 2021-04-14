41c4fe7872adc570b5adc03fa70152883c0ffce3
Brant <57786638+branfull@users.noreply.github.com>
2021-04-14 16:40:35 -0500

    Update sales_anaylyst_spec.rb

--- spec/sales_anaylyst_spec.rb
+++ spec/sales_anaylyst_spec.rb
@@ -2,7 +2,7 @@ require './lib/sales_analyst'
 
 RSpec.describe 'SalesAnalyst' do
   describe '#initialize' do
-    it 'creates an instance of SalesAnalyst'do
+    it 'creates an instance of SalesAnalyst' do
       se = SalesEngine.from_csv(
         items: './data/items.csv',
         merchants: './data/merchants.csv'
