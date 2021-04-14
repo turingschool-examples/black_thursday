3aa65f8fdada07418dafa9de40976e58ba59759a
Suzie K <suziekim.dev@gmail.com>
2021-04-13 23:14:18 -0400

    Edit Item spec test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -18,12 +18,12 @@ RSpec.describe Item do
 
     it 'has attributes' do
       i = Item.new({:id          => 1,
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal(10.99,4),
-      :created_at  => Time.now,
-      :updated_at  => Time.now,
-      :merchant_id => 2})
+                    :name        => "Pencil",
+                    :description => "You can use it to write things",
+                    :unit_price  => BigDecimal(10.99,4),
+                    :created_at  => Time.now,
+                    :updated_at  => Time.now,
+                    :merchant_id => 2})
       
       expect(i.id).to eq(1)
       expect(i.name).to eq("Pencil")
