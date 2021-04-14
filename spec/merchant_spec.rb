359440f038979a95ab00214fcba3e3d59cd928de
Caleb W. (He/Him) <78196294+Caleb1991@users.noreply.github.com>
2021-04-14 17:18:58 -0600

    Update merchant_spec.rb

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -3,7 +3,7 @@ require_relative './lib/merchant'
 
 RSpec.describe Merchant do
   it 'exists' do
-    m = Merchant.new( {id: 5, name: 'Turing School'}, 'repository' )
+    m = Merchant.new({ id: 5, name: 'Turing School' }, 'repository')
 
     expect(m).to be_instance_of(Merchant)
   end
