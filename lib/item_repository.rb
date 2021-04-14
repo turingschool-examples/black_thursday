20336f8adad96abf9e224c3c5a9b759d43017ddf
Suzie K <suziekim.dev@gmail.com>
2021-04-13 23:12:20 -0400

    Add add and find_by_id method

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -4,11 +4,29 @@ require 'time'
 
 class ItemRepo
 
+  attr_reader :items
+  def initialize 
+    @items = []
+  end
+
   def populate_information
     items = Hash.new{|h, k| h[k] = [] }
     CSV.foreach('./data/items.csv', headers: true, header_converters: :symbol) do |item_info|
       items[item_info[:id]] = Item.new(item_info)
     end
       items
+  end
+
+  def all 
+    #returns array of all known item instances
+   @items = populate_information.map do |item|
+      item[1]
+    end
+  end
+
+  def find_by_id(id)
+    populate_information.select do |item|
+      item[id] == id
+    end
   end
 end
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -18,25 +18,24 @@ RSpec.describe ItemRepo do
     it 'has attributes' do
       ir = ItemRepo.new
   
-      i = Item.new({:id          => 1,
-                    :name        => "Pencil",
-                    :description => "You can use it to write things",
-                    :unit_price  => BigDecimal(10.99,4),
-                    :created_at  => Time.now,
-                    :updated_at  => Time.now,
-                    :merchant_id => 2})
-
-      expect(i.id).to eq(1)
-      expect(i.name).to eq("Pencil")
-      expect(i.description).to eq("You can use it to write things")
-      expect(i.unit_price).to be_an_instance_of(BigDecimal)
-      expect(i.created_at).to be_an_instance_of(Time)
-      expect(i.updated_at).to be_an_instance_of(Time)
+      expect(ir.items).to eq ([])
     end
   end
 
   describe '#methods' do
     it '#populates information' do
+      ir = ItemRepo.new
+
+      expect(ir.populate_information).to be_an_instance_of(Hash)
+    end
+
+    it '#all' do
+      ir = ItemRepo.new
+
+      expect(ir.all).to be_an_instance_of(Array)
+    end
+
+    it '#find by id' do
       ir = ItemRepo.new
       i = Item.new({:id          => 1,
                     :name        => "Pencil",
@@ -44,9 +43,12 @@ RSpec.describe ItemRepo do
                     :unit_price  => BigDecimal(10.99,4),
                     :created_at  => Time.now,
                     :updated_at  => Time.now,
-                    :merchant_id => 2})
-    require 'pry'; binding.pry
-      expect(ir.populate_information).to be_an_instance_of(Hash)
+                    :merchant_id => 2
+                  })
+      id = "263395237"
+      ir.items << i 
+
+      expect(ir.find_by_id(id)).to eq(i)
     end
   end
 end
