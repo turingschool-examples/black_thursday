2dea5362427771318f1c95a9d98c50557c6c05d1
Brant <57786638+branfull@users.noreply.github.com>
2021-04-13 19:40:21 -0500

    Update item_spec.rb

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe Item do
     it 'exists' do
       expect(i).to be_an_instance_of(Item)
     end
+  end
+  describe 'instance variables' do
+    i = Item.new(
+      id: 1,
+      name: 'Pencil',
+      description: 'You can use it to write things',
+      unit_price: BigDecimal(10.99, 4),
+      created_at: Time.now,
+      updated_at: Time.now,
+      merchant_id: 2
+    )
     it 'has an id' do
       expect(i.id).to eq(1)
     end
