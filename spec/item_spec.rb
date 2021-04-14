f63fe14143e6b79ea1e0f625587967dd8997e2d5
Brant <57786638+branfull@users.noreply.github.com>
2021-04-13 19:32:09 -0500

    Update item_spec.rb

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -38,13 +38,13 @@ RSpec.describe Item do
         '12:58'
       end
       i = Item.new(
-                    id: 1,
-                    name: 'Pencil',
-                    description: 'You can use it to write things',
-                    unit_price: BigDecimal(10.99, 4),
-                    created_at: Time.now,
-                    updated_at: Time.now,
-                    merchant_id: 2
+                  id: 1,
+                  name: 'Pencil',
+                  description: 'You can use it to write things',
+                  unit_price: BigDecimal(10.99, 4),
+                  created_at: Time.now,
+                  updated_at: Time.now,
+                  merchant_id: 2
                   )
       # time = class_double("Time")
       expect(i.created_at).to eq('12:58')
@@ -67,13 +67,13 @@ RSpec.describe Item do
   end
   describe '#unit_price_to_dollars' do
     i = Item.new(
-                  id: 1,
-                  name: 'Pencil',
-                  description: 'You can use it to write things',
-                  unit_price: BigDecimal(10.99, 4),
-                  created_at: Time.now,
-                  updated_at: Time.now,
-                  merchant_id: 2
+                id: 1,
+                name: 'Pencil',
+                description: 'You can use it to write things',
+                unit_price: BigDecimal(10.99, 4),
+                created_at: Time.now,
+                updated_at: Time.now,
+                merchant_id: 2
                 )
     it 'converts unit price to dollars' do
       expect(i.unit_price_to_dollars).to eq(10.99)
