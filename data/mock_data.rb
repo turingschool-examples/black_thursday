c9303d60695e7d6d1a6969c4a57c4b7cd287b9a7
Richard <rdesilvey@gmail.com>
2021-04-14 14:24:26 -0600

    Implement get_mock_merchants

--- data/mock_data.rb
+++ data/mock_data.rb
@@ -1,6 +1,13 @@
 class MockData
 
   def self.get_mock_merchants(number_of_mocks)
-    
+    mocked_merchants = []
+    number_of_mocks.times do |merchant_number|
+      merchant = {}
+      merchant[:name] = "Merchant #{merchant_number}"
+      merchant[:id] = merchant_number
+      mocked_merchants << merchant
+    end
+    mocked_merchants
   end
 end
