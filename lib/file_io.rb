30505ef9079cd00e584615374e727dd1de5cd256
NoahZinter <noah.zinter@gmail.com>
2021-04-14 14:51:06 -0600

    Create FileIo class/spec

---
+++ lib/file_io.rb
@@ -0,0 +1,8 @@
+class FileIo
+  def self.process_csv(filename, type)
+    elements = []
+    CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+      elements << type.new(row)
+    end
+  end
+end
---
+++ spec/file_io_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+require 'CSV'
+require './lib/file_io'
+require './lib/item'
+
+describe FileIo do 
+  it 'exists' do 
+    file_io = FileIo.new 
+  
+    expect(file_io).is_a? FileIo
+  end
+
+  it 'returns an array' do 
+    mock_row = {
+      id: 12_345,
+      name: 'Widget',
+      description: 'Item desc',
+      unit_price: '12.23',
+      merchant_id: '12345'
+    }
+
+    allow(CSV).to receive(:foreach).and_yield(mock_row)
+
+    mock_file_name = './some_file.csv'
+   
+    items = FileIo.process_csv(mock_file_name, Item)
+
+    expect(items).to be_instance_of(Array)
+    expect(items.first.id).to eq(12_345)
+  end
+end 
