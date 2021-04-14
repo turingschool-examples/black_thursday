a31dfc9d8518ddb34c66e4d982828e010e11d1f3
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-14 15:04:04 -0600

    Red green helper methods for golden item

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -29,11 +29,49 @@ class SalesAnalyst
     (sum_of_squares.fdiv(merchant_ids.length - 1) ** 0.5).round(2)
   end
 
+  def merchants_with_high_item_count
+    one_standard = average_items_per_merchant + average_items_per_merchant_standard_deviation
+    merchant_ids = @merchants.all.map do |merchant|
+      merchant.id
+    end
+    merchant_ids_high_count = merchant_ids.find_all do |id|
+      number_of_items = @items.find_all_by_merchant_id(id).length
+      number_of_items > one_standard
+    end
+    @merchants.all.find_all do |merchant|
+      merchant_ids_high_count.include?(merchant.id)
+    end
+  end
+
   def average_item_price_for_merchant(id)
     items_by_merchant_array = @items.find_all_by_merchant_id(id)
     test = items_by_merchant_array.sum do |item|
       item.unit_price.to_f
     end.fdiv(items_by_merchant_array.length)
-    test
+    test.round(2)
+  end
+
+  def average_average_price_per_merchant
+    @merchants.all.sum do |merchant|
+      average_item_price_for_merchant(merchant.id)
+    end.fdiv(@merchants.all.length).round(2)
+  end
+
+  def average_item_price
+    @items.all.sum do |item|
+      item.unit_price.to_f
+    end.fdiv(@items.all.length).round(2)
+  end
+
+  def item_price_standard_deviation
+    item_average = average_item_price
+    denominator = @items.all.sum do |item|
+      (item.unit_price.to_f - item_average) ** 2
+    end
+    (denominator.fdiv((@items.all.length) -1) ** 0.5).round(2)
+  end
+
+  def golden_items
+
   end
 end
--- spec/sales_anaylyst_spec.rb
+++ spec/sales_anaylyst_spec.rb
@@ -44,15 +44,70 @@ RSpec.describe 'SalesAnalyst' do
       expect(sa.average_items_per_merchant_standard_deviation).to eq(3.26)
     end
   end
-  describe '#average_item_price_for_merchant' do
-    it "returns the average prices for the merchant identified by their id" do
+  describe '#merchants_with_high_item_count' do
+    it 'returns all merchants more than one standard deviation on number of items' do
       se = SalesEngine.from_csv(
         items: './data/items.csv',
         merchants: './data/merchants.csv'
       )
       sa = se.analyst
 
-      expect(sa.average_item_price_for_merchant('12334159')).to eq(3150.0)
+      expect(sa.merchants_with_high_item_count[0].id).to eq('12334123')
+    end
+  end
+  describe '#average_item_price_for_merchant' do
+    it 'returns the average prices for the merchant identified by their id' do
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv'
+      )
+      sa = se.analyst
+
+      expect(sa.average_item_price_for_merchant('12334159')).to eq(3150.00)
+    end
+  end
+  describe '#average_average_price_per_merchant' do
+    it 'returns the average of all average prices' do
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv'
+      )
+      sa = se.analyst
+
+      expect(sa.average_average_price_per_merchant).to eq(35029.47)
+    end
+  end
+  describe '#average_item_price' do
+    it 'returns the average of the price of all items' do
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv'
+      )
+      sa = se.analyst
+
+      expect(sa.average_item_price).to eq(25105.51)
+    end
+  end
+  describe '#item_price_standard_deviation' do
+    it 'is evil witch magic' do
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv'
+      )
+      sa = se.analyst
+
+      expect(sa.item_price_standard_deviation).to eq(290099.00)
+    end
+  end
+  describe '#golden_items' do
+    it 'returns an array of all items more than 2 standard deviations from average' do
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv'
+      )
+      sa = se.analyst
+
+      expect(sa.golden_items[0].name).to eq('Pants')
     end
   end
 end
