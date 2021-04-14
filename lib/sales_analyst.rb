053098e786dfcc6b0774a0996e872ad3cb1ff943
Richard <rdesilvey@gmail.com>
2021-04-13 18:57:07 -0600

    Implement average_items_per_merchant
    Make Merchant exist with data

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,2 +1,15 @@
+require './lib/sales_engine'
+
 class SalesAnalyst
-end 
+
+  def initialize(items, merchants)
+    @items = items
+    @merchants = merchants
+  end
+
+  def average_items_per_merchant
+    total_number_items = @items.all.length
+    total_number_merchants = @merchants.all.length
+    total_number_items / total_number_merchants
+  end
+end
