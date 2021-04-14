701ac1a990f13a3d50ac529971f20f4943f71baf
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-14 19:20:52 -0400

    Add tests for merchant class

---
+++ spec/merchants_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/merchants'
+
+RSpec.describe do
+
+  se = SalesEngine.from_csv({
+  :items     => "./data/items.csv",
+  :merchants => "./data/merchants.csv",
+  })
+  mr = se.merchants
+
+  context "Merchant" do
+    it "#id returns the merchant id" do
+      merchant = mr.all.first
+      expect(merchant.id).to eq 12334105
+    end
+    it "#name returns the merchant name" do
+      merchant_one = mr.all.first
+      expect(merchant_one.name).to eq "Shopin1901"
+      merchant_two = mr.all.last
+      expect(merchant_two.name).to eq "CJsDecor"
+    end
+  end
+
+end
