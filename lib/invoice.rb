30f9ea14453d716a617e4e004011293f886b5998
sami-p <sami.peterson14@gmail.com>
2021-04-15 16:00:35 -0500

    Instantiate Invoice with merchant id; pass merchant id test

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,9 +1,11 @@
 class Invoice
   attr_reader :id,
-              :customer_id
+              :customer_id,
+              :merchant_id
 
   def initialize(transaction_details)
     @id = transaction_details[:id]
     @customer_id = transaction_details[:customer_id]
+    @merchant_id = transaction_details[:merchant_id]
   end
 end
