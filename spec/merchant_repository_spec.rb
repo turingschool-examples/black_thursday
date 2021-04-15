1da1e26f880c73f454338ae94b66a901772a9d55
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 14:55:26 -0600

    Refactor delete and create methods to use mock data

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -76,44 +76,35 @@ describe MerchantRepository do
     end
   end
 
-  # describe '#delete' do
-  #   it 'deletes a merchant with the given id' do
-  #     mock_merchant1 = instance_double('Merchant', id: 0)
-  #     mock_merchant2 = instance_double('Merchant', id: 1)
-  #     mock_merchant3 = instance_double('Merchant', id: 2)
-  #     mock_merchant4 = instance_double('Merchant', id: 3)
-  #     mock_merchants = [mock_merchant1, mock_merchant2,
-  #                       mock_merchant3, mock_merchant4]
-  #
-  #     m_repo = MerchantRepository.new(mock_merchants)
-  #
-  #     expected_merchants = [mock_merchant1, mock_merchant2, mock_merchant4]
-  #     m_repo.delete(2)
-  #     actual_merchants = m_repo.all
-  #     expect(actual_merchants).to eq expected_merchants
-  #   end
-  # end
-  #
-  # describe '#create' do
-  #   it 'creates a new Merchant' do
-  #     merchant1 = Merchant.new(id: 1, name: 'Richard')
-  #
-  #     merchant2 = Merchant.new(id: 2, name: 'Dustin')
-  #
-  #     merchants = [merchant1, merchant2]
-  #     m_repo = MerchantRepository.new(merchants)
-  #
-  #     allow(m_repo).to receive(:newest_id).and_return(3)
-  #
-  #     m_repo.create(id: 0, name: 'Sami')
-  #
-  #     merchant_names = [merchant1.name, merchant2.name, 'Sami']
-  #     actual_names = m_repo.all.map(&:name)
-  #
-  #     expect(actual_names).to eq merchant_names
-  #   end
-  # end
-  #
+  describe '#delete' do
+    it 'deletes a merchant with the given id' do
+      details = MockData.merchants_as_hash
+      mock_data = MockData.merchants_as_mocks(details) { self }
+      allow_any_instance_of(MerchantRepository).to receive(:create_merchants).and_return(mock_data)
+      m_repo = MerchantRepository.new('fake.csv')
+
+      expect(m_repo.merchants.length).to eq 10
+      m_repo.delete(2)
+      expect(m_repo.merchants.length).to eq 9
+    end
+  end
+
+  describe '#create' do
+    it 'creates a new Merchant' do
+      details = MockData.merchants_as_hash
+      mock_data = MockData.merchants_as_mocks(details) { self }
+      allow_any_instance_of(MerchantRepository).to receive(:create_merchants).and_return(mock_data)
+      m_repo = MerchantRepository.new('fake.csv')
+
+      allow(m_repo).to receive(:newest_id).and_return(10)
+
+      m_repo.create(id: 0, name: 'Sami')
+
+      expect(m_repo.merchants.length).to eq 11
+      expect(m_repo.merchants.last.name).to eq 'Sami'
+    end
+  end
+
   # describe '#newest_id' do
   #   it 'gets the next id for a new merchant' do
   #     merchant1 = Merchant.new(id: 1, name: 'Richard')
