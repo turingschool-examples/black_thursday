383144d926ded18143d96e63fad60a4d2eb6f41a
sami-p <sami.peterson14@gmail.com>
2021-04-15 16:14:18 -0500

    Instantiate Invoice with creation time stamp; pass creation time stamp test

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -2,12 +2,14 @@ class Invoice
   attr_reader :id,
               :customer_id,
               :merchant_id,
-              :status 
+              :status,
+              :created_at 
 
   def initialize(transaction_details)
     @id = transaction_details[:id]
     @customer_id = transaction_details[:customer_id]
     @merchant_id = transaction_details[:merchant_id]
     @status = transaction_details[:status]
+    @created_at = transaction_details[:created_at]
   end
 end
