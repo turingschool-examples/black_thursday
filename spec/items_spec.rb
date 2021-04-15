ae891b87614b8698d6bae7223cbc386ec17f662b
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-15 10:07:08 -0400

    Fix tests for items_spec

--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -9,48 +9,48 @@ RSpec.describe do
   ir = se.items
 
   context "Item" do
-      it "#id returns the id" do
-        first_item = ir.all.first
-        expect(first_item.id).to eq 263395237
-        last_item = ir.all.last
-        expect(last_item.id).to eq 263567474
+      it "can return id" do
+        first_item = ir.all[0]
+        expect(first_item.id).to eq(263395237)
+        second_item = ir.all[1]
+        expect(second_item.id).to eq(263395617)
       end
 
-      it "#name returns the name" do
-        first_item = ir.all.first
-        expect(first_item.name).to eq "510+ RealPush Icon Set"
-        last_item = ir.all.last
-        expect(last_item.name).to eq "Minty Green Knit Crochet Infinity Scarf"
+      it "can return name" do
+        first_item = ir.all[0]
+        expect(first_item.name).to eq("510+ RealPush Icon Set")
+        second_item = ir.all[1]
+        expect(second_item.name).to eq("Glitter scrabble frames")
       end
 
-      it "#description returns the description" do
-        first_item = ir.all.first
-        expect(first_item.description.class).to eq String
-        expect(first_item.description.length).to eq 2237
+      it "can return description" do
+        first_item = ir.all[0]
+        expect(first_item.description.class).to eq(String)
+        expect(first_item.description.length).to eq(2237)
       end
 
-      it "#unit_price returns the unit price" do
-        first_item = ir.all.first
-        expect(first_item.unit_price).to eq 12.00
-        expect(first_item.unit_price.class).to eq BigDecimal
+      it "can return unit price" do
+        first_item = ir.all[0]
+        expect(first_item.unit_price).to eq(12.00)
+        expect(first_item.unit_price.class).to eq(BigDecimal)
       end
 
-      it "#created_at returns the Time the item was created" do
-        first_item = ir.all.first
-        expect(first_item.created_at_to_time).to eq Time.parse("2016-01-11 09:34:06 UTC")
-        expect(first_item.created_at_to_time.class).to eq Time
+      it "can return time created at" do
+        first_item = ir.all[0]
+        expect(first_item.created_at_to_time).to eq(Time.parse("2016-01-11 09:34:06 UTC"))
+        expect(first_item.created_at_to_time.class).to eq(Time)
       end
 
-      it "#updated_at returns the Time the item was last updated" do
-        first_item = ir.all.first
-        expect(first_item.updated_at_to_time).to eq Time.parse("2007-06-04 21:35:10 UTC")
-        expect(first_item.updated_at_to_time.class).to eq Time
+      it "can return time updated at" do
+        first_item = ir.all[0]
+        expect(first_item.updated_at_to_time).to eq(Time.parse("2007-06-04 21:35:10 UTC"))
+        expect(first_item.updated_at_to_time.class).to eq(Time)
       end
 
-      it "#unit_price_to_dollars returns price as Float" do
-        expected = ir.find_by_id(263397059)
-        expect(expected.unit_price_to_dollars).to eq 130.0
-        expect(expected.unit_price_to_dollars.class).to eq Float
+      it "unit_price_to_dollars returns unit price as a float" do
+        first_item = ir.all[0]
+        expect(first_item.unit_price_to_dollars).to eq(12.0)
+        expect(first_item.unit_price_to_dollars.class).to eq(Float)
       end
 
     end
