d0cfc4c5323c5b6d4b6ea767fbbabec88b712328
sami-p <sami.peterson14@gmail.com>
2021-04-15 18:24:47 -0500

    Fix tests

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -3,7 +3,7 @@ require './lib/invoice'
 
 describe Invoice do
   describe '#initialize' do
-    xit 'exists' do
+    it 'exists' do
       i = Invoice.new({
                         id: 6,
                         customer_id: 7,
@@ -16,7 +16,7 @@ describe Invoice do
       expect(i).is_a? Invoice
     end
 
-    xit 'has an id' do
+    it 'has an id' do
       i = Invoice.new({
                         id: 6,
                         customer_id: 7,
@@ -30,7 +30,7 @@ describe Invoice do
       expect(i.id).is_a? Integer
     end
 
-    xit 'has a customer id' do
+    it 'has a customer id' do
       i = Invoice.new({
                         id: 6,
                         customer_id: 7,
@@ -44,7 +44,7 @@ describe Invoice do
       expect(i.customer_id).is_a? Integer
     end
 
-    xit 'has a merchant id' do
+    it 'has a merchant id' do
       i = Invoice.new({
                         id: 6,
                         customer_id: 7,
@@ -58,7 +58,7 @@ describe Invoice do
       expect(i.merchant_id).is_a? Integer
     end
 
-    xit 'has a status' do
+    it 'has a status' do
       i = Invoice.new({
                         id: 6,
                         customer_id: 7,
@@ -72,9 +72,9 @@ describe Invoice do
       expect(i.status).is_a? String
     end
 
-    it 'time stamps when created' do
-      created_at = MockData.get_a_random_date false
-      updated_at = MockData.get_a_random_date false
+    it 'has time stamps' do
+      created_at = '2021-01-01'
+      updated_at = '2021-01-06'
 
       invoice = Invoice.new({
                               id: 6,
@@ -87,21 +87,6 @@ describe Invoice do
 
       expect(invoice.created_at).to eq created_at
       expect(invoice.updated_at).to eq updated_at
-    end
-
-    xit 'time stamps when updated' do
-      time = Time.now
-      i = Invoice.new({
-                        id: 6,
-                        customer_id: 7,
-                        merchant_id: 8,
-                        status: 'pending',
-                        created_at: Time.now,
-                        updated_at: Time.now
-                      })
-
-      expect(i.updated_at).to eq(time)
-      expect(i.updated_at).is_a? Time
     end
   end
 end
