c51b152f764de459d05bf5acd78aad0b3dfba37a
Brant <57786638+branfull@users.noreply.github.com>
2021-04-14 19:35:03 -0500

    Update item_repository_spec.rb

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -216,7 +216,8 @@ RSpec.describe 'ItemRepository' do
         name:         'Pencil',
         description:  'You can use it to write things',
         unit_price:    BigDecimal(10.99, 4),
-        merchant_id:   2)
+        merchant_id:   2
+        )
 
       expect(item.class).to eq(Item)
     end
