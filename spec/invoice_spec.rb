2599e8cfafe0881c6907a02c178c87c3d1bb7add
sami-p <sami.peterson14@gmail.com>
2021-04-15 15:42:32 -0500

    Test Invoice existence

---
+++ spec/invoice_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require '.lib/invoice'
+
+describe Invoice do
+  describe '#initiale' do
+    it 'exists' do
+      i = Invoice.new({
+        :id          => 6,
+        :customer_id => 7,
+        :merchant_id => 8,
+        :status      => "pending",
+        :created_at  => Time.now,
+        :updated_at  => Time.now,
+      })
+
+      expect(i).is_a? Invoice
+  end
+end
