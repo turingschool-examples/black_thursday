fa5df1f94c7620880c270cf501c7e875ab7973b1
sami-p <sami.peterson14@gmail.com>
2021-04-15 15:57:59 -0500

    Instantiate Invoice with a customer id; pass customer id test

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,7 +1,9 @@
 class Invoice
-  attr_reader :id
+  attr_reader :id,
+              :customer_id
 
   def initialize(transaction_details)
     @id = transaction_details[:id]
+    @customer_id = transaction_details[:customer_id]
   end
 end
