9b7ab43d2b48d3a4904ee019ee4e186aa9169133
Netia Ingram <netia1128@hotmail.com>
2021-04-14 20:30:17 -0600

    Add invoice repo #find_all_by_customer_id method and test

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -30,6 +30,12 @@ class InvoiceRepository
     end
   end
 
+  def find_all_by_customer_id(customer_id)
+    @invoices.find_all do |invoice|
+      invoice.customer_id == customer_id
+    end
+  end
+
 #   def find_by_name(name)
 #     @items.find do |item|
 #       item.name.downcase == name.downcase
