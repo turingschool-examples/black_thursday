61143c81143f353e77ef883f80e8fa1857029979
sami-p <sami.peterson14@gmail.com>
2021-04-15 16:09:14 -0500

    Instantiate Invoice with a status; pass status test

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,11 +1,13 @@
 class Invoice
   attr_reader :id,
               :customer_id,
-              :merchant_id
+              :merchant_id,
+              :status 
 
   def initialize(transaction_details)
     @id = transaction_details[:id]
     @customer_id = transaction_details[:customer_id]
     @merchant_id = transaction_details[:merchant_id]
+    @status = transaction_details[:status]
   end
 end
