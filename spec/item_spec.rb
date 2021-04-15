89b27d9f6a91500d51d3881b377ace17e002a4ed
Brant <57786638+branfull@users.noreply.github.com>
2021-04-14 19:47:20 -0500

    Update item_spec.rb

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -19,14 +19,15 @@ RSpec.describe Item do
     end
   end
   describe 'instance variables' do
-    i = Item.new(
-    { id: 1,
+    i = Item.new({
+      id: 1,
       name: 'Pencil',
       description: 'You can use it to write things',
       unit_price: BigDecimal(10.99, 4),
       created_at: Time.now,
       updated_at: Time.now,
-      merchant_id: 2 },
+      merchant_id: 2
+      },
       'repository'
     )
     it 'has an id' do
