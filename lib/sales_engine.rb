014b0df7f4663472f2e774aac9677e8fd9363848
Suzie K <suziekim.dev@gmail.com>
2021-04-15 21:47:03 -0400

    Update sales_engine and test

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,13 @@
 require 'CSV'
+require_relative 'item_repo'
+require_relative 'merchant_repo'
 
 class SalesEngine
-  attr_reader :item,
-              :merchant
-  def initialize(sales_info)
-    @item = ItemRepo.new(sales_info[:items], self)
-    @merchant = MerchantRepo.new(sales_info[:merchants], self)
-    # @items = sales_info[:items] 
-    # @merchants = sales_info[:merchants]
+  attr_reader :items
+              :merchants
+  def initialize(paths)
+    @items = ItemRepo.new(paths[:items], self)
+    @merchants = MerchantRepo.new(paths[:merchants], self)
   end
 
   def self.from_csv(paths)
@@ -16,28 +16,4 @@ class SalesEngine
   # self.from_csv(hash from i0 directions) => paths
   # from csv method will call initialize to pass 
   # the hash through, should return the instance of itself 
-
-  # def self.from_csv(sales_info)
-  #   items = sales_info[:items]
-  #   merchants = sales_info[:merchants]
-  #   CSV.foreach(items, headers: true, header_converters: :symbol) do |row|
-  #     id = row[:id].to_i
-  #     name = row[:name]
-  #     description = row[:description]
-  #     unit_price = nil
-  #     # created_at =
-  #     # updated_at =
-  #     # merchant_id =
-  #     item_info = {:id => id,
-  #                  :name => name,
-  #                  :description => description,
-  #                  :unit_price => unit_price,
-  #                  :created_at => nil,
-  #                  :updated_at => nil,
-  #                  :merchant_id => nil
-  #                }
-  #
-  #     item = Item.new(item_info)
-  #     puts "#{item.name} has been created!"
-  #   end
 end
