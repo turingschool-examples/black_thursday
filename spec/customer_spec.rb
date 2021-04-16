8ea79107bca56a42a580e8f20e2933164735fb6c
NoahZinter <noah.zinter@gmail.com>
2021-04-16 15:34:03 -0600

    Test time methods

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -31,5 +31,35 @@ describe Customer do
       expect(customer.id).to eq 6
       expect(customer.id).is_a? Fixnum
     end
+
+    it 'has a first and last name' do
+      details = ({
+        :id => 6,
+        :first_name => "Joan",
+        :last_name => "Clarke",
+        :created_at => Time.now,
+        :updated_at => Time.now
+            })
+
+      customer = Customer.new(details)
+
+      expect(customer.first_name).to eq 'Joan'
+      expect(customer.last_name).to eq 'Clarke'
+    end
+
+    it 'returns time instances' do
+      details = ({
+        :id => 6,
+        :first_name => "Joan",
+        :last_name => "Clarke",
+        :created_at => Time.now,
+        :updated_at => Time.now
+            })
+
+      customer = Customer.new(details)
+
+      expect(customer.created_at).is_a? Time
+      expect(customer.updated_at).is_a? Time
+    end
   end
 end
