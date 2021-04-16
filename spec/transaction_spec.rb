7b2afbc0773e86e751809ded772c5027c244e0eb
NoahZinter <noah.zinter@gmail.com>
2021-04-16 14:13:32 -0600

    Create Transaction class, test it exists

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/transaction'
+
+describe Transaction do
+  describe '#initialize' do
+    it 'exists' do
+      details = ({
+        :id => 6,
+        :invoice_id => 8,
+        :credit_card_number => "4242424242424242",
+        :credit_card_expiration_date => "0220",
+        :result => "success",
+        :created_at => Time.now,
+        :updated_at => Time.now
+          })
+
+      transaction = Transaction.new(details)
+
+      expect(transaction).is_a? Transaction
+    end
+
+  end
+
+end
