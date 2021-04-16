22c118068e4872a661ef3ca41191f1b6b4e6b60d
Alex Ferencz <alex.ferencz6@gmail.com>
2021-04-15 19:32:01 -0600

    Add inspect method

--- lib/repository.rb
+++ lib/repository.rb
@@ -9,6 +9,10 @@ class Repository
     @parsed_csv_data = parse_csv(path)
     @array_of_objects = [] #consider a rename #NEED TO UPDATE CHILD CLASSES TO USE THIS NAME
   end
+  
+  def inspect
+  "#<#{self.class} #{@array_of_objects.size} rows>"
+  end
 
   def parse_csv(path)
     parsed_csv = CSV.parse(File.read(path), headers: true, header_converters: :symbol).to_a
