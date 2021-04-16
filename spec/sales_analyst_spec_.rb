5b52c512d353adfbc81abbc403f6b2a7cc3ba54d
deebot10 <dee1jhill@gmail.com>
2021-04-16 11:40:46 -0600

    Create test for sales analyst instanstiation

---
+++ spec/sales_analyst_spec_.rb
@@ -0,0 +1,13 @@
+require './lib/sales_analyst'
+require './lib/sales_engine'
+require 'rspec'
+
+RSpec.describe SalesAnalyst do
+  context 'Instanstiation' do
+    it 'exists' do
+      sales_analyst = SalesEngine.analyst
+      
+      expect(sales_analyst).to be_instance_of SalesAnalyst
+    end
+  end
+end
