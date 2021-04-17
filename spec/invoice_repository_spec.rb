d9b2ee5945d87ec1e07d07b766d62166f71cde68
sami-p <sami.peterson14@gmail.com>
2021-04-17 18:25:16 -0500

    Test for update methods and delete method

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -125,4 +125,58 @@ describe InvoiceRepository do
       expect(created_invoice.id).to eq 10
     end
   end
+
+  describe '#update' do
+    it 'updates invoice attributes' do
+      mock_data = MockData.invoices_as_mocks(self)
+      allow_any_instance_of(InvoiceRepository).to receive(:create_invoices).and_return(mock_data)
+      invoice_repository = InvoiceRepository.new('fake.csv')
+
+      new_invoice = {
+        id: nil,
+        customer_id: 1,
+        merchant_id: 1,
+        status: "pending",
+        created_at: Time.now,
+        updated_at: Time.new(2020, 12, 31)
+      }
+
+      invoice_repository.create(new_invoice)
+
+      attributes = {
+        status: "shipped"
+      }
+
+      invoice_repository.update(10, attributes)
+      expected = invoice_repository.find_by_id(10)
+
+      expect(expected.status).to eq "shipped"
+      expect(expected.updated_at).to be > Time.new(2020, 12, 31)
+    end
+  end
+
+  describe '#delete' do
+    it 'deletes the object at specified id' do
+      mock_data = MockData.invoices_as_mocks(self)
+      allow_any_instance_of(InvoiceRepository).to receive(:create_invoices).and_return(mock_data)
+      invoice_repository = InvoiceRepository.new('fake.csv')
+
+      new_invoice = {
+        id: nil,
+        customer_id: 1,
+        merchant_id: 1,
+        status: "pending",
+        created_at: Time.now,
+        updated_at: Time.new(2020, 12, 31)
+      }
+
+      invoice_repository.create(new_invoice)
+
+      expect(invoice_repository.invoices.length).to eq 11
+
+      invoice_repository.delete(10)
+
+      expect(invoice_repository.invoices.length).to eq 10
+    end
+  end
 end
