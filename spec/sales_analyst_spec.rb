1bc690b44babe0f57ffe11bc6932dcefdbbe370c
Richard <rdesilvey@gmail.com>
2021-04-17 14:59:42 -0600

    Test the average item price for a merchant

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -70,4 +70,13 @@ RSpec.describe SalesAnalyst do
       end
     end
   end
+
+  describe '#average_item_price_for_merchant' do
+    it 'gets the average price for the given merchant' do
+      sales_analyst = SalesAnalystMocks.sales_analyst_mock(self)
+      actual_average = sales_analyst.average_item_price_for_merchant(3)
+      expected_average = 0
+      expect(actual_average).to eq expected_average
+    end
+  end
 end
