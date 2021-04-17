ce485380325f754087257a4c14f8357798bb732d
Caleb W. (He/Him) <78196294+Caleb1991@users.noreply.github.com>
2021-04-16 18:11:48 -0600

    Update customer_spec.rb

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,49 +5,49 @@ RSpec.describe Customer do
 
   describe 'initialize' do
     it 'exists' do
-      c = Customer.new({
+      c = Customer.new(
         id: 6,
         first_name: 'Joan',
         last_name: 'Clarke',
         created_at: Time.now,
         udated_at: Time.now
-      })
+      )
 
       expect(c).to be_an_instance_of(Customer)
     end
 
     it 'has an id' do
-      c = Customer.new({
+      c = Customer.new(
         id: 6,
         first_name: 'Joan',
         last_name: 'Clarke',
         created_at: Time.now,
         udated_at: Time.now
-      })
+      )
 
         expect(c.id).to eq(6)
     end
 
     it 'has a first name' do
-      c = Customer.new({
+      c = Customer.new(
         id: 6,
         first_name: 'Joan',
         last_name: 'Clarke',
         created_at: Time.now,
         udated_at: Time.now
-      })
+      )
 
       expect(c.first_name).to eq('Joan')
     end
 
     it 'has a last name' do
-      c = Customer.new({
+      c = Customer.new(
         id: 6,
         first_name: 'Joan',
         last_name: 'Clarke',
         created_at: Time.now,
         udated_at: Time.now
-      })
+      )
 
       expect(c.last_name).to eq('Clarke')
     end
@@ -55,13 +55,13 @@ RSpec.describe Customer do
     it 'has a time created at' do
       allow(Time).to receive(:now) do
         '12:58'
-      c = Customer.new({
+      c = Customer.new(
         id: 6,
         first_name: 'Joan',
         last_name: 'Clarke',
         created_at: Time.now,
         udated_at: Time.now
-      })
+      )
 
       expect(c.created_at).to eq('12:58')
       end
@@ -70,13 +70,13 @@ RSpec.describe Customer do
     it 'has a time upated at' do
       allow(Time).to receive(:now) do
         '12:58'
-      c = Customer.new({
+      c = Customer.new(
         id: 6,
         first_name: 'Joan',
         last_name: 'Clarke',
         created_at: Time.now,
         udated_at: Time.now
-      })
+      )
 
       expect(c.updated_at).to eq('12:58')
       end
