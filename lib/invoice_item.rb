296d7174812f6c982b3ff79405bb74c5d552fd47
Zach T <ztrokey@gmail.com>
2021-04-17 13:43:40 -0600

    add attributes tests and methods to invoice_item

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -6,6 +6,7 @@ class InvoiceItem
                 :item_id,
                 :invoice_id,
                 :quantity,
+                :unit_price,
                 :created_at,
                 :updated_at,
                 :invoice_item_repo
@@ -17,8 +18,8 @@ class InvoiceItem
     @id = (row[:id]).to_i
     @item_id = row[:item_id].to_i
     @invoice_id = (row[:invoice_id]).to_i
-    @quantity = (row[:quantity])
-    @unit_price = (row[:unit_price])
+    @quantity = (row[:quantity]).to_i
+    @unit_price = (row[:unit_price]).to_i
     @created_at = Time.parse(row[:created_at].to_s)
     @updated_at = Time.parse(row[:updated_at].to_s)
     @invoice_item_repo = invoice_item_repo
--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -21,5 +21,27 @@ RSpec.describe InvoiceItem do
       )
       expect(invoice_item).to be_instance_of(InvoiceItem)
     end
+
+    it 'has attributes' do
+      mock_invoice_item_repository = instance_double('InvoiceItemRepository')
+      invoice_item = InvoiceItem.new({
+            id: '21854 ',
+            item_id: '123654',
+            invoice_id: '654123',
+            quantity: '999',
+            unit_price: '987456',
+            created_at: '2012-03-27 14:54:09 UTC',
+            updated_at: '2013-03-27 14:54:09 UTC'
+        },
+        mock_invoice_item_repository
+      )
+      expect(invoice_item.id).to eq(21854)
+      expect(invoice_item.item_id).to eq(123654)
+      expect(invoice_item.invoice_id).to eq(654123)
+      expect(invoice_item.quantity).to eq(999)
+      expect(invoice_item.unit_price).to eq(987456)
+      expect(invoice_item.created_at.year).to eq(2012)
+      expect(invoice_item.updated_at.year).to eq(2013)
+    end
   end
 end
