8c9ea29cb9ffc3c531289c6d4e77bbca419f4ad3
NoahZinter <noah.zinter@gmail.com>
2021-04-17 12:42:14 -0600

    Test if InvoiceItemRepo class exists

---
+++ spec/invoice_item_repository_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require 'csv'
+require './lib/invoice_item'
+
+describe InvoiceItemRepository do 
+  describe '#initialize' do
+    it 'exists' do
+      allow_any_instance_of(InvoiceItemRepository).to receive(:create_invoice_items).and_return(MockData.invoice_items_as_hashes)
+      ii_repo = InvoiceItemRepository.new('fake.csv')
+
+      expect(ii_repo).is_a? InvoiceItemRepository
+    end
+  end
+end
