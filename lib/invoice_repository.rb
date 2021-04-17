af6a0ca9704b9733ee70f28fecb9c35411949376
sami-p <sami.peterson14@gmail.com>
2021-04-17 15:52:28 -0500

    Add all and find_by_id methods; pass tests

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,4 +1,19 @@
 class InvoiceRepository
+  attr_reader :invoices
+
   def initialize(filename)
-  end 
+    @invoices = create_invoices(filename)
+  end
+
+  def create_invoies(filename)
+    FileIo.process_csv(filename, Invoice)
+  end
+
+  def all
+    @invoices
+  end
+
+  def find_by_id(id)
+    @invoices.find { |invoice| invoice.id == id }
+  end
 end
