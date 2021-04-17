301aec5bd7e60e84812d97bbe9ef12546794c201
Netia Ingram <netia1128@hotmail.com>
2021-04-17 16:19:22 -0600

    Add test and code for transaction #update

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -21,9 +21,34 @@ class Transaction
     @transaction_repo = transaction_repo
   end
 
-#   def update_time_stamp
-#     @updated_at = Time.now
-#   end
+  def update(attributes)
+    update_credit_card_expiration_date(attributes)
+    update_credit_card_number(attributes)
+    update_result(attributes)
+    update_time_stamp
+  end
+
+  def update_credit_card_number(attributes)
+    if attributes[:credit_card_number] != nil
+        @credit_card_number = attributes[:credit_card_number].to_i
+    end 
+  end
+
+  def update_credit_card_expiration_date(attributes)
+    if attributes[:credit_card_expiration_date] != nil
+        @credit_card_expiration_date = Time.parse(attributes[:credit_card_expiration_date])
+    end 
+  end
+
+  def update_result(attributes)
+    if attributes[:result] != nil
+        @result = attributes[:result]
+    end 
+  end
+
+  def update_time_stamp
+    @updated_at = Time.now
+  end
 
 #   def day_created
 #     @created_at.strftime('%A').capitalize
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -50,6 +50,42 @@ RSpec.describe Transaction do
       expect(transaction.updated_at).to be_instance_of(Time)
     end
   end
+
+  describe '#update' do
+    it 'updates transactions attributes' do
+      mock_transaction_repo = instance_double('TransactionRepository')
+      test_transaction = Transaction.new({
+                                      id: '263395617 ',
+                                      invoice_id: '456789',
+                                      credit_card_number: '4297222479999999',
+                                      credit_card_expiration_date: '2016-01-11 11:51:37 UTC',
+                                      result: 'success',
+                                      created_at: '2016-01-11 11:51:37 UTC',
+                                      updated_at: '1993-09-29 11:56:40 UTC'
+                                    },
+                                    mock_transaction_repo
+                                   )
+
+      attributes = {
+                      id: '263317',
+                      invoice_id: '45678sdfsdf9',
+                      credit_card_number: '429999',
+                      credit_card_expiration_date: '2021-01-11 11:51:37 UTC',
+                      result: 'hot dog',
+                      created_at: '2016-01-11 11:51:37 UTC',
+                      updated_at: '1993-09-29 11:56:40 UTC'
+                    }
+
+      test_transaction.update(attributes)
+      expect(test_transaction.id).to eq(263395617)
+      expect(test_transaction.invoice_id).to eq(456789)
+      expect(test_transaction.credit_card_number).to eq(429999)
+      expect(test_transaction.credit_card_expiration_date.year).to eq(2021)
+      expect(test_transaction.result).to eq('hot dog')
+      expect(test_transaction.created_at.year).to eq(2016)
+      expect(test_transaction.updated_at.year).to eq(2021)
+    end
+  end
   
 #   describe '#update_time_stamp' do
 #     it 'updates the updated_at timestamp' do
@@ -69,21 +105,5 @@ RSpec.describe Transaction do
 #     end
 #   end
   
-#   describe '#day_created' do
-#     it 'shows day of week that invoice was created' do
-#       mock_invoice_repo = instance_double('InvoiceRepository')
-#       invoice = Invoice.new(  {
-#                                 id: '263395617',
-#                                 customer_id: '456789',
-#                                 merchant_id: '234567890',
-#                                 status: 'pending',
-#                                 created_at: '2016-01-11 11:51:37 UTC',
-#                                 updated_at: '1993-09-29 11:56:40 UTC'
-#                               },
-#                                 mock_invoice_repo)
-#       invoice.day_created
-    
-#       expect(invoice.day_created).to eq('Monday')
-#     end
-#   end
+#  
 end
