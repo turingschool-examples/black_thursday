88eb86e315da6380c5a00efc937e36bc066a0d35
Richard <rdesilvey@gmail.com>
2021-04-17 15:09:37 -0600

    Find a sum to test against

--- spec/sales_analyst_mocks.rb
+++ spec/sales_analyst_mocks.rb
@@ -1,6 +1,11 @@
 require './lib/sales_analyst'
 
 class SalesAnalystMocks
+
+  def self.sum_of_prices_merchant3
+    @@sum_of_prices_merchant3
+  end
+
   def self.sales_engine_mock(eg)
     sales_engine = eg.instance_double('SalesEngine')
     eg.allow(sales_engine).to eg.receive(:analyst).and_return SalesAnalyst.new(sales_engine)
@@ -21,7 +26,10 @@ class SalesAnalystMocks
     items_as_hashes = MockData.items_as_hashes(number_of_hashes: 3, merchant_id_range: (0..0))
     items_as_hashes += MockData.items_as_hashes(number_of_hashes: 7, merchant_id_range: (1..1))
     items_as_hashes += MockData.items_as_hashes(number_of_hashes: 4, merchant_id_range: (2..2))
-    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 12, merchant_id_range: (3..3))
+    items_as_hashes_merchant3 = MockData.items_as_hashes(number_of_hashes: 12, merchant_id_range: (3..3))
+    items_as_hashes += items_as_hashes_merchant3
+
+    @@sum_of_prices_merchant3 = MockData.sum_item_prices_from_hash(items_as_hashes_merchant3)
 
     items_as_mocks = MockData.items_as_mocks(eg, items_as_hashes)
     merchants_as_hashes = MockData.merchants_as_hashes(number_of_hashes: 4)
