a025d8f1defbf06a77acc54f3c392d3c6986511f
JoannaCoPo <jococopo@gmail.com>
2021-04-17 09:51:39 -0600

    Update transaction file path from CSV

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -8,12 +8,13 @@ class SalesEngine
   def initialize(paths)
     @items = ItemRepo.new(paths[:items], self)
     @merchants = MerchantRepo.new(paths[:merchants], self)
+    @transactions = TransactionRepo.new(paths[:merchants], self)
   end
 
   def self.from_csv(paths)
     new(paths)
   end
   # self.from_csv(hash from i0 directions) => paths
-  # from csv method will call initialize to pass 
-  # the hash through, should return the instance of itself 
+  # from csv method will call initialize to pass
+  # the hash through, should return the instance of itself
 end
