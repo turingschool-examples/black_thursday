c5d6c9f4b816569ac49549f6e649b3161797c362
Netia Ingram <netia1128@hotmail.com>
2021-04-17 16:32:43 -0600

    Add test and code for transaction #update_time_stamp

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -238,23 +238,22 @@ RSpec.describe Transaction do
       expect(test_transaction2.result).to eq ('success')
     end
   end
-#   describe '#update_time_stamp' do
-#     it 'updates the updated_at timestamp' do
-#       mock_invoice_repo = instance_double('InvoiceRepository')
-#       invoice = Invoice.new(  {
-#                                 id: '263395617',
-#                                     customer_id: '456789',
-#                                     merchant_id: '234567890',
-#                                     status: 'pending',
-#                                     created_at: '2016-01-11 11:51:37 UTC',
-#                                     updated_at: '1993-09-29 11:56:40 UTC'
-#                                 },
-#                                 mock_invoice_repo)
-#       invoice.update_time_stamp
-     
-#       expect(invoice.updated_at.year).to eq(2021)
-#     end
-#   end
-  
-#  
+  describe '#update_time_stamp' do
+    it 'updates the updated_at timestamp' do
+      mock_transaction_repo = instance_double('TransactionRepository')
+      transaction = Transaction.new({
+                                      id: '263395617 ',
+                                      invoice_id: '456789',
+                                      credit_card_number: '4297222479999999',
+                                      credit_card_expiration_date: '2016-01-11 11:51:37 UTC',
+                                      result: 'success',
+                                      created_at: '2016-01-11 11:51:37 UTC',
+                                      updated_at: '1993-09-29 11:56:40 UTC'
+                                    },
+                                    mock_transaction_repo
+                                  )
+      transaction.update_time_stamp
+      expect(transaction.updated_at.year).to eq(2021)
+    end
+  end
 end
