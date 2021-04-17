5e8b935261be414e628f561928db245e6f9f46dc
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-16 19:44:22 -0600

    Create tests for TRepo#initialize

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 require_relative './transaction'
+
+class TransactionRepository
+  attr_reader :transactions
+
+  def initialize(filename)
+    @transactions = create_transactions(filename)
+  end
+
+  def create_transactions(filename)
+    FileIo.process_csv(filename, Transaction)
+  end
+end
--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -2,12 +2,41 @@
 
 require './lib/transaction'
 require './lib/transaction_repository'
+require './data/mock_data'
 
 
 describe TransactionRepository do
   describe '#initialize' do
     it 'exists' do
-      
+      data_hashes = [{
+        id: 6,
+        invoice_id: 8,
+        credit_card_number: '4242424242424242',
+        credit_card_expiration_date: '0220',
+        result: 'success',
+        created_at: Time.now,
+        updated_at: Time.now
+      }]
+      mock_data = MockData.mock_generator(self, 'Transaction', data_hashes)
+      allow_any_instance_of(TransactionRepository).to receive(:create_transactions).and_return(mock_data)
+      t_repo = TransactionRepository.new('fake.csv')
+
+      expect(t_repo).is_a? TransactionRepository
+    end
+
+    it 'has an array of Transactions' do
+      data_hashes = [{
+        id: 6,
+        invoice_id: 8,
+        credit_card_number: '4242424242424242',
+        credit_card_expiration_date: '0220',
+        result: 'success',
+        created_at: Time.now,
+        updated_at: Time.now
+      }]
+      mock_data = MockData.mock_generator(self, 'Transaction', data_hashes)
+      allow_any_instance_of(TransactionRepository).to receive(:create_transactions).and_return(mock_data)
+      t_repo = TransactionRepository.new('fake.csv')
 
       expect(t_repo).is_a? TransactionRepository
     end
