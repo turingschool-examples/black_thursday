334b9fc8f3f75fd071a4fa43b35bcdae9401597e
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-17 15:50:51 -0700

    Create test for time check

--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -31,4 +31,24 @@ RSpec.describe InvoiceItem do
       expect(invoice_item.updated_at.class).to eq(Time)
     end
   end
+
+  describe '#time_check' do
+    sales_engine = SalesEngine.from_csv({
+                              :items     => "./data/items.csv",
+                              :merchants => "./data/merchants.csv",
+                              :invoices => "./data/invoices.csv",
+                              :invoice_items => "./data/invoice_items.csv",
+                              :customers => "./data/customers.csv"
+                              })
+    invoice_item = sales_engine.items.all[0]
+
+    it 'returns input as originally passed in if input is class Time' do
+      time_object = Time.parse("2007-06-04 21:35:10 UTC")
+      expect(invoice_item.time_check(time_object)).to eq(time_object)
+    end
+
+    it 'returns input converted to Time object if input is not class Time' do
+      expect(invoice_item.time_check("2007-06-04 21:35:10 UTC")).to eq(Time.parse("2007-06-04 21:35:10 UTC"))
+    end
+  end
 end
