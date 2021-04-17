34634c75278fc5d4501bd1ba0d96daed31b994b1
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-17 14:22:30 -0700

    Add requires for invoice items

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,22 +2,25 @@ require 'CSV'
 require_relative '../lib/merchant_repository'
 require_relative '../lib/item_repository'
 require_relative '../lib/invoice_repository'
+require_relative '../lib/invoice_item_repository'
 require_relative '../lib/sales_analyst'
 
 class SalesEngine
   attr_reader :merchants,
               :items,
               :invoices,
+              :invoice_items,
               :analyst
 
   def initialize(csv_data)
     @merchants = MerchantRepository.new(csv_data[:merchants])
     @items = ItemRepository.new(csv_data[:items])
     @invoices = InvoiceRepository.new(csv_data[:invoices])
+    @invoice_items = InvoiceItemRepository.new(csv_data[:invoice_items])
     @analyst = SalesAnalyst.new(self)
   end
 
-  def self.from_csv(csv_data) 
+  def self.from_csv(csv_data)
      SalesEngine.new(csv_data)
   end
 end
