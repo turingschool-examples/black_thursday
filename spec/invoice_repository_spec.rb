213d56f7f83258ab35489a570724586ef30193ab
sami-p <sami.peterson14@gmail.com>
2021-04-17 15:52:04 -0500

    Test #all and #find_by_id

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -13,4 +13,33 @@ describe InvoiceRepository do
       expect(invoice_repository).is_a? InvoiceRepository
     end
   end
+
+  describe '#all' do
+    it 'has an array of all invoices' do
+      allow_any_instance_of(InvoiceRepository).to receive(:create_invoices).and_return(MockData.invoices_as_hashes)
+      invoice_repository = InvoiceRepository.new('fake.csv')
+
+      expect(invoice_repository.all).is_a? Array
+      expect(invoice_repository.all.length).to eq 10
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'returns nil if no invoice has specified id' do
+      mock_data = MockData.invoices_as_mocks(self)
+      allow_any_instance_of(InvoiceRepository).to receive(:create_invoices).and_return(mock_data)
+      invoice_repository = InvoiceRepository.new('fake.csv')
+
+      expect(invoice_repository.find_by_id(500)).to eq nil
+    end
+
+    it 'finds invoices by id' do
+      mock_data = MockData.invoices_as_mocks(self)
+      allow_any_instance_of(InvoiceRepository).to receive(:create_invoices).and_return(mock_data)
+      invoice_repository = InvoiceRepository.new('fake.csv')
+
+      expected = invoice_repository.invoices.first
+      expect(invoice_repository.find_by_id(0)).to eq expected
+    end
+  end
 end
