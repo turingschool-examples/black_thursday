f6a614a09e040e55a14862955061510c1a66e456
JoannaCoPo <jococopo@gmail.com>
2021-04-17 07:46:56 -0600

    Create tests for find methods

--- spec/transaction_repo_spec.rb
+++ spec/transaction_repo_spec.rb
@@ -33,9 +33,62 @@ RSpec.describe ItemRepo do
   describe '#methods' do
 
     it '#all' do
-      item_repo = ItemRepo.new("./data/items.csv",@repo)
+      transaction_repo = TransactionRepo.new("./data/transactions.csv", @repo)
 
-      expect(item_repo.all).to be_an_instance_of(Array)
+      expect(transaction_repo.all).to be_an_instance_of(Array)
     end
+
+    it '#find by id' do
+      transaction_repo = TransactionRepo.new("./data/transactions.csv", @repo)
+      transaction1 = Transaction.new({:id => 6,
+                                      :invoice_id => 8,
+                                      :credit_card_number => "4242424242424242",
+                                      :credit_card_expiration_date => "0220",
+                                      :result => "success",
+                                      :created_at => Time.now,
+                                      :updated_at => Time.now
+                                    })
+
+      # item_repo.add_item(item1)
+
+      expect(transaction1.find_by_id(6)).to eq(transaction1)
+      expect(transaction1.find_by_id(999999999)).to eq(nil)
+    end
+
+    it '#find all by invoice id' do
+      transaction_repo = TransactionRepo.new("./data/transactions.csv", @repo)
+      transaction1 = Transaction.new({:id => 6,
+                                      :invoice_id => 8,
+                                      :credit_card_number => "4242424242424242",
+                                      :credit_card_expiration_date => "0220",
+                                      :result => "success",
+                                      :created_at => Time.now,
+                                      :updated_at => Time.now
+                                    })
+
+      #SOMETHING LIKE transaction_repo.add_invoice(invoice1)
+
+      expect(transaction1.find_all_by_invoice_id(8)).to eq([transaction1])
+      expect(transaction1.find_all_by_invoice_id(0)).to eq([])
+    end
+
+    it '#find all by credit card number' do
+      transaction_repo = TransactionRepo.new("./data/transactions.csv", @repo)
+      transaction1 = Transaction.new({:id => 6,
+                                      :invoice_id => 8,
+                                      :credit_card_number => "4242424242424242",
+                                      :credit_card_expiration_date => "0220",
+                                      :result => "success",
+                                      :created_at => Time.now,
+                                      :updated_at => Time.now
+                                    })
+
+      #SOMETHING LIKE transaction_repo.add_invoice(invoice1)
+
+      expect(transaction1.find_all_by_credit_card_number("4242424242424242")).to eq([transaction1])
+      expect(transaction1.find_all_by_credit_card_number("0000000000000000")).to eq([])
+    end
+
+  end
 
 end
