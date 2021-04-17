c2ab14695f35f450b459b1393ab4e62f7050e360
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-17 14:42:43 -0700

    Change name

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,1 +1,11 @@
+require_relative '../lib/invoice_items'
+require_relative '../lib/repository'
+require 'bigdecimal/util'
 
+class InvoiceItemRepository < Repository
+
+  def initialize(path)
+    super(path)
+    @array_of_objects = create_invoice_items(@parsed_csv_data)
+  end
+end
