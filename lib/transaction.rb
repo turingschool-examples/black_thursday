35a7157eab1f94af106f17f845b5473f3c6b3a79
JoannaCoPo <jococopo@gmail.com>
2021-04-17 17:09:52 -0600

    Fix typos

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -16,7 +16,6 @@ class Transaction
     @credit_card_expiration_date = transaction_info[:credit_card_expiration_date]
     @result = transaction_info[:result]
     @created_at = Time.parse(transaction_info[:created_at].to_s)
-    @updated_at = Time.parse(item_info[:updated_at].to_s)
+    @updated_at = Time.parse(transaction_info[:updated_at].to_s)
   end
-  
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -10,21 +10,19 @@ RSpec.describe Transaction do
                                       :credit_card_expiration_date => "0220",
                                       :result => "success",
                                       :created_at => Time.now,
-                                      :updated_at => Time.now
-                                    })
+                                      :updated_at => Time.now}, @repo)
 
       expect(transaction1).to be_an_instance_of(Transaction)
     end
 
-    xit 'has attributes' do
+    it 'has attributes' do
       transaction1 = Transaction.new({:id => 6,
                                       :transaction_id => 8,
                                       :credit_card_number => "4242424242424242",
                                       :credit_card_expiration_date => "0220",
                                       :result => "success",
                                       :created_at => Time.now,
-                                      :updated_at => Time.now
-                                    })
+                                      :updated_at => Time.now}, @repo)
 
       expect(transaction1.id).to eq(6)
       expect(transaction1.transaction_id).to eq(8)
