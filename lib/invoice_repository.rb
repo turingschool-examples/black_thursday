1224b51954518c17b5b526d8b100fc83724f65fe
sami-p <sami.peterson14@gmail.com>
2021-04-17 16:12:39 -0500

    Create find all by customer id method; pass test

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -16,4 +16,8 @@ class InvoiceRepository
   def find_by_id(id)
     @invoices.find { |invoice| invoice.id == id }
   end
+
+  def find_all_by_customer_id(customer_id)
+    @invoices.find_all { |invoice| invoice.customer_id == customer_id }
+  end
 end
