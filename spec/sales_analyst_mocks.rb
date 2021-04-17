ab3df13b2f93fe9af57e5091b733236d11de5de9
Richard <rdesilvey@gmail.com>
2021-04-17 14:53:16 -0600

    Update Ids

--- spec/sales_analyst_mocks.rb
+++ spec/sales_analyst_mocks.rb
@@ -18,10 +18,10 @@ class SalesAnalystMocks
     sales_engine = sales_engine_mock(eg)
     sales_analyst = sales_engine.analyst
 
-    items_as_hashes = MockData.items_as_hashes(number_of_hashes: 3, merchant_id_range: (1..1))
-    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 7, merchant_id_range: (2..2))
-    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 4, merchant_id_range: (3..3))
-    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 12, merchant_id_range: (4..4))
+    items_as_hashes = MockData.items_as_hashes(number_of_hashes: 3, merchant_id_range: (0..0))
+    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 7, merchant_id_range: (1..1))
+    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 4, merchant_id_range: (2..2))
+    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 12, merchant_id_range: (3..3))
 
     items_as_mocks = MockData.items_as_mocks(eg, items_as_hashes)
     merchants_as_hashes = MockData.merchants_as_hashes(number_of_hashes: 4)
