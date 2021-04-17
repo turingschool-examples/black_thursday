28b9899e33240b7e38ae4e35a283be335954fa00
Aliya Merali <merali4@gmail.com>
2021-04-16 19:05:47 -0600

    Add time_check method to intake string or time object

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/sales_engine'
 require_relative '../lib/item_repository'
 require 'bigdecimal/util'
+require 'time'
 
 class Item
 attr_reader  :created_at,
@@ -16,13 +17,21 @@ attr_accessor :id,
     @name = info_hash[:name]
     @description = info_hash[:description]
     @unit_price = info_hash[:unit_price].to_d
-    @created_at = info_hash[:created_at]
-    @updated_at = info_hash[:updated_at]
+    @created_at = time_check(info_hash[:created_at])
+    @updated_at = time_check(info_hash[:updated_at])
     @merchant_id = info_hash[:merchant_id].to_i
   end
 
   def unit_price_to_dollars
     (@unit_price.to_f / 100).round(4)
+  end
+
+  def time_check(time)
+    if time.class == Time
+      time
+    else
+      Time.parse(time)
+    end
   end
 
 end
