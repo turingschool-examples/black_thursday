bc9a849e160ae59168c67095379b1c2064432c6d
JoannaCoPo <jococopo@gmail.com>
2021-04-17 10:37:58 -0600

    Create find_by methods

--- lib/transaction_repo.rb
+++ lib/transaction_repo.rb
@@ -17,4 +17,32 @@ class TransactionRepo
     end
   end
 
+  def all
+    @transactions
+  end
+
+  def find_by_id(id) #clarify passing in id
+    @transactions.find do |transaction|
+      transaction.id == id
+    end
+  end
+
+  def find_by_id_invoice_id(invoice_id)
+    @transactions.find do |transaction|
+      transaction.invoice_id == invoice_id
+    end
+  end
+
+  def find_all_by_credit_card_number(number)
+    @transactions.find_all do |transaction|
+      transaction.number == number
+    end
+  end
+
+  def find_all_by_result(result)
+    @transactions.find_all do |transaction|
+      transaction.result == result
+    end
+  end
+
 end
