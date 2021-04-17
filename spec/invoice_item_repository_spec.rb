be4d48ecfa53fa03a0bb7e63066af34aeb550452
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-17 16:45:57 -0700

    Add find all by item id test

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -4,18 +4,19 @@ require_relative '../lib/merchant_repository'
 require_relative '../lib/item_repository'
 require_relative '../lib/item'
 require_relative '../lib/invoice_item_repository'
+require_relative '../lib/invoice_item'
 require_relative '../lib/invoice_repository'
 require 'bigdecimal/util'
 
 RSpec.describe InvoiceItemRepository do
   describe 'initialization' do
     sales_engine = SalesEngine.from_csv({
-                              :items     => "./data/items.csv",
-                              :merchants => "./data/merchants.csv",
-                              :invoices => "./data/invoices.csv",
-                              :invoice_items => "./data/invoice_items.csv",
-                              :customers => "./data/customers.csv"
-                              })
+                                                          :items     => "./data/items.csv",
+                                                          :merchants => "./data/merchants.csv",
+                                                          :invoices => "./data/invoices.csv",
+                                                          :invoice_items => "./data/invoice_items.csv",
+                                                          :customers => "./data/customers.csv"
+                                                          })
     invoice_item_repo = sales_engine.invoice_items
 
     it 'exists' do
@@ -29,16 +30,66 @@ RSpec.describe InvoiceItemRepository do
 
   describe 'all method' do
     sales_engine = SalesEngine.from_csv({
-                              :items     => "./data/items.csv",
-                              :merchants => "./data/merchants.csv",
-                              :invoices => "./data/invoices.csv",
-                              :invoice_items => "./data/invoice_items.csv",
-                              :customers => "./data/customers.csv"
-                              })
+                                                          :items     => "./data/items.csv",
+                                                          :merchants => "./data/merchants.csv",
+                                                          :invoices => "./data/invoices.csv",
+                                                          :invoice_items => "./data/invoice_items.csv",
+                                                          :customers => "./data/customers.csv"
+                                                          })
     invoice_item_repo = sales_engine.invoice_items
 
     it 'returns array of all invoice items' do
       expect(invoice_item_repo.all.count).to eq(21830)
+    end
+  end
+
+  describe 'various find methods' do
+    sales_engine = SalesEngine.from_csv({
+                                                          :items     => "./data/items.csv",
+                                                          :merchants => "./data/merchants.csv",
+                                                          :invoices => "./data/invoices.csv",
+                                                          :invoice_items => "./data/invoice_items.csv",
+                                                          :customers => "./data/customers.csv"
+                                                          })
+    invoice_item_repo = sales_engine.invoice_items
+
+#REPEATED IN REPOSITORY TESTS
+    xit 'find by id returns an instance by matching id' do
+      expect(invoice_item_repo.find_by_id(10).id).to eq(10)
+      expect(invoice_item_repo.find_by_id(10).item_id).to eq(263523644)
+      expect(invoice_item_repo.find_by_id(10).invoice_id).to eq(2)
+    end
+
+#REPEATED IN REPOSITORY TESTS
+    it 'find by id returns a nil if no id match' do
+      expect(invoice_item_repo.find_by_id(200000)).to eq(nil)
+    end
+
+    it 'find all by item id' do
+      expect(invoice_item_repo.find_all_by_item_id(263408101).length).to eq(11)
+    end
+
+  end
+
+  describe 'delete method' do
+    sales_engine = SalesEngine.from_csv({
+                                                          :items     => "./data/items.csv",
+                                                          :merchants => "./data/merchants.csv",
+                                                          :invoices => "./data/invoices.csv",
+                                                          :invoice_items => "./data/invoice_items.csv",
+                                                          :customers => "./data/customers.csv"
+                                                          })
+    invoice_item_repo = sales_engine.invoice_items
+
+    it "delete deletes the specified invoice" do
+      invoice_item_repo.delete(21831)
+
+      expected = invoice_item_repo.find_by_id(21831)
+      expect(expected).to eq nil
+    end
+
+    it "delete on unknown invoice does nothing" do
+      invoice_item_repo.delete(22000)
     end
   end
 end
