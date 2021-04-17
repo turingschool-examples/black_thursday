0d8959ed2087e9e8d9fcbb33b9d757e3e066c51c
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-16 18:56:35 -0600

    WIP exists test'

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -5,5 +5,11 @@ require './lib/transaction_repository'
 
 
 describe TransactionRepository do
-  
+  describe '#initialize' do
+    it 'exists' do
+      
+
+      expect(t_repo).is_a? TransactionRepository
+    end
+  end
 end
