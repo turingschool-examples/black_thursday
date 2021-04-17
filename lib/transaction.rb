4563983b013fc0f157da0a23cc9f35caecc4d7d8
Netia Ingram <netia1128@hotmail.com>
2021-04-17 12:20:24 -0600

    Add test and code for transaction #initialize it has attributes

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,23 +2,22 @@ require 'bigdecimal'
 require 'time'
 
 class Transaction
-#   attr_reader   :id,
-#                 :customer_id,
-#                 :merchant_id,
-#                 :created_at,
-#                 :invoice_repo
-
-#   attr_accessor :status,
-#                 :updated_at
+  attr_reader   :id,
+                :invoice_id,
+                :credit_card_number,
+                :credit_card_expiration_date,
+                :result,
+                :created_at,
+                :updated_at
 
   def initialize(row, transaction_repo)
     @id = (row[:id]).to_i
     @invoice_id = row[:invoice_id].to_i
     @credit_card_number = (row[:credit_card_number]).to_i
-    @credit_card_expiration_date = Time.parse(row[:credit_card_expiration_date].to_s)
+    @credit_card_expiration_date = Time.parse(row[:credit_card_expiration_date])
     @result = row[:result]
-    @created_at = Time.parse(row[:created_at].to_s)
-    @updated_at = Time.parse(row[:updated_at].to_s)
+    @created_at = Time.parse(row[:created_at])
+    @updated_at = Time.parse(row[:updated_at])
     @transaction_repo = transaction_repo
   end
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -24,28 +24,31 @@ RSpec.describe Transaction do
       expect(transaction).to be_instance_of(Transaction)
     end
   
-#     it 'has attributes' do
-#         mock_invoice_repo = instance_double('InvoiceRepository')
-#         invoice = Invoice.new({
-#             id: '263395617',
-#             customer_id: '456789',
-#             merchant_id: '234567890',
-#             status: 'pending',
-#             created_at: '2016-01-11 11:51:37 UTC',
-#             updated_at: '1993-09-29 11:56:40 UTC'
-#         },
-#         mock_invoice_repo
-#     )
+    it 'has attributes' do
+        mock_transaction_repo = instance_double('TransactionRepository')
+        transaction = Transaction.new({
+                                        id: '263395617 ',
+                                        invoice_id: '456789',
+                                        credit_card_number: '4297222479999999',
+                                        credit_card_expiration_date: '2016-01-11 11:51:37 UTC',
+                                        result: 'success',
+                                        created_at: '2016-01-11 11:51:37 UTC',
+                                        updated_at: '1993-09-29 11:56:40 UTC'
+                                      },
+                                      mock_transaction_repo
+                                     )
     
-#     expect(invoice.id).to eq(263395617)
-#       expect(invoice.customer_id).to eq(456789)
-#       expect(invoice.merchant_id).to eq(234567890)
-#       expect(invoice.status).to eq(:pending)
-#       expect(invoice.created_at.year).to eq(2016)
-#       expect(invoice.created_at).to be_instance_of(Time)
-#       expect(invoice.updated_at.year).to eq(1993)
-#       expect(invoice.updated_at).to be_instance_of(Time)
-#     end
+      expect(transaction.id).to eq(263395617)
+      expect(transaction.invoice_id).to eq(456789)
+      expect(transaction.credit_card_number).to eq(4297222479999999)
+      expect(transaction.credit_card_expiration_date.year).to eq(2016)
+      expect(transaction.credit_card_expiration_date).to be_instance_of(Time)
+      expect(transaction.result).to eq('success')
+      expect(transaction.created_at.year).to eq(2016)
+      expect(transaction.created_at).to be_instance_of(Time)
+      expect(transaction.updated_at.year).to eq(1993)
+      expect(transaction.updated_at).to be_instance_of(Time)
+    end
   end
   
 #   describe '#update_time_stamp' do
