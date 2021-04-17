d87a8bb7603cd53955778356cd19023a4ca1395c
Richard <rdesilvey@gmail.com>
2021-04-17 14:18:51 -0600

    Abstract tests for sales_analyst mock

--- spec/sales_analyst_mocks.rb
+++ spec/sales_analyst_mocks.rb
@@ -2,15 +2,34 @@ require './lib/sales_analyst'
 
 class SalesAnalystMocks
   def self.sales_engine_mock(eg)
-    sales_engine_mock = eg.instance_double('SalesEngine')
-    eg.allow(sales_engine_mock).to eg.receive(:analyst).and_return SalesAnalyst.new(sales_engine_mock)
+    sales_engine = eg.instance_double('SalesEngine')
+    eg.allow(sales_engine).to eg.receive(:analyst).and_return SalesAnalyst.new(sales_engine)
 
     mock_item_repository = eg.instance_double('ItemRepository')
     mock_merchant_repository = eg.instance_double('MerchantRepository')
 
-    eg.allow(sales_engine_mock).to eg.receive(:items).and_return mock_item_repository
-    eg.allow(sales_engine_mock).to eg.receive(:merchants).and_return mock_merchant_repository
+    eg.allow(sales_engine).to eg.receive(:items).and_return mock_item_repository
+    eg.allow(sales_engine).to eg.receive(:merchants).and_return mock_merchant_repository
 
-    sales_engine_mock
+    sales_engine
+  end
+
+  def self.sales_analyst_mock(eg)
+    sales_engine = sales_engine_mock(eg)
+    sales_analyst = sales_engine.analyst
+
+    items_as_hashes = MockData.items_as_hashes(number_of_hashes: 3, merchant_id_range: (1..1))
+    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 7, merchant_id_range: (2..2))
+    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 4, merchant_id_range: (3..3))
+    items_as_hashes += MockData.items_as_hashes(number_of_hashes: 12, merchant_id_range: (4..4))
+
+    items_as_mocks = MockData.items_as_mocks(eg, items_as_hashes)
+    merchants_as_hashes = MockData.merchants_as_hashes(number_of_hashes: 4)
+    merchants_as_mocks = MockData.merchants_as_mocks(eg, merchants_as_hashes)
+
+    eg.allow(sales_engine.items).to eg.receive(:all).and_return items_as_mocks
+    eg.allow(sales_engine.merchants).to eg.receive(:all).and_return merchants_as_mocks
+
+    sales_analyst
   end
 end
