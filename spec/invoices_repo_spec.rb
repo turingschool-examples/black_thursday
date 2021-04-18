995ccb64301ecc012ecfe920c04108cb928728f9
deebot10 <dee1jhill@gmail.com>
2021-04-18 13:38:09 -0600

    Create test for find_all_by_merchant_id on invoice repo

--- spec/invoices_repo_spec.rb
+++ spec/invoices_repo_spec.rb
@@ -29,15 +29,23 @@ RSpec.describe InvoiceRepo do
       expect(invoice_repository.all.class).to eq(Array)
       expect(invoice_repository.all.length).to eq(4985)
     end
+
     it "can find invoice by id" do
       expect(invoice_repository.find_by_id(1)).to be_instance_of(Invoice)
       expect(invoice_repository.find_by_id(10000)).to eq(nil)
       expect(invoice_repository.find_by_id(1)).to eq(invoice_repository.invoice_list[0])
     end
+
     it 'can find all by customer id' do
       expect(invoice_repository.find_all_by_customer_id(1).class).to eq(Array)
       expect(invoice_repository.find_all_by_customer_id(1).length).to eq(8)
       expect(invoice_repository.find_all_by_customer_id(1).first).to eq(invoice_repository.invoice_list[0])
+    end
+
+    it 'can find all by merchant id' do
+      expect(invoice_repository.find_all_by_merchant_id(12335938).class).to eq(Array)
+      expect(invoice_repository.find_all_by_merchant_id(12335938).length).to eq(16)
+      expect(invoice_repository.find_all_by_merchant_id(12335938).first).to eq(invoice_repository.invoice_list[0])
     end
   end
 end
