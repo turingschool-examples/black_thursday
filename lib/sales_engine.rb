1732056e37d97156860369824b24b0f514ab61c0
Suzie K <suziekim.dev@gmail.com>
2021-04-18 12:43:02 -0400

    Refactor spec tests for Item Class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,9 +2,9 @@ require 'CSV'
 require_relative 'item_repo'
 require_relative 'merchant_repo'
 # require_relative 'sales_analyst'
-# require_relative 'invoices'
+require_relative 'invoice_repo'
 # require_relative 'invoice_items'
-require_relative 'transactions'
+require_relative 'transaction_repo'
 
 class SalesEngine
   attr_reader :items,
@@ -17,9 +17,9 @@ class SalesEngine
     @items = ItemRepo.new(paths[:items], self)
     @merchants = MerchantRepo.new(paths[:merchants], self)
     # @analyst = SalesAnalyst.new(self)
-    # @invoices = InvoiceRepo.new(path[:invoices], self)
-    # @invoice_items = InvoiceItemRepo.new(path[:invoice_items], self)
-    @transactions = TransactionRepo.new(paths[:merchants], self)
+    @invoices = InvoiceRepo.new(paths[:invoices], self)
+    # @invoice_items = InvoiceItemRepo.new(paths[:invoice_items], self)
+    # @transactions = TransactionRepo.new(paths[:merchants], self)
   end
 
   def self.from_csv(paths)
