f138d16a55161032ef39a9dce9527f2428a17512
deebot10 <dee1jhill@gmail.com>
2021-04-18 13:30:38 -0600

    Create a test for find_all_by_customer_id in invoice repo

--- spec/invoices_repo_spec.rb
+++ spec/invoices_repo_spec.rb
@@ -34,5 +34,10 @@ RSpec.describe InvoiceRepo do
       expect(invoice_repository.find_by_id(10000)).to eq(nil)
       expect(invoice_repository.find_by_id(1)).to eq(invoice_repository.invoice_list[0])
     end
+    it 'can find all by customer id' do
+      expect(invoice_repository.find_all_by_customer_id(12335938).class).to eq(Array)
+      expect(invoice_repository.find_all_by_customer_id(12335938).length).to eq(16)
+      expect(invoice_repository.find_all_by_customer_id(12335938).first).to eq(invoice_repository.invoice_list[0])
+    end
   end
 end
