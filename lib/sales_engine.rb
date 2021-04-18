15a9732315c98017d5ecfb6c827d30bbad4e8745
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-18 12:05:04 -0600

    Update SalesEngine to include #transactions

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -7,14 +7,17 @@ require_relative './item_repository'
 require_relative './merchant'
 require_relative './merchant_repository'
 require_relative './sales_analyst'
+require_relative './transaction'
+require_relative './transaction_repository'
 
 class SalesEngine
-  attr_reader :items, :merchants, :invoices
+  attr_reader :items, :merchants, :invoices, :transactions
 
   def initialize(files)
     @invoices = InvoiceRepository.new(files[:invoices])
     @items = ItemRepository.new(files[:items])
     @merchants = MerchantRepository.new(files[:merchants])
+    @transactions = TransactionRepository.new(files[:transactions])
   end
 
   def self.from_csv(files)
