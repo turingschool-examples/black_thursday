222dcf52dccf0f30b4f7eaf945b608247b0f1936
Richard <rdesilvey@gmail.com>
2021-04-17 18:29:51 -0600

    Removed helper method for sale engine mock

--- spec/sales_analyst_mocks_spec.rb
+++ spec/sales_analyst_mocks_spec.rb
@@ -4,10 +4,10 @@ require './lib/sales_analyst'
 require './spec/sales_analyst_mocks'
 
 RSpec.describe SalesAnalystMocks do
-  describe '#sales_engine_mock' do
-    it 'returns a mocked sales engine' do
-      sales_engine_mock = SalesAnalystMocks.sales_engine_mock(self)
-      expect(sales_engine_mock).not_to eq nil
+  describe '#sales_analyst_mock' do
+    it 'returns a mocked sales analyst' do
+      sales_analyst_mock = SalesAnalystMocks.sales_analyst_mock(self)
+      expect(sales_analyst_mock).not_to eq nil
     end
   end
 end
