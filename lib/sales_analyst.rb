4a9ea15ef01d3e32656ec1d8567907ae75970345
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-18 14:09:21 -0700

    Refactor average_item_price_for_merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -57,13 +57,13 @@ class SalesAnalyst
 
   def average_item_price_for_merchant(merchant_id)
     merchant_items = find_all_items_by_merchant_id(merchant_id)
-    sum_of_merchant_prices = merchant_items.sum do |item|
+    sum_of_prices = merchant_items.sum do |item|
       item.unit_price
     end
-    if merchant_items.length == 0
-      0
+    if merchant_items.length != 0
+      Compute.mean(sum_of_prices, merchant_items.length)
     else
-      Compute.mean(sum_of_merchant_prices, merchant_items.length)
+      0
     end
   end
 
@@ -143,4 +143,12 @@ class SalesAnalyst
   end
 
 
+###THIS IS FOR THE NEXT PART OF ITERATION 2###
+  # def merchants_with_high_item_count
+  #   mean = average_items_per_merchant
+  #   greater_than_1sd = mean + average_items_per_merchant_standard_deviation
+  #   find_all_merchants.find_all do |merchant|
+  #     find_all_items_by_merchant_id(merchant.id).length > greater_than_1sd
+  #   end
+  # end
 end
