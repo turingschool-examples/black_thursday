3fba44f57a9053d3af2537727e22b049ca193dca
Richard <rdesilvey@gmail.com>
2021-04-18 10:48:04 -0600

    Alphabetize requires

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,9 +1,9 @@
 require 'CSV'
-require_relative './item'
-require_relative './merchant'
-require_relative './item_repository'
-require_relative './merchant_repository'
 require_relative './file_io'
+require_relative './item'
+require_relative './item_repository'
+require_relative './merchant'
+require_relative './merchant_repository'
 require_relative './sales_analyst'
 
 class SalesEngine
