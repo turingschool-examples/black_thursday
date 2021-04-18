07c0d666263d6c79647a466ad044e78d1b46f69b
danembb <dbrophy720@gmail.com>
2021-04-18 13:47:51 -0600

    Create id attribute for invoice_items

---
+++ spec/invoice_item_spec.rb
@@ -0,0 +1,36 @@
+require 'RSpec'
+require 'CSV'
+require 'bigdecimal'
+require 'invoice_item_repository'
+require 'invoice_item'
+
+RSpec.describe InvoiceItem do
+  describe 'instantiation' do
+    it '::new' do
+      invoice_items = InvoiceItem.new({:id         => 6,
+                                      :item_id     => 7,
+                                      :invoice_id  => 8,
+                                      :quantity    => 1,
+                                      :unit_price  => BigDecimal(10.99,4),
+                                      :created_at  => Time.now,
+                                      :updated_at  => Time.now,
+                                      }, @repo)
+
+      expect(invoice_items).to be_an_instance_of(InvoiceItem)
+    end
+
+    it 'has attributes' do
+      invoice_items = InvoiceItem.new({:id          => 6,
+                                       :item_id     => 7,
+                                       :invoice_id  => 8,
+                                       :quantity    => 1,
+                                       :unit_price  => BigDecimal(10.99,4),
+                                       :created_at  => Time.now,
+                                       :updated_at  => Time.now,
+                                       }, @repo)
+      expect(invoice_items.id).to eq(6)
+    end
+  end
+
+  
+end
