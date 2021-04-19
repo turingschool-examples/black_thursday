d1bf7c938d5eac70a598495e6f8f2ef5b8fe0016
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-19 15:35:46 -0400

    edit analyst method in sales engine class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -28,7 +28,7 @@ class SalesEngine
   end
 
   def analyst
-    SalesAnalyst.new(@items, @merchants, @invoices)
+    SalesAnalyst.new(@items, @merchants, @invoices, @invoice_items, @transactions, @customers)
   end
 
   def find_items_by_id(id)
