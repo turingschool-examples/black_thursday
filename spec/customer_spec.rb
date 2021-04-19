73da3c98d0f8e9dd694c90263de7898b6fb3d270
deebot10 <dee1jhill@gmail.com>
2021-04-19 11:38:36 -0600

    Created test for created at method in customer spec

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -30,5 +30,9 @@ RSpec.describe Customer do
     it 'can return customer last name' do
       expect(customer_repo.customer_list[0].last_name).to eq("Ondricka")
     end
+
+    it 'can return time created at' do
+      expect(customer_repo.customer_list[0].created_at).to eq(Time.parse("2012-03-27 14:54:09 +0000"))
+    end
   end
 end
