e18d42c39a95882d85701b673fac90dd010250fd
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-19 12:19:12 -0400

    Set up csv for customer and customer repo

---
+++ lib/customer.rb
@@ -0,0 +1,18 @@
+require 'time'
+
+class Customer
+  attr_accessor :id,
+                :first_name,
+                :last_name,
+                :created_at,
+                :updated_at
+
+  def initialize(attributes, repository)
+    @id = attributes[:id].to_i
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @created_at = Time.parse(attributes[:created_at].to_s)
+    @updated_at = Time.parse(attributes[:updated_at].to_s)
+    @repository = repository
+  end
+end 
---
+++ lib/customer_repo.rb
@@ -0,0 +1,25 @@
+require_relative './customer'
+require 'time'
+require 'csv'
+require 'bigdecimal'
+
+class CustomerRepo
+  attr_reader :customer_list
+
+  def initialize(csv_files, engine)
+    @customer_list = customer_instances(csv_files)
+    @engine = engine
+  end
+
+  def customer_instances(csv_files)
+    customers = CSV.open(csv_files, headers: true, header_converters: :symbol)
+
+    @customer_list = customers.map do |customer|
+      Customer.new(customer, self)
+    end
+  end
+
+  def all
+    @customer_list
+  end
+end 
