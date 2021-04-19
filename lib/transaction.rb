32f2121a32e12112e8a110ef5147bbb188cb1e46
ahenzler <aliciahenzler@outlook.com>
2021-04-19 17:38:09 -0600

    fix transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -12,9 +12,9 @@ class Transaction
   def initialize(attributes, repository)
     @id = attributes[:id].to_i
     @invoice_id = attributes[:invoice_id].to_i
-    @credit_card_number = attributes[:credit_card_number].to_i
-    @credit_card_expiration_date = attributes[:credit_card_expiration_date].to_i
-    @result = attributes[:result]
+    @credit_card_number = attributes[:credit_card_number]
+    @credit_card_expiration_date = attributes[:credit_card_expiration_date]
+    @result = attributes[:result].to_sym
     @created_at = Time.parse(attributes[:created_at].to_s)
     @updated_at = Time.parse(attributes[:updated_at].to_s)
     @repository = repository
