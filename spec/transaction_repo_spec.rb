274e1a79ccbbab107fdeefd6b41e9b42f8facd63
deebot10 <dee1jhill@gmail.com>
2021-04-19 12:03:17 -0600

    Fixes to the Customer repo as well as add ons to the transation repo file

---
+++ spec/transaction_repo_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+require 'bigdecimal'
+require 'time'
+require './lib/sales_engine'
+require './lib/transaction'
+require './lib/transaction_repo'
+RSpec.describe TransactionRepo do
+  se = SalesEngine.from_csv({
+  :transactions => "./data/transactions.csv"
+  })
+  transaction_repo = se.transactions
+  context 'it exists' do
+    it 'exists' do
+      expect(transaction_repo).to be_instance_of(TransactionRepo)
+    end
+  end
+  context 'methods' do
+    it 'can return all items' do
+      expect(transaction_repo.all.class).to eq(Array)
+      expect(transaction_repo.all.length).to eq(4985)
+    end
+    it "can find invoice by id" do
+      expect(invoice_repository.find_all_by_invoice_id(1)).to be_instance_of(Transaction)
+      expect(invoice_repository.find_all_by_invoice_id(10000)).to eq(nil)
+      expect(invoice_repository.find_all_by_invoice_id(1)).to eq(transaction_repo.transaction_list[0])
+    end
+    it 'can find all by credit_card_number' do
+      expect(invoice_repository.find_all_by_credit_card_number(1).class).to eq(Array)
+      expect(invoice_repository.find_all_by_credit_card_number(4068631943231473).length).to eq(1)
+      expect(invoice_repository.find_all_by_credit_card_number(1).first).to eq(transaction_repo.transaction_list[0])
+      expect(invoice_repository.find_all_by_credit_card_number(9999999)).to eq([])
+    end
+  end
+end
