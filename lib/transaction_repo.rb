f2d021c201dc6a2fd7d6e890c2f91d1220a5ff7b
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-19 12:09:27 -0400

    Set up csv for transaction and transaction repo

---
+++ lib/transaction_repo.rb
@@ -0,0 +1,25 @@
+require_relative './transactions'
+require 'time'
+require 'csv'
+require 'bigdecimal'
+
+class TransactionRepo
+  attr_reader :transaction_list
+
+  def initialize(csv_files, engine)
+    @transaction_list = transaction_instances(csv_files)
+    @engine = engine
+  end
+
+  def transaction_instances(csv_files)
+    transactions = CSV.open(csv_files, headers: true, header_converters: :symbol)
+
+    @transaction_list = transactions.map do |transaction|
+      Transaction.new(transaction, self)
+    end
+  end
+
+  def all
+    @transaction_list
+  end
+end
