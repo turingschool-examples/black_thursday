140f28c2fff15bfd39f77977feec0b66079e95d9
Netia Ingram <netia1128@hotmail.com>
2021-04-19 13:00:58 -0600

    Refactor invoice item repo

--- lib/repo_brain.rb
+++ lib/repo_brain.rb
@@ -1,11 +1,17 @@
 class RepoBrain
 
+    def self.key_by_added_value_hash(records, key, value_to_add)
+      records.each_with_object(Hash.new(0)) do |record, hash|
+        hash[record.send("#{key}")] += value_to_add
+      end
+    end
+
     def self.find_by_id(id, id_type, records)
         records.find do |record|
           record.send("#{id_type}") == id
         end
     end
-  
+
     def self.find_all_by_id(id, id_type, records)
       records.find_all do |record|
         record.send("#{id_type}") == id
@@ -17,7 +23,7 @@ class RepoBrain
           record.send("#{symbol_type}") == symbol.to_sym
         end
     end
-  
+
     def self.find_by_full_string(string, data_type, records)
       records.find do |record|
         #fix law of demeter violation here
@@ -25,7 +31,7 @@ class RepoBrain
         record.send("#{data_type}").downcase == string.downcase
       end
     end
-  
+
     def self.find_by_partial_string(string, data_type, records)
       records.find_all do |record|
         #fix law of demeter violation here
@@ -33,7 +39,7 @@ class RepoBrain
         record.send("#{data_type}").downcase.include?(string.downcase)
       end
     end
-  
+
     def self.generate_new_id(records)
       highest_id = records.max_by do |record|
         record.id
