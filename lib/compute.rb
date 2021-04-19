742df619bfb10f1731e8a8b669a539085c98779f
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-19 09:38:21 -0600

    Fix Compute::mean method to check for BigDecimal

--- lib/compute.rb
+++ lib/compute.rb
@@ -1,7 +1,11 @@
 class Compute
 
   def self.mean(sum, num_of_elements)
-    mean = sum.to_f / num_of_elements
+    if sum.class == BigDecimal
+      mean = sum / num_of_elements
+    else
+      mean = sum.to_f / num_of_elements
+    end
     mean.round(2)
   end
 
