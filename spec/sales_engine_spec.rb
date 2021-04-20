e95b5fd0b8d8b2d5df8c4807daaad7a45c44dec8
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-20 11:01:53 -0600

    Refactor SalesAnalyst Demeter violations
    Instance variables that reached through SalesEngine to reach respective
    repositories has been rectified. Methods for accessing repositories were
    made in SalesEngine and then referenced in SalesAnalyst. Tests were
    additionally written in SalesEngine spec for these methods.

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -16,9 +16,14 @@ RSpec.describe SalesEngine do
     it 'exists' do
       expect(sales_engine).to be_instance_of(SalesEngine)
     end
+
+    it 'creates instance of SalesAnalyst' do
+      expect(sales_engine.analyst).to be_an_instance_of(SalesAnalyst)
+    end
+
   end
 
-  describe '#items & #merchants' do
+  describe 'repository functionality' do
     sales_engine = SalesEngine.from_csv({
                                           :items     => "./data/items.csv",
                                           :merchants => "./data/merchants.csv",
@@ -28,16 +33,32 @@ RSpec.describe SalesEngine do
                                           :transactions => "./data/transactions.csv"
                                           })
 
-    it 'creates instance of SalesAnalyst' do
-      expect(sales_engine.analyst).to be_an_instance_of(SalesAnalyst)
-    end
-
     it 'returns an object of class MerchantRepository' do
       expect(sales_engine.merchants).to be_an_instance_of(MerchantRepository)
     end
 
     it 'returns an object of class ItemRepository' do
       expect(sales_engine.items).to be_an_instance_of(ItemRepository)
+    end
+
+    it 'gets all merchants' do
+      expect(sales_engine.get_all_merchants[1]).to be_an_instance_of(Merchant)
+    end
+
+    it 'gets all items' do
+      expect(sales_engine.get_all_items[1]).to be_an_instance_of(Item)
+    end
+
+    it 'gets all invoices' do
+      expect(sales_engine.get_all_invoices[1]).to be_an_instance_of(Invoice)
+    end
+
+    it 'gets all invoice items' do
+      expect(sales_engine.get_all_invoice_items[1]).to be_an_instance_of(InvoiceItem)
+    end
+
+    it 'gets all transactions' do
+      expect(sales_engine.get_all_transactions[1]).to be_an_instance_of(Transaction)
     end
   end
 end
