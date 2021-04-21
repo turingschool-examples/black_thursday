29635b698c069bd2fa25b0b4d2a39fb403c3797a
Suzie K <suziekim.dev@gmail.com>
2021-04-21 08:41:30 -0400

    Migrate create/update methods

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,10 @@
 class Customer
   attr_reader :id,
-                :first_name,
-                :last_name,
-                :created_at,
-                :updated_at,
-                :repo
+              :first_name,
+              :last_name,
+              :created_at,
+              :updated_at,
+              :repo
 
   def initialize(customer_info, repo)
     @id = customer_info[:id].to_i
@@ -13,5 +13,27 @@ class Customer
     @created_at = Time.parse(customer_info[:created_at].to_s)
     @updated_at = Time.parse(customer_info[:updated_at].to_s)
     @repo = repo
+  end
+
+  def update_all(attributes)
+    update_first_name(attributes)
+    update_last_name(attributes)
+    update_updated_at(attributes)
+  end
+
+  def update_first_name(attributes)
+    @first_name = attributes[:first_name] if attributes[:first_name]
+  end
+
+  def update_last_name(attributes)
+    @last_name = attributes[:last_name] if attributes[:last_name]
+  end
+
+  def update_updated_at(attributes)
+    @updated_at = attributes[:updated_at] if attributes[:updated_at]
+  end
+
+  def update_id(new_id)
+    @id = new_id + 1
   end
 end
