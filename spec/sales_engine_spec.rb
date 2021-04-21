2d115d46af109fd74baa58235ba3139d7d87814b
Zach T <ztrokey@gmail.com>
2021-04-21 11:06:41 -0600

    changed from truncated to actual data

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -7,13 +7,21 @@ require './lib/transaction_repository'
 
 RSpec.describe SalesEngine do
   before do
+    # @se = SalesEngine.from_csv({
+    #   items: './spec/truncated_data/items_truncated.csv',
+    #   merchants: './spec/truncated_data/merchants_truncated.csv',
+    #   invoices: './spec/truncated_data/invoices_truncated.csv',
+    #   customers: './spec/truncated_data/customers_truncated.csv',
+    #   invoice_items: './spec/truncated_data/invoice_items_truncated.csv',
+    #   transactions: './spec/truncated_data/transactions_truncated.csv'
+    #                         })
     @se = SalesEngine.from_csv({
-      items: './spec/truncated_data/items_truncated.csv',
-      merchants: './spec/truncated_data/merchants_truncated.csv',
-      invoices: './spec/truncated_data/invoices_truncated.csv',
+      items: './data/items.csv',
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
       customers: './spec/truncated_data/customers_truncated.csv',
-      invoice_items: './spec/truncated_data/invoice_items_truncated.csv',
-      transactions: './spec/truncated_data/transactions_truncated.csv'
+      invoice_items: './data/invoice_items.csv',
+      transactions: './data/transactions.csv'
                             })
   end
 
@@ -37,31 +45,31 @@ RSpec.describe SalesEngine do
 
   describe '#average_average_price_per_merchant' do
     it 'shows average price across all merchants' do
-      expect(@se.average_average_price_per_merchant).to eq(0.1862e2)
+      expect(@se.average_average_price_per_merchant).to eq(0.35029e3)
     end
   end
 
   describe '#average_invoices_per_merchant' do
     it 'shows average invoices per merchant' do
-      expect(@se.average_invoices_per_merchant).to eq(1.0)
+      expect(@se.average_invoices_per_merchant).to eq(10.49)
     end
   end
 
   describe '#average_item_price_for_merchant' do
     it 'shows average item price for a given merchant' do
-      expect(@se.average_item_price_for_merchant(12334113)).to eq(0.7e1)
+      expect(@se.average_item_price_for_merchant(12334113)).to eq(0.15e3)
     end
   end
 
   describe '#average_items_per_merchant' do
     it 'shows average items per merchant' do
-      expect(@se.average_items_per_merchant).to eq(1.25)
+      expect(@se.average_items_per_merchant).to eq(2.88)
     end
   end
 
   describe '#average_items_per_merchant_standard_deviation' do
     it 'shows standard deviation of average items per merchant' do
-      expect(@se.average_items_per_merchant_standard_deviation).to eq(0.5)
+      expect(@se.average_items_per_merchant_standard_deviation).to eq(3.26)
     end
   end
 
@@ -81,7 +89,7 @@ RSpec.describe SalesEngine do
 
   describe '#bottom_merchants_by_invoice_count' do
     it 'shows bottom merchants by invoice count' do
-      expect(@se.bottom_merchants_by_invoice_count.count).to eq(0)
+      expect(@se.bottom_merchants_by_invoice_count.count).to eq(4)
     end
   end
 
@@ -111,7 +119,7 @@ RSpec.describe SalesEngine do
 
   describe '#golden_items' do
     it 'shows golden items which are 2 std dev above average item price' do
-      expect(@se.golden_items.count).to eq(0)
+      expect(@se.golden_items.count).to eq(5)
     end
   end
 
@@ -123,15 +131,15 @@ RSpec.describe SalesEngine do
 
   describe '#invoice_status' do
     it 'shows percent of invoices by status' do
-      expect(@se.invoice_status(:pending)).to eq(50.0)
-      expect(@se.invoice_status(:shipped)).to eq(33.33)
-      expect(@se.invoice_status(:returned)).to eq(16.67)
+      expect(@se.invoice_status(:pending)).to eq(29.55)
+      expect(@se.invoice_status(:shipped)).to eq(56.95)
+      expect(@se.invoice_status(:returned)).to eq(13.5)
     end
   end
 
   describe '#invoice_total_hash' do
     it 'passes the invoice_total_hash from invoice_item repo to other repos' do
-      expect(@se.invoice_total_hash.keys.count).to eq(1)
+      expect(@se.invoice_total_hash.keys.count).to eq(2810)
     end
   end
   describe '#invoice_total_value' do
@@ -148,7 +156,7 @@ RSpec.describe SalesEngine do
 
   describe '#items_created_in_month' do
     it 'returns invoices created in a specified month' do
-      expect(@se.items_created_in_month("March").count).to eq(4)
+      expect(@se.items_created_in_month("March").count).to eq(475)
     end
   end
 
@@ -160,43 +168,43 @@ RSpec.describe SalesEngine do
 
   describe '#merchant_sold_item_quantity_hash' do
     it 'returns hash of sold items' do
-      expect(@se.merchant_sold_item_quantity_hash(12334105).count).to eq(0)
+      expect(@se.merchant_sold_item_quantity_hash(12334105).count).to eq(28)
     end
   end
 
   describe '#merchant_sold_item_revenue_hash' do
     it 'returns hash of sold items' do
-      expect(@se.merchant_sold_item_quantity_hash(12334105).count).to eq(0)
+      expect(@se.merchant_sold_item_quantity_hash(12334105).count).to eq(28)
     end
   end
 
   describe '#merchant_items' do
     it 'returns hash of merchant items' do
-      expect(@se.merchant_items(12334105).count).to eq(2)
+      expect(@se.merchant_items(12334105).count).to eq(3)
     end
   end
 
   describe '#merchant_invoices' do
     it 'returns merchant invoices by merchant id' do
-      expect(@se.merchant_invoices(12334105).count).to eq(0)
+      expect(@se.merchant_invoices(12334105).count).to eq(10)
     end
   end
 
   describe '#merchants_with_only_one_item' do
     it 'returns merchants with only one item' do
-      expect(@se.merchants_with_only_one_item.count).to eq(3)
+      expect(@se.merchants_with_only_one_item.count).to eq(243)
     end
   end
 
   describe '#merchants_with_only_one_item_registered_in_month' do
     it 'returns merchants with only one item in a specified month' do
-      expect(@se.merchants_with_only_one_item_registered_in_month("March").count).to eq(1)
+      expect(@se.merchants_with_only_one_item_registered_in_month("March").count).to eq(21)
     end
   end
 
   describe '#merchants_with_pending_invoices' do
     it 'returns merchants with pending invoices' do
-      expect(@se.merchants_with_pending_invoices.count).to eq(1)
+      expect(@se.merchants_with_pending_invoices.count).to eq(467)
     end
   end
 
@@ -208,13 +216,13 @@ RSpec.describe SalesEngine do
 
   describe '#merchants_with_high_item_count' do
     it 'returns merchants with high item counts' do
-      expect(@se.merchants_with_high_item_count.count).to eq(1)
+      expect(@se.merchants_with_high_item_count.count).to eq(52)
     end
   end
 
   describe '#merchant_successful_invoice_array' do
     it 'returns array of successful invoices by merchant id' do
-      expect(@se.merchant_successful_invoice_array(12334105).count).to eq(0)
+      expect(@se.merchant_successful_invoice_array(12334105).count).to eq(5)
     end
   end
 
@@ -226,25 +234,19 @@ RSpec.describe SalesEngine do
 
   describe '#stdev_invoices_per_merchant' do
     it 'returns the standard deviation of invoices per merchant' do
-      expect(@se.stdev_invoices_per_merchant).to eq(0.0)
-    end
-  end
-
-  describe '#top_buyers' do
-    it 'returns the specified number of top buyers' do
-      expect(@se.top_buyers(4).count).to eq(4)
+      expect(@se.stdev_invoices_per_merchant).to eq(3.29)
     end
   end
 
   describe '#top_days_by_invoice_count' do
     it 'shows top days by invoice count' do
-      expect(@se.top_days_by_invoice_count).to eq(['Friday', 'Saturday'])
+      expect(@se.top_days_by_invoice_count).to eq(['Wednesday'])
     end
   end
 
   describe '#top_merchants_by_invoice_count' do
     it 'shows top merchants by invoice count' do
-      expect(@se.top_merchants_by_invoice_count.count).to eq(0)
+      expect(@se.top_merchants_by_invoice_count.count).to eq(12)
     end
   end
 
@@ -262,7 +264,7 @@ RSpec.describe SalesEngine do
 
   describe '#total_revenue_by_merchant' do
     it 'returns array of merchant id\'s and total revenue' do
-      expect(@se.total_revenue_by_merchant.count).to eq(8)
+      expect(@se.total_revenue_by_merchant.count).to eq(950)
     end
   end
 
