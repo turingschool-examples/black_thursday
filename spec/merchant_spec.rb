c9d7b58e1ec30c287b645b7cacb12d5dba45c396
Suzie K <suziekim.dev@gmail.com>
2021-04-21 17:13:38 -0400

    Update merchant class spec

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -5,14 +5,20 @@ RSpec.describe Merchant do
   describe 'instantiation' do
     it '::new' do
       mock_repo = double('MerchantRepo') 
-      merchant = Merchant.new({:id => 5, :name => 'Turing School'}, mock_repo)
+      merchant = Merchant.new({:id => 5, 
+                               :name => 'Turing School',
+                               :created_at => Time.now,
+                               :updated_at => Time.now}, mock_repo)
 
       expect(merchant).to be_an_instance_of(Merchant)
     end
 
     it 'has attributes' do
       mock_repo = double("MerchantRepo") 
-      merchant = Merchant.new({:id => 5, :name => 'Turing School'}, mock_repo)
+      merchant = Merchant.new({:id => 5, 
+                               :name => 'Turing School',
+                               :created_at => Time.now,
+                               :updated_at => Time.now}, mock_repo)
 
       expect(merchant.id).to eq(5)
       expect(merchant.name).to eq('Turing School')
@@ -22,7 +28,10 @@ RSpec.describe Merchant do
   describe '#methods' do
     it '#update name' do
       mock_repo = double("MerchantRepo") 
-      merchant = Merchant.new({:id => 5, :name => 'Turing School'}, mock_repo) 
+      merchant = Merchant.new({:id => 5, 
+                               :name => 'Turing School',
+                               :created_at => Time.now,
+                               :updated_at => Time.now}, mock_repo)
 
       merchant.update_name({:name => 'George Washington University'})
 
@@ -31,7 +40,10 @@ RSpec.describe Merchant do
 
     it '#update id' do
       mock_repo = double('ItemRepo')
-      merchant = Merchant.new({:id => 5, :name => 'Turing School'}, mock_repo)
+      merchant = Merchant.new({:id => 5, 
+                               :name => 'Turing School',
+                               :created_at => Time.now,
+                               :updated_at => Time.now}, mock_repo)
 
       new_id = 10000
 
