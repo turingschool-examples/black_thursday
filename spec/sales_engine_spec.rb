c5058ad6e4d285053a16ae0c943db5ccbb8caa30
Elliot Olbright <elliotolbright@gmail.com>
2021-05-31 11:15:16 -0600

    add spec directory and SalesEngine test file

---
+++ spec/sales_engine_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/sales_engine'
+
+RSpec.describe SalesEngine do
+
+    it 'exists' do
+      data_hash = {
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+      }
+      se = SalesEngine.new(data_hash)
+
+      expect(se).to be_a(SalesEngine)
+    end
+
+    it 'can receive data from a csv' do
+      se = SalesEngine.from_csv({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+      })
+
+      expect(se).to be_a(SalesEngine)
+      expect(se.items).to eq("./data/items.csv")
+      expect(se.merchants).to eq("./data/merchants.csv")
+    end 
+end
