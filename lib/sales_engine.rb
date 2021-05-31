af28f3d961b951a48662569e24ff00aaee597e3c
Elliot Olbright <elliotolbright@gmail.com>
2021-05-31 11:16:43 -0600

    add SalesEngine class

---
+++ lib/sales_engine.rb
@@ -0,0 +1,14 @@
+class SalesEngine
+  attr_reader :items, :merchants
+
+  def initialize(data_hash)
+    @items = data_hash[:items]
+    @merchants = data_hash[:merchants]
+
+  end
+
+  def self.from_csv(data_hash)
+    SalesEngine.new(data_hash)
+  end 
+
+end 
