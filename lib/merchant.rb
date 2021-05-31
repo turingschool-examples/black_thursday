e6642a4ea769c7e139f38b32ecb22407db67930f
Michael Abbott <michael@mjabbottdesign.com>
2021-05-31 12:52:35 -0700

    Add readable Merchant attributes test and code

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,5 +1,6 @@
 class Merchant
-  
+  attr_reader :id, :name
+
   def initialize(data_hash)
     @id = data_hash[:id]
     @name = data_hash[:name]
--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Merchant do
 
     expect(m).to be_a(Merchant)
   end
+
+  it 'has attributes' do
+    m = Merchant.new({:id => 5, :name => "Turing School"})
+
+    expect(m.id).to eq(5)
+    expect(m.name).to eq('Turing School')
+  end
 end
