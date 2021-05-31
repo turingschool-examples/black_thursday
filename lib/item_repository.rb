5fc96d8b81492fe33962b20739d5bba177a1731c
Scott-Borecki <scottborecki@gmail.com>
2021-05-31 14:13:56 -0600

    add ItemRepository methods (part 1)

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -6,4 +6,20 @@ class ItemRepository
     @all = []
   end
 
+  def find_by_id(id)
+    all.find { |item| id == item.id }
+  end
+
+  def find_by_name(name)
+    all.find { |item| name == item.name }
+  end
+
+  def find_all_with_description(description)
+    all.find_all { |item| description == item.description }
+  end
+
+  def find_all_by_price(price)
+    all.find_all { |item| price == item.unit_price }
+  end
+
 end
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -7,15 +7,10 @@ require 'bigdecimal'
 SimpleCov.start
 
 RSpec.describe ItemRepository do
-  it 'exists' do
-    ir = ItemRepository.new
 
-    expect(ir).to be_a(ItemRepository)
-  end
-
-  it 'has readable attributes' do
-    ir = ItemRepository.new
-    i = Item.new({
+  before :each do
+    @ir = ItemRepository.new
+    @i = Item.new({
       :id          => 1,
       :name        => "Pencil",
       :description => "You can use it to write things",
@@ -24,14 +19,63 @@ RSpec.describe ItemRepository do
       :updated_at  => Time.now,
       :merchant_id => 2
     })
+  end
 
-    expect(ir.all).to be_a(Array)
-    expect(ir.all).to eq([])
+  describe 'Object Creation' do
 
-    ir.all << i
-    
-    expect(ir.all).to be_a(Array)
-    expect(ir.all).to eq([i])
+    it 'exists' do
+      ir = ItemRepository.new
+
+      expect(@ir).to be_a(ItemRepository)
+    end
+
+    it 'has readable attributes' do
+
+
+      expect(@ir.all).to be_a(Array)
+      expect(@ir.all).to eq([])
+
+      @ir.all << @i
+
+      expect(@ir.all).to be_a(Array)
+      expect(@ir.all).to eq([@i])
+    end
+
+  end
+
+  describe 'Object Methods' do
+
+    before :each do
+      @ir.all << @i
+    end
+
+    it 'can return Item by object ID' do
+      expect(@ir.find_by_id(1)).to eq(@i)
+      expect(@ir.find_by_id(2)).to eq(nil)
+    end
+
+    it 'can return Item by item name' do
+      expect(@ir.find_by_name("Pencil")).to eq(@i)
+      expect(@ir.find_by_name("Pen")).to eq(nil)
+    end
+
+    it 'can return all Items by item description' do
+      expect(@ir.find_all_with_description("You can use it to write things")).to eq([@i])
+      expect(@ir.find_all_with_description("You can eat it")).to eq([])
+    end
+
+    it 'can return all Items by price' do
+      expect(@ir.find_all_by_price(BigDecimal(10.99,4))).to eq([@i])
+      expect(@ir.find_all_by_price(10.99)).to eq([@i])
+      expect(@ir.find_all_by_price(BigDecimal(9.99,3))).to eq([])
+      expect(@ir.find_all_by_price(9.99)).to eq([])
+    end
+
+    # it 'can return all Items by price range' do
+    #   expect(@ir.find_all_by_price_in_range(/[10-11]/)).to eq([@i])
+    #   expect(@ir.find_all_by_price_in_range(BigDecimal(9.99,3))).to eq([])
+    # end
+
   end
 
 end
