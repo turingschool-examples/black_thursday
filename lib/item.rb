2fd76a6e167f3cf15a0809290f53e4aeee2293cb
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-05-31 09:29:51 -0600

    Create initialize and unit price method

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,19 @@
 require 'CSV'
 class Item
+
+  attr_reader :id, :name
+
+  def initialize(info)
+    @id = info[:id]
+    @name = info[:name]
+    @description = info[:description]
+    @unit_price = info[:unit_price]
+    @merchant_id = info[:merchant_id]
+    @created_at = info[:created_at]
+    @updated_at = info[:updated_at]
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
+  end
 end
