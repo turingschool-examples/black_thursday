46e35c553298d6966c8aad36acb5e82355240c5e
danembb <dbrophy720@gmail.com>
2021-06-01 16:21:29 -0600

    Create item class

---
+++ spec/item_spec.rb
@@ -0,0 +1,19 @@
+require_relative 'spec_helper'
+
+RSpec.describe Item do
+  describe 'instantiation' do
+    it 'exists' do
+      item = Item.new({
+        :id          => 1,
+        :name        => "Pencil",
+        :description => "You can use it to write things",
+        :unit_price  => BigDecimal(10.99,4),
+        :created_at  => Time.now,
+        :updated_at  => Time.now,
+        :merchant_id => 2
+        })
+
+        expect(item).to be_a(Item)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'rspec'
 require 'simplecov'
+require 'bigdecimal'
 
 SimpleCov.start do
 end
 
 require './lib/sales_engine'
+require './lib/item'
