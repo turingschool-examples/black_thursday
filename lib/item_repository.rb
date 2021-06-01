38ffc85b72a47c563d6524ebba7e32554f63e028
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-01 12:50:30 -0600

    Build method for find by name

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -14,6 +14,14 @@ class ItemRepository #< SalesEngine
       @item_repo.find { |item| item.id == id }
     else
       nil
-    end 
+    end
+  end
+
+  def find_by_name(name)
+    if @item_repo.one? { |item| item.name.upcase == name.upcase } == true
+      @item_repo.find { |item| item.name.upcase == name.upcase }
+    else
+      nil
+    end
   end
 end
