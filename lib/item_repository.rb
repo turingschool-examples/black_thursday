9c16625f702a51f08cf00363887404436b5e8c0d
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-01 12:27:38 -0600

    Build all method to return array of known item instances

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,6 +2,10 @@ require 'csv'
 
 class ItemRepository #< SalesEngine
   def initialize(items)
-    @item_instances = items 
+    @item_instances = items
+  end
+
+  def all
+    @item_instances
   end
 end
