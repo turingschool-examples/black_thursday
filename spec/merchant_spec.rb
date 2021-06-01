fa1b33ca2e75d5baa445341a794502e18bb35676
Blake Dempsey <bdempsey864@gmail.com>
2021-06-01 16:26:06 -0600

    Added Merchant class

---
+++ spec/merchant_spec.rb
@@ -0,0 +1,19 @@
+require "rspec"
+require "./lib/merchant"
+
+RSpec.describe Merchant do
+  describe '#initialize' do
+    it 'exists' do
+      merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+      expect(merchant).to be_a(Merchant)
+    end
+
+    it 'has attributes' do
+      merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+      expect(merchant.id).to eq(5)
+      expect(merchant.name).to eq("Turing School")
+    end
+  end
+end
