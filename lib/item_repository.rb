0455026724cee02650e1eb18e7a92c39fc863588
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-01 12:37:53 -0600

    Build method for find by id

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,10 +2,18 @@ require 'csv'
 
 class ItemRepository #< SalesEngine
   def initialize(items)
-    @item_instances = items
+    @item_repo = items
   end
 
   def all
-    @item_instances
+    @item_repo
+  end
+
+  def find_by_id(id)
+    if @item_repo.one? { |item| item.id == id } == true
+      @item_repo.find { |item| item.id == id }
+    else
+      nil
+    end 
   end
 end
