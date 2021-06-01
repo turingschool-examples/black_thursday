96a638506a7947286fce66f24a008126a05d6383
Matt Kragen <mattdkragen@gmail.com>
2021-06-01 16:26:32 -0700

    Add sales_engine_spec; start tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,9 @@ require 'simplecov'
 SimpleCov.start do
 end
 
+require 'CSV'
 require 'bigdecimal'
 require './lib/item'
 require './lib/item_repository'
 require './lib/merchant'
+require './lib/sales_engine'
