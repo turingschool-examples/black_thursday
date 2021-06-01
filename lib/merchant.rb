66931a58d4295d4bff8365a67baf3c6bb6dfcb33
Michael Abbott <michael@mjabbottdesign.com>
2021-05-31 19:27:26 -0700

    Add #update helper test and code

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,10 +1,12 @@
 class Merchant
   attr_reader :id, :name
 
-  attr_writer :name
-
   def initialize(data_hash)
     @id = data_hash[:id]
     @name = data_hash[:name]
+  end
+
+  def update(attributes)
+    @name = attributes[:name]
   end
 end
--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -4,16 +4,29 @@ require 'simplecov'
 SimpleCov.start
 
 RSpec.describe Merchant do
-  it 'exists' do
-    m = Merchant.new({:id => 5, :name => "Turing School"})
+  
+  describe 'instantiation' do
+    it 'exists' do
+      m = Merchant.new({:id => 5, :name => "Turing School"})
 
-    expect(m).to be_a(Merchant)
+      expect(m).to be_a(Merchant)
+    end
+
+    it 'has attributes' do
+      m = Merchant.new({:id => 5, :name => "Turing School"})
+
+      expect(m.id).to eq(5)
+      expect(m.name).to eq('Turing School')
+    end
   end
 
-  it 'has attributes' do
-    m = Merchant.new({:id => 5, :name => "Turing School"})
+  describe 'Methods' do
+    it 'can update the Merchant with provided attributes' do
+      m = Merchant.new({:id => 5, :name => "Turing School"})
+      attributes = {:name => 'turingschool.edu'}
+      m.update(attributes)
 
-    expect(m.id).to eq(5)
-    expect(m.name).to eq('Turing School')
+      expect(m.name).to eq('turingschool.edu')
+    end
   end
 end
