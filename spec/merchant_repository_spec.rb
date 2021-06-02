379438174338370724b6e6b7d5d180a505ea86f4
sami-p <sami.peterson14@gmail.com>
2021-06-02 18:33:00 -0500

    Test setup for find_by_id

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -18,4 +18,25 @@ RSpec.describe MerchantRepository do
       expect(merchant_repo.all.length).to eq(4)
     end
   end
+
+  describe 'attributes' do
+    it 'can read Merchant attributes' do
+      path = 'fixture/merchant_fixture.csv'
+      merchant_repo = MerchantRepository.new(path)
+
+      all = merchant_repo.all
+
+      expect(all.first.id).to eq(12334471)
+      expect(all.last.name).to eq('MittenUpCanada')
+    end
+
+    it 'can find by Merchant ID' do
+      path = 'fixture/merchant_fixture.csv'
+      merchant_repo = MerchantRepository.new(path)
+
+      merchant_repo.create_merchants(path)
+
+      expect(merchant_repo.find_by_id(12337011)).to eq(merchant_repo.all[2])
+    end
+  end
 end
