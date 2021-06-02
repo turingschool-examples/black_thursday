2e14a478e20a4baf4afe20cfb87d95f02371aa93
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-01 21:00:12 -0400

    Update initialize to facilitate connection to repo

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,10 +1,17 @@
 require 'CSV'
 class Merchant
-  attr_reader :id, :name
+  attr_reader :id, :name, :created_at, :updated_at
 
-  def initialize(hash)
-    @id = hash[:id]
-    @name = hash[:name]
+  def initialize(merchant_data, repo)
+    @id = merchant_data[:id].to_i
+    @name = merchant_data[:name]
+    @created_at = merchant_data[:created_at]
+    @updated_at = merchant_data[:updated_at]
+    @repo = repo
+  end
+
+  def change_name(name)
+    @name = name
   end
 
 end
