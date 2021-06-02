db1c908b54b2199c85158d3c17ab3bad3a924f57
Matt Toensing <matthew.toensing@gmail.com>
2021-06-02 17:48:31 -0600

    Update with #create methods and #new_id

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -48,4 +48,30 @@ class ItemRepository
       range === item.unit_price
     end
   end
+
+  def find_all_by_merchant_id(merchant_id)
+    @all.find_all do |item|
+      item.merchant_id == merchant_id
+    end
+  end
+
+  def new_id
+    max_id = @all.max_by do |item|
+      item.id
+    end
+    max_id.id += 1
+  end
+
+  def create(attributes)
+    Item.new({:id => new_id,
+              :name => attributes[:name],
+              :description => attributes[:description],
+              :unit_price => attributes[:unit_price],
+              :created_at => attributes[:created_at],
+              :updated_at => attributes[:updated_at],
+              :merchant_id => attributes[:merchant_id]
+              })
+  end
 end
+
+# @ir.all.max_by {|item| item.id}
