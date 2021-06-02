d5a995f7e1ca8b17e88ecac8dd40b5445eed2886
Matt Kragen <mattdkragen@gmail.com>
2021-06-01 21:19:42 -0700

    Add test for #generate; construct #generate

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,20 +1,27 @@
 require_relative './spec_helper'
+require './fixtures/items.csv'
 
 RSpec.describe ItemRepository do
   context 'instantiation' do
     it 'exists' do
-      ir = ItemRepository.new('./data/items.csv')
+      ir = ItemRepository.new('./fixtures/items.csv')
       expect(ir).to be_a(ItemRepository)
     end
   end
 
   context 'methods' do
     before :each do
-      @ir = ItemRepository.new('./data/items.csv')
+      @ir = ItemRepository.new('./fixtures/items.csv')
     end
 
     it 'generates Item instances' do
-      expect(@ir.all).to be_a(Array)
+      item1 = @ir.all[1]
+      item2 = @ir.all[-1]
+      expect(item1.id).to eq(263395295)
+      expect(item1.name).to eq('pencils')
+      expect(item2.id).to eq(268716492)
+      expect(item2.name).to eq('mattress')
+      # expect(@ir.all).to be_a(Array)
     end
   end
 end
