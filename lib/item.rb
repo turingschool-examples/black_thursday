e77881a3d5d742618e38e857831de77a6d4e088d
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-02 19:37:52 -0600

    Create multiple item methods

--- lib/item.rb
+++ lib/item.rb
@@ -14,8 +14,8 @@ class Item
     @id = item_data[:id].to_i
     @name = item_data[:name]
     @description = item_data[:description]
-    @unit_price = BigDecimal(item_data[:unit_price]) / 100 
-    @merchant_id = item_data[:merchant_id]
+    @unit_price = BigDecimal(item_data[:unit_price]) / 100
+    @merchant_id = item_data[:merchant_id].to_i
     @created_at = item_data[:created_at]
     @updated_at = item_data[:updated_at]
     @repo = repo
@@ -24,5 +24,21 @@ class Item
 
   def unit_price_to_dollars
     @unit_price.to_f
+  end
+
+  def change_name(name)
+    @name = name
+  end
+
+  def change_unit_price(unit_price)
+    @unit_price = unit_price
+  end
+
+  def change_description(description)
+    @description = description
+  end
+
+  def change_updated_at
+    @updated_at = Time.now
   end
 end
