c3cbe99b4857c00122674006d902f552d4bf8ccb
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-03 16:49:06 -0600

    Rename file and refactor create new instance of item test

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -78,34 +78,39 @@ RSpec.describe ItemRepository do
     expect(@repo.find_all_by_price_in_range(0..4)).to eq([])
   end
 
-  xit 'returns items by merchant id' do
+  it 'returns items by merchant id' do
+    item_names = []
+    @repo.find_all_by_merchant_id(20).each do |item|
+      item_names << item.name
+    end
+    expect(item_names).to eq(['baseball bat'])
+
     item_names = []
     @repo.find_all_by_merchant_id(10).each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball'])
+    expect(item_names).to eq(['baseball', 'baseball glove'])
 
-    expect(@items_repo.find_all_by_merchant_id(10)).to eq([@item1, @item3])
-    expect(@items_repo.find_all_by_merchant_id(20)).to eq([@item2])
-    expect(@items_repo.find_all_by_merchant_id(50)).to eq([])
+    expect(@repo.find_all_by_merchant_id(50)).to eq([])
+  end
+
+  it 'creates new id number' do
+    expect(@repo.new_id_number).to eq(11)
   end
 
   xit 'creates new item instance' do
-    expect(@real_repo.length).to eq(3)
+    attributes = {
+      :name        => 'jersey',
+      :description => 'clothing',
+      :unit_price  => 20,
+      :merchant_id => 5
+      }
+    @repo.create(attributes)
 
-    @items_repo.create({
-      :id          => 1,
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal(10.99,4),
-      :created_at  => Time.now,
-      :updated_at  => Time.now,
-      :merchant_id => 2
-      }, nil)
-
-    expect(@item_repo.all.length).to eq(4)
-    expect(@items_repo.all[3].id).to eq(16)
-    expect(@items_repo.all[3].name).to eq('baseball jersey')
+    expect(@repo.all.length).to eq(4)
+    @repo.all.each do |item|
+      expect(item).to be_a(Item)
+    end
   end
 
   xit 'updates item attributes by id' do
@@ -134,14 +139,16 @@ RSpec.describe ItemRepository do
     expect(@real_rep.all.length).to eq()
   end
 
-  xit 'deletes item by id' do
-    allow(@item1).to receive(:id).and_return(3)
-    allow(@item2).to receive(:id).and_return(5)
-    allow(@item3).to receive(:id).and_return(10)
+  it 'deletes item by id' do
+    @repo.delete(5)
 
-    @items_repo.delete(5)
+    expect(@repo.all.length).to eq(2)
 
-    expect(@items_repo.find_by_id(5)).to eq(nil)
-    expect(@items_repo.all.length).to eq(2)
+    item_names = []
+    @repo.all.each do |item|
+      item_names << item.name
+    end
+
+    expect(item_names).to eq(['baseball', 'baseball glove'])
   end
 end
