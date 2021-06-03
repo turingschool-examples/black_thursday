b14005c306a56f6c244ad7f5e1acd870aba0edf3
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-03 15:38:51 -0600

    Add transaction spec

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Transaction do
             :credit_card_number => "4242424242424242",
             :credit_card_expiration_date => "0220",
             :result => "success",
-            :created_at => Time.now,
-            :updated_at => Time.now
+            :created_at => Time.now.year,
+            :updated_at => Time.now.year
           }
     @repo = double('transaction repo')
     @t = Transaction.new(@data, @repo)
@@ -20,11 +20,15 @@ RSpec.describe Transaction do
   end
 
   it 'has attributes' do
+    allow_any_instance_of(Time).to receive(:now).and_return(2021)
+
     expect(@t.id).to eq(6)
     expect(@t.invoice_id).to eq(8)
     expect(@t.credit_card_number).to eq(4242424242424242)
-    expect(@t.credit_card_expiration_date).to eq(0220)
+    expect(@t.credit_card_expiration_date).to eq("0220")
     expect(@t.result).to eq('success')
-    expect(@t.created_at).to eq(Time.)
+    expect(@t.created_at).to eq(2021)
   end
+
+  
 end
