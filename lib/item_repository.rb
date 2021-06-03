5f1b7f5bfff1f0fe476580ceebe46a54c7397276
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-03 16:19:22 -0600

    Refactor methods to remove single line syntax

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -5,7 +5,8 @@ class ItemRepository
   attr_reader :all
 
   def initialize(path)
-    @all = create_items(path)
+    @all = []
+    create_items(path)
   end
 
   def inspect
@@ -13,34 +14,39 @@ class ItemRepository
   end
 
   def create_items(path)
-    @all = []
     CSV.foreach(path, headers: true, header_converters: :symbol).each do |item|
       @all << Item.new(item, self)
     end
-    # item = CSV.load(path)
-    # item.map do |item_data|
-    #   Item.new(item_data)
-    # end
   end
 
   def find_by_id(id)
-    @all.find { |item| item.id == id }
+    @all.find do |item|
+      item.id == id
+    end
   end
 
   def find_by_name(name)
-    @all.find { |item| item.name.upcase == name.upcase }
+    @all.find do |item|
+      item.name.upcase == name.upcase
+    end
   end
 
   def find_all_with_description(description)
-    @all.find_all { |item| item.description == description }
+    @all.find_all do |item|
+      item.description == description
+    end 
   end
 
   def find_all_by_price(price)
-    @all.find_all { |item| item.price == price }
+    @all.find_all do |item|
+      item.unit_price == price
+    end
   end
 
   def find_all_by_price_in_range(range)
-    @all.find_all { |item| range.include?(item.price) }
+    temp = @all.find_all do |item|
+      range.include?(item.unit_price)
+    end
   end
 
   def find_all_by_merchant_id(merchant_id)
