97c495fab4a7dcd9ed49b4e3ecf64c47ba4fec45
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-03 14:37:34 -0600

    Build tests for merchant repo class

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -1,1 +1,24 @@
-.
+require 'rspec'
+require './lib/merchant_repository'
+
+describe MerchantRepository do
+  
+  it 'exists' do
+    merchant_repository = double()
+    allow(merchant_repository).to receive(:new) {true}
+    expect(merchant_repository.new).to eq()
+  end
+
+  it 'has attributes' do
+    merchant_repository = MerchantRepository.new(file_path.csv, engine)
+
+    expect(merchant.file_path).to eq(file_path.csv)
+    expect(merchant.engine).to eq(engine)
+  end
+
+  xit 'it can create a repo' do
+    merchant_repository = MerchantRepository.new(file_path.csv, engine)
+
+
+  end
+end
