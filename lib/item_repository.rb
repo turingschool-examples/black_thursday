7328fa755b4d87046897c003203094ef6638a2ef
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-06-02 19:38:12 -0600

    Create multiple item repo methods

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -60,14 +60,34 @@ class ItemRepository
   end
 
   def find_all_by_merchant_id(merchant_id)
+    items.find_all do |item|
+      item.merchant_id == merchant_id
+    end
   end
 
   def create(attributes)
+    merchant_id = items.max { |item| item.id }
+    attributes[:id] = merchant_id.id + 1
+    attributes[:created_at] = Time.now.strftime("%Y-%m-%d")
+    attributes[:updated_at] = Time.now.strftime("%Y-%m-%d")
+    @items << Item.new(attributes, self)
   end
 
   def update(id, attributes)
+    item_by_id = find_by_id(id)
+    if item_by_id != nil
+      item_by_id.change_name(attributes[:name])
+      item_by_id.change_unit_price(attributes[:unit_price])
+      item_by_id.change_description(attributes[:description])
+      item_by_id.change_updated_at(attributes[:updated_at])
+      require "pry"; binding.pry
+    end
   end
 
   def delete(id)
+    chopping_block = items.index { |item| item.id == id }
+    if chopping_block != nil
+      items.delete_at(chopping_block)
+    end
   end
 end
