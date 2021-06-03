332fcc1b768895c5e36b9293f6d7c536f9347813
Brett Jones <bhjones4545@gmail.com>
2021-06-02 21:46:35 -0600

    Complete methods and passing tests for #MerchantRepository

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -23,4 +23,40 @@ class MerchantRepository
       merchant.id == id
     end
   end
+
+  def find_by_name(name)
+    @all.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
+  def find_all_by_name(name)
+    @all.find_all do |merchant|
+      merchant.name.downcase.include?(name.downcase)
+    end
+  end
+
+  def new_id
+    max_id = @all.max_by do |merchant|
+      merchant.id
+    end
+    max_id.id += 1
+  end
+
+  def create(attributes)
+    Merchant.new({:id => new_id,
+              :name => attributes[:name]
+              })
+  end
+
+  def update(id, attributes)
+    update_merchant = find_by_id(id)
+    update_merchant.id = attributes[:id]
+    update_merchant.name = attributes[:name]
+  end
+
+  def delete(id)
+    deleted_merchant = find_by_id(id)
+    @all.delete(deleted_merchant)
+  end
 end
--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -29,5 +29,47 @@ RSpec.describe MerchantRepository do
       expect(@mr.find_by_id(12334185)).to eq(@merchant2)
       expect(@mr.find_by_id(26866642)).to eq(nil)
     end
+
+    it 'returns merchant with matching name or nil' do
+      expect(@mr.find_by_name('Candisart')).to eq(@merchant1)
+      expect(@mr.find_by_name('Madewithgitterxx')).to eq(@merchant2)
+      expect(@mr.find_by_name('footballs')).to eq(nil)
+    end
+
+    it 'returns merchant with supplied name fragment' do
+      expect(@mr.find_all_by_name('Candi')).to eq([@merchant1])
+      expect(@mr.find_all_by_name('Madew')).to eq([@merchant2])
+      expect(@mr.find_all_by_name('footba')).to eq([])
+    end
+
+    it 'creates a new merchant' do
+      expect(@mr.new_id).to eq(12334186)
+    end
+
+    it 'creates a new merchant instance with given attributes' do
+      attributes = {
+        :id          => nil,
+        :name        => "Bob's Airplanes"
+      }
+      expect(@mr.create(attributes)).to be_a(Merchant)
+    end
+
+    it 'updates merchant by id with given attributes' do
+
+      attributes = {
+        :id          => 12334102,
+        :name        => 'Candismath'
+      }
+      @mr.update(12334112, attributes)
+
+      expect(@merchant1.id).to eq(12334102)
+      expect(@merchant1.name).to eq('Candismath')
+    end
+
+    it 'delete merchant by id' do
+      expect(@mr.all.length).to eq(20)
+      expect(@mr.delete(12334112)).to eq(@merchant1)
+      expect(@mr.all.length).to eq(19)
+    end
   end
 end
