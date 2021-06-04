3423e3073b96ce4250b1776608479a66d41d376d
Matt Kragen <mattdkragen@gmail.com>
2021-06-03 20:26:25 -0700

    Modify methods; add instance variable

--- lib/item.rb
+++ lib/item.rb
@@ -1,25 +1,25 @@
 class Item
-  attr_accessor :id,
+   attr_reader  :id,
                 :name,
                 :description,
                 :unit_price,
-                :updated_at
-
-  attr_reader :created_at,
-              :merchant_id
+                :updated_at,
+                :created_at,
+                :merchant_id,
+                :repo
 
   def initialize(info, repo)
-    @id = info[:id]
-    @name = info[:name]
-    @description = info[:description]
-    @unit_price = info[:unit_price]
-    @created_at = info[:created_at]
-    @updated_at = info[:updated_at]
-    @merchant_id = info[:merchant_id]
+    @id = info['id']
+    @name = info['name']
+    @description = info['description']
+    @unit_price = info['unit_price']
+    @created_at = info['created_at']
+    @updated_at = info['updated_at']
+    @merchant_id = info['merchant_id']
     @repo = repo
   end
 
   def unit_price_to_dollars
-    "$#{@unit_price.to_f}"
+    '%.2f' % "#{@unit_price}"
   end
 end
