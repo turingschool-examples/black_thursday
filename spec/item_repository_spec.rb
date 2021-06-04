54e961e64652441797fe3e9b651c85176227682f
Jessica Jones <daddehjones@gmail.com>
2021-06-03 22:07:03 -0400

    make it pass items spec test

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,1 +1,140 @@
-.
+require 'simplecov'
+SimpleCov.start
+require './lib/item_repository'
+require 'rspec'
+
+RSpec.describe ItemRepository do
+  it "exists" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    expect(item_repo).to be_a(ItemRepository)
+  end
+
+  it "has attributes" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    expect(item_repo.file_path).to eq('./spec/fixtures/item_fixtures.csv')
+    expect(item_repo.items).to eq([])
+  end
+
+  it "creates repos" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.items.count).to eq(12)
+  end
+
+  it "shows all" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.all.count).to eq(12)
+  end
+
+  it "finds by id" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.find_by_id(263408574).name).to eq("Adidas Azteca Fußballschuh")
+  end
+
+  it "finds by name" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.find_by_name("Adidas Azteca Fußballschuh").id).to eq(263408574)
+  end
+
+  it "finds all with description" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.find_all_with_description("Adidas Azteca mit Schraubstollen und silbernen Streifen. Hergestellt ab dem Jahr 1986.").count).to eq(2)
+  end
+
+  it "finds all by price" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.find_all_by_price(BigDecimal(75)).length).to eq(4)
+  end
+
+  it "finds all prices by range" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.find_all_by_price_in_range(50.00..100.00).length).to eq(6)
+    expect(item_repo.find_all_by_price_in_range(0.00..49.99).length).to eq(3)
+    expect(item_repo.find_all_by_price_in_range(101.00..1000.00).length).to eq(3)
+  end
+
+  it "finds all by merchant id" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+
+    expect(item_repo.find_all_by_merchant_id(12334123).length).to eq(9)
+  end
+
+  it "can create an item" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+    item_repo.create({
+      name: "Wings",
+      description: "hot, lemon pepper",
+      unit_price: "2000",
+      created_at: Time.now,
+      updated_at: Time.now,
+      merchant_id: "12334727"
+      })
+    expect(item_repo.find_by_id(263409022).name).to eq("Wings")
+  end
+
+  it "can update an item" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+    item_repo.create({
+      name: "Wings",
+      description: "hot, lemon pepper",
+      unit_price: "2000",
+      created_at: Time.now,
+      updated_at: Time.now,
+      merchant_id: "12334727"
+      })
+
+    item_repo.update(263409022,{unit_price: "3000"})
+
+    expect(item_repo.find_by_id(263409022).updated_at.to_i).to eq (Time.now).to_i
+  end
+
+  it "can delete an item" do
+    mock_engine = double("sales_engine")
+    item_repo = ItemRepository.new('./spec/fixtures/item_fixtures.csv', mock_engine)
+
+    item_repo.create_repo
+    item_repo.delete(263408355)
+
+    expect(item_repo.find_by_id(263408355)).to eq(nil)
+  end
+end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,92 @@
-.
+require 'simplecov'
+SimpleCov.start
+require 'rspec'
+require './lib/item'
+
+
+RSpec.describe Item do
+  it "exists" do
+    repo_double = double("item_repo")
+    item = Item.new({
+      id: "263399735",
+      name:  "Bootees",
+      description: "Gorgeous hand knitted baby bootees",
+      unit_price: "1200",
+      merchant_id: "12334271",
+      created_at: "2016-01-11 11:34:30 UTC",
+      updated_at: "1999-03-24 02:37:51 UTC"
+      }, repo_double)
+
+      expect(item).to be_a(Item)
+    end
+
+    it "has attributes" do
+      repo_double = double("item_repo")
+      item = Item.new({
+        id: "263399735",
+        name:  "Bootees",
+        description: "Gorgeous hand knitted baby bootees",
+        unit_price: "1200",
+        merchant_id: "12334271",
+        created_at: "2016-01-11 11:34:30 UTC",
+        updated_at: "1999-03-24 02:37:51 UTC"
+        }, repo_double)
+
+        expect(item.id).to eq(263399735)
+        expect(item.name).to eq("Bootees")
+        expect(item.description).to eq("Gorgeous hand knitted baby bootees")
+        expect(item.unit_price).to eq( 0.12e2)
+        expect(item.merchant_id).to eq(12334271)
+        expect(item.created_at).to eq Time.parse("2016-01-11 11:34:30 UTC")
+        expect(item.updated_at).to eq Time.parse("1999-03-24 02:37:51 UTC")
+      end
+
+      it "can change unit price to dollars" do
+        repo_double = double("item_repo")
+        item = Item.new({
+          id: "263399735",
+          name:  "Bootees",
+          description: "Gorgeous hand knitted baby bootees",
+          unit_price: "1200",
+          merchant_id: "12334271",
+          created_at: "2016-01-11 11:34:30 UTC",
+          updated_at: "1999-03-24 02:37:51 UTC"
+          }, repo_double)
+
+          expect(item.unit_price_to_dollars).to eq(12.0)
+      end
+
+      it "can change unit price to dollars" do
+        repo_double = double("item_repo")
+        item = Item.new({
+          id: "263399735",
+          name:  "Bootees",
+          description: "Gorgeous hand knitted baby bootees",
+          unit_price: "1200",
+          merchant_id: "12334271",
+          created_at: "2016-01-11 11:34:30 UTC",
+          updated_at: "1999-03-24 02:37:51 UTC"
+          }, repo_double)
+
+          item.change_unit_price(5)
+
+          expect(item.unit_price).to eq(5)
+      end
+
+      it "can update time" do
+        repo_double = double("item_repo")
+        item = Item.new({
+          id: "263399735",
+          name:  "Bootees",
+          description: "Gorgeous hand knitted baby bootees",
+          unit_price: "1200",
+          merchant_id: "12334271",
+          created_at: "2016-01-11 11:34:30 UTC",
+          updated_at: "1999-03-24 02:37:51 UTC"
+          }, repo_double)
+
+          item.update_time
+
+          expect(item.updated_at.to_i).to eq(Time.now.to_i)
+      end
+end
