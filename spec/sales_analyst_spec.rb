2dc7adb5b05a0ecbe1336ead3de2999625415397
Caroline Tan <carolinectan3@gmail.com>
2021-06-03 23:19:31 -0400

    Refactor tests; skip tests that need methods built

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -24,34 +24,29 @@ RSpec.describe SalesEngine do
   end
 
   it 'can return average items per merchant standard deviation' do
-# 4 is the mean
-# set = [3,4,5]
-# std_dev = sqrt( ( (3-4)^2+(4-4)^2+(5-4)^2 ) / 2 )
-
-# 1.33 is the mean
-# set = [1, 1, 2]
-# std_dev = sqrt( ( (1-1.33)^2+(1-1.33)^2+(2-1.33)^2 ) / 2 )
-# std_dev = sqrt( ( .6667 / 2 )
-# std_dev = sqrt( ( .33335 )
-# std_dev = .578
-
+    # 1.33 is the mean
+    # set = [1, 1, 2]
+    # std_dev = sqrt( ( (1-1.33)^2+(1-1.33)^2+(2-1.33)^2 ) / 2 )
+    # std_dev = sqrt( ( .6667 / 2 )
+    # std_dev = sqrt( ( .33335 )
+    # std_dev = .578
     expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq(0.58)
   end
 
-  it 'can return merchants with high item count' do
+  xit 'can return merchants with high item count' do
     expect(@sales_analyst.merchants_with_high_item_count.length).to eq(1)
     expect(@sales_analyst.merchants_with_high_item_count).to eq(@engine.merchants.find_by_id(4))
   end
 
-  it 'can return the average item price for a specific merchant' do
+  xit 'can return the average item price for a specific merchant' do
     expect(@sales_analyst.average_item_price_for_merchant(4)).to eq(16) # => BigDecimal
   end
 
-  it 'can return the average price per merchant' do
+  xit 'can return the average price per merchant' do
     expect(@sales_analyst.average_average_price_per_merchant).to eq(12.67) # => BigDecimal
   end
 
-  it 'can return golden items' do
+  xit 'can return golden items' do
     expect(@sales_analyst.golden_items).to eq([]) # => [<item>, <item>, <item>, <item>]
     # find average item price
     # find the standard deviation of the item price
