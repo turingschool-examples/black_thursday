ea77734957cfac0764498a69622ce3f29fbec3ce
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-04 16:50:58 -0600

    Add create and update methods with testing

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -5,4 +5,16 @@ class Merchant
     @name = data[:name]
     @repo = repo
   end
+
+  def self.create_merchant(attributes)
+    data_hash = {}
+    require "pry"; binding.pry
+    data_hash[:id] = @repo.next_highest_merchant_id
+    data_hash[:name] = attributes[:name]
+    new(data_hash, @repo)
+  end
+
+  def update_merchant(id, attributes)
+    @name = attributes[:name]
+  end
 end
--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -1,22 +1,34 @@
 require_relative 'spec_helper'
 require_relative '../lib/merchant'
+require_relative '../lib/merchant_repository'
 require 'csv'
 
 RSpec.describe do
+  before(:each) do
+    @data = {id: 5, name: 'Turing'}
+    # @repo = double('repo')
+    @repo = MerchantRepository.new('./spec/fixtures/mock_merchants.csv')
+    @merchant = Merchant.new(@data, @repo)
+  end
   it 'exists' do
-    data = {id: 5, name: 'Turing'}
-    repo = double('merchant repo')
-    merchant = Merchant.new(data, repo)
-
-    expect(merchant).to be_a Merchant
+    expect(@merchant).to be_a Merchant
   end
 
   it 'has attributes' do
-    data = {id: 5, name: 'Turing'}
-    repo = double('merchant repo')
-    merchant = Merchant.new(data, repo)
-
-    expect(merchant.id).to eq(5)
-    expect(merchant.name).to eq('Turing')
+    expect(@merchant.id).to eq(5)
+    expect(@merchant.name).to eq('Turing')
   end
+
+  xit 'can create new merchants' do
+    # allow(@merchant).to receive(:repo.next_highest_merchant_id).and_return(6)
+    allow(@repo).to receive(:next_highest_merchant_id).and_return(6)
+    expect(Merchant.create_merchant({name: 'Marla'})).to be_a(Merchant)
+  end
+
+  it 'can update old merchants' do
+    @merchant.update_merchant(5, {name: 'New Turing'})
+    expect(@merchant.id).to eq(5)
+    expect(@merchant.name).to eq('New Turing')
+  end
+
 end
