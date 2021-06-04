17c31cae0d79bf0dc1be47eb575c1258e8034652
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-04 18:07:54 -0400

    Create new method and test for instantiating a SalesAnalyst from Engine

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe do
   describe 'instantiation' do
     it 'creates a new sales engine' do
       se = SalesEngine.from_csv({
-        :items => './data/items.csv',
-        :merchants => './spec/fixtures/merchant_mocks_spec.csv'
+        :items => './spec/fixtures/item_fixtures.csv',
+        :merchants => './spec/fixtures/merchant_fixtures.csv'
         })
 
       expect(se).to be_an_instance_of(SalesEngine)
@@ -20,10 +20,10 @@ RSpec.describe do
   describe 'its methods' do
     it 'can list all merchants' do
       item_repository = ItemRepository.new('./data/items.csv', self).create_repo
-      merchant_repository = MerchantRepository.new('./spec/fixtures/merchant_mocks_spec.csv', self).create_repo
+      merchant_repository = MerchantRepository.new('./spec/fixtures/merchant_fixtures.csv', self).create_repo
       se = SalesEngine.from_csv({
-        :items => './data/items.csv',
-        :merchants => './spec/fixtures/merchant_mocks_spec.csv'
+        :items => './spec/fixtures/item_fixtures.csv',
+        :merchants => './spec/fixtures/merchant_fixtures.csv'
         })
 
       expect(se.merchants) == (merchant_repository)
@@ -31,8 +31,8 @@ RSpec.describe do
 
     it 'can find merchant by id' do
       se = SalesEngine.from_csv({
-        :items => './data/items.csv',
-        :merchants => './spec/fixtures/merchant_mocks_spec.csv'
+        :items => './spec/fixtures/item_fixtures.csv',
+        :merchants => './spec/fixtures/merchant_fixtures.csv'
         })
 
         expect(se.find_merchant_by_id(12334382).name).to eq("Keckenbauer")
@@ -41,19 +41,29 @@ RSpec.describe do
     it 'can list all the items' do
       @item_repository = ItemRepository.new('./data/items.csv', self).create_repo
       se = SalesEngine.from_csv({
-        :items => './data/items.csv',
-        :merchants => './spec/fixtures/merchant_mocks_spec.csv'
+        :items => './spec/fixtures/item_fixtures.csv',
+        :merchants => './spec/fixtures/merchant_fixtures.csv'
         })
         expect(se.items) == (@item_repository)
     end
 
     it 'can find items by id' do
       se = SalesEngine.from_csv({
-        :items => './data/items.csv',
-        :merchants => './spec/fixtures/merchant_mocks_spec.csv'
+        :items => './spec/fixtures/item_fixtures.csv',
+        :merchants => './spec/fixtures/merchant_fixtures.csv'
         })
 
-        expect(se.find_item_by_id(263432069).name).to eq("J Crew plaid fuzz skirt, size 6")
+        expect(se.find_item_by_id(263408574).name).to eq("Adidas Azteca Fußballschuh")
+    end
+
+    it 'can create a sales analyst class' do
+      se = SalesEngine.from_csv({
+        :items => './spec/fixtures/item_fixtures.csv',
+        :merchants => './spec/fixtures/merchant_fixtures.csv'
+        })
+
+      expect(se.analyst).to be_an_instance_of(SalesAnalyst)
+
     end
   end
 end
