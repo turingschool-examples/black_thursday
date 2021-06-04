505c9c30f18a13d3b1133d14b381d6c2b39831c5
Brett Jones <bhjones4545@gmail.com>
2021-06-04 17:01:29 -0600

    Add conversion methods

--- lib/item.rb
+++ lib/item.rb
@@ -9,17 +9,25 @@ class Item
                 :repo
 
   def initialize(info, repo)
-    @id = info['id']
+    @id = info['id'].to_i
     @name = info['name']
     @description = info['description']
-    @unit_price = info['unit_price']
+    @unit_price = info['unit_price'].to_f
     @created_at = info['created_at']
     @updated_at = info['updated_at']
-    @merchant_id = info['merchant_id']
+    @merchant_id = info['merchant_id'].to_i
     @repo = repo
   end
 
   def unit_price_to_dollars
     '%.2f' % "#{@unit_price}"
+  end
+
+  def new_id
+    max_id = @all.max_by do |item|
+      item.id
+    end
+    x = (max_id.id + 1)
+    x
   end
 end
