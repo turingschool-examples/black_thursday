6fce0ce237503a090b30ac18f5caa75f1b0357d5
Caroline Tan <carolinectan3@gmail.com>
2021-06-03 21:09:05 -0400

    Add creation of sales analyst object

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -4,6 +4,7 @@ SimpleCov.start
 require_relative '../lib/sales_engine'
 require_relative '../lib/item_repository'
 require_relative '../lib/merchant_repository'
+require_relative '../lib/sales_analyst'
 
 RSpec.describe SalesEngine do
   it 'exists' do
@@ -25,5 +26,15 @@ RSpec.describe SalesEngine do
 
     expect(se.items).to be_an_instance_of(ItemRepository)
     expect(se.merchants).to be_an_instance_of(MerchantRepository)
+  end
+
+  it 'can initialize sales analyst' do
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+    sales_analyst = se.analyst
+
+    expect(sales_analyst).to be_an_instance_of(SalesAnalyst)
   end
 end
