080480f0bea7e2f2492dbb0398ba6044c4608c81
Matt Kragen <mattdkragen@gmail.com>
2021-06-03 20:27:54 -0700

    Make changes to tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,47 +5,27 @@ RSpec.describe Item do
     before :each do
       @ir = ItemRepository.new('spec/fixtures/items.csv', @sales_engine)
       @sales_engine = SalesEngine.new({items:'spec/fixtures/items.csv', merchants:'spec/fixtures/merchants.csv'})
-      @item = Item.new({
-        :id          => 1,
-        :name        => "Pencil",
-        :description => "You can use it to write things",
-        :unit_price  => BigDecimal(10.99,4),
-        :created_at  => '1993-09-29 11:56:40 UTC',
-        :updated_at  => '2016-01-11 11:51:37 UTC',
-        :merchant_id => 2
-      }, @ir)
+      @ir.generate
+      @item = @ir.all[1]
     end
 
     it 'exists' do
-      expect(@item).to be_a(Item)
+      expect(@item).to be_an_instance_of(Item)
     end
 
     it 'has attributes' do
-      expect(@item.id).to eq(1)
-      expect(@item.name).to eq('Pencil')
-      expect(@item.description).to eq("You can use it to write things")
-      expect(@item.unit_price).to eq(BigDecimal(10.99,4))
-      expect(@item.created_at).to eq('1993-09-29 11:56:40 UTC')
-      expect(@item.updated_at).to eq('2016-01-11 11:51:37 UTC')
-      expect(@item.merchant_id).to eq(2)
+      # expect(@item.id).to eq(1)
+      expect(@item.name).to eq('pencils')
+      expect(@item.description).to eq("You can write with them")
+      expect(@item.unit_price).to eq("12")
+      expect(@item.created_at).to eq("2011-02-11 07:44:36 UTC")
+      expect(@item.updated_at).to eq("2004-03-24 10:25:11 UTC")
+      expect(@item.merchant_id).to eq("123346512")
     end
-  end
 
-  context 'methods' do
     it 'returns the price of the item in dollars formatted as a Float' do
-      ir = ItemRepository.new('spec/fixtures/items.csv', sales_engine)
-      sales_engine = SalesEngine.new({items:'spec/fixtures/items.csv', merchants:'spec/fixtures/merchants.csv'})
-      item = Item.new({
-        :id          => 1,
-        :name        => "Pencil",
-        :description => "You can use it to write things",
-        :unit_price  => BigDecimal(10.99,4),
-        :created_at  => '1993-09-29 11:56:40 UTC',
-        :updated_at  => '2016-01-11 11:51:37 UTC',
-        :merchant_id => 2
-      }, ir)
-
-      expect(item.unit_price_to_dollars).to eq("$10.99")
+       item3 = @ir.all[9]
+      expect(item3.unit_price_to_dollars).to eq("$10.99")
     end
   end
 end
