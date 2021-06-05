8010a176a259f24fe2f7e5acb900c36115e5777c
Miriam Aceves <miriammerari02@gmail.com>
2021-06-05 18:57:54 -0400

    Update create and update methods and spes tests

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 class Item
   attr_reader :id,
               :name,
@@ -12,9 +14,9 @@ class Item
     @id = item_info[:id].to_i
     @name = item_info[:name]
     @description = item_info[:description]
-    @unit_price = item_info[:unit_price]
-    @created_at = item_info[:created_at]
-    @updated_at = item_info[:updated_at]
+    @unit_price = BigDecimal(item_info[:unit_price]) / 100
+    @created_at = Time.parse(item_info[:created_at])
+    @updated_at = Time.parse(item_info[:updated_at])
     @merchant_id = item_info[:merchant_id].to_i
     @repo = repo
   end
@@ -23,21 +25,22 @@ class Item
     unit_price.to_f
   end
 
-  def create
-    biggest = @all.max_by do |item|
-      item.id
-    end
-    item[:id] = biggest.id + 1
+  def self.create(attributes, repo)
+    item = Hash.new
+    time = Time.now.utc.strftime("%m-%d-%Y %H:%M:%S %Z")
+    item[:id] = repo.new_id_number
     item[:name] = attributes[:name]
     item[:description] = attributes[:description]
     item[:unit_price] = attributes[:unit_price]
-    item[:created_at] = Time.now
+    item[:created_at] = time
+    item[:updated_at] = time
+    new(item, repo)
   end
 
-  def update(item)
-    @name = item[:name]
-    @description = item[:description]
-    @unit_price = item[:unit_price]
+  def update(attributes)
+    @name = attributes[:name]
+    @description = attributes[:description]
+    @unit_price = attributes[:unit_price]
     @updated_at = Time.now
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,101 +4,66 @@ require 'csv'
 require 'bigdecimal'
 
 RSpec.describe Item do
-  it 'exists' do
-    i = Item.new({
+  before :each do
+    @time = Time.now.utc.strftime("%m-%d-%Y %H:%M:%S %Z")
+    @repo = double('repo')
+    @i = Item.new({
       :id          => 1,
       :name        => "Pencil",
       :description => "You can use it to write things",
-      :unit_price  => BigDecimal(10.99,4),
-      :created_at  => "2021-06-01 18:37:52.006093 -0400",
-      :updated_at  => Time.now,
+      :unit_price  => 1099,
+      :created_at  => @time,
+      :updated_at  => @time,
       :merchant_id => 2
-      }, nil)
-    expect(i).to be_a(Item)
+      }, @repo)
+  end
+
+
+  it 'exists' do
+
+    expect(@i).to be_a(Item)
   end
 
   it 'has attributes' do
-    i = Item.new({
-      :id          => 1,
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal(10.99,4),
-      :created_at  => "2021-06-01 18:37:52.006093 -0400",
-      :updated_at  => Time.now,
-      :merchant_id => 2
-      }, nil)
 
-
-    expect(i.id).to eq(1)
-    expect(i.name).to eq("Pencil")
-    expect(i.description).to eq("You can use it to write things")
-    expect(i.unit_price).to eq(BigDecimal(10.99,4))
-    expect(i.created_at).to eq("2021-06-01 18:37:52.006093 -0400")
-    result = i.updated_at.to_s[0..3]
-    expect(result).to eq("2021")
-    expect(i.merchant_id).to eq(2)
+    expect(@i.id).to eq(1)
+    expect(@i.name).to eq("Pencil")
+    expect(@i.description).to eq("You can use it to write things")
+    expect(@i.unit_price).to eq(0.1099e2)
+    expect(@i.created_at).to eq(Time.parse(@time))
+    expect(@i.updated_at).to eq(Time.parse(@time))
+    expect(@i.merchant_id).to eq(2)
   end
 
-  it 'returns unit_price as a float' do
-    i = Item.new({
-      :id          => 1,
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal(10.99,4),
-      :created_at  => "2021-06-01 18:37:52.006093 -0400",
-      :updated_at  => Time.now,
-      :merchant_id => 2
-      }, nil)
+  it 'returns unit_price in dollars' do
 
-    expect(i.unit_price_to_dollars).to eq(10.99)
+    expect(@i.unit_price_to_dollars).to eq(10.99)
   end
 
   it 'can create an item' do
-    i = Item.new({
-      :id          => 1,
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal(10.99,4),
-      :created_at  => "2021-06-01 18:37:52.006093 -0400",
-      :updated_at  => Time.now,
-      :merchant_id => 2
-      }, nil)
+    attributes = {
+      :name => "Marker",
+      :description => "You can write colorfully",
+      :unit_price => BigDecimal(12.00,4),
+    }
 
-      expect(i.id).to eq(1)
-      expect(i.name).to eq("Pencil")
-      expect(i.description).to eq("You can use it to write things")
-      expect(i.unit_price).to eq(0.1099e2)
-      expect(i.merchant_id).to eq(2)
+    allow(@repo).to receive(:new_id_number).and_return(5)
+    expect(Item.create(attributes, @repo)).to be_a(Item)
   end
 
-  it 'can updaet an item' do
-    i = Item.new({
-      :id          => 2,
-      :name        => "Pencil",
-      :description => "You can use it to write things",
-      :unit_price  => BigDecimal(10.99,4),
-      :created_at  => "2021-06-01 18:37:52.006093 -0400",
-      :updated_at  => Time.now,
-      :merchant_id => 3
-      }, nil)
+  it 'can update an item' do
 
-      new_time = Time.now
-
-      i.update({
-        :id          => 2,
+      new_attributes = {
         :name        => "Pen",
-        :description => "You can write things",
-        :unit_price  => BigDecimal(11.99,4),
-        :created_at  => "2021-07-01 18:37:52.006093 -0400",
-        :updated_at  => new_time,
-        :merchant_id => 3
-        })
+        :description => "You can write important things",
+        :unit_price  => BigDecimal(11.99,4)
+        }
 
+      @i.update(new_attributes)
 
-      expect(i.id).to eq(2)
-      expect(i.name).to eq("Pen")
-      expect(i.description).to eq("You can write things")
-      expect(i.unit_price).to eq(0.1199e2)
+      expect(@i.name).to eq("Pen")
+      expect(@i.description).to eq("You can write important things")
+      expect(@i.unit_price).to eq(0.1199e2)
   end
 end
 
