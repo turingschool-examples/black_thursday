d57679bf32746f72fe4088f00b4776d2ac93c685
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-04 20:03:35 -0600

    Build test for returns invoice item by item id number

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -18,9 +18,26 @@ RSpec.describe InvoiceItemRepository do
     end
   end
 
-  it 'returns item by id number' do
+  it 'returns invoice item by id number' do
     expect(@repo.find_by_id(1).item_id).to eq(10)
-    expect(@repo.find_by_id(2).name).to eq(20)
+    expect(@repo.find_by_id(2).item_id).to eq(20)
     expect(@repo.find_by_id(20)).to eq(nil)
   end
+
+  it 'returns invoice item by item id number' do
+    ids = []
+    @repo.find_all_by_item_id(10).each do |invoice_item|
+      ids << invoice_item.id
+    end
+    expect(ids).to eq([1])
+
+    ids = []
+    @repo.find_all_by_item_id(20).each do |invoice_item|
+      ids << invoice_item.id
+    end
+    expect(ids).to eq([2, 3])
+
+    expect(@repo.find_all_by_item_id(50)).to eq([])
+  end
+
 end
