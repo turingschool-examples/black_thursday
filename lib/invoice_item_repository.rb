b31fd9dffabc0a1432ee3b6d39e6e35c113fe448
Amanda McGuire <aruwart001@gmail.com>
2021-06-05 16:04:31 -0600

    Create invoice item repository and class

---
+++ lib/invoice_item_repository.rb
@@ -0,0 +1,65 @@
+require 'csv'
+require 'bigdecimal'
+require_relative '../lib/invoice_item'
+
+class InvoiceItemRepository
+  attr_reader :all
+
+  def initialize(path)
+    @all = []
+    create_items(path)
+  end
+
+  def create_items(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |row|
+      invoice_item = InvoiceItem.new(row)
+      @all << invoice_item
+    end
+  end
+
+  def inspect
+    "#<#{self.class} #{@invoice_items.size} rows>"
+  end
+
+  def find_by_id(id)
+    @all.find do |invoice_item|
+      invoice_item.id == id
+    end
+  end
+
+  def find_all_by_item_id(item_id)
+    @all.find_all do |invoice_item|
+      invoice_item.item_id == item_id
+    end
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    @all.find_all do |invoice_item|
+      invoice_item.invoice_id == invoice_id
+    end
+  end
+
+  def create(attributes)
+    new_id = @all.max_by do |invoice_item|
+      invoice_item.id
+    end
+
+    attributes[:id] = new_id.id + 1
+
+    invoice_item = InvoiceItem.new(attributes)
+    @all << invoice_item
+    invoice_item
+  end
+
+  def update(id, attributes)
+    invoice_item = find_by_id(id)
+    if !invoice_item.nil?
+      invoice_item.update(attributes)
+    end
+  end
+
+  def delete(id)
+    delete_item = find_by_id(id)
+    @all.delete(delete_item)
+  end
+end
---
+++ spec/invoice_item_repository_spec.rb
@@ -0,0 +1,63 @@
+require 'SimpleCov'
+SimpleCov.start
+
+require_relative '../lib/invoice_item_repository'
+require_relative '../lib/invoice_item'
+
+RSpec.describe InvoiceItemRepository do
+  before(:each) do
+    @iir = InvoiceItemRepository.new('./spec/fixture_files/invoice_item_fixture.csv')
+  end
+
+  it 'exists' do
+    expect(@iir).to be_an_instance_of(InvoiceItemRepository)
+  end
+
+  it 'can create invoice item object' do
+    expect(@iir.all[0]).to be_an_instance_of(InvoiceItem)
+  end
+
+  it 'returns a list of invoice items' do
+    expect(@iir.all.length).to eq(7)
+  end
+
+  it 'can find invoice item by id' do
+    expect(@iir.find_by_id(2).item_id).to eq(1)
+  end
+
+  it 'can find all invoice items by item id' do
+    expect(@iir.find_all_by_item_id(5).length).to eq(1)
+    expect(@iir.find_all_by_item_id(1).length).to eq(2)
+  end
+
+  it 'can find all invoice items by invoice id' do
+    expect(@iir.find_all_by_invoice_id(1).length).to eq(1)
+    expect(@iir.find_all_by_invoice_id(2).length).to eq(3)
+  end
+
+  it 'can create a new invoice item object' do
+    data = @iir.create({
+                        :item_id => 4,
+                        :invoice_id => 4,
+                        :quantity => 250,
+                        :unit_price => 1875
+                      })
+
+    expect(@iir.all.length).to eq(8)
+    expect(data.id).to eq(8)
+  end
+
+  it 'can update invoice item attributes by id' do
+    data = { :quantity => 5000 }
+    @iir.update(4, data)
+
+    expect(@iir.find_by_id(4).quantity).to eq(5000)
+    expect(@iir.find_by_id(4).unit_price_to_dollars).to eq(20.00)
+  end
+
+  it 'can delete invoice item by id' do
+    @iir.delete(6)
+
+    expect(@iir.all.length).to eq(6)
+  end
+end
