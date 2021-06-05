9e943b6b7492a1c86d6a23c9fe4c3de7de2eb133
Taylor Varoglu <tava8398@colorado.edu>
2021-06-05 16:45:55 -0600

    Update comments in SalesAnalyst to test push after remote url rename

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -18,47 +18,47 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant_standard_deviation
-    # stuff
+    # do stuff
   end
 
   def merchants_with_high_item_count
-    # stuff
+    # do stuff
   end
 
   def average_item_price_for_merchant(merchant_id)
-    # stuff
+    # do stuff
   end
 
   def average_average_price_per_merchant
-    # stuff
+    # do stuff
   end
 
   def golden_items
-    # stuff
+    # do stuff
   end
 
   def average_invoices_per_merchant
-    # stuff
+    # do stuff
   end
 
   def average_invoices_per_merchant_standard_deviation
-    # stuff
+    # do stuff
   end
 
   def top_merchants_by_invoice_count
-    # stuff
+    # do stuff
   end
 
   def bottom_merchants_by_invoice_count
-    # stuff
+    # do stuff
   end
 
   def top_days_by_invoice_count
-    # stuff
+    # do stuff
   end
 
   def invoice_status
-    # stuff
+    # do stuff
   end
 
 end
