70779c558898cec9dccc6ac32f19cab91c7e3af2
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-04 19:49:30 -0600

    Build create invoice items method

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -2,9 +2,16 @@ require 'csv'
 require_relative 'invoice_item'
 
 class InvoiceItemRepository
+  attr_reader :all
 
   def initialize(path)
     @all = []
-    # create_items(path)
+    create_invoice_items(path)
+  end
+
+  def create_invoice_items(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol).each do |invoice_item|
+      @all << InvoiceItem.new(invoice_item, self)
+    end
   end
 end
