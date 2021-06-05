887e380430f84526a980c5cb26d93484282e38df
Antonio King <antonio.j.king@gmail.com>
2021-06-05 14:21:17 -0600

    Create invoice class

---
+++ lib/invoice.rb
@@ -0,0 +1,17 @@
+class Invoice
+  attr_reader :id,
+              :customer_id,
+              :merchant_id,
+              :status,
+              :created_at,
+              :updated_at
+
+  def initialize(data)
+    @id = data[:id].to_i
+    @customer_id = data[:customer_id].to_i
+    @merchant_id = data[:merchant_id].to_i
+    @status = data[:status].to_s
+    @created_at = data[:created_at].to_s
+    @updated_at = data[:updated_at].to_s 
+  end
+end
