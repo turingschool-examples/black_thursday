473a4e43a75236b218ec0a0910a62ec8163b2919
Brett Jones <bhjones4545@gmail.com>
2021-06-05 12:37:21 -0600

    Add instance variables to sales engine class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,7 +2,9 @@ class SalesEngine
   attr_reader :merchants_repo, :items_repo
   def initialize(paths)
     @items_repo = ItemRepository.new(paths[:items], self)
+    @items_repo.generate
     @merchants_repo = MerchantRepository.new(paths[:merchants], self)
+    @merchants_repo.generate
     # @analyst = SalesAnalyst.new
   end
 
