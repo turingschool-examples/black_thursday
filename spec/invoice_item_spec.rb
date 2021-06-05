25cc86a1ff0303a9b77b507b92b929631330c28d
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-04 19:00:47 -0600

    Build test for exists and attributes

--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -1,16 +1,33 @@
 require_relative 'spec_helper'
 require_relative '../lib/invoice_item'
+require 'bigdecimal'
 
 RSpec.describe InvoiceItem do
   before (:each) do
+    @time = Time.now
     @ii = InvoiceItem.new({
   :id => 6,
   :item_id => 7,
   :invoice_id => 8,
   :quantity => 1,
-  :unit_price => BigDecimal.new(10.99, 4),
-  :created_at => Time.now,
-  :updated_at => Time.now
-  })
+  :unit_price => BigDecimal(10.99, 4),
+  :created_at => @time,
+  :updated_at => @time
+  }, nil)
+  end
+
+  it 'exists' do
+    expect(@ii).to be_a(InvoiceItem)
+  end
+
+  it 'attributes' do
+    expect(@ii.id).to eq(6)
+    expect(@ii.item_id).to eq(7)
+    expect(@ii.invoice_id).to eq(8)
+    expect(@ii.quantity).to eq(1)
+    expect(@ii.unit_price).to eq(BigDecimal(10.99, 4))
+    expect(@ii.created_at).to eq(@time)
+    expect(@ii.updated_at).to eq(@time)
+    expect(@repo).to eq(nil)
   end
 end
