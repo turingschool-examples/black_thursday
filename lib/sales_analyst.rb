1606cca32dbae5bb66e9ef6e00a379db559fed93
Caroline Tan <carolinectan3@gmail.com>
2021-06-04 21:40:50 -0400

    Make style updates from Hound

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -10,26 +10,26 @@ class SalesAnalyst
 
   def merch_items_hash
     merch_items = {}
-
+    # could use merchant as the key instead of the ID in case we need to call upon the object in the future?
     @engine.merchants.all.map do |merchant|
       merch_items[merchant.id] = @engine.items.find_all_by_merchant_id(merchant.id)
-    end # could use merchant as the key instead of the ID in case we need to call upon the object in the future?
+    end
 
     merch_items
   end
 
   def items_by_merch_count
     merch_items_hash.values.map do |item|
+      require "pry"; binding.pry
       item.count
     end
   end
 
   def average_items_per_merchant_standard_deviation
     numerator = items_by_merch_count.sum do |num|
-      (num-average_items_per_merchant)**2
+      (num-average_items_per_merchant) ** 2
     end
-
-    std_dev = Math.sqrt(numerator / 2.0).round(2)
+    Math.sqrt(numerator / 2.0).round(2)
   end
 
   def merchants_with_high_item_count
@@ -72,14 +72,14 @@ class SalesAnalyst
     numerator = item_price_set.sum do |num|
       (num-average_price_per_item)**2
     end
-
-    std_dev = Math.sqrt(numerator / 2.0).round(2)
+    Math.sqrt(numerator / 2.0).round(2)
   end
 
   def golden_items
     @engine.items.all.select do |item|
+      # 28 items > 13.5 mean + 10.86 std dev times 2
       item.unit_price_to_dollars > (average_price_per_item +
-        (average_price_per_item_standard_deviation * 2)) # 28 items > 13.5 mean + 10.86 std dev times 2
+        (average_price_per_item_standard_deviation * 2))
     end
   end
 end
