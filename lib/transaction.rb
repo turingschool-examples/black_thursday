a23bfaaec1112f6c25cda94773100fd7f2133a1e
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-05 16:00:54 -0600

    Update time to convert from string to time as needed

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'time'
 
 class Transaction
   attr_reader :id,
@@ -6,32 +7,42 @@ class Transaction
               :credit_card_number,
               :credit_card_expiration_date,
               :result,
-              :created_at
+              :created_at,
+              :updated_at
   def initialize (data, repo)
     @id = data[:id].to_i
     @invoice_id = data[:invoice_id].to_i
     @credit_card_number = data[:credit_card_number]
     @credit_card_expiration_date = data[:credit_card_expiration_date]
-    @result = data[:result]
-    @created_at = data[:created_at]
+    @result = data[:result].to_sym
+    @created_at = Time.parse(data[:created_at])
+    @updated_at = Time.parse(data[:updated_at])
     @repo = repo
   end
 
   def self.create(attributes, repo)
     data_hash = {}
+    time = Time.now.utc.strftime("%m-%d-%Y %H:%M:%S %Z")
     data_hash[:id] = repo.next_highest_id
     data_hash[:invoice_id] = attributes[:invoice_id].to_i
     data_hash[:credit_card_number] = attributes[:credit_card_number]
     data_hash[:credit_card_expiration_date] = attributes[:credit_card_expiration_date]
     data_hash[:result] = attributes[:result]
-    data_hash[:created_at] = Time.now
-    data_hash[:updated_at] = Time.now
+    data_hash[:created_at] = time
+    data_hash[:updated_at] = time
     new(data_hash, repo)
   end
 
   def update(attributes)
-    @credit_card_number = attributes[:credit_card_number]
-    @credit_card_expiration_date = attributes[:credit_card_expiration_date]
-    @result = attributes[:result]
+    unless attributes[:credit_card_number].nil?
+      @credit_card_number = attributes[:credit_card_number]
+    end
+    unless attributes[:credit_card_expiration_date].nil?
+      @credit_card_expiration_date = attributes[:credit_card_expiration_date]
+    end
+    unless attributes[:result].nil?
+      @result = attributes[:result].to_sym
+    end
+    @updated_at = Time.now
   end
 end
