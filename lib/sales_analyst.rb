a7bd90598ed374534d1c8dbf1f08e49740c57b04
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-04 18:57:12 -0600

    Add mean method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,9 +1,18 @@
 require 'CSV'
 class SalesAnalyst
+  attr_reader :numbers, :engine
 
-
-  def initialize(data, engine)
-    @data = data
+  def initialize(numbers, engine)
+    @numbers = numbers
     @engine = engine
+  end
+
+  def mean
+    mean_array = 0
+    @numbers.each do |number|
+      mean_array += number
+    end
+    mean_array
+    mean = mean_array.to_f / @numbers.length
   end
 end
