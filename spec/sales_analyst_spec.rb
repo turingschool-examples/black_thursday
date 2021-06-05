db46695fc35f91f7fe2007045e6abb309367b1f8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-06-04 20:03:41 -0600

    Add standard deviation method test

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -19,4 +19,13 @@ RSpec.describe 'SalesAnalyst' do
     expect(sales_analyst_1.mean).to eq(9)
     expect(sales_analyst_2.mean).to eq(25.5)
   end
+
+  it 'can calculate standard deviation' do
+    mock_engine = double('Sales Engine')
+    sales_analyst_1 = SalesAnalyst.new([7, 8, 9, 10, 11], mock_engine)
+    sales_analyst_2 = SalesAnalyst.new([2, 45, 23, 32], mock_engine)
+    
+    expect(sales_analyst_1.standard_deviation).to eq(1.58)
+    expect(sales_analyst_2.standard_deviation).to eq(18.08)
+  end
 end
