d96075215dcba9a7e06983b5339ddce5b029dc15
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-04 19:02:10 -0600

    Build attr reader for attributes

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,10 +1,18 @@
 class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at,
+              :repo
 
   def initialize(item, repo)
     @id = item[:id]
     @item_id = item[:item_id]
     @invoice_id = item[:invoice_id]
-    @quantity = item[quantity]
+    @quantity = item[:quantity]
     @unit_price = item[:unit_price]
     @created_at = item[:created_at]
     @updated_at = item[:updated_at]
