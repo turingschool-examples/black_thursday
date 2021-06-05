070b7cc318388f7430b221c9b00397b11f62060c
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-04 19:51:54 -0600

    Build returns invoice item by id number test

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -17,4 +17,10 @@ RSpec.describe InvoiceItemRepository do
       expect(item).to be_a(InvoiceItem)
     end
   end
+
+  it 'returns item by id number' do
+    expect(@repo.find_by_id(1).item_id).to eq(10)
+    expect(@repo.find_by_id(2).name).to eq(20)
+    expect(@repo.find_by_id(20)).to eq(nil)
+  end
 end
