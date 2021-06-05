db791190025719ca893db0aa1f91c480aafdf508
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-05 10:44:55 -0600

    Build test for create new invoice item

--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -5,15 +5,17 @@ require 'bigdecimal'
 RSpec.describe InvoiceItem do
   before (:each) do
     @time = Time.now
-    @ii = InvoiceItem.new({
-  :id => 6,
-  :item_id => 7,
-  :invoice_id => 8,
-  :quantity => 1,
-  :unit_price => BigDecimal(10.99, 4),
-  :created_at => @time,
-  :updated_at => @time
-  }, nil)
+    @repo = double('repo')
+    @attributes = {
+      :id => 6,
+      :item_id => 7,
+      :invoice_id => 8,
+      :quantity => 1,
+      :unit_price => BigDecimal(10.99, 4),
+      :created_at => @time,
+      :updated_at => @time
+      }
+    @ii = InvoiceItem.new(@attributes, @repo)
   end
 
   it 'exists' do
@@ -28,10 +30,22 @@ RSpec.describe InvoiceItem do
     expect(@ii.unit_price).to eq(0.1099e2)
     expect(@ii.created_at).to eq(@time)
     expect(@ii.updated_at).to eq(@time)
-    expect(@repo).to eq(nil)
+    expect(@ii.repo).to eq(@repo)
   end
 
   it '.unit_price_to_dollars' do
     expect(@ii.unit_price_to_dollars).to eq(10.99)
+  end
+
+  it 'can create new invoice items' do
+    attributes = {
+      :item_id => 21,
+      :invoice_id => 3,
+      :quantity => 20,
+      :unit_price => BigDecimal(11.99, 4),
+      }
+
+    allow(@repo).to receive(:new_invoice_item_id).and_return(7)
+    expect(InvoiceItem.create_invoice_item(attributes, @repo)).to be_a(InvoiceItem)
   end
 end
