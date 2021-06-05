d01d7d1f19cd911835670b069a3e9bb5f53be01f
Caroline Tan <carolinectan3@gmail.com>
2021-06-05 19:59:14 -0400

    Add update time and update with attributes methods

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -12,6 +12,21 @@ class Invoice
     @merchant_id = data[:merchant_id].to_i
     @status = data[:status].to_s
     @created_at = data[:created_at].to_s
-    @updated_at = data[:updated_at].to_s 
+    @updated_at = data[:updated_at].to_s
+  end
+
+  def update_time(time)
+    if time.nil?
+      Time.now
+    elsif time.empty?
+      Time.now
+    else
+      Time.parse(time)
+    end
+  end
+
+  def update(attributes)
+    @status = attributes[:status] if !attributes[:status].nil?
+    @updated_at  = update_time("")
   end
 end
