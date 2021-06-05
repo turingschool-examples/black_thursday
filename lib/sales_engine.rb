0ef157d5dd9d48fc4b848f68a61d91139443a9ef
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-05 12:59:16 -0600

    Add invoice items initialze variable

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,15 @@
 require_relative 'item_repository'
 require_relative 'merchant_repository'
+require_relative 'invoice_item_repository'
 require 'csv'
 
 class SalesEngine
-  attr_reader :items, :merchants
+  attr_reader :items, :merchants, :invoice_items
 
   def initialize(path)
     @items = ItemRepository.new(path[:items])
     @merchants = MerchantRepository.new(path[:merchants])
+    @invoice_items = InvoiceItemRepository.new(path[:invoice_items])
   end
 
   def self.from_csv(path)
