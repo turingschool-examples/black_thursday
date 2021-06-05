e2e638d272883d6b6d1ffe71df61485542ebc0ef
Jason Knoll <jason.p.knoll@gmail.com>
2021-06-05 13:48:28 -0700

    Create invoice item class and spec

---
+++ lib/invoice_item.rb
@@ -0,0 +1,43 @@
+require 'time'
+
+class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
+
+  def initialize(data)
+    @id         = data[:id].to_i
+    @item_id    = data[:item_id].to_i
+    @invoice_id = data[:invoice_id].to_i
+    @quantity   = data[:quantity].to_i
+    @unit_price = BigDecimal(data[:unit_price].to_f / 100, 4)
+    @created_at = update_time(data[:created_at].to_s)
+    @updated_at = update_time(data[:updated_at].to_s)
+  end
+
+  def update_time(time)
+    if time.nil?
+      Time.now
+    elsif time.empty?
+      Time.now
+    else
+      Time.parse(time)
+    end
+  end
+
+  def update(attributes)
+    @quantity = attributes[:quantity] if !attributes[:quantity].nil?
+    @unit_price  = BigDecimal(attributes[:unit_price]) if !attributes[:unit_price].nil?
+    @updated_at  = update_time("")
+
+  end
+
+  def unit_price_to_dollars
+    unit_price.to_f
+  end
+
+end
---
+++ spec/invoice_item_spec.rb
@@ -0,0 +1,62 @@
+require 'SimpleCov'
+SimpleCov.start
+
+require_relative '../lib/invoice_item'
+require 'bigdecimal'
+
+RSpec.describe InvoiceItem do
+  before(:each) do
+    @ii = InvoiceItem.new({
+      :id           => 6,
+      :item_id      => 7,
+      :invoice_id   => 8,
+      :quantity     => 1,
+      :unit_price   => BigDecimal(1099, 4),
+      :created_at   => '2021-06-11 09:34:06 UTC',
+      :updated_at   => '2021-06-11 09:34:06 UTC'
+    })
+  end
+
+  it 'Exists' do
+    expect(@ii).to be_an_instance_of(InvoiceItem)
+  end
+
+  it 'Has attributes' do
+    expect(@ii.id).to eq(6)
+    expect(@ii.item_id).to eq(7)
+    expect(@ii.invoice_id).to eq(8)
+    expect(@ii.quantity).to eq(1)
+    expect(@ii.unit_price).to eq(BigDecimal(10.99, 4))
+    expect(@ii.created_at).to eq(Time.parse('2021-06-11 09:34:06 UTC'))
+    expect(@ii.updated_at).to eq(Time.parse('2021-06-11 09:34:06 UTC'))
+  end
+  it 'Can parse time or create time' do
+    expect(@ii.created_at).to be_a(Time)
+    expect(@ii.updated_at).to be_a(Time)
+  end
+  it 'Can update attributes' do
+    @ii.update({
+      :quantity => 2,
+      :unit_price => BigDecimal(25.99, 4)
+      })
+
+    expect(@ii.quantity).to eq(2)
+    expect(@ii.unit_price_to_dollars).to eq(25.99)
+    expect(@ii.updated_at).to be_a(Time)
+
+  end
+  it 'Can change unit price to dollars' do
+    ii = InvoiceItem.new({
+      :id           => 6,
+      :item_id      => 7,
+      :invoice_id   => 8,
+      :quantity     => 1,
+      :unit_price   => BigDecimal(1099),
+      :created_at   => '2021-06-11 09:34:06 UTC',
+      :updated_at   => '2021-06-11 09:34:06 UTC'
+    })
+
+    expect(ii.unit_price).to be_a(BigDecimal)
+    expect(ii.unit_price_to_dollars).to eq(10.99)
+  end
+end
