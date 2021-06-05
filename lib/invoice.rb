54e1cfb3f8bb10e09e575004b38c2b4b4d47427a
Carina Sweet <carina.sweet06@gmail.com>
2021-06-05 08:44:49 -0600

    Update id, customer id, and merchant id to be integer

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -7,9 +7,9 @@ class Invoice
               :updated_at
 
   def initialize(invoice_info, repo)
-    @id          = invoice_info[:id]
-    @customer_id = invoice_info[:customer_id]
-    @merchant_id = invoice_info[:merchant_id]
+    @id          = invoice_info[:id].to_i
+    @customer_id = invoice_info[:customer_id].to_i
+    @merchant_id = invoice_info[:merchant_id].to_i
     @status      = invoice_info[:status]
     @created_at  = invoice_info[:created_at]
     @updated_at  = invoice_info[:updated_at]
