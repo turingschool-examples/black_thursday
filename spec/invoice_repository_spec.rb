ab13ef93e2db934becdfdcc3a7a62b03515c26e5
Matt Kragen <mattdkragen@gmail.com>
2021-06-06 13:35:36 -0700

    Create test for #generate

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe InvoiceRepository do
          :merchants => 'spec/fixtures/merchants.csv',
          :invoices => 'spec/fixtures/invoices.csv'
        })
-      ivr = InvoiceRepository.new('spec/fixtures/invoices.csv')
+      ivr = InvoiceRepository.new('spec/fixtures/invoices.csv', se)
       
-       expect(ivr).to be_a(InvoiceRepository)
+      expect(ivr).to be_a(InvoiceRepository)
     end
   end
 
@@ -21,11 +21,15 @@ RSpec.describe InvoiceRepository do
         :merchants => 'spec/fixtures/merchants.csv',
         :invoices => 'spec/fixtures/invoices.csv'
       })
-      @ivr = InvoiceRepository.new('spec/fixtures/invoices.csv')
+      @ivr = InvoiceRepository.new('spec/fixtures/invoices.csv', @se)
+      @invoice1 = @ivr.all[0]
+      @invoice2 = @ivr.all[-1]
     end
 
-    it '' do
-      
+    it 'generates invoice instances' do
+      @ivr.generate
+      expect(@invoice1.id).to eq(1)
+      expect(@invoice2.id).to eq(50)
     end
   end
 end
