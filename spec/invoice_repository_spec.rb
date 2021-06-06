328d0d73e256dd1265ceeaf815b66187d1efd18d
Sawaez Amin <sawaezamin@gmail.com>
2021-06-05 19:03:44 -0600

    Test instantiation for invoice repo

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -2,17 +2,15 @@ require_relative 'spec_helper'
 
 RSpec.describe InvoiceRepository do
   before :each do
-    invoice_repo = InvoiceRepository.new()
+    @invoice_repo = InvoiceRepository.new()
   end
   describe 'instantiation' do
     it 'exists' do
-
-
-      expect(invoice_repo).to be_an(InvoiceRepository)
+      expect(@invoice_repo).to be_an(InvoiceRepository)
     end
 
-    it 'has attributes' do
-
-    end
+    # it 'has attributes' do
+    #
+    # end
   end
 end
