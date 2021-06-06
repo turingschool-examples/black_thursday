2e3f3e5b25b03c4b1f429518fe882f97eaaf2d50
Matt Kragen <mattdkragen@gmail.com>
2021-06-06 13:35:16 -0700

    Modify methods for Item; Add #generate method for Repo

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -6,12 +6,13 @@ class Invoice
               :created_at,
               :updated_at
 
-  def initialize(info)
-    @id          = info[:id]
-    @customer_id = info[:customer_id]
-    @merchant_id = info[:merchant_id]
+  def initialize(info, repo)
+    @id          = info[:id].to_i
+    @customer_id = info[:customer_id].to_i
+    @merchant_id = info[:merchant_id].to_i
     @status      = info[:status]
     @created_at  = info[:created_at]
     @updated_at  = info[:updated_at]
+    @repo = repo
   end
 end
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,5 +1,16 @@
 class InvoiceRepository
-  def initialize(file_path)
-    
+  attr_reader :sales_engine, :all, :file_path
+  
+  def initialize(file_path, sales_engine)
+    @file_path = file_path
+    @sales_engine = sales_engine
+    @all = generate
+  end
+
+  def generate
+    info = CSV.open(@file_path.to_s, headers: true, header_converters: :symbol)
+    info.map do |row|
+      Invoice.new(row, self)
+    end
   end
 end
