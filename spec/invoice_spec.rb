ff271f5e3442d979799cfbadb37f71eedfa2b1a7
Matt Kragen <mattdkragen@gmail.com>
2021-06-06 12:56:11 -0700

    Create tests for existence and attributes

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -3,7 +3,7 @@ require_relative './spec_helper'
 RSpec.describe Invoice do
   describe 'instantiation' do
     before :each do
-      i = Invoice.new({
+      @i = Invoice.new({
         :id          => 6,
         :customer_id => 7,
         :merchant_id => 8,
@@ -11,6 +11,17 @@ RSpec.describe Invoice do
         :created_at  => Time.now,
         :updated_at  => Time.now,
       })
+    end
+
+    it 'exists' do
+      expect(@i).to be_a(Invoice)
+    end
+
+    it 'has attributes' do
+      expect(@i.id).to eq(6)
+      expect(@i.customer_id).to eq(7)
+      expect(@i.merchant_id).to eq(8)
+      expect(@i.status).to eq('pending')
     end
   end
 end
