9c2d9933d430cc4e9ba74e3bbea7683f3cbf7c93
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-06 19:22:44 -0400

    Update path for initialize test

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -7,7 +7,8 @@ SimpleCov.start
 RSpec.describe InvoiceRepository do
   it 'exists' do
     mock_engine = double("sales_engine mock") #create invoice_fixtures
-    invoice_repo = InvoiceRepository.new('./spec/fixtures/invoice_fixtures.csv')
+    invoice_repo = InvoiceRepository.new('./spec/fixtures/invoice_fixtures.csv', mock_engine)
 
     expect(invoice_repo).to be_an_instance_of(InvoiceRepository)
+  end
 end
