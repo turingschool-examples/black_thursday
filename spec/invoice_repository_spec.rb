346a2c8b230d963c06dbbbb51048de5135fbcb4a
Matt Kragen <mattdkragen@gmail.com>
2021-06-06 13:16:52 -0700

    Refactor InvoiceRepo tests

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -2,17 +2,30 @@ require_relative './spec_helper'
 
 RSpec.describe InvoiceRepository do
   describe 'instantiation' do
+    it 'exists' do
+      se = SalesEngine.new({
+         :items => 'spec/fixtures/items.csv',
+         :merchants => 'spec/fixtures/merchants.csv',
+         :invoices => 'spec/fixtures/invoices.csv'
+       })
+      ivr = InvoiceRepository.new('spec/fixtures/invoices.csv')
+      
+       expect(ivr).to be_a(InvoiceRepository)
+    end
+  end
+
+  describe 'methods' do
     before :each do
       @se = SalesEngine.new({
         :items => 'spec/fixtures/items.csv',
         :merchants => 'spec/fixtures/merchants.csv',
         :invoices => 'spec/fixtures/invoices.csv'
       })
-      @ivr = @InvoiceRepository.new('spec/fixtures/invoices.csv', )
+      @ivr = InvoiceRepository.new('spec/fixtures/invoices.csv')
     end
 
-    it 'exists' do
-      expect(@ivr).to be_a(InvoiceRepository)
+    it '' do
+      
     end
   end
 end
