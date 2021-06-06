3b080c27932e0ebda9d9b098a22619818b23b5cd
Miriam Aceves <miriammerari02@gmail.com>
2021-06-06 19:33:14 -0400

    Create customer repository class with all required methods

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,3 +1,55 @@
+require 'csv'
+require_relative '../lib/customer'
+
 class CustomerRepository
 
+  def initialize(path)
+    @all = []
+    create_customer(path)
+  end
+
+  def create_customer(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |row|
+      @all << Customer.new(row, self)
+    end
+  end
+
+  def find_by_id
+    @all.find do |customer|
+      customer.id == id
+    end
+  end
+
+  def find_all_by_first_name(first_name)
+    @all.find_all do |customer|
+      customer.first_name == first_name
+    end
+  end
+
+  def find_all_by_last_name(last_name)
+    @all.find_all do |customer|
+      customer.last_name == last_name
+    end
+  end
+
+  def create(attributes)
+    new_customer = Customer.create(attributes, self)
+    @all << new_customer
+  end
+
+  def update(id, attributes)
+    unless find_by_id(id).nil?
+      find_by_id(id).update(attributes)
+    end
+  end
+
+  def delete(id)
+    @all.delete(self.find_by_id(id))
+  end
+
+  # :nocov:
+  def inspect
+    "#{self.class} #{@customer.size} rows"
+  end
+  # :nocov:
 end
