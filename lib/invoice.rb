8935341732ee5ad4d268ec21741e389441ee805e
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-06-06 15:23:34 -0600

    Reorder initialize and attr_reader for readability

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -2,24 +2,26 @@ require 'CSV'
 
 class Invoice
   attr_reader :id,
+              :customer_id,
               :merchant_id,
+              :status,
               :created_at,
               :updated_at,
-              :customer_id,
-              :status
+              :repo
 
   def initialize(invoice_data, repo)
     @id = invoice_data[:id].to_i
-    @merchant_id = invoice_data[:merchant_id].to_i
-    @created_at = Time.parse(invoice_data[:created_at].to_s)
-    @updated_at = Time.parse(invoice_data[:updated_at].to_s)
     @customer_id = invoice_data[:customer_id].to_i
-    @repo = repo
+    @merchant_id = invoice_data[:merchant_id].to_i
     @status = invoice_data[:status].to_s
+    @created_at = invoice_data[:created_at]
+    @updated_at = invoice_data[:updated_at]
+    @repo = repo
+
   end
 
   def update_time
-    @updated_at = Time.now
+    
   end
 
   def change_status(status)
