2716a57add2dcf1e296e9088f7ee6e48fd170733
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-05 20:59:09 -0400

    Update sales_analyst and spec initialize arguments

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,24 +1,42 @@
 require 'CSV'
-class SalesAnalyst
-  attr_reader :numbers, :engine
+require_relative 'sales_engine'
 
-  def initialize(numbers, engine)
-    @numbers = numbers
+class SalesAnalyst
+  attr_reader :engine
+
+  def initialize(engine)
     @engine = engine
   end
 
-  def mean
-    mean_array = 0
-    @numbers.each do |number|
-      mean_array += number
-    end
-    mean_array
-    mean = mean_array.to_f / @numbers.length
+  def average(data)
+    data.sum / data.size
   end
 
-  def standard_deviation
-    Math.sqrt(@numbers.sum do |number|
-     ((number - mean) ** 2) / (@numbers.size - 1)
-    end).round(2)
+  def standard_deviation(sample_size)
+    mean = sample_size.sum / sample_size.size.to_r
+    sample_size.map do |sample|
+      (sample - mean) ** 2 / (sample_size.size - 1)
+    end.round(2)
+
   end
+
+  def average_items_per_merchant(merchant_id)
+  end
+
+  def average_items_per_merchant_standard_deviation(merchant_id)
+  end
+  # def mean
+  #   mean_array = 0
+  #   @numbers.each do |number|
+  #     mean_array += number
+  #   end
+  #   mean_array
+  #   mean = mean_array.to_f / @numbers.length
+  # end
+  #
+  # def standard_deviation
+  #   Math.sqrt(@numbers.sum do |number|
+  #    ((number - mean) ** 2) / (@numbers.size - 1)
+  #   end).round(2)
+  # end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -6,15 +6,16 @@ require './lib/sales_analyst'
 RSpec.describe 'SalesAnalyst' do
   it 'exists' do
     mock_engine = double('Sales Engine')
-    sales_analyst = SalesAnalyst.new([7, 8, 9, 10, 11], mock_engine)
+    sales_analyst = SalesAnalyst.new(mock_engine)
 
     expect(sales_analyst).to be_a(SalesAnalyst)
   end
 
   it 'has a mean' do
     mock_engine = double('Sales Engine')
-    sales_analyst_1 = SalesAnalyst.new([7, 8, 9, 10, 11], mock_engine)
-    sales_analyst_2 = SalesAnalyst.new([2, 45, 23, 32], mock_engine)
+    sales_analyst_1 = SalesAnalyst.new(mock_engine)
+    sales_analyst_2 = SalesAnalyst.new(mock_engine)
+
 
     expect(sales_analyst_1.mean).to eq(9)
     expect(sales_analyst_2.mean).to eq(25.5)
@@ -24,7 +25,7 @@ RSpec.describe 'SalesAnalyst' do
     mock_engine = double('Sales Engine')
     sales_analyst_1 = SalesAnalyst.new([7, 8, 9, 10, 11], mock_engine)
     sales_analyst_2 = SalesAnalyst.new([2, 45, 23, 32], mock_engine)
-    
+
     expect(sales_analyst_1.standard_deviation).to eq(1.58)
     expect(sales_analyst_2.standard_deviation).to eq(18.08)
   end
