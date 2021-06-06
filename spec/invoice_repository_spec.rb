dc37fe5d9adfe8fcb922a22e7e9c76272dd354af
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-06-06 16:15:12 -0600

    Create invoice repo spec test file

---
+++ spec/invoice_repository_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/invoice_repository'
+require './lib/sales_engine'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.describe InvoiceRepository do
+  it 'exists' do
+    mock_engine = double("sales_engine mock") #create invoice_fixtures
+    invoice_repo = InvoiceRepository.new('./spec/fixtures/invoice_fixtures.csv')
+
+    expect(invoice_repo).to be_an_instance_of(InvoiceRepository)
+end
