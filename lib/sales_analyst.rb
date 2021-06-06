fab9a23e345cdf460af3645985339bc3e58b34ca
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-06-06 14:28:34 -0400

    Create methods IAW iteration 1, refactor previous attempt

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,4 +1,5 @@
 require 'CSV'
+require 'bigdecimal'
 require_relative 'sales_engine'
 
 class SalesAnalyst
@@ -9,34 +10,70 @@ class SalesAnalyst
   end
 
   def average(data)
-    data.sum / data.size
+    data.sum / data.size.to_f
   end
 
   def standard_deviation(sample_size)
-    mean = sample_size.sum / sample_size.size.to_r
-    sample_size.map do |sample|
-      (sample - mean) ** 2 / (sample_size.size - 1)
-    end.round(2)
-
+    Math.sqrt(sample_size.sum do |sample|
+      (sample - average(sample_size)) ** 2 / (sample_size.size - 1)
+    end).round(2)
   end
 
-  def average_items_per_merchant(merchant_id)
+  def items_per_merchant
+      all_items = @engine.items
+      all_merchants = @engine.merchants
+      all_merchants.all.map do |merchant|
+        all_items.find_all_by_merchant_id(merchant.id).count
+      end
   end
 
-  def average_items_per_merchant_standard_deviation(merchant_id)
+  def average_items_per_merchant
+    average(items_per_merchant)
   end
-  # def mean
-  #   mean_array = 0
-  #   @numbers.each do |number|
-  #     mean_array += number
-  #   end
-  #   mean_array
-  #   mean = mean_array.to_f / @numbers.length
-  # end
-  #
-  # def standard_deviation
-  #   Math.sqrt(@numbers.sum do |number|
-  #    ((number - mean) ** 2) / (@numbers.size - 1)
-  #   end).round(2)
-  # end
+
+  def average_items_per_merchant_standard_deviation
+    standard_deviation(items_per_merchant)
+  end
+
+  def high_item_count
+    standard_deviation(items_per_merchant) + average_items_per_merchant
+  end
+
+  def merchants_with_high_item_count
+    high_item_merchs = []
+    all_items = @engine.items
+    all_merchants = @engine.merchants.all
+    all_merchants.each do |merchant|
+      if all_items.find_all_by_merchant_id(merchant.id).count >= high_item_count
+        high_item_merchs << merchant
+      end
+    end
+    high_item_merchs
+  end
+
+  def average_item_price_for_merchant(merchant_id)
+    unit_prices = []
+    all_items = @engine.items
+    (all_items.find_all_by_merchant_id(merchant_id)).each do |item|
+      unit_prices << item.unit_price
+    end
+    average(unit_prices)
+  end
+
+  def average_average_price_per_merchant
+    all_merchants = @engine.merchants.all
+    all_merchant_averages = []
+    all_merchants.each do |merchant|
+      all_merchant_averages << average_item_price_for_merchant(merchant.id)
+    end
+    average(all_merchant_averages).round(2)
+  end
+
+  def golden_items
+    all_items_unit_prices = @engine.items.all.map { |item| item.unit_price }
+    formula = (standard_deviation(all_items_unit_prices) * 2) + average(all_items_unit_prices)
+    @engine.items.all.find_all do |item|
+      item.unit_price >= formula
+    end
+  end
 end
