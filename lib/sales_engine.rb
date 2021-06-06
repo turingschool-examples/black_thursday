6eb783bed6010cf5f66e92799f9794c615d895fc
Caroline Tan <carolinectan3@gmail.com>
2021-06-05 22:34:40 -0400

    Link invoice/invoice repo to sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,15 +1,15 @@
 require_relative '../lib/item_repository'
 require_relative '../lib/merchant_repository'
 require_relative '../lib/sales_analyst'
+require_relative '../lib/invoice_repository'
 
 class SalesEngine
-  attr_reader :items,
-              :merchants,
-              :analyst
+  attr_reader :items, :merchants, :analyst, :invoices
 
   def initialize(paths)
     @items     = ItemRepository.new(paths[:items])
     @merchants = MerchantRepository.new(paths[:merchants])
+    @invoices  = InvoiceRepository.new(paths[:invoices])
     @analyst   = SalesAnalyst.new(self)
   end
 
