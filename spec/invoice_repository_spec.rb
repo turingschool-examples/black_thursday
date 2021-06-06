38c30f9ef958f7f58488f1835a81c409a2afb3e8
Matt Kragen <mattdkragen@gmail.com>
2021-06-06 13:09:06 -0700

    Create existence test for InvoiceRepository

---
+++ spec/invoice_repository_spec.rb
@@ -0,0 +1,18 @@
+require_relative './spec_helper'
+
+RSpec.describe InvoiceRepository do
+  describe 'instantiation' do
+    before :each do
+      @se = SalesEngine.new({
+        :items => 'spec/fixtures/items.csv',
+        :merchants => 'spec/fixtures/merchants.csv',
+        :invoices => 'spec/fixtures/invoices.csv'
+      })
+      @ivr = @InvoiceRepository.new('spec/fixtures/invoices.csv', )
+    end
+
+    it 'exists' do
+      expect(@ivr).to be_a(InvoiceRepository)
+    end
+  end
+end
