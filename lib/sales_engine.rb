863b908102e5060fabf8309ceb4777864a91fd84
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-06-06 12:02:47 -0600

    Add invoice repo inside initialize and create invoice methods

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,15 @@
 require 'CSV'
 require_relative 'merchant_repository'
 require_relative 'item_repository'
+require_relative 'invoice_repository'
 
 class SalesEngine
-  attr_reader :items, :merchants
+  attr_reader :items, :merchants, :invoices
 
   def initialize(file_location)
     @item_repository = ItemRepository.new(file_location[:items], self).create_repo
     @merchant_repository = MerchantRepository.new(file_location[:merchants], self).create_repo
+    @invoice_repository = InvoiceRepository.new(file_location[:invoices], self).create_repo
   end
 
   def self.from_csv(file_location)
@@ -33,5 +35,13 @@ class SalesEngine
   def analyst
     SalesAnalyst.new(self)
   end
-  
+
+  def invoices
+    @invoice_repository
+  end
+
+  def find_invoice_by_id(id)
+    @invoice_repository.find_by_id(id)
+  end
+
 end
