a15c5a3370d7c49165374349d616098704a4259b
Antonio King <antonio.j.king@gmail.com>
2021-06-07 14:06:00 -0600

    Replace time method in invoice class with module

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -9,7 +9,7 @@ class Invoice
               :updated_at
 
   include Timeable
-  
+
   def initialize(data)
     @id          = data[:id].to_i
     @customer_id = data[:customer_id].to_i
@@ -17,10 +17,6 @@ class Invoice
     @status      = data[:status].to_sym
     @created_at  = update_time(data[:created_at].to_s)
     @updated_at  = update_time(data[:updated_at].to_s)
-  end
-
-  def update_time(time)
-    time_module(time)
   end
 
   def update(attributes)
