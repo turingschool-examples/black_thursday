0f58d74fb5f2bc0b50b280be9a5af6527f3eb86b
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-07 10:20:32 -0600

    Start total revenue by date

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,5 +1,9 @@
 require 'csv'
+require_relative 'invoice_items'
 
 class SalesAnalyst
-  
+
+  def total_revenue_by_date(date)
+    
+  end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe SalesAnalyst do
     expect(@sa).to be_a(SalesAnalyst)
   end
 
+  it 'finds the total revenue by date' do
+    @ii_repo = InvoiceItemRepository.new('./spec/fixtures/mock_analyst_invoice_items.csv')
+    expect(@sa.total_revenue_by_date('2012-3-27')).to eq(2538467)
+  end
+
 end
