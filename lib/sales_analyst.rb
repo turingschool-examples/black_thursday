a01fc349b1f389f9f60db242de49c7e465a8ab8f
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-07 16:29:29 -0600

    Build method for average items per merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,5 +1,12 @@
 require 'csv'
+require_relative 'sales_engine'
 
 class SalesAnalyst
-  
+  def initialize(sales_engine)
+    @se = sales_engine
+  end
+
+  def average_items_per_merchant
+    (@se.item_repo_total_items.to_f / @se.item_repo_total_merchants).round(2)
+  end
 end
