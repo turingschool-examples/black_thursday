8a8d8327c541303ace162fde9400a86f73a4b55b
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-07 10:35:23 -0600

    Add file paths

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,9 +1,14 @@
 require 'csv'
-require_relative 'invoice_items'
+require_relative 'sales_engine'
 
 class SalesAnalyst
+  def initialize(sales_engine)
+    @se = sales_engine
+  end
 
   def total_revenue_by_date(date)
-    
+    @se.invoice_items.find_unit_price_by_date(date).sum do |invoice_items|
+      invoice_items.unit_price
+    end
   end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -6,7 +6,12 @@ require 'csv'
 
 RSpec.describe SalesAnalyst do
   before(:each) do
-    @sa = SalesAnalyst.new
+    @se = SalesEngine.new({items: './spec/fixtures/mock_items.csv',
+                          merchants: './spec/fixtures/mock_analyst_merchants.csv',
+                          invoices: './spec/fixtures/mock_invoices.csv',
+                          invoice_items: './spec/fixtures/mock_analyst_invoice_items.csv',
+                          transactions: './spec/fixtures/mock_transactions.csv',})
+    @sa = @se.analyst
   end
 
   it 'exists' do
@@ -14,7 +19,6 @@ RSpec.describe SalesAnalyst do
   end
 
   it 'finds the total revenue by date' do
-    @ii_repo = InvoiceItemRepository.new('./spec/fixtures/mock_analyst_invoice_items.csv')
     expect(@sa.total_revenue_by_date('2012-3-27')).to eq(2538467)
   end
 
