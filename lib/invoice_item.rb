40e8ccdb5dee2e79b68a6a46fe4b18ba5fe9babc
Brett Jones <bhjones4545@gmail.com>
2021-06-07 15:48:43 -0600

    Add method for #unit_price_to_dollars

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -6,14 +6,18 @@ class InvoiceItem
               :unit_price,
               :created_at,
               :updated_at
-              
+
   def initialize(info)
-    @id = info[:id]
-    @item_id = info[:item_id]
-    @invoice_id = info[:invoice_id]
-    @quantity = info[:quantity]
-    @unit_price = info[:unit_price]
-    @created_at = info[:created_at]
-    @updated_at = info[:updated_at]
+    @id = info[:id].to_i
+    @item_id = info[:item_id].to_i
+    @invoice_id = info[:invoice_id].to_i
+    @quantity = info[:quantity].to_i
+    @unit_price = BigDecimal(info[:unit_price], 4)
+    @created_at = Time.parse(info[:created_at])
+    @updated_at = Time.parse(info[:updated_at])
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
   end
 end
