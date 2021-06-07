9e2777aba555d2c0cda8ce3c4dd290b99a262cd2
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-07 16:29:05 -0600

    Build test for average items per merchant

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -3,10 +3,17 @@ require_relative '../lib/item_repository'
 require_relative '../lib/merchant_repository'
 require_relative '../lib/sales_engine'
 require 'csv'
+require 'bigdecimal'
 
 RSpec.describe SalesAnalyst do
   before(:each) do
-    @sa = SalesAnalyst.new
+    @se = SalesEngine.new({items: './spec/fixtures/mock_items.csv',
+                          merchants: './spec/fixtures/mock_merchants.csv',
+                          invoices: './spec/fixtures/mock_invoices.csv',
+                          invoice_items: './spec/fixtures/mock_invoice_items.csv',
+                          transactions: './spec/fixtures/mock_transactions.csv',
+                          customers: './spec/fixtures/mock_customers.csv'})
+    @sa = @se.analyst
   end
 
   it 'exists' do
@@ -14,8 +21,7 @@ RSpec.describe SalesAnalyst do
   end
 
   it '.average_items_per_merchant' do
-    expect(@sa.average_items_per_merchant).to eq()
+    expect(@sa.average_items_per_merchant).to eq(2.08)
   end
-
 
 end
