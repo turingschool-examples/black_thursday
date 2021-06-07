c04b36f66536e74e338f43bd7b5072f9d7b09c86
Carina Sweet <carina.sweet06@gmail.com>
2021-06-07 13:22:57 -0600

    Update item repo tests

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -12,52 +12,46 @@ RSpec.describe ItemRepository do
   end
 
   it 'returns all' do
-    expect(@repo.all.length).to eq(3)
+    expect(@repo.all.length).to eq(50)
     @repo.all.each do |item|
       expect(item).to be_a(Item)
     end
   end
 
   it 'returns item by id number' do
-    expect(@repo.find_by_id(3).name).to eq('baseball')
-    expect(@repo.find_by_id(5).name).to eq('baseball bat')
+    expect(@repo.find_by_id(263396013).name).to eq('Free standing Woden letters')
+    expect(@repo.find_by_id(263397059).name).to eq('Etre ailleurs')
     expect(@repo.find_by_id(20)).to eq(nil)
   end
 
   it 'returns item by name' do
-    expect(@repo.find_by_name('baseball glove').id).to eq(10)
-    expect(@repo.find_by_name('BASEBALL BAT').id).to eq(5)
+    expect(@repo.find_by_name('Free standing Woden letters').id).to eq(263396013)
+    expect(@repo.find_by_name('Etre ailleurs').id).to eq(263397059)
     expect(@repo.find_by_name('baseball hat')).to eq(nil)
   end
 
   it 'returns item by description' do
     item_names = []
-    @repo.find_all_with_description('for baseball').each do |item|
+    @repo.find_all_with_description('Acrylique sur toile et collage.').each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball', 'baseball bat'])
-
-    item_names = []
-    @repo.find_all_with_description('clOTHing').each do |item|
-      item_names << item.name
-    end
-    expect(item_names).to eq(['baseball glove'])
+    expect(item_names).to eq(["Moyenne toile"])
 
     expect(@repo.find_all_with_description('for basketball')).to eq([])
   end
 
   it 'returns items by price' do
     item_names = []
-    @repo.find_all_by_price(0.1e2).each do |item|
+    @repo.find_all_by_price(0.12e2).each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball bat', 'baseball glove'])
+    expect(item_names).to eq(["510+ RealPush Icon Set", "Bootees"])
 
     item_names = []
-    @repo.find_all_by_price(0.5e1).each do |item|
+    @repo.find_all_by_price(0.11e3).each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball'])
+    expect(item_names).to eq(["Antique Rocking Horse"])
 
     expect(@repo.find_all_by_price(0.2e0)).to eq([])
   end
@@ -67,35 +61,35 @@ RSpec.describe ItemRepository do
     @repo.find_all_by_price_in_range(0.0..0.5e1).each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball'])
+    expect(item_names).to eq(["A Variety of Fragrance Oils for Oil Burners, Potpourri, Resins + More, Lavender, Patchouli, Nag Champa, Rose, Vanilla, White Linen, Angel"])
 
     item_names = []
-    @repo.find_all_by_price_in_range(0.0..0.11e2).each do |item|
+    @repo.find_all_by_price_in_range(0.5e1..0.11e2).each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball', 'baseball bat', 'baseball glove'])
+    expect(item_names).to eq(["Free standing Woden letters", "Kindersocken (Söckchen), hangestrickt, Länge ca.15 cm, beige (eierschalenfarben)"])
 
-    expect(@repo.find_all_by_price_in_range(0.0..0.4e1)).to eq([])
+    expect(@repo.find_all_by_price_in_range(0.0..0.2e1)).to eq([])
   end
 
   it 'returns items by merchant id' do
     item_names = []
-    @repo.find_all_by_merchant_id(20).each do |item|
+    @repo.find_all_by_merchant_id(12334257).each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball bat'])
+    expect(item_names).to eq(['Wooden pen and stand'])
 
     item_names = []
-    @repo.find_all_by_merchant_id(10).each do |item|
+    @repo.find_all_by_merchant_id(12334365).each do |item|
       item_names << item.name
     end
-    expect(item_names).to eq(['baseball', 'baseball glove'])
+    expect(item_names).to eq(["Green Footed Ceramic Bowl", "Spalted Maple Heart Box", "Ironstone Pitcher (Small)", "Antique Rocking Horse"])
 
     expect(@repo.find_all_by_merchant_id(50)).to eq([])
   end
 
   it 'creates new id number' do
-    expect(@repo.new_item_id_number).to eq(11)
+    expect(@repo.new_item_id_number).to eq(263401882)
   end
 
   it 'creates new item instance' do
@@ -106,10 +100,10 @@ RSpec.describe ItemRepository do
       }
     @repo.create(attributes)
 
-    expect(@repo.all.length).to eq(4)
-    expect(@repo.find_by_id(11).name).to eq('jersey')
-    expect(@repo.find_by_id(11).description).to eq('clothing')
-    expect(@repo.find_by_id(11).unit_price).to eq(0.2e0)
+    expect(@repo.all.length).to eq(51)
+    expect(@repo.find_by_id(263401882).name).to eq('jersey')
+    expect(@repo.find_by_id(263401882).description).to eq('clothing')
+    expect(@repo.find_by_id(263401882).unit_price).to eq(0.2e0)
   end
 
   it 'updates item attributes by id' do
@@ -118,24 +112,24 @@ RSpec.describe ItemRepository do
       description: 'for practice',
       unit_price: BigDecimal(20, 4)
     }
-    og_time = @repo.find_by_id(5).updated_at
-    @repo.update(5, new_attributes)
+    og_time = @repo.find_by_id(263401045).updated_at
+    @repo.update(263401045, new_attributes)
 
-    expect(@repo.find_by_id(5).name).to eq('metal bat')
-    expect(@repo.find_by_id(5).description).to eq('for practice')
-    expect(@repo.find_by_id(5).unit_price).to eq(0.2e2)
-    expect(@repo.find_by_id(5).updated_at).to_not eq(og_time)
+    expect(@repo.find_by_id(263401045).name).to eq('metal bat')
+    expect(@repo.find_by_id(263401045).description).to eq('for practice')
+    expect(@repo.find_by_id(263401045).unit_price).to eq(0.2e2)
+    expect(@repo.find_by_id(263401045).updated_at).to_not eq(og_time)
   end
 
   it 'deletes item by id' do
-    @repo.delete(5)
+    @repo.delete(263401045)
 
-    expect(@repo.all.length).to eq(2)
-    expect(@repo.find_by_id(5)).to eq(nil)
+    expect(@repo.all.length).to eq(49)
+    expect(@repo.find_by_id(263401045)).to eq(nil)
   end
 
   it 'can access item unit price to dollars method' do
-    dollars = @repo.find_by_id(3).unit_price_to_dollars
-    expect(dollars).to eq(5)
+    dollars = @repo.find_by_id(263401045).unit_price_to_dollars
+    expect(dollars).to eq(25.0)
   end
 end
