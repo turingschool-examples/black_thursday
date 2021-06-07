84472a6701589102db329872f6a2b1ac3a362929
Antonio King <antonio.j.king@gmail.com>
2021-06-07 14:09:05 -0600

    Replace time method in transaction class with module

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -10,7 +10,7 @@ class Transaction
               :updated_at
 
   include Timeable
-  
+
   def initialize(data)
     @id = data[:id].to_i
     @invoice_id = data[:invoice_id].to_i
@@ -19,10 +19,6 @@ class Transaction
     @result = data[:result].to_sym
     @created_at = update_time(data[:created_at].to_s)
     @updated_at = update_time(data[:updated_at].to_s)
-  end
-
-  def update_time(time)
-    time_module(time)
   end
 
   def update(attributes)
