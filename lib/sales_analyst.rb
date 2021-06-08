4628416d1710354cc8bbfaf4f5eac565d6391183
Carina Sweet <carina.sweet06@gmail.com>
2021-06-07 21:27:56 -0600

    Add methods and tests for invoice repo sales analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -6,24 +6,46 @@ class SalesAnalyst
     @se = sales_engine
   end
 
-  def average_invoice_per_merchant
-    @se.
+  def average_invoices_per_merchant
+    (@se.invoice_repo_total_invoices.to_f / @se.invoice_repo_total_merchants).round(2)
   end
 
   def average_invoices_per_merchant_standard_deviation
+    sum = @se.invoice_repo_invoices_per_merchant.sum do |invoice|
+      (invoice - average_invoices_per_merchant) ** 2
+    end
+    std_dev = Math.sqrt(sum / (@se.invoice_repo_total_merchants - 1))
+    std_dev.round(2)
   end
 
   def top_merchants_by_invoice_count
+    top_merchants = []
+    two_std_dev = average_invoices_per_merchant_standard_deviation * 2
+    @se.invoice_repo_group_by_merchant.each do |merchant, invoices|
+      if invoices.length - two_std_dev > average_invoices_per_merchant
+        top_merchants << @se.merchant_repo_find_by_id(merchant)
+      end
+    end
+    top_merchants
   end
 
   def bottom_merchants_by_invoice_count
+    bottom_merchants = []
+    two_std_dev = average_invoices_per_merchant_standard_deviation * 2
+    @se.invoice_repo_group_by_merchant.each do |merchant, invoices|
+      if invoices.length + two_std_dev < average_invoices_per_merchant
+        bottom_merchants << @se.merchant_repo_find_by_id(merchant)
+      end
+    end
+    bottom_merchants
   end
 
-  def top_days_by_invoice_count
-  end
+  # def top_days_by_invoice_count
+  # end
 
   def invoice_status(status)
-
+    percentage = (@se.invoice_repo_by_status(status) / @se.invoice_repo_total_invoices.to_f) * 100
+    percentage.round(2)
   end
 
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -3,14 +3,46 @@ require_relative '../lib/item_repository'
 require_relative '../lib/merchant_repository'
 require_relative '../lib/sales_engine'
 require 'csv'
+require 'bigdecimal'
 
 RSpec.describe SalesAnalyst do
   before(:each) do
-    @sa = SalesAnalyst.new
+    @se = SalesEngine.new({items:       './spec/fixtures/mock_items.csv',
+                          merchants:    './spec/fixtures/mock_merchants.csv',
+                          invoices:     './spec/fixtures/mock_invoices.csv',
+                          invoice_items: './spec/fixtures/mock_invoice_items.csv',
+                          transactions: './spec/fixtures/mock_transactions.csv',
+                          customers:    './spec/fixtures/mock_customers.csv'})
+
+    @sa = @se.analyst
   end
 
   it 'exists' do
     expect(@sa).to be_a(SalesAnalyst)
+  end
+
+  it 'calculates average invoice per merchant' do
+    expect(@sa.average_invoices_per_merchant).to eq(1.09)
+  end
+
+  it 'calculates the average invoice per merchant standard deviation' do
+    expect(@sa.average_invoices_per_merchant_standard_deviation).to eq(0.28)
+  end
+
+  it 'returns top merchants by invoice count' do
+    expected = @sa.top_merchants_by_invoice_count
+
+    expect(expected.length).to eq(4)
+  end
+
+  it 'returns bottom merchants bu invoice count' do
+    expected = @sa.bottom_merchants_by_invoice_count
+
+    expect(expected.length).to eq(0)
+  end
+
+  it 'returns total invoice by merchant' do
+    expect(@sa.invoice_status(:shipped)).to eq(60.0)
   end
 
 end
