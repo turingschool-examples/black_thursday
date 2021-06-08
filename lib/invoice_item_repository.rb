3d8738040c8536f02ef11c95d42c9929d57fdab9
Brett Jones <bhjones4545@gmail.com>
2021-06-07 19:23:43 -0600

    Add find all methods

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -21,4 +21,16 @@ class InvoiceItemRepository
       invoice_item.id == id
     end
   end
+
+  def find_all_by_item_id(item_id)
+    @all.find_all do |invoice_item|
+      invoice_item.item_id == item_id
+    end
+  end
+
+  def find_all_by_invoice_id(item_id)
+    @all.find_all do |invoice_item|
+      invoice_item.item_id == item_id
+    end
+  end
 end
