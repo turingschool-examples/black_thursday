01a49231e1be8ce4a34d0c2a724f0cfeafdc05fd
Jason Knoll <jason.p.knoll@gmail.com>
2021-06-07 18:35:47 -0700

    Add create_new method in crudable module

--- lib/modules/crudable.rb
+++ lib/modules/crudable.rb
@@ -1,3 +1,14 @@
 module Crudable
+  def create_new(attributes, new_class)
+    new_id = @all.max_by do |object|
+      object.id
+    end
+
+    attributes[:id] = new_id.id + 1
+
+    new_object = new_class.new(attributes)
+    @all << new_object
+    new_object
+  end
 
 end
