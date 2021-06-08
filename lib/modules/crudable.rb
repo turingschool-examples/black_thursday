1b642e502747c81bfbf05e860212f4d0d2c2411a
Caroline Tan <carolinectan3@gmail.com>
2021-06-07 22:14:27 -0400

    Remove extra spacing, double to single quotes

--- lib/modules/crudable.rb
+++ lib/modules/crudable.rb
@@ -3,9 +3,7 @@ module Crudable
     new_id = @all.max_by do |object|
       object.id
     end
-
     attributes[:id] = new_id.id + 1
-
     new_object = new_class.new(attributes)
     @all << new_object
     new_object
@@ -20,5 +18,4 @@ module Crudable
     delete_object = find_by_id(id)
     @all.delete(delete_object)
   end
-
 end
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -13,14 +13,15 @@ require_relative '../lib/sales_analyst'
 RSpec.describe SalesEngine do
   before(:each) do
     @se = SalesEngine.from_csv({
-                               :items         => "./data/items.csv",
-                               :merchants     => "./data/merchants.csv",
-                               :invoices      => "./data/invoices.csv",
-                               :invoice_items => "./data/invoice_items.csv",
-                               :customers     => "./data/customers.csv",
-                               :transactions  => "./data/transactions.csv"
+                               :items         => './data/items.csv',
+                               :merchants     => './data/merchants.csv',
+                               :invoices      => './data/invoices.csv',
+                               :invoice_items => './data/invoice_items.csv',
+                               :customers     => './data/customers.csv',
+                               :transactions  => './data/transactions.csv'
                              })
   end
+
   it 'exists' do
     expect(@se).to be_an_instance_of(SalesEngine)
   end
