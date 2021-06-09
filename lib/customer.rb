aaa7e8c84f6e625b2bb11cc685c8e6271f7e4441
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 19:19:12 -0700

    Add starter for #update_customer method for upcoming repo

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,4 +12,10 @@ class Customer
     @created_at = info[:created_at]
     @updated_at = info[:updated_at]
   end
+
+  # def update_customer(attributes)
+  #   @first_name = attributes[:first_name] unless attributes[:first_name].nil?
+  #   @first_name = attributes[:last_name] unless attributes[:last_name].nil?
+  #   @updated_at = Time.now
+  # end
 end
