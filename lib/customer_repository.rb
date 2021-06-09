8554a69d62e4a199b5c624ff8faecc5a9304b7c8
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 21:12:49 -0700

    Add find_all method by first name

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -23,4 +23,10 @@ class CustomerRepository
       customer.id == id
     end
   end
+
+  def find_all_by_first_name(first_name)
+    @all.find_all do |customer|
+      customer.first_name.downcase.include?(first_name.downcase)
+    end
+  end
 end
