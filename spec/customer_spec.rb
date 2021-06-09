852884a7c93c644e1a066e07631384e7d830b1cf
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 19:04:12 -0700

    Create existence test

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,13 +3,15 @@ require_relative './spec_helper'
 
 RSpec.describe Customer do
   describe 'instantiation' do
-    c = Customer.new({
-    :id         => 6,
-    :first_name => "Joan",
-    :last_name  => "Clarke",
-    :created_at => Time.now,
-    :updated_at => Time.now
-    })
-    expect(c).to be_a(Customer)
+    it 'exists' do
+      c = Customer.new({
+      :id         => 6,
+      :first_name => "Joan",
+      :last_name  => "Clarke",
+      :created_at => Time.now,
+      :updated_at => Time.now
+      })
+      expect(c).to be_a(Customer)
+    end
   end
 end
