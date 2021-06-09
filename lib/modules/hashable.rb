89811f77a03875452f5bfee398b34abd8f7d3678
Amanda McGuire <aruwart001@gmail.com>
2021-06-09 12:24:11 -0600

    update methods for sales analyst

--- lib/modules/hashable.rb
+++ lib/modules/hashable.rb
@@ -2,7 +2,7 @@ module Hashable
   def merch_items_hash
     merch_items = {}
     @engine.merchants.all.map do |merchant|
-      merch_items[merchant.id] = @engine.items.find_all_by_merchant_id(merchant.id)
+      merch_items[merchant] = @engine.items.find_all_by_merchant_id(merchant.id)
     end
     merch_items
   end
@@ -10,9 +10,37 @@ module Hashable
   def merch_invoices_hash
     merch_invoices = {}
     @engine.merchants.all.map do |merchant|
-      merch_invoices[merchant.id] = @engine.invoices.find_all_by_merchant_id(merchant.id)
+      merch_invoices[merchant] = @engine.invoices.find_all_by_merchant_id(merchant.id)
     end
     merch_invoices
+  end
+
+  def revenue_by_invoice_hash
+    revenue_invoice = {}
+    @engine.invoices.all.map do |invoice|
+      if invoice_paid_in_full?(invoice.id) == false
+        revenue_invoice[invoice] = 0.0
+      else
+        revenue_invoice[invoice] = @engine.invoice_items.find_revenue_by_invoice_id(invoice.id)
+      end
+    end
+    revenue_invoice
+  end
+
+  def revenue_by_merchant_hash
+    merch_revenue = {}
+    merch_invoices_hash.map do |merchant, invoices|
+      merch_revenue[merchant] = invoices.sum do |invoice|
+        revenue_by_invoice_hash[invoice]
+      end
+    end
+    merch_revenue
+  end
+
+  def merchants_with_one_item_hash
+    merch_items_hash.select do |merch, item|
+      item.length == 1
+    end
   end
 
   def days_invoices_hash
@@ -25,14 +53,6 @@ module Hashable
                      'Friday'    => pull_day_from_invoice.count(5),
                      'Saturday'  => pull_day_from_invoice.count(6),
                   }
-  end
-
-  def revenue_by_invoice_hash
-    revenue_invoice = {}
-    invoice_id_with_successful_payments.map do |id|
-      revenue_invoice[id] = @engine.invoice_items.find_revenue_by_invoice_id(id)
-    end
-    revenue_invoice
   end
 
   def month_name_to_number_hash
@@ -50,31 +70,5 @@ module Hashable
                       'November' => 12,
                       'December' => 12
                     }
-  end
-
-  def invoice_id_by_merchant_id_hash
-    merchid_invid = Hash.new { |hash, key| hash[key] = []}
-    merch_invoices_hash.each do |merchant, invoices|
-      invoices.map do |invoice|
-        merchid_invid[merchant] << invoice.id
-      end
-    end
-    merchid_invid
-  end
-
-  def revenue_by_merchant_id_hash
-    merch_revenue = {}
-    invoice_id_by_merchant_id_hash.each do |merchant, invoices|
-      merch_revenue[merchant] = invoices.sum do |inv|
-        revenue_by_invoice_hash[inv]
-      end
-    end
-    merch_revenue
-  end
-
-  def merchants_with_one_item_hash
-    merch_items_hash.select do |merch, item|
-      item.count == 1
-    end
   end
 end
