0646f80ce40d8040c1a015387d2474adc8e4ce85
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 18:54:48 -0700

    Create customer spec file and path in spec_helper; add fixture file

---
+++ spec/customer_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require_relative './spec_helper'
+
+RSpec.describe Customer do
+  describe 'instantiation' do
+    c = Customer.new({
+    :id         => 6,
+    :first_name => "Joan",
+    :last_name  => "Clarke",
+    :created_at => Time.now,
+    :updated_at => Time.now
+    })
+    expect(c).to be_a(Customer)
+  end
+end
