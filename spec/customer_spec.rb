5fdefc09a84cb90cff965feb42384c48ddc1c858
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 19:16:26 -0700

    Add attribute test and starter for repo update method

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,15 +3,49 @@ require_relative './spec_helper'
 
 RSpec.describe Customer do
   describe 'instantiation' do
-    it 'exists' do
-      c = Customer.new({
+    before :each do
+      @c = Customer.new({
       :id         => 6,
       :first_name => "Joan",
       :last_name  => "Clarke",
-      :created_at => Time.now,
-      :updated_at => Time.now
+      :created_at => Time.now.to_s,
+      :updated_at => Time.now.to_s
       })
-      expect(c).to be_a(Customer)
+    end
+
+    it 'exists' do
+      expect(@c).to be_a(Customer)
+    end
+
+    it 'has attributes' do
+      expect(@c.id).to eq(6)
+      expect(@c.first_name).to eq("Joan")
+      expect(@c.last_name).to eq("Clarke")
     end
   end
+
+  # describe 'methods' do
+  #   it 'updates invoice status' do
+  #     se = SalesEngine.new({
+  #        :customers => 'spec/fixtures/customers.csv',
+  #     })
+  #     cr = CustomerRepository.new('spec/fixtures/customers.csv', se)
+  #     c = Customer.new({
+  #       :id         => 6,
+  #       :first_name => "Joan",
+  #       :last_name  => "Clarke",
+  #       :created_at => Time.now.to_s,
+  #       :updated_at => Time.now.to_s
+  #     })
+  #     attributes = {
+  #       :first_name => "Jeff",
+  #       :last_name => "Jefferson"
+  #     }
+
+  #     c.update_customer(attributes)
+  #     expect(c.id).to eq(6)
+  #     expect(c.first_name).to eq("Jeff")
+  #     expect(c.last_name).to eq("Jefferson")
+  #   end
+  # end
 end
