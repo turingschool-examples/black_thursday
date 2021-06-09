c0eef4b8bdec1b852cdcd8dd0f94da21ef4e0290
danembb <dbrophy720@gmail.com>
2021-06-09 00:32:38 -0600

    Test attributes for customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,7 +6,7 @@ class Customer
               :updated_at
 
   def initialize(customer_data)
-    @id = customer_data[:id].to_i
+    @id = customer_data[:id]
     @first_name = customer_data[:first_name]
     @last_name = customer_data[:last_name]
     @created_at = customer_data[:created_at]
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -15,5 +15,14 @@ RSpec.describe Customer do
     it 'exists' do
       expect(@customer).to be_a(Customer)
     end
+
+    it 'has attributes' do
+      expect(@customer_data).to be_a(Hash)
+      expect(@customer.id).to eq(6)
+      expect(@customer.first_name).to eq("Joan")
+      expect(@customer.last_name).to eq("Clarke")
+      expect(@customer.created_at).to be_a(Time)
+      expect(@customer.updated_at).to be_a(Time)
+    end
   end
 end
