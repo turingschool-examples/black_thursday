ad8d668b66d129ee65e2767bad4604af9be6125d
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-13 19:03:46 -0400

    Add initialize method to sales engine class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,4 +1,16 @@
-
+require 'csv'
+require './lib/item_repository'
+require './lib/merchant_repository'
 
 class SalesEngine
+  attr_reader :item_path, :merchant_path
+  def self.from_csv(path)
+    item_path = path[:items]
+    merchant_path = path[:merchants]
+  end
+
+  def initialize
+    @item_repository
+    @merchant_repository
+  end
 end
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe SalesEngine do
       se = SalesEngine.new
       expect(se).to be_an_instance_of(SalesEngine)
     end
+
+    it 'can access items' do
+      se = SalesEngine.new(paths[:items])
+      expect(se).to be_an(Array)
+    end
+
+    it 'can access merchants' do
+      se = SalesEngine.new(paths[:merchants])
+      expect(se).to be_an(Array)
+    end
   end
-end 
+end
