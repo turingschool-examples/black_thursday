b04149b6427d9aa3631828532ceee9b307f6ca7f
Sam Devine <samuel.will.devine@gmail.com>
2021-09-13 16:22:18 -0600

    Add Merchant and Merchant Spec

---
+++ lib/merchant.rb
@@ -0,0 +1,9 @@
+class Merchant
+  attr_reader :id,
+              :name
+
+  def initialize(data)
+    @id   = data[:id]
+    @name = data[:name]
+  end
+end
---
+++ spec/merchant_spec.rb
@@ -0,0 +1,18 @@
+require './lib/merchant.rb'
+
+RSpec.describe Merchant do
+  it 'exists' do
+    data = {id: 5, name: "Turing School"}
+    merchant = Merchant.new(data)
+
+    expect(merchant).to be_a Merchant
+  end
+
+  it 'has attributes' do
+    data = {id: 5, name: "Turing School"}
+    merchant = Merchant.new(data)
+
+    expect(merchant.name).to eq "Turing School"
+    expect(merchant.id).to eq 5
+  end
+end
