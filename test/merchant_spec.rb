6fa8a0c798596e8b1052c2b26e6b14587debe606
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-14 14:35:11 -0600

    Create #attr_reader test

--- test/merchant_spec.rb
+++ test/merchant_spec.rb
@@ -10,6 +10,14 @@ describe Merchant do
 
       expect(m).to be_an_instance_of(Merchant)
     end
+
+    it 'has readable attributes' do
+      merch_hash = {id: 7, name: "Turing School"}
+      m = Merchant.new(merch_hash)
+
+      expect(m.id).to eq(7)
+      expect(m.name).to eq('Turing School')
+    end
   end
 
 end
