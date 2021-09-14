9cf2cc96c011543d210a789f881d05b577b4b004
Micha Bernhard <mib70@pitt.edu>
2021-09-13 18:38:51 -0600

    Feat - Write find_by_id and find_by_name methods

--- lib/itemrepository.rb
+++ lib/itemrepository.rb
@@ -4,13 +4,27 @@ class ItemRepository
 
   def initialize(path)
     @path = path
+    @rows = CSV.read(@path, headers: true, header_converters: :symbol)
+    @all = all
   end
 
-  def self.all
-    rows = CSV.read(@@filename, headers: true)
-
-    result = rows.map do |row|
-      row
+  def all
+    result = @rows.map do |row|
+      Item.new(row)
     end
+  end
+
+  def find_by_id(id)
+    result = @all.find do |row|
+      row.id == id
+    end
+    result
+  end
+
+  def find_by_name(name)
+    result = @all.find do |row|
+      row.name == name
+    end
+    result
   end
 end
