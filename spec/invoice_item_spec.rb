6bc42189042f8fec1afa1ec5e530476c74284f85
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-14 18:47:03 -0400

    Add tests for initialized methods in invoice item class

--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -10,15 +10,41 @@ RSpec.describe 'InvoiceItem' do
         :invoice_id  => 8,
         :quantity    => 1,
         :unit_price  => BigDecimal(10.99,4),
-        :created_at  => Time.now,
-        :updated_at  => Time.now,
+        :created_at  => Time.now.round(2),
+        :updated_at  => Time.now.round(2),
       })
     end
 
     it 'is an instance of InvoiceItem' do
       expect(@ii).to be_an_instance_of InvoiceItem
-    end 
+    end
+
+    it 'has an id' do
+      expect(@ii.id).to eq(6)
+    end
+
+    it 'has an item id' do
+      expect(@ii.item_id).to eq(7)
+    end
+
+    it 'has an invoice id' do
+      expect(@ii.invoice_id).to eq(8)
+    end
+
+    it 'has a quantity' do
+      expect(@ii.quantity).to eq(1)
+    end
+
+    it 'has a unit price' do
+      expect(@ii.unit_price).to eq(10.99)
+    end
+
+    it 'has a created at time' do
+      expect(@ii.created_at).to eq(Time.now.round(2))
+    end
+
+    it 'has a updated at time' do
+      expect(@ii.updated_at).to eq(Time.now.round(2))
+    end
   end
-
-
 end
