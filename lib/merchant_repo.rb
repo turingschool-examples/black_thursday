86d15232c1e895ba8595d7c28ec6fa39665f5c80
John Napier <jcee.napier@gmail.com>
2021-09-14 15:50:23 -0600

    feat: Add find methods

--- lib/merchant_repo.rb
+++ lib/merchant_repo.rb
@@ -13,8 +13,24 @@ class MerchantRepo
   end
 
   def self.find_by_id(id)
-    result = self.all.find do |merchant|
+    self.all.find do |merchant|
       merchant.id == id
     end
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
+  def self.find_all_by_name(name)
+    self.all.find_all do |merchant|
+      merchant.name.downcase.include?(name.downcase)
+    end
+  end
+
+  def self.create(attributes)
+    
   end
 end
--- spec/merchant_repo_spec.rb
+++ spec/merchant_repo_spec.rb
@@ -19,4 +19,18 @@ describe MerchantRepo do
   it ".find_by_id" do
     expect(MerchantRepo.find_by_id(12334105).name).to eq("Shopin1901")
   end
+
+  it ".find_by_name" do
+    expect(MerchantRepo.find_by_name("Urcase17").id).to eq(12334144)
+  end
+
+  it ".find_all_by_name" do
+    expect(MerchantRepo.find_all_by_name("nate brown store")).to eq([])
+    expect(MerchantRepo.find_all_by_name("neart").first.name).to eq("JamesCByrneART")
+    expect(MerchantRepo.find_all_by_name("neart")[1].name).to eq("GlassFigurineArt")
+  end
+
+  it ".create(attributes)" do
+    expect(MerchantRepo.create({:name => "John Napier"})).to eq()
+  end
 end
