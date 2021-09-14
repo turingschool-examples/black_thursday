f23db41fbe444857268e15c0fbf3fb16c679ad2c
Micha Bernhard <mib70@pitt.edu>
2021-09-13 18:38:18 -0600

    Test - Write tests for find_by_id and find_by_name methods

--- spec/itemrepository_spec.rb
+++ spec/itemrepository_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require './lib/item'
 require './lib/itemrepository'
 
 describe 'itemrepository' do
@@ -9,7 +10,36 @@ describe 'itemrepository' do
 
       expect(ir).to be_an_instance_of ItemRepository
     end
+  end
 
-    
+  describe '#all' do
+    it 'returns an array of all item instances' do
+      path = './data/items.csv'
+      ir = ItemRepository.new(path)
+
+      expect(ir.all).to be_an Array
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'returns an instance of Item matching the id' do
+      path = './data/items.csv'
+      ir = ItemRepository.new(path)
+
+      expect(ir.find_by_id("263395617")).to be_an_instance_of Item
+      expect(ir.find_by_id("263395617").id).to eq("263395617")
+      expect(ir.find_by_id("263395617").name).to eq("Glitter scrabble frames")
+    end
+  end
+
+  describe '#find_by_name' do
+    it 'returns an instance of Item matching the name' do
+      path = './data/items.csv'
+      ir = ItemRepository.new(path)
+
+      expect(ir.find_by_name("JSK Chai")).to be_an_instance_of Item
+      expect(ir.find_by_name("JSK Chai").id).to eq("263549122")
+      expect(ir.find_by_name("JSK Chai").name).to eq("JSK Chai")
+    end
   end
 end
