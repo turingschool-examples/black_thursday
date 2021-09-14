02b65a73bc523658dcf247b70f3322079d082498
Micha Bernhard <mib70@pitt.edu>
2021-09-14 17:03:42 -0600

    Test - Write test for finding by status

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -39,8 +39,9 @@ describe InvoiceRepository do
       inre = InvoiceRepository.new(path)
 
       expect(inre.find_all_by_customer_id('5')).to be_an Array
-      expect(inre.find_all_by_customer_id('5').first.merchant_id).to include('12336113')
+      expect(inre.find_all_by_customer_id('5').first.merchant_id).to eq('12336113')
       expect(inre.find_all_by_customer_id('5').length).to eq(8)
+      expect(inre.find_all_by_customer_id('12341234')).to eq([])
     end
   end
 
@@ -50,8 +51,21 @@ describe InvoiceRepository do
       inre = InvoiceRepository.new(path)
 
       expect(inre.find_all_by_merchant_id('12334912')).to be_an Array
-      expect(inre.find_all_by_merchant_id('12334912').first.id).to include('17')
+      expect(inre.find_all_by_merchant_id('12334912').first.id).to eq('17')
       expect(inre.find_all_by_merchant_id('12334912').length).to eq(15)
+      expect(inre.find_all_by_merchant_id('12341234')).to eq([])
+    end
+  end
+
+  describe '#find_all_by_status' do
+    it 'finds all instances matching the given status' do
+      path = './data/invoices.csv'
+      inre = InvoiceRepository.new(path)
+
+      expect(inre.find_all_by_status('pending')).to be_an Array
+      expect(inre.find_all_by_status('pending').first.id).to eq('1')
+      expect(inre.find_all_by_status('pending').length).to eq(1473)
+      expect(inre.find_all_by_status('this is not a status')).to eq([])
     end
   end
 end
