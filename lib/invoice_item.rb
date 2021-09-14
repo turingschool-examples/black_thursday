ecc02d9a19cea071432e240b80bb8b1d98b5025e
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-14 18:29:18 -0400

    Add attr_accessor to invoice item class

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,4 +1,19 @@
 class InvoiceItem
+  attr_accessor :id,
+                :item_id,
+                :invoice_id,
+                :quantity,
+                :unit_price,
+                :created_at,
+                :updated_at
+
   def initialize(data)
-  end 
+    @id = data[:id]
+    @item_id = data[:item_id]
+    @invoice_id = data[:invoice_id]
+    @quantity = data[:quantity]
+    @unit_price = data[:unit_price]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
+  end
 end
