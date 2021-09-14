d232df3202a9418d6c0a2b316223dc78cc9ceea1
Paul Leonard <paulpaulleonard@gmail.com>
2021-09-14 17:45:55 -0400

    Add new test for push

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,4 +1,26 @@
+# require './merchantrepository'
+require 'csv'
 class Merchant
+  # include MerchantRepository
+  attr_reader :id,
+              :name,
+              :merchants
+  @@filename = './data/merchants.csv'
+  def initialize(info)
+    @id = info[:id]
+    @name = info[:name]
 
+  end
 
+  def self.find_by_id(id)
+    rows = CSV.read(@@filename, headers: true)
+    rows.by_col
+
+    id = rows["id"]
+    
+    merchants = id.find do |merchant|
+      merchant == rows["name"]
+    end
+    merchants
+  end
 end
--- test/merchant_spec.rb
+++ test/merchant_spec.rb
@@ -5,15 +5,46 @@ require './lib/merchant'
 
 RSpec.describe 'Merchant' do
   before(:each) do
-    @merchant = Merchant.new({
+
+  end
+
+
+  it "exists" do
+    @merchant_1 = Merchant.new({
                             :id   => 5,
                             :name => "Turing School"
                             })
+    expect(@merchant_1).to be_a Merchant
   end
 
-  it "exists" do
-    expect(@merchant).to be_a Merchant
+  it "has readable info" do
+    @merchant_1 = Merchant.new({
+                            :id   => 5,
+                            :name => "Turing School"
+                            })
+    expect(@merchant_1.id).to eq(5)
+    expect(@merchant_1.name).to eq("Turing School")
   end
 
+  xit "can list all instantiated merchants" do
+    @merchant_1 = Merchant.new({
+                            :id   => 5,
+                            :name => "Turing School"
+                            })
+    @merchant_2 = Merchant.new({
+                            :id   => 10,
+                            :name => "UGA"
+                            })
+
+    # add_merchant(@merchant_1)
+    # add_merchant(@merchant_2)
+
+    expect(self.all).to eq([@merchant_1, @merchant_2])
+  end
+
+  it "can find a merchant by ID number" do
+    expect(Merchant.find_by_id(12334105)).to eq("Shopin1901")
+
+  end
 
 end
