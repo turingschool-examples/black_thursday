b4f2b6b32ba4233ea5bb994a583cfbd03b5a29fe
Micha Bernhard <mib70@pitt.edu>
2021-09-14 16:11:34 -0600

    Test - Write all tests for invoice

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -8,9 +8,9 @@ describe 'Invoice' do
             :id          => 6,
             :customer_id => 7,
             :merchant_id => 8,
-            :status      => "pending",
-            :created_at  => Time.now,
-            :updated_at  => Time.now,
+            :status      => 'pending',
+            :created_at  => Time.now.round(2),
+            :updated_at  => Time.now.round(2),
           })
     end
 
@@ -20,6 +20,26 @@ describe 'Invoice' do
 
     it 'has an id' do
       expect(@i.id).to eq(6)
+    end
+
+    it 'has a customer_id' do
+      expect(@i.customer_id).to eq(7)
+    end
+
+    it 'has a merchant_id' do
+      expect(@i.merchant_id).to eq(8)
+    end
+
+    it 'has a status' do
+      expect(@i.status).to eq('pending')
+    end
+
+    it 'has a created_at' do
+      expect(@i.created_at).to eq(Time.now.round(2))
+    end
+
+    it 'has a updated_at' do
+      expect(@i.updated_at).to eq(Time.now.round(2))
     end
   end
 end
