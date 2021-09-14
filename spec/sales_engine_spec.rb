ea0b3ab619638d6d4fe1a63cf487b34709ec5c90
Philip Medlin <phmedlin@gmail.com>
2021-09-14 14:11:40 -0600

    Test: Sales_Engine exist test

---
+++ spec/sales_engine_spec.rb
@@ -0,0 +1,13 @@
+require './lib/sales_engine'
+
+RSpec.describe SalesEngine do
+  it 'exists' do
+  se = SalesEngine.new()
+    expect(se).to be_an_instance_of(SalesEngine)
+  end
+
+  xit 'has attributes' do
+    se = SalesEngine.new()
+    expect(x).to eq("...")
+  end
+end
