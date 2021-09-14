53de1cb5e9c5efb3ac7991a5c1453e01a62cfbc8
Micha Bernhard <mib70@pitt.edu>
2021-09-14 13:27:37 -0600

    Test - Write test for delete method

--- spec/itemrepository_spec.rb
+++ spec/itemrepository_spec.rb
@@ -136,4 +136,14 @@ describe 'itemrepository' do
       expect(ir.find_by_id('263395617').unit_price).to eq(1099)
     end
   end
+
+  describe '#delete' do
+    it 'deletes an instance of Item' do
+      path = './data/items.csv'
+      ir = ItemRepository.new(path)
+      require 'pry'; binding.pry
+      ir.delete('263395617')
+      expect(ir.find_by_id('263395617')).to eq(nil)
+    end
+  end
 end
