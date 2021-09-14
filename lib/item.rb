d652a5b71e6ae51cbef4c9590b25e64d691768a5
Micha Bernhard <mib70@pitt.edu>
2021-09-13 19:12:40 -0600

    Feat - Convert unit_price to a float

--- lib/item.rb
+++ lib/item.rb
@@ -11,7 +11,7 @@ class Item
     @id = data[:id]
     @name = data[:name]
     @description = data[:description]
-    @unit_price = data[:unit_price]
+    @unit_price = data[:unit_price].to_f
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
     @merchant_id = data[:merchant_id]
