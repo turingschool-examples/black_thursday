4a95e9ab5b59d94486f94d0499248725847d1c0e
Micha Bernhard <mib70@pitt.edu>
2021-09-14 16:33:14 -0600

    Test - Write test for all method

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -4,10 +4,21 @@ require './lib/invoice_repository'
 describe InvoiceRepository do
   describe '#initialize' do
     it 'is an instance of InvoiceRepository' do
-      path = './data/items.csv'
-      inre = ItemRepository.new(path)
+      path = './data/invoices.csv'
+      inre = InvoiceRepository.new(path)
 
-      expect(inre).to be_an_instance_of ItemRepository
+      expect(inre).to be_an_instance_of InvoiceRepository
+    end
+  end
+
+  describe '#all' do
+    it 'returns an array of all invoice instances' do
+      path = './data/invoices.csv'
+      inre = InvoiceRepository.new(path)
+
+      expect(inre.all).to be_an Array
+      expect(inre.all.first).to be_an_instance_of Invoice
+      expect(inre.all.first.status).to eq("pending")
     end
   end
 end
