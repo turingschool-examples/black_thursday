4f3e14c17fdde0a6fc0868423c829f6758ddbc13
Micha Bernhard <mib70@pitt.edu>
2021-09-14 16:58:11 -0600

    Test - Write tests for finding by merchant and customer ids

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -29,6 +29,29 @@ describe InvoiceRepository do
       inre = InvoiceRepository.new(path)
 
       expect(inre.find_by_id('379').merchant_id).to eq('12334434')
+      expect(inre.find_by_id('12341234')).to eq(nil)
+    end
+  end
+
+  describe '#find_all_by_customer_id' do
+    it 'finds all instances matching the given customer id' do
+      path = './data/invoices.csv'
+      inre = InvoiceRepository.new(path)
+
+      expect(inre.find_all_by_customer_id('5')).to be_an Array
+      expect(inre.find_all_by_customer_id('5').first.merchant_id).to include('12336113')
+      expect(inre.find_all_by_customer_id('5').length).to eq(8)
+    end
+  end
+
+  describe '#find_all_by_merchant_id' do
+    it 'finds all instances matching the given merchant id' do
+      path = './data/invoices.csv'
+      inre = InvoiceRepository.new(path)
+
+      expect(inre.find_all_by_merchant_id('12334912')).to be_an Array
+      expect(inre.find_all_by_merchant_id('12334912').first.id).to include('17')
+      expect(inre.find_all_by_merchant_id('12334912').length).to eq(15)
     end
   end
 end
