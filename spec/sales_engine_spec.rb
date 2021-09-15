cd6fdba7ca69a01c49ecffe3486d8c3d063c81fb
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-15 09:01:56 -0600

    Create #items test

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -8,23 +8,27 @@ require 'csv'
 
 describe SalesEngine do
 
-  describe it 'is an instance of sales engine class' do
-   se = SalesEngine.new({
-    :items     => "./data/items.csv",
-    :merchants => "./data/merchants.csv",
-    })
-
-    expect(se).to be_an_instance_of SalesEngine
+  describe '#initialize' do
+    it 'creates an instance of SalesEngine' do
+      se = SalesEngine.new({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv"
+        })
+      expect(se).to be_an_instance_of SalesEngine
+    end
   end
 
-  it 'has readable attributes' do
-    se = SalesEngine.new({
-     :items     => "./data/items.csv",
-     :merchants => "./data/merchants.csv",
-     })
+  describe '#items' do
+    it 'returns a new instance of ItemRepository with an array of item objects' do
+      se = SalesEngine.new({
+        :items     => "test.csv",
+        :merchants => "test_2.csv"
+        })
+      ir = se.items
 
-     expect(se.merchants).to eq("./data/merchants.csv")
-     expect(se.items).to eq("./data/items.csv")
-
+      expect(ir).to be_an_instance_of(ItemRepository)
+      expect(it.all).to eq(['insert expected results here'])
+    end
   end
+
 end
