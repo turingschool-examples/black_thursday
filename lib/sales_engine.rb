7d783c8c69a29a8d92f68f2613efc531620f99ec
Sam Devine <samuel.will.devine@gmail.com>
2021-09-15 16:38:32 -0600

    refactor: spec_harness 15 failing tests on iteration 0

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,33 +1,35 @@
 require 'csv'
-require_relative 'itemrepository'
+require_relative 'item_repository'
 require_relative 'merchant_repository'
 require_relative 'invoice_repository'
 require_relative 'invoice_item_repository'
 require_relative 'transaction_repository'
 
 class SalesEngine
-  attr_reader :analyst,
-              :items,
+  attr_reader :items,
               :merchants,
               :invoices,
               :invoice_items,
               :transactions
 
   def self.from_csv(paths)
-    item_path = paths[:items]
-    merchant_path = paths[:merchants]
-    invoice_path = paths[:invoices]
-    invoice_item_path = paths[:invoice_items]
-    transaction_path = paths[:transactions]
+    # fix these names to make them more clear
+    ip         = paths[:items]
+    mp     = paths[:merchants]
+    inp      = paths[:invoices]
+    iip = paths[:invoice_items]
+    tp  = paths[:transactions]
+    cp     = paths[:customers]
+
+    SalesEngine.new(ip, mp, inp, iip, tp, cp)
   end
 
-  def initialize
-    @analyst = SalesAnalyst.new
-    @items = ItemRepository.new('./data/items.csv')
-    @merchants = MerchantRepository.new('./data/merchants.csv')
-    @invoices = InvoiceRepository.new('./data/invoices.csv')
-    @invoice_items = InvoiceItemRepository.new('./data/invoice_items.csv')
-    @transactions = TransactionRepository.new('./data/transactions.csv')
+  def initialize(ip, mp, inp, iip, tp, cp)
+    # @customer_repository = CustomerRepository.new(@customer_path)
+    @transactions = TransactionRepository.new(tp)
+    @invoice_items = InvoiceItemRepository.new(iip)
+    @invoices = InvoiceRepository.new(inp)
+    @items = ItemRepository.new(ip)
+    @merchants = MerchantRepository.new(mp)
   end
-
 end
