c9c460d39b9beed49620bbc23c8b9f7559420cab
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-15 12:12:23 -0600

    create changes

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -6,12 +6,8 @@ require './lib/items'
 require './lib/item_repository'
 require 'csv'
 
-
-<<<<<<< HEAD
-=======
 describe SalesEngine do
 
->>>>>>> 46f08309bdd33ebaca98e51906d08b7df68cf082
   describe '#initialize' do
     it 'creates an instance of SalesEngine' do
       se = SalesEngine.new({
@@ -29,7 +25,6 @@ describe SalesEngine do
         :merchants => "./data/merchants.csv"
         })
       ir = se.items
-<<<<<<< HEAD
 
       expect(ir).to be_an_instance_of(ItemRepository)
     end
@@ -43,21 +38,6 @@ describe SalesEngine do
         })
       mr = se.merchants
 
-=======
-
-      expect(ir).to be_an_instance_of(ItemRepository)
-    end
-  end
-
-  describe '#merchants' do
-    it 'returns a new instance of MerchantRepository with an array of merchant objects' do
-      se = SalesEngine.new({
-        :items     => "./data/items.csv",
-        :merchants => "./data/merchants.csv"
-        })
-      mr = se.merchants
-
->>>>>>> 46f08309bdd33ebaca98e51906d08b7df68cf082
       expect(mr).to be_an_instance_of(MerchantsRepository)
     end
   end
