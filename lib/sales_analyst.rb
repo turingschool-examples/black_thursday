4b77dd15a3695626b5c4eb636a720630759eaa2c
Haewon Jeon <haewonito@gmail.com>
2021-09-14 20:48:40 -0600

    feat: merchants with high item count

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -15,6 +15,7 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant_standard_deviation
+
     mean = self.average_items_per_merchant.to_f
     sum = 0.0
     (@mr.all).each do |merchant|
@@ -23,6 +24,33 @@ class SalesAnalyst
     end
     s_d = Math.sqrt(sum/((@mr.all.length.to_f)-1))
     s_d.round(2)
+  end
+
+  def merchants_with_high_item_count
+    array = []
+    #s_d = self.average_items_per_merchant_standard_deviation
+    s_d = 3.26
+    mean_item = self.average_items_per_merchant
+    one_above_sd = mean_item + s_d
+
+    (@mr.all).each do |merchant|
+      if @ir.find_all_by_merchant_id(merchant.id).length > one_above_sd
+        array << merchant
+      end
+    end
+    array
+  end
+
+  def average_item_price_for_merchant(id)
+
+  end
+
+  def average_average_item_price_for_merchant
+
+  end
+
+  def golden_item
+
   end
 
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -23,7 +23,24 @@ RSpec.describe SalesAnalyst do
     expect(@sales_analyst.average_items_per_merchant).to eq(2.88)
   end
 
-  it "#average_items_per_merchant_standard_deviation" do
+  it '#average_items_per_merchant_standard_deviation' do
     expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
+  end
+
+
+  it '#merchants_with_high_item_count' do #need better test
+    expect(@sales_analyst.merchants_with_high_item_count).to be_a(Array)
+  end
+
+  xit '#average_item_price_for_merchant' do
+    expect(@sales_analyst.average_item_price_for_merchant(12334159)).to eq(3150)
+  end
+
+  xit '#average_average_item_price_for_merchant' do
+    expect(@sales_analyst.average_average_item_price_for_merchant).to be_a(Float)
+  end
+
+  xit '#golden_item' do
+    expect(@sales_analyst.golden_item).to be_a(Array)
   end
 end
