f0d32d81a5cd2f278c331c827942809f9dbd1b4d
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-15 12:07:04 -0600

    Refactor #items

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,23 +5,23 @@ class SalesEngine
     @items = merch_item_hash[:items]
   end
 
-  # def items
-  #   items_array = []
-  #   items_table = CSV.read(@items, headers: true)
-  #   items_table.each do |row|
-  #     items_hash = {}
-  #     row_headers = row.headers
-  #     items_hash[row_headers[0].to_sym] = row[0]
-  #     items_hash[row_headers[1].to_sym] = row[1]
-  #     items_hash[row_headers[2].to_sym] = row[2]
-  #     items_hash[row_headers[3].to_sym] = row[3]
-  #     items_hash[row_headers[4].to_sym] = row[4]
-  #     items_hash[row_headers[5].to_sym] = row[5]
-  #     items_hash[row_headers[6].to_sym] = row[6]
-  #     items_array.push(Item.new(items_hash))
-  #   end
-  #   ItemRepository.new(items_array)
-  # end
+  def items
+    items_array = []
+    items_table = CSV.read(@items, headers: true)
+    items_table.each do |row|
+      items_hash = {}
+      row_headers = row.headers
+      items_hash[row_headers[0].to_sym] = row[0]
+      items_hash[row_headers[1].to_sym] = row[1]
+      items_hash[row_headers[2].to_sym] = row[2]
+      items_hash[row_headers[3].to_sym] = row[3]
+      items_hash[row_headers[4].to_sym] = row[4]
+      items_hash[row_headers[5].to_sym] = row[5]
+      items_hash[row_headers[6].to_sym] = row[6]
+      items_array.push(Item.new(items_hash))
+    end
+    ItemRepository.new(items_array)
+  end
 
   def merchants
     merch_array = []
