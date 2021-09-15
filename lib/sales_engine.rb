747b466e53510ae572405ae7219375ca4aa59a9a
Didi Dodson <mdodson55@gmail.com>
2021-09-15 17:11:56 -0600

    Feat: add functionality and testing

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,21 +1,31 @@
 require 'csv'
 
-se = SalesEngine.from_csv({
-  :items     => "./data/items.csv",
-  :merchants => "./data/merchants.csv",
-})
-
-
-
-# class SalesEngine
+# se = SalesEngine.from_csv({
+#   :items     => "./data/items.csv",
+#   :merchants => "./data/merchants.csv",
+# })
 #
-#   attr_reader :items, :merchants
-#
-#   def initialize(data)
-#     @items = data[:items]
-#     @merchants = data[:merchants]
-#   end
-#
-#   def from_csv
-#   end
-# end
+# mr = se.merchants
+# merchant = mr.find_by_name("CJsDecor")
+
+class SalesEngine
+
+  attr_reader :items, :merchants
+
+  def initialize(data)
+    @items = data[:items]
+    @merchants = data[:merchants]
+  end
+
+  def self.from_csv(info)
+    SalesEngine.new({ :items => "./data/items.csv",
+                      :merchants => "./data/merchants.csv"})
+  end
+
+  # def merchants
+  #
+  # end
+  #
+  # def items
+  # end
+end
--- test/sales_engine_spec.rb
+++ test/sales_engine_spec.rb
@@ -1,15 +1,15 @@
-# require './lib/sales_engine'
+require './lib/sales_engine'
 # require 'Rspec'
 # require 'csv'
+#
+describe SalesEngine do
 
-# describe SalesEngine do
-#
-#   it 'exists' do
-#     se = SalesEngine.from_csv({
-#       :items     => "./data/items.csv",
-#       :merchants => "./data/merchants.csv",
-#     })
-#
-#     expect(se).to be_a(SalesEngine)
-#   end
-# end
+  it 'exists' do
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+    })
+
+    expect(se).to be_a(SalesEngine)
+  end
+end
