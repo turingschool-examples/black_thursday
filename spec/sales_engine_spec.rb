1e1c2278b7b5cdb49b35d80a8ad5420e899440a2
Andrew Patterson <andrew.patterson747@gmail.com>
2021-09-14 21:36:45 -0500

    Add item_data to SalesEngine class

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -1,15 +1,28 @@
 require './lib/sales_engine'
+require 'csv'
 
 RSpec.describe SalesEngine do
   it 'exists' do
-
-  se = SalesEngine.new()
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
     expect(se).to be_an_instance_of(SalesEngine)
   end
 
+  it 'test csv hash' do
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
+    expect(SalesEngine.item_data).to eq("./data/items.csv")
+  end
+
   xit 'has attributes' do
-    se = SalesEngine.new()
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      })
     expect(x).to eq("...")
   end
 end
-
