cfc6190e8cf7f150136244285a98cd59acba66d3
Mallory Vining <mallorygallery12@gmail.com>
2021-09-16 09:35:03 -0600

    feat: Finish merchant class

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,5 +1,8 @@
 class Merchant
-def initialize(merchant_data)
-  # @id = merchant_data[:id]
-end
+  attr_reader :id,
+              :name
+  def initialize(merchant_data)
+    @id = merchant_data[:id]
+    @name = merchant_data[:name]
+  end
 end
--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe do
 
   expect(merchant).to be_an_instance_of(Merchant)
   end
+
+  it 'has attributes as a hash' do
+    merchant = Merchant.new({:id => 12345678, :name => 'KCAMP' })
+
+    expect(merchant.id).to eq(12345678)
+    expect(merchant.name).to eq('KCAMP')
+
+  end
 end
