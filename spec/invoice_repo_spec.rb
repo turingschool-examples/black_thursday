acab70a6e13b863456bde7dd96fa2385552a0351
Eldridge Turambi <eturambi95@gmail.com>
2021-09-16 11:25:40 -0600

    Update find_all_by_customer test

--- spec/invoice_repo_spec.rb
+++ spec/invoice_repo_spec.rb
@@ -12,4 +12,23 @@ describe InvoiceRepo do
   it "exists" do
     expect(@invoice_repo).to be_an_instance_of(InvoiceRepo)
   end
+
+  it '#all' do
+
+    expect(@invoice_repo.all).to be_an(Array)
+  end
+
+  it "#find_by_id" do
+    expect(@invoice_repo.find_by_id(1).merchant_id).to eq(12335938)
+  end
+
+  it "#find_all_by_customer_id" do
+    expect(@invoice_repo.find_all_by_customer_id(-1)).to eq []
+    expect(@invoice_repo.find_all_by_customer_id(12336642).length).to eq 7
+  end
+
+  xit "#find_all_by_merchant_id" do
+    expect(@invoice_repo.find_all_by_merchant_id(-1)).to eq []
+    expect(@invoice_repo.find_all_by_merchant_id(12336642).length).to eq 2
+  end
 end
