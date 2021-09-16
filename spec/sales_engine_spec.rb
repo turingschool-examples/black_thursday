b4ea3b9d5c5d8613055ec6d194bbadec54ad414b
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-15 18:45:30 -0600

    Refactor #initialize test

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -7,7 +7,6 @@ require './lib/item_repository'
 require 'csv'
 
 
-
 describe SalesEngine do
 
   describe '#initialize' do
@@ -18,48 +17,14 @@ describe SalesEngine do
         })
       expect(se).to be_an_instance_of SalesEngine
     end
-  end
 
-  describe '#items' do
-    xit 'returns a new instance of ItemRepository with an array of item objects' do
-      se = SalesEngine.new({
-        :items     => "./data/items.csv",
-        :merchants => "./data/items.csv"
-        })
-      ir = se.items
-
-      expect(ir).to be_an_instance_of(ItemRepository)
-    end
-  end
-
-  describe '#merchants' do
-    it 'returns a new instance of MerchantRepository with an array of merchant objects' do
-      se = SalesEngine.new({
-        :items     => "./data/items.csv",
-        :merchants => "./data/items.csv"
-        })
-      mr = se.merchants
-
-    it 'returns a new instance of ItemRepository with an array of item objects' do
+    it 'has readable attributes' do
       se = SalesEngine.new({
         :items     => "./data/items.csv",
         :merchants => "./data/merchants.csv"
         })
-      ir = se.items
-
-      expect(ir).to be_an_instance_of(ItemRepository)
-    end
-  end
-
-  describe '#merchants' do
-    it 'returns a new instance of MerchantRepository with an array of merchant objects' do
-      se = SalesEngine.new({
-        :items     => "./data/items.csv",
-        :merchants => "./data/merchants.csv"
-        })
-      mr = se.merchants
-
-      expect(mr).to be_an_instance_of(MerchantsRepository)
+      expect(se.merchants).to be_an_instance_of(MerchantsRepository)
+      expect(se.items).to be_an_instance_of(ItemRepository)
     end
   end
 
