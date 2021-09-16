6405cf96f9502bddb03738b6ad6eb5416ef85ffc
Matthew Kimball <mekimba6@gmail.com>
2021-09-16 11:56:39 -0600

    adds item_repo functionality

--- lib/merchants.rb
+++ lib/merchants.rb
@@ -1,27 +1,31 @@
 require 'csv'
 require './lib/sales_engine'
 
-
 class Merchants
-  attr_reader :id
+  attr_reader   :id
   attr_accessor :name,
                 :created_at,
                 :updated_at
 
   def initialize(merchants)
-    @id = merchants[0]
-    @name = merchants[1]
+    @id         = merchants[0]
+    @name       = merchants[1]
     @created_at = merchants[2]
     @updated_at = merchants[3]
   end
 
   def hash_convert
     headers = []
-    CSV.foreach("./data/merchants.csv", headers: true, header_converters: :symbol) do |row|
-        headers << row
+    CSV.foreach('./data/merchants.csv', headers: true,
+                                        header_converters: :symbol) do |row|
+      headers << row
     end
-    names = headers.map do |name|
-        name[:name]
-    end
+    # names = headers.map do |name|
+    #     name[:name]
+    # end
+  end
+
+  def merchant_repo
+    MerchantRepository.new(@merchants)
   end
 end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,5 +1,5 @@
 require 'csv'
-require_relative './item_repository'
+require './lib/item_repository'
 require_relative './merchant_repository'
 require_relative './items'
 require_relative './merchants'
@@ -10,39 +10,38 @@ class SalesEngine
 
   def self.from_csv(paths)
     data = {}
-    data[:items] = create_obj_csv(paths[:items], Items)
+    data[:items]     = create_obj_csv(paths[:items], Items)
     data[:merchants] = create_obj_csv(paths[:merchants], Merchants)
     SalesEngine.new(data)
   end
 
-
   def initialize(data)
-    @items     = data[:items]
+    @items     = ItemRepository.new(data[:items])
     @merchants = data[:merchants]
   end
 
   def self.create_obj_csv(locations, obj_type)
     objects = []
-    CSV.foreach(locations, headers: true, header_converters: :symbol, :quote_char => '"', liberal_parsing: true) do |row|
+    CSV.foreach(locations, headers: true, header_converters: :symbol,
+                           quote_char: '"', liberal_parsing: true) do |row|
       object = obj_type.new(row)
       objects << object
     end
     objects
+    # ItemRepository.new(objects)
+    # MerchantRepository.new(objects)
   end
 
-  def merchant_repo
-    MerchantRepository.new(@merchants)
-  end
-
-  def item_repo
-    ItemRepository.new(@items)
-  end
+  # def merchant_repo
+  # end
+  #
+  # def item_repo
+  #   ItemRepository.new(@items)
+  # end
 end
 
-se = SalesEngine.from_csv({
-  :items     => "./data/items.csv",
-  :merchants => "./data/merchants.csv"
-})
-require "pry"; binding.pry
-# se.items
-# mr = se.merchants
+# se = SalesEngine.from_csv({
+#                             items: './data/items.csv',
+#                             merchants: './data/merchants.csv'
+#                           })
+# require "pry"; binding.pry
