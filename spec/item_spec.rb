78537d69ff535b68c6a4da39e73efbd0b93a0274
Andrew Patterson <andrew.patterson747@gmail.com>
2021-09-16 16:29:33 -0500

    Add: attribute tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,9 +5,6 @@ require 'bigdecimal'
 
 
 RSpec.describe do
-
-  # before(:each) do
-
   it 'exists' do
     i = Item.new({
       :id          => 1,
@@ -17,7 +14,7 @@ RSpec.describe do
       :created_at  => Time.now,
       :updated_at  => Time.now,
       :merchant_id => 2
-                 })
+      })
 
     expect(i).to be_an_instance_of(Item)
   end
@@ -31,9 +28,29 @@ RSpec.describe do
       :created_at  => Time.now,
       :updated_at  => Time.now,
       :merchant_id => 2
-                 })
+      })
 
     expect(i.id).to eq(1)
     expect(i.id).to be_an(Integer)
+  end
+
+  it 'has attributes' do
+    i = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+      })
+
+    expect(i.id).to eq(1)
+    expect(i.name).to eq("Pencil")
+    expect(i.description).to eq("You can use it to write things")
+    expect(i.unit_price).to eq(BigDecimal(10.99,4))
+    # expect(i.created_at).to eq()
+    # expect(i.updated_at).to eq()
+    # expect(i.merchant_id).to eq(2)
   end
 end
