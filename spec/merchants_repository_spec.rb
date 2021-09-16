ca53ebf559811ba329a0df19349fd448210bac81
Devin Pile <devinpile@gmail.com>
2021-09-16 11:13:56 -0600

    Add changes to fit hound convention

--- spec/merchants_repository_spec.rb
+++ spec/merchants_repository_spec.rb
@@ -5,9 +5,9 @@ require './lib/merchant'
 describe MerchantsRepository do
   before(:each) do
     @merchant_array = []
-    @object_1 = Merchant.new({:id => 12334105, :name => "Shopin1901"})
-    @object_2 = Merchant.new({:id => 12334112, :name => "Candisart"})
-    @object_3 = Merchant.new({:id => 12334113, :name => "MiniatureBikez"})
+    @object_1 = Merchant.new({ id: 123_341_05, name: 'Shopin1901' })
+    @object_2 = Merchant.new({ id: 123_341_12, name: 'Candisart' })
+    @object_3 = Merchant.new({ id: 123_341_13, name: 'MiniatureBikez' })
     @merchant_array.push(@object_1, @object_2, @object_3)
     @mr = MerchantsRepository.new(@merchant_array)
   end
@@ -25,45 +25,45 @@ describe MerchantsRepository do
 
   describe '#find_by_id' do
     it 'can return merchant by id' do
-      expect(@mr.find_by_id(12334112)).to eq(@object_2)
+      expect(@mr.find_by_id(123_341_12)).to eq(@object_2)
     end
   end
 
   describe '#find_by_name' do
     it 'can return merchant by name' do
-      expect(@mr.find_by_name("MiniatureBikez")).to eq(@object_3)
+      expect(@mr.find_by_name('MiniatureBikez')).to eq(@object_3)
     end
   end
 
   describe '#find_all_by_name' do
     it 'can find all by name' do
-      @object_4 = Merchant.new({:id => 12334114, :name => "MiniatureBikez"})
+      @object_4 = Merchant.new({ :id => 123_341_14, :name => 'MiniatureBikez' })
       @merchant_array.push(@object_4)
 
-      expect(@mr.find_all_by_name("MiniatureBikez")).to eq([@object_3, @object_4])
+      expect(@mr.find_all_by_name('MiniatureBikez')).to eq([@object_3, @object_4])
     end
   end
 
   describe '#create' do
     it 'can create attributes' do
-      @mr.create("TestingCo")
+      @mr.create('TestingCo')
       object_4 = @mr.all[3]
       expect(@mr.all[3]).to eq(object_4)
-      expect(@mr.all.last.id).to eq(12334114)
+      expect(@mr.all.last.id).to eq(123_341_14)
     end
   end
 
   describe '#update' do
     it 'can update the merchant instance' do
-      @mr.update("Shopin1901", "TestingTesting123")
+      @mr.update('Shopin1901', 'TestingTesting123')
 
-      expect(@object_1.name).to eq("TestingTesting123")
+      expect(@object_1.name).to eq('TestingTesting123')
     end
   end
 
   describe '#delete' do
     it 'can delete a merchant instance' do
-      expect(@mr.delete(12334112)).to eq(@object_2)
+      expect(@mr.delete(123_341_12)).to eq(@object_2)
       expect(@mr.all.length).to eq(2)
     end
   end
