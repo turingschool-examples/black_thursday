ae6dcf23279dddfb0e58c11d11168e626f648886
Karan Mehta <karanm645@gmail.com>
2021-09-16 11:14:55 -0500

    Initialize info hash

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,5 +1,17 @@
 class Invoice
-  def initialize(info_hash)
+  attr_reader :id,
+              :created_at,
+              :updated_at,
+              :merchant_id,
+              :customer_id,
+              :status
 
+  def initialize(info)
+    @id   = info[:id].to_i
+    @customer_id   = info[:customer_id].to_i
+    @merchant_id   = info[:merchant_id].to_i
+    @status        = info[:status]
+    @created_at    = Time.parse(info[:created_at])
+    @updated_at    = Time.parse(info[:updated_at])
   end
 end
