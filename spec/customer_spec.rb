4b95124e60bc9e6151a4cb68011f59d2ef5227f1
Haewon Jeon <haewonito@gmail.com>
2021-09-16 17:40:46 -0600

    add customer.rb and customer_spec.rb

---
+++ spec/customer_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require './lib/customer'
+
+RSpec.describe Customer do
+
+  before(:each) do
+    @c = Customer.new({
+      :id => 6,
+      :first_name => "Joan",
+      :last_name => "Clarke",
+      :created_at => Time.now,
+      :updated_at => Time.now
+    })
+  end
+
+  it 'exists' do
+    expect(@c).to be_an_instance_of Customer
+  end
+
+  it 'has attributes' do
+    expect(@c.id).to eq(6)
+    expect(@c.first_name).to eq("Joan")
+    expect(@c.last_name).to eq("Clarke")
+    expect(@c.created_at).to be_an_instance_of Time
+    expect(@c.updated_at).to be_an_instance_of Time
+  end
+end
