9e7727e114538119ef62cd3954a0c27baa9ae822
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-17 17:15:25 -0400

    Clean up transaction and repo

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Transaction
   attr_reader   :id,
                 :invoice_id,
--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './lib/transaction'
 require './lib/transaction_repository'
 
