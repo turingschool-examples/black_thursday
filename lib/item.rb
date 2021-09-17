80fefa1ffaf3d5393c2163d9473eb9f04a56a27c
Billy Frey <bfrey16@gmail.com>
2021-09-17 14:59:14 -0600

    Refactor: item class to convert data to proper format

--- lib/item.rb
+++ lib/item.rb
@@ -5,34 +5,16 @@ class Item
   attr_accessor :name, :description, :unit_price
 
   def initialize(data)
-    @id           = data[:id]
+    @id           = (data[:id]).to_i
     @name         = data[:name]
     @description  = data[:description]
-    @unit_price   = data[:unit_price]
+    @unit_price   = (data[:unit_price]).to_d
     @created_at   = data[:created_at]
     @updated_at   = data[:updated_at]
-    @merchant_id  = data[:merchant_id]
+    @merchant_id  = (data[:merchant_id]).to_i
   end
 
   def unit_price_to_dollars
     @unit_price.to_f
   end
-
-  def unit_price_to_bigdecimal
-    @unit_price.to_d
-  end
-
-  def id_to_string
-    @id.to_s
-  end
-
-  def merchant_id_to_int
-    @merchant_id.to_i
-  end
-
-  def string_to_time
-    @updated_at.to_datetime
-    @created_at.to_datetime
-  end
-
 end
