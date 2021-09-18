dc978cb562b24967b883f04527a3e1bec852d040
Devin Pile <devinpile@gmail.com>
2021-09-18 12:47:22 -0600

    Test- Write test for #update, still broken

--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -10,18 +10,18 @@ require './lib/customer'
 
 describe CustomerRepository do
   before(:each) do
-    @c = CustomerRepository.new('./data/customers.csv')
+    @cr = CustomerRepository.new('./data/customers.csv')
   end
 
   it 'exists' do
-    expect(@c).to be_a CustomerRepository
+    expect(@cr).to be_a CustomerRepository
   end
 
   describe '#all' do
     it 'returns all customers' do
-      expect(@c.all).to be_a Array
+      expect(@cr.all).to be_a Array
 
-      @c.all.each do |customer|
+      @cr.all.each do |customer|
         expect(customer).to be_a Customer
       end
     end
@@ -29,24 +29,24 @@ describe CustomerRepository do
 
   describe '#find_by_id' do
     it 'can find customer by given id' do
-      expect(@c.find_by_id(3)).to eq(@c.all[2])
-      expect(@c.find_by_id(334_456)).to be nil
+      expect(@cr.find_by_id(3)).to eq(@cr.all[2])
+      expect(@cr.find_by_id(334_456)).to be nil
     end
   end
 
   describe '#find_all_by_first_name' do
     it 'can return all customers with given first name' do
-      expect(@c.find_all_by_first_name('Joey')).to include(@c.all[0])
-      expect(@c.find_all_by_first_name('Joey')).to be_a Array
-      expect(@c.find_all_by_first_name('Gwenifer')).to be_a Array
+      expect(@cr.find_all_by_first_name('Joey')).to include(@cr.all[0])
+      expect(@cr.find_all_by_first_name('Joey')).to be_a Array
+      expect(@cr.find_all_by_first_name('Gwenifer')).to be_a Array
     end
   end
 
   describe '#find_all_by_last_name' do
     it 'can return all customers with given last name' do
-      expect(@c.find_all_by_first_name('Kris')).to include(@c.all[11])
-      expect(@c.find_all_by_first_name('Kris')).to be_a Array
-      expect(@c.find_all_by_first_name('Maaaaaaar')).to be_a Array
+      expect(@cr.find_all_by_last_name('Kris')).to include(@cr.all[10])
+      expect(@cr.find_all_by_last_name('Kris')).to be_a Array
+      expect(@cr.find_all_by_last_name('Maaaaaaar')).to be_a Array
     end
   end
 
@@ -54,12 +54,19 @@ describe CustomerRepository do
     it 'creates a customer with given attributes' do
       first_name = "Georgamin-Keanu"
       last_name = "McSabertoothson III"
-      @c.create(first_name, last_name)
+      @cr.create(first_name, last_name)
 
-      expect(@c.all.last).to be_a Customer
-      expect(@c.all.last.first_name).to eq("Georgamin-Keanu")
-      expect(@c.all.last.last_name).to eq("McSabertoothson III")
-      expect(@c.all.customer_id.last).to eq(1001)
+      expect(@cr.all.last).to be_a Customer
+      expect(@cr.all.last.first_name).to eq("Georgamin-Keanu")
+      expect(@cr.all.last.last_name).to eq("McSabertoothson III")
+      expect(@cr.all.last.id).to eq(1001)
     end
   end
+
+  describe '#update' do
+    it 'updates a specific customer with the provided attributes' do
+      expect(@cr.update(5, "Joe Bob")).to eq(@cr.all[4])
+      expect(@cr.all[4].first_name).to eq('Joe')
+    end
+  end 
 end
