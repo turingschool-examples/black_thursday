69bb8101b701156f3c624e3e0e4ef3c7c37c2c5d
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-18 11:54:41 -0400

    Fix rspec tests for transaction files

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -20,15 +20,14 @@ RSpec.describe TransactionRepository do
     end
 
     it '#find_by_id' do
-      expect(@tr.find_by_id(1)).to be_a Transaction
       expect(@tr.find_by_id(1).credit_card_number).to eq('4068631943231473')
       expect(@tr.find_by_id(-1)).to be nil
     end
 
     it '#find_all_by_invoice_id' do
-      expect(@tr.find_all_by_invoice_id(2179)).to be_a Transaction
-      expect(@tr.find_all_by_invoice_id(2179).id).to eq 1
-      expect(@tr.find_all_by_invoice_id(-1)).to be nil
+      expect(@tr.find_all_by_invoice_id(2179)).to be_an Array
+      expect(@tr.find_all_by_invoice_id(2179)[0].id).to eq 1
+      expect(@tr.find_all_by_invoice_id(-1)).to eq([])
     end
 
     it '#find_all_by_credit_card_number' do
@@ -44,7 +43,7 @@ RSpec.describe TransactionRepository do
       result = 'failed'
 
       expect(@tr.find_all_by_result(result)).to be_an Array
-      expect(@tr.find_all_by_result(result).first.id).to eq 9
+      expect(@tr.find_all_by_result(Symbol).first).to be_a Symbol
     end
 
     it '#create' do
@@ -77,17 +76,15 @@ RSpec.describe TransactionRepository do
       }
       updated_transaction = @tr.update(id, attributes)
 
-      expect(updated_transaction).to be_a Transaction
       expect(updated_transaction.id).to eq 1
       expect(updated_transaction.credit_card_number).to eq '1111222233334444'
-      expect(updated_transaction.result).to eq 'success'
+      expect(updated_transaction.result).to be_a Symbol
       expect(updated_transaction.updated_at).to_not eq time
     end
 
     it '#delete' do
       deleted = @tr.delete(1)
 
-      expect(deleted).to be_a Transaction
       expect(deleted.id).to eq 1
       expect(@tr.all).to_not include deleted
     end
