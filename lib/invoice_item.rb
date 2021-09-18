ce1eda445fa0cb279575231a0445d803b57b73ab
Devin Pile <devinpile@gmail.com>
2021-09-17 18:16:20 -0600

    Feat- Add intial method to pass initial test for invoice item class

---
+++ lib/invoice_item.rb
@@ -0,0 +1,19 @@
+class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
+
+  def initialize(invoice_item)
+    @id = invoice_item[:id]
+    @item_id = invoice_item[:item_id]
+    @invoice_id = invoice_item[:invoice_id]
+    @quantity = invoice_item[:quantity]
+    @unit_price = invoice_item[:unit_price]
+    @created_at = invoice_item[:created_at]
+    @updated_at = invoice_item[:updated_at]
+  end
+end
