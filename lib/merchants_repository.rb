ceaf0843edc419c9b07d1b21a89b077084f1b65f
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-17 18:51:21 -0600

    Fix - Add merchant_creator method, add fixes for csv integration

--- lib/merchants_repository.rb
+++ lib/merchants_repository.rb
@@ -2,11 +2,24 @@ class MerchantsRepository
   attr_reader :all
 
   def initialize(merchant_array)
-    @all = merchant_array
+    @all = merchant_creator(merchant_array)
+  end
+
+  def merchant_creator(merchant_array)
+    merch_array = []
+    merch_table = CSV.read(merchant_array, headers: true)
+    merch_table.each do |row|
+      merch_hash = {}
+      row_headers = row.headers
+      merch_hash[row_headers[0].to_sym] = row[0]
+      merch_hash[row_headers[1].to_sym] = row[1]
+      merch_array.push(Merchant.new(merch_hash))
+    end
+    merch_array
   end
 
   def find_by_id(id)
-    @all.find { |merchant| merchant.id == id }
+    @all.find { |merchant| merchant.id.to_i == id }
   end
 
   def find_by_name(name)
@@ -38,7 +51,7 @@ class MerchantsRepository
 
   def delete(id)
     merch_object = @all.find do |merchant|
-      merchant.id == id
+      merchant.id.to_i == id
     end
     @all.delete(merch_object)
   end
