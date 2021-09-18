874bb0f28cb8c4cc7c49507471e654d642ef6937
Chaz Simons <chaz.simons@gmail.com>
2021-09-18 08:25:09 -0700

    add items spec and update item attributes

--- lib/items.rb
+++ lib/items.rb
@@ -1,11 +1,14 @@
+require 'bigdecimal'
+require 'time'
+
 class Items
   attr_accessor :name,
                 :description,
                 :unit_price,
                 :updated_at
-  attr_reader :id,
-              :merchant_id,
-              :created_at
+  attr_reader   :id,
+                :merchant_id,
+                :created_at
 
   def initialize(items)
     @id = items[:id].to_i
@@ -13,11 +16,12 @@ class Items
     @description = items[:description]
     @unit_price = items[:unit_price].to_f / 100
     @merchant_id = items[:merchant_id].to_i
-    @created_at = items[:created_at]
-    @updated_at = items[:updated_at]
+    @created_at = Time.parse(items[:created_at])
+    @updated_at = Time.parse(items[:updated_at])
   end
 
-  def item_repo
-    ItemRepository.new(@items)
+  def unit_price_to_dollars
+    #unit_price needs to be BigDecimal when it comes in, and converted to float here. Need to update ItemRepo methods when fixed
+    @unit_price
   end
 end
