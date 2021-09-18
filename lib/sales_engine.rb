27c3495145dbb8355ebfedc2f6cb1503d3c76add
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-17 18:25:50 -0600

    Fix - Removed duplicate initialize method

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -7,9 +7,4 @@ class SalesEngine
     @merchants = MerchantsRepository.new(merch_item_hash[:merchants])
     @items = ItemRepository.new(merch_item_hash[:items])
   end
-
-  def initialize(merch_item_hash)
-    @merchants = MerchantsRepository.new(merch_item_hash[:merchants])
-    @items = ItemRepository.new(merch_item_hash[:items])
-  end
 end
