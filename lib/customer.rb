5172df577374e292e5d6e5f5bbe0d8afeb86158a
John Napier <jcee.napier@gmail.com>
2021-09-18 13:44:20 -0600

    feat:Customer intialize method

---
+++ lib/customer.rb
@@ -0,0 +1,17 @@
+require 'Time'
+
+class Customer
+  attr_reader :id,
+              :first_name,
+              :last_name,
+              :created_at,
+              :updated_at
+
+  def initialize(info)
+    @id           = info[:id].to_i
+    @first_name   = info[:first_name]
+    @last_name    = info[:last_name]
+    @created_at   = Time.parse(info[:created_at])
+    @updated_at   = Time.parse(info[:updated_at])
+  end
+end
