44b06f7507280d9aba0cd532673b0c2f43b0ea1f
Christian Valesares <cvalesares@gmail.com>
2021-09-17 21:44:17 -0400

    feat: add create and delete methods + test along with refactoring attrs in items.rb

--- spec/item_repo_spec.rb
+++ spec/item_repo_spec.rb
@@ -22,16 +22,16 @@ RSpec.describe do
     results = @engine.items.find_by_id(263567474)
     results2 = @engine.items.find_by_id('Camping Chair Actor')
     expect(results.id).to eq(263567474)
-    expect(results.unit_price).to eq(3800)
+    expect(results.unit_price).to eq(38.0)
     expect(results2).to eq(nil)
   end
 
   it 'it can find an item by name' do
     results = @engine.items.find_by_name('510+ RealPush Icon Set')
     results2 = @engine.items.find_by_name('Tea For Bearded Women')
-    expect(results.id).to eq('263395237')
     expect(results.id).to eq(263395237)
-    expect(results.unit_price).to eq(1200)
+    expect(results.id).to eq(263395237)
+    expect(results.unit_price).to eq(12.0)
     expect(results2).to eq(nil)
   end
 
@@ -39,23 +39,53 @@ RSpec.describe do
     results =  @engine.items.find_all_with_description('Free standing')
     results2 = @engine.items.find_all_with_description('adfadf')
     expect(results.first.id).to eq(263396013)
-    expect(results.first.unit_price).to eq(700)
+    expect(results.first.unit_price).to eq(7.0)
     expect(results2).to eq([])
   end
 
   it "can find all with price" do
-    results = @engine.items.find_all_by_price(1300)
+    results = @engine.items.find_all_by_price(13.0)
     expect(results.first.merchant_id).to eq(12334185)
     expect(results.first.id).to eq(263395617)
     results2 = @engine.items.find_all_by_price(0.000001)
     expect(results2).to eq([])
   end
 
-it "can find_all_by_price_in_range " do
-  results = @engine.items.find_all_by_price_in_range(1300..1305)
-  expect(results.first.id).to eq("263395617")
-end
+  it "can find_all_by_price_in_range " do
+    results = @engine.items.find_all_by_price_in_range(13.0..18.0)
+    expect(results.first.id).to eq(263395617)
+  end
 
+  it '#creates attributes' do
+    results = @engine.items.create({
+        name: "Capita Defenders of Awesome 2018",
+        description: "This board both rips and shreds",
+        unit_price: BigDecimal(399.99, 5),
+        created_at: Time.now,
+        updated_at: Time.now,
+        merchant_id: 25
+      })
+    expect(results.last.id).to eq(263567475)
+  end
 
+  xit '#updates attributes' do
+    results = @engine.items.create({
+        name: "Capita Defenders of Awesome 2018",
+        description: "This board both rips and shreds",
+        unit_price: BigDecimal(399.99, 5),
+        created_at: Time.now,
+        updated_at: Time.now,
+        merchant_id: 25
+      })
+
+    # update = @engine.items.update(263567475, {unit_price: BigDecimal(379.99, 5)})
+    expect(results.last.unit_price).to eq(379.99)
+  end
+
+  it '#delete' do
+    @engine.items.delete(263567474)
+    expected = @engine.items.find_by_id(263567474)
+  expect(expected).to eq nil
+  end
 
 end
