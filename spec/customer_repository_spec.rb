bd0b3dfcf77cfc15807ec35e10a9a713d5aa27bf
Devin Pile <devinpile@gmail.com>
2021-09-18 10:57:19 -0600

    Test- Create initial tests for customer repository class

---
+++ spec/customer_repository_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'SimpleCov'
+SimpleCov.start
+
+require 'rspec'
+require 'csv'
+require './lib/customer_repository'
+require './lib/customer'
+
+describe CustomerRepository do
+  before(:each) do
+    @c = CustomerRepository.new('./data/customer.csv')
+  end
+
+  it 'exists' do
+    expect(@c).to be_a CustomerRepository
+  end
+
+  describe '#all' do
+    it 'returns all customers' do
+      expect(@c.all).to be_a Array
+
+      @c.all.each do |customer|
+        expect(customer).to be_a Customer
+      end
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'can find customer by given id' do
+      expect(@c.find_by_id(3)).to eq(@c.all[2])
+    end
+  end
+end 
