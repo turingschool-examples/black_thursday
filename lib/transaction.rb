330c2e03d9c92c83d447498f2740cfa0251cf337
Karan Mehta <karanm645@gmail.com>
2021-09-18 14:46:19 -0500

    Create and initialize transaction file

---
+++ lib/transaction.rb
@@ -0,0 +1,21 @@
+require 'bigdecimal'
+require 'time'
+
+class Transaction
+  attr_reader :id,
+              :created_at,
+              :updated_at,
+              :invoice_id,
+              :credit_card_number,
+              :credit_card_expiration_date,
+              :result
+
+  def initialize(info)
+    @id                          = info[:id].to_i
+    @created_at                  = Time.parse(info[:created_at])
+    @updated_at                  = Time.parse(info[:updated_at])
+    @invoice_id                  = info[:invoice_id].to_i
+    @credit_card_number          = info[:credit_card_number]
+    @credit_card_expiration_date = info[:credit_card_expiration_date]
+    @result                      = info[:result]
+  end
