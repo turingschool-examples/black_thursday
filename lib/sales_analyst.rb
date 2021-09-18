babe222d77408948a691fecaa7794043c3e895dd
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-18 15:00:12 -0600

    Create merch_item_hash and test

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,11 +1,29 @@
 class SalesAnalyst
 
   attr_reader :items,
-              :merchants
+              :merchants,
+              :merch_item_hash
 
   def initialize(items, merchants)
-    @items = items
-    @merchants = merchants
+    @items = items.all
+    @merchants = merchants.all
+    @merch_item_hash = hash_create #(@items, @merchants)
+  end
+
+  def hash_create #(items, merchants)
+    return_hash = {}
+    @merchants.each do |merchant|
+      @items.each do |item|
+        if merchant.id == item.merchant_id
+          if return_hash[merchant.name].nil?
+            return_hash[merchant.name] = [item]
+          else
+            return_hash[merchant.name] += [item]
+          end
+        end
+      end
+    end
+    return_hash
   end
 
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -19,6 +19,18 @@ describe SalesAnalyst do
 
       expect(sales_analyst).to be_an_instance_of(SalesAnalyst)
     end
+
+    it 'has readable attributes' do
+      se = SalesEngine.new({
+        :items => './data/items.csv',
+        :merchants => './data/merchants.csv'
+        })
+      sales_analyst = se.analyst(se.items, se.merchants)
+
+      expect(sales_analyst.items).to be_an(Array)
+      expect(sales_analyst.merchants).to be_an(Array)
+      expect(sales_analyst.merch_item_hash).to be_a(Hash)
+    end
   end
 
 end
