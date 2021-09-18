657bee386fe50575a4b1a5acca5569f2308109d2
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-18 14:44:46 -0600

    Refactor tests

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -1,20 +1,24 @@
 require 'rspec'
-require './lib/sales_analyst'
+require 'csv'
 require './lib/sales_engine'
 require './lib/merchant'
 require './lib/merchants_repository'
 require './lib/items'
 require './lib/item_repository'
-require 'csv'
-​
-describe SalesAnalyst do
-  it 'exists' do
-    se = SalesEngine.new({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-    sales_analyst = se.analyst(se.items, se.merchants)
+require './lib/sales_analyst'
 
-    expect(sales_analyst).to be_a(SalesAnalyst)
+describe SalesAnalyst do
+
+  describe '#initialize' do
+    it 'creates an instance of SalesAnalyst' do
+      se = SalesEngine.new({
+        :items => './data/items.csv',
+        :merchants => './data/merchants.csv'
+        })
+      sales_analyst = se.analyst(se.items, se.merchants)
+
+      expect(sales_analyst).to be_an_instance_of(SalesAnalyst)
+    end
   end
+
 end
