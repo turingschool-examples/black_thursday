75be66ab64e0e80be0f337b2b9632bbb41de80fd
Didi Dodson <mdodson55@gmail.com>
2021-09-19 17:07:10 -0600

    Feat: add invoice updates

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,20 +1,22 @@
 require 'csv'
 require './lib/merchantrepository'
 require './lib/itemrepository'
-# require './lib/sales_analyst'
+require './lib/invoicerepository'
 
 class SalesEngine
 
-  attr_reader :items, :merchants
+  attr_reader :items, :merchants, :invoices
 
   def initialize(data)
     @items     = data[:items]
     @merchants = data[:merchants]
+    @invoices = data[:invoices]
   end
 
   def self.from_csv(info)
     SalesEngine.new({ :items => "./data/items.csv",
-                      :merchants => "./data/merchants.csv"})
+                      :merchants => "./data/merchants.csv",
+                      :invoices => './data/invoices.csv'})
   end
 
   def merchants
@@ -36,4 +38,15 @@ class SalesEngine
     end
     ItemRepository.new(all)
   end
+
+  def invoices
+    all = []
+
+    csv = CSV.read(@invoices, headers: true, header_converters: :symbol)
+     csv.map do |row|
+       all << Invoice.new(row)
+    end
+    InvoiceRepository.new(all)
+  end
+
 end
