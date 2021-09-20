df85b0b011adb67141650e18982f14b36c8e91af
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-20 11:25:50 -0600

    Refactor syntax

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 'require_relative'
 require 'rspec'
 require './lib/sales_engine'
@@ -15,28 +17,28 @@ describe SalesEngine do
 
   describe '.from_csv' do
     it 'creates an instance of DataRepository' do
-      se = SalesEngine.from_csv({
-                                 items: "./data/items.csv",
-                                 merchants: "./data/merchants.csv"
-                                })
+      se = SalesEngine.from_csv(
+                                 items: './data/items.csv',
+                                 merchants: './data/merchants.csv'
+                                )
       expect(se).to be_an_instance_of(DataRepository)
     end
 
     it 'returns a new instance of ItemRepository with an array of item objects' do
-      se = SalesEngine.from_csv({
-                                 items: "./data/items.csv",
-                                 merchants: "./data/merchants.csv"
-                                })
+      se = SalesEngine.from_csv(
+                                 items: './data/items.csv',
+                                 merchants: './data/merchants.csv'
+                                )
       ir = se.items
 
       expect(ir).to be_an_instance_of(ItemRepository)
     end
 
     it 'returns a new instance of MerchantRepository with an array of merchant objects' do
-      se = SalesEngine.from_csv({
-                                 items: "./data/items.csv",
-                                 merchants: "./data/merchants.csv"
-                                })
+      se = SalesEngine.from_csv(
+                                 items: './data/items.csv',
+                                 merchants: './data/merchants.csv'
+                                )
       mr = se.merchants
 
       expect(mr).to be_an_instance_of(MerchantsRepository)
@@ -45,10 +47,10 @@ describe SalesEngine do
 
   describe '#analyst' do
     it 'creates a new instance of SalesAnalyst' do
-      se = SalesEngine.new({
-                            items: "./data/items.csv",
-                            merchants: "./data/merchants.csv"
-                           })
+      se = SalesEngine.new(
+                            items: './data/items.csv',
+                            merchants: './data/merchants.csv'
+                           )
       analyst = se.analyst(se.items, se.merchants)
 
       expect(analyst).to be_a(SalesAnalyst)
@@ -57,13 +59,13 @@ describe SalesEngine do
 
   describe '.from_csv' do
     it 'creates an instance of InvoiceRepository' do
-      se = SalesEngine.from_csv({invoices: "./data/invoices.csv"})
+      se = SalesEngine.from_csv(invoices: './data/invoices.csv')
 
       expect(se).to be_an_instance_of(DataRepository)
     end
 
     it 'can find an invoice by id' do
-      se = SalesEngine.from_csv({invoices: "./data/invoices.csv"})
+      se = SalesEngine.from_csv(invoices: './data/invoices.csv')
 
       expect(se.invoices.find_by_id(6)).to be_an_instance_of(Invoice)
     end
