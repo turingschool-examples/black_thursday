1c6b487b9577ab493c6ec8b0c8e0656c87669528
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-20 11:19:03 -0600

    Refactor syntax

--- spec/data_repository_spec.rb
+++ spec/data_repository_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 'require relative'
 require 'rspec'
 require './lib/sales_engine'
@@ -15,25 +17,25 @@ describe DataRepository do
 
   describe '#initialize' do
     it 'creates an instance of DataRepository' do
-      dr = DataRepository.new({
-                               items: "./data/items.csv",
-                               merchants: "./data/merchants.csv",
-                               invoices: "./data/invoices.csv"
-                              })
+      dr = DataRepository.new(
+                               items: './data/items.csv',
+                               merchants: './data/merchants.csv',
+                               invoices: './data/invoices.csv'
+                              )
 
       expect(dr).to be_an_instance_of(DataRepository)
     end
 
-   it 'has readable_attributes' do
-     dr = DataRepository.new({
-                              items: "./data/items.csv",
-                              merchants: "./data/merchants.csv",
-                              invoices: "./data/invoices.csv"
-                             })
+    it 'has readable_attributes' do
+      dr = DataRepository.new(
+                               items: './data/items.csv',
+                               merchants: './data/merchants.csv',
+                               invoices: './data/invoices.csv'
+                              )
 
-     expect(dr.items).to be_an_instance_of(ItemRepository)
-     expect(dr.merchants).to be_an_instance_of(MerchantsRepository)
-     expect(dr.invoices).to be_an_instance_of(InvoiceRepository)
-   end
+      expect(dr.items).to be_an_instance_of(ItemRepository)
+      expect(dr.merchants).to be_an_instance_of(MerchantsRepository)
+      expect(dr.invoices).to be_an_instance_of(InvoiceRepository)
+    end
   end
 end
