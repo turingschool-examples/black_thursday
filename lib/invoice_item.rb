786bc7f4d8195b8c9310eaa0d23baca44618080c
Didi Dodson <mdodson55@gmail.com>
2021-09-20 10:42:15 -0600

    Feat: add invoice_item and test

---
+++ lib/invoice_item.rb
@@ -0,0 +1,18 @@
+class InvoiceItem
+  attr_reader :id, :item_id, :created_at
+  attr_accessor :quantity, :unit_price, :updated_at
+
+  def initialize(data)
+    @id           = (data[:id]).to_i
+    @item_id      = data[:item_id].to_i
+    @invoice_id   = data[:invoice_id].to_i
+    @quantity     = data[:quantity].to_i
+    @unit_price   = data[:unit_price].to_d
+    @created_at   = data[:created_at]
+    @updated_at   = data[:updated_at]
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
+  end
+end
---
+++ test/invoice_item_spec.rb
@@ -0,0 +1,48 @@
+require './lib/item'
+require './lib/itemrepository'
+require 'bigdecimal'
+require 'bigdecimal/util'
+require 'objspace'
+require './lib/sales_engine'
+require './lib/merchant'
+require './lib/merchantrepository'
+require './lib/invoice'
+require 'csv'
+require './lib/invoice_item'
+# require './lib/invoice_item_repo'
+require 'rspec'
+
+describe InvoiceItem do
+  it 'exists' do
+    se = SalesEngine.from_csv({:invoice_item => "./data/invoice_items.csv"})
+
+    i = InvoiceItem.new({
+                    :id         => 6,
+                    :item_id    => 7,
+                    :invoice_id => 8,
+                    :quantity   => 1,
+                    :unit_price => BigDecimal(10.99, 4),
+                    :created_at  => Time.now,
+                    :updated_at  => Time.now,
+                    })
+
+    expect(i).to be_a(InvoiceItem)
+    expect(i.id).to eq(6)
+  end
+
+  it 'returns unit_price in dollars' do
+    se = SalesEngine.from_csv({:invoice_item => "./data/invoice_items.csv"})
+
+    i = InvoiceItem.new({
+                    :id         => 6,
+                    :item_id    => 7,
+                    :invoice_id => 8,
+                    :quantity   => 1,
+                    :unit_price => BigDecimal(10.99, 4),
+                    :created_at  => Time.now,
+                    :updated_at  => Time.now,
+                    })
+
+    expect(i.unit_price_to_dollars).to eq(10.99)
+  end
+end
