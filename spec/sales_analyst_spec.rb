f9856e32789c09bf77d3844e3ed28d8d6b19baaa
Christian Valesares <cvalesares@gmail.com>
2021-09-20 00:48:45 -0400

    feat:add invoice methods to sales_analyst

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -54,4 +54,31 @@ RSpec.describe SalesAnalyst do
     expect(expected.length).to eq 5
     expect(expected.first.class).to eq Item
   end
+
+  it '#average_invoices_per_merchant' do
+    expected = @sales_analyst.average_invoices_per_merchant
+    expect(expected).to eq 10.49
+    expect(expected.class).to eq Float
+  end
+
+  it "#average_invoices_per_merchant_standard_deviation" do
+    expected = @sales_analyst.average_invoices_per_merchant_standard_deviation
+
+    expect(expected).to eq 3.29
+    expect(expected.class).to eq Float
+  end
+
+  it "#top_merchants_by_invoice_count returns merchants that are two standard deviations above the mean" do
+    expected = @sales_analyst.top_merchants_by_invoice_count
+
+    expect(expected.length).to eq 12
+    expect(expected.first.class).to eq Merchant
+  end
+
+  xit "#bottom_merchants_by_invoice_count returns merchants that are two standard deviations below the mean" do
+    expected = @sales_analyst.bottom_merchants_by_invoice_count
+
+    expect(expected.length).to eq 4
+    expect(expected.first.class).to eq Merchant
+  end
 end
