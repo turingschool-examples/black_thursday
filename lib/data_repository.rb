6d63bc938088f5df29b9b5426a0141385d1c887e
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-19 19:50:55 -0600

    Create DataRepository class

---
+++ lib/data_repository.rb
@@ -0,0 +1,37 @@
+class DataRepository
+
+  attr_reader :merchants,
+              :items,
+              :invoices
+
+  def initialize(data_hash)
+    @merchants = merchant_repo(data_hash)
+    @items = item_repo(data_hash)
+    @invoices = invoice_repo(data_hash)
+  end
+
+  def merchant_repo(data_hash)
+    if data_hash[:merchants].nil?
+      nil
+    else
+      MerchantsRepository.new(data_hash[:merchants])
+    end
+  end
+
+  def item_repo(data_hash)
+    if data_hash[:items].nil?
+      nil
+    else
+      ItemRepository.new(data_hash[:items])
+    end
+  end
+
+  def invoice_repo(data_hash)
+    if data_hash[:invoices].nil?
+      nil
+    else
+      InvoiceRepository.new(data_hash[:invoices])
+    end
+  end
+
+end
