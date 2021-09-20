552d8bbb182e451e4e4ecb1a21cc2738143bcc29
Didi Dodson <mdodson55@gmail.com>
2021-09-19 18:44:22 -0600

    Feat: update invoice and invoicerepo

--- lib/invoicerepository.rb
+++ lib/invoicerepository.rb
@@ -28,62 +28,25 @@ class InvoiceRepository
      invoice.status == status
     end
   end
-#   def find_by_name(name)
-#     @all.find_all do |item|
-#       item.name == name
-#     end.pop
-#   end
-#
-#   def find_all_with_description(description)
-#
-#     @all.find_all do |item|
-#       item.description == description
-#     end
-#   end
-#   def find_all_by_price(unit_price)
-#
-#     @all.find_all do |item|
-#       item.unit_price == unit_price
-#     end
-#   end
-#   def find_all_by_price_in_range(num1, num2)
-#
-#     result = @all.find_all do |item|
-#       item.unit_price.between?(num1, num2)
-#     end
-#     result
-#   end
-#
-#   def find_all_by_merchant_id(merchant_id)
-#
-#     @all.find_all do |item|
-#       item.merchant_id == merchant_id
-#     end
-#   end
-#
-#   def create(attributes)
-#     id_max = @all.max_by {|item| item.id}
-#     attributes[:id] = id_max.id + 1
-#     new = Item.new(attributes)
-#     @all.push(new)
-#   end
-#
-#   def update(id, attributes)
-#
-#       updated_item = self.find_by_id(id)
-#       updated_item.name = attributes[:name]
-#       updated_item.description = attributes[:description]
-#       updated_item.unit_price = attributes[:unit_price]
-#     updated_item
-#
-#   end
-#
-#   def delete(id)
-#     x = (self.all).find_index(self.find_by_id(id))
-#     self.all.delete_at(x)
-#     self.all
-#   end
-#
-# end
 
+  def create(attributes)
+    id_max = @all.max_by {|invoice| invoice.id}
+    attributes[:id] = id_max.id + 1
+    new = Invoice.new(attributes)
+    @all.push(new)
+  end
+
+  def update(id, attributes)
+    updated_invoice = self.find_by_id(id)
+      updated_invoice.merge()
+      updated_invoice.status = attributes[:status]
+      updated_invoice.updated_at = attributes[:updated_at]
+    updated_invoice
+  end
+
+  def delete(id)
+    x = (self.all).find_index(self.find_by_id(id))
+    self.all.delete_at(x)
+    self.all
+  end
 end
--- test/invoicerepository_spec.rb
+++ test/invoicerepository_spec.rb
@@ -52,14 +52,85 @@ describe InvoiceRepository do
     expect(invoice1.count).to eq(12)
   end
 
-  it 'finds all by merchant id' do
+  it 'finds all by status' do
     se = SalesEngine.new({ :items => "./data/items.csv",
                       :merchants => "./data/merchants.csv",
                       :invoices => './data/invoices.csv'})
     inv = se.invoices
-    invoice1 = inv.find_all_by_merchant_id(12335955)
+    invoice1 = inv.find_all_by_status("pending")
+    invoice2 = inv.find_all_by_status("shipped")
 
     expect(invoice1).to be_a(Array)
-    expect(invoice1.count).to eq(12)
+    expect(invoice1.count).to eq(1473)
+    expect(invoice2.count).to eq(2839)
+  end
+
+  it "can create a new invoice with max_id being +1" do
+    se = SalesEngine.new({ :items => "./data/items.csv",
+                      :merchants => "./data/merchants.csv",
+                      :invoices => './data/invoices.csv'})
+    inv = se.invoices
+
+    attributes = {
+                  :id           => 4986,
+                  :customer_id  => 2,
+                  :merchant_id  => 12334839,
+                  :status       => "shipped",
+                  :created_at   => "2020-09-17",
+                  :updated_at   => "2021-08-13"
+                  }
+
+    expect(inv.all.length).to eq(4985)
+    inv.create(attributes)
+    expect(inv.all.length).to eq(4986)
+  end
+
+  xit "can update status" do
+    se = SalesEngine.new({ :items => "./data/items.csv",
+                      :merchants => "./data/merchants.csv",
+                      :invoices => './data/invoices.csv'})
+    inv = se.invoices
+
+    invoice1 = inv.find_by_id(4965)
+
+    i = {
+            :id           => 4965,
+            :customer_id  => 996,
+            :merchant_id  => 12334839,
+            :status       => "pending",
+            :created_at   => "2012-01-22",
+            :updated_at   => 2013-12-17
+            }
+
+    attributes =  {
+            :id           => 4965,
+            :customer_id  => 996,
+            :merchant_id  => 12334839,
+            :status       => "shipped",
+            :created_at   => "2012-01-22",
+            :updated_at   => Time.now
+            }
+
+    results = {
+                  :id           => 4965,
+                  :customer_id  => 996,
+                  :merchant_id  => 12334839,
+                  :status       => "shipped",
+                  :created_at   => "2012-01-22",
+                  :updated_at   => Time.now
+                  }
+
+  invoice_results = Invoice.new(results)
+  expect(inv.update(4965, attributes).merchant_id).to eq(invoice_results.merchant_id)
+  expect(inv.update(4965, attributes).customer_id).to eq(invoice_results.customer_id)
+  end
+
+  it 'deletes an invoice' do
+    se = SalesEngine.new({ :items => "./data/items.csv",
+                      :merchants => "./data/merchants.csv",
+                      :invoices => './data/invoices.csv'})
+    inv = se.invoices
+
+    expect(inv.delete(4965).length).to eq(4984)
   end
 end
