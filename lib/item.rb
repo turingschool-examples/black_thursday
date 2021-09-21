e4e946a7e039bf7d82beffd899d030c1d0ad81ba
Haewon Jeon <haewonito@gmail.com>
2021-09-21 12:29:17 -0600

    get rid of att_accessor for items

--- lib/item.rb
+++ lib/item.rb
@@ -3,12 +3,16 @@ require 'time'
 class Item
   attr_reader   :id,
                 :created_at,
-                :merchant_id
-
-  attr_accessor :name,
+                :merchant_id,
+                :name,
                 :description,
                 :unit_price,
                 :updated_at
+
+  # attr_accessor :name,
+  #               :description,
+  #               :unit_price,
+  #               :updated_at
 
   def initialize(data)
     @id = data[:id].to_i
@@ -22,5 +26,21 @@ class Item
 
   def unit_price_to_dollars
     @unit_price.to_f
+  end
+
+  def update_name(name)
+    @name = name
+  end
+
+  def update_description(description)
+    @description = description
+  end
+
+  def update_up(up)
+    @unit_price = up
+  end
+
+  def update_updated_at
+    @updated_at = Time.now
   end
 end
--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -72,16 +72,16 @@ class ItemRepository
     item_to_update = find_by_id(id)
 
     if attributes[:name] != nil
-      item_to_update.name = attributes[:name]
+      item_to_update.update_name(attributes[:name])
     end
     if attributes[:description] != nil
-      item_to_update.description = attributes[:description]
+      item_to_update.update_description(attributes[:description])
     end
     if attributes[:unit_price] != nil
-      item_to_update.unit_price = attributes[:unit_price]
+      item_to_update.update_up(attributes[:unit_price])
     end
     if item_to_update
-      item_to_update.updated_at = Time.now
+      item_to_update.update_updated_at
     end
     item_to_update
   end
