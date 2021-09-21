f7e8d189c42f38e8c8bbff3db835d05273cb41c9
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-21 10:35:05 -0600

    Refactor file paths

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -11,15 +11,17 @@ describe SalesEngine do
     it 'creates an instance of DataRepository' do
       se = SalesEngine.from_csv(
         items: './data/items.csv',
-        merchants: './data/merchants.csv'
+        merchants: './data/merchants.csv',
+        invoices: './data/invoices.csv'
       )
-      expect(se).to be_an_instance_of(DataRepository)
+      expect(se).to be_an_instance_of(SalesEngine)
     end
 
     it 'returns a new instance of ItemRepository with an array of item objects' do
       se = SalesEngine.from_csv(
         items: './data/items.csv',
-        merchants: './data/merchants.csv'
+        merchants: './data/merchants.csv',
+        invoices: './data/invoices.csv'
       )
       ir = se.items
 
@@ -29,35 +31,31 @@ describe SalesEngine do
     it 'returns a new instance of MerchantRepository with an array of merchant objects' do
       se = SalesEngine.from_csv(
         items: './data/items.csv',
-        merchants: './data/merchants.csv'
+        merchants: './data/merchants.csv',
+        invoices: './data/invoices.csv'
       )
       mr = se.merchants
 
       expect(mr).to be_an_instance_of(MerchantsRepository)
     end
-  end
 
-  describe '#analyst' do
-    it 'creates a new instance of SalesAnalyst' do
-      se = SalesEngine.new(
+    it 'returns an instance of InvoiceRepository with an array of invoice objects' do
+      se = SalesEngine.from_csv(
         items: './data/items.csv',
-        merchants: './data/merchants.csv'
+        merchants: './data/merchants.csv',
+        invoices: './data/invoices.csv'
       )
-      analyst = se.analyst(se.items, se.merchants)
+      ir = se.invoices
 
-      expect(analyst).to be_a(SalesAnalyst)
-    end
-  end
-
-  describe '.from_csv' do
-    it 'creates an instance of InvoiceRepository' do
-      se = SalesEngine.from_csv(invoices: './data/invoices.csv')
-
-      expect(se).to be_an_instance_of(DataRepository)
+      expect(ir).to be_an_instance_of(InvoiceRepository)
     end
 
     it 'can find an invoice by id' do
-      se = SalesEngine.from_csv(invoices: './data/invoices.csv')
+      se = SalesEngine.from_csv(
+        items: './data/items.csv',
+        merchants: './data/merchants.csv',
+        invoices: './data/invoices.csv'
+      )
 
       expect(se.invoices.find_by_id(6)).to be_an_instance_of(Invoice)
     end
