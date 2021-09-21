87a309cd9b3e6cbbc9f492fe226ec9001063ea84
Chris Hewitt <agop5134@gmail.com>
2021-09-21 17:35:02 -0600

    Finish transaction_repo and spec

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -12,9 +12,9 @@ class Transaction
   def initialize(data)
     @id = data[0].to_i
     @invoice_id = data[1].to_i
-    @credit_card_number = data[2].to_i
+    @credit_card_number = data[2].to_s
     @credit_card_expiration_date = data[3]
-    @result = data[4]
+    @result = data[4].to_sym
     @created_at = Time.parse(data[5])
     @updated_at = Time.parse(data[6])
   end
