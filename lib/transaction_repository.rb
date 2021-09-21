9280b60595f5e1e43439158fa17d3fa987ed7f69
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-09-21 12:15:55 -0500

    Feat: add method for 'find by id'

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -9,4 +9,16 @@ class TransactionRepository
       end
       transaction_objects)
   end
+
+  def find_by_id(id)
+    if (@all.any? do |transaction|
+      transaction.id == id
+    end) == true
+      @all.find do |transaction|
+        transaction.id == id
+      end
+    else
+      nil
+    end
+  end
 end
