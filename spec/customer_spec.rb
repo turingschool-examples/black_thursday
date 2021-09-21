d0415376636710ed106e9124917c130389df596b
Mallory Vining <mallorygallery12@gmail.com>
2021-09-20 20:50:44 -0600

    feat: Alter created and updated at

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -3,26 +3,28 @@ require './lib/customer'
 RSpec.describe Customer do
   it 'exists' do
     c = Customer.new({
-                      :id => 6,
-                      :first_name => "Joan",
-                      :last_name => "Clarke",
-                      :created_at => Time.now,
-                      :updated_at => Time.now
+                      :id => 1,
+                      :first_name => "Joey",
+                      :last_name => "Ondricka",
+                      :created_at => "2012-03-27 14:54:09 UTC",
+                      :updated_at => "2012-03-27 14:54:09 UTC"
                       })
   expect(c).to be_an_instance_of(Customer)
 end
 
   it 'has attributes' do
     c = Customer.new({
-                      :id => 6,
-                      :first_name => "Joan",
-                      :last_name => "Clarke",
-                      :created_at => Time.now,
-                      :updated_at => Time.now
+                      :id => 1,
+                      :first_name => "Joey",
+                      :last_name => "Ondricka",
+                      :created_at => "2012-03-27 14:54:09 UTC",
+                      :updated_at => "2012-03-27 14:54:09 UTC"
                       })
-  expect(c.id).to eq(6)
-  expect(c.first_name).to eq("Joan")
-  expect(c.last_name).to eq("Clarke")
-  expect(c.created_at).to eq(Time.now)
+
+  expect(c.id).to eq(1)
+  expect(c.first_name).to eq("Joey")
+  expect(c.last_name).to eq("Ondricka")
+  expect(c.created_at).to eq("2012-03-27 14:54:09 UTC")
+  expect(c.updated_at).to eq("2012-03-27 14:54:09 UTC")
   end
 end
