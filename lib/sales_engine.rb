37055c4982c4d38a222a0bbba68b97a96bcbab7a
Cole Henson <colehenson95@gmail.com>
2021-09-20 21:01:01 -0600

    fix: adjusted sales_engine and related tests to not take arguments

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,17 +1,43 @@
 class SalesEngine
   attr_reader :items,
-              :merchants
+              :merchants,
+              :invoices
 
   def initialize(merch_item_hash)
-    @items = ItemRepository.new(merch_item_hash[:items])
-    @merchants = MerchantsRepository.new(merch_item_hash[:merchants])
+    @items = item_creation(merch_item_hash)
+    @merchants = merchant_creation(merch_item_hash)
+    @invoices = invoice_creation(merch_item_hash)
+  end
+
+  def item_creation(merch_item_hash)
+    if merch_item_hash[:items].nil?
+      nil
+    else
+      ItemRepository.new(merch_item_hash[:items])
+    end
+  end
+
+  def merchant_creation(merch_item_hash)
+    if merch_item_hash[:merchants].nil?
+      nil
+    else
+      MerchantsRepository.new(merch_item_hash[:merchants])
+    end
+  end
+
+  def invoice_creation(merch_item_hash)
+    if merch_item_hash[:invoices].nil?
+      nil
+    else
+      InvoiceRepository.new(merch_item_hash[:invoices])
+    end
   end
 
   def self.from_csv(item_merch_hash)
     DataRepository.new(item_merch_hash)
   end
 
-  def analyst(items, merchant)
-    SalesAnalyst.new(items, merchants)
+  def analyst
+    SalesAnalyst.new(@items, @merchants, @invoices)
   end
 end
