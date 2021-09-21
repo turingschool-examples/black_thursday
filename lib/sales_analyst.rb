2ae9a37a6c74e7061bfd1015508592746bd1429e
Cole Henson <colehenson95@gmail.com>
2021-09-21 11:35:15 -0600

    business intelligence methods I2

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,6 +2,7 @@
 require 'BigDecimal'
 require './lib/invoice'
 require './lib/invoice_repository'
+require './lib/invoice_item'
 
 # SalesAnalyst class creates handles all analysis of merch, items, and invoices
 
@@ -9,7 +10,8 @@ class SalesAnalyst
   attr_reader :items,
               :merchants,
               :merch_item_hash,
-              :invoices
+              :invoices,
+              :status
 
   def initialize(items, merchants, invoices)
     @items = item_assign(items)
@@ -143,52 +145,71 @@ class SalesAnalyst
     (@invoices.length.to_f / @merchants.length.to_f).round(2)
   end
 
+  def merchant_invoice_count
+    merchant_invoice_hash = {}
+    invoices.each do |invoice|
+      merchants.each do |merchant|
+        if invoice.merchant_id == merchant.id
+          merchant_invoice_hash[merchant] ||= 0
+          merchant_invoice_hash[merchant] += 1
+        end
+      end
+    end
+    merchant_invoice_hash
+  end
+
   def average_invoices_per_merchant_standard_deviation
     sum_diff_squared = 0
     avg = average_invoices_per_merchant
-    @invoices.each do |invoice|
-      sum_diff_squared += ((@invoices.count - avg)**2)
+    merchant_invoice_count.each do |key, value|
+      sum_diff_squared += ((value - avg)**2)
     end
-    ((sum_diff_squared / @invoices.length.to_f)**0.5).round(2)
+    ((sum_diff_squared / (merchants.length.to_f - 1))**0.5).round(2)
+  end
+
+
+  def top_merchants_by_invoice_count
+    result_array = []
+    two_sd = (average_invoices_per_merchant_standard_deviation) * 2
+    @merchants.each do |merchant|
+      if invoices.length > (two_sd)
+        result_array.append(merchant)
+      end
+    end
+  end
+
+
+  def bottom_merchants_by_invoice_count
+    result_array = []
+    two_sd = average_invoices_per_merchant_standard_deviation * 2
+    @merchants.each do |merchant|
+    if (invoices.length + two_sd) < average_invoices_per_merchant
+      result_array.append(merchant)
+    end
   end
 end
-#
-#   def top_merchants_by_invoice_count
-#     result_array = []
-#     @merchants.each do |merchant|
-#       if invoices.length > (average_invoices_per_merchant_standard_deviation) * 2
-#         result_array.append(merchant)
-#       end
-#     end
-#   end
-#
-#
-#   def bottom_merchants_by_invoice_count
-#     result_array = []
-#     two_sd = average_invoices_per_merchant_standard_deviation * 2
-#     @merchants.each do |merchant|
-#     if (invoices.length + two_sd) < average_invoices_per_merchant
-#       result_array.append(merchant)
-#     end
-#   end
-# end
-#
-#   def top_days_by_invoice_count
-#     result_array = []
-#     invoice_created = @invoices.created_at
-#     average_created_at = average_invoices_per_merchant.created_at
-#     sd = average_invoices_per_merchant_standard_deviation
-#     @merchants.each do |merchant|
-#       if invoice_created.length > average_created_at.length + sd
-#         result_array.append(merchant)
-#       end
-#     end
-#   end
-#
-#   def invoice_status(status)
-#     ((@invoices.status.count / @invoices.length) * 100).to_f
-#   end
-#
-#   # def invoice_paid_in_full?(invoice_id)
-#
-# end
+
+  def top_days_by_invoice_count
+    result_array = []
+    invoice_created = @invoices.created_at
+    average_created_at = average_invoices_per_merchant.created_at
+    sd = average_invoices_per_merchant_standard_deviation
+    @merchants.each do |merchant|
+      if invoice_created.length > average_created_at.length + sd
+        result_array.append(merchant)
+      end
+    end
+  end
+
+  def invoice_status_count(status)
+    invoices.map do |invoice|
+      invoice if invoice.status.to_sym == status
+    end.compact
+  end
+  def invoice_status(status)
+    ((invoice_status_count(status).length.to_f / invoices.length) * 100).to_f.round(2)
+  end
+end
+
+  def invoice_paid_in_full?(invoice_id)
+  end
