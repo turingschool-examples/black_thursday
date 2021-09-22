f73c7f8ea2924bc4bfdcfda641d17eaea771bcc6
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-21 20:16:47 -0600

    Test - Add update_info and total_revenue tests to merchant spec

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -17,7 +17,23 @@ describe Merchant do
 
       expect(m.id).to eq(7)
       expect(m.name).to eq('Turing School')
+      expect(m.total_revenue).to eq(0)
     end
   end
 
+  describe '#update_info' do
+    it 'can update a merchant name' do
+      merch_hash = {id: 7, name: "Turing School"}
+      m = Merchant.new(merch_hash)
+      attributes = {
+        name: 'Hogwarts School of Witchcraft and Wizardry',
+        id: 26
+      }
+
+      m.update_info(attributes)
+
+      expect(m.id).to eq(7)
+      expect(m.name).to eq('Hogwarts School of Witchcraft and Wizardry')
+    end
+  end
 end
