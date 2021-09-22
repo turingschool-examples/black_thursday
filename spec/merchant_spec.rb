59927cd0a8cb0c5605b23ae706badbe7aeb23a32
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-21 20:25:29 -0600

    Refactor - Round 2, Made changes per hound specs

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -5,14 +5,14 @@ describe Merchant do
 
   describe '#initialize' do
     it 'is an intance of Merchant class' do
-      merch_hash = {id: 7, name: 'Turing School'}
+      merch_hash = { id: 7, name: 'Turing School' }
       m = Merchant.new(merch_hash)
 
       expect(m).to be_an_instance_of(Merchant)
     end
 
     it 'has readable attributes' do
-      merch_hash = {id: 7, name: 'Turing School'}
+      merch_hash = { id: 7, name: 'Turing School' }
       m = Merchant.new(merch_hash)
 
       expect(m.id).to eq(7)
@@ -23,7 +23,7 @@ describe Merchant do
 
   describe '#update_info' do
     it 'can update a merchant name' do
-      merch_hash = {id: 7, name: 'Turing School'}
+      merch_hash = { id: 7, name: 'Turing School' }
       m = Merchant.new(merch_hash)
       attributes = {
         name: 'Hogwarts School of Witchcraft and Wizardry',
