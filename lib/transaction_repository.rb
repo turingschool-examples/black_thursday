40468450d2c33c3eae906eef0adeef1c8f63b7c2
Andrew Patterson <andrew.patterson747@gmail.com>
2021-09-21 20:47:42 -0500

    Feat: Completed find_by invoice, credit,results. Incomplete create method

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -21,4 +21,50 @@ class TransactionRepository
       nil
     end
   end
+
+  def find_all_by_invoice_id(invoice_id)
+    matched = []
+    @all.each do |transaction|
+      if transaction.invoice_id.to_i == invoice_id
+        matched << transaction
+      else
+        []
+      end
+    end
+    matched
+  end
+
+  def find_all_by_credit_card_number(credit_card_number)
+    matched = []
+    @all.each do |transaction|
+      if transaction.credit_card_number.to_i == credit_card_number
+        matched << transaction
+      else
+        []
+      end
+    end
+    matched
+  end
+
+  def find_all_by_result(result)
+    matched = []
+    @all.each do |transaction|
+      if transaction.result == result
+        matched << transaction
+      else
+        []
+      end
+    end
+    matched
+  end
+
+  def new_highest_id
+    last = @all.last
+    new_high = last.id.to_i
+    new_high += 1
+    new_high.to_s
+  end
+
+  def create(attribute)
+  end
 end
--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -21,21 +21,60 @@ RSpec.describe do
     transaction_path = './data/transactions.csv'
     transaction_repository = TransactionRepository.new(transaction_path)
     example_transaction = transaction_repository.all[25]
-    
+
     expect(transaction_repository.find_by_id(example_transaction.id)).to eq example_transaction
     expect(transaction_repository.find_by_id(999999)).to eq nil
   end
+
+  it 'can find all transactions by invoice_id' do
+    transaction_path = './data/transactions.csv'
+    transaction_repository = TransactionRepository.new(transaction_path)
+    example_transaction1 = transaction_repository.all[23]
+    example_transaction2 = transaction_repository.all[24]
+
+    expect(transaction_repository.find_all_by_invoice_id(3477)).to eq [example_transaction1, example_transaction2]
+    expect(transaction_repository.find_all_by_invoice_id(999999)).to eq []
+  end
+
+  it 'can find all transactions by credit_card_number' do
+    transaction_path = './data/transactions.csv'
+    transaction_repository = TransactionRepository.new(transaction_path)
+    example_transaction1 = transaction_repository.all[23]
+
+    expect(transaction_repository.find_all_by_credit_card_number(4970472137240748)).to eq [example_transaction1]
+    expect(transaction_repository.find_all_by_invoice_id(999999)).to eq []
+  end
+
+  it 'can find all transactions by result' do
+    transaction_path = './data/transactions.csv'
+    transaction_repository = TransactionRepository.new(transaction_path)
+
+    expect(transaction_repository.find_all_by_result("failed").count).to eq 827
+    expect(transaction_repository.find_all_by_result("success").count).to eq 4158
+  end
+
+  it 'can make a new highest_id' do
+    transaction_path = './data/transactions.csv'
+    transaction_repository = TransactionRepository.new(transaction_path)
+
+    expect(transaction_repository.new_highest_id).to eq "4986"
+  end
+
+  xit 'can make a new transaction' do
+    transaction_path = './data/transactions.csv'
+    transaction_repository = TransactionRepository.new(transaction_path)
+    new_transaction.Transaction.new = {
+      id: transaction_repository.new_highest_id,
+      invoice_id: "111",
+      credit_card_number: "1111111111111111",
+      credit_card_expiration_date: "1111",
+      result: "success",
+      created_at: "now",
+      updated_at: "just a moment ago"
+      }
+
+    expect(transaction_repository.create(new_transaction)).to eq
+  end
+
+
 end
-
-
-
-
-
-# t = Transaction.new({
-#   :id => 6,
-#   :invoice_id => 8,
-#   :credit_card_number => "4242424242424242",
-#   :credit_card_expiration_date => "0220",
-#   :result => "success",
-#   :created_at => Time.now,
-#   :updated_at => Time.now
