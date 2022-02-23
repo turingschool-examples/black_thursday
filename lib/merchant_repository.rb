1796fcf6f63b7d26cc4eeabd3c20dfed725db756
James Harkins <jh.harkins@protonmail.com>
2022-02-22 20:40:24 -0700

    test: pass merchant_repository find_by_id

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -10,9 +10,19 @@ class MerchantRepository
     @filename = filename
   end
 
-  def all
+  def rows
     rows = CSV.read(@filename, headers: true, header_converters: :symbol)
+  end
+
+  def all
     result = rows.map {|row| Merchant.new(row)}
+  end
+
+  def find_by_id(id)
+    result = rows.find_all do |row|
+      Merchant.new(row) if row[:id] == id
+    end
+    !result.empty? ? result : nil 
   end
 
 end
--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -21,4 +21,13 @@ RSpec.describe MerchantRepository do
       expect(@merchant_repo.all.count).to eq(19)
     end
   end
+
+  describe '#find_by_id' do
+    it 'can create merchant instances for each id passed as an argument' do
+
+      expect(@merchant_repo.find_by_id("12334105").count).to eq(1)
+
+      expect(@merchant_repo.find_by_id("00000000")).to eq(nil)
+    end
+  end
 end
