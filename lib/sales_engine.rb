1da15007afb9160112f7dd961b6845ecf17aed02
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-23 16:01:24 -0700

    Feat: SalesEngine.merchants works as expected

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,5 +1,6 @@
 # require "./data/items"
 # require "./data/merchants"
+require_relative "merchant_repository"
 require_relative 'item'
 require_relative 'merchant'
 require 'csv'
@@ -16,7 +17,14 @@ class SalesEngine
     merchants = CSV.read(files[:merchants])
     merchants.shift
     merchants.each {|data| @@merchants << Merchant.new({:id => data[0], :name => data[1]})}
+  end
+
+  def self.merchants
     MerchantRepository.new(@@merchants)
+  end
+
+  def self.items
+    ItemRepository.new(@@items)
   end
 
 end
