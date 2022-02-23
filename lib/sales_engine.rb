937b9c57a2b579ec57a55e8bc06ea7e782864e39
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-23 15:14:45 -0700

    Feat: Salesengine can make instances of Item and Merchant class

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,15 +1,22 @@
 # require "./data/items"
 # require "./data/merchants"
+require_relative 'item'
+require_relative 'merchant'
 require 'csv'
 
 class SalesEngine
-  def initialize
-    @items = ''
-    @merchants = ''
-  end
+  attr_reader :items, :merchants
+    @@items = []
+    @@merchants = []
 
   def self.from_csv(files)
-    @items = files[:items]
-    @merchants = files[:merchants]
+    items = CSV.read(files[:items])
+    items.shift(2)
+    items.each {|data| @@items << Item.new({:id => data[0], :name => data[1], :description => data[2], :unit_price => data[3], :created_at => Time.now, :updated_at => Time.now, :merchant_id => data[4]})}
+    merchants = CSV.read(files[:merchants])
+    merchants.shift
+    merchants.each {|data| @@merchants << Merchant.new({:id => data[0], :name => data[1]})}
   end
+
 end
+require "pry"; binding.pry
