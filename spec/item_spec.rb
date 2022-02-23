b6b25ed9742717a53943adc888eeabe6b04211e0
Andrew Speth <speth.andrew@yahoo.com>
2022-02-23 15:23:50 -0700

    feat: Item has attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,5 @@
 require './lib/item'
+require 'bigdecimal'
 
 RSpec.describe Item do
   it "has attributes" do
@@ -6,7 +7,7 @@ RSpec.describe Item do
       :id          => 1,
       :name        => "Pencil",
       :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
+      :unit_price  => BigDecimal(10.99,4),
       :created_at  => Time.now,
       :updated_at  => Time.now,
       :merchant_id => 2
@@ -15,10 +16,9 @@ RSpec.describe Item do
     expect(i.id).to eq(1)
     expect(i.name).to eq("Pencil")
     expect(i.description).to eq("You can use it to write things")
-    expect(i.unit_price).to eq(BigDecimal.new(10.99,4))
-    expect(i.created_at).to eq(Time.now)
-    expect(i.updated_at).to eq(Time.now)
-    expect(i.updated_at).to eq(Time.now)
+    expect(i.unit_price).to eq(BigDecimal(10.99,4))
+    expect(i.created_at).to be_an_instance_of(Time)
+    expect(i.updated_at).to be_an_instance_of(Time)
     expect(i.merchant_id).to eq(2)
   end
 end
