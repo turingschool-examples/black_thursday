11045d68bd4f2f8a522aaccaf60e595834dab9a4
Andrew Speth <speth.andrew@yahoo.com>
2022-02-23 15:35:36 -0700

    test: Item can convert unit price to dollars

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,6 @@
 class Item
   attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
-  
+
   def initialize(attributes)
     @id = attributes[:id]
     @name = attributes[:name]
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -21,4 +21,18 @@ RSpec.describe Item do
     expect(i.updated_at).to be_an_instance_of(Time)
     expect(i.merchant_id).to eq(2)
   end
+
+  it "can convert unit price to dollars" do
+    i = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+      })
+
+    expect(i.unit_price_to_dollars).to eq(10.99)
+  end
 end
