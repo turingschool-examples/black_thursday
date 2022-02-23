2b92edf3211207afb10ef3b42f8ba8bf20e020ad
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-23 16:32:25 -0500

    Create existence test.

--- test/item_spec.rb
+++ test/item_spec.rb
@@ -1,5 +1,16 @@
 require './lib/item'
 
 RSpec.describe 'Item' do
-  
+  item = Item.new({
+    :id => 1,
+    :name => "Pencil",
+    :description => "You can use it to write things",
+    :unit_price => BigDecimal.new(10.99,4),
+    :created_at => Time.now,
+    :updated_at => Time.now,
+    :merchant_id => 2
+  })
+  it 'exists' do
+    expect(item).to be_an_instance_of(Item)
+  end
 end
