ad4a6bb3386cea0b9c55180ac355c5c95bd19b28
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 15:40:54 -0700

    Fix: refactor Item test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,19 +1,24 @@
 require 'rspec'
 require 'csv'
+require 'bigdecimal'
 require './lib/item'
 
 RSpec.describe Item do
-  it "exists" do
-    i = Item.new({
+  before (:each) do
+    @i = Item.new({
       :id          => 1,
       :name        => "Pencil",
       :description => "You can use it to write things",
-      :unit_price  => BigDecimal.new(10.99,4),
+      :unit_price  => BigDecimal(10.99,4),
       :created_at  => Time.now,
       :updated_at  => Time.now,
       :merchant_id => 2
     })
-
-    expect(i).to be_an_instance_of(Item)
   end
+  
+  it "exists" do
+    expect(@i).to be_an_instance_of(Item)
+  end
+
+
 end
