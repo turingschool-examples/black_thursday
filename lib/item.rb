e5609210150bd83965defb0726c678cb04d3a173
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-23 16:48:11 -0500

    feat: Add item class with readable attributes from hash.

---
+++ lib/item.rb
@@ -0,0 +1,13 @@
+class Item
+  attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
+
+  def initialize(item)
+    @id = item[:id]
+    @name = item[:name]
+    @description = item[:description]
+    @unit_price = item[:unit_price]
+    @created_at = item[:created_at]
+    @updated_at = item[:updated_at]
+    @merchant_id = item[:merchant_id]
+  end
+end
--- test/item_spec.rb
+++ test/item_spec.rb
@@ -1,16 +1,29 @@
 require './lib/item'
+require 'bigdecimal'
 
 RSpec.describe 'Item' do
   item = Item.new({
     :id => 1,
     :name => "Pencil",
     :description => "You can use it to write things",
-    :unit_price => BigDecimal.new(10.99,4),
+    :unit_price => BigDecimal(10.99,4),
     :created_at => Time.now,
     :updated_at => Time.now,
     :merchant_id => 2
   })
+
+  puts BigDecimal(10.99,4)
+
   it 'exists' do
     expect(item).to be_an_instance_of(Item)
+  end
+
+  it 'has all expected attributes' do
+    expect(item.id).to eq(1)
+    expect(item.name).to eq("Pencil")
+    expect(item.description).to eq("You can use it to write things")
+    expect(item.unit_price).to eq(BigDecimal(10.99,4))
+    expect(item.created_at).to eq(item.updated_at)
+    expect(item.merchant_id).to eq(2)
   end
 end
