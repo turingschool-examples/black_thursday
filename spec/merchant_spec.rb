bfba026df91a0e85e17c38e46050105ba0a4a02d
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-22 20:21:24 -0700

    test: merchant

---
+++ spec/merchant_spec.rb
@@ -0,0 +1,15 @@
+require './lib/merchant'
+
+RSpec.describe Merchant do
+  before :each do
+    m = Merchant.new(5, 'Turing School')
+  end
+  it 'exists' do
+    expect(@m).to be_a(Merchant)
+  end
+
+  it 'has attributes' do
+    expect(@m.id).to eq(5)
+    expect(@m.name).to eq('Turing School')
+  end
+end
