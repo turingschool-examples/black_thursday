8de77f061311421b54857c9fc13abf90b9235d26
Joseph Senecal <jasenecal2@gmail.com>
2022-02-23 17:00:19 -0700

    Add find method tests for item repository

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -17,5 +17,37 @@ RSpec.describe ItemRepository do
     expect(repo.all[0]).to be_an_instance_of(Item)
   end
 
+  it 'finds by id' do
+    item1 = Item.new({id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC", updated_at: "2:34pm UTC", merchant_id: 001})
+    item2 = Item.new({id: 2, name: "test-test", description: "another test description", unit_price: 3, created_at: "12:00am utc", udpated_at:"12:00pm UTC", merchant_id: 001})
+    repo = ItemRepository.new([item1, item2])
+    expect(repo.find_by_id(1)).to be(item1)
+  end
+
+  it 'finds by name' do
+    item1 = Item.new({id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC", updated_at: "2:34pm UTC", merchant_id: 001})
+    item2 = Item.new({id: 2, name: "test-test", description: "another test description", unit_price: 3, created_at: "12:00am utc", udpated_at:"12:00pm UTC", merchant_id: 001})
+    repo = ItemRepository.new([item1, item2])
+    expect(repo.find_by_name("TeSt")).to be(item1)
+
+  end
+
+  it "finds by description" do
+    item1 = Item.new({id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC", updated_at: "2:34pm UTC", merchant_id: 001})
+    item2 = Item.new({id: 2, name: "test-test", description: "another test description", unit_price: 3, created_at: "12:00am utc", udpated_at:"12:00pm UTC", merchant_id: 001})
+    repo = ItemRepository.new([item1, item2])
+
+    expect(repo.find_all_with_description("TE")).to eq([item1, item2])
+  end
+
+  it "finds by price" do
+    item1 = Item.new({id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC", updated_at: "2:34pm UTC", merchant_id: 001})
+    item2 = Item.new({id: 2, name: "test-test", description: "another test description", unit_price: 3, created_at: "12:00am utc", udpated_at:"12:00pm UTC", merchant_id: 001})
+    repo = ItemRepository.new([item1, item2])
+
+    expect(repo.find_all_by_price(1)).to eq([item1])
+    expect(repo.find_all_by_price(2)).to eq([])
+    expect(repo.find_all_by_price(3)).to eq([item2])
+  end
 
 end
