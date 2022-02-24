b6d09ccb868090cf9155d96911f806158b409d18
Sherman Au <shermau13@gmail.com>
2022-02-24 16:46:55 -0700

    test: find_by_name item_repo_spec

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,4 +1,4 @@
-#item_repository_spec
+# item_repository_spec
 require './lib/item_repository'
 # require './lib/items'
 require './lib/sales_engine'
@@ -7,9 +7,9 @@ require 'pry'
 RSpec.describe ItemRepository do
   before(:each) do
     @se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-    })
+                                 items: './data/items.csv',
+                                 merchants: './data/merchants.csv'
+                               })
   end
 
   it 'exists' do
@@ -21,20 +21,23 @@ RSpec.describe ItemRepository do
   end
 
   it 'can return an item id' do
-    id = 263567376
+    id = 263_567_376
     expected = @se.items.find_by_id(id)
     expect(expected.id).to eq(id)
-    expect(expected.name).to eq("The Gold Coast, Chicago, Illinois")
+    expect(expected.name).to eq('The Gold Coast, Chicago, Illinois')
   end
 
   it 'will return nil if improper id entered' do
-    id = 000
+    id = 0o00
     expected = @se.items.find_by_id(id)
 
     expect(expected).to eq(nil)
   end
 
+  xit 'can find by name' do
+    name = 'Bird houses'
+    expected = @se.items.find_by_name(name)
 
-
-
+    expect(expected.id).to eq(263_550_286)
+  end
 end
