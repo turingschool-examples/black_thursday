30aab5f6dae535535b8d90f409e17026a5abba24
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-23 19:25:32 -0800

    Test: pass item can read attributes

---
+++ spec/item_spec.rb
@@ -0,0 +1,30 @@
+require 'csv'
+require './lib/item'
+require './lib/sales_engine'
+require './spec/spec_helper'
+
+  RSpec.describe Item do
+    before (:each) do
+      @item = Item.new({ id: "263399361",
+                        name: "Exotic Beaded Women&#39;s or Men&#39;s Handmade Bracelet or Anklet",
+                        description: "Exotic Women&#39;s or Men&#39;s Handmade Bracelet or Anklet.  Sizes from 6 inches to 9 inches.  Made with premium crafted beads.  You may request colors and styles.  Most are made to order.  Email me after order is placed for specific or custom handmade designs, etc.",
+                        unit_price: "4995",
+                        merchant_id: "12334303",
+                        created_at: "2016-01-11 11:23:02 UTC",
+                        updated_at: "2012-10-27 09:12:45 UTC" })
+    end
+
+    context 'Item' do
+      it 'can read Item attributes' do
+        expect(@item.id).to eq("263399361")
+        expect(@item.name).to eq("Exotic Beaded Women&#39;s or Men&#39;s Handmade Bracelet or Anklet")
+        expect(@item.description).to eq("Exotic Women&#39;s or Men&#39;s Handmade Bracelet or Anklet.  Sizes from 6 inches to 9 inches.  Made with premium crafted beads.  You may request colors and styles.  Most are made to order.  Email me after order is placed for specific or custom handmade designs, etc.")
+        expect(@item.unit_price).to eq("4995")
+        expect(@item.merchant_id).to eq("12334303")
+        expect(@item.created_at).to eq("2016-01-11 11:23:02 UTC")
+        expect(@item.updated_at).to eq("2012-10-27 09:12:45 UTC")
+      end
+    end
+
+
+end
