bf37ade290c40f8000708f9859673318e8b3b54d
Joseph Senecal <jasenecal2@gmail.com>
2022-02-23 20:43:26 -0700

    Add final class method tests for ItemRepository class

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe ItemRepository do
     expect(repo.find_all_by_price_in_range(1..3)).to eq([item1, item2])
   end
 
-  xit "finds all items by a given merchant number" do
+  it "finds all items by a given merchant number" do
     item1 = Item.new({id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC", updated_at: "2:34pm UTC", merchant_id: 001})
     item2 = Item.new({id: 2, name: "test-test", description: "another test description", unit_price: 3, created_at: "12:00am utc", udpated_at:"12:00pm UTC", merchant_id: 001})
     repo = ItemRepository.new([item1, item2])
@@ -89,4 +89,28 @@ RSpec.describe ItemRepository do
     expect(repo.all[2].id).to eq(3)
   end
 
+  xit "can update an item's name, description and unit_price while re-stamping the updated_at with a current timestamp" do
+    item1 = Item.new({id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC", updated_at: "2:34pm UTC", merchant_id: 001})
+    item2 = Item.new({id: 2, name: "test-test", description: "another test description", unit_price: 3, created_at: "12:00am utc", udpated_at:"12:00pm UTC", merchant_id: 001})
+    item3 = Item.new({id: 3, name: "Test Item 3", description: "test_description", unit_price: 5, created_at: "9:07pm UTC", updated_at: "7:26am UTC", merchant_id: 002})
+    repo = ItemRepository.new([item1, item2, item3])
+
+    repo.update(2, {id: 49, name: "Updated Test-Test", description: "This description is updated", unit_price: 9.99, merchant_id: 75})
+
+    expect(repo.all[1].id).to eq(2)
+    expect(repo.all[1].name).to eq("Updated Test-Test")
+    expect(repo.all[1].unit_price).to eq(9.99)
+    expect(repo.all[1].description).to eq("This description is updated")
+    expect(repo.all[1].updated_at).not_to eq("12:00pm UTC")
+  end
+
+  xit "can delete an item from the library based on given id" do
+    item1 = Item.new({id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC", updated_at: "2:34pm UTC", merchant_id: 001})
+    item2 = Item.new({id: 2, name: "test-test", description: "another test description", unit_price: 3, created_at: "12:00am utc", udpated_at:"12:00pm UTC", merchant_id: 001})
+    item3 = Item.new({id: 3, name: "Test Item 3", description: "test_description", unit_price: 5, created_at: "9:07pm UTC", updated_at: "7:26am UTC", merchant_id: 002})
+    repo = ItemRepository.new([item1, item2, item3])
+
+    repo.delete(2)
+    expect(repo.all).to eq([item1, item3])
+  end
 end
