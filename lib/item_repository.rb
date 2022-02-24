04689a63c757631422cbb999e05b6bd391a89018
Joseph Senecal <jasenecal2@gmail.com>
2022-02-23 20:43:58 -0700

    Add find_all_by_merchant functionality

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -31,7 +31,10 @@ class ItemRepository < SalesEngine
 
   def find_all_by_price_in_range(price_range_object)
     @all.find_all {|item| price_range_object.member?(item.unit_price)}
-
   end
+
+  def find_all_by_merchant_id(merchant_id)
+    @all.find_all {|item| item.merchant_id == merchant_id}
+  end 
 
 end
