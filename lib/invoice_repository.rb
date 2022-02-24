95f9673f3038ee264ff95c188d3fcaa99eb2829b
Susan Boyd <sueboyd922@gmail.com>
2022-02-24 10:02:34 -0500

    feat: Invoice Repository, create repository

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -2,8 +2,21 @@ require 'csv'
 require './lib/invoice'
 
 class InvoiceRepository
+  attr_reader :repository
 
   def initialize(file)
+    @invoices = CSV.read(file, headers: true, header_converters: :symbol)
+
+    @repository = @invoices.map do |invoice|
+          Invoice.new({
+            :id => invoice[:id],
+            :customer_id => invoice[:customer_id],
+            :merchant_id => invoice[:merchant_id],
+            :status => invoice[:status],
+            :created_at => invoice[:created_at],
+            :updated_at => invoice[:updated_at]
+            })
+          end
   end
 
 end
--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -2,11 +2,18 @@ require 'rspec'
 require './lib/invoice_repository'
 
 describe InvoiceRepository do
+  before (:each) do
+    @ir = InvoiceRepository.new('./data/invoices.csv')
+  end
+
   describe '#initialize' do
     it 'exists' do
-      ir = InvoiceRepository.new('./data/invoices.csv')
+      expect(@ir).to be_an_instance_of(InvoiceRepository)
+    end
 
-      expect(ir).to be_an_instance_of(InvoiceRepository)
+    it 'creates a repository of Invoice items' do
+      expect(@ir.repository.class).to eq(Array)
+      expect(@ir.repository.sample.class).to eq(Invoice)
     end
   end
 end
