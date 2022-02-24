4cd681ae74a66c4897d4868ac541791ba3c9acf4
Chris Romero <chrisleeromero1@gmail.com>
2022-02-24 13:58:36 -0700

    Test: add sales engine initialize test and sales engine class

---
+++ lib/sales_engine.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/item'
+require_relative '../lib/merchant'
+require_relative '../lib/merchant_repository'
+require_relative '../lib/item_repository'
+require 'CSV'
+
+class SalesEngine
+
+  # def initialize(items_data_file, merchants_data_file)
+  #   @items = ItemRepository.new(items_data_file)
+  #   @merchants = MerchantRepository.new(merchants_data_file)
+  # end
+
+  def initialize
+  end
+
+
+end
---
+++ spec/sales_engine_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../lib/sales_engine'
+require_relative '../lib/item'
+require_relative '../lib/merchant'
+require_relative '../lib/merchant_repository'
+require_relative '../lib/item_repository'
+require 'CSV'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.describe SalesEngine do
+
+  describe '#initialize' do
+
+    it "it exists" do
+      se = SalesEngine.new
+      expect(se).to be_a(SalesEngine)
+    end
+
+    it "can instantiate a merchants repository" do
+      se = SalesEngine.from_csv({
+            :items     => "./data/items.csv",
+            :merchants => "./data/merchants.csv",
+            })
+      mr = se.merchants
+      expect(mr).to be_a(MerchantRepository)
+
+    end
+
+
+  end
+
+
+
+end
