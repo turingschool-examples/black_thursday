be61d6f684c3f609608ba4751f873ba96ffbc5a8
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-24 13:24:59 -0700

    Refactor: Simplify ID attr_reader method

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,17 +1,13 @@
 require 'csv'
 
 class Merchant
-  attr_reader :created_at
+  attr_reader :created_at, :id
   attr_accessor :name, :updated_at
   def initialize(data)
     @name = data[:name]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
-    @id = data[:id]
-  end
-
-  def id
-    @id.to_i
+    @id = data[:id].to_i
   end
 
   def self.read_file(csv)
