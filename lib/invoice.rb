bb5d14a95495f1d11b9eda8c8453c7583cd1144f
Susan Boyd <sueboyd922@gmail.com>
2022-02-24 09:49:02 -0500

    tests: Test invoice attributes

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,9 +1,15 @@
 # require 'csv'
 
 class Invoice
+  attr_reader :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
 
   def initialize(info)
-    @info = info
+    @id = info[:id]
+    @customer_id = info[:customer_id]
+    @merchant_id = info[:merchant_id]
+    @status = info[:status]
+    @created_at = info[:created_at]
+    @updated_at = info[:updated_at]
   end
 
 end
--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -3,17 +3,33 @@ require './lib/invoice'
 
 describe Invoice do
   describe '#initialize' do
-    it 'exists' do
-      invoice = Invoice.new({
-          :id => 6,
-          :customer_id => 7,
-          :merchant_id => 8,
-          :status => "pending",
-          :created_at => Time.now,
-          :updated_at => Time.now
-          })
+    invoice = Invoice.new({
+      :id => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status => "pending",
+      :created_at => Time.now,
+      :updated_at => Time.now
+      })
 
+    it 'exists' do
       expect(invoice).to be_an_instance_of(Invoice)
     end
+
+    it 'has its own id and customer and merchant ids' do
+      expect(invoice.id).to eq(6)
+      expect(invoice.customer_id).to eq(7)
+      expect(invoice.merchant_id).to eq(8)
+    end
+
+    it 'has a status' do
+      expect(invoice.status).to eq("pending")
+    end
+
+    xit 'can tell you when it was created and updated' do
+      expect(invoice.created_at).to eq(Time.now)
+      expect(invoice.updated_at).to eq(Time.now)
+    end
+
   end
 end
