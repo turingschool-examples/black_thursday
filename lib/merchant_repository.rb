742cc9a581789187f011c8bc482e90df0497a93b
Andrew Speth <speth.andrew@yahoo.com>
2022-02-24 14:38:57 -0700

    test: SalesEngine can pull items and merchants from csv

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -2,10 +2,10 @@
 require 'CSV'
 
 
-merchant_contents = CSV.open('./data/merchants.csv', headers: true, header_converters: :symbol)
-
-merchant_contents.each do |row|
-merchant_id = row[:id]
-puts "#{merchant_id}"
-end
+# merchant_contents = CSV.open('./data/merchants.csv', headers: true, header_converters: :symbol)
+#
+# merchant_contents.each do |row|
+# merchant_id = row[:id]
+# puts "#{merchant_id}"
+# end
 # p merchant_contents
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,9 +1,12 @@
 #sales_engine
+# require './lib/item_repository'
+# require './lib/merchant_repository'
+
 class SalesEngine
   attr_reader :items, :merchants
 
-  def initialize(hash)
-    @items = hash[:items]
-    @merchants = hash[:merchants]
+  def initialize(items, merchants)
+    @items = ItemRepository.new(items)
+    @merchants = MerchantRepository.new(merchants)
   end
 end
