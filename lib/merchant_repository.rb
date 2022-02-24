f85cf1e148417788517ed6b0dfee64406ad0c870
Chris Kjolhede <ckjolhede@gmail.com>
2022-02-23 18:27:48 -0700

    wrote test for #opening csv files

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,11 @@
+#merchant_repository
+require 'CSV'
+
+
+merchant_contents = CSV.open('./data/merchants.csv', headers: true, header_converters: :symbol)
+
+merchant_contents.each do |row|
+merchant_id = row[:id]
+puts "#{merchant_id}"
+end
+# p merchant_contents
