246f9ec02467518c5137b13b3c4dd7fb0821b0ec
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-25 11:04:44 -0700

    Test: SalesEngine can create instance of ItemsRepo

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -1,15 +1,25 @@
 require 'rspec'
 require 'csv'
 require './lib/sales_engine'
+require './lib/items_repository'
+require './lib/merchants_repository'
 
 RSpec.describe SalesEngine do
-  it "exists" do
-    se = SalesEngine.from_csv({
+  before (:each) do
+    @se = SalesEngine.from_csv({
   :items     => "./data/items.csv",
   :merchants => "./data/merchants.csv",
   })
+  end
 
-  expect(se).to be_a(SalesEngine)
+  it "exists" do
+  expect(@se).to be_a(SalesEngine)
+  end
+
+  it "creates an instnace of Item class" do
+    ir = @se.items
+
+    expect(ir).to be_an_instance_of(ItemsRepository)
   end
 
 end #RSpec end
