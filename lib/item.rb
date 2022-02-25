b92b580fab73b6a3ce4cfb59b71416bab59d4928
Sherman Au <shermau13@gmail.com>
2022-02-25 12:03:38 -0700

    refactor: attr_accessor to include name, description, unit_price, updated_at

--- lib/item.rb
+++ lib/item.rb
@@ -1,5 +1,6 @@
 class Item
-  attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
+  attr_accessor :name, :description, :unit_price, :updated_at
+  attr_reader :id, :created_at, :merchant_id
 
   def initialize(attributes)
     @id = attributes[:id].to_i
