91c39c4daaf851c07937b2ddc021fcc3df7e3160
Scott Sullivan <scottsullivanltd@gmail.com>
2022-02-24 18:34:52 -0700

    Refactor: add atts to Merchant class, Feat: add working methods for merchant and item, Test: add test functionality to Sales Engine spec

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,21 +1,44 @@
 require "csv"
+require "merchant"
 
 class SalesEngine
-
-  # se = SalesEngine.from_csv({
-  #   :items     => "./data/items.csv",
-  #   :merchants => "./data/merchants.csv",
-  # })
+  # attr_reader :merchants_data, :items_data
+ def initialize(data)
+   @items_data = data[:items]
+   @merchants_data = data[:merchants]
+ end
 
   def self.from_csv(argument)
 
-    @items = []
-    @items = CSV.read(argument[:items], headers: true, header_converters: :symbol)
+    items = CSV.read(argument[:items], headers: true, header_converters: :symbol)
+    merchants = CSV.read(argument[:merchants], headers: true, header_converters: :symbol)
 
-    @merchants = []
-    @merchants = CSV.read(argument[:merchants], headers: true, header_converters: :symbol)
-
-    return {:items=> @items, :merchants=> @merchants}
+    SalesEngine.new({:items=> items, :merchants=> merchants})
   end
+
+  def items
+    @items_data
+  end
+
+  def merchants
+    @merchants_data
+  end
+
+  def items_instanciator
+    items_instances_array = []
+    items.by_row!.each do |row|
+      items_instances_array << Item.new(row)
+    end
+    items_instances_array
+  end
+
+  def merchants_instanciator
+    merchants_instances_array = []
+    merchants.by_row!.each do |row|
+      merchants_instances_array << Merchant.new(row)
+    end
+    merchants_instances_array
+  end
+
 
 end
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -1,25 +1,30 @@
 require "csv"
 require './lib/sales_engine'
+require "./lib/merchant"
 require 'pry'
 
 RSpec.describe SalesEngine do
 
+  se = SalesEngine.from_csv({
+    :items     => "./data/items.csv",
+    :merchants => "./data/merchants.csv",
+  })
+
   it "exists" do
-    se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-    })
-    expect(se[:merchants]).to be_an_instance_of(CSV::Table)
+
+    expect(se).to be_an_instance_of(SalesEngine)
+
   end
 
   it "returns merchants" do
-    se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-    })
-    merchant_repo = se[:merchants]
-    # merchant = merchant_repo.find_by_name("CJsDecor")
-    expect(merchant_repo.count).to eq(475)
+    # binding.pry
+    expect(se.items).to be_an_instance_of(CSV::Table)
+    expect(se.merchants).to be_an_instance_of(CSV::Table)
+  end
+
+  it "creates instances of merchant and item" do
+    expect(se.items_instanciator[0]).to be_an_instance_of(Item)
+    expect(se.merchants_instanciator[0]).to be_an_instance_of(Merchant)
   end
 
 end
