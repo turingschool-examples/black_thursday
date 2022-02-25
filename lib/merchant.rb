a34d600ea61315d2fcf227bcabf81ec3f53b7486
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-25 11:22:52 -0700

    Refactor: Merchant initialize

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -2,16 +2,7 @@ class Merchant
   attr_reader :merchant_attributes
 
   def initialize(attributes)
-    merchant_attributes = {}
-    merchant_attributes[:id] = attributes[:id].to_i
-    merchant_attributes[:name] = attributes[:name]
-    merchant_attributes[:created_at] = attributes[:created_at]
-    merchant_attributes[:updated_at] = attributes[:updated_at]
-    @merchant_attributes = merchant_attributes
-
-    # @id = attributes[:id]
-    # @name = attributes[:name]
-    # @created_at = attributes[:created_at]
-    # @updated_at = attributes[:updated_at]
+    @merchant_attributes = attributes
+    @merchant_attributes[:id] = merchant_attributes[:id].to_i
   end
 end
