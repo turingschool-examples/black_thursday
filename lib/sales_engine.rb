31f54ec34526492b08bd0c4c72a235a98ebb3f5d
Will Medders <wmedders@gmail.com>
2022-02-24 21:42:55 -0500

    Feat: Begin testing sales engine

---
+++ lib/sales_engine.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class SalesEngine
+  attr_reader :items, :merchants
+  def initialize(table_hash)
+      @items = []
+    @merchants = []
+  end
+
+  def self.from_csv(path_hash)
+    table_hash = Hash.new
+    path_hash.each do |name, path|
+      csv = CSV.read(path, headers: true, header_converters: :symbol)
+      table_hash[name] = csv
+      binding.pry
+    end
+
+    SalesEngine.new({table_hash})
+  end
---
+++ spec/sales_engine_spec.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require './lib/sales_engine'
+require 'rspec'
+require 'simplecov'
+
+SimpleCov.start
+
+RSpec.describe SalesEngine do
+
+
+
+
+end
