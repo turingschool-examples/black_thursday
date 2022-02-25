c428b858c54c352b32532f19d3151b15678bc51f
Will Medders <wmedders@gmail.com>
2022-02-24 19:29:29 -0500

    Feat: merchant repo tests working through #create

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,6 +1,47 @@
+require 'pry'
 class MerchantRepository
-  attr_reader 
-  def initialize(argument)
-    @argument = argument
+  def initialize(merchants)
+    @merchants = merchants
   end
+
+  def all
+    @merchants
+  end
+
+  def find_by_id(id)
+    @merchants.find do |merchant|
+        merchant.id == id
+    end
+  end
+
+  def find_by_name(name)
+    @merchants.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
+  def find_all_by_name(name)
+    @merchants.find_all do |merchant|
+      merchant.name.downcase.include?(name.downcase)
+    end
+  end
+
+#helper method for create
+  def highest_id
+    last_id = 0
+    @merchants.max do |merchant|
+      last_id = merchant.id
+    end
+    return last_id
+  end
+
+  def create(attributes)
+    attributes[:name] = Merchant.new({:id => self.highest_id + 1, :name => attributes[:name]})
+  end
+
+
+
+
+
+
 end
--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -2,13 +2,77 @@ require 'pry'
 require 'rspec'
 require 'simplecov'
 require './lib/merchant_repository'
+require './lib/merchant'
+
 SimpleCov.start
 
 RSpec.describe MerchantRepository do
-  xit '' do
+  it 'exists' do
+    mr = MerchantRepository.new([])
+    expect(mr.class).to eq(MerchantRepository)
+  end
+
+  it 'can return an array of all merchant objects' do
+    m_1 = Merchant.new({:id => 1, :name => "True Value"})
+    m_2 = Merchant.new({:id => 2, :name => "Home Depot"})
+    m_3 = Merchant.new({:id => 3, :name => "Walmart"})
+    m_4 = Merchant.new({:id => 4, :name => "K Mart"})
+    mr = MerchantRepository.new([m_1, m_2, m_3, m_4])
+    expect(mr.all).to eq([m_1, m_2, m_3, m_4])
+  end
+
+  it 'can find a merchant by id' do
+    m_1 = Merchant.new({:id => 1, :name => "True Value"})
+    m_2 = Merchant.new({:id => 2, :name => "Home Depot"})
+    m_3 = Merchant.new({:id => 3, :name => "Walmart"})
+    m_4 = Merchant.new({:id => 4, :name => "K Mart"})
+    mr = MerchantRepository.new([m_1, m_2, m_3, m_4])
+    expect(mr.find_by_id(3)).to eq(m_3)
+  end
+
+  it 'can find a merchant by name (case insensitive)' do
+    m_1 = Merchant.new({:id => 1, :name => "True Value"})
+    m_2 = Merchant.new({:id => 2, :name => "Home Depot"})
+    m_3 = Merchant.new({:id => 3, :name => "Walmart"})
+    m_4 = Merchant.new({:id => 4, :name => "K Mart"})
+    mr = MerchantRepository.new([m_1, m_2, m_3, m_4])
+    expect(mr.find_by_name("k MarT")).to eq(m_4)
+  end
+
+  it 'can find merchants by name fragment (case insensitive)' do
+    m_1 = Merchant.new({:id => 1, :name => "True Value"})
+    m_2 = Merchant.new({:id => 2, :name => "Home Depot"})
+    m_3 = Merchant.new({:id => 3, :name => "Walmart"})
+    m_4 = Merchant.new({:id => 4, :name => "K Mart"})
+    mr = MerchantRepository.new([m_1, m_2, m_3, m_4])
+    expect(mr.find_all_by_name("MarT")).to eq([m_3, m_4])
+  end
+
+  it 'can find the highest merchant id (helper method)' do
+    m_1 = Merchant.new({:id => 1, :name => "True Value"})
+    m_2 = Merchant.new({:id => 2, :name => "Home Depot"})
+    m_3 = Merchant.new({:id => 3, :name => "Walmart"})
+    m_4 = Merchant.new({:id => 4, :name => "K Mart"})
+    mr = MerchantRepository.new([m_1, m_2, m_3, m_4])
+    expect(mr.highest_id).to eq(4)
+  end
+
+  it 'can create a merchant' do
+    m_1 = Merchant.new({:id => 1, :name => "True Value"})
+    m_2 = Merchant.new({:id => 2, :name => "Home Depot"})
+    m_3 = Merchant.new({:id => 3, :name => "Walmart"})
+    m_4 = Merchant.new({:id => 4, :name => "K Mart"})
+    mr = MerchantRepository.new([m_1, m_2, m_3, m_4])
+    binding.pry
+    expect(mr.create(:id => 9000, :name => "Walgreens").class).to eq(Merchant)
   end
 
 
 
 
+
+
 end
+
+
+# m = merchant.new({:id => 5, :name => "Turing School"})
