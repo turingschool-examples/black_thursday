399f07a51f30d2acc505301dc525a8d67178a2db
Chris Kjolhede <ckjolhede@gmail.com>
2022-02-25 09:31:14 -0700

    merchant_repostitory_spec start: exists tested

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -5,15 +5,19 @@ require 'pry'
 
 RSpec.describe MerchantRepository do
   before(:each) do
-    # @mr = MerchantRepository.new
+    @se = SalesEngine.from_csv({
+                                 items: './data/items.csv',
+                                 merchants: './data/merchants.csv'
+                               })
   end
 
-  # it 'exists' do
-  #   expect(@mr).to be_a(MerchantRepository)
-  # end
+  it 'exists' do
+    expect(@se.merchants).to be_a(MerchantRepository)
+  end
+
 
   xit "can return an array of #all elements within the merchant.csv file" do
-  expect(mr.all.length).to eq(475)
+  expect@se.merchants.all.length).to eq(475)
   end
 
 end
