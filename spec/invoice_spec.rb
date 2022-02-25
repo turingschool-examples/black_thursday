f23a3ccc35ae7104636e0eed064129f077871eaf
reuben <creubend@gmail.com>
2022-02-25 10:59:09 -0700

    test: Test for creating a Invoice class

---
+++ spec/invoice_spec.rb
@@ -0,0 +1,15 @@
+require "./lib/invoice"
+require "rspec"
+RSpec.describe Invoice do
+  it "exists" do
+    i = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => 2009-02-07,
+      :updated_at  => 2014-03-15})
+  expect(i).to be_a(Invoice)
+    
+  end
+end
