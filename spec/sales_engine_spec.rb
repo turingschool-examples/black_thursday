39e74dbc11ec30a25c1a3b2b366ae8208b806f46
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-25 11:15:41 -0700

    Test: SalesEngine can create instance of InvoiceItemsRepo

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -3,12 +3,15 @@ require 'csv'
 require './lib/sales_engine'
 require './lib/items_repository'
 require './lib/merchants_repository'
+require './lib/invoice_items_repository'
 
 RSpec.describe SalesEngine do
   before (:each) do
     @se = SalesEngine.from_csv({
   :items     => "./data/items.csv",
   :merchants => "./data/merchants.csv",
+  :invoice_items => "./data/invoice_items.csv",
+  :invoices => "./data/invoices.csv"
   })
   end
 
@@ -26,6 +29,12 @@ RSpec.describe SalesEngine do
     mr = @se.merchants
 
     expect(mr).to be_an_instance_of(MerchantsRepository)
+  end
+
+  it "creates an instance of invoice_items class" do
+    invoice_items_repo = @se.invoice_items_repo
+
+    expect(invoice_items_repo).to be_an_instance_of(InvoiceItemsRepository)
   end
 
 end #RSpec end
