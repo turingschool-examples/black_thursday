793e7951d33b2cf1b4eff7797ff8bebbb754da87
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-25 10:43:34 -0700

    Feat: incorporate RepositoryAide in ItemsRepo methods

--- lib/items_repository.rb
+++ lib/items_repository.rb
@@ -1,8 +1,10 @@
 require 'csv'
 require './lib/item'
 require 'pry'
+require './lib/repository_aide'
 
 class ItemsRepository
+  include RepositoryAide
   attr_reader :repository
 
   def initialize(file)
@@ -62,8 +64,7 @@ class ItemsRepository
   end
 
   def create(attributes)
-    attributes[:id] = (@repository.last.id.to_i + 1).to_s
-    item = Item.new(attributes)
+    item = Item.new({id: new_id.to_s, name: attributes[:name], description: attributes[:description], unit_price: attributes[:unit_price], created_at: Time.now, updated_at: Time.now, merchant_id: attributes[:merchant_id]})
     @repository << item
     item
   end
