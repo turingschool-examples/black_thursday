744bd3a62c2d52e5efc7b82bc00f90945a628418
Rin Fabian <pfabianschool@gmail.com>
2022-02-25 10:40:43 -0600

    feat; created a method able to find an index by id into the module. fix; changed the name of the module and fixed syntax in the tests

--- lib/module.rb
+++ lib/module.rb
@@ -1,3 +1,17 @@
-module Module
-  
+module IDManager
+  def find_by_id(id)
+    @all.find{|index| index.id == id}
+  end
+
+  def find_by_name(search)
+  end
+
+  def create(attributes)
+  end
+
+  def update(id, attributes)
+  end
+
+  def delete(id)
+  end
 end
--- spec/module_spec.rb
+++ spec/module_spec.rb
@@ -1,38 +1,38 @@
 require './lib/module'
 require './lib/merchant_repository'
 
-RSpec.describe Module do
+RSpec.describe IDManager do
   before (:each) do
-    merchant1 = double("merchant", id: 1234, name: "pencil")
-    merchant2 = double("merchant", id: 1320, name: "eraser")
-    mr = MerchantRepository.new([merchant1, merchant2])
+    @merchant1 = double("merchant", id: 1234, name: "pencil")
+    @merchant2 = double("merchant", id: 1320, name: "eraser")
+    @mr = MerchantRepository.new([@merchant1, @merchant2])
   end
 
   it 'can find units by id' do
-    expect(mr.find_by_id(1234)).to eq([merchant1])
-    expect(mr.find_by_id(1320)).to eq([merchant2])
+    expect(@mr.find_by_id(1234)).to eq(@merchant1)
+    expect(@mr.find_by_id(1320)).to eq(@merchant2)
 
   end
 
-  it 'can find a unit by name' do
-    expect(mr.find_by_name("pencil")).to eq([merchant1])
-    expect(mr.find_by_name("eraser")).to eq([merchant2])
+  xit 'can find a unit by name' do
+    expect(@mr.find_by_name("pencil")).to eq([@merchant1])
+    expect(@mr.find_by_name("eraser")).to eq([@merchant2])
   end
 
 
-  it 'can create attributes' do
+  xit 'can create attributes' do
     mr.create(name: "book")
-    expect(mr.all.last.id).to eq(1321)
+    expect(@mr.all.last.id).to eq(1321)
 
   end
 
-  it 'can update an instance with a specific id' do
-    mr.update(1320, name: "chicken")
-    expect(mr.all[1].name).to eq("chicken")
+  xit 'can update an instance with a specific id' do
+    @mr.update(1320, name: "chicken")
+    expect(@mr.all[1].name).to eq("chicken")
   end
 
-  it 'can delete an id' do
-    mr.delete(1320)
-    expect(mr.all).to eq([merchant1])
+  xit 'can delete an id' do
+    @mr.delete(1320)
+    expect(@mr.all).to eq([@merchant1])
   end
 end
