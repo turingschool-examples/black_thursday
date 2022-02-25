3437abaaa5e408e3cfedff05d056a3e45c7d7742
Kim G <xiaole.guo@yahoo.com>
2022-02-25 07:26:59 -0700

    change items and merchants to attributes in SalesEngine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,28 +1,27 @@
 require 'csv'
 require 'pry'
-
+require_relative 'merchant_repository'
 
 class SalesEngine
-attr_reader :merchant_lines, :item_lines
+attr_reader :merchants, :items
   def initialize(merchants, items)
-    @merchant_lines = merchants
-    @item_lines = items
+    @merchants = MerchantRepository.new(merchants)
+    @items = ItemRepository.new(items)
   end
 
   def self.from_csv(input)
     merchant_lines = File.readlines input[:merchants]
     item_lines = File.readlines input[:items]
-    # binding.binding.pry
     SalesEngine.new(merchant_lines, item_lines)
   end
 
-  def merchants
-    MerchantRepository.new(@merchant_lines)
-    # binding.pry
-  end
-
-  def items
-    ItemRepository.new(@item_lines)
-  end
+  # def merchants
+  #   MerchantRepository.new(@merchant_lines)
+  #   # binding.pry
+  # end
+  #
+  # def items
+  #   ItemRepository.new(@item_lines)
+  # end
 
 end
