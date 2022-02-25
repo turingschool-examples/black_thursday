b6e100e694c3111a07511a376692fbd5e477c4e2
Will Medders <wmedders@gmail.com>
2022-02-24 22:33:11 -0500

    Feat: sales engine all test passing

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,10 +1,13 @@
 require 'pry'
 require 'csv'
+require './lib/item'
+require './lib/merchant'
+
 class SalesEngine
-  attr_reader :items, :merchants
+  attr_reader :items_csv_object, :merchants_csv_object
   def initialize(table_hash)
-      @items = []
-    @merchants = []
+      @items_csv_object = table_hash[:items]
+    @merchants_csv_object = table_hash[:merchants]
   end
 
   def self.from_csv(path_hash)
@@ -12,9 +15,21 @@ class SalesEngine
     path_hash.each do |name, path|
       csv = CSV.read(path, headers: true, header_converters: :symbol)
       table_hash[name] = csv
-      # binding.pry
     end
-
     SalesEngine.new(table_hash)
   end
+
+  def items
+    item_array = @items_csv_object.map do |row|
+      Item.new(row)
+    end
+    ItemRepository.new(item_array)
+  end
+
+  def merchants
+    merchant_array = @merchants_csv_object.map do |row|
+      Merchant.new(row)
+    end
+    MerchantRepository.new(merchant_array)
+  end  
 end
