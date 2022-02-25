e6afeb0b01e5d97f870c8eb345c533a84ab38f2d
Andrew Speth <speth.andrew@yahoo.com>
2022-02-25 09:00:44 -0700

    refactor: sales_engine_spec

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -1,25 +1,13 @@
 require './lib/sales_engine'
 
 RSpec.describe SalesEngine do
-  xit "exists" do
-    se = SalesEngine.new({:items => "here are items", :merchants => "here are merchants"})
-
-    expect(se).to be_a(SalesEngine)
-  end
-
-  xit "can access items and merchants" do
-    se = SalesEngine.new({:items => "here are items", :merchants => "here are merchants"})
-
-    expect(se.items).to eq("here are items")
-    expect(se.merchants).to eq("here are merchants")
-  end
-
   it "can pull items and merchants from csv" do
     se = SalesEngine.from_csv({
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
     })
 
+    expect(se).to be_a(SalesEngine)
     expect(se.items).to be_a(ItemRepository)
     expect(se.merchants).to be_a(MerchantRepository)
     expect(se.items.items.length).to eq(1367)
