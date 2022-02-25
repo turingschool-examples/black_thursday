5d7d38e766e6a97477e407606af398869c6fd0c3
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-02-24 17:05:06 -0700

    Add: test merchants with high item count method and start of method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -25,8 +25,44 @@ class SalesAnalyst
     ((merchant_items.values.sum).to_f / merchant_items.keys.count).round(2)
 
   end
-
+# set = [3,4,5]
+#
+# std_dev = sqrt( ( (3-4)^2+(4-4)^2+(5-4)^2 ) / 2 )
   def average_items_per_merchant_standard_deviation
+    merchant_ids = @items.items.map {|item| item.merchant_id}
+    merchant_items = Hash.new(0)
+    merchant_ids.each do |id|
+      merchant_items[id] += 1
+    end
+
+    average_merchant_items = ((merchant_items.values.sum).to_f / merchant_items.keys.count)
+    empty = []
+    merchant_items.values.each do |number|
+      result = (number - average_merchant_items) * (number - average_merchant_items)
+      empty << result
+    end
+
+    Math.sqrt(empty.sum / (merchant_items.count - 1)).round(2)
+  end
+
+  def merchants_with_high_item_count
+    merchant_ids = @items.items.map {|item| item.merchant_id}
+    merchant_items = Hash.new(0)
+    merchant_ids.each do |id|
+      merchant_items[id] += 1
+    end
+
+    average_merchant_items = ((merchant_items.values.sum).to_f / merchant_items.keys.count)
+    item_count_standard = average_merchant_items + average_items_per_merchant_standard_deviation
+
+    # h = merchant_items.find_all do |key,value|
+    #   if value > item_count_standard
+    #     key
+    #   end
+    # end
+    #
+    # h.map {|key| key[0]}
+
 
   end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -32,6 +32,10 @@ RSpec.describe SalesAnalyst do
       expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
     end
 
+    it 'dispays merchants with high item count' do
+      expect(@sales_analyst.merchants_with_high_item_count).to to_include(Merchant)
+    end
+
   end
 
 end
