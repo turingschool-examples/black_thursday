b05427695169c9532101c95c83f0437ccb114184
reuben <creubend@gmail.com>
2022-02-25 11:12:59 -0700

    test: Readable attributes test created and passed

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,1 +1,12 @@
-
+class Invoice 
+  attr_reader :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
+    def initialize(info)
+      @id = info[:id]
+      @customer_id = info[:customer_id]
+      @merchant_id = info[:merchant_id]
+      @status = info[:status]
+      @created_at = info[:created_at]
+      @updated_at = info[:updated_at]
+    end
+    
+end 
--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -9,7 +9,22 @@ RSpec.describe Invoice do
       :status      => "pending",
       :created_at  => 2009-02-07,
       :updated_at  => 2014-03-15})
-  expect(i).to be_a(Invoice)
-    
+    expect(i).to be_a(Invoice)
+  end
+  
+  it "has readable attributes" do
+    i = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => 2009-02-07,
+      :updated_at  => 2014-03-15})
+    expect(i.id).to eq(6)
+    expect(i.customer_id).to eq(7)
+    expect(i.merchant_id).to eq(8)
+    expect(i.status).to eq("pending")
+    expect(i.created_at).to eq(2009-02-07)
+    expect(i.updated_at).to eq(2014-03-15)
   end
 end
