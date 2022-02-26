99743238f2b21e10ea0cdde7d29693c836747d6a
Joseph Senecal <jasenecal2@gmail.com>
2022-02-25 17:31:56 -0700

    Remove test for defunct ItemRepository method ::from_csv

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -13,12 +13,6 @@ RSpec.describe ItemRepository do
     expect(repo.all[0]).to be_an_instance_of(Item)
   end
 
-  it "initializes #from_csv" do
-    repo = ItemRepository.from_csv("./data/items_short.csv")
-    expect(repo).to be_an_instance_of(ItemRepository)
-    expect(repo.all.length).to eq(3)
-    expect(repo.all[0]).to be_an_instance_of(Item)
-  end
 
   it 'finds by id' do
     item1 = Item.new({ id: 1, name: "test", description: "test_description", unit_price: 1, created_at: "8:07pm UTC",
