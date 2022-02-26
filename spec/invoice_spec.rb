9c89ffefacab34c7dfdb76a7a8efeccd123c45db
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-25 18:20:42 -0700

    Test: Add invoice_spec.rb

---
+++ spec/invoice_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/item'
+require_relative '../lib/merchant'
+require_relative '../lib/merchant_repository'
+require_relative '../lib/item_repository'
+require_relative '../lib/sales_engine'
+require_relative '../lib/sales_analyst'
+require_relative '../lib/invoice'
+require 'CSV'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.describe Invoice do
+
+
+
+
+end
