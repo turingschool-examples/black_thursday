463a32c163a65c46b4060cd0242b747eb7847d10
Joseph Downs <jos.w.downs@gmail.com>
2022-02-26 11:47:16 -0700

    Fix average and stan dev method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,4 +1,15 @@
+require './mathable'
+require './merchants_repository'
+require './items_repository'
+require 'pry'
+
 class Analyst
+  include Mathable
+
+  def initialize
+    @mr = MerchantsRepository.new("./data/merchants.csv")
+    @ir = ItemsRepository.new("./data/items.csv")
+  end
 
   def get_merchants
     merchant_ids = @mr.repository.map do |merchant|
@@ -19,15 +30,17 @@ class Analyst
   end
 
   def average_items_per_merchant
-    @mean_per_merchant = average(@items_per_merchant_count)
+    average(@ir.repository.count, @mr.repository.count)
   end
 
   def average_items_per_merchant_standard_deviation
-    standard_devation(@items_per_merchant_count, @mean_per_merchant)
+    merchant_item_numbers = @ir.merchant_ids.values.map { |list| list.count }
+    @stn_dev_ipm = standard_devation(merchant_item_numbers, average_items_per_merchant)
   end
 
   def merchants_with_high_item_count
-    #find merchants with more than one standard_devation higher than average
+    # @items_per_merchant_count.find_all { |}
+    #find merchants with more items than one standard_devation higher than average
   end
 
   def average_item_price_per_merchant(merchant_id)
---
+++ spec/sales_analyst_spec.rb
@@ -0,0 +1,16 @@
+require './lib/sales_analyst'
+require 'rspec'
+
+describe Analyst do
+  before (:each) do
+    @sales_analyst = Analyst.new
+  end
+
+  it "finds average" do
+    expect(@sales_analyst.average_items_per_merchant).to eq(2.88)
+  end
+
+  it "finds standard_devation" do
+    expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
+  end
+end
