3c40b3b799f6f8e39923b658b52786d559f97508
Will Medders <wmedders@gmail.com>
2022-02-26 13:07:51 -0500

    Spec: added all tests (skipped) for Trans Repo

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -18,7 +18,72 @@ RSpec.describe TransactionRepository do
     expect(tr.all).to eq([])
   end
 
+  it 'can find a transaction by id' do
+    t = Transaction.new({
+                          :id => 6,
+                          :invoice_id => 8,
+                          :credit_card_number => "4242424242424242",
+                          :credit_card_expiration_date => "0220",
+                          :result => "success",
+                          :created_at => Time.now,
+                          :updated_at => Time.now
+                        })
+    tr = TransactionRepository.new([t])
+    # binding.pry
+    # expect(tr.find_by_id(7)).to eq(nil)
+    expect(tr.find_by_id(6)).to eq(t)
+  end
 
+  xit 'can find an array of transactions by invoice id' do
+    t = Transaction.new({
+                          :id => 6,
+                          :invoice_id => 8,
+                          :credit_card_number => "4242424242424242",
+                          :credit_card_expiration_date => "0220",
+                          :result => "success",
+                          :created_at => Time.now,
+                          :updated_at => Time.now
+                        })
+    tr = TransactionRepository.new([t])
+    expect(tr.find_all_by_invoice_id(7)).to eq([])
+    expect(tr.find_all_by_invoice_id(8)).to eq([t])
+  end
+
+  xit 'can find an array of transactions by cc number' do
+    t = Transaction.new({
+                          :id => 6,
+                          :invoice_id => 8,
+                          :credit_card_number => "4242424242424242",
+                          :credit_card_expiration_date => "0220",
+                          :result => "success",
+                          :created_at => Time.now,
+                          :updated_at => Time.now
+                        })
+    tr = TransactionRepository.new([t])
+    expect(tr.find_all_by_credit_card_number("5747635673574567")).to eq([])
+    expect(tr.find_all_by_credit_card_number("4242424242424242")).to eq([t])
+  end
+
+  xit 'can find an array of transactions by result' do
+    t = Transaction.new({
+                          :id => 6,
+                          :invoice_id => 8,
+                          :credit_card_number => "4242424242424242",
+                          :credit_card_expiration_date => "0220",
+                          :result => "success",
+                          :created_at => Time.now,
+                          :updated_at => Time.now
+                        })
+    tr = TransactionRepository.new([t])
+    expect(tr.find_all_by_result("failure")).to eq([])
+    expect(tr.find_all_by_result("success")).to eq([t])
+  end
+
+  xit 'can create a new transaction with the highest transaction id' do
+  end
+
+  xit 'can delete a transaction with the corresponding id' do
+  end
 
 
 end
