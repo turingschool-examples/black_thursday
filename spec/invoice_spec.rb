a211888e6ae3dc99db1e34768387a1fbe6739638
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-25 18:25:04 -0700

    Test: add tesst for Invoice class existing

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -7,11 +7,24 @@ require_relative '../lib/sales_analyst'
 require_relative '../lib/invoice'
 require 'CSV'
 require 'simplecov'
+require 'bigdecimal'
 SimpleCov.start
 
 RSpec.describe Invoice do
 
+  before(:each) do @i = Invoice.new({
+                                    :id          => 6,
+                                    :customer_id => 7,
+                                    :merchant_id => 8,
+                                    :status      => "pending",
+                                    :created_at  => Time.now,
+                                    :updated_at  => Time.now,
+                                  })
+  end
 
+  it 'exists' do
+    expect(@i).to be_a(Invoice)
+  end
 
 
 end
