d318a9e765632fba53f4ed32d8f9636b4c9440e2
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-25 18:27:19 -0700

    Feat: add code to pass Invoice exists test:

---
+++ lib/invoice.rb
@@ -0,0 +1,21 @@
+require_relative '../lib/item'
+require_relative '../lib/merchant'
+require_relative '../lib/merchant_repository'
+require_relative '../lib/item_repository'
+require_relative '../lib/sales_engine'
+require_relative '../lib/sales_analyst'
+require 'bigdecimal'
+require 'time'
+
+class Invoice
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @customer_id = attributes[:customer_id]
+    @merchant_id = attributes[:merchant_id]
+    @status = attributes[:status]
+    @created_at = attributes[:created_at]
+    @updated_at = attributes[:updated_at]
+  end
+
+end
