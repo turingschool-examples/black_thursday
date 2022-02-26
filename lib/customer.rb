6e099d9a777ba6a479a28f1df0844a768beb637a
Susan Boyd <sueboyd922@gmail.com>
2022-02-26 10:33:02 -0500

    create Customer class

---
+++ lib/customer.rb
@@ -0,0 +1,14 @@
+
+class Customer
+  attr_reader :id, :created_at
+  attr_accessor :first_name, :last_name, :updated_at
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @created_at = attributes[:created_at]
+    @updated_at = attributes[:updated_at]
+  end
+
+end
