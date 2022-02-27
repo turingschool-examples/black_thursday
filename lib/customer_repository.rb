3581ff89bfbcccaab1ad8a7f5771d9ae9a9041ca
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-27 12:50:53 -0700

    feat: add find_all_by_last_name

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,25 +1,37 @@
-require 'time'
-require 'CSV'
-require 'customer'
+require_relative '../lib/findable.rb'
+require_relative '../lib/customer.rb'
 
 class CustomerRepository
+  include Findable
   attr_reader :all
 
-  def initialize(path)
-    @all = []
-    populate_repository(path)
+  def initialize(customer_array)
+    @all = customer_array
   end
 
-  def populate_repository(path)
-    CSV.read(path, headers: true, header_converters: :symbol) do |row|
-      info_hash = {
-        id: row[:id].to_i,
-        first_name: row[:first_name],
-        last_name: row[:last_name],
-        created_at: Time.parse(row[:created_at]),
-        updated_at: Time.parse(row[:updated_at])
-      }
-      @all << Customer.new(info_hash)
+  def find_all_by_first_name(first_name)
+    @all.find_all do |object| object.first_name.downcase == first_name.downcase
     end
   end
+
+    def find_all_by_last_name(last_name)
+      @all.find_all do |customer| customer.last_name.downcase == last_name.downcase
+      end
+  end
+
+
+
+
+  # def populate_repository(path)
+  #   CSV.read(path, headers: true, header_converters: :symbol) do |row|
+  #     info_hash = {
+  #       id: row[:id].to_i,
+  #       first_name: row[:first_name],
+  #       last_name: row[:last_name],
+  #       created_at: Time.parse(row[:created_at]),
+  #       updated_at: Time.parse(row[:updated_at])
+  #     }
+  #     @all << Customer.new(info_hash)
+  #   end
+  # end
 end
