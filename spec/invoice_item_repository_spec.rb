f156e2dea16e86d2630e1f0d786ffa4aaf718668
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-02-27 13:00:34 -0700

    Test: find invoice item by id

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -22,13 +22,18 @@ RSpec.describe InvoiceItemRepository do
       expect(@ii_repository).to be_a(InvoiceItemRepository)
     end
 
+    it 'returns CSV :: Table Object' do
+      expect(@ii_repository.rows).to be_a(CSV::Table)
+    end
+
     it 'returns all instances of invoice items' do
       expect(@ii_repository.all.count).to eq(21830)
       expect(@ii_repository.all[0]).to be_a(InvoiceItem)
     end
 
-    it 'returns CSV :: Table Object' do   
-      expect(@ii_repository.rows).to be_a(CSV::Table)
+    it 'find invoice item by id' do
+      expect(@ii_repository.find_by_id(2)).to be_a(InvoiceItem)
+      expect(@ii_repository.find_by_id(2).id).to eq(2)
     end
 
   end
