e421c75134a327f54fff5ccc42f6653c9efdb042
Chris Kjolhede <ckjolhede@gmail.com>
2022-02-26 19:27:43 -0700

    test: #merchants_with_high_item_count

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,9 +2,12 @@ require './lib/sales_engine'
 require './lib/item_repository'
 
 class SalesAnalyst
+attr_reader :items_per_merchant
+
   def initialize(items, merchants)
     @items = items
     @merchants = merchants
+    @items_per_merchant = {}
   end
 
   def average_items_per_merchant
@@ -12,18 +15,18 @@ class SalesAnalyst
   end
 
   def total_items_per_merchant
-    items_per_merchant = {}
-    @items.map do |item|
-      items_per_merchant[item.merchant_id] = 0 if !items_per_merchant.has_key?(item.merchant_id)
-      items_per_merchant[item.merchant_id] += 1
+    @items_per_merchant = {}
+    @items.each do |item|
+      @items_per_merchant[item.merchant_id] = 0 if !@items_per_merchant.has_key?(item.merchant_id)
+      !@items_per_merchant[item.merchant_id] += 1
     end
-    items_per_merchant.values
+    @items_per_merchant
   end
 
   def average_items_per_merchant_standard_deviation
     total_items_per_merchant
     total_square_diff = 0
-    total_items_per_merchant.map do |item_count|
+    total_items_per_merchant.values.map do |item_count|
       total_square_diff += ((item_count - average_items_per_merchant) ** 2)
     end
     Math.sqrt(total_square_diff / (@merchants.count - 1)).round(2)
