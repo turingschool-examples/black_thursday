fed613a6b86a6755344cd8222a11719d55b4fb1f
John Hennerich <john.hennerich@gmail.com>
2022-02-27 11:59:03 -0700

    Feat: develop invoice_item

---
+++ lib/invoice_item.rb
@@ -0,0 +1,49 @@
+require 'time'
+require 'bigdecimal'
+
+#headers
+#id,item_id,invoice_id,quantity,unit_price,created_at,updated_at
+
+class InvoiceItem
+  attr_accessor :info
+
+  def initialize(info)
+    @info = info
+    if @info[:unit_price].class != "BigDecimal"
+      price_alt = @info[:unit_price].to_f/100
+      @info[:unit_price] = price_alt
+    end
+  end
+
+  def id
+    @info[:id].to_i
+  end
+
+  def item_id
+    @info[:item_id]
+  end
+
+  def invoice_id
+    @info[:invoice_id]
+  end
+
+  def quantity
+    @info[:quantity].to_i
+  end
+
+  def unit_price
+    unit_price = BigDecimal(@info[:unit_price], 5)
+  end
+
+  def created_at
+    Time.parse(@info[:created_at]).utc
+  end
+
+  def updated_at
+    Time.parse(@info[:updated_at]).utc
+  end
+
+  def unit_price_to_dollars
+    unit_price.to_f
+  end
+end
