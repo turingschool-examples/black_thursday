05dd2d9361a21c364e027dc45700c73b70e44016
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-27 10:26:56 -0800

    Test: pass exist test for invoice class

---
+++ spec/invoice_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../lib/invoice'
+require_relative 'spec_helper'
+require 'pry'
+
+  RSpec.describe Invoice do
+    before (:each) do
+    @invoice = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+    })
+  end
+
+
+    it 'exists' do
+      expect(@invoice).to be_a(Invoice)
+    end
+end
