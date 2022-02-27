c7054824cc19fd81f8d3dc761cfe924c62194437
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-27 14:32:52 -0700

    test: update method

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,7 +1,7 @@
 # 24,Esteban,Jenkins,2012-03-27 14:54:16 UTC,2012-03-27 14:54:16 UTC
 require 'simplecov'
 require 'time'
-require './lib/customer'
+require_relative '../lib/customer'
 
 SimpleCov.start
 
@@ -26,5 +26,18 @@ RSpec.describe Customer do
     expect(@c.last_name).to eq('Jenkins')
     expect(@c.created_at).to eq(Time)
     expect(@c.updated_at).to eq(Time)
+  end
+
+  it 'can update customer with attributes' do
+    attributes = {first_name: "Burt", last_name: "Reynolds"}
+
+    intial_update_time = @c.updated_at
+    @c.update(attributes)
+
+    expect(@c.id).to eq(24)
+    expect(@c.first_name).to eq('Burt')
+    expect(@c.last_name).to eq('Reynolds')
+    expect(@c.created_at).to eq(Time)
+    expect(@c.updated_at).to eq(intial_update_time)
   end
 end
