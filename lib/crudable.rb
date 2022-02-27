6164ac98bb8c3f1df8baeee8127ea581526ff3f8
Will Medders <wmedders@gmail.com>
2022-02-27 18:17:54 -0500

    Fix: refactor highest_id in Crudable

--- lib/crudable.rb
+++ lib/crudable.rb
@@ -2,24 +2,23 @@ module Crudable
 
   # helper method for create
   def highest_id
-    last_id = 0
-    @all.max do |one|
-      last_id = one.id
+    last_id = @all.map do |one|
+      one.id
     end
-    return last_id
+    last_id.max
   end
 
   def create(attributes)
-    attributes[:id] = self.highest_id + 1      
+    attributes[:id] = self.highest_id + 1
     object = @new_object.new(attributes)
     @all << object
   end
 
-  def update(id, attributes)
-    new_name = self.find_by_id(id)
-    new_name.name = attributes[:name]
-    return new_name
-  end
+  # def update(id, attributes)
+  #   new_name = self.find_by_id(id)
+  #   new_name.name = attributes[:name]
+  #   return new_name
+  # end
 
   def delete(id)
     erase = self.find_by_id(id)
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,12 +1,15 @@
 require_relative '../lib/findable.rb'
 require_relative '../lib/invoice.rb'
+require_relative '../lib/crudable.rb'
 require 'pry'
 
 class InvoiceRepository
   include Findable
+  include Crudable
   attr_reader :all
   def initialize(invoice_array)
     @all = invoice_array
+    @new_object = Invoice
   end
 
   def find_all_by_customer_id (id_integer)
@@ -21,14 +24,14 @@ class InvoiceRepository
     @all.find_all { |invoice| invoice.status == status_symbol}
   end
 
-  def create (info_hash)
-    new_id = (@all.max{ |invoice_a, invoice_b| invoice_a.id <=> invoice_b.id}).id + 1
-    info_hash[:id] = new_id
-    info_hash[:created_at] = Time.now.getutc.to_s
-    info_hash[:updated_at] = Time.now.getutc.to_s
-    new_invoice = Invoice.new(info_hash)
-    @all << new_invoice
-  end
+  # def create (info_hash)
+  #   new_id = (@all.max{ |invoice_a, invoice_b| invoice_a.id <=> invoice_b.id}).id + 1
+  #   info_hash[:id] = new_id
+  #   info_hash[:created_at] = Time.now.getutc.to_s
+  #   info_hash[:updated_at] = Time.now.getutc.to_s
+  #   new_invoice = Invoice.new(info_hash)
+  #   @all << new_invoice
+  # end
 
   def update (id_integer, status_symbol)
     target_invoice = find_by_id(id_integer)
