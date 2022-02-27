1ee95ffdcd5fe993ead60a62227866120d62df8e
Sherman Au <shermau13@gmail.com>
2022-02-27 14:59:37 -0700

    test: invoice attributes

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 RSpec.describe 'Iteration 2' do
   context 'Invoices' do
-    it '#all returns all invoices' do
+    it 'exists' do
       i = Invoice.new({
                         id: 6,
                         customer_id: 7,
@@ -15,6 +15,24 @@ RSpec.describe 'Iteration 2' do
                       })
 
       expect(i).to be_an(Invoice)
+    end
+
+    it '#has attributes' do
+      i = Invoice.new({
+                        id: 6,
+                        customer_id: 7,
+                        merchant_id: 8,
+                        status: 'pending',
+                        created_at: '2022-02-27 14:52:16 UTC',
+                        updated_at: '2022-02-27 14:52:16 UTC'
+                      })
+
+      expect(i.id).to eq(6)
+      expect(i.customer_id).to eq(7)
+      expect(i.merchant_id).to eq(8)
+      expect(i.status).to eq('pending')
+      expect(i.created_at).to be_an_instance_of(Time)
+      expect(i.updated_at).to be_an_instance_of(Time)
     end
   end
 end
