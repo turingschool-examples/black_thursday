5fbd029a50d7f535f1bb66fde15f22869b7b1b51
Sherman Au <shermau13@gmail.com>
2022-02-27 13:52:25 -0700

    feat: merchant array list

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,7 +2,7 @@ require_relative 'sales_engine'
 require_relative 'item_repository'
 
 class SalesAnalyst
-attr_reader :items_per_merchant
+  attr_reader
 
   def initialize(items, merchants)
     @items = items
@@ -17,7 +17,7 @@ attr_reader :items_per_merchant
   def total_items_per_merchant
     @items_per_merchant = {}
     @items.each do |item|
-      @items_per_merchant[item.merchant_id] = 0 if !@items_per_merchant.has_key?(item.merchant_id)
+      @items_per_merchant[item.merchant_id] = 0 unless @items_per_merchant.has_key?(item.merchant_id)
       !@items_per_merchant[item.merchant_id] += 1
     end
     @items_per_merchant
@@ -27,18 +27,30 @@ attr_reader :items_per_merchant
     total_items_per_merchant
     total_square_diff = 0
     total_items_per_merchant.values.map do |item_count|
-      total_square_diff += ((item_count - average_items_per_merchant) ** 2)
+      total_square_diff += ((item_count - average_items_per_merchant)**2)
     end
     Math.sqrt(total_square_diff / (@merchants.count - 1)).round(2)
   end
 
   def merchants_with_high_item_count
     @high_item_merchants = []
-    (total_items_per_merchant.select   {|k,v| v > 6 }.keys).each do |high_id|
-        @merchants.each do |merchant|
-          @high_item_merchants << merchant if merchant.id == high_id
-        end
+    total_items_per_merchant.select { |_k, v| v > 6 }.keys.each do |high_id|
+      @merchants.each do |merchant|
+        @high_item_merchants << merchant if merchant.id == high_id
+      end
     end
     @high_item_merchants
+  end
+
+  def merchant_id_array
+    @items.map do |item|
+      item.merchant_id
+    end
+  end
+
+  def items_per_merchant_list
+    merchant_id_array.each_with_object(Hash.new(0)) do |merchant_id, total|
+      total[merchant_id] += 1
+    end
   end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/sales_engine'
+require './lib/sales_engine'
 require 'pry'
 require 'bigdecimal'
 
@@ -8,36 +8,20 @@ RSpec.describe ItemRepository do
                                  items: './data/items.csv',
                                  merchants: './data/merchants.csv'
                                })
-    @sales_analyst = @se.analyst
+    @sa = @se.analyst
   end
 
-<<<<<<< HEAD
   it '#average_items_per_merchant returns average items per merchant' do
-    expected = @sales_analyst.average_items_per_merchant
-=======
-  it "#average_items_per_merchant returns average items per merchant" do
     expected = @sa.average_items_per_merchant
->>>>>>> origin
 
     expect(expected).to eq 2.88
     expect(expected.class).to eq Float
   end
 
-<<<<<<< HEAD
-  it ' ' do
-    en
-=======
-  it "#average_items_per_merchant_standard_deviation returns the standard deviation" do
-    expected = @sa.average_items_per_merchant_standard_deviation
+  it '#average_items_per_merchant_standard_deviation returns the standard deviation' do
+    expected = @sa.items_per_merchant_list
+
     expect(expected).to eq 3.26
     expect(expected.class).to eq Float
->>>>>>> origin
-  end
-
-  it "returns #merchants_with_high_item_count" do
-    expected = @sa.merchants_with_high_item_count
-
-    expect(expected.length).to eq 52
-    expect(expected.first.class).to eq Merchant
   end
 end
