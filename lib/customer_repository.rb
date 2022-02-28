e1afa354febb6203c22284135d7d6c44431944e9
reuben <creubend@gmail.com>
2022-02-28 09:01:22 -0700

    test: All test pass

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,6 +1,12 @@
 class CustomerRepository
-  def initialize[info]
-    @argument = argument
+  def initialize(info)
+    @all = info
+  end
+  def find_all_by_first_name(first_name)
+    @all.find_all{|index| index.first_name == first_name}
+  end
+  def find_all_by_last_name(last_name)
+    @all.find_all{|index| index.last_name == last_name}
   end
 
 
--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -2,10 +2,26 @@ require "./lib/customer_repository"
 require "rspec"
 RSpec.describe CustomerRepository do
   it "exists" do
-    customer1 = double("customer", :id => 1, :first_name => "Joey", :last_name => "Ondricka", :created_at => 2012-03-27 14:54:09 UTC, :updated_at => 2012-03-27 14:54:09 UTC)
-    customer2 = double("customer", :id => 2, :first_name => "Cecelia", :last_name => "Osinski", :created_at => 2012-03-27 14:54:10 UTC, :updated_at => 2012-03-27 14:54:10 UTC)
-    customer3 = double("customer", :id => 3, :first_name => "Mariah", :last_name => "Toy", :created_at => 2012-03-27 14:54:10 UTC, :updated_at => 2012-03-27 14:54:10 UTC)
-    customer_repo = CustomerRepository([])
+    customer1 = double("customer", :id => 1, :first_name => "Joey", :last_name => "Ondricka", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC")
+    customer2 = double("customer", :id => 2, :first_name => "Cecelia", :last_name => "Osinski", :created_at => "2012-03-27 14:54:10 UTC", :updated_at => "2012-03-27 14:54:10 UTC")
+    customer3 = double("customer", :id => 3, :first_name => "Mariah", :last_name => "Toy", :created_at => "2012-03-27 14:54:10 UTC", :updated_at => "2012-03-27 14:54:10 UTC")
+    customer_repo = CustomerRepository.new([customer1, customer2, customer3])
     expect(customer_repo).to be_a(CustomerRepository)
   end 
-end 
+  it "can find all first names" do
+    customer1 = double("customer", :id => 1, :first_name => "Joey", :last_name => "Ondricka", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC")
+    customer2 = double("customer", :id => 2, :first_name => "Cecelia", :last_name => "Osinski", :created_at => "2012-03-27 14:54:10 UTC", :updated_at => "2012-03-27 14:54:10 UTC")
+    customer3 = double("customer", :id => 3, :first_name => "Joey", :last_name => "Toy", :created_at => "2012-03-27 14:54:10 UTC", :updated_at => "2012-03-27 14:54:10 UTC")
+    customer_repo = CustomerRepository.new([customer1, customer2, customer3])
+    expect(customer_repo.find_all_by_first_name("Joey")).to eq([customer1, customer3])
+    expect(customer_repo.find_all_by_first_name("Cecelia")).to eq([customer2])
+  end 
+  it "can find all last names" do
+    customer1 = double("customer1", :id => 1, :first_name => "Bruce", :last_name => "Ondricka", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC")
+    customer2 = double("customer2", :id => 2, :first_name => "Cecelia", :last_name => "Osinski", :created_at => "2012-03-27 14:54:10 UTC", :updated_at => "2012-03-27 14:54:10 UTC")
+    customer3 = double("customer3", :id => 3, :first_name => "Joey", :last_name => "Osinski", :created_at => "2012-03-27 14:54:10 UTC", :updated_at => "2012-03-27 14:54:10 UTC")
+    customer_repo = CustomerRepository.new([customer1, customer2, customer3])
+    expect(customer_repo.find_all_by_last_name("Osinski")).to eq([customer2, customer3])
+    expect(customer_repo.find_all_by_last_name("Ondricka")).to eq([customer1])
+  end 
+end  
