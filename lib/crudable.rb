0dbf19c004ccc4dc682324b6d046e2cf61cc7f0f
Joseph Senecal <jasenecal2@gmail.com>
2022-02-28 11:44:20 -0700

    Fix: prevent update method from updating non-updatable attributes

--- lib/crudable.rb
+++ lib/crudable.rb
@@ -16,7 +16,7 @@ module Crudable
 
   def update(id, attributes)
     attributes.each do |k, v|
-      find_by_id(id).instance_variable_set(k.to_s.insert(0, '@').to_sym, v)
+      find_by_id(id).instance_variable_set(k.to_s.insert(0, '@').to_sym, v) unless k == :id || k == :created_at || k == :merchant_id || k == :customer_id
     end
     if find_by_id(id).instance_variables.include?(:@updated_at)
       find_by_id(id).updated_at = Time.now
