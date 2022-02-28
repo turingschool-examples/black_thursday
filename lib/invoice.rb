94e40cbd09a9c6e94ac8549d5302e3f643657fc5
reuben <creubend@gmail.com>
2022-02-28 07:32:02 -0700

    feat:InvoiceRepo passed all tests

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -4,9 +4,9 @@ class Invoice
       @id = info[:id]
       @customer_id = info[:customer_id]
       @merchant_id = info[:merchant_id]
-      @status = info[:status]
-      @created_at = info[:created_at]
-      @updated_at = info[:updated_at]
+      @status = info[:status].to_s
+      @created_at = info[:created_at].to_s
+      @updated_at = info[:updated_at].to_s
     end
     
 end 
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -3,20 +3,17 @@ class InvoiceRepository
   def initialize(info)
     @all = info
   end
+  
   def find_all_by_status(search)
-    status_array = []
-    @all.find_all{|index| index.status.include?(search.downcase)}
-    status_array << index
+    @all.find_all{|index| index.status == search}
   end
+  
   def find_all_by_merchant_id(search)
-    merchant_id_array = []
-    @all.find_all{|index| index.merchant_id.include?(search.downcase)}
-    merchant_id_array << index
+    @all.find_all{|index| index.merchant_id == search}
   end
+  
   def find_all_by_customer_id(search)
-    customer_id_array = []
-    @all.find_all{|index| index.customer_id.include?(search.downcase)}
-    customer_id_array << index
+    @all.find_all{|index| index.customer_id == search}
   end
 
 
