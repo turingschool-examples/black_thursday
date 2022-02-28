17b1167cea2e115d6be32517860f607d7b1221df
Sherman Au <shermau13@gmail.com>
2022-02-27 17:37:30 -0700

    feat: #find_by_customer_id for invoice_repo

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -14,4 +14,8 @@ class InvoiceRepository
   def find_by_id(invoice_id)
     @invoices.find { |invoice| invoice.id == invoice_id }
   end
+
+  def find_all_by_customer_id(customer_id)
+    @invoices.find_all { |invoice| invoice.customer_id == customer_id }
+  end
 end
