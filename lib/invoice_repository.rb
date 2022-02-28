f9d3715592d1de18551a4cbffd2b61e7607c0d77
Sherman Au <shermau13@gmail.com>
2022-02-27 17:44:34 -0700

    test:#find_all_by_merchant_id for invoice_repo_spec

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -18,4 +18,8 @@ class InvoiceRepository
   def find_all_by_customer_id(customer_id)
     @invoices.find_all { |invoice| invoice.customer_id == customer_id }
   end
+
+  def find_all_by_merchant_id(merchant_id)
+    @invoices.find_all { |invoice| invoice.merchant_id == merchant_id }
+  end
 end
