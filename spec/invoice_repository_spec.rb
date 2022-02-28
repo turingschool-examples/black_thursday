3785678539a36dc0491583da420db94b19f1f4c6
Sherman Au <shermau13@gmail.com>
2022-02-27 17:15:46 -0700

    test: invoice #find_by_id

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -18,5 +18,20 @@ RSpec.describe 'Iteration 2' do
 
       expect(expected.length).to eq 4985
     end
+
+    it '#find_by_id returns an invoice associated to the given id' do
+      invoice_id = 3452
+      expected = @se.invoices.find_by_id(invoice_id)
+
+      expect(expected.id).to eq invoice_id
+      expect(expected.merchant_id).to eq 12_335_690
+      expect(expected.customer_id).to eq 679
+      expect(expected.status).to eq :pending
+
+      invoice_id = 5000
+      expected = @se.invoices.find_by_id(invoice_id)
+
+      expect(expected).to eq nil
+    end
   end
 end
