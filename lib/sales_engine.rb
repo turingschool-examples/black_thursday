ddd051a6ecacc49c893fc99cd983741a393a85d5
Sherman Au <shermau13@gmail.com>
2022-02-28 20:31:50 -0700

    intialize: transaction_repo

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -9,18 +9,20 @@ require_relative 'invoice'
 require_relative 'sales_analyst'
 
 class SalesEngine
-  attr_reader :items, :merchants, :invoices
+  attr_reader :items, :merchants, :invoices, :transactions
 
-  def initialize(items_, merchants_, invoices_)
+  def initialize(items_, merchants_, invoices_, transactions_)
     @items = ItemRepository.new(items_)
     @merchants = MerchantRepository.new(merchants_)
     @invoices = InvoiceRepository.new(invoices_)
+    @transactions = TransactionRepository.new(transactions_)
   end
 
   def self.from_csv(csv_hash)
     item_contents = CSV.open csv_hash[:items], headers: true, header_converters: :symbol
     merchant_contents = CSV.open csv_hash[:merchants], headers: true, header_converters: :symbol
     invoice_contents = CSV.open csv_hash[:invoices], headers: true, header_converters: :symbol
+    transactions_contents = CSV.open csv_hash[:transactions], headers: true, header_converters: :symbol
 
     # read file, create objects, return SE object
     items = []
@@ -38,6 +40,11 @@ class SalesEngine
       invoices << Invoice.new(row)
     end
 
+    transactions = []
+    invoice_contents.each do |row|
+      invoices << Invoice.new(row)
+    end
+
     se = SalesEngine.new(items, merchants, invoices)
     # assign_items_to_merchant
   end
@@ -45,6 +52,4 @@ class SalesEngine
   def analyst
     SalesAnalyst.new(@items.all, @merchants.all, @invoices.all)
   end
-
-
 end
