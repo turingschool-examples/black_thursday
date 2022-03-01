72618120e807ce2c82204292c988b9735b3d1591
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-01 14:26:13 -0700

    feat: invoice_items_by_id

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -5,19 +5,34 @@ require_relative './findable.rb'
 require_relative './invoice_item.rb'
 require_relative './crudable.rb'
 require 'BigDecimal'
+require 'Time'
 # require 'simplecov'
 # SimpleCov.start
 
-
+class InvoiceItemRepository
   include Findable
   include Crudable
   attr_reader :all
   attr_accessor :new_object
 
-  def initialize array
-    @all = array
+  def initialize (irr_array)
+    @all = irr_array
     @new_object = InvoiceItem
   end
 
+  def find_all_by_item_id(id)
+    all.find_all {|invoice| id == invoice.item_id}
+  end
+
+  def invoice_items_by_invoice_id
+    @by_invoice ||= all.group_by {|invoice_item| invoice_item.invoice_id }
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    invoice_items_by_invoice_id[invoice_id] || []
+  end
+
+
   def inspect
   end
+end
