20002bcd0ff5b1e8567baf11df545091b0a190d6
Sherman Au <shermau13@gmail.com>
2022-02-28 18:53:28 -0700

    test:#credit_card_expiration_date for transaction

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -16,7 +16,17 @@ RSpec.describe Transaction do
   end
 
   it '#id returns the transaction id' do
-    expect(@transaction.id).to eq 6
+    expect(@transaction.id).to eq(6)
     expect(@transaction.id.class).to eq Integer
+  end
+
+  it '#credit_card_expiration_date returns the credit card expiration' do
+    expect(@transaction.credit_card_expiration_date).to eq('0220')
+    expect(@transaction.credit_card_expiration_date.class).to eq(String)
+  end
+
+  it '#result returns the result' do
+    expect(transaction.result).to eq :success
+    expect(transaction.result.class).to eq Symbol
   end
 end
