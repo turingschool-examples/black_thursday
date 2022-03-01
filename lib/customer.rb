75ca644ff5e34f8d9f72ce529b251c9c7da9dacf
Andrew Speth <speth.andrew@yahoo.com>
2022-02-28 20:23:12 -0700

    feat: customer has attributes

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,1 +1,12 @@
+require 'time'
 
+class Customer
+  attr_reader :id, :first_name, :last_name, :created_at, :updated_at
+  def initialize(attributes)
+    @id = attributes[:id]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @created_at = attributes[:created_at]
+    @updated_at = attributes[:updated_at]
+  end
+end
