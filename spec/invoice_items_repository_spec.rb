a3494f6ff23ced2552c5770b86636c1bacf58e7d
Chris Kjolhede <ckjolhede@gmail.com>
2022-02-28 21:22:13 -0700

    test: write invoice_items #find_by_id

--- spec/invoice_items_repository_spec.rb
+++ spec/invoice_items_repository_spec.rb
@@ -21,5 +21,17 @@ RSpec.describe 'Iteration 3' do
       expected = @se.invoice_items.all
       expect(expected.count).to eq 21830
     end
+
+    it "#find_by_id finds an invoice_item by id" do
+      id = 10
+      expected = @sa.invoice_items.find_by_id(id)
+
+      expect(expected.id).to eq id
+      expect(expected.item_id).to eq 263523644
+      expect(expected.invoice_id).to eq 2
+    end
+
+
+
   end
 end
