cbc4fd7ac40014c61d890d2a202052d6ff7cb1c8
Will Medders <wmedders@gmail.com>
2022-03-01 00:28:26 -0500

    Feat: avg items per merchant

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,2 +1,12 @@
 class SalesAnalyst
+  attr_reader :merchant_repo, :item_repo
+  def initialize(merchant_repo, item_repo)
+    @merchant_repo = merchant_repo
+    @item_repo = item_repo
+  end
+
+  def average_items_per_merchant
+    avg = @item_repo.all.count.to_f / @merchant_repo.all.count.to_f
+    avg.round(2)
+  end
 end
