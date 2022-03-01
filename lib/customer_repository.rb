8e7785cce55f7b1c0ddb31bdd5741c5f1d877599
Andrew Speth <speth.andrew@yahoo.com>
2022-02-28 20:52:18 -0700

    feat: find all by last name

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -16,4 +16,8 @@ class CustomerRepository
   def find_all_by_first_name(name)
     @customers.find_all { |customer| customer.first_name.downcase.include?(name.downcase) }
   end
+
+  def find_all_by_last_name(name)
+    @customers.find_all { |customer| customer.last_name.downcase.include?(name.downcase) }
+  end
 end
