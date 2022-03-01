af4629bee664e550bae05071f8b4da35490e9436
Sherman Au <shermau13@gmail.com>
2022-02-28 23:43:56 -0700

    feat:find_all_by_credit_card_number

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -18,4 +18,8 @@ class TransactionRepository
   def find_all_by_invoice_id(id)
     @transactions.find_all { |transactions| transactions.invoice_id == id }
   end
+
+  def find_all_by_credit_card_number(credit_card_number)
+    @transactions.find_all { |transaction| transaction.credit_card_number == credit_card_number }
+  end
 end
