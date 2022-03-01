903d5ecc2b7d5e10319a9e92f1ee4c15dc74b17c
Sherman Au <shermau13@gmail.com>
2022-02-28 18:54:16 -0700

    refactor:transactions.rb

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -2,15 +2,15 @@
 require 'time'
 
 class Transaction
-  attr_accessor :invoice_id, :credit_card_number, :result, :unit_price, :updated_at
-  attr_reader :id, :created_at, :merchant_id
+  attr_accessor :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :updated_at
+  attr_reader :id, :created_at
 
   def initialize(attributes)
     @id = attributes[:id].to_i
     @invoice_id = attributes[invoice_id].to_i
     @credit_card_number = attributes[:credit_card_number]
-    @cred_card_expiration_date = attributes[:cred_card_expiration_date]
-    @result = attributes[:result]
+    @credit_card_expiration_date = attributes[:credit_card_expiration_date]
+    @result = attributes[:result].to_sym
     @created_at = Time.parse(attributes[:created_at].to_s)
     @updated_at = Time.parse(attributes[:updated_at].to_s)
   end
