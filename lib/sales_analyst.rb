326ad4494bc702dd8a3f3c78ff7262e5f19d637b
Will Medders <wmedders@gmail.com>
2022-03-01 08:37:53 -0500

    Spec: groups items by merchant id

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,26 +1,41 @@
 class SalesAnalyst
-  attr_reader :merchant_repo, :item_repo
+  attr_reader :merchant_repo, :item_repo, :merchant_items_hash, :num_items_per_merchant
   def initialize(merchant_repo, item_repo)
     @merchant_repo = merchant_repo
     @item_repo = item_repo
     @merchant_items_hash = {}
+    @num_items_per_merchant = []
+    @mean = 0
+    @set_of_square_differences = []
   end
 
   def group_items_by_merchant_id
     @merchant_items_hash = @item_repo.all.group_by { |item| item.merchant_id }
   end
 
-
-  def average_items_per_merchant
-    # avg = @item_repo.all.count.to_f / @merchant_repo.all.count.to_f
+  def items_per_merchant
     group_items_by_merchant_id
-    items_per_merchant = @merchant_items_hash.map do |merchant, items|
+    @num_items_per_merchant = @merchant_items_hash.map do |merchant, items|
       items.count
     end
-    avg = items_per_merchant.sum.to_f / items_per_merchant.count
-    avg.round(2)
   end
-
-  def average_items_per_merchant_standard_deviation
-  end
+  #
+  # def square_differences
+  #   average_items_per_merchant
+  #   set = @num_items_per_merchant.map do |number|
+  #     number - @mean
+  #     binding.pry
+  #   end
+  # end
+  #
+  #
+  # def average_items_per_merchant
+  #   # avg = @item_repo.all.count.to_f / @merchant_repo.all.count.to_f
+  #   items_per_merchant
+  #   avg = items_per_merchant.sum.to_f / items_per_merchant.count
+  #   @mean = avg.round(2)
+  # end
+  #
+  # def average_items_per_merchant_standard_deviation
+  # end
 end
