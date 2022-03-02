90e9b1d4f11ac446b6917e8589614125e32d6aae
Brylan Gannon <brylan.gannon112@gmail.com>
2022-03-01 17:34:04 -0700

    Feat: average_invoices_per_merchant + standard_deviation created and tested

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -76,17 +76,37 @@ class SalesAnalyst
     return @id_counter
   end
 
+  def invoices_per_merchant
+    @id_counter = Hash.new(0)
+    @invoices.each do |index|
+      id_counter[index.merchant_id] += 1
+    end
+    return @id_counter
+  end
+
   def average_item_price
     cost_of_all_items = 0.0
     @items.each {|index| cost_of_all_items += index.unit_price.to_f}
     return cost_of_all_items/@items.length.to_f
   end
 
-
   def standard_deviation_of_item_price
     squared_item_price_total = 0.0
     average_item_price_fixed = average_item_price
     @items.each {|index| squared_item_price_total += ((index.unit_price.to_f - average_item_price_fixed)**2)}
     return (squared_item_price_total/(@items.length - 1 ))**0.5
+  end
+
+  def average_invoices_per_merchant
+    return (@invoices.length.to_f/@merchants.length.to_f).round(2)
+  end
+
+  def average_invoices_per_merchant_standard_deviation
+    invoices_per_merchant
+    total_to_be_square_rooted = 0.0
+    @id_counter.each do |index|
+      total_to_be_square_rooted += (average_invoices_per_merchant.to_f - index[1].to_f)**2
+    end
+    return ((total_to_be_square_rooted/(@merchants.length.to_f - 1))**0.5).round(2)
   end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -4,81 +4,59 @@ require './lib/sales_analyst.rb'
 
 RSpec.describe SalesAnalyst do
 
-  it 'average items per merchant' do
-    sales_engine = SalesEngine.from_csv({
+  before :each do
+    @sales_engine = SalesEngine.from_csv({
       :items     => "./data/items.csv",
       :invoices     => "./data/invoices.csv",
       :invoice_items     => "./data/invoice_items.csv",
       :customers     => "./data/customers.csv",
       :transactions     => "./data/transactions.csv",
       :merchants => "./data/merchants.csv"})
-    sales_analyst = sales_engine.analyst
+  end
+
+  it 'average items per merchant' do
+    sales_analyst = @sales_engine.analyst
     expect(sales_analyst).to be_an_instance_of(SalesAnalyst)
     expect(sales_analyst.average_items_per_merchant).to eq(2.88)
   end
 
   it 'sales ' do
-    sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :invoices     => "./data/invoices.csv",
-      :invoice_items     => "./data/invoice_items.csv",
-      :customers     => "./data/customers.csv",
-      :transactions     => "./data/transactions.csv",
-      :merchants => "./data/merchants.csv"})
-    sales_analyst = sales_engine.analyst
+    sales_analyst = @sales_engine.analyst
     expect(sales_analyst).to be_an_instance_of(SalesAnalyst)
     expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
   end
 
 
   it 'merchants with high item counts' do
-    sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :invoices     => "./data/invoices.csv",
-      :invoice_items     => "./data/invoice_items.csv",
-      :customers     => "./data/customers.csv",
-      :transactions     => "./data/transactions.csv",
-      :merchants => "./data/merchants.csv"})
-    sales_analyst = sales_engine.analyst
+    sales_analyst = @sales_engine.analyst
     expect(sales_analyst.merchants_with_high_item_count.length).to eq(52)
   end
 
   it 'can see the average price of items for a given merchant' do
-    sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :invoices     => "./data/invoices.csv",
-      :invoice_items     => "./data/invoice_items.csv",
-      :customers     => "./data/customers.csv",
-      :transactions     => "./data/transactions.csv",
-      :merchants => "./data/merchants.csv"})
-    sales_analyst = sales_engine.analyst
+    sales_analyst = @sales_engine.analyst
     expect(sales_analyst.average_item_price_for_merchant("12334105")).to eq(16.66)
     expect(sales_analyst.average_item_price_for_merchant("12334105").class).to eq(BigDecimal)
   end
 
 
   it "average item price per merchant" do
-    sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :invoices     => "./data/invoices.csv",
-      :invoice_items     => "./data/invoice_items.csv",
-      :customers     => "./data/customers.csv",
-      :transactions     => "./data/transactions.csv",
-      :merchants => "./data/merchants.csv"})
-    sales_analyst = sales_engine.analyst
+    sales_analyst = @sales_engine.analyst
     expect(sales_analyst.average_average_price_per_merchant).to eq(350.29)
     expect(sales_analyst.average_average_price_per_merchant.class).to eq(BigDecimal)
   end
 
   it "golden items" do
-    sales_engine = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :invoices     => "./data/invoices.csv",
-      :invoice_items     => "./data/invoice_items.csv",
-      :customers     => "./data/customers.csv",
-      :transactions     => "./data/transactions.csv",
-      :merchants => "./data/merchants.csv"})
-    sales_analyst = sales_engine.analyst
+    sales_analyst = @sales_engine.analyst
     expect(sales_analyst.golden_items.length).to eq(5)
+  end
+
+  it 'average_invoices_per_merchant' do
+    sales_analyst = @sales_engine.analyst
+    expect(sales_analyst.average_invoices_per_merchant).to eq(10.49)
+  end
+
+  it 'average invoices per merchant standard deviation' do
+    sales_analyst = @sales_engine.analyst
+    expect(sales_analyst.average_invoices_per_merchant_standard_deviation).to eq(3.29)
   end
 end
