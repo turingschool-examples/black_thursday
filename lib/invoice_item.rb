6b2f69b788d4d748c109199fc8c5564d39fb7d3b
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-01 20:09:34 -0700

    Test: #invoice item attributes

---
+++ lib/invoice_item.rb
@@ -0,0 +1,11 @@
+class InvoiceItem
+  attr_reader :invoice_item_attributes
+
+  def initialize(attributes)
+    @invoice_item_attributes = attributes
+  end
+
+  # def unit_price_to_dollars
+  #   invoice_item_attributes[:unit_price].to_f
+  # end
+end
--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -7,24 +7,26 @@ RSpec.describe "InvoiceItem" do
     id: 6,
     item_id: 7,
     invoice_id: 8,
+    quantity: 1,
     unit_price: BigDecimal("10.99", 4),
     created_at: Time.now,
-    updated_at: Time.now,
+    updated_at: Time.now
   })
 
   it "exists" do
     expect(ii).to be_an_instance_of(InvoiceItem)
   end
 
-#   it "has all expected attributes" do
-#     expect(item.item_attributes[:id]).to eq(1)
-#     expect(item.item_attributes[:name]).to eq("Pencil")
-#     expect(item.item_attributes[:description]).to eq("You can use it to write things")
-#     expect(item.item_attributes[:unit_price]).to eq(BigDecimal("10.99", 4))
-#     expect(item.item_attributes[:created_at]).to be_an_instance_of(Time)
-#     expect(item.item_attributes[:updated_at]).to be_an_instance_of(Time)
-#     expect(item.item_attributes[:merchant_id]).to eq(2)
-#   end
+  it "has expected attributes" do
+    expect(ii.invoice_item_attributes[:id]).to eq(6)
+    expect(ii.invoice_item_attributes[:item_id]).to eq(7)
+    expect(ii.invoice_item_attributes[:invoice_id]).to eq(8)
+    expect(ii.invoice_item_attributes[:quantity]).to eq(1)
+    expect(ii.invoice_item_attributes[:unit_price]).to eq(BigDecimal("10.99", 4))
+    expect(ii.invoice_item_attributes[:created_at]).to be_an_instance_of(Time)
+    expect(ii.invoice_item_attributes[:updated_at]).to be_an_instance_of(Time)
+  end
+end
 #
 #   it "can convert unit price to dollars" do
 #     expect(item.unit_price_to_dollars).to eq(10.99)
