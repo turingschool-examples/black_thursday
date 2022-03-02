9a6e99e1895c135e08b3d5a94b7e8f311e86a101
John Hennerich <john.hennerich@gmail.com>
2022-03-01 21:51:20 -0700

    Feat: create transaction_repository

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,10 +3,6 @@ require_relative '../lib/sales_engine'
 require_relative '../lib/transaction'
 require_relative 'spec_helper'
 
-#headers
-#id,invoice_id,credit_card_number,credit_card_expiration_date,
-#result,created_at,updated_at
-
 RSpec.describe Transaction do
   before (:each) do
     @transaction= Transaction.new({id: 1,
@@ -18,15 +14,13 @@ RSpec.describe Transaction do
       updated_at: "2012-02-26 20:56:56 UTC" })
   end
 
-    context 'InvoceItem' do
-      it 'can read Item attributes' do
-        expect(@transaction.id).to eq(1)
-        expect(@transaction.invoice_id).to eq(2179)
-        expect(@transaction.credit_card_number).to eq("4068631943231473")
-        expect(@transaction.credit_card_expiration_date).to eq("0217")
-        expect(@transaction.result).to eq("success")
-        expect(@transaction.created_at).to eq(Time.parse("2012-02-26 20:56:56 UTC"))
-        expect(@transaction.updated_at).to eq(Time.parse("2012-02-26 20:56:56 UTC"))
-      end
-    end
+  it 'can read Item attributes' do
+    expect(@transaction.id).to eq(1)
+    expect(@transaction.invoice_id).to eq(2179)
+    expect(@transaction.credit_card_number).to eq("4068631943231473")
+    expect(@transaction.credit_card_expiration_date).to eq("0217")
+    expect(@transaction.result).to eq("success")
+    expect(@transaction.created_at).to eq(Time.parse("2012-02-26 20:56:56 UTC"))
+    expect(@transaction.updated_at).to eq(Time.parse("2012-02-26 20:56:56 UTC"))
+  end
 end
