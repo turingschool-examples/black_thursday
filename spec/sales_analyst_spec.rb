f2dff738b6e8a23e80107a494aac52dfb656ac73
Phillip Kamps <phillip.kamps@gmail.com>
2022-03-02 19:07:05 -0700

    Feat: #invoice_items_by_invoice_id

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -138,6 +138,11 @@ RSpec.describe SalesAnalyst do
     expect(sa.invoices_by_merchant_id(12334753).count).to eq 15
   end
 
+  it "find all invoice items under same invoice id's and return" do
+    expect(sa.invoice_items_by_invoice_id(1).count).to eq 8
+    expect(sa.invoice_items_by_invoice_id(2).count).to eq 4
+  end
+
   xit "#top_revenue_earners(n) returns the top n merchants ranked by revenue" do
     expected = sa.top_revenue_earners(5)
     first = expected.first
