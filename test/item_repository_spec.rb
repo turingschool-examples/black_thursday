d1d30bbcd5b5e6afaa07f8f4ca51d8131d8bd331
Blake Saylor <blakesaylor@gmail.com>
2022-05-31 16:02:11 -0600

    Create skeleton for tests

---
+++ test/item_repository_spec.rb
@@ -0,0 +1,28 @@
+require './lib/item_repository'
+
+RSpec.describe ItemRepository do
+  before :each do
+
+  end
+
+  describe "#initialize" do
+    it "" do
+    end
+
+    it "" do
+    end
+
+    it "" do
+    end
+
+    it "" do
+    end
+
+    it "" do
+    end
+
+    it "" do
+    end
+
+  end
+end
