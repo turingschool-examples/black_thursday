4816b9f1722be949cb826e845ae1e15ce881591d
philmarcu <philmark18@gmail.com>
2022-05-31 15:33:21 -0600

    Finish find id, name, and find all by name methods + tests

--- lib/merchant_collection.rb
+++ lib/merchant_collection.rb
@@ -1,7 +1,39 @@
 require 'CSV'
+require './lib/merchant'
 class MerchantCollection
 
+  attr_reader :all
   def initialize(file_path)
     @file_path = file_path
+    @all = []
+
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      @all << Merchant.new(:id => row[:id], :name => row[:name])
+    end
   end
+
+  def find_by_id(id)
+    merch_name = []
+    @all.each do |merchant|
+      if merchant.id == id
+        merch_name << merchant
+      end
+    end
+    merch_name.first.name
+  end
+
+  def find_by_name(name)
+    @all.find {|merchant| merchant.name == name}
+  end
+
+  def find_all_by_name(name)
+    specific_name = []
+    @all.each do |merchant|
+      if merchant.name.upcase.include?(name.upcase)
+        specific_name << merchant
+      end
+    end
+    specific_name
+  end
+
 end
---
+++ spec/merchant_collection_spec.rb
@@ -0,0 +1,38 @@
+require "./lib/merchant_collection"
+
+RSpec.describe MerchantCollection do
+  before :each do
+    @merchant_collection = MerchantCollection.new("./data/merchants.csv")
+  end
+
+  it 'exists & has attributes' do
+    expect(@merchant_collection).to be_a(MerchantCollection)
+    expect(@merchant_collection.all).to be_a(Array)
+    expect(@merchant_collection.all.length).to eq(475)
+  end
+
+  it 'returns data from our merchant array' do
+    expect(@merchant_collection.all.first.id).to eq("12334105")
+    expect(@merchant_collection.all.last.name).to eq("CJsDecor")
+  end
+
+  it 'can find a merchant by id' do
+    expect(@merchant_collection.find_by_id("12334105")).to eq("Shopin1901")
+    expect(@merchant_collection.find_by_id("12334189")).to eq("JacquieMann")
+    expect(@merchant_collection.find_by_id("12335080")).to eq("HooksforBooks")
+  end
+
+  it 'can find a merchant by name' do
+    expect(@merchant_collection.find_by_name("JacquieMann")).to be_a(Merchant)
+  end
+
+  it 'can find all names given the characters' do
+    expect(@merchant_collection.find_all_by_name("ing").length).to eq(24)
+    expect(@merchant_collection.find_all_by_name("iNg").length).to eq(24)
+    expect(@merchant_collection.find_all_by_name("store").length).to eq(4)
+    expect(@merchant_collection.find_all_by_name("sToRe").length).to eq(4)
+    expect(@merchant_collection.find_all_by_name("Uniford").length).to eq(1)
+    expect(@merchant_collection.find_all_by_name("unIFoRd").length).to eq(1)
+    expect(@merchant_collection.find_all_by_name("uniford").first.id).to eq("12334174")
+  end
+end
