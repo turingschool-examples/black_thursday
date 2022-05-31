3ab2baf1022f24bdcbc050b634c8a49fd3053ac7
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-31 19:13:05 -0400

    Test: merchant class exists

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,11 +1,15 @@
 require 'simplecov'
 SimpleCov.start
-require './test/merchant'
+require './lib/merchant'
+require 'rspec'
 
 RSpec.describe Merchant do
 
   it 'exists' do
-    merchant = Merchant.new
+    merchant = Merchant.new({
+      :id => 5,
+      :name => "Turing School"
+      })
 
     expect(merchant).to be_instance_of(Merchant)
   end
