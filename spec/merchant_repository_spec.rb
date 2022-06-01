994526cef4d8c3d605477fabb572a554dcd6bcc2
Justin Ramirez <jusrez528@gmail.com>
2022-06-01 16:52:44 -0400

    Change merchant repository class and spec file to include previous code

---
+++ spec/merchant_repository_spec.rb
@@ -0,0 +1,18 @@
+require './lib/merchant.rb'
+require './lib/merchant_repository.rb'
+
+RSpec.describe MerchantRepository do
+  it 'exists' do
+    merchant_repo = MerchantRepository.new('./data/merchants.csv')
+    expect(merchant_repo).to be_a(MerchantRepository)
+  end
+
+  it 'returns an array of all known Merchant instances' do
+    merchant_repo = MerchantRepository.new('./data/merchants.csv')
+    expect(merchant_repo.all).to eq([])
+    merchant = Merchant.new({:id => 1, :name => "Bob's Burgers"})
+    expect(merchant_repo.all).to eq([merchant])
+
+  end
+
+end
