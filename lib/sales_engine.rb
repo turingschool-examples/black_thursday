eba5e1ba4ff96a72d29491eb43b1300648eb6eb5
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-01 10:44:43 -0400

    Initialize and create first method

---
+++ lib/sales_engine.rb
@@ -0,0 +1,14 @@
+require 'pry'
+  class SalesEngine
+    attr_reader :item_repository, :merchant_repository
+  def initialize(items_path, merchants_path)
+    @item_repository = ItemRepository.new(items_path)
+    @merchant_repository = MerchantRepository.new(merchants_path)
+  end
+
+  def self.from_csv(data)
+    # binding.pry
+    SalesEngine.new(data[:items], data[:merchants])
+  end
+
+end
