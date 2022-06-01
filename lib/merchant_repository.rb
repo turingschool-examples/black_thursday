f613758c9578a287fc087cbfce215678613db749
Sunny Moore <moore.sunny@gmail.com>
2022-06-01 13:51:43 -0700

    Create Merchant Repository class

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,14 @@
+require 'CSV'
+require_relative 'merchant'
+class MerchantRepository
+  attr_reader :file_path, :all
+
+  def initialize(file_path)
+    @file_path = file_path
+    @all =[]
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      @all << Merchant.new({:id =>row[:id], :name => row[:name]})
+      # require 'pry';binding.pry
+    end
+  end
+end
