3441043d5bd324d05636a4b7272e8b5d46fd14d3
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-01 00:02:43 -0400

    Test: returns array of all item instances

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -14,4 +14,12 @@ RSpec.describe ItemRepository do
     expect(@item_repository).to be_instance_of(ItemRepository)
   end
 
+  it 'returns array of all item instances' do
+    expect(@item_repository.all).to be_instance_of(Array)
+  end
+
+  it 'returns array of all item instances' do
+    expect(@item_repository.all.count).to be > 20
+  end
+
 end
