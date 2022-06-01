2e49e40264781889b9bb7c1c2c4af6ebd2d91192
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-01 14:28:16 -0600

    Feat - Add 'exists' test

---
+++ spec/merchant_spec.rb
@@ -0,0 +1,13 @@
+require './lib/merchant'
+
+RSpec.describe Merchant do
+  before :each do
+    @merchant = Merchant.new({:id => 5, :name => "Turing School"})
+  end
+
+  it "exists" do
+    expect(@merchant).to be_a(Merchant)
+  end
+  
+
+end
