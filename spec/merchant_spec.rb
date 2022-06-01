a79bc4c9dc1df505af5e1cc6ea3b6b910b1f09d0
Justin Ramirez <jusrez528@gmail.com>
2022-06-01 16:23:06 -0400

    Replace last test with two separate tests

---
+++ spec/merchant_spec.rb
@@ -0,0 +1,20 @@
+require './lib/merchant'
+
+RSpec.describe Merchant do
+  it "exists" do
+    merchant = Merchant.new(1, "Bob's Burgers")
+    expect(merchant).to  be_instance_of (Merchant)
+  end
+
+  it "can return a name" do
+    merchant = Merchant.new(:id => 1, :name => "Bob's Burgers")
+
+    expect(merchant.name).to eq("Bob's Burgers")
+  end
+
+	it "can return an id" do
+		merchant = Merchant.new(:id => 1, :name => "Bob's Burgers")
+
+		expect(merchant.id).to eq(1)
+	end
+end
