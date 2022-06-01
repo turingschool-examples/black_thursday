c6cc51035ba74f5c27e6c60fdce6edab3c665286
Sunny Moore <moore.sunny@gmail.com>
2022-06-01 13:55:23 -0700

    Test merch repo can return array of all known merchants

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -10,4 +10,11 @@ describe MerchantRepository do
   it "exists" do
     expect(@merchant_repository).to be_a MerchantRepository
   end
+  it "can return an array of all known merchants" do
+    expect(@merchant_repository.all).to be_a Array
+    expect(@merchant_repository.all.length).to eq(475)
+    expect(@merchant_repository.all.first).to be_a Merchant
+    expect(@merchant_repository.all.first.id).to eq("12334105")
+    expect(@merchant_repository.all.first.name).to eq("Shopin1901")
+  end
 end
