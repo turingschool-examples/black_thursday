eff794a75889fd92569145b1824cd4b72e5e816e
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-31 23:24:54 -0400

    Test: returns item description

--- test/item_test.rb
+++ test/item_test.rb
@@ -48,4 +48,18 @@ RSpec.describe Item do
     expect(item.name).to eq("Pencil")
   end
 
+  it 'returns description' do
+    item = Item.new({
+      :id => 1,
+      :name => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price => BigDecimal(10.99,4),
+      :created_at => Time.now,
+      :updated_at => Time.now,
+      :merchant_id => 2
+      })
+
+    expect(item.description).to eq("You can use it to write things")
+  end
+
 end
