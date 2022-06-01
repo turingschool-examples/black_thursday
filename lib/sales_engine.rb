2ec00a183f062e550509e7fa00f0dcc32ad04ba4
Justin Ramirez <jusrez528@gmail.com>
2022-06-01 16:28:26 -0400

    Make changes to class names in spec and class files

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,12 +2,11 @@ class SalesEngine
   attr_reader :item_collection, :merchant_collection
 
   def initialize(items_path, merchants_path)
-    @item_collection = ItemCollection.new(items_path)
-    @merchant_collection = MerchantCollection.new(merchants_path)
+    @item_repository = ItemRepository.new(items_path)
+    @merchant_repository = MerchantRepository.new(merchants_path)
   end
 
   def self.from_csv(data)
-    # binding.pry
     return SalesEngine.new(data[:items], data[:merchants])
   end
 
