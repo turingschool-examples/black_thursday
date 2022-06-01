4d0a0123aae0f27203dd480efc535bbefd062e5c
Anna Marie Sterling <sterling.am@gmail.com>
2022-06-01 15:30:10 -0600

    Add find merchant by name test

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -10,6 +10,16 @@ class MerchantRepository
 
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
       @all << Merchant.new({:id => row[:id], :name => row[:name]})
-    end 
+    end
+  end
+
+  def find_by_id(id)
+    @all.find do |merchant|
+      if merchant.id == id
+        return merchant
+      else
+        nil 
+      end
+    end
   end
 end
--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -15,8 +15,16 @@ RSpec.describe MerchantRepository do
     expect(@merchant_repository.all).to be_instance_of Array
     expect(@merchant_repository.all.length).to eq(475)
     expect(@merchant_repository.all.first).to be_instance_of Merchant
-    expect(@merchant_repository.all.first.id).to eq("12334105")
+    expect(@merchant_repository.all.first.id).to eq('12334105')
   end
 
+  it "can find merchant by ID" do
 
+    expect(@merchant_repository.find_by_id('12334105')).to be_instance_of Merchant
+  end
+
+  it "can find a merchant by name" do
+
+    expect(@merchant_repository.find_by_name('Shopin1901')).to eq('Shopin1901')
+  end
 end
