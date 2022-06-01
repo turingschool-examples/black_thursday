ff3a3f546821f416fcfa59c7ca862b2485e62a30
Sunny Moore <moore.sunny@gmail.com>
2022-06-01 13:45:14 -0700

    Test Merchant exists

---
+++ spec/merchant_spec.rb
@@ -0,0 +1,12 @@
+require 'RSpec'
+require './lib/merchant'
+
+describe Merchant do
+  it "exists and has attributes" do
+    merchant = Merchant.new({:id => 5, :name=> "Turing"})
+
+    expect(merchant).to be_a Merchant
+    expect(merchant.id).to eq(5)
+    expect(merchant.name).to eq("Turing")
+  end
+end
