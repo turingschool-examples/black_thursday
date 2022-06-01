2e4cd0c91587f3ec238022c70b4361ecaaa0a29c
Kevin Ta <kvnta@yahoo.com>
2022-05-31 21:06:26 -0600

    Complete item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -15,5 +15,18 @@ RSpec.describe Item do
   it 'exists' do
   expect(i).to be_a(Item)
   end
-  
+
+  it 'item has attributes' do
+    time = Time.new
+    expect(i.id).to eq(1)
+    expect(i.name).to eq("Pencil")
+    expect(i.description).to eq("You can use it to write things")
+    expect(i.unit_price).to eq(BigDecimal(10.99,4))
+    expect(i.created_at).to eq(time)
+    expect(i.updated_at).to eq(time)
+    expect(i.merchant_id).to eq(2)
+  end
+  it 'returns the price of the item in dollars' do
+    expect(i.unit_price_to_dollars).to eq(10.99)
+  end
 end
