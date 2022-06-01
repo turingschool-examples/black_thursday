ec10368d93054bb82bf4ab424038e2016c7e00e1
Kevin Ta <kvnta@yahoo.com>
2022-05-31 18:06:39 -0600

    Current work Iter 0

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,18 @@
+require 'CSV'
+require './lib/merchant.rb'
+class MerchantRepository
+  attr_reader :all
+  def initialize(file_path)
+    @file_path = file_path
+    @all = []
+
+    CSV.foreach(@file_path, headers: true, header_converters: :symbol) do |row|
+    @all << Merchant.new(:id => row[:id],:name => row[:name])
+
+    end
+
+
+  end
+
+
+end
