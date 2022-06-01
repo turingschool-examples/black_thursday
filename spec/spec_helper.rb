12272229ee49ab7996e5479db2381172dc05164d
Eli Sachs <easachs13@gmail.com>
2022-05-31 23:47:26 -0600

    Add SimpleCov to spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'SimpleCov'
+SimpleCov.start
+
+require_relative 'merchant_spec'
+require_relative 'merchant_repository_spec'
+require_relative 'item_spec'
+require_relative 'item_repository_spec'
