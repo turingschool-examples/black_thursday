01632e7871024b10b175b0bc20d3ca031aab632a
Michael Koulouvaris <mikekoul@gmail.com>
2022-06-01 15:05:07 -0600

    Update Item Collection

--- lib/item_collection.rb
+++ lib/item_collection.rb
@@ -4,4 +4,9 @@ class ItemCollection
   def initialize(file_path)
     @file_path = file_path
   end
+
+  CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+    @all << Item.new(:id => row[:id], :name => row[:name], :description => row[:description], :unit_price => row[:unit_price], :created_at => row[:created_at], :updated_at => row[:updated_at], :merchant_id => row[:merchant_id]
+  end
+
 end
