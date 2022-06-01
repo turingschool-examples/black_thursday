78f310443bfa8aa828c9f1e39e143d0b4d04abbf
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-01 14:53:43 -0600

    Feat - Add 'all' method

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -2,12 +2,13 @@ require 'CSV'
 require_relative 'merchant'
 
 class MerchantRepository
+  attr_reader :all
 
   def initialize(file_path)
     @file_path = file_path
-    
+    @all = []
   end
 
-
+  
 
 end
