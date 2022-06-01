8a6ba7342a5702244633d75b25fdf36e92949ce7
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-31 23:23:13 -0400

    Test: returns item id

--- test/item_test.rb
+++ test/item_test.rb
@@ -5,6 +5,7 @@ require 'rspec'
 require 'bigdecimal'
 
 RSpec.describe Item do
+
   it 'exists' do
     item = Item.new({
       :id => 1,
@@ -15,8 +16,22 @@ RSpec.describe Item do
       :updated_at => Time.now,
       :merchant_id => 2
       })
-      
+
     expect(item).to be_instance_of(Item)
+  end
+
+  it 'returns id' do
+    item = Item.new({
+      :id => 1,
+      :name => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price => BigDecimal(10.99,4),
+      :created_at => Time.now,
+      :updated_at => Time.now,
+      :merchant_id => 2
+      })
+
+    expect(item.id).to eq(1)
   end
 
 end
