d360aeabcc527cc1e252779fe3af90e7d77652c4
mkbonini <mkbonini@gmail.com>
2022-06-01 15:13:07 -0600

    Add item class with associated spec file.

--- lib/item.rb
+++ lib/item.rb
@@ -1,15 +1,18 @@
+require 'bigdecimal'
+
 class Item
 
-attr_reader :id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at
+attr_reader :id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at, :unit_price_to_dollars
 
   def initialize(data)
     @id = data[:id].to_i
     @name = data[:name]
     @description = data[:description]
-    @unit_price = data[:unit_price].to_i
+    @unit_price = BigDecimal(data[:unit_price],4)
     @merchant_id = data[:merchant_id]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
+    @unit_price_to_dollars = data[:unit_price].to_f
   end
 
   def self.create_item(data)
--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -2,7 +2,7 @@ require 'CSV'
 require './lib/item'
 
 class ItemRepository
-  
+
   attr_reader :all
 
   def initialize(file_path)
@@ -11,6 +11,7 @@ class ItemRepository
     CSV.foreach(@file_path, headers: true, header_converters: :symbol) do |row|
        @all << Item.new({id: row[:id], name: row[:name], description: row[:description], unit_price: row[:unit_price], merchant_id: row[:merchant_id]})
     end
+    require "pry"; binding.pry
   end
 
 end
