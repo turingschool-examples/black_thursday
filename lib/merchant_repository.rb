e98f51159c8751556906214b8dd14a84a9c29b9d
Blake Saylor <blakesaylor@gmail.com>
2022-06-01 15:26:53 -0600

    Add CSV parse to create array of Merchants

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,9 +1,16 @@
+require 'csv'
+require_relative 'merchant'
+
 class MerchantRepository
   attr_reader :all
 
   def initialize(file_path)
     @file_path = file_path
     @all = []
+
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      @all << Merchant.new({:id => row[:id], :name => row[:name]})
+    end
   end
 
 end
