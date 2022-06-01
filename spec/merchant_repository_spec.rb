0af2348234189cd45d902587a0094c52f85eacc3
Anna Marie Sterling <sterling.am@gmail.com>
2022-06-01 14:53:30 -0600

    Iteration 0 added merchant repo and sales engine tests

---
+++ spec/merchant_repository_spec.rb
@@ -0,0 +1,12 @@
+require './lib/merchant_repository'
+
+RSpec.describe MerchantRepository do
+  before do
+    @merchant_repository = MerchantRepository.new('./data/merchants.csv')
+  end
+
+  it "exists" do
+
+    expect(@merchant_repository).to be_instance_of MerchantRepository
+  end
+end
