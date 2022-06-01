2d001dd8dbdd57afeec2759a2f876307c172c204
Justin Ramirez <jusrez528@gmail.com>
2022-06-01 15:41:13 -0400

    Add unit price to dollars method and tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -112,8 +112,21 @@ RSpec.describe Item do
 			:updated_at  => Time.now,
 			:merchant_id => 2
 			})
-
 		expect(i.merchant_id).to eq(2)
+	end
+
+	it 'returns the unit price as a float' do
+		i = Item.new({
+			:id          => 1,
+			:name        => "Pencil",
+			:description => "You can use it to write things",
+			:unit_price  => BigDecimal(10.99,4),
+			:created_at  => Time.now,
+			:updated_at  => Time.now,
+			:merchant_id => 2
+			})
+
+		expect(i.unit_price_to_dollars).to eq(10.99)
 	end
 
 
