47975d856a9ce731be930b63243ef27af5c2aa12
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-31 23:23:52 -0400

    Test: returns item name

--- test/item_test.rb
+++ test/item_test.rb
@@ -34,4 +34,18 @@ RSpec.describe Item do
     expect(item.id).to eq(1)
   end
 
+  it 'returns name' do
+    item = Item.new({
+      :id => 1,
+      :name => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price => BigDecimal(10.99,4),
+      :created_at => Time.now,
+      :updated_at => Time.now,
+      :merchant_id => 2
+      })
+
+    expect(item.name).to eq("Pencil")
+  end
+
 end
