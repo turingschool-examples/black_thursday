bfb951cc0fa59c72f30abde8653e89f5fc0f5502
mkbonini <mkbonini@gmail.com>
2022-06-01 16:28:32 -0600

    Add tests for item repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -11,7 +11,6 @@ class ItemRepository
     CSV.foreach(@file_path, headers: true, header_converters: :symbol) do |row|
        @all << Item.new({id: row[:id], name: row[:name], description: row[:description], unit_price: row[:unit_price], merchant_id: row[:merchant_id]})
     end
-    require "pry"; binding.pry
   end
 
 end
