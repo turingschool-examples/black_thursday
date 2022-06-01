747b5666c118670507daa095738e4dc48a0993ae
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-01 14:49:14 -0500

    test: find by name, id, and description for item repo created

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -12,4 +12,16 @@ class ItemRepository
         id: row[:id], name: row[:name], unit_price: row[:unit_price], merchant_id: row[:merchant_id], created_at: row[:created_at], updated_at: row[:updated_at]})
     end
   end
+
+  def find_by_id(id)
+    @all.find do |row|
+      row.id == id
+    end
+  end
+
+  def find_by_name(name)
+    @all.find do |row|
+      row.name.downcase == name.downcase
+    end
+  end
 end
--- test/item_repository_spec.rb
+++ test/item_repository_spec.rb
@@ -12,8 +12,22 @@ RSpec.describe ItemRepository do
   it "has an all function" do
     expect(@item_repo.all.class).to equal(Array)
     expect(@item_repo.all[0].class).to equal(Item)
-    expect(@item_repo.all[0].name).to equal("510+ RealPush Icon Set")
+    expect(@item_repo.all[0].name).to eq("510+ RealPush Icon Set")
   end
 
-  
+  it "has a find by id function" do
+    expect(@item_repo.find_by_id(263395237).name).to eq("510+ RealPush Icon Set")
+    expect(@item_repo.find_by_id(123456)).to equal(nil)
+  end
+
+  it "has a find by name function" do
+    expect(@item_repo.find_by_name("510+ RealPush Icon Set").id).to eq(263395237)
+    expect(@item_repo.find_by_name("510+ REALPUSH Icon Set").id).to eq(263395237)
+    expect(@item_repo.find_by_name("MINNIE MUG").id).to eq(nil)
+  end
+
+  it "can find items by description" do
+    expect(@item_repo.find_all_with_description("tkjlds;afdkla")).to eq([])
+    expect(@item_repo.find_all_with_description("Facebook")).to eq(XXX)
+  end
 end
