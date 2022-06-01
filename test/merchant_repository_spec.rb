08429f3440f80abbd05244f909ec833fddea40ae
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-01 16:04:02 -0500

    Add merchant repository exists test

--- test/merchant_repository_spec.rb
+++ test/merchant_repository_spec.rb
@@ -1,26 +1,25 @@
-require './lib/merchant'
-require './lib/merchant_repository'
+require './test/spec_helper'
 
 
 RSpec.describe MerchantRepository do
 
-  # before :each do
-  #   merchantrepository= MerchantRepository.new("./data/merchants.csv")
-  # end
+  before :each do
+    @merchantrepository= MerchantRepository.new("./data/merchants.csv")
+  end
 
   it 'exists' do
     merchant = Merchant.new({:id => 5, :name => "Turing School"})
 
 
-    expect(merchantrepository).to be_a(Merchant_Repository)
+    expect(@merchantrepository).to be_a(MerchantRepository)
   end
 
-  # it 'returns an array of all known merchants' do
-  #   merchant = Merchant.new({:id => 5, :name => "Turing School"})
-  #
-  #
-  #
-  # end
+  xit 'returns an array of all known merchants' do
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+
+
+  end
 
 
 end
