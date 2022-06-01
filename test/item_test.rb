543eefd843faac5b04d803024f96f7e53ed10737
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-31 23:21:08 -0400

    Test: Item class exists

---
+++ test/item_test.rb
@@ -0,0 +1,22 @@
+require 'simplecov'
+SimpleCov.start
+require './lib/item'
+require 'rspec'
+require 'bigdecimal'
+
+RSpec.describe Item do
+  it 'exists' do
+    item = Item.new({
+      :id => 1,
+      :name => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price => BigDecimal(10.99,4),
+      :created_at => Time.now,
+      :updated_at => Time.now,
+      :merchant_id => 2
+      })
+      
+    expect(item).to be_instance_of(Item)
+  end
+
+end
