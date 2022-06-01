209b6c7e7075177359cb4a9d3e6208aa4e66cb2a
Blake Saylor <blakesaylor@gmail.com>
2022-06-01 17:14:50 -0600

    Add test for find_by_id

--- test/merchant_repository_spec.rb
+++ test/merchant_repository_spec.rb
@@ -6,16 +6,23 @@ RSpec.describe MerchantRepository do
     @collection = MerchantRepository.new(@filepath)
   end
 
-  describe "#initialize" do
-    it "is a MerchantRepository" do
+  describe '#initialize' do
+    it 'is a MerchantRepository' do
       expect(@collection).to be_a MerchantRepository
     end
 
-    it "can return an array of all known Merchant instances" do
+    it 'can return an array of all known Merchant instances' do
       expect(@collection.all).to be_a Array
       expect(@collection.all.count).to eq 475
       expect(@collection.all.first).to be_a Merchant
       expect(@collection.all.first.id).to eq '12334105'
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'can find a merchant by id' do
+      expect(@collection.find(12334105)).to be_a Merchant
+      expect(@collection.find(12334105)).to eq ('Shopin1901')
     end
   end
 
