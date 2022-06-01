0ed5d6e9f953d85c90780b46e276a9bbf502bcd9
Anna Marie Sterling <sterling.am@gmail.com>
2022-06-01 15:14:12 -0600

    Passed array of Merchant instances tests

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe MerchantRepository do
     expect(@merchant_repository).to be_instance_of MerchantRepository
   end
 
+  it "can return an array of all Merchant instances" do
+
+    expect(@merchant_repository.all).to be_instance_of Array
+    expect(@merchant_repository.all.length).to eq(475)
+    expect(@merchant_repository.all.first).to be_instance_of Merchant
+    expect(@merchant_repository.all.first.id).to eq("12334105")
+  end
+
+
 end
