030cf58594d3b645c5fb985704fd029969128677
Jonathan Pope <jonathanmpope@gmail.com>
2022-06-01 11:36:32 -0600

    Feat:item class and id

---
+++ test/item_spec.rb
@@ -0,0 +1,23 @@
+require_relative './spec_helper'
+require "BigDecimal"
+
+RSpec.describe Item do
+  let!(:item) {Item.new({
+    :id          => 1,
+    :name        => "Pencil",
+    :description => "You can use it to write things",
+    :unit_price  => BigDecimal(10.99,4),
+    :created_at  => Time.now,
+    :updated_at  => Time.now,
+    :merchant_id => 2
+  })}
+
+  it "is an instace of item" do
+    expect(item).to be_instance_of Item
+  end
+
+  it "has an id" do
+    expect(item.id).to eq(1)
+  end
+
+end
