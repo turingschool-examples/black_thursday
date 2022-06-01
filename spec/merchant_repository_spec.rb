09d6d0bf65f5429a20abc331c0fb5b69fd1d87ba
Thiago Silveira <thiago1992@yahoo.com>
2022-06-01 15:48:14 -0600

    Test: Merchant repo can find by merchant ID

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -9,9 +9,12 @@ RSpec.describe MerchantRepository do
 
   it 'returns an array of all known Merchant instances' do
     merchant_repo = MerchantRepository.new('./data/merchants.csv')
-    expect(merchant_repo.all).to eq([])
-    merchant = Merchant.new({:id => 1, :name => "Bob's Burgers"})
-    expect(merchant_repo.all).to eq([merchant])
+    expect(merchant_repo.all.count).to eq(475)
   end
 
+  it 'can find merchant by ID' do
+    merchant_repo = MerchantRepository.new('./data/merchants.csv')
+    id = 12334105
+    expect(merchant_repo.find_by_id).to eq('Shopin1901')
+  end
 end
