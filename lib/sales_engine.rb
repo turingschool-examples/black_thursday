d361ab9f57cef365eb9904501f7d54efe1176dfe
Eli Sachs <easachs13@gmail.com>
2022-05-31 21:38:19 -0600

    Create Sales Engine class

---
+++ lib/sales_engine.rb
@@ -0,0 +1,16 @@
+require_relative 'merchant_repository'
+
+class SalesEngine
+
+  attr_reader :merchant_repository, :item_repository
+
+  def initialize(items_filepath, merchants_filepath)
+    @item_repository = ItemRepository.new(items_filepath)
+    @merchant_repository = MerchantRepository.new(merchants_filepath)
+  end
+
+  def self.from_csv(data)
+    SalesEngine.new(data[:items], data[:merchants])
+  end
+
+end
