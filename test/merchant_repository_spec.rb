4ee6279000acf8bad9101a51f81d3e066978dd6f
Blake Saylor <blakesaylor@gmail.com>
2022-06-02 08:09:38 -0600

    Add test for update

--- test/merchant_repository_spec.rb
+++ test/merchant_repository_spec.rb
@@ -44,4 +44,20 @@ RSpec.describe MerchantRepository do
     end
   end
 
+  describe '#update' do
+    it 'can update the name of a merchant' do
+      expect(@collection.find_by_id('12334105').name).to eq 'Shopin1901'
+      @collection.update('12334105', 'my big fancy shop')
+      expect(@collection.name).to eq 'my big fancy shop'
+    end
+  end
+
+  describe '#delete' do
+    it 'can delete a Merchant based on id' do
+      expect(@collection.find_by_id('12334105').name).to eq 'Shopin1901'
+      @collection.delete('12334105')
+      expect(@collection.find_by_id('12334105')).to eq nil
+    end
+  end
+
 end
