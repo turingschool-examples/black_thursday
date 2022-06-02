3e1a659d817552742e7c0397ca9384f973035ffb
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-01 19:39:22 -0700

    Feat/wrote code for find_by_id method - GR

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,5 +1,5 @@
 require 'CSV'
-require 'item'
+require_relative 'item'
 
 class ItemRepository
   attr_reader :file_path,
@@ -12,4 +12,10 @@ class ItemRepository
       @all << Item.new({:id => row[:id], :name => row[:name], :description => row[:description], :unit_price => row[:unit_price], :created_at => row[:created_at], :updated_at => row[:updated_at], :merchant_id => row[:merchant_id]})
     end
   end
+
+  def find_by_id(id)
+    @all.find { |id| id }
+  end
+
 end
+# require 'pry'; binding.pry
