8813081692e727d96ae3ed9139d10e1812816ce9
Bryan Shears <brshears@gmail.com>
2022-06-02 11:06:38 -0600

    Update find_all_by_merchant_id test

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -13,19 +13,20 @@ RSpec.describe ItemRepository do
   end
 
   it "can return an array of all known items" do
-    expect(@item_repository.all).to be_a Array
+    expect(@item_repository.all_items).to be_a Array
   end
 
   it "can return an instance of an Item within a price range" do
-    price_in_range = @item_repository.find_all_by_price_in_range(0..500)
-    expect(price_in_range).to be_a(Array)
-    expect(price_in_range.first).to eq(510+ RealPush Icon Set)
-  end
+      # require "pry"; binding.pry
+      price_in_range = @item_repository.find_all_by_price_in_range(0..500)
+      expect(price_in_range).to be_a(Array)
+      expect(price_in_range.first.id).to eq("263397163")
+    end
 
-  it 'can find all merchants by merchant id' do
-    require "pry"; binding.pry
-    merchant_id = @item_repository.find_all_by_merchant_id(263395237)
-    expect(merchant_id).to be_a(Array)
-    expect(merchant_id.first).to be_a(Item)
+    it 'can find all merchants by merchant id' do
+
+      merchant_id = @item_repository.find_all_by_merchant_id("263395237")
+      expect(merchant_id).to be_a(Array)
+      expect(merchant_id.first.id).to eq("263395237")
+    end
   end
-end
