09c8adef93ebbdb51318c581448c2347a56370e6
Blake Saylor <blakesaylor@gmail.com>
2022-06-02 11:04:36 -0600

    Re-add Invoice after merge conflicts

---
+++ lib/invoice.rb
@@ -0,0 +1,18 @@
+class Invoice
+  attr_reader :id,
+              :customer_id,
+              :merchant_id,
+              :status,
+              :created_at,
+              :updated_at
+
+  def initialize(invoice)
+    @id = invoice[:id]
+    @customer_id = invoice[:customer_id]
+    @merchant_id = invoice[:merchant_id]
+    @status = invoice[:status]
+    @created_at = invoice[:created_at]
+    @updated_at = invoice[:updated_at]
+  end
+
+end
