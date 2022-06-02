00df6612340f0d6e23f261ede69cad7db78aeba8
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-01 19:48:56 -0700

    Test/added more robust test - GR

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -13,9 +13,13 @@ RSpec.describe ItemRepository do
 
   it "can return an array of all known items" do
     expect(@item_repository.all).to be_a Array
+    expect(@item_repository.all.first.id).to eq("263395237")
+    expect(@item_repository.all.first.name).to eq("510+ RealPush Icon Set")
   end
 
   it "can find by id" do
-    expect(@item_repository.find_by_id(263395237)).to be_a(Item)
+    expect(@item_repository.find_by_id("1")).to eq nil
+    expect(@item_repository.find_by_id("263395237")).to be_a(Item)
+    expect(@item_repository.find_by_id("263400793")).to be_a(Item)
   end
 end
