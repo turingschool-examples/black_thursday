29ca1fdabc1eca333c46299c6126657b161fc70a
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-02 02:42:33 -0600

    refactor: refactored sales_analyst_test and began skeleton for merchants_with_high_item_count

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -10,7 +10,7 @@ class SalesAnalyst
     (@item_repository.all.count.to_f / @merchant_repository.all.count.to_f).round(2)
   end
 
-  def standard_deviation
+  def standard_deviation #of a given merch or for the entire group?
     counts = []
     @merchant_repository.all.each do |merchant|
       counts <<  @item_repository.find_all_by_merchant_id(merchant.id).count
@@ -19,7 +19,22 @@ class SalesAnalyst
     Math.sqrt(total / (counts.length - 1)).round(2)
   end
 
-  def difference_between_merchant_items_and_mean
-
+  def merchants_with_high_item_count
+    merch_array = []
+    @merchant_repository.all.each do |merchant|
+      if (merchant.standard_deviation - standard_deviation) > 1 #functions aren't called properly yet
+        merch_array << merchant
+      end
+    end
+    merch_array #would return array of merch's that are "more than 1 standard deviation above avg # of products offered"
   end
+
+  # def difference_between_merchant_items_and_mean
+  #   #do we need a helper method to determine mean of all vendors?
+  #
+  # end
+  #
+  # def sum_of_differences_squared
+  #   #not sure what sum?
+  # end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -11,58 +11,38 @@ require 'rspec'
 
 RSpec.describe SalesAnalyst do
   before :each do
+    @sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+      })
+    @sales_analyst = @sales_engine.analyst
   end
 
   it "exists" do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-    sales_analyst = sales_engine.analyst
-
-    expect(sales_analyst).to be_instance_of(SalesAnalyst)
+    expect(@sales_analyst).to be_instance_of(SalesAnalyst)
   end
 
   it "can determine average items" do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-    sales_analyst = sales_engine.analyst
-
-    expect(sales_analyst.average_items_per_merchant).to eq(2.88)
+    expect(@sales_analyst.average_items_per_merchant).to eq(2.88)
   end
 
-  it 'returns difference between number of items for specified merchant and average items' do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-    sales_analyst = sales_engine.analyst
+  xit 'returns difference between number of items for specified merchant and average items' do
 
-    expect(sales_analyst.difference_between_merchant_items_and_mean(12334135)).to eq(1.88)
+    expect(@sales_analyst.difference_between_merchant_items_and_mean(12334135)).to eq(1.88)
 
-    expect(sales_analyst.difference_between_merchant_items_and_mean(12334315)).to eq(2.12)
+    expect(@sales_analyst.difference_between_merchant_items_and_mean(12334315)).to eq(2.12)
   end
 
   xit 'can return sum of differences squared' do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-    sales_analyst = sales_engine.analyst
-
-    expect(sales_analyst.sum_of_differences_squared).to eq(11338.44)
+    expect(@sales_analyst.sum_of_differences_squared).to eq(11338.44)
     # I don't feel 100% confident in this
   end
 
   it "can determine standard deviation" do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-    sales_analyst = sales_engine.analyst
+    expect(@sales_analyst.standard_deviation).to eq(3.26)
+  end
 
-    expect(sales_analyst.standard_deviation).to eq(3.26)
+  it "can determine which merchant sold the most items?" do
+    expect(@sales_analyst.merchants_with_high_item_count).to be_instance_of(Array)
   end
 end
