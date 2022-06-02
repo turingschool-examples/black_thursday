6e84d4003094822c4a5b77084f9beb0d9517813c
Bryan Shears <brshears@gmail.com>
2022-06-01 21:39:23 -0600

    Create find_all_by_in_range(range) method

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -12,4 +12,26 @@ class ItemRepository
       @all << Item.new({:id => row[:id], :name => row[:name], :description => row[:description], :unit_price => row[:unit_price], :created_at => row[:created_at], :updated_at => row[:updated_at], :merchant_id => row[:merchant_id]})
     end
   end
+
+
+  def find_all_by_price_in_range(range)
+    min = range.min
+    max = range.max
+    # require "pry"; binding.pry
+    @all.find_all do |item|
+      item.unit_price.to_i.between?(min, max)
+  end
+
+  end
+    # price_range = []
+    # require "pry"; binding.pry
+    # @all.each do |price|
+    #   if price.range == @item.price
+    #     price_range << @item
+    #   end
+    # end
+    #   price_range
+    # end
+
+
 end
