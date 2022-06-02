96eef9930ee4622e8190f2859beb8cf64402d60a
philmarcu <philmark18@gmail.com>
2022-06-02 11:17:12 -0600

    Test: finish sales engine test for iteration 0

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -3,7 +3,7 @@ require "./lib/item_collection"
 require "./lib/merchant_collection"
 
 RSpec.describe SalesEngine do
-  
+
   it "exists" do
     sales_engine = SalesEngine.from_csv({
       :items => "./data/items.csv",
@@ -19,9 +19,6 @@ RSpec.describe SalesEngine do
     })
 
     expect(sales_engine.item_collection).to be_instance_of ItemCollection
-
-
-
   end
 
   it "can return an array of all instances" do
@@ -31,9 +28,19 @@ RSpec.describe SalesEngine do
     })
 
     expect(sales_engine.merchant_collection).to be_instance_of MerchantCollection
+  end
 
+  it 'can return child instances' do
+    se = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
 
+    ic = se.item_collection
+    item = ic.find_by_name("Thukdokhin wax cord")
 
+    expect(ic).to be_a(ItemCollection)
+    expect(item.id).to eq("263404585")
   end
 
 end
