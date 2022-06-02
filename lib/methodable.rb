d5119e8ef843905a01e9767b0ff3e9afcc71c020
Brenna Stuart <brennacodes@gmail.com>
2022-06-02 16:43:07 -0600

    Feat: add methodable shell for later completion/integration

---
+++ lib/methodable.rb
@@ -0,0 +1,25 @@
+module Methodable
+  def all
+
+  end
+
+  def find_by(thing)
+
+  end
+
+  def find_all_by(thing)
+
+  end
+
+  def self.create(attributes)
+
+  end
+
+  def self.update(id, attribute)
+
+  end
+
+  def self.delete(id)
+
+  end
+end
