8c29b609a27f6ae0420927015d46c2ef036f49e2
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-01 21:25:24 -0500

    Add find by name test

--- test/merchant_repository_spec.rb
+++ test/merchant_repository_spec.rb
@@ -22,6 +22,15 @@ RSpec.describe MerchantRepository do
   it 'returns either nil or an instance of Merchant with a matching ID' do
 
     expect(@merchantrepository.find_by_id(12334132).name).to eq("perlesemoi")
+    expect(@merchantrepository.find_by_id(123456)).to eq(nil)
+    expect(@merchantrepository.find_by_id(12334132)).to be_a(Merchant)
+
+  end
+
+  it 'returns either nil or an instance of Merchant with a name' do
+
+    expect(@merchantrepository.find_by_name("MiniatureBikez")).to be_a(Merchant)
+    expect(@merchantrepository.find_by_name("AndreBikez")).to eq(nil)
 
   end
 
