11e20e0257e3a855d3d8868a4f2b379c8f43dc45
Jonathan Pope <jonathanmpope@gmail.com>
2022-06-02 09:52:28 -0600

    Test: item repo all integration test

--- test/sales_engine_spec.rb
+++ test/sales_engine_spec.rb
@@ -1,40 +1,33 @@
 require './test/spec_helper'
 
 RSpec.describe SalesEngine do
+  before :each do
+    @sales_engine = SalesEngine.from_csv({
+     :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+  end
 
-  it "exists" do
-      sales_engine = SalesEngine.from_csv({
-       :items => "./data/items.csv",
-        :merchants => "./data/merchants.csv"
-      })
-      expect(sales_engine).to be_instance_of SalesEngine
-    end
+  it 'exists' do
+    expect(@sales_engine).to be_instance_of SalesEngine
+  end
 
-    it "can return an array of all items" do
-      sales_engine = SalesEngine.from_csv({
-        :items => "./data/items.csv",
-        :merchants => "./data/merchants.csv"
-      })
+  it 'can return an array of all items' do
+    expect(@sales_engine.item_repository).to be_instance_of ItemRepository
+  end
 
-      expect(sales_engine.item_repository).to be_instance_of ItemRepository
+  it 'can return an array of all merchants' do
+    expect(@sales_engine.merchant_repository).to be_instance_of MerchantRepository
+    expect(@sales_engine.merchant_repository.all).to be_a Array
+    expect(@sales_engine.merchant_repository.all.length).to eq(475)
+  end
 
-
-    end
-
-    it "can return an array of all merchants" do
-      sales_engine = SalesEngine.from_csv({
-        :items => "./data/items.csv",
-        :merchants => "./data/merchants.csv"
-      })
-
-      expect(sales_engine.merchant_repository).to be_instance_of MerchantRepository
-
-      expect(sales_engine.merchant_repository.all).to be_a Array
-
-      expect(sales_engine.merchant_repository.all.length).to eq(475)
+  it "can return an array of all items" do
+    expect(@sales_engine.item_repository.all.class).to equal(Array)
+    expect(@sales_engine.item_repository.all[0].class).to equal(Item)
+    expect(@sales_engine.item_repository.all[0].name).to eq("510+ RealPush Icon Set")
+  end
 
 
-
-    end
 
   end
