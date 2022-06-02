88d579ffa2624d3c8464860a46c27941d8b8656b
Nick Jones <nickjones9605@gmail.com>
2022-06-02 10:25:20 -0600

    Test: Add merchant_repository.rb method find_all_by_name, and test.
    Co-authored-by: Justin <jusrez528@gmail.com>
    Co-authored-by: Thiago <thiago1992@yahoo.com>

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -27,13 +27,35 @@ RSpec.describe MerchantRepository do
 
   it 'can find all merchants by name fragment' do
     merchant_repo = MerchantRepository.new('./data/merchants.csv')
+    merchant_repo.find_all_by_name("mini").each do |merchant|
+        expect(merchant.name.downcase.include?("mini")).to eq(true)
+        expect(merchant).to be_instance_of(Merchant)
+      end
     expect(merchant_repo.find_all_by_name('mini')).to be_instance_of(Array)
+
+    merchant_repo.find_all_by_name("MiNi").each do |merchant|
+        expect(merchant.name.downcase.include?("mini")).to eq(true)
+        expect(merchant).to be_instance_of(Merchant)
+      end
+
     expect(merchant_repo.find_all_by_name('MiNi')).to be_instance_of(Array)
     expect(merchant_repo.find_all_by_name('inG')).to be_instance_of(Array)
     expect(merchant_repo.find_all_by_name('sHoPiN')).to be_instance_of(Array)
     expect(merchant_repo.find_all_by_name('sHoPiN1901')).to be_instance_of(Array)
+
+    merchant_repo.find_all_by_name("InvalidName").each do |merchant|
+        expect(merchant.name.downcase.include?("invalidname")).to eq(false)
+        expect(merchant).to eq(nil)
+      end
     expect(merchant_repo.find_all_by_name('InvalidName')).to eq([])
   end
 
-  #Nick and Thiago left off here, need to ask if lines 30 - 32 are valid tests
+  it "can create a new merchant instance" do
+    merchant_repo = MerchantRepository.new('./data/merchants.csv')
+    attribute = {name: "Turing School of Software and Design"}
+    merchant_repo.create(attribute)
+    new_merchant = merchant_repo.find_by_id(12337412)
+    expect(new_merchant.name).to be ("Turing School of Software and Design")
+  end 
+
 end
