80da7343e406357875966299889b0bdf2a0459f9
Bryan Shears <brshears@gmail.com>
2022-06-02 10:34:54 -0600

    Create find_all_by_merchant_id tests

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -19,6 +19,13 @@ RSpec.describe ItemRepository do
   it "can return an instance of an Item within a price range" do
     price_in_range = @item_repository.find_all_by_price_in_range(0..500)
     expect(price_in_range).to be_a(Array)
-    expect(price_in_range.first).to be_a(Item)
+    expect(price_in_range.first).to eq(510+ RealPush Icon Set)
+  end
+
+  it 'can find all merchants by merchant id' do
+    require "pry"; binding.pry
+    merchant_id = @item_repository.find_all_by_merchant_id(263395237)
+    expect(merchant_id).to be_a(Array)
+    expect(merchant_id.first).to be_a(Item)
   end
 end
