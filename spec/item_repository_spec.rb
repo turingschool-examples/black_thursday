3ac8f348e01925107a0128db8024e7bf78cd8629
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-02 10:04:57 -0600

    add item repository initial test

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,1 +1,14 @@
 require'./lib/item'
+require'./item_repository'
+
+RSpec.describe ItemRepository do
+  before :each do
+    @item_repository = ItemRepository.new("./data/items.csv")
+  end
+
+  it 'exists' do
+    expect(@item_repository).to be_a ItemRepository
+  end
+
+
+end
