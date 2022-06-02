13ffd9087bf4746f19315c19a19eadc91dac28ab
Thiago Silveira <thiago1992@yahoo.com>
2022-06-01 18:15:53 -0600

    Refactor: Sales engine attr reader

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,5 +1,5 @@
 class SalesEngine
-  attr_reader :item_collection, :merchant_collection
+  attr_reader :item_repository, :merchant_repository
 
   def initialize(items_path, merchants_path)
     @item_repository = ItemRepository.new(items_path)
