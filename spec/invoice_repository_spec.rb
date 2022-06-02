002d30e88c2c11eb9b195468585549e7f4d5d83e
Blake Saylor <blakesaylor@gmail.com>
2022-06-02 11:06:00 -0600

    Re-add after merge conflicts

---
+++ spec/invoice_repository_spec.rb
@@ -0,0 +1,73 @@
+require './lib/invoice_repository'
+
+RSpec.describe InvoiceRepository do
+  before :each do
+    @filepath = './data/invoices.csv'
+    @collection = InvoiceRepository.new(@filepath)
+  end
+
+  describe '#initialize' do
+    it 'is an InvoiceRepository' do
+      expect(@collection).to be_a InvoiceRepository
+    end
+
+    it 'can return an array of all known Invoice instances' do
+      expect(@collection.all).to be_a Array
+      expect(@collection.all.count).to eq 4985
+      expect(@collection.all.first).to be_a Invoice
+      expect(@collection.all.first.id).to eq '1'
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'returns nil if no invoice has the searched id' do
+      expect(@collection.find_by_id('2813308004')).to eq nil
+    end
+
+    it 'can return an invoice with a matching id' do
+      expect(@collection.find_by_id('1')).to be_a Invoice
+      expect(@collection.find_by_id('1').merchant_id).to eq '12335938'
+    end
+  end
+
+  describe '#find_all_by_customer_id' do
+    it 'returns an empty array if no invoices have matching customer id' do
+      expect(@collection.find_all_by_customer_id('2813308004')).to eq []
+    end
+
+    it 'returns an array if invoices have matching customer id' do
+      expect(@collection.find_all_by_customer_id('1').count).to eq 8
+      expect(@collection.find_all_by_customer_id('3').count).to eq 3
+    end
+  end
+
+  describe '#find_all_by_merchant_id' do
+    it 'returns an empty array if no invoices have matching merchant id' do
+      expect(@collection.find_all_by_merchant_id('2813308004')).to eq []
+    end
+
+    it 'returns an array if invoices have matching merchant id' do
+      expect(@collection.find_all_by_merchant_id('12335938').count).to eq 16
+      expect(@collection.find_all_by_merchant_id('12335955').count).to eq 12
+    end
+  end
+
+  describe '#find_all_by_status' do
+    it 'returns an empty array if no invoices have matching status' do
+      expect(@collection.find_all_by_status('in the void')).to eq []
+    end
+
+    it 'returns an array if invoices have matching status' do
+      expect(@collection.find_all_by_status('pending').count).to eq 1473
+      expect(@collection.find_all_by_status('shipped').count).to eq 2839
+    end
+  end
+
+  describe '#create' do
+    it 'can create a new Invoice with provided attributes' do
+      expect(@collection.find_by_id('4986')).to eq nil
+      @collection.create('created shop')
+      expect(@collection.find_by_id('4986')).to be_a Invoice
+    end
+  end
+end
