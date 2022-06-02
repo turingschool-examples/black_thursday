ac895c30c33a91429115af78893c09fe1c0fd3a7
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-02 16:53:53 -0500

    test: items count list tested

--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -9,19 +9,32 @@ RSpec.describe SalesAnalyst do
     @sales_analyst = sales_engine.analyst
   end
 
-  it "is a SalesAnalyst" do
+  it 'is a SalesAnalyst' do
     expect(@sales_analyst).to be_instance_of(SalesAnalyst)
   end
 
-  it "can tell you the average items per merchant" do
+  it 'can tell you the average items per merchant' do
     expect(@sales_analyst.average_items_per_merchant).to eq(2.88)
   end
 
-  it "can tell you the standard deviation of average items per merchant" do
+  it 'can tell you the standard deviation of average items per merchant' do
     expect(@sales_analyst.average_items_per_merchant_standard_deviation).to be_instance_of(Float) #change to 3.26 if population sd
   end
 
-  # xit "can tel"
+  it 'can find Merchant ids to be used to calculate standard deviation' do
+    expect(@sales_analyst.merchant_ids.class).to eq(Array)
+    expect(@sales_analyst.merchant_ids[0]).to eq(12334105)
+  end
+
+  it 'can create a list of count of items sold per merchant'do
+    @sales_analyst.merchant_ids
+    expect(@sales_analyst.items_count_list.class).to eq(Array)
+    expect(@sales_analyst.items_count_list[0]).to eq(3)
+  end
+
+  xit 'can tell you which Merchants sell the most Items' do
+
+  end
 
 
 end
