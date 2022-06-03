b5d00015f70546090d8ef16653be374ae3c234d3
mkbonini <mkbonini@gmail.com>
2022-06-03 10:53:10 -0600

    Add tests for standard deviation

--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -24,6 +24,17 @@ RSpec.describe SalesAnalyst do
 
     sales_analyst = sales_engine.analyst
 
-    expect(sales_analyst.average_items_per_merchant).to eq(5)
+    expect(sales_analyst.average_items_per_merchant).to eq(2.88)
+  end
+
+  it 'return a standard deviation of the number of items per merchant' do
+    sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
   end
 end
