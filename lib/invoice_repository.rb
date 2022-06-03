d0c6f0d61cb62463effa523422abe27189910497
Brenna Stuart <brennacodes@gmail.com>
2022-06-03 16:41:39 -0600

    Test: invoice_repo test #1 passing

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -4,7 +4,13 @@ class InvoiceRepository
   attr_reader :all
 
   def initialize(file_path)
-    @file_path = CSV.foreach(file_path, headers: true, header_converters: :symbol) {|row| Invoice.new(row)}
+    @all = make_repo(file_path)
+  end
+
+  def make_repo(file_path)
+    repo = Array.new
+    CSV.foreach(file_path, headers: true, header_converters: :symbol){|row| repo.push(Invoice.new(row))}
+    repo
   end
 
   def find_by_id(id_number)
