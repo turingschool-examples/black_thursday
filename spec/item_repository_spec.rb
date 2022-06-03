f9ef70a319bdd2419b8b53c983cc61ce425ead3b
Nick Jones <nickjones9605@gmail.com>
2022-06-02 19:36:42 -0600

    Test: item repository can find item by ID
    Co-authored-by: Thiago <thiago1992@yahoo.com>

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,3 +1,4 @@
+require './lib/item.rb'
 require './lib/item_repository'
 
 RSpec.describe ItemRepository do
@@ -9,5 +10,11 @@ RSpec.describe ItemRepository do
   it "can return an array of all known item instances" do
     item_repo = ItemRepository.new('./data/items.csv')
     expect(item_repo.all.count).to eq(1367)
+  end
+
+  it "can find item by ID" do
+    item_repo = ItemRepository.new('./data/items.csv')
+    expect(item_repo.find_by_id(263400305)).to be_a(Item)
+    expect(item_repo.find_by_id(12345678910)).to eq(nil)
   end
 end
