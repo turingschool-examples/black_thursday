ba0f742db0c4d591022b6582d3fc5e04dcfe1021
Sunny Moore <moore.sunny@gmail.com>
2022-06-03 15:53:07 -0700

    Create merchants_with_high_item_count

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -17,17 +17,26 @@ class SalesAnalyst
   end
   def average_items_per_merchant_standard_deviation
     mean = average_items_per_merchant
-    sum = items_per_merchant.sum(0.0) {|item| (item - mean) ** 2}
+    sum = items_per_merchant.values.sum(0.0) {|item| (item - mean) ** 2}
     variance = sum / (items_per_merchant.size - 1)
-    require 'pry';binding.pry
     standard_deviation = Math.sqrt(variance)
   end
+  def merchants_with_high_item_count
+    high_count =[]
+    @merchant_repository.all.each do |merchant|
+      item_count = @item_repository.find_all_by_merchant_id(merchant.id).count
+      if item_count > (average_items_per_merchant +  average_items_per_merchant_standard_deviation)
+        high_count << merchant
+      end
+    end
+    high_count
+  end
   def items_per_merchant
-    items_per_merchant =[]
+    items_per_merchant = Hash.new
 
     @merchant_repository.all.each do |merchant|
       item_count = @item_repository.find_all_by_merchant_id(merchant.id).count
-      items_per_merchant << item_count
+      items_per_merchant[merchant.id] = item_count
     end
     items_per_merchant
   end
