87ce486562466acae680dcb1652944a4f0537be3
Lucas Colwell <colwellc37@gmail.com>
2022-06-02 22:03:42 -0600

    Feat: create find_all methods for merchant_id and status

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -17,16 +17,38 @@ class InvoiceRepository
     end
   end
 
-  def find_all_by_customer_id(customer_id)
-    @all.find do |invoice|
-      invoice.id == customer_id
-    end
-  end
+  # def find_all_by_customer_id(customer_id)
+  #   @all.find do |invoice|
+  #     # require "pry"; binding.pry
+  #     invoice.id + 1 == customer_id
+  #   end
+  # end
+
+  # def find_all_by_customer_id(customer_id)
+  #   @all.find_all { |invoice| require "pry"; binding.pry invoice.customer_id == customer_id}
+  #
+  # end
 
   def find_all_by_merchant_id(merchant_id)
+    merchant_id_array = []
     @all.find do |invoice|
-      invoice.id == merchant_id
+      # require "pry"; binding.pry
+      if invoice.merchant_id == merchant_id
+        merchant_id_array << invoice.id
+      end
     end
+    merchant_id_array
+  end
+
+  def find_all_by_status(status)
+    status_array = []
+    @all.find_all do |invoice|
+      # require "pry"; binding.pry
+      if invoice.status == status
+        status_array << invoice.id
+      end
+    end
+    status_array
   end
 
 end
