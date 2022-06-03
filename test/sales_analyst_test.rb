4b2e12fdb89ff7a0346c8c266003e598d8c068f3
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-03 11:42:57 -0400

    Fix: rewrite golden_items tests

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -46,7 +46,50 @@ RSpec.describe SalesAnalyst do
     expect(@sales_analyst.average_average_price_per_merchant).to eq(35029.47)
   end
 
-  it "determins items that are TWO standard deviations above average item price" do
-    expect(@sales_analyst.golden_items).to be_instance_of(Array) #might be able to test more directly? maybe check to see if its an instance of items?
+  it 'returns overall average item price' do
+    expect(@sales_analyst.average_item_price).to eq(25105.51)
+  end
+
+  it 'returns standard deviation of item price' do
+    expect(@sales_analyst.item_price_standard_deviation).to eq(290099)
+  end
+
+  it 'returns items that are two standard deviations above average item price' do
+    expect(@sales_analyst.golden_items).to be_instance_of(Array)
+    expect(@sales_analyst.golden_items.length).to eq(5)
+    expect(@sales_analyst.golden_items.first.class).to eq(Item)
+
+  end
+
+  xit 'returns average invoices per merchant' do
+    expect(@sales_analyst.average_invoices_per_merchant).to eq(10.49)
+  end
+
+  xit 'returns average invoices per mechant standard deviation' do
+    expect(@sales_analyst.average_invoices_per_merchant_standard_deviation).to eq(3.29)
+  end
+
+  xit 'returns top merchants by invoice count' do
+    #array of merchants more than two standard deviations ABOVE the mean
+  end
+
+  xit 'returns bottom merchants by invoice count' do
+    #array of merchants more than two standard deviations BELOW the mean
+  end
+
+  xit 'returns days of the week where invoices are created at more than one standard deviation above the mean' do
+    expect(@sales_analyst.top_days_by_invoice_count).to eq(["Sunday, Saturday"])
+  end
+
+  xit 'returns percent of invoices pending' do
+    expect(@sales_analyst.invoice_status(:pending)).to eq(29.55)
+  end
+
+  xit 'returns percent of invoices shipped' do
+    expect(@sales_analyst.invoice_status(:shipped)).to eq(56.95)
+  end
+
+  xit 'returns percent of invoices returned' do
+    expect(@sales_analyst.invoice_status(:returned)).to eq(13.5)
   end
 end
