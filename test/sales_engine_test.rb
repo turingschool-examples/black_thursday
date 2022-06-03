28367f253c115e9455f014bcaab9b5b48e366491
Brenna Stuart <brennacodes@gmail.com>
2022-06-03 17:16:50 -0600

    Test: sales_engine all passing

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -7,40 +7,26 @@ RSpec.describe ItemRepository do
     :items     => "./data/items.csv",
     :merchants => "./data/merchants.csv"
   })}
+  let!(:sales_engine_inv) {SalesEngine.from_csv({:invoices => "./data/invoices.csv"})}
+  let!(:invoice_repo) {sales_engine_inv.invoices}
 
   it 'exists' do
     expect(sales_engine).to be_instance_of(SalesEngine)
   end
 
   it 'can return merchant repository' do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-      merchant_repo = sales_engine.merchants
+    merchant_repo = sales_engine.merchants
 
     expect(merchant_repo).to be_instance_of(MerchantRepository)
   end
 
   it 'can return item repository' do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
-      })
-
     item_repo = sales_engine.items
 
     expect(item_repo).to be_instance_of(ItemRepository)
   end
 
   it 'can return invoice repository' do
-    sales_engine = SalesEngine.from_csv({
-      :items => "./data/items.csv",
-      :merchants => "./data/invoices.csv"
-      })
-
-    invoice_repo = sales_engine.invoices
-
     expect(invoice_repo).to be_instance_of(InvoiceRepository)
   end
 
