29978a0f966477755f610d35715f731626b25526
Brenna Stuart <brennacodes@gmail.com>
2022-06-03 17:41:04 -0600

    Test: returns by customer id passing

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -10,8 +10,8 @@ class Invoice
 
   def initialize(input)
     @id = input[:id].to_i
-    @customer_id = input[:customer_id]
-    @merchant_id = input[:merchant_id]
+    @customer_id = input[:customer_id].to_i
+    @merchant_id = input[:merchant_id].to_i
     @status = input[:status]
     @created_at = input[:created_at]
     @updated_at = input[:updated_at]
