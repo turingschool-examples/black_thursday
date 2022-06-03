98d02574c7a6efeb19eacc2cca3cf05141eceae0
mkbonini <mkbonini@gmail.com>
2022-06-03 10:53:49 -0600

    Add method for standard deviation

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -16,6 +16,14 @@ class SalesAnalyst < SalesEngine
   end
 
   def average_items_per_merchant_standard_deviation
+    item_count = @merchants.all.map do |merchant|
+      @items.find_all_by_merchant_id(merchant.id).length
+    end
 
+    total_sum = item_count.sum do |count|
+      (count - average_items_per_merchant)**2
+    end
+
+    Math.sqrt(total_sum / (@merchants.all.length - 1)).round(2)
   end
 end
