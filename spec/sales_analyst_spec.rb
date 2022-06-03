8d2e4224779b7c009388ab989cef3e266ac09ad0
Justin Ramirez <jusrez528@gmail.com>
2022-06-03 14:34:57 -0400

    Create SalesAnalyst spec

---
+++ spec/sales_analyst_spec.rb
@@ -0,0 +1,29 @@
+require "./lib/sales_engine"
+require "./lib/item_repository"
+require "./lib/merchant_repository"
+require "./lib/sales_analyst"
+
+RSpec.describe SalesAnalyst do
+	it 'exists' do
+		sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+		sales_analyst = sales_engine.analyst
+
+		expect(sales_analyst).to be_an_instance_of(SalesAnalyst)
+	end
+
+	it 'can return the average items per merchant' do
+		#total items divided by total merchants
+		sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+		sales_analyst = sales_engine.analyst
+
+		expect(sales_analyst.average_items_per_merchant).to eq(2.88)
+
+	end
+
+end
