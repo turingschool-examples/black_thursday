368828ee7580ff6690a37ec615589a4f202917b1
Kevin Ta <kvnta@yahoo.com>
2022-06-03 11:57:44 -0600

    Fix merge conflict

--- lib/item.rb
+++ lib/item.rb
@@ -1,28 +1,12 @@
-<<<<<<< HEAD
+
 require 'CSV'
-
-class Item
-  attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
-
-  def initialize(item_attributes)
-    @id = item_attributes[:id].to_i
-    @name = item_attributes[:name]
-    @description = item_attributes[:description]
-    @unit_price = item_attributes[:unit_price]
-    @created_at = item_attributes[:created_at]
-    @updated_at = item_attributes[:updated_at]
-    @merchant_id = item_attributes[:merchant_id].to_i
-  end
-  def unit_price_to_dollars
-    @unit_price.to_f
-  end
 
 class Item
 	attr_reader :id,
 							:created_at,
 							:updated_at,
 							:merchant_id
-							
+
 	attr_accessor :name,
 								:description,
 								:unit_price
@@ -40,5 +24,5 @@ class Item
 	def unit_price_to_dollars
 		@unit_price.to_f
 	end
->>>>>>> d60f6a0a63a73fa7870db962ee7545ed9f4d505f
+
 end
