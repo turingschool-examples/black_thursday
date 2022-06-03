6d4882c37d1a87799dbfcbff4e7feb426a7d4f93
Nick Jones <nickjones9605@gmail.com>
2022-06-02 19:19:58 -0600

    Test: add test for it exists in item repository spec
    Co-authored-by: Thiago <thiago1992@yahoo.com>

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,5 +1,8 @@
-require '.lib/item_repository'
+require './lib/item_repository'
 
 RSpec.describe ItemRepository do
-
+  it "exists" do
+    item_repo = ItemRepository.new('./data/items.csv')
+    expect(item_repo).to be_a(ItemRepository)
+  end
 end
