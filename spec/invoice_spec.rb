3fe6ffdf34af6da9e5f9a8e6f97c28eb742c578e
Nick Jones <nickjones9605@gmail.com>
2022-06-04 17:09:07 -0600

    Test: finished tests and methods for invoice.rb
    Co-authored-by: Thiago <thiago1992@yahoo.com>

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -28,4 +28,53 @@ RSpec.describe Invoice do
 
      expect(inv.id).to eq(6)
   end
+
+  it "returns the customer_id" do
+    inv = Invoice.new({
+     :id              => 6,
+     :customer_id     => 7,
+     :status => "pending",
+     :created_at  => Time.now,
+     :updated_at  => Time.now,
+     :merchant_id => 8})
+
+     expect(inv.customer_id).to eq(7)
+  end
+
+  it "returns the merchant_id" do
+    inv = Invoice.new({
+     :id              => 6,
+     :customer_id     => 7,
+     :status => "pending",
+     :created_at  => Time.now,
+     :updated_at  => Time.now,
+     :merchant_id => 8})
+
+     expect(inv.merchant_id).to eq(8)
+   end
+
+   it "returns the status" do
+     inv = Invoice.new({
+      :id              => 6,
+      :customer_id     => 7,
+      :status => "pending",
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 8})
+
+      expect(inv.status).to eq("pending")
+    end
+
+    it "returns the time updated at and created at" do
+      inv = Invoice.new({
+       :id              => 6,
+       :customer_id     => 7,
+       :status => "pending",
+       :created_at  => Time.now.round,
+       :updated_at  => Time.now.round,
+       :merchant_id => 8})
+
+       expect(inv.created_at).to eq(Time.now.round)
+       expect(inv.updated_at).to eq(Time.now.round)
+     end
 end
