0eaa5251e757b65540b48c5e3d9f0147819058be
Thomas Turner <thomasturner482@yahoo.com>
2022-06-04 15:29:58 -0500

    finished transaction and transaction repository

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,7 +1,7 @@
 require 'csv'
-require_relative 'invoice'
+require_relative 'transaction'
 
-class InvoiceRepository
+class TransactionRepository
   attr_reader :all
 
   def initialize(file_path)
@@ -9,11 +9,12 @@ class InvoiceRepository
     @all = []
 
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-      @all << Invoice.new({
+      @all << Transaction.new({
         :id => row[:id],
-        :customer_id => row[:customer_id],
-        :merchant_id => row[:merchant_id],
-        :status => row[:status],
+        :invoice_id => row[:invoice_id],
+        :credit_card_number => row[:credit_card_number],
+        :credit_card_expiration_date => row[:credit_card_expiration_date],
+        :result => row[:result],
         :created_at => row[:created_at],
         :updated_at => row[:updated_at]
         })
@@ -21,58 +22,62 @@ class InvoiceRepository
   end
 
   def find_by_id(id_search)
-    @all.find do |invoice|
-      invoice.id == id_search
+    @all.find do |transaction|
+      transaction.id == id_search
     end
   end
 
-  def find_all_by_customer_id(customer_id_search)
-    @all.find_all do |invoice|
-      invoice.customer_id == customer_id_search
+  def find_all_by_invoice_id(invoice_id_search)
+    @all.find_all do |transaction|
+      transaction.invoice_id == invoice_id_search
     end
   end
 
-  def find_all_by_merchant_id(merchant_id_search)
-    @all.find_all do |invoice|
-      invoice.merchant_id == merchant_id_search
+  def find_all_by_credit_card_number(credit_card_number_search)
+    @all.find_all do |transaction|
+      transaction.credit_card_number == credit_card_number_search
     end
   end
 
-  def find_all_by_status(status_search)
-    @all.find_all do |invoice|
-      invoice.status == status_search
+  def find_all_by_result(result_search)
+    @all.find_all do |transaction|
+      transaction.result == result_search
     end
   end
 
   def create(attributes)
     new_id = 0
-    @all.each do |invoice|
-      if invoice.id.to_i > new_id
-        new_id = invoice.id.to_i + 1
+    @all.each do |transaction|
+      if transaction.id.to_i >= new_id
+        new_id = transaction.id.to_i + 1
       end
     end
-    @all << Invoice.new( {
+    @all << Transaction.new( {
       :id => new_id.to_s,
-      :customer_id => attributes[:customer_id],
-      :merchant_id => attributes[:merchant_id],
-      :status => attributes[:status],
+      :invoice_id => attributes[:invoice_id],
+      :credit_card_number => attributes[:credit_card_number],
+      :credit_card_expiration_date => attributes[:credit_card_expiration_date],
+      :result => attributes[:result],
       :created_at => attributes[:created_at],
       :updated_at => attributes[:updated_at]
       } )
   end
 
-  def update(invoice_id_search, status_update)
-    @all.find do |invoice|
-      invoice.id == invoice_id_search
-      invoice.status = status_update
-      invoice.updated_at = Time.now
+  def update(transaction_id_search, new_credit_card_number, new_credit_card_expiration_date, new_result)
+    @all.find do |transaction|
+      if transaction.id == transaction_id_search
+        transaction.credit_card_number = new_credit_card_number
+        transaction.credit_card_expiration_date = new_credit_card_expiration_date
+        transaction.result = new_result
+        transaction.updated_at = Time.now
+      end
     end
   end
 
-  def delete(invoice_id_search)
-    @all.find do |invoice|
-      invoice.id == invoice_id_search
-      @all.delete(invoice)
+  def delete(transaction_id_search)
+    @all.find do |transaction|
+      transaction.id == transaction_id_search
+      @all.delete(transaction)
     end
   end
 end
--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -1,100 +1,105 @@
-require './lib/invoice_repository'
+require './lib/transaction_repository'
 
-RSpec.describe InvoiceRepository do
+RSpec.describe TransactionRepository do
   before :each do
-    @filepath = './data/invoices.csv'
-    @collection = InvoiceRepository.new(@filepath)
+    @filepath = './data/transactions.csv'
+    @collection = TransactionRepository.new(@filepath)
     @attributes = {
-      :customer_id => '1',
-      :merchant_id => '12335938',
-      :status => 'pending',
-      :created_at => '2009-02-07',
-      :updated_at => '2014-03-15'
+      :id => 6,
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at  => '1994-05-07 23:38:43 UTC',
+      :updated_at  => '2016-01-11 11:30:35 UTC',
     }
   end
 
   describe '#initialize' do
-    it 'is an InvoiceRepository' do
-      expect(@collection).to be_a InvoiceRepository
+    it 'is an TransactionRepository' do
+      expect(@collection).to be_a TransactionRepository
     end
 
-    it 'can return an array of all known Invoice instances' do
+    it 'can return an array of all known Transaction instances' do
       expect(@collection.all).to be_a Array
       expect(@collection.all.count).to eq 4985
-      expect(@collection.all.first).to be_a Invoice
+      expect(@collection.all.first).to be_a Transaction
       expect(@collection.all.first.id).to eq '1'
     end
   end
 
   describe '#find_by_id' do
-    it 'returns nil if no invoice has the searched id' do
+    it 'returns nil if no transaction has the searched id' do
       expect(@collection.find_by_id('2813308004')).to eq nil
     end
 
-    it 'can return an invoice with a matching id' do
-      expect(@collection.find_by_id('1')).to be_a Invoice
-      expect(@collection.find_by_id('1').merchant_id).to eq '12335938'
+    it 'can return an transaction with a matching id' do
+      expect(@collection.find_by_id('1')).to be_a Transaction
+      expect(@collection.find_by_id('1').invoice_id).to eq '2179'
     end
   end
 
-  describe '#find_all_by_customer_id' do
-    it 'returns an empty array if no invoices have matching customer id' do
-      expect(@collection.find_all_by_customer_id('2813308004')).to eq []
+  describe '#find_all_by_invoice_id' do
+    it 'returns an empty array if no transactions have matching invoice id' do
+      expect(@collection.find_all_by_invoice_id('2813308004')).to eq []
     end
 
-    it 'returns an array if invoices have matching customer id' do
-      expect(@collection.find_all_by_customer_id('1').count).to eq 8
-      expect(@collection.find_all_by_customer_id('3').count).to eq 3
-    end
-  end
-
-  describe '#find_all_by_merchant_id' do
-    it 'returns an empty array if no invoices have matching merchant id' do
-      expect(@collection.find_all_by_merchant_id('2813308004')).to eq []
-    end
-
-    it 'returns an array if invoices have matching merchant id' do
-      expect(@collection.find_all_by_merchant_id('12335938').count).to eq 16
-      expect(@collection.find_all_by_merchant_id('12335955').count).to eq 12
+    it 'returns an array if transactions have matching invoice id' do
+      expect(@collection.find_all_by_invoice_id('1').count).to eq 2
+      expect(@collection.find_all_by_invoice_id('2').count).to eq 2
     end
   end
 
-  describe '#find_all_by_status' do
-    it 'returns an empty array if no invoices have matching status' do
-      expect(@collection.find_all_by_status('in the void')).to eq []
+  describe '#find_all_by_credit_card_number' do
+    it 'returns an empty array if no transactions have matching credit card number' do
+      expect(@collection.find_all_by_credit_card_number('2813308004')).to eq []
     end
 
-    it 'returns an array if invoices have matching status' do
-      expect(@collection.find_all_by_status('pending').count).to eq 1473
-      expect(@collection.find_all_by_status('shipped').count).to eq 2839
+    it 'returns an array if transactions have matching credit card number' do
+      expect(@collection.find_all_by_credit_card_number('4068631943231473').count).to eq 1
+      expect(@collection.find_all_by_credit_card_number('4426959354436260').count).to eq 1
+    end
+  end
+
+  describe '#find_all_by_result' do
+    it 'returns an empty array if no transactions have matching result' do
+      expect(@collection.find_all_by_result('in the void')).to eq []
+    end
+
+    it 'returns an array if transactions have matching result' do
+      expect(@collection.find_all_by_result('success').count).to eq 4158
+      expect(@collection.find_all_by_result('failed').count).to eq 827
     end
   end
 
   describe '#create' do
-    it 'can create a new Invoice with provided attributes' do
+    it 'can create a new transaction with provided attributes' do
       expect(@collection.find_by_id('4986')).to eq nil
       @collection.create(@attributes)
-      expect(@collection.find_by_id('4986')).to be_a Invoice
-      expect(@collection.find_by_id('4986').customer_id).to eq '1'
-      expect(@collection.find_by_id('4986').merchant_id).to eq '12335938'
-      expect(@collection.find_by_id('4986').status).to eq 'pending'
-      expect(@collection.find_by_id('4986').created_at).to eq '2009-02-07'
-      expect(@collection.find_by_id('4986').updated_at).to eq '2014-03-15'
+      expect(@collection.find_by_id('4986')).to be_a Transaction
+      expect(@collection.find_by_id('4986').invoice_id).to eq 8
+      expect(@collection.find_by_id('4986').credit_card_number).to eq '4242424242424242'
+      expect(@collection.find_by_id('4986').credit_card_expiration_date).to eq '0220'
+      expect(@collection.find_by_id('4986').result).to eq 'success'
+      expect(@collection.find_by_id('4986').created_at).to eq '1994-05-07 23:38:43 UTC'
+      expect(@collection.find_by_id('4986').updated_at).to eq '2016-01-11 11:30:35 UTC'
     end
   end
 
   describe '#update' do
-    it 'can update the status of an invoice' do
-      expect(@collection.find_by_id('1').status).to eq 'pending'
-      @collection.update('1', 'shipped')
-      expect(@collection.find_by_id('1').status).to eq 'shipped'
-      expect(@collection.find_by_id('1').updated_at).not_to eq '2014-03-15'
+    it 'can update the result of an transaction' do
+      expect(@collection.find_by_id('4980').result).to eq 'failed'
+      @collection.update('4980', '1234567890', '0923', 'success')
+      expect(@collection.find_by_id('4980').credit_card_number).to eq '1234567890'
+      expect(@collection.find_by_id('4980').credit_card_expiration_date).to eq '0923'
+      expect(@collection.find_by_id('4980').result).to eq 'success'
+      expect(@collection.find_by_id('4980').updated_at).not_to eq '2014-03-15'
     end
   end
 
   describe '#delete' do
-    it 'can delete an Invoice based on id' do
-      expect(@collection.find_by_id('1').status).to eq 'pending'
+    it 'can delete an transaction based on id' do
+      expect(@collection.find_by_id('1').result).to eq 'success'
       @collection.delete('1')
       expect(@collection.find_by_id('1')).to eq nil
     end
