4651b6b51722391200b8431a3fa79f75d12b94e9
Nicole Esquer <nicole.esquer5@gmail.com>
2022-06-04 14:05:10 -0700

    test: find by invoice id

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -19,4 +19,9 @@ RSpec.describe TransactionRepository do
     expect(@tr.find_by_id(4987)).to be_nil
   end
 
+  it "can find_all_by_invoice_id" do
+    expect(@tr.find_all_by_item_id(2179)).to be_a(Array)
+    expect(@tr.find_all_by_item_id(2179).length).to eq(164)
+  end
+
 end
