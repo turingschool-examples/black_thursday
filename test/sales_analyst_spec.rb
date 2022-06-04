b54953acca6beaacec4389078f440f6b6a9fd1a2
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-06-04 14:13:40 -0600

    add tests for invoice_status and top days by invoice

--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -176,4 +176,32 @@ RSpec.describe SalesAnalyst do
 
     expect(sales_analyst.bottom_merchants_by_invoice_count.length).to eq(4)
   end
+
+  it 'can find top_days_by_invoice_count' do
+    sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv"
+    })
+
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.top_days_by_invoice_count.length).to eq(1)
+    expect(sales_analyst.top_days_by_invoice_count.first).to eq("Wednesday")
+  end
+
+  it 'can find top_days_by_invoice_count' do
+    sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv"
+    })
+
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.invoice_status(:pending)).to eq(29.55)
+    expect(sales_analyst.invoice_status(:shipped)).to eq(56.95)
+    expect(sales_analyst.invoice_status(:returned)).to eq(13.5)
+  end
+
 end
