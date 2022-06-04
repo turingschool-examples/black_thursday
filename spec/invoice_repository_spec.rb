86430130e533d21872f9f40b1e86a6534cd21070
Nick Jones <nickjones9605@gmail.com>
2022-06-04 17:58:19 -0600

    Test: add test to item repository spec for finding all invoices by specific customer id
    Co-authored-by: Thiago <thiago1992@yahoo.com>

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -22,4 +22,13 @@ RSpec.describe InvoiceRepository do
     expect(invoice_repo.find_by_id(1)).to be_a(Invoice)
     expect(invoice_repo.find_by_id(123542345293423)).to eq(nil)
   end
+
+  it "can find all invoices by a specific customer id" do
+    invoice_repo = InvoiceRepository.new('./data/invoices.csv')
+    item_repo = ItemRepository.new('./data/items.csv')
+    merchant_repo = MerchantRepository.new('./data/merchants.csv')
+    expect(invoice_repo.find_all_by_customer_id(1)).to be_instance_of(Array)
+    expect(invoice_repo.find_all_by_customer_id(1).length).to eq(3)
+    expect(invoice_repo.find_all_by_customer_id(12345678910112)).to eq([])
+  end
 end
