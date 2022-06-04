40f1c813a38373a2f939afd328e1ed1ad0a80481
Bryce Simonds <bsimonds14@gmail.com>
2022-06-04 13:50:57 -0600

    Debug: re-working SalesEngine initialize

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,9 +3,15 @@ class SalesEngine
 
   attr_reader :item_repository, :merchant_repository
 
-  def initialize(items_path, merchants_path)
+  def initialize(items_path = nil,
+                 merchants_path = nil,
+                 invoices_path = nil,
+                 invoice_items_path = nil,
+                 customer_path = nil,
+                 transaction_path = nil)
     @item_repository = ItemRepository.new(items_path)
     @merchant_repository = MerchantRepository.new (merchants_path)
+    @invoice_path = InvoicesPath.ew (invoice_path)
   end
 
   def self.from_csv(data)
