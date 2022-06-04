6e14894083eda767248d6f2360033a9f643a8661
Bryce Simonds <bsimonds14@gmail.com>
2022-06-04 11:44:46 -0600

    Feat: added the class

---
+++ spec/invoice_item_spec.rb
@@ -0,0 +1,24 @@
+require 'CSV'
+require './lib/item'
+require 'BigDecimal'
+require 'pry'
+
+
+RSpec.describe InvoiceItem do
+  before :each do
+    @x = Time.now
+    @ii = InvoiceItem.new({
+        :id => 6,
+        :item_id => 7,
+        :invoice_id => 8,
+        :quantity => 1,
+        :unit_price => BigDecimal(10.99, 4),
+        :created_at => Time.now,
+        :updated_at => Time.now
+        })
+      end
+
+  it 'exists' do
+    expect(@ii).to be_a InvoiceItem
+  end
+end
