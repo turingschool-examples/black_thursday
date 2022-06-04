dd0d0f6c5481ce279891201b63c5c6d798de716a
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-04 14:08:41 -0600

    add invoice class

---
+++ lib/invoice.rb
@@ -0,0 +1,21 @@
+
+
+class Invoice
+  attr_reader :id,
+              :customer_id,
+              :merchant_id,
+              :status,
+              :created_at,
+              :updated_at
+
+  def initialize(details)
+    @id = details[:id].to_i
+    @customer_id = details[:customer_id].to_i
+    @merchant_id = details[:merchant_id].to_i
+    @status = details[:status]
+    @created_at = details[:created_at]
+    @updated_at = details[:updated_at]
+
+  end
+
+end
