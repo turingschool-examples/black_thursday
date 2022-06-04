3b1c16c2126bf320321e0c08778a032b5d58644f
Lucas Colwell <colwellc37@gmail.com>
2022-06-04 14:34:47 -0600

    Test: refactor customer tests

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -11,33 +11,30 @@ RSpec.describe Customer do
           :created_at => Time.now,
           :updated_at => Time.now
         })
-      @
     end
 
     it 'exists' do
       expect(@customer).to be_a(Customer)
     end
 
-    xit 'can return integer id' do
+    it 'can return integer id' do
       expect(@customer.id).to eq(6)
     end
 
-    xit "can return the first name" do
+    it "can return the first name" do
       expect(@customer.first_name).to eq("Joan")
     end
 
-    xit "can return the last name" do
+    it "can return the last name" do
       expect(@customer.last_name).to eq("Clarke")
     end
 
-    xit "can return a Time instance for the date the customer was first created" do
+    it "can return a Time instance for the date the customer was first created" do
       expect(@customer.created_at).to be_a(Time)
     end
 
     it "can return a Time instance for the date the customer was last modified" do
       expect(@customer.updated_at).to be_a(Time)
     end
-
   end
-
 end
