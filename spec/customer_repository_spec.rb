980603a796e48ffbacd71baeec21616419ceaae7
Bryan Shears <brshears@gmail.com>
2022-06-04 16:11:11 -0600

    Create find_by_id tests

--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe CustomerRepository do
   it 'exists' do
     expect(@customer_repository).to be_a CustomerRepository
   end
+
+  it "can find a customer by id and return nil if no customer is found" do
+    expect(@customer_repository.find_by_id(0)).to eq nil
+    expect(@customer_repository.find_by_id(1)).to be_a(Customer)
+    expect(@customer_repository.find_by_id(1000)).to be_a(Customer)
+  end
+
 end
