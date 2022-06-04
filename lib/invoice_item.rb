f9c7e68a47fb5d98f6e4649e0ed85cabe40abad8
Bryce Simonds <bsimonds14@gmail.com>
2022-06-04 11:50:46 -0600

    Feat: initialized InvoiceItem

---
+++ lib/invoice_item.rb
@@ -0,0 +1,22 @@
+class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :created_at
+
+  attr_accessor :unit_price,
+                :updated_at,
+                :quantity
+
+
+  def initialize(details)
+    @id = details[:id]
+    @item_id = details[:item_id]
+    @invoice_id = details[:invoice_id]
+    @quantity = details[:quantity]
+    @unit_price = details[:unit_price]
+    @created_at = details[:created_at]
+    @updated_at = details[:updated_at]
+  end
+
+end
