46e1844269feda6ad3d090085b81a92340a645a4
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-04 13:57:04 -0600

    Feat - add 'average_item_price_for_merchant' test

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -8,12 +8,24 @@ RSpec.describe SalesAnalyst do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst).to be_a SalesAnalyst
   end
-  it "exists" do
+  it "calculates average_items_per_merchant" do
     sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
     sales_analyst = sales_engine.analyst
 
     expect(sales_analyst.average_items_per_merchant).to be_a Float
     expect(sales_analyst.average_items_per_merchant).to eq 2.88
   end
+
+  it "calculates average_item_price_for_merchant" do
+    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.average_item_price_for_merchant).to be_a Float
+    expect(sales_analyst.average_item_price_for_merchant).to eq 2.88
+  end
+
+
+
+
 
 end
