9a72bbd0f90e9d139cbb7efef598623b96d6793a
Bryce Simonds <bsimonds14@gmail.com>
2022-06-04 10:46:37 -0600

    Refactor: changed description of test to show average items per merchant

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe SalesAnalyst do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst).to be_a SalesAnalyst
   end
-  it "exists" do
+  it "shows average items per merchant" do
     sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
     sales_analyst = sales_engine.analyst
 
-    expect(sales_analyst.average_items_per_merchant).to be_a Float
+    expect(sales_analyst.average_items_per_merchant.class).to eq Float
     expect(sales_analyst.average_items_per_merchant).to eq 2.88
   end
 
