875c13bcae1700e62a6aaf6944847ea18add770b
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-06-04 10:20:51 -0600

    created invoice, invoice repo, unpdated sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,18 +5,20 @@ require_relative 'merchant'
  # require_relative 'sales_analyst'
 class SalesEngine
 
-  attr_reader :merchants, :items
+  attr_reader :merchants, :items, :invoices
 
-  def initialize(items_path, merchants_path)
+  def initialize(items_path, merchants_path, invoice_path)
     @items_path = items_path
     @merchants_path = merchants_path
+    @invoice_path = invoice_path
     @items = ItemRepository.new(items_path)
     @merchants = MerchantRepository.new(merchants_path)
+    @invoices = InvoiceRepository.new(invoice_path)
     # @analyst= SalesAnalyst.new(items_path, merchants_path)
   end
 
   def self.from_csv(data)
-    SalesEngine.new(data[:items], data[:merchants])
+    SalesEngine.new(data[:items], data[:merchants], data[:invoices])
   end
 
   def analyst
