89042d8492f1f802f0d81f50b8223680f3043169
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-03 20:06:00 -0500

    feat: Add updated at attribute for Invoice

--- test/invoice_spec.rb
+++ test/invoice_spec.rb
@@ -28,16 +28,17 @@ RSpec.describe Invoice do
     expect(@i.merchant_id).to eq(8)
   end
 
-  xit 'has a status' do
+  it 'has a status' do
     expect(@i.status).to eq("pending")
   end
 
-  xit 'has a created at time stamp' do
-    expect(@i.created_at).to eq Time.parse("2015-07-10 00:00:00 -0600")
+  it 'has a created at time stamp' do
+    sample_time = Time.now
+    expect(@i.created_at).to be < sample_time
     expect(@i.created_at.class).to eq(Time)
   end
 
-  xit 'has an updated at time stamp' do
+  it 'has an updated at time stamp' do
     expect(@i.updated_at).to be_instance_of(Time)
   end
 end
