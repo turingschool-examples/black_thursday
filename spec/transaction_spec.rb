7da089b561722aa70ae4cdd9df69aeab430885e1
Nicole Esquer <nicole.esquer5@gmail.com>
2022-06-04 12:38:37 -0700

    Initialized transaction instance

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,17 @@
+require './lib/transaction'
+
+RSpec.describe Transaction do
+  before :each do
+    @t = Transaction.new({
+      :id => 6,
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at => Time.now,
+      :updated_at => Time.now
+    })
+  end
+
+
+end
