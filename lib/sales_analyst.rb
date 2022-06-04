8d1f53f837b3428110333d4b18ab940a7b485734
Nick Jones <nickjones9605@gmail.com>
2022-06-04 16:06:09 -0600

    Fix: changed require in lib files to require relative
    Co-authored-by: Thiago <thiago1992@yahoo.com>

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,6 +1,6 @@
-require "./lib/sales_engine"
-require "./lib/item_repository"
-require "./lib/merchant_repository"
+require_relative "./sales_engine"
+require_relative "./item_repository"
+require_relative "./merchant_repository"
 require "bigdecimal"
 require "bigdecimal/util"
 
