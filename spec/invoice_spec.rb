ba1beda3f9720dd85563f58c64ba3e2017b4b258
Nick Jones <nickjones9605@gmail.com>
2022-06-04 16:43:20 -0600

    Test: add test for returning the ID
    Co-authored-by: Thiago <thiago1992@yahoo.com>

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -1,16 +1,31 @@
 require './lib/item'
 require 'time'
 require 'bigdecimal'
+require './lib/invoice.rb'
 
 RSpec.describe Invoice do
 
   it "exists" do
-    inv = Invoice.new({ :id          => 1,
-     :name        => "Pencil",
-     :description => "You can use it to write things",
-     :unit_price  => BigDecimal(10.99,4),
+    inv = Invoice.new({
+     :id              => 6,
+     :customer_id     => 7,
+     :status => "pending",
      :created_at  => Time.now,
      :updated_at  => Time.now,
-     :merchant_id => 2})
+     :merchant_id => 8})
+
+     	expect(inv).to be_instance_of(Invoice)
+  end
+
+  it "returns the id" do
+    inv = Invoice.new({
+     :id              => 6,
+     :customer_id     => 7,
+     :status => "pending",
+     :created_at  => Time.now,
+     :updated_at  => Time.now,
+     :merchant_id => 8})
+
+     expect(inv.id).to eq(6)
   end
 end
