da099caf1d104c4a38811c4596c773ca6fe75a48
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-04 10:59:41 -0700

    Feat/created InvoiceIteme class, added initialize method and attr_reader/spec tests passed

---
+++ lib/invoice_item.rb
@@ -0,0 +1,19 @@
+class InvoiceItem
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
+
+  def initialize(data_hash)
+    @id = data_hash[:id]
+    @item_id = data_hash[:item_id]
+    @invoice_id = data_hash[:invoice_id]
+    @quantity = data_hash[:quantity]
+    @unit_price = data_hash[:unit_price]
+    @created_at = data_hash[:created_at]
+    @updated_at = data_hash[:updated_at]
+  end
+end
