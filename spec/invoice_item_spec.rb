cd55b4f69952ad7d2857d4b81d32da7505405616
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-04 11:44:51 -0700

    Test/added a before each and added test for returns price as a float

--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -2,8 +2,8 @@ require 'BigDecimal'
 require './lib/invoice_item'
 
 RSpec.describe InvoiceItem do
-  it "exists and has attributes" do
-    invoice_item = InvoiceItem.new(
+  before :each do
+    @invoice_item = InvoiceItem.new(
     {
     :id => 6,
     :item_id => 7,
@@ -14,13 +14,20 @@ RSpec.describe InvoiceItem do
     :updated_at => Time.now
     }
   )
-    expect(invoice_item).to be_a(InvoiceItem)
-    expect(invoice_item.id).to eq(6)
-    expect(invoice_item.item_id).to eq(7)
-    expect(invoice_item.invoice_id).to eq(8)
-    expect(invoice_item.quantity).to eq(1)
-    expect(invoice_item.unit_price).to eq(BigDecimal(10.99, 4))
-    expect(invoice_item.created_at).to be_a(Time)
-    expect(invoice_item.updated_at).to be_a(Time)
+  end
+
+  it "exists and has attributes" do
+    expect(@invoice_item).to be_a(InvoiceItem)
+    expect(@invoice_item.id).to eq(6)
+    expect(@invoice_item.item_id).to eq(7)
+    expect(@invoice_item.invoice_id).to eq(8)
+    expect(@invoice_item.quantity).to eq(1)
+    expect(@invoice_item.unit_price).to eq(BigDecimal(10.99, 4))
+    expect(@invoice_item.created_at).to be_a(Time)
+    expect(@invoice_item.updated_at).to be_a(Time)
+  end
+
+  it "returns the price of invoice items in dollars formatted as a float" do
+    expect(@invoice_item.unit_price_to_dollars).to eq 10.99
   end
 end
