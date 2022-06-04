35780ccb247df826d4f44bf7ac3caa9d20170eb0
Sunny Moore <moore.sunny@gmail.com>
2022-06-04 15:33:06 -0700

    Put unit price back to float, this will need to be refactored

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,4 @@
+require 'bigdecimal'
 class Item
 attr_accessor :id,
             :name,
@@ -13,10 +14,10 @@ attr_accessor :id,
     @description = data_hash[:description]
     @created_at = data_hash[:created_at]
     @updated_at = data_hash[:updated_at]
-    @unit_price = data_hash[:unit_price]
+    @unit_price = data_hash[:unit_price].to_f
     @merchant_id = data_hash[:merchant_id].to_i
   end
-
+#Refactor
   def unit_price_to_dollars
     @unit_price.to_f
   end
