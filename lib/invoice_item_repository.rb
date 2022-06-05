9321cf86e94a21f12976dca07ea874f2054a10b6
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 13:23:59 -0600

    Feat: added find ALL by item id method

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -33,8 +33,8 @@ class InvoiceItemRepository
     end
   end
 
-  def find_by_item_id(item_id)
-    @all.find do |invoice_item|
+  def find_all_by_item_id(item_id)
+    @all.find_all do |invoice_item|
       invoice_item.item_id.to_i == item_id.to_i
     end
   end
