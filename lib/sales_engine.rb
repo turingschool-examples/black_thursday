f40a507bd1c799aef947532e936fb7c8a29fb59c
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 16:49:08 -0600

    Refactor: took up to date github merge into file

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,18 +3,9 @@ class SalesEngine
 
   attr_reader :item_repository, :merchant_repository
 
-  def initialize(items_path = nil,
-                 merchants_path = nil,
-                 invoices_path = nil,
-                 invoice_items_path = nil,
-                 customers_path = nil,
-                 transactions_path = nil)
+  def initialize(items_path, merchants_path)
     @item_repository = ItemRepository.new(items_path)
     @merchant_repository = MerchantRepository.new (merchants_path)
-    @invoices = InvoicesPath.new (invoices_path)
-    @invoice_items_repository = InvoiceItemRepository.new (invoice_items_path)
-    @customer_repository = CustomerRepository.new (customers_path)
-    @transactions_repository = TransactionRepository.new 
   end
 
   def self.from_csv(data)
