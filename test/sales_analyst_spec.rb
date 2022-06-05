97a13cf96c7ec57f85bd5701af4b87dfe52c93ef
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-05 09:51:08 -0500

    Add in top days of the week invoice count test

--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -70,4 +70,8 @@ RSpec.describe SalesAnalyst do
   it 'finds the bottom merchants by invoice count' do
     expect(@sales_analyst.bottom_merchants_by_invoice_count.length).to eq(4)
   end
+
+  it 'finds the top days of the week by invoice count' do
+    expect(@sales_analyst.top_days_by_invoice_count.legth).to eq(1)
+  end  
 end
