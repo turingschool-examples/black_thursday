0fa5f1b81da2d426f38261a822d6e235b8452d53
Tyler Caudill <teecaud@hotmail.com>
2022-06-05 14:36:41 -0500

    added invoices to sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,17 +1,20 @@
 require 'pry'
 require_relative "merchant_repository"
 require_relative "item_repository"
-class SalesEngine
-  attr_reader :item_repository, :merchant_repository
+require_relative "invoice_repository"
 
-  def initialize(items_path, merchants_path)
+class SalesEngine
+  attr_reader :item_repository, :merchant_repository, :invoices
+
+  def initialize(items_path, merchants_path, invoice_path)
     @item_repository = ItemRepository.new(items_path)
     @merchant_repository = MerchantRepository.new(merchants_path)
+    @invoices = InvoiceRepository.new(invoice_path)
 
   end
 
   def self.from_csv(data)
-    SalesEngine.new(data[:items], data[:merchants])
+    SalesEngine.new(data[:items], data[:merchants], data[:invoices])
   end
 
 end
