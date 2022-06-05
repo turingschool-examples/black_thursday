a58b69a0d758f32a545ddddd71025a8789781344
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 16:08:24 -0600

    Test: can find all by last name

--- spec/customer_respository_spec.rb
+++ spec/customer_respository_spec.rb
@@ -28,11 +28,12 @@ RSpec.describe CustomerRepository do
     expect(@sales_engine.find_all_by_first_name(fragment)).to be_a Array
   end
 
-  # it "can find ALL by last name" do
-  #   expect(@sales_engine.find_all_by_last_name(1).count).to eq 0
-  #   expect(@sales_engine.find_all_by_last_name(9002)).to eq([])
-  #   expect(@sales_engine.find_all_by_last_name(1)).to be_a Array
-  # end
+  it "can find ALL by last name" do
+    fragment = "On"
+    expect(@sales_engine.find_all_by_last_name(fragment).count).to eq 0
+    expect(@sales_engine.find_all_by_last_name("CHEESE")).to eq([])
+    expect(@sales_engine.find_all_by_last_name(fragment)).to be_a Array
+  end
 # #
 #   it "can find all by credit_card_number" do
 #     expect(@sales_engine.find_all_by_credit_card_number(4068631943231473).count).to eq 1
