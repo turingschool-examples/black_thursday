8be1bed98bbc48172fb1c288b45add3f663a06ba
Wesley Miranda <wesleywes42@gmail.com>
2022-06-05 13:49:03 -0600

    Transaction code

---
+++ lib/transaction.rb
@@ -0,0 +1,22 @@
+require 'csv'
+
+class Transaction
+  attr_accessor :id,
+              :invoice_id,
+              :credit_card_number,
+              :credit_card_expiration_date,
+              :result,
+              :created_at,
+              :updated_at
+
+  def initialize(data_hash)
+    @id = data_hash[:id]
+    @invoice_id = data_hash[:invoice_id]
+    @credit_card_number = data_hash[:credit_card_number]
+    @credit_card_expiration_date = data_hash[:credit_card_expiration_date]
+    @result = data_hash[:result]
+    @created_at = data_hash[:created_at]
+    @updated_at = data_hash[:updated_at]
+  end
+
+end
