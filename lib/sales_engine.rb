2e4ae0d2f0627eedd96bf210ed84f86ec63f39c8
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-04 19:02:39 -0700

    Feat/initialized invoice_items_repository in sales_engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,19 +1,21 @@
 class SalesEngine
   attr_reader :item_repository,
-              :merchant_repository
-              :invoice_repository
+              :merchant_repository,
+              :invoice_repository,
+              :invoice_items_repository
 
-  def initialize(items_path, merchants_path, invoices_path)
+  def initialize(items_path, merchants_path, invoices_path, invoice_items_path)
     @item_repository = ItemRepository.new(items_path)
     @merchant_repository = MerchantRepository.new(merchants_path)
     @invoice_repository = InvoiceRepository.new(invoices_path)
+    @invoice_items_repository = InvoiceItemRepository.new(invoice_items_path)
   end
 
   def self.from_csv(data)
-    SalesEngine.new(data[:items], data[:merchants], data[:invoices])
+    SalesEngine.new(data[:items], data[:merchants], data[:invoices], data[:invoice_items])
   end
 
   def analyst
-    SalesAnalyst.new(@item_repository, @merchant_repository, @invoice_repository)
+    SalesAnalyst.new(@item_repository, @merchant_repository, @invoice_repository, @invoice_items_repository)
   end
 end
