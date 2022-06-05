234014a95e82451e61560d5b011dd390cb7de979
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 13:10:06 -0600

    Test: added find by id test

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -4,7 +4,6 @@ require'./lib/invoice_item'
 
 RSpec.describe InvoiceItemRepository do
   before :each do
-
     @sales_engine = InvoiceItemRepository.new("./data/invoice_items.csv")
   end
 
@@ -13,17 +12,15 @@ RSpec.describe InvoiceItemRepository do
   end
 
   it "returns all known Invoice Item instances" do
-
     expect(@sales_engine.all).to be_a Array
     expect(@sales_engine.all.count).to eq(21830)
   end
 #
-#   it "can find_by_id" do
-#
-#     expect(@merchant_repository.find_by_id(12334105)).to eq(@merchant_repository.all.first)
-#     expect(@merchant_repository.find_by_id(1233)).to eq(nil)
-#     expect(@merchant_repository.find_by_id(12334105)).to be_a(Merchant)
-#   end
+  it "can find_by_id" do
+    expect(@sales_engine.find_by_id(263519844)).to eq(@sales_engine.all.first)
+    expect(@sales_engine.find_by_id(8675309)).to eq(nil)
+    expect(@sales_engine.find_by_id(263519844)).to be_a(InvoiceItem)
+  end
 #
 #   it "can find_by_name" do
 #
