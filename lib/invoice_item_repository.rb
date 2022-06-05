b3e076c70e1dcdf0ae17e40023a8a88b4282dcba
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 13:07:09 -0600

    Feat: added initialize method and attr's. Also copy and pasted similiar methods but grayed out

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,8 +1,70 @@
+require 'entry'
 class InvoiceItemRepository
-  attr_reader
 
-  def initialize(data)
-    @data = data
+  attr_reader :id,
+              :item_id,
+              :invoice_id,
+              :created_at,
+              :all
+
+  attr_accessor :unit_price,
+                :updated_at,
+                :quantity
+
+  def initialize(file_path)
+    @file_path = file_path
+    @all = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      @all << InvoiceItem.new(
+        :id => row[:id].to_i,
+        :item_id => row[:item_id],
+        :invoice_id => row[:invoice_id],
+        :quantity => row[:quantity],
+        :unit_price => row[:unit_price],
+        :created_at => row[:created_at],
+        :updated_at => row[:updated_at]
+        )
+      end
   end
 
 end
+
+
+
+
+#
+# def find_by_id(id)
+#   @all.find do |merchant|
+#     merchant.id == id
+#   end
+# end
+#
+# def find_by_name(name)
+#   @all.find do |merchant|
+#     merchant.name.upcase == name.upcase
+#   end
+# end
+#
+# def find_all_by_name(name_fragment)
+#   @all.find_all do |merchant|
+#     merchant.name.upcase.include?(name_fragment.upcase)
+#   end
+# end
+#
+# def create(attributes)
+#   x = (@all.last.id + 1)
+#   @all << Merchant.new({
+#     :id => x,
+#     :name => attributes[:name]
+#     })
+# end
+#
+# def update(id, attributes)
+#   x = find_by_id(id)
+#   x.name = attributes[:name]
+# end
+#
+# def delete(id)
+#   x = find_by_id(id)
+#   @all.delete(x)
+# end
