85f08d873fd075a2d8b66ff29b55c6c5ca389a2b
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-04 20:59:18 -0700

    Test/can return an array of all known invoice item instances-GR

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe InvoiceItemRepository do
   it "exists" do
     expect(@invoice_item_repository).to be_a(InvoiceItemRepository)
   end
+
+  it "can return an array of all known invoice item instances" do
+    expect(@invoice_item_repository.all).to be_a(Array)
+    expect(@invoice_item_repository.all.length).to eq(21830)
+    expect(@invoice_item_repository.all.first).to be_a(InvoiceItem)
+    expect(@invoice_item_repository.all.first.id).to eq("1")
+    expect(@invoice_item_repository.all.first.quantity).to eq("5")
+  end
 end
