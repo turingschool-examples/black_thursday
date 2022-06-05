05c5661447945b2585e919c613ca0609cfd77dc7
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 16:28:04 -0600

    Test: testing delete method

--- spec/customer_respository_spec.rb
+++ spec/customer_respository_spec.rb
@@ -64,10 +64,10 @@ RSpec.describe CustomerRepository do
     expect(@sales_engine.find_by_id(1).last_name).to eq("Clarke")
     expect(@sales_engine.find_by_id(1).updated_at).to be_a Time
   end
-# #
-#   it "can delete a transaction instance" do
-#     expect(@sales_engine.find_by_id(1)).to be_a(Customer)
-#     @sales_engine.delete(1)
-#     expect(@sales_engine.find_by_id(1)).to eq(nil)
-#   end
+
+  it "can delete a customer instance" do
+    expect(@sales_engine.find_by_id(1)).to be_a(Customer)
+    @sales_engine.delete(1)
+    expect(@sales_engine.find_by_id(1)).to eq(nil)
+  end
 end
