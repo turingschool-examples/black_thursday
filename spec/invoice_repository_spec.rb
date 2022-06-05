842473b5278380badbdf4b29f810c0f9f6e8c5a7
Thiago Silveira <thiago1992@yahoo.com>
2022-06-04 21:19:27 -0600

    Test: Invoice repo can find all invoices by a matching status
    Co-authored-by: Nick <nickjones9605@gmail.com>
    Co-authored-by: Kevin <kvnta@yahoo.com>

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -36,4 +36,12 @@ RSpec.describe InvoiceRepository do
     expect(invoice_repo.find_all_by_merchant_id(12335938).length).to eq(16)
     expect(invoice_repo.find_all_by_merchant_id(12345678910112)).to eq([])
   end
+
+  it 'can find all invoices by a matching status' do
+    invoice_repo = InvoiceRepository.new('./data/invoices.csv')
+    expect(invoice_repo.find_all_by_status("shipped")).to be_a(Array)
+    expect(invoice_repo.find_all_by_status("returned")).to be_a(Array)
+    expect(invoice_repo.find_all_by_status("pending")).to be_a(Array)
+    expect(invoice_repo.find_all_by_status("super cool status")).to eq([])
+  end
 end
