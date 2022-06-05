8471dbfc352af9f01e8a3ae7a17c3c1fb9d5cf2a
Anna Marie Sterling <sterling.am@gmail.com>
2022-06-05 13:56:22 -0600

    Sales analyst average items standard deviation

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -6,19 +6,31 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
+    # (@items_path.all.count.to_f / @merchants_path.all.count).round(2)
     mean = array_of_sums.reduce(:+).to_f / array_of_sums.count
     mean.round(2)
   end
 
   def group_by_merchant_id
-    frog = @items_path.all.group_by do |item|
+    @items_path.all.group_by do |item|
       item.merchant_id
     end
   end
 
   def array_of_sums
-  chicken = group_by_merchant_id.map do |id, items|
+    group_by_merchant_id.map do |id, items|
       items.count
     end
+  end
+
+  def step_1
+    bunny = array_of_sums.map do |sum|
+      (sum - average_items_per_merchant)**2
+    end.sum
+  end
+
+  def average_items_per_merchant_standard_deviation
+    variance = step_1 / (array_of_sums.count - 1)
+    standard_deviation = Math.sqrt(variance).round(2)
   end
 end
