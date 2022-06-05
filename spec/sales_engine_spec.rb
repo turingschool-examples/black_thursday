65f8f5f9f96a71f69e756609d217dc24c56a107a
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-04 23:46:38 -0400

    Add invoices to data that is read by sales engine and corresponding tests

--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe SalesEngine do
   it "exists" do
     sales_engine = SalesEngine.from_csv({
       :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv"
     })
     expect(sales_engine).to be_instance_of SalesEngine
   end
@@ -12,7 +13,8 @@ RSpec.describe SalesEngine do
   it "can return an array of all items" do
     sales_engine = SalesEngine.from_csv({
       :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv"
     })
     expect(sales_engine.items).to be_instance_of ItemRepository
   end
@@ -20,8 +22,18 @@ RSpec.describe SalesEngine do
   it "can return an array of all merchants" do
     sales_engine = SalesEngine.from_csv({
       :items => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv"
     })
     expect(sales_engine.merchants).to be_instance_of MerchantRepository
+  end
+
+  it "can return an array of all invoices" do
+    sales_engine = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv"
+    })
+    expect(sales_engine.invoices).to be_instance_of InvoiceRepository
   end
 end
