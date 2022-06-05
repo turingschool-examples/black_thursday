0f15a297a8d0cc119f3ea0456b5a48af5600fc46
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 15:43:00 -0600

    Test: created and passed has attributes test

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Customer do
       :id => 6,
       :first_name => "Joan",
       :last_name => "Clarke",
-      :created_at => Time.now,
-      :updated_at => Time.now
+      :created_at => @x,
+      :updated_at => @x
     })
       end
 
@@ -16,15 +16,13 @@ RSpec.describe Customer do
     expect(@c).to be_a Customer
   end
 
-  # it 'has attributes' do
-  #   expect(@c.id).to eq(6)
-  #   expect(@c.invoice_id).to eq(8)
-  #   expect(@c.credit_card_number).to eq("4242424242424242")
-  #   expect(@c.credit_card_expiration_date).to eq("0220")
-  #   expect(@c.result).to eq("success")
-  #   expect(@c.created_at).to be_a Time
-  #   expect(@c.updated_at).to be_a Time
-  #   expect(@c.created_at).to eq @x
-  #   expect(@c.updated_at).to eq @x
-  # end
+  it 'has attributes' do
+    expect(@c.id).to eq(6)
+    expect(@c.first_name).to eq("Joan")
+    expect(@c.last_name).to eq("Clarke")
+    expect(@c.created_at).to be_a Time
+    expect(@c.updated_at).to be_a Time
+    expect(@c.created_at).to eq @x
+    expect(@c.updated_at).to eq @x
+  end
 end
