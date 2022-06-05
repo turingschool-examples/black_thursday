623b6a022540ec34195bdb7c27dfdd3b8459834f
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-06-04 18:57:31 -0700

    Test/added test for it exists

---
+++ lib/invoice_item_repository.rb
@@ -0,0 +1,15 @@
+require 'csv'
+require_relative 'invoice_item'
+
+class InvoiceItemRepository
+  attr_reader :file_path,
+              :all
+
+ def initialize(file_path)
+   @file_path = file_path
+   @all = []
+   CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+     @all << InvoiceItemI.new({:id => row[:id], :item_id => row[:item_id], :invoice_id => row[:invoice_id], :quantity => row[:quantity], :unit_price => row[:unit_price], :created_at => row[:created_at], :updated_at => row[:updated_at]})
+   end
+ end
+end
