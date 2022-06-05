9fe0023659fa603243568d21505113ef4909fa63
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 12:51:34 -0600

    Feat: created an instance of InvoiceItemRepo.

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -4,10 +4,10 @@ require'./lib/invoice_item_repository'
 RSpec.describe InvoiceItemRepository do
   before :each do
 
-    @salesengine = SalesEngine.new(:invoice_items => "./data/invoice_items.csv")
+    @salesengine = InvoiceItemRepository.new(:invoice_items => "./data/invoice_items.csv")
   end
 
   it 'exists' do
-    expect(@salesengine.details[:invoice_items]).to be_a InvoiceItemRepository
+    expect(@salesengine).to be_a InvoiceItemRepository
   end
 end
