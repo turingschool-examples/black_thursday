80e6f0c4c3c03f3581511f7b8af6449cde523e3f
Gauri Joshi <gauri728@gmail.com>
2022-06-05 10:37:33 -0700

    Feat/Test average items per merchant, group_by_merchant id, array of items

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,6 +1,28 @@
 class SalesAnalyst
+  attr_reader :items_path, :merchants_path
   def initialize(items_path, merchants_path)
     @items_path = items_path
     @merchants_path = merchants_path
+  end
+
+  def average_items_per_merchant
+    sum = array_of_sums.inject do |sum, element|
+       sum + element
+     end
+     mean = sum.to_f / array_of_sums.count
+     mean.round(2)
+
+  end
+
+  def group_by_merchant_id
+    frog = @items_path.all.group_by do |item|
+      item.merchant_id
+    end
+  end
+
+  def array_of_sums
+  chicken = group_by_merchant_id.map do |id, items|
+      items.count
+    end
   end
 end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -3,13 +3,37 @@ require "./lib/item_repository"
 require "./lib/merchant_repository"
 require "./lib/sales_analyst"
 
+
 RSpec.describe SalesAnalyst do
-  xit "exists" do
-    sales_analyst = SalesEngine.from_csv({
+  before :each do
+    @sales_engine = SalesEngine.from_csv({
       :items => "./data/items.csv",
       :merchants => "./data/merchants.csv"
-
     })
-    expect(sales_analyst).to be_instance_of SalesAnalyst
+    @sales_analyst = @sales_engine.analyst
   end
+
+  it "exists" do
+
+    expect(@sales_analyst).to be_instance_of SalesAnalyst
+    # expect(@sales_analyst.items_path).to be_instance_of Array
+  end
+
+  it "can group by merchant id" do
+
+    expect(@sales_analyst.group_by_merchant_id).to be_a Hash
+  end
+
+  it "can return an array of sums" do
+
+    expect(@sales_analyst.array_of_sums).to be_a Array
+  end
+
+  it "can calculate the average number of items per merchant" do
+
+    expect(@sales_analyst.average_items_per_merchant).to eq 2.88
+  end
+
+
+
 end
