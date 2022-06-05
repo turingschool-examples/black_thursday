3420de1526866f7e2964e4e42789501900fcbba8
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-05 12:21:26 -0600

    Test - Add 'merchants_with_high_item_count' test

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -27,11 +27,20 @@ RSpec.describe SalesAnalyst do
   it "calculates standard dev" do
     sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
     sales_analyst = sales_engine.analyst
-# require "pry"; binding.pry
 
     expect(sales_analyst.average_items_per_merchant_standard_deviation).to be_a Float
     expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq 3.26
   end
+  it "sorts merchants_with_high_item_count" do
+    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.merchants_with_high_item_count).to be_a Array
+    expect(sales_analyst.merchants_with_high_item_count.count).to eq(52)    
+    expect(sales_analyst.merchants_with_high_item_count.first).to be_a Merchant
+    expect(sales_analyst.merchants_with_high_item_count.first.id).to eq(12334195)
+  end
+
 
 
 
