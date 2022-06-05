b43f6584b4d4e0a6aa826c5458ba43a743ae30e3
Bryce Simonds <bsimonds14@gmail.com>
2022-06-05 12:48:45 -0600

    Test: added it exists test

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -4,10 +4,10 @@ require'./lib/invoice_item_repository'
 RSpec.describe InvoiceItemRepository do
   before :each do
 
-    @se = SalesEngine.from_csv(:invoice_items => "./data/invoice_items.csv")
+    @salesengine = SalesEngine.new(:invoice_items => "./data/invoice_items.csv")
   end
 
   it 'exists' do
-    expect(@se.details[:invoice_items]).to be_a InvoiceItemRepository
+    expect(@salesengine.details[:invoice_items]).to be_a InvoiceItemRepository
   end
 end
