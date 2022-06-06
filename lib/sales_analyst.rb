7f38ea166f9e8625bd29c724a1df2e713b3f5af2
Sunny Moore <moore.sunny@gmail.com>
2022-06-06 16:03:20 -0700

    Creat invoices_per_merchant helper method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -86,4 +86,19 @@ class SalesAnalyst
     merchants = @merchant_repository.all.count.to_f
     (invoices / merchants).round(2)
   end
+  def invoices_per_merchant
+    invoice_items = Hash.new
+    invoice_count = 0
+    @merchant_repository.all.each do |merchant|
+      invoice_count = @invoice_repository.find_all_by_merchant_id(merchant.id).size
+      invoice_items[merchant.id] = invoice_count
+    end
+    invoice_items
+  end
+  def average_invoices_per_merchant_standard_deviation
+    mean = average_invoices_per_merchant
+    sum = invoices_per_merchant.values.sum(0.0) {|item| (item - mean) ** 2}
+    variance = (sum / (invoices_per_merchant.size - 1)).to_f
+    standard_deviation = Math.sqrt(variance).round(2)
+  end
 end
