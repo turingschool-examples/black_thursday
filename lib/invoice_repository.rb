a212f10c554a31d39aba552f1ae5725d2ce89adb
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-05 20:08:14 -0600

    add test for returning array and find by id

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -26,4 +26,8 @@ class InvoiceRepository
 
 
 
+
+
+
+
 end
--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -1,17 +1,27 @@
 require './lib/invoice_repository'
-
+require './lib/invoice'
 
 
 RSpec.describe InvoiceRepository do
   before :each do
-    @Invoice_repository = InvoiceRepository.new("./data/invoices.csv")
+    @invoice_repository = InvoiceRepository.new("./data/invoices.csv")
   end
 
   it 'exists' do
-    expect(@Invoice_repository).to be_a InvoiceRepository
+    expect(@invoice_repository).to be_a InvoiceRepository
   end
 
+  it 'returns an array of invoice instances' do
+    expect(@invoice_repository.all).to be_a Array
+    expect(@invoice_repository.all.count).to eq(4985)
 
+  end
+
+  it 'can find by id' do
+    expect(@invoice_repository.find_by_id(263395237)).to eq(@invoice_repository.all.first)
+    expect(@invoice_repository.find_by_id(1233)).to eq(nil)
+    expect(@invoice_repository.find_by_id(263395237)).to be_a(Invoice)
+  end
 
 
 
