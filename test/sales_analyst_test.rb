13b5547e0985bb4968959e92ccd661595392d34f
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-06 13:14:21 -0400

    Fix: add csv files to test initalization

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -6,7 +6,10 @@ RSpec.describe SalesAnalyst do
   let!(:sales_engine) {SalesEngine.from_csv({
     :items     => "./data/items.csv",
     :merchants => "./data/merchants.csv",
-    :invoices => "./data/invoices.csv"
+    :invoices => "./data/invoices.csv",
+    :invoice_items => './data/invoice_items.csv',
+    :transactions => './data/transactions.csv',
+    :customers => './data/customers.csv'
   })}
   let!(:sales_analyst) {sales_engine.analyst
   }
