cd50e3b1447f7179ebea9bf38bfcd0d4ae5c4968
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-06 12:53:05 -0700

    Test -Clean up lines in spec

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -2,7 +2,10 @@ require './lib/merchant'
 
 RSpec.describe Merchant do
   before :each do
-    @merchant = Merchant.new({:id => 5, :name => "Turing School"})
+    @merchant = Merchant.new({
+      :id   => 5, 
+      :name => "Turing School"
+      })
   end
 
   it "exists" do
@@ -10,7 +13,6 @@ RSpec.describe Merchant do
   end
 
   it 'has an id and name' do
-
     expect(@merchant.id).to eq(5)
     expect(@merchant.name).to eq("Turing School")
   end
