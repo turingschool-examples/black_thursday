9fd3ca1b70364364c445f8121e77810c6b5c3802
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-06 12:53:22 -0700

    Test -Clean up lines in spec

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -1,5 +1,6 @@
 require './lib/merchant_repository'
 require './lib/merchant'
+require 'pry'
 
 RSpec.describe MerchantRepository do
   before :each do
@@ -7,25 +8,21 @@ RSpec.describe MerchantRepository do
   end
 
   it "exists" do
-
     expect(@merchant_repository).to be_a(MerchantRepository)
   end
 
   it "returns all known Merchants" do
-
     expect(@merchant_repository.all).to be_a Array
     expect(@merchant_repository.all.count).to eq(475)
   end
 
   it "can find_by_id" do
-
     expect(@merchant_repository.find_by_id(12334105)).to eq(@merchant_repository.all.first)
     expect(@merchant_repository.find_by_id(1233)).to eq(nil)
     expect(@merchant_repository.find_by_id(12334105)).to be_a(Merchant)
   end
 
   it "can find_by_name" do
-
     expect(@merchant_repository.find_by_name("Shopin1901")).to eq(@merchant_repository.all.first)
     expect(@merchant_repository.find_by_name("XYZ")).to eq(nil)
     expect(@merchant_repository.find_by_name("Shopin1901")).to be_a(Merchant)
@@ -67,7 +64,5 @@ RSpec.describe MerchantRepository do
     @merchant_repository.delete(12334105)
     expect(@merchant_repository.find_by_name("Shopin1901")).to eq(nil)
   end
-
-  
 
 end
