c1b0d11d31567576494982a6a503935cb528fc3b
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-06 13:28:26 -0700

    Feat -Add missing end

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -16,7 +16,7 @@ class SalesAnalyst
   def invoice_paid_in_full?(invoice_id)
     x = @transactions_repository.all
     y = x.find do |transaction|
-          transaction.invoice_id == invoice_id
+      transaction.invoice_id == invoice_id
       if y.result == "success"
         true
       end
@@ -26,14 +26,15 @@ class SalesAnalyst
   def invoice_total(invoice_id)
     x = @invoice_item_repository.all
     y = x.find_all do |invoice_item|
-          invoice_item.invoice_id == invoice_id
-        end
+      invoice_item.invoice_id == invoice_id
+    end
     #y should now be an array of invoice item instances
     z = y.find_all do |invoice_instance|
       invoice_instance.unit_price
     end
     #z should have an array of all unit prices from the same invoice id
     z.sum
+  end
     #looking at spec harness, they want the sum for the test to be 21067.77.
     #Looking at the unit price in the invoice items csv,
     #there are no decimals. So I am confused.... :(
