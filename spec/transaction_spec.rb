f6a90e9c2f25aca387741b5afe92ff23978b0bce
Thomas Turner <thomasturner482@yahoo.com>
2022-06-06 10:10:46 -0500

    Initial commit

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,49 @@
+require './lib/transaction'
+
+RSpec.describe Transaction do
+  before :each do
+    @transaction = Transaction.new({
+      :id => 6,
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at  => '1994-05-07 23:38:43 UTC',
+      :updated_at  => '2016-01-11 11:30:35 UTC',
+      })
+  end
+
+  describe '#initialize' do
+    it 'is an transaction' do
+      expect(@transaction).to be_a Transaction
+    end
+
+    it 'has an id' do
+      expect(@transaction.id).to eq 6
+    end
+
+    it 'has a invoice_id' do
+      expect(@transaction.invoice_id).to eq 8
+    end
+
+    it 'has a credit_card_number' do
+      expect(@transaction.credit_card_number).to eq "4242424242424242"
+    end
+
+    it 'has a credit_card_expiration_date' do
+      expect(@transaction.credit_card_expiration_date).to eq "0220"
+    end
+
+    it 'has a result' do
+      expect(@transaction.result).to eq "success"
+    end
+
+    it 'has a created_at time' do
+      expect(@transaction.created_at).to eq '1994-05-07 23:38:43 UTC'
+    end
+
+    it 'has an update_at time' do
+      expect(@transaction.updated_at).to eq '2016-01-11 11:30:35 UTC'
+    end
+  end
+end
