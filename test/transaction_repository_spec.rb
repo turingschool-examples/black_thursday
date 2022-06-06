23a25e34aaa7fdac346bf6c685e0eb23b9a8868c
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-06-06 16:37:04 -0600

    add tests for find methods, by creditcard # and invoice

---
+++ test/transaction_repository_spec.rb
@@ -0,0 +1,61 @@
+require './lib/merchant'
+require './lib/merchant_repository'
+require './lib/item'
+require './lib/item_repository'
+require './lib/sales_engine'
+require './lib/invoice'
+require './lib/invoice_repository'
+require './lib/transaction'
+require './lib/transaction_repository'
+
+describe InvoiceRepository do
+  let(:sales_engine) {SalesEngine.from_csv({
+     :items     => "./data/items.csv",
+     :merchants => "./data/merchants.csv",
+     :invoices => "./data/invoices.csv",
+     :invoice_items => "./data/invoice_items.csv",
+     :transactions => "./data/transactions.csv",
+     :customers => "./data/customers.csv"
+     })}
+  let(:transaction) {sales_engine.transactions}
+
+  it "exists" do
+    expect(transaction).to be_an(TransactionRepository)
+  end
+
+  it "has attributes" do
+    expect(transaction.all).to be_an(Array)
+    expect(transaction.all.first.id).to eq(1)
+    expect(transaction.all.first.invoice_id).to eq(2179)
+    expect(transaction.all.first.credit_card_number).to eq(4068631943231473)
+    expect(transaction.all.first.credit_card_expiration_date).to eq(0217)
+    expect(transaction.all.first.created_at).to eq("2012-02-26 20:56:56 UTC")
+    expect(transaction.all.first.updated_at).to eq("2012-02-26 20:56:56 UTC")
+    expect(transaction.all.first.result).to eq(:success)
+    expect(transaction.all.length).to eq(4985)
+  end
+
+  it 'can find by id' do
+    transactionexample = transaction.all.first.id
+
+    expect(transaction.find_by_id(1)).to eq(transactionexample)
+    expect(transaction.find_by_id(100000)).to eq(nil)
+  end
+
+  it 'can find_all_by_invoice_id' do
+    transactionexample = transaction.all.find_by_id(3)
+
+    expect(transaction.find_all_by_invoice_id(1000000)).to eq([])
+    expect(transaction.find_all_by_invoice_id(2179).length).to eq(2)
+    expect(transaction.find_all_by_invoice_id(750).first).to eq(transactionexample)
+  end
+
+  it 'can find_all_by_credit_card_number' do
+    transactionexample = transaction.all.find_by_id(3)
+
+    expect(transaction.find_all_by_credit_card_number(1000000)).to eq([])
+    expect(transaction.find_all_by_credit_card_number("4848466917766329").length).to eq(1)
+    expect(transaction.find_all_by_credit_card_number("4271805778010747").first).to eq(transactionexample)
+  end
+
+end
