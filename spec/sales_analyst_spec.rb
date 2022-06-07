3461813d571cc55cb35114f37f1c1c16378b7749
Ben Silverstein <benjsilverstein@comcast.net>
2022-06-07 17:52:22 -0600

    Fix - updated test to use @ variable in before :each statement

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -41,55 +41,37 @@ RSpec.describe SalesAnalyst do
   end
 
   it "calculates average_item_price_for_merchant" do
-    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
-    sales_analyst = sales_engine.analyst
-
-    expect(sales_analyst.average_item_price_for_merchant(12334105)).to be_a Float
-    expect(sales_analyst.average_item_price_for_merchant(12334105)).to eq 16.66
+    expect(@sales_analyst.average_item_price_for_merchant(12334105)).to be_a Float
+    expect(@sales_analyst.average_item_price_for_merchant(12334105)).to eq 16.66
   end
 
   it "calculates standard dev" do
-    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
-    sales_analyst = sales_engine.analyst
-
-    expect(sales_analyst.average_items_per_merchant_standard_deviation).to be_a Float
-    expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq 3.26
+    expect(@sales_analyst.average_items_per_merchant_standard_deviation).to be_a Float
+    expect(@sales_analyst.average_items_per_merchant_standard_deviation).to eq 3.26
   end
   it "sorts merchants_with_high_item_count" do
-    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
-    sales_analyst = sales_engine.analyst
-
-    expect(sales_analyst.merchants_with_high_item_count).to be_a Array
-    expect(sales_analyst.merchants_with_high_item_count.count).to eq(52)
-    expect(sales_analyst.merchants_with_high_item_count.first).to be_a Merchant
-    expect(sales_analyst.merchants_with_high_item_count.first.id).to eq(12334195)
+    expect(@sales_analyst.merchants_with_high_item_count).to be_a Array
+    expect(@sales_analyst.merchants_with_high_item_count.count).to eq(52)
+    expect(@sales_analyst.merchants_with_high_item_count.first).to be_a Merchant
+    expect(@sales_analyst.merchants_with_high_item_count.first.id).to eq(12334195)
   end
 
   it "can sum all of the averages and find the average price across all merchants" do
-    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
-    sales_analyst = sales_engine.analyst
-    expect(sales_analyst.average_average_price_per_merchant).to be_a Float
-    expect(sales_analyst.average_average_price_per_merchant).to eq(350.29)
+    expect(@sales_analyst.average_average_price_per_merchant).to be_a Float
+    expect(@sales_analyst.average_average_price_per_merchant).to eq(350.29)
   end
 
   it "can calculate standard_deviation of item_price" do
-    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
-    sales_analyst = sales_engine.analyst
-
-    expect(sales_analyst.price_std_dev).to eq(290099.0)
+    expect(@sales_analyst.price_std_dev).to eq(290099.0)
   end
 
   it "can return items 2 standard deviations above average" do
-    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
-    sales_analyst = sales_engine.analyst
-    expect(sales_analyst.golden_items).to be_a Array
-    expect(sales_analyst.golden_items.first.class).to eq Item
-    expect(sales_analyst.golden_items.length).to eq 5
+    expect(@sales_analyst.golden_items).to be_a Array
+    expect(@sales_analyst.golden_items.first.class).to eq Item
+    expect(@sales_analyst.golden_items.length).to eq 5
   end
 
   it "can calculate average invoices per merchant" do
-    sales_engine = SalesEngine.new("./data/items.csv", "./data/merchants.csv")
-    sales_analyst = sales_engine.analyst
     expect(sales_analyst.average_invoices_per_merchant).to eq(10.49)
   end
 
