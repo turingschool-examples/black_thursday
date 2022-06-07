58532698d125853df7481ad6be37adb11440d570
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 15:34:20 -0600

    Update to include all repositories necessary for work up to It. 3

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,21 +2,33 @@ require_relative 'item'
 require_relative 'item_repository'
 require_relative 'merchant'
 require_relative 'merchant_repository'
+require_relative 'invoice'
+require_relative 'invoice_repository'
+require_relative 'invoice_item'
+require_relative 'invoice_item_repository'
+require_relative 'transaction'
+require_relative 'transaction_repository'
+require_relative 'customer'
+require_relative 'customer_repository'
 require_relative 'sales_analyst'
 
 class SalesEngine
   attr_reader :items, :merchants
 
-  def initialize(items_path, merchant_path)
+  def initialize(items_path, merchant_path, invoices_path, invoice_item_path, transaction_path, customer_path)
     @items = ItemRepository.new(items_path)
     @merchants = MerchantRepository.new(merchant_path)
+    @invoices = InvoiceRepository.new(invoices_path)
+    @invoice_items = InvoiceItemRepository.new(invoice_item_path)
+    @transactions = TransactionRepository.new(transaction_path)
+    @customers = CustomerRepository.new(customer_path)
   end
 
   def self.from_csv(data)
-    SalesEngine.new(data[:items], data[:merchants])
+    SalesEngine.new(data[:items], data[:merchants], data[:invoices], data[:invoice_items], data[:transactions], data[:customers])
   end
 
   def analyst
-    SalesAnalyst.new(@items, @merchants)
+    SalesAnalyst.new(@items, @merchants, @invoices, @invoice_items, @transactions, @customers)
   end
 end
