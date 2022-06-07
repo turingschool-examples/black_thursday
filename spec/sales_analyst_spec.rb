cf9195d9536b72d18e44400651c914f2c0f5d206
Sunny Moore <moore.sunny@gmail.com>
2022-06-06 19:34:28 -0700

    Test invoice_status method

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -98,6 +98,11 @@ RSpec.describe SalesAnalyst do
    end
    it "can calculate day of the week invoices above 1 standard deviation" do
      expect(@sales_analyst.top_days_by_invoice_count).to be_a Array
-     expect(@sales_analyst.top_days_by_invoice_count.to_include?("Wednesday")).to eq(true)
+     expect(@sales_analyst.top_days_by_invoice_count.include?("Wednesday")).to eq(true)
+   end
+   it "can return the percentage of invoices shipped/pending/returned" do
+     expect(@sales_analyst.invoice_status(:pending)).to eq(29.55)
+     expect(@sales_analyst.invoice_status(:shipped)).to eq(56.95)
+     expect(@sales_analyst.invoice_status(:returned)).to eq(13.5)
    end
 end
