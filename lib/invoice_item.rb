7ab85e7157b54b500329dc972200987d96016b67
Nick Jones <nickjones9605@gmail.com>
2022-06-06 19:12:22 -0600

    Test: add ability to return unit price as float
    Co-authored-by: Thiago <Thiago1992@yahoo.com>
    Co-authored-by: Kevin <kvnta@yahoo.com>

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,13 +1,23 @@
 class InvoiceItem
+attr_reader :id,
+            :item_id,
+            :invoice_id,
+            :quantity,
+            :unit_price,
+            :created_at,
+            :unit_price_to_dollars,
+            :updated_at
 
   def initialize(data)
     @id = data[:id]
-    @item_id = data[:merchant_id]
+    @item_id = data[:item_id]
     @invoice_id = data[:invoice_id]
     @quantity = data[:quantity]
     @unit_price = data[:unit_price]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
-
+    @unit_price_to_dollars = @unit_price.to_f
   end
+
+
 end
--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -4,6 +4,7 @@ require "Rspec"
 
 RSpec.describe InvoiceItem do
 
+
   it 'exists' do
     data = ({:id => 1,
     :item_id => 263519844,
@@ -23,9 +24,27 @@ RSpec.describe InvoiceItem do
     :invoice_id => 1,
     :quantity => 5,
     :unit_price  => 13635,
+    :created_at  => Time.now.round,
+    :updated_at  => Time.now.round})
+
+    invoiceitem = InvoiceItem.new(data)
+    expect(invoiceitem.id).to eq(1)
+    expect(invoiceitem.item_id).to eq(263519844)
+    expect(invoiceitem.quantity).to eq(5)
+    expect(invoiceitem.created_at).to eq(Time.now.round)
+    expect(invoiceitem.updated_at).to eq(Time.now.round)
+  end
+
+  it "can return the unit price as a float" do
+    data = ({:id => 1,
+    :item_id => 263519844,
+    :invoice_id => 1,
+    :quantity => 5,
+    :unit_price  => 13635,
     :created_at  => Time.now,
     :updated_at  => Time.now})
 
     invoiceitem = InvoiceItem.new(data)
+    expect(invoiceitem.unit_price_to_dollars).to eq(13635.0)
   end
 end
