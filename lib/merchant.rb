754fe7a7bd0b547afc75c01e6da4eb41ad1be5d3
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-06 22:58:48 -0400

    Add created at and updated at variables

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -4,9 +4,12 @@ class Merchant
   def initialize(data)
     @id = data[:id].to_i
     @name = data[:name]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
   end
 
   def update(attributes)
     @name = attributes[:name] unless attributes[:name].nil?
+    @updated_at = Time.now
   end
 end
