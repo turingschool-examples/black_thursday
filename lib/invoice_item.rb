adaf0a636d84f2ed5d2242b522502923a56f523a
Nick Jones <nickjones9605@gmail.com>
2022-06-06 18:52:00 -0600

    Test: add test for returning the details of an invoice ID
    Co-authored-by: Thiago <thiago1992@yahoo.com>
    Co-authored-by: Kevin <kvnta@yahoo.com>

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,2 +1,13 @@
 class InvoiceItem
-end 
+
+  def initialize(data)
+    @id = data[:id]
+    @item_id = data[:merchant_id]
+    @invoice_id = data[:invoice_id]
+    @quantity = data[:quantity]
+    @unit_price = data[:unit_price]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
+
+  end
+end
--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -1,10 +1,31 @@
 require "./lib/invoice_item"
+require "bigdecimal"
 require "Rspec"
 
 RSpec.describe InvoiceItem do
 
   it 'exists' do
-    invoiceitem = InvoiceItem.new
+    data = ({:id => 1,
+    :item_id => 263519844,
+    :invoice_id => 1,
+    :quantity => 5,
+    :unit_price  => 13635,
+    :created_at  => Time.now,
+    :updated_at  => Time.now})
+
+    invoiceitem = InvoiceItem.new(data)
     expect(invoiceitem).to be_an_instance_of(InvoiceItem)
+  end
+
+  it "can return the details of an invoice id" do
+    data = ({:id => 1,
+    :item_id => 263519844,
+    :invoice_id => 1,
+    :quantity => 5,
+    :unit_price  => 13635,
+    :created_at  => Time.now,
+    :updated_at  => Time.now})
+
+    invoiceitem = InvoiceItem.new(data)
   end
 end
