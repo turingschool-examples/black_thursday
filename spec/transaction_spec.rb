e017167326a6bea3df61780df6f1fc5b74a8742e
Nick Jones <nickjones9605@gmail.com>
2022-06-06 20:31:38 -0600

    Test: add transaction test for it exists and it can return the details of a transaction

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,7 +5,29 @@ require "Rspec"
 RSpec.describe Transaction do
 
   it "exists" do
-    transaction = Transaction.new
+    data = ({:id => 1,
+    :invoice_id => 1,
+    :credit_card_number => 0000000000000000,
+    :result => 13635,
+    :created_at  => Time.now,
+    :updated_at  => Time.now})
+    transaction = Transaction.new(data)
     expect(transaction).to be_instance_of(Transaction)
+  end
+
+  it "can return the details of a transaction" do
+    data = ({:id => 1,
+    :invoice_id => 1,
+    :credit_card_number => 0000000000000000,
+    :result => 13635,
+    :created_at  => Time.now.round,
+    :updated_at  => Time.now.round})
+
+    transaction = Transaction.new(data)
+    expect(transaction.id).to eq(1)
+    expect(transaction.invoice_id).to eq(1)
+    expect(transaction.credit_card_number).to eq(0000000000000000)
+    expect(transaction.created_at).to eq(Time.now.round)
+    expect(transaction.updated_at).to eq(Time.now.round)
   end
 end
