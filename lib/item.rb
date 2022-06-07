ccae8522cbe300aa376284f0996085b1ca52f57b
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 13:16:08 -0600

    Update merchant_id to use integer

--- lib/item.rb
+++ lib/item.rb
@@ -3,13 +3,13 @@ class Item
   attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
   attr_writer :name, :description, :unit_price, :updated_at
   def initialize(info)
-    @id = info[:id]
+    @id = info[:id].to_i
     @name = info[:name]
     @description = info[:description]
     @unit_price = BigDecimal(info[:unit_price])/100
     @created_at = info[:created_at]
     @updated_at = info[:updated_at]
-    @merchant_id = info[:merchant_id]
+    @merchant_id = info[:merchant_id].to_i
   end
 
   def unit_price_to_dollars
