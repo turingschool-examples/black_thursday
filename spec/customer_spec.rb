55c98aa5d9c8f540b81d8ff9d2d7f479870c2b67
Justin Ramirez <jusrez528@gmail.com>
2022-06-06 21:04:57 -0400

    Add tests to confirm accessibility to instance variables

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -5,11 +5,75 @@ RSpec.describe Customer do
 
 	it 'exists' do
 		c = Customer.new({
-  :id => 6,
-  :first_name => "Joan",
-  :last_name => "Clarke",
-  :created_at => Time.now,
-  :updated_at => Time.now
-})
+  	:id => 6,
+  	:first_name => "Joan",
+  	:last_name => "Clarke",
+  	:created_at => Time.now,
+  	:updated_at => Time.now
+		})
+
+		expect(c).to be_an_instance_of Customer
+	end
+
+	it 'can return the customer id' do
+		c = Customer.new({
+		:id => 6,
+		:first_name => "Joan",
+		:last_name => "Clarke",
+		:created_at => Time.now,
+		:updated_at => Time.now
+		})
+
+		expect(c.id).to eq(6)
+
+	end
+
+	it 'can return the customers first name' do
+		c = Customer.new({
+		:id => 6,
+		:first_name => "Joan",
+		:last_name => "Clarke",
+		:created_at => Time.now,
+		:updated_at => Time.now
+		})
+
+		expect(c.first_name).to eq("Joan")
+	end
+
+	it 'can return the customers last name' do
+		c = Customer.new({
+		:id => 6,
+		:first_name => "Joan",
+		:last_name => "Clarke",
+		:created_at => Time.now,
+		:updated_at => Time.now
+		})
+
+		expect(c.last_name).to eq("Clarke")
+
+	end
+
+	it 'can return the time the customers profile was created' do
+		c = Customer.new({
+		:id => 6,
+		:first_name => "Joan",
+		:last_name => "Clarke",
+		:created_at => Time.now.round,
+		:updated_at => Time.now.round
+		})
+
+		expect(c.created_at).to eq(Time.now.round)
+	end
+
+	it 'can return the time the customers profile was updated' do
+		c = Customer.new({
+		:id => 6,
+		:first_name => "Joan",
+		:last_name => "Clarke",
+		:created_at => Time.now.round,
+		:updated_at => Time.now.round
+		})
+
+		expect(c.updated_at).to eq(Time.now.round)
 	end
 end
