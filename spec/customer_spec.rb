2b57fe6e92393b98a195764a4e284b8a49ae6b72
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-08 11:10:59 -0700

    Update x and c varaible names

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,27 +2,27 @@ require'./lib/customer'
 
 RSpec.describe Customer do
   before :each do
-    @x = Time.now
-    @c = Customer.new({
+    @time = Time.now
+    @cutomer = Customer.new({
       :id => 6,
       :first_name => "Joan",
       :last_name => "Clarke",
-      :created_at => @x,
-      :updated_at => @x
+      :created_at => @time,
+      :updated_at => @time
     })
       end
 
   it "exists" do
-    expect(@c).to be_a Customer
+    expect(@cutomer).to be_a Customer
   end
 
   it 'has attributes' do
-    expect(@c.id).to eq(6)
-    expect(@c.first_name).to eq("Joan")
-    expect(@c.last_name).to eq("Clarke")
-    expect(@c.created_at).to be_a Time
-    expect(@c.updated_at).to be_a Time
-    expect(@c.created_at).to eq @x
-    expect(@c.updated_at).to eq @x
+    expect(@cutomer.id).to eq(6)
+    expect(@cutomer.first_name).to eq("Joan")
+    expect(@cutomer.last_name).to eq("Clarke")
+    expect(@cutomer.created_at).to be_a Time
+    expect(@cutomer.updated_at).to be_a Time
+    expect(@cutomer.created_at).to eq @time
+    expect(@cutomer.updated_at).to eq @time
   end
 end
