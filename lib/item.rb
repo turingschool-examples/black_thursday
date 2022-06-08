e24287a6f5e3f36dbd82083e0191725689690b94
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 22:42:38 -0600

    Update created and updated to use Time.parse from string

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,6 @@
 require 'BigDecimal'
+require 'time'
+
 class Item
   attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at, :merchant_id
   attr_writer :name, :description, :unit_price, :updated_at
@@ -7,8 +9,8 @@ class Item
     @name = info[:name]
     @description = info[:description]
     @unit_price = BigDecimal(info[:unit_price])/100
-    @created_at = info[:created_at]
-    @updated_at = info[:updated_at]
+    @created_at = Time.parse(info[:created_at].to_s)
+    @updated_at = Time.parse(info[:updated_at].to_s)
     @merchant_id = info[:merchant_id].to_i
   end
 
