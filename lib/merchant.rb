58b97168a46054e3f71f7c778892b873a422cc7e
Brenna Stuart <brennacodes@gmail.com>
2022-06-08 12:28:47 -0600

    Test: merchant passing

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,9 +1,11 @@
 class Merchant
-  attr_reader :id
-  attr_accessor :name
+  attr_reader :id, :created_at
+  attr_accessor :name, :updated_at
 
   def initialize(input)
     @id = input[:id].to_i
     @name = input[:name]
+    @created_at = input[:created_at]
+    @updated_at = input[:updated_at]
   end
 end
--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -3,32 +3,28 @@ SimpleCov.start
 require './lib/helper'
 
 RSpec.describe Merchant do
+  let!(:time) {Time.now}
+  let!(:merchant) {Merchant.new({
+    :id => 5,
+    :name => "Turing School",
+    :created_at => time,
+    :updated_at => time
+    })}
 
   it 'exists' do
-    merchant = Merchant.new({
-      :id => 5,
-      :name => "Turing School"
-      })
-
     expect(merchant).to be_instance_of(Merchant)
   end
 
   it 'returns id' do
-    merchant = Merchant.new({
-      :id => 5,
-      :name => "Turing School"
-      })
-
     expect(merchant.id).to eq(5)
   end
 
   it 'returns name' do
-    merchant = Merchant.new({
-      :id => 5,
-      :name => "Turing School"
-      })
-
     expect(merchant.name).to eq("Turing School")
+  end
+
+  it "returns time created" do
+    expect(merchant.created_at.strftime("%Y-%m-%d %H:%M")).to eq(time.strftime("%Y-%m-%d %H:%M"))
   end
 
 end
