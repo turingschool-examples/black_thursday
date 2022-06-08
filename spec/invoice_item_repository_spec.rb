e8ccfc8a65ab3f96af49e2acd1afb70e72658324
Brenna Stuart <brennacodes@gmail.com>
2022-06-08 14:15:57 -0600

    Test: inv_item_repo passing

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -5,7 +5,7 @@ require './lib/helper'
 RSpec.describe InvoiceItemRepository do
   let!(:sales_engine) {SalesEngine.from_csv({:invoice_items => "./data/invoice_items.csv"})}
   let!(:invoice_items_repo) {sales_engine.invoice_items}
-  let(:new_invoice_item) {invoice_items_repo.make_invoice_item({
+  let(:new_invoice_item) {invoice_items_repo.create_invoice_item({
     :id => 0,
     :item_id => 7,
     :invoice_id => 8,
@@ -40,20 +40,17 @@ RSpec.describe InvoiceItemRepository do
     expect(invoice_items_repo.find_all_by_invoice_id(8).first.id).to eq(38)
   end
 
-  xit "can update an invoice item" do
+  it "can update an invoice item" do
     time = Time.now
     new_invoice_item
     expect(invoice_items_repo.find_by_id(21831)).to be_instance_of InvoiceItem
     expect(invoice_items_repo.find_by_id(21831).item_id).to eq(7)
     expect(invoice_items_repo.find_by_id(21831).quantity).to eq(1)
-    # expect(invoice_items_repo.find_by_id(21831).updated_at.strftime("%Y-%m-%d %H:%M")).to eq(invoice_items_repo.find_by_id(21831).created_at.strftime("%Y-%m-%d %H:%M"))
 
     invoice_items_repo.update(21831, {:quantity => 200, :unit_price => "14.99"})
     expect(invoice_items_repo.find_by_id(21831).item_id).to eq(7)
-    expect(invoice_items_repo.find_by_id(21831).quantity).to eq(2)
+    expect(invoice_items_repo.find_by_id(21831).quantity).to eq(200)
     expect(invoice_items_repo.find_by_id(21831).unit_price).to eq("14.99")
-    # expect(invoice_items_repo.find_by_id(21831).updated_at).to eq(time.strftime("%Y-%m-%d %H:%M"))
-    # expect(invoice_items_repo.find_by_id(1).updated_at).not_to eq(invoice_items_repo.find_by_id(21831).created_at)
   end
 
   it "can delete an invoice" do
