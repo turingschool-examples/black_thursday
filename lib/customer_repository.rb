59f380b05471b1c946b33740f7844cc67eb6ff10
Blake Saylor <blakesaylor@gmail.com>
2022-06-08 13:42:43 -0600

    Update find by name methods to downcase before checking

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -17,13 +17,13 @@ class CustomerRepository
 
   def find_all_by_first_name(first_name_search)
     @all.find_all do |customer|
-      customer.first_name == first_name_search
+      customer.first_name.downcase.include?(first_name_search.downcase)
     end
   end
 
   def find_all_by_last_name(last_name_search)
     @all.find_all do |customer|
-      customer.last_name == last_name_search
+      customer.last_name.downcase.include?(last_name_search.downcase)
     end
   end
 
