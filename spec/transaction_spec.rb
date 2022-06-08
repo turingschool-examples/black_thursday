c84fa9af7a238a2130b2829e2ff657e907d864e8
Shirley DeCesari <shirley.decesari@gmail.com>
2022-06-08 13:00:34 -0700

    Update x and t variable names

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,31 +2,32 @@ require'./lib/transaction'
 
 RSpec.describe Transaction do
   before :each do
-    @x = Time.now
-    @t = Transaction.new({
+    @time = Time.now
+    @transaction = Transaction.new({
         :id => 6,
         :invoice_id => 8,
         :credit_card_number => "4242424242424242",
         :credit_card_expiration_date => "0220",
         :result => "success",
-        :created_at => @x,
-        :updated_at => @x
+        :created_at => @time,
+        :updated_at => @time
         })
       end
 
   it "exists" do
-    expect(@t).to be_a Transaction
+    expect(@transaction).to be_a Transaction
   end
 
   it 'has attributes' do
-    expect(@t.id).to eq(6)
-    expect(@t.invoice_id).to eq(8)
-    expect(@t.credit_card_number).to eq("4242424242424242")
-    expect(@t.credit_card_expiration_date).to eq("0220")
-    expect(@t.result).to eq("success")
-    expect(@t.created_at).to be_a Time
-    expect(@t.updated_at).to be_a Time
-    expect(@t.created_at).to eq @x
-    expect(@t.updated_at).to eq @x
+    expect(@transaction.id).to eq(6)
+    expect(@transaction.invoice_id).to eq(8)
+    expect(@transaction.credit_card_number).to eq("4242424242424242")
+    expect(@transaction.credit_card_expiration_date).to eq("0220")
+    expect(@transaction.result).to eq("success")
+    expect(@transaction.created_at).to be_a Time
+    expect(@transaction.updated_at).to be_a Time
+    expect(@transaction.created_at).to eq @time
+    expect(@transaction.updated_at).to eq @time
   end
-end  
+  
+end
