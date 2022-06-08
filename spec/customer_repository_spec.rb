d309ba89daf5472dedef3f91a7140d01b7b07452
Brenna Stuart <brennacodes@gmail.com>
2022-06-08 15:48:51 -0600

    Test: cust_repo failing

--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -5,7 +5,7 @@ require './lib/helper'
 RSpec.describe CustomerRepository do
   let!(:sales_engine) {SalesEngine.from_csv({:customers => "./data/customers.csv"})}
   let!(:customer_repo) {sales_engine.customers}
-  let(:new_customer) {customer_repo.make_customer({
+  let(:new_customer) {customer_repo.create_customer({
     :id => 6,
     :first_name => "Joan",
     :last_name => "Clarke",
@@ -23,30 +23,30 @@ RSpec.describe CustomerRepository do
 
   it "can find invoices items by their id" do
     new_customer
-    expect(customer_repo.find_by_id(21831)).to be_instance_of Customer
-    expect(customer_repo.find_by_id(21831).item_id).to eq(7)
+    expect(customer_repo.find_by_id(6)).to be_instance_of Customer
+    expect(customer_repo.find_by_id(6).first_name).to eq("Joan")
   end
 
   it "can find all customers by their item id" do
     new_customer
-    expect(customer_repo.find_all_by_item_id(263519844).first).to be_instance_of Customer
-    expect(customer_repo.find_all_by_item_id(263519844).first.id).to eq(1)
+    expect(customer_repo.find_all_by_item_id(6).first).to be_instance_of Customer
+    expect(customer_repo.find_all_by_item_id(6).first.id).to eq(1)
   end
 
   it "can find customers by their invoice id" do
-    expect(customer_repo.find_all_by_invoice_id(8).first).to be_instance_of Customer
-    expect(customer_repo.find_all_by_invoice_id(8).first.id).to eq(38)
+    expect(customer_repo.find_all_by_invoice_id(6).first).to be_instance_of Customer
+    expect(customer_repo.find_all_by_invoice_id(6).first.id).to eq(38)
   end
 
   it "can update an customer" do
     time = Time.now
     new_customer
-    expect(customer_repo.find_by_id(21831)).to be_instance_of Customer
-    expect(customer_repo.find_by_id(21831).item_id).to eq(7)
-    expect(customer_repo.find_by_id(21831).quantity).to eq(1)
-    expect(customer_repo.find_by_id(21831).updated_at).to eq(customer_repo.find_by_id(21831).created_at)
+    expect(customer_repo.find_by_id(6)).to be_instance_of Customer
+    expect(customer_repo.find_by_id(6).first_name).to eq("Joan")
+    expect(customer_repo.find_by_id(6).last_name).to eq("Clarke")
+    expect(customer_repo.find_by_id(6).updated_at).to eq(customer_repo.find_by_id(6).created_at)
 
-    customer_repo.update(21831, {:quantity => 200, :unit_price => "14.99"})
+    customer_repo.update(6, {:first_name => "Mary", :last_name => "Sue"})
     expect(customer_repo.find_by_id(21831).item_id).to eq(7)
     expect(customer_repo.find_by_id(21831).quantity).to eq(2)
     expect(customer_repo.find_by_id(21831).unit_price).to eq("14.99")
