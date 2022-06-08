0b1fb431be92f8d20a31f76fd666e448c19d06e5
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 21:28:20 -0600

    Update to convert created & updated to strings then parse to Time objects

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -16,8 +16,8 @@ class Invoice
     @customer_id = invoice[:customer_id].to_i
     @merchant_id = invoice[:merchant_id].to_i
     @status = invoice[:status].to_sym
-    @created_at = Time.parse(invoice[:created_at])
-    @updated_at = Time.parse(invoice[:updated_at])
+    @created_at = Time.parse(invoice[:created_at].to_s)
+    @updated_at = Time.parse(invoice[:updated_at].to_s)
   end
 
 end
