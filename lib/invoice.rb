aa7d94d16004b45d4818eca776ff2f2f1e46f5e7
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 21:19:52 -0600

    Update created_at and updated_at to be time

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 class Invoice
   attr_reader :id,
               :customer_id,
@@ -13,9 +15,9 @@ class Invoice
     @id = invoice[:id].to_i
     @customer_id = invoice[:customer_id].to_i
     @merchant_id = invoice[:merchant_id].to_i
-    @status = invoice[:status]
-    @created_at = invoice[:created_at]
-    @updated_at = invoice[:updated_at]
+    @status = invoice[:status].to_sym
+    @created_at = Time.parse(invoice[:created_at])
+    @updated_at = Time.parse(invoice[:updated_at])
   end
 
 end
