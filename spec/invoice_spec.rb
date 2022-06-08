1a1fb9dcdcbaddec510b6ffe96ccc6e8c5883b17
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 21:21:30 -0600

    Update to use symbol for status, Time.parse for times

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -30,15 +30,15 @@ RSpec.describe Invoice do
     end
 
     it 'has a status' do
-      expect(@my_invoice.status).to eq 'pending'
+      expect(@my_invoice.status).to eq :pending
     end
 
     it 'has a created_at time' do
-      expect(@my_invoice.created_at).to eq '1994-05-07 23:38:43 UTC'
+      expect(@my_invoice.created_at).to eq Time.parse('1994-05-07 23:38:43 UTC')
     end
 
     it 'has an update_at time' do
-      expect(@my_invoice.updated_at).to eq '2016-01-11 11:30:35 UTC'
+      expect(@my_invoice.updated_at).to eq Time.parse('2016-01-11 11:30:35 UTC')
     end
   end
 end
