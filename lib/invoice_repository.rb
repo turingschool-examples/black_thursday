8bb4fdce46e6bbee40497e223aeb6e9ec554843f
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 21:12:09 -0600

    Update status to be a symbol

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -15,7 +15,7 @@ class InvoiceRepository
         :id => row[:id].to_i,
         :customer_id => row[:customer_id].to_i,
         :merchant_id => row[:merchant_id].to_i,
-        :status => row[:status],
+        :status => row[:status].to_sym,
         :created_at => row[:created_at],
         :updated_at => row[:updated_at]
         })
