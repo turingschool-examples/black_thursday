04151763053be65c50272d0ea22dfcaac3155b50
Alex Pitzel <pitzelalex@gmail.com>
2022-10-31 19:35:21 -0400

    test: Add Merchant to SimpleCov

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -4,11 +4,11 @@ require './lib/merchant'
 describe Merchant do
   describe '#initialize' do
     before(:each) do
-      merchant = Merchant.new({:id => 5, :name => "Turing School"})
+      @merchant = Merchant.new({:id => 5, :name => "Turing School"})
     end
 
     it 'is an instance of Merchant' do
-      expect(merchant).to be_a Merchant
+      expect(@merchant).to be_a Merchant
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,4 @@ require 'simplecov'
 SimpleCov.start
 
 # require_relative "test_goes_here"
+require_relative 'merchant_spec.rb'
