1af4dd71fde98a5e202432a6d4c9c876caa7f99d
Samuel Cox <samc1253@gmail.com>
2022-10-31 16:46:07 -0600

    Add method to search by name and id

--- lib/find.rb
+++ lib/find.rb
@@ -1,9 +1,33 @@
 module Find
   def all
-    if @merchants != []
+    if @merchants != [] && @merchants != nil
       return @merchants
     else
       return @items
+    end
+  end
+
+  def find_by_id(id)
+    if @merchants != [] && @merchants != nil
+      merchants.find do |merchant|
+        merchant.id == id
+      end
+    else
+      items.find do |item|
+        item.id == id
+      end
+    end
+  end
+
+  def find_by_name(name)
+    if @merchants != [] && @merchants != nil
+      merchants.find do |merchant|
+        merchant.name.downcase == name.downcase
+      end
+    else
+      items.find do |item|
+        item.name.downcase == name.downcase
+      end
     end
   end
 end
