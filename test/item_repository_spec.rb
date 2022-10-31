5a6c6cf858903b4683f328a62b65b88b0e86e30e
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-31 16:46:32 -0700

    test: Add test for find_by_name for item repo

--- test/item_repository_spec.rb
+++ test/item_repository_spec.rb
@@ -76,4 +76,29 @@ RSpec.describe ItemRepository do
     end
   end
 
+  describe '#find_by_name' do
+    it 'returns nil or an instance of item with matching name' do
+      item_1 = Item.new({:id => 1,
+                       :name => "Shoes",
+                       :description => "left shoe, right shoe" ,
+                       :unit_price => BigDecimal(78.54,4),
+                       :created_at => Time.now,
+                       :updated_at => Time.now,
+                       :merchant_id => 1})
+      item_2 = Item.new({:id => 2,
+                        :name => "Cool hat",
+                        :description => "black top hat" ,
+                        :unit_price => BigDecimal(22.24,4),
+                        :created_at => Time.now,
+                        :updated_at => Time.now,
+                        :merchant_id => 2})
+
+      items = [item_1, item_2]
+      item_repository = ItemRepository.new(items)
+
+      expect(item_repository.find_by_name("Cool hat")).to eq(item_2)
+      expect(item_repository.find_by_id("Suit")).to eq(nil)
+    end
+  end
+
 end
