f67a4977907a73c8e6ef1ff21d573cc4a125c4e8
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-31 16:53:00 -0700

    Add test for updated_at method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -86,4 +86,18 @@ RSpec.describe do
 
     expect(item.created_at).to be_a(Time)
   end
+
+  it 'creates a Time instance when it is updated' do
+    item = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
+
+    expect(item.updated_at).to be_a(Time)
+  end
 end
