6c90cd006f0f0f81bc51e2a4f3b00c44e0cfbe1c
Ryan Canton <ryancanton999@gmail.com>
2022-10-31 17:28:28 -0600

    Feat: Add find_by_name method for class MerchantRepository

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -11,4 +11,8 @@ class MerchantRepository
   def find_by_id(id)
     @merchants.find {|merchant| merchant.id == id}
   end
+
+  def find_by_name(name)
+    @merchants.find {|merchant| merchant.name.upcase == name.upcase}
+  end
 end
