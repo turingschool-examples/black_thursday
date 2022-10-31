6f6c639c1022af0d894ad5ee59b5940ac87d4180
Ryan Canton <ryancanton999@gmail.com>
2022-10-31 17:08:02 -0600

    Test: Add test for initialize method of class MerchantRepository

--- test/merchant_repository_spec.rb
+++ test/merchant_repository_spec.rb
@@ -4,7 +4,14 @@ require './lib/merchant'
 RSpec.describe MerchantRepository do
   describe '#initialize' do
     it 'exists' do
+      merchant_1 = Merchant.new({:id => 1,
+                                 :name => "Nike"})
+      merchant_2 = Merchant.new({:id => 2,
+                                 :name => "Addidas"})
+      merchants = [merchant_1, merchant_2]
+      merchant_repository = MerchantRepository.new(merchants)
 
+      expect(merchant_repository).to be_a(MerchantRepository)
     end
   end
 end
