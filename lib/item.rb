6b64fef4099836525c523861abc32a2961eb0b15
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-31 16:54:20 -0700

    Add method for updated_at

--- lib/item.rb
+++ lib/item.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'bigdecimal'
 
 class Item
-  attr_reader :id, :name, :description, :unit_price, :created_at
+  attr_reader :id, :name, :description, :unit_price, :created_at, :updated_at
 
   def initialize(item_info)
     @id = item_info[:id]
@@ -10,6 +10,7 @@ class Item
     @description = item_info[:description]
     @unit_price = item_info[:unit_price]
     @created_at = item_info[:created_at]
+    @updated_at = item_info[:updated_at]
 
   end
 end
