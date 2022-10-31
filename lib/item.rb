2886b013e7a7fd9c61195a069175a1e083f3ead7
Brandon Gray <brandonjoe88@hotmail.com>
2022-10-31 16:18:07 -0600

    feat: create item class

---
+++ lib/item.rb
@@ -0,0 +1,11 @@
+class Item
+  def initialize(params)
+    :id          => 1,
+    :name        => "Pencil",
+    :description => "You can use it to write things",
+    :unit_price  => BigDecimal(10.99,4),
+    :created_at  => Time.now,
+    :updated_at  => Time.now,
+    :merchant_id => 2
+  end
+end
