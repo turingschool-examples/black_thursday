78008c9dd12b59d107f3be9d3ba657e1bcd0ab72
Alex Pitzel <pitzelalex@gmail.com>
2022-10-31 19:45:39 -0400

    test: Add Merchant differant name and id tests

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -10,5 +10,20 @@ describe Merchant do
     it 'is an instance of Merchant' do
       expect(@merchant).to be_a Merchant
     end
+
+    it 'stores the merchants name' do
+      expect(@merchant.name).to eq("Turing School")
+    end
+    
+    it 'stores the merchants id' do
+      expect(@merchant.id).to eq(5)
+    end
+
+    it 'stores a different name and id' do
+      merchant = Merchant.new({:id => 10, :name => "Frankenstein and Sons"})
+
+      expect(merchant.name).to eq("Frankenstein and Sons")
+      expect(merchant.id).to eq(10)
+    end
   end
 end
