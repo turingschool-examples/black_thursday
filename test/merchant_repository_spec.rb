9bfcac4c53a2f31fb900dfea7a34f0a083e90397
Ryan Canton <ryancanton999@gmail.com>
2022-10-31 17:18:51 -0600

    Test: Add test for find_by_id method for class MerchantRepository

--- test/merchant_repository_spec.rb
+++ test/merchant_repository_spec.rb
@@ -27,4 +27,17 @@ RSpec.describe MerchantRepository do
       expect(merchant_repository.all).to eq([merchant_1, merchant_2])
     end
   end
+
+  describe '#find_by_id' do
+    it 'finds and returns a merchant object by id' do
+      merchant_1 = Merchant.new({:id => 1,
+                                 :name => "Nike"})
+      merchant_2 = Merchant.new({:id => 2,
+                                 :name => "Addidas"})
+      merchants = [merchant_1, merchant_2]
+      merchant_repository = MerchantRepository.new(merchants)
+
+      expect(merchant_repository.find_by_id(1)).to eq(merchant_1)
+    end
+  end
 end
