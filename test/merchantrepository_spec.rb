dd5840de2fc5393d2fe7b2ddb09920c920b94087
J Seymour <JustJakeSeymour@gmail.com>
2022-10-31 13:54:23 -0600

    framework of tests for merchant repository class

--- test/merchantrepository_spec.rb
+++ test/merchantrepository_spec.rb
@@ -5,8 +5,47 @@ RSpec.describe MerchantRepository do
 
   it 'is an merchant repository class' do
     
-    
+  end
+
+  it 'returns all merchant instances' do
+
   end
   
+  it 'can find merchant by an id' do
+
+  end
   
+  it 'returns nil if id is not within' do
+
+  end
+
+  it 'can find merchant by name *CASE INSENSITIVE*' do
+
+  end
+
+  it 'returns nil if name is not within' do
+
+  end
+
+  it 'can find ALL by supplied name fragment' do
+
+  end
+
+  it 'can create a new merchant object with attributes' do
+
+  end
+
+  it 'gives the new merchant object the highest id + 1' do
+
+  end
+
+  it 'can update a merchant (by id) with new attributes *aka name updates*' do
+
+  end
+
+  it 'can delete a merchant by supplied id' do
+
+  end
+  
+
 end
