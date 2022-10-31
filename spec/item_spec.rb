3a348f0d46d5a25f7773d7ab2ebb8b2a214d23fc
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-31 16:32:30 -0700

    Add the unit_price method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -58,4 +58,18 @@ RSpec.describe do
 
     expect(item.description).to eq("You can use it to write things")
   end
+
+  it 'has a unit price' do
+    item = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
+
+    expect(item.unit_price).to eq(BigDecimal(10.99,4))
+  end
 end
