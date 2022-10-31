8649cb93dd339986581290b2041f4ab7158ed73f
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-31 15:49:27 -0600

    feat: complete initialize method for class Item

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,12 @@
 class Item
+
+  def initialize(hash)
+    @id = hash[:id]
+    @name = hash[:name]
+    @description = hash[:item_description]
+    @unit_price = hash[:unit_price]
+    @created_at = hash[:created_at]
+    @updated_at = hash[:updated_at]
+    @merchant_id = hash[:merchant_id]    
+  end 
 end 
--- test/item_spec.rb
+++ test/item_spec.rb
@@ -4,13 +4,13 @@ require 'bigdecimal/util'
 RSpec.describe Item do 
   describe "#intialize" do
     it 'exists' do 
-      item = Item.new({"id" => 1,
-                       "name" => "item_name", 
-                       "description" => "item_description" ,
-                       "unit_price" => BigDecimal("5.99"), 
-                       "created_at" => Time.now, 
-                       "updated_at" => Time.now,
-                       "merchant_id" => 1})
+      item = Item.new({:id => 1,
+                       :name => "item_name", 
+                       :description => "item_description" ,
+                       :unit_price => BigDecimal("5.99"), 
+                       :created_at => Time.now, 
+                       :updated_at => Time.now,
+                       :merchant_id => 1})
 
     expect(item).to be_a(Item)
 
