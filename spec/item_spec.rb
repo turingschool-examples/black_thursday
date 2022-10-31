a36de3207134be2b9a83377539424ce4b0ba6156
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-31 16:56:22 -0700

    Add test for merchant_id method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -100,4 +100,18 @@ RSpec.describe do
 
     expect(item.updated_at).to be_a(Time)
   end
+
+  it 'will list its merchant id number' do
+    item = Item.new({
+      :id          => 1,
+      :name        => "Pencil",
+      :description => "You can use it to write things",
+      :unit_price  => BigDecimal(10.99,4),
+      :created_at  => Time.now,
+      :updated_at  => Time.now,
+      :merchant_id => 2
+    })
+
+    expect(item.merchant_id.to eq(2)
+  end
 end
