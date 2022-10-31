1fba99e85087f9d3a14a36ca5a4006101ee75cc5
J Seymour <JustJakeSeymour@gmail.com>
2022-10-31 13:41:51 -0600

    describe tests for merchant class

--- test/merchant_spec.rb
+++ test/merchant_spec.rb
@@ -1,11 +1,18 @@
 require './lib/merchant'
 
 RSpec.describe Merchant do
+  let!(m) {Merchant.new({:id =>5, :name => "Turing School"})}
 
   it 'is a merchant class' do
-    
-    
+    expect(m).to be_a Merchant    
+  end
+
+  it 'accepts the integer id of merchant' do
+    expect(m.id).to eq 5
   end
   
+  it 'accepts the name of merchant' do
+    expect(m.name).to eq "Turing School"
+  end
   
 end
