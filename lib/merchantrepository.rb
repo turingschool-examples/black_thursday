c562bb1aa42a923ca20464a221d6c244a5e2f6f0
Alastair Lloyd <dex8056@aol.com>
2022-10-31 15:47:51 -0600

    feat: Add find all by name method

--- lib/merchantrepository.rb
+++ lib/merchantrepository.rb
@@ -1,4 +1,5 @@
 class Merchant_Repository
+  attr_reader   :merchants
 
   def initialize
     @merchant_repository
@@ -6,18 +7,26 @@ class Merchant_Repository
   end
 
   def all
-    @merchants
+    merchants
   end
 
   def find_by_id(merchant_id)
-    @merchants.find do |merchant|
+    merchants.find do |merchant|
       merchant.id == merchant_id
     end
   end
 
   def find_by_name(merchant_name)
-    @merchants.find do |merchant|
+    merchants.find do |merchant|
       merchant.name.upcase == merchant_name.upcase
     end
   end
+
+  def find_all_by_name(merchant_name)
+    merchants.select do |merchant|
+      merchant.name.upcase.include?(merchant_name.upcase)
+    end
+  end
+
+  
 end
