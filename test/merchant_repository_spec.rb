483c9772d4a267de0f7f140931ac39ae378db528
Ryan Canton <ryancanton999@gmail.com>
2022-10-31 17:11:29 -0600

    Test: Add test for all method of class MerchantRepository

--- test/merchant_repository_spec.rb
+++ test/merchant_repository_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe MerchantRepository do
       expect(merchant_repository).to be_a(MerchantRepository)
     end
   end
+
+  describe '#all' do
+    it 'returns an array of all known merchants' do
+      merchant_1 = Merchant.new({:id => 1,
+                                 :name => "Nike"})
+      merchant_2 = Merchant.new({:id => 2,
+                                 :name => "Addidas"})
+      merchants = [merchant_1, merchant_2]
+      merchant_repository = MerchantRepository.new(merchants)
+
+      expect(merchant_repository.all).to eq([merchant_1, merchant_2])
+    end
+  end
 end
