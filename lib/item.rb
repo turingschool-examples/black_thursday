f9714630c318b0998a2fb18e0e48d4dbfd7258a3
Anhnhi Tran <anhtran811@gmail.com>
2022-10-31 17:28:15 -0600

    Initialized item and attributes

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,21 @@
-class Item
+require 'bigdecimal'
 
-  def initialize()
-    
+class Item
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :created_at,
+              :updated_at,
+              :merchant_id
+
+  def initialize(item)
+    @id = item[:id]
+    @name = item[:name]
+    @description = item[:description]
+    @unit_price = item[:unit_price]
+    @created_at = item[:created_at]
+    @updated_at = item[:updated_at]
+    @merchant_id = item[:merchant_id]
   end
-  
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,26 +1,34 @@
 require './lib/item'
 
 RSpec.describe Item do
-
-  let!(i) {Item.new({
+  time_now = Time.now
+  let!(:i) {Item.new(
+    {
   :id          => 1,
   :name        => "Pencil",
   :description => "You can use it to write things",
   :unit_price  => BigDecimal(10.99,4),
-  :created_at  => Time.now,
-  :updated_at  => Time.now,
+  :created_at  => time_now,
+  :updated_at  => time_now,
   :merchant_id => 2
-  })}
+  }
+  )}
 
   it 'is an item class' do
-    
+    expect(i).to be_a(Item)
   end
-  
+
   it 'is created with a hash storing the id, name, description, unit_price, created at, updated_at, merchant_id symbols' do
-
+    expect(i.id).to eq(1)
+    expect(i.name).to eq("Pencil")
+    expect(i.description).to eq("You can use it to write things")
+    expect(i.unit_price).to eq(10.99)
+    expect(i.created_at).to eq(time_now)
+    expect(i.updated_at).to eq(time_now)
+    expect(i.merchant_id).to eq(2)
   end
 
-  it 'can convert unit price to a dollar formatted float' do
+  xit 'can convert unit price to a dollar formatted float' do
     # expect(i.unit_price_to_dollars).to eq 23.48
   end
   
