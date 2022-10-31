738c8e9461b022a882a2bcdc544f60bc3aecaea6
Brandon Gray <brandonjoe88@hotmail.com>
2022-10-31 16:23:56 -0600

    test: item creation

--- lib/item.rb
+++ lib/item.rb
@@ -1,11 +1,5 @@
 class Item
-  def initialize(params)
-    :id          => 1,
-    :name        => "Pencil",
-    :description => "You can use it to write things",
-    :unit_price  => BigDecimal(10.99,4),
-    :created_at  => Time.now,
-    :updated_at  => Time.now,
-    :merchant_id => 2
+  def initialize(stats)
+    @stats = stats
   end
 end
