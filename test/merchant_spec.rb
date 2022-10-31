81d726391e7958c1c443024034bb511d9bda7f10
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-31 15:42:07 -0700

    Fix indendation

--- test/merchant_spec.rb
+++ test/merchant_spec.rb
@@ -1,21 +1,21 @@
 require './lib/merchant.rb'
 
 RSpec. describe Merchant do
-    it 'exists' do
-        merchant = Merchant.new({:id => 5, :name => "Turing School"})
+  it 'exists' do
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
 
-        expect(merchant).to eq(merchant)
-    end
+    expect(merchant).to eq(merchant)
+  end
 
-    it 'has an id' do
-        merchant = Merchant.new({:id => 5, :name => "Turing School"})
+  it 'has an id' do
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
 
-        expect(merchant.id).to eq(5)
-    end
+    expect(merchant.id).to eq(5)
+  end
 
-    it 'has a name' do
-        merchant = Merchant.new({:id => 5, :name => "Turing School"})
+  it 'has a name' do
+    merchant = Merchant.new({:id => 5, :name => "Turing School"})
 
-        expect(merchant.name).to eq("Turing School")
-    end
+    expect(merchant.name).to eq("Turing School")
+  end
 end
