62da357ef3002e76e963d31e969282d4b4b67cc8
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-31 16:59:06 -0700

    test: Add test for finding item repo with description

--- test/item_repository_spec.rb
+++ test/item_repository_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe ItemRepository do
     it 'exists' do
       item_1 = Item.new({:id => 1,
                        :name => "Shoes",
-                       :description => "left shoe, right shoe" ,
+                       :description => "left shoe, right shoe",
                        :unit_price => BigDecimal(78.54,4),
                        :created_at => Time.now,
                        :updated_at => Time.now,
                        :merchant_id => 1})
       item_2 = Item.new({:id => 2,
                         :name => "Cool hat",
-                        :description => "black top hat" ,
+                        :description => "black top hat",
                         :unit_price => BigDecimal(22.24,4),
                         :created_at => Time.now,
                         :updated_at => Time.now,
@@ -31,14 +31,14 @@ RSpec.describe ItemRepository do
     it 'returns an array of all known items' do
       item_1 = Item.new({:id => 1,
                        :name => "Shoes",
-                       :description => "left shoe, right shoe" ,
+                       :description => "left shoe, right shoe",
                        :unit_price => BigDecimal(78.54,4),
                        :created_at => Time.now,
                        :updated_at => Time.now,
                        :merchant_id => 1})
       item_2 = Item.new({:id => 2,
                         :name => "Cool hat",
-                        :description => "black top hat" ,
+                        :description => "black top hat",
                         :unit_price => BigDecimal(22.24,4),
                         :created_at => Time.now,
                         :updated_at => Time.now,
@@ -55,14 +55,14 @@ RSpec.describe ItemRepository do
     it 'returns nil or an instance of item with matching id' do
       item_1 = Item.new({:id => 1,
                        :name => "Shoes",
-                       :description => "left shoe, right shoe" ,
+                       :description => "left shoe, right shoe",
                        :unit_price => BigDecimal(78.54,4),
                        :created_at => Time.now,
                        :updated_at => Time.now,
                        :merchant_id => 1})
       item_2 = Item.new({:id => 2,
                         :name => "Cool hat",
-                        :description => "black top hat" ,
+                        :description => "black top hat",
                         :unit_price => BigDecimal(22.24,4),
                         :created_at => Time.now,
                         :updated_at => Time.now,
@@ -80,14 +80,14 @@ RSpec.describe ItemRepository do
     it 'returns nil or an instance of item with matching name' do
       item_1 = Item.new({:id => 1,
                        :name => "Shoes",
-                       :description => "left shoe, right shoe" ,
+                       :description => "left shoe, right shoe",
                        :unit_price => BigDecimal(78.54,4),
                        :created_at => Time.now,
                        :updated_at => Time.now,
                        :merchant_id => 1})
       item_2 = Item.new({:id => 2,
                         :name => "Cool hat",
-                        :description => "black top hat" ,
+                        :description => "black top hat",
                         :unit_price => BigDecimal(22.24,4),
                         :created_at => Time.now,
                         :updated_at => Time.now,
@@ -98,6 +98,32 @@ RSpec.describe ItemRepository do
 
       expect(item_repository.find_by_name("Cool hat")).to eq(item_2)
       expect(item_repository.find_by_id("Suit")).to eq(nil)
+    end
+  end
+
+  describe '#find_all_with_description' do
+    it 'returns ' do
+      item_1 = Item.new({:id => 1,
+                       :name => "Shoes",
+                       :description => "left shoe, right shoe",
+                       :unit_price => BigDecimal(78.54,4),
+                       :created_at => Time.now,
+                       :updated_at => Time.now,
+                       :merchant_id => 1})
+      item_2 = Item.new({:id => 2,
+                        :name => "Cool hat",
+                        :description => "black top hat",
+                        :unit_price => BigDecimal(22.24,4),
+                        :created_at => Time.now,
+                        :updated_at => Time.now,
+                        :merchant_id => 2})
+
+      items = [item_1, item_2]
+      item_repository = ItemRepository.new(items)
+
+      expect(item_repository.find_all_with_description("black top hat")).to eq(item_2)
+      expect(item_repository.find_all_with_description("BLack top Hat")).to eq(item_2)
+      expect(item_repository.find_by_id("a really nice suit")).to eq([])
     end
   end
 
