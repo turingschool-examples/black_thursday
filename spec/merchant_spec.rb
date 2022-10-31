8089fd542ea092e697629c90226e874322b112d3
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-31 15:46:25 -0600

    feat: Add merchant class and tests

---
+++ spec/merchant_spec.rb
@@ -0,0 +1,16 @@
+require './lib/merchant'
+
+RSpec.describe Merchant do
+  it 'exists' do
+    m = Merchant.new({id: 5, name: "Turing School"})
+
+    expect(m).to be_a (Merchant)
+  end
+
+  it 'has attributes' do
+    m = Merchant.new({id: 5, name: "Turing School"})
+
+    expect(m.id).to eq (5)
+    expect(m.name).to eq ("Turing School")
+  end
+end
