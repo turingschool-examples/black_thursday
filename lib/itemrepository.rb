ee9ac2ff420df345f63370c35ce5ee0441c785e0
Drew Layton <dlayton66@gmail.com>
2022-10-31 22:30:19 -0600

    feat: Create #create method for Reposable

--- lib/itemrepository.rb
+++ lib/itemrepository.rb
@@ -1,4 +1,10 @@
+require_relative 'reposable'
+
 class ItemRepository
+  include Reposable
+
+  attr_reader :all
+
   def initialize(all = [])
     @all = all
   end
--- lib/reposable.rb
+++ lib/reposable.rb
@@ -2,4 +2,8 @@ module Reposable
   def class_name
     Object.const_get(self.class.name.chomp('Repository'))
   end
+
+  def create(attributes)
+    all << class_name.new(attributes)
+  end
 end
