db4a81a1a5f70cec45914ae9871e4cd8afdbbac4
Braxton Eddings <beddings81@gmail.com>
2022-11-01 12:51:06 -0500

    Got all the initialize test to pass

---
+++ lib/item.rb
@@ -0,0 +1,19 @@
+class Item
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :created_at,
+              :updated_at,
+              :merchant_id
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @description = attributes[:description]
+    @unit_price = attributes[:unit_price]
+    @created_at = attributes[:created_at]
+    @updated_at = attributes[:updated_at]
+    @merchant_id = attributes[:merchant_id]
+  end
+end
