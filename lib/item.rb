f1143f2b22b2dce54f0f09b929c2ea8d5cdeeb53
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-31 17:38:04 -0700

    test: Add test for item class and remove lib/.gitignore

---
+++ lib/item.rb
@@ -0,0 +1,23 @@
+# This is an item held within the ItemRepository class
+# All data is passed in through a single Hash
+
+def initialize()
+
+end
+# Each item has the following methods: 
+# 'id' 
+
+# 'name'
+
+# 'description'
+
+# 'unit_price' - formatted as BigDecimal
+
+# 'created_at' - relies on Time instance module
+
+# 'updated_at' - same as above
+
+# 'merchant_id'
+
+# 'unit_price_to_dollars - returns the price of the item in dollars as Float
+
---
+++ lib/item_repository.rb
@@ -0,0 +1,21 @@
+# The repository of item instances
+
+# all - an array of all known items
+
+# find_by_id(id) - returns either nil or an instance of Item
+
+# find_by_name(name) 
+
+# find_all_with_description(description)
+
+# find_all_by_price(price)
+
+# find_all_by_price_in_range(range)
+
+# find_all_by_merchant_id(merchant_id)
+
+# create(attributes)
+
+# update(id, attributes)
+
+# delete(id)
