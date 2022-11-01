10a1c57730d638ff4ddf8166a51cc676cc43602c
Drew Layton <dlayton66@gmail.com>
2022-10-31 23:18:17 -0600

    test: Create #find_by_id test for Reposable

--- spec/reposable_spec.rb
+++ spec/reposable_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe Reposable do
         :updated_at  => Time.now,
         :merchant_id => 2
       })
-      item_repo.update({
+      item_repo.update(1, {
         :name        => "Broken Pencil",
         :description => "You now have a smaller pencil",
         :unit_price  => BigDecimal(5.99,4)
@@ -50,6 +50,23 @@ RSpec.describe Reposable do
       expect(item_repo.all[0].name).to eq "Broken Pencil"
       expect(item_repo.all[0].description).to eq "You now have a smaller pencil"
       expect(item_repo.all[0].unit_price).to eq BigDecimal(5.99,4)
+    end
+  end
+
+  describe '#find_by_id' do
+    it 'returns either nil or an instance with matching id' do
+      item = Item.new({
+        :id          => 1,
+        :name        => "Pencil",
+        :description => "You can use it to write things",
+        :unit_price  => BigDecimal(10.99,4),
+        :created_at  => Time.now,
+        :updated_at  => Time.now,
+        :merchant_id => 2
+      })  
+      item_repo = ItemRepository.new([item])
+
+      expect(item_repo.find_by_id(1)).to eq item
     end
   end
 end
