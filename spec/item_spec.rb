09993038436e3ad7b9d71f4d02e1fda205deafe2
Anhnhi Tran <anhtran811@gmail.com>
2022-10-31 19:13:50 -0600

    Changed variable names to item instead of i, add let statement for time now

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,34 +1,34 @@
 require './lib/item'
 
 RSpec.describe Item do
-  time_now = Time.now
-  let!(:i) {Item.new(
+  let!(:time_now) {Time.now}
+  let!(:item) {Item.new(
     {
   :id          => 1,
   :name        => "Pencil",
   :description => "You can use it to write things",
   :unit_price  => BigDecimal(10.99,4),
-  :created_at  => time_now,
-  :updated_at  => time_now,
+  :created_at => time_now,
+  :updated_at => time_now,
   :merchant_id => 2
   }
   )}
 
   it 'is an item class' do
-    expect(i).to be_a(Item)
+    expect(item).to be_a(Item)
   end
 
   it 'is created with a hash storing the id, name, description, unit_price, created at, updated_at, merchant_id symbols' do
-    expect(i.id).to eq(1)
-    expect(i.name).to eq("Pencil")
-    expect(i.description).to eq("You can use it to write things")
-    expect(i.unit_price).to eq(BigDecimal(10.99,4))
-    expect(i.created_at).to eq(time_now)
-    expect(i.updated_at).to eq(time_now)
-    expect(i.merchant_id).to eq(2)
+    expect(item.id).to eq(1)
+    expect(item.name).to eq("Pencil")
+    expect(item.description).to eq("You can use it to write things")
+    expect(item.unit_price).to eq(BigDecimal(10.99,4))
+    expect(item.created_at).to eq(time_now)
+    expect(item.updated_at).to eq(time_now)
+    expect(item.merchant_id).to eq(2)
   end
 
   it 'can convert unit price to a dollar formatted float' do
-    expect(i.unit_price_to_dollars).to eq(10.99)
+    expect(item.unit_price_to_dollars).to eq(10.99)
   end
 end
