590321641b242b06c343f09c504f9bee2fc78617
Joe King <j.king92@gmail.com>
2022-10-31 17:07:06 -0700

    test: that the merchant can return its name

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -2,9 +2,16 @@ require 'rspec'
 require './lib/merchant'
 
 describe Merchant do
+  let!(:merchant) {Merchant.new({:id => 5, :name => "Turing School"})}
   it 'exists' do
-    merchant = Merchant.new({:id => 5, :name => "Turing School"})
+    # merchant = Merchant.new({:id => 5, :name => "Turing School"})
 
     expect(merchant).to be_a Merchant
+  end
+
+  it 'can return the name' do
+    # merchant = Merchant.new({:id => 5, :name => "Turing School"})
+
+    expect(merchant.name).to eq("Turing School")
   end
 end
