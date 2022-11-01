83d9b186c8bb0aa76758b4e6379ce3b325e7fdef
Drew Layton <dlayton66@gmail.com>
2022-11-01 00:02:25 -0600

    refactor: Change test description

--- spec/itemrepository_spec.rb
+++ spec/itemrepository_spec.rb
@@ -4,7 +4,7 @@ require './lib/item'
 RSpec.describe ItemRepository do
   let(:item_repository) {ItemRepository.new}
 
-  it 'is an instance of ItemRepository' do
+  it 'exists' do
     expect(item_repository).to be_a ItemRepository
   end
 end
