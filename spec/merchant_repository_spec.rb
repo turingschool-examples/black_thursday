48744da1b5e8cbab9016f66db18b1a3c24a411b6
Alex Pitzel <pitzelalex@gmail.com>
2022-10-31 21:26:06 -0400

    test: Create MerchantRepository #create test

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -1,9 +1,12 @@
 require 'rspec'
+require 'csv'
 require './lib/merchant_repository'
 
 describe MerchantRepository do
+  let(:mr) { MerchantRepository.new }
+  let(:data) { CSV.open './data/merchants_test.csv', headers: true, header_converters: :symbol }
+
   describe '#initialize' do
-    let(:mr) { MerchantRepository.new }
 
     it 'is and instance of MerchantRepository' do
       expect(mr).to be_a MerchantRepository
@@ -15,9 +18,15 @@ describe MerchantRepository do
   end
 
   describe '#create' do
-    xit 'creates a Merchant and adds it to @merchants' do
+    it 'creates a Merchant and adds it to @merchants' do
+      data.each { |line| mr.create(line) }
 
-      expect(mr.merchants).to eq([])
+      m1 = Merchant.new({:id => 12334105, :name => Shopin1901})
+      m2 = Merchant.new({:id => 12334112, :name => Candisart})
+      m3 = Merchant.new({:id => 12334113, :name => MiniatureBikez})
+      m4 = Merchant.new({:id => 12334115, :name => LolaMarleys})
+
+      expect(mr.merchants).to eq([m1, m2, m3, m4])
     end
   end
 end
