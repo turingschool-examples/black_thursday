c7dff22430d579d15e2c3b17dc894a45356ccc43
Braxton Eddings <beddings81@gmail.com>
2022-11-01 12:54:41 -0500

    Clean up item spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,11 +4,15 @@ require 'bigdecimal'
 
 describe Item do
   describe '#initialize' do
-   it 'exist' do
+    it 'exist' do
       i = Item.new({
         :id          => 1,
         :name        => "Pencil",
         :description => "You can use it to write things",
+        :unit_price  => BigDecimal(10.99,4),
+        :created_at  => Time.now,
+        :updated_at  => Time.now,
+        :merchant_id => 2
       })
 
       expect(i).to be_a(Item)
