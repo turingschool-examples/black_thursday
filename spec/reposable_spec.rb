fa562bbf2b9c82f9b1137e31ae104852358d01b9
Drew Layton <dlayton66@gmail.com>
2022-10-31 22:41:45 -0600

    test: Create #update test for Reposable

--- spec/reposable_spec.rb
+++ spec/reposable_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Reposable do
   end
 
   describe '#create' do
-    it 'adds a new instance of the class corresponding to the current repo' do
+    it 'adds a new instance corresponding to the current repo' do
       item_repo = ItemRepository.new
       item_repo.create({
         :id          => 1,
@@ -26,6 +26,30 @@ RSpec.describe Reposable do
       })
 
       expect(item_repo.all[0]).to be_a Item
+    end
+  end
+
+  describe '#update' do
+    it 'updates an instance with the corresponding id' do
+      item_repo = ItemRepository.new
+      item_repo.create({
+        :id          => 1,
+        :name        => "Pencil",
+        :description => "You can use it to write things",
+        :unit_price  => BigDecimal(10.99,4),
+        :created_at  => Time.now,
+        :updated_at  => Time.now,
+        :merchant_id => 2
+      })
+      item_repo.update({
+        :name        => "Broken Pencil",
+        :description => "You now have a smaller pencil",
+        :unit_price  => BigDecimal(5.99,4)
+      })
+
+      expect(item_repo.all[0].name).to eq "Broken Pencil"
+      expect(item_repo.all[0].description).to eq "You now have a smaller pencil"
+      expect(item_repo.all[0].unit_price).to eq BigDecimal(5.99,4)
     end
   end
 end
