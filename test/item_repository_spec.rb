c2b52aa90412378085e8ec773873fce09705b6e1
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-31 17:49:58 -0700

    test: Add test for find all by merchant id to item repo

--- test/item_repository_spec.rb
+++ test/item_repository_spec.rb
@@ -123,7 +123,7 @@ RSpec.describe ItemRepository do
                         :unit_price => BigDecimal(22.24,4),
                         :created_at => Time.now,
                         :updated_at => Time.now,
-                        :merchant_id => 2})
+                        :merchant_id => 3})
 
       items = [item_1, item_2, item_3]
       item_repository = ItemRepository.new(items)
@@ -156,7 +156,7 @@ RSpec.describe ItemRepository do
                         :unit_price => BigDecimal(22.24,4),
                         :created_at => Time.now,
                         :updated_at => Time.now,
-                        :merchant_id => 2})
+                        :merchant_id => 3})
 
       items = [item_1, item_2, item_3]
       item_repository = ItemRepository.new(items)
@@ -188,13 +188,45 @@ RSpec.describe ItemRepository do
                         :unit_price => BigDecimal(32.44,4),
                         :created_at => Time.now,
                         :updated_at => Time.now,
-                        :merchant_id => 2})
+                        :merchant_id => 3})
 
       items = [item_1, item_2, item_3]
       item_repository = ItemRepository.new(items)
 
       expect(item_repository.find_all_by_price_in_range(15..35)).to eq([item_2, item_3])
       expect(item_repository.find_all_by_price_in_range(5..15)).to eq([])
+    end
+  end
+
+  describe '#find_all_by_merchant_id' do
+    it 'returns empty array or instances of item with matching merchant id' do
+      item_1 = Item.new({:id => 1,
+                       :name => "Shoes",
+                       :description => "left shoe, right shoe",
+                       :unit_price => BigDecimal(78.54,4),
+                       :created_at => Time.now,
+                       :updated_at => Time.now,
+                       :merchant_id => 1})
+      item_2 = Item.new({:id => 2,
+                        :name => "Cool hat",
+                        :description => "black top hat",
+                        :unit_price => BigDecimal(22.24,4),
+                        :created_at => Time.now,
+                        :updated_at => Time.now,
+                        :merchant_id => 2})
+      item_3 = Item.new({:id => 3,
+                        :name => "More Expensive Cool hat",
+                        :description => "black top hat",
+                        :unit_price => BigDecimal(32.44,4),
+                        :created_at => Time.now,
+                        :updated_at => Time.now,
+                        :merchant_id => 2})
+
+      items = [item_1, item_2, item_3]
+      item_repository = ItemRepository.new(items)
+
+      expect(item_repository.find_all_by_merchant_id(2)).to eq([item_2, item_3])
+      expect(item_repository.find_all_by_merchant_id(5)).to eq([])
     end
   end
 
