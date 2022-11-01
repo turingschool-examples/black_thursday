a35bcc9a446599472591857a40707f263bde67df
Thomas Casady <tscasady@gmail.com>
2022-10-31 22:21:00 -0500

    feat: Add InvoiceRepo Class.

--- lib/invoice_repo.rb
+++ lib/invoice_repo.rb
@@ -1,5 +1,48 @@
 class InvoiceRepo
   
+  attr_reader :invoices
+
+  def initialize(data = {})
+    @invoices = []
+    data.each {|invoice| create(invoice)}
+  end
+
+  def create(invoice)
+    invoice[:id] ||= (@invoices.last.id.to_i + 1).to_s
+    invoice = Invoice.new(invoice)
+    @invoices << invoice
+  end
+
+  def all
+    @invoices
+  end
+
+  def find_by_id(id)
+    match = nil
+    @invoices.each{|invoice| match = invoice if invoice.id == id.to_s}
+    match
+  end
+
+  def find_by_customer_id(id)
+    @invoices.select{|invoice| invoice.customer_id == id.to_s}
+  end
+
+  def find_by_merchant_id(id)
+    @invoices.select{|invoice| invoice.merchant_id == id.to_s}
+  end
+
+  def find_all_by_status(status)
+    @invoices.select{|invoice| invoice.status == status}
+  end
+
+  def update(id, status)
+    find_by_id(id).set_status(status)
+  end
+
+  def delete(id)
+    @invoices.delete_if{|invoice| invoice.id == id.to_s}
+  end
+
 end
 
 
