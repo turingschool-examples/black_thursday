197e708e3b0df29fc66b7d3e9c6245f1a7eb5c6c
Alex Pitzel <pitzelalex@gmail.com>
2022-11-01 15:01:11 -0400

    refactor: Fix more syntax issues

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -19,15 +19,15 @@ describe MerchantRepository do
   describe '#create' do
     it 'creates a Merchant and adds it to @merchants' do
       data.each { |line| mr.create(line) }
-      
-      expect(mr.merchants[0].id).to eq("12334105")
-      expect(mr.merchants[1].id).to eq("12334112")
-      expect(mr.merchants[2].id).to eq("12334113")
-      expect(mr.merchants[3].id).to eq("12334115")
-      expect(mr.merchants[0].name).to eq("Shopin1901")
-      expect(mr.merchants[1].name).to eq("Candisart")
-      expect(mr.merchants[2].name).to eq("MiniatureBikez")
-      expect(mr.merchants[3].name).to eq("LolaMarleys")
+
+      expect(mr.merchants[0].id).to eq('12334105')
+      expect(mr.merchants[1].id).to eq('12334112')
+      expect(mr.merchants[2].id).to eq('12334113')
+      expect(mr.merchants[3].id).to eq('12334115')
+      expect(mr.merchants[0].name).to eq('Shopin1901')
+      expect(mr.merchants[1].name).to eq('Candisart')
+      expect(mr.merchants[2].name).to eq('MiniatureBikez')
+      expect(mr.merchants[3].name).to eq('LolaMarleys')
     end
   end
 
@@ -52,7 +52,7 @@ describe MerchantRepository do
   describe '#find_by_name' do
     it 'returns nil or a Merchant instance that matches by name regardless of case' do
       data.each { |line| mr.create(line) }
-      
+
       expect(mr.find_by_name('shopin1901')).to eq(mr.merchants[0])
       expect(mr.find_by_name('nadda')).to be nil
     end
