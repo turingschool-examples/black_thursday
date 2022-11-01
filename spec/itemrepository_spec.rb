26850f5e6d7f44aaf284e3a52eaa9b09028493d7
Drew Layton <dlayton66@gmail.com>
2022-11-01 06:57:21 -0600

    test: Create #find_by_name test for ItemRepository

--- spec/itemrepository_spec.rb
+++ spec/itemrepository_spec.rb
@@ -2,9 +2,29 @@ require './lib/itemrepository'
 require './lib/item'
 
 RSpec.describe ItemRepository do
-  let(:item_repository) {ItemRepository.new}
+  let(:item_repo) {ItemRepository.new}
 
   it 'exists' do
-    expect(item_repository).to be_a ItemRepository
+    expect(item_repo).to be_a ItemRepository
+  end
+
+  describe '#find_by_name' do
+    it 'returns either nil or an instance of Item' do
+      expect(item_repo.find_by_name("Pencil")).to be nil
+
+      item = Item.new({
+        :id          => 1,
+        :name        => "Pencil",
+        :description => "You can use it to write things",
+        :unit_price  => BigDecimal(10.99,4),
+        :created_at  => Time.now,
+        :updated_at  => Time.now,
+        :merchant_id => 2
+      })
+      item_repo = ItemRepository.new([item])
+
+      expect(item_repo.find_by_name("Pencil")).to eq item
+    end
+
   end
 end
