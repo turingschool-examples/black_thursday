acf9d9b36ef39e952d504879d29476fb9ffdf610
Drew Layton <dlayton66@gmail.com>
2022-10-31 23:21:53 -0600

    feat: Create #find_by_id for Reposable

--- lib/reposable.rb
+++ lib/reposable.rb
@@ -6,4 +6,18 @@ module Reposable
   def create(attributes)
     all << class_name.new(attributes)
   end
+  
+  def find_by_id(id)
+    all.find do |entry|
+      entry.id == id
+    end
+  end
+
+
+#   def update(id,attributes)
+#     attributes.each do |attr,val|
+#       find_by_id
+# require 'pry'; binding.pry
+#     end
+  
 end
