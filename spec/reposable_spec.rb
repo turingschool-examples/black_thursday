b7b0028d636a7160cdd5ee154cb35ff920cbf883
Drew Layton <dlayton66@gmail.com>
2022-10-31 22:27:04 -0600

    test: Create #create test for Reposable

--- spec/reposable_spec.rb
+++ spec/reposable_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Reposable do
       expect(item_repo.class_name).to eq Item
     end
   end
+
+  describe '#create' do
+    it 'adds a new instance of the class corresponding to the current repo' do
+      item_repo = ItemRepository.new
+      item_repo.create({
+        :id          => 1,
+        :name        => "Pencil",
+        :description => "You can use it to write things",
+        :unit_price  => BigDecimal(10.99,4),
+        :created_at  => Time.now,
+        :updated_at  => Time.now,
+        :merchant_id => 2
+      })
+
+      expect(item_repo.all[0]).to be_a Item
+    end
+  end
 end
