09b117cbbaa0c6e1ce2c6404c0e33181a235b202
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-31 17:24:20 -0700

    test: Add find by price test

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -14,5 +14,16 @@ class ItemRepository
 
   def find_by_name(name)
     @items.find {|item| item.name.upcase == name.upcase}
-  end 
+  end
+
+  def find_all_with_description(description)
+    items_with_description = @items.find_all {|item| item.description.upcase == description.upcase}
+      if items_with_description == nil
+        []
+      else
+        items_with_description
+      end
+  end
+
+
 end
--- test/item_repository_spec.rb
+++ test/item_repository_spec.rb
@@ -102,7 +102,7 @@ RSpec.describe ItemRepository do
   end
 
   describe '#find_all_with_description' do
-    it 'returns ' do
+    it 'returns an empty array or the instance of the item with matching description' do
       item_1 = Item.new({:id => 1,
                        :name => "Shoes",
                        :description => "left shoe, right shoe",
@@ -117,14 +117,52 @@ RSpec.describe ItemRepository do
                         :created_at => Time.now,
                         :updated_at => Time.now,
                         :merchant_id => 2})
+      item_3 = Item.new({:id => 3,
+                        :name => "More Expensive Cool hat",
+                        :description => "black top hat",
+                        :unit_price => BigDecimal(22.24,4),
+                        :created_at => Time.now,
+                        :updated_at => Time.now,
+                        :merchant_id => 2})
 
-      items = [item_1, item_2]
+      items = [item_1, item_2, item_3]
       item_repository = ItemRepository.new(items)
 
-      expect(item_repository.find_all_with_description("black top hat")).to eq(item_2)
-      expect(item_repository.find_all_with_description("BLack top Hat")).to eq(item_2)
-      expect(item_repository.find_by_id("a really nice suit")).to eq([])
+      expect(item_repository.find_all_with_description("black top hat")).to eq([item_2, item_3])
+      expect(item_repository.find_all_with_description("BLack top Hat")).to eq([item_2, item_3])
+      expect(item_repository.find_all_with_description("a really nice suit")).to eq([])
     end
   end
 
+  describe '#find_all_by price' do
+    it 'returns empty array or instance of item with matching price' do
+      item_1 = Item.new({:id => 1,
+                       :name => "Shoes",
+                       :description => "left shoe, right shoe",
+                       :unit_price => BigDecimal(78.54,4),
+                       :created_at => Time.now,
+                       :updated_at => Time.now,
+                       :merchant_id => 1})
+      item_2 = Item.new({:id => 2,
+                        :name => "Cool hat",
+                        :description => "black top hat",
+                        :unit_price => BigDecimal(22.24,4),
+                        :created_at => Time.now,
+                        :updated_at => Time.now,
+                        :merchant_id => 2})
+      item_3 = Item.new({:id => 3,
+                        :name => "More Expensive Cool hat",
+                        :description => "black top hat",
+                        :unit_price => BigDecimal(22.24,4),
+                        :created_at => Time.now,
+                        :updated_at => Time.now,
+                        :merchant_id => 2})
+
+      items = [item_1, item_2, item_3]
+      item_repository = ItemRepository.new(items)
+
+      expect(item_repository.find_all_by_price(22.24)).to eq([item_2, item_3])
+      expect(item_repository.find_all_by_price(45.21)).to eq([])
+    end
+  end
 end
