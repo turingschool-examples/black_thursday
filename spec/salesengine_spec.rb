b9bf5f7adbeb1f198f3bfc9bf576c783483b6d38
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-01 13:48:31 -0700

    feat: got item repo to initialize and load items after testing

--- spec/salesengine_spec.rb
+++ spec/salesengine_spec.rb
@@ -5,22 +5,25 @@ require './lib/merchant'
 require 'csv'
 
 RSpec.describe SalesEngine do
-  # let(:se) {SalesEngine.from_csv({
-  #                                 :merchants => './data/merchants.csv'})}
+  let(:se) {SalesEngine.from_csv({:items => './data/items.csv',
+                                  :merchants => './data/merchants.csv'})}
 
-  before(:each) do 
-    @se = SalesEngine.from_csv({:merchants => './data/merchants.csv'})
-  end
+  # before(:each) do 
+  #   @se = SalesEngine.from_csv({:merchants => './data/merchants.csv'})
+  
 
   it 'is an instance of a sales engine' do
-    expect(@se).to be_a(SalesEngine)
+    expect(se).to be_a(SalesEngine)
   end
 
   it 'has a method to access the merchant repository with all the merchants loaded' do
-    
-    expect(@se.merchants).to be_a(Merchant_Repository)
-    expect(@se.merchants.find_by_name("Bowlsbychris")).to be_a(Merchant)
+    expect(se.merchants).to be_a(Merchant_Repository)
+    expect(se.merchants.find_by_name("Bowlsbychris")).to be_a(Merchant)
   end
 
+  it 'has a method to access the item repository with all items loaded' do
+    expect(se.items).to be_a(ItemRepository)
+    expect(se.items.find_by_name("Silver Plated Clutch with Swarovski Element Crystals")).to be_a(Item)
+  end
 end
 # :items => './data/items.csv',
