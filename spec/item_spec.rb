a22018fdf591ed36da7a7148bad07f7a3a81c7d2
Samuel Cox <samc1253@gmail.com>
2022-11-01 11:50:29 -0600

    Create item class and add tests for retrieving attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,17 +1,49 @@
 require './lib/item'
+require 'bigdecimal'
 
 RSpec.describe Item do
-  it 'exists' do
-    item = Item.new({
+  before :each do
+    @time = Time.now
+    @item = Item.new({
       :id          => 1,
       :name        => "Pencil",
       :description => "You can use it to write things",
       :unit_price  => BigDecimal(10.99,4),
-      :created_at  => Time.now,
-      :updated_at  => Time.now,
+      :created_at  => @time,
+      :updated_at  => @time,
       :merchant_id => 2
     })
+  end
 
-    expect(item).to be_a(Item)
+  it 'exists' do
+    expect(@item).to be_a(Item)
+  end
+
+  it 'has an id' do
+    expect(@item.id).to eq 1
+  end
+
+  it 'has a name' do
+    expect(@item.name).to eq("Pencil")
+  end
+
+  it 'has a description' do
+    expect(@item.description).to eq("You can use it to write things")
+  end
+
+  it 'has a unit price' do
+    expect(@item.unit_price).to eq(BigDecimal(10.99,4))
+  end
+
+  it 'has a time created at' do
+    expect(@item.created_at).to eq(@time)
+  end
+
+  it 'has a time updated at' do
+    expect(@item.updated_at).to eq(@time)
+  end
+
+  it 'has a merchant id' do
+    expect(@item.merchant_id).to eq 2
   end
 end
