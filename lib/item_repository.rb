dc3336f03648be600eea59620bf755b21367fdc5
Samuel Cox <samc1253@gmail.com>
2022-11-01 15:19:03 -0600

    Make changes to lib and spec files to reflect relevance to interaction pattern

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -6,22 +6,26 @@ require_relative "item"
 class ItemRepository
 include Find
   attr_reader :items
-  
-  def initialize(csv_file)
-    @csv_file = csv_file
+
+  def initialize
+    # @csv_file = csv_file
     @items = []
-    contents = CSV.open(csv_file, headers: true, header_converters: :symbol)
-    contents.each do |row|
-      @items << Item.new(
-        id: row[:id].to_i,
-        name: row[:name],
-        description: row[:description],
-        unit_price: row[:unit_price].to_f,
-        created_at: row[:created_at],
-        updated_at: row[:updated_at],
-        merchant_id: row[:merchant_id].to_i
-        )
-    end
+    # contents = CSV.open(csv_file, headers: true, header_converters: :symbol)
+    # contents.each do |row|
+    #   @items << Item.new(
+    #     id: row[:id].to_i,
+    #     name: row[:name],
+    #     description: row[:description],
+    #     unit_price: row[:unit_price].to_f,
+    #     created_at: row[:created_at],
+    #     updated_at: row[:updated_at],
+    #     merchant_id: row[:merchant_id].to_i
+    #     )
+    # end
+  end
+
+  def add(data)
+    @items << Item.new(data)
   end
 
   def find_all_with_description(descrip)
@@ -32,7 +36,7 @@ include Find
 
   def find_all_by_price(price)
     @items.find_all do |item|
-      item.unit_price == price
+      item.unit_price_to_dollars == price
     end
   end
 
--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,41 +1,43 @@
 require "./lib/item_repository"
 require './lib/item'
+require './lib/sales_engine'
 require "pry"
 
 RSpec.describe ItemRepository do
   before :each do
-    @item_repository = ItemRepository.new("./data/items.csv")
+    se = SalesEngine.from_csv({items: './data/items.csv', merchants: './data/merchants.csv'})
+    @ir = se.items
   end
 
   it 'exists' do
-    expect(@item_repository).to be_a(ItemRepository)
+    expect(@ir).to be_a(ItemRepository)
   end
 
   it 'returns an array of all known Item instances' do
-    expect(@item_repository.items).to be_a(Array)
+    expect(@ir.items).to be_a(Array)
   end
 
   it 'finds by id, returns nil otherwise' do
-    expect(@item_repository.find_by_id(263395237)).to eq(@item_repository.items.first)
-    expect(@item_repository.find_by_id(263567474)).to eq(@item_repository.items.last)
-    expect(@item_repository.find_by_id(263395617)).to eq(@item_repository.items[1])
-    expect(@item_repository.find_by_id(999999999)).to eq(nil)
-    expect(@item_repository.find_by_id(263395237)).to be_a(Item)
+    expect(@ir.find_by_id(263395237)).to eq(@ir.items.first)
+    expect(@ir.find_by_id(263567474)).to eq(@ir.items.last)
+    expect(@ir.find_by_id(263395617)).to eq(@ir.items[1])
+    expect(@ir.find_by_id(999999999)).to eq(nil)
+    expect(@ir.find_by_id(263395237)).to be_a(Item)
   end
 
   it 'finds by name (case insensitive), returns nil otherwise' do
-    expect(@item_repository.find_by_name("510+ RealPush Icon Set")).to eq(@item_repository.items.first)
-    expect(@item_repository.find_by_name("510+ reAlPUSh IcoN seT")).to eq(@item_repository.items.first)
-    expect(@item_repository.find_by_name("Glitter scrabble frames")).to eq(@item_repository.items[1])
-    expect(@item_repository.find_by_name("GLITter SCRAbbLe fraMES")).to eq(@item_repository.items[1])
-    expect(@item_repository.find_by_name("XXXXXXXXXX")).to eq(nil)
-    expect(@item_repository.find_by_name("510+ RealPush Icon Set")).to be_a(Item)
+    expect(@ir.find_by_name("510+ RealPush Icon Set")).to eq(@ir.items.first)
+    expect(@ir.find_by_name("510+ reAlPUSh IcoN seT")).to eq(@ir.items.first)
+    expect(@ir.find_by_name("Glitter scrabble frames")).to eq(@ir.items[1])
+    expect(@ir.find_by_name("GLITter SCRAbbLe fraMES")).to eq(@ir.items[1])
+    expect(@ir.find_by_name("XXXXXXXXXX")).to eq(nil)
+    expect(@ir.find_by_name("510+ RealPush Icon Set")).to be_a(Item)
   end
 
   it 'finds all instances given a description (case insensitive), returns empty array otherwise' do
-    item1 = @item_repository.find_all_with_description("Disney")
-    item2 = @item_repository.find_all_with_description("diSNeY")
-    item3 = @item_repository.find_all_with_description("XXXXXXXXX")
+    item1 = @ir.find_all_with_description("Disney")
+    item2 = @ir.find_all_with_description("diSNeY")
+    item3 = @ir.find_all_with_description("XXXXXXXXX")
 
     expect(item1.count).to eq(5)
     expect(item2.count).to eq(5)
@@ -47,9 +49,9 @@ RSpec.describe ItemRepository do
   end
 
   it 'finds items with exact price given, returns empty array otherwise' do
-    item1 = @item_repository.find_all_by_price(1300)
-    item2 = @item_repository.find_all_by_price(1350)
-    item3 = @item_repository.find_all_by_price(9999)
+    item1 = @ir.find_all_by_price(1300)
+    item2 = @ir.find_all_by_price(1350)
+    item3 = @ir.find_all_by_price(9999)
 
     expect(item1.count).to eq(8)
     expect(item2.first.id).to eq(263395721)
@@ -58,8 +60,8 @@ RSpec.describe ItemRepository do
   end
 
   it 'finds item within a given price range, returns empty array otherwise' do
-    item1 = @item_repository.find_all_by_price_in_range(1300..1360)
-    item2 = @item_repository.find_all_by_price_in_range(1..5)
+    item1 = @ir.find_all_by_price_in_range(1300..1360)
+    item2 = @ir.find_all_by_price_in_range(1..5)
 
     expect(item1.count).to eq(9)
     expect(item1.first.id).to eq(263395617)
@@ -69,8 +71,8 @@ RSpec.describe ItemRepository do
   end
 
   it 'finds item with matching merchant ID, returns empty array otherwise' do
-    item1 = @item_repository.find_all_by_merchant_id(12334105)
-    item2 = @item_repository.find_all_by_merchant_id(11111111)
+    item1 = @ir.find_all_by_merchant_id(12334105)
+    item2 = @ir.find_all_by_merchant_id(11111111)
 
     expect(item1.count).to eq(3)
     expect(item1.first.id).to eq(263396209)
