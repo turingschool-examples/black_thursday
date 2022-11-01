6b4f722a248c6ce7763337af1443680840fca4d0
Alex Pitzel <pitzelalex@gmail.com>
2022-10-31 21:12:20 -0400

    test: Add 'can store an array of merchants'

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -3,12 +3,21 @@ require './lib/merchant_repository'
 
 describe MerchantRepository do
   describe '#initialize' do
-    before(:each) do
-      @mr = MerchantRepository.new
-    end
+    let(:mr) { MerchantRepository.new }
 
     it 'is and instance of MerchantRepository' do
-      expect(@mr).to be_a MerchantRepository
+      expect(mr).to be_a MerchantRepository
+    end
+
+    it 'can store an array of merchants' do
+      expect(mr.merchants).to eq([])
+    end
+  end
+
+  describe '#create' do
+    xit 'creates a Merchant and adds it to @merchants' do
+
+      expect(mr.merchants).to eq([])
     end
   end
 end
