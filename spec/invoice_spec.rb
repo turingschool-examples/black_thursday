93c2613198d48855d459d882f974a8691ac0988f
Samuel Cox <samc1253@gmail.com>
2022-11-01 16:40:28 -0600

    Add tests for attributes and allow invoice to be created

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -1,9 +1,23 @@
 require './lib/invoice'
 
 RSpec.describe Invoice do
-  it 'exists' do
-    invoice = Invoice.new
+  it 'exists and has attributes' do
+    time = Time.now
+    invoice = Invoice.new({
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => time,
+      :updated_at  => time,
+    })
 
     expect(invoice).to be_a(Invoice)
+    expect(invoice.id).to eq 6
+    expect(invoice.customer_id).to eq 7
+    expect(invoice.merchant_id).to eq 8
+    expect(invoice.status).to eq("pending")
+    expect(invoice.created_at).to eq time
+    expect(invoice.updated_at).to eq time
   end
 end
