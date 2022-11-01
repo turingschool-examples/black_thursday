8fd9cd38072b0fdff5df96c0fc58fa738c0cd821
KaraJH <karas.jones@yahoo.com>
2022-11-01 11:35:06 -0600

    Adds extra tests for merchant class

--- spec/merchant_spec.rb
+++ spec/merchant_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe do
 
     expect(m).to be_a(Merchant)
   end
+
+  it 'has a name and id' do
+      m = Merchant.new({:id => 5, :name => "Turing School"})
+
+      expect(m.id).to eq(5)
+      expect(m.name).to eq("Turing School")
+  end
 end
