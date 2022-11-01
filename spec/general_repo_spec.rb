be939cbe60c04d8dee4240349d56ed482715911e
Thomas Casady <tscasady@gmail.com>
2022-11-01 17:31:26 -0500

    test: Update tests to not use let for objects.

--- spec/general_repo_spec.rb
+++ spec/general_repo_spec.rb
@@ -5,16 +5,17 @@ require 'CSV'
 RSpec.describe GeneralRepo do
   let(:data){ CSV.readlines('./data/general_test.csv', headers: true, header_converters: :symbol) }
 
-  let(:gr) { GeneralRepo.new }
+  let(:gr) { GeneralRepo.new(data) }
 
-  let(:object1) { gr.create(data[0]) }
-  let(:object2) { gr.create(data[1]) }
-  let(:object3) { gr.create(data[2]) }
-  let(:object4) { gr.create(data[3]) }
-  let(:object5) { gr.create(data[4]) }
+  
 
   describe '#initialize' do
     it 'exists and has readable attrs' do
+      object1 = gr.repository[0]
+      object2 = gr.repository[1]
+      object3 = gr.repository[2]
+      object4 = gr.repository[3]
+      object5 = gr.repository[4]
       expect(gr).to be_a GeneralRepo
       expect(gr.repository).to eq [object1, object2, object3, object4, object5]
     end
@@ -22,18 +23,36 @@ RSpec.describe GeneralRepo do
 
   describe '#all' do
     it 'returns a list of objects in its repository' do
-      expect(gr.repository).to eq []
+      object1 = gr.repository[0]
+      object2 = gr.repository[1]
+      object3 = gr.repository[2]
+      object4 = gr.repository[3]
+      object5 = gr.repository[4]
+
+      expect(gr.repository).to eq [object1, object2, object3, object4, object5]
     end
   end
 
   describe '#find_by_id' do
     it 'finds one object in the repository with matching id' do
+      object1 = gr.repository[0]
+      object2 = gr.repository[1]
+      object3 = gr.repository[2]
+      object4 = gr.repository[3]
+      object5 = gr.repository[4]
+
       expect(gr.find_by_id(3)).to eq object3
     end
   end
 
   describe '#create' do
     it 'takes in CSV data and makes an new object and stores it in the repo' do
+      object1 = gr.repository[0]
+      object2 = gr.repository[1]
+      object3 = gr.repository[2]
+      object4 = gr.repository[3]
+      object5 = gr.repository[4]
+
       new_data = {attribute: 'fuscia'}
       new_object = gr.create(new_data)
       expect(new_object.attribute).to eq 'fuscia'
@@ -43,13 +62,20 @@ RSpec.describe GeneralRepo do
 
   describe '#update' do
     it 'can update an object(id) with attributes' do
+      
       gr.update(3, {attribute: 'olive'})
-      expect(gr.find_by_id(id).attribute).to eq 'olive'
+      expect(gr.find_by_id(3).attribute).to eq 'olive'
     end
   end
 
-  descirbe '#delete' do
+  describe '#delete' do
     it 'can remove and object from its repository' do
+      object1 = gr.repository[0]
+      object2 = gr.repository[1]
+      object3 = gr.repository[2]
+      object4 = gr.repository[3]
+      object5 = gr.repository[4]
+      
       gr.delete(2)
       expect(gr.repository).to eq [object1, object3, object4, object5]
     end
