a8e338a9cb342d1d531d9bdfe5cafde22014a98e
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-02 18:33:47 -0500

    Add simplecov test

--- lib/salesengine.rb
+++ lib/salesengine.rb
@@ -14,5 +14,4 @@ class SalesEngine
   def self.from_csv(files)
     SalesEngine.new(files)
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'simplecov'
 SimpleCov.start
+require 'item_repository_spec'
+require 'merchant_repository_spec'
+require 'item_spec'
+require 'merchant_spec'
+require 'salesengine_spec'
 
 #require spec files
-#require_relative 
+#require_relative
