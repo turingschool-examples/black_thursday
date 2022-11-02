c3c96cdb04a149569478202bd093a4f94a2d594c
Drew Layton <dlayton66@gmail.com>
2022-11-01 18:01:45 -0600

    test: Create SalesAnalyst Merchant tests

--- lib/salesanalyst.rb
+++ lib/salesanalyst.rb
@@ -1,4 +1,6 @@
 class SalesAnalyst
+  attr_reader :merchants, :items
+  
   def initialize(merchants,items)
     @merchants = merchants
     @items = items
--- spec/salesanalyst_spec.rb
+++ spec/salesanalyst_spec.rb
@@ -1,5 +1,6 @@
 require './lib/salesanalyst'
 require './lib/salesengine'
+require './lib/itemrepository'
 
 RSpec.describe SalesAnalyst do
   let(:sales_engine) {SalesEngine.from_csv({:items => './data/items.csv',
@@ -13,6 +14,24 @@ RSpec.describe SalesAnalyst do
   describe '#average_items_per_merchant' do
     it 'gives how many items a merchant has on average' do
       expect(sales_analyst.average_items_per_merchant).to eq 2.88
+    end
+  end
+
+  describe '#average_items_per_merchant_standard_deviation' do
+    it 'returns the stdev of merchant average # of items' do
+      expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq 3.26
+    end
+  end
+
+  describe '#merchants_with_high_item_count' do
+    it 'returns merchants whose average # of items is >1 stdev' do
+      avg = sales_analyst.average_items_per_merchant
+      stdev = sales_analyst.average_items_per_merchant_standard_deviation
+
+      expect(
+        sales_analyst.merchants_with_high_item_count.all? {
+          |merchant| sales_analyst.items.find_all_by_merchant_id(merchant.id).count > avg + stdev 
+          }).to be true
     end
   end
 end
