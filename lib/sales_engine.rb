2243ec2ebab8cbc8cd19c2a9b4020ff80b17624e
Anhnhi Tran <anhtran811@gmail.com>
2022-11-01 19:55:19 -0600

    Add common roots test in SE spec and started lib

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -13,4 +13,16 @@ class SalesEngine
     merchants_contents = CSV.open(csv_hash[:merchants], headers: true, header_converters: :symbol)
     sales_engine = SalesEngine.new(items_contents, merchants_contents)
   end
+
+  def items
+    item_repository
+  end
+  
+  # def merchants
+  #   mr = MerchantRepository.new
+  #   @merchants.each do |merchant|
+  #     mr.add({id: merchant[:id], name: merchant[:name]})
+  #   end
+  #   mr
+  # end
 end
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe SalesEngine do
 
     expect(sales_engine).to be_a(SalesEngine)
   end
+
+  it 'has common roots to their repositories' do
+    sales_engine = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+    expect(sales_engine.items).to be_a(ItemRepository)
+    expect(sales_engine.merchants).to be_a(MerchantRepository)
+  end
 end
