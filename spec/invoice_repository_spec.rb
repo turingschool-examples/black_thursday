4680dac36656e3b6d95f211661bb2b6f8bbe03c8
Samuel Cox <samc1253@gmail.com>
2022-11-01 20:16:56 -0600

    Add test for finding all

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe InvoiceRepository do
     expect(invr.invoices).to eq([])
   end
 
+  it 'can find all' do
+    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
+
+    expect(se.invoices.all.first).to be_a Invoice
+    expect(se.invoices.all).to be_a Array
+  end
+
   it 'can find by id' do
     se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
 
