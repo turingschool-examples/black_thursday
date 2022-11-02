d459f1e6f8ffa8740f1a3729438dc03e34f1b683
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-02 13:18:43 -0700

    Add method names

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,6 +2,33 @@ require './lib/sales_engine'
 require 'bigdecimal/util'
 
 class SalesAnalyst
+  attr_reader :items,
+              :merchants
   def initialize
+    @items =
+    @merchants =
   end
+  
+  def items_per_merchant
+    
+  end
+  
+  def average_items_per_merchant
+  end
+  
+  def average_items_per_merchant_standard_deviation
+  end
+  
+  def merchants_with_high_item_count
+  end
+  
+  def average_average_price_for_merchant(merchant)
+  end
+  
+  def average_average_price_per_merchant
+  end
+  
+  def golden_items
+  end
+  
 end
