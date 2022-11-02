64eff934978f69215c3ccd5d4299a23cee5b96be
Anhnhi Tran <anhtran811@gmail.com>
2022-11-02 15:26:06 -0600

    Fix item repo and merchant repo name to include underscores

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ SimpleCov.start
 
 # vv syntax for including test files vv
 require_relative 'merchant_spec'
-require_relative 'merchantrepository_spec'
+require_relative 'merchant_repository_spec'
 require_relative 'item_spec'
-require_relative 'itemrepository_spec'
+require_relative 'item_repository_spec'
