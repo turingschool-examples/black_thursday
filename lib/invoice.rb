2ad5d3796e652ed4ef365d0f97310dfe579935b4
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-02 11:39:26 -0600

    Feat: add initialize method for class Invoice

---
+++ lib/invoice.rb
@@ -0,0 +1,17 @@
+class Invoice
+  attr_reader :id, 
+              :customer_id, 
+              :merchant_id, 
+              :status, 
+              :created_at, 
+              :updated_at
+
+ def initialize(invoice_info)
+  @id = invoice_info[:id]
+  @customer_id = invoice_info[:customer_id]
+  @merchant_id = invoice_info[:merchant_id]
+  @status = invoice_info[:status]
+  @created_at = invoice_info[:created_at]
+  @updated_at = invoice_info[:updated_at]
+ end 
+end 
--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -17,7 +17,20 @@ RSpec.describe Invoice do
       expect(i.customer_id).to eq(7)
       expect(i.merchant_id).to eq(8)
       expect(i.status).to eq("pending")
-      
     end 
-  end 
+    
+    it 'will have a time' do 
+        i = Invoice.new({
+          :id          => 6,
+          :customer_id => 7,
+          :merchant_id => 8,
+          :status      => "pending",
+          :created_at  => "2022-11-02 11:33:36.699596 -0600",
+          :updated_at  =>  "2022-11-02 11:33:36.699596 -0600",
+        })
+          
+          expect(i.created_at).to eq("2022-11-02 11:33:36.699596 -0600")
+          expect(i.updated_at).to eq("2022-11-02 11:33:36.699596 -0600")
+    end  
+  end      
 end 
