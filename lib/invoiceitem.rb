48ec8124b120e0457ebed69f3ef1c1b39b9d70e8
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-01 19:32:11 -0700

    feat: invoice item unit price to dollars method

--- lib/invoiceitem.rb
+++ lib/invoiceitem.rb
@@ -8,15 +8,19 @@ class InvoiceItem
               :updated_at
 
   def initialize(ii_input)
-    @id = ii_input[:id]
-    @item_id = ii_input[:item_id]
-    @invoice_id = ii_input[:invoice_id]
-    @quantity = ii_input[:quantity]
-    @unit_price = ii_input[:unit_price]
-    @created_at = ii_input[:created_at]
-    @updated_at = ii_input[:updated_at]
+    @id           = ii_input[:id]
+    @item_id      = ii_input[:item_id]
+    @invoice_id   = ii_input[:invoice_id]
+    @quantity     = ii_input[:quantity]
+    @unit_price   = ii_input[:unit_price]
+    @created_at   = ii_input[:created_at]
+    @updated_at   = ii_input[:updated_at]
   end
 
-  
+  def unit_price_to_dollars
+    @unit_price.to_f
+  end
+
+
 
 end
