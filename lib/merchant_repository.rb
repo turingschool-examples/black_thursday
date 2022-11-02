98357078e09f19d4f3051f7fc2cb99f5ef32e6d7
Alex Pitzel <pitzelalex@gmail.com>
2022-11-01 21:36:07 -0400

    refactor: Remove old commented code to finish refactor

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -16,28 +16,11 @@ class MerchantRepository < GeneralRepo
     @repository << Merchant.new(data)
   end
 
-  def all
-    @repository
-  end
-
-  def find_by_id(id)
-    repository.find { |merchant| merchant.id == id }
-  end
-
   def find_by_name(name)
     repository.find { |merchant| merchant.name.casecmp?(name) }
   end
 
   def find_all_by_name(name)
     repository.select { |merchant| merchant.name.downcase.include? name.downcase }
-  end
-
-  def update(id, name)
-    merchant = find_by_id(id)
-    merchant.update(name)
-  end
-
-  def delete(id)
-    @repository.delete(find_by_id(id))
   end
 end
