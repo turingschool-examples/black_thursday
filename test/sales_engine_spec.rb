f7ecd2d96cf242b25c9ce9e2c1711f51ea9eb3e9
Ryan Canton <ryancanton999@gmail.com>
2022-11-02 09:05:09 -0600

    Test: Add test for method items for class SalesEngine

--- test/sales_engine_spec.rb
+++ test/sales_engine_spec.rb
@@ -2,12 +2,25 @@ require './spec_helper'
 require './lib/sales_engine'
 
 RSpec.describe SalesEngine do
-  it 'exists' do
-    se = se = SalesEngine.new({
-      :items     => './data/items.csv',
-      :merchants => './data/merchants.csv',
-    })
+  describe '#initialize' do
+    it 'exists' do
+      se = se = SalesEngine.new({
+        :items     => './data/items.csv',
+        :merchants => './data/merchants.csv'})
 
-    expect(se).to be_a(SalesEngine)
+      expect(se).to be_a(SalesEngine)
+    end
+  end
+
+  describe '#items' do
+    it 'creates and returns an item repository object' do
+      se = se = SalesEngine.new({
+        :items     => './data/items.csv',
+        :merchants => './data/merchants.csv'})
+
+      ir = se.items
+
+      expect(ir).to be_a(ItemRepository)
+    end
   end
 end
