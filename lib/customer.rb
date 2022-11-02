fed42a155a94a6d31f187069aa6a2ba4ab142666
Thomas Casady <tscasady@gmail.com>
2022-11-02 18:56:59 -0500

    feat: Add update method to Customer.

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,5 +14,8 @@ class Customer
   end
 
   def update(attributes)
+    @first_name = attributes[:first_name] unless attributes[:first_name].nil?
+    @last_name = attributes[:last_name] unless attributes[:last_name].nil?
+    @updated_at = Time.now
   end
 end
