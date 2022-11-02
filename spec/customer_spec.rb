0d541a76fa1c4e11b365ca8e05d309e5445a00c3
Thomas Casady <tscasady@gmail.com>
2022-11-02 18:18:32 -0500

    test: Add Customer class tests.

---
+++ spec/customer_spec.rb
@@ -0,0 +1,24 @@
+require './lib/customer'
+
+RSpec.describe Customer do
+  describe '#initialize' do
+    it 'exists and has readable attributes' do
+      time1 = Time.now
+      time2 = Time.now
+      data = {
+                id:          6,
+                first_name:  'Joan',
+                last_name: 'Clarke',
+                created_at:   time1,
+                updated_at:   time2
+              }
+      customer = customer.new(data)
+
+      expect(customer).to be_a Customer
+      expect(customer.id).to eq 6
+      expect(customer.first_name).to eq 'Joan'
+      expect(customer.last_name).to eq 'Clarke'
+      expect(customer.created_at).to eq time1
+      expect(customer.updated_at).to eq time2
+    end
+  end
