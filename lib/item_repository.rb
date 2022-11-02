94d5af36529e0bbad10dbcc1528e5d4af692b8b5
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-02 14:03:15 -0700

    fixed: debugged iteration0

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -11,6 +11,36 @@ class ItemRepository
     @all = all
   end
 
+  def create(attributes)
+    all << Item.new({ :name         => attributes[:name],
+                      :id           => next_id,
+                      :description  => attributes[:description],
+                      :unit_price   => attributes[:unit_price],
+                      :created_at   => attributes[:created_at],
+                      :updated_at   => attributes[:updated_at],
+                      :merchant_id  => attributes[:merchant_id]
+                    })
+                    
+  end
+
+  def update(id,attributes)
+    if find_by_id(id) == nil 
+      return
+    else
+    find_by_id(id).updated_at = Time.now
+    attributes.each do |att,val|
+      case att
+      when :name
+        find_by_id(id).name = val
+      when :description
+        find_by_id(id).description = val
+      when :unit_price
+        find_by_id(id).unit_price = val
+      end
+    end
+    end
+  end
+
   def find_by_name(name)
     all.find do |item|
       item.name.upcase == name.upcase
@@ -25,7 +55,7 @@ class ItemRepository
 
   def find_all_by_price(price)
     all.select do |item|
-      item.unit_price == price
+      item.unit_price.to_f == price
     end
   end
 
--- lib/reposable.rb
+++ lib/reposable.rb
@@ -3,18 +3,6 @@ module Reposable
     Object.const_get(self.class.name.chomp('Repository'))
   end
 
-  def create(attributes)
-    stuff = {:name        => attributes[:name],
-            :id           => next_id,
-            :description  => attributes[:description],
-            :unit_price   => attributes[:unit_price],
-            :created_at   => attributes[:created_at],
-            :updated_at   => attributes[:updated_at],
-            :merchant_id  => attributes[:merchant_id]}
-            
-    all.push(class_name.new(stuff))
-  end
-
   def next_id
     if all.empty?
       1
@@ -29,18 +17,18 @@ module Reposable
     end
   end
 
-  def update(id,attributes)
-    attributes.each do |att,val|
-      case att
-      when :name
-        find_by_id(id).name = val
-      when :description
-        find_by_id(id).description = val
-      when :unit_price
-        find_by_id(id).unit_price = val
-      end
-    end
-  end
+  # def update(id,attributes)
+  #   attributes.each do |att,val|
+  #     case att
+  #     when :name
+  #       find_by_id(id).name = val
+  #     when :description
+  #       find_by_id(id).description = val
+  #     when :unit_price
+  #       find_by_id(id).unit_price = val
+  #     end
+  #   end
+  # end
 
   def delete(id)
     all.delete(find_by_id(id))
