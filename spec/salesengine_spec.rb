2d81d0e556fcb53bd0a40fca5961a78986cb49ba
Braxton Eddings <beddings81@gmail.com>
2022-11-02 16:32:32 -0500

    Add sales engine item test

--- spec/salesengine_spec.rb
+++ spec/salesengine_spec.rb
@@ -1,12 +1,22 @@
+require './lib/salesengine.rb'
+require './lib/item.rb'
+require 'csv'
 
 RSpec.describe SalesEngine do
 
   it 'exists' do
-    se = SalesEngine.from_csv({
+    se = SalesEngine.new
+    
+      expect(se).to be_a(SalesEngine)
+  end
+
+  it 'can have an item repository' do
+    files = {
       :items     => "./data/items.csv",
       :merchants => "./data/merchants.csv",
-      })
+    }
+    se = SalesEngine.new(files)
 
-      expect(se).to be_a(SalesEngine)
+    expect(se.items.all.all?(Item)).to be(true)
   end
 end
