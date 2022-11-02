3c540afdcfa3505347cfaf98a44168b2a0aecf30
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-01 22:35:48 -0600

    Add standard_deviation method, pass respective test

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -7,13 +7,31 @@ class SalesAnalyst
 
   def average_items_per_merchant
     (engine.items.all.length.to_f / engine.merchants.all.length).round(2)
-
   end
 
+  def merch_hash
+    hsh = engine.items.all.group_by do |item|
+      item.merchant_id
+    end
+    hsh.map do |keys, values|
+      hsh[keys] = values.count
+    end
+    hsh
+  end
+
+  # def std_dev(nums, avg)
+  #   total = nums.sum do |num|
+  #     (num - avg)**2
+  #   end
+  #   Math.sqrt(total / (nums.length - 1)).round(2)
+  # end
+
   def average_items_per_merchant_standard_deviation
-    merchant_sample = []
-    merchant_sample << engine.merchants.all.sample(3)
-    require 'pry'; binding.pry
-    std_dev = sqrt( ( (3-4)^2+(4-4)^2+(5-4)^2 ) / 2 )
+    sqr_diff = 0.0
+    merch_hash.each do |merchant, items|
+      sqr_diff += (items - average_items_per_merchant)**2
+    end
+    std_dev = Math.sqrt((sqr_diff / (merch_hash.keys.count - 1)))
+    std_dev.round(2)
   end
 end
