0007de7936297578a511109cfda6b53725ae9e82
Thomas Casady <tscasady@gmail.com>
2022-11-02 11:47:40 -0500

    refactor: Change update method to both change multiple attributes.

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -6,12 +6,12 @@ describe Invoice do
       time1 = Time.now
       time2 = Time.now
       data = {
-                id:          => 6,
-                customer_id: => 7,
-                merchant_id: => 8,
-                status:     => 'pending',
-                created_at:  => time1,
-                updated_at:  => time2
+                id:          6,
+                customer_id: 7,
+                merchant_id: 8,
+                status:     'pending',
+                created_at:  time1,
+                updated_at:  time2
               }
       invoice = Invoice.new(data)
 
@@ -23,41 +23,23 @@ describe Invoice do
       expect(invoice.updated_at).to eq time2
     end
   end
-
-  describe '#set_status'do
-    it 'can change the status'do
-      time1 = Time.now
-      time2 = Time.now
-      data = {
-                id:          => 6,
-                customer_id: => 7,
-                merchant_id: => 8,
-                status:     => 'pending',
-                created_at:  => time1,
-                updated_at:  => time2
-             }
-      invoice = Invoice.new(data)
-      invoice.set_status('shipped')
-
-      expect(invoice.status).to eq 'shipped'
-    end
-  end
-
+  
   describe '#update' do
     it 'can change the updated_at to the current Time' do
       time1 = Time.now
       time2 = Time.now
       data = {
-                id:          => 6,
-                customer_id: => 7,
-                merchant_id: => 8,
-                status:     => 'pending',
-                created_at:  => time1,
-                updated_at:  => time2
+                id:           6,
+                customer_id: 7,
+                merchant_id: 8,
+                status:     'pending',
+                created_at:  time1,
+                updated_at:  time2
              }
       invoice = Invoice.new(data)
 
-      expect(invoice.update).to be_within(0.5).of Time.now
+      expect(invoice.update({status: 'shipped'})).to be_within(0.5).of Time.now
+      expect(invoice.status).to eq 'shipped'
     end
   end
 end
