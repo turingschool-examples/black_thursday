d8d3e5cae484087971360ff1da519d608634e184
Thomas Casady <tscasady@gmail.com>
2022-11-02 15:42:15 -0500

    Update tests to account for new repo instance variables.

--- lib/invoice_repo.rb
+++ lib/invoice_repo.rb
@@ -1,23 +1,22 @@
-# InvoiceRepo holds, creates, updates, destroys, and finds Invoices.
+# InvoiceRepo holds, creates, updates, destroys, and finds repository.
 require './lib/general_repo'
 
 class InvoiceRepo < GeneralRepo
-  attr_reader :invoices
-
+  
   def initialize(data)
-    super(data, 'Invoice')
+    super('Invoice', data)
   end
 
   def find_by_customer_id(id)
-    @invoices.select { |invoice| invoice.customer_id == id.to_s }
+    @repository.select { |invoice| invoice.customer_id == id.to_s }
   end
 
   def find_by_merchant_id(id)
-    @invoices.select { |invoice| invoice.merchant_id == id.to_s }
+    @repository.select { |invoice| invoice.merchant_id == id.to_s }
   end
 
   def find_all_by_status(status)
-    @invoices.select { |invoice| invoice.status == status }
+    @repository.select { |invoice| invoice.status == status }
   end
 end
 
