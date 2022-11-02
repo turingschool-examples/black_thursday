2e4af8e26f642d5979929b237f65be979f4b3cc5
Lacey Weaver <jlweave89@gmail.com>
2022-11-02 14:53:48 -0600

    Add non specific tests to invoice

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -8,52 +8,62 @@ RSpec.describe Invoice do
     expect(invoice).to be_a(Invoice)
   end
 
-  it 'can return an invoice by id' do
+  it 'returns all the invoice instances' do
+    expect(invoice.all).to eq()
+  end
 
+  it 'can return an invoice by id' do
+    expect(invoice.find_by_id()).to eq()
   end
 
   it 'returns nil if it is not within' do
-
+    expect(invoice.find_by_id()).to eq(nil)
+    expect(invoice.find_by_id()).to eq(nil)
   end
 
   it 'can return the customer id' do
-
+    expect(invoice.find_all_by_customer_id()).to eq()
   end
 
   it 'returns nil if it is not within' do
-
+    expect(invoice.find_all_by_customer_id()).to eq(nil)
+    expect(invoice.find_all_by_customer_id()).to eq(nil)
   end
 
   it 'can return the merchants id' do
-
+    expect(invoice.find_all_by_merchant_id()).to eq()
   end
 
   it 'returns nil if it is not within' do
-
+    expect(invoice.find_all_by_merchant_id()).to eq(nil)
+    expect(invoice.find_all_by_merchant_id()).to eq(nil)
   end
 
   it 'can return the status' do
-
+    expect(invoice.status).to eq()
   end
 
   it 'returns nil if it is not within' do
-
+    expect(invoice.status).to eq(nil)
+    expect(invoice.status).to eq(nil)
   end
 
   it 'can return the time instance when item was first created' do
-
+    expect(invoice.created_at).to eq()
   end
 
   it 'returns nil if it is not within' do
-
+    expect(invoice.created_at).to eq(nil)
+    expect(invoice.created_at).to eq(nil)
   end
 
   it 'can return the time for when the item was last modified' do
-
+    expect(invoice.updated_at).to eq()
   end
 
   it 'returns nil if it is not within' do
-
+    expect(invoice.updated_at).to eq(nil)
+    expect(invoice.updated_at).to eq(nil)
   end
 end
 
