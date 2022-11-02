2be336139c2ac8fec9405d4839f4b837034c6a13
Lacey Weaver <jlweave89@gmail.com>
2022-11-02 15:28:12 -0600

    Add untested instance variable to invoice

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,6 +1,11 @@
 class Invoice
 
   def initialize
-    
+    @id = id
+    @customer_id = customer_id
+    @merchant_id = merchant_id
+    @status = status
+    # @created_at = created_at
+    # @updated_at = updated_at
   end
 end
