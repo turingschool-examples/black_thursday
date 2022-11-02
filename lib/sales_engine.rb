66656a4e3ca548725db72b53b8e8f9fd8395e357
Alex Pitzel <pitzelalex@gmail.com>
2022-11-02 16:13:19 -0400

    feat: Create SE #from_csv

---
+++ lib/sales_engine.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# This is the SalesEngine class
+class SalesEngine
+  attr_reader :repository
+
+  def initialize(data = {})
+    @repository = data
+  end
+
+  def self.from_csv(data)
+    @mr = 
+  end
+end
