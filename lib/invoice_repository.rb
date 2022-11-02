9ef984f6196ea7c5c34b444414d2a0ba95285dee
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-02 13:26:23 -0600

    Feat: add all method for class invoice repo

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,7 +1,12 @@
 class InvoiceRepository
   attr_reader :invoices
-  
+
   def initialize(invoices)
     @invoices = invoices
   end 
+
+  def all 
+    @invoices
+  end 
+
 end 
--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -3,29 +3,39 @@ require './lib/invoice'
 require './lib/invoice_repository'
 
 RSpec.describe InvoiceRepository do 
+  let (:invoice_1) {Invoice.new({
+    :id          => 6,
+    :customer_id => 7,
+    :merchant_id => 8,
+    :status      => "pending",
+    :created_at  => Time.now,
+    :updated_at  => Time.now
+})}
+
+let (:invoice_2) {Invoice.new({
+    :id          => 4,
+    :customer_id => 5,
+    :merchant_id => 6,
+    :status      => "paid",
+    :created_at  => Time.now,
+    :updated_at  => Time.now   
+    })}
   describe "#initialize" do 
     it 'will exist' do 
-      invoice_1 = Invoice.new({
-        :id          => 6,
-        :customer_id => 7,
-        :merchant_id => 8,
-        :status      => "pending",
-        :created_at  => Time.now,
-        :updated_at  => Time.now,
-      })
-
-      invoice_2 = Invoice.new({
-        :id          => 4,
-        :customer_id => 5,
-        :merchant_id => 6,
-        :status      => "paid",
-        :created_at  => Time.now,
-        :updated_at  => Time.now,
-      }) 
       invoices = [invoice_1, invoice_2]
       invoice_repo = InvoiceRepository.new(invoices)
 
       expect(invoice_repo).to be_a(InvoiceRepository)
+    end 
+  end 
+
+  describe "#all" do 
+    it 'will return all invoices' do 
+      invoices = [invoice_1, invoice_2]
+      invoice_repo = InvoiceRepository.new(invoices)
+
+      expect(invoice_repo.all).to eq([invoice_1, invoice_2])
+     
     end 
   end 
 end 
