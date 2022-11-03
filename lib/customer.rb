098aacc8478247121fbbb3096f20edc3c852120c
Drew Layton <dlayton66@gmail.com>
2022-11-03 16:05:06 -0600

    feat: Create Customer class

---
+++ lib/customer.rb
@@ -0,0 +1,14 @@
+require 'time'
+
+class Customer
+  attr_reader :id, :first_name, :last_name
+  attr_accessor :created_at, :updated_at
+
+  def initialize(attributes)
+    @id           = attributes[:id]
+    @first_name   = attributes[:first_name]
+    @last_name    = attributes[:last_name]
+    @created_at   = attributes[:created_at]
+    @updated_at   = attributes[:updated_at]  
+  end
+end
