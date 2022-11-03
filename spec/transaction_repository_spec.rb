5ab67278ee2817ddf04f96cfaa1a719a6a8230bf
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-03 14:59:19 -0600

    Create test for find_by_id

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe TransactionRepository do
 
     expect(tr.transactions).to eq([])
   end
+
+  it 'can find by ID, otherwise return nil' do
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv",
+      :transactions => "./data/transactions.csv"
+      })
+
+    expect(se.transactions.find_by_id(1).id).to eq(se.transactions.all[0].id)
+  end
 end
