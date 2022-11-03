89723dbc432a7a4e58e9417ac3ae3d9723c780f7
Joe King <j.king92@gmail.com>
2022-11-03 14:00:03 -0700

    feat: Correct #average_items_per_merchant_standard_deviation

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,26 +1,26 @@
-class SalesAnalyst 
+class SalesAnalyst
   attr_reader :items,
               :merchants
 
 
-  def initialize(items, merchants) 
+  def initialize(items, merchants)
     @items = items
     @merchants = merchants
   end
 
-  def average_items_per_merchant 
+  def average_items_per_merchant
     (@items.all.size.to_f / @merchants.all.size).round(2)
   end
 
   def average_items_per_merchant_standard_deviation
-    mean = average_items_per_merchant
+    mean = (@items.all.size.to_f / @merchants.all.size)
     sum = array_of_items_per_merchant.sum(0.0) { |element| (element - mean) ** 2 }
-    variance = sum / (@items.all.size)
+    variance = sum / (@merchants.all.size - 1)
     return Math.sqrt(variance).round(2)
   end
 
-  def array_of_items_per_merchant 
-    @merchants.map do |merchant|
+  def array_of_items_per_merchant
+    @merchants.all.map do |merchant|
       @items.find_all_by_merchant_id(merchant.id).size
     end
   end
--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -5,36 +5,36 @@ require_relative '../lib/merchant_repository'
 require_relative '../lib/sales_engine'
 require_relative '../lib/sales_analyst'
 
-RSpec.describe SalesAnalyst do 
+RSpec.describe SalesAnalyst do
   let(:se) {SalesEngine.from_csv({
     :items     => "./data/items.csv",
     :merchants => "./data/merchants.csv",
     })}
 
-  it 'exists' do 
-    sales_analyst = SalesAnalyst.new(ItemRepository.new, MerchantRepository.new) 
+  it 'exists' do
+    sales_analyst = SalesAnalyst.new(ItemRepository.new, MerchantRepository.new)
 
     expect(sales_analyst).to be_a(SalesAnalyst)
   end
 
-  describe '#analyst' do 
-    it 'creates an instance of SalesAnalyst' do 
+  describe '#analyst' do
+    it 'creates an instance of SalesAnalyst' do
       expect(se.analyst).to be_a(SalesAnalyst)
     end
   end
 
-  describe '#average_items_per_merchant' do 
-    it 'returns the an average amount of items a merchant sells' do 
+  describe '#average_items_per_merchant' do
+    it 'returns the an average amount of items a merchant sells' do
       sales_analyst = se.analyst
 
       expect(sales_analyst.average_items_per_merchant).to eq(2.88)
     end
   end
 
-  describe '#average_items_per_merchant_standard_deviation' do 
-    it 'returns the standard deviation of the average items per merchant' do 
+  describe '#average_items_per_merchant_standard_deviation' do
+    it 'returns the standard deviation of the average items per merchant' do
       sales_analyst = se.analyst
-
+      # require "pry"; binding.pry
       expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
     end
   end
