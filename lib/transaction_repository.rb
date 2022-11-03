45425630daeeb146e002b6c433341d6fa737199e
Alastair Lloyd <dex8056@aol.com>
2022-11-03 15:01:33 -0600

    test: Add tests

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -9,18 +9,18 @@ class TransactionRepository
     @all = all
   end
 
-  def find_all_by_invoice_id
+  def find_all_by_invoice_id(invoice_id)
   end
 
-  def find_all_by_credit_card_number
+  def find_all_by_credit_card_number(card_num)
   end
 
-  def find_all_by_result
+  def find_all_by_result(result)
   end
 
-  def create
+  def create(attributes)
   end
 
-  def update
+  def update(attributes)
   end
 end
--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -18,15 +18,30 @@ RSpec.describe TransactionRepository do
   end
 
   it "#find_all_by_invoice_id returns all transactions matching the given id" do
+    expect(transaction_repository.find_all_by_invoice_id(5)).to eq nil
+    expect(transaction_repository.find_all_by_invoice_id(8)).to eq (transaction)
   end
 
   it "#find_all_by_credit_card_number returns all transactions matching given credit card number" do
+    expect(transaction_repository.find_all_by_credit_card_number("4242424242423333")).to eq nil
+    expect(transaction_repository.find_all_by_credit_card_number("4242424242424242")).to eq (transaction)
   end
 
   it "#find_all_by_result returns all transactions matching given result" do
+    expect(transaction_repository.find_all_by_result("failed")).to eq nil
+    expect(transaction_repository.find_all_by_result("success")).to eq (transaction)
   end
 
   it "#create creates a new transaction instance" do
+    expect(transaction_repository.create({Transaction.new({
+      :id => 6, 
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date  => "0220",
+      :result => "success",
+      :created_at => Time.now,
+      :updated_at => Time.now
+    })})).to eq (transaction)
   end
 
   it "#update updates a transaction" do
