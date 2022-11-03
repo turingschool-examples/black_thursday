0f89356e3fef7b4828f7f5d8faa3f82b9a0a8707
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-03 16:30:51 -0600

    feat: Add find by id method and tests

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,9 +1,23 @@
 require_relative 'customer'
+require_relative 'modify'
+require_relative 'find'
 
 class CustomerRepository
-  attr_reader :customers
+include Find
+include Modify
 
+  attr_reader :customers
   def initialize
     @customers = []
+  end
+
+  def find_by_id(cust_id)
+    @customers.find do |customer|
+      customer.id == cust_id
+    end
+  end
+
+  def add(customer)
+    @customers << Customer.new(customer)
   end
 end
--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -1,8 +1,31 @@
 require_relative '../lib/customer_repository'
+require './lib/sales_engine'
 
 RSpec.describe CustomerRepository do
-  it 'exists' do
+  it 'exists and has no customers by default' do
     cust = CustomerRepository.new
+
     expect(cust).to be_a(CustomerRepository)
+    expect(cust.customers).to eq([])
   end
+
+  it 'can find customers by id' do
+    se = SalesEngine.from_csv({:customers => "./data/customers.csv"})
+
+    expect(se.customers.find_by_id(6)).to be_a (Customer)
+  end
+
+  # all - returns an array of all known Customer instances
+  xit 'returns an array of all known Customer instances' do
+    se = SalesEngine.from_csv({:customers => "./data/customers.csv"})
+   
+    expect(se.customers.all).to eq (Array)
+  end
+
+  # find_by_id - returns either nil or an instance of Customer with a matching ID
+  # find_all_by_first_name - returns either [] or one or more matches which have a first name matching the substring fragment supplied
+  # find_all_by_last_name - returns either [] or one or more matches which have a last name matching the substring fragment supplied
+  # create(attributes) - create a new Customer instance with the provided attributes. The new Customer’s id should be the current highest Customer id plus 1.
+  # update(id, attribute) - update the Customer instance with the corresponding id with the provided attributes. Only the customer’s first_name and last_name can be updated. This method will also change the customer’s updated_at attribute to the current time.
+  #delete(id) - delete the Customer instance with the corresponding id
 end
