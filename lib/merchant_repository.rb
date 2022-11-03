34ec4a9e2c0693c2deabf1f19b390e6f2b5e24a5
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-11-03 10:51:41 -0700

    Refactor tests, fix merge conflict issue??

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -3,4 +3,42 @@ class MerchantRepository
   def initialize
     @merchants = []
   end
+
+def create(attributes)
+  if merchants.last.nil? == false
+    attributes[:id] = (@merchants.last.id + 1)
+  end
+  new_merchant = Merchant.new(attributes)
+  @merchants << new_merchant
+  new_merchant
+end
+
+  def all
+    @merchants
+  end
+
+  def find_by_id(id)
+    merchants.find {|merchant| merchant.id == id}
+   end
+
+  def find_by_name(name)
+    merchants.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
+  def find_all_by_name(name)
+    merchants.select do |merchant|
+      merchant.name.downcase.include?(name.downcase)
+    end
+  end
+
+  def delete(id)
+    merchants.delete_if{|merchant| merchant.id. == id }
+  end
+
+  def update(id, attributes)
+    updated_merchant = find_by_id(id)
+    updated_merchant.name = attributes
+  end
 end
