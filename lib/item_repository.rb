e8b42bcfaaeb46c8b6f42c3cad275e9ead188fe7
Max MacGillivray <mary.macgillivray@gmail.com>
2022-11-03 10:47:49 -0700

    WIP fixing item_repo to work with spec_harness

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -15,9 +15,9 @@ class ItemRepository
     @items
   end
 
-  # def find_by_id(id)
-  #   @items.find {|item| item.id == id}
-  # end
+  def find_by_id(id)
+    @items.find {|item| item.id == id}
+  end
 
   def find_by_name(name)
     @items.find {|item| item.name.upcase == name.upcase}
@@ -28,7 +28,8 @@ class ItemRepository
   end
 
   def find_all_by_price(price)
-    @items.find_all {|item| item.unit_price_to_dollars == price}
+    @items.find_all {|item| item.unit_price == price}
+    # require 'pry'; binding.pry
   end
 
   def find_all_by_price_in_range(range)
@@ -44,19 +45,24 @@ class ItemRepository
   end
 
   def create(attributes)
-    ids = @items.map { |item| item.id}
-    attributes[:id] = ids.max + 1
+    attributes[:id] = all_ids.max + 1
     new_item = Item.new(attributes)
     @items.push(new_item)
     new_item
   end
+  
+  def all_ids
+    @items.map { |item| item.id}
+  end
 
   def update(id, attributes)
-    updated_item = find_by_id(attributes[:id])
-    updated_item.update_name(attributes[:name])
-    updated_item.update_description(attributes[:description])
-    updated_item.update_unit_price(attributes[:unit_price])
-    updated_item
+    if all_ids.include?(id)
+      updated_item = find_by_id(attributes[:id])
+      updated_item.update_name(attributes[:name])
+      updated_item.update_description(attributes[:description])
+      updated_item.update_unit_price(attributes[:unit_price])
+      updated_item
+    end
   end
 
   def delete(id)
