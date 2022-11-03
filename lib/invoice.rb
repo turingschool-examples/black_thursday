9e62ec652ba17c6d1197bd1e45158e15ee804b45
Brady Rohrig <brady.rohrig@gmail.com>
2022-11-02 20:03:34 -0700

    worked on passing tests for iteration 2

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -2,15 +2,15 @@ class Invoice
   attr_reader :id,
               :customer_id,
               :merchant_id,
-              :status,
-              :created_at,
-              :updated_at
+              :created_at
+  attr_accessor :status,
+                :updated_at
 
   def initialize(input_hash)
-    @id           = input_hash[:id]
-    @customer_id  = input_hash[:customer_id]
-    @merchant_id  = input_hash[:merchant_id]
-    @status       = input_hash[:status]
+    @id           = input_hash[:id].to_i
+    @customer_id  = input_hash[:customer_id].to_i
+    @merchant_id  = input_hash[:merchant_id].to_i
+    @status       = input_hash[:status].to_sym
     @created_at   = Time.parse(input_hash[:created_at].to_s)
     @updated_at   = Time.parse(input_hash[:updated_at].to_s)
   end
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -17,15 +17,44 @@ class InvoiceRepository
   end
   
   def find_all_by_merchant_id(merchant_id)
-
     @all.find_all do |invoice|
-      merchant_id.to_s == invoice.merchant_id
+      merchant_id == invoice.merchant_id
     end
   end
 
   def find_all_by_status(invoice_status)
     @all.find_all do |invoice|
       invoice.status == invoice_status
+    end
+  end
+
+  def create(attributes)
+    all << Invoice.new({ :id            => next_id,
+                          :customer_id  => attributes[:customer_id],
+                          :merchant_id  => attributes[:merchant_id],
+                          :created_at   => attributes[:created_at],
+                          :updated_at   => attributes[:updated_at],
+                          :status       => attributes[:status]
+                      })    
+  end
+ 
+  def update(id,attributes)
+    if find_by_id(id) == nil 
+      return
+    else
+    find_by_id(id).updated_at = Time.now
+    attributes.each do |att,val|
+      case att
+      when :name
+        find_by_id(id).name = val
+      when :description
+        find_by_id(id).description = val
+      when :unit_price
+        find_by_id(id).unit_price = val
+      when :status
+        find_by_id(id).status = val
+      end
+    end
     end
   end
 
