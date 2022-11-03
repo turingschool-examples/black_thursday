9898a30b38428cca328b33ef9114248b2925f291
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-03 11:15:44 -0600

    Add credit_card_number attribute, pass respective test

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,11 @@
 class Transaction
-  attr_accessor :id, :invoice_id
+  attr_accessor :id,
+                :invoice_id,
+                :credit_card_number
 
   def initialize(data)
     @id = data[:id].to_i
     @invoice_id = data[:invoice_id].to_i
+    @credit_card_number = data[:credit_card_number]
   end
 end
