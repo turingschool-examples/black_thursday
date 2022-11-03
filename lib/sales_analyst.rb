5713a381054b694a1e4ae08183a4c2d8a8056465
Ryan Canton <ryancanton999@gmail.com>
2022-11-03 09:55:00 -0600

    Adding require_relative statements for SalesEngine and SalesAnalyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -5,31 +5,34 @@ require_relative 'item'
 require_relative 'merchant'
 require_relative 'item_repository'
 require_relative 'merchant_repository'
+require_relative 'invoice'
+require_relative 'invoice_repository'
 
 class SalesAnalyst
   attr_reader :items,
               :merchants
-  def initialize(item_repo, merchant_repo)
+  def initialize(item_repo, merchant_repo, invoice_repo)
     @items = item_repo
     @merchants = merchant_repo
+    @invoices = invoice_repo
   end
-  
+
   def item_count
     items.all.count
   end
-  
+
   def items_per_merchant(id)
     items.find_all_by_merchant_id(id).count
   end
-  
+
   def merchant_count
     merchants.all.count
   end
-  
+
   def average_items_per_merchant
     (item_count / merchant_count).to_f.round(2)
   end
-  
+
   def average_items_per_merchant_standard_deviation
     # binding.pry
     merchants = @merchants.all.map do |merchant|
@@ -42,21 +45,21 @@ class SalesAnalyst
     end
     Math.sqrt(differences / (merchants.count - 1).to_f).round(2)
   end
-  # 
+  #
   # def merchants_with_high_item_count
-  # 
+  #
   # end
-  # 
+  #
   # def average_average_price_for_merchant(merchant)
-  # 
+  #
   # end
-  # 
+  #
   # def average_average_price_per_merchant
-  # 
+  #
   # end
-  # 
+  #
   # def golden_items
-  # 
+  #
   # end
-  
+
 end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,14 +3,25 @@ require_relative 'item'
 require_relative 'merchant'
 require_relative 'item_repository'
 require_relative 'merchant_repository'
+require_relative 'sales_analyst'
+require_relative 'invoice'
+require_relative 'invoice_repository'
 class SalesEngine
 
   attr_reader :items,
-              :merchants
+              :merchants,
+              :invoices,
+              :analyst
 
   def initialize(csv_hash)
     @items = create_item_repo(csv_hash[:items])
     @merchants = create_merchant_repo(csv_hash[:merchants])
+    if (csv_hash.has_key(:invoices))
+      @invoices = create_invoice_repo(csv_hash[:invoices])
+    else
+      @invoices = nil
+    end
+      @anaylyst = create_analyst(@items, @merchants, @invoices)
   end
 
   def self.from_csv(csv_hash)
@@ -40,6 +51,21 @@ class SalesEngine
                                    :name => row[:name]}))
     end
     merchant_repo = MerchantRepository.new(merchants)
+  end
+
+  def create_invoice_repo(invoice_csv)
+    items = []
+    contents = CSV.open invoice_csv, headers: true, header_converters: :symbol
+    contents.each do |row|
+      items.push(Item.new({:id => row[:id].to_i,
+                           :name => row[:name],
+                           :description => row[:description],
+                           :unit_price => row[:unit_price].to_d,
+                           :created_at => row[:created_at],
+                           :updated_at => row[:updated_at],
+                           :merchant_id => row[:merchant_id].to_i}))
+    end
+    item_repo = ItemRepository.new(items)
   end
 
 end
