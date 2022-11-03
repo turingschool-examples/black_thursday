9376e4249aa4d565ebb9991185901597c39aa7c4
Drew Layton <dlayton66@gmail.com>
2022-11-03 16:05:38 -0600

    refactor: Dry up SalesEngine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,31 +2,29 @@ require 'csv'
 require_relative './merchant_repository'
 require_relative './item_repository'
 require_relative './invoice_repository'
+# require_relative './customer_repository'
 require_relative './item'
 require_relative './merchant'
 require_relative './invoice'
+require_relative './customer'
 
 class SalesEngine
-  attr_reader :items, :merchants, :invoices
+  # repo_list = ["merchants","items","invoices","customers"]
+  # repo_list.each { |repo| attr_reader repo.to_sym }
+  attr_reader :items, :merchants, :invoices, :customers
 
   def initialize(data)
-    repo_creation(data[:merchants], :merchants)
-    repo_creation(data[:items], :items)
-    repo_creation(data[:invoices], :invoices)
+    repo_list.each do |repo|
+      repo_creation(data[repo.to_sym], repo.to_sym)
+    end
   end
 
   def self.from_csv(data)
     new(data)
   end
 
-  def repo_creation(input_data, input_type)
-    if input_type == :merchants 
-      @merchants = MerchantRepository.new(thing_creation(input_data, input_type))
-    elsif input_type == :items
-      @items = ItemRepository.new(thing_creation(input_data, input_type))
-    elsif input_type == :invoices
-      @invoices = InvoiceRepository.new(thing_creation(input_data, input_type))
-    end
+  def repo_creation(input_data, repo_type)
+    instance_variable_set("@#{repo_type.to_s}", repo_class(repo_type).new(thing_creation(input_data, repo_type)))
   end
 
   def thing_creation(second_data, second_type)
@@ -37,13 +35,20 @@ class SalesEngine
   end
 
   def thing_type(type, row)
-    if type == :merchants
-      Merchant.new(row)
-    elsif type == :items
-      Item.new(row)
-    elsif type == :invoices
-      Invoice.new(row)
-    end
+    repo_child_class(type.to_s).new(row)
+  end
+
+  def repo_list
+    ["merchants","items","invoices"]
+  end
+
+  def repo_child_class(repo)
+    Object.const_get(repo.capitalize.chomp('s'))
+  end
+
+  def repo_class(repo)
+    class_string = repo.to_s.capitalize.chomp('s') + 'Repository'
+    Object.const_get(class_string)
   end
 
   def analyst
