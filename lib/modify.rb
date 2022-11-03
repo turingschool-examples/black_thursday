fb78d5b66826860d53cb845b36c264c76937a286
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-03 17:11:06 -0600

    Update Modify module to accommodate TransactionRepository update method

--- lib/modify.rb
+++ lib/modify.rb
@@ -9,28 +9,34 @@ module Modify
 
   def update_overall(type, id, attributes)
     updated_type = find_by_id(id)
-    if type = @merchants
+    if type == @merchants
       if updated_type != nil
         updated_type.name = attributes[:name]
       end
-    elsif type = @items
+    elsif type == @items
       if updated_type != nil
         updated_type.name = attributes[:name] if attributes.keys.include?(:name)
         updated_type.description = attributes[:description] if attributes.keys.include?(:description)
         updated_type.unit_price = attributes[:unit_price].to_d if attributes.keys.include?(:unit_price)
         updated_type.updated_at = Time.now
       end
-    elsif type = @invoices
+    elsif type == @invoices
       if updated_type != nil
         updated_type.status = attributes[:status].to_sym if attributes.keys.include?(:status)
         updated_type.updated_at = Time.now
       end
-    elsif type = @invoice_items
+    elsif type == @invoice_items
       if updated_type != nil
         updated_type.quantity = attributes[:quantity].to_i if attributes.keys.include?(:quantity)
         updated_type.unit_price = attributes[:unit_price].to_d if attributes.keys.include?(:unit_price)
         updated_type.updated_at = Time.now
       end
+    elsif type == @transactions
+      if updated_type != nil
+        updated_type.credit_card_number = attributes[:credit_card_number] if attributes.keys.include?(:credit_card_number)
+        updated_type.credit_card_expiration_date = attributes[:credit_card_expiration_date] if attributes.keys.include?(:credit_card_expiration_date)
+        updated_type.result = attributes[:result] if attributes.keys.include?(:result)
+        updated_type.updated_at = Time.now
     end
   end
 
