49ab3d724afe0b959a013b6431156481a3d414f5
J Seymour <JustJakeSeymour@gmail.com>
2022-11-02 18:52:54 -0600

    unsure of merge conflict, committing again

---
+++ lib/invoice_repository.rb
@@ -0,0 +1,72 @@
+class InvoiceRepository
+  def initialize(invoices)
+    @invoices = invoices
+  end
+
+  def all
+    @invoices
+  end
+
+  def find_by_id(id)
+    if !a_valid_id?()
+      return nil
+    else
+      @invoices.find do |invoice|
+        invoice.id == id
+      end
+    end
+  end
+
+  def find_all_by_customer_id(id)
+    if !a_valid_id?()
+      return nil
+    else
+      @invoices.find do |invoice|
+        invoice.id == id
+      end
+    end
+  end
+
+  def a_valid_id?(id)
+    @merchants.any? do |merchant| merchant.id == id
+    end 
+  end
+
+  def find_all_by_merchant_id(id)
+    if !a_valid_id?()
+      return nil
+    else
+      @invoices.find do |invoice|
+        invoice.id == id
+      end
+    end
+  end
+
+  def find_all_by_status(id)
+    if !a_valid_id?()
+      return nil
+    else
+      @invoices.find do |invoice|
+        invoice.id == id
+      end
+    end
+  end
+
+  def create(attribute)
+    new_invoice = @invoices.last.id + 1
+    @invoices << Invoice.new({:id => new_id, :name => attribute})
+  end
+
+  def update(id, attribute)
+    @invoices.each do |invoice|
+      if invoice.id == id
+        invoice_new_status = invoice.name.replace(attribute)
+        return invoice_new_status
+      end
+    end
+  end
+
+  def delete(id)
+    @invoices.delete(find_by_id(id))
+  end
+end
---
+++ spec/invoice_spec.rb
@@ -0,0 +1,89 @@
+require './lib/invoice_repository'
+require './lib/invoice'
+
+RSpec.describe Invoice do
+  let!(:invoice) {Invoice.new()}
+
+  it 'is an invoice class' do
+    expect(invoice).to be_a(Invoice)
+  end
+
+  it 'returns all the invoice instances' do
+    expect(invoice.all).to eq([])
+  end
+
+  it 'can return an invoice by id' do
+    expect(invoice.find_by_id()).to eq()
+  end
+
+  it 'returns nil if it is not within' do
+    expect(invoice.find_by_id()).to eq(nil)
+    expect(invoice.find_by_id()).to eq(nil)
+  end
+
+  it 'can return the customer id' do
+    expect(invoice.find_all_by_customer_id()).to eq()
+  end
+
+  it 'returns nil if it is not within' do
+    expect(invoice.find_all_by_customer_id()).to eq(nil)
+    expect(invoice.find_all_by_customer_id()).to eq(nil)
+  end
+
+  it 'can return the merchants id' do
+    expect(invoice.find_all_by_merchant_id()).to eq()
+  end
+
+  it 'returns nil if it is not within' do
+    expect(invoice.find_all_by_merchant_id()).to eq(nil)
+    expect(invoice.find_all_by_merchant_id()).to eq(nil)
+  end
+
+  it 'can return the status' do
+    expect(invoice.status).to eq()
+  end
+
+  it 'returns nil if it is not within' do
+    expect(invoice.status).to eq(nil)
+    expect(invoice.status).to eq(nil)
+  end
+
+  it 'can return the time instance when item was first created' do
+    expect(invoice.created_at).to eq()
+  end
+
+  it 'returns nil if it is not within' do
+    expect(invoice.created_at).to eq(nil)
+    expect(invoice.created_at).to eq(nil)
+  end
+
+  it 'can return the time for when the item was last modified' do
+    expect(invoice.updated_at).to eq()
+  end
+
+  it 'returns nil if it is not within' do
+    expect(invoice.updated_at).to eq(nil)
+    expect(invoice.updated_at).to eq(nil)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# id - returns the integer id
+# customer_id - returns the customer id
+# merchant_id - returns the merchant id
+# status - returns the status
+# created_at - returns a Time instance for the date the item was first created
+# updated_at - returns a Time instance for the date the item was last modified
