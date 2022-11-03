0805e6e4a60f0a5fd65af72fc82f580749a3b7bb
J Seymour <JustJakeSeymour@gmail.com>
2022-11-03 15:33:39 -0600

    fix item repository tests to use real csv examples

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -61,35 +61,35 @@ RSpec.describe ItemRepository do
   end
 
   it 'can find item by name' do
-    expect(item_repository.find_by_name("Pencil Crayons")).to eq(item_3)
-    expect(item_repository.find_by_name("Pencil CrAyOns")).to eq(item_3)
+    expect(item_repository.find_by_name("Wooden pen and stand")).to eq(item_repository.all[14])
+    expect(item_repository.find_by_name("WoOdeN pEn aNd sTanD")).to eq(item_repository.all[14])
     expect(item_repository.find_by_name("Textbook")).to eq(nil)
   end
 
   it 'can find all items with included description' do
-    expect(item_repository.find_all_with_description("You can use it to write things")).to eq([item_1])
-    expect(item_repository.find_all_with_description("You can USE it to WrITE things")).to eq([item_1])
+    expect(item_repository.find_all_with_description("Acrylique sur toile, mesurant environs 80/50cm.")).to eq([item_repository.all[30]])
+    expect(item_repository.find_all_with_description("Acrylique sur toile, mesurant environs 80/50cm.")).to eq([item_repository.all[30]])
     expect(item_repository.find_all_with_description("You can erase things")).to eq([])
   end
 
   it 'can find all items by a given price' do
-    expect(item_repository.find_all_by_price(8.99)).to eq([item_2])
+    expect(item_repository.find_all_by_price(199)).to eq([item_repository.all[94]])
     expect(item_repository.find_all_by_price(5.00)).to eq([])
   end
 
   it 'can find all items by price, within a range' do
-    expect(item_repository.find_all_by_price_in_range(8.00, 11.00)).to eq([item_1, item_2])
-    expect(item_repository.find_all_by_price_in_range(8, 11)).to eq([item_1, item_2])
+    expect(item_repository.find_all_by_price_in_range(593, 597)).to eq([item_repository.all[543], item_repository.all[1271]])
     expect(item_repository.find_all_by_price_in_range(1.00, 3.00)).to eq([])
   end
 
   it 'can find all items that are a merchants (id)' do
-    expect(item_repository.find_all_by_merchant_id(2)).to eq([item_1, item_2, item_3])
+    expect(item_repository.find_all_by_merchant_id("12334105")).to eq([item_repository.all[4], item_repository.all[665], item_repository.all[671]])
     expect(item_repository.find_all_by_merchant_id(5)).to eq([])
   end
 
-  it 'can create a new item by supplying the attributes' do
-    expect(item_repository.all.last.name).to eq("Pencil crayons")
+  it 'can create a new item by supplying the attributes and it has the highest id + 1' do
+    expect(item_repository.all.last.name).to eq("Minty Green Knit Crochet Infinity Scarf")
+    expect(item_repository.all.last.id).to eq(263567474)
 
     item_repository.create(
       {
@@ -97,53 +97,35 @@ RSpec.describe ItemRepository do
       :name => "Stapler", 
       :description => "You can use this to staple things", 
       :unit_price => 7.00,
-      :created_at => time_now,
-      :updated_at => time_now,
+      :created_at => Time.now,
+      :updated_at => Time.now,
       :merchant_id => 2
      }
     )
     expect(item_repository.all.last.name).to eq("Stapler") 
-  end
-
-  it 'gives the new item the highest item id + 1' do
-    expect(item_repository.all.last.id).to eq(3)
-
-    item_repository.create(
-      {
-      :id => 0,
-      :name => "Stapler", 
-      :description => "You can use this to staple things", 
-      :unit_price => 7.00,
-      :created_at => time_now,
-      :updated_at => time_now,
-      :merchant_id => 2
-     }
-    )
-
-    expect(item_repository.all.last.id).to eq(4)
+    expect(item_repository.all.last.id).to eq(263567475)
   end
 
   it 'can update an item by the id, giving new attributes' do
-    item_repository.update(1, {
+    expect(item_repository.all[10].updated_at).to eq("1988-09-01 17:09:25 UTC")
+    item_repository.update(263397163, {
       :name => "Mechanical Pencil",
       :description => "You can use it to write things down",
       :unit_price => 9.99
       })
 
-    expect(item_1.name).to eq("Mechanical Pencil")
-    expect(item_1.description).to eq("You can use it to write things down")
-    expect(item_1.unit_price).to eq(9.99)
-    expect(item_1.created_at).to eq(time_now)
-    expect(item_1.updated_at).to eq(time_now)
+    expect(item_repository.all[10].name).to eq("Mechanical Pencil")
+    expect(item_repository.all[10].description).to eq("You can use it to write things down")
+    expect(item_repository.all[10].unit_price).to eq(9.99)
+    expect(item_repository.all[10].created_at).to eq("2016-01-11 10:51:02 UTC")
+    expect(item_repository.all[10].updated_at).not_to eq("1988-09-01 17:09:25 UTC")
   end
 
   it 'can delete an item by supplied id' do
-    expect(item_repository.all.length).to eq(3)
-    expect(item_repository.all).to eq([item_1, item_2, item_3])
+    expect(item_repository.all.length).to eq(1367)
 
-    item_repository.delete(1)
+    item_repository.delete(263397163)
  
-    expect(item_repository.all.length).to eq(2)
-    expect(item_repository.all).to eq([item_2, item_3])
+    expect(item_repository.all.length).to eq(1366)
   end
 end
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -18,42 +18,6 @@ RSpec.describe SalesEngine do
     expect(sales_engine.merchants).to be_a(MerchantRepository)
   end
 
-  xit 'can perform ItemRepository methods' do
-    ir = sales_engine.items
-    # require 'pry'; binding.pry
-    expect(ir.find_by_name("Wooden pen and stand")).to eq(ir.all[14])
-    expect(ir.find_all_with_description("Acrylique sur toile, mesurant environs 80/50cm.")).to eq([ir.all[30]])
-    expect(ir.find_all_by_price(199)).to eq([ir.all[94]])
-    expect(ir.find_all_by_price_in_range(593, 597)).to eq([ir.all[543], ir.all[1271]])
-    expect(ir.find_all_by_merchant_id("12334105")).to eq([ir.all[4], ir.all[665], ir.all[671]])
-  end
-
-  xit 'can create a new item, and the id is the largest id + 1' do
-    ir = sales_engine.items
-
-    expect(ir.all.last.name).to eq("Minty Green Knit Crochet Infinity Scarf")
-    expect(ir.all.last.id).to eq(263567474)
-
-    ir.create(
-          {
-          :id => 0,
-          :name => "Stapler", 
-          :description => "You can use this to staple things", 
-          :unit_price => 7.00,
-          :created_at => Time.now,
-          :updated_at => Time.now,
-          :merchant_id => 2
-        }
-        )
-
-    expect(ir.all.last.name).to eq("Stapler")
-    expect(ir.all.last.id).to eq(263567475)
-  end
-
-  xit 'can update an item with attributes, and updated_at reflects time changed' do
-
-  end
-
   xit 'can perform MerchantRepository methods' do
     mr = sales_engine.merchants
     # require 'pry'; binding.pry
