e80a4237bc02bbec8b31d8d55fa40bad02d3710c
Lacey Weaver <jlweave89@gmail.com>
2022-11-03 08:59:33 -0600

    Add instance in transaction spec

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,7 +2,15 @@ require './lib/transaction'
 require './lib/transaction_repository'
 
 RSpec.describe Transaction do
-  let!(:transaction){Transaction.new()}
+  let!(:transaction){Transaction.new({
+  :id => 6,
+  :invoice_id => 8,
+  :credit_card_number => "4242424242424242",
+  :credit_card_expiration_date => "0220",
+  :result => "success",
+  :created_at => Time.now,
+  :updated_at => Time.now
+})}
 
   it 'can return an id' do
     expect(transaction.id).to eq()
