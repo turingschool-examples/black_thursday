6ff6589a6b595804fd4d5749d28c7269efec3620
Alastair Lloyd <dex8056@aol.com>
2022-11-03 16:20:47 -0600

    test/feat: Add and pass tests

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -12,22 +12,33 @@ class TransactionRepository
   end
 
   def find_all_by_invoice_id(invoice_id)
-    all.find_all do |invoice_item|
-      invoice_id.to_i == invoice_item.invoice_id.to_i
+    all.find_all do |transaction|
+      invoice_id.to_i == transaction.invoice_id.to_i
     end
   end
   #this is a repeated test, maybe reposable?
 
   def find_all_by_credit_card_number(card_num)
-    all.find_all do |transaction_item|
-      card_num == transaction_item.card_num
+    all.find_all do |transaction|
+      card_num == transaction.credit_card_number
     end
   end
 
   def find_all_by_result(result)
+    all.find_all do |transaction|
+      result == transaction.result
+    end
   end
 
   def create(attributes)
+    all << Transaction.new({ :id                           => attributes[:id],
+                             :invoice_id                   => attributes[:invoice_id],
+                             :credit_card_number           => attributes[:credit_card_number],
+                             :credit_card_expiration_date  => attributes[:credit_card_expiration_date],
+                             :result                       => attributes[:result],
+                             :created_at                   => attributes[:created_at],
+                             :updated_at                   => attributes[:updated_at]
+                           })
   end
 
   def update(attributes)
--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -18,22 +18,28 @@ RSpec.describe TransactionRepository do
   end
 
   it "#find_all_by_invoice_id returns all transactions matching the given id" do
-    expect(transaction_repository.find_all_by_invoice_id(5)).to eq nil
-    expect(transaction_repository.find_all_by_invoice_id(8)).to eq (transaction)
+    transaction_repository.all << transaction
+
+    expect(transaction_repository.find_all_by_invoice_id(5)).to eq []
+    expect(transaction_repository.find_all_by_invoice_id(8)).to eq [transaction]
   end
 
   it "#find_all_by_credit_card_number returns all transactions matching given credit card number" do
-    expect(transaction_repository.find_all_by_credit_card_number("4242424242423333")).to eq nil
-    expect(transaction_repository.find_all_by_credit_card_number("4242424242424242")).to eq (transaction)
+    transaction_repository.all << transaction
+
+    expect(transaction_repository.find_all_by_credit_card_number("4242424242423333")).to eq []
+    expect(transaction_repository.find_all_by_credit_card_number("4242424242424242")).to eq [transaction]
   end
 
   it "#find_all_by_result returns all transactions matching given result" do
-    expect(transaction_repository.find_all_by_result("failed")).to eq nil
-    expect(transaction_repository.find_all_by_result("success")).to eq (transaction)
+    transaction_repository.all << transaction
+
+    expect(transaction_repository.find_all_by_result("failed")).to eq []
+    expect(transaction_repository.find_all_by_result("success")).to eq [transaction]
   end
 
   it "#create creates a new transaction instance" do
-    expect(transaction_repository.create({Transaction.new({
+    attributes = {
       :id => 6, 
       :invoice_id => 8,
       :credit_card_number => "4242424242424242",
@@ -41,15 +47,16 @@ RSpec.describe TransactionRepository do
       :result => "success",
       :created_at => Time.now,
       :updated_at => Time.now
-    })})).to eq (transaction)
+      }
+    expect(transaction_repository.create(attributes)).to be_a {Transaction}
   end
 
-  it "#update updates a transaction" do
+  xit "#update updates a transaction" do
   end
 
-  it "#update cannot update id, invoice_id, or created_at" do
+  xit "#update cannot update id, invoice_id, or created_at" do
   end
 
-  it "#update on unknown transaction does nothing" do
+  xit "#update on unknown transaction does nothing" do
   end
 end
