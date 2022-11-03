37a7477d217e163af01da433b10a1226a0e31b99
Christian McCabe <christian.mccabe89@gmail.com>
2022-11-03 15:50:30 -0600

    Refactor tests - only include transactions csv

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -14,14 +14,15 @@ RSpec.describe TransactionRepository do
     expect(tr.transactions).to eq([])
   end
 
+  it 'returns all transactions' do
+    se = SalesEngine.from_csv({:transactions => "./data/transactions.csv"})
+    
+    expect(se.transactions.all.first).to be_a(Transaction)
+    expect(se.transactions.all).to be_a(Array)
+  end
+
   it 'can find by ID, otherwise return nil' do
-    se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv",
-      :invoice_items => "./data/invoice_items.csv",
-      :transactions => "./data/transactions.csv"
-      })
+    se = SalesEngine.from_csv({:transactions => "./data/transactions.csv"})
 
     expect(se.transactions.find_by_id(1).id).to eq(se.transactions.all[0].id)
   end
