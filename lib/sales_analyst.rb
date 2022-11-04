c84228b665ab5cffb16951d68299feedb4621385
KaraJH <karas.jones@yahoo.com>
2022-11-04 17:59:15 -0600

    Put standard deviation into a variable to run the test quicker

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -4,7 +4,6 @@ class SalesAnalyst
     @engine = engine
   end
   def average_items_per_merchant
-    # require 'pry' ; binding.pry
     (item_amount.sum / item_amount.length.to_f).round(2)
   end
 
@@ -33,10 +32,11 @@ class SalesAnalyst
   end
 
   def merchants_with_high_item_count
+    std_dev = average_items_per_merchant_standard_deviation
+    #require 'pry' ;binding.pry
     @engine.merchants.all.find_all do |merchant|
       merchant.items.length >
-      (average_items_per_merchant +
-       average_items_per_merchant_standard_deviation)
+      (average_items_per_merchant + std_dev)
     end
   end
 
@@ -45,8 +45,5 @@ class SalesAnalyst
       item.unit_price
     end
     (prices.sum / prices.length).round(2)
-
   end
-
-
 end
