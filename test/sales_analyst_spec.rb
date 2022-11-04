7e4fe38db8ca498a19d4b0b543389bc8a6e4697d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-03 21:22:47 -0700

    Test: golden items

--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -9,8 +9,7 @@ RSpec.describe SalesAnalyst do
                            items: './data/items.csv',
                            merchants: './data/merchants.csv'
                          })
-                        #  let(:sales_analyst) { sales_engine.analyst }
-                         
+    #  let(:sales_analyst) { sales_engine.analyst }
   end
 
   it 'exists' do
@@ -28,21 +27,27 @@ RSpec.describe SalesAnalyst do
     expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
   end
 
-  it "returns merchants with high item counts" do
+  it 'returns merchants with high item counts' do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst.merchants_with_high_item_count).to be_a(Array)
     expect(sales_analyst.merchants_with_high_item_count[0]).to be_a(Merchant)
-
   end
 
   it 'checks average item price for merchant)' do
     sales_analyst = sales_engine.analyst
-    expect(sales_analyst.average_item_price_for_merchant(12334159)).to be_a(BigDecimal)
-
+    expect(sales_analyst.average_item_price_for_merchant(12_334_159)).to be_a(BigDecimal)
   end
 
   it '#average average price per merchant' do
+    sales_analyst = sales_engine.analyst
+    expect(sales_analyst.average_average_price_per_merchant).to be_a(BigDecimal)
+  end
+
+  it '#golden items' do
   sales_analyst = sales_engine.analyst
-  expect(sales_analyst.average_average_price_per_merchant). to be_a(BigDecimal)
+  expect(sales_analyst.golden_items).to be_a(Array)
+  expect(sales_analyst.golden_items[0]).to be_a(Item)
+
+
 end
 end
