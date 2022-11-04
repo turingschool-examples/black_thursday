36097e4296720b3cbccd15741df364eec453e113
Thomas Casady <tscasady@gmail.com>
2022-11-03 20:41:37 -0500

    refactor: Change avrg items per merchant to be calculated by merchant repo.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,29 +2,31 @@
 
 # Sales Analyst performs various data operations.
 class SalesAnalyst
-  def initialize()end
-
-  def average_items_per_merchant(item_repo, merch_repo)
-    item_repo.all.length / merch_repo.all.length
+  def initialize(engine)
+    @engine = engine
   end
 
-  def average_items_per_merchant_standard_deviation(item_repo, merch_repo)
-    number_of_items_per_merchant = merch_repo.all.map do |merchant|
-      find_all_by_merchant_id(merchant.id).length
-    end
-    average = average_items_per_merchant(item_repo, merch_repo)
-    number_of_items_per_merchant.map { |number| (number - average)**2 }.sum / merch_repo.all.length
+  def average_items_per_merchant
+    @engine.average_items_per_merchant
   end
 
-  def average_item_price_for_merchant(item_repo, merchant_id)
-    all_items = item_repo.find_all_by_merchant_id(merchant_id)
-    all_items.sum.fdiv(all_items.length)
-  end
+  # def average_items_per_merchant_standard_deviation(item_repo, merch_repo)
+  #   number_of_items_per_merchant = merch_repo.all.map do |merchant|
+  #     find_all_by_merchant_id(merchant.id).length
+  #   end
+  #   average = average_items_per_merchant(item_repo, merch_repo)
+  #   number_of_items_per_merchant.map { |number| (number - average)**2 }.sum / merch_repo.all.length
+  # end
 
-  def average_average_price_per_merchant
-    mr.all.map do |merchant|
-      average_item_price_for_merchant(merchant.id)
-    end.sum  / mr.all.length
-  end
+  # def average_item_price_for_merchant(item_repo, merchant_id)
+  #   all_items = item_repo.find_all_by_merchant_id(merchant_id)
+  #   all_items.sum.fdiv(all_items.length)
+  # end
+
+  # def average_average_price_per_merchant
+  #   mr.all.map do |merchant|
+  #     average_item_price_for_merchant(merchant.id)
+  #   end.sum  / mr.all.length
+  # end
 
 end
