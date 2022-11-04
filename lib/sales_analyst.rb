06bfb507291e9848ba4f4dc78c82bef8f4b9eb06
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-03 18:57:26 -0700

    Test: average items per merchant standard deviation

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,21 +1,21 @@
 require_relative 'sales_engine'
 
 class SalesAnalyst
-    attr_reader :sales_engine
+  attr_reader :sales_engine
 
-    def initialize(sales_engine)
-        @sales_engine = sales_engine
-    end
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+  end
 
-    def average_items_per_merchant
-        (sales_engine.items.all.count / sales_engine.merchants.all.count.to_f).round(2)
-        # find_all_by_merchant_id(merchant_id)
-        # require 'pry'; binding.pry
-        # test = sales_engine.items.all.map do |item|
-        #     sales_engine.items.find_all_by_merchant_id(item.merchant_id).length
-        # end
-        # require 'pry'; binding.pry  
-        
-        
-    end
+  def average_items_per_merchant
+    (sales_engine.items.all.count / sales_engine.merchants.all.count.to_f).round(2)
+    # find_all_by_merchant_id(merchant_id)
+    # require 'pry'; binding.pry
+    # test = sales_engine.items.all.map do |item|
+    #     sales_engine.items.find_all_by_merchant_id(item.merchant_id).length
+    # end
+    # require 'pry'; binding.pry
+  end
+
+  def average_items_per_merchant_standard_deviation; end
 end
--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -4,23 +4,25 @@ require './lib/sales_engine'
 require './lib/sales_analyst'
 
 RSpec.describe SalesAnalyst do
+  let(:sales_engine) do
+    SalesEngine.from_csv({
+                           items: './data/items.csv',
+                           merchants: './data/merchants.csv'
+                         })
+  end
+
   it 'exists' do
-    sales_engine = SalesEngine.from_csv({
-                                          items: './data/items.csv',
-                                          merchants: './data/merchants.csv'
-                                        })
-                                        # require 'pry'; binding.pry
     sales_analyst = sales_engine.analyst
     expect(sales_analyst).to be_instance_of(SalesAnalyst)
   end
 
-  it 'checks average items per merchnt' do
-    sales_engine = SalesEngine.from_csv({
-                                          items: './data/items.csv',
-                                          merchants: './data/merchants.csv'
-                                        })
+  it 'checks average items per merchant' do
     sales_analyst = sales_engine.analyst
-
     expect(sales_analyst.average_items_per_merchant).to eq(2.88)
+  end
+
+  it 'checks average items per merchant standard deviation' do
+    sales_analyst = sales_engine.analyst
+    expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
   end
 end
