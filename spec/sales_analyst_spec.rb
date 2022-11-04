df310d3a03111cae00ee2b4654410505718439bc
Thomas Casady <tscasady@gmail.com>
2022-11-03 20:40:20 -0500

    refactor: Change test setup to use real CSVs.

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -3,34 +3,38 @@
 require './lib/sales_analyst'
 require './lib/merchant_repository'
 require './lib/item_repository'
+require './lib/sales_engine'
 require 'bigdecimal'
 
 RSpec.describe SalesAnalyst do
-  let(:sa) { SalesAnalyst.new }
-
+  let(:data) { {
+      items:         './data/items.csv', 
+      merchants: './data/merchants.csv',
+      invoices: './data/invoices.csv',
+      invoice_items: './data/invoices.csv',
+      transactions: './data/transactions.csv',
+      customers:   './data/customers.csv',
+      } }
+  let(:engine) { SalesEngine.from_csv(data) }
+  let(:analyst) { SalesAnalyst.new(engine) }
   describe '#initialize' do
     it 'exists' do
-      expect(sa).to be_a SalesAnalyst
+      expect(analyst).to be_a SalesAnalyst
     end
   end
 
   describe '#average_items_per_merchant' do
-    it 'returns a float for average # of items per merchant' do
-      mr = MerchantRepository.new
-      ir = ItemRepository.new
-      allow(mr).to receive(:all).and_return(['merchant1', 'merchant2',])
-      allow(ir).to receive(:all).and_return(['item1', 'item2', 'item3', 'item4'])
-      
-      expect(sa.average_items_per_merchant(ir,mr)).to eq (2)
+    it 'returns average # of items per merchant' do
+      expect(analyst.average_items_per_merchant).to eq(0)
     end
   end
 
   describe '#average_items_per_merchant_standard_deviation' do
     it 'returns the strd dev for # of items per merchant' do
-      mr = double('MerchantRepo')
-      ir = double('ItemRepo')
-      allow(mr).to receive(:all).and_return(['merchant1', 'merchant2',])
-      allow(ir).to receive(:all).and_return(['item1', 'item2', 'item3', 'item4'])
+      m_repo = double('MerchantRepo')
+      i_repo = double('ItemRepo')
+      allow(m_repo).to receive(:all).and_return(['merchant1', 'merchant2',])
+      allow(i_repo).to receive(:all).and_return(['item1', 'item2', 'item3', 'item4'])
       
       expect(sa.average_items_per_merchant_standard_deviation(ir, mr)).to eq(0)
     end
@@ -38,7 +42,7 @@ RSpec.describe SalesAnalyst do
 
   describe '#merchants_with_high_item_count' do
     it 'returns a list of merchants whose standard deviation of # of items is 1 or greater' do
-    
+
     end
   end
 
