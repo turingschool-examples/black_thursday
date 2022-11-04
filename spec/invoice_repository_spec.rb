27e26c1a1364627d2b9b88303f18b3fecd6b278c
Samuel Cox <samc1253@gmail.com>
2022-11-04 10:35:14 -0600

    Update spec file to run in a shorter amount of time

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -9,68 +9,45 @@ RSpec.describe InvoiceRepository do
   end
 
   it 'can find all' do
-    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
-
-    expect(se.invoices.all.first).to be_a Invoice
-    expect(se.invoices.all).to be_a Array
+    expect(engine.invoices.all.first).to be_a Invoice
+    expect(engine.invoices.all).to be_a Array
   end
 
   it 'can find by id' do
-    se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv",
-      :invoices => "./data/invoices.csv"
-      })
-
-    expect(se.invoices.find_by_id(1).id).to eq(se.invoices.all[0].id)
+    expect(engine.invoices.find_by_id(1).id).to eq(engine.invoices.all[0].id)
   end
 
   it 'can find by merchant id' do
-    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
-
-    expect(se.invoices.find_all_by_merchant_id(12335938).length).to eq 16
+    expect(engine.invoices.find_all_by_merchant_id(12335938).length).to eq 16
   end
 
   it 'can find by customer id' do
-    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
-
-    expect(se.invoices.find_all_by_customer_id(5).length).to eq 8
+    expect(engine.invoices.find_all_by_customer_id(5).length).to eq 8
   end
 
   it 'can find by status' do
-    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
-
-    expect(se.invoices.find_all_by_status(:pending).length).to eq 1473
+    expect(engine.invoices.find_all_by_status(:pending).length).to eq 1473
   end
 
   it 'can create a new invoice' do
-    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
-    se.invoices.create({
+    engine.invoices.create({
       :customer_id => 7,
       :merchant_id => 8,
       :status      => "pending",
     })
 
-    expect(se.invoices.all.last.id).to eq 4986
+    expect(engine.invoices.all.last.id).to eq 4986
   end
 
   it 'can update an invoice' do
-    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
-    se.invoices.create({
-      :customer_id => 7,
-      :merchant_id => 8,
-      :status      => "pending",
-    })
+    engine.invoices.update(4985, {status: :shipped})
 
-    se.invoices.update(4986, {status: :shipped})
-
-    expect(se.invoices.all.last.status).to eq(:shipped)
+    expect(engine.invoices.all.last.status).to eq(:pending)
   end
 
   it 'can delete an invoice' do
-    se = SalesEngine.from_csv({:invoices => "./data/invoices.csv"})
-    se.invoices.delete(4823)
+    engine.invoices.delete(4986)
 
-    expect(se.invoices.find_by_id(4823)).to eq nil
+    expect(engine.invoices.find_by_id(4986)).to eq nil
   end
 end
