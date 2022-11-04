4eb1253a82eae430275fc5ab46d89a30a0171619
Ryan Canton <ryancanton999@gmail.com>
2022-11-04 15:40:06 -0600

    Fixes: Fixed calc error in golden_items

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -10,7 +10,8 @@ require_relative 'invoice_repository'
 
 class SalesAnalyst
   attr_reader :items,
-              :merchants
+              :merchants,
+              :invoices
   def initialize(item_repo = nil, merchant_repo = nil, invoice_repo = nil)
     @items = item_repo
     @merchants = merchant_repo
@@ -65,7 +66,6 @@ class SalesAnalyst
     (array.sum / sellers.count).round(2)
   end
 
-
   def merchants_with_high_item_count
     threshold = average_items_per_merchant + average_items_per_merchant_standard_deviation
     @merchants.all.find_all { |merchant| items_per_merchant(merchant.id) > threshold }
@@ -74,11 +74,9 @@ class SalesAnalyst
   def golden_items
     unit_price_sum = @items.all.map {|item| item.unit_price }.sum
     price_average = unit_price_sum / @items.all.count
-    difference_sum = @items.all.map {|item| price_average - item.unit_price }.sum
-    price_stdrd_dev = Math.sqrt(difference_sum / @items.all.count)
-    # require 'pry'; binding.pry
-
-    @items.all.find_all  {|item|  item.unit_price >= price_average + (price_stdrd_dev * 2)}
+    difference_sum = @items.all.map {|item| (price_average - item.unit_price)**2 }.sum
+    price_stdrd_dev = Math.sqrt((difference_sum / @items.all.count).abs)
+    @items.all.find_all  {|item|  item.unit_price > price_average + (price_stdrd_dev * 2)}
   end
 
 end
