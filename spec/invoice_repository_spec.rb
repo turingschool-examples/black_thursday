0907dbfec8c6b551778a5d3753c035d3f9dbbeca
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-05 12:15:36 -0500

    Add invoice all test

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/invoice_repository'
+require_relative '../lib/invoice'
 
 RSpec.describe InvoiceRepository do
   it 'exists' do
@@ -11,5 +12,22 @@ RSpec.describe InvoiceRepository do
     ivr = InvoiceRepository.new
 
     expect(ivr.data).to eq([])
+  end
+
+  it 'can return all invoices' do
+    ivr = InvoiceRepository.new
+
+    i = Invoice.new(
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => created = Time.now.to_s,
+      :updated_at  => updated = Time.now.to_s
+    )
+
+    ivr.all << i 
+
+    expect(ivr.all).to eq([i])
   end
 end
