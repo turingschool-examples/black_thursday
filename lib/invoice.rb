93be7ab94494cdce6cdbf97b83bbd40c0a4c7ce3
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-04 19:07:12 -0600

    instantiated Invoice

---
+++ lib/invoice.rb
@@ -0,0 +1,15 @@
+require_relative './invoice_repository'
+require 'pry'
+require 'csv'
+
+class Invoice
+  def initialize
+    # @id = id
+    # @customer_id = customer_id
+    # @merchant_id = merchant_id
+    # @status = status
+    # @created_at = created_at
+    # @updated_at = updated_at
+  end
+
+end
--- test/invoice_spec.rb
+++ test/invoice_spec.rb
@@ -2,7 +2,14 @@ require 'simplecov'
 SimpleCov.start
 require 'rspec'
 require 'pry'
+require './lib/invoice'
 
 RSpec.describe Invoice do
+
+  it 'invoice exists and has attributes' do
+   invoice = Invoice.new
+
+   expect(invoice).to be_instance_of(Invoice)
+  end
 
 end
