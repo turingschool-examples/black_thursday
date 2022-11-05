962ee36bb217262fa200d692f1bfd60008b86b78
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-04 20:00:53 -0600

    Test: added more to the attribute test

--- test/invoice_repository_spec.rb
+++ test/invoice_repository_spec.rb
@@ -12,12 +12,18 @@ RSpec.describe InvoiceRepository do
                           customer_id: 1,
                           merchant_id: 12335938,
                           status: "pending",
-                          created_at: "2009-02-07",
-                          updated_at: "2014-03-15"
+                          created_at: Time.now,
+                          updated_at: Time.now
                           })
 
     expect(invoice_repo1).to be_instance_of(InvoiceRepository)
     expect(inv_creation).to be_instance_of(Invoice)
+    expect(inv_creation.id).to eq(1)
+    expect(inv_creation.customer_id).to eq(1)
+    expect(inv_creation.merchant_id).to eq(12335938)
+    expect(inv_creation.status).to eq("pending")
+    expect(inv_creation.created_at).to be_instance_of(Time)
+    expect(inv_creation.updated_at).to be_instance_of(Time)
   end
 
 end
