d996867c4d7c99d2663b7144cb6f619c30859951
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-05 11:42:46 -0500

    Pass invoice exists and attributes tests

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Invoice do
       :customer_id => 7,
       :merchant_id => 8,
       :status      => "pending",
-      :created_at  => Time.now,
-      :updated_at  => Time.now,
+      :created_at  => Time.now.to_s,
+      :updated_at  => Time.now.to_s,
     })
 
     expect(i).to be_a(Invoice)
@@ -20,15 +20,15 @@ RSpec.describe Invoice do
       :customer_id => 7,
       :merchant_id => 8,
       :status      => "pending",
-      :created_at  => created = Time.now,
-      :updated_at  => updated = Time.now,
+      :created_at  => created = Time.now.to_s,
+      :updated_at  => updated = Time.now.to_s,
     })
 
     expect(i.id).to eq(6)
     expect(i.customer_id).to eq(7)
     expect(i.merchant_id).to eq(8)
     expect(i.status).to eq("pending")
-    expect(i.created_at).to eq(created)
-    expect(i.updated_at).to eq(updated)
+    expect(i.created_at).to eq(Time.parse(created))
+    expect(i.updated_at).to eq(Time.parse(updated))
   end
 end
