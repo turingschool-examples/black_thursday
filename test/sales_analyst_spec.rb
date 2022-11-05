71a04fa26c8a5bdc5b411c861c2a53bd2c2c2367
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-05 13:46:39 -0600

    fixed the tests in sales_analyst to include for invoices

--- test/sales_analyst_spec.rb
+++ test/sales_analyst_spec.rb
@@ -7,43 +7,44 @@ RSpec.describe SalesAnalyst do
   let(:sales_engine) do
     SalesEngine.from_csv({
                            items: './data/items.csv',
-                           merchants: './data/merchants.csv'
+                           merchants: './data/merchants.csv',
+                           invoices: './data/invoices.csv'
                          })
     #  let(:sales_analyst) { sales_engine.analyst }
   end
 
-  xit 'exists' do
+  it 'exists' do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst).to be_instance_of(SalesAnalyst)
   end
 
-  xit 'checks average items per merchant' do
+  it 'checks average items per merchant' do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst.average_items_per_merchant).to eq(2.88)
   end
 
-  xit 'checks average items per merchant standard deviation' do
+  it 'checks average items per merchant standard deviation' do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
   end
 
-  xit 'returns merchants with high item counts' do
+  it 'returns merchants with high item counts' do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst.merchants_with_high_item_count).to be_a(Array)
     expect(sales_analyst.merchants_with_high_item_count[0]).to be_a(Merchant)
   end
 
-  xit 'checks average item price for merchant)' do
+  it 'checks average item price for merchant)' do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst.average_item_price_for_merchant(12_334_159)).to be_a(BigDecimal)
   end
 
-  xit '#average average price per merchant' do
+  it '#average average price per merchant' do
     sales_analyst = sales_engine.analyst
     expect(sales_analyst.average_average_price_per_merchant).to be_a(BigDecimal)
   end
 
-  xit '#golden items' do
+  it '#golden items' do
   sales_analyst = sales_engine.analyst
   expect(sales_analyst.golden_items).to be_a(Array)
   expect(sales_analyst.golden_items[0]).to be_a(Item)
--- test/sales_engine_spec.rb
+++ test/sales_engine_spec.rb
@@ -30,10 +30,11 @@ RSpec.describe SalesEngine do
     expect(se.invoices).to be_instance_of(InvoiceRepository)
   end
 
-  xit 'creates a merchant and merchant repository' do
+  it 'creates a merchant and merchant repository' do
     se = SalesEngine.from_csv({
                                 items: './data/items.csv',
-                                merchants: './data/merchants.csv'
+                                merchants: './data/merchants.csv',
+                                invoices: './data/invoices.csv'
                               })
     mr = se.merchants
     merchant = mr.find_by_name('CJsDecor')
@@ -41,10 +42,11 @@ RSpec.describe SalesEngine do
     expect(merchant).to be_instance_of(Merchant)
   end
 
-  xit 'creates item and item repository' do
+  it 'creates item and item repository' do
     se = SalesEngine.from_csv({
                                 items: './data/items.csv',
-                                merchants: './data/merchants.csv'
+                                merchants: './data/merchants.csv',
+                                invoices: './data/invoices.csv'
                               })
     ir   = se.items
     item = ir.find_by_name('disney scrabble frames')
@@ -52,7 +54,7 @@ RSpec.describe SalesEngine do
     expect(item).to be_instance_of(Item)
   end
 
-  xit 'creates invoice and invoice repository' do
+  it 'creates invoice and invoice repository' do
     se = SalesEngine.from_csv({
                                 items: './data/items.csv',
                                 merchants: './data/merchants.csv',
@@ -60,7 +62,7 @@ RSpec.describe SalesEngine do
                               })
     inr   = se.invoices
     invoice = inr.find_by_id(6)
-    expect(inr).to be_instance_of(ItemRepository)
+    expect(inr).to be_instance_of(InvoiceRepository)
     expect(invoice).to be_instance_of(Invoice)
   end
 end
