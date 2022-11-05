242c2d2d2c6b9f8258ab2df4506452f8b5ef17b2
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-04 19:33:02 -0600

    add attributes for test

--- test/invoice_repository_spec.rb
+++ test/invoice_repository_spec.rb
@@ -6,7 +6,14 @@ require 'pry'
 
 RSpec.describe InvoiceRepository do
   it 'exists and has attributes' do
-    invoice_repo = InvoiceRepository.new
+    invoice_repo = InvoiceRepository.new({
+                                        id: 1,
+                                        customer_id: 1,
+                                        merchant_id: 12335938,
+                                        status: "pending",
+                                        created_at: "2009-02-07",
+                                        updated_at: "2014-03-15"
+                                        })
 
     expect(invoice_repo).to be_instance_of(InvoiceRepository)
   end
