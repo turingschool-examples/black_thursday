fc76f43424f200f87c95ff25c927c7640e9a23d8
KaraJH <karas.jones@yahoo.com>
2022-11-04 18:54:20 -0600

    Creates prices method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,4 +1,6 @@
+require_relative '../lib/modules/standard_deviation'
 class SalesAnalyst
+  include StandardDeviation
   attr_reader :engine
   def initialize(engine = nil)
     @engine = engine
@@ -8,27 +10,13 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant_standard_deviation
-    Math.sqrt(divide_diff_and_square_sum).round(2)
+    standard_deviation(item_amount)
   end
 
   def item_amount
     @engine.merchants.all.map do |merchant|
       @engine.find_all_by_merchant_id(merchant.id).length
     end
-  end
-
-  def diff_and_square
-    item_amount.map do |amount|
-      (amount - average_items_per_merchant)**2
-    end
-  end
-
-  def diff_and_square_sum
-    diff_and_square.sum
-  end
-
-  def divide_diff_and_square_sum
-    diff_and_square_sum / (item_amount.length - 1)
   end
 
   def merchants_with_high_item_count
@@ -41,10 +29,7 @@ class SalesAnalyst
   end
 
   def average_item_price_for_merchant(merchant_id)
-    prices = @engine.merchants.find_by_id(merchant_id).items.map do |item|
-      item.unit_price
-    end
-    (prices.sum / prices.length).round(2)
+    (prices(merchant_id).sum / prices(merchant_id).length).round(2)
   end
 
   def average_average_price_per_merchant
@@ -52,5 +37,19 @@ class SalesAnalyst
       average_item_price_for_merchant(merchant.id)
     end
     (averages.sum / averages.length).round(2)
+
   end
+
+  def prices(merchant_id)
+    @engine.merchants.find_by_id(merchant_id).items.map do |item|
+      item.unit_price
+    end
+  end
+
+  def all_merchant_prices
+    @engine.merchants.all.flat_map do |merchant|
+      prices(merchant.id)
+    end
+  end
+
 end
