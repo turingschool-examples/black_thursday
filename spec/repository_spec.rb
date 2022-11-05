f925539fc2900e4e865c98d2dd7c7de1b511714a
Elle Majors <hellolovely@ellemajors>
2022-11-05 11:26:39 -0600

    test: Create #add to repo test

--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -1,12 +1,22 @@
 require_relative '../lib/repository'
 
 RSpec.describe Repository do
+  let(:repository) { Repository.new }
   describe '#initialize' do
     it 'exist' do
-      repository = Repository.new
-
       expect(repository).to be_a(Repository)
     end
   end
-  
+
+  describe '#add to repo' do
+    it 'can add an instance to repo' do 
+      invoice_1 = double("Thing")
+      invoice_2 = double("Thing2")
+
+      repository.add_to_repo(invoice_1)
+      repository.add_to_repo(invoice_2)
+
+      expect(repository.all).to eq([invoice_1, invoice_2])
+    end
+  end
 end
