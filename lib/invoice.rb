05fea3e2da1c971550041b737171373b1f9b48fd
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-04 19:32:22 -0600

    added attr_reader

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -3,13 +3,19 @@ require 'pry'
 require 'csv'
 
 class Invoice
-  def initialize
-    # @id = id
-    # @customer_id = customer_id
-    # @merchant_id = merchant_id
-    # @status = status
-    # @created_at = created_at
-    # @updated_at = updated_at
+  attr_reader :id, 
+              :customer_id, 
+              :merchant_id, 
+              :status, 
+              :created_at, 
+              :updated_at
+  def initialize(info)
+    @id = id
+    @customer_id = customer_id
+    @merchant_id = merchant_id
+    @status = status
+    @created_at = created_at
+    @updated_at = updated_at
   end
 
 end
