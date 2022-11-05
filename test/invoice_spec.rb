3090e53c5b92c637ff27d1aaa4ae6d397c6544d8
Leo Banos Garcia <bsleo625@gmail.com>
2022-11-04 20:56:17 -0600

    Test: for instantiation and attribtues

--- test/invoice_spec.rb
+++ test/invoice_spec.rb
@@ -7,9 +7,22 @@ require './lib/invoice'
 RSpec.describe Invoice do
 
   it 'invoice exists and has attributes' do
-   invoice = Invoice.new
+   invoice = Invoice.new ({
+                           id: 1,
+                           customer_id: 1,
+                           merchant_id: 12335938,
+                           status: "pending",
+                           created_at: Time.now,
+                           updated_at: Time.now
+                          })
 
    expect(invoice).to be_instance_of(Invoice)
+   expect(invoice.id).to eq(1)
+   expect(invoice.customer_id).to eq(1)
+   expect(invoice.merchant_id).to eq(12335938)
+   expect(invoice.status).to eq(:pending)
+   expect(invoice.created_at).to be_instance_of(Time)
+   expect(invoice.updated_at).to be_instance_of(Time)
   end
 
 end
