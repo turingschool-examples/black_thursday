1bf26150d5f2065e6228760bbac4570b9e53c2e4
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-05 12:15:11 -0500

    Pass sales engine invoice repo test

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,13 +2,16 @@ require_relative '../lib/item_repository.rb'
 require_relative '../lib/item.rb'
 require_relative '../lib/merchant_repository.rb'
 require_relative '../lib/merchant.rb'
+require_relative '../lib/invoice_repository.rb'
+require_relative '../lib/invoice.rb'
 
 class SalesEngine
-  attr_reader :items, :merchants
+  attr_reader :items, :merchants, :invoices
 
   def initialize(files)
     @items = ItemRepository.new(files[:items], self)
     @merchants = MerchantRepository.new(files[:merchants], self)
+    @invoices = InvoiceRepository.new(files[:invoices], self)
   end
 
   def self.from_csv(files)
