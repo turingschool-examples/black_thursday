33e39cb784e7dffa8ce3c33be0d7f32cb9065240
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-11-06 15:45:49 -0600

    Add and pass child and all tests for customer repo

--- spec/customer_repository_spec.rb
+++ spec/customer_repository_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/customer_repository'
+require_relative '../lib/customer'
 require_relative '../lib/sales_engine'
 
 RSpec.describe CustomerRepository do
@@ -16,6 +17,12 @@ RSpec.describe CustomerRepository do
       expect(cr.all.length).to eq(10)
     end
 
+    it 'has a child' do
+      cr = CustomerRepository.new
+
+      expect(cr.child).to eq(Customer)
+    end
+
     it 'can have an engine' do
       se = SalesEngine.new(customers: './data/test_data/customers_test.csv')
       cr = CustomerRepository.new('./data/test_data/customers_test.csv', se)
@@ -26,16 +33,10 @@ RSpec.describe CustomerRepository do
   describe '#module methods' do
     describe '#all' do
       it 'returns an array of all customer instances' do
-        cr = CustomerRepository.new
-        c = Customer.new(
-        :id => 6,
-        :first_name => "Joan",
-        :last_name => "Clarke",
-        :created_at => created = Time.now.to_s,
-        :updated_at => updated = Time.now.to_s
-        ) 
+        cr = CustomerRepository.new('./data/test_data/customers_test.csv')
 
-        cr.all << c
+        expect(cr.all.length).to eq(10)
+        expect(cr.all.all?(Customer)).to be true
       end
     end
   end
