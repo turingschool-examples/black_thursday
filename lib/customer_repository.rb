954baa56fd41ec42020668dde92c91b1ef0c8224
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-06 12:12:47 -0700

    Test: add test for delete method

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -20,11 +20,12 @@ class CustomerRepository
   end
 
   def find_all_by_first_name(first_name)
-    @customers.find_all {|customer| customer.first_name == first_name}
+    @customers.find_all { |customer| customer.first_name.upcase.include?(first_name.upcase)}
   end
 
+
   def find_all_by_last_name(last_name)
-    @customers.find_all {|customer| customer.last_name == last_name}
+    @customers.find_all { |customer| customer.last_name.upcase.include?(last_name.upcase)}
   end
 
   def create(attributes)
@@ -42,12 +43,17 @@ class CustomerRepository
   def update(id, attributes)
     if all_ids.include?(id)
       updated_customer = find_by_id(id)
-        if updated_customer.first_name != nil || updated_customer.last_name != nil
+        if attributes[:first_name] != nil 
            updated_customer.update_first_name(attributes[:first_name])
+        end 
+
+        if attributes[:last_name] != nil
            updated_customer.update_last_name(attributes[:last_name])
-           updated_customer.update_time
         end
+        updated_customer.update_time
         updated_customer
     end
   end
+
+  
 end
