9091a319096eb5aae06d02d294cef9997e9d6355
KaraJH <karas.jones@yahoo.com>
2022-11-06 13:41:15 -0700

    Creates test for find all by invoice id

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -46,4 +46,24 @@ RSpec.describe TransactionRepository do
       expect(tr.find_by_id(6)).to eq(t)
       expect(tr.find_by_id(7)).to eq(nil)
   end
+
+  describe '#find_all_by_invoice_id' do
+    it ' returns [] or one or more matches w/ matching invoice id' do
+      tr = TransactionRepository.new
+      t = Transaction.new({
+        :id => 6,
+        :invoice_id => 8,
+        :credit_card_number => "4242424242424242",
+        :credit_card_expiration_date => "0220",
+        :result => "success",
+        :created_at => created = Time.now.to_s,
+        :updated_at => updated = Time.now.to_s
+        })
+
+        tr.all << t
+
+        expect(tr.find_all_by_invoice_id(8)).to eq([t])
+        expect(tr.find_all_by_invoice_id(34)).to eq([])
+    end
+  end
 end
