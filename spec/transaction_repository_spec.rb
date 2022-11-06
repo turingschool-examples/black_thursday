18d84603947249e84c0985969160f29d02ee10dc
KaraJH <karas.jones@yahoo.com>
2022-11-06 13:32:40 -0700

    Creates test for all method

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe TransactionRepository do
 
     expect(tr).to be_a(TransactionRepository)
   end
+
+  describe '#all' do
+    it 'returns all transaction instances' do
+      tr = TransactionRepository.new
+      t = Transaction.new({
+        :id => 6,
+        :invoice_id => 8,
+        :credit_card_number => "4242424242424242",
+        :credit_card_expiration_date => "0220",
+        :result => "success",
+        :created_at => created = Time.now.to_s,
+        :updated_at => updated = Time.now.to_s
+        })
+
+      expect(tr.all).to eq([])
+      
+      tr.all << t
+
+      expect(tr.all).to eq([t])
+    end
+  end
 end
