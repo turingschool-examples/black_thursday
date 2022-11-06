48acf70624558a1575239c94b937c29845454cfc
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-06 14:08:48 -0600

    Have the unit_price test passing

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -4,12 +4,14 @@ class InvoiceItem
   attr_reader :id,
               :item_id,
               :invoice_id,
-              :quantity
+              :quantity,
+              :unit_price
 
   def initialize(attributes)
     @id         = attributes[:id]
     @item_id    = attributes[:item_id]
     @invoice_id = attributes[:invoice_id]
     @quantity   = attributes[:quantity]
+    @unit_price = attributes[:unit_price]
   end
 end
