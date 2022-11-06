fc90adc4b53fb358986f6f379e9b74c4ca7f71dc
KaraJH <karas.jones@yahoo.com>
2022-11-06 13:45:19 -0700

    Creates find all by credit card number test

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -66,4 +66,24 @@ RSpec.describe TransactionRepository do
         expect(tr.find_all_by_invoice_id(34)).to eq([])
     end
   end
+
+  describe '#find_all_by_credit_card_number' do
+    it ' finds all transactions by matching id or returns empty []' do
+    tr = TransactionRepository.new
+    t = Transaction.new({
+      :id => 6,
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at => created = Time.now.to_s,
+      :updated_at => updated = Time.now.to_s
+      })
+
+      tr.all << t
+
+      expect(tr.find_all_by_credit_card_number("4242424242424242")).to eq([t])
+      expect(tr.find_all_by_credit_card_number("0000123")).to eq([])
+    end
+  end
 end
