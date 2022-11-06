270608a4362fd7f721238ccb9630db5c8f9de687
Brandon Gray <brandonjoe88@hotmail.com>
2022-11-06 12:56:08 -0700

    feat: adds test for month to int

--- spec/timeable_spec.rb
+++ spec/timeable_spec.rb
@@ -16,10 +16,18 @@ describe Timeable do
   end
 
   describe '#convert_int_to_day' do
-    it 'converts a number from day of week string' do
+    it 'converts a number index value to a day of week string' do
       expect(@dummy.convert_int_to_day(@convert1)).to eq('Tuesday')
       expect(@dummy.convert_int_to_day(@convert2)).to eq(nil)
       expect(@dummy.convert_int_to_day(@convert3)).to eq('Friday')
+    end
+  end
+
+  describe '#month_to_int' do
+    it 'converts a string representing month of the year to integer index value' do
+      expect(@dummy.month_to_int('September')).to eq(8)
+      expect(@dummy.month_to_int('March')).to eq(2)
+      expect(@dummy.month_to_int('Banana')).to eq(nil)
     end
   end
 end
