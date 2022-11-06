7f4692c4d8432ac7e93ce619459edfbb703000ad
KaraJH <karas.jones@yahoo.com>
2022-11-06 13:48:42 -0700

    Creates test for find all by result method

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -86,4 +86,24 @@ RSpec.describe TransactionRepository do
       expect(tr.find_all_by_credit_card_number("0000123")).to eq([])
     end
   end
+
+  describe '#find_all_by_result' do
+    it 'finds all the tranactions by a matching result or returns empty []' do
+      tr = TransactionRepository.new
+      t = Transaction.new({
+        :id => 6,
+        :invoice_id => 8,
+        :credit_card_number => "4242424242424242",
+        :credit_card_expiration_date => "0220",
+        :result => "success",
+        :created_at => created = Time.now.to_s,
+        :updated_at => updated = Time.now.to_s
+        })
+
+        tr.all << t
+
+        expect(tr.find_all_by_result("success")).to eq([t])
+        expect(tr.find_all_by_result("not success")).to eq([])
+    end
+  end
 end
