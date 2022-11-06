59308a5d6a8c4f44888339e239a8b0e866be8bee
KaraJH <karas.jones@yahoo.com>
2022-11-06 13:41:28 -0700

    Creates method for find all by invoice id with passing test

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -6,4 +6,10 @@ class TransactionRepository
     @data = []
     @engine = engine
   end
+
+  def find_all_by_invoice_id(invoice_id)
+    all.find_all do |transaction|
+      transaction.invoice_id == invoice_id
+    end
+  end
 end
