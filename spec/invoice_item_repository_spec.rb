1c784638e6f03ac2b3b81a798f9d8be406ebbf6f
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-06 16:11:42 -0600

    Add test for invoice item repository

--- spec/invoice_item_repository_spec.rb
+++ spec/invoice_item_repository_spec.rb
@@ -17,4 +17,41 @@ RSpec.describe InvoiceItemRepository do
     expect(invoice_items.data).to eq []
   end
 
+  it 'can find an invoice items' do
+    invoice_items = InvoiceItemRepository.new
+
+    expect(invoice_items.data).to eq []
+  end
+
+  it 'can find an invoice id' do
+    ii = InvoiceItem.new({
+      :id => 6,
+      :item_id => 7,
+      :invoice_id => 8,
+      :quantity => 1,
+      :unit_price => BigDecimal(10.99, 4),
+      :created_at => Time.now,
+      :updated_at => Time.now
+      })
+    invoice_items = InvoiceItemRepository.new
+    invoice_items.all << ii
+
+    expect(invoice_items.find_by_id(6)).to eq (ii)
+  end
+
+  it 'can find all invoices by item id' do
+    ii = InvoiceItem.new({
+      :id => 6,
+      :item_id => 7,
+      :invoice_id => 8,
+      :quantity => 1,
+      :unit_price => BigDecimal(10.99, 4),
+      :created_at => Time.now,
+      :updated_at => Time.now
+      })
+    invoice_items = InvoiceItemRepository.new
+    invoice_items.all << ii
+
+    expect(invoice_items.find_by_id(6)).to eq (ii)
+
 end
--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -113,4 +113,18 @@ RSpec.describe InvoiceItem do
 
     expect(ii.updated_at).to be < Time.now
   end
+
+  xit 'can convert unit price to dollars' do
+    ii = InvoiceItem.new({
+  :id => 6,
+  :item_id => 7,
+  :invoice_id => 8,
+  :quantity => 1,
+  :unit_price => BigDecimal(10.99, 4),
+  :created_at => Time.now,
+  :updated_at => Time.now
+    })
+
+    expect(ii.unit_price_to_dollars).to be()
+  end
 end
