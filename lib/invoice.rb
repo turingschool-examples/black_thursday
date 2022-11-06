0650afbb2678b1279e43269b7d9e7915a4b6ec37
Joe King <j.king92@gmail.com>
2022-11-06 14:48:14 -0700

    fix: Change data types of IDs to integer

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -1,7 +1,7 @@
 require_relative 'make_time'
 
-class Invoice 
-  include MakeTime 
+class Invoice
+  include MakeTime
 
   attr_reader :id,
               :customer_id,
@@ -11,10 +11,10 @@ class Invoice
               :updated_at
 
   def initialize(invoice_data)
-    @id = invoice_data[:id]
-    @customer_id = invoice_data[:customer_id]
-    @merchant_id = invoice_data[:merchant_id]
-    @status = invoice_data[:status]
+    @id = invoice_data[:id].to_i
+    @customer_id = invoice_data[:customer_id].to_i
+    @merchant_id = invoice_data[:merchant_id].to_i
+    @status = invoice_data[:status].to_sym
     @created_at = return_time_from(invoice_data[:created_at])
     @updated_at = return_time_from(invoice_data[:updated_at])
   end
