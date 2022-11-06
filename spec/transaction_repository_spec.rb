f3ec3e5331dd07ccbbc96c9bc046b2fa19e0b168
KaraJH <karas.jones@yahoo.com>
2022-11-06 13:36:18 -0700

    Creates find by id test

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -22,10 +22,28 @@ RSpec.describe TransactionRepository do
         })
 
       expect(tr.all).to eq([])
-      
+
       tr.all << t
 
       expect(tr.all).to eq([t])
     end
+  end
+
+  it 'finds the transaction instance by id' do
+    tr = TransactionRepository.new
+    t = Transaction.new({
+      :id => 6,
+      :invoice_id => 8,
+      :credit_card_number => "4242424242424242",
+      :credit_card_expiration_date => "0220",
+      :result => "success",
+      :created_at => created = Time.now.to_s,
+      :updated_at => updated = Time.now.to_s
+      })
+
+      tr.all << t
+
+      expect(tr.find_by_id(6)).to eq(t)
+      expect(tr.find_by_id(7)).to eq(nil)
   end
 end
