fb7bc4f4bbd3169649a087c6e7f86e9b8497b22c
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-06 13:56:06 -0600

    Write test for id

---
+++ spec/invoice_item_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../lib/invoice_item.rb'
+
+RSpec.describe InvoiceItem do
+
+  it 'exists' do
+    ii = InvoiceItem.new({
+  :id => 6,
+  :item_id => 7,
+  :invoice_id => 8,
+  :quantity => 1,
+  :unit_price => BigDecimal(10.99, 4),
+  :created_at => Time.now,
+  :updated_at => Time.now
+    })
+
+    expect(ii).to be_a(InvoiceItem)
+  end
+
+  it 'has an id' do
+    ii = InvoiceItem.new({
+  :id => 6,
+  :item_id => 7,
+  :invoice_id => 8,
+  :quantity => 1,
+  :unit_price => BigDecimal(10.99, 4),
+  :created_at => Time.now,
+  :updated_at => Time.now
+    })
+
+    expect(ii.id).to eq(6)
+  end
+end
