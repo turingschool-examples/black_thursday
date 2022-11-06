1b0796c67cbcc6e45151abb82fa30c5ec1819fcd
KaraJH <karas.jones@yahoo.com>
2022-11-06 13:01:16 -0700

    Fixes syntax error in test

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Transaction do
       :credit_card_number => "4242424242424242",
       :credit_card_expiration_date => "0220",
       :result => "success",
-      :created_at => Time.now,
-      :updated_at => Time.now
+      :created_at => created = Time.now.to_s,
+      :updated_at => updated = Time.now.to_s
       })
 
     expect(t).to be_a(Transaction)
@@ -19,7 +19,7 @@ RSpec.describe Transaction do
     expect(t.credit_card_number).to eq("4242424242424242")
     expect(t.credit_card_expiration_date).to eq("0220")
     expect(t.result).to eq("success")
-    expect(t.created_at).to eq(Time.now)
-    expect(t.updated_at).to eq(Time.now)
+    expect(t.created_at).to eq(Time.parse(created))
+    expect(t.updated_at).to eq(Time.parse(updated))
   end
 end
