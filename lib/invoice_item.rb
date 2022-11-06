fbf8d202b465c1efa08a8d54250cec3af2d30141
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-06 14:05:57 -0600

    Have quantity test passing

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -3,11 +3,13 @@ require 'bigdecimal'
 class InvoiceItem
   attr_reader :id,
               :item_id,
-              :invoice_id
+              :invoice_id,
+              :quantity
 
   def initialize(attributes)
-    @id      = attributes[:id]
-    @item_id = attributes[:item_id]
+    @id         = attributes[:id]
+    @item_id    = attributes[:item_id]
     @invoice_id = attributes[:invoice_id]
+    @quantity   = attributes[:quantity]
   end
 end
