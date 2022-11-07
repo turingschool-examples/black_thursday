2af53446d4ee2e1257d90a374052aaa7303e70ce
Elle Majors <hellolovely@ellemajors>
2022-11-06 19:39:18 -0700

    feat: #find_all_by_invoice_id

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,5 +1,9 @@
 require_relative 'repository'
 
 class TransactionRepository < Repository
- 
+  def find_all_by_invoice_id(invoice_id)
+    @all.find_all do |item|
+      item.invoice_id == invoice_id
+    end
+  end
 end
