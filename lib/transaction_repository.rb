95bc6c837ce135d67700dd509ddc0dc1c325d836
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-06 19:48:32 -0800

    Feature: find all by invoice id

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -2,8 +2,8 @@ require_relative 'repository'
 require_relative 'transaction'
 
 class TransactionRepository < Repository
-    # def initialize
-    # @repo = []
-    # end
+  def find_all_by_invoice_id(id)
+    @repo.select { |invoice_item| invoice_item.invoice_id == id }
+  end
 
 end
