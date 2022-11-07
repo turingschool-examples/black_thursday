65ed43dd67f51723291f54998be063ec46df5176
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-06 20:08:50 -0800

    Feat: update

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -2,6 +2,10 @@ require_relative 'repository'
 require_relative 'transaction'
 
 class TransactionRepository < Repository
+  def new_obj(attributes)
+    new_obj_class(attributes, Transaction)
+  end
+
   def find_all_by_invoice_id(id)
     @repo.select { |invoice_item| invoice_item.invoice_id == id }
   end
