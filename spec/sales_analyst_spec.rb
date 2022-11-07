297207a0e477d1d1bd2e66086cd14d62006c8cdd
Braxton Eddings <beddings81@gmail.com>
2022-11-07 10:27:45 -0600

    Add test for invoice paid in full method

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -277,4 +277,20 @@ RSpec.describe SalesAnalyst do
     expect(sales_analyst.invoice_status(:shipped)).to eq(9.52)
     expect(sales_analyst.invoice_status(:returned)).to eq(14.29)
   end
+
+  it 'can check if an invoice is paid in full' do
+    sales_engine = SalesEngine.from_csv(
+
+      :items     => './data/test_data/items_test.csv',
+      :merchants => './data/test_data/merchant_invoices_test2.csv',
+      :invoices  => './data/test_data/invoices_transactions_test.csv',
+      :invoice_items => './data/invoice_items.csv',
+      :transactions => './data/test_data/transactions_test.csv',
+      :customers => './data/customers.csv'
+    )
+    sales_analyst = sales_engine.analyst
+
+    expect(sales_analyst.invoice_paid_in_full?(1)).to be(true)
+    expect(sales_analyst.invoice_paid_in_full?(2)).to be(false)
+  end
 end
