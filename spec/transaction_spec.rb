d312c83f8d5b0ee2ea01661445a55ffb3642d069
Elle Majors <hellolovely@ellemajors>
2022-11-06 21:21:14 -0700

    update: hash and string syntax

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,21 +1,21 @@
 require_relative '../lib/transaction'
 
 RSpec.describe Transaction do
-  let(:t_1) { Transaction.new({ :id => 6,
-                  :invoice_id => 8,
-                  :credit_card_number => "4242424242424242",
-                  :credit_card_expiration_date => "0220",
-                  :result => "success",
-                  :created_at => Time.now,
-                  :updated_at => Time.now
+  let(:t_1) { Transaction.new({ id: 6,
+                  invoice_id: 8,
+                  credit_card_number: '4242424242424242',
+                  credit_card_expiration_date: '0220',
+                  result: 'success',
+                  created_at: Time.now,
+                  updated_at: Time.now
                 }) }
-  let(:t_2) { Transaction.new({ :id => 1, 
-                  :invoice_id => 2,
-                  :credit_card_number => "4242424242424243",
-                  :credit_card_expiration_date => "0221",
-                  :result => "failed",
-                  :created_at => Time.now,
-                  :updated_at => Time.now
+  let(:t_2) { Transaction.new({ id 1, 
+                  invoice_id: 2,
+                  credit_card_number: '4242424242424243',
+                  credit_card_expiration_date: '0221',
+                  result: 'failed',
+                  created_at: Time.now,
+                  updated_at: Time.now
                 }) }  
  
 describe '#initialize' do
@@ -41,22 +41,22 @@ describe '#id' do
 
   describe '#credit_card_number' do 
     it 'returns matches of credit card number' do 
-      expect(t_1.credit_card_number).to eq("4242424242424242")
-      expect(t_2.credit_card_number).to eq("4242424242424243")
+      expect(t_1.credit_card_number).to eq('4242424242424242')
+      expect(t_2.credit_card_number).to eq('4242424242424243')
     end
   end
 
   describe '#credit_card_expiration_date' do 
     it 'returns matches of credit card expiration date' do 
-      expect(t_1.credit_card_expiration_date).to eq("0220")
-      expect(t_2.credit_card_expiration_date).to eq("0221")
+      expect(t_1.credit_card_expiration_date).to eq('0220')
+      expect(t_2.credit_card_expiration_date).to eq('0221')
     end
   end
 
   describe '#result' do 
     it 'returns the result' do 
-      expect(t_1.result).to eq("success")
-      expect(t_2.result).to eq("failed")
+      expect(t_1.result).to eq('success')
+      expect(t_2.result).to eq('failed')
     end
   end  
 
