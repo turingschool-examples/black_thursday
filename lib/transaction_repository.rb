810e4be55d1217e8619cdd4abc8bd748f9b6f475
Elle Majors <hellolovely@ellemajors>
2022-11-06 20:14:06 -0700

    feat: #find_all_by_result and update find_all_by_credit_card_number

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -8,8 +8,14 @@ class TransactionRepository < Repository
   end
   
   def find_all_by_credit_card_number(credit_card_number)
-    @all.find_all do |item|
-      item.credit_card_number == credit_card_number
+    @all.find_all do |number|
+      number.credit_card_number == credit_card_number
+    end
+  end
+
+  def find_all_by_result(result)
+    @all.select do |results|
+      results.result == result
     end
   end
 end
