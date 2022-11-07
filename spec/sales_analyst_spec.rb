712c2fba6254c9a777fecf935bd1e72b4bcd6fbb
Ryan Canton <ryancanton999@gmail.com>
2022-11-07 11:49:38 -0700

    Feat: Add invoice_paid_in_full? method to class SalesAnalyst

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -241,21 +241,21 @@ RSpec.describe SalesAnalyst do
                         :invoice_id => 1,
                         :credit_card_number => "2424242424242424",
                         :credit_card_expiration_date => "0424",
-                        :result => "success",
+                        :result => :success,
                         :created_at => Time.now,
                         :updated_at => Time.now})}
     let (:transaction_2) {Transaction.new({:id => 2,
                         :invoice_id => 2,
                         :credit_card_number => "2424242424242424",
                         :credit_card_expiration_date => "0424",
-                        :result => "failed",
+                        :result => :failed,
                         :created_at => Time.now,
                         :updated_at => Time.now})}
     let (:transaction_3) {Transaction.new({:id => 3,
                         :invoice_id => 3,
                         :credit_card_number => "2424242424242424",
                         :credit_card_expiration_date => "0424",
-                        :result => "failed",
+                        :result => :failed,
                         :created_at => Time.now,
                         :updated_at => Time.now})}
     let (:transactions) {[transaction_1, transaction_2, transaction_3]}
@@ -267,5 +267,7 @@ RSpec.describe SalesAnalyst do
       expect(sales_analyst.invoice_paid_in_full?(2)).to eq(false)
       expect(sales_analyst.invoice_paid_in_full?(3)).to eq(false)
     end
+
+    
   end
 end
