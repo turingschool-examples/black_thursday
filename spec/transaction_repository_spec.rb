9398c13230e3b0de00a8ea6abe7124d9ec99e51b
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-06 19:39:41 -0800

    Test: all

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -2,18 +2,20 @@ require './lib/transaction_repository'
 require './lib/sales_engine'
 
 RSpec.describe TransactionRepository do
-  it 'exists' do
-    sales_engine = SalesEngine.from_csv({
+    let(:sales_engine) {
+        SalesEngine.from_csv({
         items: './data/items.csv',
         merchants: './data/merchants.csv',
         invoices: './data/invoices.csv',
         invoice_items: './data/invoice_items.csv',
         transactions: "./data/transactions.csv"})
+    }
+  it 'exists' do
     transaction = sales_engine.transactions.find_by_id(6)
     expect(transaction.id).to eq(6)
   end
 
   it '#all' do
-  expect(transactions.all).to be_a(Array)
+  expect(sales_engine.transactions.all).to be_a(Array)
 end
 end
