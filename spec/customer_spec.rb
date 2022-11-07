8da12822569519164f6cd133fffb1fd1cbe55fd4
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-11-06 20:56:52 -0700

    feat: Add initialize with passing tests

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,9 +2,23 @@ require_relative '../lib/customer'
 
 RSpec.describe Customer do
   let(:customer_1) { Customer.new({
-    :id => 6,
-    :first_name => 'Joan',
-    :last_name => 'Clarke',
+    id: 6,
+    first_name: 'Joan',
+    last_name: 'Clarke',
+    created_at:  Time.now,
+    updated_at:  Time.now }) }
+
+  let(:customer_2) { Customer.new({
+    :id => 2,
+    :first_name => 'Michael',
+    :last_name => 'Jackson',
+    :created_at => Time.now,
+    :updated_at => Time.now }) }
+
+  let(:customer_3) { Customer.new({
+    :id => 8,
+    :first_name => 'Argo',
+    :last_name => 'Angus',
     :created_at => Time.now,
     :updated_at => Time.now }) }
 
