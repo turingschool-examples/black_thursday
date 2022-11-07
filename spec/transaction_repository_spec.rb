5454f7486008f645bb290410aeb9bc2e0f247ab2
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-06 19:44:27 -0800

    Test: find_by_id

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -2,20 +2,29 @@ require './lib/transaction_repository'
 require './lib/sales_engine'
 
 RSpec.describe TransactionRepository do
-    let(:sales_engine) {
+    let(:transactions) { 
+        # sales_engine =
         SalesEngine.from_csv({
         items: './data/items.csv',
         merchants: './data/merchants.csv',
         invoices: './data/invoices.csv',
         invoice_items: './data/invoice_items.csv',
-        transactions: "./data/transactions.csv"})
+        transactions: "./data/transactions.csv"}).transactions
+
     }
+    
   it 'exists' do
-    transaction = sales_engine.transactions.find_by_id(6)
+    transaction = transactions.find_by_id(6)
     expect(transaction.id).to eq(6)
   end
 
   it '#all' do
-  expect(sales_engine.transactions.all).to be_a(Array)
-end
+    expect(transactions.all).to be_a(Array)
+  end
+
+  it '#find_by_id' do
+  expect(transactions.find_by_id(0)).to eq(nil)
+  expect(transactions.find_by_id(4)).to be_instance_of(Transaction)
+  
+  end
 end
