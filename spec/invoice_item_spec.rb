762d5cc23917f171622e398809fa0e8028c2b64f
Kelsie Porter <krporter22@gmail.com>
2022-11-06 18:47:15 -0700

    Create attribute tests

--- spec/invoice_item_spec.rb
+++ spec/invoice_item_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/invoice_item'
 require 'bigdecimal'
 
 RSpec.describe InvoiceItem do 
-  let(:invoice_item) { InvoiceItem.new(
+  let(:invoice_item) { InvoiceItem.new({
                       :id => 6,
                       :item_id => 7,
                       :invoice_id => 8,
@@ -10,11 +10,34 @@ RSpec.describe InvoiceItem do
                       :unit_price => BigDecimal("1099",4),
                       :created_at => Time.now,
                       :updated_at => Time.now
-                    )}
+                    })}
 
   describe '#initialize' do
     it 'exists' do
       expect(invoice_item).to be_a(InvoiceItem)
     end
-  end 
+  end
+
+  describe 'attributes' do 
+    it '#id' do 
+      expect(invoice_item.id).to eq(6)
+    end
+
+    it '#item_id' do 
+      expect(invoice_item.item_id).to eq(7)
+    end
+
+    it '#invoice_id' do 
+      expect(invoice_item.invoice_id).to eq(8)
+    end
+
+    it '#quantity' do 
+      expect(invoice_item.quantity).to eq(1)
+    end
+
+    it '#unit_price' do 
+      expect(invoice_item.unit_price).to eq(10.99)
+    end
+  end
+
 end
