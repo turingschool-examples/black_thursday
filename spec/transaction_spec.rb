f5b475bd31fad9a69773953c9a5fe289da37541e
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-08 19:22:04 -0600

    Write test for time converter method

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -23,9 +23,18 @@ RSpec.describe Transaction do
     expect(t.updated_at).to eq(Time.parse(updated))
   end
 
-  xit 'can convert a time' do
-    time = "2022-11-08 18:04:14.248601 -0600"
+  it 'can convert a time' do
+      t = Transaction.new({
+        :id => 6,
+        :invoice_id => 8,
+        :credit_card_number => "4242424242424242",
+        :credit_card_expiration_date => "0220",
+        :result => "success",
+        :created_at => created = Time.now.to_s,
+        :updated_at => updated = Time.now.to_s
+        })
 
-    expect()
+    expect(t.created_at).to be_a(Time)
+    expect(t.updated_at).to be_a(Time)
   end
 end
