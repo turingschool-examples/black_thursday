9aece333196c4c665353c169d22adb0ef474b938
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-11-08 19:26:19 -0600

    Add test for time converter method for the invoice spec

--- spec/invoice_spec.rb
+++ spec/invoice_spec.rb
@@ -31,4 +31,18 @@ RSpec.describe Invoice do
     expect(i.created_at).to eq(Time.parse(created))
     expect(i.updated_at).to eq(Time.parse(updated))
   end
+
+  it 'can convert a time' do
+    i = Invoice.new(
+      :id          => 6,
+      :customer_id => 7,
+      :merchant_id => 8,
+      :status      => "pending",
+      :created_at  => created = Time.now.to_s,
+      :updated_at  => updated = Time.now.to_s
+    )
+
+    expect(i.created_at).to be_a(Time)
+    expect(i.updated_at).to be_a(Time)
+  end
 end
